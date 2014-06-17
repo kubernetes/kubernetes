c27ad1390d9371a948f096dbb9dfbc19e1042d82
Daniel Smith <dbsmith@google.com>
2014-06-16 19:27:59 -0700

    etcd does some trickery that was avoiding the pid capturing in our tests. Run in subshell.

--- hack/integration-test.sh
+++ hack/integration-test.sh
@@ -27,7 +27,7 @@ $(dirname $0)/build-go.sh
 ETCD_DIR=$(mktemp -d -t kube-integration.XXXXXX)
 trap "rm -rf ${ETCD_DIR}" EXIT
 
-etcd -name test -data-dir ${ETCD_DIR} > /tmp/etcd.log &
+(etcd -name test -data-dir ${ETCD_DIR} > /tmp/etcd.log) &
 ETCD_PID=$!
 
 sleep 5
