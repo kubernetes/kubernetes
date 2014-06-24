2c404aba0f09cd54440951b9797ee4861afb6c83
Brendan Burns <bburns@google.com>
2014-06-24 15:02:48 -0700

    Fixed the boilerplate hook to work on OS X, where apparently wc add's spaces

--- hooks/boilerplate.sh
+++ hooks/boilerplate.sh
@@ -2,8 +2,8 @@
 
 # Print 1 if the file in $1 has the correct boilerplate header, 0 otherwise.
 FILE=$1
-LINES=$(cat "$(dirname $0)/boilerplate.txt" | wc -l)
-DIFFER=$(head -$LINES "${FILE}" | diff -q - "$(dirname $0)/boilerplate.txt")
+LINES=$(cat "$(dirname $0)/boilerplate.txt" | wc -l | tr -d ' ')
+DIFFER=$(head "-${LINES}" "${FILE}" | diff -q - "$(dirname $0)/boilerplate.txt")
 
 if [[ -z "${DIFFER}" ]]; then
   echo "1"
