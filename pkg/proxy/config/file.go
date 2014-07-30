99461ff3b8d22c6414a6b17ad4d9ef90fbd7af58
Michal Fojtik <mfojtik@redhat.com>
2014-07-30 13:33:27 +0200

    Use fsnotify to watch changes in live proxy_config file

--- pkg/proxy/config/file.go
+++ pkg/proxy/config/file.go
@@ -32,15 +32,16 @@ limitations under the License.
 package config
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"reflect"
-	"time"
 
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/util"
 	"github.com/golang/glog"
+	fsnotify "gopkg.in/fsnotify.v0"
 )
 
 // serviceConfig is a deserialized form of the config file format which ConfigSourceFile accepts.
@@ -62,7 +63,7 @@ type ConfigSourceFile struct {
 // NewConfigSourceFile creates a new ConfigSourceFile and let it immediately runs the created ConfigSourceFile in a goroutine.
 func NewConfigSourceFile(filename string, serviceChannel chan ServiceUpdate, endpointsChannel chan EndpointsUpdate) ConfigSourceFile {
 	config := ConfigSourceFile{
-		filename:         filename,
+		filename:         filepath.Clean(filename),
 		serviceChannel:   serviceChannel,
 		endpointsChannel: endpointsChannel,
 	}
@@ -72,59 +73,84 @@ func NewConfigSourceFile(filename string, serviceChannel chan ServiceUpdate, end
 
 // Run begins watching the config file.
 func (s ConfigSourceFile) Run() {
-	glog.Infof("Watching file %s", s.filename)
-	var lastData []byte
-	var lastServices []api.Service
-	var lastEndpoints []api.Endpoints
 
-	sleep := 5 * time.Second
-	// Used to avoid spamming the error log file, makes error logging edge triggered.
-	hadSuccess := true
-	for {
-		data, err := ioutil.ReadFile(s.filename)
-		if err != nil {
-			msg := fmt.Sprintf("Couldn't read file: %s : %v", s.filename, err)
-			if hadSuccess {
-				glog.Error(msg)
-			} else {
-				glog.V(1).Info(msg)
-			}
-			hadSuccess = false
-			time.Sleep(sleep)
-			continue
-		}
-		hadSuccess = true
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		glog.Errorf("Error initializing filesystem watcher for %s: %v", s.filename, err)
+		return
+	}
 
-		if bytes.Equal(lastData, data) {
-			time.Sleep(sleep)
-			continue
-		}
-		lastData = data
+	defer watcher.Close()
 
-		config := &serviceConfig{}
-		if err = json.Unmarshal(data, config); err != nil {
-			glog.Errorf("Couldn't unmarshal configuration from file : %s %v", data, err)
-			continue
-		}
-		// Ok, we have a valid configuration, send to channel for
-		// rejiggering.
-		newServices := make([]api.Service, len(config.Services))
-		newEndpoints := make([]api.Endpoints, len(config.Services))
-		for i, service := range config.Services {
-			newServices[i] = api.Service{JSONBase: api.JSONBase{ID: service.Name}, Port: service.Port}
-			newEndpoints[i] = api.Endpoints{JSONBase: api.JSONBase{ID: service.Name}, Endpoints: service.Endpoints}
-		}
-		if !reflect.DeepEqual(lastServices, newServices) {
-			serviceUpdate := ServiceUpdate{Op: SET, Services: newServices}
-			s.serviceChannel <- serviceUpdate
-			lastServices = newServices
-		}
-		if !reflect.DeepEqual(lastEndpoints, newEndpoints) {
-			endpointsUpdate := EndpointsUpdate{Op: SET, Endpoints: newEndpoints}
-			s.endpointsChannel <- endpointsUpdate
-			lastEndpoints = newEndpoints
-		}
+	if _, err := os.Stat(s.filename); err == nil {
+		s.updateConfig()
+	}
 
-		time.Sleep(sleep)
+	go util.Forever(func() { fileWatcher(&s, watcher) }, 1)
+
+	glog.Infof("Watching %s configuration file", s.filename)
+	if err := watcher.Add(filepath.Dir(s.filename)); err != nil {
+		glog.Fatalf("Error watching the %s file: %v", s.filename, err)
+	}
+
+	select {}
+}
+
+func fileWatcher(s *ConfigSourceFile, w *fsnotify.Watcher) {
+	select {
+	case ev := <-w.Events:
+		if ev.Name != s.filename {
+			return
+		}
+		switch ev.Op {
+		case fsnotify.Write:
+			s.updateConfig()
+		case fsnotify.Remove:
+			s.unloadConfig()
+		}
+	case err := <-w.Errors:
+		glog.Infof("Config file watcher for %s failed: %v", s.filename, err)
+	}
+}
+
+func (s ConfigSourceFile) unloadConfig() {
+	glog.Infof("Unloading configuration for %s", s.filename)
+	s.serviceChannel <- ServiceUpdate{Op: SET}
+	s.endpointsChannel <- EndpointsUpdate{Op: SET}
+}
+
+func (s ConfigSourceFile) updateConfig() {
+	var (
+		lastServices  []api.Service
+		lastEndpoints []api.Endpoints
+	)
+	data, err := ioutil.ReadFile(s.filename)
+	if err != nil {
+		glog.Errorf("Unable to read the configuration file %s: %v", s.filename, err)
+		return
+	}
+	config := &serviceConfig{}
+	if err = json.Unmarshal(data, config); err != nil {
+		glog.Errorf("Couldn't unmarshal configuration from file : %s %v", data, err)
+		return
+	}
+	// Ok, we have a valid configuration, send to channel for
+	// rejiggering.
+	glog.Infof("Updating configuration for %s", s.filename)
+	newServices := make([]api.Service, len(config.Services))
+	newEndpoints := make([]api.Endpoints, len(config.Services))
+	for i, service := range config.Services {
+		newServices[i] = api.Service{JSONBase: api.JSONBase{ID: service.Name}, Port: service.Port}
+		newEndpoints[i] = api.Endpoints{JSONBase: api.JSONBase{ID: service.Name}, Endpoints: service.Endpoints}
+	}
+	if !reflect.DeepEqual(lastServices, newServices) {
+		serviceUpdate := ServiceUpdate{Op: SET, Services: newServices}
+		s.serviceChannel <- serviceUpdate
+		lastServices = newServices
+	}
+	if !reflect.DeepEqual(lastEndpoints, newEndpoints) {
+		endpointsUpdate := EndpointsUpdate{Op: SET, Endpoints: newEndpoints}
+		s.endpointsChannel <- endpointsUpdate
+		lastEndpoints = newEndpoints
 	}
 }
