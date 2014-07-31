7ac85cbfe86af3c761f6ba78f8b7b89ed04d2125
Dan Mace <ironcladlou@gmail.com>
2014-07-31 11:58:24 -0400

    WIP job collapse into BuildController

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -374,39 +374,14 @@ type WatchEvent struct {
 	Object APIObject
 }
 
-// Job is a task that can be scheduled.
-type Job struct {
-	JSONBase `json:",inline" yaml:",inline"`
-	Status   JobStatus `json:"status,omitempty" yaml:"status,omitempty"`
-	Success  bool      `json:"success,omitempty" yaml:"success,omitempty"`
-	Pod      Pod       `json:"pod,omitempty" yaml:"pod,omitempty"`
-	PodID    string    `json:"podID,omitempty" yaml:"podID,omitempty"`
-}
-
-// JobStatus represents the status of a Job at a point in time.
-type JobStatus string
-
-const (
-	JobNew      JobStatus = "new"
-	JobPending  JobStatus = "pending"
-	JobRunning  JobStatus = "running"
-	JobComplete JobStatus = "complete"
-)
-
-// JobList is a collection of Jobs.
-type JobList struct {
-	JSONBase `json:",inline" yaml:",inline"`
-	Items    []Job `json:"items,omitempty" yaml:"items,omitempty"`
-}
-
 // Build encapsulates the inputs needed to produce a new deployable image, as well as
 // the status of the operation and a reference to the Job which runs the build.
 type Build struct {
-	JSONBase `json:",inline" yaml:",inline"`
-	Context  map[string]string `json:"context,omitempty" yaml:"context,omitempty"`
-	Status   BuildStatus       `json:"status,omitempty" yaml:"status,omitempty"`
-	Success  bool              `json:"success,omitempty" yaml:"success,omitempty"`
-	JobID    string            `json:"jobID,omitempty" yaml:"jobID,omitempty"`
+	JSONBase    `json:",inline" yaml:",inline"`
+	BuildConfig BuildConfig
+	Status      BuildStatus `json:"status,omitempty" yaml:"status,omitempty"`
+	Pod         Pod         `json:"pod,omitempty" yaml:"pod,omitempty"`
+	PodID       string      `json:"podID,omitempty" yaml:"podID,omitempty"`
 }
 
 // BuildStatus represents the status of a Build at a point in time.
@@ -417,7 +392,15 @@ const (
 	BuildPending  BuildStatus = "pending"
 	BuildRunning  BuildStatus = "running"
 	BuildComplete BuildStatus = "complete"
+	BuildFailed   BuildStatus = "failed"
 )
+
+type BuildConfig struct {
+	Type      BuildType
+	SourceURI string
+}
+
+type BuildType string
 
 // BuildList is a collection of Builds.
 type BuildList struct {
--- pkg/build/build_controller.go
+++ pkg/build/build_controller.go
@@ -1,8 +1,6 @@
 package build
 
 import (
-	"errors"
-	"fmt"
 	"time"
 
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
@@ -12,29 +10,13 @@ import (
 )
 
 type BuildController struct {
-	kubeClient  client.Interface
-	syncTime    <-chan time.Time
-	buildRunner BuildRunner
-}
-
-// BuildRunner is an interface that knows how to run builds and was
-// created as an interface to allow testing.
-type BuildRunner interface {
-	// run a job for the specified build
-	run(build api.Build) error
-}
-
-// DefaultBuildRunner is the default implementation of BuildRunner interface.
-type DefaultBuildRunner struct {
 	kubeClient client.Interface
+	syncTime   <-chan time.Time
 }
 
 func MakeBuildController(kubeClient client.Interface) *BuildController {
 	bc := &BuildController{
 		kubeClient: kubeClient,
-		buildRunner: &DefaultBuildRunner{
-			kubeClient: kubeClient,
-		},
 	}
 
 	return bc
@@ -50,130 +32,85 @@ func (bc *BuildController) Run(period time.Duration) {
 func (bc *BuildController) synchronize() {
 	builds, err := bc.kubeClient.ListBuilds()
 	if err != nil {
-		glog.Errorf("Synchronization error: %v (%#v)", err, err)
+		glog.Errorf("Error listing builds: %v (%#v)", err, err)
 		return
 	}
+
 	for _, build := range builds.Items {
-		err = bc.syncBuildState(build)
+		nextStatus, err = bc.process(build)
 		if err != nil {
-			glog.Errorf("Error synchronizing: %#v", err)
+			glog.Errorf("Error processing build ID %v: %#v", build.ID, err)
+		}
+
+		if nextStatus != build.Status {
+			build.Status = nextStatus
+			if _, err := bc.kubeClient.UpdateBuild(build); err != nil {
+				glog.Errorf("Error updating build ID %v to status %v: %#v", build.ID, nextStatus, err)
+			}
 		}
 	}
 }
 
 // Sync loop brings the build state into sync with its corresponding job.
 // TODO: improve handling of illegal state transitions
-func (bc *BuildController) syncBuildState(build api.Build) error {
-	glog.Infof("Syncing build state for build ID %s", build.ID)
-	glog.Infof("Build details: %#v", build)
-	if build.Status == api.BuildNew {
-		return bc.buildRunner.run(build)
-	} else if build.Status == api.BuildPending && len(build.JobID) == 0 {
-		return nil
-	}
+func (bc *BuildController) process(build *api.Build) (api.BuildStatus, error) {
+	glog.Infof("Syncing build %#v", build)
 
-	glog.Infof("Retrieving info for job ID %s for build ID %s", build.JobID, build.ID)
-	job, err := bc.kubeClient.GetJob(build.JobID)
-	if err != nil {
-		return err
-	}
+	switch build.Status {
+	case api.BuildNew:
+		build.PodID = "build-" + build.BuildConfig.Type // TODO: better naming
+		return api.BuildPending, nil
+	case api.BuildPending:
+		podSpec := bc.buildPodSpec(build)
+		_, err := r.kubeClient.CreatePod(podSpec)
 
-	update := false
-
-	glog.Infof("Status for job ID %s: %s", job.ID, job.Status)
-	switch job.Status {
-	case api.JobRunning:
-		switch build.Status {
-		case api.BuildNew, api.BuildPending:
-			glog.Infof("Setting build state to running")
-			build.Status = api.BuildRunning
-			update = true
-		case api.BuildComplete:
-			return errors.New("Illegal state transition")
-		}
-	case api.JobPending:
-		switch build.Status {
-		case api.BuildNew:
-			build.Status = api.BuildPending
-			update = true
-		case api.BuildComplete:
-			return errors.New("Illegal state transition")
-		}
-	case api.JobComplete:
-		if build.Status == api.BuildComplete {
-			glog.Infof("Build status is already complete - no-op")
-			return nil
+		// TODO: strongly typed error checking
+		switch {
+		case err == "pod_exists":
+			return build.Status, err // no transition, already handled by someone else
+		case err != nil:
+			return api.BuildFailed, err
 		}
 
-		// TODO: better way of evaluating job completion
-		glog.Infof("Setting build state to complete")
-		build.Status = api.BuildComplete
-		build.Success = job.Success
-		update = true
-	}
-
-	if update {
-		_, err := bc.kubeClient.UpdateBuild(build)
+		return api.BuildRunning, nil
+	case api.BuildRunning:
+		pod, err := bc.kubeClient.GetPod(build.PodID)
 		if err != nil {
-			return err
+			glog.Errorf("Error retrieving pod for build ID %v: %#v", build.ID, err)
+			return build.Status, err
+		}
+
+		// check the exit codes of all the containers in the pod
+		if pod.CurrentState.Status == api.PodStopped {
+			for _, info := range pod.CurrentState.Info {
+				if info.State.ExitCode != 0 {
+					return api.BuildFailed, nil
+				}
+			}
+
+			return api.BuildComplete, nil
 		}
 	}
 
-	return nil
+	return build.Status, nil
 }
 
-func (r *DefaultBuildRunner) run(build api.Build) error {
-	var envVars []api.EnvVar
-	for k, v := range build.Context {
-		envVars = append(envVars, api.EnvVar{Name: k, Value: v})
-	}
-
-	job := api.Job{
-		Pod: api.Pod{
-			Labels: map[string]string{},
-			DesiredState: api.PodState{
-				Manifest: api.ContainerManifest{
-					Version: "v1beta1",
-					Containers: []api.Container{
-						{
-							Name:          "docker-build",
-							Image:         "ironcladlou/openshift-docker-builder",
-							Privileged:    true,
-							RestartPolicy: "runOnce",
-							Env:           envVars,
-						},
+func (bc *BuildController) buildPodSpec(build api.Build) api.Pod {
+	return api.Pod{
+		ID:     build.PodID,
+		Labels: map[string]string{},
+		DesiredState: api.PodState{
+			Manifest: api.ContainerManifest{
+				Version: "v1beta1",
+				Containers: []api.Container{
+					{
+						Name:          "docker-build",
+						Image:         "ironcladlou/openshift-docker-builder",
+						Privileged:    true,
+						RestartPolicy: "runOnce",
 					},
 				},
 			},
 		},
 	}
-
-	glog.Infof("Creating job for build %s", build.ID)
-	job, err := r.kubeClient.CreateJob(job)
-
-	if err != nil {
-		glog.Errorf("Error creating job for build ID %s: %s\n", build.ID, err.Error())
-
-		build.Status = api.BuildComplete
-		build.Success = false
-
-		_, err := r.kubeClient.UpdateBuild(build)
-		if err != nil {
-			return fmt.Errorf("Couldn't update build: %#v : %s", build, err.Error())
-		}
-
-		//TODO should this return nil or some error?
-		return nil
-	}
-
-	glog.Infof("Setting build state to pending, job id = %s", job.ID)
-	build.Status = api.BuildPending
-	build.JobID = job.ID
-
-	_, err = r.kubeClient.UpdateBuild(build)
-	if err != nil {
-		return fmt.Errorf("Couldn't update Job: %#v : %s", job, err.Error())
-	}
-
-	return nil
 }
