d8d09ace41dc2d6eb3834bfbf0b781d39c8b38fa
Jeff Mendoza <jeffmendoza@live.com>
2014-08-22 15:48:06 -0700

    Refactor Azure deploy scripts.
    Refactored to work with cluster/* scripts which require
    actions to be defined as methods in cluster/azure/util.sh

--- release/azure/config.sh
+++ release/azure/config.sh
@@ -12,13 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+INSTANCE_PREFIX=kubenertes
 AZ_LOCATION='West US'
-AZ_SSH_KEY=$HOME/.ssh/azure
-AZ_SSH_CERT=$HOME/.ssh/azure.pem
-AZ_IMAGE=b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_04-LTS-amd64-server-20140618.1-en-us-30GB
-AZ_SUBNET=Subnet-1
 TAG=testing
-INSTANCE_PREFIX=kubernetes
 
 if [ -z "$(which azure)" ]; then
     echo "Couldn't find azure in PATH"
@@ -33,19 +29,12 @@ if [ -z "$(azure account list | grep true)" ]; then
 fi
 
 account=$(azure account list | grep true | awk '{ print $2 }')
-
 if which md5 > /dev/null 2>&1; then
-  hsh=$(md5 -q -s $account)
+  AZ_HSH=$(md5 -q -s $account)
 else
-  hsh=$(echo -n "$account" | md5sum)
+  AZ_HSH=$(echo -n "$account" | md5sum)
 fi
-hsh=${hsh:0:7}
-
-STG_ACCOUNT=kube$hsh
-
-AZ_VNET=kube-$hsh
-AZ_CS=kube-$hsh
-
+AZ_HSH=${AZ_HSH:0:7}
+AZ_STG=kube$AZ_HSH
 CONTAINER=kube-$TAG
-
-FULL_URL="https://${STG_ACCOUNT}.blob.core.windows.net/$CONTAINER/master-release.tgz"
+FULL_URL="https://${AZ_STG}.blob.core.windows.net/$CONTAINER/master-release.tgz"
