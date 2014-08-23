b037989478a4ae67a390dedb813a617c09584972
Clayton Coleman <ccoleman@redhat.com>
2014-08-22 21:12:16 -0400

    Add an integration test for etcd

---
+++ test/integration/utils.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2014 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// +build integration
+
+package integration
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/coreos/go-etcd/etcd"
+	"github.com/golang/glog"
+)
+
+func newEtcdClient() *etcd.Client {
+	return etcd.NewClient([]string{})
+}
+
+func requireEtcd() {
+	if _, err := newEtcdClient().Get("/", false, false); err != nil {
+		glog.Fatalf("unable to connect to etcd for integration testing: %v", err)
+	}
+}
+
+func withEtcdKey(f func(string)) {
+	prefix := fmt.Sprintf("/test-%d", rand.Int63())
+	defer newEtcdClient().Delete(prefix, true)
+	f(prefix)
+}
