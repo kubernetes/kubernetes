a711770e19c80e5aea0c4d0fd8860f7c8ab82e3a
Tim Hockin <thockin@google.com>
2014-09-02 13:18:13 -0700

    Revert "Fixed annoying typo"

--- release/azure/config.sh
+++ release/azure/config.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-INSTANCE_PREFIX=kubernetes
+INSTANCE_PREFIX=kubenertes
 AZ_LOCATION='West US'
 TAG=testing
 
