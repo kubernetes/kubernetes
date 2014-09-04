53e3bff4641cce88f58a901759381d4229c634b3
Brendan Burns <bburns@google.com>
2014-09-04 09:54:50 -0700

    Re-enable travis for Go tip

--- hack/test-go.sh
+++ hack/test-go.sh
@@ -40,6 +40,11 @@ find_test_dirs() {
     \) -name '*_test.go' -print0 | xargs -0n1 dirname | sort -u | xargs -n1 printf "${KUBE_GO_PACKAGE}/%s\n"
 }
 
+# there is currently a race in the coverage code in tip.  Remove this when it is fixed
+# see https://code.google.com/p/go/issues/detail?id=8630 for details.
+if [[ ${TRAVIS_GO_VERSION} eq "tip" ]]; then
+  KUBE_COVER=""
+fi
 # -covermode=atomic becomes default with -race in Go >=1.3
 KUBE_COVER=${KUBE_COVER:--cover -covermode=atomic}
 KUBE_TIMEOUT=${KUBE_TIMEOUT:--timeout 30s}
