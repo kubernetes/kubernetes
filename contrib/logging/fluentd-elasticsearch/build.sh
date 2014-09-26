47425dc08b1b0002a98d05d86d2d87783414e919
Satnam Singh <satnam@google.com>
2014-09-26 15:48:57 -0700

    Remove small shell utilities, apply comments in pull request, add README.md

--- contrib/logging/fluentd-elasticsearch/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Build a Docker image for running a version of fluentd
-# configure with an Elasticsearch plug-in which processes
-# Docker log files.
-
-set -e 
-
-docker build -t satnam6502/docker-fluentd .
