6daeb8a05c1197127fbc2a511c7cc7e212bccacb
Brendan Burns <bburns@google.com>
2014-09-26 15:09:49 -0700

    Add the github.com/skratchdot/open-golang/open package as a dependency.

---
+++ Godeps/_workspace/src/github.com/skratchdot/open-golang/open/exec.go
@@ -0,0 +1,15 @@
+// +build !windows,!darwin
+
+package open
+
+import (
+	"os/exec"
+)
+
+func open(input string) *exec.Cmd {
+	return exec.Command("xdg-open", input)
+}
+
+func openWith(input string, appName string) *exec.Cmd {
+	return exec.Command(appName, input)
+}
