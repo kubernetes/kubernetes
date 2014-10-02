159035fba2efb260e7be0bfe8755077c80509cce
Johan Euphrosine <proppy@google.com>
2014-10-02 01:22:25 -0700

    podex: add missing license header and examples

--- podex/podex.go
+++ podex/podex.go
@@ -1,5 +1,26 @@
-// podex is a command line tool to generate kubernetes container manifest
-// from docker image metadata.
+/*
+Copyright 2014 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// podex is a command line tool to generate a kubernetes container manifest placeholder from docker image metadata
+//
+// Example usage:
+//
+// $ docker pull google/nodejs-hello
+// $ podex -yaml google/nodejs-hello > google/nodejs-hello/pod.yaml
+// $ podex -json google/nodejs-hello > google/nodejs-hello/pod.json
 
 package main
 
