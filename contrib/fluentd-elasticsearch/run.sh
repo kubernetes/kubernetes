4b5e4cedd62e8b3c21f2b37ecdbe3ff2b719055e
Satnam Singh <satnam@google.com>
2014-10-02 22:40:14 -0700

    Source files for flutend to Elasticsearch and Kibana demo.

---
+++ contrib/fluentd-elasticsearch/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+OUTER_HOST=`tail -n 1 /outerhost | awk '{print $3}'`
+echo Outer host is ${OUTER_HOST}
+cp td-agent.conf /etc/td-agent
+sed -i -e "s/ES_HOST/${OUTER_HOST}/" /etc/td-agent/td-agent.conf
+/usr/sbin/td-agent
