84d84c50c234dc96497d48766e7daae587f0f889
Clayton Coleman <ccoleman@redhat.com>
2014-12-03 08:40:30 -0500

    Add a strongly typed error for unrecognized kind/type/version

---
+++ pkg/conversion/error.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2014 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package conversion
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type notRegisteredErr struct {
+	kind    string
+	version string
+	t       reflect.Type
+}
+
+func (k *notRegisteredErr) Error() string {
+	if k.t != nil {
+		return fmt.Sprintf("no kind is registered for the type %v", k.t)
+	}
+	if len(k.kind) == 0 {
+		return fmt.Sprintf("no version %q has been registered", k.version)
+	}
+	if len(k.version) == 0 {
+		return fmt.Sprintf("no kind %q is registered for the default version", k.kind)
+	}
+	return fmt.Sprintf("no kind %q is registered for version %q", k.kind, k.version)
+}
+
+// IsNotRegisteredError returns true if the error indicates the provided
+// object or input data is not registered.
+func IsNotRegisteredError(err error) bool {
+	if err == nil {
+		return false
+	}
+	_, ok := err.(*notRegisteredErr)
+	return ok
+}
