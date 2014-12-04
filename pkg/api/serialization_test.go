22eee15fe78b2c4c6ae0ee3c8fa49df83e76df27
Jordan Liggitt <jliggitt@redhat.com>
2014-12-04 15:00:23 -0800

    Remove uint64 bitshift workaround

--- pkg/api/serialization_test.go
+++ pkg/api/serialization_test.go
@@ -50,10 +50,7 @@ var apiObjectFuzzer = fuzz.New().NilChance(.5).NumElements(1, 1).Funcs(
 		j.Kind = ""
 
 		j.Name = c.RandString()
-		// TODO: Fix JSON/YAML packages and/or write custom encoding
-		// for uint64's. Somehow the LS *byte* of this is lost, but
-		// only when all 8 bytes are set.
-		j.ResourceVersion = strconv.FormatUint(c.RandUint64()>>8, 10)
+		j.ResourceVersion = strconv.FormatUint(c.RandUint64(), 10)
 		j.SelfLink = c.RandString()
 
 		var sec, nsec int64
@@ -69,10 +66,7 @@ var apiObjectFuzzer = fuzz.New().NilChance(.5).NumElements(1, 1).Funcs(
 	},
 	func(j *api.ObjectMeta, c fuzz.Continue) {
 		j.Name = c.RandString()
-		// TODO: Fix JSON/YAML packages and/or write custom encoding
-		// for uint64's. Somehow the LS *byte* of this is lost, but
-		// only when all 8 bytes are set.
-		j.ResourceVersion = strconv.FormatUint(c.RandUint64()>>8, 10)
+		j.ResourceVersion = strconv.FormatUint(c.RandUint64(), 10)
 		j.SelfLink = c.RandString()
 
 		var sec, nsec int64
@@ -81,10 +75,7 @@ var apiObjectFuzzer = fuzz.New().NilChance(.5).NumElements(1, 1).Funcs(
 		j.CreationTimestamp = util.Unix(sec, nsec).Rfc3339Copy()
 	},
 	func(j *api.ListMeta, c fuzz.Continue) {
-		// TODO: Fix JSON/YAML packages and/or write custom encoding
-		// for uint64's. Somehow the LS *byte* of this is lost, but
-		// only when all 8 bytes are set.
-		j.ResourceVersion = strconv.FormatUint(c.RandUint64()>>8, 10)
+		j.ResourceVersion = strconv.FormatUint(c.RandUint64(), 10)
 		j.SelfLink = c.RandString()
 	},
 	func(j *api.PodPhase, c fuzz.Continue) {
@@ -119,10 +110,6 @@ var apiObjectFuzzer = fuzz.New().NilChance(.5).NumElements(1, 1).Funcs(
 			intstr.IntVal = 0
 			intstr.StrVal = c.RandString()
 		}
-	},
-	func(u64 *uint64, c fuzz.Continue) {
-		// TODO: uint64's are NOT handled right.
-		*u64 = c.RandUint64() >> 8
 	},
 	func(pb map[docker.Port][]docker.PortBinding, c fuzz.Continue) {
 		// This is necessary because keys with nil values get omitted.
