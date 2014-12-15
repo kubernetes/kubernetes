a6fd5601c29b2d85b01205d9373893121fc54465
Rajdeep Dua <dua_rajdeep@yahoo.com>
2014-12-14 21:41:34 -0800

    Added test cases for * Event Create * Event List

--- pkg/client/events_test.go
+++ pkg/client/events_test.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/api/testapi"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/util"
 )
 
@@ -61,7 +62,6 @@ func TestEventCreate(t *testing.T) {
 	}
 	timeStamp := util.Now()
 	event := &api.Event{
-		//namespace: namespace{"default"},
 		Status:         "running",
 		InvolvedObject: *objReference,
 		Timestamp:      timeStamp,
@@ -83,5 +83,86 @@ func TestEventCreate(t *testing.T) {
 
 	if e, a := *objReference, response.InvolvedObject; !reflect.DeepEqual(e, a) {
 		t.Errorf("%#v != %#v.", e, a)
+	}
+}
+
+func TestEventGet(t *testing.T) {
+	objReference := &api.ObjectReference{
+		Kind:            "foo",
+		Namespace:       "nm",
+		Name:            "objref1",
+		UID:             "uid",
+		APIVersion:      "apiv1",
+		ResourceVersion: "1",
+	}
+	timeStamp := util.Now()
+	event := &api.Event{
+		Status:         "running",
+		InvolvedObject: *objReference,
+		Timestamp:      timeStamp,
+	}
+	c := &testClient{
+		Request: testRequest{
+			Method: "GET",
+			Path:   "/events/1",
+			Body:   nil,
+		},
+		Response: Response{StatusCode: 200, Body: event},
+	}
+
+	response, err := c.Setup().Events("").Get("1")
+
+	if err != nil {
+		t.Errorf("%#v should be nil.", err)
+	}
+
+	if e, r := event.InvolvedObject, response.InvolvedObject; !reflect.DeepEqual(e, r) {
+		t.Errorf("%#v != %#v.", e, r)
+	}
+}
+
+func TestEventList(t *testing.T) {
+	ns := api.NamespaceDefault
+	objReference := &api.ObjectReference{
+		Kind:            "foo",
+		Namespace:       ns,
+		Name:            "objref1",
+		UID:             "uid",
+		APIVersion:      "apiv1",
+		ResourceVersion: "1",
+	}
+	timeStamp := util.Now()
+	eventList := &api.EventList{
+		Items: []api.Event{
+			{
+				Status:         "running",
+				InvolvedObject: *objReference,
+				Timestamp:      timeStamp,
+			},
+		},
+	}
+	c := &testClient{
+		Request: testRequest{
+			Method: "GET",
+			Path:   "/events",
+			Body:   nil,
+		},
+		Response: Response{StatusCode: 200, Body: eventList},
+	}
+	response, err := c.Setup().Events(ns).List(labels.Everything(),
+		labels.Everything())
+
+	if err != nil {
+		t.Errorf("%#v should be nil.", err)
+	}
+
+	if len(response.Items) != 1 {
+		t.Errorf("%#v response.Items should have len 1.", response.Items)
+	}
+
+	responseEvent := response.Items[0]
+	if e, r := eventList.Items[0].InvolvedObject,
+		responseEvent.InvolvedObject; !reflect.DeepEqual(e, r) {
+		t.Errorf("%#v != %#v.", e, r)
 	}
 }
