072560755659d9aae74e443bd2ee3f5a142a5bef
Clayton Coleman <ccoleman@redhat.com>
2014-12-17 19:55:56 -0500

    v1beta1 should return Minion as kind, rather than Node
    This changes the internal name logic (for conversion) to prefer the
    internal registered preferred name for a resource, and then makes
    v1beta1 and v1beta2 prefer Minion.
    
    Fixes #3010
    
    (cherry picked from commit 7fde4583f2604f3a43da2e3cc4137c05cf87b6cc)

--- pkg/api/v1beta2/conversion_test.go
+++ pkg/api/v1beta2/conversion_test.go
@@ -17,10 +17,11 @@ limitations under the License.
 package v1beta2_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	newer "github.com/GoogleCloudPlatform/kubernetes/pkg/api"
-	current "github.com/GoogleCloudPlatform/kubernetes/pkg/api/v1beta1"
+	current "github.com/GoogleCloudPlatform/kubernetes/pkg/api/v1beta2"
 )
 
 func TestServiceEmptySelector(t *testing.T) {
@@ -56,6 +57,15 @@ func TestServiceEmptySelector(t *testing.T) {
 }
 
 func TestNodeConversion(t *testing.T) {
+	version, kind, err := newer.Scheme.ObjectVersionAndKind(&current.Minion{})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if version != "v1beta2" || kind != "Minion" {
+		t.Errorf("unexpected version and kind: %s %s", version, kind)
+	}
+
+	newer.Scheme.Log(t)
 	obj, err := current.Codec.Decode([]byte(`{"kind":"Node","apiVersion":"v1beta2"}`))
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
@@ -75,5 +85,18 @@ func TestNodeConversion(t *testing.T) {
 	obj = &newer.Node{}
 	if err := current.Codec.DecodeInto([]byte(`{"kind":"Node","apiVersion":"v1beta2"}`), obj); err != nil {
 		t.Fatalf("unexpected error: %v", err)
+	}
+
+	obj = &newer.Node{}
+	data, err := current.Codec.Encode(obj)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	m := map[string]interface{}{}
+	if err := json.Unmarshal(data, &m); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if m["kind"] != "Minion" {
+		t.Errorf("unexpected encoding: %s - %#v", m["kind"], string(data))
 	}
 }
