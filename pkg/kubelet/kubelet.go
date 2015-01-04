c9adb3ec42f1cf0b70b8877851625798eddb52b8
Clayton Coleman <ccoleman@redhat.com>
2015-01-03 23:50:28 -0500

    Simplify Kubelet podfullname lookup to account for source change
    server.go is currently source agnostic, so a method should be
    exposed on the Kubelet that makes it simpler for server.go to
    find the proper pod.  Also, correct an error in stats retrieval
    and better indicate that the code is broken.

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -1124,6 +1124,8 @@ func (kl *Kubelet) syncLoop(updates <-chan PodUpdate, handler SyncHandler) {
 
 // GetKubeletContainerLogs returns logs from the container
 // The second parameter of GetPodInfo and FindPodContainer methods represents pod UUID, which is allowed to be blank
+// TODO: this method is returning logs of random container attempts, when it should be returning the most recent attempt
+// or all of them.
 func (kl *Kubelet) GetKubeletContainerLogs(podFullName, containerName, tail string, follow bool, stdout, stderr io.Writer) error {
 	_, err := kl.GetPodInfo(podFullName, "")
 	if err == dockertools.ErrNoContainersInPod {
@@ -1143,6 +1145,18 @@ func (kl *Kubelet) GetKubeletContainerLogs(podFullName, containerName, tail stri
 // GetBoundPods returns all pods bound to the kubelet and their spec
 func (kl *Kubelet) GetBoundPods() ([]api.BoundPod, error) {
 	return kl.pods, nil
+}
+
+// GetPodFullName provides the first pod that matches namespace and name, or false
+// if no such pod can be found.
+func (kl *Kubelet) GetPodFullName(namespace, name string) (string, bool) {
+	for i := range kl.pods {
+		pod := &kl.pods[i]
+		if pod.Namespace == namespace && pod.Name == name {
+			return GetPodFullName(pod), true
+		}
+	}
+	return "", false
 }
 
 // GetPodInfo returns information from Docker about the containers in a pod
