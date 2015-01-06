e66e30183b0b62ea9d1fa5bb51f5fc0c9e9a38bc
Zach Loafman <zml@google.com>
2015-01-05 18:02:46 -0800

    Add script to push continuous integration artifacts to gs://kubernetes-release/ci
    This pushes artifacts in a similar manner to the official release,
    except that instead of release/vFOO, it goes to ci/$(git describe),
    e.g.: gs://kubernetes-release/ci/v0.7.0-315-gcae5722
    
    It also pushes a text file to gs://kubernetes-release/ci/latest.txt,
    so anyone can do, for instance:
    
    gsutil ls gs://kubernetes-release/ci/$(gsutil cat gs://kubernetes-release/ci/latest.txt)
    
    (In a parallel change, I'm going to flip the jenkins scripts over to
    use git describe, since it's shorter and a little more descriptive)

---
+++ build/push-ci-build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Pushes a continuous integration build to our official CI repository
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+LATEST=$(git describe)
+KUBE_GCS_NO_CACHING=n
+KUBE_GCS_MAKE_PUBLIC=y
+KUBE_GCS_UPLOAD_RELEASE=y
+KUBE_GCS_RELEASE_BUCKET=kubernetes-release
+KUBE_GCS_PROJECT=google-containers
+KUBE_GCS_RELEASE_PREFIX="ci/${LATEST}"
+KUBE_GCS_LATEST_FILE="ci/latest.txt"
+KUBE_GCS_LATEST_CONTENTS=${LATEST}
+
+KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
+source "$KUBE_ROOT/build/common.sh"
+
+kube::release::gcs::release
+kube::release::gcs::publish_latest
