c40a5f2cf952ccb088d7c3abe443d2fa37b98de4
Nalin Dahyabhai <nalin@redhat.com>
2015-01-12 14:46:40 -0500

    Use saslauthd and gssproxy in the apiserver
    Add the necessary interfaces and support for accepting Negotiate
    authentication from clients by using a gss-proxy service, and for
    checking passwords offered via Basic authentication using a saslauthd
    service.
    
    When the apiserver is started with -gssproxy=/socket/path, it will
    contact a running gss-proxy using the specified socket, and ask it to
    perform the server portion of Negotiate authentication.  That gss-proxy
    service's configuration should instruct it to use the server's Kerberos
    keytab to accept authentication requests from clients, like so:
    
      [service/kube-apiserver]
        mechs = krb5
        socket = /run/gssproxy-kube-apiserver.sock
        cred_store = keytab:/etc/kube.keytab
        cred_usage = accept
        euid = 0
    
    When the apiserver is started with the -saslauthd flag, which takes
    arguments "[realm][,[socket][,[servicename]]]", the server will attempt
    to hand Basic credentials off to a running saslauthd service for
    checking.  The default socket location, if none or an empty value is
    specified, is "/var/run/saslauthd/mux".  The default service name, if
    none or an empty value is specified, is based on the name of the binary
    ("kube-apiserver", typically).
    
    On most Fedora or Red Hat systems, saslauthd is configured to use PAM by
    default, so either the service name should be set to a valid PAM
    configuration ("password-auth"), or a configuration should be created
    for the name which the server will use.
    
    The server will accept any of bearer tokens, negotiate, or basic auth
    now, and requires that at least one of them be configured.

---
+++ plugin/pkg/auth/authenticator/gssapi/gssproxy/gssproxy.go
@@ -0,0 +1,94 @@
+/*
+Copyright 2015 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gssproxy
+
+import (
+	"encoding/base64"
+	"errors"
+	"net"
+
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/auth/authenticator"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/auth/user"
+	"github.com/nalind/gss/pkg/gss/proxy"
+)
+
+type UserConversion interface {
+	User(name proxy.SecCtx) (user.Info, bool, error)
+}
+
+type UserConversionFunc func(context proxy.SecCtx) (user.Info, bool, error)
+
+func (f UserConversionFunc) User(context proxy.SecCtx) (user.Info, bool, error) {
+	return f(context)
+}
+
+type GssProxyAuthenticator struct {
+	proxyPath   string
+	userConvert UserConversion
+}
+
+func NewGssProxy(proxyPath string, userConversion UserConversion) (authenticator.Token, error) {
+	return &GssProxyAuthenticator{
+		proxyPath:   proxyPath,
+		userConvert: userConversion,
+	}, nil
+}
+
+func (a *GssProxyAuthenticator) AuthenticateToken(b64token string) (user.Info, bool, error) {
+	var callCtx proxy.CallCtx
+	var secCtx proxy.SecCtx
+	if len(b64token) == 0 {
+		return nil, false, errors.New("Requested Negotiate auth, but provided no auth data")
+	}
+	token, err := base64.StdEncoding.DecodeString(b64token)
+	if err != nil {
+		return nil, false, err
+	}
+	conn, err := net.Dial("unix", a.proxyPath)
+	if err != nil {
+		return nil, false, errors.New("Error contacting gss-proxy: " + err.Error())
+	}
+	defer conn.Close()
+	gccr, err := proxy.GetCallContext(&conn, &callCtx, nil)
+	if err != nil {
+		return nil, false, err
+	}
+	if gccr.Status.MajorStatus != proxy.S_COMPLETE {
+		return nil, false, errors.New("Error returned from gss-proxy GetCallContext function: " + gccr.Status.MajorStatusString + "/" + gccr.Status.MinorStatusString)
+	}
+	ascr, err := proxy.AcceptSecContext(&conn, &callCtx, &secCtx, nil, token, nil, false, nil)
+	if err != nil {
+		return nil, false, err
+	}
+	if secCtx.NeedsRelease {
+		defer proxy.ReleaseSecCtx(&conn, &callCtx, &secCtx)
+	}
+	if ascr.Status.MajorStatus == proxy.S_CONTINUE_NEEDED {
+		// We can't handle multiple-round-trip requests right.
+		return nil, false, errors.New("Negotiate authentication canceled (incomplete): " + ascr.Status.MajorStatusString + "/" + ascr.Status.MinorStatusString)
+	}
+	if ascr.Status.MajorStatus != proxy.S_COMPLETE {
+		// Authentication failed.
+		return nil, false, errors.New("Negotiate authentication failed: " + ascr.Status.MajorStatusString + "/" + ascr.Status.MinorStatusString)
+	}
+	// We also don't have a method for returning ascr.OutputToken to the client yet.
+	return a.userConvert.User(secCtx)
+}
+
+var DefaultUserInfo UserConversionFunc = func(context proxy.SecCtx) (user.Info, bool, error) {
+	return &user.DefaultInfo{Name: context.SrcName.DisplayName}, context.Open, nil
+}
---
+++ plugin/pkg/auth/authenticator/password/saslauthd/saslauthd.go
@@ -0,0 +1,138 @@
+/*
+Copyright 2015 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package saslauthd
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/auth/authenticator"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/auth/user"
+	"net"
+	"os"
+	"strings"
+)
+
+type saslAuthdAuthenticator struct {
+	serviceName  string
+	socketPath   string
+	defaultRealm string
+}
+
+// NewSaslAuthd returns a password authenticator that checks passwords using
+// saslauthd by connecting directly to its socket.  The request format matches
+// the one expected by saslauthd versions 2.0.5 and later (through at least
+// 2.1.26): login ID, password, service name, and realm, each preceded by a
+// 16-bit network-order length, with no padding.
+func NewSaslAuthd(defaultRealm, socketPath, serviceName string) (authenticator.Password, error) {
+	if serviceName == "" {
+		serviceName = os.Args[0]
+	}
+	index := strings.LastIndex(serviceName, "/")
+	if index >= 0 {
+		serviceName = serviceName[index+1:]
+	}
+	if serviceName == "" {
+		return nil, errors.New("error determining SASL service name")
+	}
+	return saslAuthdAuthenticator{serviceName: serviceName, socketPath: socketPath, defaultRealm: defaultRealm}, nil
+}
+
+// AuthenticatePassword implements authenticator.Password by handing it off to
+// saslauthd, using the specified username as the login ID.  If successful, a
+// user.DefaultInfo is returned.  That structure will contain the username, an
+// empty UID, and no group information.
+func (s saslAuthdAuthenticator) AuthenticatePassword(username, password string) (user.Info, bool, error) {
+	var replen uint16
+	realm := s.defaultRealm
+	index := strings.LastIndex(username, "@")
+	if index >= 0 {
+		realm = username[index+1:]
+		username = username[0:index]
+	}
+	service := s.serviceName
+	if len(username) >= 256 {
+		return nil, false, errors.New("User name would be too long for saslauthd")
+	}
+	if len(password) >= 256 {
+		return nil, false, errors.New("Password would be too long for saslauthd")
+	}
+	if len(realm) >= 256 {
+		return nil, false, errors.New("Realm name would be too long for saslauthd")
+	}
+	if len(service) >= 256 {
+		return nil, false, errors.New("Service name would be too long for saslauthd")
+	}
+	path := s.socketPath
+	if path == "" {
+		path = "/var/run/saslauthd/mux"
+	} else {
+		fi, err := os.Stat(path)
+		if err != nil {
+			return nil, false, err
+		}
+		if fi.IsDir() {
+			path = path + "/mux"
+		}
+	}
+	conn, err := net.Dial("unix", path)
+	if err != nil {
+		return nil, false, err
+	}
+	defer conn.Close()
+	req := new(bytes.Buffer)
+	if req == nil {
+		return nil, false, nil
+	}
+	binary.Write(req, binary.BigEndian, uint16(len(username)))
+	req.WriteString(username)
+	binary.Write(req, binary.BigEndian, uint16(len(password)))
+	req.WriteString(password)
+	binary.Write(req, binary.BigEndian, uint16(len(service)))
+	req.WriteString(service)
+	binary.Write(req, binary.BigEndian, uint16(len(realm)))
+	req.WriteString(realm)
+	rb := req.Bytes()
+	n, err := conn.Write(rb)
+	if n < len(rb) {
+		return nil, false, errors.New("Error sending request to saslauthd")
+	}
+	binary.Read(conn, binary.BigEndian, &replen)
+	if replen < 2 {
+		return nil, false, errors.New("Response from saslauthd would be too short")
+	}
+	if replen > 1024 {
+		return nil, false, errors.New("Response from saslauthd would be too long")
+	}
+	rep := make([]byte, replen)
+	n, err = conn.Read(rep)
+	b := make([]byte, 1)
+	for n > 0 {
+		n, err = conn.Read(b)
+		if err != nil {
+			break
+		}
+	}
+	if rep[0] == 'O' && rep[1] == 'K' {
+		return &user.DefaultInfo{Name: username + "@" + realm}, true, nil
+	}
+	if rep[0] == 'N' && rep[1] == 'O' {
+		return nil, false, errors.New(fmt.Sprintf("Authentication rejected by saslauthd: %s", bytes.NewBuffer(rep).String()))
+	}
+	return nil, false, errors.New("Unknown error authenticating with saslauthd")
+}
