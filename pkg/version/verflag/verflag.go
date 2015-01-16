827f0b7ec1a8ca9892a08228c541e7669484c7d4
Filipe Brandenburger <filbranden@google.com>
2015-01-16 10:17:53 -0800

    Update verflag.go to work with pflag so that --version keeps working.
    This is a limitation of pflag that could be addressed by ogier/pflag#11, so once that is implemented we can look into reverting this one.
    
    For now, this seems to be as good as we can do it, so let's proceed with it to make at least the expected --version calls work as one would expect.

--- pkg/version/verflag/verflag.go
+++ pkg/version/verflag/verflag.go
@@ -21,78 +21,23 @@ package verflag
 import (
 	"fmt"
 	"os"
-	"strconv"
 
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/version"
 	flag "github.com/spf13/pflag"
 )
 
-type versionValue int
-
-const (
-	VersionFalse versionValue = 0
-	VersionTrue  versionValue = 1
-	VersionRaw   versionValue = 2
-)
-
-const strRawVersion string = "raw"
-
-func (v *versionValue) IsBoolFlag() bool {
-	return true
-}
-
-func (v *versionValue) Get() interface{} {
-	return versionValue(*v)
-}
-
-func (v *versionValue) Set(s string) error {
-	if s == strRawVersion {
-		*v = VersionRaw
-		return nil
-	}
-	boolVal, err := strconv.ParseBool(s)
-	if boolVal {
-		*v = VersionTrue
-	} else {
-		*v = VersionFalse
-	}
-	return err
-}
-
-func (v *versionValue) String() string {
-	if *v == VersionRaw {
-		return strRawVersion
-	}
-	return fmt.Sprintf("%v", bool(*v == VersionTrue))
-}
-
-// The type of the flag as requred by the pflag.Value interface
-func (v *versionValue) Type() string {
-	return "version"
-}
-
-func VersionVar(p *versionValue, name string, value versionValue, usage string) {
-	*p = value
-	flag.Var(p, name, usage)
-}
-
-func Version(name string, value versionValue, usage string) *versionValue {
-	p := new(versionValue)
-	VersionVar(p, name, value, usage)
-	return p
-}
-
 var (
-	versionFlag = Version("version", VersionFalse, "Print version information and quit")
+	versionFlag    = flag.Bool("version", false, "Print version information and quit")
+	rawVersionFlag = flag.Bool("raw_version", false, "Print raw version information and quit")
 )
 
 // PrintAndExitIfRequested will check if the -version flag was passed
 // and, if so, print the version and exit.
 func PrintAndExitIfRequested() {
-	if *versionFlag == VersionRaw {
+	if *rawVersionFlag {
 		fmt.Printf("%#v\n", version.Get())
 		os.Exit(0)
-	} else if *versionFlag == VersionTrue {
+	} else if *versionFlag {
 		fmt.Printf("Kubernetes %s\n", version.Get())
 		os.Exit(0)
 	}
