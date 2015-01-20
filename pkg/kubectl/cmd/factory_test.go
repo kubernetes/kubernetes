500bb3a7fa0983fdd8b1e6cff7772ab3afa9e088
deads2k <deads@redhat.com>
2015-01-20 17:07:36 -0500

    make kubectl factory flag binding optional

---
+++ pkg/kubectl/cmd/factory_test.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2014 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"testing"
+
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/client/clientcmd"
+	clientcmdapi "github.com/GoogleCloudPlatform/kubernetes/pkg/client/clientcmd/api"
+)
+
+func TestNewFactoryDefaultFlagBindings(t *testing.T) {
+	factory := NewFactory(nil)
+
+	if !factory.flags.HasFlags() {
+		t.Errorf("Expected flags, but didn't get any")
+	}
+}
+
+func TestNewFactoryNoFlagBindings(t *testing.T) {
+	clientConfig := clientcmd.NewDefaultClientConfig(*clientcmdapi.NewConfig(), &clientcmd.ConfigOverrides{})
+	factory := NewFactory(clientConfig)
+
+	if factory.flags.HasFlags() {
+		t.Errorf("Expected zero flags, but got %v", factory.flags)
+	}
+}
