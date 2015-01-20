8c804c520b0c5489f06a00d0b43788bc3bc00ae1
Dawn Chen <dawnchen@google.com>
2015-01-20 15:57:09 -0800

    Kubernetes version v0.8.2-dev

--- pkg/version/base.go
+++ pkg/version/base.go
@@ -36,8 +36,8 @@ package version
 var (
 	// TODO: Deprecate gitMajor and gitMinor, use only gitVersion instead.
 	gitMajor     string = "0"              // major version, always numeric
-	gitMinor     string = "8.2"            // minor version, numeric possibly followed by "+"
-	gitVersion   string = "v0.8.2"         // version from git, output of $(git describe)
+	gitMinor     string = "8.2+"           // minor version, numeric possibly followed by "+"
+	gitVersion   string = "v0.8.2-dev"     // version from git, output of $(git describe)
 	gitCommit    string = ""               // sha1 from git, output of $(git rev-parse HEAD)
 	gitTreeState string = "not a git tree" // state of git tree, either "clean" or "dirty"
 )
