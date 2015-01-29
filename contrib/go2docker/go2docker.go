1420c45565853e0fc70ce65365f7ba8fbf81af9a
Johan Euphrosine <proppy@google.com>
2015-01-29 14:34:08 -0800

    contrib/go2docker: update license header

--- contrib/go2docker/go2docker.go
+++ contrib/go2docker/go2docker.go
@@ -1,6 +1,18 @@
-// Copyright 2015 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+/*
+Copyright 2014 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 // The go2docker command compiles a go main package and forge a minimal
 // docker image from the resulting static binary.
