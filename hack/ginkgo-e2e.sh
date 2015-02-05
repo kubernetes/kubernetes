bace7593d0a3a4258fd80a400c73c54c8ef025d3
Zach Loafman <zml@google.com>
2015-02-04 17:07:20 -0800

    Enable ginkgo.v (console output is missing spec without it)

--- hack/ginkgo-e2e.sh
+++ hack/ginkgo-e2e.sh
@@ -102,5 +102,6 @@ fi
 "${e2e}" "${auth_config[@]:+${auth_config[@]}}" \
   --host="https://${KUBE_MASTER_IP-}" \
   --provider="${KUBERNETES_PROVIDER}" \
+  --ginkgo.v \
   ${E2E_REPORT_DIR+"--report_dir=${E2E_REPORT_DIR}"} \
   "${@}"
