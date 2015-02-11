9257b58e687760703bdba128accf1c6eb8f25c72
Zach Loafman <zml@google.com>
2015-02-11 12:44:29 -0800

    Fix e2e-internal scripts for cluster-size and watch-events
    Fixes #4340

--- hack/e2e-internal/e2e-cluster-size.sh
+++ hack/e2e-internal/e2e-cluster-size.sh
@@ -31,6 +31,4 @@ source "${KUBE_VERSION_ROOT}/cluster/${KUBERNETES_PROVIDER}/util.sh"
 
 prepare-e2e
 
-while true; do
-  ${KUBECTL} --watch-only get events
-done
+${KUBECTL} get minions --no-headers | wc -l
