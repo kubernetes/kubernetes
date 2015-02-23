8a881ddeb3ebc6057bfe28f77bc83bdce208c045
Huamin Chen <hchen@redhat.com>
2015-02-23 13:34:03 -0500

    fix header so boilerpate can pass
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- pkg/kubelet/volume/iscsi_pd/iscsi_util.go
+++ pkg/kubelet/volume/iscsi_pd/iscsi_util.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2015 Red Hat Inc. All rights reserved.
+Copyright 2015 Google Inc. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package iscsi_pd
 
 import (
