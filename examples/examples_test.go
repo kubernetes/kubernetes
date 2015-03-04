e8388769425feeadfbab892d56e177d4d9c72496
Michael Jeffrey <mjeffrey@mjeffrey-macpro.kir.corp.google.com>
2015-03-04 12:38:25 -0800

    Removing old sample JSON; updating references

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -98,14 +98,8 @@ func walkJSONFiles(inDir string, fn func(name, path string, data []byte)) error 
 
 func TestExampleObjectSchemas(t *testing.T) {
 	cases := map[string]map[string]runtime.Object{
-		"../api/examples": {
-			"controller":       &api.ReplicationController{},
-			"controller-list":  &api.ReplicationControllerList{},
+		"../docs/getting-started-guides": {
 			"pod":              &api.Pod{},
-			"pod-list":         &api.PodList{},
-			"service":          &api.Service{},
-			"external-service": &api.Service{},
-			"service-list":     &api.ServiceList{},
 		},
 		"../examples/guestbook": {
 			"frontend-controller":     &api.ReplicationController{},
