4ee6a7b053be601e2e3e0a38df1d3d3969453549
Shai Weinstein <shai.weinstein@gmail.com>
2015-03-22 14:18:26 +0200

    removing full stack of meanio
    Revert "adding meanio example"
    
    This reverts commit e71b1d02d6c662c9a04346da48d551c9f03bbe23.

--- examples/meanio/k8s-meanio-docker/packages/theme/public/services/theme.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('mean.theme').factory('Theme', [
-  function() {
-    return {
-      name: 'theme'
-    };
-  }
-]);
--- examples/meanio/k8s-meanio-docker/tools/test/mocha-req.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-process.env.NODE_ENV = 'test';
-var appRoot = __dirname + '/../../';
-require(appRoot + 'server.js');
-require('meanio/lib/core_modules/module/util').preload(appRoot + '/packages/**/server', 'model');
