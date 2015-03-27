243de3a4720f5fa425da70af890509b3bcbcbf7c
Jeff Lowdermilk <jeffml@google.com>
2015-03-27 12:26:00 -0700

    Fix kubectl log for single-container pods

--- pkg/kubectl/cmd/util/helpers.go
+++ pkg/kubectl/cmd/util/helpers.go
@@ -49,7 +49,7 @@ func CheckErr(err error) {
 		if client.IsUnexpectedStatusError(err) {
 			glog.FatalDepth(1, fmt.Sprintf("Unexpected status received from server: %s", err.Error()))
 		}
-		glog.FatalDepth(1, fmt.Sprintf("Error: %s", err.Error()))
+		glog.FatalDepth(1, fmt.Sprintf("Client error: %s", err.Error()))
 	}
 }
 
