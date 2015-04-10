c989f30a009330d050e1399bd2bd5877a355cd07
Tim Hockin <thockin@google.com>
2015-04-10 09:38:12 -0700

    make DeepHashObject use SpewKeys

--- pkg/util/hash.go
+++ pkg/util/hash.go
@@ -27,6 +27,11 @@ import (
 // ensuring the hash does not change when a pointer changes.
 func DeepHashObject(hasher hash.Hash, objectToWrite interface{}) {
 	hasher.Reset()
-	printer := spew.ConfigState{Indent: " ", SortKeys: true}
+	printer := spew.ConfigState{
+		Indent:         " ",
+		SortKeys:       true,
+		DisableMethods: true,
+		SpewKeys:       true,
+	}
 	printer.Fprintf(hasher, "%#v", objectToWrite)
 }
