afb94c43b1ef1ee4750d424215b190f1e36cbd72
Wojciech Tyczynski <wojtekt@google.com>
2015-04-15 14:01:36 +0200

    Fix listing nodes in scheduler

--- pkg/registry/minion/rest.go
+++ pkg/registry/minion/rest.go
@@ -107,7 +107,8 @@ type ResourceGetter interface {
 // NodeToSelectableFields returns a label set that represents the object.
 func NodeToSelectableFields(node *api.Node) fields.Set {
 	return fields.Set{
-		"metadata.name": node.Name,
+		"metadata.name":      node.Name,
+		"spec.unschedulable": fmt.Sprint(node.Spec.Unschedulable),
 	}
 }
 
