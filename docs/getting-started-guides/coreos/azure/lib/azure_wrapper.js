ff00dbe2692ea6cafc863385c3766bb67154aca5
Ido Shamun <idoesh1@gmail.com>
2015-04-18 14:27:30 +0300

    update azure coreos images

--- docs/getting-started-guides/coreos/azure/lib/azure_wrapper.js
+++ docs/getting-started-guides/coreos/azure/lib/azure_wrapper.js
@@ -13,9 +13,9 @@ var inspect = require('util').inspect;
 var util = require('./util.js');
 
 var coreos_image_ids = {
-  'stable': '2b171e93f07c4903bcad35bda10acf22__CoreOS-Stable-607.0.0',
-  'beta': '2b171e93f07c4903bcad35bda10acf22__CoreOS-Beta-612.1.0', // untested
-  'alpha': '2b171e93f07c4903bcad35bda10acf22__CoreOS-Alpha-626.0.0', // untested
+  'stable': '2b171e93f07c4903bcad35bda10acf22__CoreOS-Stable-633.1.0',
+  'beta': '2b171e93f07c4903bcad35bda10acf22__CoreOS-Beta-647.0.0', // untested
+  'alpha': '2b171e93f07c4903bcad35bda10acf22__CoreOS-Alpha-647.0.0' // untested
 };
 
 var conf = {};
