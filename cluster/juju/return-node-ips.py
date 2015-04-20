7802db4862ebbf483ad3300d4635363a569a7583
Matt Bruzek <matthew.bruzek@canonical.com>
2015-04-20 16:57:42 -0500

    Added protetecion if the arguments are not there.

--- cluster/juju/return-node-ips.py
+++ cluster/juju/return-node-ips.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import json
 import sys
-nodes = json.loads(sys.argv[1])
-for num in nodes:
-    print num['Stdout'].rstrip()
+# This script helps parse out the private IP addreses from the
+# `juju run` command's JSON object, see cluster/juju/util.sh
 
+if len(sys.argv) > 1:
+    print(sys.argv[1])
+    # It takes the JSON output as the first argument.
+    nodes = json.loads(sys.argv[1])
+    # There can be multiple nodes to print the Stdout.
+    for num in nodes:
+        print num['Stdout'].rstrip()
