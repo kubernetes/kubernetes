b69792535d478f0c5c95e560192991ea2f91b73f
Matt Bruzek <mbruzek@gmail.com>
2015-04-22 15:05:46 -0500

    Updating guestbook with the v1beta3 json files.

--- cluster/juju/charms/trusty/kubernetes-master/scripts/guestbook.sh
+++ cluster/juju/charms/trusty/kubernetes-master/scripts/guestbook.sh
@@ -13,33 +13,37 @@ if [[ "$1" == "-d" ]] || [[ "$1" == "--debug" ]]; then
 fi
 cd /opt/kubernetes/
 # Step One Turn up the redis master
-kubectl create -f examples/guestbook/redis-master.json
+kubectl create -f examples/guestbook/v1beta3/redis-master.json
 if [[ "${DEBUG}" == true ]]; then
   kubectl get pods
 fi
 # Step Two: Turn up the master service
-kubectl create -f examples/guestbook/redis-master-service.json
+kubectl create -f examples/guestbook/v1beta3/redis-master-service.json
 if [[ "${DEBUG}" == true ]]; then
   kubectl get services
 fi
 # Step Three: Turn up the replicated slave pods
-kubectl create -f examples/guestbook/redis-slave-controller.json
+kubectl create -f examples/guestbook/v1beta3/redis-slave-controller.json
 if [[ "${DEBUG}" == true ]]; then
   kubectl get replicationcontrollers
   kubectl get pods
 fi
 # Step Four: Create the redis slave service
-kubectl create -f examples/guestbook/redis-slave-service.json
+kubectl create -f examples/guestbook/v1beta3/redis-slave-service.json
 if [[ "${DEBUG}" == true ]]; then
   kubectl get services
 fi
 # Step Five: Create the frontend pod
-kubectl create -f examples/guestbook/frontend-controller.json
+kubectl create -f examples/guestbook/v1beta3/frontend-controller.json
 if [[ "${DEBUG}" == true ]]; then
   kubectl get replicationcontrollers
   kubectl get pods
 fi
-
+# Step Six: Create the guestbook service
+kubectl create -f examples/guestbook/v1beta3/frontend-service.json
+if [[ "${DEBUG}" == true ]]; then
+  kubectl get services
+fi
 set +x
 
 echo "# Now run the following commands on your juju client"
