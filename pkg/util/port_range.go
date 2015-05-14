99bdfae7a7a94d47c81eeb2b41892c06feb9d736
Tim Hockin <thockin@google.com>
2015-05-14 13:05:01 -0700

    Pick nits with PortRange
    This should be a "fixup" to the main PortRange comit.

--- pkg/util/port_range.go
+++ pkg/util/port_range.go
@@ -22,22 +22,30 @@ import (
 	"strings"
 )
 
+// PortRange represents a range of TCP/UDP ports.  To represent a single port,
+// set Size to 1.
 type PortRange struct {
 	Base int
 	Size int
 }
 
+// Contains tests whether a given port falls within the PortRange.
 func (pr *PortRange) Contains(p int) bool {
 	return (p >= pr.Base) && ((p - pr.Base) < pr.Size)
 }
 
+// String converts the PortRange to a string representation, which can be
+// parsed by PortRange.Set or ParsePortRange.
 func (pr PortRange) String() string {
 	if pr.Size == 0 {
 		return ""
 	}
-	return fmt.Sprintf("[%d-%d]", pr.Base, pr.Base+pr.Size-1)
+	return fmt.Sprintf("%d-%d", pr.Base, pr.Base+pr.Size-1)
 }
 
+// Set parses a string of the form "min-max", inclusive at both ends, and
+// sets the PortRange from it.  This is part of the flag.Value and pflag.Value
+// interfaces.
 func (pr *PortRange) Set(value string) error {
 	value = strings.TrimSpace(value)
 
@@ -52,7 +60,7 @@ func (pr *PortRange) Set(value string) error {
 
 	hyphenIndex := strings.Index(value, "-")
 	if hyphenIndex == -1 {
-		return fmt.Errorf("Expected hyphen (-) in port range")
+		return fmt.Errorf("expected hyphen in port range")
 	}
 
 	var err error
@@ -63,21 +71,25 @@ func (pr *PortRange) Set(value string) error {
 		high, err = strconv.Atoi(value[hyphenIndex+1:])
 	}
 	if err != nil {
-		return fmt.Errorf("Unable to parse port range: %s", value)
+		return fmt.Errorf("unable to parse port range: %s", value)
 	}
 
 	if high < low {
-		return fmt.Errorf("Port range end port cannot be less than start port: %s", value)
+		return fmt.Errorf("end port cannot be less than start port: %s", value)
 	}
 	pr.Base = low
 	pr.Size = 1 + high - low
 	return nil
 }
 
+// Type returns a descriptive string about this type.  This is part of the
+// pflag.Value interface.
 func (*PortRange) Type() string {
 	return "portRange"
 }
 
+// ParsePortRange parses a string of the form "min-max", inclusive at both
+// ends, and initializs a new PortRange from it.
 func ParsePortRange(value string) (*PortRange, error) {
 	pr := &PortRange{}
 	err := pr.Set(value)
