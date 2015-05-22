2682bb83cc127669ace8decbd829a13ad7f8c7ce
Jeff Grafton <jgrafton@google.com>
2015-05-21 17:39:37 -0700

    DO NOT MERGE - fake change to test Jenkins PR builder

--- hack/ginkgo-e2e.sh
+++ hack/ginkgo-e2e.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# DO NOT SUBMIT - fake change
+
 set -o errexit
 set -o nounset
 set -o pipefail
