ab08d8e8735aa632745b036334b629d8843025ac
jackgr <jackgr@google.com>
2015-05-26 20:25:37 -0700

    Adding files generated by ui build for graph tab.

--- www/master/shared/config/generated-config.js
+++ www/master/shared/config/generated-config.js
@@ -4,7 +4,7 @@ angular.module("kubernetesApp.config", [])
 	"/": {
 		"k8sApiServer": "/api/v1beta2",
 		"k8sApiv1beta3Server": "/api/v1beta3",
-		"k8sDataServer": "/cluster",
+		"k8sDataServer": "",
 		"k8sDataPollMinIntervalSec": 10,
 		"k8sDataPollMaxIntervalSec": 120,
 		"k8sDataPollErrorThreshold": 5,
