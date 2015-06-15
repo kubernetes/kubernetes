3a562f161906edcb4fc5a19c14ca64a33af76131
Marcin Wielgus <mwielgus@google.com>
2015-06-15 18:21:27 +0200

    Check for existing GCE resources in kube-up

--- cluster/gce/config-default.sh
+++ cluster/gce/config-default.sh
@@ -82,3 +82,6 @@ DNS_REPLICAS=1
 
 # Admission Controllers to invoke prior to persisting objects in cluster
 ADMISSION_CONTROL=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ServiceAccount,ResourceQuota
+
+# Optional: if set to true kube-up will automatically check for existing resources and clean them up.
+KUBE_UP_AUTOMATIC_CLEANUP=${KUBE_UP_AUTOMATIC_CLEANUP:-false}
