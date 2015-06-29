d1893bc366304ca271266b4842b85476d9b07d1e
BC Broussard <bc@kismatic.io>
2015-06-29 15:08:14 -0700

    Update Web UI Explore
    Hide blank label dropdowns
    Escape characters in group descriptions and label selectors
    Update label selectors

--- www/app/assets/js/app.js
+++ www/app/assets/js/app.js
@@ -1042,7 +1042,7 @@ app.controller('GroupCtrl', [
     $scope.clearSelector = function(grouping) { $location.path("/dashboard/groups/" + grouping + "/selector/"); };
 
     $scope.changeGroupBy = function() {
-      var grouping = $scope.selectedGroupBy;
+      var grouping = encodeURIComponent($scope.selectedGroupBy);
 
       var s = _.clone($location.search());
       if ($scope.routeParams.grouping != grouping)
@@ -1067,14 +1067,16 @@ app.controller('GroupCtrl', [
       $scope.groupBy = parts;
       $scope.loading = true;
       $scope.selector = selector;
+      $scope.selectorName = decodeURIComponent(selector);
       var args = [];
       var type = "";
+      var selectedHost = "";
       if (selector && selector.length > 0) {
         $scope.selectorPieces = selector.split(",");
         var labels = [];
         var fields = [];
         for (var i = 0; i < $scope.selectorPieces.length; i++) {
-          var piece = $scope.selectorPieces[i];
+          var piece = decodeURIComponent($scope.selectorPieces[i]);
           if (piece[0] == '$') {
             fields.push(piece.slice(2));
           } else {
@@ -1083,13 +1085,21 @@ app.controller('GroupCtrl', [
               if (labelParts.length > 1) {
                 type = labelParts[1];
               }
-            } else {
+            }
+            else if (piece.indexOf("host=") === 0){
+              var labelParts = piece.split("=");
+              if (labelParts.length > 1) {
+                selectedHost = labelParts[1];
+              }
+            }
+            else {
               labels.push(piece);
             }
           }
         }
+
         if (labels.length > 0) {
-          args.push("labels=" + encodeURI(labels.join(",")));
+          args.push("labelSelector=" + encodeURI(labels.join(",")));
         }
         if (fields.length > 0) {
           args.push("fields=" + encodeURI(fields.join(",")));
@@ -1098,6 +1108,9 @@ app.controller('GroupCtrl', [
       var query = "?" + args.join("&");
       var list = [];
       var count = type.length > 0 ? 1 : 3;
+
+      $scope.selectedGroupByName = decodeURIComponent($routeParams.grouping)
+
       var barrier = $scope.createBarrier(count, function() {
         $scope.groups = $scope.groupData(list, 0);
         $scope.loading = false;
@@ -1109,8 +1122,9 @@ app.controller('GroupCtrl', [
         k8sApi.getPods(query).success(function(data) {
           $scope.addLabel("type", "pod", data.items);
           for (var i = 0; data.items && i < data.items.length; ++i) {
-            data.items[i].metadata.labels.host = data.items[i].spec.host;
-            list.push(data.items[i]);
+            data.items[i].metadata.labels.host = data.items[i].spec.nodeName;
+            if(selectedHost.length == 0 || selectedHost == data.items[i].metadata.labels.host)
+              list.push(data.items[i]);
           }
           barrier();
         }).error($scope.handleError);
@@ -1141,7 +1155,7 @@ app.controller('GroupCtrl', [
       }
       for (var i = 0; i < items.length; i++) {
         if (!items[i].metadata.labels) {
-          items[i].metadata.labels = [];
+          items[i].metadata.labels = {};
         }
         items[i].metadata.labels[key] = value;
       }
@@ -1153,7 +1167,7 @@ app.controller('GroupCtrl', [
         "kind": "grouping"
       };
       for (var i = 0; i < items.length; i++) {
-        key = items[i].metadata.labels[$scope.groupBy[index]];
+        key = items[i].metadata.labels[decodeURIComponent($scope.groupBy[index])];
         if (!key) {
           key = "";
         }
