efa76b72ae4c5d50b1ba416638e0097651fc0bd3
Tim Hockin <thockin@google.com>
2015-07-02 14:29:59 -0700

    Run the build once during presubmit

--- hack/verify-generated-deep-copies.sh
+++ hack/verify-generated-deep-copies.sh
@@ -22,7 +22,6 @@ KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
 source "${KUBE_ROOT}/hack/lib/init.sh"
 
 kube::golang::setup_env
-"${KUBE_ROOT}/hack/build-go.sh" cmd/gendeepcopy
 
 genconversion=$(kube::util::find-binary "gendeepcopy")
 
