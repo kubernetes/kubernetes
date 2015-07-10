90d371c98fc9ba586348b85bb1c369a1d63cd99c
nikhiljindal <nikhiljindal@google.com>
2015-07-09 17:49:45 -0700

    Updating nettest image to latest code

--- test/e2e/networking.go
+++ test/e2e/networking.go
@@ -164,7 +164,7 @@ var _ = Describe("Networking", func() {
 				"Rerun it with at least two nodes to get complete coverage.")
 		}
 
-		podNames := LaunchNetTestPodPerNode(f, nodes, svcname, "1.4")
+		podNames := LaunchNetTestPodPerNode(f, nodes, svcname, "1.6")
 
 		// Clean up the pods
 		defer func() {
