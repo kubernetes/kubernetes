90f4c79bb9d7fae99c4f78e43b56423ca2774679
feihujiang <jiangfeihu@huawei.com>
2015-07-13 16:25:22 +0800

    kubectl port-forward accept a pod without -p

--- pkg/kubectl/cmd/portforward_test.go
+++ pkg/kubectl/cmd/portforward_test.go
@@ -85,6 +85,68 @@ func TestPortForward(t *testing.T) {
 			ff.pfErr = fmt.Errorf("pf error")
 		}
 		cmd := &cobra.Command{}
+		cmd.Flags().StringP("pod", "p", "", "Pod name")
+		err := RunPortForward(f, cmd, []string{"foo", ":5000", ":1000"}, ff)
+
+		if test.pfErr && err != ff.pfErr {
+			t.Errorf("%s: Unexpected exec error: %v", test.name, err)
+		}
+		if !test.pfErr && ff.req.URL().Path != test.pfPath {
+			t.Errorf("%s: Did not get expected path for portforward request", test.name)
+		}
+		if !test.pfErr && err != nil {
+			t.Errorf("%s: Unexpected error: %v", test.name, err)
+		}
+	}
+}
+
+func TestPortForwardWithPFlag(t *testing.T) {
+	version := testapi.Version()
+
+	tests := []struct {
+		name, version, podPath, pfPath, container string
+		pod                                       *api.Pod
+		pfErr                                     bool
+	}{
+		{
+			name:    "pod portforward",
+			version: version,
+			podPath: "/api/" + version + "/namespaces/test/pods/foo",
+			pfPath:  "/api/" + version + "/namespaces/test/pods/foo/portforward",
+			pod:     execPod(),
+		},
+		{
+			name:    "pod portforward error",
+			version: version,
+			podPath: "/api/" + version + "/namespaces/test/pods/foo",
+			pfPath:  "/api/" + version + "/namespaces/test/pods/foo/portforward",
+			pod:     execPod(),
+			pfErr:   true,
+		},
+	}
+	for _, test := range tests {
+		f, tf, codec := NewAPIFactory()
+		tf.Client = &client.FakeRESTClient{
+			Codec: codec,
+			Client: client.HTTPClientFunc(func(req *http.Request) (*http.Response, error) {
+				switch p, m := req.URL.Path, req.Method; {
+				case p == test.podPath && m == "GET":
+					body := objBody(codec, test.pod)
+					return &http.Response{StatusCode: 200, Body: body}, nil
+				default:
+					// Ensures no GET is performed when deleting by name
+					t.Errorf("%s: unexpected request: %#v\n%#v", test.name, req.URL, req)
+					return nil, nil
+				}
+			}),
+		}
+		tf.Namespace = "test"
+		tf.ClientConfig = &client.Config{Version: test.version}
+		ff := &fakePortForwarder{}
+		if test.pfErr {
+			ff.pfErr = fmt.Errorf("pf error")
+		}
+		cmd := &cobra.Command{}
 		podPtr := cmd.Flags().StringP("pod", "p", "", "Pod name")
 		*podPtr = "foo"
 		err := RunPortForward(f, cmd, []string{":5000", ":1000"}, ff)
