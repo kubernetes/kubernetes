9b82c4716a1ffbd96ab01150d6e87ed70f95eaa1
wizard <wizard_cxy@hotmail.com>
2015-07-16 11:03:20 +0800

    upgrade k8s to 1.0.1

--- cluster/ubuntu/config-default.sh
+++ cluster/ubuntu/config-default.sh
@@ -20,10 +20,10 @@
 # And separated with blank space like <user_1@ip_1> <user_2@ip_2> <user_3@ip_3> 
 export nodes=${nodes:-"vcap@10.10.103.250 vcap@10.10.103.162 vcap@10.10.103.223"}
 # Define all your nodes role: a(master) or i(minion) or ai(both master and minion), must be the order same 
-roles=${roles:-"ai i i"}
+role=${role:-"ai i i"}
 # If it practically impossible to set an array as an environment variable
 # from a script, so assume variable is a string then convert it to an array
-export roles=($roles)
+export roles=($role)
 
 # Define minion numbers
 export NUM_MINIONS=${NUM_MINIONS:-3}
@@ -32,8 +32,7 @@ export NUM_MINIONS=${NUM_MINIONS:-3}
 export SERVICE_CLUSTER_IP_RANGE=${SERVICE_CLUSTER_IP_RANGE:-192.168.3.0/24}  # formerly PORTAL_NET
 # define the IP range used for flannel overlay network, should not conflict with above SERVICE_CLUSTER_IP_RANGE
 export FLANNEL_NET=${FLANNEL_NET:-172.16.0.0/16}
-echo "FLANNEL_NET"
-echo $FLANNEL_NET
+
 export FLANNEL_OPTS=${FLANNEL_OPTS:-"Network": 172.16.0.0/16}
 
 # Admission Controllers to invoke prior to persisting objects in cluster
