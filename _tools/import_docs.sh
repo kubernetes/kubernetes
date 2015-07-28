98521df9bd9e132c513bef166fb5c61f9cd8569b
Eric Tune <etune@google.com>
2015-07-28 16:01:26 -0700

    Manually revert pull request #11840

--- _tools/import_docs.sh
+++ _tools/import_docs.sh
@@ -29,12 +29,11 @@ rmdir $tmpdir
 echo stripping
 for dir in docs examples; do
     find ${OUTDIR}/${dir} -type f -name \*.md | while read X; do
-        sed -i .import_docs_tmp \
+        $SED -i \
             -e '/<!-- BEGIN STRIP_FOR_RELEASE.*/,/<!-- END STRIP_FOR_RELEASE.*/d' \
             -e "s|releases.k8s.io/HEAD|releases.k8s.io/${REPORT_TAG}|g" \
             ${X}
     done
-    rm $(find ${OUTDIR}/${dir} -name \*.import_docs_tmp)
     git stage ${OUTDIR}/${dir}
 done
 
