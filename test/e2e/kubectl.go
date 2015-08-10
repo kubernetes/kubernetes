0f6ed340b1256426f6436ad736ef087851c5ea15
Amy Unruh <amyu@google.com>
2015-08-10 11:00:44 -0700

    Use GCR images from 'google-samples' project; allow switch on whether dns service is supported, or to use env vars to get service host info.
    Test change to reflect php filename change.

--- test/e2e/kubectl.go
+++ test/e2e/kubectl.go
@@ -746,7 +746,7 @@ func makeRequestToGuestbook(c *client.Client, cmd, value string, ns string) (str
 		Namespace(ns).
 		Resource("services").
 		Name("frontend").
-		Suffix("/index.php").
+		Suffix("/guestbook.php").
 		Param("cmd", cmd).
 		Param("key", "messages").
 		Param("value", value).
