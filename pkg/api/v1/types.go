8fbe4645e35fe9d282d6aba14b35566396c61cb7
derekwaynecarr <decarr@redhat.com>
2015-08-24 15:18:45 -0400

    Add fields to LimitRange object

--- pkg/api/v1/types.go
+++ pkg/api/v1/types.go
@@ -2327,9 +2327,12 @@ type LimitRangeItem struct {
 	Max ResourceList `json:"max,omitempty"`
 	// Min usage constraints on this kind by resource name.
 	Min ResourceList `json:"min,omitempty"`
-	// Default usage constraints on this kind by resource name.
-	// Default values on this kind by resource name if omitted.
+	// Default resource requirement limit value by resource name if resource limit is omitted.
 	Default ResourceList `json:"default,omitempty"`
+	// DefaultRequest is the default resource requirement request value by resource name if resource request is omitted.
+	DefaultRequest ResourceList `json:"defaultRequest,omitempty"`
+	// MaxLimitRequestRatio if specified, the named resource must have a request and limit that are both non-zero where limit divided by request is less than or equal to the enumerated value; this represents the max burst for the named resource.
+	MaxLimitRequestRatio ResourceList `json:"maxLimitRequestRatio,omitempty"`
 }
 
 // LimitRangeSpec defines a min/max usage limit for resources that match on kind.
