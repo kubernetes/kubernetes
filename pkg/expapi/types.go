8f9444025f95035e30bfecf43aa7edeb96d2650f
markturansky <mturansk@redhat.com>
2015-09-02 22:46:36 -0400

    removed expapi descriptions from internal types

--- pkg/expapi/types.go
+++ pkg/expapi/types.go
@@ -355,7 +355,9 @@ type DaemonList struct {
 
 type ThirdPartyResourceDataList struct {
 	api.TypeMeta `json:",inline"`
-	api.ListMeta `json:"metadata,omitempty" description:"standard list metadata; see http://docs.k8s.io/api-conventions.md#metadata"`
-
-	Items []ThirdPartyResourceData `json:"items" description:"items is a list of third party objects"`
+	// Standard list metadata
+	// More info: http://docs.k8s.io/api-conventions.md#metadata
+	api.ListMeta `json:"metadata,omitempty"`
+	// Items is a list of third party objects
+	Items []ThirdPartyResourceData `json:"items"`
 }
