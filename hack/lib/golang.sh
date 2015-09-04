b25e1fda5fa3609b3e59681af3178779ad852238
Paul Tiplady <paultiplady@gmail.com>
2015-09-04 12:13:48 -0700

    Revert "Temporary hack to disable building on unsupported platforms."
    This reverts commit b8bd17a7a97044aa309a488af242d7f71f5bede3.

--- hack/lib/golang.sh
+++ hack/lib/golang.sh
@@ -97,6 +97,9 @@ readonly KUBE_CLIENT_PLATFORMS=(
   linux/amd64
   linux/386
   linux/arm
+  darwin/amd64
+  darwin/386
+  windows/amd64
 )
 
 # Gigabytes desired for parallel platform builds. 11 is fairly
