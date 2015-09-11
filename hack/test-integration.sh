7ff4f8912fc7299e37af26533ca44c349a4f3afe
Chao Xu <xuchao@google.com>
2015-09-11 15:49:44 -0700

    register experimental/v1 in test-cmd.sh and test-integration.sh

--- hack/test-integration.sh
+++ hack/test-integration.sh
@@ -47,12 +47,12 @@ runTests() {
   KUBE_GOFLAGS="-tags 'integration no-docker' " \
     KUBE_RACE="-race" \
     KUBE_TEST_API_VERSIONS="$1" \
-    KUBE_API_VERSIONS="v1" \
+    KUBE_API_VERSIONS="v1,experimental/v1" \
     "${KUBE_ROOT}/hack/test-go.sh" test/integration
 
   kube::log::status "Running integration test scenario"
 
-  KUBE_API_VERSIONS="v1" KUBE_TEST_API_VERSIONS="$1" "${KUBE_OUTPUT_HOSTBIN}/integration" --v=${LOG_LEVEL} \
+  KUBE_API_VERSIONS="v1,experimental/v1" KUBE_TEST_API_VERSIONS="$1" "${KUBE_OUTPUT_HOSTBIN}/integration" --v=${LOG_LEVEL} \
     --max-concurrency="${KUBE_INTEGRATION_TEST_MAX_CONCURRENCY}"
 
   cleanup
