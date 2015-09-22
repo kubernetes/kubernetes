34eaebcbe142792b4ad45a5c7a4a82037da2efab
Prashanth Balasubramanian <beeps@google.com>
2015-09-22 11:26:36 -0700

    Ingress registry

---
+++ pkg/registry/ingress/etcd/etcd.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package etcd
+
+import (
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/experimental"
+	"k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/labels"
+	"k8s.io/kubernetes/pkg/registry/generic"
+	etcdgeneric "k8s.io/kubernetes/pkg/registry/generic/etcd"
+	ingress "k8s.io/kubernetes/pkg/registry/ingress"
+	"k8s.io/kubernetes/pkg/runtime"
+	"k8s.io/kubernetes/pkg/storage"
+)
+
+const (
+	IngressPath string = "/ingress"
+)
+
+// rest implements a RESTStorage for replication controllers against etcd
+type REST struct {
+	*etcdgeneric.Etcd
+}
+
+// NewREST returns a RESTStorage object that will work against replication controllers.
+func NewREST(s storage.Interface) *REST {
+	store := &etcdgeneric.Etcd{
+		NewFunc: func() runtime.Object { return &experimental.Ingress{} },
+
+		// NewListFunc returns an object capable of storing results of an etcd list.
+		NewListFunc: func() runtime.Object { return &experimental.IngressList{} },
+		// Produces a ingress that etcd understands, to the root of the resource
+		// by combining the namespace in the context with the given prefix
+		KeyRootFunc: func(ctx api.Context) string {
+			return etcdgeneric.NamespaceKeyRootFunc(ctx, IngressPath)
+		},
+		// Produces a ingress that etcd understands, to the resource by combining
+		// the namespace in the context with the given prefix
+		KeyFunc: func(ctx api.Context, name string) (string, error) {
+			return etcdgeneric.NamespaceKeyFunc(ctx, IngressPath, name)
+		},
+		// Retrieve the name field of a replication controller
+		ObjectNameFunc: func(obj runtime.Object) (string, error) {
+			return obj.(*experimental.Ingress).Name, nil
+		},
+		// Used to match objects based on labels/fields for list and watch
+		PredicateFunc: func(label labels.Selector, field fields.Selector) generic.Matcher {
+			return ingress.MatchIngress(label, field)
+		},
+		EndpointName: "ingress",
+
+		// Used to validate controller creation
+		CreateStrategy: ingress.Strategy,
+
+		// Used to validate controller updates
+		UpdateStrategy: ingress.Strategy,
+
+		Storage: s,
+	}
+
+	return &REST{store}
+}
