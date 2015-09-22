f8c0057c69e45b8713fb51c262a9a80849b1fb34
Chao Xu <xuchao@google.com>
2015-09-22 11:23:55 -0700

    fix the package in cmd/kube-version-change/import_known_versions.go

--- cmd/kube-version-change/import_known_versions.go
+++ cmd/kube-version-change/import_known_versions.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package master
+package main
 
 // These imports are the API groups the kube-version-change tool will support.
 import (
