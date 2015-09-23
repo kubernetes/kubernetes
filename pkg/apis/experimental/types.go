9779bf68efa090ded41fde9b0098365a0b974c9b
Prashanth Balasubramanian <beeps@google.com>
2015-09-22 20:20:13 -0700

    Validation

--- pkg/apis/experimental/types.go
+++ pkg/apis/experimental/types.go
@@ -558,7 +558,12 @@ type HTTPIngressRuleValue struct {
 // IngressPath associates a path regex with an IngressBackend.
 // Incoming urls matching the Path are forwarded to the Backend.
 type HTTPIngressPath struct {
-	// Path is a regex matched against the url of an incoming request.
+	// Path is a extended POSIX regex as defined by IEEE Std 1003.1,
+	// (i.e this follows the egrep/unix syntax, not the perl syntax)
+	// matched against the path of an incoming request. Currently it can
+	// contain characters disallowed from the conventional "path"
+	// part of a URL as defined by RFC 3986. Paths must begin with
+	// a '/'.
 	Path string `json:"path,omitempty"`
 
 	// Define the referenced service endpoint which the traffic will be
@@ -572,5 +577,5 @@ type IngressBackend struct {
 	ServiceName string `json:"serviceName"`
 
 	// Specifies the port of the referenced service.
-	ServicePort util.IntOrString `json:"servicePort,omitempty"`
+	ServicePort util.IntOrString `json:"servicePort"`
 }
