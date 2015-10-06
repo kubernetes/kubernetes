09b761a5b38e1cb71df8061b078a9ff9aa01d42f
Mike Danese <mikedanese@gmail.com>
2015-10-06 16:54:53 -0700

    fix bug in daemonset

--- pkg/controller/daemon/controller.go
+++ pkg/controller/daemon/controller.go
@@ -448,13 +448,18 @@ func (dsc *DaemonSetsController) updateDaemonSetStatus(ds *experimental.DaemonSe
 		shouldRun := nodeSelector.Matches(labels.Set(node.Labels))
 		numDaemonPods := len(nodeToDaemonPods[node.Name])
 
-		if numDaemonPods > 0 {
+		// TODO(mikedanese): this does not count nodes that should be running
+		// exactly one daemon pod but are running more than one daemon pods.
+
+		if shouldRun && numDaemonPods == 1 {
 			currentNumberScheduled++
 		}
 
 		if shouldRun {
 			desiredNumberScheduled++
-		} else if numDaemonPods >= 0 {
+		}
+
+		if !shouldRun && numDaemonPods > 0 {
 			numberMisscheduled++
 		}
 	}
