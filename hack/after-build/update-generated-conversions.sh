cc4ec80ec74019e49835857b2291d16cae3b7ad7
Chao Xu <xuchao@google.com>
2015-10-09 15:19:13 -0700

    in scripts, experimental/v1alpha1->extensions/v1beta1

--- hack/after-build/update-generated-conversions.sh
+++ hack/after-build/update-generated-conversions.sh
@@ -43,7 +43,7 @@ EOF
 }
 
 # TODO(lavalamp): get this list by listing the pkg/apis/ directory?
-DEFAULT_GROUP_VERSIONS="v1 experimental/v1alpha1"
+DEFAULT_GROUP_VERSIONS="v1 extensions/v1beta1"
 VERSIONS=${VERSIONS:-$DEFAULT_GROUP_VERSIONS}
 for ver in $VERSIONS; do
   # Ensure that the version being processed is registered by setting
