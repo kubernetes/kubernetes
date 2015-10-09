6c55b6697d7a7012d68d320aa5a8dba49360043c
Chao Xu <xuchao@google.com>
2015-10-08 20:30:24 -0700

    run goimports

--- pkg/kubectl/resource/helper.go
+++ pkg/kubectl/resource/helper.go
@@ -17,7 +17,6 @@ limitations under the License.
 package resource
 
 import (
-	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/meta"
 	"k8s.io/kubernetes/pkg/labels"
 	"k8s.io/kubernetes/pkg/runtime"
