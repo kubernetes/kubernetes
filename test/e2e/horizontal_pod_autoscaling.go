ba996944f0746fb021df73775ab1adb76a4547bd
Jerzy Szczepkowski <jsz@google.com>
2015-10-13 17:24:23 +0200

    HorizontalPodAutoscaler API: removal of ResourceConsumption target, introduction of CPU request utilization & other cleanups.

--- test/e2e/horizontal_pod_autoscaling.go
+++ test/e2e/horizontal_pod_autoscaling.go
@@ -17,10 +17,7 @@ limitations under the License.
 package e2e
 
 import (
-	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/api/resource"
-	"k8s.io/kubernetes/pkg/apis/extensions"
-
+	"github.com/golang/glog"
 	. "github.com/onsi/ginkgo"
 )
 
@@ -34,7 +31,45 @@ var _ = Describe("Horizontal pod autoscaling", func() {
 	f := NewFramework("horizontal-pod-autoscaling")
 
 	// CPU tests
-	It("[Skipped][Autoscaling Suite] should scale from 1 pod to 3 pods (scale resource: CPU)", func() {
+	It("[Skipped] should scale from 1 pod to 3 pods and from 3 to 5 (scale resource: CPU)", func() {
+		rc = NewDynamicResourceConsumer("rc", 1, 250, 0, 400, 100, f)
+		defer rc.CleanUp()
+		createCPUHorizontalPodAutoscaler(rc, "0.1")
+		rc.WaitForReplicas(3)
+		rc.ConsumeCPU(700)
+		rc.WaitForReplicas(5)
+	})
+
+	It("[Skipped] should scale from 5 pods to 3 pods and from 3 to 1 (scale resource: CPU)", func() {
+		rc = NewDynamicResourceConsumer("rc", 5, 700, 0, 200, 100, f)
+		defer rc.CleanUp()
+		createCPUHorizontalPodAutoscaler(rc, "0.3")
+		rc.WaitForReplicas(3)
+		rc.ConsumeCPU(100)
+		rc.WaitForReplicas(1)
+	})
+
+	// Memory tests
+	It("[Skipped] should scale from 1 pod to 3 pods and from 3 to 5 (scale resource: Memory)", func() {
+		rc = NewDynamicResourceConsumer("rc", 1, 0, 2200, 100, 2500, f)
+		defer rc.CleanUp()
+		createMemoryHorizontalPodAutoscaler(rc, "1000")
+		rc.WaitForReplicas(3)
+		rc.ConsumeMem(4200)
+		rc.WaitForReplicas(5)
+	})
+
+	It("[Skipped] should scale from 5 pods to 3 pods and from 3 to 1 (scale resource: Memory)", func() {
+		rc = NewDynamicResourceConsumer("rc", 5, 0, 2200, 100, 2500, f)
+		defer rc.CleanUp()
+		createMemoryHorizontalPodAutoscaler(rc, "1000")
+		rc.WaitForReplicas(3)
+		rc.ConsumeMem(100)
+		rc.WaitForReplicas(1)
+	})
+
+	// Backup tests, currently disabled
+	It("[Skipped][Horizontal pod autoscaling Suite] should scale from 1 pod to 3 pods (scale resource: CPU)", func() {
 		rc = NewDynamicResourceConsumer("rc", 1, 700, 0, 800, 100, f)
 		createCPUHorizontalPodAutoscaler(rc, "0.3")
 		rc.WaitForReplicas(3)
@@ -151,46 +186,12 @@ var _ = Describe("Horizontal pod autoscaling", func() {
 })
 
 func createCPUHorizontalPodAutoscaler(rc *ResourceConsumer, cpu string) {
-	hpa := &extensions.HorizontalPodAutoscaler{
-		ObjectMeta: api.ObjectMeta{
-			Name:      rc.name,
-			Namespace: rc.framework.Namespace.Name,
-		},
-		Spec: extensions.HorizontalPodAutoscalerSpec{
-			ScaleRef: &extensions.SubresourceReference{
-				Kind:        kind,
-				Name:        rc.name,
-				Namespace:   rc.framework.Namespace.Name,
-				Subresource: subresource,
-			},
-			MinReplicas: 1,
-			MaxReplicas: 5,
-			Target:      extensions.ResourceConsumption{Resource: api.ResourceCPU, Quantity: resource.MustParse(cpu)},
-		},
-	}
-	_, errHPA := rc.framework.Client.Extensions().HorizontalPodAutoscalers(rc.framework.Namespace.Name).Create(hpa)
-	expectNoError(errHPA)
+	glog.Fatal("createCPUHorizontalPodAutoscaler not implemented!")
+	// TODO: reimplemente e2e tests for the new API.
 }
 
 // argument memory is in megabytes
 func createMemoryHorizontalPodAutoscaler(rc *ResourceConsumer, memory string) {
-	hpa := &extensions.HorizontalPodAutoscaler{
-		ObjectMeta: api.ObjectMeta{
-			Name:      rc.name,
-			Namespace: rc.framework.Namespace.Name,
-		},
-		Spec: extensions.HorizontalPodAutoscalerSpec{
-			ScaleRef: &extensions.SubresourceReference{
-				Kind:        kind,
-				Name:        rc.name,
-				Namespace:   rc.framework.Namespace.Name,
-				Subresource: subresource,
-			},
-			MinReplicas: 1,
-			MaxReplicas: 5,
-			Target:      extensions.ResourceConsumption{Resource: api.ResourceMemory, Quantity: resource.MustParse(memory + "M")},
-		},
-	}
-	_, errHPA := rc.framework.Client.Extensions().HorizontalPodAutoscalers(rc.framework.Namespace.Name).Create(hpa)
-	expectNoError(errHPA)
+	glog.Fatal("createMemoryHorizontalPodAutoscaler not implemented!")
+	// TODO: reimplemente e2e tests for the new API.
 }
