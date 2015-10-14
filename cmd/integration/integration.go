e3ca3b8ae6a137b4473b25d4d0599b74274970c4
Abhishek Shah <abshah@google.com>
2015-10-14 10:34:29 -0700

    SkyDNS is the only NS for Pods with DNSPolicy=ClusterFirst

--- cmd/integration/integration.go
+++ cmd/integration/integration.go
@@ -248,7 +248,7 @@ func startComponents(firstManifestURL, secondManifestURL string) (string, string
 		3*time.Second,  /* NodeStatusUpdateFrequency */
 		10*time.Second, /* SyncFrequency */
 		40,             /* MaxPods */
-		cm)
+		cm, net.ParseIP("127.0.0.1"))
 
 	kubeletapp.RunKubelet(kcfg)
 	// Kubelet (machine)
@@ -281,7 +281,8 @@ func startComponents(firstManifestURL, secondManifestURL string) (string, string
 		10*time.Second, /* SyncFrequency */
 
 		40, /* MaxPods */
-		cm)
+		cm,
+		net.ParseIP("127.0.0.1"))
 
 	kubeletapp.RunKubelet(kcfg)
 	return apiServer.URL, configFilePath
