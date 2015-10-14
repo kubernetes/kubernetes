7861cd69e9eeffca765c0b2a8039f12f833401ad
Mike Danese <mikedanese@gmail.com>
2015-10-14 11:04:12 -0700

    autogenerated api changes

--- pkg/apis/extensions/v1beta1/types_swagger_doc_generated.go
+++ pkg/apis/extensions/v1beta1/types_swagger_doc_generated.go
@@ -363,6 +363,27 @@ func (NodeUtilization) SwaggerDoc() map[string]string {
 	return map_NodeUtilization
 }
 
+var map_PodSelector = map[string]string{
+	"":                 "A pod selector is a label query over a set of pods. The result of matchLabels and matchExpressions are ANDed. An empty pod selector matches all objects. A null pod selector matches no objects.",
+	"matchLabels":      "matchLabels is a map of {key,value} pairs. A single {key,value} in the matchLabels map is equivalent to an element of matchExpressions, whose key field is \"key\", the operator is \"In\", and the values array contains only \"value\". The requirements are ANDed.",
+	"matchExpressions": "matchExpressions is a list of pod selector requirements. The requirements are ANDed.",
+}
+
+func (PodSelector) SwaggerDoc() map[string]string {
+	return map_PodSelector
+}
+
+var map_PodSelectorRequirement = map[string]string{
+	"":         "A pod selector requirement is a selector that contains values, a key, and an operator that relates the key and values.",
+	"key":      "key is the label key that the selector applies to.",
+	"operator": "operator represents a key's relationship to a set of values. Valid operators ard In, NotIn, Exists and DoesNotExist.",
+	"values":   "values is an array of string values. If the operator is In or NotIn, the values array must be non-empty. If the operator is Exists or DoesNotExist, the values array must be empty. This array is replaced during a strategic merge patch.",
+}
+
+func (PodSelectorRequirement) SwaggerDoc() map[string]string {
+	return map_PodSelectorRequirement
+}
+
 var map_ReplicationControllerDummy = map[string]string{
 	"": "Dummy definition",
 }
