d269a4447b4ec54fbd51717d26e9bd30a623e4fe
Jordan Liggitt <jliggitt@redhat.com>
2015-10-18 21:26:39 -0400

    Platform-specific setRLimit implementations

--- pkg/proxy/userspace/proxier.go
+++ pkg/proxy/userspace/proxier.go
@@ -23,7 +23,6 @@ import (
 	"strings"
 	"sync"
 	"sync/atomic"
-	"syscall"
 	"time"
 
 	"github.com/golang/glog"
@@ -156,10 +155,6 @@ func NewProxier(loadBalancer LoadBalancer, listenIP net.IP, iptables iptables.In
 
 	glog.V(2).Infof("Setting proxy IP to %v and initializing iptables", hostIP)
 	return createProxier(loadBalancer, listenIP, iptables, hostIP, proxyPorts, syncPeriod)
-}
-
-func setRLimit(limit uint64) error {
-	return syscall.Setrlimit(syscall.RLIMIT_NOFILE, &syscall.Rlimit{Max: limit, Cur: limit})
 }
 
 func createProxier(loadBalancer LoadBalancer, listenIP net.IP, iptables iptables.Interface, hostIP net.IP, proxyPorts PortAllocator, syncPeriod time.Duration) (*Proxier, error) {
