71bd54adc619f882257cc1987941ad7d6ae32481
Sergiusz Urbaniak <sur@mesosphere.io>
2015-10-20 14:40:24 -0700

    scheduler: remove shadowed `offer` var in doSchedule
    The variable `offer` is shadowed in the if block causing the `then`
    branch to be non-effective.

--- contrib/mesos/pkg/scheduler/plugin.go
+++ contrib/mesos/pkg/scheduler/plugin.go
@@ -313,11 +313,7 @@ func (k *kubeScheduler) doSchedule(task *podtask.T) (string, error) {
 
 	if task.HasAcceptedOffer() {
 		// verify that the offer is still on the table
-		offerId := task.GetOfferId()
-		if offer, ok := k.api.offers().Get(offerId); ok && !offer.HasExpired() {
-			// skip tasks that have already have assigned offers
-			offer = task.Offer
-		} else {
+		if offer, ok := k.api.offers().Get(task.GetOfferId()); !ok || offer.HasExpired() {
 			task.Offer.Release()
 			task.Reset()
 			if err = k.api.tasks().Update(task); err != nil {
