d683200bf89f581e9644fb93ac10862f2da8b413
qiaolei <qiaolei2@huawei.com>
2015-10-21 20:39:07 +0800

    Fixed some typos and improved some comments

--- pkg/proxy/iptables/proxier.go
+++ pkg/proxy/iptables/proxier.go
@@ -87,7 +87,7 @@ func ShouldUseIptablesProxier() (bool, error) {
 	}
 
 	// Check for the required sysctls.  We don't care about the value, just
-	// that it exists.  If this Proxier is chosen, we'll iniialize it as we
+	// that it exists.  If this Proxier is chosen, we'll initialize it as we
 	// need.
 	_, err = utilsysctl.GetSysctl(sysctlRouteLocalnet)
 	if err != nil {
