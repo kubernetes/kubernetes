9c4424f0bd55b8f2fb4610a4616fbd5cd30abc9a
Madhusudan.C.S <madhusudancs@google.com>
2015-10-22 12:14:56 -0700

    Report out of disk as a node condition when node goes out of disk.
    Define a new out of disk node condition and use it to report when node
    goes out of disk.
    
    Make a copy of loop range clause variable in node listers so that it
    is available outside the for loop.
    
    Also update/implement unit tests.

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -1491,6 +1491,9 @@ type NodeConditionType string
 const (
 	// NodeReady means kubelet is healthy and ready to accept pods.
 	NodeReady NodeConditionType = "Ready"
+	// NodeOutOfDisk means the kubelet will not accept new pods due to insufficient free disk
+	// space on the node.
+	NodeOutOfDisk NodeConditionType = "OutOfDisk"
 )
 
 type NodeCondition struct {
--- pkg/api/v1/types.go
+++ pkg/api/v1/types.go
@@ -1868,6 +1868,9 @@ type NodeConditionType string
 const (
 	// NodeReady means kubelet is healthy and ready to accept pods.
 	NodeReady NodeConditionType = "Ready"
+	// NodeOutOfDisk means the kubelet will not accept new pods due to insufficient free disk
+	// space on the node.
+	NodeOutOfDisk NodeConditionType = "OutOfDisk"
 )
 
 // NodeCondition contains condition infromation for a node.
