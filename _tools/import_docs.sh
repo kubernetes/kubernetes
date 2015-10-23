3ae283023ae8c811f501b06724a8885cc3d378e9
Tim Hockin <thockin@google.com>
2015-10-23 14:18:26 -0700

    make import_docs version safe

--- _tools/import_docs.sh
+++ _tools/import_docs.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 # This is a hack to import from a branch and call it a versioned snapshot.
 
-VERSION=v1.0
-OUTDIR=v1.0
-REPORT_TAG=v1.0.1
-BRANCH=release-1.0
+if [ $# != 4 ]; then
+    echo "usage: $0 <version> <outdir> <report_tag> <branch>"
+    echo
+    echo "ARGS:"
+    echo "  version: the major.minor version to report (matches the k8s.io URL)"
+    echo "  outdir: where to write the output of this tool"
+    echo "  report_tag: what string to report was imported (usually a git tag)"
+    echo "  branch: what git branch to pull docs from"
+    echo
+    echo "example: $0 v1.0 _v1.0 v1.0.6 release-1.0"
+    exit 1
+fi
+
+VERSION=$1
+OUTDIR=$2
+REPORT_TAG=$3
+BRANCH=$4
 
 set -e
 
@@ -32,6 +45,7 @@ echo adding new
 mv $tmpdir/docs ${OUTDIR}
 mv $tmpdir/examples ${OUTDIR}
 git add ${OUTDIR}/docs/ ${OUTDIR}/examples/ > /dev/null
+git add _includes/*{definitions,operations}.html
 rmdir $tmpdir
 
 echo stripping
