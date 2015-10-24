4ef96babef54a0643c381ee9e79a092a30f18ab3
Justin Santa Barbara <justin@fathomdb.com>
2015-10-24 18:01:34 -0400

    Bump aws-sdk-go version to workaround #16238
    This bumps aws-sdk-go to the latest version (currently unreleased), to
    workaround a Godep problem when a package listed in Godeps no longer
    exists on the master branch.  See #16238 for the gory details.
    
    Fixes #16238.
    
    We will likely want to bump up again to the next released version once
    it is released, but this version appears to be pretty similar to the
    last released version (0.9.16)

--- Godeps/_workspace/src/github.com/aws/aws-sdk-go/private/protocol/ec2query/build.go
+++ Godeps/_workspace/src/github.com/aws/aws-sdk-go/private/protocol/ec2query/build.go
@@ -1,25 +1,24 @@
-// Package query provides serialisation of AWS query requests, and responses.
-package query
+// Package ec2query provides serialisation of AWS EC2 requests and responses.
+package ec2query
 
-//go:generate go run ../../fixtures/protocol/generate.go ../../fixtures/protocol/input/query.json build_test.go
+//go:generate go run ../../../models/protocol_tests/generate.go ../../../models/protocol_tests/input/ec2.json build_test.go
 
 import (
 	"net/url"
 
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/request"
-	"github.com/aws/aws-sdk-go/internal/protocol/query/queryutil"
+	"github.com/aws/aws-sdk-go/private/protocol/query/queryutil"
 )
 
-// Build builds a request for an AWS Query service.
+// Build builds a request for the EC2 protocol.
 func Build(r *request.Request) {
 	body := url.Values{
 		"Action":  {r.Operation.Name},
 		"Version": {r.Service.APIVersion},
 	}
-	if err := queryutil.Parse(body, r.Params, false); err != nil {
-		r.Error = awserr.New("SerializationError", "failed encoding Query request", err)
-		return
+	if err := queryutil.Parse(body, r.Params, true); err != nil {
+		r.Error = awserr.New("SerializationError", "failed encoding EC2 Query request", err)
 	}
 
 	if r.ExpireTime == 0 {
--- Godeps/_workspace/src/github.com/aws/aws-sdk-go/private/protocol/rest/build.go
+++ Godeps/_workspace/src/github.com/aws/aws-sdk-go/private/protocol/rest/build.go
@@ -6,6 +6,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
+	"net/http"
 	"net/url"
 	"path"
 	"reflect"
@@ -22,6 +23,8 @@ const RFC822 = "Mon, 2 Jan 2006 15:04:05 GMT"
 
 // Whether the byte value can be sent without escaping in AWS URLs
 var noEscape [256]bool
+
+var errValueNotSet = fmt.Errorf("value not set")
 
 func init() {
 	for i := 0; i < len(noEscape); i++ {
@@ -67,16 +70,18 @@ func buildLocationElements(r *request.Request, v reflect.Value) {
 				continue
 			}
 
+			var err error
 			switch field.Tag.Get("location") {
 			case "headers": // header maps
-				buildHeaderMap(r, m, field.Tag.Get("locationName"))
+				err = buildHeaderMap(&r.HTTPRequest.Header, m, field.Tag.Get("locationName"))
 			case "header":
-				buildHeader(r, m, name)
+				err = buildHeader(&r.HTTPRequest.Header, m, name)
 			case "uri":
-				buildURI(r, m, name)
+				err = buildURI(r.HTTPRequest.URL, m, name)
 			case "querystring":
-				buildQueryString(r, m, name, query)
+				err = buildQueryString(query, m, name)
 			}
+			r.Error = err
 		}
 		if r.Error != nil {
 			return
@@ -112,45 +117,77 @@ func buildBody(r *request.Request, v reflect.Value) {
 	}
 }
 
-func buildHeader(r *request.Request, v reflect.Value, name string) {
+func buildHeader(header *http.Header, v reflect.Value, name string) error {
 	str, err := convertType(v)
-	if err != nil {
-		r.Error = awserr.New("SerializationError", "failed to encode REST request", err)
-	} else if str != nil {
-		r.HTTPRequest.Header.Add(name, *str)
+	if err == errValueNotSet {
+		return nil
+	} else if err != nil {
+		return awserr.New("SerializationError", "failed to encode REST request", err)
 	}
+
+	header.Add(name, str)
+
+	return nil
 }
 
-func buildHeaderMap(r *request.Request, v reflect.Value, prefix string) {
+func buildHeaderMap(header *http.Header, v reflect.Value, prefix string) error {
 	for _, key := range v.MapKeys() {
 		str, err := convertType(v.MapIndex(key))
-		if err != nil {
-			r.Error = awserr.New("SerializationError", "failed to encode REST request", err)
-		} else if str != nil {
-			r.HTTPRequest.Header.Add(prefix+key.String(), *str)
+		if err == errValueNotSet {
+			continue
+		} else if err != nil {
+			return awserr.New("SerializationError", "failed to encode REST request", err)
+
 		}
+
+		header.Add(prefix+key.String(), str)
 	}
+	return nil
 }
 
-func buildURI(r *request.Request, v reflect.Value, name string) {
+func buildURI(u *url.URL, v reflect.Value, name string) error {
 	value, err := convertType(v)
-	if err != nil {
-		r.Error = awserr.New("SerializationError", "failed to encode REST request", err)
-	} else if value != nil {
-		uri := r.HTTPRequest.URL.Path
-		uri = strings.Replace(uri, "{"+name+"}", EscapePath(*value, true), -1)
-		uri = strings.Replace(uri, "{"+name+"+}", EscapePath(*value, false), -1)
-		r.HTTPRequest.URL.Path = uri
+	if err == errValueNotSet {
+		return nil
+	} else if err != nil {
+		return awserr.New("SerializationError", "failed to encode REST request", err)
 	}
+
+	uri := u.Path
+	uri = strings.Replace(uri, "{"+name+"}", EscapePath(value, true), -1)
+	uri = strings.Replace(uri, "{"+name+"+}", EscapePath(value, false), -1)
+	u.Path = uri
+
+	return nil
 }
 
-func buildQueryString(r *request.Request, v reflect.Value, name string, query url.Values) {
-	str, err := convertType(v)
-	if err != nil {
-		r.Error = awserr.New("SerializationError", "failed to encode REST request", err)
-	} else if str != nil {
-		query.Set(name, *str)
+func buildQueryString(query url.Values, v reflect.Value, name string) error {
+	switch value := v.Interface().(type) {
+	case []*string:
+		for _, item := range value {
+			query.Add(name, *item)
+		}
+	case map[string]*string:
+		for key, item := range value {
+			query.Add(key, *item)
+		}
+	case map[string][]*string:
+		for key, items := range value {
+			for _, item := range items {
+				query.Add(key, *item)
+			}
+		}
+	default:
+		str, err := convertType(v)
+		if err == errValueNotSet {
+			return nil
+		} else if err != nil {
+			return awserr.New("SerializationError", "failed to encode REST request", err)
+		}
+		query.Set(name, str)
 	}
+
+	return nil
 }
 
 func updatePath(url *url.URL, urlPath string) {
@@ -189,10 +226,10 @@ func EscapePath(path string, encodeSep bool) string {
 	return buf.String()
 }
 
-func convertType(v reflect.Value) (*string, error) {
+func convertType(v reflect.Value) (string, error) {
 	v = reflect.Indirect(v)
 	if !v.IsValid() {
-		return nil, nil
+		return "", errValueNotSet
 	}
 
 	var str string
@@ -211,7 +248,7 @@ func convertType(v reflect.Value) (*string, error) {
 		str = value.UTC().Format(RFC822)
 	default:
 		err := fmt.Errorf("Unsupported value for param %v (%s)", v.Interface(), v.Type())
-		return nil, err
+		return "", err
 	}
-	return &str, nil
+	return str, nil
 }
