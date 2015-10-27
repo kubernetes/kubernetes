db0f7687ef68dec630f476d69fa73ad4b82c9e34
Anthony Yeh <enisoc@google.com>
2015-10-27 15:16:32 -0700

    examples/vitess: Update for Vitess v2.0.0-alpha5
    This also enables built-in backup, so the caveat about starting new pods
    no longer applies.

--- examples/vitess/vtctld-up.sh
+++ examples/vitess/vtctld-up.sh
@@ -25,7 +25,14 @@ echo "Creating vtctld service..."
 $KUBECTL create -f vtctld-service.yaml
 
 echo "Creating vtctld replicationcontroller..."
-$KUBECTL create -f vtctld-controller.yaml
+# Expand template variables
+sed_script=""
+for var in backup_flags; do
+  sed_script+="s,{{$var}},${!var},g;"
+done
+
+# Instantiate template and send to kubectl.
+cat vtctld-controller-template.yaml | sed -e "$sed_script" | $KUBECTL create -f -
 
 server=$(get_vtctld_addr)
 echo
