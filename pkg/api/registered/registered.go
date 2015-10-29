c16f738262da7bbb714a9783048dd4b8fa453823
Mike Danese <mikedanese@gmail.com>
2015-10-29 15:23:47 -0700

    add component config api group

--- pkg/api/registered/registered.go
+++ pkg/api/registered/registered.go
@@ -32,12 +32,13 @@ var RegisteredVersions []string
 func init() {
 	// TODO: caesarxuchao: rename this variable to validGroupVersions
 	validAPIVersions := map[string]bool{
-		"v1":                 true,
-		"extensions/v1beta1": true,
+		"v1":                       true,
+		"extensions/v1beta1":       true,
+		"componentconfig/v1alpha1": true,
 	}
 
 	// The default list of supported api versions, in order of most preferred to the least.
-	defaultSupportedVersions := "v1,extensions/v1beta1"
+	defaultSupportedVersions := "v1,extensions/v1beta1,componentconfig/v1alpha1"
 	// Env var KUBE_API_VERSIONS is a comma separated list of API versions that should be registered in the scheme.
 	// The versions should be in the order of most preferred to the least.
 	supportedVersions := os.Getenv("KUBE_API_VERSIONS")
