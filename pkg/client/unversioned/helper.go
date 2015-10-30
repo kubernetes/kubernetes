9b75b88d4e9eb3f3b07cc1459c9b052543250bf2
Kris <krousey@google.com>
2015-10-30 10:39:08 -0700

    Move client's tranport to a separate package

--- pkg/client/unversioned/helper.go
+++ pkg/client/unversioned/helper.go
@@ -28,7 +28,6 @@ import (
 	"reflect"
 	gruntime "runtime"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/golang/glog"
@@ -444,124 +443,6 @@ func UnversionedRESTClientFor(config *Config) (*RESTClient, error) {
 		client.Client = &http.Client{Transport: transport}
 	}
 	return client, nil
-}
-
-var (
-	// tlsTransports stores reusable round trippers with custom TLSClientConfig options
-	tlsTransports = map[string]*http.Transport{}
-
-	// tlsTransportLock protects retrieval and storage of round trippers into the tlsTransports map
-	tlsTransportLock sync.Mutex
-)
-
-// tlsTransportFor returns a http.RoundTripper for the given config, or an error
-// The same RoundTripper will be returned for configs with identical TLS options
-// If the config has no custom TLS options, http.DefaultTransport is returned
-func tlsTransportFor(config *Config) (http.RoundTripper, error) {
-	// Get a unique key for the TLS options in the config
-	key, err := tlsConfigKey(config)
-	if err != nil {
-		return nil, err
-	}
-
-	// Ensure we only create a single transport for the given TLS options
-	tlsTransportLock.Lock()
-	defer tlsTransportLock.Unlock()
-
-	// See if we already have a custom transport for this config
-	if cachedTransport, ok := tlsTransports[key]; ok {
-		return cachedTransport, nil
-	}
-
-	// Get the TLS options for this client config
-	tlsConfig, err := TLSConfigFor(config)
-	if err != nil {
-		return nil, err
-	}
-	// The options didn't require a custom TLS config
-	if tlsConfig == nil {
-		return http.DefaultTransport, nil
-	}
-
-	// Cache a single transport for these options
-	tlsTransports[key] = util.SetTransportDefaults(&http.Transport{
-		TLSClientConfig: tlsConfig,
-	})
-	return tlsTransports[key], nil
-}
-
-// TransportFor returns an http.RoundTripper that will provide the authentication
-// or transport level security defined by the provided Config. Will return the
-// default http.DefaultTransport if no special case behavior is needed.
-func TransportFor(config *Config) (http.RoundTripper, error) {
-	hasCA := len(config.CAFile) > 0 || len(config.CAData) > 0
-	hasCert := len(config.CertFile) > 0 || len(config.CertData) > 0
-
-	// Set transport level security
-	if config.Transport != nil && (hasCA || hasCert || config.Insecure) {
-		return nil, fmt.Errorf("using a custom transport with TLS certificate options or the insecure flag is not allowed")
-	}
-
-	var (
-		transport http.RoundTripper
-		err       error
-	)
-
-	if config.Transport != nil {
-		transport = config.Transport
-	} else {
-		transport, err = tlsTransportFor(config)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	// Call wrap prior to adding debugging wrappers
-	if config.WrapTransport != nil {
-		transport = config.WrapTransport(transport)
-	}
-
-	switch {
-	case bool(glog.V(9)):
-		transport = NewDebuggingRoundTripper(transport, CurlCommand, URLTiming, ResponseHeaders)
-	case bool(glog.V(8)):
-		transport = NewDebuggingRoundTripper(transport, JustURL, RequestHeaders, ResponseStatus, ResponseHeaders)
-	case bool(glog.V(7)):
-		transport = NewDebuggingRoundTripper(transport, JustURL, RequestHeaders, ResponseStatus)
-	case bool(glog.V(6)):
-		transport = NewDebuggingRoundTripper(transport, URLTiming)
-	}
-
-	transport, err = HTTPWrappersForConfig(config, transport)
-	if err != nil {
-		return nil, err
-	}
-
-	// TODO: use the config context to wrap a transport
-
-	return transport, nil
-}
-
-// HTTPWrappersForConfig wraps a round tripper with any relevant layered behavior from the
-// config. Exposed to allow more clients that need HTTP-like behavior but then must hijack
-// the underlying connection (like WebSocket or HTTP2 clients). Pure HTTP clients should use
-// the higher level TransportFor or RESTClientFor methods.
-func HTTPWrappersForConfig(config *Config, rt http.RoundTripper) (http.RoundTripper, error) {
-	// Set authentication wrappers
-	hasBasicAuth := config.Username != "" || config.Password != ""
-	if hasBasicAuth && config.BearerToken != "" {
-		return nil, fmt.Errorf("username/password or bearer token may be set, but not both")
-	}
-	switch {
-	case config.BearerToken != "":
-		rt = NewBearerAuthRoundTripper(config.BearerToken, rt)
-	case hasBasicAuth:
-		rt = NewBasicAuthRoundTripper(config.Username, config.Password, rt)
-	}
-	if len(config.UserAgent) > 0 {
-		rt = NewUserAgentRoundTripper(config.UserAgent, rt)
-	}
-	return rt, nil
 }
 
 // DefaultServerURL converts a host, host:port, or URL string to the default base server API path
