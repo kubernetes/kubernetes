6b7b898febb4a10b00034f75a59b568b66f3f73b
Isaac Hollander McCreery <ihmccreery@google.com>
2015-11-05 15:51:42 -0800

    Rework instructions in build-official-release to clarify

--- release/build-official-release.sh
+++ release/build-official-release.sh
@@ -90,15 +90,23 @@ SHA1=$(sha1 "${KUBE_BUILD_DIR}/kubernetes.tar.gz")
 
 cat <<- EOM
 
-Success!  You must now do the following (you may want to cut and paste these
+Success! You must now do the following (you may want to cut and paste these
 instructions elsewhere):
 
-  1) (cd ${KUBE_BUILD_DIR}; build/push-official-release.sh ${KUBE_RELEASE_VERSION})
+  1) pushd ${KUBE_BUILD_DIR}; build/push-official-release.sh ${KUBE_RELEASE_VERSION}
+
   2) Go to https://github.com/GoogleCloudPlatform/kubernetes/releases
-     and create a new 'Release ${KUBE_RELEASE_VERSION} Candidate' release
-     with the ${KUBE_RELEASE_VERSION} tag. Mark it as a pre-release.
-  3) Upload the ${KUBE_BUILD_DIR}/kubernetes.tar.gz to GitHub
-  4) Use this template for the release:
+     and create a new release with the ${KUBE_RELEASE_VERSION} tag.
+
+     a) Mark it as a pre-release (someone on the GKE team will mark it as an
+     official release when it's being rolled out, but should not be considered
+     stable prior to that).
+
+     b) Title it:
+
+       Release ${KUBE_RELEASE_VERSION}
+
+     c) Use this template for the description:
 
 ## [Documentation](http://releases.k8s.io/${KUBE_RELEASE_VERSION}/docs/README.md)
 ## [Examples](http://releases.k8s.io/${KUBE_RELEASE_VERSION}/examples)
@@ -112,7 +120,11 @@ binary | hash alg | hash
 \`kubernetes.tar.gz\` | sha1 | \`${SHA1}\`
 
      We'll fill in the release notes in the next stage.
-  5) Ensure all the binaries are in place on GitHub and GCS before cleaning.
-  6) (cd ${KUBE_BUILD_DIR}; make clean; cd -; rm -rf ${KUBE_BUILD_DIR})
+
+  3) Upload the ${KUBE_BUILD_DIR}/kubernetes.tar.gz to GitHub
+
+  4) Ensure all the binaries are in place on GitHub and GCS before cleaning.
+
+  5) (make clean; popd; rm -rf ${KUBE_BUILD_DIR})
 
 EOM
