bd302d95343ef944a0a008daab6984c250e609ea
Chao Xu <xuchao@google.com>
2015-11-06 17:16:48 -0800

    address comments and fix

--- hack/verify-api-reference-docs.sh
+++ hack/verify-api-reference-docs.sh
@@ -26,16 +26,16 @@ source "${KUBE_ROOT}/hack/lib/init.sh"
 kube::golang::setup_env
 
 API_REFERENCE_DOCS_ROOT="${KUBE_ROOT}/docs/api-reference"
-HOST_OUTPUT_DIR="${KUBE_ROOT}/_tmp/api-reference"
-mkdir -p ${HOST_OUTPUT_DIR}
+OUTPUT_DIR="${KUBE_ROOT}/_tmp/api-reference"
+mkdir -p ${OUTPUT_DIR}
 TMP_ROOT="${KUBE_ROOT}/_tmp"
 
 # Generate API reference docs in tmp.
-"./hack/update-api-reference-docs.sh" "${HOST_OUTPUT_DIR}"
+"./hack/update-api-reference-docs.sh" "${OUTPUT_DIR}"
 
 echo "diffing ${API_REFERENCE_DOCS_ROOT} against freshly generated docs"
 ret=0
-diff -Naupr -I 'Last update' --exclude=*.md "${API_REFERENCE_DOCS_ROOT}" "${HOST_OUTPUT_DIR}" || ret=$?
+diff -Naupr -I 'Last update' --exclude=*.md "${API_REFERENCE_DOCS_ROOT}" "${OUTPUT_DIR}" || ret=$?
 rm -rf "${TMP_ROOT}"
 if [[ $ret -eq 0 ]]
 then
