14fff93c0d0b244df9c91f0ca6d7e0700ad13918
Brendan Burns <bburns@google.com>
2015-11-12 21:43:21 -0800

    Add kubectl to the hyperkube binary

--- cmd/kubectl/app/kubectl.go
+++ cmd/kubectl/app/kubectl.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2014 The Kubernetes Authors All rights reserved.
+Copyright 2015 The Kubernetes Authors All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,19 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
+package app
 
 import (
 	"os"
-	"runtime"
 
-	"k8s.io/kubernetes/cmd/kubectl/app"
+	"k8s.io/kubernetes/pkg/kubectl/cmd"
+	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 )
 
-func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	if err := app.Run(); err != nil {
-		os.Exit(1)
-	}
-	os.Exit(0)
+func Run() error {
+	cmd := cmd.NewKubectlCommand(cmdutil.NewFactory(nil), os.Stdin, os.Stdout, os.Stderr)
+	return cmd.Execute()
 }
