4eeae78d37b2979859fbd8612210453cecbb1369
cephaslr <cephaslr@yahoo.com>
2015-11-16 20:15:40 -0500

    Rename kubelet variable pods to maxPods in kubelet pkg to match pods variable in kubelet cmd.
    Remove comment requesting modification of mesos contrib file, dependencies should be maintained in unit tests

--- cmd/kubelet/app/server.go
+++ cmd/kubelet/app/server.go
@@ -17,9 +17,6 @@ limitations under the License.
 // Package app makes it easy to create a kubelet server for various contexts.
 package app
 
-// Note: if you change code in this file, you might need to change code in
-// contrib/mesos/pkg/executor/service/.
-
 import (
 	"crypto/tls"
 	"fmt"
