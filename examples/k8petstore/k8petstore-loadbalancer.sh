efe566621f17c876b081b5a1a7b866ddafbfcf13
Tim Hockin <thockin@google.com>
2015-11-17 23:53:39 -0800

    try harder to timeout hung curls in examples test

--- examples/k8petstore/k8petstore-loadbalancer.sh
+++ examples/k8petstore/k8petstore-loadbalancer.sh
@@ -253,7 +253,7 @@ function pollfor {
   do
       ### Just testing that the front end comes up.  Not sure how to test total entries etc... (yet)
       echo "Trying curl ... $PUBLIC_IP:3000 , attempt $i . expect a few failures while pulling images... "
-      curl --connect-timeout 1 "$PUBLIC_IP:3000" > result
+      curl --max-time 1 "$PUBLIC_IP:3000" > result
       cat result
       cat result | grep -q "k8-bps"
       if [ $? -eq 0 ]; then
@@ -279,7 +279,7 @@ function tests {
     for i in `seq 1 $_SECONDS`;
      do
         echo "curl : $PUBLIC_IP:3000 , $i of $_SECONDS"
-        curr_cnt="`curl --connect-timeout 1 "$PUBLIC_IP:3000/llen"`" 
+        curr_cnt="`curl --max-time 1 "$PUBLIC_IP:3000/llen"`" 
         ### Write CSV File of # of trials / total transcations.
         echo "$i $curr_cnt" >> result
         echo "total transactions so far : $curr_cnt"
