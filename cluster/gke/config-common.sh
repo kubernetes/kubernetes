1846cfc12911b8189427722f13985a7015d36a2d
Brad Erickson <eosrei@gmail.com>
2015-11-23 19:05:07 -0800

    Minion->Node rename: NODE_ROOT_DISK_SIZE, NODE_ROOT_DISK_TYPE, etc
    NODE_SCOPES

--- cluster/gke/config-common.sh
+++ cluster/gke/config-common.sh
@@ -27,7 +27,7 @@ FIREWALL_SSH="${FIREWALL_SSH:-${NETWORK}-allow-ssh}"
 GCLOUD="${GCLOUD:-gcloud}"
 CMD_GROUP="${CMD_GROUP:-}"
 GCLOUD_CONFIG_DIR="${GCLOUD_CONFIG_DIR:-${HOME}/.config/gcloud/kubernetes}"
-MINION_SCOPES="${MINION_SCOPES:-"compute-rw,storage-ro"}"
+NODE_SCOPES="${NODE_SCOPES:-"compute-rw,storage-ro"}"
 MACHINE_TYPE="${MACHINE_TYPE:-n1-standard-2}"
 
 # WARNING: any new vars added here must correspond to options that can be
