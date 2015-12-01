aad8cb68890ee4228567760763cfd7713eedbdbf
Isaac Hollander McCreery <ihmccreery@google.com>
2015-12-01 14:59:08 -0800

    Make job names consistent

--- hack/jenkins/e2e.sh
+++ hack/jenkins/e2e.sh
@@ -1167,16 +1167,16 @@ case ${JOB_NAME} in
   #
   # Configurations for step1, step3, and step5 live in the release-1.0 branch.
 
-  kubernetes-upgrade-1.0-master-gke-step2-upgrade-master)
-    configure_upgrade_step 'configured-in-release-1.0' 'ci/latest' 'gke-upgrade-1-0-master' 'kubernetes-jenkins-gke-upgrade'
+  kubernetes-upgrade-gke-1.0-master-step2-upgrade-master)
+    configure_upgrade_step 'configured-in-release-1.0' 'ci/latest' 'upgrade-gke-1-0-master' 'kubernetes-jenkins-gke-upgrade'
     ;;
 
-  kubernetes-upgrade-1.0-master-gke-step4-upgrade-cluster)
-    configure_upgrade_step 'configured-in-release-1.0' 'ci/latest' 'gke-upgrade-1-0-master' 'kubernetes-jenkins-gke-upgrade'
+  kubernetes-upgrade-gke-1.0-master-step4-upgrade-cluster)
+    configure_upgrade_step 'configured-in-release-1.0' 'ci/latest' 'upgrade-gke-1-0-master' 'kubernetes-jenkins-gke-upgrade'
     ;;
 
-  kubernetes-upgrade-1.0-master-gke-step6-e2e-new)
-    configure_upgrade_step 'configured-in-release-1.0' 'ci/latest' 'gke-upgrade-1-0-master' 'kubernetes-jenkins-gke-upgrade'
+  kubernetes-upgrade-gke-1.0-master-step6-e2e-new)
+    configure_upgrade_step 'configured-in-release-1.0' 'ci/latest' 'upgrade-gke-1-0-master' 'kubernetes-jenkins-gke-upgrade'
     ;;
 
   # kubernetes-upgrade-gce
