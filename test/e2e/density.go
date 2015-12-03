fd85657156cd361baa92431bf74972579478b671
Wojciech Tyczynski <wojtekt@google.com>
2015-12-03 09:13:45 +0100

    Increase verification limits for kubeproxy in scalability tests

--- test/e2e/density.go
+++ test/e2e/density.go
@@ -89,7 +89,7 @@ func density30AddonResourceVerifier() map[string]resourceConstraint {
 		memoryConstraint: 100 * (1024 * 1024),
 	}
 	constraints["kube-proxy"] = resourceConstraint{
-		cpuConstraint:    0.02,
+		cpuConstraint:    0.05,
 		memoryConstraint: 20 * (1024 * 1024),
 	}
 	constraints["l7-lb-controller"] = resourceConstraint{
