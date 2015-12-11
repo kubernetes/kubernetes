80a8295bceb3456d97a5bf7d7ce61a8eaf71250a
Chao Xu <xuchao@google.com>
2015-12-10 22:31:29 -0800

    update the client-gen to use the plural namer

--- cmd/libs/go2idl/client-gen/generators/client-generator.go
+++ cmd/libs/go2idl/client-gen/generators/client-generator.go
@@ -32,10 +32,16 @@ import (
 
 // NameSystems returns the name system used by the generators in this package.
 func NameSystems() namer.NameSystems {
+	pluralExceptions := map[string]string{
+		"Endpoints":       "endpoints",
+		"ComponentStatus": "componentStatus",
+	}
 	return namer.NameSystems{
-		"public":  namer.NewPublicNamer(0),
-		"private": namer.NewPrivateNamer(0),
-		"raw":     namer.NewRawNamer("", nil),
+		"public":        namer.NewPublicNamer(0),
+		"private":       namer.NewPrivateNamer(0),
+		"raw":           namer.NewRawNamer("", nil),
+		"publicPlural":  namer.NewPluralNamer(pluralExceptions, true),
+		"privatePlural": namer.NewPluralNamer(pluralExceptions, false),
 	}
 }
 
