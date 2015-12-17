b878f8596092835fa6ba00f8d24a154d8d5a353e
Eric Tune <etune@google.com>
2015-12-17 09:36:20 -0800

    Fix update/verify-codecgen.sh for OSX.

--- hack/verify-codecgen.sh
+++ hack/verify-codecgen.sh
@@ -54,7 +54,7 @@ result=""
 function depends {
   file=${generated_files[$1]//\.generated\.go/.go}
   deps=$(go list -f "{{.Deps}}" ${file} | tr "[" " " | tr "]" " ")
-  fullpath=$(readlink -f ${generated_files[$2]//\.generated\.go/.go})
+  fullpath=$(readlinkdashf "${generated_files[$2]//\.generated\.go/.go}")
   candidate=$(dirname "${fullpath}")
   result=false
   for dep in ${deps}; do
