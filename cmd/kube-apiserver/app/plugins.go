f175a224436c38a32979c4629fc85cb85dcccf61
Andy Goldstein <agoldste@redhat.com>
2015-12-18 16:48:49 -0500

    Add admission controller to force image pulls
    Add an admission controller that forces every container's image pull policy to
    Always when a pod is created.

--- cmd/kube-apiserver/app/plugins.go
+++ cmd/kube-apiserver/app/plugins.go
@@ -25,6 +25,7 @@ import (
 
 	// Admission policies
 	_ "k8s.io/kubernetes/plugin/pkg/admission/admit"
+	_ "k8s.io/kubernetes/plugin/pkg/admission/alwayspullimages"
 	_ "k8s.io/kubernetes/plugin/pkg/admission/deny"
 	_ "k8s.io/kubernetes/plugin/pkg/admission/exec"
 	_ "k8s.io/kubernetes/plugin/pkg/admission/initialresources"
