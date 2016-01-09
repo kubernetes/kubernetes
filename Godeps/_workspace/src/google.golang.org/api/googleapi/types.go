704ec3600935e38b8c6734a9f6eaf28222be42ec
Prashanth Balasubramanian <beeps@google.com>
2016-01-08 17:32:19 -0800

    Update gce compute api godep.

--- Godeps/_workspace/src/google.golang.org/api/googleapi/types.go
+++ Godeps/_workspace/src/google.golang.org/api/googleapi/types.go
@@ -148,3 +148,35 @@ func (s Float64s) MarshalJSON() ([]byte, error) {
 		return strconv.AppendFloat(dst, s[i], 'g', -1, 64)
 	})
 }
+
+/*
+ * Helper routines for simplifying the creation of optional fields of basic type.
+ */
+
+// Bool is a helper routine that allocates a new bool value
+// to store v and returns a pointer to it.
+func Bool(v bool) *bool { return &v }
+
+// Int32 is a helper routine that allocates a new int32 value
+// to store v and returns a pointer to it.
+func Int32(v int32) *int32 { return &v }
+
+// Int64 is a helper routine that allocates a new int64 value
+// to store v and returns a pointer to it.
+func Int64(v int64) *int64 { return &v }
+
+// Float64 is a helper routine that allocates a new float64 value
+// to store v and returns a pointer to it.
+func Float64(v float64) *float64 { return &v }
+
+// Uint32 is a helper routine that allocates a new uint32 value
+// to store v and returns a pointer to it.
+func Uint32(v uint32) *uint32 { return &v }
+
+// Uint64 is a helper routine that allocates a new uint64 value
+// to store v and returns a pointer to it.
+func Uint64(v uint64) *uint64 { return &v }
+
+// String is a helper routine that allocates a new string value
+// to store v and returns a pointer to it.
+func String(v string) *string { return &v }
