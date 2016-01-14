2d436ff0808d39bb6b8539a13fd4e602cfaade42
Solly Ross <sross@redhat.com>
2016-01-14 15:45:08 -0500

    Scheduler predicate for capping node volume count
    For certain volume types (e.g. AWS EBS or GCE PD), a limitted
    number of such volumes can be attached to a given node.  This commit
    introduces a predicate with allows cluster admins to cap
    the maximum number of volumes matching a particular type attached to a
    given node.
    
    The volume type is configurable by passing a pair of filter functions,
    and the maximum number of such volumes is configurable to allow node
    admins to reserve a certain number of volumes for system use.
    
    By default, the predicate is exposed as MaxEBSVolumeCount and
    MaxGCEPDVolumeCount (for AWS ElasticBlocKStore and GCE PersistentDisk
    volumes, respectively), each of which can be configured using the
    `KUBE_MAX_PD_VOLS` environment variable.
    
    Fixes #7835

--- plugin/pkg/scheduler/algorithmprovider/defaults/defaults.go
+++ plugin/pkg/scheduler/algorithmprovider/defaults/defaults.go
@@ -18,13 +18,40 @@ limitations under the License.
 package defaults
 
 import (
+	"os"
+	"strconv"
+
 	"k8s.io/kubernetes/pkg/util/sets"
 	"k8s.io/kubernetes/plugin/pkg/scheduler"
 	"k8s.io/kubernetes/plugin/pkg/scheduler/algorithm"
 	"k8s.io/kubernetes/plugin/pkg/scheduler/algorithm/predicates"
 	"k8s.io/kubernetes/plugin/pkg/scheduler/algorithm/priorities"
 	"k8s.io/kubernetes/plugin/pkg/scheduler/factory"
+
+	"github.com/golang/glog"
 )
+
+// Amazon reccomends having no more that 40 volumes attached to an instance,
+// and at least one of those is for the system root volume.
+const DefaultMaxEBSVolumes = 39
+
+// GCE instances can have up to 16 PD volumes attached.
+const DefaultMaxGCEPDVolumes = 16
+
+// getMaxVols checks the max PD volumes environment variable, otherwise returning a default value
+func getMaxVols(defaultVal int) int {
+	if rawMaxVols := os.Getenv("KUBE_MAX_PD_VOLS"); rawMaxVols != "" {
+		if parsedMaxVols, err := strconv.Atoi(rawMaxVols); err != nil {
+			glog.Errorf("Unable to parse maxiumum PD volumes value, using default of %v: %v", defaultVal, err)
+		} else if parsedMaxVols <= 0 {
+			glog.Errorf("Maximum PD volumes must be a positive value, using default of %v", defaultVal)
+		} else {
+			return parsedMaxVols
+		}
+	}
+
+	return defaultVal
+}
 
 func init() {
 	factory.RegisterAlgorithmProvider(factory.DefaultProvider, defaultPredicates(), defaultPriorities())
@@ -80,6 +107,26 @@ func defaultPredicates() sets.String {
 		),
 		// Fit is determined by the presence of the Host parameter and a string match
 		factory.RegisterFitPredicate("HostName", predicates.PodFitsHost),
+
+		// Fit is determined by whether or not there would be too many AWS EBS volumes attached to the node
+		factory.RegisterFitPredicateFactory(
+			"MaxEBSVolumeCount",
+			func(args factory.PluginFactoryArgs) algorithm.FitPredicate {
+				// TODO: allow for generically parameterized scheduler predicates, because this is a bit ugly
+				maxVols := getMaxVols(DefaultMaxEBSVolumes)
+				return predicates.NewMaxPDVolumeCountPredicate(predicates.EBSVolumeFilter, maxVols, args.PVInfo, args.PVCInfo)
+			},
+		),
+
+		// Fit is determined by whether or not there would be too many GCE PD volumes attached to the node
+		factory.RegisterFitPredicateFactory(
+			"MaxGCEPDVolumeCount",
+			func(args factory.PluginFactoryArgs) algorithm.FitPredicate {
+				// TODO: allow for generically parameterized scheduler predicates, because this is a bit ugly
+				maxVols := getMaxVols(DefaultMaxGCEPDVolumes)
+				return predicates.NewMaxPDVolumeCountPredicate(predicates.GCEPDVolumeFilter, maxVols, args.PVInfo, args.PVCInfo)
+			},
+		),
 	)
 }
 
