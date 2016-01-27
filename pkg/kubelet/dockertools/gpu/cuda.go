36e1272a009b8e82e7e017df4442183ce4f9ea44
Hans Han <haniceboy@gmail.com>
2016-01-27 14:10:31 +0800

    temp save

--- pkg/kubelet/dockertools/gpu/cuda.go
+++ pkg/kubelet/dockertools/gpu/cuda.go
@@ -3,3 +3,9 @@ package gpu
 import (
 	nvd "github.com/haniceboy/nvidia-docker/tools/src/nvidia"
 )
+
+func Detect(gpuDevice *GPUDevice) error {
+	//drv, err := nvd.GetDriverVersion()
+	_, err := nvd.GetDriverVersion()
+	return err
+}
