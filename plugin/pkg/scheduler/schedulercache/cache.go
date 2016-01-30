415aa330dc4c36e6c1f229f85096e2276b0028aa
Hongchao Deng <hongchaodeng1@gmail.com>
2016-01-29 16:55:17 -0800

    schedulercache API and impl: add GetNodeNameToInfoMap()

---
+++ plugin/pkg/scheduler/schedulercache/cache.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package schedulercache
+
+import "k8s.io/kubernetes/pkg/labels"
+
+type cache struct {
+	podLister PodLister
+}
+
+// New returns a Cache implementation.
+func New(podLister PodLister) Cache {
+	return &cache{
+		podLister: podLister,
+	}
+}
+
+func (c *cache) GetNodeNameToInfoMap() map[string]*NodeInfo {
+	pods, err := c.podLister.List(labels.Everything())
+	if err != nil {
+		panic("local cache shouldn't have any List error")
+	}
+	return CreateNodeNameToInfoMap(pods)
+}
