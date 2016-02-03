cff0272b7557cbc690817db741c471b699162c90
Remi Cattiau <remi@cattiau.com>
2016-02-03 10:30:49 +0100

    Fix header

--- docs/getting-started-guides/docker-multinode/worker.sh
+++ docs/getting-started-guides/docker-multinode/worker.sh
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 # A scripts to install k8s worker node.
 # Author @wizard_cxy @reouser @loopingz
 
