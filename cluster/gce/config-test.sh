4458ad227800e07ab690e1a2b8db49eee6df8a1e
Robert Bailey <robertbailey@google.com>
2016-02-10 14:38:44 -0800

    Remove the SecurityContextDeny admission controller so that the testing environment matches the production environment. This was removed from config-default.sh in #16986.

--- cluster/gce/config-test.sh
+++ cluster/gce/config-test.sh
@@ -117,7 +117,7 @@ if [[ "${ENABLE_NODE_AUTOSCALER}" == "true" ]]; then
   TARGET_NODE_UTILIZATION="${KUBE_TARGET_NODE_UTILIZATION:-0.7}"
 fi
 
-ADMISSION_CONTROL="${KUBE_ADMISSION_CONTROL:-NamespaceLifecycle,LimitRanger,SecurityContextDeny,ServiceAccount,ResourceQuota,PersistentVolumeLabel}"
+ADMISSION_CONTROL="${KUBE_ADMISSION_CONTROL:-NamespaceLifecycle,LimitRanger,ServiceAccount,ResourceQuota,PersistentVolumeLabel}"
 
 # Optional: if set to true kube-up will automatically check for existing resources and clean them up.
 KUBE_UP_AUTOMATIC_CLEANUP=${KUBE_UP_AUTOMATIC_CLEANUP:-false}
