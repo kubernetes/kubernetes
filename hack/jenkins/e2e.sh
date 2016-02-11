86e33e683390cfa11fde85a0870fdcdbc3e4fbbd
Isaac Hollander McCreery <ihmccreery@google.com>
2016-02-10 18:15:29 -0800

    WIP move e2e.sh into YAML files

--- hack/jenkins/e2e.sh
+++ hack/jenkins/e2e.sh
@@ -218,8 +218,6 @@ CURRENT_RELEASE_PUBLISHED_VERSION="ci/latest-1.1"
 # Kubernetes.
 case ${JOB_NAME} in
 
-  # PR builder
-
   # Runs a subset of tests on GCE in parallel. Run against all pending PRs.
   kubernetes-pull-build-test-e2e-gce)
     : ${E2E_CLUSTER_NAME:="jnks-e2e-gce-${NODE_NAME}-${EXECUTOR_NUMBER}"}
@@ -234,418 +232,58 @@ case ${JOB_NAME} in
     NUM_NODES=${NUM_NODES_PARALLEL}
     ;;
 
-  # GCE core jobs
-
-  # Runs all non-slow, non-serial, non-flaky, tests on GCE in parallel.
-  kubernetes-e2e-gce)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-e2e"}
-    : ${E2E_PUBLISH_GREEN_VERSION:="true"}
-    : ${E2E_NETWORK:="e2e-gce"}
-    # This list should match the list in kubernetes-pull-build-test-e2e-gce.
-    : ${GINKGO_TEST_ARGS:="--ginkgo.skip=\[Slow\]|\[Serial\]|\[Disruptive\]|\[Flaky\]|\[Feature:.+\]"}
-    : ${GINKGO_PARALLEL:="y"}
-    : ${KUBE_GCE_INSTANCE_PREFIX="e2e-gce"}
-    : ${PROJECT:="k8s-jkns-e2e-gce"}
-    : ${ENABLE_DEPLOYMENTS:=true}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    ;;
-
-  # Runs slow tests on GCE, sequentially.
-  kubernetes-e2e-gce-slow)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-e2e-slow"}
-    : ${E2E_NETWORK:="e2e-slow"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Slow\] \
-                           --ginkgo.skip=\[Serial\]|\[Disruptive\]|\[Flaky\]|\[Feature:.+\]"}
-    : ${GINKGO_PARALLEL:="y"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-slow"}
-    : ${PROJECT:="k8s-jkns-e2e-gce-slow"}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    ;;
-
-  # Run the [Serial], [Disruptive], and [Feature:Restart] tests on GCE.
-  kubernetes-e2e-gce-serial)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-e2e-serial"}
-    : ${E2E_NETWORK:="jenkins-gce-e2e-serial"}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Serial\]|\[Disruptive\] \
-                           --ginkgo.skip=\[Flaky\]|\[Feature:.+\]"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-serial"}
-    : ${PROJECT:="kubernetes-jkns-e2e-gce-serial"}
-    ;;
-
-  # Runs only the ingress tests on GCE.
-  kubernetes-e2e-gce-ingress)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-e2e-ingress"}
-    : ${E2E_NETWORK:="e2e-ingress"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Feature:Ingress\]"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-ingress"}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    # TODO: Move this into a different project. Currently, since this test
-    # shares resources with various other networking tests, so it's easier
-    # to zero in on the source of a leak if it's run in isolation.
-    : ${PROJECT:="kubernetes-flannel"}
-    ;;
-
-  # Runs only the ingress tests on GKE.
-  kubernetes-e2e-gke-ingress)
-    : ${E2E_CLUSTER_NAME:="jenkins-gke-e2e-ingress"}
-    : ${E2E_NETWORK:="e2e-gke-ingress"}
-    : ${E2E_SET_CLUSTER_API_VERSION:=y}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Feature:Ingress\]"}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-gke-ingress"}
-    # TODO: Move this into a different project. Currently, since this test
-    # shares resources with various other networking tests, it's easier to
-    # zero in on the source of a leak if it's run in isolation.
-    : ${PROJECT:="kubernetes-flannel"}
-    ;;
-
-  # Runs the flaky tests on GCE, sequentially.
-  kubernetes-e2e-gce-flaky)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-e2e-flaky"}
-    : ${E2E_NETWORK:="e2e-flaky"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Flaky\] \
-                           --ginkgo.skip=\[Feature:.+\]"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-flaky"}
-    : ${PROJECT:="k8s-jkns-e2e-gce-flaky"}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    : ${E2E_DOWN:="true"}
-    ;;
-
-  # GKE core jobs
-
-  # Runs all non-slow, non-serial, non-flaky, tests on GKE in parallel.
-  kubernetes-e2e-gke)
-    : ${E2E_CLUSTER_NAME:="jkns-gke-e2e-ci"}
-    : ${E2E_NETWORK:="e2e-gke-ci"}
-    : ${E2E_SET_CLUSTER_API_VERSION:=y}
-    : ${PROJECT:="k8s-jkns-e2e-gke-ci"}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.skip=\[Slow\]|\[Serial\]|\[Disruptive\]|\[Flaky\]|\[Feature:.+\]"}
-    : ${GINKGO_PARALLEL:="y"}
-    ;;
-
-  kubernetes-e2e-gke-slow)
-    : ${E2E_CLUSTER_NAME:="jkns-gke-e2e-slow"}
-    : ${E2E_NETWORK:="e2e-gke-slow"}
-    : ${E2E_SET_CLUSTER_API_VERSION:=y}
-    : ${PROJECT:="k8s-jkns-e2e-gke-slow"}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Slow\] \
-                           --ginkgo.skip=\[Serial\]|\[Disruptive\]|\[Flaky\]|\[Feature:.+\]"}
-    : ${GINKGO_PARALLEL:="y"}
-    ;;
-
-  # Run the [Serial], [Disruptive], and [Feature:Restart] tests on GKE.
-  kubernetes-e2e-gke-serial)
-    : ${E2E_CLUSTER_NAME:="jenkins-gke-e2e-serial"}
-    : ${E2E_NETWORK:="jenkins-gke-e2e-serial"}
-    : ${E2E_SET_CLUSTER_API_VERSION:=y}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Serial\]|\[Disruptive\] \
-                           --ginkgo.skip=\[Flaky\]|\[Feature:.+\]"}
-    : ${PROJECT:="jenkins-gke-e2e-serial"}
-    ;;
-
-  kubernetes-e2e-gke-flaky)
-    : ${E2E_CLUSTER_NAME:="kubernetes-gke-e2e-flaky"}
-    : ${E2E_NETWORK:="gke-e2e-flaky"}
-    : ${E2E_SET_CLUSTER_API_VERSION:=y}
-    : ${PROJECT:="k8s-jkns-e2e-gke-ci-flaky"}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Flaky\] \
-                           --ginkgo.skip=\[Feature:.+\]"}
-    ;;
-
-  # AWS core jobs
-
-  # Runs all non-flaky, non-slow tests on AWS, sequentially.
-  kubernetes-e2e-aws)
-    : ${E2E_PUBLISH_GREEN_VERSION:=true}
-    : ${E2E_CLUSTER_NAME:="jenkins-aws-e2e"}
-    : ${E2E_ZONE:="us-west-2a"}
-    : ${ZONE:="us-west-2a"}
-    : ${E2E_NETWORK:="e2e-aws"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.skip=\[Slow\]|\[Serial\]|\[Disruptive\]|\[Flaky\]|\[Feature:.+\]"}
-    : ${GINKGO_PARALLEL:="y"}
-    : ${KUBE_GCE_INSTANCE_PREFIX="e2e-aws"}
-    : ${PROJECT:="k8s-jkns-e2e-aws"}
-    : ${ENABLE_DEPLOYMENTS:=true}
-    : ${AWS_CONFIG_FILE:='/var/lib/jenkins/.aws/credentials'}
-    : ${AWS_SSH_KEY:='/var/lib/jenkins/.ssh/kube_aws_rsa'}
-    : ${KUBE_SSH_USER:='ubuntu'}
-    # This is needed to be able to create PD from the e2e test
-    : ${AWS_SHARED_CREDENTIALS_FILE:='/var/lib/jenkins/.aws/credentials'}
-    ;;
-
-  # Feature jobs
-
-  # Runs only the examples tests on GCE.
-  kubernetes-e2e-gce-examples)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-e2e-examples"}
-    : ${E2E_NETWORK:="e2e-examples"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Feature:Example\]"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-examples"}
-    : ${PROJECT:="kubernetes-jenkins"}
-    ;;
-
-  # Runs only the autoscaling tests on GCE.
-  kubernetes-e2e-gce-autoscaling)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-e2e-autoscaling"}
-    : ${E2E_NETWORK:="e2e-autoscaling"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Feature:ClusterSizeAutoscaling\]|\[Feature:InitialResources\] \
-                           --ginkgo.skip=\[Flaky\]"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-autoscaling"}
-    : ${PROJECT:="k8s-jnks-e2e-gce-autoscaling"}
-    : ${FAIL_ON_GCP_RESOURCE_LEAK:="true"}
-    : ${ENABLE_DEPLOYMENTS:=true}
-    # Override GCE default for cluster size autoscaling purposes.
-    ENABLE_CLUSTER_MONITORING="googleinfluxdb"
-    ADMISSION_CONTROL="NamespaceLifecycle,InitialResources,LimitRanger,SecurityContextDeny,ServiceAccount,ResourceQuota"
-    ;;
-
-  # Runs the performance/scalability tests on GCE. A larger cluster is used.
-  kubernetes-e2e-gce-scalability)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-e2e-scalability"}
-    : ${E2E_NETWORK:="e2e-scalability"}
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=\[Feature:Performance\] \
-        --gather-resource-usage=true \
-        --gather-metrics-at-teardown=true \
-        --gather-logs-sizes=true \
-        --output-print-type=json"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-scalability"}
-    : ${PROJECT:="kubernetes-jenkins"}
-    # Override GCE defaults.
-    MASTER_SIZE="n1-standard-4"
-    NODE_SIZE="n1-standard-2"
-    NODE_DISK_SIZE="50GB"
-    NUM_NODES="100"
-    # Reduce logs verbosity
-    TEST_CLUSTER_LOG_LEVEL="--v=2"
-    # TODO: Remove when we figure out the reason for ocassional failures #19048
-    KUBELET_TEST_LOG_LEVEL="--v=4"
-    # Increase resync period to simulate production
-    TEST_CLUSTER_RESYNC_PERIOD="--min-resync-period=12h"
-    ;;
-
-  # Runs e2e on GCE with flannel and VXLAN.
-  kubernetes-e2e-gce-flannel)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-e2e-flannel"}
-    : ${E2E_PUBLISH_GREEN_VERSION:="true"}
-    : ${E2E_NETWORK:="e2e-gce-flannel"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-flannel"}
-    : ${PROJECT:="kubernetes-flannel"}
-    # Override GCE defaults.
-    NETWORK_PROVIDER="flannel"
-    ;;
-
   # Runs the performance/scalability test on huge 1000-node cluster on GCE.
   # Flannel is used as network provider.
   # Allows a couple of nodes to be NotReady during startup
   kubernetes-e2e-gce-enormous-cluster)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-enormous-cluster"}
-    : ${E2E_NETWORK:="e2e-enormous-cluster"}
+    export E2E_CLUSTER_NAME="jenkins-gce-enormous-cluster"
+    export E2E_NETWORK="e2e-enormous-cluster"
     # TODO: Currently run only density test.
     # Once this is stable, run the whole [Performance] suite.
-    : ${GINKGO_TEST_ARGS:="--ginkgo.focus=starting\s30\spods\sper\snode"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-enormous-cluster"}
-    : ${PROJECT:="kubernetes-scale"}
+    export GINKGO_TEST_ARGS="--ginkgo.focus=starting\s30\spods\sper\snode"
+    export KUBE_GCE_INSTANCE_PREFIX="e2e-enormous-cluster"
+    export PROJECT="kubernetes-scale"
     # Override GCE defaults.
-    NETWORK_PROVIDER="flannel"
+    export NETWORK_PROVIDER="flannel"
     # Temporarily switch of Heapster, as this will not schedule anywhere.
     # TODO: Think of a solution to enable it.
-    ENABLE_CLUSTER_MONITORING="none"
-    E2E_ZONE="asia-east1-a"
-    MASTER_SIZE="n1-standard-32"
-    NODE_SIZE="n1-standard-1"
-    NODE_DISK_SIZE="50GB"
-    NUM_NODES="1000"
-    ALLOWED_NOTREADY_NODES="2"
-    EXIT_ON_WEAK_ERROR="false"
+    export ENABLE_CLUSTER_MONITORING="none"
+    export E2E_ZONE="asia-east1-a"
+    export MASTER_SIZE="n1-standard-32"
+    export NODE_SIZE="n1-standard-1"
+    export NODE_DISK_SIZE="50GB"
+    export NUM_NODES="1000"
+    export ALLOWED_NOTREADY_NODES="2"
+    export EXIT_ON_WEAK_ERROR="false"
     # Reduce logs verbosity
-    TEST_CLUSTER_LOG_LEVEL="--v=1"
+    export TEST_CLUSTER_LOG_LEVEL="--v=1"
     # Increase resync period to simulate production
-    TEST_CLUSTER_RESYNC_PERIOD="--min-resync-period=12h"
+    export TEST_CLUSTER_RESYNC_PERIOD="--min-resync-period=12h"
     ;;
 
   # Starts and tears down 1000-node cluster on GCE using flannel networking
   # Requires all 1000 nodes to come up.
   kubernetes-e2e-gce-enormous-startup)
-    : ${E2E_CLUSTER_NAME:="jenkins-gce-enormous-startup"}
+    export E2E_CLUSTER_NAME="jenkins-gce-enormous-startup"
     # TODO: increase a quota for networks in kubernetes-scale and move this test to its own network
-    : ${E2E_NETWORK:="e2e-enormous-cluster"}
-    : ${E2E_TEST:="false"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="e2e-enormous-startup"}
-    : ${PROJECT:="kubernetes-scale"}
+    export E2E_NETWORK="e2e-enormous-cluster"
+    export E2E_TEST="false"
+    export KUBE_GCE_INSTANCE_PREFIX="e2e-enormous-startup"
+    export PROJECT="kubernetes-scale"
     # Override GCE defaults.
-    NETWORK_PROVIDER="flannel"
+    export NETWORK_PROVIDER="flannel"
     # Temporarily switch of Heapster, as this will not schedule anywhere.
     # TODO: Think of a solution to enable it.
-    ENABLE_CLUSTER_MONITORING="none"
-    E2E_ZONE="us-east1-b"
-    MASTER_SIZE="n1-standard-32"
-    NODE_SIZE="n1-standard-1"
-    NODE_DISK_SIZE="50GB"
-    NUM_NODES="1000"
+    export ENABLE_CLUSTER_MONITORING="none"
+    export E2E_ZONE="us-east1-b"
+    export MASTER_SIZE="n1-standard-32"
+    export NODE_SIZE="n1-standard-1"
+    export NODE_DISK_SIZE="50GB"
+    export NUM_NODES="1000"
     # Reduce logs verbosity
-    TEST_CLUSTER_LOG_LEVEL="--v=1"
+    export TEST_CLUSTER_LOG_LEVEL="--v=1"
     # Increase resync period to simulate production
-    TEST_CLUSTER_RESYNC_PERIOD="--min-resync-period=12h"
-    ;;
-
-  # Run Kubemark test on a fake 100 node cluster to have a comparison
-  # to the real results from scalability suite
-  kubernetes-kubemark-gce)
-    : ${E2E_CLUSTER_NAME:="kubernetes-kubemark"}
-    : ${E2E_NETWORK:="kubernetes-kubemark"}
-    : ${PROJECT:="k8s-jenkins-kubemark"}
-    : ${E2E_UP:="true"}
-    : ${E2E_DOWN:="true"}
-    : ${E2E_TEST:="false"}
-    : ${USE_KUBEMARK:="true"}
-    : ${KUBEMARK_TESTS:="should\sallow\sstarting\s30\spods\sper\snode"}
-    # Override defaults to be indpendent from GCE defaults and set kubemark parameters
-    KUBE_GCE_INSTANCE_PREFIX="kubemark100"
-    NUM_NODES="10"
-    MASTER_SIZE="n1-standard-2"
-    NODE_SIZE="n1-standard-1"
-    E2E_ZONE="asia-east1-a"
-    KUBEMARK_MASTER_SIZE="n1-standard-4"
-    KUBEMARK_NUM_NODES="100"
-    ;;
-
-  # Run Kubemark test on a fake 500 node cluster to test for regressions on
-  # bigger clusters
-  kubernetes-kubemark-500-gce)
-    : ${E2E_CLUSTER_NAME:="kubernetes-kubemark-500"}
-    : ${E2E_NETWORK:="kubernetes-kubemark-500"}
-    : ${PROJECT:="kubernetes-scale"}
-    : ${E2E_UP:="true"}
-    : ${E2E_DOWN:="true"}
-    : ${E2E_TEST:="false"}
-    : ${USE_KUBEMARK:="true"}
-    : ${KUBEMARK_TESTS:="\[Feature:Performance\]"}
-    # Override defaults to be indpendent from GCE defaults and set kubemark parameters
-    NUM_NODES="6"
-    MASTER_SIZE="n1-standard-4"
-    NODE_SIZE="n1-standard-8"
-    KUBE_GCE_INSTANCE_PREFIX="kubemark500"
-    E2E_ZONE="us-east1-b"
-    KUBEMARK_MASTER_SIZE="n1-standard-16"
-    KUBEMARK_NUM_NODES="500"
-    ;;
-
-  # Run big Kubemark test, this currently means a 1000 node cluster and 16 core master
-  kubernetes-kubemark-gce-scale)
-    : ${E2E_CLUSTER_NAME:="kubernetes-kubemark-scale"}
-    : ${E2E_NETWORK:="kubernetes-kubemark-scale"}
-    : ${PROJECT:="kubernetes-scale"}
-    : ${E2E_UP:="true"}
-    : ${E2E_DOWN:="true"}
-    : ${E2E_TEST:="false"}
-    : ${USE_KUBEMARK:="true"}
-    : ${KUBEMARK_TESTS:="should\sallow\sstarting\s30\spods\sper\snode"}
-    # Override defaults to be indpendent from GCE defaults and set kubemark parameters
-    # We need 11 so that we won't hit max-pods limit (set to 100). TODO: do it in a nicer way.
-    NUM_NODES="11"
-    MASTER_SIZE="n1-standard-4"
-    NODE_SIZE="n1-standard-8"   # Note: can fit about 17 hollow nodes per core
-    #                                     so NUM_NODES x cores_per_node should
-    #                                     be set accordingly.
-    KUBE_GCE_INSTANCE_PREFIX="kubemark1000"
-    E2E_ZONE="us-east1-b"
-    KUBEMARK_MASTER_SIZE="n1-standard-16"
-    KUBEMARK_NUM_NODES="1000"
-    ;;
-
-  # Soak jobs
-
-  # Sets up the GCE soak cluster weekly using the latest CI release.
-  kubernetes-soak-weekly-deploy-gce)
-    : ${E2E_CLUSTER_NAME:="gce-soak-weekly"}
-    : ${E2E_DOWN:="false"}
-    : ${E2E_NETWORK:="gce-soak-weekly"}
-    : ${E2E_TEST:="false"}
-    : ${E2E_UP:="true"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="gce-soak-weekly"}
-    : ${HAIRPIN_MODE:="false"}
-    : ${PROJECT:="kubernetes-jenkins"}
-    ;;
-
-  # Runs tests on GCE soak cluster.
-  kubernetes-soak-continuous-e2e-gce)
-    : ${E2E_CLUSTER_NAME:="gce-soak-weekly"}
-    : ${E2E_DOWN:="false"}
-    : ${E2E_NETWORK:="gce-soak-weekly"}
-    : ${E2E_UP:="false"}
-    # Clear out any orphaned namespaces in case previous run was interrupted.
-    : ${E2E_CLEAN_START:="true"}
-    # We should be testing the reliability of a long-running cluster. The
-    # [Disruptive] tests kill/restart components or nodes in the cluster,
-    # defeating the purpose of a soak cluster. (#15722)
-    : ${GINKGO_TEST_ARGS:="--ginkgo.skip=\[Disruptive\]|\[Flaky\]|\[Feature:.+\]"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="gce-soak-weekly"}
-    : ${HAIRPIN_MODE:="false"}
-    : ${PROJECT:="kubernetes-jenkins"}
-    ;;
-
-  # Clone of kubernetes-soak-weekly-deploy-gce. Issue #20832.
-  kubernetes-soak-weekly-deploy-gce-2)
-    : ${E2E_CLUSTER_NAME:="gce-soak-weekly-2"}
-    : ${E2E_DOWN:="false"}
-    : ${E2E_NETWORK:="gce-soak-weekly-2"}
-    : ${E2E_TEST:="false"}
-    : ${E2E_UP:="true"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="gce-soak-weekly-2"}
-    : ${PROJECT:="kubernetes-jenkins"}
-    ;;
-
-  # Clone of kubernetes-soak-continuous-e2e-gce. Issue #20832.
-  kubernetes-soak-continuous-e2e-gce-2)
-    : ${E2E_CLUSTER_NAME:="gce-soak-weekly-2"}
-    : ${E2E_DOWN:="false"}
-    : ${E2E_NETWORK:="gce-soak-weekly-2"}
-    : ${E2E_UP:="false"}
-    # Clear out any orphaned namespaces in case previous run was interrupted.
-    : ${E2E_CLEAN_START:="true"}
-    # We should be testing the reliability of a long-running cluster. The
-    # [Disruptive] tests kill/restart components or nodes in the cluster,
-    # defeating the purpose of a soak cluster. (#15722)
-    #
-    # TODO(ihmccreery) remove [Skipped] once tests are relabeled
-    : ${GINKGO_TEST_ARGS:="--ginkgo.skip=\[Disruptive\]|\[Flaky\]|\[Feature:.+\]|\[Skipped\]"}
-    : ${KUBE_GCE_INSTANCE_PREFIX:="gce-soak-weekly-2"}
-    : ${PROJECT:="kubernetes-jenkins"}
-    ;;
-
-  # Sets up the GKE soak cluster weekly using the latest CI release.
-  kubernetes-soak-weekly-deploy-gke)
-    : ${E2E_CLUSTER_NAME:="jenkins-gke-soak-weekly"}
-    : ${E2E_DOWN:="false"}
-    : ${E2E_NETWORK:="gke-soak-weekly"}
-    : ${E2E_SET_CLUSTER_API_VERSION:=y}
-    : ${JENKINS_PUBLISHED_VERSION:="ci/latest"}
-    : ${E2E_TEST:="false"}
-    : ${E2E_UP:="true"}
-    : ${PROJECT:="kubernetes-jenkins"}
-    # Need at least n1-standard-2 nodes to run kubelet_perf tests
-    NODE_SIZE="n1-standard-2"
-    ;;
-
-  # Runs tests on GKE soak cluster.
-  kubernetes-soak-continuous-e2e-gke)
-    : ${E2E_CLUSTER_NAME:="jenkins-gke-soak-weekly"}
-    : ${E2E_NETWORK:="gke-soak-weekly"}
-    : ${E2E_DOWN:="false"}
-    : ${E2E_UP:="false"}
-    # Clear out any orphaned namespaces in case previous run was interrupted.
-    : ${E2E_CLEAN_START:="true"}
-    : ${PROJECT:="kubernetes-jenkins"}
-    : ${E2E_OPT:="--check_version_skew=false"}
-    # We should be testing the reliability of a long-running cluster. The
-    # [Disruptive] tests kill/restart components or nodes in the cluster,
-    # defeating the purpose of a soak cluster. (#15722)
-    : ${GINKGO_TEST_ARGS:="--ginkgo.skip=\[Disruptive\]|\[Flaky\]|\[Feature:.+\]"}
+    export TEST_CLUSTER_RESYNC_PERIOD="--min-resync-period=12h"
     ;;
 
   # Upgrade jobs
