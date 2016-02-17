299ca36405d0132e99bd84f5e29fcde739a89bbc
Phillip Wittrock <pwittroc@google.com>
2016-02-16 17:16:06 -0800

    Fix volume secret_test.go on darwin by not checking volume usage metrics

--- pkg/volume/util/fs_darwin.go
+++ pkg/volume/util/fs_darwin.go
@@ -1,7 +1,7 @@
-// +build !linux,!darwin
+// +build darwin
 
 /*
-Copyright 2014 The Kubernetes Authors All rights reserved.
+Copyright 2016 The Kubernetes Authors All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -21,15 +21,27 @@ package util
 import (
 	"errors"
 	"fmt"
+	"os/exec"
+	"strings"
 
 	"k8s.io/kubernetes/pkg/api/resource"
 )
 
-// FSInfo unsupported returns 0 values for available and capacity and an error.
+// FSInfo linux returns (available bytes, byte capacity, error) for the filesystem that
+// path resides upon.
 func FsInfo(path string) (int64, int64, error) {
 	return 0, 0, errors.New("FsInfo not supported for this build.")
 }
 
 func Du(path string) (*resource.Quantity, error) {
-	return nil, fmt.Errorf("Du not support for this build.")
+	out, err := exec.Command("nice", "-n", "19", "du", "-s", path).CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("failed command 'du' ($ nice -n 19 du -s) on path %s with error %v", path, err)
+	}
+	used, err := resource.ParseQuantity(strings.Fields(string(out))[0])
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse 'du' output %s due to error %v", out, err)
+	}
+	used.Format = resource.BinarySI
+	return used, nil
 }
