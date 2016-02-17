877b6fe8dacf608fc95ca410e9bc47c40924aaec
Shaya Potter <spotter@gmail.com>
2016-02-16 23:35:25 -0800

    update boilerplate to avoid travis complaining

--- pkg/kubelet/disk_manager.go
+++ pkg/kubelet/disk_manager.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2015 The Kubernetes Authors All rights reserved.
+Copyright 2016 The Kubernetes Authors All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@ You may obtain a copy of the License at
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-GSee the License for the specific language governing permissions and
+See the License for the specific language governing permissions and
 limitations under the License.
 */
 
