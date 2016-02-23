a161b7ecb24a373e99e11f667df2d4dd58ef82d1
Daniel Smith <dbsmith@google.com>
2016-02-22 17:57:32 -0800

    godeps changes?

--- Godeps/_workspace/src/github.com/rackspace/gophercloud/openstack/identity/v3/roles/urls.go
+++
@@ -1,7 +0,0 @@
-package roles
-
-import "github.com/rackspace/gophercloud"
-
-func listAssignmentsURL(client *gophercloud.ServiceClient) string {
-	return client.ServiceURL("role_assignments")
-}
--- Godeps/_workspace/src/github.com/rackspace/gophercloud/rackspace/cdn/v1/base/doc.go
+++
@@ -1,4 +0,0 @@
-// Package base provides information and interaction with the base API
-// resource in the Rackspace CDN service. This API resource allows for
-// retrieving the Home Document and pinging the root URL.
-package base
