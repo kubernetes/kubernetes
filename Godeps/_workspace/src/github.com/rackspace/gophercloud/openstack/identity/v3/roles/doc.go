0122da15fd3fc098d882d9f68a4be4a66a4c1e03
Edward Muller <edwardam@interlix.com>
2016-02-23 10:08:47 -0800

    Clean up vendored deps using latest version of godep

--- Godeps/_workspace/src/github.com/rackspace/gophercloud/openstack/identity/v3/roles/doc.go
+++
@@ -1,3 +0,0 @@
-// Package roles provides information and interaction with the roles API
-// resource for the OpenStack Identity service.
-package roles
--- Godeps/_workspace/src/github.com/rackspace/gophercloud/rackspace/compute/v2/networks/doc.go
+++
@@ -1,3 +0,0 @@
-// Package networks provides information and interaction with the network
-// API resource for the Rackspace Cloud Servers service.
-package networks
