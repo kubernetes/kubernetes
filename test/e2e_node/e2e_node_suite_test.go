77d4f0108023e305ca645b97281965028ee133b9
Phillip Wittrock <pwittroc@google.com>
2016-02-24 13:12:42 -0800

    Node e2e test runner - run against images - support allocating gce instances from images and running tests against them - set --hostname-override to match node name - add jenkins script to source to reproduce jenkins build locally

--- test/e2e_node/e2e_node_suite_test.go
+++ test/e2e_node/e2e_node_suite_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2015 The Kubernetes Authors All rights reserved.
+Copyright 2016 The Kubernetes Authors All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -61,7 +61,7 @@ var _ = BeforeSuite(func() {
 	}
 
 	if *startServices {
-		e2es = newE2eService()
+		e2es = newE2eService(*nodeName)
 		if err := e2es.start(); err != nil {
 			Fail(fmt.Sprintf("Unable to start node services.\n%v", err))
 		}
