6c57e1f29dddd88f8d2221a44a86731507f22b56
Peter Souter <p.morsou@gmail.com>
2016-03-08 09:31:05 +0000

    Adds a method to compare semantic versions
    Taken from examples from: http://stackoverflow.com/questions/4023830/bash-how-compare-two-strings-in-version-format

--- docs/getting-started-guides/docker-multinode/worker.sh
+++ docs/getting-started-guides/docker-multinode/worker.sh
@@ -56,6 +56,38 @@ command_exists() {
     command -v "$@" > /dev/null 2>&1
 }
 
+# Compare semvers
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
 lsb_dist=""
 
 # Detect the OS distro, we support ubuntu, debian, mint, centos, fedora dist
