ba13bdbaa5967476d7c3f2bd44d4cc30370baa31
Wojciech Tyczynski <wojtekt@google.com>
2016-03-08 09:53:16 +0100

    Generate protobuf-related files in Docker

--- hack/update-generated-protobuf.sh
+++ hack/update-generated-protobuf.sh
@@ -19,12 +19,36 @@ set -o nounset
 set -o pipefail
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
-source "${KUBE_ROOT}/hack/lib/init.sh"
+source "${KUBE_ROOT}/build/common.sh"
 
 kube::golang::setup_env
 
-"${KUBE_ROOT}/hack/build-go.sh" cmd/libs/go2idl/go-to-protobuf cmd/libs/go2idl/go-to-protobuf/protoc-gen-gogo
+function prereqs() {
+  kube::log::status "Verifying Prerequisites...."
+  kube::build::ensure_docker_in_path || return 1
+  if kube::build::is_osx; then
+      kube::build::docker_available_on_osx || return 1
+  fi
+  kube::build::ensure_docker_daemon_connectivity || return 1
 
-"${KUBE_ROOT}/hack/after-build/update-generated-protobuf.sh" "$@"
+  KUBE_ROOT_HASH=$(kube::build::short_hash "$KUBE_ROOT/go-to-protobuf")
+  KUBE_BUILD_IMAGE_TAG="build-${KUBE_ROOT_HASH}"
+  KUBE_BUILD_IMAGE="${KUBE_BUILD_IMAGE_REPO}:${KUBE_BUILD_IMAGE_TAG}"
+  KUBE_BUILD_CONTAINER_NAME="kube-build-${KUBE_ROOT_HASH}"
+  KUBE_BUILD_DATA_CONTAINER_NAME="kube-build-data-${KUBE_ROOT_HASH}"
+  DOCKER_MOUNT_ARGS=(
+    --volume "${KUBE_ROOT}:/go/src/${KUBE_GO_PACKAGE}"
+    --volume /etc/localtime:/etc/localtime:ro
+    --volumes-from "${KUBE_BUILD_DATA_CONTAINER_NAME}"
+	)
+  LOCAL_OUTPUT_BUILD_CONTEXT="${LOCAL_OUTPUT_IMAGE_STAGING}/${KUBE_BUILD_IMAGE}"
+}
+
+prereqs
+mkdir -p "${LOCAL_OUTPUT_BUILD_CONTEXT}"
+cp "${KUBE_ROOT}/cmd/libs/go2idl/go-to-protobuf/build-image/Dockerfile" "${LOCAL_OUTPUT_BUILD_CONTEXT}/Dockerfile"
+kube::build::update_dockerfile
+kube::build::docker_build "${KUBE_BUILD_IMAGE}" "${LOCAL_OUTPUT_BUILD_CONTEXT}" 'false'
+kube::build::run_build_command hack/after-build/update-generated-protobuf.sh "$@"
 
 # ex: ts=2 sw=2 et filetype=sh
