5543e2e3fd8a314295ae6ef0aabb5c9a3cc20925
Eric Paris <eparis@redhat.com>
2016-03-10 13:45:36 -0500

    Create a script to update godeps
    Since its hard for an individual to remember that we need codecgen and
    ginkgo in godeps but don't actually have that dependancy listed in a way
    that godep can automatically find.

---
+++ hack/godep-save.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright 2016 The Kubernetes Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
+GODEP="${GODEP:-godep}"
+
+# Some things we want in godeps aren't code dependencies, so ./...
+# won't pick them up.
+REQUIRED_BINS=(
+  "github.com/ugorji/go/codec/codecgen"
+  "github.com/onsi/ginkgo/ginkgo"
+  "./..."
+)
+
+pushd "${KUBE_ROOT}" > /dev/null
+  GO15VENDOREXPERIMENT=1 ${GODEP} save "${REQUIRED_BINS[@]}"
+popd > /dev/null
