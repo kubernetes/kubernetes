9265d28e325cf9dc6234f05120f61d624756558c
Fabiano Franz <ffranz@redhat.com>
2016-03-11 19:06:42 -0300

    Mark filename flags for completions

--- pkg/kubectl/cmd/util/helpers.go
+++ pkg/kubectl/cmd/util/helpers.go
@@ -331,6 +331,7 @@ func GetFlagDuration(cmd *cobra.Command, flag string) time.Duration {
 func AddValidateFlags(cmd *cobra.Command) {
 	cmd.Flags().Bool("validate", true, "If true, use a schema to validate the input before sending it")
 	cmd.Flags().String("schema-cache-dir", fmt.Sprintf("~/%s/%s", clientcmd.RecommendedHomeDir, clientcmd.RecommendedSchemaName), fmt.Sprintf("If non-empty, load/store cached API schemas in this directory, default is '$HOME/%s/%s'", clientcmd.RecommendedHomeDir, clientcmd.RecommendedSchemaName))
+	cmd.MarkFlagFilename("schema-cache-dir")
 }
 
 func AddRecursiveFlag(cmd *cobra.Command, value *bool) {
