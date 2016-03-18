a9c308851d7bc20a95de7287c7ea5005c559d3a6
jianhuiz <jianhui@outlook.com>
2016-03-18 11:22:38 -0700

    add api group 'controlplane' and 'cluster' object

---
+++ pkg/apis/controlplane/validation/validation_test.go
@@ -0,0 +1,106 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validation
+
+import (
+	"testing"
+
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/controlplane"
+)
+
+func TestValidateCluster(t *testing.T) {
+	successCases := []controlplane.Cluster{
+		{
+			ObjectMeta: api.ObjectMeta{Name: "cluster-s"},
+			Spec: controlplane.ClusterSpec{
+				Address: controlplane.ClusterAddress{
+					Url: "http://localhost:8888",
+				},
+			},
+		},
+	}
+	for _, successCase := range successCases {
+		errs := ValidateCluster(&successCase)
+		if len(errs) != 0 {
+			t.Errorf("expect success: %v", errs)
+		}
+	}
+
+	errorCases := map[string]controlplane.Cluster{
+		"missing cluster address": {
+			ObjectMeta: api.ObjectMeta{Name: "cluster-f"},
+		},
+		"invalid_label": {
+			ObjectMeta: api.ObjectMeta{
+				Name: "cluster-f",
+				Labels: map[string]string{
+					"NoUppercaseOrSpecialCharsLike=Equals": "bar",
+				},
+			},
+		},
+	}
+	for testName, errorCase := range errorCases {
+		errs := ValidateCluster(&errorCase)
+		if len(errs) == 0 {
+			t.Errorf("expected failur for %s", testName)
+		}
+	}
+}
+
+func TestValidateClusterUpdate(t *testing.T) {
+	type clusterUpdateTest struct {
+		old    controlplane.Cluster
+		update controlplane.Cluster
+	}
+	successCases := []clusterUpdateTest{
+		{
+			old: controlplane.Cluster{
+				ObjectMeta: api.ObjectMeta{Name: "cluster-s"},
+				Spec: controlplane.ClusterSpec{
+					Address: controlplane.ClusterAddress{
+						Url: "http://localhost:8888",
+					},
+				},
+			},
+			update: controlplane.Cluster{
+				ObjectMeta: api.ObjectMeta{Name: "cluster-s"},
+				Spec: controlplane.ClusterSpec{
+					Address: controlplane.ClusterAddress{
+						Url: "http://127.0.0.1:8888",
+					},
+				},
+			},
+		},
+	}
+	for _, successCase := range successCases {
+		successCase.old.ObjectMeta.ResourceVersion = "1"
+		successCase.update.ObjectMeta.ResourceVersion = "1"
+		errs := ValidateClusterUpdate(&successCase.update, &successCase.old)
+		if len(errs) != 0 {
+			t.Errorf("expect success: %v", errs)
+		}
+	}
+
+	errorCases := map[string]clusterUpdateTest{}
+	for testName, errorCase := range errorCases {
+		errs := ValidateClusterUpdate(&errorCase.update, &errorCase.old)
+		if len(errs) == 0 {
+			t.Errorf("expected failure: %s", testName)
+		}
+	}
+}
