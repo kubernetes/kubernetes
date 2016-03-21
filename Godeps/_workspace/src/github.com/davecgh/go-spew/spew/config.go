f90758234e18073350ff5ff27d8a67d43bb32cc0
Joe Finney <spxtr@google.com>
2016-03-21 15:27:34 -0700

    Bump spew godep to make hash tests pass on go 1.6.

--- Godeps/_workspace/src/github.com/davecgh/go-spew/spew/config.go
+++ Godeps/_workspace/src/github.com/davecgh/go-spew/spew/config.go
@@ -61,7 +61,10 @@ type ConfigState struct {
 	// with a pointer receiver could technically mutate the value, however,
 	// in practice, types which choose to satisify an error or Stringer
 	// interface with a pointer receiver should not be mutating their state
-	// inside these interface methods.
+	// inside these interface methods.  As a result, this option relies on
+	// access to the unsafe package, so it will not have any effect when
+	// running in environments without access to the unsafe package such as
+	// Google App Engine or with the "disableunsafe" build tag specified.
 	DisablePointerMethods bool
 
 	// ContinueOnMethod specifies whether or not recursion should continue once
