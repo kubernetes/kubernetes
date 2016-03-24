9e5ddcb822068e1dca76a2bcef1740b6914e60c6
Vishnu kannan <vishnuk@google.com>
2016-03-24 12:05:08 -0700

    Do not track resource usage for host path volumes. They can contain loops.
    Signed-off-by: Vishnu kannan <vishnuk@google.com>

--- pkg/volume/host_path/host_path_test.go
+++ pkg/volume/host_path/host_path_test.go
@@ -20,7 +20,6 @@ package host_path
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"testing"
 
@@ -270,51 +269,5 @@ func TestPersistentClaimReadOnlyFlag(t *testing.T) {
 
 	if !builder.GetAttributes().ReadOnly {
 		t.Errorf("Expected true for builder.IsReadOnly")
-	}
-}
-
-// TestMetrics tests that MetricProvider methods return sane values.
-func TestMetrics(t *testing.T) {
-	// Create an empty temp directory for the volume
-	tmpDir, err := ioutil.TempDir(os.TempDir(), "host_path_test")
-	if err != nil {
-		t.Fatalf("Can't make a tmp dir: %v", err)
-	}
-	defer os.RemoveAll(tmpDir)
-
-	plugMgr := volume.VolumePluginMgr{}
-	plugMgr.InitPlugins(ProbeVolumePlugins(volume.VolumeConfig{}), volumetest.NewFakeVolumeHost(tmpDir, nil, nil))
-
-	plug, err := plugMgr.FindPluginByName("kubernetes.io/host-path")
-	if err != nil {
-		t.Errorf("Can't find the plugin by name")
-	}
-	spec := &api.Volume{
-		Name:         "vol1",
-		VolumeSource: api.VolumeSource{HostPath: &api.HostPathVolumeSource{Path: tmpDir}},
-	}
-	pod := &api.Pod{ObjectMeta: api.ObjectMeta{UID: types.UID("poduid")}}
-	builder, err := plug.NewBuilder(volume.NewSpecFromVolume(spec), pod, volume.VolumeOptions{})
-	if err != nil {
-		t.Errorf("Failed to make a new Builder: %v", err)
-	}
-
-	expectedEmptyDirUsage, err := volumetest.FindEmptyDirectoryUsageOnTmpfs()
-	if err != nil {
-		t.Errorf("Unexpected error finding expected empty directory usage on tmpfs: %v", err)
-	}
-
-	metrics, err := builder.GetMetrics()
-	if err != nil {
-		t.Errorf("Unexpected error when calling GetMetrics %v", err)
-	}
-	if e, a := expectedEmptyDirUsage.Value(), metrics.Used.Value(); e != a {
-		t.Errorf("Unexpected value for empty directory; expected %v, got %v", e, a)
-	}
-	if metrics.Capacity.Value() <= 0 {
-		t.Errorf("Expected Capacity to be greater than 0")
-	}
-	if metrics.Available.Value() <= 0 {
-		t.Errorf("Expected Available to be greater than 0")
 	}
 }
