952e9e00065091fca41d13934dfcbfe92849424b
zhouhaibing089 <zhouhaibing089@gmail.com>
2016-03-26 10:53:40 +0800

    rolling update will check whether the image is distinct or not, however the image with tag latest should be excluded

--- pkg/kubectl/cmd/rollingupdate.go
+++ pkg/kubectl/cmd/rollingupdate.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/golang/glog"
@@ -246,7 +247,7 @@ func RunRollingUpdate(f *cmdutil.Factory, out io.Writer, cmd *cobra.Command, arg
 			}
 			fmt.Fprintf(out, "Found existing update in progress (%s), resuming.\n", newRc.Name)
 		} else {
-			if oldRc.Spec.Template.Spec.Containers[0].Image == image {
+			if oldRc.Spec.Template.Spec.Containers[0].Image == image && !strings.HasSuffix(image, ":latest") {
 				return cmdutil.UsageError(cmd, "Specified --image must be distinct from existing container image")
 			}
 			newRc, err = kubectl.CreateNewControllerFromCurrentController(client, codec, cmdNamespace, oldName, newName, image, container, deploymentKey)
