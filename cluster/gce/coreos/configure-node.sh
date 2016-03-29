820954de16d4ac2a6b325cdc5685419a409c96a2
Mike Danese <mikedanese@google.com>
2016-03-29 14:27:28 -0700

    stop create secrets for dns, it uses the kube-system service account now

--- cluster/gce/coreos/configure-node.sh
+++ cluster/gce/coreos/configure-node.sh
@@ -256,7 +256,7 @@ function create-salt-master-auth() {
     # NB: If this list ever changes, this script actually has to
     # change to detect the existence of this file, kill any deleted
     # old tokens and add any new tokens (to handle the upgrade case).
-    local -r service_accounts=("system:scheduler" "system:controller_manager" "system:logging" "system:monitoring" "system:dns")
+    local -r service_accounts=("system:scheduler" "system:controller_manager" "system:logging" "system:monitoring")
     for account in "${service_accounts[@]}"; do
       token=$(dd if=/dev/urandom bs=128 count=1 2>/dev/null | base64 | tr -d "=+/" | dd bs=32 count=1 2>/dev/null)
       echo "${token},${account},${account}" >> "${KNOWN_TOKENS_FILE}"
