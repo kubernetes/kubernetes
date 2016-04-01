f1f7b5a813547f4616e6c13d0b09b7b1ecc4b280
Eric Paris <eparis@redhat.com>
2016-04-01 15:16:44 -0400

    Update Godeps with v63

--- vendor/github.com/rackspace/gophercloud/rackspace/compute/v2/flavors/urls.go
+++
@@ -1,9 +0,0 @@
-package flavors
-
-import (
-	"github.com/rackspace/gophercloud"
-)
-
-func getURL(client *gophercloud.ServiceClient, id string) string {
-	return client.ServiceURL("flavors", id)
-}
--- vendor/github.com/rackspace/gophercloud/rackspace/objectstorage/v1/objects/doc.go
+++
@@ -1,3 +0,0 @@
-// Package objects provides information and interaction with the Object
-// API resource for the Rackspace Cloud Files service.
-package objects
