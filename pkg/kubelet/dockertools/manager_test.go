548ba1fedd7ee8c2cc5f33e2184d600ba1f8b54a
Random-Liu <taotaotheripper@gmail.com>
2016-04-01 00:58:46 -0700

    Change the label key to follow the k8s and docker label naming convention.

--- pkg/kubelet/dockertools/manager_test.go
+++ pkg/kubelet/dockertools/manager_test.go
@@ -503,8 +503,8 @@ func TestKillContainerInPodWithPreStop(t *testing.T) {
 			Name: "/k8s_foo_qux_new_1234_42",
 			Config: &docker.Config{
 				Labels: map[string]string{
-					kubernetesPodLabel:           string(podString),
-					kubernetesContainerNameLabel: "foo",
+					podLabel:           string(podString),
+					containerNameLabel: "foo",
 				},
 			},
 		},
