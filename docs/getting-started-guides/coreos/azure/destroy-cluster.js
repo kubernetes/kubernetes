12ddbef656daf11414a78edec26245a310e3ed3f
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-04-04 16:10:06 +0100

    coreos/azure: Move into a separate repo

--- docs/getting-started-guides/coreos/azure/destroy-cluster.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-var azure = require('./lib/azure_wrapper.js');
-
-azure.destroy_cluster(process.argv[2]);
-
-console.log('The cluster had been destroyed, you can delete the state file now.');
