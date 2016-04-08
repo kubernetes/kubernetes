b28e6e94c59c2f843e3261eec950c32e5bdeaa03
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2016-04-08 18:27:08 +0900

    Make shell script compatible with older bash

--- cluster/openstack/util.sh
+++ cluster/openstack/util.sh
@@ -91,7 +91,7 @@ function create-stack() {
   echo "[INFO] Execute commands to create Kubernetes cluster"
   # It is required for some cloud provider like CityCloud where swift client has different credentials
   source "${ROOT}/openrc-swift.sh"
-  if [[ ! -v OS_PROJECT_ID ]]; then
+  if [[ -z "${OS_PROJECT_ID}" ]]; then
     SWIFT_PROJECT_ID="${OS_TENANT_ID}"
   else
     SWIFT_PROJECT_ID="${OS_PROJECT_ID}"
