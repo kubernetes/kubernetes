19c129ebe110b9ed09088e4233958b8f4b958933
Ruddarraju, Uday Kumar Raju <uruddarraju@ebay.com>
2016-04-11 19:51:45 -0700

    Updating go-restful dependency to fix thirdparty

--- Godeps/_workspace/src/github.com/emicklei/go-restful/examples/google_app_engine/restful-user-service.go
+++ Godeps/_workspace/src/github.com/emicklei/go-restful/examples/google_app_engine/restful-user-service.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/emicklei/go-restful"
 	"github.com/emicklei/go-restful/swagger"
-	"google.golang.com/appengine"
-	"google.golang.com/appengine/memcache"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/memcache"
 	"net/http"
 )
 
