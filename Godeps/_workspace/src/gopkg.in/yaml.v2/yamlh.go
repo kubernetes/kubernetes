c91126209e6d8990d1f3342248f81fac158eabf6
Thomas Liu <tyangliu@gmail.com>
2016-04-11 17:05:05 -0700

    Bump up yaml.v2 version

--- Godeps/_workspace/src/gopkg.in/yaml.v2/yamlh.go
+++ Godeps/_workspace/src/gopkg.in/yaml.v2/yamlh.go
@@ -296,7 +296,7 @@ const (
 
 	// Not in original libyaml.
 	yaml_BINARY_TAG = "tag:yaml.org,2002:binary"
-	yaml_MERGE_TAG = "tag:yaml.org,2002:merge"
+	yaml_MERGE_TAG  = "tag:yaml.org,2002:merge"
 
 	yaml_DEFAULT_SCALAR_TAG   = yaml_STR_TAG // The default scalar tag is !!str.
 	yaml_DEFAULT_SEQUENCE_TAG = yaml_SEQ_TAG // The default sequence tag is !!seq.
