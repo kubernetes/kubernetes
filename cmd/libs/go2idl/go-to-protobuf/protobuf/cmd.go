cfc56bbf3dbd1e4a59962c360c0f43acbd34c751
George Tankersley <george.tankersley@coreos.com>
2016-04-19 13:22:08 -0700

    codegen: add certificates group to generators

--- cmd/libs/go2idl/go-to-protobuf/protobuf/cmd.go
+++ cmd/libs/go2idl/go-to-protobuf/protobuf/cmd.go
@@ -74,6 +74,7 @@ func New() *Generator {
 			`k8s.io/kubernetes/pkg/apis/apps/v1alpha1`,
 			`k8s.io/kubernetes/pkg/apis/rbac/v1alpha1`,
 			`k8s.io/kubernetes/federation/apis/federation/v1alpha1`,
+			`k8s.io/kubernetes/pkg/apis/certificates/v1alpha1`,
 		}, ","),
 		DropEmbeddedFields: "k8s.io/kubernetes/pkg/api/unversioned.TypeMeta",
 	}
