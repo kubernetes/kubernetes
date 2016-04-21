4a0eafaee9a4586803107a5e153046dc2e5b27df
derekwaynecarr <decarr@redhat.com>
2016-04-21 16:58:06 -0400

    Add utility for determining qos of a pod

--- pkg/kubelet/qos/util/qos.go
+++ pkg/kubelet/qos/util/qos.go
@@ -18,6 +18,7 @@ package util
 
 import (
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/util/sets"
 )
 
 const (
@@ -44,6 +45,25 @@ func isResourceBestEffort(container *api.Container, resource api.ResourceName) b
 	// If a request is specified, then the user expects some kind of resource guarantee.
 	req, hasReq := container.Resources.Requests[resource]
 	return !hasReq || req.Value() == 0
+}
+
+// GetPodQos returns the QoS class of a pod.
+// The QoS class of a pod is the lowest QoS class for each resource in each container.
+func GetPodQos(pod *api.Pod) string {
+	qosValues := sets.NewString()
+	for _, container := range pod.Spec.Containers {
+		qosPerResource := GetQoS(&container)
+		for _, qosValue := range qosPerResource {
+			qosValues.Insert(qosValue)
+		}
+	}
+	if qosValues.Has(BestEffort) {
+		return BestEffort
+	}
+	if qosValues.Has(Burstable) {
+		return Burstable
+	}
+	return Guaranteed
 }
 
 // GetQos returns a mapping of resource name to QoS class of a container
