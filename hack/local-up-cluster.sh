6dfeeb43f5525824191bd7f32d70aa085653e50d
Jeff Vance <jvance@redhat.com>
2016-04-22 09:51:02 -0700

    added claim_binder sync period

--- hack/local-up-cluster.sh
+++ hack/local-up-cluster.sh
@@ -110,6 +110,7 @@ RKT_STAGE1_IMAGE=${RKT_STAGE1_IMAGE:-""}
 CHAOS_CHANCE=${CHAOS_CHANCE:-0.0}
 CPU_CFS_QUOTA=${CPU_CFS_QUOTA:-false}
 ENABLE_HOSTPATH_PROVISIONER=${ENABLE_HOSTPATH_PROVISIONER:-"false"}
+CLAIM_BINDER_SYNC_PERIOD=${CLAIM_BINDER_SYNC_PERIOD:-"10m"} # current k8s default
 
 function test_apiserver_off {
     # For the common local scenario, fail fast if server is already running.
@@ -288,6 +289,7 @@ function start_controller_manager {
       --root-ca-file="${ROOT_CA_FILE}" \
       --enable-hostpath-provisioner="${ENABLE_HOSTPATH_PROVISIONER}" \
       ${node_cidr_args} \
+      --pvclaimbinder-sync-period="${CLAIM_BINDER_SYNC_PERIOD}" \
       --master="${API_HOST}:${API_PORT}" >"${CTLRMGR_LOG}" 2>&1 &
     CTLRMGR_PID=$!
 }
