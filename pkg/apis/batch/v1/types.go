eb88e63609ed4a0ad8003fe367c34e0c71d364c2
Chao Xu <xuchao@google.com>
2016-04-26 15:12:06 -0700

    add release_1_3 clientset in update-codegen

--- pkg/apis/batch/v1/types.go
+++ pkg/apis/batch/v1/types.go
@@ -21,6 +21,8 @@ import (
 	"k8s.io/kubernetes/pkg/api/v1"
 )
 
+// +genclient=true
+
 // Job represents the configuration of a single job.
 type Job struct {
 	unversioned.TypeMeta `json:",inline"`
