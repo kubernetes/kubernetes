9a0744efb4323b6938790ab98a97193e47e48b33
Jay Vyas <jay@apache.org>
2016-04-26 12:22:18 -0400

    Standard input for kubectl tests rebased to include gobindata as well.

--- hack/boilerplate/boilerplate.py
+++ hack/boilerplate/boilerplate.py
@@ -105,7 +105,8 @@ def file_passes(filename, refs, regexs):
 def file_extension(filename):
     return os.path.splitext(filename)[1].split(".")[-1].lower()
 
-skipped_dirs = ['vendor', 'third_party', '_gopath', '_output', '.git', 'cluster/env.sh', 'vendor']
+skipped_dirs = ['Godeps', 'third_party', '_gopath', '_output', '.git', 'cluster/env.sh', "vendor", "test/e2e/generated/bindata.go"]
+
 def normalize_files(files):
     newfiles = []
     for pathname in files:
