adf3c387ee9b822baf17dc0f63d7212729589b2f
Clayton Coleman <ccoleman@redhat.com>
2016-05-02 00:40:04 -0400

    Regenerate api/unversioned by hand so that it is complete
    We need to break the circular dependency on generation to the scheme.

--- pkg/api/unversioned/deep_copy_generated.go
+++ pkg/api/unversioned/deep_copy_generated.go
@@ -21,12 +21,124 @@ limitations under the License.
 package unversioned
 
 import (
+	"time"
+
 	conversion "k8s.io/kubernetes/pkg/conversion"
-	time "time"
 )
+
+func DeepCopy_unversioned_APIGroup(in APIGroup, out *APIGroup, c *conversion.Cloner) error {
+	if err := DeepCopy_unversioned_TypeMeta(in.TypeMeta, &out.TypeMeta, c); err != nil {
+		return err
+	}
+	out.Name = in.Name
+	if in.Versions != nil {
+		in, out := in.Versions, &out.Versions
+		*out = make([]GroupVersionForDiscovery, len(in))
+		for i := range in {
+			if err := DeepCopy_unversioned_GroupVersionForDiscovery(in[i], &(*out)[i], c); err != nil {
+				return err
+			}
+		}
+	} else {
+		out.Versions = nil
+	}
+	if err := DeepCopy_unversioned_GroupVersionForDiscovery(in.PreferredVersion, &out.PreferredVersion, c); err != nil {
+		return err
+	}
+	if in.ServerAddressByClientCIDRs != nil {
+		in, out := in.ServerAddressByClientCIDRs, &out.ServerAddressByClientCIDRs
+		*out = make([]ServerAddressByClientCIDR, len(in))
+		for i := range in {
+			if err := DeepCopy_unversioned_ServerAddressByClientCIDR(in[i], &(*out)[i], c); err != nil {
+				return err
+			}
+		}
+	} else {
+		out.ServerAddressByClientCIDRs = nil
+	}
+	return nil
+}
+
+func DeepCopy_unversioned_APIGroupList(in APIGroupList, out *APIGroupList, c *conversion.Cloner) error {
+	if err := DeepCopy_unversioned_TypeMeta(in.TypeMeta, &out.TypeMeta, c); err != nil {
+		return err
+	}
+	if in.Groups != nil {
+		in, out := in.Groups, &out.Groups
+		*out = make([]APIGroup, len(in))
+		for i := range in {
+			if err := DeepCopy_unversioned_APIGroup(in[i], &(*out)[i], c); err != nil {
+				return err
+			}
+		}
+	} else {
+		out.Groups = nil
+	}
+	return nil
+}
+
+func DeepCopy_unversioned_APIResource(in APIResource, out *APIResource, c *conversion.Cloner) error {
+	out.Name = in.Name
+	out.Namespaced = in.Namespaced
+	out.Kind = in.Kind
+	return nil
+}
+
+func DeepCopy_unversioned_APIResourceList(in APIResourceList, out *APIResourceList, c *conversion.Cloner) error {
+	if err := DeepCopy_unversioned_TypeMeta(in.TypeMeta, &out.TypeMeta, c); err != nil {
+		return err
+	}
+	out.GroupVersion = in.GroupVersion
+	if in.APIResources != nil {
+		in, out := in.APIResources, &out.APIResources
+		*out = make([]APIResource, len(in))
+		for i := range in {
+			if err := DeepCopy_unversioned_APIResource(in[i], &(*out)[i], c); err != nil {
+				return err
+			}
+		}
+	} else {
+		out.APIResources = nil
+	}
+	return nil
+}
+
+func DeepCopy_unversioned_APIVersions(in APIVersions, out *APIVersions, c *conversion.Cloner) error {
+	if err := DeepCopy_unversioned_TypeMeta(in.TypeMeta, &out.TypeMeta, c); err != nil {
+		return err
+	}
+	if in.Versions != nil {
+		in, out := in.Versions, &out.Versions
+		*out = make([]string, len(in))
+		copy(*out, in)
+	} else {
+		out.Versions = nil
+	}
+	if in.ServerAddressByClientCIDRs != nil {
+		in, out := in.ServerAddressByClientCIDRs, &out.ServerAddressByClientCIDRs
+		*out = make([]ServerAddressByClientCIDR, len(in))
+		for i := range in {
+			if err := DeepCopy_unversioned_ServerAddressByClientCIDR(in[i], &(*out)[i], c); err != nil {
+				return err
+			}
+		}
+	} else {
+		out.ServerAddressByClientCIDRs = nil
+	}
+	return nil
+}
 
 func DeepCopy_unversioned_Duration(in Duration, out *Duration, c *conversion.Cloner) error {
 	out.Duration = in.Duration
+	return nil
+}
+
+func DeepCopy_unversioned_ExportOptions(in ExportOptions, out *ExportOptions, c *conversion.Cloner) error {
+	if err := DeepCopy_unversioned_TypeMeta(in.TypeMeta, &out.TypeMeta, c); err != nil {
+		return err
+	}
+	out.Export = in.Export
+	out.Exact = in.Exact
 	return nil
 }
 
@@ -44,6 +156,12 @@ func DeepCopy_unversioned_GroupResource(in GroupResource, out *GroupResource, c 
 
 func DeepCopy_unversioned_GroupVersion(in GroupVersion, out *GroupVersion, c *conversion.Cloner) error {
 	out.Group = in.Group
+	out.Version = in.Version
+	return nil
+}
+
+func DeepCopy_unversioned_GroupVersionForDiscovery(in GroupVersionForDiscovery, out *GroupVersionForDiscovery, c *conversion.Cloner) error {
+	out.GroupVersion = in.GroupVersion
 	out.Version = in.Version
 	return nil
 }
@@ -102,6 +220,76 @@ func DeepCopy_unversioned_LabelSelectorRequirement(in LabelSelectorRequirement, 
 func DeepCopy_unversioned_ListMeta(in ListMeta, out *ListMeta, c *conversion.Cloner) error {
 	out.SelfLink = in.SelfLink
 	out.ResourceVersion = in.ResourceVersion
+	return nil
+}
+
+func DeepCopy_unversioned_Patch(in Patch, out *Patch, c *conversion.Cloner) error {
+	return nil
+}
+
+func DeepCopy_unversioned_RootPaths(in RootPaths, out *RootPaths, c *conversion.Cloner) error {
+	if in.Paths != nil {
+		in, out := in.Paths, &out.Paths
+		*out = make([]string, len(in))
+		copy(*out, in)
+	} else {
+		out.Paths = nil
+	}
+	return nil
+}
+
+func DeepCopy_unversioned_ServerAddressByClientCIDR(in ServerAddressByClientCIDR, out *ServerAddressByClientCIDR, c *conversion.Cloner) error {
+	out.ClientCIDR = in.ClientCIDR
+	out.ServerAddress = in.ServerAddress
+	return nil
+}
+
+func DeepCopy_unversioned_Status(in Status, out *Status, c *conversion.Cloner) error {
+	if err := DeepCopy_unversioned_TypeMeta(in.TypeMeta, &out.TypeMeta, c); err != nil {
+		return err
+	}
+	if err := DeepCopy_unversioned_ListMeta(in.ListMeta, &out.ListMeta, c); err != nil {
+		return err
+	}
+	out.Status = in.Status
+	out.Message = in.Message
+	out.Reason = in.Reason
+	if in.Details != nil {
+		in, out := in.Details, &out.Details
+		*out = new(StatusDetails)
+		if err := DeepCopy_unversioned_StatusDetails(*in, *out, c); err != nil {
+			return err
+		}
+	} else {
+		out.Details = nil
+	}
+	out.Code = in.Code
+	return nil
+}
+
+func DeepCopy_unversioned_StatusCause(in StatusCause, out *StatusCause, c *conversion.Cloner) error {
+	out.Type = in.Type
+	out.Message = in.Message
+	out.Field = in.Field
+	return nil
+}
+
+func DeepCopy_unversioned_StatusDetails(in StatusDetails, out *StatusDetails, c *conversion.Cloner) error {
+	out.Name = in.Name
+	out.Group = in.Group
+	out.Kind = in.Kind
+	if in.Causes != nil {
+		in, out := in.Causes, &out.Causes
+		*out = make([]StatusCause, len(in))
+		for i := range in {
+			if err := DeepCopy_unversioned_StatusCause(in[i], &(*out)[i], c); err != nil {
+				return err
+			}
+		}
+	} else {
+		out.Causes = nil
+	}
+	out.RetryAfterSeconds = in.RetryAfterSeconds
 	return nil
 }
 
