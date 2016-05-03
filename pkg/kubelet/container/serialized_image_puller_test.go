f091ea5eda15205d76b1f01892b7e64dc47d6850
Andy Goldstein <agoldste@redhat.com>
2016-05-03 13:22:39 -0400

    Handle image digests in node status and image GC
    Start including Docker image digests in the node status and consider image digests during image
    garbage collection.

--- pkg/kubelet/container/serialized_image_puller_test.go
+++ pkg/kubelet/container/serialized_image_puller_test.go
@@ -108,7 +108,7 @@ func TestSerializedPuller(t *testing.T) {
 		fakeRecorder := &record.FakeRecorder{}
 		puller := NewSerializedImagePuller(fakeRecorder, fakeRuntime, backOff)
 
-		fakeRuntime.ImageList = []Image{{"present_image", nil, 0}}
+		fakeRuntime.ImageList = []Image{{"present_image", nil, nil, 0}}
 		fakeRuntime.Err = c.pullerErr
 		fakeRuntime.InspectErr = c.inspectErr
 
