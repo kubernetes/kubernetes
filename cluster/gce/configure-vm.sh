212be162230fa67f0274cb3f8cf3af11b1ae8947
Alex Robinson <arob@google.com>
2016-05-05 22:04:38 +0000

    Don't surround node-tags list with square brackets
    That's not how yaml list parsing works...

--- cluster/gce/configure-vm.sh
+++ cluster/gce/configure-vm.sh
@@ -778,7 +778,7 @@ EOF
 
   if [[ -n "${NODE_INSTANCE_PREFIX:-}" ]]; then
     cat <<EOF >>/etc/gce.conf
-node-tags = [${NODE_INSTANCE_PREFIX}]
+node-tags = ${NODE_INSTANCE_PREFIX}
 EOF
     CLOUD_CONFIG=/etc/gce.conf
   fi
