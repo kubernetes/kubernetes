4d86c7b9e979b09426c47010a67e01decb5badf2
Wojciech Tyczynski <wojtekt@google.com>
2016-05-09 10:57:26 +0200

    Enable using protobufs in e2e tests

--- test/e2e/framework/test_context.go
+++ test/e2e/framework/test_context.go
@@ -28,6 +28,7 @@ import (
 type TestContextType struct {
 	KubeConfig            string
 	KubeContext           string
+	KubeAPIContentType    string
 	KubeVolumeDir         string
 	CertDir               string
 	Host                  string
@@ -90,6 +91,7 @@ func RegisterFlags() {
 
 	flag.StringVar(&TestContext.KubeConfig, clientcmd.RecommendedConfigPathFlag, os.Getenv(clientcmd.RecommendedConfigPathEnvVar), "Path to kubeconfig containing embedded authinfo.")
 	flag.StringVar(&TestContext.KubeContext, clientcmd.FlagContext, "", "kubeconfig context to use/override. If unset, will use value from 'current-context'")
+	flag.StringVar(&TestContext.KubeAPIContentType, "kube-api-content-type", "", "ContentType used to communicate with apiserver")
 	flag.StringVar(&TestContext.KubeVolumeDir, "volume-dir", "/var/lib/kubelet", "Path to the directory containing the kubelet volumes.")
 	flag.StringVar(&TestContext.CertDir, "cert-dir", "", "Path to the directory containing the certs. Default is empty, which doesn't use certs.")
 	flag.StringVar(&TestContext.Host, "host", "", "The host, or apiserver, to connect to")
