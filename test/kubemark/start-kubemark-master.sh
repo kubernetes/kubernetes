4a5ec11ce68706fc9c5afa457d18ec1482045e9e
gmarek <gmarek@google.com>
2016-05-10 18:01:15 +0200

    Pipe variables from kubemark config to master binaries

--- test/kubemark/start-kubemark-master.sh
+++ test/kubemark/start-kubemark-master.sh
@@ -37,22 +37,19 @@ ulimit -n 65536
 
 tar xzf kubernetes-server-linux-amd64.tar.gz
 
-kubernetes/server/bin/kube-scheduler --master=127.0.0.1:8080 --v=2 &> /var/log/kube-scheduler.log &
+kubernetes/server/bin/kube-scheduler --master=127.0.0.1:8080 $(cat scheduler_flags) &> /var/log/kube-scheduler.log &
 
 kubernetes/server/bin/kube-apiserver \
-	--portal-net=10.0.0.1/24 \
 	--address=0.0.0.0 \
 	--etcd-servers=http://127.0.0.1:4001 \
 	--etcd-servers-overrides=/events#${EVENT_STORE_URL} \
-	--v=4 \
 	--tls-cert-file=/srv/kubernetes/server.cert \
 	--tls-private-key-file=/srv/kubernetes/server.key \
 	--client-ca-file=/srv/kubernetes/ca.crt \
 	--token-auth-file=/srv/kubernetes/known_tokens.csv \
 	--secure-port=443 \
 	--basic-auth-file=/srv/kubernetes/basic_auth.csv \
-	--service-cluster-ip-range=10.0.0.0/16 \
-	--delete-collection-workers=16 &> /var/log/kube-apiserver.log &
+	$(cat apiserver_flags) &> /var/log/kube-apiserver.log &
 
 # kube-contoller-manager now needs running kube-api server to actually start
 until [ "$(curl 127.0.0.1:8080/healthz 2> /dev/null)" == "ok" ]; do
@@ -62,6 +59,4 @@ kubernetes/server/bin/kube-controller-manager \
   --master=127.0.0.1:8080 \
   --service-account-private-key-file=/srv/kubernetes/server.key \
   --root-ca-file=/srv/kubernetes/ca.crt \
-  --v=2 \
-  --allocate-node-cidrs=true \
-  --cluster-cidr=10.180.0.0/13 &> /var/log/kube-controller-manager.log &
+  $(cat controllers_flags) &> /var/log/kube-controller-manager.log &
