b5beafdc200bd0a9c68bafab7178ae76e0099fda
saadali <saadali@google.com>
2016-05-11 16:59:55 -0700

    Update vendor dir and Godeps.json with new Godep

--- vendor/github.com/rackspace/gophercloud/openstack/compute/v2/extensions/quotasets/doc.go
+++
@@ -1,3 +0,0 @@
-// Package quotasets provides information and interaction with QuotaSet
-// extension for the OpenStack Compute service.
-package quotasets
--- vendor/github.com/rackspace/gophercloud/openstack/compute/v2/extensions/tenantnetworks/doc.go
+++
@@ -1,2 +0,0 @@
-// Package tenantnetworks provides the ability for tenants to see information about the networks they have access to
-package tenantnetworks
