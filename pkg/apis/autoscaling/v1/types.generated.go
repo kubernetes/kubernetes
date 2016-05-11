2bc46d5085d70a01ecd9b1c5e5365e0110f0dedc
Matt Liggett <mml@google.com>
2016-05-11 14:01:23 -0700

    It's 2016, yo.

--- pkg/apis/autoscaling/v1/types.generated.go
+++ pkg/apis/autoscaling/v1/types.generated.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2015 The Kubernetes Authors All rights reserved.
+Copyright 2016 The Kubernetes Authors All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
