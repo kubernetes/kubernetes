87693ba54945b227975c2650df14ca48eed85cb1
PingWang <present.wp@icloud.com>
2016-05-20 08:49:53 +0800

    Add the length detection of the "predicateFuncs" in generic_scheduler.go
    The PR add the length detection of the "predicateFuncs" for "findNodesThatFit" function of generic_scheduler.go. 
    In “findNodesThatFit” function, if the length of the "predicateFuncs" parameter is 0, it can set filtered equals nodes.Items, and needn't to traverse the nodes.Items.

--- plugin/pkg/scheduler/generic_scheduler.go
+++ plugin/pkg/scheduler/generic_scheduler.go
@@ -137,30 +137,34 @@ func (g *genericScheduler) selectHost(priorityList schedulerapi.HostPriorityList
 // Filters the nodes to find the ones that fit based on the given predicate functions
 // Each node is passed through the predicate functions to determine if it is a fit
 func findNodesThatFit(pod *api.Pod, nodeNameToInfo map[string]*schedulercache.NodeInfo, predicateFuncs map[string]algorithm.FitPredicate, nodes api.NodeList, extenders []algorithm.SchedulerExtender) (api.NodeList, FailedPredicateMap, error) {
-	predicateResultLock := sync.Mutex{}
 	filtered := []api.Node{}
 	failedPredicateMap := FailedPredicateMap{}
-	errs := []error{}
 
-	checkNode := func(i int) {
-		nodeName := nodes.Items[i].Name
-		fits, failedPredicate, err := podFitsOnNode(pod, nodeNameToInfo[nodeName], predicateFuncs)
-
-		predicateResultLock.Lock()
-		defer predicateResultLock.Unlock()
-		if err != nil {
-			errs = append(errs, err)
-			return
+	if len(predicateFuncs) == 0 {
+		filtered = nodes.Items
+	} else {
+		predicateResultLock := sync.Mutex{}
+		errs := []error{}
+		checkNode := func(i int) {
+			nodeName := nodes.Items[i].Name
+			fits, failedPredicate, err := podFitsOnNode(pod, nodeNameToInfo[nodeName], predicateFuncs)
+	
+			predicateResultLock.Lock()
+			defer predicateResultLock.Unlock()
+			if err != nil {
+				errs = append(errs, err)
+				return
+			}
+			if fits {
+				filtered = append(filtered, nodes.Items[i])
+			} else {
+				failedPredicateMap[nodeName] = failedPredicate
+			}
 		}
-		if fits {
-			filtered = append(filtered, nodes.Items[i])
-		} else {
-			failedPredicateMap[nodeName] = failedPredicate
+		workqueue.Parallelize(16, len(nodes.Items), checkNode)
+		if len(errs) > 0 {
+			return api.NodeList{}, FailedPredicateMap{}, errors.NewAggregate(errs)
 		}
-	}
-	workqueue.Parallelize(16, len(nodes.Items), checkNode)
-	if len(errs) > 0 {
-		return api.NodeList{}, FailedPredicateMap{}, errors.NewAggregate(errs)
 	}
 
 	if len(filtered) > 0 && len(extenders) != 0 {
