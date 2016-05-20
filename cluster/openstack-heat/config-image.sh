7eb23470b50c2a09566155a07656422fb8495988
Elson O Rodriguez <elson.o.rodriguez@intel.com>
2016-05-20 12:33:13 -0700

    Updating CentOS image, adding heat back to the required cli tools.

--- cluster/openstack-heat/config-image.sh
+++ cluster/openstack-heat/config-image.sh
@@ -20,7 +20,7 @@
 OPENSTACK_IMAGE_NAME=${OPENSTACK_IMAGE_NAME:-CentOS7}
 
 # Downloaded image name for Openstack project
-IMAGE_FILE=${IMAGE_FILE:-CentOS-7-x86_64-GenericCloud-1510.qcow2}
+IMAGE_FILE=${IMAGE_FILE:-CentOS-7-x86_64-GenericCloud-1604.qcow2}
 
 # Absolute path where image file is stored.
 IMAGE_PATH=${IMAGE_PATH:-~/Downloads/openstack}
