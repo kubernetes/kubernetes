8c30e8fbf14686a62a9b2db73cd07784ef72e2bc
derekwaynecarr <decarr@redhat.com>
2016-05-24 10:05:00 -0400

    Fix vagrant issues with salt bootstrap

--- cluster/vagrant/provision-master.sh
+++ cluster/vagrant/provision-master.sh
@@ -118,8 +118,7 @@ if ! which /usr/libexec/cockpit-ws &>/dev/null; then
 
   pushd /etc/yum.repos.d
     curl -OL https://copr.fedorainfracloud.org/coprs/g/cockpit/cockpit-preview/repo/fedora-23/msuchy-cockpit-preview-fedora-23.repo
-    dnf install -y cockpit cockpit-kubernetes socat ethtool
-    dnf update -y docker
+    dnf install -y cockpit cockpit-kubernetes docker socat ethtool
   popd
 
   systemctl enable cockpit.socket
