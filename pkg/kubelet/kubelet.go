5262d09dc4156cca043765b213435cb49f55e179
yaxinlx <liuxu@asiainfo.com>
2016-05-31 18:13:46 +0800

    avoid kublet crashing and memory leaking when killing pods

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -2243,7 +2243,12 @@ func (kl *Kubelet) HandlePodCleanups() error {
 func (kl *Kubelet) podKiller() {
 	killing := sets.NewString()
 	resultCh := make(chan types.UID)
-	defer close(resultCh)
+	defer func() {
+		close(resultCh)
+
+		// avoid possible memory leaking caused by blocking pod-killing goroutines.
+		for range resultCh {}
+	}()
 	for {
 		select {
 		case podPair, ok := <-kl.podKillingCh:
@@ -2258,6 +2263,10 @@ func (kl *Kubelet) podKiller() {
 			}
 			killing.Insert(string(runningPod.ID))
 			go func(apiPod *api.Pod, runningPod *kubecontainer.Pod, ch chan types.UID) {
+				defer func() {
+					// avoid crashing caused by writing into closed ch
+					recover()
+				}()
 				defer func() {
 					ch <- runningPod.ID
 				}()
