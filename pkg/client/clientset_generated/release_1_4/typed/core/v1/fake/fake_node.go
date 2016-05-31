6d4a7ab525a99e2aea862fbcdd93a03ddf344081
Chao Xu <xuchao@google.com>
2016-05-31 13:08:04 -0700

    stop update release_1_3 clientset; create release_1_4

---
+++ pkg/client/clientset_generated/release_1_4/typed/core/v1/fake/fake_node.go
@@ -0,0 +1,118 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package fake
+
+import (
+	api "k8s.io/kubernetes/pkg/api"
+	unversioned "k8s.io/kubernetes/pkg/api/unversioned"
+	v1 "k8s.io/kubernetes/pkg/api/v1"
+	core "k8s.io/kubernetes/pkg/client/testing/core"
+	labels "k8s.io/kubernetes/pkg/labels"
+	watch "k8s.io/kubernetes/pkg/watch"
+)
+
+// FakeNodes implements NodeInterface
+type FakeNodes struct {
+	Fake *FakeCore
+}
+
+var nodesResource = unversioned.GroupVersionResource{Group: "", Version: "v1", Resource: "nodes"}
+
+func (c *FakeNodes) Create(node *v1.Node) (result *v1.Node, err error) {
+	obj, err := c.Fake.
+		Invokes(core.NewRootCreateAction(nodesResource, node), &v1.Node{})
+	if obj == nil {
+		return nil, err
+	}
+	return obj.(*v1.Node), err
+}
+
+func (c *FakeNodes) Update(node *v1.Node) (result *v1.Node, err error) {
+	obj, err := c.Fake.
+		Invokes(core.NewRootUpdateAction(nodesResource, node), &v1.Node{})
+	if obj == nil {
+		return nil, err
+	}
+	return obj.(*v1.Node), err
+}
+
+func (c *FakeNodes) UpdateStatus(node *v1.Node) (*v1.Node, error) {
+	obj, err := c.Fake.
+		Invokes(core.NewRootUpdateSubresourceAction(nodesResource, "status", node), &v1.Node{})
+	if obj == nil {
+		return nil, err
+	}
+	return obj.(*v1.Node), err
+}
+
+func (c *FakeNodes) Delete(name string, options *api.DeleteOptions) error {
+	_, err := c.Fake.
+		Invokes(core.NewRootDeleteAction(nodesResource, name), &v1.Node{})
+	return err
+}
+
+func (c *FakeNodes) DeleteCollection(options *api.DeleteOptions, listOptions api.ListOptions) error {
+	action := core.NewRootDeleteCollectionAction(nodesResource, listOptions)
+
+	_, err := c.Fake.Invokes(action, &v1.NodeList{})
+	return err
+}
+
+func (c *FakeNodes) Get(name string) (result *v1.Node, err error) {
+	obj, err := c.Fake.
+		Invokes(core.NewRootGetAction(nodesResource, name), &v1.Node{})
+	if obj == nil {
+		return nil, err
+	}
+	return obj.(*v1.Node), err
+}
+
+func (c *FakeNodes) List(opts api.ListOptions) (result *v1.NodeList, err error) {
+	obj, err := c.Fake.
+		Invokes(core.NewRootListAction(nodesResource, opts), &v1.NodeList{})
+	if obj == nil {
+		return nil, err
+	}
+
+	label := opts.LabelSelector
+	if label == nil {
+		label = labels.Everything()
+	}
+	list := &v1.NodeList{}
+	for _, item := range obj.(*v1.NodeList).Items {
+		if label.Matches(labels.Set(item.Labels)) {
+			list.Items = append(list.Items, item)
+		}
+	}
+	return list, err
+}
+
+// Watch returns a watch.Interface that watches the requested nodes.
+func (c *FakeNodes) Watch(opts api.ListOptions) (watch.Interface, error) {
+	return c.Fake.
+		InvokesWatch(core.NewRootWatchAction(nodesResource, opts))
+}
+
+// Patch applies the patch and returns the patched node.
+func (c *FakeNodes) Patch(name string, pt api.PatchType, data []byte) (result *v1.Node, err error) {
+	obj, err := c.Fake.
+		Invokes(core.NewRootPatchAction(nodesResource, name, data), &v1.Node{})
+	if obj == nil {
+		return nil, err
+	}
+	return obj.(*v1.Node), err
+}
---
+++ pkg/client/clientset_generated/release_1_4/typed/core/v1/generated_expansion.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+type ComponentStatusExpansion interface{}
+
+type ConfigMapExpansion interface{}
+
+type EndpointsExpansion interface{}
+
+type EventExpansion interface{}
+
+type LimitRangeExpansion interface{}
+
+type NamespaceExpansion interface{}
+
+type NodeExpansion interface{}
+
+type PersistentVolumeExpansion interface{}
+
+type PodExpansion interface{}
+
+type PodTemplateExpansion interface{}
+
+type ReplicationControllerExpansion interface{}
+
+type ResourceQuotaExpansion interface{}
+
+type SecretExpansion interface{}
+
+type ServiceExpansion interface{}
+
+type ServiceAccountExpansion interface{}
