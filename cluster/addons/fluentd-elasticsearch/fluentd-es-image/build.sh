f3f9b1e4921ce0336606f0f299d60e6c477a5660
Manuel de Brito Fontes <aledbf@gmail.com>
2016-06-01 19:25:51 -0400

    Reduce size of fluentd-elasticsearch image using ubuntu-slim

---
+++ cluster/addons/fluentd-elasticsearch/fluentd-es-image/build.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Copyright 2016 The Kubernetes Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Install prerequisites.
+apt-get update
+
+apt-get install -y -q --no-install-recommends \
+  curl ca-certificates make g++ sudo bash
+
+# Install Fluentd.
+/usr/bin/curl -sSL https://td-toolbelt.herokuapp.com/sh/install-ubuntu-trusty-td-agent2.sh | bash
+
+# Change the default user and group to root.
+# Needed to allow access to /var/log/docker/... files.
+sed -i -e "s/USER=td-agent/USER=root/" -e "s/GROUP=td-agent/GROUP=root/" /etc/init.d/td-agent
+
+# Install the Elasticsearch Fluentd plug-in.
+td-agent-gem install fluent-plugin-kubernetes_metadata_filter fluent-plugin-elasticsearch
+
+apt-get remove -y make g++
+apt-get autoremove -y 
+apt-get clean -y 
+
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
