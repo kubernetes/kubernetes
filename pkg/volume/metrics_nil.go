6415c2d288d70f434a60e292342cfc4817667336
Paul Morie <pmorie@redhat.com>
2016-06-04 14:14:00 -0400

    Wrap more comments in pkg/volume

--- pkg/volume/metrics_nil.go
+++ pkg/volume/metrics_nil.go
@@ -21,11 +21,12 @@ import "errors"
 var _ MetricsProvider = &MetricsNil{}
 
 // MetricsNil represents a MetricsProvider that does not support returning
-// Metrics.  It serves as a placeholder for Volumes that do not yet support metrics.
+// Metrics.  It serves as a placeholder for Volumes that do not yet support
+// metrics.
 type MetricsNil struct{}
 
-// See MetricsProvider.GetMetrics
 // GetMetrics returns an empty Metrics and an error.
+// See MetricsProvider.GetMetrics
 func (*MetricsNil) GetMetrics() (*Metrics, error) {
 	return &Metrics{}, errors.New("metrics are not supported for MetricsNil Volumes")
 }
