83fd56fbfd3b80ad4a8ceb529caf11ee26602e9f
Josh Ellithorpe <josh@apcera.com>
2016-06-04 15:52:10 -0700

    Remove SecurityContextDeny from aws configurations

--- cluster/aws/config-default.sh
+++ cluster/aws/config-default.sh
@@ -134,7 +134,7 @@ fi
 
 # Admission Controllers to invoke prior to persisting objects in cluster
 # If we included ResourceQuota, we should keep it at the end of the list to prevent incremeting quota usage prematurely.
-ADMISSION_CONTROL=NamespaceLifecycle,LimitRanger,SecurityContextDeny,ServiceAccount,PersistentVolumeLabel,ResourceQuota
+ADMISSION_CONTROL=NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,ResourceQuota
 
 # Optional: Enable/disable public IP assignment for minions.
 # Important Note: disable only if you have setup a NAT instance for internet access and configured appropriate routes!
