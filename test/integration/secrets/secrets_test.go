f3de21bdf0c72a4b878b7fa8279c5bba1342b406
Mike Danese <mikedanese@google.com>
2016-06-08 11:44:10 -0700

    move integration tests into individual pacakges
    Signed-off-by: Mike Danese <mikedanese@google.com>

--- test/integration/secrets/secrets_test.go
+++ test/integration/secrets/secrets_test.go
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package integration
+package secrets
 
 // This file tests use of the secrets API resource.
 
@@ -27,6 +27,7 @@ import (
 	"k8s.io/kubernetes/pkg/api/testapi"
 	"k8s.io/kubernetes/pkg/client/restclient"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
+	"k8s.io/kubernetes/test/integration"
 	"k8s.io/kubernetes/test/integration/framework"
 )
 
@@ -105,14 +106,14 @@ func DoTestSecrets(t *testing.T, client *client.Client, ns *api.Namespace) {
 	if _, err := client.Pods(ns.Name).Create(pod); err != nil {
 		t.Errorf("Failed to create pod: %v", err)
 	}
-	defer deletePodOrErrorf(t, client, ns.Name, pod.Name)
+	defer integration.DeletePodOrErrorf(t, client, ns.Name, pod.Name)
 
 	// Create a pod that consumes non-existent secret.
 	pod.ObjectMeta.Name = "uses-non-existent-secret"
 	if _, err := client.Pods(ns.Name).Create(pod); err != nil {
 		t.Errorf("Failed to create pod: %v", err)
 	}
-	defer deletePodOrErrorf(t, client, ns.Name, pod.Name)
+	defer integration.DeletePodOrErrorf(t, client, ns.Name, pod.Name)
 	// This pod may fail to run, but we don't currently prevent this, and this
 	// test can't check whether the kubelet actually pulls the secret.
 
