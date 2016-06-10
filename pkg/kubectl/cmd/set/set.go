b1354d2793032b79b11244541c42529de6c84a60
Jacob Tanenbaum <jtanenba@redhat.com>
2016-06-10 17:25:56 -0400

    Add 'kubectl set limit'
    Add a way to set resource limits/requests on running pods
    
    Ref: https://github.com/kubernetes/kubernetes/issues/21648
    
    I squashed the commits to make rebasing easier
    Change log:
    
    - fixed a typo that caused the command to be run with kubectl set set instead of the correct kubectl set limit
    
    - added a ResourcesWithPodTemplates to pkg/kubectl/cmd/util/factory.go
         instead of hardcoding these resources move there description all in one place
    
    - Fixing some of the flow control in kubectl set limit
    
    - update the help info
    
    - changed the name of ResourcesWithPodTemplates to ResourcesWithPodSpecs to more accuratly describe what it is doing
        and changed the variable names to lower case to conform to go's variable naming convention
    
    - changing the name of the command from 'set limit' to 'set resources'
    
    - Adding the new file pkg/kubectl/cmd/set/set_resources.go
    
    - changes to the test cases to reflect the change from 'kubectl set limit' to 'kubectl set resources'
    
    - comment removed
    
    - adding the man page to the git repository attempting to fix Jenkins tests
    
    - adding the user guide
    
    - fixed a few typos
    
    - typo in hack/cmd-test.sh
    
    - implamenting suggestions for command help text
    
    - adding the dry-run flag
    
    - removing the "remove" option in favor of zeroing out request/limits in order to remove them
    
    - changed limits/requests to requests/limit
    
    - changing ResourcesWithPodSpec
    
    - updated generated docs and removed whitespace
    
    - change priint on success message from "resource limits/requests updated" to "resource requirements updated"
    
    - minor rebasing issues - 'hack/test-cmd.sh' now passes
    
    - cmdutil.PrintSuccess added another argument
    
    - fixing mungedocs failure
    
    - removed whitespace from hack/make-rules/test-cmd.sh and an erroneous entry from pkg/cloudprovider/providers/openstack/MAINTAINERS.md
    
    - fixed typo in Short: field of the cobra command

--- pkg/kubectl/cmd/set/set.go
+++ pkg/kubectl/cmd/set/set.go
@@ -32,8 +32,7 @@ var (
 	set_example = dedent.Dedent(``)
 )
 
-func NewCmdSet(f cmdutil.Factory, out io.Writer) *cobra.Command {
-
+func NewCmdSet(f cmdutil.Factory, out io.Writer, err io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "set SUBCOMMAND",
 		Short:   "Set specific features on objects",
@@ -46,6 +45,7 @@ func NewCmdSet(f cmdutil.Factory, out io.Writer) *cobra.Command {
 
 	// add subcommands
 	cmd.AddCommand(NewCmdImage(f, out))
+	cmd.AddCommand(NewCmdResources(f, out, err))
 
 	return cmd
 }
