cba846d4a0ded2063e389c99c737e7b4d1875a7e
Jacob Tanenbaum <jtanenba@redhat.com>
2016-06-10 17:25:56 -0400

    UPSTREAM: 27206: Add kubectl set resources
    add kubectl set resources
    
    :100644 100644 aa4e895... 5701eca... M	pkg/kubectl/cmd/cmd.go
    :100644 100644 2dd208f... 2494927... M	pkg/kubectl/cmd/set/set.go
    :000000 100644 0000000... ef62da7... A	pkg/kubectl/cmd/set/set_resources.go
    :100644 100644 e6ed544... b9fd9bd... M	pkg/kubectl/cmd/util/factory.go

--- pkg/kubectl/cmd/cmd.go
+++ pkg/kubectl/cmd/cmd.go
@@ -261,7 +261,7 @@ Find more information at https://github.com/kubernetes/kubernetes.`,
 				NewCmdCreate(f, out),
 				NewCmdExposeService(f, out),
 				NewCmdRun(f, in, out, err),
-				set.NewCmdSet(f, out),
+				set.NewCmdSet(f, out, err),
 			},
 		},
 		{
