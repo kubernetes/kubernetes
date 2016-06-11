da90405844fc0895265088aef8e3e5cd2b1b3a4c
Dawn Chen <dawnchen@google.com>
2016-06-10 17:56:03 -0700

    Revert "e2e-runner.sh: don't overwrite readonly var"

--- hack/jenkins/e2e-runner.sh
+++ hack/jenkins/e2e-runner.sh
@@ -34,12 +34,13 @@ function fetch_output_tars() {
 }
 
 function fetch_server_version_tars() {
-    local -r server_version="$(gcloud ${CMD_GROUP:-} container get-server-config --project=${PROJECT} --zone=${ZONE}  --format='value(defaultClusterVersion)')"
+    local -r build_version="$(gcloud ${CMD_GROUP:-} container get-server-config --project=${PROJECT} --zone=${ZONE}  --format='value(defaultClusterVersion)')"
     # Use latest build of the server version's branch for test files.
-    fetch_published_version_tars "ci/latest-${server_version:0:3}"
+    fetch_published_version_tars "ci/latest-${build_version:0:3}"
     # Unset cluster api version; we want to use server default for the cluster
     # version.
     unset CLUSTER_API_VERSION
+    unpack_binaries
 }
 
 # Use a published version like "ci/latest" (default), "release/latest",
