e73398b48f035ed373d02cc455d6b3043d34e3d6
Daniel Smith <dbsmith@google.com>
2016-06-16 14:27:12 -0700

    remove unused prefix code, fix comment

--- test/integration/framework/master_utils.go
+++ test/integration/framework/master_utils.go
@@ -155,9 +155,9 @@ func startMasterOrDie(masterConfig *master.Config) (*master.Master, *httptest.Se
 func NewMasterConfig() *master.Config {
 	config := storagebackend.Config{
 		ServerList: []string{"http://127.0.0.1:4001"},
-		// TODO: this is a quick hack to work around #27179. It
-		// conveniently exercises the prefix code, so maybe it's worth
-		// leaving in.
+		// This causes the integration tests to exercise the etcd
+		// prefix code, so please don't change without ensuring
+		// sufficient coverage in other ways.
 		Prefix: uuid.New(),
 	}
 
