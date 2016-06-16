2b47458b862b5ecdf1f2263aaff7bd488b85c5fc
Jing Liu <jliu23@ebay.com>
2016-06-16 12:19:34 -0700

    improve iptables-restore implementation #27559
    - improve restoreInternal implementation in iptables
    - add SetStdin and SetStdout functions to Cmd interface
    - modify kubelet/prober and some tests in order to work with Cmd interface

--- pkg/util/exec/fake_exec.go
+++ pkg/util/exec/fake_exec.go
@@ -18,6 +18,7 @@ package exec
 
 import (
 	"fmt"
+	"io"
 )
 
 // A simple scripted Interface type.
@@ -49,6 +50,8 @@ type FakeCmd struct {
 	CombinedOutputCalls  int
 	CombinedOutputLog    [][]string
 	Dirs                 []string
+	Stdin                io.Reader
+	Stdout               io.Writer
 }
 
 func InitFakeCmd(fake *FakeCmd, cmd string, args ...string) Cmd {
@@ -60,6 +63,14 @@ type FakeCombinedOutputAction func() ([]byte, error)
 
 func (fake *FakeCmd) SetDir(dir string) {
 	fake.Dirs = append(fake.Dirs, dir)
+}
+
+func (fake *FakeCmd) SetStdin(in io.Reader) {
+	fake.Stdin = in
+}
+
+func (fake *FakeCmd) SetStdout(out io.Writer) {
+	fake.Stdout = out
 }
 
 func (fake *FakeCmd) CombinedOutput() ([]byte, error) {
