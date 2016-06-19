85c346f0632c2e24690572410e23b279d03c388f
Harry Zhang <harryzhang@zju.edu.cn>
2016-06-19 10:59:13 -0400

    Omit invalid affinity error in admission

--- plugin/pkg/admission/antiaffinity/admission.go
+++ plugin/pkg/admission/antiaffinity/admission.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/golang/glog"
 	"k8s.io/kubernetes/pkg/admission"
 	"k8s.io/kubernetes/pkg/api"
 	apierrors "k8s.io/kubernetes/pkg/api/errors"
@@ -59,7 +60,8 @@ func (p *plugin) Admit(attributes admission.Attributes) (err error) {
 	}
 	affinity, err := api.GetAffinityFromPodAnnotations(pod.Annotations)
 	if err != nil {
-		return err
+		glog.V(5).Infof("Invalid Affinity detected, but we will leave handling of this to validation phase")
+		return nil
 	}
 	if affinity.PodAntiAffinity != nil {
 		var podAntiAffinityTerms []api.PodAffinityTerm
