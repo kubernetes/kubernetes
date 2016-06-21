487bba9b1da37bbb20599c5e31c7886f9d77edb9
Zach Loafman <zml@google.com>
2016-06-20 17:37:49 -0700

    GCE provider: Limit Filter calls to regexps rather than insane blobs
    Filters can't exceed 4k, and GET requests against the GCE API are also
    limited, so these break down in different ways at different cluster
    counts. Fix it by introducing an advisory node-instance-prefix
    configuration in the GCE provider that can hint the
    EnsureLoadBalancer/UpdateLoadBalancer code (and the firewall
    creation/update code). If it's not there, or wrong (a hostname that's
    registered violates it), just ignore it and grab the whole project.

--- cluster/gce/trusty/configure-helper.sh
+++ cluster/gce/trusty/configure-helper.sh
@@ -349,6 +349,7 @@ EOF
     use_cloud_config="true"
     cat <<EOF >>/etc/gce.conf
 node-tags = ${NODE_INSTANCE_PREFIX}
+node-instance-prefix = ${NODE_INSTANCE_PREFIX}
 EOF
   fi
   if [ -n "${MULTIZONE:-}" ]; then
