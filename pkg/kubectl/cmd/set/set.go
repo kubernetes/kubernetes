a90d1136d1be0a8ac8a9588ff8839059f893a0ec
Jacob Tanenbaum <jtanenba@redhat.com>
2016-06-22 17:02:35 -0400

    changing the name of the command from 'set limit' to 'set resources'

--- pkg/kubectl/cmd/set/set.go
+++ pkg/kubectl/cmd/set/set.go
@@ -44,7 +44,7 @@ func NewCmdSet(f *cmdutil.Factory, out io.Writer, err io.Writer) *cobra.Command 
 
 	// add subcommands
 	cmd.AddCommand(NewCmdImage(f, out))
-	cmd.AddCommand(NewCmdLimit(f, out, err))
+	cmd.AddCommand(NewCmdResources(f, out, err))
 
 	return cmd
 }
