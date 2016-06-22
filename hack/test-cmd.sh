def774f8d8e1afa555f13edda13011d487948dd9
Chao Xu <xuchao@google.com>
2016-06-22 14:57:26 -0700

    fix test-cmd multi-resource test

--- hack/test-cmd.sh
+++ hack/test-cmd.sh
@@ -2031,7 +2031,7 @@ __EOF__
     fi
     kubectl describe -f "${file}" "${kube_flags[@]}"
     # Command
-    kubectl replace -f $replace_file --force "${kube_flags[@]}"
+    kubectl replace -f $replace_file --force --cascade "${kube_flags[@]}"
     # Post-condition: mock service (and mock2) and mock rc (and mock2) are replaced
     if [ "$has_svc" = true ]; then
       kube::test::get_object_assert 'services mock' "{{${labels_field}.status}}" 'replaced'
