0acca44dc165395587becce1a656176c896eb2ca
Dr. Stefan Schimanski <sttts@redhat.com>
2016-06-24 13:05:59 +0200

    Error out on non-existing container in kubectl attach

--- pkg/kubectl/cmd/run.go
+++ pkg/kubectl/cmd/run.go
@@ -332,8 +332,12 @@ func handleAttachPod(f *cmdutil.Factory, c *client.Client, pod *api.Pod, opts *A
 	if err != nil {
 		return err
 	}
+	ctrName, err := opts.GetContainerName(pod)
+	if err != nil {
+		return err
+	}
 	if status == api.PodSucceeded || status == api.PodFailed {
-		req, err := f.LogsForObject(pod, &api.PodLogOptions{Container: opts.GetContainerName(pod)})
+		req, err := f.LogsForObject(pod, &api.PodLogOptions{Container: ctrName})
 		if err != nil {
 			return err
 		}
@@ -350,7 +354,7 @@ func handleAttachPod(f *cmdutil.Factory, c *client.Client, pod *api.Pod, opts *A
 	opts.Namespace = pod.Namespace
 	if err := opts.Run(); err != nil {
 		fmt.Fprintf(opts.Out, "Error attaching, falling back to logs: %v\n", err)
-		req, err := f.LogsForObject(pod, &api.PodLogOptions{Container: opts.GetContainerName(pod)})
+		req, err := f.LogsForObject(pod, &api.PodLogOptions{Container: ctrName})
 		if err != nil {
 			return err
 		}
