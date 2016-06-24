cb3a620faf9985469732052e50b8605b7a48a309
Andy Goldstein <agoldste@redhat.com>
2016-06-24 11:43:05 -0400

    UPSTREAM: 28025: Add EndpointReconciler to master Config
    :100644 100644 87ceb7b... 88e5e9c... M	pkg/master/master.go
    :100644 100644 88d42f7... 90b9e39... M	pkg/master/master_test.go
    :100644 100644 e345602... 90f058b... M	test/integration/openshift_test.go

---
+++ test/integration/openshift_test.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2015 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package integration
+
+import (
+	"testing"
+
+	"k8s.io/kubernetes/pkg/genericapiserver"
+	"k8s.io/kubernetes/pkg/master"
+)
+
+// This test references methods that OpenShift uses to customize the master on startup, that
+// are not referenced directly by a master.
+func TestMasterExportsSymbols(t *testing.T) {
+	_ = &master.Config{
+		Config: &genericapiserver.Config{
+			EnableUISupport:      false,
+			EnableSwaggerSupport: false,
+			RestfulContainer:     nil,
+		},
+		EnableCoreControllers: false,
+	}
+	m := &master.Master{
+		GenericAPIServer: &genericapiserver.GenericAPIServer{},
+	}
+	_ = (m).NewBootstrapController(master.EndpointReconcilerConfig{})
+}
