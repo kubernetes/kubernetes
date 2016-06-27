37a0c0ab5d1bc515d097011d08e582aa25f8785a
k8s-merge-robot <k8s.production.user@gmail.com>
2016-06-27 16:02:40 -0700

    Merge pull request #27944 from wongma7/pvc-selector-hack
    Automatic merge from submit-queue
    
    Fix pvc label selector validation error
    
    This is for https://github.com/kubernetes/kubernetes/issues/26866. Other selectors are e.g. v1beta1.x , the pvc one is unversioned.x
    (cherry picked from commit 11e41581b42d4bfa771df19eebdf22f5032e81e8)

--- pkg/api/validation/schema.go
+++ pkg/api/validation/schema.go
@@ -294,7 +294,7 @@ func (s *SwaggerSchema) isGenericArray(p swagger.ModelProperty) bool {
 }
 
 // This matches type name in the swagger spec, such as "v1.Binding".
-var versionRegexp = regexp.MustCompile(`^v.+\..*`)
+var versionRegexp = regexp.MustCompile(`^(v.+|unversioned)\..*`)
 
 func (s *SwaggerSchema) validateField(value interface{}, fieldName, fieldType string, fieldDetails *swagger.ModelProperty) []error {
 	// TODO: caesarxuchao: because we have multiple group/versions and objects
