8c42c08363864cb4a654e9baa43d63637b9c7ac9
Tim Hockin <thockin@google.com>
2016-06-28 22:48:59 -0700

    move forked golang code to a common dir with LICENSE

--- pkg/conversion/deep_equal.go
+++ pkg/conversion/deep_equal.go
@@ -17,7 +17,7 @@ limitations under the License.
 package conversion
 
 import (
-	"k8s.io/kubernetes/third_party/forked/reflect"
+	"k8s.io/kubernetes/third_party/forked/golang/reflect"
 )
 
 // The code for this type must be located in third_party, since it forks from
--- pkg/util/proxy/dial.go
+++ pkg/util/proxy/dial.go
@@ -26,7 +26,7 @@ import (
 	"github.com/golang/glog"
 
 	utilnet "k8s.io/kubernetes/pkg/util/net"
-	"k8s.io/kubernetes/third_party/forked/netutil"
+	"k8s.io/kubernetes/third_party/forked/golang/netutil"
 )
 
 func DialURL(url *url.URL, transport http.RoundTripper) (net.Conn, error) {
