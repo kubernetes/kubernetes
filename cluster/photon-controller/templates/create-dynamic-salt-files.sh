761f4eca379c26b0e3a9921f4cc7951c514b1c99
k8s-merge-robot <k8s.production.user@gmail.com>
2016-06-29 14:09:46 -0700

    Merge pull request #28132 from madhusudancs/fed-kubedns-flags-nodebootstrap
    Automatic merge from submit-queue
    
    Substitute federation_domain_map parameter with its value in node bootstrap scripts.
    
    This PR also removes the substitution code we added to the build scripts.
    
    **Release Note**
    
    ```release-note
    If you use one of the kube-dns replication controller manifest in `cluster/saltbase/salt/kube-dns`, i.e. `cluster/saltbase/salt/kube-dns/{skydns-rc.yaml.base,skydns-rc.yaml.in}`, either substitute one of `__PILLAR__FEDERATIONS__DOMAIN__MAP__` or `{{ pillar['federations_domain_map'] }}` with the corresponding federation name to domain name value or remove them if you do not support cluster federation at this time. If you plan to substitute the parameter with its value, here is an example for `{{ pillar['federations_domain_map'] }`
    pillar['federations_domain_map'] = "- --federations=myfederation=federation.test"
    where `myfederation` is the name of the federation and `federation.test` is the domain name registered for the federation.
    ```
    
    cc @erictune  @kubernetes/sig-cluster-federation @MikeSpreitzer @luxas
    
    [![Analytics](https://kubernetes-site.appspot.com/UA-36037335-10/GitHub/.github/PULL_REQUEST_TEMPLATE.md?pixel)]()
    
    (cherry picked from commit 3a6494e9aeab8878f38f3367708fe80d7e6cf773)

--- cluster/photon-controller/templates/create-dynamic-salt-files.sh
+++ cluster/photon-controller/templates/create-dynamic-salt-files.sh
@@ -120,6 +120,7 @@ enable_cluster_dns: "${ENABLE_CLUSTER_DNS:-false}"
 dns_replicas: ${DNS_REPLICAS:-1}
 dns_server: $DNS_SERVER_IP
 dns_domain: $DNS_DOMAIN
+federations_domain_map: ''
 e2e_storage_test_environment: "${E2E_STORAGE_TEST_ENVIRONMENT:-false}"
 cluster_cidr: "$NODE_IP_RANGES"
 allocate_node_cidrs: "${ALLOCATE_NODE_CIDRS:-true}"
