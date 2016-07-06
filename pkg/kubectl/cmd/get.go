7e5582dba33158abb00205bbff211566673eecf6
Davanum Srinivas <davanum@gmail.com>
2016-07-06 08:03:09 -0400

    Add custom-columns and custom-template-file to --help text
    Fixes #28314

--- pkg/kubectl/cmd/get.go
+++ pkg/kubectl/cmd/get.go
@@ -87,7 +87,7 @@ func NewCmdGet(f *cmdutil.Factory, out io.Writer) *cobra.Command {
 	}
 
 	cmd := &cobra.Command{
-		Use:     "get [(-o|--output=)json|yaml|wide|go-template=...|go-template-file=...|jsonpath=...|jsonpath-file=...] (TYPE [NAME | -l label] | TYPE/NAME ...) [flags]",
+		Use:     "get [(-o|--output=)json|yaml|wide|custom-columns=...|custom-columns-file=...|go-template=...|go-template-file=...|jsonpath=...|jsonpath-file=...] (TYPE [NAME | -l label] | TYPE/NAME ...) [flags]",
 		Short:   "Display one or many resources",
 		Long:    get_long,
 		Example: get_example,
