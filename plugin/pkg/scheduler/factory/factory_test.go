26d00d7d2de67aa0ef61790b5f1c416411a397a3
Wojciech Tyczynski <wojtekt@google.com>
2016-07-08 08:25:49 +0200

    Avoid computing QOS multiple times

--- plugin/pkg/scheduler/factory/factory_test.go
+++ plugin/pkg/scheduler/factory/factory_test.go
@@ -116,11 +116,11 @@ func TestCreateFromEmptyConfig(t *testing.T) {
 	factory.CreateFromConfig(policy)
 }
 
-func PredicateOne(pod *api.Pod, nodeInfo *schedulercache.NodeInfo) (bool, error) {
+func PredicateOne(pod *api.Pod, meta interface{}, nodeInfo *schedulercache.NodeInfo) (bool, error) {
 	return true, nil
 }
 
-func PredicateTwo(pod *api.Pod, nodeInfo *schedulercache.NodeInfo) (bool, error) {
+func PredicateTwo(pod *api.Pod, meta interface{}, nodeInfo *schedulercache.NodeInfo) (bool, error) {
 	return true, nil
 }
 
