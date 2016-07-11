28bc2062fe2237ef3747adfc35c6cdae09f918f6
Seth Jennings <sjenning@redhat.com>
2016-07-11 11:29:08 -0500

    UPSTREAM: 28263: Allow specifying secret data using strings
    :100644 100644 91fc2f2... 55434a1... M	api/swagger-spec/v1.json
    :100644 100644 866323f... d84a942... M	pkg/api/v1/conversion.go
    :100644 100644 67eff6d... 35323bc... M	pkg/api/v1/conversion_generated.go
    :100644 100644 ea1f301... 6f368ad... M	pkg/api/v1/deep_copy_generated.go
    :100644 100644 841f0f9... ed4c0d2... M	pkg/api/v1/generated.pb.go
    :100644 100644 c2c5f52... 20612a4... M	pkg/api/v1/generated.proto
    :100644 100644 7a52060... 663d722... M	pkg/api/v1/types.go
    :100644 100644 5adbb1e... 93f009a... M	pkg/api/v1/types_swagger_doc_generated.go

--- pkg/api/v1/deep_copy_generated.go
+++ pkg/api/v1/deep_copy_generated.go
@@ -2860,6 +2860,15 @@ func DeepCopy_v1_Secret(in Secret, out *Secret, c *conversion.Cloner) error {
 	} else {
 		out.Data = nil
 	}
+	if in.StringData != nil {
+		in, out := in.StringData, &out.StringData
+		*out = make(map[string]string)
+		for key, val := range in {
+			(*out)[key] = val
+		}
+	} else {
+		out.StringData = nil
+	}
 	out.Type = in.Type
 	return nil
 }
