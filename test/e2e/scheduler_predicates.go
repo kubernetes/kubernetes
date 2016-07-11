85aa0848ba1905f9943145ab92195daaaafac888
Lantao Liu <taotaotheripper@gmail.com>
2016-07-11 00:19:15 -0700

    Revert "Workardound KubeProxy failures in test framework"

--- test/e2e/scheduler_predicates.go
+++ test/e2e/scheduler_predicates.go
@@ -222,7 +222,7 @@ var _ = framework.KubeDescribe("SchedulerPredicates [Serial]", func() {
 			}
 		}
 
-		err = framework.WaitForPodsRunningReady(c, api.NamespaceSystem, int32(systemPodsNo), framework.PodReadyBeforeTimeout, ignoreLabels, false)
+		err = framework.WaitForPodsRunningReady(c, api.NamespaceSystem, int32(systemPodsNo), framework.PodReadyBeforeTimeout, ignoreLabels)
 		Expect(err).NotTo(HaveOccurred())
 
 		for _, node := range nodeList.Items {
