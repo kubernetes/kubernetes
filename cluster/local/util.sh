0e4580f4ae6bf48077574f1267db6df1faf7d8f9
Angus Salkeld <asalkeld@mirantis.com>
2016-07-12 09:29:49 +0200

    Add detect-master to local provider to get e2e working
    go run hack/e2e.go -v -test --check_node_count=false --test_args="--ginkgo.focus=\[Feature:Volumes\]"

--- cluster/local/util.sh
+++ cluster/local/util.sh
@@ -30,3 +30,10 @@ detect-master() {
   KUBE_MASTER_URL="http://${KUBE_MASTER_IP}:8080"
   echo "Using master: $KUBE_MASTER (external IP: $KUBE_MASTER_IP)"
 }
+
+detect-master() {
+  KUBE_MASTER=localhost
+  KUBE_MASTER_IP=127.0.0.1
+  KUBE_MASTER_URL="http://${KUBE_MASTER_IP}:8080"
+  echo "Using master: $KUBE_MASTER (external IP: $KUBE_MASTER_IP)"
+}
