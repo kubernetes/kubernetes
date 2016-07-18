d5d5cbf668c752b0ed7f5f2eb1956d7b13afcd3c
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2016-07-18 16:33:53 +0200

    UPSTREAM: 27263: Cherry-picked
    Dynamically derive AttachOptions.CommandName
    
    :100644 100644 3150bcc... d406fa0... M	pkg/kubectl/cmd/attach.go
    :100644 100644 d8fd075... 875a344... M	pkg/kubectl/cmd/run.go

--- pkg/kubectl/cmd/run.go
+++ pkg/kubectl/cmd/run.go
@@ -243,6 +243,8 @@ func Run(f *cmdutil.Factory, opts *RunOptions, cmdIn io.Reader, cmdOut, cmdErr i
 			Stdin: interactive,
 			TTY:   tty,
 
+			CommandName: cmd.Parent().CommandPath() + " attach",
+
 			Attach: &DefaultRemoteAttach{},
 		}
 		config, err := f.ClientConfig()
@@ -362,7 +364,6 @@ func handleAttachPod(f *cmdutil.Factory, c *client.Client, pod *api.Pod, opts *A
 	opts.Client = c
 	opts.PodName = pod.Name
 	opts.Namespace = pod.Namespace
-	opts.CommandName = "kubectl attach"
 	if err := opts.Run(); err != nil {
 		fmt.Fprintf(opts.Out, "Error attaching, falling back to logs: %v\n", err)
 		req, err := f.LogsForObject(pod, &api.PodLogOptions{Container: opts.GetContainerName(pod)})
