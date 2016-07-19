e47917a51a2ec9a1bb6f2117ec5c2b328081ba92
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2016-07-19 11:08:21 +0300

    Remove GOMAXPROCS() calls because they are unnecessary

--- contrib/mesos/cmd/k8sm-controller-manager/main.go
+++ contrib/mesos/cmd/k8sm-controller-manager/main.go
@@ -19,7 +19,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"runtime"
 
 	"k8s.io/kubernetes/pkg/healthz"
 	"k8s.io/kubernetes/pkg/util"
@@ -36,7 +35,6 @@ func init() {
 }
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	s := controllermanager.NewCMServer()
 	s.AddFlags(pflag.CommandLine)
