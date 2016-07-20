b467a792f38dd506f5ef06abf1c1f8b031982d75
deads2k <deads@redhat.com>
2016-07-20 11:10:28 -0400

    fix String protobuf generator

--- cmd/libs/go2idl/go-to-protobuf/protobuf/parser.go
+++ cmd/libs/go2idl/go-to-protobuf/protobuf/parser.go
@@ -132,7 +132,7 @@ func rewriteOptionalMethods(decl ast.Decl, isOptional OptionalFunc) {
 		switch t.Name.Name {
 		case "Unmarshal":
 			ast.Walk(&optionalItemsVisitor{}, t.Body)
-		case "MarshalTo", "Size":
+		case "MarshalTo", "Size", "String":
 			ast.Walk(&optionalItemsVisitor{}, t.Body)
 			fallthrough
 		case "Marshal":
