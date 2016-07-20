915f82d87925f71b05ad27d5c81d7a7f76bdd8a4
Jess Frazelle <me@jessfraz.com>
2016-07-20 09:38:32 -0400

    add golint to test dockerfile
    Signed-off-by: Jess Frazelle <me@jessfraz.com>

--- hack/jenkins/gotest-dockerized.sh
+++ hack/jenkins/gotest-dockerized.sh
@@ -44,5 +44,5 @@ docker run --rm=true \
   -e "KUBE_VERIFY_GIT_BRANCH=${KUBE_VERIFY_GIT_BRANCH:-}" \
   -e "REPO_DIR=${REPO_DIR}" \
   -e "HOST_ARTIFACTS_DIR=${HOST_ARTIFACTS_DIR}" \
-  -i gcr.io/google_containers/kubekins-test:go1.6.3-docker1.9.1-rev1 \
+  -i gcr.io/google_containers/kubekins-test:go1.6.3-docker1.9.1-rev2 \
   bash -c "cd kubernetes && ${KUBE_TEST_SCRIPT:-./hack/jenkins/test-dockerized.sh}"
