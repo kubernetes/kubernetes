60b04ba5af74656b06aa55ea0cd91b1ad98fd376
Robert Rati <rrati@redhat.com>
2016-07-20 11:31:14 -0400

    Update Route Controller to use Informer instead of re-listing

--- pkg/controller/route/routecontroller.go
+++ pkg/controller/route/routecontroller.go
@@ -26,12 +26,21 @@ import (
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/errors"
 	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/client/cache"
 	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	"k8s.io/kubernetes/pkg/cloudprovider"
+	"k8s.io/kubernetes/pkg/controller"
+	"k8s.io/kubernetes/pkg/controller/framework"
+	"k8s.io/kubernetes/pkg/runtime"
 	"k8s.io/kubernetes/pkg/util/metrics"
 	nodeutil "k8s.io/kubernetes/pkg/util/node"
+	utilruntime "k8s.io/kubernetes/pkg/util/runtime"
 	"k8s.io/kubernetes/pkg/util/wait"
+	"k8s.io/kubernetes/pkg/util/workqueue"
+	"k8s.io/kubernetes/pkg/watch"
 )
+
+type StatusType string
 
 const (
 	// Maximal number of concurrent CreateRoute API calls.
@@ -40,7 +49,10 @@ const (
 	// Maximum number of retries of route creations.
 	maxRetries int = 5
 	// Maximum number of retries of node status update.
-	updateNodeStatusMaxRetries int = 3
+	updateNodeStatusMaxRetries int        = 3
+	statusDelete               StatusType = "Delete"
+	statusAdd                  StatusType = "Add"
+	statusUpdate               StatusType = "Update"
 )
 
 type RouteController struct {
@@ -48,64 +60,123 @@ type RouteController struct {
 	kubeClient  clientset.Interface
 	clusterName string
 	clusterCIDR *net.IPNet
+	// Node framework and store
+	nodeController *framework.Controller
+	nodeStore      cache.StoreToNodeLister
+	// Nodes that need to be synced
+	queue *workqueue.Type
+	// To allow injection of syncRoutes for testing.
+	syncHandler func(nodeStatus NodeStatus) error
+	rateLimiter chan struct{}
+}
+
+type NodeStatus struct {
+	node   *api.Node
+	status StatusType
 }
 
 func New(routes cloudprovider.Routes, kubeClient clientset.Interface, clusterName string, clusterCIDR *net.IPNet) *RouteController {
 	if kubeClient != nil && kubeClient.Core().GetRESTClient().GetRateLimiter() != nil {
 		metrics.RegisterMetricAndTrackRateLimiterUsage("route_controller", kubeClient.Core().GetRESTClient().GetRateLimiter())
 	}
-	return &RouteController{
+	rc := &RouteController{
 		routes:      routes,
 		kubeClient:  kubeClient,
 		clusterName: clusterName,
 		clusterCIDR: clusterCIDR,
+		queue:       workqueue.New(),
+	}
+	rc.nodeStore.Store, rc.nodeController = framework.NewInformer(
+		&cache.ListWatch{
+			ListFunc: func(options api.ListOptions) (runtime.Object, error) {
+				return rc.kubeClient.Core().Nodes().List(options)
+			},
+			WatchFunc: func(options api.ListOptions) (watch.Interface, error) {
+				return rc.kubeClient.Core().Nodes().Watch(options)
+			},
+		},
+		&api.Node{},
+		controller.NoResyncPeriodFunc(),
+		framework.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				nodeStatus := NodeStatus{
+					node:   obj.(*api.Node),
+					status: statusAdd,
+				}
+				rc.queue.Add(nodeStatus)
+			},
+			UpdateFunc: func(old, new interface{}) {
+				nodeStatus := NodeStatus{
+					node:   new.(*api.Node),
+					status: statusUpdate,
+				}
+				rc.queue.Add(nodeStatus)
+			},
+			DeleteFunc: func(obj interface{}) {
+				nodeStatus := NodeStatus{
+					node:   obj.(*api.Node),
+					status: statusDelete,
+				}
+				rc.queue.Add(nodeStatus)
+			},
+		},
+	)
+	rc.syncHandler = rc.reconcileNodeRoutes
+	rc.rateLimiter = make(chan struct{}, maxConcurrentRouteCreations)
+	return rc
+}
+
+func (rc *RouteController) Run(workers int, stopCh <-chan struct{}) {
+	defer utilruntime.HandleCrash()
+	go rc.nodeController.Run(stopCh)
+
+	for i := 0; i < workers; i++ {
+		go wait.Until(rc.worker, time.Second, stopCh)
+	}
+	<-stopCh
+	rc.queue.ShutDown()
+}
+
+func (rc *RouteController) worker() {
+	workFunc := func() bool {
+		statusObj, quit := rc.queue.Get()
+		if quit {
+			return true
+		}
+		defer rc.queue.Done(statusObj)
+		err := rc.syncHandler(statusObj.(NodeStatus))
+		if err != nil {
+			glog.Errorf("Error syncing route: %v", err)
+		}
+		return false
+	}
+	for {
+		if quit := workFunc(); quit {
+			glog.Infof("route controller worker shutting down")
+			return
+		}
 	}
 }
 
-func (rc *RouteController) Run(syncPeriod time.Duration) {
-	// TODO: If we do just the full Resync every 5 minutes (default value)
-	// that means that we may wait up to 5 minutes before even starting
-	// creating a route for it. This is bad.
-	// We should have a watch on node and if we observe a new node (with CIDR?)
-	// trigger reconciliation for that node.
-	go wait.NonSlidingUntil(func() {
-		if err := rc.reconcileNodeRoutes(); err != nil {
-			glog.Errorf("Couldn't reconcile node routes: %v", err)
-		}
-	}, syncPeriod, wait.NeverStop)
-}
-
-func (rc *RouteController) reconcileNodeRoutes() error {
+func (rc *RouteController) reconcileNodeRoutes(nodeStatus NodeStatus) error {
 	routeList, err := rc.routes.ListRoutes(rc.clusterName)
 	if err != nil {
 		return fmt.Errorf("error listing routes: %v", err)
 	}
-	// TODO (cjcullen): use pkg/controller/framework.NewInformer to watch this
-	// and reduce the number of lists needed.
-	nodeList, err := rc.kubeClient.Core().Nodes().List(api.ListOptions{})
-	if err != nil {
-		return fmt.Errorf("error listing nodes: %v", err)
-	}
-	return rc.reconcile(nodeList.Items, routeList)
+	return rc.reconcile(nodeStatus, routeList)
 }
 
-func (rc *RouteController) reconcile(nodes []api.Node, routes []*cloudprovider.Route) error {
-	// nodeCIDRs maps nodeName->nodeCIDR
-	nodeCIDRs := make(map[string]string)
-	// routeMap maps routeTargetInstance->route
+func (rc *RouteController) reconcile(nodeStatus NodeStatus, routes []*cloudprovider.Route) error {
+	node := *nodeStatus.node
+
 	routeMap := make(map[string]*cloudprovider.Route)
 	for _, route := range routes {
 		routeMap[route.TargetInstance] = route
 	}
 
 	wg := sync.WaitGroup{}
-	rateLimiter := make(chan struct{}, maxConcurrentRouteCreations)
 
-	for _, node := range nodes {
-		// Skip if the node hasn't been assigned a CIDR yet.
-		if node.Spec.PodCIDR == "" {
-			continue
-		}
+	if (nodeStatus.status == statusAdd || nodeStatus.status == statusUpdate) && node.Spec.PodCIDR != "" {
 		// Check if we have a route for this node w/ the correct CIDR.
 		r := routeMap[node.Name]
 		if r == nil || r.DestinationCIDR != node.Spec.PodCIDR {
@@ -122,10 +193,10 @@ func (rc *RouteController) reconcile(nodes []api.Node, routes []*cloudprovider.R
 					startTime := time.Now()
 					// Ensure that we don't have more than maxConcurrentRouteCreations
 					// CreateRoute calls in flight.
-					rateLimiter <- struct{}{}
+					rc.rateLimiter <- struct{}{}
 					glog.Infof("Creating route for node %s %s with hint %s, throttled %v", nodeName, route.DestinationCIDR, nameHint, time.Now().Sub(startTime))
 					err := rc.routes.CreateRoute(rc.clusterName, nameHint, route)
-					<-rateLimiter
+					<-rc.rateLimiter
 
 					rc.updateNetworkingCondition(nodeName, err == nil)
 					if err != nil {
@@ -139,24 +210,25 @@ func (rc *RouteController) reconcile(nodes []api.Node, routes []*cloudprovider.R
 		} else {
 			rc.updateNetworkingCondition(node.Name, true)
 		}
-		nodeCIDRs[node.Name] = node.Spec.PodCIDR
 	}
-	for _, route := range routes {
-		if rc.isResponsibleForRoute(route) {
-			// Check if this route applies to a node we know about & has correct CIDR.
-			if nodeCIDRs[route.TargetInstance] != route.DestinationCIDR {
-				wg.Add(1)
-				// Delete the route.
-				go func(route *cloudprovider.Route, startTime time.Time) {
-					glog.Infof("Deleting route %s %s", route.Name, route.DestinationCIDR)
-					if err := rc.routes.DeleteRoute(rc.clusterName, route); err != nil {
-						glog.Errorf("Could not delete route %s %s after %v: %v", route.Name, route.DestinationCIDR, time.Now().Sub(startTime), err)
-					} else {
-						glog.Infof("Deleted route %s %s after %v", route.Name, route.DestinationCIDR, time.Now().Sub(startTime))
-					}
-					wg.Done()
+	if nodeStatus.status == statusDelete {
+		for _, route := range routes {
+			if rc.isResponsibleForRoute(route) {
+				// Check if this route applies to a node we know about & has correct CIDR.
+				if node.Name == route.TargetInstance && node.Spec.PodCIDR == route.DestinationCIDR {
+					wg.Add(1)
+					// Delete the route.
+					go func(route *cloudprovider.Route, startTime time.Time) {
+						glog.Infof("Deleting route %s %s", route.Name, route.DestinationCIDR)
+						if err := rc.routes.DeleteRoute(rc.clusterName, route); err != nil {
+							glog.Errorf("Could not delete route %s %s after %v: %v", route.Name, route.DestinationCIDR, time.Now().Sub(startTime), err)
+						} else {
+							glog.Infof("Deleted route %s %s after %v", route.Name, route.DestinationCIDR, time.Now().Sub(startTime))
+						}
+						wg.Done()
 
-				}(route, time.Now())
+					}(route, time.Now())
+				}
 			}
 		}
 	}
