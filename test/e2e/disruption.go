f4bd4c2d1ccbf1a18b0e81cd70be78037d53121b
Matt Liggett <mml@google.com>
2016-07-20 11:24:53 -0700

    Update a PDB.

--- test/e2e/disruption.go
+++ test/e2e/disruption.go
@@ -17,6 +17,9 @@ limitations under the License.
 package e2e
 
 import (
+	"fmt"
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"k8s.io/kubernetes/pkg/api"
@@ -24,6 +27,7 @@ import (
 	"k8s.io/kubernetes/pkg/apis/policy"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
 	"k8s.io/kubernetes/pkg/util/intstr"
+	"k8s.io/kubernetes/pkg/util/wait"
 	"k8s.io/kubernetes/test/e2e/framework"
 )
 
@@ -50,6 +54,51 @@ var _ = framework.KubeDescribe("DisruptionController [Feature:PodDisruptionbudge
 		}
 		_, err := c.Policy().PodDisruptionBudgets(ns).Create(&pdb)
 		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should update PodDisruptionBudget status", func() {
+		pdb := policy.PodDisruptionBudget{
+			ObjectMeta: api.ObjectMeta{
+				Name:      "foo",
+				Namespace: ns,
+			},
+			Spec: policy.PodDisruptionBudgetSpec{
+				Selector:     &unversioned.LabelSelector{MatchLabels: map[string]string{"foo": "bar"}},
+				MinAvailable: intstr.FromInt(2),
+			},
+		}
+		_, err := c.Policy().PodDisruptionBudgets(ns).Create(&pdb)
+		Expect(err).NotTo(HaveOccurred())
+		for i := 0; i < 2; i++ {
+			pod := &api.Pod{
+				ObjectMeta: api.ObjectMeta{
+					Name:      fmt.Sprintf("pod-%d", i),
+					Namespace: ns,
+					Labels:    map[string]string{"foo": "bar"},
+				},
+				Spec: api.PodSpec{
+					Containers: []api.Container{
+						{
+							Name:  "busybox",
+							Image: "gcr.io/google_containers/echoserver:1.4",
+						},
+					},
+					RestartPolicy: api.RestartPolicyAlways,
+				},
+			}
+
+			_, err := c.Pods(ns).Create(pod)
+			framework.ExpectNoError(err, "Creating pod %q in namespace %q", pod.Name, ns)
+		}
+		err = wait.PollImmediate(framework.Poll, 60*time.Second, func() (bool, error) {
+			pdb, err := c.Policy().PodDisruptionBudgets(ns).Get("foo")
+			if err != nil {
+				return false, err
+			}
+			return pdb.Status.PodDisruptionAllowed, nil
+		})
+		Expect(err).NotTo(HaveOccurred())
+
 	})
 
 })
