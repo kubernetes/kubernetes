35ac0fdb2fc754243647f4d5d37c037534e56d61
Michael Taufen <mtaufen@google.com>
2016-07-21 11:49:59 -0700

    Implement Alpha Dynamic Kubelet Configuration
    See Issue #27980 and Proposal PR #29459

--- pkg/util/config/feature_gate.go
+++ pkg/util/config/feature_gate.go
@@ -38,14 +38,16 @@ const (
 	// specification of gates. Examples:
 	//   AllAlpha=false,NewFeature=true  will result in newFeature=true
 	//   AllAlpha=true,NewFeature=false  will result in newFeature=false
-	allAlphaGate = "AllAlpha"
+	allAlphaGate         = "AllAlpha"
+	dynamicKubeletConfig = "DynamicKubeletConfig"
 )
 
 var (
 	// Default values for recorded features.  Every new feature gate should be
 	// represented here.
 	knownFeatures = map[string]featureSpec{
-		allAlphaGate: {false, alpha},
+		allAlphaGate:         {false, alpha},
+		dynamicKubeletConfig: {false, alpha},
 	}
 
 	// Special handling for a few gates.
@@ -86,6 +88,7 @@ type FeatureGate interface {
 	// MyFeature() bool
 
 	// TODO: Define accessors for each non-API alpha feature.
+	DynamicKubeletConfig() bool
 }
 
 // featureGate implements FeatureGate as well as pflag.Value for flag parsing.
@@ -152,6 +155,11 @@ func (f *featureGate) String() string {
 
 func (f *featureGate) Type() string {
 	return "mapStringBool"
+}
+
+// DynamicKubeletConfig returns value for dynamicKubeletConfig
+func (f *featureGate) DynamicKubeletConfig() bool {
+	return f.lookup(dynamicKubeletConfig)
 }
 
 func (f *featureGate) lookup(key string) bool {
