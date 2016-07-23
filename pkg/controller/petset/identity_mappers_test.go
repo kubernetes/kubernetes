7d3d6a7f644c66ac44f276d5784c4ac812d59c46
lixiaobing10051267 <li.xiaobing1@zte.com.cn>
2016-07-23 14:18:09 +0800

    t.Errorf output wrong variables in identity_mapper_test.go

--- pkg/controller/petset/identity_mappers_test.go
+++ pkg/controller/petset/identity_mappers_test.go
@@ -54,11 +54,11 @@ func TestPetIDDNS(t *testing.T) {
 			t.Fatalf("Failed to generate pet %v", err)
 		}
 		if hostname, ok := pod.Annotations[api_pod.PodHostnameAnnotation]; !ok || hostname != petName {
-			t.Errorf("Wrong hostname: %v", petName)
+			t.Errorf("Wrong hostname: %v", hostname)
 		}
 		// TODO: Check this against the governing service.
 		if subdomain, ok := pod.Annotations[api_pod.PodSubdomainAnnotation]; !ok || subdomain != petSubdomain {
-			t.Errorf("Wrong subdomain: %v", petName)
+			t.Errorf("Wrong subdomain: %v", subdomain)
 		}
 	}
 }
