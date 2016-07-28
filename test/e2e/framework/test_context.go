adf6b6ddf06259d6c83be8fe5cdaf057767c370c
Ron Lai <ronl@google.com>
2016-07-28 12:05:59 -0700

    Added an node e2e test for pod evictions due to disk pressure.

--- test/e2e/framework/test_context.go
+++ test/e2e/framework/test_context.go
@@ -71,6 +71,8 @@ type TestContextType struct {
 	NodeName string
 	// Whether to enable the QoS Cgroup Hierarchy or not
 	CgroupsPerQOS bool
+	// The hard eviction thresholds
+	EvictionHard string
 }
 
 type CloudConfig struct {
@@ -150,4 +152,5 @@ func RegisterClusterFlags() {
 func RegisterNodeFlags() {
 	flag.StringVar(&TestContext.NodeName, "node-name", "", "Name of the node to run tests on (node e2e suite only).")
 	flag.BoolVar(&TestContext.CgroupsPerQOS, "cgroups-per-qos", false, "Enable creation of QoS cgroup hierarchy, if true top level QoS and pod cgroups are created.")
+	flag.StringVar(&TestContext.EvictionHard, "eviction-hard", "", "The hard eviction thresholds. If set, pods get evicted when the specified resources drop below the thresholds.")
 }
