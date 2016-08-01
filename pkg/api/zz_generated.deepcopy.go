e2262ccb4e7538699a7fa4fc55e11d5f28f9cede
Christian Simon <simon@swine.de>
2016-08-01 11:45:03 +0100

    Adds datasetUUID for referencing flocker datasets
    * flocker datasets should be attached using an unique identifier. This
      is not the case for the name metadata used by datasetName
    * allow only one of datasetUUID / datasetName specified

--- pkg/api/zz_generated.deepcopy.go
+++ pkg/api/zz_generated.deepcopy.go
@@ -1178,6 +1178,7 @@ func DeepCopy_api_FlockerVolumeSource(in interface{}, out interface{}, c *conver
 		in := in.(*FlockerVolumeSource)
 		out := out.(*FlockerVolumeSource)
 		out.DatasetName = in.DatasetName
+		out.DatasetUUID = in.DatasetUUID
 		return nil
 	}
 }
