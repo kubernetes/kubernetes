4c8e3d7049a21fa87181b537a1d7accccbe99943
Michal Fojtik <mfojtik@redhat.com>
2016-08-03 13:28:16 +0200

    Add quota evaluator for extension resources

---
+++ pkg/quota/evaluator/extensions/deployments.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package extensions
+
+import (
+	"k8s.io/kubernetes/pkg/admission"
+	"k8s.io/kubernetes/pkg/api"
+	extensionapi "k8s.io/kubernetes/pkg/apis/extensions"
+	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	"k8s.io/kubernetes/pkg/quota"
+	"k8s.io/kubernetes/pkg/quota/generic"
+	"k8s.io/kubernetes/pkg/runtime"
+)
+
+// NewDeploymentEvaluator returns an evaluator that can evaluate deployments
+func NewDeploymentEvaluator(kubeClient clientset.Interface) quota.Evaluator {
+	allResources := []api.ResourceName{"deployments"}
+	return &generic.GenericEvaluator{
+		Name:              "Evaluator.Deployment",
+		InternalGroupKind: extensionapi.Kind("Deployment"),
+		InternalOperationResources: map[admission.Operation][]api.ResourceName{
+			admission.Create: allResources,
+		},
+		MatchedResourceNames: allResources,
+		MatchesScopeFunc:     generic.MatchesNoScopeFunc,
+		ConstraintsFunc:      generic.ObjectCountConstraintsFunc("deployments"),
+		UsageFunc:            generic.ObjectCountUsageFunc("deployments"),
+		ListFuncByNamespace: func(namespace string, options api.ListOptions) (runtime.Object, error) {
+			return kubeClient.Extensions().Deployments(namespace).List(options)
+		},
+	}
+}
