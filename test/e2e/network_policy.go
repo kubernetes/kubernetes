a107aedad44a13daa22927a4b38185ea8cf982ad
Rob Brockbank <rob@tigera.io>
2016-08-04 13:29:48 -0700

    Further build script related fixes: boilerplate headers, gofmt and known flags

--- test/e2e/network_policy.go
+++ test/e2e/network_policy.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2014 The Kubernetes Authors All rights reserved.
+Copyright 2016 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -423,7 +423,7 @@ func addNetworkPolicyOpenPort(f *framework.Framework, namespace *api.Namespace, 
 		},
 		Spec: extensions.NetworkPolicySpec{
 			Ingress: []extensions.NetworkPolicyIngressRule{
-				extensions.NetworkPolicyIngressRule{
+				{
 					Ports: []extensions.NetworkPolicyPort{
 						{
 							Protocol: &protocol,
