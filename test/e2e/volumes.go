deb9d7104c732c71ef4f748f12ffbc125f50a888
Jan Safranek <jsafrane@redhat.com>
2016-08-10 10:55:44 +0200

    Revert "Add test for supplemental gid annotation to pv e2e test"
    This reverts commit a7145c013e16b81b7103ee00059bb78decb40b62.

--- test/e2e/volumes.go
+++ test/e2e/volumes.go
@@ -68,8 +68,6 @@ type VolumeTestConfig struct {
 	serverImage string
 	// Ports to export from the server pod. TCP only.
 	serverPorts []int
-	// Arguments to pass to the container image.
-	serverArgs []string
 	// Volumes needed to be mounted to the server container from the host
 	// map <host (source) path> -> <container (dst.) path>
 	volumes map[string]string
@@ -136,7 +134,6 @@ func startVolumeServer(client *client.Client, config VolumeTestConfig) *api.Pod 
 					SecurityContext: &api.SecurityContext{
 						Privileged: privileged,
 					},
-					Args:         config.serverArgs,
 					Ports:        serverPodPorts,
 					VolumeMounts: mounts,
 				},
