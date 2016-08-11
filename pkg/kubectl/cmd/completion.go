4075117507a44969abf7c6807f42e2b82dd6b711
juanvallejo <jvallejo@redhat.com>
2016-08-11 13:38:11 -0400

    UPSTREAM: 30460: Add zsh compatibility note `completion` cmd help
    :100644 100644 678a079... debb769... M	pkg/kubectl/cmd/completion.go

--- pkg/kubectl/cmd/completion.go
+++ pkg/kubectl/cmd/completion.go
@@ -41,10 +41,11 @@ $ brew install bash-completion
 $ source $(brew --prefix)/etc/bash_completion
 $ source <(kubectl completion bash)
 
-If you use zsh, the following will load kubectl zsh completion:
+If you use zsh*, the following will load kubectl zsh completion:
 
 $ source <(kubectl completion zsh)
-`
+
+* zsh completions are only supported in versions of zsh >= 5.2`
 )
 
 var (
