c5b1eb1a66d61d3426adbae8de9bedae81c983b7
mbohlool <mehdy@google.com>
2016-08-10 23:28:21 -0700

    Start etcd-empty-dir-cleanup pod automatically on master

--- cluster/gce/gci/configure-helper.sh
+++ cluster/gce/gci/configure-helper.sh
@@ -549,6 +549,10 @@ function prepare-etcd-manifest {
   mv "${temp_file}" /etc/kubernetes/manifests
 }
 
+function start-etcd-empty-dir-cleanup-pod {
+  cp "${KUBE_HOME}/kube-manifests/kubernetes/gci-trusty/etcd-empty-dir-cleanup/etcd-empty-dir-cleanup.yaml" "/etc/kubernetes/manifests"
+}
+
 # Starts etcd server pod (and etcd-events pod if needed).
 # More specifically, it prepares dirs and files, sets the variable value
 # in the manifests, and copies them to /etc/kubernetes/manifests.
@@ -1038,6 +1042,7 @@ start-kubelet
 if [[ "${KUBERNETES_MASTER:-}" == "true" ]]; then
   compute-master-manifest-variables
   start-etcd-servers
+  start-etcd-empty-dir-cleanup-pod
   start-kube-apiserver
   start-kube-controller-manager
   start-kube-scheduler
