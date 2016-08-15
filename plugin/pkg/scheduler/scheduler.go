5bf6bb80a2bf4074d9bf7dbafebed5e0b758673c
m1093782566 <dj199008@gmail.com>
2016-08-15 12:10:42 +0800

    update comment info for scheduler binding fails
    Change-Id: I1cc4985255c517bc6b26364bb460c4ed1441a6a8

--- plugin/pkg/scheduler/scheduler.go
+++ plugin/pkg/scheduler/scheduler.go
@@ -111,12 +111,10 @@ func (s *Scheduler) scheduleOne() {
 
 	// Optimistically assume that the binding will succeed and send it to apiserver
 	// in the background.
-	// The only risk in this approach is that if the binding fails because of some
-	// reason, scheduler will be assuming that it succeeded while scheduling next
-	// pods, until the assumption in the internal cache expire (expiration is
-	// defined as "didn't read the binding via watch within a given timeout",
-	// timeout is currently set to 30s). However, after this timeout, the situation
-	// will self-repair.
+	// If the binding fails because of some reasons, scheduler will release the
+	// resources(CPU, Memory, GPU, etc.) which have been allocated to the assumed
+	// pod immediately instead of waiting for the assumption in the internal cache
+	// expired.
 	assumed := *pod
 	assumed.Spec.NodeName = dest
 	if err := s.config.SchedulerCache.AssumePod(&assumed); err != nil {
