4f0f8d2be0d80d4be949046ee33eb6c02b566819
Brendan Burns <bburns@google.com>
2016-08-15 21:59:49 -0700

    Initial support for TokenFile in the client config.

--- pkg/client/unversioned/clientcmd/api/v1/types.go
+++ pkg/client/unversioned/clientcmd/api/v1/types.go
@@ -82,6 +82,8 @@ type AuthInfo struct {
 	ClientKeyData []byte `json:"client-key-data,omitempty"`
 	// Token is the bearer token for authentication to the kubernetes cluster.
 	Token string `json:"token,omitempty"`
+	// TokenFile is a pointer to a file that contains a bearer token (as described above).  If both Token and TokenFile are present, Token takes precedence.
+	TokenFile string `json:"tokenFile,omitempty"`
 	// Impersonate is the username to imperonate.  The name matches the flag.
 	Impersonate string `json:"as,omitempty"`
 	// Username is the username for basic authentication to the kubernetes cluster.
