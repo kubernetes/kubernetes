ab3bc03a4f4b70e9e484e2b34cc1f916d98a8a5c
Michail Kargakis <mkargaki@redhat.com>
2016-08-17 18:18:16 +0200

    api: add readyReplicas in RC/RS

--- pkg/api/v1/types.go
+++ pkg/api/v1/types.go
@@ -2001,6 +2001,9 @@ type ReplicationControllerStatus struct {
 	// The number of pods that have labels matching the labels of the pod template of the replication controller.
 	FullyLabeledReplicas int32 `json:"fullyLabeledReplicas,omitempty" protobuf:"varint,2,opt,name=fullyLabeledReplicas"`
 
+	// The number of ready replicas for this replication controller.
+	ReadyReplicas int32 `json:"readyReplicas,omitempty"`
+
 	// ObservedGeneration reflects the generation of the most recently observed replication controller.
 	ObservedGeneration int64 `json:"observedGeneration,omitempty" protobuf:"varint,3,opt,name=observedGeneration"`
 }
