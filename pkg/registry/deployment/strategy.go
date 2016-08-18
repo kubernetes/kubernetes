ba76e0dc5d7d6fa8143d8bff2693dea600d4be8d
Chao Xu <xuchao@google.com>
2016-08-17 19:19:44 -0700

    revert stratety for job and deployment

--- pkg/registry/deployment/strategy.go
+++ pkg/registry/deployment/strategy.go
@@ -21,7 +21,6 @@ import (
 	"reflect"
 
 	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/api/rest"
 	"k8s.io/kubernetes/pkg/apis/extensions"
 	"k8s.io/kubernetes/pkg/apis/extensions/validation"
 	"k8s.io/kubernetes/pkg/fields"
@@ -40,12 +39,6 @@ type deploymentStrategy struct {
 // Strategy is the default logic that applies when creating and updating Deployment
 // objects via the REST API.
 var Strategy = deploymentStrategy{api.Scheme, api.SimpleNameGenerator}
-
-// DefaultGarbageCollectionBehavior returns Orphan because that's the default
-// behavior before the server-side garbage collection is implemented.
-func (deploymentStrategy) DefaultGarbageCollectionBehavior() rest.GarbageCollectionBehavior {
-	return rest.Orphan
-}
 
 // NamespaceScoped is true for deployment.
 func (deploymentStrategy) NamespaceScoped() bool {
