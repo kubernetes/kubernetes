26a66232610e627d4912fbd3dfb37afa8036dcf8
Jordan Liggitt <jliggitt@redhat.com>
2016-08-18 00:56:52 -0400

    kubelet: '--experimental-bootstrap-kubeconfig' refactor.
    Move bootstrap functions to separate files.
    Split some of the functions into small sub-functions for reusability.
    Other cleanups

--- pkg/util/certificates/csr_test.go
+++ pkg/util/certificates/csr_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package certificates
 
 import (
@@ -19,7 +35,11 @@ func TestNewCertificateRequest(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	_, _, err = NewCertificateRequest(keyData, subject, dnsSANs, ipSANs)
+	key, err := ParsePrivateKey(keyData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = NewCertificateRequest(key, subject, dnsSANs, ipSANs)
 	if err != nil {
 		t.Error(err)
 	}
