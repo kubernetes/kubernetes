ee7ca938435a6f96affcd3b4bd9198a199fc2cba
Andy Goldstein <agoldste@redhat.com>
2016-08-18 22:51:40 -0400

    UPSTREAM: 28744: Allow a FIFO client to requeue under lock
    The Pop method should allow a caller to requeue an item while under the
    fifo lock, to avoid races on deletes.
    
    :100644 100644 533d3e5... 93ee40e... M	pkg/client/cache/delta_fifo.go
    :100644 100644 fb6e2a4... 98b2f8f... M	pkg/client/cache/delta_fifo_test.go
    :100644 100644 2a50982... 82df122... M	pkg/client/cache/fifo.go
    :100644 100644 667c168... 969ad3f... M	pkg/client/cache/fifo_test.go

--- pkg/client/cache/delta_fifo_test.go
+++ pkg/client/cache/delta_fifo_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package cache
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
@@ -81,6 +82,50 @@ func TestDeltaFIFO_basic(t *testing.T) {
 		default:
 			t.Fatalf("unexpected type %#v", obj)
 		}
+	}
+}
+
+func TestDeltaFIFO_requeueOnPop(t *testing.T) {
+	f := NewDeltaFIFO(testFifoObjectKeyFunc, nil, nil)
+
+	f.Add(mkFifoObj("foo", 10))
+	_, err := f.Pop(func(obj interface{}) error {
+		if obj.(Deltas)[0].Object.(testFifoObject).name != "foo" {
+			t.Fatalf("unexpected object: %#v", obj)
+		}
+		return ErrRequeue{Err: nil}
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if _, ok, err := f.GetByKey("foo"); !ok || err != nil {
+		t.Fatalf("object should have been requeued: %t %v", ok, err)
+	}
+
+	_, err = f.Pop(func(obj interface{}) error {
+		if obj.(Deltas)[0].Object.(testFifoObject).name != "foo" {
+			t.Fatalf("unexpected object: %#v", obj)
+		}
+		return ErrRequeue{Err: fmt.Errorf("test error")}
+	})
+	if err == nil || err.Error() != "test error" {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if _, ok, err := f.GetByKey("foo"); !ok || err != nil {
+		t.Fatalf("object should have been requeued: %t %v", ok, err)
+	}
+
+	_, err = f.Pop(func(obj interface{}) error {
+		if obj.(Deltas)[0].Object.(testFifoObject).name != "foo" {
+			t.Fatalf("unexpected object: %#v", obj)
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if _, ok, err := f.GetByKey("foo"); ok || err != nil {
+		t.Fatalf("object should have been removed: %t %v", ok, err)
 	}
 }
 
