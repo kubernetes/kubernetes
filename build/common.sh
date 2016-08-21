2c5040d7dfd664520339131d057b3ec4f5270004
Joe Beda <joe.github@bedafamily.com>
2016-08-21 13:32:06 -0700

    Make update-generated-*.sh not hack around with docker images
    make-generated-{protobuf,runtime}.sh was doing some really nasty stuff with how
    the build container was managed in order to copy results out.  Since we have
    more flexibility to grab results out of the build container, we can now avoid
    all of this.  Ideally we wouldn't have `hack` calling `build` at all, but we
    aren't there yet.

--- build/common.sh
+++ build/common.sh
@@ -664,8 +664,11 @@ function kube::build::copy_output() {
   rsync $rsync_extra \
     --password-file="${LOCAL_OUTPUT_BUILD_CONTEXT}/rsyncd.password" \
     --prune-empty-dirs \
+    --filter='- /vendor/' \
     --filter='+ /_output/dockerized/bin/**' \
     --filter='+ zz_generated.*' \
+    --filter='+ generated.proto' \
+    --filter='+ *.pb.go' \
     --filter='+ */' \
     --filter='- /**' \
     --archive --perms \
