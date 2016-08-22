eed4eae680d5dada79d6fe27b19e12e9cc2472b6
Wojciech Tyczynski <wojtekt@google.com>
2016-08-22 15:07:17 +0200

    Avoid sorting List when unnecessary

--- pkg/client/cache/thread_safe_store.go
+++ pkg/client/cache/thread_safe_store.go
@@ -151,7 +151,7 @@ func (c *threadSafeMap) Index(indexName string, obj interface{}) ([]interface{},
 	returnKeySet := sets.String{}
 	for _, indexKey := range indexKeys {
 		set := index[indexKey]
-		for _, key := range set.List() {
+		for _, key := range set.UnsortedList() {
 			returnKeySet.Insert(key)
 		}
 	}
