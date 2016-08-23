63d9e82bed0f7d0673f79c42a0f8b2a45f649a3f
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-08-23 15:25:46 +0100

    Implement `kubeadm {init,join}` with JWS
    - for now init starts the disco server in foreground
    - join is supper basic and untested

--- pkg/kubeadm/cmd/join.go
+++ pkg/kubeadm/cmd/join.go
@@ -17,10 +17,15 @@ limitations under the License.
 package kubecmd
 
 import (
+	"fmt"
 	"io"
 
+	"github.com/renstrom/dedent"
 	"github.com/spf13/cobra"
+
 	kubeadmapi "k8s.io/kubernetes/pkg/kubeadm/api"
+	kubenode "k8s.io/kubernetes/pkg/kubeadm/node"
+	kubeadmutil "k8s.io/kubernetes/pkg/kubeadm/util"
 )
 
 func NewCmdJoin(out io.Writer, params *kubeadmapi.BootstrapParams) *cobra.Command {
@@ -28,7 +33,44 @@ func NewCmdJoin(out io.Writer, params *kubeadmapi.BootstrapParams) *cobra.Comman
 		Use:   "join",
 		Short: "Run this on other servers to join an existing cluster.",
 		Run: func(cmd *cobra.Command, args []string) {
+			if params.Discovery.BearerToken == "" {
+				out.Write([]byte(fmt.Sprintf("Must specify --token (see --help)\n")))
+				return
+			}
+			if params.Discovery.ApiServerURLs == "" {
+				out.Write([]byte(fmt.Sprintf("Must specify --api-server-urls (see --help)\n")))
+				return
+			}
+			kubeconfig, err := kubenode.RetrieveTrustedClusterInfo(params)
+			if err != nil {
+				out.Write([]byte(fmt.Sprintf("Failed to bootstrap: %s\n", err)))
+				return
+			}
+			//fmt.Println("recieved signed certificate from the API server, will write `/etc/kubernetes/kubelet.conf`...")
+
+			err = kubeadmutil.WriteKubeconfigIfNotExists(params, "kubelet", kubeconfig)
+			if err != nil {
+				out.Write([]byte(fmt.Sprintf("Unable to write config for node:\n%s\n", err)))
+				return
+			}
+			out.Write([]byte(dedent.Dedent(`
+				Node join complete:
+				* Certificate signing request sent to master and response
+				  received.
+				* Kubelet informed of new secure connection details.
+
+				Run 'kubectl get nodes' on the master to see this node join.
+
+			`)))
 		},
 	}
+
+	// TODO this should become `kubeadm join --token=<...> <master-ip-addr>`
+	cmd.PersistentFlags().StringVarP(&params.Discovery.ApiServerURLs, "api-server-urls", "", "",
+		`Comma separated list of API server URLs. Typically this might be just
+		https://<address-of-master>:8080/`)
+	cmd.PersistentFlags().StringVarP(&params.Discovery.BearerToken, "token", "", "",
+		`Shared secret used to secure bootstrap. Must match output of 'init-master'.`)
+
 	return cmd
 }
---
+++ pkg/kubeadm/node/discovery.go
@@ -0,0 +1,65 @@
+package kubenode
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/square/go-jose"
+	clientcmdapi "k8s.io/kubernetes/pkg/client/unversioned/clientcmd/api"
+	kubeadmapi "k8s.io/kubernetes/pkg/kubeadm/api"
+)
+
+func RetrieveTrustedClusterInfo(params *kubeadmapi.BootstrapParams) (*clientcmdapi.Config, error) {
+	apiServerURL, err := url.Parse(strings.Split(params.Discovery.ApiServerURLs, ",")[0])
+	if err != nil {
+		return nil, err
+	}
+
+	host, port := apiServerURL.Host, 8081
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:%d/api/v1alpha1/testclusterinfo", host, port), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+	io.Copy(buf, res.Body)
+	res.Body.Close()
+
+	object, err := jose.ParseSigned(buf.String())
+	if err != nil {
+		return nil, err
+	}
+
+	output, err := object.Verify([]byte(params.Discovery.BearerToken))
+	if err != nil {
+		return nil, err
+	}
+
+	clusterInfo := kubeadmapi.ClusterInfo{}
+
+	if err := json.Unmarshal(output, &clusterInfo); err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("ClusterInfo: %#v", clusterInfo)
+
+	if len(clusterInfo.CertificateAuthorities) == 0 || len(clusterInfo.Endpoints) == 0 {
+		return nil, fmt.Errorf("Cluster discovery object (ClusterInfo) is invalid")
+	}
+
+	// TODO figure out what we should do when there is a chain of certificates and more then one API endpoint
+	apiServer := clusterInfo.Endpoints[0]
+	caCert := []byte(clusterInfo.CertificateAuthorities[0])
+
+	return PerformTLSBootstrap(params, apiServer, caCert)
+}
