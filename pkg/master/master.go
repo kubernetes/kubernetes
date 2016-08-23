f46f7d2520aac1b8bc1cbd3aabfd05ca530bdb4f
deads2k <deads@redhat.com>
2016-08-23 10:09:54 -0400

    refactor genericapiserver new to combine initialization

--- pkg/master/master.go
+++ pkg/master/master.go
@@ -179,7 +179,7 @@ func New(c *Config) (*Master, error) {
 		return nil, fmt.Errorf("Master.New() called with config.KubeletClient == nil")
 	}
 
-	s, err := genericapiserver.New(c.Config)
+	s, err := c.Config.New()
 	if err != nil {
 		return nil, err
 	}
