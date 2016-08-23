bb01653d4151afd92444ea0b6ea99c3b42210c44
lojies <lu.zhenxing@zte.com.cn>
2016-08-23 11:11:05 +0800

    change to stderr

--- pkg/kubectl/cmd/describe_test.go
+++ pkg/kubectl/cmd/describe_test.go
@@ -36,8 +36,8 @@ func TestDescribeUnknownSchemaObject(t *testing.T) {
 	}
 	tf.Namespace = "non-default"
 	buf := bytes.NewBuffer([]byte{})
-
-	cmd := NewCmdDescribe(f, buf)
+	buferr := bytes.NewBuffer([]byte{})
+	cmd := NewCmdDescribe(f, buf, buferr)
 	cmd.Run(cmd, []string{"type", "foo"})
 
 	if d.Name != "foo" || d.Namespace != "non-default" {
@@ -68,8 +68,8 @@ func TestDescribeObject(t *testing.T) {
 	}
 	tf.Namespace = "test"
 	buf := bytes.NewBuffer([]byte{})
-
-	cmd := NewCmdDescribe(f, buf)
+	buferr := bytes.NewBuffer([]byte{})
+	cmd := NewCmdDescribe(f, buf, buferr)
 	cmd.Flags().Set("filename", "../../../examples/guestbook/legacy/redis-master-controller.yaml")
 	cmd.Run(cmd, []string{})
 
@@ -94,7 +94,8 @@ func TestDescribeListObjects(t *testing.T) {
 
 	tf.Namespace = "test"
 	buf := bytes.NewBuffer([]byte{})
-	cmd := NewCmdDescribe(f, buf)
+	buferr := bytes.NewBuffer([]byte{})
+	cmd := NewCmdDescribe(f, buf, buferr)
 	cmd.Run(cmd, []string{"pods"})
 	if buf.String() != fmt.Sprintf("%s\n\n%s", d.Output, d.Output) {
 		t.Errorf("unexpected output: %s", buf.String())
@@ -113,7 +114,8 @@ func TestDescribeObjectShowEvents(t *testing.T) {
 
 	tf.Namespace = "test"
 	buf := bytes.NewBuffer([]byte{})
-	cmd := NewCmdDescribe(f, buf)
+	buferr := bytes.NewBuffer([]byte{})
+	cmd := NewCmdDescribe(f, buf, buferr)
 	cmd.Flags().Set("show-events", "true")
 	cmd.Run(cmd, []string{"pods"})
 	if d.Settings.ShowEvents != true {
@@ -133,7 +135,8 @@ func TestDescribeObjectSkipEvents(t *testing.T) {
 
 	tf.Namespace = "test"
 	buf := bytes.NewBuffer([]byte{})
-	cmd := NewCmdDescribe(f, buf)
+	buferr := bytes.NewBuffer([]byte{})
+	cmd := NewCmdDescribe(f, buf, buferr)
 	cmd.Flags().Set("show-events", "false")
 	cmd.Run(cmd, []string{"pods"})
 	if d.Settings.ShowEvents != false {
