023423680e9d21cd276198dc1396ec6022556977
Minhan Xia <mixia@google.com>
2016-08-24 16:58:26 -0700

    bump debian-iptables version

--- build/common.sh
+++ build/common.sh
@@ -105,7 +105,7 @@ kube::build::get_docker_wrapped_binaries() {
           kube-apiserver,busybox
           kube-controller-manager,busybox
           kube-scheduler,busybox
-          kube-proxy,gcr.io/google_containers/debian-iptables-amd64:v3
+          kube-proxy,gcr.io/google_containers/debian-iptables-amd64:v4
           federation-apiserver,busybox
           federation-controller-manager,busybox
         );;
@@ -114,7 +114,7 @@ kube::build::get_docker_wrapped_binaries() {
           kube-apiserver,armel/busybox
           kube-controller-manager,armel/busybox
           kube-scheduler,armel/busybox
-          kube-proxy,gcr.io/google_containers/debian-iptables-arm:v3
+          kube-proxy,gcr.io/google_containers/debian-iptables-arm:v4
           federation-apiserver,armel/busybox
           federation-controller-manager,armel/busybox
         );;
@@ -123,7 +123,7 @@ kube::build::get_docker_wrapped_binaries() {
           kube-apiserver,aarch64/busybox
           kube-controller-manager,aarch64/busybox
           kube-scheduler,aarch64/busybox
-          kube-proxy,gcr.io/google_containers/debian-iptables-arm64:v3
+          kube-proxy,gcr.io/google_containers/debian-iptables-arm64:v4
           federation-apiserver,aarch64/busybox
           federation-controller-manager,aarch64/busybox
         );;
@@ -132,7 +132,7 @@ kube::build::get_docker_wrapped_binaries() {
           kube-apiserver,ppc64le/busybox
           kube-controller-manager,ppc64le/busybox
           kube-scheduler,ppc64le/busybox
-          kube-proxy,gcr.io/google_containers/debian-iptables-ppc64le:v3
+          kube-proxy,gcr.io/google_containers/debian-iptables-ppc64le:v4
           federation-apiserver,ppc64le/busybox
           federation-controller-manager,ppc64le/busybox
         );;
