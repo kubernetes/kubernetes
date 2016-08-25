9aefb3821d3939fb801699a49fdd86d042c459f8
Jedrzej Nowak <pigmej@gmail.com>
2016-08-25 12:15:35 +0200

    added auto generated stuff

--- pkg/apis/extensions/v1beta1/types_swagger_doc_generated.go
+++ pkg/apis/extensions/v1beta1/types_swagger_doc_generated.go
@@ -207,7 +207,7 @@ func (HTTPIngressPath) SwaggerDoc() map[string]string {
 }
 
 var map_HTTPIngressRuleValue = map[string]string{
-	"":      "HTTPIngressRuleValue is a list of http selectors pointing to backends. In the example: http://<host>/<path>?<searchpart> -> backend where parts of the url correspond to RFC 3986, this resource will be used to match against everything after the last '/' and before the first '?' or '#'.",
+	"":      "HTTPIngressRuleValue is a list of http selectors pointing to backends. In the example: http://<host>/<path>?<searchpart> -> backend where where parts of the url correspond to RFC 3986, this resource will be used to match against everything after the last '/' and before the first '?' or '#'.",
 	"paths": "A collection of paths that map requests to backends.",
 }
 
