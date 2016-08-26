86c6ce3b8863914dfd794a2a8e03119a2502d64c
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-08-26 15:47:50 +0200

    Deploy `kube-discovery` and the secret for it

--- pkg/kubeadm/master/discovery.go
+++ pkg/kubeadm/master/discovery.go
@@ -9,6 +9,11 @@ import (
 
 	"github.com/square/go-jose"
 
+	"k8s.io/kubernetes/pkg/api"
+	unversionedapi "k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	//"k8s.io/kubernetes/pkg/client/unversioned"
 	kubeadmapi "k8s.io/kubernetes/pkg/kubeadm/api"
 )
 
@@ -51,4 +56,83 @@ func NewDiscoveryEndpoint(params *kubeadmapi.BootstrapParams, caCert string) {
 	log.Printf("Shared secret is %q", params.Discovery.BearerToken)
 	log.Fatal(http.Serve(listener, nil))
 
+}
+
+func CreateDiscoveryDeploymentAndSecret(params *kubeadmapi.BootstrapParams, client *clientset.Clientset, caCert string) error {
+	//TODO create the secret
+	//zero := int64(0)
+	l := map[string]string{"name": "kube-discovery"}
+	kubediscoveryDeployment := &extensions.Deployment{
+		ObjectMeta: api.ObjectMeta{Name: "kube-discovery"},
+		Spec: extensions.DeploymentSpec{
+			Replicas: 1,
+			Selector: &unversionedapi.LabelSelector{MatchLabels: l},
+			Template: api.PodTemplateSpec{
+				ObjectMeta: api.ObjectMeta{Labels: l},
+				Spec: api.PodSpec{
+					//TerminationGracePeriodSeconds: &zero,
+					SecurityContext: &api.PodSecurityContext{HostNetwork: true},
+					Containers: []api.Container{
+						{
+							Name:    "kube-discovery",
+							Image:   HYPERKUBE_IMAGE,
+							Command: []string{"/usr/bin/kube-discovery"},
+							VolumeMounts: []api.VolumeMount{
+								{
+									Name:      "clusterinfosecret",
+									MountPath: "/tmp/secret", // TODO use a shared constant
+									ReadOnly:  true,
+								},
+							},
+						},
+					},
+					Volumes: []api.Volume{
+						{
+							Name: "clusterinfosecret",
+							VolumeSource: api.VolumeSource{
+								Secret: &api.SecretVolumeSource{
+									SecretName: "clusterinfo",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// TODO ListenIP is probably not the right now, although it's best we have right now
+	// if user provides a DNS name, or anything else, we should use that, may be it's really
+	// the list of all SANs (minus internal DNS names and service IP)?
+	endpointList := []string{fmt.Sprintf("https://%s:443", params.Discovery.ListenIP)}
+
+	tokenMap := map[string]string{"todo": params.Discovery.BearerToken} // TODO should use dot-separated tokens
+
+	secretData := map[string][]byte{
+		"ca.pem": []byte(caCert),
+	}
+	var err error
+	secretData["endpoint-list.json"], err = json.MarshalIndent(endpointList, "", "  ")
+	if err != nil {
+		return err
+	}
+	secretData["token-map.json"], err = json.MarshalIndent(tokenMap, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	kubediscoverySecret := &api.Secret{
+		ObjectMeta: api.ObjectMeta{Name: "clusterinfo"},
+		Data:       secretData,
+		Type:       api.SecretTypeOpaque,
+	}
+
+	if _, err := client.Extensions().Deployments(api.NamespaceSystem).Create(kubediscoveryDeployment); err != nil {
+		return err
+	}
+	if _, err := client.Secrets(api.NamespaceSystem).Create(kubediscoverySecret); err != nil {
+		return err
+	}
+
+	return nil
 }
