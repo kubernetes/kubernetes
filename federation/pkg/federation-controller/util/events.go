7d58432aaa88fbcc286b67a3bb8362da01925adf
Marcin Wielgus <mwielgus@google.com>
2016-08-26 17:25:59 +0200

    EventSink for federated controllers

---
+++ federation/pkg/federation-controller/util/events.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	federation_release_1_4 "k8s.io/kubernetes/federation/client/clientset_generated/federation_release_1_4"
+	api "k8s.io/kubernetes/pkg/api"
+	api_v1 "k8s.io/kubernetes/pkg/api/v1"
+)
+
+// Implemnts k8s.io/kubernetes/pkg/client/record.EventSink.
+// type EventSink interface {
+//	 Create(event *api.Event) (*api.Event, error)
+//	 Update(event *api.Event) (*api.Event, error)
+//	 Patch(oldEvent *api.Event, data []byte) (*api.Event, error)
+// }
+type FederatedEventSink struct {
+	clientset federation_release_1_4.Interface
+}
+
+func NewFederatedEventSink(clientset federation_release_1_4.Interface) *FederatedEventSink {
+	return &FederatedEventSink{
+		clientset: clientset,
+	}
+}
+
+func (fes *FederatedEventSink) Create(event *api.Event) (*api.Event, error) {
+	return fes.executeOperation(event, func(eventV1 *api_v1.Event) (*api_v1.Event, error) {
+		return fes.clientset.Core().Events(event.Namespace).Create(eventV1)
+	})
+}
+
+func (fes *FederatedEventSink) Update(event *api.Event) (*api.Event, error) {
+	return fes.executeOperation(event, func(eventV1 *api_v1.Event) (*api_v1.Event, error) {
+		return fes.clientset.Core().Events(event.Namespace).Update(eventV1)
+	})
+}
+
+func (fes *FederatedEventSink) Patch(event *api.Event, data []byte) (*api.Event, error) {
+	return fes.executeOperation(event, func(eventV1 *api_v1.Event) (*api_v1.Event, error) {
+		return fes.clientset.Core().Events(event.Namespace).Patch(event.Name, api.StrategicMergePatchType, data)
+	})
+}
+
+func (fes *FederatedEventSink) executeOperation(event *api.Event, operation func(*api_v1.Event) (*api_v1.Event, error)) (*api.Event, error) {
+	var versionedEvent api_v1.Event
+	if err := api.Scheme.Convert(event, &versionedEvent, nil); err != nil {
+		return nil, err
+	}
+	versionedEventPtr, err := operation(&versionedEvent)
+	if err != nil {
+		return nil, err
+	}
+	var unversionedEvent api.Event
+	if err := api.Scheme.Convert(versionedEventPtr, &unversionedEvent, nil); err != nil {
+		return nil, err
+	}
+	return &unversionedEvent, nil
+}
---
+++ federation/pkg/federation-controller/util/events_test.go
@@ -0,0 +1,84 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"testing"
+	"time"
+
+	fake_federation_release_1_4 "k8s.io/kubernetes/federation/client/clientset_generated/federation_release_1_4/fake"
+	api "k8s.io/kubernetes/pkg/api"
+	api_v1 "k8s.io/kubernetes/pkg/api/v1"
+	"k8s.io/kubernetes/pkg/client/testing/core"
+	"k8s.io/kubernetes/pkg/runtime"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEventSink(t *testing.T) {
+	fakeFederationClient := &fake_federation_release_1_4.Clientset{}
+	createdChan := make(chan runtime.Object, 100)
+	fakeFederationClient.AddReactor("create", "events", func(action core.Action) (bool, runtime.Object, error) {
+		createAction := action.(core.CreateAction)
+		obj := createAction.GetObject()
+		createdChan <- obj
+		return true, obj, nil
+	})
+	updateChan := make(chan runtime.Object, 100)
+	fakeFederationClient.AddReactor("update", "events", func(action core.Action) (bool, runtime.Object, error) {
+		updateAction := action.(core.UpdateAction)
+		obj := updateAction.GetObject()
+		updateChan <- obj
+		return true, obj, nil
+	})
+
+	event := api.Event{
+		ObjectMeta: api.ObjectMeta{
+			Name:      "bzium",
+			Namespace: "ns",
+		},
+	}
+	sink := NewFederatedEventSink(fakeFederationClient)
+	eventUpdated, err := sink.Create(&event)
+	assert.NoError(t, err)
+	eventV1 := GetObjectFromChan(createdChan).(*api_v1.Event)
+	assert.NotNil(t, eventV1)
+	// Just some simple sanity checks.
+	assert.Equal(t, event.Name, eventV1.Name)
+	assert.Equal(t, event.Name, eventUpdated.Name)
+
+	eventUpdated, err = sink.Update(&event)
+	assert.NoError(t, err)
+	eventV1 = GetObjectFromChan(updateChan).(*api_v1.Event)
+	assert.NotNil(t, eventV1)
+	// Just some simple sanity checks.
+	assert.Equal(t, event.Name, eventV1.Name)
+	assert.Equal(t, event.Name, eventUpdated.Name)
+}
+
+// GetObjectFromChan tries to get an api object from the given channel
+// within a reasonable time (1 min).
+// TODO: move this and version from util/test elsewhere. Util tests cannot use util/tests because
+// a cycle is created.
+func GetObjectFromChan(c chan runtime.Object) runtime.Object {
+	select {
+	case obj := <-c:
+		return obj
+	case <-time.After(time.Minute):
+		return nil
+	}
+}
