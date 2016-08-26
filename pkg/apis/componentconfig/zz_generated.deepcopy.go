895d9d689e42e8b7e17e27c048c1e7e2071f2a02
Michael Taufen <mtaufen@google.com>
2016-08-26 15:06:07 -0700

    Add FeatureGates field to KubeletConfiguration

--- pkg/apis/componentconfig/zz_generated.deepcopy.go
+++ pkg/apis/componentconfig/zz_generated.deepcopy.go
@@ -344,6 +344,7 @@ func DeepCopy_componentconfig_KubeletConfiguration(in interface{}, out interface
 		} else {
 			out.AllowedUnsafeSysctls = nil
 		}
+		out.FeatureGates = in.FeatureGates
 		return nil
 	}
 }
