98174737df7763a0779101a8bce98159c9644967
Wojciech Tyczynski <wojtekt@google.com>
2016-08-29 09:28:44 +0200

    Increase cache size for RCs

--- pkg/registry/cachesize/cachesize.go
+++ pkg/registry/cachesize/cachesize.go
@@ -85,6 +85,7 @@ func InitializeWatchCacheSizes(expectedRAMCapacityMB int) {
 	// is supposed to have non-default value.
 	//
 	// TODO: Figure out which resource we should have non-default value.
+	watchCacheSizes[Controllers] = maxInt(5*clusterSize, 100)
 	watchCacheSizes[Endpoints] = maxInt(10*clusterSize, 1000)
 	watchCacheSizes[Nodes] = maxInt(3*clusterSize, 1000)
 	watchCacheSizes[Pods] = maxInt(10*clusterSize, 1000)
