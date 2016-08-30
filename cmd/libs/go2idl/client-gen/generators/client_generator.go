2351cc81cd05047d2d66da19f34d51bb2ebbc3a9
Daniel Smith <dbsmith@google.com>
2016-08-30 10:12:09 -0700

    rewrite import paths

--- cmd/libs/go2idl/client-gen/generators/client_generator.go
+++ cmd/libs/go2idl/client-gen/generators/client_generator.go
@@ -22,13 +22,13 @@ import (
 	"path/filepath"
 	"strings"
 
-	"k8s.io/kubernetes/cmd/libs/go2idl/args"
+	"k8s.io/gengo/args"
 	clientgenargs "k8s.io/kubernetes/cmd/libs/go2idl/client-gen/args"
 	"k8s.io/kubernetes/cmd/libs/go2idl/client-gen/generators/fake"
 	"k8s.io/kubernetes/cmd/libs/go2idl/client-gen/generators/normalization"
-	"k8s.io/kubernetes/cmd/libs/go2idl/generator"
-	"k8s.io/kubernetes/cmd/libs/go2idl/namer"
-	"k8s.io/kubernetes/cmd/libs/go2idl/types"
+	"k8s.io/gengo/generator"
+	"k8s.io/gengo/namer"
+	"k8s.io/gengo/types"
 	"k8s.io/kubernetes/pkg/api/unversioned"
 
 	"github.com/golang/glog"
