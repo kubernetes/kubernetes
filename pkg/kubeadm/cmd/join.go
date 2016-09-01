8b9f513847b3678c0a372c7e2716da017d9f2b8c
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-09-01 10:40:44 +0100

    More cleanup in cmds

--- pkg/kubeadm/cmd/join.go
+++ pkg/kubeadm/cmd/join.go
@@ -26,6 +26,18 @@ import (
 	kubeadmapi "k8s.io/kubernetes/pkg/kubeadm/api"
 	kubenode "k8s.io/kubernetes/pkg/kubeadm/node"
 	kubeadmutil "k8s.io/kubernetes/pkg/kubeadm/util"
+	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+)
+
+var (
+	join_done_msgf = dedent.Dedent(`
+		Node join complete:
+		* Certificate signing request sent to master and response
+		  received.
+		* Kubelet informed of new secure connection details.
+
+		Run 'kubectl get nodes' on the master to see this node join.
+		`)
 )
 
 func NewCmdJoin(out io.Writer, params *kubeadmapi.BootstrapParams) *cobra.Command {
@@ -33,40 +45,8 @@ func NewCmdJoin(out io.Writer, params *kubeadmapi.BootstrapParams) *cobra.Comman
 		Use:   "join",
 		Short: "Run this on other servers to join an existing cluster.",
 		Run: func(cmd *cobra.Command, args []string) {
-			ok, err := kubeadmutil.UseGivenTokenIfValid(params)
-			if !ok {
-				if err != nil {
-					out.Write([]byte(fmt.Sprintf("%s (see --help)\n", err)))
-					return
-				}
-				out.Write([]byte(fmt.Sprintf("Must specify --token (see --help)\n")))
-				return
-			}
-			if params.Discovery.ApiServerURLs == "" {
-				out.Write([]byte(fmt.Sprintf("Must specify --api-server-urls (see --help)\n")))
-				return
-			}
-			kubeconfig, err := kubenode.RetrieveTrustedClusterInfo(params)
-			if err != nil {
-				out.Write([]byte(fmt.Sprintf("Failed to bootstrap: %s\n", err)))
-				return
-			}
-			//fmt.Println("recieved signed certificate from the API server, will write `/etc/kubernetes/kubelet.conf`...")
-
-			err = kubeadmutil.WriteKubeconfigIfNotExists(params, "kubelet", kubeconfig)
-			if err != nil {
-				out.Write([]byte(fmt.Sprintf("Unable to write config for node:\n%s\n", err)))
-				return
-			}
-			out.Write([]byte(dedent.Dedent(`
-				Node join complete:
-				* Certificate signing request sent to master and response
-				  received.
-				* Kubelet informed of new secure connection details.
-
-				Run 'kubectl get nodes' on the master to see this node join.
-
-			`)))
+			err := RunJoin(out, cmd, args, params)
+			cmdutil.CheckErr(err)
 		},
 	}
 
@@ -78,4 +58,30 @@ func NewCmdJoin(out io.Writer, params *kubeadmapi.BootstrapParams) *cobra.Comman
 		`Shared secret used to secure bootstrap. Must match output of 'init-master'.`)
 
 	return cmd
+}
+
+func RunJoin(out io.Writer, cmd *cobra.Command, args []string, params *kubeadmapi.BootstrapParams) error {
+	ok, err := kubeadmutil.UseGivenTokenIfValid(params)
+	if !ok {
+		if err != nil {
+			return fmt.Errorf("%s (see --help)\n", err)
+		}
+		return fmt.Errorf("Must specify --token (see --help)\n")
+	}
+	if params.Discovery.ApiServerURLs == "" {
+		return fmt.Errorf("Must specify --api-server-urls (see --help)\n")
+	}
+
+	kubeconfig, err := kubenode.RetrieveTrustedClusterInfo(params)
+	if err != nil {
+		return fmt.Errorf("Failed to bootstrap: %s\n", err)
+	}
+
+	err = kubeadmutil.WriteKubeconfigIfNotExists(params, "kubelet", kubeconfig)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(out, join_done_msgf)
+	return nil
 }
