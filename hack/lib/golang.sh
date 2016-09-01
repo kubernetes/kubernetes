f1c1a38788a71c4bbc950957aaed1a5339becb69
Joe Beda <joe.github@bedafamily.com>
2016-09-01 12:34:49 -0700

    Install test subpackages too
    We weren't getting incremental builds because of new test only subpackages.  Our
    voodoo combo of 'go install' and 'go test -c' didn't cache things like
    'test/e2e_node/services'.  Add the '-i' flag to 'go test' to install test only
    dependencies too.

--- hack/lib/golang.sh
+++ hack/lib/golang.sh
@@ -543,7 +543,7 @@ kube::golang::build_binaries_for_platform() {
         "${testpkg}"
 
     mkdir -p "$(dirname ${outfile})"
-    go test -c \
+    go test -i -c \
       "${goflags[@]:+${goflags[@]}}" \
       -gcflags "${gogcflags}" \
       -ldflags "${goldflags}" \
