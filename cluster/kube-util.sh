0c562bac48f139bdd4c83928a018705ad48a6585
Colin Hom <colin.hom@coreos.com>
2016-09-01 13:02:10 -0700

    federation: aws compatibility for cluster and e2e frameworks

--- cluster/kube-util.sh
+++ cluster/kube-util.sh
@@ -38,27 +38,34 @@ fi
 # Should NOT be called within the global scope, unless setting the desired global zone vars
 # This function is currently NOT USED in the global scope
 function set-federation-zone-vars {
-    zone="$1"
-    export OVERRIDE_CONTEXT="federation-e2e-${KUBERNETES_PROVIDER}-$zone"
-    echo "Setting zone vars to: $OVERRIDE_CONTEXT"
-    if [[ "$KUBERNETES_PROVIDER" == "gce"  ]];then
+  zone="$1"
+  export OVERRIDE_CONTEXT="federation-e2e-${KUBERNETES_PROVIDER}-$zone"
+  echo "Setting zone vars to: $OVERRIDE_CONTEXT"
+  if [[ "$KUBERNETES_PROVIDER" == "gce"  ]];then
 
-	export KUBE_GCE_ZONE="$zone"
-	# gcloud has a 61 character limit, and for firewall rules this
-	# prefix gets appended to itself, with some extra information
-	# need tot keep it short
-	export KUBE_GCE_INSTANCE_PREFIX="${USER}-${zone}"
+    export KUBE_GCE_ZONE="$zone"
+    # gcloud has a 61 character limit, and for firewall rules this
+    # prefix gets appended to itself, with some extra information
+    # need tot keep it short
+    export KUBE_GCE_INSTANCE_PREFIX="${USER}-${zone}"
 
-    elif [[ "$KUBERNETES_PROVIDER" == "gke"  ]];then
+  elif [[ "$KUBERNETES_PROVIDER" == "gke"  ]];then
 
-	export CLUSTER_NAME="${USER}-${zone}"
+    export CLUSTER_NAME="${USER}-${zone}"
 
-    elif [[ "$KUBERNETES_PROVIDER" == "aws"  ]];then
+  elif [[ "$KUBERNETES_PROVIDER" == "aws"  ]];then
 
-	export KUBE_AWS_ZONE="$zone"
-	export KUBE_AWS_INSTANCE_PREFIX="${USER}-${zone}"
-    else
-	echo "Provider \"${KUBERNETES_PROVIDER}\" is not supported"
-	exit 1
-    fi
+    export KUBE_AWS_ZONE="$zone"
+    export KUBE_AWS_INSTANCE_PREFIX="${USER}-${zone}"
+
+    # WARNING: This is hack
+    # After KUBE_AWS_INSTANCE_PREFIX is changed,
+    # we need to make sure the config-xxx.sh file is
+    # re-sourced so the change propogates to dependent computed values
+    # (eg: MASTER_SG_NAME, NODE_SG_NAME, etc)
+    source "${KUBE_ROOT}/cluster/aws/util.sh"
+  else
+    echo "Provider \"${KUBERNETES_PROVIDER}\" is not supported"
+    exit 1
+  fi
 }
