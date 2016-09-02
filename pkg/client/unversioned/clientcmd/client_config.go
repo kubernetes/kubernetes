52c63bc8c3aff8bec9cb78377eb644cfc0ccbe44
Clayton Coleman <ccoleman@redhat.com>
2016-09-02 01:53:43 -0400

    Check for EmptyConfig errors when trying to use in-cluster config
    By removing the default "localhost:8080" behavior several paths in
    client config began returning err == ErrEmptyConfig rather than err ==
    nil.  The code checking for in cluster config was wrong - the logic
    should be:
    
    1. If loading the underlying config returns a non-empty error, fail
    2. If the underlying config is not equal to the default config,
       return that config (it's got user input)
    3. If it is possible to use in cluster config, do so
    4. Otherwise return the default config (and or default EmptyConfig
       error).

--- pkg/client/unversioned/clientcmd/client_config.go
+++ pkg/client/unversioned/clientcmd/client_config.go
@@ -358,21 +358,21 @@ func (inClusterClientConfig) ClientConfig() (*restclient.Config, error) {
 	return restclient.InClusterConfig()
 }
 
-func (inClusterClientConfig) Namespace() (string, error) {
+func (inClusterClientConfig) Namespace() (string, bool, error) {
 	// This way assumes you've set the POD_NAMESPACE environment variable using the downward API.
 	// This check has to be done first for backwards compatibility with the way InClusterConfig was originally set up
 	if ns := os.Getenv("POD_NAMESPACE"); ns != "" {
-		return ns, nil
+		return ns, true, nil
 	}
 
 	// Fall back to the namespace associated with the service account token, if available
 	if data, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err == nil {
 		if ns := strings.TrimSpace(string(data)); len(ns) > 0 {
-			return ns, nil
+			return ns, true, nil
 		}
 	}
 
-	return "default", nil
+	return "default", false, nil
 }
 
 func (inClusterClientConfig) ConfigAccess() ConfigAccess {
