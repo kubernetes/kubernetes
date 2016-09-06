56476ab7e4916308659e1a45787fe796e81ba837
Angus Salkeld <asalkeld@mirantis.com>
2016-09-06 11:42:30 +1000

    Simplify the output in AfterPrint()
    - Change FinishPrint() to AfterPrint()
      (As suggested in #31160)
    - add a newline to separate it from the table
    - remove the "objects" and ()
    - assume plural

--- pkg/kubectl/cmd/cmd_test.go
+++ pkg/kubectl/cmd/cmd_test.go
@@ -166,7 +166,7 @@ func (t *testPrinter) HandledResources() []string {
 	return []string{}
 }
 
-func (t *testPrinter) FinishPrint(output io.Writer, res string) error {
+func (t *testPrinter) AfterPrint(output io.Writer, res string) error {
 	return nil
 }
 
