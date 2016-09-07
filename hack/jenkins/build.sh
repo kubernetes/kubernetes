edc28d4346dbad2cb26945edd38c0b49f6cd2102
David McMahon <djmm@google.com>
2016-09-07 14:59:47 -0700

    Change push-ci-build.sh to push-build.sh and some cleanup.

--- hack/jenkins/build.sh
+++ hack/jenkins/build.sh
@@ -39,15 +39,7 @@ export CLOUDSDK_COMPONENT_MANAGER_DISABLE_UPDATE_CHECK=true
 # FEDERATION?
 : ${FEDERATION:="false"}
 : ${KUBE_RELEASE_RUN_TESTS:="n"}
-
-# New kubernetes/release/push-ci-build.sh values
-# RELEASE_INFRA_PUSH=true when we're using kubernetes/release/push-ci-build.sh
-: ${RELEASE_INFRA_PUSH:="false"}
-# SET_NOMOCK_FLAG=true means we're doing full pushes and we pass --nomock to 
-# push-ci-build.sh. This is set to false in the
-# testing jobs and only used in the RELEASE_INFRA_PUSH=true scope below.
-: ${SET_NOMOCK_FLAG:="true"}
-export KUBE_RELEASE_RUN_TESTS RELEASE_INFRA_PUSH FEDERATION SET_NOMOCK_FLAG
+export KUBE_RELEASE_RUN_TESTS
 
 # Clean stuff out. Assume the last build left the tree in an odd
 # state.
@@ -72,19 +64,19 @@ else
   mkdir -p ${WORKSPACE}/_tmp
   git clone https://github.com/kubernetes/release ${release_infra_clone}
 
-  if [[ ! -x ${release_infra_clone}/push-ci-build.sh ]]; then
-    echo "FATAL: Something went wrong." \
-         "${release_infra_clone}/push-ci-build.sh isn't available." \
-         "Exiting..." >&2
-    exit 1
-  fi
+  push_build=${release_infra_clone}/push-build.sh
 
+  if [[ ! -x ${push_build} ]]; then
+    # TODO: Remove/Restore this with the full deprecation PR
+    push_build=${release_infra_clone}/push-ci-build.sh
+    #echo "FATAL: Something went wrong. ${push_build} isn't available." \
+    #     "Exiting..." >&2
+    #exit 1
+  fi
   [[ -n "${KUBE_GCS_RELEASE_BUCKET-}" ]] \
    && bucket_flag="--bucket=${KUBE_GCS_RELEASE_BUCKET-}"
   ${FEDERATION} && federation_flag="--federation"
-  ${SET_NOMOCK_FLAG} && mock_flag="--nomock"
-  ${release_infra_clone}/push-ci-build.sh ${bucket_flag-} ${federation_flag-} \
-                                          ${mock_flag-} --verbose
+  ${push_build} ${bucket_flag-} ${federation_flag-} --nomock --verbose --ci
 fi
 
 sha256sum _output/release-tars/kubernetes*.tar.gz
