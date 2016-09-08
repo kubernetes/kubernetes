029581853ee6e539354ee90209b77e89d232e544
Andreas John <himself@derjohn.de>
2016-09-08 17:06:54 +0200

    [cluster/addons] Update elasticsearch to version 2.4 and switch from Ubuntu base image to a apline base.

--- cluster/addons/fluentd-elasticsearch/es-image/run.sh
+++ cluster/addons/fluentd-elasticsearch/es-image/run.sh
@@ -14,9 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+export ES_VERSION='2.4.0'
 export NODE_MASTER=${NODE_MASTER:-true}
 export NODE_DATA=${NODE_DATA:-true}
-/elasticsearch_logging_discovery >> /elasticsearch-1.5.2/config/elasticsearch.yml
+export NODE_ENDPOINTS=$(curl -k -s -L --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/peng/endpoints?labelSelector="k8s-app=elasticsearch-logging" | jq -r '.items[].subsets[].addresses | map(.ip) | join(",")')
+echo "Discovered initial Elasticsearch Node Endpoints: $NODE_ENDPOINTS"
 export HTTP_PORT=${HTTP_PORT:-9200}
 export TRANSPORT_PORT=${TRANSPORT_PORT:-9300}
-/elasticsearch-1.5.2/bin/elasticsearch
+/elasticsearch-${ES_VERSION}/bin/elasticsearch -Des.insecure.allow.root=true
