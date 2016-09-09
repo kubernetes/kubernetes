b57d1c437c9b22802fff53a8e9a8b45374546e7e
Clayton Coleman <ccoleman@redhat.com>
2016-09-09 01:09:14 -0400

    UPSTREAM: 32353: Doc page for scheduler is not stable (for man pages)
    Need to sort maps

--- plugin/pkg/scheduler/factory/plugins.go
+++ plugin/pkg/scheduler/factory/plugins.go
@@ -19,6 +19,7 @@ package factory
 import (
 	"fmt"
 	"regexp"
+	"sort"
 	"strings"
 	"sync"
 
@@ -317,5 +318,6 @@ func ListAlgorithmProviders() string {
 	for name := range algorithmProviderMap {
 		availableAlgorithmProviders = append(availableAlgorithmProviders, name)
 	}
+	sort.Strings(availableAlgorithmProviders)
 	return strings.Join(availableAlgorithmProviders, " | ")
 }
