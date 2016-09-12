f12fb7ff0a7d760eb8a6363a841ce208901b9595
Chao Xu <xuchao@google.com>
2016-09-12 16:10:19 -0700

    set --alsologtostderr=true in integration test

--- hack/make-rules/test-integration.sh
+++ hack/make-rules/test-integration.sh
@@ -62,7 +62,7 @@ runTests() {
   make -C "${KUBE_ROOT}" test \
       WHAT="$(kube::test::find_integration_test_dirs ${2-} | paste -sd' ' -)" \
       KUBE_GOFLAGS="${KUBE_GOFLAGS:-} -tags 'integration no-docker'" \
-      KUBE_TEST_ARGS="--vmodule=garbage*collector=6" \
+      KUBE_TEST_ARGS="${KUBE_TEST_ARGS:-} --vmodule=garbage*collector*=6 --alsologtostderr=true" \
       KUBE_RACE="" \
       KUBE_TIMEOUT="${KUBE_TIMEOUT}" \
       KUBE_TEST_API_VERSIONS="$1"
