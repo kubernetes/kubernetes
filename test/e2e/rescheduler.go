719d35e46ee27b2f416f966b22512df101d5d0d6
Piotr Szczesniak <pszczesniak@google.com>
2016-09-12 17:25:13 +0200

    Revert "Enabled Rescheduler e2e for gke"

--- test/e2e/rescheduler.go
+++ test/e2e/rescheduler.go
@@ -35,7 +35,7 @@ var _ = framework.KubeDescribe("Rescheduler [Serial]", func() {
 	var totalMillicores int
 
 	BeforeEach(func() {
-		framework.SkipUnlessProviderIs("gce", "gke")
+		framework.SkipUnlessProviderIs("gce")
 		ns = f.Namespace.Name
 		nodes := framework.GetReadySchedulableNodesOrDie(f.Client)
 		nodeCount := len(nodes.Items)
