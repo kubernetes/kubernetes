642228102e03f2024ed48264ed3ba0d566583b4d
Maciej Szulik <maszulik@redhat.com>
2016-09-13 11:24:01 +0200

    Move ScheduledJob controller to use generated clientset

--- pkg/controller/scheduledjob/controller.go
+++ pkg/controller/scheduledjob/controller.go
@@ -38,8 +38,9 @@ import (
 	"k8s.io/kubernetes/pkg/api/errors"
 	"k8s.io/kubernetes/pkg/api/unversioned"
 	"k8s.io/kubernetes/pkg/apis/batch"
+	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	unversionedcore "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/typed/core/unversioned"
 	"k8s.io/kubernetes/pkg/client/record"
-	client "k8s.io/kubernetes/pkg/client/unversioned"
 	"k8s.io/kubernetes/pkg/controller/job"
 	"k8s.io/kubernetes/pkg/runtime"
 	utilerrors "k8s.io/kubernetes/pkg/util/errors"
@@ -51,21 +52,21 @@ import (
 // Utilities for dealing with Jobs and ScheduledJobs and time.
 
 type ScheduledJobController struct {
-	kubeClient *client.Client
+	kubeClient clientset.Interface
 	jobControl jobControlInterface
 	sjControl  sjControlInterface
 	podControl podControlInterface
 	recorder   record.EventRecorder
 }
 
-func NewScheduledJobController(kubeClient *client.Client) *ScheduledJobController {
+func NewScheduledJobController(kubeClient clientset.Interface) *ScheduledJobController {
 	eventBroadcaster := record.NewBroadcaster()
 	eventBroadcaster.StartLogging(glog.Infof)
 	// TODO: remove the wrapper when every clients have moved to use the clientset.
-	eventBroadcaster.StartRecordingToSink(kubeClient.Events(""))
+	eventBroadcaster.StartRecordingToSink(&unversionedcore.EventSinkImpl{Interface: kubeClient.Core().Events("")})
 
-	if kubeClient != nil && kubeClient.GetRateLimiter() != nil {
-		metrics.RegisterMetricAndTrackRateLimiterUsage("scheduledjob_controller", kubeClient.GetRateLimiter())
+	if kubeClient != nil && kubeClient.Core().GetRESTClient().GetRateLimiter() != nil {
+		metrics.RegisterMetricAndTrackRateLimiterUsage("scheduledjob_controller", kubeClient.Core().GetRESTClient().GetRateLimiter())
 	}
 
 	jm := &ScheduledJobController{
@@ -73,13 +74,13 @@ func NewScheduledJobController(kubeClient *client.Client) *ScheduledJobControlle
 		jobControl: realJobControl{KubeClient: kubeClient},
 		sjControl:  &realSJControl{KubeClient: kubeClient},
 		podControl: &realPodControl{KubeClient: kubeClient},
-		recorder:   eventBroadcaster.NewRecorder(api.EventSource{Component: "scheduled-job-controller"}),
+		recorder:   eventBroadcaster.NewRecorder(api.EventSource{Component: "scheduledjob-controller"}),
 	}
 
 	return jm
 }
 
-func NewScheduledJobControllerFromClient(kubeClient *client.Client) *ScheduledJobController {
+func NewScheduledJobControllerFromClient(kubeClient clientset.Interface) *ScheduledJobController {
 	jm := NewScheduledJobController(kubeClient)
 	return jm
 }
