50e20129032cddedc866c41c3d2947293e7943f0
Maciej Szulik <maszulik@redhat.com>
2016-09-14 11:08:58 +0200

    Generated changes for clientset

--- pkg/client/clientset_generated/internalclientset/typed/extensions/unversioned/extensions_client.go
+++ pkg/client/clientset_generated/internalclientset/typed/extensions/unversioned/extensions_client.go
@@ -109,12 +109,10 @@ func setConfigDefaults(config *restclient.Config) error {
 	if config.UserAgent == "" {
 		config.UserAgent = restclient.DefaultKubernetesUserAgent()
 	}
-	// TODO: Unconditionally set the config.Version, until we fix the config.
-	//if config.Version == "" {
-	copyGroupVersion := g.GroupVersion
-	config.GroupVersion = &copyGroupVersion
-	//}
-
+	if config.GroupVersion == nil || config.GroupVersion.Group != g.GroupVersion.Group {
+		copyGroupVersion := g.GroupVersion
+		config.GroupVersion = &copyGroupVersion
+	}
 	config.NegotiatedSerializer = api.Codecs
 
 	if config.QPS == 0 {
