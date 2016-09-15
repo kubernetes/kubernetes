274a40fcd6d5db47324c669ef01e3f96325ad1c3
mayman <mayman_bg@hotmail.com>
2016-09-15 15:09:56 +0100

    Adding the option for the system to use the HOME env for windows.

--- pkg/util/homedir/homedir.go
+++ pkg/util/homedir/homedir.go
@@ -23,7 +23,7 @@ import (
 
 // HomeDir returns the home directory for the current user
 func HomeDir() string {
-	if runtime.GOOS == "windows" {
+	if runtime.GOOS == "windows"; len(os.Getenv("HOME")) <= 0) {
 		if homeDrive, homePath := os.Getenv("HOMEDRIVE"), os.Getenv("HOMEPATH"); len(homeDrive) > 0 && len(homePath) > 0 {
 			homeDir := homeDrive + homePath
 			if _, err := os.Stat(homeDir); err == nil {
