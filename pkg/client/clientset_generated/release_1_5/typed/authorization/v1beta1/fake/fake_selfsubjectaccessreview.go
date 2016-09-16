7a82ebbd551d8ef0095f2678903ea48c18756ed0
mbohlool <mehdy@google.com>
2016-09-16 00:44:02 -0700

    Other generated stuff, not related

--- pkg/client/clientset_generated/release_1_5/typed/authorization/v1beta1/fake/fake_selfsubjectaccessreview.go
+++ pkg/client/clientset_generated/release_1_5/typed/authorization/v1beta1/fake/fake_selfsubjectaccessreview.go
@@ -14,6 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1beta1
+package fake
 
-type SelfSubjectAccessReviewExpansion interface{}
+// FakeSelfSubjectAccessReviews implements SelfSubjectAccessReviewInterface
+type FakeSelfSubjectAccessReviews struct {
+	Fake *FakeAuthorization
+}
