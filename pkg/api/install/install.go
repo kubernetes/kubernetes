4aa61ddcb2ad486078c48775b2b4d64e184c07b2
deads2k <deads@redhat.com>
2016-09-16 16:29:25 -0400

    remove dead mapper

--- pkg/api/install/install.go
+++ pkg/api/install/install.go
@@ -82,7 +82,6 @@ func enableVersions(externalVersions []unversioned.GroupVersion) error {
 	if err := registered.RegisterGroup(groupMeta); err != nil {
 		return err
 	}
-	api.RegisterRESTMapper(groupMeta.RESTMapper)
 	return nil
 }
 
