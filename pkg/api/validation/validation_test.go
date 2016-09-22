f5578f326814ad16ecba83433485cce123c14e0d
Maciej Kwiek <mkwiek@mirantis.com>
2016-09-22 16:07:47 +0200

    Validate None Service ClusterIP against LB type
    If the Service is a Load Balancer, it should not have None Cluster IP.
    If it does, Service validation fails.

--- pkg/api/validation/validation_test.go
+++ pkg/api/validation/validation_test.go
@@ -5148,6 +5148,14 @@ func TestValidateService(t *testing.T) {
 			},
 			numErrs: 1,
 		},
+		{
+			name: "LoadBalancer type cannot have None ClusterIP",
+			tweakSvc: func(s *api.Service) {
+				s.Spec.ClusterIP = "None"
+				s.Spec.Type = api.ServiceTypeLoadBalancer
+			},
+			numErrs: 1,
+		},
 	}
 
 	for _, tc := range testCases {
@@ -6387,6 +6395,14 @@ func TestValidateServiceUpdate(t *testing.T) {
 				newSvc.Spec.Type = api.ServiceTypeNodePort
 			},
 			numErrs: 0,
+		},
+		{
+			name: "LoadBalancer type cannot have None ClusterIP",
+			tweakSvc: func(oldSvc, newSvc *api.Service) {
+				newSvc.Spec.ClusterIP = "None"
+				newSvc.Spec.Type = api.ServiceTypeLoadBalancer
+			},
+			numErrs: 1,
 		},
 	}
 
