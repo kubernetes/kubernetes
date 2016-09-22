ccb8d53a399fc4bb8b8d8b6d1b41080c6bd50f38
Kiall Mac Innes <kiall@hpe.com>
2016-09-22 12:27:25 +0100

    Remove unused WaitForDetach from Detacher interface and plugins
    This has been unused since 542f2dc7, and relies on deviceName, which
    can no longer be relied upon (see issue #33128).
    
    This needs to be removed now, as part of #33128, as the code can't be
    updated to attempt device detection and fallback through to the Cinder
    provided deviceName, as detection "fails" when the device is gone, and
    if cinder has reported a deviceName that another volume has used in
    relaity, then this will block forever (or until the other, unreleated,
    volume has been detached)

--- pkg/volume/vsphere_volume/attacher.go
+++ pkg/volume/vsphere_volume/attacher.go
@@ -251,27 +251,6 @@ func (detacher *vsphereVMDKDetacher) Detach(deviceMountPath string, nodeName typ
 	return nil
 }
 
-func (detacher *vsphereVMDKDetacher) WaitForDetach(devicePath string, timeout time.Duration) error {
-	ticker := time.NewTicker(checkSleepDuration)
-	defer ticker.Stop()
-	timer := time.NewTimer(timeout)
-	defer timer.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			glog.V(5).Infof("Checking device %q is detached.", devicePath)
-			if pathExists, err := volumeutil.PathExists(devicePath); err != nil {
-				return fmt.Errorf("Error checking if device path exists: %v", err)
-			} else if !pathExists {
-				return nil
-			}
-		case <-timer.C:
-			return fmt.Errorf("Timeout reached; Device %v is still attached", devicePath)
-		}
-	}
-}
-
 func (detacher *vsphereVMDKDetacher) UnmountDevice(deviceMountPath string) error {
 	return volumeutil.UnmountPath(deviceMountPath, detacher.mounter)
 }
