a750488447dc4132ee500ae8dd0db8e04251a70b
Clayton Coleman <ccoleman@redhat.com>
2016-09-24 17:25:43 -0400

    Flag all packages as needing defaulters

--- pkg/apis/componentconfig/v1alpha1/doc.go
+++ pkg/apis/componentconfig/v1alpha1/doc.go
@@ -17,5 +17,6 @@ limitations under the License.
 // +k8s:deepcopy-gen=package,register
 // +k8s:conversion-gen=k8s.io/kubernetes/pkg/apis/componentconfig
 // +k8s:openapi-gen=true
+// +k8s:defaulter-gen=TypeMeta
 
 package v1alpha1 // import "k8s.io/kubernetes/pkg/apis/componentconfig/v1alpha1"
