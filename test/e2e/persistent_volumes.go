2d2cb66a70b54e616e42eb072fdaea87e4b60e2c
Jon Cope <jcope@redhat.com>
2016-09-29 16:28:33 -0500

    wrapped entire gce test in It

--- test/e2e/persistent_volumes.go
+++ test/e2e/persistent_volumes.go
@@ -526,7 +526,7 @@ var _ = framework.KubeDescribe("PersistentVolumes", func() {
 			}
 		})
 
-		Context("with Single PV - PVC pairs", func() {
+		XContext("with Single PV - PVC pairs", func() {
 
 			var pv *api.PersistentVolume
 			var pvc *api.PersistentVolumeClaim
@@ -648,66 +648,12 @@ var _ = framework.KubeDescribe("PersistentVolumes", func() {
 	///////////////////////////////////////////////////////////////////////
 	// Testing configurations of single a PV/PVC pair attached to a GCE PD
 	framework.KubeDescribe("PV:GCEPD", func() {
-
-		pv := &api.PersistentVolume{
-			ObjectMeta: api.ObjectMeta{
-				GenerateName: "gce-",
-				Annotations: map[string]string{
-					volumehelper.VolumeGidAnnotationKey: "777",
-				},
-			},
-			Spec: api.PersistentVolumeSpec{
-				PersistentVolumeReclaimPolicy: api.PersistentVolumeReclaimRetain,
-				Capacity: api.ResourceList{
-					api.ResourceName(api.ResourceStorage): resource.MustParse("2Gi"),
-				},
-				PersistentVolumeSource: api.PersistentVolumeSource{
-					GCEPersistentDisk: volume.GCEPersistentDisk,
-				},
-				AccessModes: []api.PersistentVolumeAccessMode{
-					api.ReadWriteOnce,
-					api.ReadOnlyMany,
-				},
-				ClaimRef: &api.ObjectReference{
-					Name:      "",
-					Namespace: "",
-				},
-			},
-		}
-
-		pvc := &api.PersistentVolumeClaim{
-			ObjectMeta: api.ObjectMeta{
-				GenerateName: "gce-pvc-",
-				Namespace:    ns,
-			},
-			Spec: api.PersistentVolumeClaimSpec{
-				AccessModes: []api.PersistentVolumeAccessMode{
-					api.ReadWriteOnce,
-					api.ReadWriteOnce,
-				},
-				Resources: api.ResourceRequirements{
-					Requests: api.ResourceList{
-						api.ResourceName(api.ResourceStorage): resource.MustParse("2Gi"),
-					},
-				},
-			},
-		}
-		config := VolumeTestConfig{
-			namespace: ns,
-			prefix:    "pd",
-		}
-
-		volume := api.VolumeSource{
-			GCEPersistentDisk: &api.GCEPersistentDiskVolumeSource{
-				PDName:   volumeName,
-				FSType:   "ext3",
-				ReadOnly: false,
-			},
-		}
-
 		It("should test that deleting a PVC before the Pod does not cause unmount to fail on pod delete", func() {
-			framework.SkipUnlessProviderIs("gce", "gke")
-
+			//framework.SkipUnlessProviderIs("gce", "gke")
+			config := VolumeTestConfig{
+				namespace: ns,
+				prefix:    "pd",
+			}
 			defer func() {
 				volumeTestCleanup(f, config)
 			}()
@@ -715,8 +661,58 @@ var _ = framework.KubeDescribe("PersistentVolumes", func() {
 			By("creating a test gce pd volume")
 			volumeName, err := createPDWithRetry()
 			Expect(err).NotTo(HaveOccurred())
-
+			volume := api.VolumeSource{
+				GCEPersistentDisk: &api.GCEPersistentDiskVolumeSource{
+					PDName:   volumeName,
+					FSType:   "ext3",
+					ReadOnly: false,
+				},
+			}
 			defer deletePDWithRetry(volumeName)
+
+			pv := &api.PersistentVolume{
+				ObjectMeta: api.ObjectMeta{
+					GenerateName: "gce-",
+					Annotations: map[string]string{
+						volumehelper.VolumeGidAnnotationKey: "777",
+					},
+				},
+				Spec: api.PersistentVolumeSpec{
+					PersistentVolumeReclaimPolicy: api.PersistentVolumeReclaimRetain,
+					Capacity: api.ResourceList{
+						api.ResourceName(api.ResourceStorage): resource.MustParse("2Gi"),
+					},
+					PersistentVolumeSource: api.PersistentVolumeSource{
+						GCEPersistentDisk: volume.GCEPersistentDisk,
+					},
+					AccessModes: []api.PersistentVolumeAccessMode{
+						api.ReadWriteOnce,
+						api.ReadOnlyMany,
+					},
+					ClaimRef: &api.ObjectReference{
+						Name:      "",
+						Namespace: "",
+					},
+				},
+			}
+
+			pvc := &api.PersistentVolumeClaim{
+				ObjectMeta: api.ObjectMeta{
+					GenerateName: "gce-pvc-",
+					Namespace:    ns,
+				},
+				Spec: api.PersistentVolumeClaimSpec{
+					AccessModes: []api.PersistentVolumeAccessMode{
+						api.ReadWriteOnce,
+						api.ReadWriteOnce,
+					},
+					Resources: api.ResourceRequirements{
+						Requests: api.ResourceList{
+							api.ResourceName(api.ResourceStorage): resource.MustParse("2Gi"),
+						},
+					},
+				},
+			}
 
 			By("Creating the PV and PVC")
 			pv, err = c.PersistentVolumes().Create(pv)
