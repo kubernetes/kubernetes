89d647cebc56add2f485a1de98417ab2b990fe82
Mike Danese <mikedanese@google.com>
2016-09-28 23:36:18 -0700

    kubeadm: refactor config
    1) break object into substructures
    2) seperate a config object for master and node

--- cmd/kubeadm/app/master/tokens.go
+++ cmd/kubeadm/app/master/tokens.go
@@ -28,7 +28,7 @@ import (
 	"k8s.io/kubernetes/pkg/util/uuid"
 )
 
-func generateTokenIfNeeded(s *kubeadmapi.KubeadmConfig) error {
+func generateTokenIfNeeded(s *kubeadmapi.Secrets) error {
 	ok, err := kubeadmutil.UseGivenTokenIfValid(s)
 	// TODO(phase1+) @krousey: I know it won't happen with the way it is currently implemented, but this doesn't handle case where ok is true and err is non-nil.
 	if !ok {
@@ -39,7 +39,7 @@ func generateTokenIfNeeded(s *kubeadmapi.KubeadmConfig) error {
 		if err != nil {
 			return err
 		}
-		fmt.Printf("<master/tokens> generated token: %q\n", s.Secrets.GivenToken)
+		fmt.Printf("<master/tokens> generated token: %q\n", s.GivenToken)
 	} else {
 		fmt.Println("<master/tokens> accepted provided token")
 	}
@@ -47,15 +47,15 @@ func generateTokenIfNeeded(s *kubeadmapi.KubeadmConfig) error {
 	return nil
 }
 
-func CreateTokenAuthFile(s *kubeadmapi.KubeadmConfig) error {
-	tokenAuthFilePath := path.Join(s.EnvParams["host_pki_path"], "tokens.csv")
+func CreateTokenAuthFile(s *kubeadmapi.Secrets) error {
+	tokenAuthFilePath := path.Join(kubeadmapi.GetEnvParams()["host_pki_path"], "tokens.csv")
 	if err := generateTokenIfNeeded(s); err != nil {
 		return fmt.Errorf("<master/tokens> failed to generate token(s) [%v]", err)
 	}
-	if err := os.MkdirAll(s.EnvParams["host_pki_path"], 0700); err != nil {
-		return fmt.Errorf("<master/tokens> failed to create directory %q [%v]", s.EnvParams["host_pki_path"], err)
+	if err := os.MkdirAll(kubeadmapi.GetEnvParams()["host_pki_path"], 0700); err != nil {
+		return fmt.Errorf("<master/tokens> failed to create directory %q [%v]", kubeadmapi.GetEnvParams()["host_pki_path"], err)
 	}
-	serialized := []byte(fmt.Sprintf("%s,kubeadm-node-csr,%s,system:kubelet-bootstrap\n", s.Secrets.BearerToken, uuid.NewUUID()))
+	serialized := []byte(fmt.Sprintf("%s,kubeadm-node-csr,%s,system:kubelet-bootstrap\n", s.BearerToken, uuid.NewUUID()))
 	// DumpReaderToFile create a file with mode 0600
 	if err := cmdutil.DumpReaderToFile(bytes.NewReader(serialized), tokenAuthFilePath); err != nil {
 		return fmt.Errorf("<master/tokens> failed to save token auth file (%q) [%v]", tokenAuthFilePath, err)
