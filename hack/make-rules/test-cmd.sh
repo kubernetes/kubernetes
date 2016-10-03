5b63fb5fb6d8ace335b63b26469ee546202243b2
Alexander Brand <abrand@apprenda.com>
2016-10-03 10:57:46 -0400

    Fixed test for view cmd

--- hack/make-rules/test-cmd.sh
+++ hack/make-rules/test-cmd.sh
@@ -2069,9 +2069,10 @@ __EOF__
   kube::test::get_object_assert deployment "{{range.items}}{{$deployment_image_field}}:{{end}}" "${IMAGE_DEPLOYMENT_R1}:"
   kube::test::get_object_assert deployment "{{range.items}}{{$deployment_second_image_field}}:{{end}}" "${IMAGE_PERL}:"
   # Get image of a deployment
-  output_message=$(! kubectl view image deployment/nginx-deployment)
+  output_message=$(kubectl view image deployment/nginx-deployment)
   # Post condition: both images are listed
-  kube::test::if_has_string "${output_message}" "${IMAGE_DEPLOYMENT_R1}\n${IMAGE_PERL}"
+  expected_message=$(echo ${IMAGE_DEPLOYMENT_R1}; echo ${IMAGE_PERL})
+  kube::test::if_has_string "${output_message}" "${expected_message}"
   # Clean up
   kubectl delete deployment nginx-deployment "${kube_flags[@]}"
 
