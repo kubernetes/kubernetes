6d5c49b6e119ef2c5f42fabe5b27f4b45ad17963
Derek McQuay <dmcquay@apprenda.com>
2016-10-04 13:45:24 -0700

    kubeadm implement preflight checks
    Includes checks for verifying services exist and are enabled, ports are
    open, directories do not exist or are empty, and required binaries are
    in the path.
    
    Checks that user running kubeamd init and join is root and will only execute
    command if user is root. Moved away from using kubectl error handling to
    having kubeadm handle its own errors. This should allow kubeadm to have
    more meaningful errors, exit codes, and logging for specific kubeadm use
    cases.

--- cmd/kubeadm/app/preflight/checks.go
+++ cmd/kubeadm/app/preflight/checks.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package checks
+package preflight
 
 import (
 	"fmt"
@@ -22,12 +22,23 @@ import (
 	"net"
 	"os"
 	"os/exec"
+
+	"k8s.io/kubernetes/pkg/util/initsystem"
 )
+
+type PreFlightError struct {
+	Msg   string
+	Count int
+}
+
+func (e *PreFlightError) Error() string {
+	return fmt.Sprintf("preflight check error\n  count: %d \n  msg: %s", e.Count, e.Msg)
+}
 
 // PreFlightCheck validates the state of the system to ensure kubeadm will be
 // successful as often as possilble.
 type PreFlightCheck interface {
-	Check() (warnings []string, errors []string)
+	Check() (warnings, errors []error)
 }
 
 // ServiceCheck verifies that the given service is enabled and active. If we do not
@@ -37,29 +48,29 @@ type ServiceCheck struct {
 	service string
 }
 
-func (sc ServiceCheck) Check() (warnings []string, errors []string) {
+func (sc ServiceCheck) Check() (warnings, errors []error) {
 
-	initSystem := getInitSystem()
+	initSystem := initsystem.GetInitSystem()
 	if initSystem == nil {
-		return []string{"no supported init system detected, skipping service checks"}, nil
+		return []error{fmt.Errorf("no supported init system detected, skipping service checks for %s", sc.service)}, nil
 	}
 
-	warnings = []string{}
+	warnings = []error{}
 
 	if !initSystem.ServiceExists(sc.service) {
-		warnings = append(warnings, fmt.Sprintf("%s service does not exist", sc.service))
+		warnings = append(warnings, fmt.Errorf("%s service does not exist", sc.service))
 		return warnings, nil
 	}
 
 	if !initSystem.ServiceIsEnabled(sc.service) {
 		warnings = append(warnings,
-			fmt.Sprintf("%s service is not enabled, please run 'systemctl enable %s.service'",
+			fmt.Errorf("%s service is not enabled, please run 'systemctl enable %s.service'",
 				sc.service, sc.service))
 	}
 
 	if !initSystem.ServiceIsActive(sc.service) {
 		errors = append(errors,
-			fmt.Sprintf("%s service is not active, please run 'systemctl start %s.service'",
+			fmt.Errorf("%s service is not active, please run 'systemctl start %s.service'",
 				sc.service, sc.service))
 	}
 
@@ -71,15 +82,29 @@ type PortOpenCheck struct {
 	port int
 }
 
-func (poc PortOpenCheck) Check() (warnings []string, errors []string) {
-	errors = []string{}
+func (poc PortOpenCheck) Check() (warnings, errors []error) {
+	errors = []error{}
 	// TODO: Get IP from KubeadmConfig
 	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", poc.port))
 	if err != nil {
-		errors = append(errors, fmt.Sprintf("Port %d is in use.", poc.port))
+		errors = append(errors, fmt.Errorf("Port %d is in use", poc.port))
 	}
 	if ln != nil {
 		ln.Close()
+	}
+
+	return nil, errors
+}
+
+// IsRootCheck verifies user is root
+type IsRootCheck struct {
+	root bool
+}
+
+func (irc IsRootCheck) Check() (warnings, errors []error) {
+	errors = []error{}
+	if os.Getuid() != 0 {
+		errors = append(errors, fmt.Errorf("user is not running as root"))
 	}
 
 	return nil, errors
@@ -91,8 +116,8 @@ type DirAvailableCheck struct {
 	path string
 }
 
-func (dac DirAvailableCheck) Check() (warnings []string, errors []string) {
-	errors = []string{}
+func (dac DirAvailableCheck) Check() (warnings, errors []error) {
+	errors = []error{}
 	// If it doesn't exist we are good:
 	if _, err := os.Stat(dac.path); os.IsNotExist(err) {
 		return nil, nil
@@ -100,14 +125,14 @@ func (dac DirAvailableCheck) Check() (warnings []string, errors []string) {
 
 	f, err := os.Open(dac.path)
 	if err != nil {
-		errors = append(errors, fmt.Sprintf("Unable to check if %s is empty: %s", dac.path, err))
+		errors = append(errors, fmt.Errorf("unable to check if %s is empty: %s", dac.path, err))
 		return nil, errors
 	}
 	defer f.Close()
 
 	_, err = f.Readdirnames(1)
 	if err != io.EOF {
-		errors = append(errors, fmt.Sprintf("%s is not empty", dac.path))
+		errors = append(errors, fmt.Errorf("%s is not empty", dac.path))
 	}
 
 	return nil, errors
@@ -119,22 +144,23 @@ type InPathCheck struct {
 	mandatory  bool
 }
 
-func (ipc InPathCheck) Check() (warnings []string, errors []string) {
+func (ipc InPathCheck) Check() (warnings, errors []error) {
 	_, err := exec.LookPath(ipc.executable)
 	if err != nil {
 		if ipc.mandatory {
 			// Return as an error:
-			return nil, []string{fmt.Sprintf("%s not found in system path.", ipc.executable)}
+			return nil, []error{fmt.Errorf("%s not found in system path", ipc.executable)}
 		}
 		// Return as a warning:
-		return []string{fmt.Sprintf("%s not found in system path.", ipc.executable)}, nil
+		return []error{fmt.Errorf("%s not found in system path", ipc.executable)}, nil
 	}
 	return nil, nil
 }
 
-func RunInitMasterChecks() {
+func RunInitMasterChecks() error {
 	// TODO: Some of these ports should come from kubeadm config eventually:
 	checks := []PreFlightCheck{
+		IsRootCheck{root: true},
 		ServiceCheck{service: "kubelet"},
 		ServiceCheck{service: "docker"},
 		PortOpenCheck{port: 443},
@@ -146,46 +172,68 @@ func RunInitMasterChecks() {
 		DirAvailableCheck{path: "/etc/kubernetes"},
 		DirAvailableCheck{path: "/var/lib/etcd"},
 		DirAvailableCheck{path: "/var/lib/kubelet"},
-		InPathCheck{executable: "socat", mandatory: true},
+		InPathCheck{executable: "ebtables", mandatory: true},
 		InPathCheck{executable: "ethtool", mandatory: true},
+		InPathCheck{executable: "ip", mandatory: true},
+		InPathCheck{executable: "iptables", mandatory: true},
+		InPathCheck{executable: "mount", mandatory: true},
+		InPathCheck{executable: "nsenter", mandatory: true},
+		InPathCheck{executable: "socat", mandatory: true},
+		InPathCheck{executable: "tc", mandatory: false},
+		InPathCheck{executable: "touch", mandatory: false},
 	}
 
-	runChecks(checks)
+	return runChecks(checks)
 }
 
-func RunJoinNodeChecks() {
+func RunJoinNodeChecks() error {
 	// TODO: Some of these ports should come from kubeadm config eventually:
 	checks := []PreFlightCheck{
-		ServiceCheck{service: "kubelet"},
+		IsRootCheck{root: true},
 		ServiceCheck{service: "docker"},
+		ServiceCheck{service: "kubelet"},
 		PortOpenCheck{port: 8080},
 		PortOpenCheck{port: 10250},
 		PortOpenCheck{port: 10251},
 		PortOpenCheck{port: 10252},
 		DirAvailableCheck{path: "/etc/kubernetes"},
 		DirAvailableCheck{path: "/var/lib/kubelet"},
-		InPathCheck{executable: "socat", mandatory: true},
+		InPathCheck{executable: "ebtables", mandatory: true},
 		InPathCheck{executable: "ethtool", mandatory: true},
+		InPathCheck{executable: "ip", mandatory: true},
+		InPathCheck{executable: "iptables", mandatory: true},
+		InPathCheck{executable: "mount", mandatory: true},
+		InPathCheck{executable: "nsenter", mandatory: true},
+		InPathCheck{executable: "socat", mandatory: true},
+		InPathCheck{executable: "tc", mandatory: false},
+		InPathCheck{executable: "touch", mandatory: false},
 	}
 
-	runChecks(checks)
+	return runChecks(checks)
 }
 
 // runChecks runs each check, displays it's warnings/errors, and once all
 // are processed will exit if any errors occurred.
-func runChecks(checks []PreFlightCheck) {
-	foundErrors := false
-	for _, check := range checks {
-		warnings, errors := check.Check()
-		for _, warnMsg := range warnings {
-			fmt.Printf("WARNING: %s\n", warnMsg)
+func runChecks(checks []PreFlightCheck) error {
+	found := []error{}
+	for _, c := range checks {
+		warnings, errors := c.Check()
+		for _, w := range warnings {
+			fmt.Printf("WARNING: %s\n", w)
 		}
-		for _, errMsg := range errors {
-			foundErrors = true
-			fmt.Printf("ERROR: %s\n", errMsg)
+		for _, e := range errors {
+			found = append(found, e)
 		}
 	}
-	if foundErrors {
-		os.Exit(1)
+	if len(found) > 0 {
+		errors := "\n"
+		for _, i := range found {
+			errors += "\t" + i.Error() + "\n"
+		}
+		return &PreFlightError{
+			Msg:   errors,
+			Count: len(found),
+		}
 	}
+	return nil
 }
