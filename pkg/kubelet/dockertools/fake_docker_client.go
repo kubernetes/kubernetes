77371c3bf48688311ecac97055abbe5106a64391
Wojciech Tyczynski <wojtekt@google.com>
2016-10-08 10:19:22 +0200

    Revert "Kubelet: Use RepoDigest for ImageID when available"

--- pkg/kubelet/dockertools/fake_docker_client.go
+++ pkg/kubelet/dockertools/fake_docker_client.go
@@ -310,19 +310,9 @@ func (f *FakeDockerClient) InspectContainer(id string) (*dockertypes.ContainerJS
 	return nil, fmt.Errorf("container %q not found", id)
 }
 
-// InspectImageByRef is a test-spy implementation of DockerInterface.InspectImageByRef.
+// InspectImage is a test-spy implementation of DockerInterface.InspectImage.
 // It adds an entry "inspect" to the internal method call record.
-func (f *FakeDockerClient) InspectImageByRef(name string) (*dockertypes.ImageInspect, error) {
-	f.Lock()
-	defer f.Unlock()
-	f.called = append(f.called, calledDetail{name: "inspect_image"})
-	err := f.popError("inspect_image")
-	return f.Image, err
-}
-
-// InspectImageByID is a test-spy implementation of DockerInterface.InspectImageByID.
-// It adds an entry "inspect" to the internal method call record.
-func (f *FakeDockerClient) InspectImageByID(name string) (*dockertypes.ImageInspect, error) {
+func (f *FakeDockerClient) InspectImage(name string) (*dockertypes.ImageInspect, error) {
 	f.Lock()
 	defer f.Unlock()
 	f.called = append(f.called, calledDetail{name: "inspect_image"})
