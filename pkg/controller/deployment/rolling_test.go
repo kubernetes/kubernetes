301dbad3c4bb080a61a9f5914222de4db8beff5d
Michail Kargakis <mkargaki@redhat.com>
2016-10-10 19:02:31 +0200

    Revert "Error out when any RS has more available pods then its spec replicas"
    This reverts commit 32ababbe1b0bf9ee6786aa555cc3cd7f54f2014a.

--- pkg/controller/deployment/rolling_test.go
+++ pkg/controller/deployment/rolling_test.go
@@ -395,7 +395,6 @@ func TestDeploymentController_scaleDownOldReplicaSetsForRollingUpdate(t *testing
 		oldReplicas         int
 		scaleExpected       bool
 		expectedOldReplicas int
-		errorExpected       bool
 	}{
 		{
 			deploymentReplicas:  10,
@@ -404,7 +403,6 @@ func TestDeploymentController_scaleDownOldReplicaSetsForRollingUpdate(t *testing
 			oldReplicas:         10,
 			scaleExpected:       true,
 			expectedOldReplicas: 9,
-			errorExpected:       false,
 		},
 		{
 			deploymentReplicas:  10,
@@ -413,7 +411,6 @@ func TestDeploymentController_scaleDownOldReplicaSetsForRollingUpdate(t *testing
 			oldReplicas:         10,
 			scaleExpected:       true,
 			expectedOldReplicas: 8,
-			errorExpected:       false,
 		},
 		{
 			deploymentReplicas: 10,
@@ -421,7 +418,6 @@ func TestDeploymentController_scaleDownOldReplicaSetsForRollingUpdate(t *testing
 			readyPods:          8,
 			oldReplicas:        10,
 			scaleExpected:      false,
-			errorExpected:      false,
 		},
 		{
 			deploymentReplicas: 10,
@@ -429,7 +425,6 @@ func TestDeploymentController_scaleDownOldReplicaSetsForRollingUpdate(t *testing
 			readyPods:          10,
 			oldReplicas:        0,
 			scaleExpected:      false,
-			errorExpected:      true,
 		},
 		{
 			deploymentReplicas: 10,
@@ -437,7 +432,6 @@ func TestDeploymentController_scaleDownOldReplicaSetsForRollingUpdate(t *testing
 			readyPods:          1,
 			oldReplicas:        10,
 			scaleExpected:      false,
-			errorExpected:      false,
 		},
 	}
 
@@ -479,7 +473,7 @@ func TestDeploymentController_scaleDownOldReplicaSetsForRollingUpdate(t *testing
 			eventRecorder: &record.FakeRecorder{},
 		}
 		scaled, err := controller.scaleDownOldReplicaSetsForRollingUpdate(allRSs, oldRSs, deployment)
-		if !test.errorExpected && err != nil {
+		if err != nil {
 			t.Errorf("unexpected error: %v", err)
 			continue
 		}
