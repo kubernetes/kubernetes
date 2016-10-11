12c9ba6425d9fe58a22a5a7ede3bd10e10c2e4e9
Mike Danese <mikedanese@google.com>
2016-10-11 10:43:05 -0700

    kubeadm: fix conversion macros and add kubeadm to round trip testing

--- cmd/kubeadm/app/apis/kubeadm/types.go
+++ cmd/kubeadm/app/apis/kubeadm/types.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package api
+package kubeadm
 
 import "k8s.io/kubernetes/pkg/api/unversioned"
 
