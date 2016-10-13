fc2454f3f4dc88ef55e465e263817a9f7dfff50f
Cole Mickens <cole.mickens@gmail.com>
2016-10-13 08:35:12 -0700

    azure: lower log priority for skipped nic update message

--- pkg/cloudprovider/providers/azure/azure_loadbalancer.go
+++ pkg/cloudprovider/providers/azure/azure_loadbalancer.go
@@ -575,7 +575,7 @@ func (az *Cloud) ensureHostInPool(serviceName, machineName string, backendPoolID
 	if az.PrimaryAvailabilitySetName != "" {
 		expectedAvailabilitySetName := az.getAvailabilitySetID(az.PrimaryAvailabilitySetName)
 		if !strings.EqualFold(*machine.Properties.AvailabilitySet.ID, expectedAvailabilitySetName) {
-			glog.V(1).Infof(
+			glog.V(3).Infof(
 				"nicupdate(%s): skipping nic (%s) since it is not in the primaryAvailabilitSet(%s)",
 				serviceName, nicName, az.PrimaryAvailabilitySetName)
 			return nil
