dba5051b3aed1975af43879c167de2f694d86e96
Brendan Burns <bburns@google.com>
2016-10-17 22:17:33 -0700

    improve unit tests.

--- pkg/kubectl/cmd/cp.go
+++ pkg/kubectl/cmd/cp.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2014 The Kubernetes Authors.
+Copyright 2016 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -33,14 +33,20 @@ import (
 
 var (
 	cp_example = dedent.Dedent(`
-		# Copy /tmp/foo local file to /tmp/bar in a remote pod
+	    # Copy /tmp/foo_dir local directory to /tmp/bar_dir in a remote pod in the default namespace
+		kubectl cp /tmp/foo_dir <some-pod>:/tmp/bar_dir
+
+        # Copy /tmp/foo local file to /tmp/bar in a remote pod in a specific container
+		kubectl cp /tmp/foo <some-pod>:/tmp/bar -c <specific-container>
+
+		# Copy /tmp/foo local file to /tmp/bar in a remote pod in namespace <some-namespace>
 		kubectl cp /tmp/foo <some-namespace>/<some-pod>:/tmp/bar
 
 		# Copy /tmp/foo from a remote pod to /tmp/bar locally
 		kubectl cp <some-namespace>/<some-pod>:/tmp/foo /tmp/bar`)
 
 	cpUsageStr = dedent.Dedent(`
-	    expected 'cp <file-spec-src> <file-spec-dest>'.
+	    expected 'cp <file-spec-src> <file-spec-dest> [-c container]'.
 	    <file-spec> is:
 		[namespace/]pod-name:/file/path for a remote file
 		/file/path for a local file`)
@@ -50,8 +56,8 @@ var (
 func NewCmdCp(f cmdutil.Factory, cmdIn io.Reader, cmdOut, cmdErr io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "cp <file-spec-src> <file-spec-dest>",
-		Short:   "Copy files to and from containers.",
-		Long:    "Copy files to and from containers.",
+		Short:   "Copy files and directories to and from containers.",
+		Long:    "Copy files and directories to and from containers.",
 		Example: cp_example,
 		Run: func(cmd *cobra.Command, args []string) {
 			cmdutil.CheckErr(runCopy(f, cmd, cmdOut, cmdErr, args))
@@ -117,49 +123,6 @@ func runCopy(f cmdutil.Factory, cmd *cobra.Command, out, cmderr io.Writer, args 
 	return cmdutil.UsageError(cmd, "One of src or dest must be a remote file specification")
 }
 
-func makeTar(filepath string, writer io.Writer) error {
-	tarWriter := tar.NewWriter(writer)
-	defer tarWriter.Close()
-	return recursiveTar(path.Dir(filepath), path.Base(filepath), tarWriter)
-}
-
-func recursiveTar(base, file string, tw *tar.Writer) error {
-	filepath := path.Join(base, file)
-	fmt.Printf("Starting to archive: %s\n", filepath)
-	stat, err := os.Stat(filepath)
-	if err != nil {
-		return err
-	}
-	if stat.IsDir() {
-		files, err := ioutil.ReadDir(filepath)
-		if err != nil {
-			return err
-		}
-		for _, f := range files {
-			if err := recursiveTar(base, path.Join(file, f.Name()), tw); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-	hdr, err := tar.FileInfoHeader(stat, filepath)
-	if err != nil {
-		return err
-	}
-	hdr.Name = file
-	if err := tw.WriteHeader(hdr); err != nil {
-		return err
-	}
-	f, err := os.Open(filepath)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	fmt.Printf("Copying %s to archive\n", filepath)
-	_, err = io.Copy(tw, f)
-	return err
-}
-
 func copyToPod(f cmdutil.Factory, cmd *cobra.Command, stdout, stderr io.Writer, src, dest fileSpec) error {
 	reader, writer := io.Pipe()
 	go func() {
@@ -214,6 +177,47 @@ func copyFromPod(f cmdutil.Factory, cmd *cobra.Command, out, cmderr io.Writer, s
 	prefix := getPrefix(src.File)
 
 	return untarAll(reader, dest.File, prefix)
+}
+
+func makeTar(filepath string, writer io.Writer) error {
+	tarWriter := tar.NewWriter(writer)
+	defer tarWriter.Close()
+	return recursiveTar(path.Dir(filepath), path.Base(filepath), tarWriter)
+}
+
+func recursiveTar(base, file string, tw *tar.Writer) error {
+	filepath := path.Join(base, file)
+	stat, err := os.Stat(filepath)
+	if err != nil {
+		return err
+	}
+	if stat.IsDir() {
+		files, err := ioutil.ReadDir(filepath)
+		if err != nil {
+			return err
+		}
+		for _, f := range files {
+			if err := recursiveTar(base, path.Join(file, f.Name()), tw); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	hdr, err := tar.FileInfoHeader(stat, filepath)
+	if err != nil {
+		return err
+	}
+	hdr.Name = file
+	if err := tw.WriteHeader(hdr); err != nil {
+		return err
+	}
+	f, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = io.Copy(tw, f)
+	return err
 }
 
 func untarAll(reader io.Reader, destFile, prefix string) error {
