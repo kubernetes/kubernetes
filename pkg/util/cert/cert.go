34a15e144b75532af99f8117b07584f2224e2e2d
Dr. Stefan Schimanski <sttts@redhat.com>
2016-10-21 13:23:03 +0200

    Fix CommonName in auto-generated certs

--- pkg/util/cert/cert.go
+++ pkg/util/cert/cert.go
@@ -138,7 +138,7 @@ func GenerateSelfSignedCertKey(host string, alternateIPs []net.IP, alternateDNS 
 	template := x509.Certificate{
 		SerialNumber: big.NewInt(1),
 		Subject: pkix.Name{
-			CommonName: fmt.Sprintf("%s@%d", host, time.Now().Unix()),
+			CommonName: host,
 		},
 		NotBefore: time.Now(),
 		NotAfter:  time.Now().Add(time.Hour * 24 * 365),
