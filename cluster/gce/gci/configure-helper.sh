aceddfa7fcb06fa080761fa26dc4cda0feddf2ec
Vishnu kannan <vishnuk@google.com>
2016-10-21 17:43:16 -0700

    Enable containerized storage mounter on GCI
    Signed-off-by: Vishnu kannan <vishnuk@google.com>

--- cluster/gce/gci/configure-helper.sh
+++ cluster/gce/gci/configure-helper.sh
@@ -475,7 +475,10 @@ function start-kubelet {
   flags+=" --config=/etc/kubernetes/manifests"
   flags+=" --kubelet-cgroups=/kubelet"
   flags+=" --system-cgroups=/system"
-
+  flags+=" --experimental-mounter-path=${KUBE_HOME}/bin/mounter"
+  # Note: This patch must match the rootfs path in mounter/mounter
+  flags+=" --experimental-mounter-rootfs-path=/media/root"
+  
   if [[ -n "${KUBELET_PORT:-}" ]]; then
     flags+=" --port=${KUBELET_PORT}"
   fi
