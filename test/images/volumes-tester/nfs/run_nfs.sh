98184bd0a951593e568a2f52bb4bc3a3c9eb4c05
Matthew Wong <mawong@redhat.com>
2016-10-24 17:46:12 -0400

    Update NFS testing image to use ganesha, to work on GCI

--- test/images/volumes-tester/nfs/run_nfs.sh
+++ test/images/volumes-tester/nfs/run_nfs.sh
@@ -16,64 +16,81 @@
 
 function start()
 {
+	unset gid
+	# accept "-G gid" option
+	while getopts "G:" opt; do
+		case ${opt} in
+			G) gid=${OPTARG};;
+		esac
+	done
+	shift $(($OPTIND - 1))
 
-    unset gid
-    # accept "-G gid" option
-    while getopts "G:" opt; do
-        case ${opt} in
-            G) gid=${OPTARG};;
-        esac
-    done
-    shift $(($OPTIND - 1))
+	# prepare ganesha config
+	# set mountd port to 20048
+	cat <<-EOF >> /tmp/ganesha.conf
+	NFS_Core_Param
+	{
+		MNT_Port = 20048;
+		NFS_PROTOCOLS = 4;
+	}
+	EOF
+	# assign unique export ids
+	id=1
+	# if testing group permissions, need to squash
+	squash="no_root_squash"
+	if [ -v gid ] ; then
+		squash="root_id_squash"
+	fi
+	for i in "$@"; do
+		mkdir -p $i
+		if [ -v gid ] ; then
+			chmod 071 $i
+			chgrp $gid $i
+		fi
+		cat <<- EOF >> /tmp/ganesha.conf
+		EXPORT
+		{
+			# Export Id (mandatory, each EXPORT must have a unique Export_Id)
+			Export_Id = $id;
 
-    # prepare /etc/exports
-    for i in "$@"; do
-        # fsid=0: needed for NFSv4
-        echo "$i *(rw,fsid=0,insecure,no_root_squash)" >> /etc/exports
-        if [ -v gid ] ; then
-            chmod 070 $i
-            chgrp $gid $i
-        fi
-        # move index.html to here
-        /bin/cp /tmp/index.html $i/
-        chmod 644 $i/index.html
-        echo "Serving $i"
-    done
-  
-    # start rpcbind if it is not started yet
-    /usr/sbin/rpcinfo 127.0.0.1 > /dev/null; s=$?
-    if [ $s -ne 0 ]; then
-       echo "Starting rpcbind"
-       /usr/sbin/rpcbind -w
-    fi
+			Filesystem_Id = $id.$id;
 
-    mount -t nfsd nfds /proc/fs/nfsd
+			# Exported path (mandatory)
+			Path = "$i";
 
-    # -N 4.x: disable NFSv4
-    # -V 3: enable NFSv3
-    /usr/sbin/rpc.mountd -N 2 -V 3 -N 4 -N 4.1
+			# Pseudo Path (required for NFS v4)
+			Pseudo = "$i";
 
-    /usr/sbin/exportfs -r
-    # -G 10 to reduce grace time to 10 seconds (the lowest allowed)
-    /usr/sbin/rpc.nfsd -G 10 -N 2 -V 3 -N 4 -N 4.1 2
-    /usr/sbin/rpc.statd --no-notify
-    echo "NFS started"
+			# Required for access (default is None)
+			# Could use CLIENT blocks instead
+			Access_Type = RW;
+
+			Squash = $squash;
+			SecType = sys;
+			Protocols = 4;
+			# Exporting FSAL
+			FSAL {
+				Name = VFS;
+			}
+		}
+		EOF
+		# move index.html to here
+		/bin/cp /tmp/index.html $i/
+		chmod 644 $i/index.html
+		echo "Serving $i"
+		id=$((id + 1))
+	done
+
+	ganesha.nfsd -L /tmp/ganesha.log -f /tmp/ganesha.conf
+
+	echo "NFS started"
 }
 
 function stop()
 {
-    echo "Stopping NFS"
-
-    /usr/sbin/rpc.nfsd 0
-    /usr/sbin/exportfs -au
-    /usr/sbin/exportfs -f
-
-    kill $( pidof rpc.mountd )
-    umount /proc/fs/nfsd
-    echo > /etc/exports
-    exit 0
+	echo "Stopping NFS"
+	exit 0
 }
-
 
 trap stop TERM
 
@@ -81,5 +98,5 @@ start "$@"
 
 # Ugly hack to do nothing and wait for SIGTERM
 while true; do
-    sleep 5
+	sleep 5
 done
