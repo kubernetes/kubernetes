67749d664ee11b7f34af79880dc817f14ad076d2
Di Xu <stephenhsu90@gmail.com>
2016-10-25 11:26:44 +0800

    specify custom ca file to verify the keystone server

--- pkg/genericapiserver/options/server_run_options.go
+++ pkg/genericapiserver/options/server_run_options.go
@@ -91,6 +91,7 @@ type ServerRunOptions struct {
 	InsecureBindAddress          net.IP
 	InsecurePort                 int
 	KeystoneURL                  string
+	KeystoneCAFile               string
 	KubernetesServiceNodePort    int
 	LongRunningRequestRE         string
 	MasterCount                  int
@@ -377,6 +378,11 @@ func (s *ServerRunOptions) AddUniversalFlags(fs *pflag.FlagSet) {
 
 	fs.StringVar(&s.KeystoneURL, "experimental-keystone-url", s.KeystoneURL,
 		"If passed, activates the keystone authentication plugin.")
+
+	fs.StringVar(&s.KeystoneCAFile, "experimental-keystone-ca-file", s.KeystoneCAFile, ""+
+		"If set, any keystone request presenting a client certificate signed by one of "+
+		"the authorities in the experimental-keystone-ca-file is authenticated with an identity "+
+		"corresponding to the CommonName of the client certificate.")
 
 	// See #14282 for details on how to test/try this option out.
 	// TODO: remove this comment once this option is tested in CI.
