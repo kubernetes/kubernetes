c601d735aa00daef0e2c5a367df67a1996b7e76a
Andy Goldstein <agoldste@redhat.com>
2016-10-25 15:00:13 -0400

    Add lister-gen

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -26,11 +26,13 @@ kube::golang::setup_env
 BUILD_TARGETS=(
   cmd/libs/go2idl/client-gen
   cmd/libs/go2idl/set-gen
+  cmd/libs/go2idl/lister-gen
 )
 make -C "${KUBE_ROOT}" WHAT="${BUILD_TARGETS[*]}"
 
 clientgen=$(kube::util::find-binary "client-gen")
 setgen=$(kube::util::find-binary "set-gen")
+listergen=$(kube::util::find-binary "lister-gen")
 
 # Please do not add any logic to this shell script. Add logic to the go code
 # that generates the set-gen program.
@@ -62,5 +64,19 @@ ${clientgen} --clientset-name="release_1_5" --input="${GV_DIRS_CSV}" "$@"
 ${clientgen} --clientset-name=federation_internalclientset --clientset-path=k8s.io/kubernetes/federation/client/clientset_generated --input="../../federation/apis/federation/","api/","extensions/" --included-types-overrides="api/Service,api/Namespace,extensions/ReplicaSet,api/Secret,extensions/Ingress,extensions/Deployment,extensions/DaemonSet,api/ConfigMap,api/Event"   "$@"
 ${clientgen} --clientset-name=federation_release_1_5 --clientset-path=k8s.io/kubernetes/federation/client/clientset_generated --input="../../federation/apis/federation/v1beta1","api/v1","extensions/v1beta1" --included-types-overrides="api/v1/Service,api/v1/Namespace,extensions/v1beta1/ReplicaSet,api/v1/Secret,extensions/v1beta1/Ingress,extensions/v1beta1/Deployment,extensions/v1beta1/DaemonSet,api/v1/ConfigMap,api/v1/Event"   "$@"
 ${setgen} "$@"
+
+LISTERGEN_APIS=(
+pkg/api
+pkg/api/v1
+$(
+  cd ${KUBE_ROOT}
+  find pkg/apis -name types.go | xargs dirname | sort
+)
+)
+
+LISTERGEN_APIS=(${LISTERGEN_APIS[@]/#/k8s.io/kubernetes/})
+LISTERGEN_APIS=$(IFS=,; echo "${LISTERGEN_APIS[*]}")
+
+${listergen} --input-dirs "${LISTERGEN_APIS}" "$@"
 
 # You may add additional calls of code generators like set-gen above.
