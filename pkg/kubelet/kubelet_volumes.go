7fb99442a654f372fc0ce3dc8a4200cd02d87bf1
Paul Morie <pmorie@redhat.com>
2016-10-25 11:51:11 -0400

    Refactor pkg/util/selinux

--- pkg/kubelet/kubelet_volumes.go
+++ pkg/kubelet/kubelet_volumes.go
@@ -94,7 +94,7 @@ func (kl *Kubelet) relabelVolumes(pod *api.Pod, volumes kubecontainer.VolumeMap)
 		return err
 	}
 
-	selinuxRunner := selinux.NewSelinuxContextRunner()
+	selinuxRunner := selinux.NewSELinuxRunner()
 	// Apply the pod's Level to the rootDirContext
 	rootDirSELinuxOptions, err := securitycontext.ParseSELinuxOptions(rootDirContext)
 	if err != nil {
@@ -115,7 +115,7 @@ func (kl *Kubelet) relabelVolumes(pod *api.Pod, volumes kubecontainer.VolumeMap)
 				if err != nil {
 					return err
 				}
-				return selinuxRunner.SetContext(path, volumeContext)
+				return selinuxRunner.Setfilecon(path, volumeContext)
 			})
 			if err != nil {
 				return err
