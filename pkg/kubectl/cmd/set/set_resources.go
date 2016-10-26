0c06e0e53515b20a57616253c9fea31fd8c96bf6
xilabao <chenr.fnst@cn.fujitsu.com>
2016-10-26 16:00:35 +0800

    fix invalid request to username/password

--- pkg/kubectl/cmd/set/set_resources.go
+++ pkg/kubectl/cmd/set/set_resources.go
@@ -38,7 +38,7 @@ var (
 
 		for each compute resource, if a limit is specified and a request is omitted, the request will default to the limit.
 
-		Possible resources include (case insensitive):`)
+		Possible resources include (case insensitive): replicationcontrollers, deployments, daemonsets, jobs, replicasets`)
 
 	resources_example = templates.Examples(`
 		# Set a deployments nginx container cpu limits to "200m" and memory to "512Mi"
@@ -87,17 +87,11 @@ func NewCmdResources(f cmdutil.Factory, out io.Writer, errOut io.Writer) *cobra.
 		Out: out,
 		Err: errOut,
 	}
-	var pod_specs string
-	RESTMappings := cmdutil.ResourcesWithPodSpecs()
-	for _, Map := range RESTMappings {
-		pod_specs = pod_specs + ", " + Map.Resource
-
-	}
 
 	cmd := &cobra.Command{
 		Use:     "resources (-f FILENAME | TYPE NAME)  ([--limits=LIMITS & --requests=REQUESTS]",
 		Short:   "update resource requests/limits on objects with pod templates",
-		Long:    resources_long + " " + pod_specs[2:],
+		Long:    resources_long,
 		Example: resources_example,
 		Run: func(cmd *cobra.Command, args []string) {
 			cmdutil.CheckErr(options.Complete(f, cmd, args))
