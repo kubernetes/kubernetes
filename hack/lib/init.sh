4850314a5cee4064e7d1b7f23ecc14232c7c3eee
deads2k <deads@redhat.com>
2016-10-26 15:02:16 -0400

    add script to check for updates to the files for generation

--- hack/lib/init.sh
+++ hack/lib/init.sh
@@ -66,7 +66,10 @@ storage.k8s.io/v1beta1\
 # not all group versions are exposed by the server.  This list contains those
 # which are not available so we don't generate clients or swagger for them
 KUBE_NONSERVER_GROUP_VERSIONS="
- imagepolicy.k8s.io/v1alpha1 
+ abac.authorization.kubernetes.io/v0 \
+ abac.authorization.kubernetes.io/v1beta1 \
+ componentconfig/v1alpha1 \
+ imagepolicy.k8s.io/v1alpha1\
 "
 
 # This emulates "readlink -f" which is not available on MacOS X.
