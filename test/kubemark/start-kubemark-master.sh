935214f26fe75066b46661dfa2f4ddc35b2eaf70
Wojciech Tyczynski <wojtekt@google.com>
2016-10-27 19:27:02 +0200

    Rename TEST_ETCD_VERSION to ETCD_VERSION

--- test/kubemark/start-kubemark-master.sh
+++ test/kubemark/start-kubemark-master.sh
@@ -19,10 +19,10 @@
 EVENT_STORE_IP=$1
 EVENT_STORE_URL="http://${EVENT_STORE_IP}:4002"
 NUM_NODES=$2
-TEST_ETCD_VERSION=$3
-if [[ -z "${TEST_ETCD_VERSION}" ]]; then
+KUBEMARK_ETCD_VERSION=$3
+if [[ -z "${KUBEMARK_ETCD_VERSION}" ]]; then
   # Default etcd version.
-  TEST_ETCD_VERSION="2.2.1"
+  KUBEMARK_ETCD_VERSION="2.2.1"
 fi
 
 function retry() {
@@ -80,7 +80,7 @@ if [ "${EVENT_STORE_IP}" == "127.0.0.1" ]; then
 	# Retry starting etcd to avoid pulling image errors.
 	retry sudo docker run --net=host \
 		-v /var/etcd/data-events:/var/etcd/data -v /var/log:/var/log -d \
-		gcr.io/google_containers/etcd:${TEST_ETCD_VERSION} /bin/sh -c "/usr/local/bin/etcd \
+		gcr.io/google_containers/etcd:${KUBEMARK_ETCD_VERSION} /bin/sh -c "/usr/local/bin/etcd \
 		--listen-peer-urls http://127.0.0.1:2381 \
 		--advertise-client-urls=http://127.0.0.1:4002 \
 		--listen-client-urls=http://0.0.0.0:4002 \
@@ -90,7 +90,7 @@ fi
 # Retry starting etcd to avoid pulling image errors.
 retry sudo docker run --net=host \
 	-v /var/etcd/data:/var/etcd/data -v /var/log:/var/log -d \
-	gcr.io/google_containers/etcd:${TEST_ETCD_VERSION} /bin/sh -c "/usr/local/bin/etcd \
+	gcr.io/google_containers/etcd:${KUBEMARK_ETCD_VERSION} /bin/sh -c "/usr/local/bin/etcd \
 	--listen-peer-urls http://127.0.0.1:2380 \
 	--advertise-client-urls=http://127.0.0.1:2379 \
 	--listen-client-urls=http://0.0.0.0:2379 \
