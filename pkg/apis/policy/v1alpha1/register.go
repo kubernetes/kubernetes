3bc87f9338c8f406ca38d6320620a9abe248d9a9
Andy Goldstein <agoldste@redhat.com>
2016-11-02 15:56:37 -0400

    Add generated informers

--- pkg/apis/policy/v1alpha1/register.go
+++ pkg/apis/policy/v1alpha1/register.go
@@ -29,6 +29,11 @@ const GroupName = "policy"
 // SchemeGroupVersion is group version used to register these objects
 var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha1"}
 
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
 var (
 	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
 	AddToScheme   = SchemeBuilder.AddToScheme
