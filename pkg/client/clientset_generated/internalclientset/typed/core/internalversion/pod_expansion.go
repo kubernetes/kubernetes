3bcdd1e2eeccabea1ca748ec4dc970740455ac2a
ymqytw <mengqiy@google.com>
2016-11-03 22:09:12 -0700

    change back to hacking URL

--- pkg/client/clientset_generated/internalclientset/typed/core/internalversion/pod_expansion.go
+++ pkg/client/clientset_generated/internalclientset/typed/core/internalversion/pod_expansion.go
@@ -18,27 +18,18 @@ package internalversion
 
 import (
 	"k8s.io/kubernetes/pkg/api"
-	policy "k8s.io/kubernetes/pkg/apis/policy"
 	"k8s.io/kubernetes/pkg/client/restclient"
 )
 
 // The PodExpansion interface allows manually adding extra methods to the PodInterface.
 type PodExpansion interface {
 	Bind(binding *api.Binding) error
-	Evict(eviction *policy.Eviction) error
 	GetLogs(name string, opts *api.PodLogOptions) *restclient.Request
 }
 
 // Bind applies the provided binding to the named pod in the current namespace (binding.Namespace is ignored).
 func (c *pods) Bind(binding *api.Binding) error {
 	return c.client.Post().Namespace(c.ns).Resource("pods").Name(binding.Name).SubResource("binding").Body(binding).Do().Error()
-}
-
-// Evict trys to voluntarily evict pods based on its corresponding PodDisruptionBudget policy
-func (c *pods) Evict(eviction *policy.Eviction) error {
-	// TODO: We hack which codec func Body() is using to build the request body. To be fixed, when we found a good way
-	// to make eviction works cross Core and Policy group.
-	return c.client.Post().Namespace(c.ns).Resource("pods").Name(eviction.Name).SubResource("eviction").Body(eviction).Do().Error()
 }
 
 // Get constructs a request for getting the logs for a pod
