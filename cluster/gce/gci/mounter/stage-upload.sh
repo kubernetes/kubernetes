773ad9be292fda4a16e7be91bb81edc152fc36da
Vishnu kannan <vishnuk@google.com>
2016-11-04 17:09:40 -0700

    Make gci mounter pre-fetch mounter image to reduce startup latency during runtime
    Signed-off-by: Vishnu kannan <vishnuk@google.com>

--- cluster/gce/gci/mounter/stage-upload.sh
+++ cluster/gce/gci/mounter/stage-upload.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+# Copyright 2016 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Due to the GCE custom metadata size limit, we split the entire script into two
+# files configure.sh and configure-helper.sh. The functionality of downloading
+# kubernetes configuration, manifests, docker images, and binary files are
+# put in configure.sh, which is uploaded via GCE custom metadata.
+
 set -o errexit
 set -o pipefail
 set -o nounset
