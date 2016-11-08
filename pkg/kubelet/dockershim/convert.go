99ee3f4b762d5a9f5d6736b62ffeba383b3d7beb
Random-Liu <taotaotheripper@gmail.com>
2016-11-07 23:35:11 -0800

    Add non-numeric user name support.

--- pkg/kubelet/dockershim/convert.go
+++ pkg/kubelet/dockershim/convert.go
@@ -18,7 +18,6 @@ package dockershim
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 	"time"
 
@@ -57,24 +56,14 @@ func imageInspectToRuntimeAPIImage(image *dockertypes.ImageInspect) (*runtimeApi
 		return nil, fmt.Errorf("unable to convert a nil pointer to a runtime API image")
 	}
 
-	var err error
-	var uid int64
 	size := uint64(image.VirtualSize)
-	imageUid := dockertools.GetUidFromUser(image.Config.User)
-	// Convert image UID to int64 format. Not that it assumes the process in
-	// the image is running as root if image.Config.User is not set.
-	if imageUid != "" {
-		uid, err = strconv.ParseInt(imageUid, 10, 64)
-		if err != nil {
-			return nil, fmt.Errorf("non-numeric user (%q)", imageUid)
-		}
-	}
+	user := dockertools.GetUserFromImageUser(image.Config.User)
 	return &runtimeApi.Image{
 		Id:          &image.ID,
 		RepoTags:    image.RepoTags,
 		RepoDigests: image.RepoDigests,
 		Size_:       &size,
-		Uid:         &uid,
+		User:        &user,
 	}, nil
 }
 
