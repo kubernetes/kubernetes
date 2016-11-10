14246a94946ce1a93f1fc8b00be3ea50bb90a42b
nikhiljindal <nikhiljindal@google.com>
2016-11-09 17:50:25 -0800

    Updating federation e2e tests to verify cascading deletion

--- test/e2e/federation-deployment.go
+++ test/e2e/federation-deployment.go
@@ -19,9 +19,10 @@ package e2e
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
-	"k8s.io/kubernetes/federation/client/clientset_generated/federation_release_1_5"
+	fedclientset "k8s.io/kubernetes/federation/client/clientset_generated/federation_release_1_5"
 	fedutil "k8s.io/kubernetes/federation/pkg/federation-controller/util"
 	"k8s.io/kubernetes/pkg/api/unversioned"
 	"k8s.io/kubernetes/pkg/api/v1"
@@ -49,14 +50,9 @@ var _ = framework.KubeDescribe("Federation deployments [Feature:Federation]", fu
 		AfterEach(func() {
 			framework.SkipUnlessFederated(f.ClientSet)
 
-			// Delete registered deployments.
+			// Delete all deployments.
 			nsName := f.FederationNamespace.Name
-			deploymentList, err := f.FederationClientset_1_5.Extensions().Deployments(nsName).List(v1.ListOptions{})
-			Expect(err).NotTo(HaveOccurred())
-			for _, deployment := range deploymentList.Items {
-				err := f.FederationClientset_1_5.Extensions().Deployments(nsName).Delete(deployment.Name, &v1.DeleteOptions{})
-				Expect(err).NotTo(HaveOccurred())
-			}
+			deleteAllDeploymentsOrFail(f.FederationClientset_1_5, nsName)
 		})
 
 		It("should be created and deleted successfully", func() {
@@ -89,6 +85,8 @@ var _ = framework.KubeDescribe("Federation deployments [Feature:Federation]", fu
 		})
 
 		AfterEach(func() {
+			nsName := f.FederationNamespace.Name
+			deleteAllDeploymentsOrFail(f.FederationClientset_1_5, nsName)
 			unregisterClusters(clusters, f)
 		})
 
@@ -111,25 +109,101 @@ var _ = framework.KubeDescribe("Federation deployments [Feature:Federation]", fu
 			waitForDeploymentOrFail(f.FederationClientset_1_5, nsName, dep.Name, clusters)
 			By(fmt.Sprintf("Successfuly updated and synced deployment %q/%q to clusters", nsName, dep.Name))
 		})
+
+		It("should be deleted from underlying clusters when OrphanDependents is false", func() {
+			framework.SkipUnlessFederated(f.ClientSet)
+			nsName := f.FederationNamespace.Name
+			orphanDependents := false
+			verifyCascadingDeletionForDeployment(f.FederationClientset_1_5, clusters, &orphanDependents, nsName)
+			By(fmt.Sprintf("Verified that deployments were deleted from underlying clusters"))
+		})
+
+		It("should not be deleted from underlying clusters when OrphanDependents is true", func() {
+			framework.SkipUnlessFederated(f.ClientSet)
+			nsName := f.FederationNamespace.Name
+			orphanDependents := true
+			verifyCascadingDeletionForDeployment(f.FederationClientset_1_5, clusters, &orphanDependents, nsName)
+			By(fmt.Sprintf("Verified that deployments were not deleted from underlying clusters"))
+		})
+
+		It("should not be deleted from underlying clusters when OrphanDependents is nil", func() {
+			framework.SkipUnlessFederated(f.ClientSet)
+			nsName := f.FederationNamespace.Name
+			verifyCascadingDeletionForDeployment(f.FederationClientset_1_5, clusters, nil, nsName)
+			By(fmt.Sprintf("Verified that deployments were not deleted from underlying clusters"))
+		})
+
 	})
 })
 
-func waitForDeploymentOrFail(c *federation_release_1_5.Clientset, namespace string, replicaSetName string, clusters map[string]*cluster) {
-	err := waitForDeployment(c, namespace, replicaSetName, clusters)
-	framework.ExpectNoError(err, "Failed to verify deployment %q/%q, err: %v", namespace, replicaSetName, err)
+// deleteAllDeploymentsOrFail deletes all deployments in the given namespace name.
+func deleteAllDeploymentsOrFail(clientset *fedclientset.Clientset, nsName string) {
+	deploymentList, err := clientset.Extensions().Deployments(nsName).List(v1.ListOptions{})
+	Expect(err).NotTo(HaveOccurred())
+	orphanDependents := false
+	for _, deployment := range deploymentList.Items {
+		deleteDeploymentOrFail(clientset, nsName, deployment.Name, &orphanDependents)
+	}
 }
 
-func waitForDeployment(c *federation_release_1_5.Clientset, namespace string, replicaSetName string, clusters map[string]*cluster) error {
+// verifyCascadingDeletionForDeployment verifies that deployments are deleted
+// from underlying clusters when orphan dependents is false and they are not
+// deleted when orphan dependents is true.
+func verifyCascadingDeletionForDeployment(clientset *fedclientset.Clientset, clusters map[string]*cluster, orphanDependents *bool, nsName string) {
+	deployment := createDeploymentOrFail(clientset, nsName)
+	deploymentName := deployment.Name
+	// Check subclusters if the deployment was created there.
+	By(fmt.Sprintf("Waiting for deployment %s to be created in all underlying clusters", deploymentName))
+	err := wait.Poll(5*time.Second, 2*time.Minute, func() (bool, error) {
+		for _, cluster := range clusters {
+			_, err := cluster.Extensions().Deployments(nsName).Get(deploymentName)
+			if err != nil && errors.IsNotFound(err) {
+				return false, nil
+			}
+			if err != nil {
+				return false, err
+			}
+		}
+		return true, nil
+	})
+	framework.ExpectNoError(err, "Not all deployments created")
+
+	By(fmt.Sprintf("Deleting deployment %s", deploymentName))
+	deleteDeploymentOrFail(clientset, nsName, deploymentName, orphanDependents)
+
+	By(fmt.Sprintf("Verifying deployments %s in underlying clusters", deploymentName))
+	errMessages := []string{}
+	// deployment should be present in underlying clusters unless orphanDependents is false.
+	shouldExist := orphanDependents == nil || *orphanDependents == true
+	for clusterName, clusterClientset := range clusters {
+		_, err := clusterClientset.Extensions().Deployments(nsName).Get(deploymentName)
+		if shouldExist && errors.IsNotFound(err) {
+			errMessages = append(errMessages, fmt.Sprintf("unexpected NotFound error for deployment %s in cluster %s, expected deployment to exist", deploymentName, clusterName))
+		} else if shouldExist && !errors.IsNotFound(err) {
+			errMessages = append(errMessages, fmt.Sprintf("expected NotFound error for deployment %s in cluster %s, got error: %v", deploymentName, clusterName, err))
+		}
+	}
+	if len(errMessages) != 0 {
+		framework.Failf("%s", strings.Join(errMessages, "; "))
+	}
+}
+
+func waitForDeploymentOrFail(c *fedclientset.Clientset, namespace string, deploymentName string, clusters map[string]*cluster) {
+	err := waitForDeployment(c, namespace, deploymentName, clusters)
+	framework.ExpectNoError(err, "Failed to verify deployment %q/%q, err: %v", namespace, deploymentName, err)
+}
+
+func waitForDeployment(c *fedclientset.Clientset, namespace string, deploymentName string, clusters map[string]*cluster) error {
 	err := wait.Poll(10*time.Second, FederatedDeploymentTimeout, func() (bool, error) {
-		fdep, err := c.Deployments(namespace).Get(replicaSetName)
+		fdep, err := c.Deployments(namespace).Get(deploymentName)
 		if err != nil {
 			return false, err
 		}
 		specReplicas, statusReplicas := int32(0), int32(0)
 		for _, cluster := range clusters {
-			dep, err := cluster.Deployments(namespace).Get(replicaSetName)
+			dep, err := cluster.Deployments(namespace).Get(deploymentName)
 			if err != nil && !errors.IsNotFound(err) {
-				By(fmt.Sprintf("Failed getting deployment: %q/%q/%q, err: %v", cluster.name, namespace, replicaSetName, err))
+				By(fmt.Sprintf("Failed getting deployment: %q/%q/%q, err: %v", cluster.name, namespace, deploymentName, err))
 				return false, err
 			}
 			if err == nil {
@@ -158,7 +232,7 @@ func equivalentDeployment(fedDeployment, localDeployment *v1beta1.Deployment) bo
 		reflect.DeepEqual(fedDeployment.Spec, localDeploymentSpec)
 }
 
-func createDeploymentOrFail(clientset *federation_release_1_5.Clientset, namespace string) *v1beta1.Deployment {
+func createDeploymentOrFail(clientset *fedclientset.Clientset, namespace string) *v1beta1.Deployment {
 	if clientset == nil || len(namespace) == 0 {
 		Fail(fmt.Sprintf("Internal error: invalid parameters passed to createDeploymentOrFail: clientset: %v, namespace: %v", clientset, namespace))
 	}
@@ -172,7 +246,7 @@ func createDeploymentOrFail(clientset *federation_release_1_5.Clientset, namespa
 	return deployment
 }
 
-func updateDeploymentOrFail(clientset *federation_release_1_5.Clientset, namespace string) *v1beta1.Deployment {
+func updateDeploymentOrFail(clientset *fedclientset.Clientset, namespace string) *v1beta1.Deployment {
 	if clientset == nil || len(namespace) == 0 {
 		Fail(fmt.Sprintf("Internal error: invalid parameters passed to updateDeploymentOrFail: clientset: %v, namespace: %v", clientset, namespace))
 	}
@@ -185,6 +259,24 @@ func updateDeploymentOrFail(clientset *federation_release_1_5.Clientset, namespa
 	By(fmt.Sprintf("Successfully updated federation deployment %q in namespace %q", FederationDeploymentName, namespace))
 
 	return newRs
+}
+
+func deleteDeploymentOrFail(clientset *fedclientset.Clientset, nsName string, deploymentName string, orphanDependents *bool) {
+	By(fmt.Sprintf("Deleting deployment %q in namespace %q", deploymentName, nsName))
+	err := clientset.Extensions().Deployments(nsName).Delete(deploymentName, &v1.DeleteOptions{OrphanDependents: orphanDependents})
+	framework.ExpectNoError(err, "Error deleting deployment %q in namespace %q", deploymentName, nsName)
+
+	// Wait for the deployment to be deleted.
+	err = wait.Poll(5*time.Second, wait.ForeverTestTimeout, func() (bool, error) {
+		_, err := clientset.Extensions().Deployments(nsName).Get(deploymentName)
+		if err != nil && errors.IsNotFound(err) {
+			return true, nil
+		}
+		return false, err
+	})
+	if err != nil {
+		framework.Failf("Error in deleting deployment %s: %v", deploymentName, err)
+	}
 }
 
 func newDeploymentForFed(namespace string, name string, replicas int32) *v1beta1.Deployment {
