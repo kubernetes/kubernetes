888bc5d24bf191ae4c27eddda3bbca4c93c16070
chentao1596 <chen.tao3@zte.com.cn>
2016-11-11 10:43:05 +0800

    config attr has beean changed, new attr shoule be used

--- cluster/ubuntu/util.sh
+++ cluster/ubuntu/util.sh
@@ -296,7 +296,7 @@ KUBELET_OPTS="\
  --logtostderr=true \
  --cluster-dns=${3} \
  --cluster-domain=${4} \
- --config=${5} \
+ --pod-manifest-path=${5} \
  --allow-privileged=${6}
  $cni_opts"
 EOF
@@ -558,7 +558,7 @@ function provision-node() {
       '${MASTER_IP}' \
       '${DNS_SERVER_IP}' \
       '${DNS_DOMAIN}' \
-      '${KUBELET_CONFIG}' \
+      '${KUBELET_POD_MANIFEST_PATH}' \
       '${ALLOW_PRIVILEGED}' \
       '${CNI_PLUGIN_CONF}'
     create-kube-proxy-opts \
@@ -660,7 +660,7 @@ function provision-masterandnode() {
       '${MASTER_IP}' \
       '${DNS_SERVER_IP}' \
       '${DNS_DOMAIN}' \
-      '${KUBELET_CONFIG}' \
+      '${KUBELET_POD_MANIFEST_PATH}' \
       '${ALLOW_PRIVILEGED}' \
       '${CNI_PLUGIN_CONF}'
     create-kube-proxy-opts \
