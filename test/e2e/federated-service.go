ce910a8f6fce10950ba976e32f98717a5bfde0e6
nikhiljindal <nikhiljindal@google.com>
2016-11-23 16:26:02 -0800

    Creating configmap for kube-dns

--- test/e2e/federated-service.go
+++ test/e2e/federated-service.go
@@ -21,6 +21,7 @@ import (
 	"os"
 	"reflect"
 	"strconv"
+	"strings"
 	"time"
 
 	"k8s.io/kubernetes/pkg/api/v1"
@@ -34,6 +35,9 @@ const (
 
 	FederatedServiceName    = "federated-service"
 	FederatedServicePodName = "federated-service-test-pod"
+
+	KubeDNSConfigMapName      = "kube-dns"
+	KubeDNSConfigMapNamespace = "kube-system"
 )
 
 var FederatedServiceLabels = map[string]string{
@@ -122,6 +126,30 @@ var _ = framework.KubeDescribe("[Feature:Federation]", func() {
 				framework.SkipUnlessFederated(f.ClientSet)
 
 				nsName := f.FederationNamespace.Name
+				// Create kube-dns configmap for kube-dns to accept federation queries.
+				federationDomain := os.Getenv("DNS_ZONE_NAME")
+				if federationDomain == "" {
+					framework.Failf("missing required env var DNS_ZONE_NAME")
+				}
+				// Remove the dot (".") at end, if any.
+				federationDomain = strings.TrimSuffix(federationDomain, ".")
+
+				kubeDNSConfigMap := v1.ConfigMap{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      KubeDNSConfigMapName,
+						Namespace: KubeDNSConfigMapNamespace,
+					},
+					Data: map[string]string{
+						"federations": fmt.Sprintf("%s=%s", federationName, federationDomain),
+					},
+				}
+				// Create this configmap in all clusters.
+				for clusterName, cluster := range clusters {
+					By(fmt.Sprintf("Creating kube dns config map in cluster: %s", clusterName))
+					_, err := cluster.Clientset.Core().ConfigMaps(KubeDNSConfigMapNamespace).Create(&kubeDNSConfigMap)
+					framework.ExpectNoError(err, fmt.Sprintf("Error in creating config map in cluster %s", clusterName))
+				}
+
 				createBackendPodsOrFail(clusters, nsName, FederatedServicePodName)
 				service = createServiceOrFail(f.FederationClientset_1_5, nsName, FederatedServiceName)
 				waitForServiceShardsOrFail(nsName, service, clusters)
@@ -142,6 +170,12 @@ var _ = framework.KubeDescribe("[Feature:Federation]", func() {
 					service = nil
 				} else {
 					By("No service to delete.  Service is nil")
+				}
+				// Delete the kube-dns config map from all clusters.
+				for clusterName, cluster := range clusters {
+					By(fmt.Sprintf("Deleting kube dns config map from cluster: %s", clusterName))
+					err := cluster.Clientset.Core().ConfigMaps(KubeDNSConfigMapNamespace).Delete(KubeDNSConfigMapName, nil)
+					framework.ExpectNoError(err, fmt.Sprintf("Error in deleting config map from cluster %s", clusterName))
 				}
 			})
 
