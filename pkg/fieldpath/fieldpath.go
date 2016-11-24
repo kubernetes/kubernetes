d25e30433d559a2fc76edd74264472e773baf889
timchenxiaoyu <837829664@qq.com>
2016-11-24 11:37:30 +0800

    Update fieldpath.go

--- pkg/fieldpath/fieldpath.go
+++ pkg/fieldpath/fieldpath.go
@@ -27,7 +27,7 @@ import (
 	"k8s.io/kubernetes/pkg/api/resource"
 )
 
-// formatMap formats map[string]string to a string.
+// FormatMap formats map[string]string to a string.
 func FormatMap(m map[string]string) (fmtStr string) {
 	for key, value := range m {
 		fmtStr += fmt.Sprintf("%v=%q\n", key, value)
