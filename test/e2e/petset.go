d2c411aafe560373243a0a0e6f8b2b4ea2f82599
Janet Kuo <chiachenk@google.com>
2016-11-23 16:31:04 -0800

    Revert "Revert "Remove [Slow] from statefulset basic tests""
    This reverts commit ebce195ce1d739ee02a5d842752cf2f9c9778e7c.

--- test/e2e/petset.go
+++ test/e2e/petset.go
@@ -68,10 +68,9 @@ var (
 	StatefulSetGroupVersionResource = unversioned.GroupVersionResource{Group: apps.GroupName, Version: "v1beta1", Resource: "statefulsets"}
 )
 
-// Time: 25m, slow by design.
 // GCE Quota requirements: 3 pds, one per pet manifest declared above.
 // GCE Api requirements: nodes and master need storage r/w permissions.
-var _ = framework.KubeDescribe("StatefulSet [Slow]", func() {
+var _ = framework.KubeDescribe("StatefulSet", func() {
 	f := framework.NewDefaultFramework("statefulset")
 	var ns string
 	var c clientset.Interface
@@ -353,7 +352,7 @@ var _ = framework.KubeDescribe("StatefulSet [Slow]", func() {
 		})
 	})
 
-	framework.KubeDescribe("Deploy clustered applications [Feature:StatefulSet]", func() {
+	framework.KubeDescribe("Deploy clustered applications [Feature:StatefulSet] [Slow]", func() {
 		var pst *statefulSetTester
 		var appTester *clusterAppTester
 
@@ -392,7 +391,7 @@ var _ = framework.KubeDescribe("StatefulSet [Slow]", func() {
 	})
 })
 
-var _ = framework.KubeDescribe("Stateful Set recreate [Slow]", func() {
+var _ = framework.KubeDescribe("Stateful Set recreate", func() {
 	f := framework.NewDefaultFramework("pet-set-recreate")
 	var c clientset.Interface
 	var ns string
