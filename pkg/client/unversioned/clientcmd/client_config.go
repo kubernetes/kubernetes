9e3fa7fa5d0362d41f5168ea7c56f125bd81ff05
juanvallejo <jvallejo@redhat.com>
2016-11-30 14:17:39 -0500

    Add restclientconfig helper fn for parsing timeout
    This patch adds a package `pkg/client/unversioned/clientcmd/util` and
    defines a `ParseTimeout` helper function for parsing time from a
    user-defined string. This allows code re-use in other packages that
    require the creation of a new restclient (and therefore must set the
    `--global-timeout` flag value manually).

--- pkg/client/unversioned/clientcmd/client_config.go
+++ pkg/client/unversioned/clientcmd/client_config.go
@@ -27,13 +27,11 @@ import (
 	"github.com/golang/glog"
 	"github.com/imdario/mergo"
 
-	"strconv"
-	"time"
-
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/client/restclient"
 	clientauth "k8s.io/kubernetes/pkg/client/unversioned/auth"
 	clientcmdapi "k8s.io/kubernetes/pkg/client/unversioned/clientcmd/api"
+	clientcmdutil "k8s.io/kubernetes/pkg/client/unversioned/clientcmd/util"
 )
 
 var (
@@ -129,13 +127,11 @@ func (config *DirectClientConfig) ClientConfig() (*restclient.Config, error) {
 	clientConfig.Host = configClusterInfo.Server
 
 	if len(config.overrides.Timeout) > 0 {
-		if i, err := strconv.ParseInt(config.overrides.Timeout, 10, 64); err == nil && i >= 0 {
-			clientConfig.Timeout = time.Duration(i) * time.Second
-		} else if requestTimeout, err := time.ParseDuration(config.overrides.Timeout); err == nil {
-			clientConfig.Timeout = requestTimeout
-		} else {
-			return nil, fmt.Errorf("Invalid value for option '--request-timeout'. Value must be a single integer, or an integer followed by a corresponding time unit (e.g. 1s | 2m | 3h)")
+		timeout, err := clientcmdutil.ParseTimeout(config.overrides.Timeout)
+		if err != nil {
+			return nil, err
 		}
+		clientConfig.Timeout = timeout
 	}
 
 	if u, err := url.ParseRequestURI(clientConfig.Host); err == nil && u.Opaque == "" && len(u.Path) > 1 {
