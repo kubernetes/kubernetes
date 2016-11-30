e93fc406acaa8778c17020db2c7914e7fd0813ec
Robert Rati <rrati@redhat.com>
2016-11-30 11:51:44 -0500

    [scheduling] Auto-generated file updates from moving node affinity from annotations to api fields. #35518

--- pkg/api/zz_generated.deepcopy.go
+++ pkg/api/zz_generated.deepcopy.go
@@ -2699,6 +2699,15 @@ func DeepCopy_api_PodSpec(in interface{}, out interface{}, c *conversion.Cloner)
 		}
 		out.Hostname = in.Hostname
 		out.Subdomain = in.Subdomain
+		if in.Affinity != nil {
+			in, out := &in.Affinity, &out.Affinity
+			*out = new(Affinity)
+			if err := DeepCopy_api_Affinity(*in, *out, c); err != nil {
+				return err
+			}
+		} else {
+			out.Affinity = nil
+		}
 		return nil
 	}
 }
