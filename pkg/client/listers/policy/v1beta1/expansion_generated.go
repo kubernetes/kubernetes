ad13153491debf312bbc604302bf8779cf353faf
ymqytw <mengqiy@google.com>
2016-12-02 11:19:33 -0800

    copy eviction from internal clientset to release_1_5 clientset

--- pkg/client/listers/policy/v1beta1/expansion_generated.go
+++ pkg/client/listers/policy/v1beta1/expansion_generated.go
@@ -18,6 +18,14 @@ limitations under the License.
 
 package v1beta1
 
+// EvictionListerExpansion allows custom methods to be added to
+// EvictionLister.
+type EvictionListerExpansion interface{}
+
+// EvictionNamespaceListerExpansion allows custom methods to be added to
+// EvictionNamespaeLister.
+type EvictionNamespaceListerExpansion interface{}
+
 // PodDisruptionBudgetListerExpansion allows custom methods to be added to
 // PodDisruptionBudgetLister.
 type PodDisruptionBudgetListerExpansion interface{}
