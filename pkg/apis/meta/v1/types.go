c30862a488919717b49ec5711cb40bc593044569
Clayton Coleman <ccoleman@redhat.com>
2016-12-03 22:42:29 -0500

    Move OwnerReference to pkg/apis/meta/v1 and remove metatypes pkg
    OwnerReference is common.

--- pkg/apis/meta/v1/types.go
+++ pkg/apis/meta/v1/types.go
@@ -30,6 +30,8 @@ import (
 	"strings"
 
 	"github.com/ugorji/go/codec"
+
+	"k8s.io/kubernetes/pkg/types"
 )
 
 // TypeMeta describes an individual object in an API response or request
@@ -69,6 +71,26 @@ type ListMeta struct {
 	// More info: http://releases.k8s.io/HEAD/docs/devel/api-conventions.md#concurrency-control-and-consistency
 	// +optional
 	ResourceVersion string `json:"resourceVersion,omitempty" protobuf:"bytes,2,opt,name=resourceVersion"`
+}
+
+// OwnerReference contains enough information to let you identify an owning
+// object. Currently, an owning object must be in the same namespace, so there
+// is no namespace field.
+type OwnerReference struct {
+	// API version of the referent.
+	APIVersion string `json:"apiVersion" protobuf:"bytes,5,opt,name=apiVersion"`
+	// Kind of the referent.
+	// More info: http://releases.k8s.io/HEAD/docs/devel/api-conventions.md#types-kinds
+	Kind string `json:"kind" protobuf:"bytes,1,opt,name=kind"`
+	// Name of the referent.
+	// More info: http://kubernetes.io/docs/user-guide/identifiers#names
+	Name string `json:"name" protobuf:"bytes,3,opt,name=name"`
+	// UID of the referent.
+	// More info: http://kubernetes.io/docs/user-guide/identifiers#uids
+	UID types.UID `json:"uid" protobuf:"bytes,4,opt,name=uid,casttype=k8s.io/kubernetes/pkg/types.UID"`
+	// If true, this reference points to the managing controller.
+	// +optional
+	Controller *bool `json:"controller,omitempty" protobuf:"varint,6,opt,name=controller"`
 }
 
 // ExportOptions is the query options to the standard REST get call.
--- pkg/controller/garbagecollector/garbagecollector_test.go
+++ pkg/controller/garbagecollector/garbagecollector_test.go
@@ -27,7 +27,6 @@ import (
 
 	_ "k8s.io/kubernetes/pkg/api/install"
 
-	"k8s.io/kubernetes/pkg/api/meta/metatypes"
 	"k8s.io/kubernetes/pkg/api/v1"
 	"k8s.io/kubernetes/pkg/apimachinery/registered"
 	metav1 "k8s.io/kubernetes/pkg/apis/meta/v1"
@@ -170,7 +169,7 @@ func TestProcessItem(t *testing.T) {
 	gc := setupGC(t, clientConfig)
 	item := &node{
 		identity: objectReference{
-			OwnerReference: metatypes.OwnerReference{
+			OwnerReference: metav1.OwnerReference{
 				Kind:       pod.Kind,
 				APIVersion: pod.APIVersion,
 				Name:       pod.Name,
@@ -326,8 +325,8 @@ func TestDependentsRace(t *testing.T) {
 	go func() {
 		for i := 0; i < updates; i++ {
 			dependent := &node{}
-			gc.propagator.addDependentToOwners(dependent, []metatypes.OwnerReference{{UID: ownerUID}})
-			gc.propagator.removeDependentFromOwners(dependent, []metatypes.OwnerReference{{UID: ownerUID}})
+			gc.propagator.addDependentToOwners(dependent, []metav1.OwnerReference{{UID: ownerUID}})
+			gc.propagator.removeDependentFromOwners(dependent, []metav1.OwnerReference{{UID: ownerUID}})
 		}
 	}()
 	go func() {
@@ -366,7 +365,7 @@ func TestGCListWatcher(t *testing.T) {
 func podToGCNode(pod *v1.Pod) *node {
 	return &node{
 		identity: objectReference{
-			OwnerReference: metatypes.OwnerReference{
+			OwnerReference: metav1.OwnerReference{
 				Kind:       pod.Kind,
 				APIVersion: pod.APIVersion,
 				Name:       pod.Name,
