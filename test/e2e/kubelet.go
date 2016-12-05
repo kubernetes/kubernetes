5e66927af20ce33228e02d80ebfce752a0698b65
Jeff Vance <jvance@redhat.com>
2016-12-05 13:14:56 -0800

    test for host cleanup in unfavorable pod deletes

--- test/e2e/kubelet.go
+++ test/e2e/kubelet.go
@@ -18,10 +18,14 @@ package e2e
 
 import (
 	"fmt"
+	"path/filepath"
 	"strings"
 	"time"
 
+	apierrs "k8s.io/kubernetes/pkg/api/errors"
 	"k8s.io/kubernetes/pkg/api/v1"
+	"k8s.io/kubernetes/pkg/apimachinery/registered"
+	metav1 "k8s.io/kubernetes/pkg/apis/meta/v1"
 	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/release_1_5"
 	"k8s.io/kubernetes/pkg/util/sets"
 	"k8s.io/kubernetes/pkg/util/uuid"
@@ -127,8 +131,142 @@ func updateNodeLabels(c clientset.Interface, nodeNames sets.String, toAdd, toRem
 	}
 }
 
+// Calls startVolumeServer to create and run a nfs-server pod. Returns server pod and its
+// ip address.
+// Note: startVolumeServer() waits for the nfs-server pod to be Running and sleeps some
+//   so that the nfs server can start up.
+func createNfsServerPod(c clientset.Interface, config VolumeTestConfig) (*v1.Pod, string) {
+
+	pod := startVolumeServer(c, config)
+	Expect(pod).NotTo(BeNil())
+	ip := pod.Status.PodIP
+	Expect(len(ip)).NotTo(BeZero())
+	framework.Logf("NFS server IP address: %v", ip)
+
+	return pod, ip
+}
+
+// Creates a pod that mounts an nfs volume that is served by the nfs-server pod. Waits
+// for the pod to start.
+// Note that the nfs plugin is defined inline, no PV or PVC.
+func createPodUsingNfs(f *framework.Framework, c clientset.Interface, ns, ip string) *v1.Pod {
+
+	isPrivileged := true
+
+	pod := &v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: registered.GroupOrDie(v1.GroupName).GroupVersion.String(),
+		},
+		ObjectMeta: v1.ObjectMeta{
+			GenerateName: "pod-nfs-vol-",
+			Namespace:    ns,
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:    "pod-nfs-vol",
+					Image:   "gcr.io/google_containers/busybox:1.24",
+					Command: []string{"/bin/sh"},
+					//Note: timing window where nfs-server pod is deleted while/before this pod
+					//  accesses the nfs mount. In this case this pod won't be deleted regardless
+					//  of the wait time. This scenario should be its own It test.
+					///Args:    []string{"-c", "rm /mnt/SUCCESS* && touch /mnt/SUCCESS && cat /mnt/SUCCESS && sleep 6000"},
+					Args: []string{"-c", "sleep 6000"},
+					VolumeMounts: []v1.VolumeMount{
+						{
+							Name:      "nfs-vol",
+							MountPath: "/mnt",
+						},
+					},
+					SecurityContext: &v1.SecurityContext{
+						Privileged: &isPrivileged,
+					},
+				},
+			},
+			RestartPolicy: v1.RestartPolicyOnFailure,
+			Volumes: []v1.Volume{
+				{
+					Name: "nfs-vol",
+					VolumeSource: v1.VolumeSource{
+						NFS: &v1.NFSVolumeSource{
+							Server:   ip,
+							Path:     "/exports",
+							ReadOnly: false,
+						},
+					},
+				},
+			},
+		},
+	}
+	rtnPod, err := c.Core().Pods(ns).Create(pod)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(rtnPod).NotTo(BeNil())
+
+	// wait for pod to be ready
+	err = f.WaitForPodRunningSlow(rtnPod.Name) // longer wait
+	Expect(err).NotTo(HaveOccurred())
+
+	return rtnPod
+}
+
+// Deletes the passed-in pod and conditionally waits for the pod to be terminated.
+func deletePodwithWait(f *framework.Framework, c clientset.Interface, pod *v1.Pod, wait bool) {
+
+	framework.Logf("Deleting pod %v", pod.Name)
+	err := c.Core().Pods(pod.Namespace).Delete(pod.Name, nil)
+	Expect(err).NotTo(HaveOccurred())
+
+	if wait {
+		// wait for pod to terminate. Expect apierr NotFound
+		err = f.WaitForPodTerminated(pod.Name, "")
+		Expect(err).To(HaveOccurred())
+		if !apierrs.IsNotFound(err) {
+			framework.Failf("Unexpected error deleting pod %v: %v", pod.Name, err)
+		}
+		framework.Logf("Pod %v successfully deleted", pod.Name)
+	}
+}
+
+// Checks for a lingering nfs mount and/or uid directory on the passed-in pod's node *after*
+// pod has been deleted. If expectClean is true then we expect the node to be cleaned up and
+// thus commands like `ls <uid-dir>` should fail (since that dir was removed). If expectClean
+// is false then we expect the node is not cleaned up, and thus cmds like `ls <uid-dir>`
+// should succeed.
+// Note: uses ssh to the target node.
+// Note: expects framework.TestContext.Provider to be filled in.
+// Note: since the passed-in pod has been deleted the pod api object is stale.
+func checkPodCleanup(pod *v1.Pod, expectClean bool) {
+
+	check := func(e int) {
+		if expectClean {
+			Expect(e).NotTo(BeZero())
+		} else {
+			Expect(e).To(BeZero())
+		}
+	}
+
+	nodeName := pod.Spec.NodeName
+	if len(nodeName) == 0 {
+		nodeName = pod.Status.PodIP // use ip instead of name
+	}
+	dir := filepath.Join("/var/lib/kubelet/pods", string(pod.UID)) // pod's uid dir
+
+	cmd := fmt.Sprintf("ls %v", dir)
+	result, _ := nodeExec(nodeName, cmd)
+	framework.Logf("ssh execution of cmd %q on node %q\nOutput: %v", cmd, nodeName, result)
+	check(result.Code)
+
+	dir = filepath.Join(dir, "volumes", "kubernetes.io~nfs")
+	cmd = fmt.Sprintf("mount | grep %v", dir)
+	result, _ = nodeExec(nodeName, cmd)
+	framework.Logf("ssh execution of cmd %q on node %q\nOutput: %v", cmd, nodeName, result)
+	check(result.Code)
+}
+
 var _ = framework.KubeDescribe("kubelet", func() {
 	var c clientset.Interface
+	var ns string
 	var numNodes int
 	var nodeNames sets.String
 	var nodeLabels map[string]string
@@ -137,6 +275,7 @@ var _ = framework.KubeDescribe("kubelet", func() {
 
 	BeforeEach(func() {
 		c = f.ClientSet
+		ns = f.Namespace.Name
 		// Use node labels to restrict the pods to be assigned only to the
 		// nodes we observe initially.
 		nodeLabels = make(map[string]string)
@@ -144,6 +283,7 @@ var _ = framework.KubeDescribe("kubelet", func() {
 
 		nodes := framework.GetReadySchedulableNodesOrDie(c)
 		numNodes = len(nodes.Items)
+		Expect(numNodes).NotTo(BeZero())
 		nodeNames = sets.NewString()
 		// If there are a lot of nodes, we don't want to use all of them
 		// (if there are 1000 nodes in the cluster, starting 10 pods/node
@@ -201,7 +341,7 @@ var _ = framework.KubeDescribe("kubelet", func() {
 				// running on the nodes according to kubelet. The timeout is set to
 				// only 30 seconds here because framework.RunRC already waited for all pods to
 				// transition to the running status.
-				Expect(waitTillNPodsRunningOnNodes(f.ClientSet, nodeNames, rcName, f.Namespace.Name, totalPods,
+				Expect(waitTillNPodsRunningOnNodes(f.ClientSet, nodeNames, rcName, ns, totalPods,
 					time.Second*30)).NotTo(HaveOccurred())
 				if resourceMonitor != nil {
 					resourceMonitor.LogLatest()
@@ -217,7 +357,7 @@ var _ = framework.KubeDescribe("kubelet", func() {
 				//   - a bug in graceful termination (if it is enabled)
 				//   - docker slow to delete pods (or resource problems causing slowness)
 				start := time.Now()
-				Expect(waitTillNPodsRunningOnNodes(f.ClientSet, nodeNames, rcName, f.Namespace.Name, 0,
+				Expect(waitTillNPodsRunningOnNodes(f.ClientSet, nodeNames, rcName, ns, 0,
 					itArg.timeout)).NotTo(HaveOccurred())
 				framework.Logf("Deleting %d pods on %d nodes completed in %v after the RC was deleted", totalPods, len(nodeNames),
 					time.Since(start))
@@ -226,5 +366,76 @@ var _ = framework.KubeDescribe("kubelet", func() {
 				}
 			})
 		}
+	})
+
+	//
+	// Delete nfs server pod after another pods accesses the mounted nfs volume.
+	framework.KubeDescribe("Kubelet host cleanup with volume mounts [Jeff]", func() {
+		var (
+			nfsServerPod *v1.Pod
+			nfsIP        string
+		)
+		NFSconfig := VolumeTestConfig{
+			namespace:   v1.NamespaceDefault,
+			prefix:      "nfs",
+			serverImage: "gcr.io/google_containers/volume-nfs:0.7",
+			serverPorts: []int{2049},
+			serverArgs:  []string{"-G", "777", "/exports"},
+		}
+
+		BeforeEach(func() {
+			if nfsServerPod == nil {
+				// Create the nfs server pod in the "default" ns
+				nfsServerPod, nfsIP = createNfsServerPod(c, NFSconfig)
+			}
+		})
+
+		AfterEach(func() {
+			if nfsServerPod != nil {
+				deletePodwithWait(f, c, nfsServerPod, true /*wait*/)
+				nfsServerPod = nil
+			}
+		})
+
+		Context("Host cleanup after pod using NFS mount is deleted", func() {
+			// issue #31272
+			var pod *v1.Pod
+
+			BeforeEach(func() {
+				if pod == nil {
+					pod = createPodUsingNfs(f, c, ns, nfsIP)
+				}
+			})
+
+			AfterEach(func() {
+				if pod != nil {
+					deletePodwithWait(f, c, pod, true /*wait*/)
+				}
+			})
+
+			It("after deleting the nfs-server, the host should be cleaned-up when deleting sleeping pod which mounts an NFS vol", func() {
+				By("Delete the NFS server pod")
+				Expect(nfsServerPod).NotTo(BeNil())
+				deletePodwithWait(f, c, nfsServerPod, true /*wait*/)
+				nfsServerPod = nil
+
+				By("Delete the pod accessing the NFS volume")
+				deletePodwithWait(f, c, pod, true /*wait*/)
+
+				By("Check if host running deleted pod has been cleaned up")
+				checkPodCleanup(pod, false /*expect host not cleaned up*/)
+
+				By("Recreate the nfs server pod")
+				nfsServerPod, nfsIP = createNfsServerPod(c, NFSconfig)
+
+				By("Verify host running the deleted pod is now cleaned up")
+				checkPodCleanup(pod, true /*expect host cleaned up*/)
+
+				By("create a new pod using the same nfs volume")
+				pod = createPodUsingNfs(f, c, ns, nfsIP)
+				// let the AfterEach delete this pod
+			})
+
+		})
 	})
 })
