7e1a24efc0ffb354b1d473f62456fdb6446779b3
Shyam Jeedigunta <shyamjvs@google.com>
2016-12-06 00:20:11 +0100

    Refactored kubemark code into provider-specific and provider-independent parts

--- test/kubemark/cloud-provider-config.sh
+++ test/kubemark/cloud-provider-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2015 The Kubernetes Authors.
+# Copyright 2017 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This script assumes that kubectl binary is present in PATH.
-kubectl config set-cluster hollow-cluster --server=http://localhost:8080 --insecure-skip-tls-verify=true
-kubectl config set-credentials $(whoami)
-kubectl config set-context hollow-context --cluster=hollow-cluster --user=$(whoami)
+CLOUD_PROVIDER="${CLOUD_PROVIDER:-gce}"
