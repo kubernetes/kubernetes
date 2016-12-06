61afd7dd311d0421c24d4db08b264fa924f9a9c0
Dan Williams <dcbw@redhat.com>
2016-12-06 15:58:44 -0600

    dockertools: split out network operations and run them under a mutex
    We need to tear down networking when garbage collecting containers too,
    and GC is run from a different goroutine in kubelet.  We don't want
    container network operations running for the same pod concurrently.

---
+++ pkg/kubelet/dockertools/docker_network.go
@@ -0,0 +1,106 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dockertools
+
+import (
+	"fmt"
+	"sync"
+
+	dockertypes "github.com/docker/engine-api/types"
+	"github.com/golang/glog"
+
+	kubecontainer "k8s.io/kubernetes/pkg/kubelet/container"
+	"k8s.io/kubernetes/pkg/kubelet/network"
+)
+
+type dockerNetwork struct {
+	// Network plugin.
+	networkPlugin network.NetworkPlugin
+
+	// Pod network operations lock
+	mu sync.Mutex
+}
+
+func newDockerNetwork(networkPlugin network.NetworkPlugin) *dockerNetwork {
+	return &dockerNetwork{
+		networkPlugin: networkPlugin,
+	}
+}
+
+// container must not be nil
+func getDockerNetworkMode(container *dockertypes.ContainerJSON) string {
+	if container.HostConfig != nil {
+		return string(container.HostConfig.NetworkMode)
+	}
+	return ""
+}
+
+func (dn *dockerNetwork) pluginDisablesDockerNetworking() bool {
+	return dn.networkPlugin.Name() == "cni" || dn.networkPlugin.Name() == "kubenet"
+}
+
+// determineContainerIP determines the IP address of the given container.  It is expected
+// that the container passed is the infrastructure container of a pod and the responsibility
+// of the caller to ensure that the correct container is passed.
+func (dn *dockerNetwork) determineContainerIP(podNamespace, podName string, container *dockertypes.ContainerJSON) (string, error) {
+	dn.mu.Lock()
+	defer dn.mu.Unlock()
+
+	result := getContainerIP(container)
+
+	networkMode := getDockerNetworkMode(container)
+	isHostNetwork := networkMode == namespaceModeHost
+
+	// For host networking or default network plugin, GetPodNetworkStatus doesn't work
+	if !isHostNetwork && dn.networkPlugin.Name() != network.DefaultPluginName {
+		netStatus, err := dn.networkPlugin.GetPodNetworkStatus(podNamespace, podName, kubecontainer.DockerID(container.ID).ContainerID())
+		if err != nil {
+			return result, fmt.Errorf("NetworkPlugin %s failed on the status hook for pod '%s': %v", dn.networkPlugin.Name(), podName, err)
+		} else if netStatus != nil {
+			result = netStatus.IP.String()
+		}
+	}
+
+	return result, nil
+}
+
+func (dn *dockerNetwork) setUpPod(podNamespace, podName string, containerID kubecontainer.ContainerID) error {
+	dn.mu.Lock()
+	defer dn.mu.Unlock()
+
+	fullPodName := kubecontainer.BuildPodFullName(podName, podNamespace)
+	glog.V(3).Infof("Calling network plugin %s to setup pod for %s", dn.networkPlugin.Name(), fullPodName)
+	if err := dn.networkPlugin.SetUpPod(podNamespace, podName, containerID); err != nil {
+		// TODO: (random-liu) There shouldn't be "Skipping pod" in sync result message
+		return fmt.Errorf("Failed to setup network for pod %q using network plugins %q: %v; Skipping pod", fullPodName, dn.networkPlugin.Name(), err)
+	}
+
+	return nil
+}
+
+func (dn *dockerNetwork) tearDownPod(podNamespace, podName string, containerID kubecontainer.ContainerID) error {
+	dn.mu.Lock()
+	defer dn.mu.Unlock()
+
+	fullPodName := kubecontainer.BuildPodFullName(podName, podNamespace)
+	glog.V(3).Infof("Calling network plugin %s to tear down pod for %s", dn.networkPlugin.Name(), fullPodName)
+	if err := dn.networkPlugin.TearDownPod(podNamespace, podName, containerID); err != nil {
+		return fmt.Errorf("Failed to teardown network for pod %q using network plugins %q: %v", fullPodName, dn.networkPlugin.Name(), err)
+	}
+
+	return nil
+}
