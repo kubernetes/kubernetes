d3da5947109aed1ac594e7a4e74aadc59c6dc5e2
Morgan Bauer <mbauer@us.ibm.com>
2016-12-06 14:53:59 -0800

    documentation string for DefaultExternalAddress

--- pkg/genericapiserver/options/server_run_options.go
+++ pkg/genericapiserver/options/server_run_options.go
@@ -85,6 +85,10 @@ func NewServerRunOptions() *ServerRunOptions {
 	}
 }
 
+// DefaultAdvertiseAddress sets the field AdvertiseAddress if
+// unset. The field will be set based on the SecureServingOptions. If
+// the SecureServingOptions is not present, DefaultExternalAddress
+// will fall back to the insecure ServingOptions.
 func (s *ServerRunOptions) DefaultAdvertiseAddress(secure *SecureServingOptions, insecure *ServingOptions) error {
 	if s.AdvertiseAddress == nil || s.AdvertiseAddress.IsUnspecified() {
 		switch {
