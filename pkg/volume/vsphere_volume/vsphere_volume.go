4d57ad6fca1bd90768edb65e859a4067d192cd18
Balu Dontu <bdontu@vmware.com>
2016-12-07 16:15:51 -0800

    Fix space in volumePath in vSphere

--- pkg/volume/vsphere_volume/vsphere_volume.go
+++ pkg/volume/vsphere_volume/vsphere_volume.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/golang/glog"
 	"k8s.io/kubernetes/pkg/api/resource"
@@ -125,6 +126,7 @@ func (plugin *vsphereVolumePlugin) ConstructVolumeSpec(volumeName, mountPath str
 	if err != nil {
 		return nil, err
 	}
+	volumePath = strings.Replace(volumePath, "\\040", " ", -1)
 	glog.V(5).Infof("vSphere volume path is %q", volumePath)
 	vsphereVolume := &v1.Volume{
 		Name: volumeName,
