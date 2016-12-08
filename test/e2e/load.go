c9e78f1cd5eefd064e4943811871b0839a5534c6
gmarek <gmarek@google.com>
2016-12-08 15:13:45 +0100

    Add an option to run Job in Density/Load config

--- test/e2e/load.go
+++ test/e2e/load.go
@@ -29,6 +29,7 @@ import (
 
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/v1"
+	"k8s.io/kubernetes/pkg/apis/batch"
 	"k8s.io/kubernetes/pkg/apis/extensions"
 	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/release_1_5"
@@ -394,6 +395,8 @@ func generateConfigsForGroup(
 			config = &testutils.ReplicaSetConfig{RCConfig: *baseConfig}
 		case extensions.Kind("Deployment"):
 			config = &testutils.DeploymentConfig{RCConfig: *baseConfig}
+		case batch.Kind("Job"):
+			config = &testutils.JobConfig{RCConfig: *baseConfig}
 		default:
 			framework.Failf("Unsupported kind for config creation: %v", kind)
 		}
