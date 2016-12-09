0c23667fe9e65492b53c8fafc295076722c1e2a2
Jeff Grafton <jgrafton@google.com>
2016-12-09 11:28:45 -0800

    Don't require docker or other tools when running `make clean`

--- build/make-clean.sh
+++ build/make-clean.sh
@@ -22,5 +22,5 @@ set -o pipefail
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
 source "${KUBE_ROOT}/build/common.sh"
 
-kube::build::verify_prereqs
+kube::build::verify_prereqs false
 kube::build::clean
