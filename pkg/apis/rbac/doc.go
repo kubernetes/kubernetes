577b8aa63d2dbd285977944066ed13d44a5b7011
Jordan Liggitt <jliggitt@redhat.com>
2016-12-09 02:39:04 -0500

    stop generating openapi schemas for internal types

--- pkg/apis/rbac/doc.go
+++ pkg/apis/rbac/doc.go
@@ -15,7 +15,5 @@ limitations under the License.
 */
 
 // +k8s:deepcopy-gen=package,register
-// +k8s:openapi-gen=true
-
 // +groupName=rbac.authorization.k8s.io
 package rbac // import "k8s.io/kubernetes/pkg/apis/rbac"
