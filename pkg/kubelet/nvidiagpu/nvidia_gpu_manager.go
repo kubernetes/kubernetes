370372d91ec0748c260bffcf4ca0457289e8495f
Hui-Zhi <zhaohuizhi@gmail.com>
2016-12-12 16:37:54 +0800

    Correct the allocable GPUs number

--- pkg/kubelet/nvidiagpu/nvidia_gpu_manager.go
+++ pkg/kubelet/nvidiagpu/nvidia_gpu_manager.go
@@ -122,7 +122,7 @@ func (ngm *NvidiaGPUManager) isAvailable(path string) bool {
 		}
 
 		for _, device := range devices {
-			if Valid(device.PathOnHost) {
+			if Valid(device.PathOnHost) && path == device.PathOnhost {
 				return false
 			}
 		}
