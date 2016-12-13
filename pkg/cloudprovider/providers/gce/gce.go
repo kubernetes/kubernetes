d10ef08d6351622c0a762c8a0777d51d8f9b611b
gmarek <gmarek@google.com>
2016-12-13 15:38:57 +0100

    Don't retry creating route if it already exists

--- pkg/cloudprovider/providers/gce/gce.go
+++ pkg/cloudprovider/providers/gce/gce.go
@@ -2400,7 +2400,12 @@ func (gce *GCECloud) CreateRoute(clusterName string, nameHint string, route *clo
 		Description:     k8sNodeRouteTag,
 	}).Do()
 	if err != nil {
-		return err
+		if !isHTTPErrorCode(err, http.StatusConflict) {
+			return err
+		} else {
+			glog.Info("Route %v already exists.")
+			return nil
+		}
 	}
 	return gce.waitForGlobalOp(insertOp)
 }
