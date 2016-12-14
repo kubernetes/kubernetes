9d9c5bc88d0409569dccf7692a5d783bc60af0c0
Brendan Burns <bburns@google.com>
2016-12-13 23:18:11 -0800

    Add initial translation support.

--- hack/boilerplate/boilerplate.py
+++ hack/boilerplate/boilerplate.py
@@ -130,7 +130,8 @@ def file_extension(filename):
     return os.path.splitext(filename)[1].split(".")[-1].lower()
 
 skipped_dirs = ['Godeps', 'third_party', '_gopath', '_output', '.git', 'cluster/env.sh',
-                "vendor", "test/e2e/generated/bindata.go", "hack/boilerplate/test"]
+                "vendor", "test/e2e/generated/bindata.go", "hack/boilerplate/test",
+                "pkg/generated/bindata.go"]
 
 def normalize_files(files):
     newfiles = []
