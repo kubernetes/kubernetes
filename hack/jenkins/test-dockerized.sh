c1290ca956f05b401e295c66e25cea36cc69c60a
Joe Finney <spxtr@google.com>
2016-12-21 14:35:58 -0800

    Stop running most unit tests outside of bazel.

--- hack/jenkins/test-dockerized.sh
+++ hack/jenkins/test-dockerized.sh
@@ -50,13 +50,12 @@ export KUBE_INTEGRATION_TEST_MAX_CONCURRENCY=4
 export LOG_LEVEL=4
 
 cd /go/src/k8s.io/kubernetes
-rm -rf Godeps/_workspace # Temporary until _workspace is fully obliterated
 
 make generated_files
 go install ./cmd/...
 ./hack/install-etcd.sh
 
-make test
+make test WHAT="cmd/kubeadm/test cmd/libs/go2idl/openapi-gen/generators"
 make test-cmd
 make test-integration
 ./hack/test-update-storage-objects.sh
