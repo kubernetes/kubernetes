e0b8ca87127f5ee5516ac14d2d1c19af7b92c8e2
deads2k <deads@redhat.com>
2016-12-21 10:58:43 -0500

    make bazel happy

--- pkg/kubeapiserver/authorizer/config_test.go
+++ pkg/kubeapiserver/authorizer/config_test.go
@@ -18,6 +18,9 @@ package authorizer
 
 import (
 	"testing"
+
+	// needed to keep bazel happy
+	_ "k8s.io/kubernetes/pkg/auth/authorizer/abac"
 )
 
 // New has multiple return possibilities. This test
