0c7bf6d6f957736bab39841727b6ece7bdbd271f
Irfan Ur Rehman <irfan.rehman@huawei.com>
2016-12-27 22:23:46 +0530

    [Federation] Review comment fixes for wait for control plane pods in kubefed init

--- federation/pkg/kubefed/testing/testing.go
+++ federation/pkg/kubefed/testing/testing.go
@@ -23,6 +23,7 @@ import (
 	"net/http"
 	"os"
 
+	fedclient "k8s.io/kubernetes/federation/client/clientset_generated/federation_clientset"
 	"k8s.io/kubernetes/federation/pkg/kubefed/util"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/apimachinery/registered"
@@ -51,6 +52,18 @@ func NewFakeAdminConfig(f cmdutil.Factory, kubeconfigGlobal string) (util.AdminC
 
 func (f *fakeAdminConfig) PathOptions() *clientcmd.PathOptions {
 	return f.pathOptions
+}
+
+func (f *fakeAdminConfig) FederationClientset(context, kubeconfigPath string) (*fedclient.Clientset, error) {
+	fakeRestClient, err := f.hostFactory.RESTClient()
+	if err != nil {
+		return nil, err
+	}
+
+	// we ignore the function params and use the client from
+	// the same fakefactory to create a federation clientset
+	// our fake factory exposes only the healthz api for this client
+	return fedclient.New(fakeRestClient), nil
 }
 
 func (f *fakeAdminConfig) HostFactory(host, kubeconfigPath string) cmdutil.Factory {
