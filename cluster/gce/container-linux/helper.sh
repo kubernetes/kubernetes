a2bde374b97da83e52f40a5f1ffb0a6d484c92d7
Yifan Gu <yifan.gu@coreos.com>
2016-12-29 15:10:06 -0800

    cluster/gce: Rename coreos to container-linux.

--- cluster/gce/container-linux/helper.sh
+++ cluster/gce/container-linux/helper.sh
@@ -14,6 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# A library of helper functions and constants for the CoreOS distro
+# A library of helper functions and constants for the Container Linux distro.
 
 # This file intentionally left blank
