39198e06957346d3a1ce49eed4da867b769f1b81
NickrenREN <yuquan.ren@easystack.cn>
2016-12-30 17:10:37 +0800

    remove never used var LargeClusterThreshold is never used now,remove it

--- pkg/controller/node/controller_utils.go
+++ pkg/controller/node/controller_utils.go
@@ -38,11 +38,6 @@ import (
 	"github.com/golang/glog"
 )
 
-const (
-	// Number of Nodes that needs to be in the cluster for it to be treated as "large"
-	LargeClusterThreshold = 20
-)
-
 // deletePods will delete all pods from master running on given node, and return true
 // if any pods were deleted, or were found pending deletion.
 func deletePods(kubeClient clientset.Interface, recorder record.EventRecorder, nodeName, nodeUID string, daemonStore cache.StoreToDaemonSetLister) (bool, error) {
