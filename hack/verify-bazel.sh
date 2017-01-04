ab0c903c7f52f06332015d5792f6890b9976904f
Jeff Grafton <jgrafton@google.com>
2017-01-04 15:41:38 -0800

    Update to gazel v13

--- hack/verify-bazel.sh
+++ hack/verify-bazel.sh
@@ -20,10 +20,9 @@ set -o pipefail
 export KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
 source "${KUBE_ROOT}/hack/lib/init.sh"
 
-go get -u gopkg.in/mikedanese/gazel.v8/gazel
-if [[ $("${GOPATH}/bin/gazel" -dry-run -print-diff -root="$(kube::realpath ${KUBE_ROOT})" 2>&1 | tee /dev/stderr | wc -l | tr -d '[:space:]') != 0 ]]; then
+go get -u gopkg.in/mikedanese/gazel.v13/gazel
+if ! "${GOPATH}/bin/gazel" -validate -print-diff -root="$(kube::realpath ${KUBE_ROOT})" ; then
   echo
-  echo "BUILD files are not up to date"
   echo "Run ./hack/update-bazel.sh"
   exit 1
 fi
