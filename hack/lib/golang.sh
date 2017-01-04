266bf8040484a7713e0c987cf423fdf692ca02d2
Bowei Du <bowei@google.com>
2017-01-04 13:33:07 -0800

    Remove kube-dns from golang.sh

--- hack/lib/golang.sh
+++ hack/lib/golang.sh
@@ -188,7 +188,6 @@ readonly KUBE_ALL_BINARIES=("${KUBE_ALL_TARGETS[@]##*/}")
 readonly KUBE_STATIC_LIBRARIES=(
   kube-apiserver
   kube-controller-manager
-  kube-dns
   kube-scheduler
   kube-proxy
   kube-discovery
