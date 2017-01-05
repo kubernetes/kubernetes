03a6ed5c307eb8a07a28f40c20e941e9aaa47323
Slava Semushin <vsemushi@redhat.com>
2017-01-05 18:07:50 +0100

    Improve an error message when privileged containers are disallowed globally on the cluster.

--- pkg/api/validation/validation_test.go
+++ pkg/api/validation/validation_test.go
@@ -8601,7 +8601,7 @@ func TestValidateSecurityContext(t *testing.T) {
 		"request privileged when capabilities forbids": {
 			sc:          privRequestWithGlobalDeny,
 			errorType:   "FieldValueForbidden",
-			errorDetail: "disallowed by policy",
+			errorDetail: "disallowed by cluster policy",
 		},
 		"negative RunAsUser": {
 			sc:          negativeRunAsUser,
