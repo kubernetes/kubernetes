7b957c2192d4302bb5bfe992a760a7b652375ca4
Dominika Hodovska <dhodovsk@redhat.com>
2017-01-06 12:05:54 +0100

    Show InitContainers and InitContainerStatus in PodSpec

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -1853,8 +1853,6 @@ const (
 // PodSpec is a description of a pod
 type PodSpec struct {
 	Volumes []Volume
-	// List of initialization containers belonging to the pod.
-	InitContainers []Container
 	// List of containers belonging to the pod.
 	Containers []Container
 	// +optional
@@ -1911,6 +1909,8 @@ type PodSpec struct {
 	// If not specified, the pod will be dispatched by default scheduler.
 	// +optional
 	SchedulerName string
+	// List of initialization containers belonging to the pod.
+	InitContainers []Container
 }
 
 // Sysctl defines a kernel parameter to be set
@@ -2019,11 +2019,6 @@ type PodStatus struct {
 	// +optional
 	QOSClass PodQOSClass
 
-	// The list has one entry per init container in the manifest. The most recent successful
-	// init container will have ready = true, the most recently started container will have
-	// startTime set.
-	// More info: http://kubernetes.io/docs/user-guide/pod-states#container-statuses
-	InitContainerStatuses []ContainerStatus
 	// The list has one entry per container in the manifest. Each entry is
 	// currently the output of `docker inspect`. This output format is *not*
 	// final and should not be relied upon.
@@ -2031,6 +2026,11 @@ type PodStatus struct {
 	// when we have done this.
 	// +optional
 	ContainerStatuses []ContainerStatus
+	// The list has one entry per init container in the manifest. The most recent successful
+	// init container will have ready = true, the most recently started container will have
+	// startTime set.
+	// More info: http://kubernetes.io/docs/user-guide/pod-states#container-statuses
+	InitContainerStatuses []ContainerStatus
 }
 
 // PodStatusResult is a wrapper for PodStatus returned by kubelet that can be encode/decoded
