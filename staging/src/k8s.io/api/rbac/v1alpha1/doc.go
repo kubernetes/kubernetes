ee3894dcf2d0dd7a7a64e483c620ae97f4c0efac
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-01-08 14:16:16 +0100

    deepcopy: remove register annotations

--- staging/src/k8s.io/api/rbac/v1alpha1/doc.go
+++ staging/src/k8s.io/api/rbac/v1alpha1/doc.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:deepcopy-gen=package,register
+// +k8s:deepcopy-gen=package
 // +k8s:openapi-gen=true
 
 // +groupName=rbac.authorization.k8s.io
--- staging/src/k8s.io/kube-gen/cmd/client-gen/test_apis/testgroup/doc.go
+++ staging/src/k8s.io/kube-gen/cmd/client-gen/test_apis/testgroup/doc.go
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:deepcopy-gen=package,register
+// +k8s:deepcopy-gen=package
 // +groupName=testgroup.k8s.io
 package testgroup // import "k8s.io/kube-gen/cmd/client-gen/test_apis/testgroup"
