e8316652d6adf66376c8c0b38a792fc7c42f5b0d
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-01-08 14:16:16 +0100

    deepcopy: remove register annotations

--- pkg/apis/authentication/v1/doc.go
+++ pkg/apis/authentication/v1/doc.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:deepcopy-gen=package,register
+// +k8s:deepcopy-gen=package
 // +k8s:conversion-gen=k8s.io/kubernetes/pkg/apis/authentication
 // +groupName=authentication.k8s.io
 // +k8s:openapi-gen=true
--- pkg/apis/autoscaling/v2alpha1/doc.go
+++ pkg/apis/autoscaling/v2alpha1/doc.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:deepcopy-gen=package,register
+// +k8s:deepcopy-gen=package
 // +k8s:conversion-gen=k8s.io/kubernetes/pkg/apis/autoscaling
 // +k8s:openapi-gen=true
 
