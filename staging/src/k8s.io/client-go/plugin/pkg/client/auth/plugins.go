a0cebcb559c5c0ab8a2e50b1ee11cc62f9ebb3a8
zhouhaibing089 <zhouhaibing089@gmail.com>
2017-01-09 09:52:46 +0800

    plugin/pkg/client/auth: add openstack auth provider

--- staging/src/k8s.io/client-go/plugin/pkg/client/auth/plugins.go
+++ staging/src/k8s.io/client-go/plugin/pkg/client/auth/plugins.go
@@ -21,4 +21,5 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
