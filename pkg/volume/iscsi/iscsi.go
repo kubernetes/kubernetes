6c9785352a8af2370e90db8ec83d3a666953cf76
rkouj <rkouj@google.com>
2017-01-11 14:27:22 -0800

    Check if path exists before performing unmount

--- pkg/volume/iscsi/iscsi.go
+++ pkg/volume/iscsi/iscsi.go
@@ -230,6 +230,12 @@ func (c *iscsiDiskUnmounter) TearDown() error {
 }
 
 func (c *iscsiDiskUnmounter) TearDownAt(dir string) error {
+	if pathExists, pathErr := ioutil.PathExists(dir); pathErr != nil {
+		return fmt.Errorf("Error checking if path exists: %v", pathErr)
+	} else if !pathExists {
+		glog.Warningf("Warning: Unmount skipped because path does not exist: %v", dir)
+		return nil
+	}
 	return diskTearDown(c.manager, *c, dir, c.mounter)
 }
 
