b94294e429dc5df46471ac0e1db7481c3aaf181a
zhouhaibing089 <zhouhaibing089@gmail.com>
2017-01-11 09:03:11 +0800

    test/e2e/third-party.go: add import of pkg/api/v1

--- test/e2e/third-party.go
+++ test/e2e/third-party.go
@@ -24,6 +24,7 @@ import (
 
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/api/v1"
 	"k8s.io/kubernetes/pkg/apimachinery/registered"
 	"k8s.io/kubernetes/pkg/apis/extensions"
 	"k8s.io/kubernetes/pkg/runtime"
