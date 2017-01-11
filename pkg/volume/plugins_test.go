1e3049cf2bb2976e11843e07d1fbcb3420dd376e
Tomas Smetana <tsmetana@redhat.com>
2017-01-11 14:17:30 +0100

    Add ConstructVolumeFromSpecName also to the volume tests interfaces

--- pkg/volume/plugins_test.go
+++ pkg/volume/plugins_test.go
@@ -84,6 +84,10 @@ func (plugin *testPlugins) NewUnmounter(name string, podUID types.UID) (Unmounte
 	return nil, nil
 }
 
+func (plugin *testPlugins) ConstructVolumeSpecFromName(volumeName string) (*Spec, error) {
+	return nil, nil
+}
+
 func (plugin *testPlugins) ConstructVolumeSpec(volumeName, mountPath string) (*Spec, error) {
 	return nil, nil
 }
