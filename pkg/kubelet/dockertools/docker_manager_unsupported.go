970bc61534fc9744069145368af345691ed767e2
Paulo Pires <pires@apprenda.com>
2017-01-11 10:37:53 +0000

    dockertools: fix Windows implementation. Fixes #39005 Fixes #39714
    Conflicts:
    	pkg/kubelet/dockertools/docker_manager_unsupported.go
    	pkg/kubelet/dockertools/docker_manager_windows.go

--- pkg/kubelet/dockertools/docker_manager_unsupported.go
+++ pkg/kubelet/dockertools/docker_manager_unsupported.go
@@ -22,10 +22,14 @@ import (
 	"k8s.io/kubernetes/pkg/api"
 
 	dockertypes "github.com/docker/engine-api/types"
+	dockercontainer "github.com/docker/engine-api/types/container"
+
+	"k8s.io/kubernetes/pkg/api/v1"
+	kubecontainer "k8s.io/kubernetes/pkg/kubelet/container"
 )
 
 // These two functions are OS specific (for now at least)
-func updateHostConfig(config *dockercontainer.HostConfig) {
+func updateHostConfig(hc *dockercontainer.HostConfig, opts *kubecontainer.RunContainerOptions) {
 }
 
 func DefaultMemorySwap() int64 {
