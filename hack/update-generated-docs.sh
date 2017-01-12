b7f574cdf549713bea527e225d3dcf27c88c0836
David McMahon <djmm@google.com>
2017-01-12 15:34:03 -0800

    Complete *-munge-docs.sh deprecation.

--- hack/update-generated-docs.sh
+++ hack/update-generated-docs.sh
@@ -51,7 +51,3 @@ shopt -u dotglob
 
 # Replace with placeholder docs
 kube::util::set-placeholder-gen-docs
-
-echo "Generated docs have been placed in the repository tree. Running hack/update-munge-docs.sh."
-
-"${KUBE_ROOT}/hack/update-munge-docs.sh"
