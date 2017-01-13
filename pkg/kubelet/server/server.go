645db81f51f50bf1083840bb78638012fa5453b9
foxyriver <liu.hao89@zte.com.cn>
2017-01-13 09:27:23 +0800

    modify-comment

--- pkg/kubelet/server/server.go
+++ pkg/kubelet/server/server.go
@@ -285,7 +285,7 @@ func (s *Server) InstallDefaultHandlers() {
 
 const pprofBasePath = "/debug/pprof/"
 
-// InstallDeguggingHandlers registers the HTTP request patterns that serve logs or run commands/containers
+// InstallDebuggingHandlers registers the HTTP request patterns that serve logs or run commands/containers
 func (s *Server) InstallDebuggingHandlers(criHandler http.Handler) {
 	var ws *restful.WebService
 
