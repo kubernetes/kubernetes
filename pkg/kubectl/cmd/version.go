15d52614f4ac7f86ae32afb4cf1d0b2ca132e25f
Alejandro Escobar <jaescobar.cell@gmail.com>
2017-01-13 20:37:57 -0800

    made changes according to pr suggestions

--- pkg/kubectl/cmd/version.go
+++ pkg/kubectl/cmd/version.go
@@ -62,85 +62,60 @@ func NewCmdVersion(f cmdutil.Factory, out io.Writer) *cobra.Command {
 
 func RunVersion(f cmdutil.Factory, out io.Writer, cmd *cobra.Command) error {
 	of := strings.ToLower(cmdutil.GetFlagString(cmd, "output"))
+	vo := VersionObj{nil, nil}
+
+	cvg := version.Get()
+	if cmdutil.GetFlagBool(cmd, "short") {
+		cvg = version.Info{GitVersion: version.Get().GitVersion}
+	}
+	vo.ClientVersion = &cvg
+
+	if !cmdutil.GetFlagBool(cmd, "client") {
+		clientSet, err := f.ClientSet()
+		if err != nil {
+			return err
+		}
+
+		serverVersion, err := clientSet.Discovery().ServerVersion()
+		if err != nil {
+			return err
+		}
+
+		sv := serverVersion
+		if cmdutil.GetFlagBool(cmd, "short") {
+			sv = &version.Info{GitVersion: serverVersion.GitVersion}
+		}
+		vo.ServerVersion = sv
+	}
 
 	switch {
 	case len(of) == 0:
-		v := fmt.Sprintf("%#v", version.Get())
-		if cmdutil.GetFlagBool(cmd, "short") {
-			v = version.Get().GitVersion
-		}
-
-		fmt.Fprintf(out, "Client Version: %s\n", v)
+		fmt.Fprintf(out, "Client Version: %s\n", fmt.Sprintf("%#v", *vo.ClientVersion))
 		if cmdutil.GetFlagBool(cmd, "client") {
 			return nil
 		}
 
-		clientSet, err := f.ClientSet()
-		if err != nil {
-			return err
-		}
-
-		serverVersion, err := clientSet.Discovery().ServerVersion()
-		if err != nil {
-			return err
-		}
-
-		v = fmt.Sprintf("%#v", *serverVersion)
-		if cmdutil.GetFlagBool(cmd, "short") {
-			v = serverVersion.GitVersion
-		}
-
-		fmt.Fprintf(out, "Server Version: %s\n", v)
+		fmt.Fprintf(out, "Server Version: %s\n", fmt.Sprintf("%#v", *vo.ServerVersion))
 		return nil
-	case of == "yaml" || of == "json":
-		cv := version.Get()
-		if cmdutil.GetFlagBool(cmd, "short") {
-			cv = version.Info{GitVersion: version.Get().GitVersion}
-		}
-
-		if cmdutil.GetFlagBool(cmd, "client") {
-			return printRightFormat(out, of, VersionObj{&cv, nil})
-		}
-
-		clientSet, err := f.ClientSet()
-		if err != nil {
-			return err
-		}
-
-		serverVersion, err := clientSet.Discovery().ServerVersion()
-		if err != nil {
-			return err
-		}
-
-		sv := *serverVersion
-		if cmdutil.GetFlagBool(cmd, "short") {
-			sv = version.Info{GitVersion: serverVersion.GitVersion}
-		}
-
-		return printRightFormat(out, of, VersionObj{&cv, &sv})
-	default:
-		return errors.New("invalid output format: " + of)
-
-	}
-}
-
-func printRightFormat(out io.Writer, outputFormat string, vo interface{}) error {
-	switch outputFormat {
-	case "yaml":
+	case of == "yaml":
 		y, err := yaml.Marshal(&vo)
 		if err != nil {
 			return err
 		}
+
 		fmt.Fprintf(out, string(y))
-	case "json":
+
+		return nil
+	case of == "json":
 		y, err := json.Marshal(&vo)
 		if err != nil {
 			return err
 		}
 		fmt.Fprintf(out, string(y))
-	default:
-		return errors.New("unexpected output format!")
-	}
 
-	return nil
+		return nil
+	default:
+		return errors.New("invalid output format: " + of)
+
+	}
 }
