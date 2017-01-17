5a5d31555b3a754e727b7ae96116fcee6bd8ff6b
Clayton Coleman <ccoleman@redhat.com>
2017-01-17 12:26:09 -0500

    Swagger doc generation needs to work on meta/v1

--- hack/lib/util.sh
+++ hack/lib/util.sh
@@ -324,6 +324,9 @@ kube::util::group-version-to-pkg-path() {
     meta/v1)
       echo "vendor/k8s.io/apimachinery/pkg/apis/meta/v1"
       ;;
+    meta/v1)
+      echo "../vendor/k8s.io/apimachinery/pkg/apis/meta/v1"
+      ;;
     unversioned)
       echo "pkg/api/unversioned"
       ;;
