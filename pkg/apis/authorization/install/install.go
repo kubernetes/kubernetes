7cc9702402fe338e14650d49f489be14ea0b5136
deads2k <deads@redhat.com>
2017-01-20 16:28:48 -0500

    separate announce factories

--- pkg/apis/authorization/install/install.go
+++ pkg/apis/authorization/install/install.go
@@ -29,11 +29,11 @@ import (
 )
 
 func init() {
-	Install(api.Registry, api.Scheme)
+	Install(api.GroupFactoryRegistry, api.Registry, api.Scheme)
 }
 
 // Install registers the API group and adds types to a scheme
-func Install(registry *registered.APIRegistrationManager, scheme *runtime.Scheme) {
+func Install(groupFactoryRegistry announced.APIGroupFactoryRegistry, registry *registered.APIRegistrationManager, scheme *runtime.Scheme) {
 	if err := announced.NewGroupMetaFactory(
 		&announced.GroupMetaFactoryArgs{
 			GroupName:                  authorization.GroupName,
@@ -45,7 +45,7 @@ func Install(registry *registered.APIRegistrationManager, scheme *runtime.Scheme
 		announced.VersionToSchemeFunc{
 			v1beta1.SchemeGroupVersion.Version: v1beta1.AddToScheme,
 		},
-	).Announce().RegisterAndEnable(registry, scheme); err != nil {
+	).Announce(groupFactoryRegistry).RegisterAndEnable(registry, scheme); err != nil {
 		panic(err)
 	}
 }
