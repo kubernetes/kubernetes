c0a206b76210a994d5d1cd55ab998045c8f6977d
David Ashpole <dashpole@google.com>
2017-01-20 11:57:24 -0800

    dont delete pods until volumes are deleted

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -716,7 +716,7 @@ func NewMainKubelet(kubeCfg *componentconfig.KubeletConfiguration, kubeDeps *Kub
 	}
 	klet.imageManager = imageManager
 
-	klet.statusManager = status.NewManager(kubeClient, klet.podManager)
+	klet.statusManager = status.NewManager(kubeClient, klet.podManager, klet.safeToDeletePod)
 
 	klet.probeManager = prober.NewManager(
 		klet.statusManager,
