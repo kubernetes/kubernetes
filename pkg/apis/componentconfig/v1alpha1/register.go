838fa7eb71551b14542819ee3fe6faef7c0f4fc2
Derek Carr <decarr@redhat.com>
2017-01-20 17:27:03 -0500

    Add ResourceQuota component configuration

--- pkg/apis/componentconfig/v1alpha1/register.go
+++ pkg/apis/componentconfig/v1alpha1/register.go
@@ -38,6 +38,7 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&KubeSchedulerConfiguration{},
 		&KubeletConfiguration{},
 		&AdmissionConfiguration{},
+		&ResourceQuotaConfiguration{},
 	)
 	return nil
 }
@@ -46,3 +47,4 @@ func (obj *KubeProxyConfiguration) GetObjectKind() schema.ObjectKind     { retur
 func (obj *KubeSchedulerConfiguration) GetObjectKind() schema.ObjectKind { return &obj.TypeMeta }
 func (obj *KubeletConfiguration) GetObjectKind() schema.ObjectKind       { return &obj.TypeMeta }
 func (obj *AdmissionConfiguration) GetObjectKind() schema.ObjectKind     { return &obj.TypeMeta }
+func (obj *ResourceQuotaConfiguration) GetObjectKind() schema.ObjectKind { return &obj.TypeMeta }
