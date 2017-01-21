8d5298cb125c0c805f6f2615378061ebe3b7cece
Sen Lu <senlu@google.com>
2017-01-20 16:07:13 -0800

    Add --ssh-user to conformance script as well

--- test/e2e_node/jenkins/conformance/conformance-jenkins.sh
+++ test/e2e_node/jenkins/conformance/conformance-jenkins.sh
@@ -33,7 +33,7 @@ TIMEOUT=${TIMEOUT:-"45m"}
 mkdir -p ${ARTIFACTS}
 
 go run test/e2e_node/runner/remote/run_remote.go  conformance \
-  --logtostderr --vmodule=*=4 --ssh-env="gce" \
+  --logtostderr --vmodule=*=4 --ssh-env="gce" --ssh-user="$GCE_USER" \
   --zone="$GCE_ZONE" --project="$GCE_PROJECT" --hosts="$GCE_HOSTS" \
   --images="$GCE_IMAGES" --image-project="$GCE_IMAGE_PROJECT" \
   --image-config-file="$GCE_IMAGE_CONFIG_PATH" --cleanup="$CLEANUP" \
