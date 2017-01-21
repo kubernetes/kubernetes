ec44a52fe27e847d133dc45800f15856897ed1a9
Clayton Coleman <ccoleman@redhat.com>
2017-01-21 18:28:51 -0500

    Tests that check metav1

--- cmd/libs/go2idl/client-gen/test_apis/testgroup/register.go
+++ cmd/libs/go2idl/client-gen/test_apis/testgroup/register.go
@@ -19,7 +19,6 @@ package testgroup
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/kubernetes/pkg/api"
 )
 
 var SchemeGroupVersion = schema.GroupVersion{Group: "testgroup.k8s.io", Version: runtime.APIVersionInternal}
