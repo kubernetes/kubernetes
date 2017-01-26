30b7205e0e07516d3bcb9e4cc3c86a8243c03693
Lukasz Zajaczkowski <lukasz.zajaczkowski@ts.fujitsu.com>
2017-01-26 07:57:04 +0100

    Set message for kubectl cordon or drain

--- pkg/kubectl/cmd/drain.go
+++ pkg/kubectl/cmd/drain.go
@@ -21,13 +21,13 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"os"
 	"reflect"
 	"strings"
 	"time"
 
 	"github.com/jonboulle/clockwork"
 	"github.com/spf13/cobra"
-
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -35,7 +35,9 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/util/clock"
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/v1"
 	"k8s.io/kubernetes/pkg/apis/policy"
 	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	"k8s.io/kubernetes/pkg/kubectl"
@@ -57,6 +59,7 @@ type DrainOptions struct {
 	backOff            clockwork.Clock
 	DeleteLocalData    bool
 	mapper             meta.RESTMapper
+	message            string
 	nodeInfo           *resource.Info
 	out                io.Writer
 	errOut             io.Writer
@@ -92,7 +95,11 @@ var (
 
 	cordon_example = templates.Examples(`
 		# Mark node "foo" as unschedulable.
-		kubectl cordon foo`)
+		kubectl cordon foo
+
+		# Mark node "foo" as unschedulable and set event message.
+		kubectl cordon foo --message="cordon for foo"
+		`)
 )
 
 func NewCmdCordon(f cmdutil.Factory, out io.Writer) *cobra.Command {
@@ -108,6 +115,7 @@ func NewCmdCordon(f cmdutil.Factory, out io.Writer) *cobra.Command {
 			cmdutil.CheckErr(options.RunCordonOrUncordon(true))
 		},
 	}
+	cmd.Flags().StringVar(&options.message, "message", "", "Set event message to describe why this node was cordoned")
 	return cmd
 }
 
@@ -117,7 +125,10 @@ var (
 
 	uncordon_example = templates.Examples(`
 		# Mark node "foo" as schedulable.
-		$ kubectl uncordon foo`)
+		$ kubectl uncordon foo
+
+		# Mark node "foo" as schedulable and set event message.
+		kubectl uncordon foo --message="uncordon for foo"`)
 )
 
 func NewCmdUncordon(f cmdutil.Factory, out io.Writer) *cobra.Command {
@@ -133,6 +144,7 @@ func NewCmdUncordon(f cmdutil.Factory, out io.Writer) *cobra.Command {
 			cmdutil.CheckErr(options.RunCordonOrUncordon(false))
 		},
 	}
+	cmd.Flags().StringVar(&options.message, "message", "", "Set event message to describe why this node was uncordoned")
 	return cmd
 }
 
@@ -187,6 +199,7 @@ func NewCmdDrain(f cmdutil.Factory, out, errOut io.Writer) *cobra.Command {
 	cmd.Flags().BoolVar(&options.DeleteLocalData, "delete-local-data", false, "Continue even if there are pods using emptyDir (local data that will be deleted when the node is drained).")
 	cmd.Flags().IntVar(&options.GracePeriodSeconds, "grace-period", -1, "Period of time in seconds given to each pod to terminate gracefully. If negative, the default value specified in the pod will be used.")
 	cmd.Flags().DurationVar(&options.Timeout, "timeout", 0, "The length of time to wait before giving up, zero means infinite")
+	cmd.Flags().StringVar(&options.message, "message", "", "Set event message to describe why this node was drained")
 	return cmd
 }
 
@@ -633,6 +646,8 @@ func (o *DrainOptions) RunCordonOrUncordon(desired bool) error {
 			if err != nil {
 				return err
 			}
+			o.sendEventWithMessage(desired)
+
 			cmdutil.PrintSuccess(o.mapper, false, o.out, o.nodeInfo.Mapping.Resource, o.nodeInfo.Name, false, changed(desired))
 		}
 	} else {
@@ -656,4 +671,48 @@ func changed(desired bool) string {
 		return "cordoned"
 	}
 	return "uncordoned"
+}
+
+func (o *DrainOptions) sendEventWithMessage(desired bool) {
+	reason := "NodeUncordoned"
+	if desired {
+		reason = "NodeCordoned"
+	}
+
+	t := metav1.Time{Time: clock.RealClock{}.Now()}
+	namespace := o.nodeInfo.Namespace
+	if namespace == "" {
+		namespace = metav1.NamespaceDefault
+	}
+
+	ref, err := api.GetReference(api.Scheme, o.nodeInfo.Object)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to get reference: %v\n", err)
+		return
+	}
+
+	event := &api.Event{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%v.%x", o.nodeInfo.Name, t.UnixNano()),
+			Namespace: namespace,
+		},
+		InvolvedObject: *ref,
+		Source: api.EventSource{
+			Component: "kubectl",
+			Host:      o.nodeInfo.Name,
+		},
+		Reason:         reason,
+		Message:        fmt.Sprintf("Node %s: %s", o.nodeInfo.Name, o.message),
+		FirstTimestamp: t,
+		LastTimestamp:  t,
+		Count:          1,
+		Type:           v1.EventTypeNormal,
+	}
+
+	_, err = o.client.Core().Events(namespace).Create(event)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to send event: %v\n", err)
+	}
 }
