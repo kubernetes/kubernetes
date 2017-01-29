0120f726eacaae17369703b3e9cd715524504e6a
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-01-29 15:27:38 +0100

    pkg/genericapiserver/endpoint/filters: cut off from pkg/api and pkg/apis

--- pkg/genericapiserver/endpoints/filters/impersonation_test.go
+++ pkg/genericapiserver/endpoints/filters/impersonation_test.go
@@ -27,7 +27,7 @@ import (
 	"k8s.io/apiserver/pkg/authentication/user"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 	"k8s.io/apiserver/pkg/endpoints/request"
-	authenticationapi "k8s.io/kubernetes/pkg/apis/authentication"
+	authenticationapi "k8s.io/client-go/pkg/apis/authentication"
 )
 
 type impersonateAuthorizer struct{}
