368607368a8b746031fc042a84d3a13663755bfb
Jordan Liggitt <jliggitt@redhat.com>
2017-01-30 15:27:41 -0500

    Fix typo in v1beta1 field name

--- pkg/apis/authorization/v1/types.go
+++ pkg/apis/authorization/v1/types.go
@@ -129,12 +129,12 @@ type SubjectAccessReviewSpec struct {
 	NonResourceAttributes *NonResourceAttributes `json:"nonResourceAttributes,omitempty" protobuf:"bytes,2,opt,name=nonResourceAttributes"`
 
 	// User is the user you're testing for.
-	// If you specify "User" but not "Group", then is it interpreted as "What if User were not a member of any groups
+	// If you specify "User" but not "Groups", then is it interpreted as "What if User were not a member of any groups
 	// +optional
 	User string `json:"user,omitempty" protobuf:"bytes,3,opt,name=verb"`
 	// Groups is the groups you're testing for.
 	// +optional
-	Groups []string `json:"group,omitempty" protobuf:"bytes,4,rep,name=group"`
+	Groups []string `json:"groups,omitempty" protobuf:"bytes,4,rep,name=groups"`
 	// Extra corresponds to the user.Info.GetExtra() method from the authenticator.  Since that is input to the authorizer
 	// it needs a reflection here.
 	// +optional
