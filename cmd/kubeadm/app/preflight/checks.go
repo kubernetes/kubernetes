2c0b3da430bd3880a2b8dc2938716f65080e830a
Derek McQuay <dmcquay@apprenda.com>
2017-01-31 15:42:46 -0800

    kubeadm: preflight check for incorrect FQDN?

--- cmd/kubeadm/app/preflight/checks.go
+++ cmd/kubeadm/app/preflight/checks.go
@@ -243,6 +243,10 @@ func (hc HostnameCheck) Check() (warnings, errors []error) {
 	for _, msg := range validation.ValidateNodeName(hostname, false) {
 		errors = append(errors, fmt.Errorf("hostname \"%s\" %s", hostname, msg))
 	}
+	addr, err := net.LookupHost(hostname)
+	if addr == nil || err != nil {
+		errors = append(errors, fmt.Errorf("hostname \"%s\" %s", hostname, err))
+	}
 	return nil, errors
 }
 
