8ece00222fbe126c9a74ec3c75ffd78058cd38c5
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-01-31 09:11:53 +0100

    client-go: fix examples

--- staging/src/k8s.io/client-go/examples/out-of-cluster/main.go
+++ staging/src/k8s.io/client-go/examples/out-of-cluster/main.go
@@ -21,8 +21,8 @@ import (
 	"fmt"
 	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -43,7 +43,7 @@ func main() {
 		panic(err.Error())
 	}
 	for {
-		pods, err := clientset.Core().Pods("").List(v1.ListOptions{})
+		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
 		if err != nil {
 			panic(err.Error())
 		}
