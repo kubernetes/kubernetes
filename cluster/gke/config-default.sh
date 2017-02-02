b37d762e54a7e635817e15ef7c3e8f211fdff445
Walter Fender <wfender@google.com>
2017-02-02 12:45:01 -0800

    GCE will properly regenerate basic_auth.csv on kube-apiserver start. If the file does not exist we will generate it as normal. If the file exists we will remove the old admin password before adding the "new" one. (Turns in to a no-op if the password exists). This did not work properly before because we were replacing by key, where the key was the password. New password would not match and so not replace the old password. Added a METADATA_CLOBBERS_CONFIG flag METADATA_CLOBBERS_CONFIG controls if we consider the values on disk or in metadata to be the canonical source of truth. Currently defaulting to false for GCE and forcing to true for GKE.

--- cluster/gke/config-default.sh
+++ cluster/gke/config-default.sh
@@ -42,3 +42,6 @@ ENABLE_L7_LOADBALANCING="${KUBE_ENABLE_L7_LOADBALANCING:-glbc}"
 ENABLE_CLUSTER_MONITORING="${KUBE_ENABLE_CLUSTER_MONITORING:-standalone}"
 
 KUBE_DELETE_NETWORK=${KUBE_DELETE_NETWORK:-false}
+
+# Indicates if the values (eg. kube password) should be treated as canonical
+METADATA_CLOBBERS_CONFIG=true
