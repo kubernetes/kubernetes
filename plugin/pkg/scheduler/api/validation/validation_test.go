6fd76dc13947c07f8d1c76b5c3fb54daa42d874c
tanshanshan <tan.shanshan@zte.com.cn>
2017-02-03 10:01:03 +0800

    fix

--- plugin/pkg/scheduler/api/validation/validation_test.go
+++ plugin/pkg/scheduler/api/validation/validation_test.go
@@ -50,3 +50,18 @@ func TestValidatePriorityWithNegativeWeight(t *testing.T) {
 		t.Errorf("Expected error about priority weight not being positive")
 	}
 }
+
+func TestValidateExtenderWithNonNegativeWeight(t *testing.T) {
+	extenderPolicy := api.Policy{ExtenderConfigs: []api.ExtenderConfig{{URLPrefix: "http://127.0.0.1:8081/extender", FilterVerb: "filter", Weight: 2}}}
+	errs := ValidatePolicy(extenderPolicy)
+	if errs != nil {
+		t.Errorf("Unexpected errors %v", errs)
+	}
+}
+
+func TestValidateExtenderWithNegativeWeight(t *testing.T) {
+	extenderPolicy := api.Policy{ExtenderConfigs: []api.ExtenderConfig{{URLPrefix: "http://127.0.0.1:8081/extender", FilterVerb: "filter", Weight: -2}}}
+	if ValidatePolicy(extenderPolicy) == nil {
+		t.Errorf("Expected error about priority weight for extender not being positive")
+	}
+}
