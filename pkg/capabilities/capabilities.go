56fa75123269cee7d09eda2a86d84d2cddbcb99a
00171108 <song.ruixia@zte.com.cn>
2017-02-03 14:33:40 +0800

    Fix comment for method SetForTests

--- pkg/capabilities/capabilities.go
+++ pkg/capabilities/capabilities.go
@@ -68,7 +68,7 @@ func Setup(allowPrivileged bool, privilegedSources PrivilegedSources, perConnect
 	})
 }
 
-// SetCapabilitiesForTests.  Convenience method for testing.  This should only be called from tests.
+// SetForTests sets capabilities for tests.  Convenience method for testing.  This should only be called from tests.
 func SetForTests(c Capabilities) {
 	lock.Lock()
 	defer lock.Unlock()
