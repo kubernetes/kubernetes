085d49f0bf2c7698f66c79b8f40387c6f55c567a
Maru Newby <marun@redhat.com>
2017-02-05 20:28:20 -0800

    fed: Abstract secret controller interaction with the secret type
    This change uses an adapter class to abstracts the interaction of the
    secret controller with the secret api type.  This is the first step to
    creating a generic controller that can target any type for which an
    adapter exists.

--- federation/pkg/typeadapters/adapter.go
+++ federation/pkg/typeadapters/adapter.go
@@ -20,8 +20,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	pkgruntime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
 	federationclientset "k8s.io/kubernetes/federation/client/clientset_generated/federation_clientset"
-	"k8s.io/kubernetes/pkg/client/clientset_generated/clientset"
+	kubeclientset "k8s.io/kubernetes/pkg/client/clientset_generated/clientset"
 )
 
 // FederatedTypeAdapter defines operations for interacting with a
@@ -31,18 +32,28 @@ type FederatedTypeAdapter interface {
 	SetClient(client federationclientset.Interface)
 
 	Kind() string
+	ObjectType() pkgruntime.Object
+	IsExpectedType(obj interface{}) bool
+	Copy(obj pkgruntime.Object) pkgruntime.Object
 	Equivalent(obj1, obj2 pkgruntime.Object) bool
-	ObjectMeta(obj pkgruntime.Object) *metav1.ObjectMeta
 	NamespacedName(obj pkgruntime.Object) types.NamespacedName
+	ObjectMeta(obj pkgruntime.Object) *metav1.ObjectMeta
 
 	// Fed* operations target the federation control plane
 	FedCreate(obj pkgruntime.Object) (pkgruntime.Object, error)
-	FedGet(namespacedName types.NamespacedName) (pkgruntime.Object, error)
-	FedUpdate(obj pkgruntime.Object) (pkgruntime.Object, error)
 	FedDelete(namespacedName types.NamespacedName, options *metav1.DeleteOptions) error
+	FedGet(namespacedName types.NamespacedName) (pkgruntime.Object, error)
+	FedList(namespace string, options metav1.ListOptions) (pkgruntime.Object, error)
+	FedUpdate(obj pkgruntime.Object) (pkgruntime.Object, error)
+	FedWatch(namespace string, options metav1.ListOptions) (watch.Interface, error)
 
 	// The following operations are intended to target a cluster that is a member of a federation
-	ClusterGet(client clientset.Interface, namespacedName types.NamespacedName) (pkgruntime.Object, error)
+	ClusterCreate(client kubeclientset.Interface, obj pkgruntime.Object) (pkgruntime.Object, error)
+	ClusterDelete(client kubeclientset.Interface, nsName types.NamespacedName, options *metav1.DeleteOptions) error
+	ClusterGet(client kubeclientset.Interface, namespacedName types.NamespacedName) (pkgruntime.Object, error)
+	ClusterList(client kubeclientset.Interface, namespace string, options metav1.ListOptions) (pkgruntime.Object, error)
+	ClusterUpdate(client kubeclientset.Interface, obj pkgruntime.Object) (pkgruntime.Object, error)
+	ClusterWatch(client kubeclientset.Interface, namespace string, options metav1.ListOptions) (watch.Interface, error)
 
 	NewTestObject(namespace string) pkgruntime.Object
 }
