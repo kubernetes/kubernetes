a348fd66f4f804c1f32b8a9fb3db9c943aa8fa44
deads2k <deads@redhat.com>
2017-02-06 13:15:16 -0500

    move admission read logic

--- federation/cmd/federation-apiserver/app/server.go
+++ federation/cmd/federation-apiserver/app/server.go
@@ -163,7 +163,7 @@ func Run(s *options.ServerRunOptions) error {
 
 	admissionControlPluginNames := strings.Split(s.GenericServerRunOptions.AdmissionControl, ",")
 	pluginInitializer := kubeapiserveradmission.NewPluginInitializer(client, sharedInformers, apiAuthorizer)
-	admissionConfigProvider, err := kubeapiserveradmission.ReadAdmissionConfiguration(admissionControlPluginNames, s.GenericServerRunOptions.AdmissionControlConfigFile)
+	admissionConfigProvider, err := admission.ReadAdmissionConfiguration(admissionControlPluginNames, s.GenericServerRunOptions.AdmissionControlConfigFile)
 	if err != nil {
 		return fmt.Errorf("failed to read plugin config: %v", err)
 	}
