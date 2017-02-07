0eb33de306d1f9d87f5bae0aae1b085efb0db9d8
Aleksandra Malinowska <aleksandram@google.com>
2017-02-07 12:28:23 +0100

    Add Stackdriver API v3 dependency

---
+++ test/e2e/stackdriver_monitoring.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"k8s.io/kubernetes/test/e2e/framework"
+
+	. "github.com/onsi/ginkgo"
+
+	gcm "google.golang.org/api/monitoring/v3"
+)
+
+var _ = &gcm.CollectdValue{}
+
+var _ = framework.KubeDescribe("Stackdriver metrics", func() {
+	_ = framework.NewDefaultFramework("monitoring")
+
+	BeforeEach(func() {
+		framework.SkipUnlessProviderIs("gke")
+	})
+
+	It("should publish metrics to Stackdriver [Feature:Stackdriver][Flaky]", func() {
+		framework.Logf("Start creating Stackdriver monitoring test")
+		testStackdriverMonitoring(f.ClientSet)
+	})
+})
+
+func testStackdriverMonitoring() {
+}
