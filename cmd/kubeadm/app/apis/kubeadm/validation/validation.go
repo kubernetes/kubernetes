4499cbeb34dda927477d425522608d11eebc7241
Lucas Käldström <lucas@weave.works>
2017-02-07 23:42:41 +0200

    WIP: First stab at the kubeadm beta API: add a Certificates Phase object

--- cmd/kubeadm/app/apis/kubeadm/validation/validation.go
+++ cmd/kubeadm/app/apis/kubeadm/validation/validation.go
@@ -17,8 +17,13 @@ limitations under the License.
 package validation
 
 import (
+	"fmt"
+	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	"net"
+
 	"k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm"
+	apivalidation "k8s.io/kubernetes/pkg/api/validation"
 )
 
 func ValidateMasterConfiguration(c *kubeadm.MasterConfiguration) field.ErrorList {
@@ -66,5 +71,43 @@ func ValidateHTTPSDiscovery(c *kubeadm.HTTPSDiscovery, fldPath *field.Path) fiel
 
 func ValidateTokenDiscovery(c *kubeadm.TokenDiscovery, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
+	return allErrs
+}
+
+func ValidateCertificatesPhase(c *kubeadm.CertificatesPhase, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	// If SelfSign specified, validate its properties
+	if c.SelfSign != nil {
+		for _, altname := range c.SelfSign.AltNames {
+			ip := net.ParseIP(altname)
+			if len(validation.IsDNS1123Subdomain(altname)) != 0 && ip == nil {
+				allErrs = append(allErrs, field.Invalid(fldPath, nil, fmt.Sprintf("altname %s is not a valid dns label or ip address", altname)))
+			}
+		}
+	}
+	return allErrs
+}
+
+func ValidateIPFromString(ipaddr string, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	if net.ParseIP(ipaddr) == nil {
+		allErrs = append(allErrs, field.Invalid(fldPath, nil, "ip address is not valid"))
+	}
+	return allErrs
+}
+
+func ValidateIPNetFromString(ipaddr string, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	if _, _, err := net.ParseCIDR(ipaddr); err != nil {
+		allErrs = append(allErrs, field.Invalid(fldPath, nil, "cidr is not valid"))
+	}
+	return allErrs
+}
+
+func ValidateNetworking(c *kubeadm.Networking) field.ErrorList {
+	allErrs := field.ErrorList{}
+	allErrs = append(allErrs, apivalidation.ValidateDNS1123Subdomain(c.DNSDomain, field.NewPath("dns-domain"))...)
+	allErrs = append(allErrs, ValidateIPNetFromString(c.ServiceSubnet, field.NewPath("service-subnet"))...)
+	allErrs = append(allErrs, ValidateIPNetFromString(c.PodSubnet, field.NewPath("pod-subnet"))...)
 	return allErrs
 }
