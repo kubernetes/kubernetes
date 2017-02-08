d94e81aa8dfb1954da99815bddd19c2b9e354ebe
Derek Carr <decarr@redhat.com>
2017-02-08 18:06:44 -0500

    Add support for ResourceQuota limited resources

---
+++ plugin/pkg/admission/resourcequota/apis/resourcequota/types.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resourcequota
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+// Configuration provides configuration for the ResourceQuota admission controller.
+type Configuration struct {
+	metav1.TypeMeta
+
+	// LimitedResources whose consumption is limited by default.
+	// +optional
+	LimitedResources []LimitedResource
+}
+
+// LimitedResource matches a resource whose consumption is limited by default.
+// To consume the resource, there must exist an associated quota that limits
+// its consumption.
+type LimitedResource struct {
+	// API group of the object.
+	// +optional
+	APIGroup string
+
+	// API resource of the object.
+	APIResource string
+
+	// If a resource name tracked by quota for this entity matches this expression, and there is
+	// no associated quota for that resource name defined, the request will be denied by the quota
+	// system.  The MatchExpression is a regular expression value according to
+	// https://golang.org/pkg/regexp/
+	MatchExpression string
+}
