7d321064c6659964a268a685ee5e759cb2a09831
Huamin Chen <hchen@redhat.com>
2017-02-08 21:50:26 +0000

    add volume controller metrics
    Signed-off-by: Huamin Chen <hchen@redhat.com>

---
+++ pkg/volume/util/metrics/metrics.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metrics
+
+import (
+	"sync"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	VolumeControllerSubsystem = "volume_controller"
+)
+
+var (
+	VolumeAttachLatency = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Subsystem: VolumeControllerSubsystem,
+			Name:      "volume_attach_latency_milliseconds",
+			Help:      "Volume attach latency",
+			Buckets:   prometheus.ExponentialBuckets(100, 2, 10),
+		},
+	)
+	VolumeDetachLatency = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Subsystem: VolumeControllerSubsystem,
+			Name:      "volume_detach_latency_milliseconds",
+			Help:      "Volume detach latency",
+			Buckets:   prometheus.ExponentialBuckets(100, 2, 10),
+		},
+	)
+	VolumeProvisionLatency = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Subsystem: VolumeControllerSubsystem,
+			Name:      "volume_provision_latency_milliseconds",
+			Help:      "Volume provisioning latency",
+			Buckets:   prometheus.ExponentialBuckets(100, 2, 10),
+		},
+	)
+	VolumeDeleteLatency = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Subsystem: VolumeControllerSubsystem,
+			Name:      "volume_delete_latency_milliseconds",
+			Help:      "Volume deletion latency",
+			Buckets:   prometheus.ExponentialBuckets(1000, 2, 10),
+		},
+	)
+)
+
+var registerMetrics sync.Once
+
+// Register all metrics.
+func Register() {
+	// Register the metrics.
+	registerMetrics.Do(func() {
+		prometheus.MustRegister(VolumeAttachLatency)
+		prometheus.MustRegister(VolumeDetachLatency)
+		prometheus.MustRegister(VolumeProvisionLatency)
+		prometheus.MustRegister(VolumeDeleteLatency)
+	})
+}
+
+// Gets the time since the specified start in milliseconds.
+func SinceInMilliseconds(start time.Time) float64 {
+	return float64(time.Since(start).Nanoseconds() / time.Millisecond.Nanoseconds())
+}
