3951cedd99fdee8b08619a4bd4b7f71c1ee04683
Wojciech Tyczynski <wojtekt@google.com>
2017-02-10 11:29:45 +0100

    Backup etcd only before migration

--- cluster/images/etcd/migrate-if-needed.sh
+++ cluster/images/etcd/migrate-if-needed.sh
@@ -152,7 +152,7 @@ ROLLBACK="${ROLLBACK:-/usr/local/bin/rollback}"
 # If we are upgrading from 2.2.1 and this is the first try for upgrade,
 # do the backup to allow restoring from it in case of failed upgrade.
 BACKUP_DIR="${DATA_DIRECTORY}/migration-backup"
-if [ "${CURRENT_VERSION}" = "2.2.1" -a ! -d "${BACKUP_DIR}" ]; then
+if [ "${CURRENT_VERSION}" = "2.2.1" -a ! "${CURRENT_VERSION}" != "${TARGET_VERSION}" -a -d "${BACKUP_DIR}" ]; then
   echo "Backup etcd before starting migration"
   mkdir ${BACKUP_DIR}
   ETCDCTL_CMD="/usr/local/bin/etcdctl-2.2.1"
