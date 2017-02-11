41f2f2f27b96c1a2d2ce8014b494f64d0ffa9787
Stephen Kriss <stephen.kriss@gmail.com>
2017-02-11 15:01:22 -0800

    implement configmap upgrade test

--- test/e2e/cluster_upgrade.go
+++ test/e2e/cluster_upgrade.go
@@ -33,6 +33,7 @@ var upgradeTests = []upgrades.Test{
 	&upgrades.ServiceUpgradeTest{},
 	&upgrades.SecretUpgradeTest{},
 	&upgrades.DeploymentUpgradeTest{},
+	&upgrades.ConfigMapUpgradeTest{},
 }
 
 var _ = framework.KubeDescribe("Upgrade [Feature:Upgrade]", func() {
