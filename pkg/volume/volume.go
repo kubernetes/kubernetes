b4f35632af29888151879993c5c5833d7ed6fbd6
Hemant Kumar <hekumar@redhat.com>
2017-02-12 23:40:30 -0500

    Impement bulk polling of volumes
    This implements Bulk volume polling using ideas presented by
    justin in https://github.com/kubernetes/kubernetes/pull/39564
    
    But it changes the implementation to use an interface
    and doesn't affect other implementations.

--- pkg/volume/volume.go
+++ pkg/volume/volume.go
@@ -187,6 +187,13 @@ type Attacher interface {
 	MountDevice(spec *Spec, devicePath string, deviceMountPath string) error
 }
 
+type BulkVolumeVerifier interface {
+	// BulkVerifyVolumes checks whether the list of volumes still attached to the
+	// the clusters in the node. It returns a map which maps from the volume spec to the checking result.
+	// If an error is occured during checking, the error will be returned
+	BulkVerifyVolumes(volumesByNode map[types.NodeName][]*Spec) (map[types.NodeName]map[*Spec]bool, error)
+}
+
 // Detacher can detach a volume from a node.
 type Detacher interface {
 	// Detach the given device from the node with the given Name.
