0306c9a6a9a948c5757ace4c4c813b82ec0b9609
shawyeok <kmter@live.com>
2017-02-14 03:20:27 +0800

    Centos provider: generate SSL certificates for etcd cluster.

--- cluster/centos/util.sh
+++ cluster/centos/util.sh
@@ -208,6 +208,7 @@ echo "[INFO] tear-down-master on $1"
         fi"
   done
   kube-ssh "${1}" "sudo rm -rf /opt/kubernetes"
+  kube-ssh "${1}" "sudo rm -rf /srv/kubernetes"
   kube-ssh "${1}" "sudo rm -rf ${KUBE_TEMP}"
   kube-ssh "${1}" "sudo rm -rf /var/lib/etcd"
 }
@@ -226,6 +227,7 @@ echo "[INFO] tear-down-node on $1"
   done
   kube-ssh "$1" "sudo rm -rf /run/flannel"
   kube-ssh "$1" "sudo rm -rf /opt/kubernetes"
+  kube-ssh "$1" "sudo rm -rf /srv/kubernetes"
   kube-ssh "$1" "sudo rm -rf ${KUBE_TEMP}"
 }
 
@@ -239,6 +241,7 @@ function make-ca-cert() {
 #
 # Assumed vars:
 #   $1 (master)
+#   $2 (etcd_name)
 #   KUBE_TEMP
 #   ETCD_SERVERS
 #   ETCD_INITIAL_CLUSTER
@@ -250,12 +253,21 @@ function provision-master() {
   local master_ip="${master#*@}"
   local etcd_name="$2"
   ensure-setup-dir "${master}"
+  ensure-etcd-cert "${etcd_name}" "${master_ip}"
 
   kube-scp "${master}" "${ROOT}/ca-cert ${ROOT}/binaries/master ${ROOT}/master ${ROOT}/config-default.sh ${ROOT}/util.sh" "${KUBE_TEMP}"
+  kube-scp "${master}" "${ROOT}/etcd-cert/ca.pem \
+    ${ROOT}/etcd-cert/client.pem \
+    ${ROOT}/etcd-cert/client-key.pem \
+    ${ROOT}/etcd-cert/server-${etcd_name}.pem \
+    ${ROOT}/etcd-cert/server-${etcd_name}-key.pem \
+    ${ROOT}/etcd-cert/peer-${etcd_name}.pem \
+    ${ROOT}/etcd-cert/peer-${etcd_name}-key.pem" "${KUBE_TEMP}/etcd-cert"
   kube-ssh "${master}" " \
     sudo rm -rf /opt/kubernetes/bin; \
     sudo cp -r ${KUBE_TEMP}/master/bin /opt/kubernetes; \
-    sudo mkdir -p /srv/kubernetes; sudo cp -f ${KUBE_TEMP}/ca-cert/* /srv/kubernetes; \
+    sudo mkdir -p /srv/kubernetes/; sudo cp -f ${KUBE_TEMP}/ca-cert/* /srv/kubernetes/; \
+    sudo mkdir -p /srv/kubernetes/etcd; sudo cp -f ${KUBE_TEMP}/etcd-cert/* /srv/kubernetes/etcd/; \
     sudo chmod -R +x /opt/kubernetes/bin; \
     sudo ln -sf /opt/kubernetes/bin/* /usr/local/bin/; \
     sudo bash ${KUBE_TEMP}/master/scripts/etcd.sh ${etcd_name} ${master_ip} ${ETCD_INITIAL_CLUSTER}; \
@@ -298,12 +310,17 @@ function provision-node() {
   local dns_domain=${DNS_DOMAIN#*@}
   ensure-setup-dir ${node}
 
-  kube-scp ${node} "${ROOT}/binaries/node ${ROOT}/node ${ROOT}/config-default.sh ${ROOT}/util.sh" ${KUBE_TEMP}
+  kube-scp "${node}" "${ROOT}/binaries/node ${ROOT}/node ${ROOT}/config-default.sh ${ROOT}/util.sh" "${KUBE_TEMP}"
+  kube-scp "${node}" "${ROOT}/etcd-cert/ca.pem \
+    ${ROOT}/etcd-cert/client.pem \
+    ${ROOT}/etcd-cert/client-key.pem" "${KUBE_TEMP}/etcd-cert"
   kube-ssh "${node}" " \
     rm -rf /opt/kubernetes/bin; \
     sudo cp -r ${KUBE_TEMP}/node/bin /opt/kubernetes; \
     sudo chmod -R +x /opt/kubernetes/bin; \
+    sudo mkdir -p /srv/kubernetes/etcd; sudo cp -f ${KUBE_TEMP}/etcd-cert/* /srv/kubernetes/etcd/; \
     sudo ln -s /opt/kubernetes/bin/* /usr/local/bin/; \
+    sudo mkdir -p /srv/kubernetes/etcd; sudo cp -f ${KUBE_TEMP}/etcd-cert/* /srv/kubernetes/etcd/; \
     sudo bash ${KUBE_TEMP}/node/scripts/flannel.sh ${ETCD_SERVERS} ${FLANNEL_NET}; \
     sudo bash ${KUBE_TEMP}/node/scripts/docker.sh \"${DOCKER_OPTS}\"; \
     sudo bash ${KUBE_TEMP}/node/scripts/kubelet.sh ${MASTER_ADVERTISE_ADDRESS} ${node_ip} ${dns_ip} ${dns_domain}; \
@@ -316,8 +333,52 @@ function provision-node() {
 #   KUBE_TEMP
 function ensure-setup-dir() {
   kube-ssh "${1}" "mkdir -p ${KUBE_TEMP}; \
+                   mkdir -p ${KUBE_TEMP}/etcd-cert; \
                    sudo mkdir -p /opt/kubernetes/bin; \
                    sudo mkdir -p /opt/kubernetes/cfg"
+}
+
+function ensure-etcd-cert() {
+  ensure-setup-cfssl
+
+  local etcd_name="$1"
+  local master_ip="$2"
+
+  local cert_dir="${ROOT}/etcd-cert"
+  pushd "${cert_dir}"
+
+  echo '{"CN":"'${etcd_name}'","hosts":[""],"key":{"algo":"rsa","size":2048}}' \
+   | ./cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server -hostname="${master_ip},127.0.0.1" - \
+   | ./cfssljson -bare "server-${etcd_name}"
+  echo '{"CN":"'${etcd_name}'","hosts":[""],"key":{"algo":"rsa","size":2048}}' \
+   | ./cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=peer -hostname="${master_ip},127.0.0.1" - \
+   | ./cfssljson -bare "peer-${etcd_name}"
+
+  popd
+}
+
+function ensure-setup-cfssl() {
+  local cert_dir="${ROOT}/etcd-cert"
+  pushd "${cert_dir}"
+
+  if [ ! -x cfssl ] && [ ! -x cfssljson ]; then
+    echo "Download cfssl & cfssljson ..."
+    curl -sSL -o cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+    curl -sSL -o cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+    chmod +x cfssl cfssljson
+  fi
+  if [ ! -r ca.pem ] && [ ! -r ca-key.pem ]; then
+    echo "Generate CA certificates ..."
+    ./cfssl gencert -initca ca-csr.json | ./cfssljson -bare ca -
+  fi
+  if [ ! -r client.pem ] && [ ! -r client-key.pem ]; then
+    echo "Generate client certificates"
+    echo '{"CN":"client","hosts":[""],"key":{"algo":"rsa","size":2048}}' \
+     | ./cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=client - \
+     | ./cfssljson -bare client
+  fi
+
+  popd
 }
 
 # Run command over ssh
@@ -333,6 +394,16 @@ function kube-scp() {
   local src=($2)
   local dst="$3"
   scp -r ${SSH_OPTS} ${src[*]} "${host}:${dst}"
+}
+
+# Run pushd without stack output
+function pushd() {
+  command pushd $@ > /dev/null
+}
+
+# Run popd without stack output
+function popd() {
+  command popd $@ > /dev/null
 }
 
 # Ensure that we have a password created for validating to the master. Will
