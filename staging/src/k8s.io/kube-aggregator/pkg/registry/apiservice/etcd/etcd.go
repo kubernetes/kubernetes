e2a779ee9e1e9ce3c2bdb203856a659e5b237d58
deads2k <deads@redhat.com>
2017-02-14 10:00:53 -0500

    bulk move of kube-aggregator

---
+++ staging/src/k8s.io/kube-aggregator/pkg/registry/apiservice/etcd/etcd.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package etcd
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apiserver/pkg/registry/generic"
+	genericregistry "k8s.io/apiserver/pkg/registry/generic/registry"
+	"k8s.io/client-go/pkg/api"
+	"k8s.io/kube-aggregator/pkg/apis/apiregistration"
+	"k8s.io/kube-aggregator/pkg/registry/apiservice"
+)
+
+// rest implements a RESTStorage for API services against etcd
+type REST struct {
+	*genericregistry.Store
+}
+
+// NewREST returns a RESTStorage object that will work against API services.
+func NewREST(optsGetter generic.RESTOptionsGetter) *REST {
+	store := &genericregistry.Store{
+		Copier:      api.Scheme,
+		NewFunc:     func() runtime.Object { return &apiregistration.APIService{} },
+		NewListFunc: func() runtime.Object { return &apiregistration.APIServiceList{} },
+		ObjectNameFunc: func(obj runtime.Object) (string, error) {
+			return obj.(*apiregistration.APIService).Name, nil
+		},
+		PredicateFunc:     apiservice.MatchAPIService,
+		QualifiedResource: apiregistration.Resource("apiservices"),
+		WatchCacheSize:    100,
+
+		CreateStrategy: apiservice.Strategy,
+		UpdateStrategy: apiservice.Strategy,
+		DeleteStrategy: apiservice.Strategy,
+	}
+	options := &generic.StoreOptions{RESTOptions: optsGetter, AttrFunc: apiservice.GetAttrs}
+	if err := store.CompleteWithOptions(options); err != nil {
+		panic(err) // TODO: Propagate error up
+	}
+	return &REST{store}
+}
