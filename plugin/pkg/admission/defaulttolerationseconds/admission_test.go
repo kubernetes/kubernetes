9f661ab4a8100cb0a340439c6305cca87790bd23
Kevin <wangzefeng@huawei.com>
2017-02-16 23:12:57 +0800

    rename package and file path to match plugin name

--- plugin/pkg/admission/defaulttolerationseconds/admission_test.go
+++ plugin/pkg/admission/defaulttolerationseconds/admission_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package tolerationseconds
+package defaulttolerationseconds
 
 import (
 	"encoding/json"
