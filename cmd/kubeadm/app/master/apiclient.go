a6b5845cac5ff15ba074978005a3c72fd4e626a2
Lucas Käldström <lucas@weave.works>
2017-02-17 21:36:58 +0200

    kubeadm: Move {admin,kubelet}.conf out as constants and make a separate util package for kubeconfig logic

--- cmd/kubeadm/app/master/apiclient.go
+++ cmd/kubeadm/app/master/apiclient.go
@@ -29,31 +29,14 @@ import (
 	"k8s.io/client-go/pkg/api"
 	"k8s.io/client-go/pkg/api/v1"
 	extensions "k8s.io/client-go/pkg/apis/extensions/v1beta1"
-	"k8s.io/client-go/tools/clientcmd"
 	kubeadmapi "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm"
 	kubeadmconstants "k8s.io/kubernetes/cmd/kubeadm/app/constants"
 	kubeadmutil "k8s.io/kubernetes/cmd/kubeadm/app/util"
+	kubeconfigutil "k8s.io/kubernetes/cmd/kubeadm/app/util/kubeconfig"
 )
 
-func CreateClientFromFile(path string) (*clientset.Clientset, error) {
-	adminKubeconfig, err := clientcmd.LoadFromFile(path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to load admin kubeconfig [%v]", err)
-	}
-	adminClientConfig, err := clientcmd.NewDefaultClientConfig(*adminKubeconfig, &clientcmd.ConfigOverrides{}).ClientConfig()
-	if err != nil {
-		return nil, fmt.Errorf("failed to create API client configuration [%v]", err)
-	}
-
-	client, err := clientset.NewForConfig(adminClientConfig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create API client [%v]", err)
-	}
-	return client, nil
-}
-
 func CreateClientAndWaitForAPI(file string) (*clientset.Clientset, error) {
-	client, err := CreateClientFromFile(file)
+	client, err := kubeconfigutil.ClientSetFromFile(file)
 	if err != nil {
 		return nil, err
 	}
