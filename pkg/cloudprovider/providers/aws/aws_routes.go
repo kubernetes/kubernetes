0b5ae5391ef9eeba337f1dfa62b6f3125e28c86c
Justin Santa Barbara <justin@fathomdb.com>
2017-02-18 13:11:08 -0500

    AWS: Support shared tag
    We recognize an additional cluster tag:
    
    kubernetes.io/cluster/<clusterid>
    
    This now allows us to share resources, in particular subnets.
    
    In addition, the value is used to track ownership/lifecycle.  When we
    create objects, we record the value as "owned".
    
    We also refactor out tags into its own file & class, as we are touching
    most of these functions anyway.

--- pkg/cloudprovider/providers/aws/aws_routes.go
+++ pkg/cloudprovider/providers/aws/aws_routes.go
@@ -29,12 +29,18 @@ func (c *Cloud) findRouteTable(clusterName string) (*ec2.RouteTable, error) {
 	// This should be unnecessary (we already filter on TagNameKubernetesCluster,
 	// and something is broken if cluster name doesn't match, but anyway...
 	// TODO: All clouds should be cluster-aware by default
-	filters := []*ec2.Filter{newEc2Filter("tag:"+TagNameKubernetesCluster, clusterName)}
-	request := &ec2.DescribeRouteTablesInput{Filters: c.addFilters(filters)}
+	request := &ec2.DescribeRouteTablesInput{Filters: c.tagging.addFilters(nil)}
 
-	tables, err := c.ec2.DescribeRouteTables(request)
+	response, err := c.ec2.DescribeRouteTables(request)
 	if err != nil {
 		return nil, err
+	}
+
+	var tables []*ec2.RouteTable
+	for _, table := range response {
+		if c.tagging.hasClusterTag(table.Tags) {
+			tables = append(tables, table)
+		}
 	}
 
 	if len(tables) == 0 {
