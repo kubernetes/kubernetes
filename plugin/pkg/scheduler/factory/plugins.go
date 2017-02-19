b1a15b0c94fdfabce2bc03f0a575e6d5d5467cba
Bobby (Babak) Salamat <bsalamat@bsalamat-macbookpro.roam.corp.google.com>
2017-02-18 21:38:22 -0800

    fixed spacing

--- plugin/pkg/scheduler/factory/plugins.go
+++ plugin/pkg/scheduler/factory/plugins.go
@@ -38,7 +38,7 @@ type PluginFactoryArgs struct {
 	ServiceLister                  algorithm.ServiceLister
 	ControllerLister               algorithm.ControllerLister
 	ReplicaSetLister               algorithm.ReplicaSetLister
-	StatefulSetLister							 algorithm.StatefulSetLister
+	StatefulSetLister              algorithm.StatefulSetLister
 	NodeLister                     algorithm.NodeLister
 	NodeInfo                       predicates.NodeInfo
 	PVInfo                         predicates.PersistentVolumeInfo
