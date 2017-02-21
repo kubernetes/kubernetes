b8274c128367743b4c76a6ac3aebd0501ee3574e
Cao Shufeng <caosf.fnst@cn.fujitsu.com>
2017-02-21 05:13:11 -0500

    Ensure invalid username/password returns 401 error, not 403
    If a user attempts to use basic auth, and the username/password combination
    is rejected, the authenticator should return an error. This distinguishes
    requests that did not provide username/passwrod (and are unauthenticated
    without error) from ones that attempted to, and failed.

--- pkg/kubeapiserver/authenticator/config.go
+++ pkg/kubeapiserver/authenticator/config.go
@@ -201,7 +201,8 @@ func (config AuthenticatorConfig) New() (authenticator.Request, *spec.SecurityDe
 	authenticator = group.NewGroupAdder(authenticator, []string{user.AllAuthenticated})
 
 	if config.Anonymous {
-		// If the authenticator chain returns an error, return an error (don't consider a bad bearer token anonymous).
+		// If the authenticator chain returns an error, return an error (don't consider a bad bearer token
+		// or invalid username/password combination anonymous).
 		authenticator = union.NewFailOnError(authenticator, anonymous.NewAuthenticator())
 	}
 
