395be3b4010b0eef63e98961a14064360fdca26c
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-02-21 12:49:19 +0100

    apimachinery: handle duplicated and conflicting type registration

--- staging/src/k8s.io/apiserver/pkg/apis/apiserver/register.go
+++ staging/src/k8s.io/apiserver/pkg/apis/apiserver/register.go
@@ -17,7 +17,6 @@ limitations under the License.
 package apiserver
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -47,6 +46,5 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&AdmissionConfiguration{},
 	)
-	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
 }
