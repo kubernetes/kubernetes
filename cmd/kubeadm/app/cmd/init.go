1bd8bf6c5bf0b6499590efe65a7030077463ae27
Alexander Kanevskiy <alexander.kanevskiy@intel.com>
2017-02-21 20:09:53 +0200

    Changing --use-kubernetes-version to --kubernetes-version as it was agreed on SIG-Cluster-Lifecycle meeting.

--- cmd/kubeadm/app/cmd/init.go
+++ cmd/kubeadm/app/cmd/init.go
@@ -106,7 +106,7 @@ func NewCmdInit(out io.Writer) *cobra.Command {
 		`Use alternative domain for services, e.g. "myorg.internal"`,
 	)
 	cmd.PersistentFlags().StringVar(
-		&cfg.KubernetesVersion, "use-kubernetes-version", cfg.KubernetesVersion,
+		&cfg.KubernetesVersion, "kubernetes-version", cfg.KubernetesVersion,
 		`Choose a specific Kubernetes version for the control plane`,
 	)
 
