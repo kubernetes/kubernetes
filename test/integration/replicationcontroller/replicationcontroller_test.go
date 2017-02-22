9a0f223f65ef35261ca6d991b9992ac4585e1c6c
Anthony Yeh <enisoc@google.com>
2017-02-22 11:52:47 -0800

    RC/RS: Respect ControllerRef regardless of GC setting.
    This is part of the completion of ControllerRef, as described here:
    
    https://github.com/kubernetes/community/blob/master/contributors/design-proposals/controller-ref.md#controller-specific-behavior
    
    """
    When ControllerRef was first added to these controllers, the main
    purpose was to enable server-side cascading deletion via the Garbage
    Collector, so it made sense to gate it behind the same flag.
    
    However, in order to achieve the goals of this proposal, it is necessary
    to set ControllerRefs and perform adoption/orphaning regardless of
    whether server-side cascading deletion (the Garbage Collector) is
    enabled. For example, turning off the GC should not cause controllers to
    start fighting again. Therefore, these controllers will be updated to
    always enable ControllerRef.
    """

--- test/integration/replicationcontroller/replicationcontroller_test.go
+++ test/integration/replicationcontroller/replicationcontroller_test.go
@@ -123,7 +123,7 @@ func verifyRemainingObjects(t *testing.T, clientSet clientset.Interface, namespa
 	return ret, nil
 }
 
-func rmSetup(t *testing.T, stopCh chan struct{}, enableGarbageCollector bool) (*httptest.Server, *replication.ReplicationManager, informers.SharedInformerFactory, clientset.Interface) {
+func rmSetup(t *testing.T, stopCh chan struct{}) (*httptest.Server, *replication.ReplicationManager, informers.SharedInformerFactory, clientset.Interface) {
 	masterConfig := framework.NewIntegrationTestMasterConfig()
 	_, s := framework.RunAMaster(masterConfig)
 
@@ -135,7 +135,7 @@ func rmSetup(t *testing.T, stopCh chan struct{}, enableGarbageCollector bool) (*
 	resyncPeriod := 12 * time.Hour
 
 	informers := informers.NewSharedInformerFactory(clientSet, resyncPeriod)
-	rm := replication.NewReplicationManager(informers.Core().V1().Pods(), informers.Core().V1().ReplicationControllers(), clientSet, replication.BurstReplicas, 4096, enableGarbageCollector)
+	rm := replication.NewReplicationManager(informers.Core().V1().Pods(), informers.Core().V1().ReplicationControllers(), clientSet, replication.BurstReplicas, 4096)
 	informers.Start(stopCh)
 
 	return s, rm, informers, clientSet
@@ -209,7 +209,7 @@ func TestAdoption(t *testing.T) {
 	}
 	for i, tc := range testCases {
 		stopCh := make(chan struct{})
-		s, rm, informers, clientSet := rmSetup(t, stopCh, true)
+		s, rm, informers, clientSet := rmSetup(t, stopCh)
 		ns := framework.CreateTestingNamespace(fmt.Sprintf("adoption-%d", i), s, t)
 		defer framework.DeleteTestingNamespace(ns, s, t)
 
@@ -286,7 +286,7 @@ func TestUpdateSelectorToAdopt(t *testing.T) {
 	// matches pod1 only; change the selector to match pod2 as well. Verify
 	// there is only one pod left.
 	stopCh := make(chan struct{})
-	s, rm, _, clientSet := rmSetup(t, stopCh, true)
+	s, rm, _, clientSet := rmSetup(t, stopCh)
 	ns := framework.CreateTestingNamespace("update-selector-to-adopt", s, t)
 	defer framework.DeleteTestingNamespace(ns, s, t)
 	rc := newRC("rc", ns.Name, 1)
@@ -325,7 +325,7 @@ func TestUpdateSelectorToRemoveControllerRef(t *testing.T) {
 	// that rc creates one more pod, so there are 3 pods. Also verify that
 	// pod2's controllerRef is cleared.
 	stopCh := make(chan struct{})
-	s, rm, informers, clientSet := rmSetup(t, stopCh, true)
+	s, rm, informers, clientSet := rmSetup(t, stopCh)
 	ns := framework.CreateTestingNamespace("update-selector-to-remove-controllerref", s, t)
 	defer framework.DeleteTestingNamespace(ns, s, t)
 	rc := newRC("rc", ns.Name, 2)
@@ -370,7 +370,7 @@ func TestUpdateLabelToRemoveControllerRef(t *testing.T) {
 	// that rc creates one more pod, so there are 3 pods. Also verify that
 	// pod2's controllerRef is cleared.
 	stopCh := make(chan struct{})
-	s, rm, _, clientSet := rmSetup(t, stopCh, true)
+	s, rm, _, clientSet := rmSetup(t, stopCh)
 	ns := framework.CreateTestingNamespace("update-label-to-remove-controllerref", s, t)
 	defer framework.DeleteTestingNamespace(ns, s, t)
 	rc := newRC("rc", ns.Name, 2)
@@ -411,7 +411,7 @@ func TestUpdateLabelToBeAdopted(t *testing.T) {
 	// controller adopts pod2 and delete one of them, so there is only 1 pod
 	// left.
 	stopCh := make(chan struct{})
-	s, rm, _, clientSet := rmSetup(t, stopCh, true)
+	s, rm, _, clientSet := rmSetup(t, stopCh)
 	ns := framework.CreateTestingNamespace("update-label-to-be-adopted", s, t)
 	defer framework.DeleteTestingNamespace(ns, s, t)
 	rc := newRC("rc", ns.Name, 1)
