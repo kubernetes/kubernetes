3330e99e2ca891d5e52f99fa234d7459bf6bc2fe
Jan Safranek <jsafrane@redhat.com>
2017-02-23 12:23:03 +0100

    Regenerate everything

--- pkg/generated/openapi/zz_generated.openapi.go
+++ pkg/generated/openapi/zz_generated.openapi.go
@@ -5312,6 +5312,13 @@ func GetOpenAPIDefinitions(ref openapi.ReferenceCallback) map[string]openapi.Ope
 								Format:      "",
 							},
 						},
+						"storageClassName": {
+							SchemaProps: spec.SchemaProps{
+								Description: "Name of the StorageClass required by the claim. More info: http://kubernetes.io/docs/user-guide/persistent-volumes#class-1",
+								Type:        []string{"string"},
+								Format:      "",
+							},
+						},
 					},
 				},
 			},
@@ -5689,6 +5696,13 @@ func GetOpenAPIDefinitions(ref openapi.ReferenceCallback) map[string]openapi.Ope
 						"persistentVolumeReclaimPolicy": {
 							SchemaProps: spec.SchemaProps{
 								Description: "What happens to a persistent volume when released from its claim. Valid options are Retain (default) and Recycle. Recycling must be supported by the volume plugin underlying this persistent volume. More info: http://kubernetes.io/docs/user-guide/persistent-volumes#recycling-policy",
+								Type:        []string{"string"},
+								Format:      "",
+							},
+						},
+						"storageClassName": {
+							SchemaProps: spec.SchemaProps{
+								Description: "Name of StorageClass to which this persistent volume belongs. Empty value means that this volume does not belong to any StorageClass.",
 								Type:        []string{"string"},
 								Format:      "",
 							},
