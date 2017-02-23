a2ee41d32c2a7f6b2dc25923e044577711910dc7
Maciej Szulik <maszulik@redhat.com>
2017-02-23 18:17:29 +0100

    Generated changes for apps/v1beta1.deployments

--- pkg/client/informers/informers_generated/externalversions/apps/v1beta1/interface.go
+++ pkg/client/informers/informers_generated/externalversions/apps/v1beta1/interface.go
@@ -24,6 +24,8 @@ import (
 
 // Interface provides access to all the informers in this group version.
 type Interface interface {
+	// Deployments returns a DeploymentInformer.
+	Deployments() DeploymentInformer
 	// StatefulSets returns a StatefulSetInformer.
 	StatefulSets() StatefulSetInformer
 }
@@ -35,6 +37,11 @@ type version struct {
 // New returns a new Interface.
 func New(f internalinterfaces.SharedInformerFactory) Interface {
 	return &version{f}
+}
+
+// Deployments returns a DeploymentInformer.
+func (v *version) Deployments() DeploymentInformer {
+	return &deploymentInformer{factory: v.SharedInformerFactory}
 }
 
 // StatefulSets returns a StatefulSetInformer.
