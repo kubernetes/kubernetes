fe70fd4942217d04121e75f8d6766aaa9d74980e
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-02-23 18:13:15 +0100

    update-staging-{client-go,godeps}.sh: no godep-restore, pin godep, check workdir

--- hack/update-staging-godeps.sh
+++ hack/update-staging-godeps.sh
@@ -23,102 +23,84 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+V=""
+DRY_RUN=false
+FAIL_ON_DIFF=false
+while getopts ":vdf" opt; do
+  case $opt in
+    v) # increase verbosity
+      V="-v"
+      ;;
+    d) # do not godep-restore into a temporary directory, but use the existing GOPATH
+      DRY_RUN=true
+      ;;
+    f) # fail if something in the Godeps.json files changed
+      FAIL_ON_DIFF=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+readonly V IN_PLACE
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
 source "${KUBE_ROOT}/hack/lib/init.sh"
+source "${KUBE_ROOT}/hack/lib/util.sh"
 
-kube::golang::setup_env
-
-ORIGINAL_GOPATH="${GOPATH}"
-
-godepBinDir=${TMPDIR:-/tmp/}/kube-godep-bin
-mkdir -p "${godepBinDir}"
-godep=${godepBinDir}/bin/godep
-pushd "${godepBinDir}" 2>&1 > /dev/null
-	# Build the godep tool
-	GOPATH="${godepBinDir}"
-	rm -rf *
-	go get -u github.com/tools/godep 2>/dev/null
-	export GODEP="${GOPATH}/bin/godep"
-	pin-godep() {
-		pushd "${GOPATH}/src/github.com/tools/godep" > /dev/null
-			git checkout "$1"
-			"${GODEP}" go install
-		popd > /dev/null
-	}
-	# Use to following if we ever need to pin godep to a specific version again
-	pin-godep 'v74'
-	"${godep}" version
-popd 2>&1 > /dev/null
-
-# keep the godep restore path reasonably stable to avoid unnecessary restores
-godepRestoreDir=${TMPDIR:-/tmp/}/kube-godep-restore
-
-TARGET_DIR=${TARGET_DIR:-${KUBE_ROOT}/staging}
-echo "working in ${TARGET_DIR}"
-
-SKIP_RESTORE=${SKIP_RESTORE:-}
-if [ "${SKIP_RESTORE}" != "true" ]; then
-	echo "starting godep restore"
-	mkdir -p "${godepRestoreDir}"
-
-	# add the vendor folder so that we don't redownload things during restore
-	GOPATH="${godepRestoreDir}:${KUBE_ROOT}/staging:${ORIGINAL_GOPATH}"
-	# restore from kubernetes godeps to ensure we get the correct levels
-	# you get errors about the staging repos not using a known version control system
-	${godep} restore > ${godepRestoreDir}/godep-restore.log
-	echo "finished godep restore"
+echo "Checking whether godeps are restored"
+if ! kube::util::godep_restored 2>&1 | sed 's/^/  /'; then
+  echo -e '\nRun 'godep restore' to download dependencies.' 1>&2
+  exit 1
 fi
 
-echo "forcing fake godep folders to match the current state of master in tmp"
-for stagingRepo in $(ls ${KUBE_ROOT}/staging/src/k8s.io); do
-	echo "    creating ${stagingRepo}"
-	rm -rf ${godepRestoreDir}/src/k8s.io/${stagingRepo}
-	cp -a ${KUBE_ROOT}/staging/src/k8s.io/${stagingRepo} ${godepRestoreDir}/src/k8s.io
+kube::util::ensure_clean_working_dir
+kube::util::ensure-temp-dir
+kube::util::ensure_godep_version v74
 
-	# we need a git commit in the godep folder, otherwise godep won't save
-	pushd ${godepRestoreDir}/src/k8s.io/${stagingRepo}
-	git init > /dev/null
-	# we need this so later commands work, but nothing should ever actually include these commits
-	# these are local only, not global
-	git config user.email "you@example.com"
-	git config user.name "Your Name"
-	git add . > /dev/null
-	git commit -qm "fake commit"
-	popd
-done
+TMP_GOPATH="${KUBE_TEMP}/go"
 
 function updateGodepManifest() {
-	local repo=${1}
+  local repo="${1}"
+  pushd "${TMP_GOPATH}/src/k8s.io/${repo}" >/dev/null
+    echo "Updating godeps for k8s.io/${repo}"
+    rm -rf Godeps # remove the current Godeps.json so we always rebuild it
+    GOPATH="${TMP_GOPATH}:${GOPATH}:${KUBE_ROOT}/staging" godep save ${V} ./... 2>&1 | sed 's/^/  /'
 
-	echo "starting ${repo} save"
-	mkdir -p ${TARGET_DIR}/src/k8s.io
-	# if target_dir isn't the same as source dir, you need copy
-	test "${KUBE_ROOT}/staging" = "${TARGET_DIR}" || cp -a ${KUBE_ROOT}/staging/src/${repo} ${TARGET_DIR}/src/k8s.io
-	# remove the current Godeps.json so we always rebuild it
-	rm -rf ${TARGET_DIR}/src/${repo}/Godeps
-	GOPATH="${godepRestoreDir}:${TARGET_DIR}"
-	pushd ${TARGET_DIR}/src/${repo}
-	${godep} save ./...
+    echo "Rewriting Godeps.json to remove commits that don't really exist because we haven't pushed the prereqs yet"
+    go run "${KUBE_ROOT}/staging/godeps-json-updater.go" --godeps-file="${TMP_GOPATH}/src/k8s.io/${repo}/Godeps/Godeps.json" --client-go-import-path="k8s.io/${repo}"
 
-	# now remove all the go files.	We'll re-run a restore, go get, godep save cycle in the sync scripts
-	# to get the commits for other staging k8s.io repos anyway, so we don't need the added files
-	rm -rf vendor
-
-	echo "rewriting Godeps.json to remove commits that don't really exist because we haven't pushed the prereqs yet"
-	GOPATH="${ORIGINAL_GOPATH}"
-	go run "${KUBE_ROOT}/staging/godeps-json-updater.go" --godeps-file="${TARGET_DIR}/src/${repo}/Godeps/Godeps.json" --client-go-import-path="${repo}"
-
-	popd
-	echo "finished ${repo} save"
+    # commit so that following repos do not see this repo as dirty
+    git add vendor >/dev/null
+    git commit -a -m "Updated Godeps.json" >/dev/null
+  popd >/dev/null
 }
 
 # move into staging and save the dependencies for everything in order
-for stagingRepo in $(ls ${KUBE_ROOT}/staging/src/k8s.io); do
-	# we have to skip client-go because it does unusual manipulation of its godeps
-	if [ "${stagingRepo}" == "client-go" ]; then
-		continue
-	fi
+mkdir -p "${TMP_GOPATH}/src/k8s.io"
+for repo in $(ls ${KUBE_ROOT}/staging/src/k8s.io); do
+  # we have to skip client-go because it does unusual manipulation of its godeps
+  if [ "${repo}" == "client-go" ]; then
+    continue
+  fi
 
-	updateGodepManifest "k8s.io/${stagingRepo}"
+  cp -a "${KUBE_ROOT}/staging/src/k8s.io/${repo}" "${TMP_GOPATH}/src/k8s.io/"
+
+  pushd "${TMP_GOPATH}/src/k8s.io/${repo}" >/dev/null
+    git init >/dev/null
+    git config --local user.email "nobody@k8s.io"
+    git config --local user.name "$0"
+    git add . >/dev/null
+    git commit -q -m "Snapshot" >/dev/null
+  popd >/dev/null
+
+  updateGodepManifest "${repo}"
+
+  if [ "${FAIL_ON_DIFF}" == true ]; then
+    diff --ignore-matching-lines='^\s*\"Comment\"' -u "${KUBE_ROOT}/staging/src/k8s.io/${repo}/Godeps" "${TMP_GOPATH}/src/k8s.io/${repo}/Godeps/Godeps.json"
+  fi
+  if [ "${DRY_RUN}" != true ]; then
+    cp "${TMP_GOPATH}/src/k8s.io/${repo}/Godeps/Godeps.json" "${KUBE_ROOT}/staging/src/k8s.io/${repo}/Godeps"
+  fi
 done
--- staging/copy.sh
+++ staging/copy.sh
@@ -18,9 +18,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+
+KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
+source "${KUBE_ROOT}/hack/lib/init.sh"
+
+
 FAIL_ON_CHANGES=false
 DRY_RUN=false
-while getopts ":fd" opt; do
+RUN_FROM_UPDATE_SCRIPT=false
+while getopts ":fdu" opt; do
   case $opt in
     f)
       FAIL_ON_CHANGES=true
@@ -28,24 +34,30 @@ while getopts ":fd" opt; do
     d)
       DRY_RUN=true
       ;;
+    u)
+      RUN_FROM_UPDATE_SCRIPT=true
+      ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
+      exit 1
       ;;
   esac
 done
 readonly FAIL_ON_CHANGES DRY_RUN
 
-echo "**PLEASE** run \"godep restore\" before running this script"
+if [ "${RUN_FROM_UPDATE_SCRIPT}" != true ]; then
+  echo "Do not run this script directly, but via hack/update-staging-client-go.sh."
+  exit 1
+fi
+
 # PREREQUISITES: run `godep restore` in the main repo before calling this script.
 CLIENTSET="clientset"
 MAIN_REPO_FROM_SRC="k8s.io/kubernetes"
-MAIN_REPO="${GOPATH%:*}/src/${MAIN_REPO_FROM_SRC}"
+MAIN_REPO="$(cd "${KUBE_ROOT}"; pwd)" # absolute path
 CLIENT_REPO_FROM_SRC="k8s.io/client-go"
 CLIENT_REPO_TEMP_FROM_SRC="k8s.io/_tmp"
 CLIENT_REPO="${MAIN_REPO}/staging/src/${CLIENT_REPO_FROM_SRC}"
 CLIENT_REPO_TEMP="${MAIN_REPO}/staging/src/${CLIENT_REPO_TEMP_FROM_SRC}"
-
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if LANG=C sed --help 2>&1 | grep -q GNU; then
   SED="sed"
@@ -127,7 +139,15 @@ rm -rf "${CLIENT_REPO_TEMP}"/vendor/k8s.io/kubernetes
 mv "${CLIENT_REPO_TEMP}"/vendor "${CLIENT_REPO_TEMP}"/_vendor
 
 echo "rewriting Godeps.json"
-go run "${DIR}/godeps-json-updater.go" --godeps-file="${CLIENT_REPO_TEMP}/Godeps/Godeps.json" --client-go-import-path="${CLIENT_REPO_FROM_SRC}"
+# Because we set two paths in $GOPATH, godep mistakenly constructs import paths
+# of dependencies found in the secondary GOPATH with the primary GOPATH. We
+# remove the extra relative path.
+#${SED} -i 's|k8s.io/kubernetes/staging/src/||g' "${CLIENT_REPO_TEMP}/Godeps/Godeps.json"
+
+# The entries for k8s.io/apimahcinery are not removed from Godeps.json, though
+# they contain the invalid commit revision. The publish robot will set the
+# correct commit revision.
+go run "${KUBE_ROOT}/staging/godeps-json-updater.go" --godeps-file="${CLIENT_REPO_TEMP}/Godeps/Godeps.json" --client-go-import-path="${CLIENT_REPO_FROM_SRC}" --ignored-prefixes="k8s.io/client-go,k8s.io/kubernetes"
 
 echo "rewriting imports"
 grep -Rl "\"${MAIN_REPO_FROM_SRC}" "${CLIENT_REPO_TEMP}" | \
@@ -211,7 +231,7 @@ if [ "${FAIL_ON_CHANGES}" = true ]; then
       cleanup
       exit 0
     else
-      echo "${CLIENT_REPO} is out of date. Please run hack/update-client-go.sh"
+      echo "${CLIENT_REPO} is out of date. Please run hack/update-staging-client-go.sh"
       cleanup
       exit 1
     fi
