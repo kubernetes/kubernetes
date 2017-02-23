74266e0dc02d6dd6232ec6ac8566eb5e9a30da04
Wojciech Tyczynski <wojtekt@google.com>
2017-02-23 14:46:09 +0100

    Release 3.0.17 etcd image

--- federation/pkg/kubefed/init/init.go
+++ federation/pkg/kubefed/init/init.go
@@ -622,7 +622,7 @@ func createAPIServer(clientset *client.Clientset, namespace, name, image, creden
 						},
 						{
 							Name:  "etcd",
-							Image: "gcr.io/google_containers/etcd:3.0.17-alpha.1",
+							Image: "gcr.io/google_containers/etcd:3.0.17",
 							Command: []string{
 								"/usr/local/bin/etcd",
 								"--data-dir",
