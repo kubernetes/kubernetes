01b8ecffe9018a8451e8a3aa475977425d135d51
Miao Luo <miaol@vmware.com>
2017-02-23 15:58:49 -0800

    Remove VCenterPort from vsphere cloud provider.
    The url which is used for communicating with govmomi should not include
    port number. A port number other than 443 will result in 404 error.
    VCenterPort stays in VSphereConfig structure for backward compatibility.

--- pkg/cloudprovider/providers/vsphere/vsphere.go
+++ pkg/cloudprovider/providers/vsphere/vsphere.go
@@ -276,6 +276,9 @@ func newVSphere(cfg VSphereConfig) (*VSphere, error) {
 	if cfg.Global.RoundTripperCount == 0 {
 		cfg.Global.RoundTripperCount = RoundTripperDefaultCount
 	}
+	if cfg.Global.VCenterPort != "" {
+		glog.Warningf("port is a deprecated field in vsphere.conf and will be removed in future release.")
+	}
 
 	c, err := newClient(&cfg, context.TODO())
 	if err != nil {
@@ -313,7 +316,7 @@ func logout(vs *VSphere) {
 
 func newClient(cfg *VSphereConfig, ctx context.Context) (*govmomi.Client, error) {
 	// Parse URL from string
-	u, err := url.Parse(fmt.Sprintf("https://%s:%s/sdk", cfg.Global.VCenterIP, cfg.Global.VCenterPort))
+	u, err := url.Parse(fmt.Sprintf("https://%s/sdk", cfg.Global.VCenterIP))
 	if err != nil {
 		return nil, err
 	}
