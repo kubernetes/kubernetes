c7954a251e172232b1630d8c8ce8661e673adc63
Jonathan MacMillan <dvorak@google.com>
2017-02-24 00:33:40 -0800

    [Federation] Use service accounts instead of the user's credentials when accessing joined clusters' API servers.

--- federation/pkg/kubefed/join_test.go
+++ federation/pkg/kubefed/join_test.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"testing"
 
 	apiequality "k8s.io/apimachinery/pkg/api/equality"
@@ -38,6 +39,8 @@ import (
 	"k8s.io/kubernetes/pkg/api/testapi"
 	"k8s.io/kubernetes/pkg/api/v1"
 	"k8s.io/kubernetes/pkg/apis/extensions/v1beta1"
+	rbacv1beta1 "k8s.io/kubernetes/pkg/apis/rbac/v1beta1"
+	"k8s.io/kubernetes/pkg/kubectl"
 	cmdtesting "k8s.io/kubernetes/pkg/kubectl/cmd/testing"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 )
@@ -75,6 +78,7 @@ func TestJoinFederation(t *testing.T) {
 		expectedServer     string
 		expectedErr        string
 		dnsProvider        string
+		isRBACAPIAvailable bool
 	}{
 		{
 			cluster:            "syndicate",
@@ -86,6 +90,19 @@ func TestJoinFederation(t *testing.T) {
 			expectedServer:     "https://10.20.30.40",
 			expectedErr:        "",
 			dnsProvider:        util.FedDNSProviderCoreDNS,
+			isRBACAPIAvailable: true,
+		},
+		{
+			cluster:            "syndicate",
+			clusterCtx:         "",
+			secret:             "",
+			server:             "https://10.20.30.40",
+			token:              "badge",
+			kubeconfigGlobal:   fakeKubeFiles[0],
+			kubeconfigExplicit: "",
+			expectedServer:     "https://10.20.30.40",
+			expectedErr:        "",
+			isRBACAPIAvailable: false,
 		},
 		{
 			cluster:            "ally",
@@ -96,6 +113,7 @@ func TestJoinFederation(t *testing.T) {
 			kubeconfigExplicit: fakeKubeFiles[1],
 			expectedServer:     "https://ally256.example.com:80",
 			expectedErr:        "",
+			isRBACAPIAvailable: true,
 		},
 		{
 			cluster:            "confederate",
@@ -106,6 +124,7 @@ func TestJoinFederation(t *testing.T) {
 			kubeconfigExplicit: fakeKubeFiles[2],
 			expectedServer:     "https://10.8.8.8",
 			expectedErr:        "",
+			isRBACAPIAvailable: true,
 		},
 		{
 			cluster:            "associate",
@@ -116,6 +135,7 @@ func TestJoinFederation(t *testing.T) {
 			kubeconfigExplicit: fakeKubeFiles[2],
 			expectedServer:     "https://10.8.8.8",
 			expectedErr:        "",
+			isRBACAPIAvailable: true,
 		},
 		{
 			cluster:            "affiliate",
@@ -126,6 +146,7 @@ func TestJoinFederation(t *testing.T) {
 			kubeconfigExplicit: "",
 			expectedServer:     "https://10.20.30.40",
 			expectedErr:        fmt.Sprintf("error: cluster context %q not found", "affiliate"),
+			isRBACAPIAvailable: true,
 		},
 		{
 			cluster:            "associate",
@@ -142,15 +163,23 @@ func TestJoinFederation(t *testing.T) {
 
 	for i, tc := range testCases {
 		cmdErrMsg = ""
-		f := testJoinFederationFactory(tc.cluster, tc.secret, tc.expectedServer)
+		f := testJoinFederationFactory(tc.cluster, tc.secret, tc.expectedServer, tc.isRBACAPIAvailable)
 		buf := bytes.NewBuffer([]byte{})
 
-		hostFactory, err := fakeJoinHostFactory(tc.cluster, tc.clusterCtx, tc.secret, tc.server, tc.token, tc.dnsProvider)
+		hostFactory, err := fakeJoinHostFactory(tc.cluster, tc.clusterCtx, tc.secret, tc.server, tc.token, tc.dnsProvider, tc.isRBACAPIAvailable)
 		if err != nil {
 			t.Fatalf("[%d] unexpected error: %v", i, err)
 		}
 
-		targetClusterFactory, err := fakeJoinTargetClusterFactory(tc.cluster, tc.clusterCtx, tc.dnsProvider)
+		// The fake discovery client caches results by default, so invalidate it by modifying the temporary directory.
+		// Refer to pkg/kubectl/cmd/testing/fake (fakeAPIFactory.DiscoveryClient()) for details of tmpDir
+		tmpDirPath, err := ioutil.TempDir("", "")
+		if err != nil {
+			t.Fatalf("[%d] unexpected error: %v", i, err)
+		}
+		defer os.Remove(tmpDirPath)
+
+		targetClusterFactory, err := fakeJoinTargetClusterFactory(tc.cluster, tc.clusterCtx, tc.dnsProvider, tmpDirPath, tc.isRBACAPIAvailable)
 		if err != nil {
 			t.Fatalf("[%d] unexpected error: %v", i, err)
 		}
@@ -195,9 +224,9 @@ func TestJoinFederation(t *testing.T) {
 	}
 }
 
-func testJoinFederationFactory(clusterName, secretName, server string) cmdutil.Factory {
+func testJoinFederationFactory(clusterName, secretName, server string, isRBACAPIAvailable bool) cmdutil.Factory {
 
-	want := fakeCluster(clusterName, secretName, server)
+	want := fakeCluster(clusterName, secretName, server, isRBACAPIAvailable)
 	f, tf, _, _ := cmdtesting.NewAPIFactory()
 	codec := testapi.Federation.Codec()
 	ns := dynamic.ContentConfig().NegotiatedSerializer
@@ -236,55 +265,77 @@ func testJoinFederationFactory(clusterName, secretName, server string) cmdutil.F
 	return f
 }
 
-func fakeJoinHostFactory(clusterName, clusterCtx, secretName, server, token, dnsProvider string) (cmdutil.Factory, error) {
+func fakeJoinHostFactory(clusterName, clusterCtx, secretName, server, token, dnsProvider string, isRBACAPIAvailable bool) (cmdutil.Factory, error) {
 	if clusterCtx == "" {
 		clusterCtx = clusterName
-	}
-
-	kubeconfig := clientcmdapi.Config{
-		Clusters: map[string]*clientcmdapi.Cluster{
-			clusterCtx: {
-				Server: server,
-			},
-		},
-		AuthInfos: map[string]*clientcmdapi.AuthInfo{
-			clusterCtx: {
-				Token: token,
-			},
-		},
-		Contexts: map[string]*clientcmdapi.Context{
-			clusterCtx: {
-				Cluster:  clusterCtx,
-				AuthInfo: clusterCtx,
-			},
-		},
-		CurrentContext: clusterCtx,
-	}
-	configBytes, err := clientcmd.Write(kubeconfig)
-	if err != nil {
-		return nil, err
 	}
 
 	placeholderSecretName := secretName
 	if placeholderSecretName == "" {
 		placeholderSecretName = "secretName"
 	}
-	secretObject := v1.Secret{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Secret",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      placeholderSecretName,
-			Namespace: util.DefaultFederationSystemNamespace,
-			Annotations: map[string]string{
-				federation.FederationNameAnnotation: testFederationName,
-				federation.ClusterNameAnnotation:    clusterName,
+	var secretObject v1.Secret
+	if isRBACAPIAvailable {
+		secretObject = v1.Secret{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Secret",
+				APIVersion: "v1",
 			},
-		},
-		Data: map[string][]byte{
-			"kubeconfig": configBytes,
-		},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      placeholderSecretName,
+				Namespace: util.DefaultFederationSystemNamespace,
+				Annotations: map[string]string{
+					federation.FederationNameAnnotation: testFederationName,
+					federation.ClusterNameAnnotation:    clusterName,
+				},
+			},
+			Data: map[string][]byte{
+				"ca.crt": []byte("cert"),
+				"token":  []byte("token"),
+			},
+		}
+	} else {
+		kubeconfig := clientcmdapi.Config{
+			Clusters: map[string]*clientcmdapi.Cluster{
+				clusterCtx: {
+					Server: server,
+				},
+			},
+			AuthInfos: map[string]*clientcmdapi.AuthInfo{
+				clusterCtx: {
+					Token: token,
+				},
+			},
+			Contexts: map[string]*clientcmdapi.Context{
+				clusterCtx: {
+					Cluster:  clusterCtx,
+					AuthInfo: clusterCtx,
+				},
+			},
+			CurrentContext: clusterCtx,
+		}
+		configBytes, err := clientcmd.Write(kubeconfig)
+		if err != nil {
+			return nil, err
+		}
+
+		secretObject = v1.Secret{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Secret",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      placeholderSecretName,
+				Namespace: util.DefaultFederationSystemNamespace,
+				Annotations: map[string]string{
+					federation.FederationNameAnnotation: testFederationName,
+					federation.ClusterNameAnnotation:    clusterName,
+				},
+			},
+			Data: map[string][]byte{
+				"kubeconfig": configBytes,
+			},
+		}
 	}
 
 	cmName := "controller-manager"
@@ -351,7 +402,11 @@ func fakeJoinHostFactory(clusterName, clusterCtx, secretName, server, token, dns
 	return f, nil
 }
 
-func fakeJoinTargetClusterFactory(clusterName, clusterCtx, dnsProvider string) (cmdutil.Factory, error) {
+func saName(clusterName string) string {
+	return fmt.Sprintf("%s-substrate", clusterName)
+}
+
+func fakeJoinTargetClusterFactory(clusterName, clusterCtx, dnsProvider, tmpDirPath string, isRBACAPIAvailable bool) (cmdutil.Factory, error) {
 	if clusterCtx == "" {
 		clusterCtx = clusterName
 	}
@@ -369,6 +424,38 @@ func fakeJoinTargetClusterFactory(clusterName, clusterCtx, dnsProvider string) (
 			util.FedDomainMapKey: fmt.Sprintf("%s=%s", clusterCtx, zoneName),
 		},
 	}
+
+	saSecretName := "serviceaccountsecret"
+	saSecret := v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      saSecretName,
+			Namespace: util.DefaultFederationSystemNamespace,
+			Annotations: map[string]string{
+				federation.FederationNameAnnotation: testFederationName,
+				federation.ClusterNameAnnotation:    clusterName,
+			},
+		},
+		Data: map[string][]byte{
+			"ca.crt": []byte("cert"),
+			"token":  []byte("token"),
+		},
+		Type: v1.SecretTypeServiceAccountToken,
+	}
+
+	saName := saName(clusterName)
+
+	serviceAccount := v1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: saName,
+			Annotations: map[string]string{
+				federation.FederationNameAnnotation: testFederationName,
+				federation.ClusterNameAnnotation:    clusterName,
+			},
+		},
+		Secrets: []v1.ObjectReference{
+			{Name: saSecretName},
+		},
+	}
 	if dnsProvider == util.FedDNSProviderCoreDNS {
 		annotations := map[string]string{
 			util.FedDNSProvider: util.FedDNSProviderCoreDNS,
@@ -378,14 +465,92 @@ func fakeJoinTargetClusterFactory(clusterName, clusterCtx, dnsProvider string) (
 		configmapObject = populateStubDomainsIfRequiredTest(configmapObject, annotations)
 	}
 
+	namespace := v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "federation-system",
+			Annotations: map[string]string{
+				federation.FederationNameAnnotation: testFederationName,
+				federation.ClusterNameAnnotation:    clusterName,
+			},
+		},
+	}
+
+	roleName := util.ClusterRoleName(saName)
+	clusterRole := rbacv1beta1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      roleName,
+			Namespace: util.DefaultFederationSystemNamespace,
+			Annotations: map[string]string{
+				federation.FederationNameAnnotation: testFederationName,
+				federation.ClusterNameAnnotation:    clusterName,
+			},
+		},
+		Rules: []rbacv1beta1.PolicyRule{
+			rbacv1beta1.NewRule(rbacv1beta1.VerbAll).Groups(rbacv1beta1.APIGroupAll).Resources(rbacv1beta1.ResourceAll).RuleOrDie(),
+		},
+	}
+
+	clusterRoleBinding, err := rbacv1beta1.NewClusterBinding(roleName).SAs(util.DefaultFederationSystemNamespace, saName).Binding()
+	if err != nil {
+		return nil, err
+	}
+
+	testGroup := metav1.APIGroup{
+		Name: "testAPIGroup",
+		Versions: []metav1.GroupVersionForDiscovery{
+			{
+				GroupVersion: "testAPIGroup/testAPIVersion",
+				Version:      "testAPIVersion",
+			},
+		},
+	}
+	apiGroupList := &metav1.APIGroupList{}
+	apiGroupList.Groups = append(apiGroupList.Groups, testGroup)
+	if isRBACAPIAvailable {
+		rbacGroup := metav1.APIGroup{
+			Name: rbacv1beta1.GroupName,
+			Versions: []metav1.GroupVersionForDiscovery{
+				{
+					GroupVersion: rbacv1beta1.GroupName + "/v1beta1",
+					Version:      "v1beta1",
+				},
+			},
+		}
+		apiGroupList.Groups = append(apiGroupList.Groups, rbacGroup)
+	}
+
 	f, tf, codec, _ := cmdtesting.NewAPIFactory()
+	defaultCodec := testapi.Default.Codec()
+	rbacCodec := testapi.Rbac.Codec()
 	ns := dynamic.ContentConfig().NegotiatedSerializer
+	tf.TmpDir = tmpDirPath
 	tf.ClientConfig = kubefedtesting.DefaultClientConfig()
 	tf.Client = &fake.RESTClient{
 		APIRegistry:          api.Registry,
 		NegotiatedSerializer: ns,
 		Client: fake.CreateHTTPClient(func(req *http.Request) (*http.Response, error) {
-			switch p, m := req.URL.Path, req.Method; {
+			switch p, m, r := req.URL.Path, req.Method, isRBACAPIAvailable; {
+			case p == "/api/v1/namespaces" && m == http.MethodPost:
+				return &http.Response{StatusCode: http.StatusOK, Header: kubefedtesting.DefaultHeader(), Body: kubefedtesting.ObjBody(defaultCodec, &namespace)}, nil
+
+			case p == "/api" && m == http.MethodGet:
+				return &http.Response{StatusCode: http.StatusOK, Header: kubefedtesting.DefaultHeader(), Body: kubefedtesting.ObjBody(codec, &metav1.APIVersions{})}, nil
+			case p == "/apis" && m == http.MethodGet:
+				return &http.Response{StatusCode: http.StatusOK, Header: kubefedtesting.DefaultHeader(), Body: kubefedtesting.ObjBody(codec, apiGroupList)}, nil
+
+			case p == fmt.Sprintf("/api/v1/namespaces/federation-system/serviceaccounts/%s", saName) && m == http.MethodGet && r:
+				return &http.Response{StatusCode: http.StatusOK, Header: kubefedtesting.DefaultHeader(), Body: kubefedtesting.ObjBody(defaultCodec, &serviceAccount)}, nil
+			case p == "/api/v1/namespaces/federation-system/serviceaccounts" && m == http.MethodPost && r:
+				return &http.Response{StatusCode: http.StatusOK, Header: kubefedtesting.DefaultHeader(), Body: kubefedtesting.ObjBody(defaultCodec, &serviceAccount)}, nil
+
+			case p == "/apis/rbac.authorization.k8s.io/v1beta1/clusterroles" && m == http.MethodPost && r:
+				return &http.Response{StatusCode: http.StatusOK, Header: kubefedtesting.DefaultHeader(), Body: kubefedtesting.ObjBody(rbacCodec, &clusterRole)}, nil
+			case p == "/apis/rbac.authorization.k8s.io/v1beta1/clusterrolebindings" && m == http.MethodPost && r:
+				return &http.Response{StatusCode: http.StatusOK, Header: kubefedtesting.DefaultHeader(), Body: kubefedtesting.ObjBody(rbacCodec, &clusterRoleBinding)}, nil
+
+			case p == "/api/v1/namespaces/federation-system/secrets/serviceaccountsecret" && m == http.MethodGet && r:
+				return &http.Response{StatusCode: http.StatusOK, Header: kubefedtesting.DefaultHeader(), Body: kubefedtesting.ObjBody(defaultCodec, &saSecret)}, nil
+
 			case p == "/api/v1/namespaces/kube-system/configmaps/" && m == http.MethodPost:
 				body, err := ioutil.ReadAll(req.Body)
 				if err != nil {
@@ -405,11 +570,12 @@ func fakeJoinTargetClusterFactory(clusterName, clusterCtx, dnsProvider string) (
 			}
 		}),
 	}
+
 	return f, nil
 }
 
-func fakeCluster(clusterName, secretName, server string) federationapi.Cluster {
-	return federationapi.Cluster{
+func fakeCluster(clusterName, secretName, server string, isRBACAPIAvailable bool) federationapi.Cluster {
+	cluster := federationapi.Cluster{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: clusterName,
 		},
@@ -425,6 +591,15 @@ func fakeCluster(clusterName, secretName, server string) federationapi.Cluster {
 			},
 		},
 	}
+	if isRBACAPIAvailable {
+		saName := saName(clusterName)
+		annotations := map[string]string{
+			kubectl.ServiceAccountNameAnnotation: saName,
+			kubectl.ClusterRoleNameAnnotation:    util.ClusterRoleName(saName),
+		}
+		cluster.ObjectMeta.SetAnnotations(annotations)
+	}
+	return cluster
 }
 
 // TODO: Reuse the function populateStubDomainsIfRequired once that function is converted to use versioned objects.
