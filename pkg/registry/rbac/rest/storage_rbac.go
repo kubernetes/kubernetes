4127322f233af16f7a072fac018e291dc146ca28
Jordan Liggitt <jliggitt@redhat.com>
2017-02-27 15:09:27 -0500

    Restore wait for etcd in rbac initialization

--- pkg/registry/rbac/rest/storage_rbac.go
+++ pkg/registry/rbac/rest/storage_rbac.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/golang/glog"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -131,7 +132,16 @@ func PostStartHook(hookContext genericapiserver.PostStartHookContext) error {
 	err := wait.Poll(1*time.Second, 30*time.Second, func() (done bool, err error) {
 		clientset, err := rbacclient.NewForConfig(hookContext.LoopbackClientConfig)
 		if err != nil {
+			utilruntime.HandleError(fmt.Errorf("unable to initialize client: %v", err))
+			return false, nil
+		}
+		// Make sure etcd is responding before we start reconciling
+		if _, err := clientset.ClusterRoles().List(metav1.ListOptions{}); err != nil {
 			utilruntime.HandleError(fmt.Errorf("unable to initialize clusterroles: %v", err))
+			return false, nil
+		}
+		if _, err := clientset.ClusterRoleBindings().List(metav1.ListOptions{}); err != nil {
+			utilruntime.HandleError(fmt.Errorf("unable to initialize clusterrolebindings: %v", err))
 			return false, nil
 		}
 
