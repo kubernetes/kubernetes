584b57b8b4596d52559bdd6b79503f8f70281aec
Jan Safranek <jsafrane@redhat.com>
2017-03-02 08:55:25 +0100

    Add storage.k8s.io/v1

--- hack/lib/init.sh
+++ hack/lib/init.sh
@@ -70,6 +70,7 @@ rbac.authorization.k8s.io/v1beta1 \
 rbac.authorization.k8s.io/v1alpha1 \
 settings.k8s.io/v1alpha1 \
 storage.k8s.io/v1beta1 \
+storage.k8s.io/v1\
 }"
 
 # not all group versions are exposed by the server.  This list contains those
