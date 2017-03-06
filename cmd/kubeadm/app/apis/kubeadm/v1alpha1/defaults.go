e56aded7c6082297c29989bfe9e6e0888a491e73
xilabao <chenr.fnst@cn.fujitsu.com>
2017-03-06 13:56:34 +0800

    Allow multiple providers for authorizationMode

--- cmd/kubeadm/app/apis/kubeadm/v1alpha1/defaults.go
+++ cmd/kubeadm/app/apis/kubeadm/v1alpha1/defaults.go
@@ -61,8 +61,8 @@ func SetDefaults_MasterConfiguration(obj *MasterConfiguration) {
 		obj.Networking.DNSDomain = DefaultServiceDNSDomain
 	}
 
-	if obj.AuthorizationMode == "" {
-		obj.AuthorizationMode = DefaultAuthorizationMode
+	if len(obj.AuthorizationModes) == 0 {
+		obj.AuthorizationModes = []string{DefaultAuthorizationMode}
 	}
 
 	if obj.CertificatesDir == "" {
--- cmd/kubeadm/app/apis/kubeadm/v1alpha1/types.go
+++ cmd/kubeadm/app/apis/kubeadm/v1alpha1/types.go
@@ -25,12 +25,12 @@ import (
 type MasterConfiguration struct {
 	metav1.TypeMeta `json:",inline"`
 
-	API               API        `json:"api"`
-	Etcd              Etcd       `json:"etcd"`
-	Networking        Networking `json:"networking"`
-	KubernetesVersion string     `json:"kubernetesVersion"`
-	CloudProvider     string     `json:"cloudProvider"`
-	AuthorizationMode string     `json:"authorizationMode"`
+	API                API        `json:"api"`
+	Etcd               Etcd       `json:"etcd"`
+	Networking         Networking `json:"networking"`
+	KubernetesVersion  string     `json:"kubernetesVersion"`
+	CloudProvider      string     `json:"cloudProvider"`
+	AuthorizationModes []string   `json:"authorizationModes"`
 
 	Token    string        `json:"token"`
 	TokenTTL time.Duration `json:"tokenTTL"`
