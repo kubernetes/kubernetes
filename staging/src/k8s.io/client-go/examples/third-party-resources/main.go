94cd2aa039b7aeef5dac4cdc0d3c7eb1c1fcf56f
Chao Xu <xuchao@google.com>
2017-03-06 22:34:12 -0800

    adding note

--- staging/src/k8s.io/client-go/examples/third-party-resources/main.go
+++ staging/src/k8s.io/client-go/examples/third-party-resources/main.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Note: the example only works with the code within the same release/branch.
 package main
 
 import (
