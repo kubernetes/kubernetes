d2ffdfd337767e1ce17b0e42d1984d4a08fd7755
Cao Shufeng <caosf.fnst@cn.fujitsu.com>
2017-03-06 20:40:48 -0500

    fix "role craete" cli example
    trivial fix

--- pkg/kubectl/cmd/create_role.go
+++ pkg/kubectl/cmd/create_role.go
@@ -42,7 +42,7 @@ var (
 		kubectl create role pod-reader --verb=get --verb=list --verb=watch --resource=pods
 
 		# Create a Role named "pod-reader" with ResourceName specified
-		kubectl create role pod-reader --verb=get --verg=list --verb=watch --resource=pods --resource-name=readablepod`)
+		kubectl create role pod-reader --verb=get --verb=list --verb=watch --resource=pods --resource-name=readablepod`)
 
 	// Valid resource verb list for validation.
 	validResourceVerbs = []string{"*", "get", "delete", "list", "create", "update", "patch", "watch", "proxy", "redirect", "deletecollection", "use"}
