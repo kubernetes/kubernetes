5fe7b865b7a1cf8d391cd7860c95e59d3c6de211
Angus Lees <gus@inodes.org>
2017-03-07 17:33:37 +1100

    Arrange for elasticsearch to shutdown cleanly
    Kubernetes initiates "graceful shutdown" by sending SIGTERM to pid 1.
    The way the existing startup scripts worked, this signal arrived at
    the shell wrapper, not elasticsearch, and the shell wrapper exited,
    killing the container immediately.
    
    Before this change:
        1 ?        Ss     0:00 /bin/sh -c /run.sh
        6 ?        S      0:00 /bin/bash /run.sh
       13 ?        S      0:00  \_ /bin/su -c /elasticsearch/bin/elasticsearch elasticsearch
       14 ?        Ss     0:00      \_ sh -c /elasticsearch/bin/elasticsearch
       15 ?        Sl    19:18          \_ /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java ... org.elasticsearch.bootstrap.Elasticsearch start
    
    After this change:
        1 ?        Ssl    0:29 /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java ... org.elasticsearch.bootstrap.Elasticsearch start

--- cluster/addons/fluentd-elasticsearch/es-image/run.sh
+++ cluster/addons/fluentd-elasticsearch/es-image/run.sh
@@ -25,4 +25,4 @@ export MINIMUM_MASTER_NODES=${MINIMUM_MASTER_NODES:-2}
 
 chown -R elasticsearch:elasticsearch /data
 
-/bin/su -c /elasticsearch/bin/elasticsearch elasticsearch
+exec gosu elasticsearch /elasticsearch/bin/elasticsearch
