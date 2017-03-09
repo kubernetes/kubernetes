26f4034814dfec4fb377449c1c65af9007e1357a
Michael Fraenkel <michael.fraenkel@gmail.com>
2017-03-09 08:10:10 -0800

    Update docs and client

--- staging/src/k8s.io/client-go/pkg/api/types.go
+++ staging/src/k8s.io/client-go/pkg/api/types.go
@@ -1503,8 +1503,8 @@ type Container struct {
 	// +optional
 	Ports []ContainerPort
 	// List of sources to populate environment variables in the container.
-	// The keys defined within a source must be a C_IDENTIFIER. An invalid key
-	// will prevent the container from starting. When a key exists in multiple
+	// The keys defined within a source must be a C_IDENTIFIER. All invalid keys
+	// will be reported as an event when the container is starting. When a key exists in multiple
 	// sources, the value associated with the last source will take precedence.
 	// Values defined by an Env with a duplicate key will take precedence.
 	// Cannot be updated.
