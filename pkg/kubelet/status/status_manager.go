11f13df901cdde32669f3a7f44ae3d0897c0bc3a
David Ashpole <dashpole@google.com>
2017-03-08 19:40:49 -0800

    do not skip pods that can be deleted

--- pkg/kubelet/status/status_manager.go
+++ pkg/kubelet/status/status_manager.go
@@ -414,7 +414,7 @@ func (m *manager) syncBatch() {
 
 // syncPod syncs the given status with the API server. The caller must not hold the lock.
 func (m *manager) syncPod(uid types.UID, status versionedPodStatus) {
-	if !m.needsUpdate(uid, status) {
+	if !m.needsUpdate(uid, status) && !m.couldBeDeleted(uid, status.status) {
 		glog.V(1).Infof("Status for pod %q is up-to-date; skipping", uid)
 		return
 	}
