4bc933d5adb062dc2e8c01b0068eb0db68da9cf5
StudyNick <wei.chuanhui1@zte.com.cn>
2017-03-10 09:59:59 +0800

    'ReplicationControllerNamespaeLister' word error

--- pkg/client/listers/core/v1/replicationcontroller_expansion.go
+++ pkg/client/listers/core/v1/replicationcontroller_expansion.go
@@ -30,7 +30,7 @@ type ReplicationControllerListerExpansion interface {
 }
 
 // ReplicationControllerNamespaceListerExpansion allows custom methods to be added to
-// ReplicationControllerNamespaeLister.
+// ReplicationControllerNamespaceLister.
 type ReplicationControllerNamespaceListerExpansion interface{}
 
 // GetPodControllers returns a list of ReplicationControllers that potentially match a pod.
