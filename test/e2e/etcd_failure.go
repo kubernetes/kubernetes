8f49f63ba1e3e74798a7f9089fb6af74dd3e1405
Michail Kargakis <mkargaki@redhat.com>
2017-03-11 16:50:36 +0100

    test: update replica set e2e test

--- test/e2e/etcd_failure.go
+++ test/e2e/etcd_failure.go
@@ -73,7 +73,7 @@ func etcdFailTest(f *framework.Framework, failCommand, fixCommand string) {
 
 	checkExistingRCRecovers(f)
 
-	ServeImageOrFail(f, "basic", "gcr.io/google_containers/serve_hostname:v1.4")
+	testReplicationControllerServeImageOrFail(f, "basic", "gcr.io/google_containers/serve_hostname:v1.4")
 }
 
 // For this duration, etcd will be failed by executing a failCommand on the master.
