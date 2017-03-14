dc9d75568dc5d14e2a8468e4e2d8640af7d0663a
Miao Luo <miaol@vmware.com>
2017-03-14 15:48:02 -0700

    Update photon controller go SDK in vendor code.

--- vendor/github.com/vmware/photon-controller-go-sdk/photon/images.go
+++ vendor/github.com/vmware/photon-controller-go-sdk/photon/images.go
@@ -10,6 +10,7 @@
 package photon
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 )
@@ -24,13 +25,13 @@ type ImageGetOptions struct {
 	Name string `urlParam:"name"`
 }
 
-var imageUrl string = "/images"
+var imageUrl string = rootUrl + "/images"
 
 // Uploads a new image, reading from the specified image path.
 // If options is nil, default options are used.
 func (api *ImagesAPI) CreateFromFile(imagePath string, options *ImageCreateOptions) (task *Task, err error) {
 	params := imageCreateOptionsToMap(options)
-	res, err := api.client.restClient.MultipartUploadFile(api.client.Endpoint+imageUrl, imagePath, params, api.client.options.TokenOptions.AccessToken)
+	res, err := api.client.restClient.MultipartUploadFile(api.client.Endpoint+imageUrl, imagePath, params, api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
@@ -43,9 +44,9 @@ func (api *ImagesAPI) CreateFromFile(imagePath string, options *ImageCreateOptio
 // Name is a descriptive name of the image, it is used in the filename field of the Content-Disposition header,
 // and does not need to be unique.
 // If options is nil, default options are used.
-func (api *ImagesAPI) Create(reader io.Reader, name string, options *ImageCreateOptions) (task *Task, err error) {
+func (api *ImagesAPI) Create(reader io.ReadSeeker, name string, options *ImageCreateOptions) (task *Task, err error) {
 	params := imageCreateOptionsToMap(options)
-	res, err := api.client.restClient.MultipartUpload(api.client.Endpoint+imageUrl, reader, name, params, api.client.options.TokenOptions.AccessToken)
+	res, err := api.client.restClient.MultipartUpload(api.client.Endpoint+imageUrl, reader, name, params, api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
@@ -60,7 +61,7 @@ func (api *ImagesAPI) GetAll(options *ImageGetOptions) (images *Images, err erro
 	if options != nil {
 		uri += getQueryString(options)
 	}
-	res, err := api.client.restClient.GetList(api.client.Endpoint, uri, api.client.options.TokenOptions.AccessToken)
+	res, err := api.client.restClient.GetList(api.client.Endpoint, uri, api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
@@ -72,7 +73,7 @@ func (api *ImagesAPI) GetAll(options *ImageGetOptions) (images *Images, err erro
 
 // Gets details of image with the specified ID.
 func (api *ImagesAPI) Get(imageID string) (image *Image, err error) {
-	res, err := api.client.restClient.Get(api.client.Endpoint+imageUrl+"/"+imageID, api.client.options.TokenOptions.AccessToken)
+	res, err := api.client.restClient.Get(api.client.Endpoint+imageUrl+"/"+imageID, api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
@@ -88,7 +89,7 @@ func (api *ImagesAPI) Get(imageID string) (image *Image, err error) {
 
 // Deletes image with the specified ID.
 func (api *ImagesAPI) Delete(imageID string) (task *Task, err error) {
-	res, err := api.client.restClient.Delete(api.client.Endpoint+imageUrl+"/"+imageID, api.client.options.TokenOptions.AccessToken)
+	res, err := api.client.restClient.Delete(api.client.Endpoint+imageUrl+"/"+imageID, api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
@@ -105,13 +106,69 @@ func (api *ImagesAPI) GetTasks(id string, options *TaskGetOptions) (result *Task
 		uri += getQueryString(options)
 	}
 
-	res, err := api.client.restClient.GetList(api.client.Endpoint, uri, api.client.options.TokenOptions.AccessToken)
+	res, err := api.client.restClient.GetList(api.client.Endpoint, uri, api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
 
 	result = &TaskList{}
 	err = json.Unmarshal(res, result)
+	return
+}
+
+// Gets IAM Policy of an image.
+func (api *ImagesAPI) GetIam(imageID string) (policy *[]PolicyEntry, err error) {
+	res, err := api.client.restClient.Get(
+		api.client.Endpoint+imageUrl+"/"+imageID+"/iam",
+		api.client.options.TokenOptions)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	res, err = getError(res)
+	if err != nil {
+		return
+	}
+	var result []PolicyEntry
+	err = json.NewDecoder(res.Body).Decode(&result)
+	return &result, nil
+}
+
+// Sets IAM Policy on an image.
+func (api *ImagesAPI) SetIam(imageID string, policy *[]PolicyEntry) (task *Task, err error) {
+	body, err := json.Marshal(policy)
+	if err != nil {
+		return
+	}
+	res, err := api.client.restClient.Post(
+		api.client.Endpoint+imageUrl+"/"+imageID+"/iam",
+		"application/json",
+		bytes.NewReader(body),
+		api.client.options.TokenOptions)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	task, err = getTask(getError(res))
+	return
+}
+
+// Modifies IAM Policy on an image.
+func (api *ImagesAPI) ModifyIam(imageID string, policyDelta *PolicyDelta) (task *Task, err error) {
+	body, err := json.Marshal(policyDelta)
+	if err != nil {
+		return
+	}
+	res, err := api.client.restClient.Put(
+		api.client.Endpoint+imageUrl+"/"+imageID+"/iam",
+		"application/json",
+		bytes.NewReader(body),
+		api.client.options.TokenOptions)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	task, err = getTask(getError(res))
 	return
 }
 
--- vendor/github.com/vmware/photon-controller-go-sdk/photon/networks.go
+++ vendor/github.com/vmware/photon-controller-go-sdk/photon/networks.go
@@ -15,28 +15,28 @@ import (
 )
 
 // Contains functionality for networks API.
-type SubnetsAPI struct {
+type NetworksAPI struct {
 	client *Client
 }
 
 // Options used for GetAll API
-type SubnetGetOptions struct {
+type NetworkGetOptions struct {
 	Name string `urlParam:"name"`
 }
 
-var subnetUrl string = "/subnets"
+var networkUrl string = rootUrl + "/subnets"
 
 // Creates a network.
-func (api *SubnetsAPI) Create(networkSpec *SubnetCreateSpec) (task *Task, err error) {
+func (api *NetworksAPI) Create(networkSpec *NetworkCreateSpec) (task *Task, err error) {
 	body, err := json.Marshal(networkSpec)
 	if err != nil {
 		return
 	}
 	res, err := api.client.restClient.Post(
-		api.client.Endpoint+subnetUrl,
+		api.client.Endpoint+networkUrl,
 		"application/json",
-		bytes.NewBuffer(body),
-		api.client.options.TokenOptions.AccessToken)
+		bytes.NewReader(body),
+		api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
@@ -46,8 +46,8 @@ func (api *SubnetsAPI) Create(networkSpec *SubnetCreateSpec) (task *Task, err er
 }
 
 // Deletes a network with specified ID.
-func (api *SubnetsAPI) Delete(id string) (task *Task, err error) {
-	res, err := api.client.restClient.Delete(api.client.Endpoint+subnetUrl+"/"+id, api.client.options.TokenOptions.AccessToken)
+func (api *NetworksAPI) Delete(id string) (task *Task, err error) {
+	res, err := api.client.restClient.Delete(api.client.Endpoint+networkUrl+"/"+id, api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
@@ -57,8 +57,8 @@ func (api *SubnetsAPI) Delete(id string) (task *Task, err error) {
 }
 
 // Gets a network with the specified ID.
-func (api *SubnetsAPI) Get(id string) (network *Subnet, err error) {
-	res, err := api.client.restClient.Get(api.client.Endpoint+subnetUrl+"/"+id, api.client.options.TokenOptions.AccessToken)
+func (api *NetworksAPI) Get(id string) (network *Network, err error) {
+	res, err := api.client.restClient.Get(api.client.Endpoint+networkUrl+"/"+id, api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
@@ -67,31 +67,31 @@ func (api *SubnetsAPI) Get(id string) (network *Subnet, err error) {
 	if err != nil {
 		return
 	}
-	var result Subnet
+	var result Network
 	err = json.NewDecoder(res.Body).Decode(&result)
 	return &result, nil
 }
 
 // Returns all networks
-func (api *SubnetsAPI) GetAll(options *SubnetGetOptions) (result *Subnets, err error) {
-	uri := api.client.Endpoint + subnetUrl
+func (api *NetworksAPI) GetAll(options *NetworkGetOptions) (result *Networks, err error) {
+	uri := api.client.Endpoint + networkUrl
 	if options != nil {
 		uri += getQueryString(options)
 	}
-	res, err := api.client.restClient.GetList(api.client.Endpoint, uri, api.client.options.TokenOptions.AccessToken)
+	res, err := api.client.restClient.GetList(api.client.Endpoint, uri, api.client.options.TokenOptions)
 
-	result = &Subnets{}
+	result = &Networks{}
 	err = json.Unmarshal(res, result)
 	return
 }
 
 // Sets default network.
-func (api *SubnetsAPI) SetDefault(id string) (task *Task, err error) {
+func (api *NetworksAPI) SetDefault(id string) (task *Task, err error) {
 	res, err := api.client.restClient.Post(
-		api.client.Endpoint+subnetUrl+"/"+id+"/set_default",
+		api.client.Endpoint+networkUrl+"/"+id+"/set_default",
 		"application/json",
-		bytes.NewBuffer([]byte("")),
-		api.client.options.TokenOptions.AccessToken)
+		bytes.NewReader([]byte("")),
+		api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
