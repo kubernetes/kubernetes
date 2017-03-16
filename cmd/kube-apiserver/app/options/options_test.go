0994b4a83672a1fc99632f84ef9e38a398ff55b7
zhengjiajin <zhengjiajin2016@gmail.io>
2017-03-16 23:05:50 +0800

    add test

--- cmd/kube-apiserver/app/options/options_test.go
+++ cmd/kube-apiserver/app/options/options_test.go
@@ -23,20 +23,22 @@ import (
 )
 
 func TestAddFlagsFlag(t *testing.T) {
-	// TODO: This only tests the enable-swagger-ui flag for now.
 	// Expand the test to include other flags as well.
 	f := pflag.NewFlagSet("addflagstest", pflag.ContinueOnError)
 	s := NewServerRunOptions()
 	s.AddFlags(f)
-	if s.Features.EnableSwaggerUI {
+
+	if s.Features.EnableSwaggerUI || s.Features.EnableContentionProfiling || !s.Features.EnableProfiling {
 		t.Errorf("Expected s.EnableSwaggerUI to be false by default")
 	}
 
 	args := []string{
 		"--enable-swagger-ui=true",
+		"--profiling=true",
+		"--contention-profiling=false",
 	}
 	f.Parse(args)
-	if !s.Features.EnableSwaggerUI {
+	if !s.Features.EnableSwaggerUI || !s.Features.EnableContentionProfiling || !s.Features.EnableProfiling {
 		t.Errorf("Expected s.EnableSwaggerUI to be true")
 	}
 }
