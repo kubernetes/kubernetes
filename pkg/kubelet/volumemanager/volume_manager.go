a451daca0daa557c525e721669232c0c92be3dc9
NickrenREN <nickren19@gmail.com>
2017-03-21 15:40:32 +0800

    cleanup: remove TODO(resolved) and var(unused)

--- pkg/kubelet/volumemanager/volume_manager.go
+++ pkg/kubelet/volumemanager/volume_manager.go
@@ -84,10 +84,6 @@ const (
 	// operation is waiting it only blocks other operations on the same device,
 	// other devices are not affected.
 	waitForAttachTimeout time.Duration = 10 * time.Minute
-
-	// reconcilerStartGracePeriod is the maximum amount of time volume manager
-	// can wait to start reconciler
-	reconcilerStartGracePeriod time.Duration = 60 * time.Second
 )
 
 // VolumeManager runs a set of asynchronous loops that figure out which volumes
@@ -123,8 +119,6 @@ type VolumeManager interface {
 	// attached to this node and remains "in use" until it is removed from both
 	// the desired state of the world and the actual state of the world, or it
 	// has been unmounted (as indicated in actual state of world).
-	// TODO(#27653): VolumesInUse should be handled gracefully on kubelet'
-	// restarts.
 	GetVolumesInUse() []v1.UniqueVolumeName
 
 	// ReconcilerStatesHasBeenSynced returns true only after the actual states in reconciler
