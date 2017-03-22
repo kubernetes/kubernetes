f4d9bbc7d89f6a9ced4b98902b6c6e7d7be635e9
caleb miles <caleb.miles@coreos.com>
2017-03-22 15:43:43 -0700

    Bump CNI consumers to latest version
    - vendored CNI plugins properly handle `DEL` on missing resources
    - [based on v0.5.1](https://github.com/kubernetes/kubernetes/issues/43488#issuecomment-288525151)

--- cluster/gce/container-linux/configure.sh
+++ cluster/gce/container-linux/configure.sh
@@ -111,8 +111,8 @@ function install-kube-binary-config {
   if [[ "${NETWORK_PROVIDER:-}" == "kubenet" ]] || \
      [[ "${NETWORK_PROVIDER:-}" == "cni" ]]; then
     #TODO(andyzheng0831): We should make the cni version number as a k8s env variable.
-    local -r cni_tar="cni-07a8a28637e97b22eb8dfe710eeae1344f69d16e.tar.gz"
-    local -r cni_sha1="19d49f7b2b99cd2493d5ae0ace896c64e289ccbb"
+    local -r cni_tar="cni-0799f5732f2a11b329d9e3d51b9c8f2e3759f2ff.tar.gz"
+    local -r cni_sha1="1d9788b0f5420e1a219aad2cb8681823fc515e7c"
     download-or-bust "${cni_sha1}" "https://storage.googleapis.com/kubernetes-release/network-plugins/${cni_tar}"
     local -r cni_dir="${KUBE_HOME}/cni"
     mkdir -p "${cni_dir}"
