6465fa5d6bd07b8b1d5d06bfe64d0524287b72b8
Maru Newby <marun@redhat.com>
2017-03-21 20:59:48 -0700

    fed: Add simple upgrade test

--- test/e2e_federation/framework/framework.go
+++ test/e2e_federation/framework/framework.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/kubernetes/federation/client/clientset_generated/federation_clientset"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/v1"
+	kubeclientset "k8s.io/kubernetes/pkg/client/clientset_generated/clientset"
 	"k8s.io/kubernetes/test/e2e/framework"
 
 	. "github.com/onsi/ginkgo"
@@ -234,4 +235,13 @@ func (f *Framework) GetUnderlyingFederatedContexts() []E2EContext {
 
 func (f *Framework) GetRegisteredClusters() ClusterSlice {
 	return getRegisteredClusters(f)
+}
+
+func (f *Framework) GetClusterClients() []kubeclientset.Interface {
+	clusters := getRegisteredClusters(f)
+	var clusterClients []kubeclientset.Interface
+	for _, c := range clusters {
+		clusterClients = append(clusterClients, c.Clientset)
+	}
+	return clusterClients
 }
