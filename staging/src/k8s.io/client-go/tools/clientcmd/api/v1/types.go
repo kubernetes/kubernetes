67d1d6aef40024fa53bbd4a48428fb8929c8e065
Cao Shufeng <caosf.fnst@cn.fujitsu.com>
2017-03-23 23:18:53 -0400

    add --as-group option to cli
    The usecase of this change:
    When a super user grant some RBAC permissions to a group, he can use
    --as-group to test whether the group get the permissions.
    Note that now we support as-groups, as-user-extra in kubeconfig file
    after this change.

--- staging/src/k8s.io/client-go/tools/clientcmd/api/v1/types.go
+++ staging/src/k8s.io/client-go/tools/clientcmd/api/v1/types.go
@@ -103,6 +103,12 @@ type AuthInfo struct {
 	// Impersonate is the username to imperonate.  The name matches the flag.
 	// +optional
 	Impersonate string `json:"as,omitempty"`
+	// ImpersonateGroups is the groups to imperonate.
+	// +optional
+	ImpersonateGroups []string `json:"as-groups,omitempty"`
+	// ImpersonateUserExtra contains additional information for impersonated user.
+	// +optional
+	ImpersonateUserExtra map[string][]string `json:"as-user-extra,omitempty"`
 	// Username is the username for basic authentication to the kubernetes cluster.
 	// +optional
 	Username string `json:"username,omitempty"`
