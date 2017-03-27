63f547e1b15ed94ef91c69a7e294b3506bd8c918
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-03-27 13:26:03 +0200

    pkg/admission: make plugin registry non-global

--- plugin/pkg/admission/securitycontext/scdeny/admission.go
+++ plugin/pkg/admission/securitycontext/scdeny/admission.go
@@ -23,10 +23,11 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apiserver/pkg/admission"
 	"k8s.io/kubernetes/pkg/api"
+	kubeapiserveradmission "k8s.io/kubernetes/pkg/kubeapiserver/admission"
 )
 
 func init() {
-	admission.RegisterPlugin("SecurityContextDeny", func(config io.Reader) (admission.Interface, error) {
+	kubeapiserveradmission.Plugins.Register("SecurityContextDeny", func(config io.Reader) (admission.Interface, error) {
 		return NewSecurityContextDeny(), nil
 	})
 }
