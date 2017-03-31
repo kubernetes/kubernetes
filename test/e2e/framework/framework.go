f73dce8fb52613d7e19f0888c26a4f5a967421e8
Marcin Owsiany <porridge@google.com>
2017-03-31 08:57:11 +0200

    Fix two typos.

--- test/e2e/framework/framework.go
+++ test/e2e/framework/framework.go
@@ -70,8 +70,8 @@ type Framework struct {
 
 	gatherer *containerResourceGatherer
 	// Constraints that passed to a check which is executed after data is gathered to
-	// see if 99% of results are within acceptable bounds. It as to be injected in the test,
-	// as expectations vary greatly. Constraints are groupped by the container names.
+	// see if 99% of results are within acceptable bounds. It has to be injected in the test,
+	// as expectations vary greatly. Constraints are grouped by the container names.
 	AddonResourceConstraints map[string]ResourceConstraint
 
 	logsSizeWaitGroup    sync.WaitGroup
