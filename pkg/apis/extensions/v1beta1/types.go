c1a86011e1188811d036371d90b63fdd0bc3f63f
Josh Horwitz <horwitzja@gmail.com>
2017-04-01 09:12:21 -0400

    Added host path whitelist to psp

--- pkg/apis/extensions/v1beta1/types.go
+++ pkg/apis/extensions/v1beta1/types.go
@@ -903,6 +903,10 @@ type PodSecurityPolicySpec struct {
 	// will not be forced to.
 	// +optional
 	ReadOnlyRootFilesystem bool `json:"readOnlyRootFilesystem,omitempty" protobuf:"varint,14,opt,name=readOnlyRootFilesystem"`
+	// AllowedHostPaths is a white list of allowed host path prefixes. Empty indicates that all
+	// host paths may be used.
+	// +optional
+	AllowedHostPaths []string `json:"allowedHostPaths,omitempty" protobuf:"bytes,15,opt,name=allowedHostPaths"`
 }
 
 // FS Type gives strong typing to different file systems that are used by volumes.
