c775357b056eaafbc201c9c502479fc48580576e
Mike Danese <mikedanese@google.com>
2017-04-05 09:26:39 -0700

    make kubeadm default to stable-1.6

--- cmd/kubeadm/app/apis/kubeadm/v1alpha1/defaults.go
+++ cmd/kubeadm/app/apis/kubeadm/v1alpha1/defaults.go
@@ -26,7 +26,7 @@ import (
 const (
 	DefaultServiceDNSDomain  = "cluster.local"
 	DefaultServicesSubnet    = "10.96.0.0/12"
-	DefaultKubernetesVersion = "stable"
+	DefaultKubernetesVersion = "stable-1.6"
 	DefaultAPIBindPort       = 6443
 	DefaultDiscoveryBindPort = 9898
 	DefaultAuthorizationMode = "RBAC"
