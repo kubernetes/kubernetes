f6500ded85565cb35f907d9a0d732888ef2c2a7a
nikhiljindal <nikhiljindal@google.com>
2017-04-05 02:20:00 -0700

    Using apimachinerymeta to manipulate finalizers to remove duplicate code

--- federation/pkg/federation-controller/util/slice/slice.go
+++ federation/pkg/federation-controller/util/slice/slice.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package deletionhelper
+package slice
 
 // ContainsString returns true if the given string slice contains the given string.
 // Returns false otherwise.
