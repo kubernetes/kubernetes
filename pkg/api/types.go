f683d891c76f63fee69c0617edc5ef1576879655
zhangxiaoyu-zidif <zhang.xiaoyu33@zte.com.cn>
2017-04-08 05:53:08 +0800

    ie should be i.e.

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -1249,7 +1249,7 @@ type EnvVar struct {
 	// using the previous defined environment variables in the container and
 	// any service environment variables.  If a variable cannot be resolved,
 	// the reference in the input string will be unchanged.  The $(VAR_NAME)
-	// syntax can be escaped with a double $$, ie: $$(VAR_NAME).  Escaped
+	// syntax can be escaped with a double $$, i.e.: $$(VAR_NAME).  Escaped
 	// references will never be expanded, regardless of whether the variable
 	// exists or not.
 	// +optional
@@ -1502,14 +1502,14 @@ type Container struct {
 	// Optional: The docker image's entrypoint is used if this is not provided; cannot be updated.
 	// Variable references $(VAR_NAME) are expanded using the container's environment.  If a variable
 	// cannot be resolved, the reference in the input string will be unchanged.  The $(VAR_NAME) syntax
-	// can be escaped with a double $$, ie: $$(VAR_NAME).  Escaped references will never be expanded,
+	// can be escaped with a double $$, i.e.: $$(VAR_NAME).  Escaped references will never be expanded,
 	// regardless of whether the variable exists or not.
 	// +optional
 	Command []string
 	// Optional: The docker image's cmd is used if this is not provided; cannot be updated.
 	// Variable references $(VAR_NAME) are expanded using the container's environment.  If a variable
 	// cannot be resolved, the reference in the input string will be unchanged.  The $(VAR_NAME) syntax
-	// can be escaped with a double $$, ie: $$(VAR_NAME).  Escaped references will never be expanded,
+	// can be escaped with a double $$, i.e.: $$(VAR_NAME).  Escaped references will never be expanded,
 	// regardless of whether the variable exists or not.
 	// +optional
 	Args []string
