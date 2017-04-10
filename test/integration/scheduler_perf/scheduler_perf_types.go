3e7b23982ff13a22d4f147374887c93910bf6be9
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2017-04-10 14:57:30 -0400

    Changes for removing the nodes and pods from input struct

---
+++ test/integration/scheduler_perf/scheduler_perf_types.go
@@ -0,0 +1,28 @@
+package benchmark
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+		"k8s.io/kubernetes/pkg/api/v1"
+)
+
+// High Level Configuration for all predicates and priorities.
+type schedulerPerfConfig struct {
+	NodeAffinity *nodeAffinity
+	InterPodAffinity *interPodAffinity
+}
+
+// interPodAffinity priority configuration details.
+type interPodAffinity struct {
+	Operator    metav1.LabelSelectorOperator	// LabelSelector operator like IN, EXISTS etc.
+	affinityKey string				// Selectors key
+	Labels      map[string]string			// Labels on pods.
+	TopologyKey string				// Topology Key
+}
+
+// nodeAffinity priority configuration details.
+type nodeAffinity struct {
+	numGroups       int				// number of Node-Pod sets with Pods NodeAffinity matching given Nodes.
+	nodeAffinityKey string				// Node Selection Key.
+	Operator        v1.NodeSelectorOperator		// Operator like IN, EXISTS etc.
+}
+
