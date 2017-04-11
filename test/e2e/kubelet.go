5f7bcd839103bdfc8cd22e250d7fea436ab88af5
Jeff Vance <jvance@redhat.com>
2017-04-11 14:45:54 -0700

    remove [Flaky]

--- test/e2e/kubelet.go
+++ test/e2e/kubelet.go
@@ -419,7 +419,7 @@ var _ = framework.KubeDescribe("kubelet", func() {
 	})
 
 	// Test host cleanup when disrupting the volume environment.
-	framework.KubeDescribe("host cleanup with volume mounts [Disruptive][Volume][HostCleanup]", func() {
+	framework.KubeDescribe("host cleanup with volume mounts [Volume][HostCleanup][Flaky]", func() {
 
 		type hostCleanupTest struct {
 			itDescr string
