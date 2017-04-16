72013031444c6c207fc68cd9aa5ee3ea47978523
Richard Wall <richard.wall@jetstack.io>
2017-04-16 09:58:31 +0100

    comment out the GCP auth import

--- staging/src/k8s.io/client-go/examples/informer/main.go
+++ staging/src/k8s.io/client-go/examples/informer/main.go
@@ -34,7 +34,7 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 
 	// Only required to authenticate against GKE clusters
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
 func main() {
