5255f247778629ff1d1ffc0dcbec0c51bbb415cb
Jonathan MacMillan <dvorak@google.com>
2017-04-24 17:05:24 -0700

    [Federation] Convert ConfigMaps to use the generic sync controller.

--- test/e2e_federation/crud.go
+++ test/e2e_federation/crud.go
@@ -33,7 +33,7 @@ var _ = framework.KubeDescribe("Federated types [Feature:Federation][Experimenta
 	f := fedframework.NewDefaultFederatedFramework("federated-types")
 
 	for name, fedType := range federatedtypes.FederatedTypes() {
-		Describe(fmt.Sprintf("Federated %q resources", name), func() {
+		Describe(fmt.Sprintf("dvorak Federated %q resources", name), func() {
 			It("should be created, read, updated and deleted successfully", func() {
 				fedframework.SkipUnlessFederated(f.ClientSet)
 
