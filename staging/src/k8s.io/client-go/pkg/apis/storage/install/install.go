cde2746821954c8eba5ef4d2ab55691619cae4df
Jan Safranek <jsafrane@redhat.com>
2017-04-25 10:13:38 +0200

    Regenerate everything

--- staging/src/k8s.io/client-go/pkg/apis/storage/install/install.go
+++ staging/src/k8s.io/client-go/pkg/apis/storage/install/install.go
@@ -37,9 +37,8 @@ func init() {
 func Install(groupFactoryRegistry announced.APIGroupFactoryRegistry, registry *registered.APIRegistrationManager, scheme *runtime.Scheme) {
 	if err := announced.NewGroupMetaFactory(
 		&announced.GroupMetaFactoryArgs{
-			GroupName: storage.GroupName,
-			// TODO:  change the order when GKE supports v1
-			VersionPreferenceOrder:     []string{v1beta1.SchemeGroupVersion.Version, v1.SchemeGroupVersion.Version},
+			GroupName:                  storage.GroupName,
+			VersionPreferenceOrder:     []string{v1.SchemeGroupVersion.Version, v1beta1.SchemeGroupVersion.Version},
 			ImportPrefix:               "k8s.io/client-go/pkg/apis/storage",
 			RootScopedKinds:            sets.NewString("StorageClass"),
 			AddInternalObjectsToScheme: storage.AddToScheme,
