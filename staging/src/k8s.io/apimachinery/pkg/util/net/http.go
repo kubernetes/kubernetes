d95cca790754e6e122f06ec3df725ab851b6be9d
Mike Danese <mikedanese@google.com>
2017-04-26 10:45:58 -0700

    replace CloneTLSConfig() with (*tls.Config).Clone()

--- staging/src/k8s.io/apimachinery/pkg/util/net/http.go
+++ staging/src/k8s.io/apimachinery/pkg/util/net/http.go
@@ -112,34 +112,6 @@ func DialerFor(transport http.RoundTripper) (DialFunc, error) {
 	}
 }
 
-// CloneTLSConfig returns a tls.Config with all exported fields except SessionTicketsDisabled and SessionTicketKey copied.
-// This makes it safe to call CloneTLSConfig on a config in active use by a server.
-// TODO: replace with tls.Config#Clone when we move to go1.8
-func CloneTLSConfig(cfg *tls.Config) *tls.Config {
-	if cfg == nil {
-		return &tls.Config{}
-	}
-	return &tls.Config{
-		Rand:                     cfg.Rand,
-		Time:                     cfg.Time,
-		Certificates:             cfg.Certificates,
-		NameToCertificate:        cfg.NameToCertificate,
-		GetCertificate:           cfg.GetCertificate,
-		RootCAs:                  cfg.RootCAs,
-		NextProtos:               cfg.NextProtos,
-		ServerName:               cfg.ServerName,
-		ClientAuth:               cfg.ClientAuth,
-		ClientCAs:                cfg.ClientCAs,
-		InsecureSkipVerify:       cfg.InsecureSkipVerify,
-		CipherSuites:             cfg.CipherSuites,
-		PreferServerCipherSuites: cfg.PreferServerCipherSuites,
-		ClientSessionCache:       cfg.ClientSessionCache,
-		MinVersion:               cfg.MinVersion,
-		MaxVersion:               cfg.MaxVersion,
-		CurvePreferences:         cfg.CurvePreferences,
-	}
-}
-
 type TLSClientConfigHolder interface {
 	TLSClientConfig() *tls.Config
 }
