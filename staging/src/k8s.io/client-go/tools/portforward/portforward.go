812086533dbd61b58532951596a8a88a59db0cca
Chao Xu <xuchao@google.com>
2017-04-26 17:13:48 -0700

    easy changes

--- staging/src/k8s.io/client-go/tools/portforward/portforward.go
+++ staging/src/k8s.io/client-go/tools/portforward/portforward.go
@@ -29,7 +29,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/util/httpstream"
 	"k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/client-go/pkg/api"
+	"k8s.io/client-go/pkg/api/v1"
 )
 
 // TODO move to API machinery and re-unify with kubelet/server/portfoward
@@ -263,9 +263,9 @@ func (pf *PortForwarder) handleConnection(conn net.Conn, port ForwardedPort) {
 
 	// create error stream
 	headers := http.Header{}
-	headers.Set(api.StreamType, api.StreamTypeError)
-	headers.Set(api.PortHeader, fmt.Sprintf("%d", port.Remote))
-	headers.Set(api.PortForwardRequestIDHeader, strconv.Itoa(requestID))
+	headers.Set(v1.StreamType, v1.StreamTypeError)
+	headers.Set(v1.PortHeader, fmt.Sprintf("%d", port.Remote))
+	headers.Set(v1.PortForwardRequestIDHeader, strconv.Itoa(requestID))
 	errorStream, err := pf.streamConn.CreateStream(headers)
 	if err != nil {
 		runtime.HandleError(fmt.Errorf("error creating error stream for port %d -> %d: %v", port.Local, port.Remote, err))
@@ -287,7 +287,7 @@ func (pf *PortForwarder) handleConnection(conn net.Conn, port ForwardedPort) {
 	}()
 
 	// create data stream
-	headers.Set(api.StreamType, api.StreamTypeData)
+	headers.Set(v1.StreamType, v1.StreamTypeData)
 	dataStream, err := pf.streamConn.CreateStream(headers)
 	if err != nil {
 		runtime.HandleError(fmt.Errorf("error creating forwarding stream for port %d -> %d: %v", port.Local, port.Remote, err))
