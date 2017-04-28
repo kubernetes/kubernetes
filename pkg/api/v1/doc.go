57e6d6df06daf7bf9b866a7a10464b325851627a
Chao Xu <xuchao@google.com>
2017-04-28 12:54:43 -0700

    generated defaulters

--- pkg/api/v1/doc.go
+++ pkg/api/v1/doc.go
@@ -17,6 +17,7 @@ limitations under the License.
 // +k8s:conversion-gen=k8s.io/kubernetes/pkg/api,external_types=../../../vendor/k8s.io/api/core/v1
 // +k8s:openapi-gen=true
 // +k8s:defaulter-gen=TypeMeta
+// +k8s:defaulter-gen-input=../../../vendor/k8s.io/api/core/v1
 
 // Package v1 is the v1 version of the API.
 package v1 // import "k8s.io/kubernetes/pkg/api/v1"
