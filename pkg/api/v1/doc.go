a54dcdae3e880787d8cb32f82494a375ba033112
Chao Xu <xuchao@google.com>
2017-04-28 10:26:09 -0700

    remove deepcopygen tag from pkg/api/v1/doc

--- pkg/api/v1/doc.go
+++ pkg/api/v1/doc.go
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:deepcopy-gen=package,register
 // +k8s:conversion-gen=k8s.io/kubernetes/pkg/api
 // +k8s:openapi-gen=true
 // +k8s:defaulter-gen=TypeMeta
