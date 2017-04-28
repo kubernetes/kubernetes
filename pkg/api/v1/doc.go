d4c97a5c6b363259d67d5569756e0b8ae38dfbca
Chao Xu <xuchao@google.com>
2017-04-28 10:48:49 -0700

    add conversion tag to pkg/api/v1/doc.go, external_types=../../../vendor/k8s.io/api/core/v1

--- pkg/api/v1/doc.go
+++ pkg/api/v1/doc.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:conversion-gen=k8s.io/kubernetes/pkg/api
+// +k8s:conversion-gen=k8s.io/kubernetes/pkg/api,external_types=../../../vendor/k8s.io/api/core/v1
 // +k8s:openapi-gen=true
 // +k8s:defaulter-gen=TypeMeta
 
