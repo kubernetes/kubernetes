ce323df55fa77768fa3d528dca38b0afa3e6b3af
Chao Xu <xuchao@google.com>
2017-04-28 14:58:20 -0700

    will be gone after rebase

--- pkg/apis/autoscaling/v2alpha1/register.go
+++ pkg/apis/autoscaling/v2alpha1/register.go
@@ -28,6 +28,11 @@ const GroupName = "autoscaling"
 // SchemeGroupVersion is group version used to register these objects
 var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v2alpha1"}
 
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
 var (
 	// TODO: move SchemeBuilder with zz_generated.deepcopy.go to k8s.io/api.
 	// localSchemeBuilder and AddToScheme will stay in k8s.io/kubernetes.
