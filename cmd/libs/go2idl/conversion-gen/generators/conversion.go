ff1eb0fafa0dde0ffb975e44ccb561d20ff7638e
Chao Xu <xuchao@google.com>
2017-04-28 11:00:52 -0700

    let conversion-gen to choose localSchemeBuiler

--- cmd/libs/go2idl/conversion-gen/generators/conversion.go
+++ cmd/libs/go2idl/conversion-gen/generators/conversion.go
@@ -594,7 +594,7 @@ func (g *genConversion) Init(c *generator.Context, w io.Writer) error {
 	}
 	sw := generator.NewSnippetWriter(w, c, "$", "$")
 	sw.Do("func init() {\n", nil)
-	sw.Do("SchemeBuilder.Register(RegisterConversions)\n", nil)
+	sw.Do("localSchemeBuilder.Register(RegisterConversions)\n", nil)
 	sw.Do("}\n", nil)
 
 	scheme := c.Universe.Type(types.Name{Package: runtimePackagePath, Name: "Scheme"})
