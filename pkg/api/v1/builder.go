b8f3ad523d15ec365436280f287ead5645d24423
Chao Xu <xuchao@google.com>
2017-04-27 23:04:57 -0700

    duplicate some global variables/functions in pkg/api/v1/builder.go, add todo to remove these

--- pkg/api/v1/builder.go
+++ pkg/api/v1/builder.go
@@ -18,6 +18,7 @@ package v1
 
 import (
 	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 var (
@@ -30,4 +31,16 @@ func init() {
 	// generated functions takes place in the generated files. The separation
 	// makes the code compile even when the generated files are missing.
 	localSchemeBuilder.Register(v1.AddKnownTypes, addDefaultingFuncs, addConversionFuncs, addFastPathConversionFuncs)
+}
+
+// TODO: remove these global varialbes
+// GroupName is the group name use in this package
+const GroupName = ""
+
+// SchemeGroupVersion is group version used to register these objects
+var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1"}
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
