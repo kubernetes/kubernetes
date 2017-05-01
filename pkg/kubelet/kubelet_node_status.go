7b5716f6dd968332599b86d4a39b1c5183f133e0
Davanum Srinivas <davanum@gmail.com>
2017-04-30 21:45:24 -0400

    Set Node ExternalIP for local-up-cluster scenarios
    When a cloud provider is not specified which is common in
    local-up-cluster scenarios, there should be a way to set the
    ExternalIP for a node as we should still be able to run tests
    like the following that checks if external ip's are present
    on the node:
    Services [It] should be able to up and down services
    
    So if "--node-ip" is set or if HostnameOverride has an ip address,
    then we should add another entry for external ip address.

--- pkg/kubelet/kubelet_node_status.go
+++ pkg/kubelet/kubelet_node_status.go
@@ -470,6 +470,7 @@ func (kl *Kubelet) setNodeAddress(node *v1.Node) error {
 		node.Status.Addresses = nodeAddresses
 	} else {
 		var ipAddr net.IP
+		var externalIpAddr net.IP
 		var err error
 
 		// 1) Use nodeIP if set
@@ -478,9 +479,11 @@ func (kl *Kubelet) setNodeAddress(node *v1.Node) error {
 		// 4) Try to get the IP from the network interface used as default gateway
 		if kl.nodeIP != nil {
 			ipAddr = kl.nodeIP
+			externalIpAddr = kl.nodeIP
 			node.ObjectMeta.Annotations[kubeletapis.AnnotationProvidedIPAddr] = kl.nodeIP.String()
 		} else if addr := net.ParseIP(kl.hostname); addr != nil {
 			ipAddr = addr
+			externalIpAddr = addr
 		} else {
 			var addrs []net.IP
 			addrs, err = net.LookupIP(node.Name)
@@ -504,6 +507,11 @@ func (kl *Kubelet) setNodeAddress(node *v1.Node) error {
 			{Type: v1.NodeInternalIP, Address: ipAddr.String()},
 			{Type: v1.NodeHostName, Address: kl.GetHostname()},
 		}
+		if externalIpAddr != nil {
+			node.Status.Addresses = append(node.Status.Addresses,
+				v1.NodeAddress{Type: v1.NodeExternalIP, Address: externalIpAddr.String()})
+		}
+
 	}
 	return nil
 }
