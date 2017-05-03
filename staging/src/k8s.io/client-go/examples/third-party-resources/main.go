2ef8fe47fbecfea506a28f4696296790d584b292
Mike Spreitzer <mspreitz@us.ibm.com>
2017-05-02 23:14:22 -0400

    Added informer to third-party-resources example of client-go
    Demonstrates how to list and watch third party objects (i.e., objects
    of a Kind defined by a ThirdPartyResource).
    
    This makes a tiny contribution towards client-go issue #128

--- staging/src/k8s.io/client-go/examples/third-party-resources/main.go
+++ staging/src/k8s.io/client-go/examples/third-party-resources/main.go
@@ -20,17 +20,26 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/golang/glog"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/pkg/api"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -51,6 +60,8 @@ func main() {
 		panic(err)
 	}
 
+	kind_created_here := false
+
 	// initialize third party resource if it does not exist
 	tpr, err := clientset.ExtensionsV1beta1().ThirdPartyResources().Get("example.k8s.io", metav1.GetOptions{})
 	if err != nil {
@@ -69,6 +80,7 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
+			kind_created_here = true
 			fmt.Printf("CREATED: %#v\nFROM: %#v\n", result, tpr)
 		} else {
 			panic(err)
@@ -109,13 +121,19 @@ func main() {
 			}
 
 			var result Example
-			err = tprclient.Post().
+			req := tprclient.Post().
 				Resource("examples").
 				Namespace(v1.NamespaceDefault).
-				Body(example).
+				Body(example)
+			err = req.
 				Do().Into(&result)
 
 			if err != nil {
+				fmt.Println()
+				if kind_created_here {
+					glog.Infoln("Probably because of delay issue noted in https://github.com/kubernetes/features/issues/95 ...")
+				}
+				glog.Fatalf("Unable to create example1 --- request=%#v, result=%#v, err=%#v", req, result, err)
 				panic(err)
 			}
 			fmt.Printf("CREATED: %#v\n", result)
@@ -133,6 +151,9 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("LIST: %#v\n", exampleList)
+
+	fmt.Printf("Starting watch!\n")
+	watch(tprclient)
 }
 
 func buildConfig(kubeconfig string) (*rest.Config, error) {
@@ -164,4 +185,88 @@ func configureClient(config *rest.Config) {
 		})
 	metav1.AddToGroupVersion(scheme.Scheme, groupversion)
 	schemeBuilder.AddToScheme(scheme.Scheme)
+}
+
+func watch(client *rest.RESTClient) {
+
+	stop := make(chan struct{}, 1)
+	source := cache.NewListWatchFromClient(
+		client,
+		"examples",
+		api.NamespaceAll,
+		fields.Everything())
+
+	store, controller := cache.NewInformer(
+		source,
+
+		// The object type.
+		&Example{},
+
+		// resyncPeriod
+		// Every resyncPeriod, all resources in the cache will retrigger events.
+		// Set to 0 to disable the resync.
+		time.Second*60,
+
+		// Your custom resource event handlers.
+		cache.ResourceEventHandlerFuncs{
+			// Takes a single argument of type interface{}.
+			// Called on controller startup and when new resources are created.
+			AddFunc: create,
+
+			// Takes two arguments of type interface{}.
+			// Called on resource update and every resyncPeriod on existing resources.
+			UpdateFunc: update,
+
+			// Takes a single argument of type interface{}.
+			// Called on resource deletion.
+			DeleteFunc: delete,
+		})
+
+	// store can be used to List and Get
+	// NEVER modify objects from the store. It's a read-only, local cache.
+	fmt.Println("listing examples from store:")
+	for _, obj := range store.List() {
+		example := obj.(*Example)
+
+		// This will likely be empty the first run, but may not
+		fmt.Printf("%#v\n", example)
+	}
+
+	// the controller run starts the event processing loop
+	go controller.Run(stop)
+
+	// and now we block on a signal
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	s := <-signals
+	fmt.Printf("received signal %#v, exiting...\n", s)
+	close(stop)
+	os.Exit(0)
+}
+
+// Handler functions as per the controller above.
+// Note the coercion of the interface{} into a pointer of the expected type.
+
+func create(obj interface{}) {
+	example := obj.(*Example)
+
+	fmt.Println("CREATED:", printExample(example))
+}
+
+func update(old, new interface{}) {
+	oldExample := old.(*Example)
+	newExample := new.(*Example)
+
+	fmt.Printf("UPDATED:\n  old: %s\n  new: %s\n", printExample(oldExample), printExample(newExample))
+}
+
+func delete(obj interface{}) {
+	example := obj.(*Example)
+
+	fmt.Println("DELETED:", printExample(example))
+}
+
+// convenience functions
+func printExample(example *Example) string {
+	return fmt.Sprintf("%s/%s, APIVersion: %s, Kind: %s, Value: %#v", example.Metadata.Namespace, example.Metadata.Name, example.APIVersion, example.Kind, example)
 }
