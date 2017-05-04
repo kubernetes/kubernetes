018e17568ec16953f878b8c5adaf0dbf89b09932
Eric Chiang <eric.chiang.m@gmail.com>
2017-05-03 17:13:21 -0700

    api types: fix protobuf names that are different from JSON name

--- pkg/apis/authorization/v1beta1/types.go
+++ pkg/apis/authorization/v1beta1/types.go
@@ -131,7 +131,7 @@ type SubjectAccessReviewSpec struct {
 	// User is the user you're testing for.
 	// If you specify "User" but not "Group", then is it interpreted as "What if User were not a member of any groups
 	// +optional
-	User string `json:"user,omitempty" protobuf:"bytes,3,opt,name=verb"`
+	User string `json:"user,omitempty" protobuf:"bytes,3,opt,name=user"`
 	// Groups is the groups you're testing for.
 	// +optional
 	Groups []string `json:"group,omitempty" protobuf:"bytes,4,rep,name=group"`
