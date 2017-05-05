5adcf1bd40f48060b1d01ade340b372f2b38dc5a
Chao Xu <xuchao@google.com>
2017-05-04 23:15:35 -0700

    remove doulbe registration of addknowntypes, need to fix for other apis as well

--- pkg/api/v1/builder.go
+++ pkg/api/v1/builder.go
@@ -30,7 +30,7 @@ func init() {
 	// We only register manually written functions here. The registration of the
 	// generated functions takes place in the generated files. The separation
 	// makes the code compile even when the generated files are missing.
-	localSchemeBuilder.Register(v1.AddKnownTypes, addDefaultingFuncs, addConversionFuncs, addFastPathConversionFuncs)
+	localSchemeBuilder.Register(addDefaultingFuncs, addConversionFuncs, addFastPathConversionFuncs)
 }
 
 // TODO: remove these global varialbes
