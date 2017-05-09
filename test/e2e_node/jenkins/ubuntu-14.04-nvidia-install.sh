ead52147713469489a6812ba4c06a4b659730bf2
Vishnu kannan <vishnuk@google.com>
2017-05-09 13:15:43 -0700

    update godeps
    Signed-off-by: Vishnu kannan <vishnuk@google.com>

--- test/e2e_node/jenkins/ubuntu-14.04-nvidia-install.sh
+++ test/e2e_node/jenkins/ubuntu-14.04-nvidia-install.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# Copyright 2017 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script is meant to install Nvidia drivers on Ubuntu 14.04 GCE VMs.
+# This script is meant to facilitate testing of Nvidia GPU support in Kubernetes.
 echo "Checking for CUDA and installing."
 # Check for CUDA and try to install.
 if ! dpkg-query -W cuda; then
