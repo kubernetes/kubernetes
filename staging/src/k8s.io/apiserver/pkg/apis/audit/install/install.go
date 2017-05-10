342ed595a9c136b8dd561db00edf45140b09b9ef
Tim St. Clair <stclair@google.com>
2017-05-10 14:11:19 -0700

    Fix install import path

--- staging/src/k8s.io/apiserver/pkg/apis/audit/install/install.go
+++ staging/src/k8s.io/apiserver/pkg/apis/audit/install/install.go
@@ -23,9 +23,9 @@ import (
 	"k8s.io/apimachinery/pkg/apimachinery/registered"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apiserver/pkg/apis/audit"
+	"k8s.io/apiserver/pkg/apis/audit/v1alpha1"
 	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/apis/audit"
-	"k8s.io/kubernetes/pkg/apis/audit/v1alpha1"
 )
 
 func init() {
