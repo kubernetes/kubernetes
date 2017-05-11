4aeadce030fedddf056bbbb5aa34f0ea808f6a66
zhengjiajin <393926893@qq.com>
2017-05-11 15:55:45 +0800

    Fix test not use test flags

--- pkg/kubectl/cmd/create_quota_test.go
+++ pkg/kubectl/cmd/create_quota_test.go
@@ -47,30 +47,30 @@ func TestCreateQuota(t *testing.T) {
 	tf.Namespace = "test"
 
 	tests := map[string]struct {
-		flags          map[string]string
+		flags          []string
 		expectedOutput string
 	}{
 		"single resource": {
-			flags:          map[string]string{"hard": "cpu=1", "output": "name"},
+			flags:          []string{"--hard=cpu=1"},
 			expectedOutput: "resourcequota/" + resourceQuotaObject.Name + "\n",
 		},
 		"single resource with a scope": {
-			flags:          map[string]string{"hard": "cpu=1", "output": "name", "scopes": "BestEffort"},
+			flags:          []string{"--hard=cpu=1", "--scopes=BestEffort"},
 			expectedOutput: "resourcequota/" + resourceQuotaObject.Name + "\n",
 		},
 		"multiple resources": {
-			flags:          map[string]string{"hard": "cpu=1,pods=42", "output": "name", "scopes": "BestEffort"},
+			flags:          []string{"--hard=cpu=1,pods=42", "--scopes=BestEffort"},
 			expectedOutput: "resourcequota/" + resourceQuotaObject.Name + "\n",
 		},
 		"single resource with multiple scopes": {
-			flags:          map[string]string{"hard": "cpu=1", "output": "name", "scopes": "BestEffort,NotTerminating"},
+			flags:          []string{"--hard=cpu=1", "--scopes=BestEffort,NotTerminating"},
 			expectedOutput: "resourcequota/" + resourceQuotaObject.Name + "\n",
 		},
 	}
 	for name, test := range tests {
 		buf := bytes.NewBuffer([]byte{})
 		cmd := NewCmdCreateQuota(f, buf)
-		cmd.Flags().Set("hard", "cpu=1")
+		cmd.Flags().Parse(test.flags)
 		cmd.Flags().Set("output", "name")
 		cmd.Run(cmd, []string{resourceQuotaObject.Name})
 
