eeb0a571f78ac9638445a600ec38e863734b47c1
Wojciech Tyczynski <wojtekt@google.com>
2017-05-11 20:05:55 +0200

    Bump l7-lb-controller resource usage threshold in tests

--- test/e2e/perf/density.go
+++ test/e2e/perf/density.go
@@ -146,7 +146,7 @@ func density30AddonResourceVerifier(numNodes int) map[string]framework.ResourceC
 	}
 	constraints["l7-lb-controller"] = framework.ResourceConstraint{
 		CPUConstraint:    0.15,
-		MemoryConstraint: 60 * (1024 * 1024),
+		MemoryConstraint: 75 * (1024 * 1024),
 	}
 	constraints["influxdb"] = framework.ResourceConstraint{
 		CPUConstraint:    2,
