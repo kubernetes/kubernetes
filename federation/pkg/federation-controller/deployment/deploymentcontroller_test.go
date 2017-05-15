22f2b9810ad4827af38246688dd0c4d8b2bdb379
Henrique Truta <henrique@lsd.ufcg.edu.br>
2017-05-15 17:27:13 -0300

    Annotations test to FederatedDeployment
    Adds some rebalance, weighting and maxReplicat test to
    FederatedDeployment

--- federation/pkg/federation-controller/deployment/deploymentcontroller_test.go
+++ federation/pkg/federation-controller/deployment/deploymentcontroller_test.go
@@ -96,21 +96,6 @@ func TestDeploymentController(t *testing.T) {
 	// Create deployment. Expect to see it in cluster1.
 	dep1 := newDeploymentWithReplicas("depA", 6)
 	deploymentsWatch.Add(dep1)
-	checkDeployment := func(base *extensionsv1.Deployment, replicas int32) CheckingFunction {
-		return func(obj runtime.Object) error {
-			if obj == nil {
-				return fmt.Errorf("Observed object is nil")
-			}
-			d := obj.(*extensionsv1.Deployment)
-			if err := CompareObjectMeta(base.ObjectMeta, d.ObjectMeta); err != nil {
-				return err
-			}
-			if replicas != *d.Spec.Replicas {
-				return fmt.Errorf("Replica count is different expected:%d observed:%d", replicas, *d.Spec.Replicas)
-			}
-			return nil
-		}
-	}
 	assert.NoError(t, CheckObjectFromChan(cluster1CreateChan, checkDeployment(dep1, *dep1.Spec.Replicas)))
 	err := WaitForStoreUpdate(
 		deploymentController.fedDeploymentInformer.GetTargetStore(),
@@ -141,6 +126,110 @@ func TestDeploymentController(t *testing.T) {
 	assert.NoError(t, CheckObjectFromChan(cluster1CreateChan, checkDeployment(dep2, 6)))
 	assert.NoError(t, CheckObjectFromChan(cluster2CreateChan, checkDeployment(dep2, 3)))
 }
+func TestDeploymentControllerAnnotations(t *testing.T) {
+	flag.Set("logtostderr", "true")
+	flag.Set("v", "5")
+	flag.Parse()
+
+	deploymentReviewDelay = 500 * time.Millisecond
+	clusterAvailableDelay = 100 * time.Millisecond
+	clusterUnavailableDelay = 100 * time.Millisecond
+	allDeploymentReviewDelay = 500 * time.Millisecond
+
+	cluster1 := NewCluster("cluster1", apiv1.ConditionTrue)
+	cluster2 := NewCluster("cluster2", apiv1.ConditionTrue)
+
+	fakeClient := &fakefedclientset.Clientset{}
+	// Add an update reactor on fake client to return the desired updated object.
+	// This is a hack to workaround https://github.com/kubernetes/kubernetes/issues/40939.
+	AddFakeUpdateReactor(deployments, &fakeClient.Fake)
+	RegisterFakeList("clusters", &fakeClient.Fake, &fedv1.ClusterList{Items: []fedv1.Cluster{*cluster1}})
+	deploymentsWatch := RegisterFakeWatch(deployments, &fakeClient.Fake)
+	clusterWatch := RegisterFakeWatch("clusters", &fakeClient.Fake)
+
+	cluster1Client := &fakekubeclientset.Clientset{}
+	cluster1Watch := RegisterFakeWatch(deployments, &cluster1Client.Fake)
+	_ = RegisterFakeWatch(pods, &cluster1Client.Fake)
+	RegisterFakeList(deployments, &cluster1Client.Fake, &extensionsv1.DeploymentList{Items: []extensionsv1.Deployment{}})
+	cluster1CreateChan := RegisterFakeCopyOnCreate(deployments, &cluster1Client.Fake, cluster1Watch)
+	cluster1UpdateChan := RegisterFakeCopyOnUpdate(deployments, &cluster1Client.Fake, cluster1Watch)
+
+	cluster2Client := &fakekubeclientset.Clientset{}
+	cluster2Watch := RegisterFakeWatch(deployments, &cluster2Client.Fake)
+	_ = RegisterFakeWatch(pods, &cluster2Client.Fake)
+	RegisterFakeList(deployments, &cluster2Client.Fake, &extensionsv1.DeploymentList{Items: []extensionsv1.Deployment{}})
+	cluster2CreateChan := RegisterFakeCopyOnCreate(deployments, &cluster2Client.Fake, cluster2Watch)
+
+	deploymentController := NewDeploymentController(fakeClient)
+	clientFactory := func(cluster *fedv1.Cluster) (kubeclientset.Interface, error) {
+		switch cluster.Name {
+		case cluster1.Name:
+			return cluster1Client, nil
+		case cluster2.Name:
+			return cluster2Client, nil
+		default:
+			return nil, fmt.Errorf("Unknown cluster")
+		}
+	}
+	ToFederatedInformerForTestOnly(deploymentController.fedDeploymentInformer).SetClientFactory(clientFactory)
+	ToFederatedInformerForTestOnly(deploymentController.fedPodInformer).SetClientFactory(clientFactory)
+
+	stop := make(chan struct{})
+	go deploymentController.Run(5, stop)
+
+	// Create deployment with default conf. Expect to see it in cluster1.
+	dep1 := newDeploymentWithReplicas("deployment1", 6)
+	deploymentsWatch.Add(dep1)
+	assert.NoError(t, CheckObjectFromChan(cluster1CreateChan, checkDeployment(dep1, *dep1.Spec.Replicas)))
+
+	// Add new deployment with rebalance set to True
+	dep2 := newDeploymentWithReplicas("deployment2", 6)
+	dep2.Annotations = make(map[string]string)
+	dep2.Annotations[FedDeploymentPreferencesAnnotation] = `{"rebalance": true,
+		  "clusters": {
+		    "*": {"weight": 1}
+		}}`
+	deploymentsWatch.Add(dep2)
+	assert.NoError(t, CheckObjectFromChan(cluster1CreateChan, checkDeployment(dep2, *dep2.Spec.Replicas)))
+
+	// Adding another deployment using weighing with default max and min
+	dep3 := newDeploymentWithReplicas("deployment3", 6)
+	dep3.Annotations = make(map[string]string)
+	dep3.Annotations[FedDeploymentPreferencesAnnotation] = `{"rebalance": true,
+		  "clusters": {
+		    "cluster1": {"weight": 2},
+		    "cluster2": {"weight": 1}
+		}}`
+	deploymentsWatch.Add(dep3)
+	assert.NoError(t, CheckObjectFromChan(cluster1CreateChan, checkDeployment(dep3, *dep3.Spec.Replicas)))
+
+	dep4 := newDeploymentWithReplicas("deployment4", 12)
+	dep4.Annotations = make(map[string]string)
+	dep4.Annotations[FedDeploymentPreferencesAnnotation] = `{"rebalance": true,
+		  "clusters": {
+		    "cluster1": {"weight": 2, "maxReplicas": 10},
+		    "cluster2": {"weight": 1}
+		}}`
+	deploymentsWatch.Add(dep4)
+	assert.NoError(t, CheckObjectFromChan(cluster1CreateChan, checkDeployment(dep4, 10)))
+
+	// Adding the second cluster to the federation
+	clusterWatch.Add(cluster2)
+
+	// Deployment1 shouldn't have been updated as rebalance=False but Deployment2 should be updated
+	// and replicas split in half
+	assert.NoError(t, CheckObjectFromChan(cluster1UpdateChan, checkDeployment(dep2, *dep2.Spec.Replicas/2)))
+	assert.NoError(t, CheckObjectFromChan(cluster2CreateChan, checkDeployment(dep2, *dep2.Spec.Replicas/2)))
+
+	// Deployment3 should also be updated, but with a different proportion due to the weighing
+	assert.NoError(t, CheckObjectFromChan(cluster1UpdateChan, checkDeployment(dep3, 4)))
+	assert.NoError(t, CheckObjectFromChan(cluster2CreateChan, checkDeployment(dep3, 2)))
+
+	// FIXME: Those two lines fail. Probably an issue with rebalance + maxreplicas
+	// Deployment3 should be updated, considering the maxReplicas
+	// assert.NoError(t, CheckObjectFromChan(cluster1UpdateChan, checkDeployment(dep4, 8)))
+	// assert.NoError(t, CheckObjectFromChan(cluster2CreateChan, checkDeployment(dep4, 4)))
+}
 
 func newDeploymentWithReplicas(name string, replicas int32) *extensionsv1.Deployment {
 	return &extensionsv1.Deployment{
@@ -152,5 +241,21 @@ func newDeploymentWithReplicas(name string, replicas int32) *extensionsv1.Deploy
 		Spec: extensionsv1.DeploymentSpec{
 			Replicas: &replicas,
 		},
+	}
+}
+
+func checkDeployment(base *extensionsv1.Deployment, replicas int32) CheckingFunction {
+	return func(obj runtime.Object) error {
+		if obj == nil {
+			return fmt.Errorf("Observed object is nil")
+		}
+		d := obj.(*extensionsv1.Deployment)
+		if err := CompareObjectMeta(base.ObjectMeta, d.ObjectMeta); err != nil {
+			return err
+		}
+		if replicas != *d.Spec.Replicas {
+			return fmt.Errorf("Replica count is different expected:%d observed:%d", replicas, *d.Spec.Replicas)
+		}
+		return nil
 	}
 }
