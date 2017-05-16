a82676a126587e9d5d404bf3c29e5c0db0bcdab2
Zihong Zheng <zihongz@google.com>
2017-05-16 14:25:30 -0700

    Keep annotation_key_constants consistent

--- pkg/api/annotation_key_constants.go
+++ pkg/api/annotation_key_constants.go
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// This file should be consistent with pkg/api/v1/annotation_key_constants.go.
+
 package api
 
 const (
