8f0585a6b8c15475ba46b3d101ca9f7fedec0a84
Random-Liu <taotaotheripper@gmail.com>
2017-05-19 11:18:10 -0700

    Fix kuberuntime GetPods.

--- pkg/kubelet/kuberuntime/helpers.go
+++ pkg/kubelet/kuberuntime/helpers.go
@@ -104,14 +104,14 @@ func (m *kubeGenericRuntimeManager) toKubeContainer(c *runtimeapi.Container) (*k
 		return nil, fmt.Errorf("unable to convert a nil pointer to a runtime container")
 	}
 
-	labeledInfo := getContainerInfoFromLabels(c.Labels)
 	annotatedInfo := getContainerInfoFromAnnotations(c.Annotations)
 	return &kubecontainer.Container{
-		ID:    kubecontainer.ContainerID{Type: m.runtimeName, ID: c.Id},
-		Name:  labeledInfo.ContainerName,
-		Image: c.Image.Image,
-		Hash:  annotatedInfo.Hash,
-		State: toKubeContainerState(c.State),
+		ID:      kubecontainer.ContainerID{Type: m.runtimeName, ID: c.Id},
+		Name:    c.GetMetadata().Name,
+		ImageID: c.ImageRef,
+		Image:   c.Image.Image,
+		Hash:    annotatedInfo.Hash,
+		State:   toKubeContainerState(c.State),
 	}, nil
 }
 
