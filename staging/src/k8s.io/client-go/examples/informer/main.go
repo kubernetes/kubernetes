28a17cb7a4ac10431a51808863c03b950a34025c
Richard Wall <richard.wall@jetstack.io>
2017-05-19 17:19:46 +0100

    Hopefully this version is clearer.

--- staging/src/k8s.io/client-go/examples/informer/main.go
+++ staging/src/k8s.io/client-go/examples/informer/main.go
@@ -19,12 +19,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"time"
 
 	"github.com/golang/glog"
 
-	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/cache"
@@ -55,82 +54,44 @@ func main() {
 		glog.Fatal(err)
 	}
 
-	source := cache.NewListWatchFromClient(
-		clientset.Core().RESTClient(),
-		"pods",
-		v1.NamespaceAll,
-		fields.Everything())
+	factory := informers.NewSharedInformerFactory(clientset, time.Second*0)
 
-	store, controller := cache.NewInformer(
-		source,
-
-		// The object type.
-		&v1.Pod{},
-
-		// resyncPeriod
-		// Every resyncPeriod, all resources in the cache will retrigger events.
-		// Set to 0 to disable the resync.
-		time.Second*0,
-
-		// Your custom resource event handlers.
-		cache.ResourceEventHandlerFuncs{
-			// Takes a single argument of type interface{}.
-			// Called on controller startup and when new resources are created.
-			AddFunc: create,
-
-			// Takes two arguments of type interface{}.
-			// Called on resource update and every resyncPeriod on existing resources.
-			UpdateFunc: update,
-
-			// Takes a single argument of type interface{}.
-			// Called on resource deletion.
-			DeleteFunc: delete,
-		})
+	informer := factory.Core().V1().Pods().Informer()
 
 	stop := make(chan struct{})
 	defer close(stop)
 
 	// the controller run starts the event processing loop
-	go controller.Run(stop)
+	go informer.Run(stop)
 
 	// wait until a store finished its initial synchronization
-	cache.WaitForCacheSync(stop, controller.HasSynced)
+	cache.WaitForCacheSync(stop, informer.HasSynced)
 
-	// store can be used to List and Get
-	// NEVER modify objects from the store. It's a read-only, local cache.
-	glog.Info("listing pods from store:")
-	for _, obj := range store.List() {
-		pod := obj.(*v1.Pod)
-		glog.Info(pod.ObjectMeta.Name)
-	}
+	informer.AddEventHandler(
+		// Your custom resource event handlers.
+		cache.ResourceEventHandlerFuncs{
+			// Called on creation
+			AddFunc: func(obj interface{}) {
+				pod := obj.(*v1.Pod)
+				glog.Infof("POD CREATED: %s/%s", pod.Namespace, pod.Name)
+			},
+			// Called on resource update and every resyncPeriod on existing resources.
+			// The Generation will not change unless the Pod has changed.
+			UpdateFunc: func(old, new interface{}) {
+				oldPod := old.(*v1.Pod)
+				newPod := new.(*v1.Pod)
+				glog.Infof(
+					"POD UPDATED. %s/%s %s",
+					oldPod.Namespace, oldPod.Name, newPod.Status.Phase,
+				)
+			},
+			// Called on resource deletion.
+			DeleteFunc: func(obj interface{}) {
+				pod := obj.(*v1.Pod)
+				glog.Infof("POD DELETED: %s/%s", pod.Namespace, pod.Name)
+			},
+		},
+	)
 
 	select {}
-}
-
-// Handler functions as per the controller above.
-// Note the coercion of the interface{} into a pointer of the expected type.
-func create(obj interface{}) {
-	pod := obj.(*v1.Pod)
-
-	glog.Info("POD CREATED:", podWithNamespace(pod))
-}
-
-func update(old, new interface{}) {
-	oldPod := old.(*v1.Pod)
-	newPod := new.(*v1.Pod)
-
-	glog.Info("POD UPDATED:")
-	glog.Info("old:", podWithNamespace(oldPod))
-	glog.Info("new:", podWithNamespace(newPod))
-}
-
-func delete(obj interface{}) {
-	pod := obj.(*v1.Pod)
-
-	glog.Info("POD DELETED:", podWithNamespace(pod))
-}
-
-// convenience functions
-func podWithNamespace(pod *v1.Pod) string {
-	return fmt.Sprintf("%s/%s", pod.Namespace, pod.Name)
 }
