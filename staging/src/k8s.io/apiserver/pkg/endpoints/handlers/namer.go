25f0fe1adb199697565487b9dfacc4ed8ecdccbb
Wojciech Tyczynski <wojtekt@google.com>
2017-05-22 16:50:24 +0200

    Pass Context to GenerateLink

--- staging/src/k8s.io/apiserver/pkg/endpoints/handlers/namer.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/handlers/namer.go
@@ -45,7 +45,7 @@ type ScopeNamer interface {
 	SetSelfLink(obj runtime.Object, url string) error
 	// GenerateLink creates an encoded URI for a given runtime object that represents the canonical path
 	// and query.
-	GenerateLink(req *http.Request, obj runtime.Object) (uri string, err error)
+	GenerateLink(ctx request.Context, obj runtime.Object) (uri string, err error)
 	// GenerateListLink creates an encoded URI for a list that represents the canonical path and query.
 	GenerateListLink(req *http.Request) (uri string, err error)
 }
@@ -90,8 +90,8 @@ func (n ContextBasedNaming) Name(req *http.Request) (namespace, name string, err
 	return ns, requestInfo.Name, nil
 }
 
-func (n ContextBasedNaming) GenerateLink(req *http.Request, obj runtime.Object) (uri string, err error) {
-	requestInfo, ok := request.RequestInfoFrom(n.GetContext(req))
+func (n ContextBasedNaming) GenerateLink(ctx request.Context, obj runtime.Object) (uri string, err error) {
+	requestInfo, ok := request.RequestInfoFrom(ctx)
 	if !ok {
 		return "", fmt.Errorf("missing requestInfo")
 	}
