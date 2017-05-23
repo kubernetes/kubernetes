48d76edc744ea2f8857717b51e6a564a4d49544b
Anirudh <ramanathana@google.com>
2017-05-23 07:35:35 -0700

    PDB MaxUnavailable: Generated

--- staging/src/k8s.io/client-go/pkg/apis/policy/v1beta1/types_swagger_doc_generated.go
+++ staging/src/k8s.io/client-go/pkg/apis/policy/v1beta1/types_swagger_doc_generated.go
@@ -56,9 +56,10 @@ func (PodDisruptionBudgetList) SwaggerDoc() map[string]string {
 }
 
 var map_PodDisruptionBudgetSpec = map[string]string{
-	"":             "PodDisruptionBudgetSpec is a description of a PodDisruptionBudget.",
-	"minAvailable": "An eviction is allowed if at least \"minAvailable\" pods selected by \"selector\" will still be available after the eviction, i.e. even in the absence of the evicted pod.  So for example you can prevent all voluntary evictions by specifying \"100%\".",
-	"selector":     "Label query over pods whose evictions are managed by the disruption budget.",
+	"":               "PodDisruptionBudgetSpec is a description of a PodDisruptionBudget.",
+	"minAvailable":   "An eviction is allowed if at least \"minAvailable\" pods selected by \"selector\" will still be available after the eviction, i.e. even in the absence of the evicted pod.  So for example you can prevent all voluntary evictions by specifying \"100%\".",
+	"selector":       "Label query over pods whose evictions are managed by the disruption budget.",
+	"maxUnavailable": "An eviction is allowed if at most \"maxUnavailable\" pods selected by \"selector\" are unavailable after the eviction, i.e. even in absence of the evicted pod. For example, one can prevent all voluntary evictions by specifying 0. This is a mutually exclusive setting with \"minAvailable\".",
 }
 
 func (PodDisruptionBudgetSpec) SwaggerDoc() map[string]string {
