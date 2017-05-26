akhilerm/apimachinery

Support replaceKeys patch strategy and directive
Kubernetes-commit: 16e07c7d557ea32e47b9565a02331ff7939bb08d

pkg/util/mergepatch/errors.go
@@ -23,9 +23,11 @@ import (
 )
 
 var (
-	ErrBadJSONDoc                     = errors.New("Invalid JSON document")
-	ErrNoListOfLists                  = errors.New("Lists of lists are not supported")
-	ErrBadPatchFormatForPrimitiveList = errors.New("Invalid patch format of primitive list")
+	ErrBadJSONDoc                     = errors.New("invalid JSON document")
+	ErrNoListOfLists                  = errors.New("lists of lists are not supported")
+	ErrBadPatchFormatForPrimitiveList = errors.New("invalid patch format of primitive list")
+	ErrBadPatchFormatForRetainKeys    = errors.New("invalid patch format of retainKeys")
+	ErrPatchContentNotMatchRetainKeys = errors.New("patch content doesn't match retainKeys list")
 )
 
 func ErrNoMergeKey(m map[string]interface{}, k string) error {

kubernetes/kubernetes

staging/src/k8s.io/apimachinery/pkg/util/mergepatch/errors.go
@@ -23,9 +23,11 @@ import (
 )
 
 var (
-	ErrBadJSONDoc                     = errors.New("Invalid JSON document")
-	ErrNoListOfLists                  = errors.New("Lists of lists are not supported")
-	ErrBadPatchFormatForPrimitiveList = errors.New("Invalid patch format of primitive list")
+	ErrBadJSONDoc                      = errors.New("invalid JSON document")
+	ErrNoListOfLists                   = errors.New("lists of lists are not supported")
+	ErrBadPatchFormatForPrimitiveList  = errors.New("invalid patch format of primitive list")
+	ErrBadPatchFormatForReplaceKeys    = errors.New("invalid patch format of replaceKeys")
+	ErrPatchContentNotMatchReplaceKeys = errors.New("patch content doesn't match replaceKeys list")
 )
 
 func ErrNoMergeKey(m map[string]interface{}, k string) error {
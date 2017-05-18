d8e01ed319fb37293fab566ad34d37b54c84e4ec
ymqytw <mengqiy@google.com>
2017-05-17 21:20:50 -0700

    Support replaceKeys patch strategy and directive

--- staging/src/k8s.io/apimachinery/third_party/forked/golang/json/fields.go
+++ staging/src/k8s.io/apimachinery/third_party/forked/golang/json/fields.go
@@ -17,15 +17,21 @@ import (
 	"unicode/utf8"
 )
 
+const (
+	patchStrategyTagKey = "patchStrategy"
+	patchMergeKeyTagKey = "patchMergeKey"
+)
+
 // Finds the patchStrategy and patchMergeKey struct tag fields on a given
 // struct field given the struct type and the JSON name of the field.
+// It returns field type, a slice of patch strategies, merge key and error.
 // TODO: fix the returned errors to be introspectable.
-func LookupPatchMetadata(t reflect.Type, jsonField string) (reflect.Type, string, string, error) {
+func LookupPatchMetadata(t reflect.Type, jsonField string) (reflect.Type, []string, string, error) {
 	if t.Kind() == reflect.Map {
-		return t.Elem(), "", "", nil
+		return t.Elem(), nil, "", nil
 	}
 	if t.Kind() != reflect.Struct {
-		return nil, "", "", fmt.Errorf("merging an object in json but data type is not map or struct, instead is: %s",
+		return nil, nil, "", fmt.Errorf("merging an object in json but data type is not map or struct, instead is: %s",
 			t.Kind().String())
 	}
 	jf := []byte(jsonField)
@@ -50,11 +56,12 @@ func LookupPatchMetadata(t reflect.Type, jsonField string) (reflect.Type, string
 		for i := 1; i < len(f.index); i++ {
 			tjf = tjf.Type.Field(f.index[i])
 		}
-		patchStrategy := tjf.Tag.Get("patchStrategy")
-		patchMergeKey := tjf.Tag.Get("patchMergeKey")
-		return tjf.Type, patchStrategy, patchMergeKey, nil
+		patchStrategy := tjf.Tag.Get(patchStrategyTagKey)
+		patchMergeKey := tjf.Tag.Get(patchMergeKeyTagKey)
+		patchStrategies := strings.Split(patchStrategy, ",")
+		return tjf.Type, patchStrategies, patchMergeKey, nil
 	}
-	return nil, "", "", fmt.Errorf("unable to find api field in struct %s for the json field %q", t.Name(), jsonField)
+	return nil, nil, "", fmt.Errorf("unable to find api field in struct %s for the json field %q", t.Name(), jsonField)
 }
 
 // A field represents a single field found in a struct.
