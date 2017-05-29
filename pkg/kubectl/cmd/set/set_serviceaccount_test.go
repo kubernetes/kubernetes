ea5d5de523e6c10fa354ebf901c8f592f6572b16
chandanmad <chandanmadhesia@gmail.com>
2017-05-29 06:56:09 +0000

    added test case for remote patch request

--- pkg/kubectl/cmd/set/set_serviceaccount_test.go
+++ pkg/kubectl/cmd/set/set_serviceaccount_test.go
@@ -18,11 +18,18 @@ package set
 
 import (
 	"bytes"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"path"
 	"strings"
 	"testing"
 
-	restclient "k8s.io/client-go/rest"
+	"fmt"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest/fake"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/testapi"
@@ -32,9 +39,9 @@ import (
 	cmdtesting "k8s.io/kubernetes/pkg/kubectl/cmd/testing"
 	"k8s.io/kubernetes/pkg/kubectl/resource"
 	"k8s.io/kubernetes/pkg/printers"
-
-	"github.com/stretchr/testify/assert"
 )
+
+const serviceAccount = "serviceaccount1"
 
 func TestServiceAccountLocal(t *testing.T) {
 	inputs := []struct {
@@ -49,19 +56,15 @@ func TestServiceAccountLocal(t *testing.T) {
 		{yaml: "../../../../examples/storage/cassandra/cassandra-statefulset.yaml", apiGroup: apps.GroupName},
 	}
 
-	f, tf, _, ns := cmdtesting.NewAPIFactory()
+	f, tf, _, _ := cmdtesting.NewAPIFactory()
 	tf.Client = &fake.RESTClient{
-		APIRegistry:          api.Registry,
-		NegotiatedSerializer: ns,
+		APIRegistry: api.Registry,
 		Client: fake.CreateHTTPClient(func(req *http.Request) (*http.Response, error) {
 			t.Fatalf("unexpected request: %s %#v\n%#v", req.Method, req.URL, req)
 			return nil, nil
 		}),
 	}
-
 	tf.Namespace = "test"
-	tf.ClientConfig = &restclient.Config{ContentConfig: restclient.ContentConfig{GroupVersion: &api.Registry.GroupOrDie(api.GroupName).GroupVersion}}
-
 	out := bytes.NewBuffer([]byte{})
 	cmd := NewCmdServiceAccount(f, out, out)
 	cmd.SetOutput(out)
@@ -72,9 +75,9 @@ func TestServiceAccountLocal(t *testing.T) {
 		tf.Printer = printers.NewVersionedPrinter(&printers.YAMLPrinter{}, testapi.Default.Converter(), *testapi.Default.GroupVersion())
 		saConfig := ServiceAccountConfig{fileNameOptions: resource.FilenameOptions{
 			Filenames: []string{input.yaml}},
-			Out:   out,
-			Local: true}
-		err := saConfig.Complete(f, cmd, []string{"serviceaccount1"})
+			out:   out,
+			local: true}
+		err := saConfig.Complete(f, cmd, []string{serviceAccount})
 		if err == nil {
 			err = saConfig.Validate()
 		}
@@ -84,6 +87,132 @@ func TestServiceAccountLocal(t *testing.T) {
 		if err != nil {
 			t.Fatalf("unexpected error: %v", err)
 		}
-		assert.True(t, strings.Contains(out.String(), "serviceAccountName: serviceaccount1"))
+		assert.True(t, strings.Contains(out.String(), "serviceAccountName: "+serviceAccount))
 	}
+}
+
+func TestServiceAccountRemote(t *testing.T) {
+	inputs := []struct {
+		object              runtime.Object
+		apiPrefix, apiGroup string
+		args                []string
+	}{
+		{
+			object: &extensions.ReplicaSet{
+				TypeMeta:   metav1.TypeMeta{Kind: "ReplicaSet", APIVersion: api.Registry.GroupOrDie(extensions.GroupName).GroupVersion.String()},
+				ObjectMeta: metav1.ObjectMeta{Name: "nginx"},
+			},
+			apiPrefix: "/apis", apiGroup: extensions.GroupName,
+			args: []string{"replicaset", "nginx", serviceAccount},
+		},
+		{
+			object: &extensions.DaemonSet{
+				TypeMeta:   metav1.TypeMeta{Kind: "DaemonSet", APIVersion: api.Registry.GroupOrDie(extensions.GroupName).GroupVersion.String()},
+				ObjectMeta: metav1.ObjectMeta{Name: "nginx"},
+			},
+			apiPrefix: "/apis", apiGroup: extensions.GroupName,
+			args: []string{"daemonset", "nginx", serviceAccount},
+		},
+		{
+			object: &api.ReplicationController{
+				TypeMeta:   metav1.TypeMeta{Kind: "ReplicationController", APIVersion: api.Registry.GroupOrDie(api.GroupName).GroupVersion.String()},
+				ObjectMeta: metav1.ObjectMeta{Name: "nginx"},
+			},
+			apiPrefix: "/api", apiGroup: api.GroupName,
+			args: []string{"replicationcontroller", "nginx", serviceAccount}},
+		{
+			object: &extensions.Deployment{
+				TypeMeta:   metav1.TypeMeta{Kind: "Deployment", APIVersion: api.Registry.GroupOrDie(extensions.GroupName).GroupVersion.String()},
+				ObjectMeta: metav1.ObjectMeta{Name: "nginx"},
+			},
+			apiPrefix: "/apis", apiGroup: extensions.GroupName,
+			args: []string{"deployment", "nginx", serviceAccount},
+		},
+		{
+			object: &batch.Job{
+				TypeMeta:   metav1.TypeMeta{Kind: "Job", APIVersion: api.Registry.GroupOrDie(batch.GroupName).GroupVersion.String()},
+				ObjectMeta: metav1.ObjectMeta{Name: "nginx"},
+			},
+			apiPrefix: "/apis", apiGroup: batch.GroupName,
+			args: []string{"job", "nginx", serviceAccount},
+		},
+		{
+			object: &apps.StatefulSet{
+				TypeMeta:   metav1.TypeMeta{Kind: "StatefulSet", APIVersion: api.Registry.GroupOrDie(apps.GroupName).GroupVersion.String()},
+				ObjectMeta: metav1.ObjectMeta{Name: "nginx"},
+			},
+			apiPrefix: "/apis", apiGroup: apps.GroupName,
+			args: []string{"statefulset", "nginx", serviceAccount},
+		},
+	}
+	for _, input := range inputs {
+
+		groupVersion := api.Registry.GroupOrDie(input.apiGroup).GroupVersion
+		//input.object.GetObjectKind().SetGroupVersionKind(groupVersion.WithKind(input.args[0]))
+		testapi.Default = testapi.Groups[input.apiGroup]
+		f, tf, codec, _ := cmdtesting.NewAPIFactory()
+		tf.Printer = printers.NewVersionedPrinter(&printers.YAMLPrinter{}, testapi.Default.Converter(), *testapi.Default.GroupVersion())
+		tf.Namespace = "test"
+		tf.Client = &fake.RESTClient{
+			APIRegistry:          api.Registry,
+			NegotiatedSerializer: testapi.Default.NegotiatedSerializer(),
+			Client: fake.CreateHTTPClient(func(req *http.Request) (*http.Response, error) {
+				resourcePath := testapi.Default.ResourcePath(input.args[0]+"s", tf.Namespace, input.args[1])
+				switch p, m := req.URL.Path, req.Method; {
+				case p == resourcePath && m == http.MethodGet:
+					return &http.Response{StatusCode: http.StatusOK, Header: defaultHeader(), Body: objBody(codec, input.object)}, nil
+				case p == resourcePath && m == http.MethodPatch:
+					stream, err := req.GetBody()
+					if err != nil {
+						return nil, err
+					}
+					bytes, err := ioutil.ReadAll(stream)
+					if err != nil {
+						return nil, err
+					}
+					assert.True(t, strings.Contains(string(bytes), `"serviceAccountName":`+`"`+serviceAccount+`"`))
+					return &http.Response{StatusCode: http.StatusOK, Header: defaultHeader(), Body: objBody(codec, input.object)}, nil
+				default:
+					t.Errorf("%s: unexpected request: %s %#v\n%#v", "serviceaccount", req.Method, req.URL, req)
+					return nil, fmt.Errorf("unexpected request")
+				}
+			}),
+			VersionedAPIPath: path.Join(input.apiPrefix, groupVersion.String()),
+			GroupName:        input.apiGroup,
+		}
+		out := bytes.NewBuffer([]byte{})
+		cmd := NewCmdServiceAccount(f, out, out)
+		cmd.SetOutput(out)
+		cmd.Flags().Set("output", "yaml")
+
+		saConfig := ServiceAccountConfig{
+			out:   out,
+			local: false}
+		err := saConfig.Complete(f, cmd, input.args)
+		if err == nil {
+			saConfig.categoryExpander = resource.LegacyCategoryExpander
+			err = saConfig.Validate()
+		}
+		if err == nil {
+			err = saConfig.Run()
+		}
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	}
+
+}
+
+func objBody(codec runtime.Codec, obj runtime.Object) io.ReadCloser {
+	return ioutil.NopCloser(bytes.NewReader([]byte(runtime.EncodeOrDie(codec, obj))))
+}
+
+func defaultHeader() http.Header {
+	header := http.Header{}
+	header.Set("Content-Type", runtime.ContentTypeJSON)
+	return header
+}
+
+func bytesBody(bodyBytes []byte) io.ReadCloser {
+	return ioutil.NopCloser(bytes.NewReader(bodyBytes))
 }
