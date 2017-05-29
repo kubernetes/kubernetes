73555e7ce94a82d2ff2e35fa9f48713ab65e775c
Abhinav Dahiya <abhinav.dahiya@coreos.com>
2017-05-29 16:58:45 -0700

    Fix inconsistenty in finding cni binaries Fixes [#46476]
    Signed-off-by: Abhinav Dahiya <abhinav.dahiya@coreos.com>

--- pkg/kubelet/network/cni/cni.go
+++ pkg/kubelet/network/cni/cni.go
@@ -134,7 +134,7 @@ func getDefaultCNINetwork(pluginDir, binDir, vendorCNIDirPrefix string) (*cniNet
 		// Search for vendor-specific plugins as well as default plugins in the CNI codebase.
 		vendorDir := vendorCNIDir(vendorCNIDirPrefix, confType)
 		cninet := &libcni.CNIConfig{
-			Path: []string{binDir, vendorDir},
+			Path: []string{vendorDir, binDir},
 		}
 		network := &cniNetwork{name: confList.Name, NetworkConfig: confList, CNIConfig: cninet}
 		return network, nil
