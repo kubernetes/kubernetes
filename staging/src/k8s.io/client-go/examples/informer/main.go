c48e6af96b4ad2955e40d747a11cc478d9b19887
Richard Wall <richard.wall@jetstack.io>
2017-05-30 17:20:34 +0100

    Use a longer resync period

--- staging/src/k8s.io/client-go/examples/informer/main.go
+++ staging/src/k8s.io/client-go/examples/informer/main.go
@@ -54,7 +54,7 @@ func main() {
 		glog.Fatal(err)
 	}
 
-	factory := informers.NewSharedInformerFactory(clientset, time.Second*10)
+	factory := informers.NewSharedInformerFactory(clientset, time.Hour*24)
 
 	informer := factory.Core().V1().Pods().Informer()
 
