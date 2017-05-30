b1f708758c70dfbb3dde4dec7d2f423b7303e661
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-05-30 10:58:33 +0200

    aggregation: handle ClusterIPNone correctly

--- staging/src/k8s.io/apiserver/pkg/util/proxy/proxy.go
+++ staging/src/k8s.io/apiserver/pkg/util/proxy/proxy.go
@@ -101,6 +101,8 @@ func ResolveCluster(services listersv1.ServiceLister, namespace, id string) (*ur
 	port := intstr.FromInt(443)
 
 	switch {
+	case svc.Spec.Type == v1.ServiceTypeClusterIP && svc.Spec.ClusterIP == v1.ClusterIPNone:
+		return nil, fmt.Errorf(`cannot route to service with ClusterIP "None"`)
 	// use IP from a clusterIP for these service types
 	case svc.Spec.Type == v1.ServiceTypeClusterIP, svc.Spec.Type == v1.ServiceTypeLoadBalancer, svc.Spec.Type == v1.ServiceTypeNodePort:
 		svcPort, err := findServicePort(svc, port)
