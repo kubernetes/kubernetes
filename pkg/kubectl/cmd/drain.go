524badf4c62e8a79ce1355bf89d3661bbf316a8e
Suyog Barve <Suyog.Barve@walmart.com>
2017-05-29 23:34:31 -0500

    show message when retrying on 429 error-code

--- pkg/kubectl/cmd/drain.go
+++ pkg/kubectl/cmd/drain.go
@@ -495,8 +495,10 @@ func (o *DrainOptions) evictPods(pods []api.Pod, policyGroupVersion string, getP
 				if err == nil {
 					break
 				} else if apierrors.IsTooManyRequests(err) {
-					time.Sleep(5 * time.Second)
-				} else {
+					retryInterval := 5
+					fmt.Fprintf(o.ErrOut, "Cannot evict pod as it would violate the pod's disruption budget. Retry in %v seconds\n", retryInterval)
+					time.Sleep(time.Duration(retryInterval) * time.Second)
+				} else if !apierrors.IsNotFound(err) {
 					errCh <- fmt.Errorf("error when evicting pod %q: %v", pod.Name, err)
 					return
 				}
