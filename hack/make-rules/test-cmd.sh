33cecc349948846f0519c9e57b1f0deb65d24b58
Cao Shufeng <caosf.fnst@cn.fujitsu.com>
2017-05-31 15:20:09 +0800

    Update deprecated flags for "make test-cmd"

--- hack/make-rules/test-cmd.sh
+++ hack/make-rules/test-cmd.sh
@@ -40,15 +40,13 @@ function run_kube_apiserver() {
   AUTHORIZATION_MODE="RBAC,AlwaysAllow"
 
   "${KUBE_OUTPUT_HOSTBIN}/kube-apiserver" \
-    --address="127.0.0.1" \
-    --public-address-override="127.0.0.1" \
-    --port="${API_PORT}" \
+    --insecure-bind-address="127.0.0.1" \
+    --bind-address="127.0.0.1" \
+    --insecure-port="${API_PORT}" \
     --authorization-mode="${AUTHORIZATION_MODE}" \
     --secure-port="${SECURE_API_PORT}" \
     --admission-control="${ADMISSION_CONTROL}" \
     --etcd-servers="http://${ETCD_HOST}:${ETCD_PORT}" \
-    --public-address-override="127.0.0.1" \
-    --kubelet-port=${KUBELET_PORT} \
     --runtime-config=api/v1 \
     --storage-media-type="${KUBE_TEST_API_STORAGE_TYPE-}" \
     --cert-dir="${TMPDIR:-/tmp/}" \
