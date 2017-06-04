163f1de5edcc0980ad474250ad94d02760308cb0
Shyam Jeedigunta <shyamjvs@google.com>
2017-06-04 20:07:36 +0200

    Avoid double printing output of gcloud commands in kubemark

--- test/kubemark/gce/util.sh
+++ test/kubemark/gce/util.sh
@@ -24,7 +24,6 @@ function run-gcloud-compute-with-retries {
     # We don't use 'local' to declare gcloud_result as then ret_val always gets value 0.
     # We use tee to output to &5 (redirected to stdout) while also storing it in the variable.
     gcloud_result=$(gcloud compute "$@" 2>&1 | tee >(cat - >&5)) || local ret_val="$?"
-    echo "${gcloud_result}"
     if [[ "${ret_val:-0}" -ne "0" ]]; then
       if [[ $(echo "${gcloud_result}" | grep -c "already exists") -gt 0 ]]; then
         if [[ "${attempt}" == 1 ]]; then
