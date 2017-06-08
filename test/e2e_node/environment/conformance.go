7e5c43a0425ac85247e2451cf05a100ee847ba8f
Davanum Srinivas <davanum@gmail.com>
2017-06-08 15:56:59 -0400

    Run cAdvisor on the same interface as kubelet
    cAdvisor currently binds to all interfaces. Currently the only
    solution is to use iptables to block access to the port. We
    are better off making cAdvisor to bind to the interface that
    kubelet uses for better security.
    
    Fixes #11710

--- test/e2e_node/environment/conformance.go
+++ test/e2e_node/environment/conformance.go
@@ -99,7 +99,7 @@ func containerRuntime() error {
 	}
 
 	// Setup cadvisor to check the container environment
-	c, err := cadvisor.New(0 /*don't start the http server*/, "docker", "/var/lib/kubelet")
+	c, err := cadvisor.New("", 0 /*don't start the http server*/, "docker", "/var/lib/kubelet")
 	if err != nil {
 		return printError("Container Runtime Check: %s Could not start cadvisor %v", failed, err)
 	}
