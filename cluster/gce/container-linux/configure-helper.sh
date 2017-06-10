948c6c8027f47f0ac28098976dcd464770f785d4
Casey Davenport <casey@tigera.io>
2017-06-10 16:58:16 -0700

    Change how Typha CPU / replias are determined.

--- cluster/gce/container-linux/configure-helper.sh
+++ cluster/gce/container-linux/configure-helper.sh
@@ -36,7 +36,7 @@ function create-dirs {
 
 # Vars assumed:
 #   NUM_NODES
-function get-calico-cpu {
+function get-calico-node-cpu {
   local suggested_calico_cpus=100m
   if [[ "${NUM_NODES}" -gt "10" ]]; then
     suggested_calico_cpus=250m
@@ -48,6 +48,38 @@ function get-calico-cpu {
     suggested_calico_cpus=1000m
   fi
   echo "${suggested_calico_cpus}"
+}
+
+# Vars assumed:
+#    NUM_NODES
+function get-calico-typha-replicas {
+  local typha_count=1
+  if [[ "${NUM_NODES}" -gt "10" ]]; then
+    typha_count=2
+  fi
+  if [[ "${NUM_NODES}" -gt "100" ]]; then
+    typha_count=3
+  fi
+  if [[ "${NUM_NODES}" -gt "250" ]]; then
+    typha_count=4
+  fi
+  if [[ "${NUM_NODES}" -gt "500" ]]; then
+    typha_count=5
+  fi
+  echo "${typha_count}"
+}
+
+# Vars assumed:
+#    NUM_NODES
+function get-calico-typha-cpu {
+  local typha_cpu=200m
+  if [[ "${NUM_NODES}" -gt "10" ]]; then
+    typha_cpu=500m
+  fi
+  if [[ "${NUM_NODES}" -gt "100" ]]; then
+    typha_cpu=1000m
+  fi
+  echo "${typha_cpu}"
 }
 
 # Create directories referenced in the kube-controller-manager manifest for
@@ -1229,9 +1261,13 @@ function start-kube-addons {
   if [[ "${NETWORK_POLICY_PROVIDER:-}" == "calico" ]]; then
     setup-addon-manifests "addons" "calico-policy-controller"
 
-    # Configure Calico resource requests based on cluster size.
-    local -r calico_file="${dst_dir}/calico-policy-controller/calico-node-daemonset.yaml"
-    sed -i -e "s@__CALICO_NODE_CPU__@$(get-calico-cpu)@g" "${calico_file}"
+    # Configure Calico based on cluster size and image type. 
+    local -r ds_file="${dst_dir}/calico-policy-controller/calico-node-daemonset.yaml"
+    local -r typha_dep_file="${dst_dir}/calico-policy-controller/typha-deployment.yaml"
+    sed -i -e "s@__CALICO_CNI_DIR__@/opt/cni/bin@g" "${ds_file}"
+    sed -i -e "s@__CALICO_NODE_CPU__@$(get-calico-node-cpu)@g" "${ds_file}"
+    sed -i -e "s@__CALICO_TYPHA_CPU__@$(get-calico-typha-cpu)@g" "${typha_dep_file}"
+    sed -i -e "s@__CALICO_TYPHA_REPLICAS__@$(get-calico-typha-replicas)@g" "${typha_dep_file}"
   fi
   if [[ "${ENABLE_DEFAULT_STORAGE_CLASS:-}" == "true" ]]; then
     setup-addon-manifests "addons" "storage-class/gce"
