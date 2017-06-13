8de41f43e5092a2a64933d52951b4aa5ff5d5761
Lucas Käldström <lucas@weave.works>
2017-06-13 16:18:33 +0300

    kubeadm: Use the v1.7 branch by default

--- cmd/kubeadm/app/apis/kubeadm/v1alpha1/defaults.go
+++ cmd/kubeadm/app/apis/kubeadm/v1alpha1/defaults.go
@@ -26,7 +26,7 @@ import (
 const (
 	DefaultServiceDNSDomain  = "cluster.local"
 	DefaultServicesSubnet    = "10.96.0.0/12"
-	DefaultKubernetesVersion = "stable-1.6"
+	DefaultKubernetesVersion = "stable-1.7"
 	DefaultAPIBindPort       = 6443
 	DefaultDiscoveryBindPort = 9898
 	DefaultAuthorizationMode = "RBAC"
