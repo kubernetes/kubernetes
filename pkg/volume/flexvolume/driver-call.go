894b9b2add77034772cc8860857ca566a1eee110
Chakravarthy Nelluri <nelcyguy@gmail.com>
2017-06-14 03:03:38 -0400

    Remove controller node plugin driver dependency for non-attachable flex volume drivers (Ex: NFS).

--- pkg/volume/flexvolume/driver-call.go
+++ pkg/volume/flexvolume/driver-call.go
@@ -58,6 +58,8 @@ const (
 	optionKeyPodUID       = "kubernetes.io/pod.uid"
 
 	optionKeyServiceAccountName = "kubernetes.io/serviceAccount.name"
+
+	attachCapability = "attach"
 )
 
 const (
@@ -201,6 +203,10 @@ type DriverStatus struct {
 	VolumeName string `json:"volumeName,omitempty"`
 	// Represents volume is attached on the node
 	Attached bool `json:"attached,omitempty"`
+	// Returns capabilities of the driver.
+	// By default we assume all the capabilities are supported.
+	// If the plugin does not support a capability, it can return false for that capability.
+	Capabilities map[string]bool
 }
 
 // isCmdNotSupportedErr checks if the error corresponds to command not supported by
