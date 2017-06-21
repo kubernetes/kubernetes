b6e08ec39dde2a6f3c24648aa8867c6aee9853cb
Josh Horwitz <horwitzja@gmail.com>
2017-06-21 11:54:20 -0400

    Revert "Merge pull request #43946 from jhorwit2/jah/host-path-psp"
    This reverts commit b5eadb5d6b9ce34a673caeafe4fa0971c4b91139, reversing
    changes made to 1889d654f5c76fcc6ce14c6d3702c06a6498695d.

--- pkg/apis/extensions/v1beta1/zz_generated.conversion.go
+++ pkg/apis/extensions/v1beta1/zz_generated.conversion.go
@@ -1219,7 +1219,6 @@ func autoConvert_v1beta1_PodSecurityPolicySpec_To_extensions_PodSecurityPolicySp
 		return err
 	}
 	out.ReadOnlyRootFilesystem = in.ReadOnlyRootFilesystem
-	out.AllowedHostPaths = *(*[]string)(unsafe.Pointer(&in.AllowedHostPaths))
 	return nil
 }
 
@@ -1261,7 +1260,6 @@ func autoConvert_extensions_PodSecurityPolicySpec_To_v1beta1_PodSecurityPolicySp
 		return err
 	}
 	out.ReadOnlyRootFilesystem = in.ReadOnlyRootFilesystem
-	out.AllowedHostPaths = *(*[]string)(unsafe.Pointer(&in.AllowedHostPaths))
 	return nil
 }
 
