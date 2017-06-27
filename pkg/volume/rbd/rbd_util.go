ef3c0c72de7c9757a46fe0a5fa44fbc0daa7fa87
zhangningmin <zhangningmin@wanda.cn>
2017-06-27 12:50:42 +0800

    Improve the warning if the rbd cmd is not found.

--- pkg/volume/rbd/rbd_util.go
+++ pkg/volume/rbd/rbd_util.go
@@ -44,6 +44,7 @@ import (
 const (
 	imageWatcherStr = "watcher="
 	kubeLockMagic   = "kubelet_lock_magic_"
+	rbdCmdErr       = "rbd_cmd_not_found"
 )
 
 // search /sys/bus for rbd device that matches given pool and image
@@ -139,7 +140,10 @@ func (util *RBDUtil) rbdLock(b rbdMounter, lock bool) error {
 			append([]string{"lock", "list", b.Image, "--pool", b.Pool, "--id", b.Id, "-m", mon}, secret_opt...))
 		output = string(cmd)
 		glog.Infof("lock list output %q", output)
-		if err != nil {
+		if cmd == nil {
+			err = fmt.Errorf(rbdCmdErr)
+			continue
+		} else if err != nil {
 			continue
 		}
 
@@ -269,7 +273,11 @@ func (util *RBDUtil) AttachDisk(b rbdMounter) error {
 
 		// fence off other mappers
 		if err = util.fencing(b); err != nil {
-			return fmt.Errorf("rbd: image %s is locked by other nodes", b.Image)
+			if err.Error() == rbdCmdErr {
+				return fmt.Errorf("rbd: rbd cmd not found")
+			} else {
+				return fmt.Errorf("rbd: image %s is locked by other nodes", b.Image)
+			}
 		}
 		// rbd lock remove needs ceph and image config
 		// but kubelet doesn't get them from apiserver during teardown
@@ -315,6 +323,7 @@ func (util *RBDUtil) AttachDisk(b rbdMounter) error {
 }
 
 func (util *RBDUtil) DetachDisk(c rbdUnmounter, mntPath string) error {
+	var cmd []byte
 	device, cnt, err := mount.GetDeviceNameFromMount(c.mounter, mntPath)
 	if err != nil {
 		return fmt.Errorf("rbd detach disk: failed to get device from mnt: %s\nError: %v", mntPath, err)
@@ -325,8 +334,10 @@ func (util *RBDUtil) DetachDisk(c rbdUnmounter, mntPath string) error {
 	// if device is no longer used, see if can unmap
 	if cnt <= 1 {
 		// rbd unmap
-		_, err = c.plugin.execCommand("rbd", []string{"unmap", device})
-		if err != nil {
+		cmd, err = c.plugin.execCommand("rbd", []string{"unmap", device})
+		if cmd == nil {
+			return fmt.Errorf("rbd: rbd cmd not found")
+		} else if err != nil {
 			return fmt.Errorf("rbd: failed to unmap device %s:Error: %v", device, err)
 		}
 
