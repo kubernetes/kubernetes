c6bfbaef237cc60388d607e80ba3f796d233b811
Chao Xu <xuchao@google.com>
2017-07-13 13:23:28 -0700

    remove apimachinery's dependency on k8s.io/api

--- staging/src/k8s.io/apimachinery/pkg/test/util.go
+++ staging/src/k8s.io/apimachinery/pkg/test/util.go
@@ -25,13 +25,20 @@ import (
 	apiserializer "k8s.io/apimachinery/pkg/runtime/serializer"
 )
 
-// List holds a list of objects, which may not be known by the server.
+// List and ListV1 should be kept in sync with k8s.io/kubernetes/pkg/api#List
+// and k8s.io/api/core/v1#List.
 type List struct {
 	metav1.TypeMeta
-	// +optional
 	metav1.ListMeta
 
 	Items []runtime.Object
+}
+
+type ListV1 struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+
+	Items []runtime.RawExtension `json:"items" protobuf:"bytes,2,rep,name=items"`
 }
 
 func TestScheme() (*runtime.Scheme, apiserializer.CodecFactory) {
