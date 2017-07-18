adae600c8d13f0b8a8d39535c837a4855cdabdc3
Brendan Burns <bburns@microsoft.com>
2017-07-18 23:33:19 +0000

    Fix up imports.

--- pkg/volume/azure_dd/azure_dd.go
+++ pkg/volume/azure_dd/azure_dd.go
@@ -23,10 +23,6 @@ import (
 
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/kubernetes/pkg/api/v1"
-	"k8s.io/kubernetes/pkg/cloudprovider"
-	"k8s.io/kubernetes/pkg/cloudprovider/providers/azure"
-	"k8s.io/kubernetes/pkg/util/exec"
-	"k8s.io/kubernetes/pkg/util/keymutex"
 	"k8s.io/kubernetes/pkg/util/mount"
 	"k8s.io/kubernetes/pkg/volume"
 )
