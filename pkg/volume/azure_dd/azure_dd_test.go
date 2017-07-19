1146b03e4d5b065c97a457aedc292039f22c61f9
Sean Knox <knoxville@gmail.com>
2017-07-18 18:31:59 -0700

    Remove unused imports

--- pkg/volume/azure_dd/azure_dd_test.go
+++ pkg/volume/azure_dd/azure_dd_test.go
@@ -20,12 +20,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/arm/compute"
-
-	"k8s.io/apimachinery/pkg/types"
 	utiltesting "k8s.io/client-go/util/testing"
 	"k8s.io/kubernetes/pkg/api/v1"
-	"k8s.io/kubernetes/pkg/util/mount"
 	"k8s.io/kubernetes/pkg/volume"
 	volumetest "k8s.io/kubernetes/pkg/volume/testing"
 )
