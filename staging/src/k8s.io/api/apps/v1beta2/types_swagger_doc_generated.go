b1d99e9f7e0a76cdf41179fb6a9a68cd203b00b6
Michail Kargakis <mkargaki@redhat.com>
2017-07-19 19:40:46 +0200

    Update d.status.unavailableReplicas api comment

--- staging/src/k8s.io/api/apps/v1beta2/types_swagger_doc_generated.go
+++ staging/src/k8s.io/api/apps/v1beta2/types_swagger_doc_generated.go
@@ -167,7 +167,7 @@ var map_DeploymentStatus = map[string]string{
 	"updatedReplicas":     "Total number of non-terminated pods targeted by this deployment that have the desired template spec.",
 	"readyReplicas":       "Total number of ready pods targeted by this deployment.",
 	"availableReplicas":   "Total number of available pods (ready for at least minReadySeconds) targeted by this deployment.",
-	"unavailableReplicas": "Total number of unavailable pods targeted by this deployment.",
+	"unavailableReplicas": "Total number of unavailable pods targeted by this deployment. This is the total number of pods that are still required for the deployment to have 100% available capacity. They may either be pods that are running but not yet available or pods that still have not been created.",
 	"conditions":          "Represents the latest available observations of a deployment's current state.",
 	"collisionCount":      "Count of hash collisions for the Deployment. The Deployment controller uses this field as a collision avoidance mechanism when it needs to create the name for the newest ReplicaSet.",
 }
