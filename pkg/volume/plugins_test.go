c18ecb08259d7fc03375c3a7deb93a9093d6f289
Jing Xu <jinxu@google.com>
2017-07-25 14:55:32 -0700

    Check volumespec is nil in FindPluginBySpec
    This PR fixes issue #49513

--- pkg/volume/plugins_test.go
+++ pkg/volume/plugins_test.go
@@ -112,6 +112,17 @@ func TestVolumePluginMgrFunc(t *testing.T) {
 	if plug.GetPluginName() != "testPlugin" {
 		t.Errorf("Wrong name: %s", plug.GetPluginName())
 	}
+
+	plug, err = vpm.FindPluginBySpec(nil)
+	if err == nil {
+		t.Errorf("Should return error if volume spec is nil")
+	}
+
+	volumeSpec := &Spec{}
+	plug, err = vpm.FindPluginBySpec(volumeSpec)
+	if err != nil {
+		t.Errorf("Should return test plugin if volume spec is not nil")
+	}
 }
 
 func Test_ValidatePodTemplate(t *testing.T) {
