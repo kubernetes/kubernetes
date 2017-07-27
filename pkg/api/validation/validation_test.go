1c50db68a0c66569df68d611148716bd68c0f046
Avesh Agarwal <avagarwa@redhat.com>
2017-07-27 14:33:25 -0400

    Fix upgrade issue from 1.6 to 1.7 when PreferredDuringSchedulingIgnoredDuringExecution pod anti affinity is used without topology key.
    Fixes issue: https://github.com/kubernetes/kubernetes/issues/49492

--- pkg/api/validation/validation_test.go
+++ pkg/api/validation/validation_test.go
@@ -5128,31 +5128,6 @@ func TestValidatePodWithDisabledAffinityInAnnotations(t *testing.T) {
 				},
 			}),
 		},
-		{
-			ObjectMeta: metav1.ObjectMeta{Name: "123", Namespace: "ns"},
-			Spec: validPodSpec(&api.Affinity{
-				PodAntiAffinity: &api.PodAntiAffinity{
-					PreferredDuringSchedulingIgnoredDuringExecution: []api.WeightedPodAffinityTerm{
-						{
-							Weight: 10,
-							PodAffinityTerm: api.PodAffinityTerm{
-								LabelSelector: &metav1.LabelSelector{
-									MatchExpressions: []metav1.LabelSelectorRequirement{
-										{
-											Key:      "key2",
-											Operator: metav1.LabelSelectorOpNotIn,
-											Values:   []string{"value1", "value2"},
-										},
-									},
-								},
-								Namespaces:  []string{"ns"},
-								TopologyKey: "",
-							},
-						},
-					},
-				},
-			}),
-		},
 	}
 
 	for _, v := range errorCases {
