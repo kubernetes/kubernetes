76d98d29211dc08a1b61fe1edbb2160d023baa15
yiqinguo <468894985@qq.com>
2017-07-28 16:22:17 +0800

    Avoid running panic

--- pkg/kubelet/rkt/rkt.go
+++ pkg/kubelet/rkt/rkt.go
@@ -1804,6 +1804,10 @@ func (r *Runtime) SyncPod(pod *v1.Pod, _ v1.PodStatus, podStatus *kubecontainer.
 		}
 	}()
 	// TODO: (random-liu) Stop using running pod in SyncPod()
+	if podStatus == nil {
+		glog.Error("podStatus is nil")
+		return
+	}
 	runningPod := kubecontainer.ConvertPodStatusToRunningPod(r.Type(), podStatus)
 	// Add references to all containers.
 	unidentifiedContainers := make(map[kubecontainer.ContainerID]*kubecontainer.Container)
