ad7012e9746784c2b1dd7643d54feff7800b1b93
Lucas Käldström <lucas@weave.works>
2017-08-09 19:22:40 +0300

    kubeadm: Upload configuration used at 'kubeadm init' time to ConfigMap for easier upgrades

--- cmd/kubeadm/app/cmd/phases/certs.go
+++ cmd/kubeadm/app/cmd/phases/certs.go
@@ -25,7 +25,6 @@ import (
 
 	kubeadmapi "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm"
 	kubeadmapiext "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1alpha1"
-	"k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/validation"
 	kubeadmconstants "k8s.io/kubernetes/cmd/kubeadm/app/constants"
 	certphase "k8s.io/kubernetes/cmd/kubeadm/app/phases/certs"
 	"k8s.io/kubernetes/cmd/kubeadm/app/phases/certs/pkiutil"
@@ -132,24 +131,9 @@ func runCmdFunc(cmdFunc func(cfg *kubeadmapi.MasterConfiguration) error, cfgPath
 	// are shared between sub commands and gets access to current value e.g. flags value.
 
 	return func(cmd *cobra.Command, args []string) {
-		internalcfg := &kubeadmapi.MasterConfiguration{}
 
-		// Takes passed flags into account; the defaulting is executed once again enforcing assignement of
-		// static default values to cfg only for values not provided with flags
-		api.Scheme.Default(cfg)
-		api.Scheme.Convert(cfg, internalcfg, nil)
-
-		// Loads configuration from config file, if provided
-		// Nb. --config overrides command line flags
-		err := configutil.TryLoadMasterConfiguration(*cfgPath, internalcfg)
-		kubeadmutil.CheckErr(err)
-
-		// Applies dynamic defaults to settings not provided with flags
-		err = configutil.SetInitDynamicDefaults(internalcfg)
-		kubeadmutil.CheckErr(err)
-
-		// Validates cfg (flags/configs + defaults + dynamic defaults)
-		err = validation.ValidateMasterConfiguration(internalcfg).ToAggregate()
+		// This call returns the ready-to-use configuration based on the configuration file that might or might not exist and the default cfg populated by flags
+		internalcfg, err := configutil.ConfigFileAndDefaultsToInternalConfig(*cfgPath, cfg)
 		kubeadmutil.CheckErr(err)
 
 		// Execute the cmdFunc
