3989b18586203eb366257fa4c8e9c0d605ec41df
cedric lamoriniere <cedric.lamoriniere@amadeus.com>
2017-08-16 16:05:32 +0200

    update API v1 Job object
    Add new fields in api v1.JobSpec object for backoff policy
    - BackoffLimit
    - FailedPodsLimit
    
    fixes: https://github.com/kubernetes/community/pull/583

--- pkg/apis/batch/validation/validation.go
+++ pkg/apis/batch/validation/validation.go
@@ -113,6 +113,9 @@ func validateJobSpec(spec *batch.JobSpec, fldPath *field.Path) field.ErrorList {
 	if spec.ActiveDeadlineSeconds != nil {
 		allErrs = append(allErrs, apivalidation.ValidateNonnegativeField(int64(*spec.ActiveDeadlineSeconds), fldPath.Child("activeDeadlineSeconds"))...)
 	}
+	if spec.BackoffLimit != nil {
+		allErrs = append(allErrs, apivalidation.ValidateNonnegativeField(int64(*spec.BackoffLimit), fldPath.Child("backoffLimit"))...)
+	}
 
 	allErrs = append(allErrs, apivalidation.ValidatePodTemplateSpec(&spec.Template, fldPath.Child("template"))...)
 	if spec.Template.Spec.RestartPolicy != api.RestartPolicyOnFailure &&
