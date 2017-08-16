b452c09c24a7e9666add62b7aee582094fa13aba
Connor Doyle <connor.p.doyle@intel.com>
2017-08-15 22:32:37 -0700

    Added unit tests for takeByTopology in cpumanager
    - Moved takeByTopology to a new file.
    - Made whole-socket, whole-core and cpu branches deterministic.
    - Fixed a bug where asking for zero cpus yielded a nonempty cpuset.

---
+++ pkg/kubelet/cpumanager/util.go
@@ -0,0 +1,138 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cpumanager
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/golang/glog"
+	"k8s.io/kubernetes/pkg/kubelet/cpumanager/topology"
+	"k8s.io/kubernetes/pkg/kubelet/cpuset"
+)
+
+func takeByTopology(topo *topology.CPUTopology, availableCPUs cpuset.CPUSet, numCPUs int) (cpuset.CPUSet, error) {
+	result := cpuset.NewCPUSet()
+
+	if numCPUs == 0 {
+		return result, nil
+	}
+	if numCPUs > availableCPUs.Size() {
+		return nil, fmt.Errorf("not enough cpus available to satisfy request")
+	}
+
+	// Algorithm: topology-aware best-fit
+	CPUsPerCore := topo.NumCPUs / topo.NumCores
+	CPUsPerSocket := topo.NumCPUs / topo.NumSockets
+	topoDetails := topo.CPUtopoDetails.KeepOnly(availableCPUs)
+
+	// Auxilliary closure to update intermediate results:
+	// - Adds cpus to result
+	// - Recalculates availableCPUs
+	// - Prunes topoDetails
+	// - decrements numCPUs
+	take := func(cpus cpuset.CPUSet) {
+		result = result.Union(cpus)
+		availableCPUs = availableCPUs.Difference(result)
+		topoDetails = topoDetails.KeepOnly(availableCPUs)
+		numCPUs -= cpus.Size()
+	}
+
+	// Returns true if the supplied socket is fully available in
+	// `topoDetails`.
+	isFullSocket := func(socketID int) bool {
+		return topoDetails.CPUsInSocket(socketID).Size() == CPUsPerSocket
+	}
+
+	// Returns true if the supplied core is fully available in
+	// `topoDetails`.
+	isFullCore := func(coreID int) bool {
+		return topoDetails.CPUsInCore(coreID).Size() == CPUsPerCore
+	}
+
+	// 1. Acquire whole sockets, if available and the container requires
+	//    at least a socket's-worth of CPUs.
+	for _, s := range topoDetails.Sockets().Filter(isFullSocket).AsSlice() {
+		if numCPUs >= CPUsPerSocket {
+			glog.V(4).Infof("[cpumanager] takeByTopology: claiming socket [%d]", s)
+			take(topoDetails.CPUsInSocket(s))
+			if numCPUs < 1 {
+				return result, nil
+			}
+		}
+	}
+
+	// 2. Acquire whole cores, if available and the container requires
+	//    at least a core's-worth of CPUs.
+
+	// `socketIDs` are sorted by:
+	// - the number of whole available cores, ascending.
+	socketIDs := topoDetails.Sockets().AsSlice()
+	sort.Slice(socketIDs,
+		func(i, j int) bool {
+			iCores := topoDetails.CoresInSocket(socketIDs[i]).Filter(isFullCore)
+			jCores := topoDetails.CoresInSocket(socketIDs[j]).Filter(isFullCore)
+			return iCores.Size() < jCores.Size()
+		})
+
+	for _, s := range socketIDs {
+		sCores := topoDetails.CoresInSocket(s).Filter(isFullCore)
+		for _, core := range sCores.AsSlice() {
+			if numCPUs >= CPUsPerCore {
+				glog.V(4).Infof("[cpumanager] takeByTopology: claiming core [%d]", core)
+				take(topoDetails.CPUsInCore(core))
+				if numCPUs < 1 {
+					return result, nil
+				}
+			}
+		}
+	}
+
+	// 3. Acquire single threads, preferring to fill partially-allocated cores
+	//    on the same sockets as the whole cores we have already taken.
+
+	// `cpuIDs` are sorted by:
+	// - the number of available CPUs on the same core, ascending
+	// - the number of already assigned CPUs for this allocation on the
+	//   same socket, descending
+	cpuIDs := availableCPUs.AsSlice()
+	sort.Slice(cpuIDs,
+		func(i, j int) bool {
+			// Compute the number of CPUs on the same socket as i and j in
+			// the result.
+			iSocketScore := topo.CPUtopoDetails.CPUsInSocket(i).Intersection(result).Size()
+			jSocketScore := topo.CPUtopoDetails.CPUsInSocket(j).Intersection(result).Size()
+
+			// Compute the number of available CPUs on the same core as i and j.
+			iCoreScore := topoDetails.CPUsInCore(topoDetails[i].CoreID).Size()
+			jCoreScore := topoDetails.CPUsInCore(topoDetails[j].CoreID).Size()
+
+			return iSocketScore > jSocketScore || iCoreScore < jCoreScore || i < j
+		})
+
+	for _, cpu := range cpuIDs {
+		glog.V(4).Infof("[cpumanager] takeByTopology: claiming CPU [%d]", cpu)
+		if numCPUs > 0 {
+			take(cpuset.NewCPUSet(cpu))
+		}
+		if numCPUs < 1 {
+			return result, nil
+		}
+	}
+
+	return nil, fmt.Errorf("failed to allocate cpus")
+}
---
+++ pkg/kubelet/cpumanager/util_test.go
@@ -0,0 +1,96 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cpumanager
+
+import (
+	"testing"
+
+	"k8s.io/kubernetes/pkg/kubelet/cpumanager/topology"
+	"k8s.io/kubernetes/pkg/kubelet/cpuset"
+)
+
+type takeByTopologyTest struct {
+	description   string
+	topo          *topology.CPUTopology
+	availableCPUs cpuset.CPUSet
+	numCPUs       int
+	expErr        error
+	expResult     cpuset.CPUSet
+}
+
+func TestTakeByTopology(t *testing.T) {
+	testCases := []takeByTopologyTest{
+		{
+			"take zero cpus from single socket with HT",
+			topoSingleSocketHT,
+			cpuset.NewCPUSet(0, 1, 2, 3, 4, 5, 6, 7),
+			0,
+			nil,
+			cpuset.NewCPUSet(),
+		},
+		{
+			"take one cpu from single socket with HT",
+			topoSingleSocketHT,
+			cpuset.NewCPUSet(0, 1, 2, 3, 4, 5, 6, 7),
+			1,
+			nil,
+			cpuset.NewCPUSet(0),
+		},
+		{
+			"take two cpus from single socket with HT",
+			topoSingleSocketHT,
+			cpuset.NewCPUSet(0, 1, 2, 3, 4, 5, 6, 7),
+			2,
+			nil,
+			cpuset.NewCPUSet(0, 4),
+		},
+		{
+			"take all cpus from single socket with HT",
+			topoSingleSocketHT,
+			cpuset.NewCPUSet(0, 1, 2, 3, 4, 5, 6, 7),
+			8,
+			nil,
+			cpuset.NewCPUSet(0, 1, 2, 3, 4, 5, 6, 7),
+		},
+		{
+			"take two cpus from single socket with HT, only one core totally free",
+			topoSingleSocketHT,
+			cpuset.NewCPUSet(0, 1, 2, 3, 6),
+			2,
+			nil,
+			cpuset.NewCPUSet(2, 6),
+		},
+		{
+			"take a socket of cpus from dual socket with HT",
+			topoDualSocketHT,
+			cpuset.NewCPUSet(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),
+			6,
+			nil,
+			cpuset.NewCPUSet(0, 2, 4, 6, 8, 10),
+		},
+	}
+
+	for _, tc := range testCases {
+		result, err := takeByTopology(tc.topo, tc.availableCPUs, tc.numCPUs)
+		if err != tc.expErr {
+			t.Errorf("expected error to be [%v] but it was [%v] in test \"%s\"", tc.expErr, err, tc.description)
+		}
+		if !result.Equals(tc.expResult) {
+			t.Errorf("expected result [%s] to equal [%s] in test \"%s\"", result, tc.expResult, tc.description)
+		}
+	}
+}
