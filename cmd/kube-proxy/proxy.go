3196300fc247493fbbb45e380c6f27eef276efca
huzhifeng <huzhifeng@douyu.tv>
2017-08-18 16:03:25 +0800

    Run go fmt
    Signed-off-by: huzhifeng <huzhifeng@douyu.tv>

--- cmd/kube-proxy/proxy.go
+++ cmd/kube-proxy/proxy.go
@@ -42,8 +42,8 @@ func main() {
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
-	if err := command.Execute(); err != nil {		
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
