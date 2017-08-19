df3a6990690a55dfd68f45972ee2770862ffbd9f
Klaus Ma <madaxa@cn.ibm.com>
2017-08-19 10:24:50 +0800

    Revert #50362.

--- test/integration/scheduler/scheduler_test.go
+++ test/integration/scheduler/scheduler_test.go
@@ -139,8 +139,7 @@ func TestSchedulerCreationFromConfigMap(t *testing.T) {
 	// Verify that the config is applied correctly.
 	schedPredicates := sched.Config().Algorithm.Predicates()
 	schedPrioritizers := sched.Config().Algorithm.Prioritizers()
-	// Includes one mandatory predicates.
-	if len(schedPredicates) != 3 || len(schedPrioritizers) != 2 {
+	if len(schedPredicates) != 2 || len(schedPrioritizers) != 2 {
 		t.Errorf("Unexpected number of predicates or priority functions. Number of predicates: %v, number of prioritizers: %v", len(schedPredicates), len(schedPrioritizers))
 	}
 	// Check a predicate and a priority function.
