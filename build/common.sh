ec2fbbc85d2555cf9affe85e7f2f289c224da700
Di Xu <stephenhsu90@gmail.com>
2017-08-19 14:06:48 +0800

    bump new version due to base image changed

--- build/common.sh
+++ build/common.sh
@@ -85,7 +85,7 @@ readonly KUBE_CONTAINER_RSYNC_PORT=8730
 #
 # $1 - server architecture
 kube::build::get_docker_wrapped_binaries() {
-  debian_iptables_version=v7
+  debian_iptables_version=v8
   ### If you change any of these lists, please also update DOCKERIZED_BINARIES
   ### in build/BUILD.
   case $1 in
