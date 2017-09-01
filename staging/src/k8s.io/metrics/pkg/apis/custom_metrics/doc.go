48cca6b6cb2e7748a395025c6dc7038047f6c024
Solly Ross <sross@redhat.com>
2017-09-01 15:06:07 -0400

    Rename custom metrics API to custom.metrics.k8s.io
    This renames custom-metrics.metrics.k8s.io to custom.metrics.k8s.io,
    in order to reduce stutter in the API name.

--- staging/src/k8s.io/metrics/pkg/apis/custom_metrics/doc.go
+++ staging/src/k8s.io/metrics/pkg/apis/custom_metrics/doc.go
@@ -15,5 +15,5 @@ limitations under the License.
 */
 
 // +k8s:deepcopy-gen=package,register
-// +groupName=custom-metrics.metrics.k8s.io
+// +groupName=custom.metrics.k8s.io
 package custom_metrics
