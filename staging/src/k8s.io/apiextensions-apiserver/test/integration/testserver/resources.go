64be85e9dee08c133375704d66820cb9945acaf6
Lucas Käldström <lucas@weave.works>
2017-09-03 18:49:29 +0300

    Fix arm (32-bit) e2e.test compile failure

--- staging/src/k8s.io/apiextensions-apiserver/test/integration/testserver/resources.go
+++ staging/src/k8s.io/apiextensions-apiserver/test/integration/testserver/resources.go
@@ -33,6 +33,10 @@ import (
 	"k8s.io/client-go/dynamic"
 )
 
+const (
+	noxuInstanceNum int64 = 9223372036854775807
+)
+
 //NewRandomNameCustomResourceDefinition generates a CRD with random name to avoid name conflict in e2e tests
 func NewRandomNameCustomResourceDefinition(scope apiextensionsv1beta1.ResourceScope) *apiextensionsv1beta1.CustomResourceDefinition {
 	// ensure the singular doesn't end in an s for now
@@ -84,7 +88,7 @@ func NewNoxuInstance(namespace, name string) *unstructured.Unstructured {
 				"key": "value",
 			},
 			"num": map[string]interface{}{
-				"num1": 9223372036854775807,
+				"num1": noxuInstanceNum,
 				"num2": 1000000,
 			},
 		},
