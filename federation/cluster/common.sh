7a633b5519e78d17c581e4c658493d4e41bd659d
shashidharatd <shashidhara.td@huawei.com>
2017-09-18 07:18:18 +0530

    Add switch to control use of pv for etcd in federation CI jobs

--- federation/cluster/common.sh
+++ federation/cluster/common.sh
@@ -25,6 +25,7 @@ source "${KUBE_ROOT}/cluster/kube-util.sh"
 FEDERATION_NAME="${FEDERATION_NAME:-e2e-federation}"
 FEDERATION_NAMESPACE=${FEDERATION_NAMESPACE:-federation-system}
 FEDERATION_KUBE_CONTEXT="${FEDERATION_KUBE_CONTEXT:-${FEDERATION_NAME}}"
+FEDERATION_USE_PV_FOR_ETCD=${FEDERATION_USE_PV_FOR_ETCD:-false}
 HOST_CLUSTER_ZONE="${FEDERATION_HOST_CLUSTER_ZONE:-}"
 # If $HOST_CLUSTER_ZONE isn't specified, arbitrarily choose
 # last zone as the host cluster zone.
--- federation/cluster/federation-up.sh
+++ federation/cluster/federation-up.sh
@@ -30,7 +30,7 @@ KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../..
 # "${KUBE_ROOT}/cluster/lib/logging.sh" and DEFAULT_KUBECONFIG
 source "${KUBE_ROOT}/cluster/common.sh"
 # For $FEDERATION_NAME, $FEDERATION_NAMESPACE, $FEDERATION_KUBE_CONTEXT,
-# and $HOST_CLUSTER_CONTEXT.
+# $HOST_CLUSTER_CONTEXT and $FEDERATION_USE_PV_FOR_ETCD.
 source "${KUBE_ROOT}/federation/cluster/common.sh"
 
 DNS_ZONE_NAME="${FEDERATION_DNS_ZONE_NAME:-}"
@@ -118,6 +118,7 @@ function init() {
       --apiserver-enable-token-auth=true \
       --apiserver-arg-overrides="--runtime-config=api/all=true,--v=4" \
       --controllermanager-arg-overrides="--v=4" \
+      --etcd-persistent-storage=${FEDERATION_USE_PV_FOR_ETCD} \
       --v=4
 }
 
