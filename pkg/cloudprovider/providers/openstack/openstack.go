870195ad59b76cc011ae956e61a19e7ee888b84e
Richard Gonzalez <damnpoet@gmail.com>
2017-09-17 23:32:28 -0400

    Added openstack instance metadata search order

--- pkg/cloudprovider/providers/openstack/openstack.go
+++ pkg/cloudprovider/providers/openstack/openstack.go
@@ -100,13 +100,18 @@ type RouterOpts struct {
 	RouterId string `gcfg:"router-id"` // required
 }
 
+type MetadataOpts struct {
+	SearchOrder string `gcfg:"search-order"`
+}
+
 // OpenStack is an implementation of cloud provider Interface for OpenStack.
 type OpenStack struct {
-	provider  *gophercloud.ProviderClient
-	region    string
-	lbOpts    LoadBalancerOpts
-	bsOpts    BlockStorageOpts
-	routeOpts RouterOpts
+	provider     *gophercloud.ProviderClient
+	region       string
+	lbOpts       LoadBalancerOpts
+	bsOpts       BlockStorageOpts
+	routeOpts    RouterOpts
+	metadataOpts MetadataOpts
 	// InstanceID of the server where this OpenStack object is instantiated.
 	localInstanceID string
 }
@@ -128,6 +133,7 @@ type Config struct {
 	LoadBalancer LoadBalancerOpts
 	BlockStorage BlockStorageOpts
 	Route        RouterOpts
+	Metadata     MetadataOpts
 }
 
 func init() {
@@ -181,6 +187,7 @@ func readConfig(config io.Reader) (Config, error) {
 	// Set default values for config params
 	cfg.BlockStorage.BSVersion = "auto"
 	cfg.BlockStorage.TrustDevicePath = false
+	cfg.Metadata.SearchOrder = fmt.Sprintf("%s,%s", configDriveID, metadataID)
 
 	err := gcfg.ReadInto(&cfg, config)
 	return cfg, err
@@ -198,7 +205,7 @@ func (c *Caller) Call(f func()) {
 	}
 }
 
-func readInstanceID() (string, error) {
+func readInstanceID(searchOrder string) (string, error) {
 	// Try to find instance ID on the local filesystem (created by cloud-init)
 	const instanceIDFile = "/var/lib/cloud/data/instance-id"
 	idBytes, err := ioutil.ReadFile(instanceIDFile)
@@ -212,7 +219,7 @@ func readInstanceID() (string, error) {
 		// Fall through to metadata server lookup
 	}
 
-	md, err := getMetadata()
+	md, err := getMetadata(searchOrder)
 	if err != nil {
 		return "", err
 	}
@@ -275,16 +282,19 @@ func newOpenStack(cfg Config) (*OpenStack, error) {
 		err = openstack.Authenticate(provider, cfg.toAuthOptions())
 	}
 
+	err = validateMetadataSearchOrder(cfg.Metadata.SearchOrder)
+
 	if err != nil {
 		return nil, err
 	}
 
 	os := OpenStack{
-		provider:  provider,
-		region:    cfg.Global.Region,
-		lbOpts:    cfg.LoadBalancer,
-		bsOpts:    cfg.BlockStorage,
-		routeOpts: cfg.Route,
+		provider:     provider,
+		region:       cfg.Global.Region,
+		lbOpts:       cfg.LoadBalancer,
+		bsOpts:       cfg.BlockStorage,
+		routeOpts:    cfg.Route,
+		metadataOpts: cfg.Metadata,
 	}
 
 	err = checkOpenStackOpts(&os)
@@ -539,7 +549,7 @@ func (os *OpenStack) Zones() (cloudprovider.Zones, bool) {
 }
 
 func (os *OpenStack) GetZone() (cloudprovider.Zone, error) {
-	md, err := getMetadata()
+	md, err := getMetadata(os.metadataOpts.SearchOrder)
 	if err != nil {
 		return cloudprovider.Zone{}, err
 	}
@@ -741,4 +751,28 @@ func (os *OpenStack) volumeService(forceVersion string) (volumeService, error) {
 		glog.Warningf(err_txt)
 		return nil, errors.New(err_txt)
 	}
+}
+
+func validateMetadataSearchOrder(order string) error {
+	if order == "" {
+		return errors.New("Invalid value in section [Metadata] with key `search-order`. Value cannot be empty")
+	}
+
+	elements := strings.Split(order, ",")
+	if len(elements) > 2 {
+		return errors.New("Invalid value in section [Metadata] with key `search-order`. Value cannot contain more than 2 elements")
+	}
+
+	for _, o := range elements {
+		switch o {
+		case configDriveID:
+		case metadataID:
+		default:
+			errTxt := "Invalid element '%s' found in section [Metadata] with key `search-order`." +
+				"Supported elements include '%s' and '%s'"
+			return fmt.Errorf(errTxt, o, configDriveID, metadataID)
+		}
+	}
+
+	return nil
 }
