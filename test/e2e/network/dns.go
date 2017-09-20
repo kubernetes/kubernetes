44d95ece00e6469b3a0d86a7c4fe0a252645516e
xiangpengzhao <zhao.xiangpeng@zte.com.cn>
2017-09-20 11:03:09 +0800

    Change ClusterIP in dns e2e test

--- test/e2e/network/dns.go
+++ test/e2e/network/dns.go
@@ -449,7 +449,7 @@ var _ = SIGDescribe("DNS", func() {
 		By("changing the service to type=ClusterIP")
 		_, err = framework.UpdateService(f.ClientSet, f.Namespace.Name, serviceName, func(s *v1.Service) {
 			s.Spec.Type = v1.ServiceTypeClusterIP
-			s.Spec.ClusterIP = "10.0.0.123"
+			s.Spec.ClusterIP = "10.0.0.111"
 			s.Spec.Ports = []v1.ServicePort{
 				{Port: 80, Name: "http", Protocol: "TCP"},
 			}
@@ -464,6 +464,6 @@ var _ = SIGDescribe("DNS", func() {
 		By("creating a third pod to probe DNS")
 		pod3 := createDNSPod(f.Namespace.Name, wheezyProbeCmd, jessieProbeCmd, true)
 
-		validateTargetedProbeOutput(f, pod3, []string{wheezyFileName, jessieFileName}, "10.0.0.123")
+		validateTargetedProbeOutput(f, pod3, []string{wheezyFileName, jessieFileName}, "10.0.0.111")
 	})
 })
