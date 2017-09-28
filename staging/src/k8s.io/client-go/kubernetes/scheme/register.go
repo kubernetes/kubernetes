7d6e5951d6a1ff8fd9a1aea06affe337d907826f
Janet Kuo <chiachenk@google.com>
2017-09-28 14:11:34 -0700

    Autogen

--- staging/src/k8s.io/client-go/kubernetes/scheme/register.go
+++ staging/src/k8s.io/client-go/kubernetes/scheme/register.go
@@ -18,6 +18,7 @@ package scheme
 
 import (
 	admissionregistrationv1alpha1 "k8s.io/api/admissionregistration/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
 	appsv1beta1 "k8s.io/api/apps/v1beta1"
 	appsv1beta2 "k8s.io/api/apps/v1beta2"
 	authenticationv1 "k8s.io/api/authentication/v1"
@@ -74,6 +75,7 @@ func AddToScheme(scheme *runtime.Scheme) {
 	admissionregistrationv1alpha1.AddToScheme(scheme)
 	appsv1beta1.AddToScheme(scheme)
 	appsv1beta2.AddToScheme(scheme)
+	appsv1.AddToScheme(scheme)
 	authenticationv1.AddToScheme(scheme)
 	authenticationv1beta1.AddToScheme(scheme)
 	authorizationv1.AddToScheme(scheme)
