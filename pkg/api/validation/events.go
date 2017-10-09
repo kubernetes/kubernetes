a6f0cd01eee2fa2af0e43d9691c00052443c668c
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-10-09 18:37:44 +0200

    Get rid of pkg/api/util

--- pkg/api/validation/events.go
+++ pkg/api/validation/events.go
@@ -25,7 +25,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/kubernetes/pkg/api"
-	apiutil "k8s.io/kubernetes/pkg/api/util"
 )
 
 // ValidateEvent makes sure that the event makes sense.
@@ -63,12 +62,16 @@ func ValidateEvent(event *api.Event) field.ErrorList {
 
 // Check whether the kind in groupVersion is scoped at the root of the api hierarchy
 func isNamespacedKind(kind, groupVersion string) (bool, error) {
-	group := apiutil.GetGroup(groupVersion)
-	g, err := api.Registry.Group(group)
+	gv, err := schema.ParseGroupVersion(groupVersion)
 	if err != nil {
 		return false, err
 	}
-	restMapping, err := g.RESTMapper.RESTMapping(schema.GroupKind{Group: group, Kind: kind}, apiutil.GetVersion(groupVersion))
+	g, err := api.Registry.Group(gv.Group)
+	if err != nil {
+		return false, err
+	}
+
+	restMapping, err := g.RESTMapper.RESTMapping(schema.GroupKind{Group: gv.Group, Kind: kind}, gv.Version)
 	if err != nil {
 		return false, err
 	}
