20446ee7c6013f7157869f04543c835bfc08e2d9
xiangpengzhao <zhao.xiangpeng@zte.com.cn>
2017-10-12 16:25:34 +0800

    No need to do type cast for the same type

--- cmd/kube-proxy/app/server.go
+++ cmd/kube-proxy/app/server.go
@@ -410,7 +410,7 @@ func createClients(config componentconfig.ClientConnectionConfiguration, masterO
 	kubeConfig.ContentType = config.ContentType
 	kubeConfig.QPS = config.QPS
 	//TODO make config struct use int instead of int32?
-	kubeConfig.Burst = int(config.Burst)
+	kubeConfig.Burst = config.Burst
 
 	client, err := clientset.NewForConfig(kubeConfig)
 	if err != nil {
