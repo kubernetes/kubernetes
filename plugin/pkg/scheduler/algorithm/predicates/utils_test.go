606c9846130e3ce1aedb93f651816ebbb44f89d1
Ti Zhou <tizhou1986@gmail.com>
2017-10-16 19:57:02 +0800

    Revised unit test cases for predicates/utils.

--- plugin/pkg/scheduler/algorithm/predicates/utils_test.go
+++ plugin/pkg/scheduler/algorithm/predicates/utils_test.go
@@ -17,32 +17,87 @@ limitations under the License.
 package predicates
 
 import (
-	"fmt"
+	"reflect"
+	"testing"
 
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/diff"
 )
 
-// ExampleUtils is a https://blog.golang.org/examples styled unit test.
-func ExampleFindLabelsInSet() {
+func TestFindLabelsInSet(t *testing.T) {
+
+	nsPod := v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pod1",
+			Namespace: "ns1",
+			Labels: map[string]string{
+				"label1": "foo",
+				"label2": "foo",
+				"label3": "bar",
+			},
+		},
+	}
+
+	expected := map[string]string{
+		"label1": "foo",
+		"label2": "foo",
+	}
+
+	result := FindLabelsInSet([]string{"label1", "label2"}, nsPod.ObjectMeta.Labels)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Got different result than expected.\nDifference detected on:\n%s", diff.ObjectReflectDiff(expected, result))
+	}
+
+}
+
+func TestAddUnsetLabelsToMap(t *testing.T) {
+
 	labelSubset := labels.Set{}
 	labelSubset["label1"] = "value1"
 	labelSubset["label2"] = "value2"
-	// Lets make believe that these pods are on the cluster.
-	// Utility functions will inspect their labels, filter them, and so on.
+
+	nsPod := v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pod1",
+			Namespace: "ns1",
+			Labels: map[string]string{
+				"label1": "foo",
+				"label2": "foo",
+				"label3": "bar",
+			},
+		},
+	}
+
+	expected := labels.Set{
+		"label1": "value1",
+		"label2": "value2",
+		"label3": "bar",
+	}
+
+	AddUnsetLabelsToMap(labelSubset, []string{"label1", "label2", "label3"}, nsPod.ObjectMeta.Labels)
+
+	if !reflect.DeepEqual(labelSubset, expected) {
+		t.Errorf("Got different result than expected.\nDifference detected on:\n%s", diff.ObjectReflectDiff(expected, labelSubset))
+	}
+
+}
+
+func TestFilterPodsByNamespace(t *testing.T) {
+
 	nsPods := []*v1.Pod{
 		{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "pod1",
 				Namespace: "ns1",
 				Labels: map[string]string{
-					"label1": "wontSeeThis",
-					"label2": "wontSeeThis",
-					"label3": "will_see_this",
+					"label1": "foo",
+					"label2": "foo",
+					"label3": "bar",
 				},
 			},
-		}, // first pod which will be used via the utilities
+		},
 		{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "pod2",
@@ -56,15 +111,30 @@ func ExampleFindLabelsInSet() {
 			},
 		},
 	}
-	fmt.Println(FindLabelsInSet([]string{"label1", "label2", "label3"}, nsPods[0].ObjectMeta.Labels)["label3"])
-	AddUnsetLabelsToMap(labelSubset, []string{"label1", "label2", "label3"}, nsPods[0].ObjectMeta.Labels)
-	fmt.Println(labelSubset)
 
-	for _, pod := range FilterPodsByNamespace(nsPods, "ns1") {
-		fmt.Print(pod.Name, ",")
+	expected := []*v1.Pod{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "pod1",
+				Namespace: "ns1",
+				Labels: map[string]string{
+					"label1": "foo",
+					"label2": "foo",
+					"label3": "bar",
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "pod2",
+				Namespace: "ns1",
+			},
+		},
 	}
-	// Output:
-	// will_see_this
-	// label1=value1,label2=value2,label3=will_see_this
-	// pod1,pod2,
+
+	result := FilterPodsByNamespace(nsPods, "ns1")
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Got different result than expected.\nDifference detected on:\n%s", diff.ObjectReflectDiff(expected, result))
+	}
+
 }
