d6a794f444119170543e476ff04152627ff4b36f
Davanum Srinivas <davanum@gmail.com>
2017-10-18 11:46:01 -0400

    Avoid cloud provider calls for determining ExternalHost
    As part of the larger plan to drop --cloud-provider and --cloud-config
    from kube-apiserver, we need to stop calling Cloud Provider API to
    find the external ip address when one is not specified on the command
    line.
    
    When ExternalHost is not specified, we check if AdvertiseAddress is
    specified and use that, if that is missing then we use os.Hostname().
    
    When testing this feature, found a problem that when ExternalHost
    is specified, the port was not added in the generated URL. So fixed
    that as well.

--- federation/cmd/federation-apiserver/app/server.go
+++ federation/cmd/federation-apiserver/app/server.go
@@ -22,6 +22,7 @@ package app
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 	"time"
 
@@ -103,8 +104,18 @@ func NonBlockingRun(s *options.ServerRunOptions, stopCh <-chan struct{}) error {
 	if err := s.SecureServing.MaybeDefaultWithSelfSignedCerts(s.GenericServerRunOptions.AdvertiseAddress.String(), nil, nil); err != nil {
 		return fmt.Errorf("error creating self-signed certificates: %v", err)
 	}
-	if err := s.CloudProvider.DefaultExternalHost(s.GenericServerRunOptions); err != nil {
-		return fmt.Errorf("error setting the external host value: %v", err)
+	if len(s.GenericServerRunOptions.ExternalHost) == 0 {
+		if len(s.GenericServerRunOptions.AdvertiseAddress) > 0 {
+			s.GenericServerRunOptions.ExternalHost = s.GenericServerRunOptions.AdvertiseAddress.String()
+		} else {
+			if hostname, err := os.Hostname(); err == nil {
+				s.GenericServerRunOptions.ExternalHost = hostname
+			} else {
+				return fmt.Errorf("error finding host name: %v", err)
+			}
+		}
+		glog.Infof("external host was not specified, using %v when generating "+
+			"externalized URLs for this master", s.GenericServerRunOptions.ExternalHost)
 	}
 
 	s.Authentication.ApplyAuthorization(s.Authorization)
