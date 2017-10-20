a6af82789833322d47fe52be6b038eb4b4387744
Lion-Wei <liangwei14@huawei.com>
2017-10-20 17:08:43 +0800

    local up cluster in ipvs mode by add env 'KUBEPROXY_MODE=ipvs'

--- hack/local-up-cluster.sh
+++ hack/local-up-cluster.sh
@@ -57,6 +57,7 @@ EVICTION_PRESSURE_TRANSITION_PERIOD=${EVICTION_PRESSURE_TRANSITION_PERIOD:-"1m"}
 # and we don't know the IP of the DNS pod to pass in as --cluster-dns.
 # To set this up by hand, set this flag and change DNS_SERVER_IP.
 # Note also that you need API_HOST (defined above) for correct DNS.
+KUBEPROXY_MODE=${KUBEPROXY_MODE:-""}
 ENABLE_CLUSTER_DNS=${KUBE_ENABLE_CLUSTER_DNS:-true}
 DNS_SERVER_IP=${KUBE_DNS_SERVER_IP:-10.0.0.10}
 DNS_DOMAIN=${KUBE_DNS_NAME:-"cluster.local"}
@@ -112,6 +113,11 @@ if [ "${CLOUD_PROVIDER}" == "openstack" ]; then
         echo "Cloud config ${CLOUD_CONFIG} doesn't exist"
         exit 1
     fi
+fi
+
+#set feature gates if using ipvs mode
+if [ "${KUBEPROXY_MODE}" == "ipvs" ]; then
+    FEATURE_GATES="$FEATURE_GATES,SupportIPVSProxyMode=true"
 fi
 
 # warn if users are running with swap allowed
@@ -738,6 +744,16 @@ clientConnection:
 hostnameOverride: ${HOSTNAME_OVERRIDE}
 featureGates: ${FEATURE_GATES}
 EOF
+    if [ "${KUBEPROXY_MODE}" == "ipvs" ]; then
+        cat <<EOF >> /tmp/kube-proxy.yaml
+mode: ipvs
+EOF
+        sudo modprobe ip_vs
+        sudo modprobe ip_vs_rr
+        sudo modprobe ip_vs_wrr
+        sudo modprobe ip_vs_sh
+        sudo modprobe nf_conntrack_ipv4
+    fi
 
     sudo "${GO_OUT}/hyperkube" proxy \
       --config=/tmp/kube-proxy.yaml \
