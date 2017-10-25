9a217217c13dd85ecc6b6c4dca5efc705aba48bb
Davanum Srinivas <davanum@gmail.com>
2017-10-25 16:40:08 -0400

    Fix log collection for kubeadm-gce tests
    Separate out kuberenetes-anywhere provider under cluster/ but
    delegate all the functionality to the "gce" one since the code
    would be the same. Except for the name of the node, the
    NODE_INSTANCE_PREFIX will be different, so account for that.

--- cluster/log-dump/log-dump.sh
+++ cluster/log-dump/log-dump.sh
@@ -37,9 +37,9 @@ else
   readonly use_custom_instance_list=
 fi
 
-readonly master_ssh_supported_providers="gce aws"
-readonly node_ssh_supported_providers="gce gke aws"
-readonly gcloud_supported_providers="gce gke"
+readonly master_ssh_supported_providers="gce aws kubernetes-anywhere"
+readonly node_ssh_supported_providers="gce gke aws kubernetes-anywhere"
+readonly gcloud_supported_providers="gce gke kubernetes-anywhere"
 
 readonly master_logfiles="kube-apiserver kube-apiserver-audit kube-scheduler rescheduler kube-controller-manager etcd etcd-events glbc cluster-autoscaler kube-addon-manager fluentd"
 readonly node_logfiles="kube-proxy fluentd node-problem-detector"
@@ -141,7 +141,7 @@ function save-logs() {
       fi
     else
       case "${KUBERNETES_PROVIDER}" in
-        gce|gke)
+        gce|gke|kubernetes-anywhere)
           files="${files} ${gce_logfiles}"
           ;;
         aws)
