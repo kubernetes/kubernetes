997861d86635c8c9517ee290f1d79ebb85da155d
Abdul Rehman <arehmandev@outlook.com>
2017-10-31 18:06:05 +0000

    Added listing of pods and creation date to example

--- staging/src/k8s.io/client-go/examples/out-of-cluster-client-configuration/main.go
+++ staging/src/k8s.io/client-go/examples/out-of-cluster-client-configuration/main.go
@@ -62,7 +62,9 @@ func main() {
 		// Examples for error handling:
 		// - Use helper functions like e.g. errors.IsNotFound()
 		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		_, err = clientset.CoreV1().Pods("default").Get("example-xxxxx", metav1.GetOptions{})
+		namespace := "default"
+
+		pods, err = clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{})
 		if errors.IsNotFound(err) {
 			fmt.Printf("Pod not found\n")
 		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
@@ -70,7 +72,13 @@ func main() {
 		} else if err != nil {
 			panic(err.Error())
 		} else {
-			fmt.Printf("Found pod\n")
+			fmt.Printf("Found pods\n")
+
+			// Example of retrieving pod names and creationtimestamps by iterating through the Items
+			for element := range pods.Items {
+				fmt.Println("Pod name:", pods.Items[element].Name, "Created on:", pods.Items[element].CreationTimestamp)
+			}
+
 		}
 
 		time.Sleep(10 * time.Second)
@@ -83,3 +91,4 @@ func homeDir() string {
 	}
 	return os.Getenv("USERPROFILE") // windows
 }
+
