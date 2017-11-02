f4b8276001c5c895ae0f22c7f0ad1d987375072d
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-11-02 16:48:22 +0100

    Update generated code

--- staging/src/k8s.io/code-generator/_examples/apiserver/informers/externalversions/generic.go
+++ staging/src/k8s.io/code-generator/_examples/apiserver/informers/externalversions/generic.go
@@ -51,7 +51,7 @@ func (f *genericInformer) Lister() cache.GenericLister {
 // TODO extend this to unknown resources with a client pool
 func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource) (GenericInformer, error) {
 	switch resource {
-	// Group=Example, Version=V1
+	// Group=example.apiserver.code-generator.k8s.io, Version=v1
 	case v1.SchemeGroupVersion.WithResource("testtypes"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Example().V1().TestTypes().Informer()}, nil
 
