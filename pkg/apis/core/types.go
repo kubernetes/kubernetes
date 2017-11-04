80ac705ef32177fd6c5b72fc103ec51e8ea44c4a
Connor Doyle <connor.p.doyle@intel.com>
2017-11-04 00:52:15 -0700

    Removed opaque integer resources.

--- pkg/apis/core/types.go
+++ pkg/apis/core/types.go
@@ -3406,8 +3406,6 @@ const (
 )
 
 const (
-	// Namespace prefix for opaque counted resources (alpha).
-	ResourceOpaqueIntPrefix = "pod.alpha.kubernetes.io/opaque-int-resource-"
 	// Default namespace prefix.
 	ResourceDefaultNamespacePrefix = "kubernetes.io/"
 	// Name prefix for huge page resources (alpha).
