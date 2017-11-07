0a9c42525bb7e01f2afdd979727bd3b05a83833a
Michal Fojtik <mfojtik@redhat.com>
2017-11-07 10:24:04 +0100

    Fix protobuf generator for aliases to repeated types

--- staging/src/k8s.io/code-generator/cmd/go-to-protobuf/protobuf/generator.go
+++ staging/src/k8s.io/code-generator/cmd/go-to-protobuf/protobuf/generator.go
@@ -513,10 +513,13 @@ func memberTypeToProtobufField(locator ProtobufLocator, field *protoField, t *ty
 				log.Printf("failed to alias: %s %s: err %v", t.Name, t.Underlying.Name, err)
 				return err
 			}
-			if field.Extras == nil {
-				field.Extras = make(map[string]string)
+			// If this is not an alias to a slice, cast to the alias
+			if !field.Repeated {
+				if field.Extras == nil {
+					field.Extras = make(map[string]string)
+				}
+				field.Extras["(gogoproto.casttype)"] = strconv.Quote(locator.CastTypeName(t.Name))
 			}
-			field.Extras["(gogoproto.casttype)"] = strconv.Quote(locator.CastTypeName(t.Name))
 		}
 	case types.Slice:
 		if t.Elem.Name.Name == "byte" && len(t.Elem.Name.Package) == 0 {
