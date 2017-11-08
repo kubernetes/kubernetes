9a9d11bb75010f43d771bf0449d462bc153760ce
David McMahon <djmm@google.com>
2017-11-08 15:37:49 -0800

    Capture git export-subst strings in version.sh for 'git archive' use.
    Eliminate the need to update pkg/version/base.go on release branch tagging.

--- pkg/version/base.go
+++ pkg/version/base.go
@@ -51,9 +51,13 @@ var (
 	// semantic version is a git hash, but the version itself is no
 	// longer the direct output of "git describe", but a slight
 	// translation to be semver compliant.
+
+	// NOTE: The $Format strings are replaced during 'git archive' thanks to the
+	// companion .gitattributes file containing 'export-subst' in this same
+	// directory.  See also https://git-scm.com/docs/gitattributes
 	gitVersion   = "v0.0.0-master+$Format:%h$"
-	gitCommit    = "$Format:%H$"    // sha1 from git, output of $(git rev-parse HEAD)
-	gitTreeState = "not a git tree" // state of git tree, either "clean" or "dirty"
+	gitCommit    = "$Format:%H$" // sha1 from git, output of $(git rev-parse HEAD)
+	gitTreeState = ""            // state of git tree, either "clean" or "dirty"
 
 	buildDate = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
 )
