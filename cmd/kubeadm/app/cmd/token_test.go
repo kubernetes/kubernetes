7de8d545f344d85badb349367abce59c691835b5
xiangpengzhao <zhao.xiangpeng@zte.com.cn>
2017-11-10 17:32:08 +0800

    Add test case for RunCreateToken

--- cmd/kubeadm/app/cmd/token_test.go
+++ cmd/kubeadm/app/cmd/token_test.go
@@ -20,6 +20,12 @@ import (
 	"bytes"
 	"regexp"
 	"testing"
+
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
+	core "k8s.io/client-go/testing"
 )
 
 const (
@@ -42,5 +48,84 @@ func TestRunGenerateToken(t *testing.T) {
 	}
 	if !matched {
 		t.Errorf("RunGenerateToken's output did not match expected regex; wanted: [%s], got: [%s]", TokenExpectedRegex, output)
+	}
+}
+
+func TestRunCreateToken(t *testing.T) {
+	var buf bytes.Buffer
+	fakeClient := &fake.Clientset{}
+	fakeClient.AddReactor("get", "secrets", func(action core.Action) (handled bool, ret runtime.Object, err error) {
+		return true, nil, errors.NewNotFound(v1.Resource("secrets"), "foo")
+	})
+
+	testCases := []struct {
+		name          string
+		token         string
+		usages        []string
+		extraGroups   []string
+		expectedError bool
+	}{
+		{
+			name:          "valid: empty token",
+			token:         "",
+			usages:        []string{"signing", "authentication"},
+			extraGroups:   []string{"system:bootstrappers:foo"},
+			expectedError: false,
+		},
+		{
+			name:          "valid: non-empty token",
+			token:         "abcdef.1234567890123456",
+			usages:        []string{"signing", "authentication"},
+			extraGroups:   []string{"system:bootstrappers:foo"},
+			expectedError: false,
+		},
+		{
+			name:          "valid: no extraGroups",
+			token:         "abcdef.1234567890123456",
+			usages:        []string{"signing", "authentication"},
+			extraGroups:   []string{},
+			expectedError: false,
+		},
+		{
+			name:          "invalid: incorrect token",
+			token:         "123456.AABBCCDDEEFFGGHH",
+			usages:        []string{"signing", "authentication"},
+			extraGroups:   []string{},
+			expectedError: true,
+		},
+		{
+			name:          "invalid: incorrect extraGroups",
+			token:         "abcdef.1234567890123456",
+			usages:        []string{"signing", "authentication"},
+			extraGroups:   []string{"foo"},
+			expectedError: true,
+		},
+		{
+			name:          "invalid: specifying --groups when --usages doesn't include authentication",
+			token:         "abcdef.1234567890123456",
+			usages:        []string{"signing"},
+			extraGroups:   []string{"foo"},
+			expectedError: true,
+		},
+		{
+			name:          "invalid: partially incorrect usages",
+			token:         "abcdef.1234567890123456",
+			usages:        []string{"foo", "authentication"},
+			extraGroups:   []string{"system:bootstrappers:foo"},
+			expectedError: true,
+		},
+		{
+			name:          "invalid: all incorrect usages",
+			token:         "abcdef.1234567890123456",
+			usages:        []string{"foo", "bar"},
+			extraGroups:   []string{"system:bootstrappers:foo"},
+			expectedError: true,
+		},
+	}
+	for _, tc := range testCases {
+		err := RunCreateToken(&buf, fakeClient, tc.token, 0, tc.usages, tc.extraGroups, "", false, "")
+		if (err != nil) != tc.expectedError {
+			t.Errorf("Test case %s: RunCreateToken expected error: %v, saw: %v", tc.name, tc.expectedError, (err != nil))
+		}
 	}
 }
