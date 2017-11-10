6ef1a5a2efed8ac7ce53652ae8c01a4682fbc94e
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-11-10 17:08:18 +0100

    pkg/api/core: move pkg/api/pod

--- plugin/pkg/auth/authorizer/node/graph.go
+++ plugin/pkg/auth/authorizer/node/graph.go
@@ -20,8 +20,8 @@ import (
 	"sync"
 
 	pvutil "k8s.io/kubernetes/pkg/api/persistentvolume"
-	podutil "k8s.io/kubernetes/pkg/api/pod"
 	api "k8s.io/kubernetes/pkg/apis/core"
+	podutil "k8s.io/kubernetes/pkg/apis/core/helper/pod"
 	"k8s.io/kubernetes/third_party/forked/gonum/graph"
 	"k8s.io/kubernetes/third_party/forked/gonum/graph/simple"
 )
