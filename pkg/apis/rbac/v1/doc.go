f8ea46e8fe3d4162eef71502a130b910a4657b46
Maciej Szulik <maszulik@redhat.com>
2017-11-14 14:03:15 +0100

    Use full gopath for externalTypes

--- pkg/apis/rbac/v1/doc.go
+++ pkg/apis/rbac/v1/doc.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 
 // +k8s:conversion-gen=k8s.io/kubernetes/pkg/apis/rbac
-// +k8s:conversion-gen-external-types=../../../../vendor/k8s.io/api/rbac/v1
+// +k8s:conversion-gen-external-types=k8s.io/api/rbac/v1
 // +k8s:defaulter-gen=TypeMeta
 // +k8s:defaulter-gen-input=../../../../vendor/k8s.io/api/rbac/v1
 
