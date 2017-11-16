0085e2208dd85fe7945a4b8fb213f33534f8fc05
Lantao Liu <lantaol@google.com>
2017-11-16 00:41:27 +0000

    Rename log-dump env to `LOG_DUMP_SYSTEMD_SERVICES`.

--- test/e2e/framework/util.go
+++ test/e2e/framework/util.go
@@ -4573,7 +4573,7 @@ func CoreDump(dir string) {
 		Logf("Dumping logs locally to: %s", dir)
 		cmd = exec.Command(path.Join(TestContext.RepoRoot, "cluster", "log-dump", "log-dump.sh"), dir)
 	}
-	cmd.Env = append(os.Environ(), fmt.Sprintf("SYSTEMD_SERVICES=%s", parseSystemdServices(TestContext.SystemdServices)))
+	cmd.Env = append(os.Environ(), fmt.Sprintf("LOG_DUMP_SYSTEMD_SERVICES=%s", parseSystemdServices(TestContext.SystemdServices)))
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
