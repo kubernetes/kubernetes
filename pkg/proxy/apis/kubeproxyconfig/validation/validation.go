200c95b43ef9975102620bb7e9b3d51b91138db0
Tim Hockin <thockin@google.com>
2017-11-19 00:28:05 -0800

    Add util func for validate.Positive()

--- pkg/proxy/apis/kubeproxyconfig/validation/validation.go
+++ pkg/proxy/apis/kubeproxyconfig/validation/validation.go
@@ -48,13 +48,8 @@ func Validate(config *kubeproxyconfig.KubeProxyConfiguration) field.ErrorList {
 		allErrs = append(allErrs, field.Invalid(newPath.Child("OOMScoreAdj"), *config.OOMScoreAdj, "must be within the range [-1000, 1000]"))
 	}
 
-	if config.UDPIdleTimeout.Duration <= 0 {
-		allErrs = append(allErrs, field.Invalid(newPath.Child("UDPIdleTimeout"), config.UDPIdleTimeout, "must be greater than 0"))
-	}
-
-	if config.ConfigSyncPeriod.Duration <= 0 {
-		allErrs = append(allErrs, field.Invalid(newPath.Child("ConfigSyncPeriod"), config.ConfigSyncPeriod, "must be greater than 0"))
-	}
+	allErrs = append(allErrs, validate.Positive(int64(config.UDPIdleTimeout.Duration), newPath.Child("UDPIdleTimeout"))...)
+	allErrs = append(allErrs, validate.Positive(int64(config.ConfigSyncPeriod.Duration), newPath.Child("ConfigSyncPeriod"))...)
 
 	if net.ParseIP(config.BindAddress) == nil {
 		allErrs = append(allErrs, field.Invalid(newPath.Child("BindAddress"), config.BindAddress, "not a valid textual representation of an IP address"))
@@ -85,9 +80,7 @@ func validateKubeProxyIPTablesConfiguration(config kubeproxyconfig.KubeProxyIPTa
 		allErrs = append(allErrs, field.Invalid(fldPath.Child("MasqueradeBit"), config.MasqueradeBit, "must be within the range [0, 31]"))
 	}
 
-	if config.SyncPeriod.Duration <= 0 {
-		allErrs = append(allErrs, field.Invalid(fldPath.Child("SyncPeriod"), config.SyncPeriod, "must be greater than 0"))
-	}
+	allErrs = append(allErrs, validate.Positive(int64(config.SyncPeriod.Duration), fldPath.Child("SyncPeriod"))...)
 
 	allErrs = append(allErrs, validate.NonNegativeDuration(config.MinSyncPeriod.Duration, fldPath.Child("MinSyncPeriod"))...)
 
@@ -101,9 +94,7 @@ func validateKubeProxyIPTablesConfiguration(config kubeproxyconfig.KubeProxyIPTa
 func validateKubeProxyIPVSConfiguration(config kubeproxyconfig.KubeProxyIPVSConfiguration, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
 
-	if config.SyncPeriod.Duration <= 0 {
-		allErrs = append(allErrs, field.Invalid(fldPath.Child("SyncPeriod"), config.SyncPeriod, "must be greater than 0"))
-	}
+	allErrs = append(allErrs, validate.Positive(int64(config.SyncPeriod.Duration), fldPath.Child("SyncPeriod"))...)
 
 	allErrs = append(allErrs, validate.NonNegativeDuration(config.MinSyncPeriod.Duration, fldPath.Child("MinSyncPeriod"))...)
 
