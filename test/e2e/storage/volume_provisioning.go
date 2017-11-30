d80dbe7fea5f15ab68f348e26b5e74c25f21b000
edisonxiang <xiang.edison@gmail.com>
2017-11-30 09:28:13 +0800

    Fix GCE CreateVolume allocates in chunks of GiB incorrectly

--- test/e2e/storage/volume_provisioning.go
+++ test/e2e/storage/volume_provisioning.go
@@ -256,8 +256,8 @@ var _ = SIGDescribe("Dynamic Provisioning", func() {
 						"type": "pd-ssd",
 						"zone": cloudZone,
 					},
-					"1.5Gi",
-					"2Gi",
+					"1.5G",
+					"2G",
 					func(volume *v1.PersistentVolume) error {
 						return checkGCEPD(volume, "pd-ssd")
 					},
@@ -269,8 +269,8 @@ var _ = SIGDescribe("Dynamic Provisioning", func() {
 					map[string]string{
 						"type": "pd-standard",
 					},
-					"1.5Gi",
-					"2Gi",
+					"1.5G",
+					"2G",
 					func(volume *v1.PersistentVolume) error {
 						return checkGCEPD(volume, "pd-standard")
 					},
@@ -435,8 +435,8 @@ var _ = SIGDescribe("Dynamic Provisioning", func() {
 				map[string]string{
 					"type": "pd-standard",
 				},
-				"1Gi",
-				"1Gi",
+				"1G",
+				"1G",
 				func(volume *v1.PersistentVolume) error {
 					return checkGCEPD(volume, "pd-standard")
 				},
@@ -469,8 +469,8 @@ var _ = SIGDescribe("Dynamic Provisioning", func() {
 				map[string]string{
 					"type": "pd-standard",
 				},
-				"1Gi",
-				"1Gi",
+				"1G",
+				"1G",
 				func(volume *v1.PersistentVolume) error {
 					return checkGCEPD(volume, "pd-standard")
 				},
@@ -520,7 +520,7 @@ var _ = SIGDescribe("Dynamic Provisioning", func() {
 				name:        "unmanaged_zone",
 				provisioner: "kubernetes.io/gce-pd",
 				parameters:  map[string]string{"zone": unmanagedZone},
-				claimSize:   "1Gi",
+				claimSize:   "1G",
 			}
 			sc := newStorageClass(test, ns, suffix)
 			sc, err = c.StorageV1().StorageClasses().Create(sc)
@@ -640,6 +640,14 @@ var _ = SIGDescribe("Dynamic Provisioning", func() {
 				claimSize:    "2Gi",
 				expectedSize: "2Gi",
 			}
+			// gce or gke
+			if getDefaultPluginName() == "kubernetes.io/gce-pd" {
+				// using GB not GiB as e2e test unit since gce-pd returns GB,
+				// or expectedSize may be greater than claimSize.
+				test.claimSize = "2G"
+				test.expectedSize = "2G"
+			}
+
 			claim := newClaim(test, ns, "default")
 			testDynamicProvisioning(test, c, claim, nil)
 		})
