f2405cf2942739996af2bb76347c2cb0641153aa
Maxim Ivanov <ivanov.maxim@gmail.com>
2017-12-01 11:09:57 +0000

    Disable session affinity for internal kuberntes service
    Under following conditions session affinity leads to a deadlock:
      - Self hosted controller-manager, where it talks to API servers
        via kubernetes service ClusterIP
      - default master-count reconcilier is used
      - --apiserver-count is set to >1 according to the help message
      - number of responsive APIServers goes below `apiserver-count`
      - all controller-managers happen to be hashed to APIServers which
        are down.
    
    What then happens is that controller managers never be able to
    contact APIServer, despite correctly working APIServer available.
    
    Less serious outages also possible for other consumers of kubernetes
    service, such as operators, kube-dns, flannel & calico, etc.  There is
    always non zero chance, that given consumer is hashed  to an apiserver
    which is down.
    
    Revert "give the kubernetes service client ip session affinity"
    This reverts commit e21ebbcac404bb6ed2b6caaf7d067a966bed2683.

--- pkg/master/controller.go
+++ pkg/master/controller.go
@@ -274,7 +274,7 @@ func (c *Controller) CreateOrUpdateMasterServiceIfNeeded(serviceName string, ser
 			// maintained by this code, not by the pod selector
 			Selector:        nil,
 			ClusterIP:       serviceIP.String(),
-			SessionAffinity: api.ServiceAffinityClientIP,
+			SessionAffinity: api.ServiceAffinityNone,
 			Type:            serviceType,
 		},
 	}
