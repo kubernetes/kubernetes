208df08ea6648b295bb394ef7a14ad0a442f6672
Cao Shufeng <caosf.fnst@cn.fujitsu.com>
2017-12-11 16:42:31 +0800

    remove useless validation from pod's resourcequota admission
    ResourceQuota is a validating admission plugin. Before it runs, pods
    has already been validated. It's not necessary to validate it again.

--- pkg/quota/evaluator/core/pods_test.go
+++ pkg/quota/evaluator/core/pods_test.go
@@ -36,32 +36,6 @@ func TestPodConstraintsFunc(t *testing.T) {
 		required []api.ResourceName
 		err      string
 	}{
-		"init container resource invalid": {
-			pod: &api.Pod{
-				Spec: api.PodSpec{
-					InitContainers: []api.Container{{
-						Resources: api.ResourceRequirements{
-							Requests: api.ResourceList{api.ResourceCPU: resource.MustParse("2m")},
-							Limits:   api.ResourceList{api.ResourceCPU: resource.MustParse("1m")},
-						},
-					}},
-				},
-			},
-			err: `spec.initContainers[0].resources.requests: Invalid value: "2m": must be less than or equal to cpu limit`,
-		},
-		"container resource invalid": {
-			pod: &api.Pod{
-				Spec: api.PodSpec{
-					Containers: []api.Container{{
-						Resources: api.ResourceRequirements{
-							Requests: api.ResourceList{api.ResourceCPU: resource.MustParse("2m")},
-							Limits:   api.ResourceList{api.ResourceCPU: resource.MustParse("1m")},
-						},
-					}},
-				},
-			},
-			err: `spec.containers[0].resources.requests: Invalid value: "2m": must be less than or equal to cpu limit`,
-		},
 		"init container resource missing": {
 			pod: &api.Pod{
 				Spec: api.PodSpec{
