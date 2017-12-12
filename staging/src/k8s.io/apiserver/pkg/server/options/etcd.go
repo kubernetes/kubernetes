7c4c321c9899ebe0c8cb21477f47d9f9743ddf19
Yu Liao <yliao@google.com>
2017-12-12 12:55:20 -0800

    added defaults for --watch-cache-sizes description.

--- staging/src/k8s.io/apiserver/pkg/server/options/etcd.go
+++ staging/src/k8s.io/apiserver/pkg/server/options/etcd.go
@@ -120,7 +120,8 @@ func (s *EtcdOptions) AddFlags(fs *pflag.FlagSet) {
 	fs.StringSliceVar(&s.WatchCacheSizes, "watch-cache-sizes", s.WatchCacheSizes, ""+
 		"List of watch cache sizes for every resource (pods, nodes, etc.), comma separated. "+
 		"The individual override format: resource#size, where size is a number. It takes effect "+
-		"when watch-cache is enabled.")
+		"when watch-cache is enabled. Some resources (replicationcontrollers, endpoints, nodes, pods, services, apiservices) "+
+		"have system defaults set by heuristics, others default to default-watch-cache-size")
 
 	fs.StringVar(&s.StorageConfig.Type, "storage-backend", s.StorageConfig.Type,
 		"The storage backend for persistence. Options: 'etcd3' (default), 'etcd2'.")
