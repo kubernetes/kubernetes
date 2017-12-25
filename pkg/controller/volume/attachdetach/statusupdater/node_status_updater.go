646eb152f53bf2c8fa957f988342835cb323c35e
zouyee <zouyee1989@gmail.com>
2017-12-25 23:39:37 +0800

    move function to node util

--- pkg/controller/volume/attachdetach/statusupdater/node_status_updater.go
+++ pkg/controller/volume/attachdetach/statusupdater/node_status_updater.go
@@ -19,18 +19,15 @@ limitations under the License.
 package statusupdater
 
 import (
-	"encoding/json"
-	"fmt"
-
 	"github.com/golang/glog"
 
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/strategicpatch"
 	clientset "k8s.io/client-go/kubernetes"
 	corelisters "k8s.io/client-go/listers/core/v1"
 	"k8s.io/kubernetes/pkg/controller/volume/attachdetach/cache"
+	"k8s.io/kubernetes/pkg/util/node"
 )
 
 // NodeStatusUpdater defines a set of operations for updating the
@@ -99,48 +96,6 @@ func (nsu *nodeStatusUpdater) UpdateNodeStatuses() error {
 }
 
 func (nsu *nodeStatusUpdater) updateNodeStatus(nodeName types.NodeName, nodeObj *v1.Node, attachedVolumes []v1.AttachedVolume) error {
-	node := nodeObj.DeepCopy()
-
-	// TODO: Change to pkg/util/node.UpdateNodeStatus.
-	oldData, err := json.Marshal(node)
-	if err != nil {
-		return fmt.Errorf(
-			"failed to Marshal oldData for node %q. %v",
-			nodeName,
-			err)
-	}
-
-	node.Status.VolumesAttached = attachedVolumes
-
-	newData, err := json.Marshal(node)
-	if err != nil {
-		return fmt.Errorf(
-			"failed to Marshal newData for node %q. %v",
-			nodeName,
-			err)
-	}
-
-	patchBytes, err :=
-		strategicpatch.CreateTwoWayMergePatch(oldData, newData, node)
-	if err != nil {
-		return fmt.Errorf(
-			"failed to CreateTwoWayMergePatch for node %q. %v",
-			nodeName,
-			err)
-	}
-
-	_, err = nsu.kubeClient.CoreV1().Nodes().PatchStatus(string(nodeName), patchBytes)
-	if err != nil {
-		return fmt.Errorf(
-			"failed to kubeClient.CoreV1().Nodes().Patch for node %q. %v",
-			nodeName,
-			err)
-	}
-	glog.V(4).Infof(
-		"Updating status for node %q succeeded. patchBytes: %q VolumesAttached: %v",
-		nodeName,
-		string(patchBytes),
-		node.Status.VolumesAttached)
-
-	return nil
+	nodeCopy := nodeObj.DeepCopy()
+	return node.UpdateNodeStatus(nsu.kubeClient.CoreV1(), nodeName, nodeCopy, attachedVolumes)
 }
