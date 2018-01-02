e9545701825694f0a5ea84629c1a6da8160d6651
Manjunath A Kumatagi <mkumatag@in.ibm.com>
2018-01-02 03:38:35 -0500

    Add 2018 to boilerplate.py

--- hack/boilerplate/boilerplate.py
+++ hack/boilerplate/boilerplate.py
@@ -176,7 +176,7 @@ def get_regexs():
     # Search for "YEAR" which exists in the boilerplate, but shouldn't in the real thing
     regexs["year"] = re.compile( 'YEAR' )
     # dates can be 2014, 2015, 2016, or 2017; company holder names can be anything
-    regexs["date"] = re.compile( '(2014|2015|2016|2017)' )
+    regexs["date"] = re.compile( '(2014|2015|2016|2017|2018)' )
     # strip // +build \n\n build constraints
     regexs["go_build_constraints"] = re.compile(r"^(// \+build.*\n)+\n", re.MULTILINE)
     # strip #!.* from shell scripts
