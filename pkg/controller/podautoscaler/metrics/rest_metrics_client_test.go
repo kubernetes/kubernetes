eb688e098f5abd095584d84a50dba1beeea97db5
mattjmcnaughton <mattjmcnaughton@gmail.com>
2018-01-05 08:40:24 -0500

    Add RESTClient Custom metrics empty test
    Add testing for a previously untested path, which is tested when getting
    resource metrics.

--- pkg/controller/podautoscaler/metrics/rest_metrics_client_test.go
+++ pkg/controller/podautoscaler/metrics/rest_metrics_client_test.go
@@ -249,6 +249,16 @@ func TestRESTClientQpsSumEqualZero(t *testing.T) {
 	tc.runTest(t)
 }
 
+func TestRESTClientQpsEmptyMetrics(t *testing.T) {
+	tc := restClientTestCase{
+		metricName:           "qps",
+		desiredError:         fmt.Errorf("no metrics returned from custom metrics API"),
+		reportedMetricPoints: []metricPoint{},
+	}
+
+	tc.runTest(t)
+}
+
 func TestRESTClientCPUEmptyMetrics(t *testing.T) {
 	tc := restClientTestCase{
 		resourceName:         v1.ResourceCPU,
