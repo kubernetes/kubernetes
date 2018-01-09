0ffb70c12cf104a90339b8b912741220f3c2671d
xiangpengzhao <zhao.xiangpeng@zte.com.cn>
2018-01-09 14:03:45 +0800

    Update kube-proxy configuration API.

--- pkg/proxy/apis/kubeproxyconfig/types.go
+++ pkg/proxy/apis/kubeproxyconfig/types.go
@@ -26,32 +26,32 @@ import (
 
 // ClientConnectionConfiguration contains details for constructing a client.
 type ClientConnectionConfiguration struct {
-	// kubeConfigFile is the path to a kubeconfig file.
+	// KubeConfigFile is the path to a kubeconfig file.
 	KubeConfigFile string
-	// acceptContentTypes defines the Accept header sent by clients when connecting to a server, overriding the
+	// AcceptContentTypes defines the Accept header sent by clients when connecting to a server, overriding the
 	// default value of 'application/json'. This field will control all connections to the server used by a particular
 	// client.
 	AcceptContentTypes string
-	// contentType is the content type used when sending data to the server from this client.
+	// ContentType is the content type used when sending data to the server from this client.
 	ContentType string
-	// qps controls the number of queries per second allowed for this connection.
+	// QPS controls the number of queries per second allowed for this connection.
 	QPS float32
-	// burst allows extra queries to accumulate when a client is exceeding its rate.
+	// Burst allows extra queries to accumulate when a client is exceeding its rate.
 	Burst int32
 }
 
 // KubeProxyIPTablesConfiguration contains iptables-related configuration
 // details for the Kubernetes proxy server.
 type KubeProxyIPTablesConfiguration struct {
-	// masqueradeBit is the bit of the iptables fwmark space to use for SNAT if using
+	// MasqueradeBit is the bit of the iptables fwmark space to use for SNAT if using
 	// the pure iptables proxy mode. Values must be within the range [0, 31].
 	MasqueradeBit *int32
-	// masqueradeAll tells kube-proxy to SNAT everything if using the pure iptables proxy mode.
+	// MasqueradeAll tells kube-proxy to SNAT everything if using the pure iptables proxy mode.
 	MasqueradeAll bool
-	// syncPeriod is the period that iptables rules are refreshed (e.g. '5s', '1m',
+	// SyncPeriod is the period that iptables rules are refreshed (e.g. '5s', '1m',
 	// '2h22m').  Must be greater than 0.
 	SyncPeriod metav1.Duration
-	// minSyncPeriod is the minimum period that iptables rules are refreshed (e.g. '5s', '1m',
+	// MinSyncPeriod is the minimum period that iptables rules are refreshed (e.g. '5s', '1m',
 	// '2h22m').
 	MinSyncPeriod metav1.Duration
 }
@@ -59,32 +59,32 @@ type KubeProxyIPTablesConfiguration struct {
 // KubeProxyIPVSConfiguration contains ipvs-related configuration
 // details for the Kubernetes proxy server.
 type KubeProxyIPVSConfiguration struct {
-	// syncPeriod is the period that ipvs rules are refreshed (e.g. '5s', '1m',
+	// SyncPeriod is the period that ipvs rules are refreshed (e.g. '5s', '1m',
 	// '2h22m').  Must be greater than 0.
 	SyncPeriod metav1.Duration
-	// minSyncPeriod is the minimum period that ipvs rules are refreshed (e.g. '5s', '1m',
+	// MinSyncPeriod is the minimum period that ipvs rules are refreshed (e.g. '5s', '1m',
 	// '2h22m').
 	MinSyncPeriod metav1.Duration
-	// ipvs scheduler
+	// Scheduler is an ipvs scheduler
 	Scheduler string
 }
 
 // KubeProxyConntrackConfiguration contains conntrack settings for
 // the Kubernetes proxy server.
 type KubeProxyConntrackConfiguration struct {
-	// max is the maximum number of NAT connections to track (0 to
+	// Max is the maximum number of NAT connections to track (0 to
 	// leave as-is).  This takes precedence over maxPerCore and min.
 	Max *int32
-	// maxPerCore is the maximum number of NAT connections to track
+	// MaxPerCore is the maximum number of NAT connections to track
 	// per CPU core (0 to leave the limit as-is and ignore min).
 	MaxPerCore *int32
-	// min is the minimum value of connect-tracking records to allocate,
+	// Min is the minimum value of connect-tracking records to allocate,
 	// regardless of maxPerCore (set maxPerCore=0 to leave the limit as-is).
 	Min *int32
-	// tcpEstablishedTimeout is how long an idle TCP connection will be kept open
+	// TCPEstablishedTimeout is how long an idle TCP connection will be kept open
 	// (e.g. '2s').  Must be greater than 0 to set.
 	TCPEstablishedTimeout *metav1.Duration
-	// tcpCloseWaitTimeout is how long an idle conntrack entry
+	// TCPCloseWaitTimeout is how long an idle conntrack entry
 	// in CLOSE_WAIT state will remain in the conntrack
 	// table. (e.g. '60s'). Must be greater than 0 to set.
 	TCPCloseWaitTimeout *metav1.Duration
@@ -97,7 +97,7 @@ type KubeProxyConntrackConfiguration struct {
 type KubeProxyConfiguration struct {
 	metav1.TypeMeta
 
-	// featureGates is a comma-separated list of key=value pairs that control
+	// FeatureGates is a comma-separated list of key=value pairs that control
 	// which alpha/beta features are enabled.
 	//
 	// TODO this really should be a map but that requires refactoring all
@@ -106,48 +106,48 @@ type KubeProxyConfiguration struct {
 	// pairs.
 	FeatureGates string
 
-	// bindAddress is the IP address for the proxy server to serve on (set to 0.0.0.0
+	// BindAddress is the IP address for the proxy server to serve on (set to 0.0.0.0
 	// for all interfaces)
 	BindAddress string
-	// healthzBindAddress is the IP address and port for the health check server to serve on,
+	// HealthzBindAddress is the IP address and port for the health check server to serve on,
 	// defaulting to 0.0.0.0:10256
 	HealthzBindAddress string
-	// metricsBindAddress is the IP address and port for the metrics server to serve on,
+	// MetricsBindAddress is the IP address and port for the metrics server to serve on,
 	// defaulting to 127.0.0.1:10249 (set to 0.0.0.0 for all interfaces)
 	MetricsBindAddress string
-	// enableProfiling enables profiling via web interface on /debug/pprof handler.
+	// EnableProfiling enables profiling via web interface on /debug/pprof handler.
 	// Profiling handlers will be handled by metrics server.
 	EnableProfiling bool
-	// clusterCIDR is the CIDR range of the pods in the cluster. It is used to
+	// ClusterCIDR is the CIDR range of the pods in the cluster. It is used to
 	// bridge traffic coming from outside of the cluster. If not provided,
 	// no off-cluster bridging will be performed.
 	ClusterCIDR string
-	// hostnameOverride, if non-empty, will be used as the identity instead of the actual hostname.
+	// HostnameOverride, if non-empty, will be used as the identity instead of the actual hostname.
 	HostnameOverride string
-	// clientConnection specifies the kubeconfig file and client connection settings for the proxy
+	// ClientConnection specifies the kubeconfig file and client connection settings for the proxy
 	// server to use when communicating with the apiserver.
 	ClientConnection ClientConnectionConfiguration
-	// iptables contains iptables-related configuration options.
+	// IPTables contains iptables-related configuration options.
 	IPTables KubeProxyIPTablesConfiguration
-	// ipvs contains ipvs-related configuration options.
+	// IPVS contains ipvs-related configuration options.
 	IPVS KubeProxyIPVSConfiguration
-	// oomScoreAdj is the oom-score-adj value for kube-proxy process. Values must be within
+	// OOMScoreAdj is the oom-score-adj value for kube-proxy process. Values must be within
 	// the range [-1000, 1000]
 	OOMScoreAdj *int32
-	// mode specifies which proxy mode to use.
+	// Mode specifies which proxy mode to use.
 	Mode ProxyMode
-	// portRange is the range of host ports (beginPort-endPort, inclusive) that may be consumed
+	// PortRange is the range of host ports (beginPort-endPort, inclusive) that may be consumed
 	// in order to proxy service traffic. If unspecified (0-0) then ports will be randomly chosen.
 	PortRange string
-	// resourceContainer is the absolute name of the resource-only container to create and run
+	// ResourceContainer is the absolute name of the resource-only container to create and run
 	// the Kube-proxy in (Default: /kube-proxy).
 	ResourceContainer string
-	// udpIdleTimeout is how long an idle UDP connection will be kept open (e.g. '250ms', '2s').
+	// UDPIdleTimeout is how long an idle UDP connection will be kept open (e.g. '250ms', '2s').
 	// Must be greater than 0. Only applicable for proxyMode=userspace.
 	UDPIdleTimeout metav1.Duration
-	// conntrack contains conntrack-related configuration options.
+	// Conntrack contains conntrack-related configuration options.
 	Conntrack KubeProxyConntrackConfiguration
-	// configSyncPeriod is how often configuration from the apiserver is refreshed. Must be greater
+	// ConfigSyncPeriod is how often configuration from the apiserver is refreshed. Must be greater
 	// than 0.
 	ConfigSyncPeriod metav1.Duration
 }
@@ -169,7 +169,7 @@ const (
 )
 
 // IPVSSchedulerMethod is the algorithm for allocating TCP connections and
-// UDP datagrams to real servers.  Scheduling algorithms are imple-
+// UDP datagrams to real servers. Scheduling algorithms are imple-
 //wanted as kernel modules. Ten are shipped with the Linux Virtual Server.
 type IPVSSchedulerMethod string
 
