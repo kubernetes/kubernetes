9493f5fceb05505b2877bf41282d389b7be31fe2
Bobby (Babak) Salamat <bsalamat@google.com>
2018-01-18 01:04:32 -0800

    Update PriorityClassName API doc

--- staging/src/k8s.io/api/core/v1/types.go
+++ staging/src/k8s.io/api/core/v1/types.go
@@ -2879,9 +2879,10 @@ type PodSpec struct {
 	// +patchMergeKey=ip
 	// +patchStrategy=merge
 	HostAliases []HostAlias `json:"hostAliases,omitempty" patchStrategy:"merge" patchMergeKey:"ip" protobuf:"bytes,23,rep,name=hostAliases"`
-	// If specified, indicates the pod's priority. "SYSTEM" is a special keyword
-	// which indicates the highest priority. Any other name must be defined by
-	// creating a PriorityClass object with that name.
+	// If specified, indicates the pod's priority. "system-node-critical" and
+	// "system-cluster-critical" are two special keywords which indicate the
+	// highest priorities with the former being the highest priority. Any other
+	// name must be defined by creating a PriorityClass object with that name.
 	// If not specified, the pod priority will be default or zero if there is no
 	// default.
 	// +optional
