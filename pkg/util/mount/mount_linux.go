9aae15a41b826a8182c3a07e80e3df9c7372ef6b
yue9944882 <291271447@qq.com>
2018-01-18 13:52:47 +0800

    bugfix(mount): lstat with abs path of parent instead of '/..'

--- pkg/util/mount/mount_linux.go
+++ pkg/util/mount/mount_linux.go
@@ -271,7 +271,7 @@ func (mounter *Mounter) IsLikelyNotMountPoint(file string) (bool, error) {
 	if err != nil {
 		return true, err
 	}
-	rootStat, err := os.Lstat(file + "/..")
+	rootStat, err := os.Lstat(filepath.Dir(strings.TrimSuffix(file, "/")))
 	if err != nil {
 		return true, err
 	}
