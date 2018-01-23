5f60c1b07bc1dd458184a7f37b180845af74ed36
李剑锋 <lijianfeng@lijianfengdeMacBook-Pro.local>
2018-01-23 14:53:51 +0800

    test_test

--- cmd/kube-scheduler/scheduler.go
+++ cmd/kube-scheduler/scheduler.go
@@ -21,7 +21,7 @@ import (
 	"math/rand"
 	"os"
 	"time"
-
+	"fmt"
 	"github.com/spf13/pflag"
 	utilflag "k8s.io/apiserver/pkg/util/flag"
 	"k8s.io/apiserver/pkg/util/logs"
@@ -46,5 +46,6 @@ func main() {
 
 	if err := command.Execute(); err != nil {
 		os.Exit(1)
+		fmt.Println("test_test")
 	}
 }
