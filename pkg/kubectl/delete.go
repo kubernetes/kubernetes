4a74f98a0a6b472548186a7d51050c77365b2dfc
Shyam JVS <shyamjvs@google.com>
2018-01-30 15:39:02 +0100

    Revert "removes the remainder from ScalerFor method"

--- pkg/kubectl/delete.go
+++ pkg/kubectl/delete.go
@@ -347,7 +347,7 @@ func (reaper *StatefulSetReaper) Stop(namespace, name string, timeout time.Durat
 func (reaper *JobReaper) Stop(namespace, name string, timeout time.Duration, gracePeriod *metav1.DeleteOptions) error {
 	jobs := reaper.client.Jobs(namespace)
 	pods := reaper.podClient.Pods(namespace)
-	scaler := ScalerFor(schema.GroupKind{Group: batch.GroupName, Kind: "Job"}, reaper.client, nil, schema.GroupResource{})
+	scaler := &JobScaler{reaper.client}
 	job, err := jobs.Get(name, metav1.GetOptions{})
 	if err != nil {
 		return err
