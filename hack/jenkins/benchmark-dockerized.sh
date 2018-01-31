9ddaeae2c62941defcc132668067582b9a403caf
Shyam Jeedigunta <shyamjvs@google.com>
2018-01-31 13:34:12 +0100

    Disable JUnit-style reporting for benchmark script

--- hack/jenkins/benchmark-dockerized.sh
+++ hack/jenkins/benchmark-dockerized.sh
@@ -26,23 +26,20 @@ retry() {
   "$@"
 }
 
-# Runs benchmark integration tests, producing JUnit-style XML test
-# reports in ${WORKSPACE}/artifacts. This script is intended to be run from
+# Runs benchmark integration tests, producing pretty-printed results
+# in ${WORKSPACE}/artifacts. This script can also be run within a
 # kubekins-test container with a kubernetes repo mounted (at the path
-# /go/src/k8s.io/kubernetes). See k8s.io/test-infra/scenarios/kubernetes_verify.py.
+# /go/src/k8s.io/kubernetes).
 
 export PATH=${GOPATH}/bin:${PWD}/third_party/etcd:/usr/local/go/bin:${PATH}
 
-retry go get github.com/jstemmer/go-junit-report
 retry go get github.com/cespare/prettybench
 
 # Disable the Go race detector.
 export KUBE_RACE=" "
 # Disable coverage report
 export KUBE_COVER="n"
-# Produce a JUnit-style XML test report.
-export KUBE_JUNIT_REPORT_DIR=${WORKSPACE}/artifacts
-export ARTIFACTS_DIR=${WORKSPACE}/artifacts
+export ARTIFACTS_DIR=${WORKSPACE}/_artifacts
 
 mkdir -p "${ARTIFACTS_DIR}"
 cd /go/src/k8s.io/kubernetes
