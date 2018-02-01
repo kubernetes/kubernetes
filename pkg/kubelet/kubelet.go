49f1a2fcaff63801409dd8b93979666f357446dc
Yecheng Fu <fuyecheng@qiniu.com>
2018-02-01 13:45:35 +0800

    Fix kubelet PVC metrics using a volume stats collector.
    Volumes on each node changes, we should not only add PVC metrics into
    gauge vector. It's better use a collector to collector metrics from
    stats.

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -81,6 +81,7 @@ import (
 	"k8s.io/kubernetes/pkg/kubelet/kuberuntime"
 	"k8s.io/kubernetes/pkg/kubelet/lifecycle"
 	"k8s.io/kubernetes/pkg/kubelet/metrics"
+	"k8s.io/kubernetes/pkg/kubelet/metrics/collectors"
 	"k8s.io/kubernetes/pkg/kubelet/network"
 	"k8s.io/kubernetes/pkg/kubelet/pleg"
 	kubepod "k8s.io/kubernetes/pkg/kubelet/pod"
@@ -1250,7 +1251,7 @@ func (kl *Kubelet) StartGarbageCollection() {
 // Note that the modules here must not depend on modules that are not initialized here.
 func (kl *Kubelet) initializeModules() error {
 	// Prometheus metrics.
-	metrics.Register(kl.runtimeCache)
+	metrics.Register(kl.runtimeCache, collectors.NewVolumeStatsCollector(kl))
 
 	// Setup filesystem directories.
 	if err := kl.setupDataDirs(); err != nil {
