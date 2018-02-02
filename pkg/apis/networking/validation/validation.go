621d0609916c1cbbea64fc7da62e25a35277c434
hzxuzhonghu <xuzhonghu@huawei.com>
2018-02-02 14:30:13 +0800

    add pkg/registry/networking golint ignore

--- pkg/apis/networking/validation/validation.go
+++ pkg/apis/networking/validation/validation.go
@@ -319,11 +319,12 @@ func validateIngressBackend(backend *networking.IngressBackend, fldPath *field.P
 	// All backends must reference a single local service by name, and a single service port by name or number.
 	if len(backend.ServiceName) == 0 {
 		return append(allErrs, field.Required(fldPath.Child("serviceName"), ""))
-	} else {
-		for _, msg := range apivalidation.ValidateServiceName(backend.ServiceName, false) {
-			allErrs = append(allErrs, field.Invalid(fldPath.Child("serviceName"), backend.ServiceName, msg))
-		}
 	}
+
+	for _, msg := range apivalidation.ValidateServiceName(backend.ServiceName, false) {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("serviceName"), backend.ServiceName, msg))
+	}
+
 	allErrs = append(allErrs, apivalidation.ValidatePortNumOrName(backend.ServicePort, fldPath.Child("servicePort"))...)
 	return allErrs
 }
