26d3916b4a969d615293897d6ae6fbf68fc25d48
Joe Betz <jpbetz@google.com>
2018-02-13 13:53:29 -0800

    Bump etcd server patch version to 3.2.16

--- cmd/kubeadm/app/constants/constants_test.go
+++ cmd/kubeadm/app/constants/constants_test.go
@@ -137,12 +137,12 @@ func TestEtcdSupportedVersion(t *testing.T) {
 		},
 		{
 			kubernetesVersion: "1.10.0",
-			expectedVersion:   version.MustParseSemantic("3.2.14"),
+			expectedVersion:   version.MustParseSemantic("3.2.16"),
 			expectedError:     nil,
 		},
 		{
 			kubernetesVersion: "1.10.1",
-			expectedVersion:   version.MustParseSemantic("3.2.14"),
+			expectedVersion:   version.MustParseSemantic("3.2.16"),
 			expectedError:     nil,
 		},
 	}
