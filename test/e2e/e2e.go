e724886ad5b20aa1d0211098e62223ef4872c2f8
Jeremy L. Morris <jeremylevanmorris@gmail.com>
2018-02-17 22:25:38 -0500

    Removed newlines from e2e log statements.

--- test/e2e/e2e.go
+++ test/e2e/e2e.go
@@ -56,7 +56,7 @@ func setupProviderConfig() error {
 		glog.Info("The --provider flag is not set.  Treating as a conformance test.  Some tests may not be run.")
 
 	case "gce", "gke":
-		framework.Logf("Fetching cloud provider for %q\r\n", framework.TestContext.Provider)
+		framework.Logf("Fetching cloud provider for %q\r", framework.TestContext.Provider)
 		zone := framework.TestContext.CloudConfig.Zone
 		region := framework.TestContext.CloudConfig.Region
 
@@ -295,7 +295,7 @@ func gatherTestSuiteMetrics() error {
 			return fmt.Errorf("error writing to %q: %v", filePath, err)
 		}
 	} else {
-		framework.Logf("\n\nTest Suite Metrics:\n%s\n\n", metricsJSON)
+		framework.Logf("\n\nTest Suite Metrics:\n%s\n", metricsJSON)
 	}
 
 	return nil
