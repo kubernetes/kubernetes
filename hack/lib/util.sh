afc459758e1500885705695e25c24dbe6887cb25
Ismo Puustinen <ismo.puustinen@intel.com>
2018-02-20 12:25:27 +0200

    hack/lib/util.sh: remove shadowed case statements.
    The topmost case statements shadow those coming after them. Since they
    are never run, remove them.

--- hack/lib/util.sh
+++ hack/lib/util.sh
@@ -272,14 +272,8 @@ kube::util::group-version-to-pkg-path() {
     meta/v1)
       echo "vendor/k8s.io/apimachinery/pkg/apis/meta/v1"
       ;;
-    meta/v1)
-      echo "../vendor/k8s.io/apimachinery/pkg/apis/meta/v1"
-      ;;
     meta/v1beta1)
       echo "vendor/k8s.io/apimachinery/pkg/apis/meta/v1beta1"
-      ;;
-    meta/v1beta1)
-      echo "../vendor/k8s.io/apimachinery/pkg/apis/meta/v1beta1"
       ;;
     unversioned)
       echo "pkg/api/unversioned"
