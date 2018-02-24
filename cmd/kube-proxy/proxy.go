fc4426f0403c72e1cbd6eaa28fc87ddeab9ccf4b
Davanum Srinivas <davanum@gmail.com>
2018-02-24 13:35:45 -0500

    Partial revert to fix local-up-cluster.sh

--- cmd/kube-proxy/proxy.go
+++ cmd/kube-proxy/proxy.go
@@ -17,10 +17,13 @@ limitations under the License.
 package main
 
 import (
+	goflag "flag"
 	"fmt"
 	"math/rand"
 	"os"
 	"time"
+
+	"github.com/spf13/pflag"
 
 	utilflag "k8s.io/apiserver/pkg/util/flag"
 	"k8s.io/apiserver/pkg/util/logs"
@@ -34,7 +37,12 @@ func main() {
 
 	command := app.NewProxyCommand()
 
-	utilflag.InitFlags()
+	// TODO: once we switch everything over to Cobra commands, we can go back to calling
+	// utilflag.InitFlags() (by removing its pflag.Parse() call). For now, we have to set the
+	// normalize func and add the go flag set by hand.
+	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+	// utilflag.InitFlags()
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
