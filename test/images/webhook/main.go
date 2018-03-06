f3b86121d58f5558a96d6a60df4029cc98bc337e
lcfang <licuifang@huawei.com>
2018-03-06 11:19:56 +0800

    modify the ref file about admission group consts

--- test/images/webhook/main.go
+++ test/images/webhook/main.go
@@ -29,6 +29,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/consts/admission"
 	// TODO: try this library to see if it generates correct json patch
 	// https://github.com/mattbaird/jsonpatch
 )
@@ -130,7 +131,7 @@ func mutatePods(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 	reviewResponse.Allowed = true
 	if pod.Name == "webhook-to-be-mutated" {
 		reviewResponse.Patch = []byte(addInitContainerPatch)
-		pt := v1beta1.PatchTypeJSONPatch
+		pt := admission.PatchTypeJSONPatch
 		reviewResponse.PatchType = &pt
 	}
 	return &reviewResponse
@@ -189,7 +190,7 @@ func mutateConfigmaps(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 		reviewResponse.Patch = []byte(patch2)
 	}
 
-	pt := v1beta1.PatchTypeJSONPatch
+	pt := admission.PatchTypeJSONPatch
 	reviewResponse.PatchType = &pt
 
 	return &reviewResponse
@@ -218,7 +219,7 @@ func mutateCRD(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 	if cr.Data["mutation-stage-1"] == "yes" {
 		reviewResponse.Patch = []byte(patch2)
 	}
-	pt := v1beta1.PatchTypeJSONPatch
+	pt := admission.PatchTypeJSONPatch
 	reviewResponse.PatchType = &pt
 	return &reviewResponse
 }
