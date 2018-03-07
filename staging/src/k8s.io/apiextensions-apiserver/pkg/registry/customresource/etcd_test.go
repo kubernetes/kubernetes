3dcb42382e906694cca20bca304bb7ff0d752bb0
பாலாஜி <rbalajis25@gmail.com>
2018-03-07 16:17:57 +0530

    TestDelete is changed with TestDeleteGraceful
    Signed-off-by: பாலாஜி <rbalajis25@gmail.com>

--- staging/src/k8s.io/apiextensions-apiserver/pkg/registry/customresource/etcd_test.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/registry/customresource/etcd_test.go
@@ -157,7 +157,7 @@ func TestDelete(t *testing.T) {
 	defer server.Terminate(t)
 	defer storage.CustomResource.Store.DestroyFunc()
 	test := registrytest.New(t, storage.CustomResource.Store)
-	test.TestDelete(validNewCustomResource())
+	test.TestDeleteGraceful(validNewCustomResource(), 30)
 }
 
 func TestCategories(t *testing.T) {
