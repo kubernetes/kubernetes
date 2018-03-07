a942e2d0d3a7d110b1b49a41b2508c073fed470d
hangaoshuai <hangaoshuai1@huawei.com>
2018-03-07 10:38:29 +0800

    add unit test for function ValidateLabelSelector and ValidateDeleteOptions

--- staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/validation/validation_test.go
+++ staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/validation/validation_test.go
@@ -20,6 +20,7 @@ import (
 	"strings"
 	"testing"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
@@ -88,6 +89,298 @@ func TestValidateLabels(t *testing.T) {
 		} else {
 			if !strings.Contains(errs[0].Detail, labelValueErrorCases[i].expect) {
 				t.Errorf("case[%d]: error details do not include %q: %q", i, labelValueErrorCases[i].expect, errs[0].Detail)
+			}
+		}
+	}
+}
+
+func TestValidateLabelSelector(t *testing.T) {
+	successCases := []struct {
+		name     string
+		selector metav1.LabelSelector
+	}{
+		{
+			name: "test when two keys and values in MatchLabels while one key and two values in MatchExpressions",
+			selector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"id":  "app2",
+					"id2": "app3",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "In", Values: []string{"bar1", "bar2"}},
+				},
+			},
+		},
+		{
+			name: "test when one key and two values in MatchExpressions but no MatchLabels",
+			selector: metav1.LabelSelector{
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "key2", Operator: "In", Values: []string{"value1", "value2"}},
+				},
+			},
+		},
+		{
+			name: "test when one key and one value in MatchLabels but no MatchExpressions",
+			selector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"id": "app2",
+				},
+			},
+		},
+		{
+			name: "test when MatchLabels and MatchExpressions exist while Operator NotIn",
+			selector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"id":  "app2",
+					"id2": "app3",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "NotIn", Values: []string{"bar1", "bar2"}},
+				},
+			},
+		},
+		{
+			name: "test when MatchLabels and MatchExpressions exist while Operator Exists",
+			selector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"id": "app2",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "Exists"},
+				},
+			},
+		},
+		{
+			name: "test when MatchLabels and MatchExpressions exist while Operator DoesNotExist",
+			selector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"id": "app2",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "DoesNotExist"},
+				},
+			},
+		},
+	}
+	for i := range successCases {
+		errs := ValidateLabelSelector(&successCases[i].selector, field.NewPath("field"))
+		if len(errs) != 0 {
+			t.Errorf("successCases: case[%d]:%v expected success, got %#v", i, successCases[i].name, errs)
+		}
+	}
+
+	namePartErrMsg := "name part must consist of"
+	nameErrMsg := "a qualified name must consist of"
+	labelErrMsg := "a valid label must be an empty string or consist of"
+	maxLengthErrMsg := "must be no more than"
+
+	matchLabelErrorCases := []struct {
+		labelselector metav1.LabelSelector
+		expect        string
+	}{
+		{
+			labelselector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"ids^=@": "app2",
+					"id2":    "app3",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "In", Values: []string{"bar1", "bar2"}},
+				},
+			},
+			expect: namePartErrMsg,
+		},
+		{
+			labelselector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"only/one/slas": "app2",
+					"id2":           "app3",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "In", Values: []string{"bar1", "bar2"}},
+				},
+			},
+			expect: nameErrMsg,
+		},
+		{
+			labelselector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"only": "app2",
+					"good": "app3=@",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "In", Values: []string{"bar1", "bar2"}},
+				},
+			},
+			expect: labelErrMsg,
+		},
+		{
+			labelselector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"only": "app2",
+					"good": strings.Repeat("a", 64),
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "In", Values: []string{"bar1", "bar2"}},
+				},
+			},
+			expect: maxLengthErrMsg,
+		},
+	}
+	for i := range matchLabelErrorCases {
+		errs := ValidateLabelSelector(&matchLabelErrorCases[i].labelselector, field.NewPath("field"))
+		if len(errs) == 0 {
+			t.Errorf("MatchLabelErrorCases: case[%d]:%v expected failure", i, matchLabelErrorCases[i])
+		} else {
+			if !strings.Contains(errs[0].Detail, matchLabelErrorCases[i].expect) {
+				t.Errorf("MatchLabelErrorCases: case[%d]:%v error details do not include %q: %q", i, matchLabelErrorCases[i], matchLabelErrorCases[i].expect, errs[0].Detail)
+			}
+		}
+	}
+
+	opInOrNotInErrMsg := "must be specified when `operator` is 'In' or 'NotIn'"
+	opExistsOrNotExistsErrMsg := "may not be specified when `operator` is 'Exists' or 'DoesNotExist'"
+	opNotValidErrMsg := "not a valid selector operator"
+
+	matchExpressionErrorCases := []struct {
+		labelselector metav1.LabelSelector
+		expect        string
+	}{
+		{
+			labelselector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"ids": "app2",
+					"id2": "app3",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "In", Values: []string{}},
+				},
+			},
+			expect: opInOrNotInErrMsg,
+		},
+		{
+			labelselector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"only": "app2",
+					"id2":  "app3",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "NotIn", Values: []string{}},
+				},
+			},
+			expect: opInOrNotInErrMsg,
+		},
+		{
+			labelselector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"only": "app2",
+					"good": "app3",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "Exists", Values: []string{"bar1", "bar2"}},
+				},
+			},
+			expect: opExistsOrNotExistsErrMsg,
+		},
+		{
+			labelselector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"only": "app2",
+					"good": "app1",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "DoesNotExist", Values: []string{"bar1", "bar2"}},
+				},
+			},
+			expect: opExistsOrNotExistsErrMsg,
+		},
+		{
+			labelselector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"ids": "app2",
+					"id2": "app3",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo", Operator: "Out", Values: []string{"bar1", "bar2"}},
+				},
+			},
+			expect: opNotValidErrMsg,
+		},
+		{
+			labelselector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"ids": "app2",
+					"id2": "app3",
+				},
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{Key: "foo^=@", Operator: "In", Values: []string{"bar1", "bar2"}},
+				},
+			},
+			expect: namePartErrMsg,
+		},
+	}
+	for i := range matchExpressionErrorCases {
+		errs := ValidateLabelSelector(&matchExpressionErrorCases[i].labelselector, field.NewPath("field"))
+		if len(errs) == 0 {
+			t.Errorf("MatchExpressionErrorCases: case[%d]:%v expected failure", i, matchExpressionErrorCases[i])
+		} else {
+			if !strings.Contains(errs[0].Detail, matchExpressionErrorCases[i].expect) {
+				t.Errorf("MatchExpressionErrorCases: case[%d]:%v error details do not include %q: %q", i, matchExpressionErrorCases[i], matchExpressionErrorCases[i].expect, errs[0].Detail)
+			}
+		}
+	}
+}
+
+func TestValidateDeleteOptions(t *testing.T) {
+	falseVar := false
+	foregroundPolicy := metav1.DeletePropagationForeground
+	backgroundPolicy := metav1.DeletePropagationBackground
+	orphanPolicy := metav1.DeletePropagationOrphan
+	var invaildPolicy metav1.DeletionPropagation = "testinvaild"
+
+	successCases := []metav1.DeleteOptions{
+		{OrphanDependents: &falseVar},
+		{PropagationPolicy: &orphanPolicy},
+		{PropagationPolicy: &foregroundPolicy},
+		{PropagationPolicy: &backgroundPolicy},
+	}
+	for i := range successCases {
+		errs := ValidateDeleteOptions(&successCases[i])
+		if len(errs) != 0 {
+			t.Errorf("successCases:case[%d] expected success, got %#v", i, errs)
+		}
+	}
+
+	opBothSetErrMsg := "OrphanDependents and DeletionPropagation cannot be both set"
+	opNotVaildErrMsg := "DeletionPropagation need to be one of"
+	filedCases := []struct {
+		deleteoptions metav1.DeleteOptions
+		expect        string
+	}{
+		{
+			deleteoptions: metav1.DeleteOptions{OrphanDependents: &falseVar, PropagationPolicy: &orphanPolicy},
+			expect:        opBothSetErrMsg,
+		},
+		{
+			deleteoptions: metav1.DeleteOptions{OrphanDependents: &falseVar, PropagationPolicy: &backgroundPolicy},
+			expect:        opBothSetErrMsg,
+		},
+		{
+			deleteoptions: metav1.DeleteOptions{OrphanDependents: &falseVar, PropagationPolicy: &foregroundPolicy},
+			expect:        opBothSetErrMsg,
+		},
+		{
+			deleteoptions: metav1.DeleteOptions{PropagationPolicy: &invaildPolicy},
+			expect:        opNotVaildErrMsg,
+		},
+	}
+	for i := range filedCases {
+		errs := ValidateDeleteOptions(&filedCases[i].deleteoptions)
+		if len(errs) == 0 {
+			t.Errorf("filedCases: case[%d]:%v expected failure", i, filedCases[i])
+		} else {
+			if !strings.Contains(errs[0].Detail, filedCases[i].expect) {
+				t.Errorf("filedCases: case[%d]:%v error details do not include %q: %q", i, filedCases[i], filedCases[i].expect, errs[0].Detail)
 			}
 		}
 	}
