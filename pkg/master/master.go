456fd386dc6db8ba5ced338a5935de8229c14047
David Eads <deads@redhat.com>
2018-03-09 14:36:04 -0500

    update metrics to true like it is for kube-apiserver

--- pkg/master/master.go
+++ pkg/master/master.go
@@ -284,9 +284,6 @@ func (cfg *Config) Complete(informers informers.SharedInformerFactory) Completed
 		c.ExtraConfig.EndpointReconcilerConfig.Reconciler = cfg.createEndpointReconciler()
 	}
 
-	// this has always been hardcoded true in the past
-	c.GenericConfig.EnableMetrics = true
-
 	return CompletedConfig{&c}
 }
 
