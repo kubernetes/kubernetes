2b7fd92dce96a1f7285cd6b8acb75f98db7e191d
hzxuzhonghu <xuzhonghu@huawei.com>
2018-03-09 09:22:30 +0800

    userspace: move udp echo server to proxier_test.go

--- pkg/proxy/winuserspace/proxier_test.go
+++ pkg/proxy/winuserspace/proxier_test.go
@@ -84,6 +84,31 @@ func waitForClosedPortUDP(p *Proxier, proxyPort int) error {
 	return fmt.Errorf("port %d still open", proxyPort)
 }
 
+// udpEchoServer is a simple echo server in UDP, intended for testing the proxy.
+type udpEchoServer struct {
+	net.PacketConn
+}
+
+func newUDPEchoServer() (*udpEchoServer, error) {
+	packetconn, err := net.ListenPacket("udp", ":0")
+	if err != nil {
+		return nil, err
+	}
+	return &udpEchoServer{packetconn}, nil
+}
+
+func (r *udpEchoServer) Loop() {
+	var buffer [4096]byte
+	for {
+		n, cliAddr, err := r.ReadFrom(buffer[0:])
+		if err != nil {
+			fmt.Printf("ReadFrom failed: %v\n", err)
+			continue
+		}
+		r.WriteTo(buffer[0:n], cliAddr)
+	}
+}
+
 var tcpServerPort int32
 var udpServerPort int32
 
