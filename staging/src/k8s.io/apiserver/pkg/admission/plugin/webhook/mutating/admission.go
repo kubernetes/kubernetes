ae2acec0c664f62c9a8513281fdd9126e988806b
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-03-09 08:27:19 +0100

    webhook-admission: quickfix of registering AdmissionReview in provided scheme

--- staging/src/k8s.io/apiserver/pkg/admission/plugin/webhook/mutating/admission.go
+++ staging/src/k8s.io/apiserver/pkg/admission/plugin/webhook/mutating/admission.go
@@ -131,6 +131,9 @@ func (a *MutatingWebhook) SetServiceResolver(sr config.ServiceResolver) {
 // SetScheme sets a serializer(NegotiatedSerializer) which is derived from the scheme
 func (a *MutatingWebhook) SetScheme(scheme *runtime.Scheme) {
 	if scheme != nil {
+		// TODO: we should not mutate a scheme we don't own. We should have a fallback serializer instead.
+		admissionv1beta1.AddToScheme(scheme)
+
 		a.clientManager.SetNegotiatedSerializer(serializer.NegotiatedSerializerWrapper(runtime.SerializerInfo{
 			Serializer: serializer.NewCodecFactory(scheme).LegacyCodec(admissionv1beta1.SchemeGroupVersion),
 		}))
