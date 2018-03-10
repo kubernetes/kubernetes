214b77cf50371aae2deb52a61b59b98c43b8e9df
Davanum Srinivas <davanum@gmail.com>
2018-03-09 19:52:07 -0500

    Split out the hostname when default dhcp_domain is used in nova.conf
    When /etc/nova/nova.conf does not have specify dhcp_domain to empty
    string, a default string of '.novalocal' is returned by the meta data
    service. So we need to just split the string and pick the first one
    in the array.
    
    ```
    $ curl http://169.254.169.254/latest/meta-data/hostname
    testvm-1.novalocal
    $ curl http://169.254.169.254/latest/meta-data/public-hostname
    testvm-1.novalocal
    $ curl http://169.254.169.254/latest/meta-data/local-hostname
    testvm-1.novalocal
    ```

--- pkg/cloudprovider/providers/openstack/openstack_instances.go
+++ pkg/cloudprovider/providers/openstack/openstack_instances.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/golang/glog"
 	"github.com/gophercloud/gophercloud"
@@ -60,7 +61,7 @@ func (i *Instances) CurrentNodeName(ctx context.Context, hostname string) (types
 	if err != nil {
 		return "", err
 	}
-	return types.NodeName(md.Hostname), nil
+	return types.NodeName(strings.Split(md.Hostname, ".")[0]), nil
 }
 
 // AddSSHKeyToAllInstances is not implemented for OpenStack
