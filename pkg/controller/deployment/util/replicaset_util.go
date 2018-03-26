cda3f18b8c8a76205defd4fdc3eec7bfa84a7460
Janet Kuo <chiachenk@google.com>
2018-03-26 15:52:17 -0700

    Remove unused Deployment util functions

--- pkg/controller/deployment/util/replicaset_util.go
+++ pkg/controller/deployment/util/replicaset_util.go
@@ -17,8 +17,6 @@ limitations under the License.
 package util
 
 import (
-	"fmt"
-
 	"github.com/golang/glog"
 
 	extensions "k8s.io/api/extensions/v1beta1"
@@ -26,8 +24,6 @@ import (
 	unversionedextensions "k8s.io/client-go/kubernetes/typed/extensions/v1beta1"
 	extensionslisters "k8s.io/client-go/listers/extensions/v1beta1"
 	"k8s.io/client-go/util/retry"
-	"k8s.io/kubernetes/pkg/controller"
-	labelsutil "k8s.io/kubernetes/pkg/util/labels"
 )
 
 // TODO: use client library instead when it starts to support update retries
@@ -61,11 +57,4 @@ func UpdateRSWithRetries(rsClient unversionedextensions.ReplicaSetInterface, rsL
 	}
 
 	return rs, retryErr
-}
-
-// GetReplicaSetHash returns the pod template hash of a ReplicaSet's pod template space
-func GetReplicaSetHash(rs *extensions.ReplicaSet, uniquifier *int32) (string, error) {
-	rsTemplate := rs.Spec.Template.DeepCopy()
-	rsTemplate.Labels = labelsutil.CloneAndRemoveLabel(rsTemplate.Labels, extensions.DefaultDeploymentUniqueLabelKey)
-	return fmt.Sprintf("%d", controller.ComputeHash(rsTemplate, uniquifier)), nil
 }
