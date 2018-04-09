0334989450da71ee53f9a17d1b81e567938ca6d8
Antoine Pelisse <apelisse@google.com>
2018-04-09 16:43:22 -0700

    Add "LastApplied" field in ObjectMeta
    This new field will keep track of users/controllers/workflow-id intent
    when they apply something. This is part of the server-side apply.

--- staging/src/k8s.io/apiserver/pkg/registry/rest/create.go
+++ staging/src/k8s.io/apiserver/pkg/registry/rest/create.go
@@ -97,6 +97,11 @@ func BeforeCreate(strategy RESTCreateStrategy, ctx context.Context, obj runtime.
 		objectMeta.SetInitializers(nil)
 	}
 
+	// Ensure LastApplied is not set unless the feature is enabled
+	if !utilfeature.DefaultFeatureGate.Enabled(features.ServerSideApply) {
+		objectMeta.SetLastApplied(nil)
+	}
+
 	// ClusterName is ignored and should not be saved
 	objectMeta.SetClusterName("")
 
