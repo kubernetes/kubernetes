35de82094ac81713dd34c3f03d0e95d604731f61
Mike Danese <mikedanese@google.com>
2018-04-18 16:31:59 -0700

    authz: nodes should not be able to delete themselves

--- test/integration/auth/node_test.go
+++ test/integration/auth/node_test.go
@@ -418,7 +418,8 @@ func TestNodeAuthorizer(t *testing.T) {
 	expectAllowed(t, createNode2MirrorPodEviction(node2Client))
 	expectAllowed(t, createNode2(node2Client))
 	expectAllowed(t, updateNode2Status(node2Client))
-	expectAllowed(t, deleteNode2(node2Client))
+	// cleanup node
+	expectAllowed(t, deleteNode2(superuserClient))
 
 	// create a pod as an admin to add object references
 	expectAllowed(t, createNode2NormalPod(superuserClient))
@@ -508,8 +509,10 @@ func TestNodeAuthorizer(t *testing.T) {
 	expectAllowed(t, unsetNode2ConfigSource(superuserClient))
 	// node2 can no longer get the configmap after it is unassigned as its config source
 	expectForbidden(t, getConfigMapConfigSource(node2Client))
+	// node should not be able to delete itself
+	expectForbidden(t, deleteNode2(node2Client))
 	// clean up node2
-	expectAllowed(t, deleteNode2(node2Client))
+	expectAllowed(t, deleteNode2(superuserClient))
 
 	//TODO(mikedanese): integration test node restriction of TokenRequest
 }
