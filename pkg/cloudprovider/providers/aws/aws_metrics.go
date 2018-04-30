5abe207eefb60a07e77d1e7e8d4afa897a1cd97a
Fabio Bertinatto <fbertina@redhat.com>
2018-04-30 15:57:04 +0200

    Add metric for throttled requests in AWS

--- pkg/cloudprovider/providers/aws/aws_metrics.go
+++ pkg/cloudprovider/providers/aws/aws_metrics.go
@@ -32,9 +32,29 @@ var (
 			Help: "AWS API errors",
 		},
 		[]string{"request"})
+
+	awsAPIThrottlesMetric = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "cloudprovider_aws_api_throttled_requests_total",
+			Help: "AWS API throttled requests",
+		},
+		[]string{"operation_name"})
 )
+
+func recordAWSMetric(actionName string, timeTaken float64, err error) {
+	if err != nil {
+		awsAPIErrorMetric.With(prometheus.Labels{"request": actionName}).Inc()
+	} else {
+		awsAPIMetric.With(prometheus.Labels{"request": actionName}).Observe(timeTaken)
+	}
+}
+
+func recordAWSThrottlesMetric(operation string) {
+	awsAPIThrottlesMetric.With(prometheus.Labels{"operation_name": operation}).Inc()
+}
 
 func registerMetrics() {
 	prometheus.MustRegister(awsAPIMetric)
 	prometheus.MustRegister(awsAPIErrorMetric)
+	prometheus.MustRegister(awsAPIThrottlesMetric)
 }
