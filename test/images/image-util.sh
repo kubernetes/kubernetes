ae23e19f2048c6af01fd375f6895622beb0e4540
Nitesh Konkar <niteshkonkar@in.ibm.com>
2018-05-08 21:00:34 +0530

    Push fat manifest for multi-arch images
    Signed-off-by: Nitesh Konkar <niteshkonkar@in.ibm.com>

--- test/images/image-util.sh
+++ test/images/image-util.sh
@@ -99,6 +99,10 @@ push() {
     TAG=$(<${IMAGE}/VERSION)
     docker push ${REGISTRY}/${IMAGE}-${arch}:${TAG}
   done
+
+  # Make archs list into OS/architecture pair. Eg: 'amd64 ppc64le' to 'linux/amd64,linux/ppc64le'
+  archs=$(echo $archs | sed -e 's/[^ ]* */linux\/&/g' -e 's/ /,/g')
+  manifest-tool push from-args --platforms ${archs} --template ${REGISTRY}/${IMAGE}-ARCH:${TAG} --target ${REGISTRY}/${IMAGE}:${TAG}
 }
 
 # This function is for building the go code
