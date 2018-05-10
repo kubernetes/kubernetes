fd3432ef058d35301bee6cb23a12fd37b4125704
Michael Taufen <mtaufen@google.com>
2018-05-10 15:04:23 -0700

    add dynamic config metrics
    This PR exports config-releated metrics from the Kubelet.
    The Guages for active, assigned, and last-known-good config can be used
    to identify config versions and produce aggregate counts across several
    nodes. The error-reporting Gauge can be used to determine whether a node
    is experiencing a config-related error, and to prodouce an aggregate
    count of nodes in an error state.

--- test/e2e_node/util.go
+++ test/e2e_node/util.go
@@ -45,6 +45,7 @@ import (
 	"k8s.io/kubernetes/pkg/kubelet/remote"
 	"k8s.io/kubernetes/test/e2e/framework"
 	"k8s.io/kubernetes/test/e2e/framework/metrics"
+	frameworkmetrics "k8s.io/kubernetes/test/e2e/framework/metrics"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -332,6 +333,24 @@ func logKubeletMetrics(metricKeys ...string) {
 	} else {
 		framework.Logf("Kubelet Metrics: %+v", framework.GetKubeletMetrics(metric, metricSet))
 	}
+}
+
+// returns config related metrics from the local kubelet, filtered to the filterMetricNames passed in
+func getKubeletMetrics(filterMetricNames sets.String) (frameworkmetrics.KubeletMetrics, error) {
+	// grab Kubelet metrics
+	ms, err := metrics.GrabKubeletMetricsWithoutProxy(framework.TestContext.NodeName + ":10255")
+	if err != nil {
+		return nil, err
+	}
+
+	filtered := metrics.NewKubeletMetrics()
+	for name := range ms {
+		if !filterMetricNames.Has(name) {
+			continue
+		}
+		filtered[name] = ms[name]
+	}
+	return filtered, nil
 }
 
 // runCommand runs the cmd and returns the combined stdout and stderr, or an
