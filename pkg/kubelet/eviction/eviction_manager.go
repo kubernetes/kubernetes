cymtrick/kubernetes-error-fuzzer

fix formatting for memcg threshold

pkg/kubelet/eviction/eviction_manager.go
@@ -19,6 +19,7 @@ package eviction
 import (
 	"fmt"
 	"sort"
+	"strconv"
 	"sync"
 	"time"
 
@@ -211,7 +212,7 @@ func (m *managerImpl) startMemoryThresholdNotifier(summary *statsapi.Summary, ha
 		memcgThreshold.Sub(*evictionThresholdQuantity)
 		memcgThreshold.Add(*inactiveFile)
 		description := fmt.Sprintf("<%s available", formatThresholdValue(threshold.Value))
-		memcgThresholdNotifier, err := NewMemCGThresholdNotifier(cgpath, attribute, memcgThreshold.String(), description, handler)
+		memcgThresholdNotifier, err := NewMemCGThresholdNotifier(cgpath, attribute, strconv.FormatInt(memcgThreshold.Value(), 10), description, handler)
 		if err != nil {
 			return err
 		}

kubernetes/kubernetes

pkg/kubelet/eviction/eviction_manager.go
@@ -19,6 +19,7 @@ package eviction
 import (
 	"fmt"
 	"sort"
+	"strconv"
 	"sync"
 	"time"
 
@@ -207,7 +208,7 @@ func startMemoryThresholdNotifier(thresholds []evictionapi.Threshold, observatio
 		usageThreshold := resource.NewQuantity(observed.capacity.Value(), resource.DecimalSI)
 		usageThreshold.Sub(*quantity)
 		description := fmt.Sprintf("<%s available", formatThresholdValue(threshold.Value))
-		memcgThresholdNotifier, err := NewMemCGThresholdNotifier(cgpath, attribute, usageThreshold.String(), description, handler)
+		memcgThresholdNotifier, err := NewMemCGThresholdNotifier(cgpath, attribute, strconv.FormatInt(usageThreshold.Value(), 10), description, handler)
 		if err != nil {
 			return err
 		}