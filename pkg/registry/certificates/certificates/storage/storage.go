bba01e97ed7f7cbbdfed1ab44e2480cf8d1f84d6
Haowei Cai <haoweic@google.com>
2018-05-18 18:22:15 -0700

    Add GET PATCH support for two /status:
    apiservices/status under apiregistration.k8s.io
    certificatesigningrequests/status under certificates.k8s.io

--- pkg/registry/certificates/certificates/storage/storage.go
+++ pkg/registry/certificates/certificates/storage/storage.go
@@ -19,6 +19,7 @@ package storage
 import (
 	"context"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/registry/generic"
 	genericregistry "k8s.io/apiserver/pkg/registry/generic/registry"
@@ -83,10 +84,17 @@ func (r *StatusREST) New() runtime.Object {
 	return &certificates.CertificateSigningRequest{}
 }
 
+// Get retrieves the object from the storage. It is required to support Patch.
+func (r *StatusREST) Get(ctx context.Context, name string, options *metav1.GetOptions) (runtime.Object, error) {
+	return r.store.Get(ctx, name, options)
+}
+
 // Update alters the status subset of an object.
 func (r *StatusREST) Update(ctx context.Context, name string, objInfo rest.UpdatedObjectInfo, createValidation rest.ValidateObjectFunc, updateValidation rest.ValidateObjectUpdateFunc) (runtime.Object, bool, error) {
 	return r.store.Update(ctx, name, objInfo, createValidation, updateValidation)
 }
+
+var _ = rest.Patcher(&StatusREST{})
 
 // ApprovalREST implements the REST endpoint for changing the approval state of a CSR.
 type ApprovalREST struct {
