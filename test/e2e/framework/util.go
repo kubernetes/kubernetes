31bf75c116d774f69598dda0cf2f0e2c55d1283f
Jacob Gillespie <jacobwgillespie@gmail.com>
2018-05-21 09:16:36 -0500

    Fix running e2e tests with completed kube-system pods

--- test/e2e/framework/util.go
+++ test/e2e/framework/util.go
@@ -704,10 +704,8 @@ func WaitForPodsRunningReady(c clientset.Interface, ns string, minPods, allowedN
 			case res && err == nil:
 				nOk++
 			case pod.Status.Phase == v1.PodSucceeded:
-				Logf("The status of Pod %s is Succeeded which is unexpected", pod.ObjectMeta.Name)
-				badPods = append(badPods, pod)
-				// it doesn't make sense to wait for this pod
-				return false, errors.New("unexpected Succeeded pod state")
+				// pod status is succeeded, it doesn't make sense to wait for this pod
+				continue
 			case pod.Status.Phase != v1.PodFailed:
 				Logf("The status of Pod %s is %s (Ready = false), waiting for it to be either Running (with Ready = true) or Failed", pod.ObjectMeta.Name, pod.Status.Phase)
 				notReady++
