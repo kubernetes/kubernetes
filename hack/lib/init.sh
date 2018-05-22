f38e952f4eb3a5f4996a2a41bd0eedc98b6637de
wojtekt <wojtekt@google.com>
2018-05-22 16:34:25 +0200

    Add coordination API group with Lease type

--- hack/lib/init.sh
+++ hack/lib/init.sh
@@ -72,6 +72,7 @@ batch/v1 \
 batch/v1beta1 \
 batch/v2alpha1 \
 certificates.k8s.io/v1beta1 \
+coordination.k8s.io/v1beta1 \
 extensions/v1beta1 \
 events.k8s.io/v1beta1 \
 imagepolicy.k8s.io/v1alpha1 \
