015871bf57e7e688ac36518eadceb2de6561abcb
Tardis Xu <xiaoxubeii@gmail.com>
2018-05-25 00:07:41 +0800

    Format

--- pkg/volume/rbd/rbd_test.go
+++ pkg/volume/rbd/rbd_test.go
@@ -329,7 +329,7 @@ func TestPlugin(t *testing.T) {
 					RBDPool:      "pool1",
 					RBDImage:     "image1",
 					FSType:       "ext4",
-					ReadOnly:	  true,
+					ReadOnly:     true,
 				},
 			},
 		}),
