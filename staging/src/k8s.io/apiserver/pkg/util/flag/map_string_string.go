0ad320cbbbbb4002d652baef8e48bc1f5b4cfde7
Clayton Coleman <ccoleman@redhat.com>
2018-05-24 15:02:08 -0400

    MapString* should return empty string for String() when null

--- staging/src/k8s.io/apiserver/pkg/util/flag/map_string_string.go
+++ staging/src/k8s.io/apiserver/pkg/util/flag/map_string_string.go
@@ -50,6 +50,9 @@ func NewMapStringStringNoSplit(m *map[string]string) *MapStringString {
 
 // String implements github.com/spf13/pflag.Value
 func (m *MapStringString) String() string {
+	if m == nil || m.Map == nil {
+		return ""
+	}
 	pairs := []string{}
 	for k, v := range *m.Map {
 		pairs = append(pairs, fmt.Sprintf("%s=%s", k, v))
