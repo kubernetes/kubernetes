3464b5aebacfcd0cfe18cabd3afa3a3bb385d7eb
minherz <minherz@gmail.com>
2018-05-26 16:17:16 +0300

    introduce custom prefix for service load balancer name
    allow defining custom prefix to LB name for load balancers.
    implements FR #62640

--- pkg/cloudprovider/cloud.go
+++ pkg/cloudprovider/cloud.go
@@ -21,12 +21,18 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"unicode"
 
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/informers"
 	"k8s.io/kubernetes/pkg/controller"
 )
+
+// ServiceAnnotationLoadBalancerNamePrefix is the annotation used on the service
+// of type LoadBalancer for defining a prefix of the load balancer name. The prefix
+// is limited to alphanumeric and hyphen symbols with maximal length of 8
+const ServiceAnnotationLoadBalancerNamePrefix = "service.beta.kubernetes.io/load-balancer-name-prefix"
 
 // Interface is an abstract, pluggable interface for cloud providers.
 type Interface interface {
@@ -62,13 +68,24 @@ type Clusters interface {
 	Master(ctx context.Context, clusterName string) (string, error)
 }
 
-// TODO(#6812): Use a shorter name that's less likely to be longer than cloud
-// providers' name length limits.
+// GetLoadBalancerName generates unique LB name considering different providers' limits:
+// (1) GCE requires LB name to start with a lower case letter.
+// (2) AWS requires LB name max length to be 32 chars.
 func GetLoadBalancerName(service *v1.Service) string {
-	//GCE requires that the name of a load balancer starts with a lower case letter.
-	ret := "a" + string(service.UID)
-	ret = strings.Replace(ret, "-", "", -1)
-	//AWS requires that the name of a load balancer is shorter than 32 bytes.
+	prefix := "a"
+	l := service.Annotations[ServiceAnnotationLoadBalancerNamePrefix]
+
+	// set custom prefix (max lenght 8) if defined
+	if len(l) > 0 && unicode.IsLetter(l[0]) {
+		if len(l) > 8 {
+			prefix = l[:8]
+		} else {
+			prefix = l
+		}
+	}
+
+	ret := prefix + strings.Replace(string(service.UID), "-", "", -1)
+	// limit length
 	if len(ret) > 32 {
 		ret = ret[:32]
 	}
