c24842d806d3291c103ad253149851b711afc38f
Jonathan Basseri <misterikkit@google.com>
2018-05-29 12:11:23 -0700

    Add a package docstring to core/equivalence.

--- pkg/scheduler/core/equivalence/eqivalence.go
+++ pkg/scheduler/core/equivalence/eqivalence.go
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package equivalence defines Pod equivalence classes and the equivalence class
+// cache.
 package equivalence
 
 import (
