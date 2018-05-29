381ceca9602ae7e7f7a37a2340b0d11968fd3d06
Guoliang Wang <iamwgliang@gmail.com>
2018-05-29 17:49:05 +0800

    FloatingIP should disabled when front port is diffrent from backend port

--- pkg/cloudprovider/providers/azure/azure_loadbalancer.go
+++ pkg/cloudprovider/providers/azure/azure_loadbalancer.go
@@ -680,7 +680,7 @@ func (az *Cloud) reconcileLoadBalancer(clusterName string, service *v1.Service, 
 				LoadDistribution: loadDistribution,
 				FrontendPort:     to.Int32Ptr(port.Port),
 				BackendPort:      to.Int32Ptr(port.Port),
-				EnableFloatingIP: to.BoolPtr(true),
+				EnableFloatingIP: to.BoolPtr(to.Int32Ptr(port.Port) == to.Int32Ptr(port.Port)),
 			},
 		}
 
