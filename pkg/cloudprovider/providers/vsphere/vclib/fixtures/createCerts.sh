bec497765af84fd132ee733e10419e0370add75f
Hannes Hörl <hhorl@pivotal.io>
2018-06-08 14:42:54 +0100

    Create new certs & cleanup cert generation
    The test certificates are now valid for 200 years.
    The generation script has been cleaned up a bit.

--- pkg/cloudprovider/providers/vsphere/vclib/fixtures/createCerts.sh
+++ pkg/cloudprovider/providers/vsphere/vclib/fixtures/createCerts.sh
@@ -16,41 +16,64 @@
 
 set -eu
 
+readonly VALID_DAYS='73000'
+readonly RSA_KEY_SIZE='4096'
+
 createKey() {
-  openssl genrsa -out "$1" 4096
+  openssl genrsa \
+    -out "$1" \
+    "$RSA_KEY_SIZE"
 }
 
 createCaCert() {
-  openssl req -x509 \
-    -subj "/C=US/ST=CA/O=Acme, Inc./CN=someCA" \
-    -new -nodes -key "$2" -sha256 -days 10240 -out "$1"
+  openssl req \
+    -x509 \
+    -subj "$( getSubj 'someCA' )" \
+    -new \
+    -nodes \
+    -key "$2" \
+    -sha256 \
+    -days "$VALID_DAYS" \
+    -out "$1"
 }
 
 createCSR() {
-  openssl req -new -sha256 \
+  openssl req \
+    -new \
+    -sha256 \
     -key "$2" \
-    -subj "/C=US/ST=CA/O=Acme, Inc./CN=localhost" \
+    -subj "$( getSubj 'localhost' )" \
     -reqexts SAN \
-    -config <(
-      cat /etc/ssl/openssl.cnf ; \
-      printf '\n[SAN]\n' ; \
-      getSAN
-      ) \
+    -config <( getSANConfig ) \
     -out "$1"
 }
 
 signCSR() {
-  openssl x509 -req -in "$2" \
+  openssl x509 \
+    -req \
+    -in "$2" \
     -CA "$3" \
     -CAkey "$4" \
     -CAcreateserial \
-    -days 3650 -sha256 \
+    -days "$VALID_DAYS" \
+    -sha256 \
     -extfile <( getSAN ) \
     -out "$1"
 }
 
+getSubj() {
+  local cn="${1:-someRandomCN}"
+  echo "/C=US/ST=CA/O=Acme, Inc./CN=${cn}"
+}
+
 getSAN() {
   printf "subjectAltName=DNS:localhost,IP:127.0.0.1"
+}
+
+getSANConfig() {
+  cat /etc/ssl/openssl.cnf
+  printf '\n[SAN]\n'
+  getSAN
 }
 
 main() {
