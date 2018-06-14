20cc40a5dc51f0f876098d99f05a71daeaf0edc5
Jess Frazelle <me@jessfraz.com>
2018-06-14 18:05:18 -0400

    ProcMount: add dockershim support
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- pkg/kubelet/dockershim/security_context.go
+++ pkg/kubelet/dockershim/security_context.go
@@ -137,6 +137,9 @@ func modifyHostConfig(sc *runtimeapi.LinuxContainerSecurityContext, hostConfig *
 		hostConfig.SecurityOpt = append(hostConfig.SecurityOpt, "no-new-privileges")
 	}
 
+	hostConfig.MaskedPaths = sc.MaskedPaths
+	hostConfig.ReadonlyPaths = sc.ReadonlyPaths
+
 	return nil
 }
 
