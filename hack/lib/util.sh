510f6b8916c9e105474fe6b55cb645106e93ab46
Brandon Dalla Rosa <brandondr96@gmail.com>
2018-06-19 16:43:07 -0400

    Adding IKS functionality to kubemark

--- hack/lib/util.sh
+++ hack/lib/util.sh
@@ -787,6 +787,8 @@ if [[ -z "${color_start-}" ]]; then
   declare -r color_red="${color_start}0;31m"
   declare -r color_yellow="${color_start}0;33m"
   declare -r color_green="${color_start}0;32m"
+  declare -r color_blue="${color_start}1;34m"
+  declare -r color_cyan="${color_start}1;36m"
   declare -r color_norm="${color_start}0m"
 fi
 
