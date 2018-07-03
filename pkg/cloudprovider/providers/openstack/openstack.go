a78fc5926d6091fcae9df2ccac3a5a3182832ce4
Jesse Haka <haka.jesse@gmail.com>
2018-07-03 23:24:24 +0300

    if loadbalancer section is not defined in cloudconfig, do not initialize lb support
    use reflect because structs with []string cannot be compared

--- pkg/cloudprovider/providers/openstack/openstack.go
+++ pkg/cloudprovider/providers/openstack/openstack.go
@@ -27,6 +27,7 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"reflect"
 	"strings"
 	"time"
 
@@ -572,6 +573,11 @@ func (os *OpenStack) HasClusterID() bool {
 // LoadBalancer initializes a LbaasV2 object
 func (os *OpenStack) LoadBalancer() (cloudprovider.LoadBalancer, bool) {
 	glog.V(4).Info("openstack.LoadBalancer() called")
+
+	if reflect.DeepEqual(os.lbOpts, LoadBalancerOpts{}) {
+		glog.V(4).Info("LoadBalancer section is empty/not defined in cloud-config")
+		return nil, false
+	}
 
 	network, err := os.NewNetworkV2()
 	if err != nil {
