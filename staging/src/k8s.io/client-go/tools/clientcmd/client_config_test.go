4254df27f38fedfb737cf6c48653412d1c1c3310
Christoph Blecker <admin@toph.ca>
2018-07-04 12:52:01 -0700

    Move from mergo.Merge to mergo.MergeWithOverwrite

--- staging/src/k8s.io/client-go/tools/clientcmd/client_config_test.go
+++ staging/src/k8s.io/client-go/tools/clientcmd/client_config_test.go
@@ -28,21 +28,23 @@ import (
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
-func TestOldMergoLib(t *testing.T) {
+func TestMergoSemantics(t *testing.T) {
 	type T struct {
 		X string
 	}
 	dst := T{X: "one"}
 	src := T{X: "two"}
-	mergo.Merge(&dst, &src)
+	mergo.MergeWithOverwrite(&dst, &src)
 	if dst.X != "two" {
-		// mergo.Merge changed in an incompatible way with
+		// The mergo library has previously changed in a an incompatible way.
+		// example:
 		//
 		//   https://github.com/imdario/mergo/commit/d304790b2ed594794496464fadd89d2bb266600a
 		//
-		// We have to stay with the old version which still does eager
-		// copying from src to dst in structs.
-		t.Errorf("mergo.Merge library found with incompatible, new behavior")
+		// This test verifies that the semantics of the merge are what we expect.
+		// If they are not, the mergo library may have been updated and broken
+		// unexpectedly.
+		t.Errorf("mergo.MergeWithOverwrite did not provide expected output")
 	}
 }
 
