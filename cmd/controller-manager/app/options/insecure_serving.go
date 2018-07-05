13f6db1d01c069a920627e96a30f2563c19f152f
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-07-05 15:01:19 +0200

    controller-manager: allow high ports in secure serving validation

--- cmd/controller-manager/app/options/insecure_serving.go
+++ cmd/controller-manager/app/options/insecure_serving.go
@@ -53,8 +53,8 @@ func (s *InsecureServingOptions) Validate() []error {
 
 	errors := []error{}
 
-	if s.BindPort < 0 || s.BindPort > 32767 {
-		errors = append(errors, fmt.Errorf("--insecure-port %v must be between 0 and 32767, inclusive. 0 for turning off insecure (HTTP) port", s.BindPort))
+	if s.BindPort < 0 || s.BindPort > 65335 {
+		errors = append(errors, fmt.Errorf("--insecure-port %v must be between 0 and 65535, inclusive. 0 for turning off insecure (HTTP) port", s.BindPort))
 	}
 
 	return errors
