70b03d134758b7030b01d22e0d01ef0b96c3e981
Tardis Xu <xiaoxubeii@gmail.com>
2018-07-16 15:44:22 +0800

    Add HealthzPort to kube proxy configuration.

--- pkg/proxy/apis/kubeproxyconfig/v1alpha1/types.go
+++ pkg/proxy/apis/kubeproxyconfig/v1alpha1/types.go
@@ -102,8 +102,10 @@ type KubeProxyConfiguration struct {
 	// bindAddress is the IP address for the proxy server to serve on (set to 0.0.0.0
 	// for all interfaces)
 	BindAddress string `json:"bindAddress"`
-	// healthzBindAddress is the IP address and port for the health check server to serve on,
-	// defaulting to 0.0.0.0:10256
+	// healthzPort is the port of the health check server (set to 0 to disable)
+	HealthzPort *int32 `json:"healthzPort,omitempty"`
+	// healthzBindAddress is the IP address for the health check server to serve on,
+	// defaulting to 0.0.0.0
 	HealthzBindAddress string `json:"healthzBindAddress"`
 	// metricsBindAddress is the IP address and port for the metrics server to serve on,
 	// defaulting to 127.0.0.1:10249 (set to 0.0.0.0 for all interfaces)
