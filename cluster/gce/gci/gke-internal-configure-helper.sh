a22061920f2fa61f467b2a8807e5cc3b51776301
Slawomir Chylek <schylek@google.com>
2018-07-23 12:48:06 +0200

    Squash VPA patches in master
    Change-Id: I0488604e61423284b3e5cb056d6b05b4b11efdc2
    Change-Id: I48021bc0ee86399e5a665325c145e9acbdb09a0d
    Change-Id: Ia997ea692ba9544e43ab1a280d41c2dfebbc68e3
    Change-Id: I14a1485b31de8a20a935f96140bc25c4d97a7e76
    Change-Id: I83f96747a1d06ac18b5c8f42a9bb9e8584ab4406
    Change-Id: Iebfa73a605b7762be5204b4155bb0d8691615cc2
    Change-Id: Ic701c40eb8e341b25fdb4f88a1eb3573dc6505b7
    Change-Id: Ib8ddec4de98ef7b88ee029ca09149c0510647d9c
    Change-Id: Ia088cd9f3347081ebf3dd4bf213cead113bc6d96
    Change-Id: I70305c3f59cbb146cb09da735d543efea9941b2a
    Change-Id: Ib5de053d8b106b1bce2a87e1fb20dcbcc64b58ed
    Change-Id: I0b6e64562eda8b89c331a2f06ddca2c12380e809
    Change-Id: I0285ec21d07278f443e422e6ef131308bcce9305
    Change-Id: Iccc9f7517a6fa12551e5ba17418bb99ef3adcdae
    Change-Id: I990b57dc2d7183a3eab3abf1e6b49dce0acd3518
    Change-Id: I79f913e60abe8109d335b59bf61d8511bde08851
    Change-Id: I676b8089e4dc2d9d8a952a20e7aad870e7c8dc2e
    Change-Id: I54ad248d3cccbd45dd60b9393c3679b893862e87

--- cluster/gce/gci/gke-internal-configure-helper.sh
+++ cluster/gce/gci/gke-internal-configure-helper.sh
@@ -28,9 +28,134 @@ function start_nap {
   fi
 }
 
+function add_vpa_admission_webhook_host {
+  original_ipv6_loopback_line=`grep "^::1[[:space:]]" /etc/hosts`
+  tmp_file=`mktemp`
+  grep -v "^::1[[:space:]]" /etc/hosts >${tmp_file}
+  cat ${tmp_file} >/etc/hosts
+  if [[ -n "${original_ipv6_loopback_line:-}" ]]; then
+    echo "${original_ipv6_loopback_line} vpa.admissionwebhook.localhost" >>/etc/hosts
+  else
+    echo "::1 vpa.admissionwebhook.localhost" >>/etc/hosts
+  fi
+}
+
+function start_vertical_pod_autoscaler {
+  if [[ "${ENABLE_VERTICAL_POD_AUTOSCALER:-}" == "true" ]]; then
+    echo "Start Vertical Pod Autoscaler (VPA)"
+    generate_vertical_pod_autoscaler_admission_controller_certs
+    add_vpa_admission_webhook_host
+
+    local -r manifests_dir="${KUBE_HOME}/kube-manifests/kubernetes/gci-trusty"
+
+    mkdir -p "${manifests_dir}/vertical-pod-autoscaler"
+
+    cp "${manifests_dir}/internal-vpa-crd.yaml" "${manifests_dir}/vertical-pod-autoscaler"
+    cp "${manifests_dir}/internal-vpa-rbac.yaml" "${manifests_dir}/vertical-pod-autoscaler"
+    setup-addon-manifests "addons" "vertical-pod-autoscaler"
+
+    for component in admission-controller recommender updater; do
+      token=$(dd if=/dev/urandom bs=128 count=1 2>/dev/null | base64 | tr -d "=+/" | dd bs=32 count=1 2>/dev/null)
+      append_or_replace_prefixed_line /etc/srv/kubernetes/known_tokens.csv "${token}," "vpa-${component},uid:vpa-${component}"
+      create-vpa-kubeconfig vpa-${component} ${token}
+
+      # Prepare manifest
+      local src_file="${manifests_dir}/internal-vpa-${component}.manifest"
+
+      sed -i -e "s@{{cloud_config_mount}}@${CLOUD_CONFIG_MOUNT}@g" "${src_file}"
+      sed -i -e "s@{{cloud_config_volume}}@${CLOUD_CONFIG_VOLUME}@g" "${src_file}"
+      sed -i -e "s@{%.*%}@@g" "${src_file}"
+
+      cp "${src_file}" /etc/kubernetes/manifests
+    done
+
+  fi
+}
+
+function base64_decode_or_die {
+  local variable_name=$1
+  local out_file=$2
+  if [[ -n "${!variable_name}" ]]; then
+    if ! base64 -d - <<<${!variable_name} >${out_file}; then
+      echo "==error base 64 decoding ${variable_name}=="
+      echo "==the value of the variable is ${!variable_name}=="
+      exit 1
+    fi
+  else
+    echo "==VPA enabled but ${variable_name} is not set=="
+    exit 1
+  fi
+}
+
+function generate_vertical_pod_autoscaler_admission_controller_certs {
+  local certs_dir="/etc/tls-certs" #TODO: what is the best place for certs?
+  echo "Generating certs for the VPA Admission Controller in ${certs_dir}."
+  mkdir -p ${certs_dir}
+  if [[ -n "${CA_CERT:-}" ]] && [[ -n "${VPA_AC_KEY:-}" ]] && [[ -n "${VPA_AC_CERT:-}" ]]; then
+    base64_decode_or_die "CA_CERT" ${certs_dir}/caCert.pem
+    base64_decode_or_die "VPA_AC_KEY" ${certs_dir}/serverKey.pem
+    base64_decode_or_die "VPA_AC_CERT" ${certs_dir}/serverCert.pem
+  elif [[ "${MULTIMASTER:-}" == "true" ]]; then
+    echo "==At least one of CA_CERT, VPA_AC_KEY, VPA_AC_CERT is missing for multi master cluster=="
+    exit 1
+  else
+    # TODO(b/119761988): Stop falling back when it's safe.
+    echo "At least one of CA_CERT, VPA_AC_KEY, VPA_AC_CERT is missing, falling back to generating certificates"
+    cat > ${certs_dir}/server.conf << EOF
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+extendedKeyUsage = clientAuth, serverAuth
+subjectAltName=DNS:localhost
+subjectAltName=DNS:vpa.admissionwebhook.localhost
+EOF
+
+    # Create a certificate authority
+    openssl genrsa -out ${certs_dir}/caKey.pem 2048
+    openssl req -x509 -new -nodes -key ${certs_dir}/caKey.pem -days 100000 -out ${certs_dir}/caCert.pem -subj "/CN=gke_vpa_webhook_ca"
+
+    # Create a server certiticate
+    openssl genrsa -out ${certs_dir}/serverKey.pem 2048
+    # Note the CN is the DNS name of the service of the webhook.
+    openssl req -new -key ${certs_dir}/serverKey.pem -out ${certs_dir}/server.csr -subj "/CN=vpa.admissionwebhook.localhost" -config ${certs_dir}/server.conf
+    openssl x509 -req -in ${certs_dir}/server.csr -CA ${certs_dir}/caCert.pem -CAkey ${certs_dir}/caKey.pem -CAcreateserial -out ${certs_dir}/serverCert.pem -days 100000 -extensions v3_req -extfile ${certs_dir}/server.conf
+  fi
+}
+
+function create-vpa-kubeconfig {
+  component_=$1
+  token_=$2
+  echo "Creating kubeconfig file for VPA component ${component_}"
+  mkdir -p /etc/srv/kubernetes/${component_}
+  cat <<EOF >/etc/srv/kubernetes/${component_}/kubeconfig
+apiVersion: v1
+kind: Config
+users:
+- name: ${component_}
+  user:
+    token: ${token_}
+clusters:
+- name: local
+  cluster:
+    insecure-skip-tls-verify: true
+    server: https://localhost:443
+contexts:
+- context:
+    cluster: local
+    user: ${component_}
+  name: ${component_}
+current-context: ${component_}
+EOF
+}
+
 function gke-internal-master-start {
   echo "Internal GKE configuration start"
   compute-master-manifest-variables
   start_nap
+  start_vertical_pod_autoscaler
   echo "Internal GKE configuration done"
 }
