5bf3b2119b7d697ad202c7d9ff414ac2b37d1ba5
Pengfei Ni <peni@microsoft.com>
2018-07-25 17:39:07 +0800

    Update compute API to 2018-04-01

--- pkg/cloudprovider/providers/azure/azure_fakes.go
+++ pkg/cloudprovider/providers/azure/azure_fakes.go
@@ -29,7 +29,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/kubernetes/pkg/cloudprovider"
 
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2017-12-01/compute"
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-04-01/compute"
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2017-09-01/network"
 	"github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2017-10-01/storage"
 	"github.com/Azure/go-autorest/autorest"
