68988e5df801700d0832d9aac5102e346f14c493
kangsub song <thdrnsdk@naver.com>
2018-08-11 08:20:47 +0900

    Delete not used imported package
    Delete not used imported package

--- test/e2e/common/configmap_volume.go
+++ test/e2e/common/configmap_volume.go
@@ -27,7 +27,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/uuid"
 	"k8s.io/kubernetes/test/e2e/framework"
-	imageutils "k8s.io/kubernetes/test/utils/image"
 )
 
 var _ = Describe("[sig-storage] ConfigMap", func() {
