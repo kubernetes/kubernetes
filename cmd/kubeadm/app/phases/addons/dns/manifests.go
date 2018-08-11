25f9909cbad44a6fd460f37913b90255d3be52a0
Di Xu <stephenhsu90@gmail.com>
2018-08-11 22:30:25 +0800

    add --dns-loop-detect option to dnsmasq run by kube-dns

--- cmd/kubeadm/app/phases/addons/dns/manifests.go
+++ cmd/kubeadm/app/phases/addons/dns/manifests.go
@@ -122,6 +122,7 @@ spec:
         - -k
         - --cache-size=1000
         - --no-negcache
+        - --dns-loop-detect
         - --log-facility=-
         - --server=/{{ .DNSDomain }}/{{ .DNSBindAddr }}#10053
         - --server=/in-addr.arpa/{{ .DNSBindAddr }}#10053
