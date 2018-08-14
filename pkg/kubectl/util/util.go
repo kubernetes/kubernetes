78602cea5679cae042cf7122c3ec8438273e8119
kondapally1989 <kondapally9@gmail.com>
2018-08-14 22:50:46 +0530

    add support for ~ operator in filePath, fix alignment

--- pkg/kubectl/util/util.go
+++ pkg/kubectl/util/util.go
@@ -17,38 +17,38 @@ limitations under the License.
 package util
 
 import (
-    "crypto/md5"
-    "errors"
-    "fmt"
-    "path"
-    "path/filepath"
-    "strings"
-    "time"
-    "os"
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
+	"os"
     runtimeOs "runtime"
 
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    "k8s.io/apimachinery/pkg/runtime"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // ParseRFC3339 parses an RFC3339 date in either RFC3339Nano or RFC3339 format.
 func ParseRFC3339(s string, nowFn func() metav1.Time) (metav1.Time, error) {
-    if t, timeErr := time.Parse(time.RFC3339Nano, s); timeErr == nil {
-        return metav1.Time{Time: t}, nil
-    }
-    t, err := time.Parse(time.RFC3339, s)
-    if err != nil {
-        return metav1.Time{}, err
-    }
-    return metav1.Time{Time: t}, nil
+	if t, timeErr := time.Parse(time.RFC3339Nano, s); timeErr == nil {
+		return metav1.Time{Time: t}, nil
+	}
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		return metav1.Time{}, err
+	}
+	return metav1.Time{Time: t}, nil
 }
 
 func HashObject(obj runtime.Object, codec runtime.Codec) (string, error) {
-    data, err := runtime.Encode(codec, obj)
-    if err != nil {
-        return "", err
-    }
-    return fmt.Sprintf("%x", md5.Sum(data)), nil
+	data, err := runtime.Encode(codec, obj)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", md5.Sum(data)), nil
 }
 
 // ParseFileSource parses the source given.
@@ -60,37 +60,37 @@ func HashObject(obj runtime.Object, codec runtime.Codec) (string, error) {
 //
 // Key names cannot include '='.
 func ParseFileSource(source string) (keyName, filePath string, err error) {
-    numSeparators := strings.Count(source, "=")
-    switch {
-    case numSeparators == 0:
-        return path.Base(filepath.ToSlash(source)), parseFilePath(source), nil
-    case numSeparators == 1 && strings.HasPrefix(source, "="):
-        return "", "", fmt.Errorf("key name for file path %v missing.", strings.TrimPrefix(source, "="))
-    case numSeparators == 1 && strings.HasSuffix(source, "="):
-        return "", "", fmt.Errorf("file path for key name %v missing.", strings.TrimSuffix(source, "="))
-    case numSeparators > 1:
-        return "", "", errors.New("Key names or file paths cannot contain '='.")
-    default:
-        components := strings.Split(source, "=")
-        return components[0], parseFilePath(components[1]), nil
-    }
+	numSeparators := strings.Count(source, "=")
+	switch {
+	case numSeparators == 0:
+		return path.Base(filepath.ToSlash(source)), parseFilePath(source), nil
+	case numSeparators == 1 && strings.HasPrefix(source, "="):
+		return "", "", fmt.Errorf("key name for file path %v missing.", strings.TrimPrefix(source, "="))
+	case numSeparators == 1 && strings.HasSuffix(source, "="):
+		return "", "", fmt.Errorf("file path for key name %v missing.", strings.TrimSuffix(source, "="))
+	case numSeparators > 1:
+		return "", "", errors.New("Key names or file paths cannot contain '='.")
+	default:
+		components := strings.Split(source, "=")
+		return components[0], parseFilePath(components[1]), nil
+	}
 }
 
 // ParseLiteralSource parses the source key=val pair into its component pieces.
 // This functionality is distinguished from strings.SplitN(source, "=", 2) since
 // it returns an error in the case of empty keys, values, or a missing equals sign.
 func ParseLiteralSource(source string) (keyName, value string, err error) {
-    // leading equal is invalid
-    if strings.Index(source, "=") == 0 {
-        return "", "", fmt.Errorf("invalid literal source %v, expected key=value", source)
-    }
-    // split after the first equal (so values can have the = character)
-    items := strings.SplitN(source, "=", 2)
-    if len(items) != 2 {
-        return "", "", fmt.Errorf("invalid literal source %v, expected key=value", source)
-    }
+	// leading equal is invalid
+	if strings.Index(source, "=") == 0 {
+		return "", "", fmt.Errorf("invalid literal source %v, expected key=value", source)
+	}
+	// split after the first equal (so values can have the = character)
+	items := strings.SplitN(source, "=", 2)
+	if len(items) != 2 {
+		return "", "", fmt.Errorf("invalid literal source %v, expected key=value", source)
+	}
 
-    return items[0], items[1], nil
+	return items[0], items[1], nil
 }
 
 // this function returns home directory and has been copied from 
