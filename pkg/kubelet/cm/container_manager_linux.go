e87123e8a887a39b478af7869a050c6a929be6f4
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2018-08-21 16:45:04 +0900

    kubelet/cm: ignore sysctl error when running in userns
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- pkg/kubelet/cm/container_manager_linux.go
+++ pkg/kubelet/cm/container_manager_linux.go
@@ -32,6 +32,7 @@ import (
 	"github.com/opencontainers/runc/libcontainer/cgroups"
 	"github.com/opencontainers/runc/libcontainer/cgroups/fs"
 	"github.com/opencontainers/runc/libcontainer/configs"
+	libcontainersystem "github.com/opencontainers/runc/libcontainer/system"
 	"k8s.io/klog"
 	utilio "k8s.io/utils/io"
 	"k8s.io/utils/mount"
@@ -415,7 +416,11 @@ func setupKernelTunables(option KernelTunableBehavior) error {
 			klog.V(2).Infof("Updating kernel flag: %v, expected value: %v, actual value: %v", flag, expectedValue, val)
 			err = sysctl.SetSysctl(flag, expectedValue)
 			if err != nil {
-				errList = append(errList, err)
+				if libcontainersystem.RunningInUserNS() {
+					klog.Warningf("Updating kernel flag failed: %v: %v (running in UserNS)", flag, err)
+				} else {
+					errList = append(errList, err)
+				}
 			}
 		}
 	}
