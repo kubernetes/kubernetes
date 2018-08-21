9d889c94611919d53ed9860ab19c2ffb8dfaf211
Maciej Borsz <maciejborsz@google.com>
2018-08-21 10:12:13 +0200

    Fix validation logic in E2ETestNodePreparer.

--- test/e2e/framework/util.go
+++ test/e2e/framework/util.go
@@ -4876,8 +4876,8 @@ func NewE2ETestNodePreparer(client clientset.Interface, countToStrategy []testut
 func (p *E2ETestNodePreparer) PrepareNodes() error {
 	nodes := GetReadySchedulableNodesOrDie(p.client)
 	numTemplates := 0
-	for k := range p.countToStrategy {
-		numTemplates += k
+	for _, v := range p.countToStrategy {
+		numTemplates += v.Count
 	}
 	if numTemplates > len(nodes.Items) {
 		return fmt.Errorf("Can't prepare Nodes. Got more templates than existing Nodes.")
