f0660c134cb79ba4d2a6d47486c12760ef78e420
noqcks <benny@noqcks.io>
2018-09-07 23:32:07 -0400

    changing package name from watch to versioned

--- staging/src/k8s.io/client-go/rest/watch/doc.go
+++ staging/src/k8s.io/client-go/rest/watch/doc.go
@@ -14,4 +14,4 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package watch // import "k8s.io/client-go/rest/watch"
+package versioned // import "k8s.io/client-go/rest/watch"
