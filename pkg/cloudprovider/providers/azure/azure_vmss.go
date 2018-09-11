b7280a001a0eb7582612dc41c46635ea15ad73aa
Pengfei Ni <peni@microsoft.com>
2018-09-11 12:59:13 +0800

    Get public IP for Azure vmss nodes
    This is required for getting public IP when --public-ip-per-vm
    (publicIpAddressConfiguration) is enabled for vmss.

--- pkg/cloudprovider/providers/azure/azure_vmss.go
+++ pkg/cloudprovider/providers/azure/azure_vmss.go
@@ -244,8 +244,6 @@ func (ss *scaleSet) GetPrimaryVMSetName() string {
 }
 
 // GetIPByNodeName gets machine private IP and public IP by node name.
-// TODO(feiskyer): Azure vmss doesn't support associating a public IP to single virtual machine yet,
-// fix this after it is supported.
 func (ss *scaleSet) GetIPByNodeName(nodeName, vmSetName string) (string, string, error) {
 	nic, err := ss.GetPrimaryInterface(nodeName, vmSetName)
 	if err != nil {
@@ -259,8 +257,25 @@ func (ss *scaleSet) GetIPByNodeName(nodeName, vmSetName string) (string, string,
 		return "", "", err
 	}
 
-	targetIP := *ipConfig.PrivateIPAddress
-	return targetIP, "", nil
+	internalIP := *ipConfig.PrivateIPAddress
+	publicIP := ""
+	if ipConfig.PublicIPAddress != nil && ipConfig.PublicIPAddress.ID != nil {
+		pipID := *ipConfig.PublicIPAddress.ID
+		pipName, err := getLastSegment(pipID)
+		if err != nil {
+			return "", "", fmt.Errorf("failed to get publicIP name for node %q with pipID %q", nodeName, pipID)
+		}
+
+		pip, existsPip, err := ss.getPublicIPAddress(ss.ResourceGroup, pipName)
+		if err != nil {
+			return "", "", err
+		}
+		if existsPip {
+			publicIP = *pip.IPAddress
+		}
+	}
+
+	return internalIP, publicIP, nil
 }
 
 // This returns the full identifier of the primary NIC for the given VM.
