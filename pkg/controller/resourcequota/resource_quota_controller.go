726df3434b7fce554c90253d52d5a627af2ae395
nebulazhang <1049704936@qq.com>
2018-09-14 17:06:58 +0800

    reduce resource quota version conflict in controller

--- pkg/controller/resourcequota/resource_quota_controller.go
+++ pkg/controller/resourcequota/resource_quota_controller.go
@@ -243,8 +243,12 @@ func (rq *ResourceQuotaController) addQuota(obj interface{}) {
 // worker runs a worker thread that just dequeues items, processes them, and marks them done.
 func (rq *ResourceQuotaController) worker(queue workqueue.RateLimitingInterface) func() {
 	workFunc := func() bool {
+		if queue.Len() == 0 {
+			return true
+		}
 		key, quit := queue.Get()
 		if quit {
+			glog.Infof("resource quota controller worker shutting down")
 			return true
 		}
 		defer queue.Done(key)
@@ -263,7 +267,6 @@ func (rq *ResourceQuotaController) worker(queue workqueue.RateLimitingInterface)
 	return func() {
 		for {
 			if quit := workFunc(); quit {
-				glog.Infof("resource quota controller worker shutting down")
 				return
 			}
 		}
