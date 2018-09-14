57d28e149fd08b787e11431c129d291302eaaa6b
choury <choury@users.noreply.github.com>
2018-09-14 13:20:04 +0800

    Don't skip sync when pod failed

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -1542,7 +1542,7 @@ func (kl *Kubelet) syncPod(o syncPodOptions) error {
 	kl.statusManager.SetPodStatus(pod, apiPodStatus)
 
 	// Kill pod if it should not be running
-	if !runnable.Admit || pod.DeletionTimestamp != nil || apiPodStatus.Phase == v1.PodFailed {
+	if !runnable.Admit || pod.DeletionTimestamp != nil {
 		var syncErr error
 		if err := kl.killPod(pod, nil, podStatus, nil); err != nil {
 			kl.recorder.Eventf(pod, v1.EventTypeWarning, events.FailedToKillPod, "error killing pod: %v", err)
