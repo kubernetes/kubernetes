ba35cabe5e31cbecfe3c9b2ae71b30d39f6d05d6
tianshapjq <pu.junqi@zte.com.cn>
2018-09-25 17:16:51 +0800

    mark Node.Spec.Unschedulable deprecated

--- staging/src/k8s.io/api/core/v1/types_swagger_doc_generated.go
+++ staging/src/k8s.io/api/core/v1/types_swagger_doc_generated.go
@@ -1087,7 +1087,7 @@ var map_NodeSpec = map[string]string{
 	"":              "NodeSpec describes the attributes that a node is created with.",
 	"podCIDR":       "PodCIDR represents the pod IP range assigned to the node.",
 	"providerID":    "ID of the node assigned by the cloud provider in the format: <ProviderName>://<ProviderSpecificNodeID>",
-	"unschedulable": "Unschedulable controls node schedulability of new pods. By default, node is schedulable. More info: https://kubernetes.io/docs/concepts/nodes/node/#manual-node-administration",
+	"unschedulable": "Deprecated. TaintNodeByCondition has replaced the existing node condition. After that feature becomes GA, this will be removed. see: https://github.com/kubernetes/kubernetes/issues/69010",
 	"taints":        "If specified, the node's taints.",
 	"configSource":  "If specified, the source to get node configuration from The DynamicKubeletConfig feature gate must be enabled for the Kubelet to use this field",
 	"externalID":    "Deprecated. Not all kubelets will set this field. Remove field after 1.13. see: https://issues.k8s.io/61966",
