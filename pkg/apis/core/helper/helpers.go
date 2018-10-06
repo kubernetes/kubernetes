kubernetes/kubernetes

commit 42da186b629b7edaaeada9acfdaa0b610e0e842f

Address review comments

pkg/kubelet/kubelet_pods.go
@@ -488,7 +488,7 @@ var masterServices = sets.NewString("kubernetes")
 
 // getServiceEnvVarMap makes a map[string]string of env vars for services a
 // pod in namespace ns should see.
-func (kl *Kubelet) getServiceEnvVarMap(ns string, enableServiceLinks *bool) (map[string]string, error) {
+func (kl *Kubelet) getServiceEnvVarMap(ns string, enableServiceLinks bool) (map[string]string, error) {
 	var (
 		serviceMap = make(map[string]*v1.Service)
 		m          = make(map[string]string)
@@ -522,7 +522,7 @@ func (kl *Kubelet) getServiceEnvVarMap(ns string, enableServiceLinks *bool) (map
 			if _, exists := serviceMap[serviceName]; !exists {
 				serviceMap[serviceName] = service
 			}
-		} else if service.Namespace == ns && *enableServiceLinks {
+		} else if service.Namespace == ns && enableServiceLinks {
 			serviceMap[serviceName] = service
 		}
 	}
@@ -550,7 +550,7 @@ func (kl *Kubelet) makeEnvironmentVariables(pod *v1.Pod, container *v1.Container
 	// To avoid this users can: (1) wait between starting a service and starting; or (2) detect
 	// missing service env var and exit and be restarted; or (3) use DNS instead of env vars
 	// and keep trying to resolve the DNS name of the service (recommended).
-	serviceEnv, err := kl.getServiceEnvVarMap(pod.Namespace, pod.Spec.EnableServiceLinks)
+	serviceEnv, err := kl.getServiceEnvVarMap(pod.Namespace, *pod.Spec.EnableServiceLinks)
 	if err != nil {
 		return result, err
 	}

pkg/api/testing/pod_specs.go
@@ -24,7 +24,7 @@ import (
 // DeepEqualSafePodSpec returns a PodSpec which is ready to be used with apiequality.Semantic.DeepEqual
 func DeepEqualSafePodSpec() api.PodSpec {
 	grace := int64(30)
-	enableServiceLinks := api.DefaultEnableServiceLinks
+	enableServiceLinks := v1.DefaultEnableServiceLinks
 	return api.PodSpec{
 		RestartPolicy:                 api.RestartPolicyAlways,
 		DNSPolicy:                     api.DNSClusterFirst,
@@ -38,7 +38,7 @@ func DeepEqualSafePodSpec() api.PodSpec {
 // V1DeepEqualSafePodSpec returns a PodSpec which is ready to be used with apiequality.Semantic.DeepEqual
 func V1DeepEqualSafePodSpec() v1.PodSpec {
 	grace := int64(30)
-	enableServiceLinks := api.DefaultEnableServiceLinks
+	enableServiceLinks := v1.DefaultEnableServiceLinks
 	return v1.PodSpec{
 		RestartPolicy:                 v1.RestartPolicyAlways,
 		DNSPolicy:                     v1.DNSClusterFirst,

pkg/api/testing/serialization_test.go
@@ -214,7 +214,7 @@ func TestRoundTripTypes(t *testing.T) {
 // decoded without information loss or mutation.
 func TestEncodePtr(t *testing.T) {
 	grace := int64(30)
-	enableServiceLinks := api.DefaultEnableServiceLinks
+	enableServiceLinks := v1.DefaultEnableServiceLinks
 	pod := &api.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Labels: map[string]string{"name": "foo"},

pkg/apis/apps/v1/defaults_test.go
@@ -39,7 +39,7 @@ func TestSetDefaultDaemonSetSpec(t *testing.T) {
 	defaultLabels := map[string]string{"foo": "bar"}
 	maxUnavailable := intstr.FromInt(1)
 	period := int64(v1.DefaultTerminationGracePeriodSeconds)
-	enableServiceLinks := api.DefaultEnableServiceLinks
+	enableServiceLinks := v1.DefaultEnableServiceLinks
 	defaultTemplate := v1.PodTemplateSpec{
 		Spec: v1.PodSpec{
 			DNSPolicy:                     v1.DNSClusterFirst,
@@ -178,7 +178,7 @@ func TestSetDefaultStatefulSet(t *testing.T) {
 	var defaultReplicas int32 = 1
 
 	period := int64(v1.DefaultTerminationGracePeriodSeconds)
-	enableServiceLinks := api.DefaultEnableServiceLinks
+	enableServiceLinks := v1.DefaultEnableServiceLinks
 	defaultTemplate := v1.PodTemplateSpec{
 		Spec: v1.PodSpec{
 			DNSPolicy:                     v1.DNSClusterFirst,
@@ -291,7 +291,7 @@ func TestSetDefaultDeployment(t *testing.T) {
 	defaultIntOrString := intstr.FromString("25%")
 	differentIntOrString := intstr.FromInt(5)
 	period := int64(v1.DefaultTerminationGracePeriodSeconds)
-	enableServiceLinks := api.DefaultEnableServiceLinks
+	enableServiceLinks := v1.DefaultEnableServiceLinks
 	defaultTemplate := v1.PodTemplateSpec{
 		Spec: v1.PodSpec{
 			DNSPolicy:                     v1.DNSClusterFirst,

pkg/apis/apps/v1beta1/defaults_test.go
@@ -38,7 +38,7 @@ func TestSetDefaultDeployment(t *testing.T) {
 	defaultIntOrString := intstr.FromString("25%")
 	differentIntOrString := intstr.FromInt(5)
 	period := int64(v1.DefaultTerminationGracePeriodSeconds)
-	enableServiceLinks := api.DefaultEnableServiceLinks
+	enableServiceLinks := v1.DefaultEnableServiceLinks
 	defaultTemplate := v1.PodTemplateSpec{
 		Spec: v1.PodSpec{
 			DNSPolicy:                     v1.DNSClusterFirst,

pkg/apis/apps/v1beta2/defaults_test.go
@@ -39,7 +39,7 @@ func TestSetDefaultDaemonSetSpec(t *testing.T) {
 	defaultLabels := map[string]string{"foo": "bar"}
 	maxUnavailable := intstr.FromInt(1)
 	period := int64(v1.DefaultTerminationGracePeriodSeconds)
-	enableServiceLinks := api.DefaultEnableServiceLinks
+	enableServiceLinks := v1.DefaultEnableServiceLinks
 	defaultTemplate := v1.PodTemplateSpec{
 		Spec: v1.PodSpec{
 			DNSPolicy:                     v1.DNSClusterFirst,
@@ -178,7 +178,7 @@ func TestSetDefaultStatefulSet(t *testing.T) {
 	var defaultReplicas int32 = 1
 
 	period := int64(v1.DefaultTerminationGracePeriodSeconds)
-	enableServiceLinks := api.DefaultEnableServiceLinks
+	enableServiceLinks := v1.DefaultEnableServiceLinks
 	defaultTemplate := v1.PodTemplateSpec{
 		Spec: v1.PodSpec{
 			DNSPolicy:                     v1.DNSClusterFirst,
@@ -291,7 +291,7 @@ func TestSetDefaultDeployment(t *testing.T) {
 	defaultIntOrString := intstr.FromString("25%")
 	differentIntOrString := intstr.FromInt(5)
 	period := int64(v1.DefaultTerminationGracePeriodSeconds)
-	enableServiceLinks := api.DefaultEnableServiceLinks
+	enableServiceLinks := v1.DefaultEnableServiceLinks
 	defaultTemplate := v1.PodTemplateSpec{
 		Spec: v1.PodSpec{
 			DNSPolicy:                     v1.DNSClusterFirst,

pkg/apis/core/fuzzer/fuzzer.go
@@ -23,6 +23,7 @@ import (
 
 	fuzz "github.com/google/gofuzz"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -85,7 +86,7 @@ var Funcs = func(codecs runtimeserializer.CodecFactory) []interface{} {
 				s.SchedulerName = core.DefaultSchedulerName
 			}
 			if s.EnableServiceLinks == nil {
-				enableServiceLinks := core.DefaultEnableServiceLinks
+				enableServiceLinks := corev1.DefaultEnableServiceLinks
 				s.EnableServiceLinks = &enableServiceLinks
 			}
 		},

pkg/apis/core/types.go
@@ -2604,11 +2604,6 @@ type PodSpec struct {
 	EnableServiceLinks *bool
 }
 
-const (
-	// The default value for enableServiceLinks attribute.
-	DefaultEnableServiceLinks = true
-)
-
 // HostAlias holds the mapping between IP and hostnames that will be injected as an entry in the
 // pod's hosts file.
 type HostAlias struct {

commit 3b4cae54dd8cf2fe06f8612a09e08268847e56f9

fix crash in makeEnvironmentVariables during getServiceEnvVarMap
This patch fixes bug, which was introduced by  42da186,
e8366c8 commits. New helper is intended to prevent crashing
in case when EnableServiceLinks is nil.
This commit doesn't touch kubelet_pods_test.go, but maybe it
worth.

Signed-off-by: Alexey Perevalov <a.perevalov@samsung.com>
Fixes: #69494

pkg/apis/core/helper/helpers.go
@@ -537,3 +537,11 @@ func PersistentVolumeClaimHasClass(claim *core.PersistentVolumeClaim) bool {
 
 	return false
 }
+
+func GetEnableServiceLinks(ps *core.PodSpec) bool {
+	if ps != nil && ps.EnableServiceLinks != nil {
+		return *ps.EnableServiceLinks
+	} else {
+		return true
+	}
+}

pkg/apis/core/v1/helper/helpers.go
@@ -525,3 +525,11 @@ func ScopedResourceSelectorRequirementsAsSelector(ssr v1.ScopedResourceSelectorR
 	selector = selector.Add(*r)
 	return selector, nil
 }
+
+func GetEnableServiceLinks(ps *v1.PodSpec) bool {
+	if ps != nil && ps.EnableServiceLinks != nil {
+		return *ps.EnableServiceLinks
+	} else {
+		return true
+	}
+}

pkg/kubelet/kubelet_pods.go
@@ -550,7 +550,7 @@ func (kl *Kubelet) makeEnvironmentVariables(pod *v1.Pod, container *v1.Container
 	// To avoid this users can: (1) wait between starting a service and starting; or (2) detect
 	// missing service env var and exit and be restarted; or (3) use DNS instead of env vars
 	// and keep trying to resolve the DNS name of the service (recommended).
-	serviceEnv, err := kl.getServiceEnvVarMap(pod.Namespace, *pod.Spec.EnableServiceLinks)
+	serviceEnv, err := kl.getServiceEnvVarMap(pod.Namespace, v1helper.GetEnableServiceLinks(&pod.Spec))
 	if err != nil {
 		return result, err
 	}