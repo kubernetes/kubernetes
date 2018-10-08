3d7848758c5f79b1d313f52b584e925b32262c69
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-10-08 12:41:17 +0200

    webhook admission: add status error test coverage and fix 403+500 error messages

--- staging/src/k8s.io/apiserver/pkg/admission/plugin/webhook/errors/statuserror.go
+++ staging/src/k8s.io/apiserver/pkg/admission/plugin/webhook/errors/statuserror.go
@@ -18,27 +18,72 @@ package errors
 
 import (
 	"fmt"
+	"net/http"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apiserver/pkg/admission"
 )
 
 // ToStatusErr returns a StatusError with information about the webhook plugin
-func ToStatusErr(webhookName string, result *metav1.Status) *apierrors.StatusError {
-	deniedBy := fmt.Sprintf("admission webhook %q denied the request", webhookName)
-	const noExp = "without explanation"
-
+func ToStatusErr(attr admission.Attributes, webhookName string, result *metav1.Status) *apierrors.StatusError {
 	if result == nil {
-		result = &metav1.Status{Status: metav1.StatusFailure}
+		result = &metav1.Status{}
+	} else {
+		// deepcopy before mutating it
+		result = result.DeepCopy()
 	}
 
+	gr := attr.GetResource().GroupResource()
+	name := attr.GetName()
+	if len(name) == 0 {
+		name = "Unknown"
+	}
+
+	// sanitize status
+	if result.Code == 0 {
+		result.Code = http.StatusForbidden
+	}
+	if len(result.Status) == 0 {
+		result.Status = metav1.StatusFailure
+	}
+
+	deniedBy := fmt.Sprintf("admission webhook %q denied the request", webhookName)
 	switch {
 	case len(result.Message) > 0:
 		result.Message = fmt.Sprintf("%s: %s", deniedBy, result.Message)
 	case len(result.Reason) > 0:
 		result.Message = fmt.Sprintf("%s: %s", deniedBy, result.Reason)
 	default:
-		result.Message = fmt.Sprintf("%s %s", deniedBy, noExp)
+		result.Message = fmt.Sprintf("%s without explanation", deniedBy)
+	}
+
+	if result.Details == nil {
+		if result.Code == http.StatusInternalServerError {
+			result.Details = &metav1.StatusDetails{
+				Causes: []metav1.StatusCause{{Message: result.Message}},
+			}
+		} else {
+			result.Details = &metav1.StatusDetails{
+				Group: attr.GetResource().Group,
+				Kind:  attr.GetResource().Resource, // yes, this is odd. But we replicate apierrors.NewForbidden here which does the same.
+				Name:  name,
+			}
+		}
+	}
+
+	prefix := fmt.Sprintf("%s %q is forbidden", gr.String(), name)
+	if result.Code == http.StatusInternalServerError {
+		prefix = "Internal error occurred"
+	}
+	result.Message = fmt.Sprintf("%s: %s", prefix, result.Message)
+
+	if len(result.Reason) == 0 {
+		if result.Code == http.StatusInternalServerError {
+			result.Reason = metav1.StatusReasonInternalError
+		} else {
+			result.Reason = metav1.StatusReasonForbidden
+		}
 	}
 
 	return &apierrors.StatusError{
--- staging/src/k8s.io/apiserver/pkg/admission/plugin/webhook/errors/statuserror_test.go
+++ staging/src/k8s.io/apiserver/pkg/admission/plugin/webhook/errors/statuserror_test.go
@@ -18,56 +18,132 @@ package errors
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 
+	"k8s.io/apimachinery/pkg/api/equality"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/diff"
+	"k8s.io/apiserver/pkg/admission"
+	"k8s.io/apiserver/pkg/authentication/user"
 )
 
 func TestToStatusErr(t *testing.T) {
 	hookName := "foo"
 	deniedBy := fmt.Sprintf("admission webhook %q denied the request", hookName)
 	tests := []struct {
-		name          string
-		result        *metav1.Status
-		expectedError string
+		name       string
+		attributes mockAttributes
+		result     *metav1.Status
+		expected   metav1.Status
 	}{
 		{
 			"nil result",
+			mockAttributes{"example"},
 			nil,
-			deniedBy + " without explanation",
+			admission.NewForbidden(mockAttributes{"example"}, fmt.Errorf(deniedBy+" without explanation")).(*apierrors.StatusError).Status(),
 		},
 		{
 			"only message",
+			mockAttributes{"example"},
 			&metav1.Status{
 				Message: "you shall not pass",
 			},
-			deniedBy + ": you shall not pass",
+			admission.NewForbidden(mockAttributes{"example"}, fmt.Errorf(deniedBy+": you shall not pass")).(*apierrors.StatusError).Status(),
 		},
 		{
 			"only reason",
+			mockAttributes{"example"},
 			&metav1.Status{
 				Reason: metav1.StatusReasonForbidden,
 			},
-			deniedBy + ": Forbidden",
+			admission.NewForbidden(mockAttributes{"example"}, fmt.Errorf(deniedBy+": Forbidden")).(*apierrors.StatusError).Status(),
 		},
 		{
 			"message and reason",
+			mockAttributes{"example"},
 			&metav1.Status{
 				Message: "you shall not pass",
 				Reason:  metav1.StatusReasonForbidden,
 			},
-			deniedBy + ": you shall not pass",
+			admission.NewForbidden(mockAttributes{"example"}, fmt.Errorf(deniedBy+": you shall not pass")).(*apierrors.StatusError).Status(),
 		},
 		{
 			"no message, no reason",
+			mockAttributes{"example"},
 			&metav1.Status{},
-			deniedBy + " without explanation",
+			admission.NewForbidden(mockAttributes{"example"}, fmt.Errorf(deniedBy+" without explanation")).(*apierrors.StatusError).Status(),
+		},
+		{
+			"unknown name",
+			mockAttributes{},
+			&metav1.Status{},
+			admission.NewForbidden(mockAttributes{}, fmt.Errorf(deniedBy+" without explanation")).(*apierrors.StatusError).Status(),
+		},
+		{
+			"500 error",
+			mockAttributes{},
+			&metav1.Status{Code: http.StatusInternalServerError, Message: "internal error"},
+			apierrors.NewInternalError(fmt.Errorf(deniedBy + ": internal error")).Status(),
 		},
 	}
 	for _, test := range tests {
-		err := ToStatusErr(hookName, test.result)
-		if err == nil || err.Error() != test.expectedError {
-			t.Errorf("%s: expected an error saying %q, but got %v", test.name, test.expectedError, err)
+		err := ToStatusErr(test.attributes, hookName, test.result)
+		if err == nil || !equality.Semantic.DeepEqual(err.Status(), test.expected) {
+			t.Errorf("%s: unexpected error (got A, expected B):\n%s", test.name, diff.ObjectDiff(err.Status(), test.expected))
 		}
 	}
+}
+
+type mockAttributes struct {
+	name string
+}
+
+var _ admission.Attributes = &mockAttributes{}
+
+func (a mockAttributes) GetKind() schema.GroupVersionKind {
+	return schema.GroupVersionKind{Group: "foo", Version: "v1", Kind: "Bar"}
+}
+
+func (a mockAttributes) GetNamespace() string {
+	return "default"
+}
+
+func (a mockAttributes) GetName() string {
+	return a.name
+}
+
+func (a mockAttributes) GetResource() schema.GroupVersionResource {
+	return schema.GroupVersionResource{Group: "foo", Version: "v1", Resource: "bars"}
+}
+
+func (a mockAttributes) GetSubresource() string {
+	return ""
+}
+
+func (a mockAttributes) GetOperation() admission.Operation {
+	return admission.Create
+}
+
+func (a mockAttributes) IsDryRun() bool {
+	return false
+}
+
+func (a mockAttributes) GetObject() runtime.Object {
+	return nil
+}
+
+func (a mockAttributes) GetOldObject() runtime.Object {
+	return nil
+}
+
+func (a mockAttributes) GetUserInfo() user.Info {
+	return nil
+}
+
+func (a mockAttributes) AddAnnotation(key, value string) error {
+	return nil
 }
