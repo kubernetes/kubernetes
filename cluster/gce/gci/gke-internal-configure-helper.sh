b476aa3478354aef51df91865b76dd60383d5dcd
Aleksandra Malinowska <aleksandram@google.com>
2018-10-11 13:50:10 +0200

    Update scripts for starting internal Cluster Autoscaler without NAP
    Change-Id: I8d3ab894ec1ea019db5a3228d38e6c202e2dcd34

--- cluster/gce/gci/gke-internal-configure-helper.sh
+++ cluster/gce/gci/gke-internal-configure-helper.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-function start_nap {
+function start_internal_cluster_autoscaler {
   if [[ "${ENABLE_NAP:-}" == "true" ]]; then
     echo "Start Node Auto-Provisioning (NAP)"
-    local -r manifests_dir="${KUBE_HOME}/kube-manifests/kubernetes/gci-trusty"
-
-    # Re-using Cluster Autoscaler setup functions from OSS
-    setup-addon-manifests "addons" "rbac/cluster-autoscaler"
-    create-clusterautoscaler-kubeconfig
-    prepare-log-file /var/log/cluster-autoscaler.log
-
-    # Add our GKE specific CRD
-    mkdir -p "${manifests_dir}/autoscaling"
-    cp "${manifests_dir}/internal-capacity-request-crd.yaml" "${manifests_dir}/autoscaling"
-    setup-addon-manifests "addons" "autoscaling"
-
-    # Prepare Autoscaler manifest
-    local -r src_file="${manifests_dir}/internal-cluster-autoscaler.manifest"
-    local params="${CLOUD_CONFIG_OPT} ${NAP_CONFIG:-}"
-
-    sed -i -e "s@{{params}}@${params:-}@g" "${src_file}"
-    sed -i -e "s@{{cloud_config_mount}}@${CLOUD_CONFIG_MOUNT}@g" "${src_file}"
-    sed -i -e "s@{{cloud_config_volume}}@${CLOUD_CONFIG_VOLUME}@g" "${src_file}"
-    sed -i -e "s@{%.*%}@@g" "${src_file}"
-
-    cp "${src_file}" /etc/kubernetes/manifests
+    start_internal_ca "${NAP_CONFIG:-} --node-autoprovisioning-enabled=true"
+  elif [[ "${ENABLE_GKE_CLUSTER_AUTOSCALER:-}" == "true" ]]; then
+    echo "Start Cluster Autoscaler from closed source"
+    start_internal_ca "${GKE_CLUSTER_AUTOSCALER_CONFIG:-}"
+  else
+    echo "Not using closed source Cluster Autoscaler"
   fi
+}
+
+function start_internal_ca {
+  local -r manifests_dir="${KUBE_HOME}/kube-manifests/kubernetes/gci-trusty"
+
+  # Re-using Cluster Autoscaler setup functions from OSS
+  setup-addon-manifests "addons" "rbac/cluster-autoscaler"
+  create-clusterautoscaler-kubeconfig
+  prepare-log-file /var/log/cluster-autoscaler.log
+
+  # Add our GKE specific CRD
+  mkdir -p "${manifests_dir}/autoscaling"
+  cp "${manifests_dir}/internal-capacity-request-crd.yaml" "${manifests_dir}/autoscaling"
+  setup-addon-manifests "addons" "autoscaling"
+
+  # Prepare Autoscaler manifest
+  local -r src_file="${manifests_dir}/internal-cluster-autoscaler.manifest"
+  local params="${CLOUD_CONFIG_OPT} $1"
+
+  sed -i -e "s@{{params}}@${params:-}@g" "${src_file}"
+  sed -i -e "s@{{cloud_config_mount}}@${CLOUD_CONFIG_MOUNT}@g" "${src_file}"
+  sed -i -e "s@{{cloud_config_volume}}@${CLOUD_CONFIG_VOLUME}@g" "${src_file}"
+  sed -i -e "s@{%.*%}@@g" "${src_file}"
+
+  cp "${src_file}" /etc/kubernetes/manifests
 }
 
 function add_vpa_admission_webhook_host {
@@ -155,7 +164,7 @@ EOF
 function gke-internal-master-start {
   echo "Internal GKE configuration start"
   compute-master-manifest-variables
-  start_nap
+  start_internal_cluster_autoscaler
   start_vertical_pod_autoscaler
   echo "Internal GKE configuration done"
 }
