067a965e1eddeb75b8429ed2a05e5fc0f0b52ef9
mooncake <xcoder@tenxcloud.com>
2018-10-14 22:18:10 +0800

    Fix typos: 'utilities'

--- pkg/kubectl/cmd/taint/utils.go
+++ pkg/kubectl/cmd/taint/utils.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package taints implements utilites for working with taints
+// Package taints implements utilities for working with taints
 package taint
 
 import (
