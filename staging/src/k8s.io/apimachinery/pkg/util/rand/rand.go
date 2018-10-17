678b7648e8f8f6ca72f521d6c8058cd888196f6f
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2018-10-17 23:13:22 +1100

    Add missing methods to mirror math/rand

--- staging/src/k8s.io/apimachinery/pkg/util/rand/rand.go
+++ staging/src/k8s.io/apimachinery/pkg/util/rand/rand.go
@@ -15,6 +15,8 @@ limitations under the License.
 */
 
 // Package rand provides utilities related to randomization.
+// Unlike math/rand, this package is safe to use because the RNG is seeded
+// on package initialization and hence there is no chance of using an unseeded RNG.
 package rand
 
 import (
@@ -30,6 +32,34 @@ var rng = struct {
 	rand: rand.New(rand.NewSource(time.Now().UnixNano())),
 }
 
+// ExpFloat64 returns an exponentially distributed float64 in the range
+// (0, +math.MaxFloat64] with an exponential distribution whose rate parameter
+// (lambda) is 1 and whose mean is 1/lambda (1).
+// To produce a distribution with a different rate parameter,
+// callers can adjust the output using:
+//
+//  sample = ExpFloat64() / desiredRateParameter
+//
+func ExpFloat64() float64 {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.ExpFloat64()
+}
+
+// Float32 returns, as a float32, a pseudo-random number in [0.0,1.0).
+func Float32() float32 {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.Float32()
+}
+
+// Float64 returns, as a float64, a pseudo-random number in [0.0,1.0).
+func Float64() float64 {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.Float64()
+}
+
 // Int returns a non-negative pseudo-random int.
 func Int() int {
 	rng.Lock()
@@ -37,12 +67,111 @@ func Int() int {
 	return rng.rand.Int()
 }
 
-// Intn generates an integer in range [0,max).
-// By design this should panic if input is invalid, <= 0.
+// Int31 returns a non-negative pseudo-random 31-bit integer as an int32.
+func Int31() int32 {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.Int31()
+}
+
+// Int31n returns, as an int32, a non-negative pseudo-random number in [0,n).
+// It panics if n <= 0.
+func Int31n(n int32) int32 {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.Int31n(n)
+}
+
+// Int63 returns a non-negative pseudo-random 63-bit integer as an int64.
+func Int63() int64 {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.Int63()
+}
+
+// Int63n returns, as an int64, a non-negative pseudo-random number in [0,n).
+// It panics if n <= 0.
+func Int63n(n int64) int64 {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.Int63n(n)
+}
+
+// Intn returns, as an int, a non-negative pseudo-random number in [0,n).
+// It panics if n <= 0.
 func Intn(max int) int {
 	rng.Lock()
 	defer rng.Unlock()
 	return rng.rand.Intn(max)
+}
+
+// NormFloat64 returns a normally distributed float64 in
+// the range -math.MaxFloat64 through +math.MaxFloat64 inclusive,
+// with standard normal distribution (mean = 0, stddev = 1).
+// To produce a different normal distribution, callers can
+// adjust the output using:
+//
+//  sample = NormFloat64() * desiredStdDev + desiredMean
+//
+func NormFloat64() float64 {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.NormFloat64()
+}
+
+// Perm returns, as a slice of n ints, a pseudo-random permutation of the integers [0,n)
+// from the default Source.
+func Perm(n int) []int {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.Perm(n)
+}
+
+// Read generates len(p) random bytes and writes them into p. It
+// always returns len(p) and a nil error.
+func Read(p []byte) (n int, err error) {
+	rng.Lock()
+	defer rng.Unlock()
+	// Godoc on the Read() method says that it should not be called concurrently with any other Rand method,
+	// but godoc on this method does not repeat that because it is safe in our case - we grab an exclusive lock.
+	return rng.rand.Read(p)
+}
+
+// Seed seeds the rng with the provided seed.
+func Seed(seed int64) {
+	UseSource(rand.NewSource(seed))
+}
+
+// UseSource makes the rng use the provided source.
+func UseSource(source rand.Source) {
+	rng.Lock()
+	defer rng.Unlock()
+
+	rng.rand = rand.New(source)
+}
+
+// Shuffle pseudo-randomizes the order of elements.
+// n is the number of elements. Shuffle panics if n < 0.
+// swap swaps the elements with indexes i and j.
+func Shuffle(n int, swap func(i, j int)) {
+	rng.Lock()
+	defer rng.Unlock()
+
+	rng.rand.Shuffle(n, swap)
+}
+
+// Uint32 returns a pseudo-random 32-bit value as a uint32.
+func Uint32() uint32 {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.Uint32()
+}
+
+// Uint64 returns a pseudo-random 64-bit value as a uint64.
+func Uint64() uint64 {
+	rng.Lock()
+	defer rng.Unlock()
+	return rng.rand.Uint64()
 }
 
 // IntnRange generates an integer in range [min,max).
@@ -61,22 +190,6 @@ func Int63nRange(min, max int64) int64 {
 	return rng.rand.Int63n(max-min) + min
 }
 
-// Seed seeds the rng with the provided seed.
-func Seed(seed int64) {
-	rng.Lock()
-	defer rng.Unlock()
-
-	rng.rand = rand.New(rand.NewSource(seed))
-}
-
-// Perm returns, as a slice of n ints, a pseudo-random permutation of the integers [0,n)
-// from the default Source.
-func Perm(n int) []int {
-	rng.Lock()
-	defer rng.Unlock()
-	return rng.rand.Perm(n)
-}
-
 const (
 	// We omit vowels from the set of available characters to reduce the chances
 	// of "bad words" being formed.
@@ -88,6 +201,13 @@ const (
 	// No. of random letters we can extract from a single int63.
 	maxAlphanumsPerInt = 63 / alphanumsIdxBits
 )
+
+// NewSource returns a new pseudo-random Source seeded with the given value.
+// Unlike the default Source used by top-level functions, this source is not
+// safe for concurrent use by multiple goroutines.
+func NewSource(seed int64) rand.Source {
+	return rand.NewSource(seed)
+}
 
 // String generates a random alphanumeric string, without vowels, which is n
 // characters long.  This will panic if n is less than zero.
