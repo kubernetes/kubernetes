803ee8426da86bcc02babb0712b3b84bc9a156ce
Matt Matejczyk <mmat@google.com>
2018-10-18 09:25:27 -0400

    Add EndpointsLastTriggerTime annotation.

--- pkg/apis/core/annotation_key_constants.go
+++ pkg/apis/core/annotation_key_constants.go
@@ -82,4 +82,10 @@ const (
 	//
 	// Not all cloud providers support this annotation, though AWS & GCE do.
 	AnnotationLoadBalancerSourceRangesKey = "service.beta.kubernetes.io/load-balancer-source-ranges"
+
+	// EndpointsLastTriggerTime is the annotation key, set for endpoints objects, that represents the
+	// timestamp of the last change that triggered the endpoints object update.
+	// This annotation will be used to compute the in-cluster network programming SLI. See
+	// https://github.com/kubernetes/community/blob/master/sig-scalability/slos/network_programming_latency.md
+	EndpointsLastTriggerTime = "kubernetes.io/endpoints.last-trigger-time"
 )
