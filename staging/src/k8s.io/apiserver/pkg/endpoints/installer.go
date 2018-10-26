31372f16129b3f6fa50e85e175643b3a43ba3da2
Neha Yadav <inehayadav28@gmail.com>
2018-10-27 00:40:10 +0530

    fix(bug): make body a optional parameter in [DELETE] /apis/batch/v1/namespaces/{namespace}/jobs/{name}

--- staging/src/k8s.io/apiserver/pkg/endpoints/installer.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/installer.go
@@ -738,6 +738,7 @@ func (a *APIInstaller) registerResourceHandlers(path string, storage rest.Storag
 				Returns(http.StatusAccepted, "Accepted", versionedStatus)
 			if isGracefulDeleter {
 				route.Reads(versionedDeleterObject)
+				route.ParameterNamed("body").Required(false)
 				if err := addObjectParams(ws, route, versionedDeleteOptions); err != nil {
 					return nil, err
 				}
