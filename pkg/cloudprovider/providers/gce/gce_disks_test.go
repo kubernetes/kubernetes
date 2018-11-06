fe0f7936a07daa49f80a888562b2f7f5d1709cc1
Jing Xu <jinxu@google.com>
2018-11-05 17:15:33 -0800

    WIP
    WIP

--- pkg/cloudprovider/providers/gce/gce_disks_test.go
+++ pkg/cloudprovider/providers/gce/gce_disks_test.go
@@ -110,7 +110,7 @@ func TestCreateRegionalDisk_Basic(t *testing.T) {
 	tags := make(map[string]string)
 	tags["test-tag"] = "test-value"
 
-	expectedDiskTypeURI := gceComputeAPIEndpointBeta + "projects/" + fmt.Sprintf(
+	expectedDiskTypeURI := gceComputeAPIEndpoint + "projects/" + fmt.Sprintf(
 		diskTypeURITemplateRegional, gceProjectID, gceRegion, diskType)
 	expectedDescription := "{\"test-tag\":\"test-value\"}"
 
@@ -723,9 +723,10 @@ func (manager *FakeServiceManager) CreateRegionalDiskOnCloudProvider(
 	tagsStr string,
 	diskType string,
 	zones sets.String) error {
-	manager.createDiskCalled = true
-	diskTypeURI := gceComputeAPIEndpointBeta + "projects/" + fmt.Sprintf(diskTypeURITemplateRegional, manager.gceProjectID, manager.gceRegion, diskType)
 
+	manager.createDiskCalled = true
+	diskTypeURI := gceComputeAPIEndpoint + "projects/" + fmt.Sprintf(diskTypeURITemplateRegional, manager.gceProjectID, manager.gceRegion, diskType)
+	glog.Infof("manager targetAPI %v", manager.targetAPI)
 	switch t := manager.targetAPI; t {
 	case targetStable:
 		diskToCreateV1 := &compute.Disk{
@@ -738,7 +739,15 @@ func (manager *FakeServiceManager) CreateRegionalDiskOnCloudProvider(
 		manager.regionalDisks[diskToCreateV1.Name] = zones
 		return nil
 	case targetBeta:
-		return fmt.Errorf("regionalDisk CreateDisk op not supported in beta")
+		diskToCreateBeta := &computebeta.Disk{
+			Name:        name,
+			SizeGb:      sizeGb,
+			Description: tagsStr,
+			Type:        diskTypeURI,
+		}
+		manager.diskToCreateBeta = diskToCreateBeta
+		manager.regionalDisks[diskToCreateBeta.Name] = zones
+		return nil
 	case targetAlpha:
 		return fmt.Errorf("regionalDisk CreateDisk op not supported in alpha")
 	default:
