4ee2010111af602988bf9bedf66d1a0c36419b09
Mikalai Radchuk <radchuk.m@gmail.com>
2018-11-12 00:06:35 +0000

    Fixes address parsing in port-forward
    The rules for address parsing are:
    
    * Explicitly specified addresses must bind successfully
    * `localhost` is pinned to `127.0.0.1` and `::1` and at least one of those must bind successfully
    
    This change also makes output of the command consistent
    between runs with the same arguments.
    Previously the command was using the range via map of addresses
    which sometimes was producing different output because the order
    of values is not guaranteed in Go.

--- staging/src/k8s.io/client-go/tools/portforward/portforward.go
+++ staging/src/k8s.io/client-go/tools/portforward/portforward.go
@@ -23,6 +23,7 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -122,10 +123,14 @@ func parseAddresses(addressesToParse []string) ([]listenAddress, error) {
 	parsed := make(map[string]listenAddress)
 	for _, address := range addressesToParse {
 		if address == "localhost" {
-			ip := listenAddress{address: "127.0.0.1", protocol: "tcp4", failureMode: "all"}
-			parsed[ip.address] = ip
-			ip = listenAddress{address: "::1", protocol: "tcp6", failureMode: "all"}
-			parsed[ip.address] = ip
+			if _, exists := parsed["127.0.0.1"]; !exists {
+				ip := listenAddress{address: "127.0.0.1", protocol: "tcp4", failureMode: "all"}
+				parsed[ip.address] = ip
+			}
+			if _, exists := parsed["::1"]; !exists {
+				ip := listenAddress{address: "::1", protocol: "tcp6", failureMode: "all"}
+				parsed[ip.address] = ip
+			}
 		} else if net.ParseIP(address).To4() != nil {
 			parsed[address] = listenAddress{address: address, protocol: "tcp4", failureMode: "any"}
 		} else if net.ParseIP(address) != nil {
@@ -140,6 +145,9 @@ func parseAddresses(addressesToParse []string) ([]listenAddress, error) {
 		addresses[id] = v
 		id++
 	}
+	// Sort addresses before returning to get a stable order
+	sort.Slice(addresses, func(i, j int) bool { return addresses[i].address < addresses[j].address })
+
 	return addresses, nil
 }
 
