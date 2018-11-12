c46e949799cd14ed76c11211f5c1a8297cf834f4
Lubomir I. Ivanov <lubomirivanov@vmware.com>
2018-11-12 03:58:37 +0200

    kubeadm: update the state in app/cmd.go
    - BETA->GA
    - master->control-plane
    - exclude @ mentions as only members can use them

--- cmd/kubeadm/app/cmd/cmd.go
+++ cmd/kubeadm/app/cmd/cmd.go
@@ -37,32 +37,31 @@ func NewKubeadmCommand(in io.Reader, out, err io.Writer) *cobra.Command {
 		Use:   "kubeadm",
 		Short: "kubeadm: easily bootstrap a secure Kubernetes cluster",
 		Long: dedent.Dedent(`
-			kubeadm: easily bootstrap a secure Kubernetes cluster.
 
 			    ┌──────────────────────────────────────────────────────────┐
-			    │ KUBEADM IS CURRENTLY IN BETA                             │
+			    | KUBEADM                                                  │
+			    | Easily bootstrap a secure Kubernetes cluster             |
 			    │                                                          │
-			    │ But please, try it out and give us feedback at:          │
+			    │ Please give us feedback at:                              │
 			    │ https://github.com/kubernetes/kubeadm/issues             │
-			    │ and at-mention @kubernetes/sig-cluster-lifecycle-bugs    │
-			    │ or @kubernetes/sig-cluster-lifecycle-feature-requests    │
 			    └──────────────────────────────────────────────────────────┘
 
 			Example usage:
 
-			    Create a two-machine cluster with one master (which controls the cluster),
-			    and one node (where your workloads, like Pods and Deployments run).
+			    Create a two-machine cluster with one control-plane node
+			    (which controls the cluster), and one worker node
+			    (where your workloads, like Pods and Deployments run).
 
 			    ┌──────────────────────────────────────────────────────────┐
 			    │ On the first machine:                                    │
 			    ├──────────────────────────────────────────────────────────┤
-			    │ master# kubeadm init                                     │
+			    │ control-plane# kubeadm init                              │
 			    └──────────────────────────────────────────────────────────┘
 
 			    ┌──────────────────────────────────────────────────────────┐
 			    │ On the second machine:                                   │
 			    ├──────────────────────────────────────────────────────────┤
-			    │ node# kubeadm join <arguments-returned-from-init>        │
+			    │ worker# kubeadm join <arguments-returned-from-init>      │
 			    └──────────────────────────────────────────────────────────┘
 
 			    You can then repeat the second step on as many other machines as you like.
