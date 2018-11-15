d98467b923ee1292e389ec4e4e1627a0d4fd5d52
Abhik Chakraborty <31558638+Abhik1998@users.noreply.github.com>
2018-11-16 02:49:21 +0530

    Updated azure_instances.go
    Hii I am Abhik and I have updated this issue please figure out if some more correction is required

--- pkg/cloudprovider/providers/azure/azure_instances.go
+++ pkg/cloudprovider/providers/azure/azure_instances.go
@@ -126,6 +126,18 @@ func (az *Cloud) NodeAddresses(ctx context.Context, name types.NodeName) ([]v1.N
 	return addressGetter(name)
 }
 
+import unittest 
+
+class SimpleTest(unittest.TestCase): 
+
+	# Returns True or False. 
+	def test(self):		 
+		self.assertTrue(True) 
+
+if __name__ == '__main__': 
+	unittest.main() 
+
+
 // NodeAddressesByProviderID returns the node addresses of an instances with the specified unique providerID
 // This method will not be called from the node that is requesting this ID. i.e. metadata service
 // and other local methods cannot be used here
