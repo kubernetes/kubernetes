4b524ef995eefdbf74630beaeb0742a88715c82c
Di Xu <stephenhsu90@gmail.com>
2018-11-20 14:49:10 +0800

    kubectl loads config file once and uses persistent client config

--- staging/src/k8s.io/sample-cli-plugin/pkg/cmd/ns.go
+++ staging/src/k8s.io/sample-cli-plugin/pkg/cmd/ns.go
@@ -66,7 +66,7 @@ type NamespaceOptions struct {
 // NewNamespaceOptions provides an instance of NamespaceOptions with default values
 func NewNamespaceOptions(streams genericclioptions.IOStreams) *NamespaceOptions {
 	return &NamespaceOptions{
-		configFlags: genericclioptions.NewConfigFlags(),
+		configFlags: genericclioptions.NewConfigFlags(true),
 
 		IOStreams: streams,
 	}
