6ac2fc180673ceb735097b15de7a4d0ecdfeb2b2
Yongkun Gui <ygui@google.com>
2018-12-06 10:37:50 -0800

    Garbage collect LB resources for GCE

--- cluster/gce/delete-stranded-load-balancers.sh
+++ cluster/gce/delete-stranded-load-balancers.sh
@@ -14,22 +14,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# A utility for deleting target pools and forwarding rules that are unattached to VMs
+# A utility for deleting stranded load balancer resources in GCE
 PROJECT=${PROJECT:-kubernetes-jenkins}
 REGION=${REGION:-us-central1}
 
+# Deleting external load balancer resources
 LIST=$(gcloud --project=${PROJECT} compute target-pools list --format='value(name)')
-
-result=0
 for x in ${LIST}; do
-    if ! gcloud compute --project=${PROJECT} target-pools get-health "${x}" --region=${REGION} 2>/dev/null >/dev/null; then
-	echo DELETING "${x}"
-	gcloud compute --project=${PROJECT} firewall-rules delete "k8s-fw-${x}" -q
-	gcloud compute --project=${PROJECT} forwarding-rules delete "${x}" --region=${REGION} -q
-	gcloud compute --project=${PROJECT} addresses delete "${x}" --region=${REGION} -q
-	gcloud compute --project=${PROJECT} target-pools delete "${x}" --region=${REGION} -q
-        result=1
-    fi
+  # Check the existence of vm instance to see if the load balancer resources are
+  # actively used. Only delete them if they are not.
+  if ! gcloud compute --project=${PROJECT} target-pools get-health "${x}" --region=${REGION} 2>/dev/null >/dev/null; then
+    echo DELETING LB "${x}"
+    gcloud compute --project=${PROJECT} firewall-rules delete "k8s-fw-${x}" -q
+    gcloud compute --project=${PROJECT} forwarding-rules delete "${x}" --region=${REGION} -q
+    gcloud compute --project=${PROJECT} addresses delete "${x}" --region=${REGION} -q
+    gcloud compute --project=${PROJECT} target-pools delete "${x}" --region=${REGION} -q
+  fi
 done
-exit ${result}
+
+# Deleting internal load balancer resources
+ILB_LIST=$(gcloud --project=${PROJECT} compute backend-services list --format='value(name)')
+for x in ${ILB_LIST}; do
+  # Check the existence of vm instance to see if the load balancer resources are
+  # actively used. Only delete them if they are not.
+  if ! gcloud compute --project=${PROJECT} backend-services get-health "${x}" --region=${REGION} 2>/dev/null >/dev/null; then
+    echo DELETING internal LB "${x}"
+    ig=$(gcloud compute --project=${PROJECT} backend-services list --regions=${REGION} 2>/dev/null | grep "${x}" | awk '{print $2}' | cut -d'/' -f3)
+    zone=$(gcloud compute --project=${PROJECT} instance-groups unmanaged list | grep ${ig} | awk '{print $2}')
+    gcloud compute --project=${PROJECT} firewall-rules delete "${x}" -q
+    gcloud compute --project=${PROJECT} forwarding-rules delete "${x}" --region=${REGION} -q
+    gcloud compute --project=${PROJECT} backend-services delete "${x}" --region=${REGION} -q
+    gcloud compute --project=${PROJECT} instance-groups unmanaged delete "${ig}" --zone=${zone} -q
+  fi
+done
 
