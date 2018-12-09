50529c719a517d3a0f08322ff55d470b81b736d9
Maxim Ivanov <ivanov.maxim@gmail.com>
2018-12-09 16:37:21 +0000

    Clarify comments regarding resync period in Informers

--- staging/src/k8s.io/client-go/tools/cache/controller.go
+++ staging/src/k8s.io/client-go/tools/cache/controller.go
@@ -167,9 +167,9 @@ func (c *controller) processLoop() {
 //  * OnUpdate is called when an object is modified. Note that oldObj is the
 //      last known state of the object-- it is possible that several changes
 //      were combined together, so you can't use this to see every single
-//      change. OnUpdate is also called when a re-list happens, and it will
-//      get called even if nothing changed. This is useful for periodically
-//      evaluating or syncing something.
+//      change. OnUpdate is also called when a re-list and resync happens, and
+//      it will get called even if nothing changed. This is useful for
+//      periodically evaluating or syncing something.
 //  * OnDelete will get the final state of the item if it is known, otherwise
 //      it will get an object of type DeletedFinalStateUnknown. This can
 //      happen if the watch is closed and misses the delete event and we don't
@@ -270,10 +270,8 @@ func DeletionHandlingMetaNamespaceKeyFunc(obj interface{}) (string, error) {
 //  * lw is list and watch functions for the source of the resource you want to
 //    be informed of.
 //  * objType is an object of the type that you expect to receive.
-//  * resyncPeriod: if non-zero, will re-list this often (you will get OnUpdate
-//    calls, even if nothing changed). Otherwise, re-list will be delayed as
-//    long as possible (until the upstream source closes the watch or times out,
-//    or you stop the controller).
+//  * resyncPeriod: if non-zero, will resync this often (you will get OnUpdate
+//    calls, even if nothing changed).
 //  * h is the object you want notifications sent to.
 //
 func NewInformer(
@@ -335,10 +333,8 @@ func NewInformer(
 //  * lw is list and watch functions for the source of the resource you want to
 //    be informed of.
 //  * objType is an object of the type that you expect to receive.
-//  * resyncPeriod: if non-zero, will re-list this often (you will get OnUpdate
-//    calls, even if nothing changed). Otherwise, re-list will be delayed as
-//    long as possible (until the upstream source closes the watch or times out,
-//    or you stop the controller).
+//  * resyncPeriod: if non-zero, will resync this often (you will get OnUpdate
+//    calls, even if nothing changed).
 //  * h is the object you want notifications sent to.
 //  * indexers is the indexer for the received object type.
 //
