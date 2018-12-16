c1b0e399c1e564652a6ad4a1b749c3e817c2169b
yue9944882 <291271447@qq.com>
2018-12-17 00:00:52 +0800

    update code generator

--- staging/src/k8s.io/apiextensions-apiserver/hack/update-codegen.sh
+++ staging/src/k8s.io/apiextensions-apiserver/hack/update-codegen.sh
@@ -21,13 +21,20 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
+
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 CLIENTSET_NAME_VERSIONED=clientset \
-CLIENTSET_NAME_INTERNAL=internalclientset \
-${CODEGEN_PKG}/generate-internal-groups.sh all \
+${CODEGEN_PKG}/generate-groups.sh client,lister,informer \
+  k8s.io/apiextensions-apiserver/pkg/client k8s.io/apiextensions-apiserver/pkg/apis \
+  "apiextensions:v1beta1" \
+  --output-base "$(dirname ${BASH_SOURCE})/../../.." \
+  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
+
+CLIENTSET_NAME_VERSIONED=clientset \
+${CODEGEN_PKG}/generate-internal-groups.sh deepcopy,defaulter,conversion \
   k8s.io/apiextensions-apiserver/pkg/client k8s.io/apiextensions-apiserver/pkg/apis k8s.io/apiextensions-apiserver/pkg/apis \
   "apiextensions:v1beta1" \
   --output-base "$(dirname ${BASH_SOURCE})/../../.." \
