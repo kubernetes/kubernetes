28ac02baa3a9f22eeba81865fddcf1fd54071940
Sen Lu <senlu@google.com>
2018-12-18 15:18:42 -0800

    make integration/verify script look for k8s under GOPATH

--- hack/jenkins/test-dockerized.sh
+++ hack/jenkins/test-dockerized.sh
@@ -47,7 +47,7 @@ export KUBE_KEEP_VERBOSE_TEST_OUTPUT=y
 export KUBE_INTEGRATION_TEST_MAX_CONCURRENCY=4
 export LOG_LEVEL=4
 
-cd /go/src/k8s.io/kubernetes
+cd "${GOPATH}/src/k8s.io/kubernetes"
 
 make generated_files
 go install ./cmd/...
