5affbc5fb280cb4b4e03c8b52b8db7976d549b58
Fabio Bertinatto <fbertina@redhat.com>
2019-01-04 19:45:02 +0100

    Fix TestBootstrapClusterRoles

--- plugin/pkg/auth/authorizer/rbac/bootstrappolicy/policy_test.go
+++ plugin/pkg/auth/authorizer/rbac/bootstrappolicy/policy_test.go
@@ -25,7 +25,7 @@ import (
 
 	"sigs.k8s.io/yaml"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
