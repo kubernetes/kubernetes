88abcb74199d404e6164ae4c35e4b238e30fcc80
Rajath Agasthya <rajathagasthya@gmail.com>
2019-01-07 14:32:05 -0800

    Move VolumeSnapshotDataSource feature gate check from validation

--- pkg/api/persistentvolumeclaim/util_test.go
+++ pkg/api/persistentvolumeclaim/util_test.go
@@ -117,3 +117,96 @@ func TestDropAlphaPVCVolumeMode(t *testing.T) {
 		}
 	}
 }
+
+func TestDropDisabledDataSource(t *testing.T) {
+	pvcWithoutDataSource := func() *core.PersistentVolumeClaim {
+		return &core.PersistentVolumeClaim{
+			Spec: core.PersistentVolumeClaimSpec{
+				DataSource: nil,
+			},
+		}
+	}
+	apiGroup := "snapshot.storage.k8s.io"
+	pvcWithDataSource := func() *core.PersistentVolumeClaim {
+		return &core.PersistentVolumeClaim{
+			Spec: core.PersistentVolumeClaimSpec{
+				DataSource: &core.TypedLocalObjectReference{
+					APIGroup: &apiGroup,
+					Kind:     "VolumeSnapshot",
+					Name:     "test_snapshot",
+				},
+			},
+		}
+	}
+
+	pvcInfo := []struct {
+		description   string
+		hasDataSource bool
+		pvc           func() *core.PersistentVolumeClaim
+	}{
+		{
+			description:   "pvc without DataSource",
+			hasDataSource: false,
+			pvc:           pvcWithoutDataSource,
+		},
+		{
+			description:   "pvc with DataSource",
+			hasDataSource: true,
+			pvc:           pvcWithDataSource,
+		},
+		{
+			description:   "is nil",
+			hasDataSource: false,
+			pvc:           func() *core.PersistentVolumeClaim { return nil },
+		},
+	}
+
+	for _, enabled := range []bool{true, false} {
+		for _, oldpvcInfo := range pvcInfo {
+			for _, newpvcInfo := range pvcInfo {
+				oldPvcHasDataSource, oldpvc := oldpvcInfo.hasDataSource, oldpvcInfo.pvc()
+				newPvcHasDataSource, newpvc := newpvcInfo.hasDataSource, newpvcInfo.pvc()
+				if newpvc == nil {
+					continue
+				}
+
+				t.Run(fmt.Sprintf("feature enabled=%v, old pvc %v, new pvc %v", enabled, oldpvcInfo.description, newpvcInfo.description), func(t *testing.T) {
+					defer utilfeaturetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.VolumeSnapshotDataSource, enabled)()
+
+					var oldpvcSpec *core.PersistentVolumeClaimSpec
+					if oldpvc != nil {
+						oldpvcSpec = &oldpvc.Spec
+					}
+					DropDisabledFields(&newpvc.Spec, oldpvcSpec)
+
+					// old pvc should never be changed
+					if !reflect.DeepEqual(oldpvc, oldpvcInfo.pvc()) {
+						t.Errorf("old pvc changed: %v", diff.ObjectReflectDiff(oldpvc, oldpvcInfo.pvc()))
+					}
+
+					switch {
+					case enabled || oldPvcHasDataSource:
+						// new pvc should not be changed if the feature is enabled, or if the old pvc had DataSource
+						if !reflect.DeepEqual(newpvc, newpvcInfo.pvc()) {
+							t.Errorf("new pvc changed: %v", diff.ObjectReflectDiff(newpvc, newpvcInfo.pvc()))
+						}
+					case newPvcHasDataSource:
+						// new pvc should be changed
+						if reflect.DeepEqual(newpvc, newpvcInfo.pvc()) {
+							t.Errorf("new pvc was not changed")
+						}
+						// new pvc should not have DataSource
+						if !reflect.DeepEqual(newpvc, pvcWithoutDataSource()) {
+							t.Errorf("new pvc had DataSource: %v", diff.ObjectReflectDiff(newpvc, pvcWithoutDataSource()))
+						}
+					default:
+						// new pvc should not need to be changed
+						if !reflect.DeepEqual(newpvc, newpvcInfo.pvc()) {
+							t.Errorf("new pvc changed: %v", diff.ObjectReflectDiff(newpvc, newpvcInfo.pvc()))
+						}
+					}
+				})
+			}
+		}
+	}
+}
