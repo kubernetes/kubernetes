5d6599cecec5ed441d393ed1c43da046ff5fc373
Sridhar Mocherla <srmocher@email.arizona.edu>
2019-01-08 21:47:44 +0530

    add missing comma

--- test/e2e/storage/testpatterns/testpattern.go
+++ test/e2e/storage/testpatterns/testpattern.go
@@ -63,8 +63,8 @@ var (
 		Name:    "Inline-volume (default fs)",
 		VolType: InlineVolume,
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:  "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 	// DefaultFsPreprovisionedPV is TestPattern for "Pre-provisioned PV (default fs)"
@@ -72,8 +72,8 @@ var (
 		Name:    "Pre-provisioned PV (default fs)",
 		VolType: PreprovisionedPV,
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 	// DefaultFsDynamicPV is TestPattern for "Dynamic PV (default fs)"
@@ -81,8 +81,8 @@ var (
 		Name:    "Dynamic PV (default fs)",
 		VolType: DynamicPV,
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 
@@ -94,8 +94,8 @@ var (
 		VolType: InlineVolume,
 		FsType:  "ext3",
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 	// Ext3PreprovisionedPV is TestPattern for "Pre-provisioned PV (ext3)"
@@ -104,8 +104,8 @@ var (
 		VolType: PreprovisionedPV,
 		FsType:  "ext3",
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 	// Ext3DynamicPV is TestPattern for "Dynamic PV (ext3)"
@@ -114,8 +114,8 @@ var (
 		VolType: DynamicPV,
 		FsType:  "ext3",
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 
@@ -127,8 +127,8 @@ var (
 		VolType: InlineVolume,
 		FsType:  "ext4",
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 	// Ext4PreprovisionedPV is TestPattern for "Pre-provisioned PV (ext4)"
@@ -137,8 +137,8 @@ var (
 		VolType: PreprovisionedPV,
 		FsType:  "ext4",
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 	// Ext4DynamicPV is TestPattern for "Dynamic PV (ext4)"
@@ -147,8 +147,8 @@ var (
 		VolType: DynamicPV,
 		FsType:  "ext4",
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi"
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 
@@ -160,8 +160,8 @@ var (
 		VolType: InlineVolume,
 		FsType:  "xfs",
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 	// XfsPreprovisionedPV is TestPattern for "Pre-provisioned PV (xfs)"
@@ -170,8 +170,8 @@ var (
 		VolType: PreprovisionedPV,
 		FsType:  "xfs",
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 	// XfsDynamicPV is TestPattern for "Dynamic PV (xfs)"
@@ -180,8 +180,8 @@ var (
 		VolType: DynamicPV,
 		FsType:  "xfs",
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 
@@ -193,8 +193,8 @@ var (
 		VolType: PreprovisionedPV,
 		VolMode: v1.PersistentVolumeFilesystem,
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 	// FsVolModeDynamicPV is TestPattern for "Dynamic PV (filesystem)"
@@ -203,8 +203,8 @@ var (
 		VolType: DynamicPV,
 		VolMode: v1.PersistentVolumeFilesystem,
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 
@@ -216,8 +216,8 @@ var (
 		VolType: PreprovisionedPV,
 		VolMode: v1.PersistentVolumeBlock,
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 	// BlockVolModeDynamicPV is TestPattern for "Dynamic PV (block)(immediate bind)"
@@ -226,8 +226,8 @@ var (
 		VolType: DynamicPV,
 		VolMode: v1.PersistentVolumeBlock,
 		SupportedSizeRange: framework.SizeRange{
-			Min:   "1Mi",
-			Max:   "10Gi",
+			Min: "1Mi",
+			Max: "10Gi",
 		},
 	}
 )
