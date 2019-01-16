07670fefed6b23a31add4b1bd19c60cd788c4950
WanLinghao <wanlh.fnst@cn.fujitsu.com>
2019-01-16 13:59:27 +0800

    When user try execute command like `kubectl get pod test -o custom-columns=CONTAINER:.spec.containers[-1].name` It will throw a panic about slice index out of bounds. This patch fix it.

--- staging/src/k8s.io/client-go/util/jsonpath/parser.go
+++ staging/src/k8s.io/client-go/util/jsonpath/parser.go
@@ -325,6 +325,7 @@ Loop:
 			if i == 1 {
 				params[i].Known = true
 				params[i].Value = params[0].Value + 1
+				params[i].Derived = true
 			} else {
 				params[i].Known = false
 				params[i].Value = 0
