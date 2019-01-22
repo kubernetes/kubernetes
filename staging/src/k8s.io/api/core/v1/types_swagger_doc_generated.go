41ebc7921c48ae99c800e276444b82d1254bacfd
Di Xu <stephenhsu90@gmail.com>
2019-01-22 17:10:49 +0800

    auto-generated files

--- staging/src/k8s.io/api/core/v1/types_swagger_doc_generated.go
+++ staging/src/k8s.io/api/core/v1/types_swagger_doc_generated.go
@@ -810,7 +810,7 @@ var map_Handler = map[string]string{
 	"":          "Handler defines a specific action that should be taken",
 	"exec":      "One and only one of the following should be specified. Exec specifies the action to take.",
 	"httpGet":   "HTTPGet specifies the http request to perform.",
-	"tcpSocket": "TCPSocket specifies an action involving a TCP port. TCP hooks not yet supported",
+	"tcpSocket": "TCPSocket specifies an action involving a TCP port.",
 }
 
 func (Handler) SwaggerDoc() map[string]string {
