ba4ed66a9623443591ae9f520ca0fa8dd6d39e27
Xiangyang Chu <xychu2008@gmail.com>
2019-02-01 16:41:06 +0800

    Fix wrong boilerplate header

--- test/cmd/legacy-script.sh
+++ test/cmd/legacy-script.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2034
 
 # Copyright 2016 The Kubernetes Authors.
 #
@@ -15,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# shellcheck disable=SC2034
+# https://github.com/koalaman/shellcheck/wiki/SC2034
 # This contains util code for testing kubectl.
 
 set -o errexit
