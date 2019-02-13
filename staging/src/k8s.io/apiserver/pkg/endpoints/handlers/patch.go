5e6fc5dce8b12c5ce80e016b208a51c81a8c9ce8
Chao Xu <xuchao@google.com>
2019-02-12 23:37:01 -0800

    Limit the number of operations in a single json patch to be 10,000

--- staging/src/k8s.io/apiserver/pkg/endpoints/handlers/patch.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/handlers/patch.go
@@ -49,6 +49,11 @@ import (
 	utiltrace "k8s.io/utils/trace"
 )
 
+const (
+	// maximum number of operations a single json patch may contain.
+	maxJSONPatchOperations = 10000
+)
+
 // PatchResource returns a function that will handle a resource patch.
 func PatchResource(r rest.Patcher, scope RequestScope, admit admission.Interface, patchTypes []string) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
@@ -330,6 +335,11 @@ func (p *jsonPatcher) applyJSPatch(versionedJS []byte) (patchedJS []byte, retErr
 		patchObj, err := jsonpatch.DecodePatch(p.patchBytes)
 		if err != nil {
 			return nil, errors.NewBadRequest(err.Error())
+		}
+		if len(patchObj) > maxJSONPatchOperations {
+			return nil, errors.NewRequestEntityTooLargeError(
+				fmt.Sprintf("The allowed maximum operations in a JSON patch is %d, got %d",
+					maxJSONPatchOperations, len(patchObj)))
 		}
 		patchedJS, err := patchObj.Apply(versionedJS)
 		if err != nil {
