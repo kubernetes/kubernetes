bfe3e195704b874292cc592095e3b189060249f1
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2019-02-13 14:22:45 +0100

    apimachinery: alias k8s.io/utils/fields.Path into pkg/util/validation/field

--- staging/src/k8s.io/apimachinery/pkg/util/validation/field/path.go
+++ staging/src/k8s.io/apimachinery/pkg/util/validation/field/path.go
@@ -16,76 +16,12 @@ limitations under the License.
 
 package field
 
-import (
-	"bytes"
-	"fmt"
-	"strconv"
-)
+import "k8s.io/utils/field"
 
 // Path represents the path from some root to a particular field.
-type Path struct {
-	name   string // the name of this field or "" if this is an index
-	index  string // if name == "", this is a subscript (index or map key) of the previous element
-	parent *Path  // nil if this is the root element
-}
+type Path = field.Path
 
 // NewPath creates a root Path object.
 func NewPath(name string, moreNames ...string) *Path {
-	r := &Path{name: name, parent: nil}
-	for _, anotherName := range moreNames {
-		r = &Path{name: anotherName, parent: r}
-	}
-	return r
-}
-
-// Root returns the root element of this Path.
-func (p *Path) Root() *Path {
-	for ; p.parent != nil; p = p.parent {
-		// Do nothing.
-	}
-	return p
-}
-
-// Child creates a new Path that is a child of the method receiver.
-func (p *Path) Child(name string, moreNames ...string) *Path {
-	r := NewPath(name, moreNames...)
-	r.Root().parent = p
-	return r
-}
-
-// Index indicates that the previous Path is to be subscripted by an int.
-// This sets the same underlying value as Key.
-func (p *Path) Index(index int) *Path {
-	return &Path{index: strconv.Itoa(index), parent: p}
-}
-
-// Key indicates that the previous Path is to be subscripted by a string.
-// This sets the same underlying value as Index.
-func (p *Path) Key(key string) *Path {
-	return &Path{index: key, parent: p}
-}
-
-// String produces a string representation of the Path.
-func (p *Path) String() string {
-	// make a slice to iterate
-	elems := []*Path{}
-	for ; p != nil; p = p.parent {
-		elems = append(elems, p)
-	}
-
-	// iterate, but it has to be backwards
-	buf := bytes.NewBuffer(nil)
-	for i := range elems {
-		p := elems[len(elems)-1-i]
-		if p.parent != nil && len(p.name) > 0 {
-			// This is either the root or it is a subscript.
-			buf.WriteString(".")
-		}
-		if len(p.name) > 0 {
-			buf.WriteString(p.name)
-		} else {
-			fmt.Fprintf(buf, "[%s]", p.index)
-		}
-	}
-	return buf.String()
+	return field.NewPath(name, moreNames...)
 }
