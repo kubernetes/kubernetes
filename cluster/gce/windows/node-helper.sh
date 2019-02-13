6d78f2b6668132e39b49fdf4ab5a8d0d65de2a56
Peter Hornyack <peterhornyack@google.com>
2019-02-12 16:38:49 -0800

    Default to Windows Server version 1809 for Windows nodes.
    Removes all references to 1803, including moving "win1803" directory to
    just "windows". A single Windows directory suffices for now, if
    necessary in the future we can shard it into directories for each
    Windows version.
    
    We've been running tests with Windows 1809 nodes for a couple days in
    our fork without major problems:
    https://testgrid.k8s.io/google-windows#windows-prototype&width=20.
    Testing on Azure is already using 1809:
    https://testgrid.k8s.io/sig-windows#Conformance%20acs-engine%20on%20Azure&width=20.

--- cluster/gce/windows/node-helper.sh
+++ cluster/gce/windows/node-helper.sh
@@ -24,10 +24,10 @@ function get-windows-node-instance-metadata-from-file {
   # To get startup script output run "gcloud compute instances
   # get-serial-port-output <instance>" from the location where you're running
   # kube-up.
-  metadata+="windows-startup-script-ps1=${KUBE_ROOT}/cluster/gce/${WINDOWS_NODE_OS_DISTRIBUTION}/configure.ps1,"
-  metadata+="common-psm1=${KUBE_ROOT}/cluster/gce/${WINDOWS_NODE_OS_DISTRIBUTION}/common.psm1,"
-  metadata+="k8s-node-setup-psm1=${KUBE_ROOT}/cluster/gce/${WINDOWS_NODE_OS_DISTRIBUTION}/k8s-node-setup.psm1,"
-  metadata+="user-profile-psm1=${KUBE_ROOT}/cluster/gce/${WINDOWS_NODE_OS_DISTRIBUTION}/user-profile.psm1,"
+  metadata+="windows-startup-script-ps1=${KUBE_ROOT}/cluster/gce/windows/configure.ps1,"
+  metadata+="common-psm1=${KUBE_ROOT}/cluster/gce/windows/common.psm1,"
+  metadata+="k8s-node-setup-psm1=${KUBE_ROOT}/cluster/gce/windows/k8s-node-setup.psm1,"
+  metadata+="user-profile-psm1=${KUBE_ROOT}/cluster/gce/windows/user-profile.psm1,"
   metadata+="${NODE_EXTRA_METADATA}"
   echo "${metadata}"
 }
