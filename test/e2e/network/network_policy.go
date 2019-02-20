a47d5131690392305b193d3748dd348ccb38e735
Johannes M. Scheuermann <joh.scheuer@gmail.com>
2019-02-20 08:45:03 +0100

    Use wget instead of curl for e2e network policy tests

--- test/e2e/network/network_policy.go
+++ test/e2e/network/network_policy.go
@@ -17,7 +17,7 @@ limitations under the License.
 package network
 
 import (
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
