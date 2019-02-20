453498fe2cedffe93e9219a8d33f516238469b68
Bobby (Babak) Salamat <bsalamat@google.com>
2019-02-20 11:42:42 -0800

    Graduate PriorityClass to GA

--- pkg/apis/scheduling/install/install.go
+++ pkg/apis/scheduling/install/install.go
@@ -23,6 +23,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/kubernetes/pkg/api/legacyscheme"
 	"k8s.io/kubernetes/pkg/apis/scheduling"
+	"k8s.io/kubernetes/pkg/apis/scheduling/v1"
 	"k8s.io/kubernetes/pkg/apis/scheduling/v1alpha1"
 	"k8s.io/kubernetes/pkg/apis/scheduling/v1beta1"
 )
@@ -34,7 +35,9 @@ func init() {
 // Install registers the API group and adds types to a scheme
 func Install(scheme *runtime.Scheme) {
 	utilruntime.Must(scheduling.AddToScheme(scheme))
+	utilruntime.Must(v1.AddToScheme(scheme))
 	utilruntime.Must(v1beta1.AddToScheme(scheme))
 	utilruntime.Must(v1alpha1.AddToScheme(scheme))
-	utilruntime.Must(scheme.SetVersionPriority(v1beta1.SchemeGroupVersion, v1alpha1.SchemeGroupVersion))
+	// TODO(bsalamat): update this in 1.15 to use v1 API.
+	utilruntime.Must(scheme.SetVersionPriority(v1beta1.SchemeGroupVersion, v1.SchemeGroupVersion, v1alpha1.SchemeGroupVersion))
 }
