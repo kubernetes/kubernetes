f02a98608110af024ef31364dee20be4c43b63dc
danielqsj <qsj.daniel@gmail.com>
2019-02-24 20:35:46 +0800

    add comments to shell function

--- cluster/local/util.sh
+++ cluster/local/util.sh
@@ -18,6 +18,13 @@ function prepare-e2e() {
   echo "Local doesn't need special preparations for e2e tests" 1>&2
 }
 
+# Detect the IP for the master
+#
+# Vars set:
+#   KUBE_MASTER
+#   KUBE_MASTER_IP
+# Vars exported:
+#   KUBE_MASTER_URL
 function detect-master {
   KUBE_MASTER=localhost
   KUBE_MASTER_IP=127.0.0.1
