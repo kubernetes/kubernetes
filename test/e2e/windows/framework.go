cee02e62b7cd23c498e9c528cdc82c793e5afa8f
danielqsj <qsj.daniel@gmail.com>
2019-02-25 11:41:27 +0800

    Fix golint failures for e2e/windows

--- test/e2e/windows/framework.go
+++ test/e2e/windows/framework.go
@@ -18,6 +18,7 @@ package windows
 
 import "github.com/onsi/ginkgo"
 
+// SIGDescribe annotates the test with the SIG label.
 func SIGDescribe(text string, body func()) bool {
 	return ginkgo.Describe("[sig-windows] "+text, body)
 }
