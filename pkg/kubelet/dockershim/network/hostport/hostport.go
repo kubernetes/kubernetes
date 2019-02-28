2de3d567cb8c9080d8955442602231ba66fe3006
Edmund Rhudy <erhudy@bloomberg.net>
2019-02-28 17:00:37 -0500

    fix import

--- pkg/kubelet/dockershim/network/hostport/hostport.go
+++ pkg/kubelet/dockershim/network/hostport/hostport.go
@@ -24,8 +24,8 @@ import (
 	"k8s.io/klog"
 
 	"k8s.io/api/core/v1"
-	utiliptables "k8s.io/kubernetes/pkg/util/iptables"
 	initsystem "k8s.io/kubernetes/pkg/util/initsystem"
+	utiliptables "k8s.io/kubernetes/pkg/util/iptables"
 )
 
 const (
@@ -111,7 +111,7 @@ func portMappingToHostport(portMapping *PortMapping) hostport {
 }
 
 // ensureKubeHostportChains ensures the KUBE-HOSTPORTS chain is setup correctly
-func ensureKubeHostportChains(iptables utiliptables.Interface, natInterfaceName string, initSystem initSystem.InitSystem) error {
+func ensureKubeHostportChains(iptables utiliptables.Interface, natInterfaceName string, initSystem initsystem.InitSystem) error {
 	klog.V(4).Info("Ensuring kubelet hostport chains")
 	// Ensure kubeHostportChain
 	if _, err := iptables.EnsureChain(utiliptables.TableNAT, kubeHostportsChain); err != nil {
