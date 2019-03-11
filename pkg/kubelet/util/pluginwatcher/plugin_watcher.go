817c9ca9536d86b3d029f3daea845fb6733d56e5
Fabio Bertinatto <fbertina@redhat.com>
2019-03-11 13:05:07 +0100

    Process plugins found before the ones watched
    Currently, the method `pluginwatcher.traversePluginDir` descends into
    a directory adding filesystem watchers and creating synthetic `create`
    events when it finds sockets files. However, a race condition might
    happen when a recently-added watcher observes a `delete` event in a
    socket file before `pluginwatcher.traversePlugindir` itself notices
    this file.
    
    This patch changes this behavior by registering watchers on
    directories, enqueueing and processing `create` events from sockets
    found, and only then processing the events from the registered watchers.

--- pkg/kubelet/util/pluginwatcher/plugin_watcher.go
+++ pkg/kubelet/util/pluginwatcher/plugin_watcher.go
@@ -102,9 +102,24 @@ func (w *Watcher) Start() error {
 	}
 	w.fsWatcher = fsWatcher
 
+	// Traverse plugin dir and add filesystem watchers before starting the plugin processing goroutine.
+	if err := w.traversePluginDir(w.path); err != nil {
+		w.Stop()
+		return fmt.Errorf("failed to traverse plugin socket path %q, err: %v", w.path, err)
+	}
+
+	// Traverse deprecated plugin dir, if specified.
+	if len(w.deprecatedPath) != 0 {
+		if err := w.traversePluginDir(w.deprecatedPath); err != nil {
+			w.Stop()
+			return fmt.Errorf("failed to traverse deprecated plugin socket path %q, err: %v", w.deprecatedPath, err)
+		}
+	}
+
 	w.wg.Add(1)
 	go func(fsWatcher *fsnotify.Watcher) {
 		defer w.wg.Done()
+
 		for {
 			select {
 			case event := <-fsWatcher.Events:
@@ -138,20 +153,6 @@ func (w *Watcher) Start() error {
 			}
 		}
 	}(fsWatcher)
-
-	// Traverse plugin dir after starting the plugin processing goroutine
-	if err := w.traversePluginDir(w.path); err != nil {
-		w.Stop()
-		return fmt.Errorf("failed to traverse plugin socket path %q, err: %v", w.path, err)
-	}
-
-	// Traverse deprecated plugin dir, if specified.
-	if len(w.deprecatedPath) != 0 {
-		if err := w.traversePluginDir(w.deprecatedPath); err != nil {
-			w.Stop()
-			return fmt.Errorf("failed to traverse deprecated plugin socket path %q, err: %v", w.deprecatedPath, err)
-		}
-	}
 
 	return nil
 }
@@ -211,14 +212,14 @@ func (w *Watcher) traversePluginDir(dir string) error {
 				return fmt.Errorf("failed to watch %s, err: %v", path, err)
 			}
 		case mode&os.ModeSocket != 0:
-			w.wg.Add(1)
-			go func() {
-				defer w.wg.Done()
-				w.fsWatcher.Events <- fsnotify.Event{
-					Name: path,
-					Op:   fsnotify.Create,
-				}
-			}()
+			event := fsnotify.Event{
+				Name: path,
+				Op:   fsnotify.Create,
+			}
+			//TODO: Handle errors by taking corrective measures
+			if err := w.handleCreateEvent(event); err != nil {
+				klog.Errorf("error %v when handling create event: %s", err, event)
+			}
 		default:
 			klog.V(5).Infof("Ignoring file %s with mode %v", path, mode)
 		}
