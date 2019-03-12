37f545e91c63905729cee027f939d86f2ba6bd04
WanLinghao <wanlh.fnst@cn.fujitsu.com>
2019-03-12 11:01:47 +0800

    remove unused functions in pkg/controller/nodelifecycle/node_lifecycle_controller.go

--- pkg/controller/nodelifecycle/node_lifecycle_controller.go
+++ pkg/controller/nodelifecycle/node_lifecycle_controller.go
@@ -23,8 +23,6 @@ package nodelifecycle
 
 import (
 	"fmt"
-	"hash/fnv"
-	"io"
 	"sync"
 	"time"
 
@@ -1305,10 +1303,4 @@ func (nc *Controller) reconcileNodeLabels(nodeName string) error {
 		return fmt.Errorf("failed update labels for node %+v", node)
 	}
 	return nil
-}
-
-func hash(val string, max int) int {
-	hasher := fnv.New32a()
-	io.WriteString(hasher, val)
-	return int(hasher.Sum32()) % max
 }
