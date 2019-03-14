73873faad52a73d99c0859c6f988626b2547baec
n-i-x <jeff@rebeiro.net>
2019-03-14 13:48:33 -0700

    mark HorizontalPodAutoscalerStatus.conditions as optional

--- staging/src/k8s.io/api/autoscaling/v2beta2/types.go
+++ staging/src/k8s.io/api/autoscaling/v2beta2/types.go
@@ -252,7 +252,7 @@ type HorizontalPodAutoscalerStatus struct {
 
 	// conditions is the set of conditions required for this autoscaler to scale its target,
 	// and indicates whether or not those conditions are met.
-	Conditions []HorizontalPodAutoscalerCondition `json:"conditions" protobuf:"bytes,6,rep,name=conditions"`
+	Conditions []HorizontalPodAutoscalerCondition `json:"conditions,omitempty" protobuf:"bytes,6,rep,name=conditions"`
 }
 
 // HorizontalPodAutoscalerConditionType are the valid conditions of
