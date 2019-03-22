e0dc2e9b84bdedacf2647c84f99f145dff032dec
zawachte-msft <zawachte@microsoft.com>
2019-03-22 08:43:53 -0700

    Fixing DSR by adding outbound nat endpoint policy

--- pkg/proxy/winkernel/hnsV2.go
+++ pkg/proxy/winkernel/hnsV2.go
@@ -67,12 +67,15 @@ func (hns hnsV2) getEndpointByID(id string) (*endpointsInfo, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	pol := policiesToPoliciesinfo(hnsendpoint.Policies)
 	return &endpointsInfo{ //TODO: fill out PA
 		ip:         hnsendpoint.IpConfigurations[0].IpAddress,
 		isLocal:    uint32(hnsendpoint.Flags&hcn.EndpointFlagsRemoteEndpoint) == 0, //TODO: Change isLocal to isRemote
 		macAddress: hnsendpoint.MacAddress,
 		hnsID:      hnsendpoint.Id,
 		hns:        hns,
+		policies:   pol,
 	}, nil
 }
 func (hns hnsV2) getEndpointByIpAddress(ip string, networkName string) (*endpointsInfo, error) {
@@ -89,17 +92,31 @@ func (hns hnsV2) getEndpointByIpAddress(ip string, networkName string) (*endpoin
 			equal = endpoint.IpConfigurations[0].IpAddress == ip
 		}
 		if equal && strings.EqualFold(endpoint.HostComputeNetwork, hnsnetwork.Id) {
+			pol := policiesToPoliciesinfo(endpoint.Policies)
 			return &endpointsInfo{
 				ip:         endpoint.IpConfigurations[0].IpAddress,
 				isLocal:    uint32(endpoint.Flags&hcn.EndpointFlagsRemoteEndpoint) == 0, //TODO: Change isLocal to isRemote
 				macAddress: endpoint.MacAddress,
 				hnsID:      endpoint.Id,
 				hns:        hns,
+				policies:   pol,
 			}, nil
 		}
 	}
 
 	return nil, fmt.Errorf("Endpoint %v not found on network %s", ip, networkName)
+}
+func policiesToPoliciesinfo(hcnendpointpolicies []hcn.EndpointPolicy) []*policiesinfo {
+	var endpointPolicies []*policiesinfo
+	for _, po := range hcnendpointpolicies {
+		klog.V(3).Infof("endpoint policy:%s", po)
+		endpointPolicies = append(endpointPolicies, &policiesinfo{
+			Type:     string(po.Type),
+			Settings: po.Settings,
+		})
+		klog.V(3).Infof("endpoint policy we placed:%s", string(po.Settings))
+	}
+	return endpointPolicies
 }
 func (hns hnsV2) createEndpoint(ep *endpointsInfo, networkName string) (*endpointsInfo, error) {
 	hnsNetwork, err := hcn.GetNetworkByName(networkName)
@@ -149,6 +166,7 @@ func (hns hnsV2) createEndpoint(ep *endpointsInfo, networkName string) (*endpoin
 			return nil, fmt.Errorf("Local endpoint creation failed: %v", err)
 		}
 	}
+	pol := policiesToPoliciesinfo(createdEndpoint.Policies)
 	return &endpointsInfo{
 		ip:              createdEndpoint.IpConfigurations[0].IpAddress,
 		isLocal:         uint32(createdEndpoint.Flags&hcn.EndpointFlagsRemoteEndpoint) == 0,
@@ -156,7 +174,20 @@ func (hns hnsV2) createEndpoint(ep *endpointsInfo, networkName string) (*endpoin
 		hnsID:           createdEndpoint.Id,
 		providerAddress: ep.providerAddress, //TODO get from createdEndpoint
 		hns:             hns,
+		policies:        pol,
 	}, nil
+}
+func (hns hnsV2) updateEndpointPolicy(endpointID string, policy json.RawMessage) error {
+	requestMessage := &hcn.ModifyEndpointSettingRequest{
+		ResourceType: "Policy",
+		RequestType:  "Add",
+		Settings:     policy,
+	}
+
+	klog.V(3).Infof("Local endpoint policy added to %s", endpointID)
+	LogJson(policy, "Local endpoint policy:", 1)
+
+	return hcn.ModifyEndpointSettings(endpointID, requestMessage)
 }
 func (hns hnsV2) deleteEndpoint(hnsID string) error {
 	hnsendpoint, err := hcn.GetEndpointByID(hnsID)
