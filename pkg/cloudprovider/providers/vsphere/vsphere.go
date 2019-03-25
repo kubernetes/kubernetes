cymtrick/kubernetes-error-fuzzer

stop vsphere cloud provider from spamming logs with `failed to patch IP` Fixes: #75236

pkg/cloudprovider/providers/vsphere/vsphere.go
@@ -585,7 +585,7 @@ func getLocalIP() ([]v1.NodeAddress, error) {
 							)
 							klog.V(4).Infof("Detected local IP address as %q", ipnet.IP.String())
 						} else {
-							klog.Warningf("Failed to patch IP as MAC address %q does not belong to a VMware platform", vmMACAddr)
+							klog.V(4).Infof("Failed to patch IP for interface %q as MAC address %q does not belong to a VMware platform", i.Name, vmMACAddr)
 						}
 					}
 				}

kubernetes/kubernetes

pkg/cloudprovider/providers/vsphere/vsphere.go
@@ -574,7 +574,7 @@ func getLocalIP() ([]v1.NodeAddress, error) {
 							)
 							glog.V(4).Infof("Detected local IP address as %q", ipnet.IP.String())
 						} else {
-							glog.Warningf("Failed to patch IP as MAC address %q does not belong to a VMware platform", vmMACAddr)
+							glog.V(4).Infof("Failed to patch IP for interface %q as MAC address %q does not belong to a VMware platform", i.Name, vmMACAddr)
 						}
 					}
 				}