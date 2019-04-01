530e9719301c06bf2bdfbd92d4730a5fe779c2d4
caiweidong <caiweidong@baidu.com>
2019-04-01 13:20:32 +0800

    Change the default max backoff time value

--- pkg/util/goroutinemap/exponentialbackoff/exponential_backoff.go
+++ pkg/util/goroutinemap/exponentialbackoff/exponential_backoff.go
@@ -34,7 +34,7 @@ const (
 	// durationBeforeRetry will grow to due to exponential backoff.
 	// Value is slightly offset from 2 minutes to make timeouts due to this
 	// constant recognizable.
-	maxDurationBeforeRetry time.Duration = 2*time.Minute + 2*time.Second
+	maxDurationBeforeRetry time.Duration = 30 * time.Second
 )
 
 // ExponentialBackoff contains the last occurrence of an error and the duration
