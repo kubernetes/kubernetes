30a56bab42ab14a3a2ee21f5581b02525d4fd110
Benjamin Elder <bentheelder@google.com>
2019-04-01 18:39:17 -0700

    Revert "add timeout suuport for watch"
    This reverts commit c676e234cc1036a2a1147f2eeef9c89468671330.

--- staging/src/k8s.io/apiserver/pkg/storage/cacher/cacher_whitebox_test.go
+++ staging/src/k8s.io/apiserver/pkg/storage/cacher/cacher_whitebox_test.go
@@ -19,7 +19,6 @@ package cacher
 import (
 	"context"
 	"fmt"
-	"math"
 	"reflect"
 	"strconv"
 	"sync"
@@ -64,8 +63,8 @@ func TestCacheWatcherCleanupNotBlockedByResult(t *testing.T) {
 	}
 	// set the size of the buffer of w.result to 0, so that the writes to
 	// w.result is blocked.
-	w = newCacheWatcher(0, filter, forget, testVersioner{}, time.Duration(math.MaxInt64))
-	go w.process(context.Background(), initEvents, 0)
+	w = newCacheWatcher(0, filter, forget, testVersioner{})
+	go w.process(initEvents, 0)
 	w.Stop()
 	if err := wait.PollImmediate(1*time.Second, 5*time.Second, func() (bool, error) {
 		lock.RLock()
@@ -183,9 +182,8 @@ TestCase:
 		for j := range testCase.events {
 			testCase.events[j].ResourceVersion = uint64(j) + 1
 		}
-
-		w := newCacheWatcher(0, filter, forget, testVersioner{}, time.Duration(math.MaxInt64))
-		go w.process(context.Background(), testCase.events, 0)
+		w := newCacheWatcher(0, filter, forget, testVersioner{})
+		go w.process(testCase.events, 0)
 		ch := w.ResultChan()
 		for j, event := range testCase.expected {
 			e := <-ch
