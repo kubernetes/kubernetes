9a6d9693c236491c039c807f6b6b2a331a712c26
Maciej Szulik <maszulik@redhat.com>
2019-04-02 13:31:29 +0200

    Fix create secret generic paths examples
    Add RUNTIME_REQUEST_TIMEOUT to local-up-cluster.sh
    
    The changes to local-cluster-up.sh from #75662 is causing the
    CRI-O CI to break since it picks up the --runtime-request-timeout
    flag as part of the container-runtime-endpoint flag.
    Adding a new env so that we can set the runtime-request-timeout
    value for our CI.
    
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>
    
    Extract Method for kubeadm
    
    Signed-off-by: PingWang <wang.ping5@zte.com.cn>
    
    update pull request
    
    update pull request
    
    update pull request
    
    Fix shellcheck failures in test-dockerized.sh and upload-to-gcs.sh
    
    Fix shellcheck failures in test-dockerized.sh and upload-to-gcs.sh
    
    Fix shellcheck failures in test-dockerized.sh and upload-to-gcs.sh
    
    Fix shellcheck failures in test-dockerized.sh and upload-to-gcs.sh
    
    Fix shellcheck failures in test-dockerized.sh and upload-to-gcs.sh

--- pkg/kubectl/cmd/create/create_secret.go
+++ pkg/kubectl/cmd/create/create_secret.go
@@ -61,7 +61,7 @@ var (
 	  kubectl create secret generic my-secret --from-file=path/to/bar
 
 	  # Create a new secret named my-secret with specified keys instead of names on disk
-	  kubectl create secret generic my-secret --from-file=ssh-privatekey=~/.ssh/id_rsa --from-file=ssh-publickey=~/.ssh/id_rsa.pub
+            kubectl create secret generic my-secret --from-file=ssh-privatekey=~/.ssh/id_rsa --from-file=ssh-publickey=~/.ssh/id_rsa.pub
 
 	  # Create a new secret named my-secret with key1=supersecret and key2=topsecret
 	  kubectl create secret generic my-secret --from-literal=key1=supersecret --from-literal=key2=topsecret
