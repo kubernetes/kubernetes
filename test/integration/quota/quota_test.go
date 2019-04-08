8e55c7bf24609c3967e468c92a6a48969c15625a
Mansi Agarwal <mansia@pa-dbc1101.eng.vmware.com>
2019-04-08 14:24:51 -0700

    Accept admission request if resource is being deleted

--- test/integration/quota/quota_test.go
+++ test/integration/quota/quota_test.go
@@ -25,6 +25,7 @@ import (
 	"time"
 
 	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -44,6 +45,7 @@ import (
 	quotainstall "k8s.io/kubernetes/pkg/quota/v1/install"
 	"k8s.io/kubernetes/plugin/pkg/admission/resourcequota"
 	resourcequotaapi "k8s.io/kubernetes/plugin/pkg/admission/resourcequota/apis/resourcequota"
+	"k8s.io/kubernetes/test/integration"
 	"k8s.io/kubernetes/test/integration/framework"
 )
 
@@ -182,6 +184,27 @@ func waitForQuota(t *testing.T, quota *v1.ResourceQuota, clientset *clientset.Cl
 	}
 }
 
+func waitForQuotaDelete(t *testing.T, quotaName string, ns string, clientset *clientset.Clientset) {
+	if err := clientset.CoreV1().ResourceQuotas(ns).Delete(quotaName, nil); err != nil {
+		t.Errorf("unable to delete quota %v: %v", quotaName, err)
+	}
+	err := wait.PollImmediate(10*time.Second, 1*time.Minute, func() (bool, error) {
+		_, err := clientset.CoreV1().ResourceQuotas(ns).Get(quotaName, metav1.GetOptions{})
+		if err == nil {
+			return false, nil
+		} else {
+			if errors.IsNotFound(err) {
+				return true, nil
+			} else {
+				return false, err
+			}
+		}
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
 func scale(t *testing.T, namespace string, clientset *clientset.Clientset) {
 	target := int32(100)
 	rc := &v1.ReplicationController{
@@ -244,7 +267,135 @@ func scale(t *testing.T, namespace string, clientset *clientset.Clientset) {
 	}
 }
 
-func TestQuotaLimitedResourceDenial(t *testing.T) {
+// func TestQuotaLimitedResourceDenial(t *testing.T) {
+// 	// Set up a master
+// 	h := &framework.MasterHolder{Initialized: make(chan struct{})}
+// 	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+// 		<-h.Initialized
+// 		h.M.GenericAPIServer.Handler.ServeHTTP(w, req)
+// 	}))
+
+// 	admissionCh := make(chan struct{})
+// 	clientset := clientset.NewForConfigOrDie(&restclient.Config{QPS: -1, Host: s.URL, ContentConfig: restclient.ContentConfig{GroupVersion: &schema.GroupVersion{Group: "", Version: "v1"}}})
+
+// 	// stop creation of a pod resource unless there is a quota
+// 	config := &resourcequotaapi.Configuration{
+// 		LimitedResources: []resourcequotaapi.LimitedResource{
+// 			{
+// 				Resource:      "pods",
+// 				MatchContains: []string{"pods"},
+// 			},
+// 		},
+// 	}
+// 	qca := quotainstall.NewQuotaConfigurationForAdmission()
+// 	admission, err := resourcequota.NewResourceQuota(config, 5, admissionCh)
+// 	if err != nil {
+// 		t.Fatalf("unexpected error: %v", err)
+// 	}
+// 	admission.SetExternalKubeClientSet(clientset)
+// 	externalInformers := informers.NewSharedInformerFactory(clientset, controller.NoResyncPeriodFunc())
+// 	admission.SetExternalKubeInformerFactory(externalInformers)
+// 	admission.SetQuotaConfiguration(qca)
+// 	defer close(admissionCh)
+
+// 	masterConfig := framework.NewIntegrationTestMasterConfig()
+// 	masterConfig.GenericConfig.AdmissionControl = admission
+// 	_, _, closeFn := framework.RunAMasterUsingServer(masterConfig, s, h)
+// 	defer closeFn()
+
+// 	ns := framework.CreateTestingNamespace("quota", s, t)
+// 	defer framework.DeleteTestingNamespace(ns, s, t)
+
+// 	controllerCh := make(chan struct{})
+// 	defer close(controllerCh)
+
+// 	informers := informers.NewSharedInformerFactory(clientset, controller.NoResyncPeriodFunc())
+// 	rm := replicationcontroller.NewReplicationManager(
+// 		informers.Core().V1().Pods(),
+// 		informers.Core().V1().ReplicationControllers(),
+// 		clientset,
+// 		replicationcontroller.BurstReplicas,
+// 	)
+// 	rm.SetEventRecorder(&record.FakeRecorder{})
+// 	go rm.Run(3, controllerCh)
+
+// 	discoveryFunc := clientset.Discovery().ServerPreferredNamespacedResources
+// 	listerFuncForResource := generic.ListerFuncForResourceFunc(informers.ForResource)
+// 	qc := quotainstall.NewQuotaConfigurationForControllers(listerFuncForResource)
+// 	informersStarted := make(chan struct{})
+// 	resourceQuotaControllerOptions := &resourcequotacontroller.ResourceQuotaControllerOptions{
+// 		QuotaClient:               clientset.CoreV1(),
+// 		ResourceQuotaInformer:     informers.Core().V1().ResourceQuotas(),
+// 		ResyncPeriod:              controller.NoResyncPeriodFunc,
+// 		InformerFactory:           informers,
+// 		ReplenishmentResyncPeriod: controller.NoResyncPeriodFunc,
+// 		DiscoveryFunc:             discoveryFunc,
+// 		IgnoredResourcesFunc:      qc.IgnoredResources,
+// 		InformersStarted:          informersStarted,
+// 		Registry:                  generic.NewRegistry(qc.Evaluators()),
+// 	}
+// 	resourceQuotaController, err := resourcequotacontroller.NewResourceQuotaController(resourceQuotaControllerOptions)
+// 	if err != nil {
+// 		t.Fatalf("unexpected err: %v", err)
+// 	}
+// 	go resourceQuotaController.Run(2, controllerCh)
+
+// 	// Periodically the quota controller to detect new resource types
+// 	go resourceQuotaController.Sync(discoveryFunc, 30*time.Second, controllerCh)
+
+// 	externalInformers.Start(controllerCh)
+// 	informers.Start(controllerCh)
+// 	close(informersStarted)
+
+// 	// try to create a pod
+// 	pod := &v1.Pod{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Name:      "foo",
+// 			Namespace: ns.Name,
+// 		},
+// 		Spec: v1.PodSpec{
+// 			Containers: []v1.Container{
+// 				{
+// 					Name:  "container",
+// 					Image: "busybox",
+// 				},
+// 			},
+// 		},
+// 	}
+// 	if _, err := clientset.CoreV1().Pods(ns.Name).Create(pod); err == nil {
+// 		t.Fatalf("expected error for insufficient quota")
+// 	}
+
+// 	// now create a covering quota
+// 	// note: limited resource does a matchContains, so we now have "pods" matching "pods" and "count/pods"
+// 	quota := &v1.ResourceQuota{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Name:      "quota",
+// 			Namespace: ns.Name,
+// 		},
+// 		Spec: v1.ResourceQuotaSpec{
+// 			Hard: v1.ResourceList{
+// 				v1.ResourcePods:               resource.MustParse("1000"),
+// 				v1.ResourceName("count/pods"): resource.MustParse("1000"),
+// 			},
+// 		},
+// 	}
+// 	waitForQuota(t, quota, clientset)
+
+// 	// attempt to create a new pod once the quota is propagated
+// 	err = wait.PollImmediate(5*time.Second, time.Minute, func() (bool, error) {
+// 		// retry until we succeed (to allow time for all changes to propagate)
+// 		if _, err := clientset.CoreV1().Pods(ns.Name).Create(pod); err == nil {
+// 			return true, nil
+// 		}
+// 		return false, nil
+// 	})
+// 	if err != nil {
+// 		t.Fatalf("unexpected error: %v", err)
+// 	}
+// }
+
+func TestQuotaLimitedResourceDelete(t *testing.T) {
 	// Set up a master
 	h := &framework.MasterHolder{Initialized: make(chan struct{})}
 	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
@@ -367,6 +518,16 @@ func TestQuotaLimitedResourceDenial(t *testing.T) {
 		}
 		return false, nil
 	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	// now delete the quota
+	waitForQuotaDelete(t, quota.Name, ns.Name, clientset)
+
+	// now delete the pod
+	integration.DeletePodOrErrorf(t, clientset, ns.Name, pod.Name)
+	err = integration.WaitForPodToDisappear(clientset.CoreV1().Pods(ns.Name), pod.Name, 10*time.Second, 1*time.Minute)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
