cymtrick/kubernetes-error-fuzzer

Delete only unscheduled pods if node doesn't exist anymore.

pkg/controller/daemon/daemon_controller_test.go
@@ -2447,7 +2447,7 @@ func TestDeleteNoDaemonPod(t *testing.T) {
 	}
 }
 
-func TestDeletePodForNotExistingNode(t *testing.T) {
+func TestDeleteUnscheduledPodForNotExistingNode(t *testing.T) {
 	for _, f := range []bool{true, false} {
 		defer utilfeaturetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.ScheduleDaemonSetPods, f)()
 		for _, strategy := range updateStrategies() {
@@ -2461,6 +2461,26 @@ func TestDeletePodForNotExistingNode(t *testing.T) {
 			addNodes(manager.nodeStore, 0, 1, nil)
 			addPods(manager.podStore, "node-0", simpleDaemonSetLabel, ds, 1)
 			addPods(manager.podStore, "node-1", simpleDaemonSetLabel, ds, 1)
+
+			podScheduledUsingAffinity := newPod("pod1-node-3", "", simpleDaemonSetLabel, ds)
+			podScheduledUsingAffinity.Spec.Affinity = &v1.Affinity{
+				NodeAffinity: &v1.NodeAffinity{
+					RequiredDuringSchedulingIgnoredDuringExecution: &v1.NodeSelector{
+						NodeSelectorTerms: []v1.NodeSelectorTerm{
+							{
+								MatchFields: []v1.NodeSelectorRequirement{
+									{
+										Key:      schedulerapi.NodeFieldSelectorKeyNodeName,
+										Operator: v1.NodeSelectorOpIn,
+										Values:   []string{"node-2"},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			manager.podStore.Add(podScheduledUsingAffinity)
 			if f {
 				syncAndValidateDaemonSets(t, manager, ds, podControl, 0, 1, 0)
 			} else {

kubernetes/kubernetes

pkg/controller/daemon/daemon_controller_test.go
@@ -2664,7 +2664,7 @@ func TestDeleteNoDaemonPod(t *testing.T) {
 	}
 }
 
-func TestDeletePodForNotExistingNode(t *testing.T) {
+func TestDeleteUnscheduledPodForNotExistingNode(t *testing.T) {
 	for _, f := range []bool{true, false} {
 		setFeatureGate(t, features.ScheduleDaemonSetPods, f)
 		for _, strategy := range updateStrategies() {
@@ -2678,6 +2678,26 @@ func TestDeletePodForNotExistingNode(t *testing.T) {
 			addNodes(manager.nodeStore, 0, 1, nil)
 			addPods(manager.podStore, "node-0", simpleDaemonSetLabel, ds, 1)
 			addPods(manager.podStore, "node-1", simpleDaemonSetLabel, ds, 1)
+
+			podScheduledUsingAffinity := newPod("pod1-node-3", "", simpleDaemonSetLabel, ds)
+			podScheduledUsingAffinity.Spec.Affinity = &v1.Affinity{
+				NodeAffinity: &v1.NodeAffinity{
+					RequiredDuringSchedulingIgnoredDuringExecution: &v1.NodeSelector{
+						NodeSelectorTerms: []v1.NodeSelectorTerm{
+							{
+								MatchFields: []v1.NodeSelectorRequirement{
+									{
+										Key:      algorithm.NodeFieldSelectorKeyNodeName,
+										Operator: v1.NodeSelectorOpIn,
+										Values:   []string{"node-2"},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			manager.podStore.Add(podScheduledUsingAffinity)
 			if f {
 				syncAndValidateDaemonSets(t, manager, ds, podControl, 0, 1, 0)
 			} else {