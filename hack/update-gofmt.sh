e78410c88a8b6d8ccdbbd91d75d4f64a54680ae6
Sean Sullivan <seans@google.com>
2019-04-12 15:47:59 -0700

    Revert "Fix shellcheck failures of hack/update-gofmt.sh hack/update-translations.sh"

--- hack/update-gofmt.sh
+++ hack/update-gofmt.sh
@@ -20,7 +20,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
 source "${KUBE_ROOT}/hack/lib/init.sh"
 
 kube::golang::verify_go_version
@@ -44,4 +44,4 @@ find_files() {
 }
 
 GOFMT="gofmt -s -w"
-find_files | xargs "${GOFMT}"
+find_files | xargs ${GOFMT}
