23b91b80437b52ec5942252109b9ae5e5ee4ad9f
qingsenLi <li.qingsen5@zte.com.cn>
2019-04-18 22:06:40 +0800

    remove some unused const in test/e2e

--- test/e2e/autoscaling/cluster_autoscaler_scalability.go
+++ test/e2e/autoscaling/cluster_autoscaler_scalability.go
@@ -42,10 +42,7 @@ const (
 	memoryReservationTimeout = 5 * time.Minute
 	largeResizeTimeout       = 8 * time.Minute
 	largeScaleUpTimeout      = 10 * time.Minute
-	largeScaleDownTimeout    = 20 * time.Minute
-	minute                   = 1 * time.Minute
-
-	maxNodes = 1000
+	maxNodes                 = 1000
 )
 
 type clusterPredicates struct {
