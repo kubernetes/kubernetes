28e1dee3336c77bafca5b5b7b6a92a1801b3b7e7
Claudiu Belu <cbelu@cloudbasesolutions.com>
2019-04-18 14:38:12 +0000

    test images: Adds Windows support for resource-consumer
    stress was being used for the memory consumption part. This adds a golang equivalent,
    which will also work on Windows.

--- test/images/resource-consumer/consume-cpu/common.go
+++ test/images/resource-consumer/consume-cpu/common.go
@@ -1,7 +1,5 @@
-// +build !windows
-
 /*
-Copyright 2015 The Kubernetes Authors.
+Copyright 2021 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -20,24 +18,20 @@ package main
 
 import (
 	"flag"
+	"math"
 	"time"
-
-	"bitbucket.org/bertimus9/systemstat"
 )
 
-func main() {
-	flag.Parse()
-	// convert millicores to percentage
-	millicoresPct := float64(*millicores) / float64(10)
-	duration := time.Duration(*durationSec) * time.Second
-	start := time.Now()
-	first := systemstat.GetProcCPUSample()
-	for time.Since(start) < duration {
-		cpu := systemstat.GetProcCPUAverage(first, systemstat.GetProcCPUSample(), systemstat.GetUptime().Uptime)
-		if cpu.TotalPct < millicoresPct {
-			doSomething()
-		} else {
-			time.Sleep(sleep)
-		}
+const sleep = time.Duration(10) * time.Millisecond
+
+func doSomething() {
+	for i := 1; i < 10000000; i++ {
+		x := float64(0)
+		x += math.Sqrt(0)
 	}
 }
+
+var (
+	millicores  = flag.Int("millicores", 0, "millicores number")
+	durationSec = flag.Int("duration-sec", 0, "duration time in seconds")
+)
