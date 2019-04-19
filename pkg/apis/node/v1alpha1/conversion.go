02288bc34f98618d57660d3eb8b4c53e79a9cac9
Yassine TIJANI <yasstij11@gmail.com>
2019-04-20 01:50:39 +0200

    reuse autoconversion

--- pkg/apis/node/v1alpha1/conversion.go
+++ pkg/apis/node/v1alpha1/conversion.go
@@ -21,7 +21,6 @@ import (
 	conversion "k8s.io/apimachinery/pkg/conversion"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	node "k8s.io/kubernetes/pkg/apis/node"
-	"unsafe"
 )
 
 func addConversionFuncs(s *runtime.Scheme) error {
@@ -34,13 +33,17 @@ func addConversionFuncs(s *runtime.Scheme) error {
 func Convert_v1alpha1_RuntimeClass_To_node_RuntimeClass(in *v1alpha1.RuntimeClass, out *node.RuntimeClass, s conversion.Scope) error {
 	out.ObjectMeta = in.ObjectMeta
 	out.Handler = in.Spec.RuntimeHandler
-	out.Topology = (*node.Topology)(unsafe.Pointer(in.Spec.Topology))
+	if in.Spec.Topology != nil {
+		autoConvert_v1alpha1_Topology_To_node_Topology(in.Spec.Topology,out.Topology,s)
+	}
 	return nil
 }
 
 func Convert_node_RuntimeClass_To_v1alpha1_RuntimeClass(in *node.RuntimeClass, out *v1alpha1.RuntimeClass, s conversion.Scope) error {
 	out.ObjectMeta = in.ObjectMeta
 	out.Spec.RuntimeHandler = in.Handler
-	out.Spec.Topology = (*v1alpha1.Topology)(unsafe.Pointer(in.Topology))
+	if in.Topology != nil {
+		autoConvert_node_Topology_To_v1alpha1_Topology(in.Topology,out.Spec.Topology,s)
+	}
 	return nil
 }
