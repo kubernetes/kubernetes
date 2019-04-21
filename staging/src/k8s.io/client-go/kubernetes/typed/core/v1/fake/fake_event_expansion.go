a297e08c80fc44499a5185639ec8cd5214190e88
John Schnake <jschnake@vmware.com>
2019-04-20 20:24:49 -0500

    Fixup

--- staging/src/k8s.io/client-go/kubernetes/typed/core/v1/fake/fake_event_expansion.go
+++ staging/src/k8s.io/client-go/kubernetes/typed/core/v1/fake/fake_event_expansion.go
@@ -17,7 +17,6 @@ limitations under the License.
 package fake
 
 import (
-	"k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
