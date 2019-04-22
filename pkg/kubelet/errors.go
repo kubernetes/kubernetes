f5736f3fd12ae2814f2809913030c2e760b9bc33
SataQiu <qiushida@beyondcent.com>
2019-04-22 10:40:51 +0800

    fix some golint failures of pkg/kubelet

--- pkg/kubelet/errors.go
+++ pkg/kubelet/errors.go
@@ -19,9 +19,11 @@ package kubelet
 import "errors"
 
 const (
+	// NetworkNotReadyErrorMsg is used to describe the error that network is not ready
 	NetworkNotReadyErrorMsg = "network is not ready"
 )
 
 var (
+	// ErrNetworkUnknown indicates the network state is unknown
 	ErrNetworkUnknown = errors.New("network state unknown")
 )
