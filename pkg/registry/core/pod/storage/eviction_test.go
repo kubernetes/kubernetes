37f266349c3297cf24f16e031ca8d73c473d8b25
Antoine Pelisse <apelisse@google.com>
2019-04-23 15:16:32 -0700

    Fix eviction dry-run

--- pkg/registry/core/pod/storage/eviction_test.go
+++ pkg/registry/core/pod/storage/eviction_test.go
@@ -17,16 +17,25 @@ limitations under the License.
 package storage
 
 import (
+	"context"
 	"testing"
 
 	policyv1beta1 "k8s.io/api/policy/v1beta1"
+	"k8s.io/apimachinery/pkg/api/apitesting"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	examplev1 "k8s.io/apiserver/pkg/apis/example/v1"
 	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
+	"k8s.io/apiserver/pkg/registry/generic"
+	genericregistry "k8s.io/apiserver/pkg/registry/generic/registry"
+	"k8s.io/apiserver/pkg/storage"
+	etcdtesting "k8s.io/apiserver/pkg/storage/etcd/testing"
 	"k8s.io/client-go/kubernetes/fake"
 	api "k8s.io/kubernetes/pkg/apis/core"
 	"k8s.io/kubernetes/pkg/apis/policy"
+	"k8s.io/kubernetes/pkg/registry/registrytest"
 )
 
 func TestEviction(t *testing.T) {
@@ -132,6 +141,122 @@ func TestEviction(t *testing.T) {
 				if existingPod.(*api.Pod).DeletionTimestamp == nil {
 					t.Errorf("expected gracefully deleted pod with deletionTimestamp set, got %#v", existingPod)
 				}
+			}
+		})
+	}
+}
+
+type FailDeleteUpdateStorage struct {
+	storage.Interface
+}
+
+func (f FailDeleteUpdateStorage) Delete(ctx context.Context, key string, out runtime.Object, precondition *storage.Preconditions) error {
+	return storage.NewKeyNotFoundError(key, 0)
+}
+
+func (f FailDeleteUpdateStorage) GuaranteedUpdate(ctx context.Context, key string, ptrToType runtime.Object, ignoreNotFound bool,
+	preconditions *storage.Preconditions, tryUpdate storage.UpdateFunc, suggestion ...runtime.Object) error {
+	return storage.NewKeyNotFoundError(key, 0)
+}
+
+var scheme = runtime.NewScheme()
+var codecs = serializer.NewCodecFactory(scheme)
+
+func newFailDeleteUpdateStorage(t *testing.T) (*REST, *etcdtesting.EtcdTestServer) {
+	etcdStorage, server := registrytest.NewEtcdStorage(t, "")
+	restOptions := generic.RESTOptions{
+		StorageConfig:           etcdStorage,
+		Decorator:               generic.UndecoratedStorage,
+		DeleteCollectionWorkers: 3,
+		ResourcePrefix:          "pods",
+	}
+	storage := NewStorage(restOptions, nil, nil, nil)
+	storage.Pod.Store.Storage = genericregistry.DryRunnableStorage{
+		Storage: FailDeleteUpdateStorage{storage.Pod.Store.Storage.Storage},
+		Codec:   apitesting.TestStorageCodec(codecs, examplev1.SchemeGroupVersion),
+	}
+	return storage.Pod, server
+}
+
+func TestEvictionDryRun(t *testing.T) {
+	testcases := []struct {
+		name            string
+		evictionOptions *metav1.DeleteOptions
+		requestOptions  *metav1.CreateOptions
+		pod             *api.Pod
+		pdbs            []runtime.Object
+	}{
+		{
+			name:            "just request-options",
+			requestOptions:  &metav1.CreateOptions{DryRun: []string{"All"}},
+			evictionOptions: &metav1.DeleteOptions{},
+			pod: func() *api.Pod {
+				pod := validNewPod()
+				pod.Labels = map[string]string{"a": "true"}
+				pod.Spec.NodeName = "foo"
+				return pod
+			}(),
+		},
+		{
+			name:            "just eviction-options",
+			requestOptions:  &metav1.CreateOptions{},
+			evictionOptions: &metav1.DeleteOptions{DryRun: []string{"All"}},
+			pod: func() *api.Pod {
+				pod := validNewPod()
+				pod.Labels = map[string]string{"a": "true"}
+				pod.Spec.NodeName = "foo"
+				return pod
+			}(),
+		},
+		{
+			name:            "both options",
+			evictionOptions: &metav1.DeleteOptions{DryRun: []string{"All"}},
+			requestOptions:  &metav1.CreateOptions{DryRun: []string{"All"}},
+			pod: func() *api.Pod {
+				pod := validNewPod()
+				pod.Labels = map[string]string{"a": "true"}
+				pod.Spec.NodeName = "foo"
+				return pod
+			}(),
+		},
+		{
+			name:            "with pdbs",
+			evictionOptions: &metav1.DeleteOptions{DryRun: []string{"All"}},
+			requestOptions:  &metav1.CreateOptions{DryRun: []string{"All"}},
+			pod: func() *api.Pod {
+				pod := validNewPod()
+				pod.Labels = map[string]string{"a": "true"}
+				pod.Spec.NodeName = "foo"
+				return pod
+			}(),
+			pdbs: []runtime.Object{&policyv1beta1.PodDisruptionBudget{
+				ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"},
+				Spec:       policyv1beta1.PodDisruptionBudgetSpec{Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"a": "true"}}},
+				Status:     policyv1beta1.PodDisruptionBudgetStatus{PodDisruptionsAllowed: 1},
+			}},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			testContext := genericapirequest.WithNamespace(genericapirequest.NewContext(), metav1.NamespaceDefault)
+			storage, server := newFailDeleteUpdateStorage(t)
+			defer server.Terminate(t)
+			defer storage.Store.DestroyFunc()
+
+			pod := validNewPod()
+			pod.Labels = map[string]string{"a": "true"}
+			pod.Spec.NodeName = "foo"
+			if _, err := storage.Create(testContext, pod, nil, &metav1.CreateOptions{}); err != nil {
+				t.Error(err)
+			}
+
+			client := fake.NewSimpleClientset()
+			evictionRest := newEvictionStorage(storage.Store, client.PolicyV1beta1())
+			eviction := &policy.Eviction{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"}, DeleteOptions: tc.evictionOptions}
+			_, err := evictionRest.Create(testContext, eviction, nil, tc.requestOptions)
+			if err != nil {
+				t.Fatalf("Failed to run eviction: %v", err)
 			}
 		})
 	}
