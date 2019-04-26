33713087f43e4fd25fc314dbf5e91afc98a09724
Eric Ernst <eric.ernst@intel.com>
2019-04-26 12:24:11 -0700

    pod-overhead: drop from PodSpec based on feature-gate
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- pkg/api/pod/util.go
+++ pkg/api/pod/util.go
@@ -375,6 +375,11 @@ func dropDisabledFields(
 		podSpec.RuntimeClassName = nil
 	}
 
+	if !utilfeature.DefaultFeatureGate.Enabled(features.PodOverhead) && !overheadInUse(oldPodSpec) {
+		// Set Overhead to nil only if the feature is disabled and it is not used
+		podSpec.Overhead = nil
+	}
+
 	dropDisabledProcMountField(podSpec, oldPodSpec)
 
 	dropDisabledCSIVolumeSourceAlphaFields(podSpec, oldPodSpec)
@@ -522,6 +527,18 @@ func runtimeClassInUse(podSpec *api.PodSpec) bool {
 		return true
 	}
 	return false
+}
+
+// overheadInUse returns true if the pod spec is non-nil and has Overhead set
+func overheadInUse(podSpec *api.PodSpec) bool {
+	if podSpec == nil {
+		return false
+	}
+	if podSpec.Overhead != nil {
+		return true
+	}
+	return false
+
 }
 
 // procMountInUse returns true if the pod spec is non-nil and has a SecurityContext's ProcMount field set to a non-default value
