565c88de14ab35be16bbb51315e3b9a955bb73df
Ted Yu <yuzhihong@gmail.com>
2019-04-29 14:05:06 -0700

    Return early when node expansion is determined

--- pkg/volume/csi/csi_client.go
+++ pkg/volume/csi/csi_client.go
@@ -688,6 +688,7 @@ func (c *csiDriverClient) NodeSupportsNodeExpand(ctx context.Context) (bool, err
 		for _, capability := range capabilities {
 			if capability.GetRpc().GetType() == csipbv1.NodeServiceCapability_RPC_EXPAND_VOLUME {
 				nodeExpandSet = true
+				return true, nil
 			}
 		}
 		return nodeExpandSet, nil
