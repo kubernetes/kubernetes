4faeeb8c28ae243da2dc7e55a61325d6343bf8d2
Han Kang <hankang@google.com>
2019-04-29 10:45:08 -0700

    remove two unused metrics

--- pkg/master/master.go
+++ pkg/master/master.go
@@ -89,7 +89,6 @@ import (
 	"k8s.io/kubernetes/pkg/serviceaccount"
 	nodeutil "k8s.io/kubernetes/pkg/util/node"
 
-	"github.com/prometheus/client_golang/prometheus"
 	"k8s.io/klog"
 
 	// RESTStorage installers
