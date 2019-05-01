e6fbb593bb1646369db27e802ae70ee65a2d41be
Han Kang <hankang@google.com>
2019-05-01 15:49:28 -0700

    move framework files to subdirectory for isolation

--- pkg/util/metrics/framework/version_parser_test.go
+++ pkg/util/metrics/framework/version_parser_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package metrics
+package framework
 
 import (
 	apimachineryversion "k8s.io/apimachinery/pkg/version"
--- pkg/util/metrics/framework/wrappers.go
+++ pkg/util/metrics/framework/wrappers.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package metrics
+package framework
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
