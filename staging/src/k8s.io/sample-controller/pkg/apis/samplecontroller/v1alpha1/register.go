55d45867e6a8b6336be0c56da0b4843dd552b1b0
Baasbank <baasb4real@yahoo.com>
2019-05-03 23:10:40 +0100

    fixes golint error in staging/src/k8s.io/sample-controller/pkg/apis/samplecontroller/v1alpha1
    fixes golint error in staging/src/k8s.io/sample-controller/pkg/apis/samplecontroller
    
    fixes golint errors in staging/src/k8s.io/sample-apiserver/pkg/cmd/server
    
    fixex golint errors in staging/src/k8s.io/sample-apiserver/pkg/apiserver

--- staging/src/k8s.io/sample-controller/pkg/apis/samplecontroller/v1alpha1/register.go
+++ staging/src/k8s.io/sample-controller/pkg/apis/samplecontroller/v1alpha1/register.go
@@ -38,8 +38,10 @@ func Resource(resource string) schema.GroupResource {
 }
 
 var (
+	// SchemeBuilder initializes a scheme builder
 	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
-	AddToScheme   = SchemeBuilder.AddToScheme
+	// AddToScheme is a global function that registers this API group & version to a scheme
+	AddToScheme = SchemeBuilder.AddToScheme
 )
 
 // Adds the list of known types to Scheme.
