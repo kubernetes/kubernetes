834dad7a371eecf98c999e18efaafab1cf63e3f0
xiangqian <xiangqian@google.com>
2019-05-06 16:53:24 -0700

    rebase from k8s master

--- pkg/controller/volume/persistentvolume/pv_controller.go
+++ pkg/controller/volume/persistentvolume/pv_controller.go
@@ -22,7 +22,7 @@ import (
 	"strings"
 	"time"
 
-	// v1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	storage "k8s.io/api/storage/v1"
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
