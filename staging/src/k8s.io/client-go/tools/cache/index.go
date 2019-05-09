a0de974adca5ce8d28dc5dd9940a48bb4abaf7a1
Mike Spreitzer <mspreitz@us.ibm.com>
2019-05-09 00:29:18 -0400

    Fixed and clarified comments and parameter names in index.go
    Fixed the comment on IndexFunc to say that it returns multiple indexed values.
    
    Clarified the comments and parameter names in the Indexer interface to
    consistently use a dichotomy between "storage keys" and "indexed
    values".

--- staging/src/k8s.io/client-go/tools/cache/index.go
+++ staging/src/k8s.io/client-go/tools/cache/index.go
@@ -23,17 +23,28 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 )
 
-// Indexer is a storage interface that lets you list objects using multiple indexing functions
+// Indexer is a storage interface that lets you list objects using multiple indexing functions.
+// There are three sorts of strings here.
+// One is a storage key, as defined in the Store interface.
+// Another sort is a name of an IndexFunc.
+// The third sort of string is an "indexed value", produced by an IndexFunc.
 type Indexer interface {
 	Store
-	// Retrieve list of objects that match on the named indexing function
+	// Index returns the stored objects whose set of indexed values
+	// intersects the set of indexed values of the given object, where
+	// the indexed value sets are produced by the named IndexFunc
 	Index(indexName string, obj interface{}) ([]interface{}, error)
-	// IndexKeys returns the set of keys that match on the named indexing function.
-	IndexKeys(indexName, indexKey string) ([]string, error)
-	// ListIndexFuncValues returns the list of generated values of an Index func
+	// IndexKeys returns the storage keys of the stored objects whose
+	// set of indexed values from the named IndexFunc includes the
+	// given indexed value
+	IndexKeys(indexName, indexedValue string) ([]string, error)
+	// ListIndexFuncValues returns the union of the indexed value
+	// sets, produced by the named IndexFunc, of the objects in this
+	// Store
 	ListIndexFuncValues(indexName string) []string
-	// ByIndex lists object that match on the named indexing function with the exact key
-	ByIndex(indexName, indexKey string) ([]interface{}, error)
+	// ByIndex returns the stored objects whose set of indexed values
+	// from the named IndexFunc includes the given indexed value
+	ByIndex(indexName, indexedValue string) ([]interface{}, error)
 	// GetIndexer return the indexers
 	GetIndexers() Indexers
 
@@ -42,7 +53,7 @@ type Indexer interface {
 	AddIndexers(newIndexers Indexers) error
 }
 
-// IndexFunc knows how to provide an indexed value for an object.
+// IndexFunc knows how to extract the set of indexed values for an object.
 type IndexFunc func(obj interface{}) ([]string, error)
 
 // IndexFuncToKeyFuncAdapter adapts an indexFunc to a keyFunc.  This is only useful if your index function returns
