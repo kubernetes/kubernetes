8cb46b846e6ab543e3d251e7119f5790c0c1380b
Mucahit Kurt <mucahitkurt@gmail.com>
2019-05-17 00:57:52 +0300

    unit tests for the volume plugin name that's used inside GeneratedUnmapVolumeFunc for csi migration on/off scenarios
    Signed-off-by: Mucahit Kurt <mucahitkurt@gmail.com>

--- pkg/volume/csi/csi_plugin.go
+++ pkg/volume/csi/csi_plugin.go
@@ -59,8 +59,9 @@ const (
 	volDataFileName = "vol_data.json"
 	fsTypeBlockName = "block"
 
+	// CsiResyncPeriod is default resync period duration
 	// TODO: increase to something useful
-	csiResyncPeriod = time.Minute
+	CsiResyncPeriod = time.Minute
 )
 
 var deprecatedSocketDirVersions = []string{"0.1.0", "0.2.0", "0.3.0", "0.4.0"}
