159967a69b931c37032f2358d5cd2e0c9e6be849
linzhaoming <teleyic@gmail.com>
2019-05-17 10:49:50 +0800

    Fixed typo of docs

--- staging/src/k8s.io/api/admissionregistration/v1beta1/types.go
+++ staging/src/k8s.io/api/admissionregistration/v1beta1/types.go
@@ -232,7 +232,7 @@ type Webhook struct {
 	// +optional
 	NamespaceSelector *metav1.LabelSelector `json:"namespaceSelector,omitempty" protobuf:"bytes,5,opt,name=namespaceSelector"`
 
-	// SideEffects states whether this webhookk has side effects.
+	// SideEffects states whether this webhook has side effects.
 	// Acceptable values are: Unknown, None, Some, NoneOnDryRun
 	// Webhooks with side effects MUST implement a reconciliation system, since a request may be
 	// rejected by a future step in the admission change and the side effects therefore need to be undone.
