e58b4d4324acb82ef3f08e23892a39837c5b5ced
Mark Janssen <mark@praseodym.net>
2019-05-20 14:36:28 +0200

    Fix golint failures of pkg/kubelet/qos

--- pkg/kubelet/qos/doc.go
+++ pkg/kubelet/qos/doc.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// package qos contains helper functions for quality of service.
+// Package qos contains helper functions for quality of service.
 // For each resource (memory, CPU) Kubelet supports three classes of containers.
 // Memory guaranteed containers will receive the highest priority and will get all the resources
 // they need.
