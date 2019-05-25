5efe2c1bb7ceb0854a38205e6726911378baecb1
obitech <obitech@users.noreply.github.com>
2019-05-25 21:08:57 +0200

    Fix golint issues in pkg/kubelet/stats/log_metrics_provider.go

--- pkg/kubelet/stats/log_metrics_provider.go
+++ pkg/kubelet/stats/log_metrics_provider.go
@@ -20,12 +20,14 @@ import (
 	"k8s.io/kubernetes/pkg/volume"
 )
 
+// LogMetricsService defines an interface for providing LogMetrics functionality.
 type LogMetricsService interface {
 	createLogMetricsProvider(path string) volume.MetricsProvider
 }
 
 type logMetrics struct{}
 
+// NewLogMetricsService returns a new LogMetricsService type struct.
 func NewLogMetricsService() LogMetricsService {
 	return logMetrics{}
 }
