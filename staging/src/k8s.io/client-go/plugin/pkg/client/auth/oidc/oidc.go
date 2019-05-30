4e336fa9a40b5c1f78572bbf5180b3f692706166
Haines Chan <zhinhai@gmail.com>
2019-05-30 16:14:31 +0800

    Fix typo

--- staging/src/k8s.io/client-go/plugin/pkg/client/auth/oidc/oidc.go
+++ staging/src/k8s.io/client-go/plugin/pkg/client/auth/oidc/oidc.go
@@ -285,7 +285,7 @@ func (p *oidcAuthProvider) idToken() (string, error) {
 		// providers (Okta) don't return this value.
 		//
 		// See https://github.com/kubernetes/kubernetes/issues/36847
-		return "", fmt.Errorf("token response did not contain an id_token, either the scope \"openid\" wasn't requested upon login, or the provider doesn't support id_tokens as part of the refresh response.")
+		return "", fmt.Errorf("token response did not contain an id_token, either the scope \"openid\" wasn't requested upon login, or the provider doesn't support id_token as part of the refresh response.")
 	}
 
 	// Create a new config to persist.
