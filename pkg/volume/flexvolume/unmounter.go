cymtrick/kubernetes-error-fuzzer

fix flexvol stuck issue due to corrupted mnt point
fix comments about PathExists

fix comments

revert change in PathExists func

pkg/volume/flexvolume/unmounter.go
@@ -42,15 +42,15 @@ func (f *flexVolumeUnmounter) TearDown() error {
 }
 
 func (f *flexVolumeUnmounter) TearDownAt(dir string) error {
-
 	pathExists, pathErr := mount.PathExists(dir)
-	if !pathExists {
-		klog.Warningf("Warning: Unmount skipped because path does not exist: %v", dir)
-		return nil
-	}
-
-	if pathErr != nil && !mount.IsCorruptedMnt(pathErr) {
-		return fmt.Errorf("Error checking path: %v", pathErr)
+	if pathErr != nil {
+		// only log warning here since plugins should anyways have to deal with errors
+		klog.Warningf("Error checking path: %v", pathErr)
+	} else {
+		if !pathExists {
+			klog.Warningf("Warning: Unmount skipped because path does not exist: %v", dir)
+			return nil
+		}
 	}
 
 	call := f.plugin.NewDriverCall(unmountCmd)

kubernetes/kubernetes

pkg/volume/flexvolume/unmounter.go
@@ -43,15 +43,15 @@ func (f *flexVolumeUnmounter) TearDown() error {
 }
 
 func (f *flexVolumeUnmounter) TearDownAt(dir string) error {
-
 	pathExists, pathErr := util.PathExists(dir)
-	if !pathExists {
-		glog.Warningf("Warning: Unmount skipped because path does not exist: %v", dir)
-		return nil
-	}
-
-	if pathErr != nil && !util.IsCorruptedMnt(pathErr) {
-		return fmt.Errorf("Error checking path: %v", pathErr)
+	if pathErr != nil {
+		// only log warning here since plugins should anyways have to deal with errors
+		glog.Warningf("Error checking path: %v", pathErr)
+	} else {
+		if !pathExists {
+			glog.Warningf("Warning: Unmount skipped because path does not exist: %v", dir)
+			return nil
+		}
 	}
 
 	call := f.plugin.NewDriverCall(unmountCmd)