ff176aac2f3a0bc81b008b97a77f54a41780088c
Sean Sullivan <seans@google.com>
2019-06-19 19:13:22 -0700

    Update import statements to reference code moved to staging

--- pkg/kubectl/generate/versioned/secret_for_docker_registry.go
+++ pkg/kubectl/generate/versioned/secret_for_docker_registry.go
@@ -23,8 +23,8 @@ import (
 
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/kubectl/pkg/util/hash"
 	"k8s.io/kubernetes/pkg/kubectl/generate"
-	"k8s.io/kubernetes/pkg/kubectl/util/hash"
 )
 
 // SecretForDockerRegistryGeneratorV1 supports stable generation of a docker registry secret
