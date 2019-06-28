bb0c23aed4f46446628a4214b6369dc1ad453e23
Pengfei Ni <peni@microsoft.com>
2019-06-28 16:11:05 +0800

    Default resourceGroup should be used when value of annotation azure-load-balancer-resource-group is empty string

--- staging/src/k8s.io/legacy-cloud-providers/azure/azure_loadbalancer_test.go
+++ staging/src/k8s.io/legacy-cloud-providers/azure/azure_loadbalancer_test.go
@@ -448,6 +448,38 @@ func TestServiceOwnsPublicIP(t *testing.T) {
 	}
 }
 
+func TestGetPublicIPAddressResourceGroup(t *testing.T) {
+	az := getTestCloud()
+
+	for i, c := range []struct {
+		desc        string
+		annotations map[string]string
+		expected    string
+	}{
+		{
+			desc:     "no annotation",
+			expected: "rg",
+		},
+		{
+			desc:        "annoation with empty string resource group",
+			annotations: map[string]string{ServiceAnnotationLoadBalancerResourceGroup: ""},
+			expected:    "rg",
+		},
+		{
+			desc:        "annoation with non-empty resource group ",
+			annotations: map[string]string{ServiceAnnotationLoadBalancerResourceGroup: "rg2"},
+			expected:    "rg2",
+		},
+	} {
+		t.Run(c.desc, func(t *testing.T) {
+			s := &v1.Service{}
+			s.Annotations = c.annotations
+			real := az.getPublicIPAddressResourceGroup(s)
+			assert.Equal(t, c.expected, real, "TestCase[%d]: %s", i, c.desc)
+		})
+	}
+}
+
 func TestGetServiceTags(t *testing.T) {
 	tests := []struct {
 		desc     string
