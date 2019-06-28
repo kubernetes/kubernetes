dfc78caa2116acc94b40e8f8c5e584b1efe1cb65
Fabio Bertinatto <fbertina@redhat.com>
2019-06-28 13:31:42 +0200

    Add missing CSINodeInformer when creating ConfigFactoryArgs

--- test/integration/scheduler/util.go
+++ test/integration/scheduler/util.go
@@ -97,6 +97,7 @@ func createConfiguratorWithPodInformer(
 		ServiceInformer:                informerFactory.Core().V1().Services(),
 		PdbInformer:                    informerFactory.Policy().V1beta1().PodDisruptionBudgets(),
 		StorageClassInformer:           informerFactory.Storage().V1().StorageClasses(),
+		CSINodeInformer:                informerFactory.Storage().V1beta1().CSINodes(),
 		Registry:                       pluginRegistry,
 		Plugins:                        plugins,
 		PluginConfig:                   pluginConfig,
