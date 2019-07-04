7aa86c755204ed0365b80abd61154dbe1bef4c9a
yue9944882 <291271447@qq.com>
2019-07-04 16:43:22 +0800

    [DO NOT REVIEW]: trival codes

--- staging/src/k8s.io/client-go/kubernetes/fake/register.go
+++ staging/src/k8s.io/client-go/kubernetes/fake/register.go
@@ -41,6 +41,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	eventsv1beta1 "k8s.io/api/events/v1beta1"
 	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
+	flowcontrolv1alpha1 "k8s.io/api/flowcontrol/v1alpha1"
 	networkingv1 "k8s.io/api/networking/v1"
 	networkingv1beta1 "k8s.io/api/networking/v1beta1"
 	nodev1alpha1 "k8s.io/api/node/v1alpha1"
@@ -89,6 +90,7 @@ var localSchemeBuilder = runtime.SchemeBuilder{
 	corev1.AddToScheme,
 	eventsv1beta1.AddToScheme,
 	extensionsv1beta1.AddToScheme,
+	flowcontrolv1alpha1.AddToScheme,
 	networkingv1.AddToScheme,
 	networkingv1beta1.AddToScheme,
 	nodev1alpha1.AddToScheme,
