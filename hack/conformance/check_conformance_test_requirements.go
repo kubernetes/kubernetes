fd9dd13cc2d0729f91b8acf5808822e17ea0c75a
ramnar <ramanarayana_m@infosys.com>
2019-07-04 17:25:33 +0530

    Remove duplicate error messages
    Remove duplicate error messages when command fails

--- hack/conformance/check_conformance_test_requirements.go
+++ hack/conformance/check_conformance_test_requirements.go
@@ -151,7 +151,6 @@ func newCommand() *cobra.Command {
 func main() {
 	command := newCommand()
 	if err := command.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
