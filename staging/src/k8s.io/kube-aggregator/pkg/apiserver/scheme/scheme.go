c7ca6d0308fc875326fa9a98fa20c4246a7c777f
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2019-07-08 20:25:19 +0200

    apiextensions,kube-aggregator: unify scheme setup code

--- staging/src/k8s.io/kube-aggregator/pkg/apiserver/scheme/scheme.go
+++ staging/src/k8s.io/kube-aggregator/pkg/apiserver/scheme/scheme.go
@@ -17,14 +17,12 @@ limitations under the License.
 package scheme
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 
-	"k8s.io/kube-aggregator/pkg/apis/apiregistration"
 	"k8s.io/kube-aggregator/pkg/apis/apiregistration/install"
-	"k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
-	"k8s.io/kube-aggregator/pkg/apis/apiregistration/v1beta1"
 )
 
 var (
@@ -36,13 +34,6 @@ var (
 )
 
 func init() {
-	AddToScheme(Scheme)
 	install.Install(Scheme)
-}
-
-// AddToScheme adds the types of this group into the given scheme.
-func AddToScheme(scheme *runtime.Scheme) {
-	utilruntime.Must(v1beta1.AddToScheme(scheme))
-	utilruntime.Must(v1.AddToScheme(scheme))
-	utilruntime.Must(apiregistration.AddToScheme(scheme))
+	metav1.AddToGroupVersion(Scheme, schema.GroupVersion{Group: "", Version: "v1"})
 }
