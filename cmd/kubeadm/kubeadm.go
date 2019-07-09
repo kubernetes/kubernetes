4c1e48e4053eba7ef189662881e67c1851ef25c0
SataQiu <qiushida@beyondcent.com>
2019-07-09 10:42:22 +0800

    kubeadm: move klog.InitFlags into app/kubeadm.go

--- cmd/kubeadm/kubeadm.go
+++ cmd/kubeadm/kubeadm.go
@@ -19,12 +19,10 @@ package main
 import (
 	"os"
 
-	"k8s.io/klog"
 	"k8s.io/kubernetes/cmd/kubeadm/app"
 )
 
 func main() {
-	klog.InitFlags(nil)
 	if err := app.Run(); err != nil {
 		os.Exit(1)
 	}
