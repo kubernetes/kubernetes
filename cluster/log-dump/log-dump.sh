ed43a6c039d6aeb7deb3fa8faf38962ec1902c24
Angela Li <yanhuil@google.com>
2019-07-09 17:31:24 -0700

    Add timestamp to the docker test logs

--- cluster/log-dump/log-dump.sh
+++ cluster/log-dump/log-dump.sh
@@ -204,7 +204,7 @@ function save-logs() {
 function export-windows-docker-event-log() {
     local -r node="${1}"
 
-    local -r powershell_cmd="powershell.exe -Command \$log=\$(Get-EventLog -LogName Application -Source Docker); Set-Content '${WINDOWS_LOGS_DIR}\\docker.log' \$log.Message"
+    local -r powershell_cmd="powershell.exe -Command \$logs=\$(Get-EventLog -LogName Application -Source Docker); foreach (\$log in \$logs) {Add-Content '${WINDOWS_LOGS_DIR}\\docker.log' \$(Write-Output \$log.TimeGenerated \$log.Message)}"
 
     # Retry up to 3 times to allow ssh keys to be properly propagated and
     # stored.
