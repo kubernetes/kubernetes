3dff717256d49c9d6a2f8e99628e035b7c6e3606
Roc Chan <rockerchen@tencent.com>
2019-07-12 19:50:15 +0800

    Update autogenerated files

--- pkg/proxy/apis/config/v1alpha1/zz_generated.conversion.go
+++ pkg/proxy/apis/config/v1alpha1/zz_generated.conversion.go
@@ -223,6 +223,7 @@ func autoConvert_v1alpha1_KubeProxyIPVSConfiguration_To_config_KubeProxyIPVSConf
 	out.Scheduler = in.Scheduler
 	out.ExcludeCIDRs = *(*[]string)(unsafe.Pointer(&in.ExcludeCIDRs))
 	out.StrictARP = in.StrictARP
+	out.ExcludeExternalIP = in.ExcludeExternalIP
 	return nil
 }
 
@@ -237,6 +238,7 @@ func autoConvert_config_KubeProxyIPVSConfiguration_To_v1alpha1_KubeProxyIPVSConf
 	out.Scheduler = in.Scheduler
 	out.ExcludeCIDRs = *(*[]string)(unsafe.Pointer(&in.ExcludeCIDRs))
 	out.StrictARP = in.StrictARP
+	out.ExcludeExternalIP = in.ExcludeExternalIP
 	return nil
 }
 
