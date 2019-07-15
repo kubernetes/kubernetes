1e8f5feffdcc9e0f485153c22c1e19d1f8b69596
Caleb Woodbine <caleb@ii.coop>
2019-07-15 03:53:56 +0000

    Fix imports, Formatting of text

--- test/e2e/apps/statefulset.go
+++ test/e2e/apps/statefulset.go
@@ -25,7 +25,7 @@ import (
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	klabels "k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
@@ -763,11 +763,11 @@ var _ = SIGDescribe("StatefulSet", func() {
 		})
 
 		/*
-						Release : v1.16
-						Testname: StatefulSet resource Replica scaling
-						Description: Create a StatefulSet resource.
-			            Newly created StatefulSet resource MUST have a scale of one.
-			            Bring the scale of the StatefulSet resource up to two. StatefulSet scale MUST be at two replicas.
+			Release : v1.16
+			Testname: StatefulSet resource Replica scaling
+			Description: Create a StatefulSet resource.
+			Newly created StatefulSet resource MUST have a scale of one.
+			Bring the scale of the StatefulSet resource up to two. StatefulSet scale MUST be at two replicas.
 		*/
 		framework.ConformanceIt("should have a working scale subresource", func() {
 			ginkgo.By("Creating statefulset " + ssName + " in namespace " + ns)
