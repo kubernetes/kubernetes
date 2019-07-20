a0b39bd6d37305de2d748c8266076907caac9b0d
Philippe Martin <contact@elol.fr>
2019-07-20 22:34:47 +0200

    "kubectl explain" can display fields by categories

--- staging/src/k8s.io/kubectl/pkg/explain/fields_printer.go
+++ staging/src/k8s.io/kubectl/pkg/explain/fields_printer.go
@@ -16,10 +16,20 @@ limitations under the License.
 
 package explain
 
-import "k8s.io/kube-openapi/pkg/util/proto"
+import (
+	"sort"
+	"strconv"
+	"strings"
+
+	"k8s.io/kube-openapi/pkg/util/proto"
+)
 
 // indentDesc is the level of indentation for descriptions.
-const indentDesc = 2
+const (
+	indentDesc        = 2
+	otherCategory     = "99.other"
+	extensionCategory = "x-kubernetes-category"
+)
 
 // regularFieldsPrinter prints fields with their type and description.
 type regularFieldsPrinter struct {
@@ -38,24 +48,74 @@ func (f *regularFieldsPrinter) VisitArray(a *proto.Array) {
 // VisitKind prints a Kind type. It prints each key in the kind, with
 // the type, the required flag, and the description.
 func (f *regularFieldsPrinter) VisitKind(k *proto.Kind) {
+	categories := make(map[string][]string)
 	for _, key := range k.Keys() {
+		added := false
 		v := k.Fields[key]
-		required := ""
-		if k.IsRequired(key) {
-			required = " -required-"
+		extensions := v.GetExtensions()
+		if intCategory, ok := extensions[extensionCategory]; ok {
+			if category, ok2 := intCategory.(string); ok2 {
+				categories[category] = append(categories[category], key)
+				added = true
+			}
 		}
+		if !added {
+			categories[otherCategory] = append(categories[otherCategory], key)
+		}
+	}
 
-		if err := f.Writer.Write("%s\t<%s>%s", key, GetTypeName(v), required); err != nil {
-			f.Error = err
-			return
+	titleFromCategory := func(category string) string {
+		parts := strings.SplitN(category, ".", 2)
+		if len(parts) == 2 {
+			category = parts[1]
 		}
-		if err := f.Writer.Indent(indentDesc).WriteWrapped("%s", v.GetDescription()); err != nil {
-			f.Error = err
-			return
+		return strings.ReplaceAll(strings.ToUpper(category), "-", " ")
+	}
+
+	orderedCategories := make([]string, 0)
+	for category := range categories {
+		orderedCategories = append(orderedCategories, category)
+	}
+
+	orderByNumber := func(i, j int) bool {
+		ms := strings.Split(orderedCategories[i], ".")[0]
+		ns := strings.Split(orderedCategories[j], ".")[0]
+		m, err1 := strconv.Atoi(ms)
+		n, err2 := strconv.Atoi(ns)
+		if err1 != nil || err2 != nil {
+			return true
 		}
-		if err := f.Writer.Write(""); err != nil {
-			f.Error = err
-			return
+		return m < n
+	}
+	sort.Slice(orderedCategories, orderByNumber)
+
+	for _, category := range orderedCategories {
+		fields := categories[category]
+		if len(categories) > 1 || (len(categories) == 1 && len(categories[otherCategory]) == 0) {
+			if err := f.Writer.Write("%s\n", titleFromCategory(category)); err != nil {
+				f.Error = err
+				return
+			}
+		}
+		for _, key := range fields {
+			v := k.Fields[key]
+			required := ""
+			if k.IsRequired(key) {
+				required = " -required-"
+			}
+
+			if err := f.Writer.Write("%s\t<%s>%s", key, GetTypeName(v), required); err != nil {
+				f.Error = err
+				return
+			}
+			if err := f.Writer.Indent(indentDesc).WriteWrapped("%s", v.GetDescription()); err != nil {
+				f.Error = err
+				return
+			}
+			if err := f.Writer.Write(""); err != nil {
+				f.Error = err
+				return
+			}
 		}
 	}
 }
