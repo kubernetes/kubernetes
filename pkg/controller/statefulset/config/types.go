5846025fcb1c2f26bf47c6985cf5718b5822e39e
YueHonghui <hongf.yue@hotmail.com>
2019-07-26 15:00:09 +0800

    1. fix doc comment of statefulset config types.go 2. add missing deps k8s.io/kubernetes/pkg/controller/statefulset/config

--- pkg/controller/statefulset/config/types.go
+++ pkg/controller/statefulset/config/types.go
@@ -18,8 +18,8 @@ package config
 
 // StatefulSetControllerConfiguration contains elements describing StatefulSetController.
 type StatefulSetControllerConfiguration struct {
-	// concurrentStatefulSetSyncs is the number of deployment objects that are
-	// allowed to sync concurrently. Larger number = more responsive deployments,
+	// concurrentStatefulSetSyncs is the number of statefulset objects that are
+	// allowed to sync concurrently. Larger number = more responsive statefulsets,
 	// but more CPU (and network) load.
 	ConcurrentStatefulSetSyncs int32
 }
