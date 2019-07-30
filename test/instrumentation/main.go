a5377a684c28c1bec30418989fe3a9ccdab8fe07
Marek Siarkowicz <siarkowicz@google.com>
2019-07-30 11:45:21 +0200

    Implement stable metric validation and verification
    Based on KEP:
    https://github.com/kubernetes/enhancements/blob/master/keps/sig-instrumentation/20190605-metrics-validation-and-verification.md
    
    Add //test/instrumentation:stable_metric_test that compares metrics
    in source code to those available in
    "test/instrumentation/testdata/stable-metrics-list.yaml".

--- test/instrumentation/main.go
+++ test/instrumentation/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 The Kubernetes Authors.
+Copyright 2019 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,6 +16,120 @@ limitations under the License.
 
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	metricFrameworkPath = `"k8s.io/kubernetes/staging/src/k8s.io/component-base/metrics"`
+	// Should equal to final directory name of metricFrameworkPath
+	defaultFrameworkImportName = "metrics"
+)
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) < 1 {
+		fmt.Fprintf(os.Stderr, "USAGE: %s <DIR or FILE> [...]\n", os.Args[0])
+		os.Exit(64)
+	}
+
+	stableMetrics := []metric{}
+	errors := []error{}
+
+	for _, arg := range flag.Args() {
+		ms, es := searchPathForStableMetrics(arg)
+		stableMetrics = append(stableMetrics, ms...)
+		errors = append(errors, es...)
+	}
+	for _, err := range errors {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+	}
+	if len(errors) != 0 {
+		os.Exit(1)
+	}
+	sort.Sort(byFQName(stableMetrics))
+	data, err := yaml.Marshal(stableMetrics)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+	fmt.Print(string(data))
+}
+
+func searchPathForStableMetrics(path string) ([]metric, []error) {
+	ms := []metric{}
+	errors := []error{}
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if strings.HasPrefix(path, "vendor") {
+			return filepath.SkipDir
+		}
+		if !strings.HasSuffix(path, ".go") {
+			return nil
+		}
+		ms, es := searchFileForStableMetrics(path, nil)
+		errors = append(errors, es...)
+		ms = append(ms, ms...)
+		return nil
+	})
+	if err != nil {
+		errors = append(errors, err)
+	}
+	return ms, errors
+}
+
+// Pass either only filename of existing file or src including source code in any format and a filename that it comes from
+func searchFileForStableMetrics(filename string, src interface{}) ([]metric, []error) {
+	fileset := token.NewFileSet()
+	tree, err := parser.ParseFile(fileset, filename, src, parser.AllErrors)
+	if err != nil {
+		return []metric{}, []error{err}
+	}
+	metricsImportName, err := getMetricsFrameworkImportName(tree)
+	if err != nil {
+		return []metric{}, addFileInformationToErrors([]error{err}, fileset)
+	}
+	if metricsImportName == "" {
+		return []metric{}, []error{}
+	}
+
+	stableMetricsFunctionCalls, errors := findStableMetricDeclaration(tree, metricsImportName)
+	metrics, es := decodeMetricCalls(stableMetricsFunctionCalls, metricsImportName)
+	errors = append(errors, es...)
+	return metrics, addFileInformationToErrors(errors, fileset)
+}
+
+func getMetricsFrameworkImportName(tree *ast.File) (string, error) {
+	var importName string
+	for _, im := range tree.Imports {
+		if im.Path.Value == metricFrameworkPath {
+			if im.Name == nil {
+				importName = defaultFrameworkImportName
+			} else {
+				if im.Name.Name == "." {
+					return "", newDecodeErrorf(im, errImport)
+				}
+				importName = im.Name.Name
+			}
+		}
+	}
+	return importName, nil
+}
+
+func addFileInformationToErrors(es []error, fileset *token.FileSet) []error {
+	for i := range es {
+		if de, ok := es[i].(*decodeError); ok {
+			es[i] = de.errorWithFileInformation(fileset)
+		}
+	}
+	return es
 }
