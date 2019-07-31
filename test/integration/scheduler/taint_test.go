6a291934380b7341d6e45d29c47a55813b19ef7f
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2019-07-31 10:05:58 -0700

    Convert tbe e2e to integration test

--- test/integration/scheduler/taint_test.go
+++ test/integration/scheduler/taint_test.go
@@ -38,6 +38,8 @@ import (
 	schedulerapi "k8s.io/kubernetes/pkg/scheduler/api"
 	"k8s.io/kubernetes/plugin/pkg/admission/podtolerationrestriction"
 	pluginapi "k8s.io/kubernetes/plugin/pkg/admission/podtolerationrestriction/apis/podtolerationrestriction"
+	"k8s.io/kubernetes/test/e2e/framework/pod"
+	imageutils "k8s.io/kubernetes/test/utils/image"
 )
 
 func newPod(nsName, name string, req, limit v1.ResourceList) *v1.Pod {
@@ -570,4 +572,360 @@ func TestTaintNodeByCondition(t *testing.T) {
 			waitForSchedulerCacheCleanup(context.scheduler, t)
 		})
 	}
+}
+
+func TestTaintBasedEvictionsForNodeUnreachable(t *testing.T) {
+	// Enable TaintNodeByCondition
+	defer featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.TaintBasedEvictions, true)()
+
+	// Build PodToleration Admission.
+	admission := podtolerationrestriction.NewPodTolerationsPlugin(&pluginapi.Configuration{})
+
+	context := initTestMaster(t, "default", admission)
+
+	// Build clientset and informers for controllers.
+	externalClientset := kubernetes.NewForConfigOrDie(&restclient.Config{
+		QPS:           -1,
+		Host:          context.httpServer.URL,
+		ContentConfig: restclient.ContentConfig{GroupVersion: &schema.GroupVersion{Group: "", Version: "v1"}}})
+	externalInformers := informers.NewSharedInformerFactory(externalClientset, time.Second)
+
+	admission.SetExternalKubeClientSet(externalClientset)
+	admission.SetExternalKubeInformerFactory(externalInformers)
+
+	// Apply feature gates to enable TaintNodesByCondition
+	algorithmprovider.ApplyFeatureGates()
+
+	context = initTestScheduler(t, context, false, nil)
+	cs := context.clientSet
+	informers := context.informerFactory
+	//nsName := context.ns.Name
+
+	// Start NodeLifecycleController for taint.
+	nc, err := nodelifecycle.NewNodeLifecycleController(
+		informers.Coordination().V1beta1().Leases(),
+		informers.Core().V1().Pods(),
+		informers.Core().V1().Nodes(),
+		informers.Apps().V1().DaemonSets(),
+		cs,
+		5*time.Second,    // Node monitor grace period
+		time.Minute,      // Node startup grace period
+		time.Millisecond, // Node monitor period
+		time.Second,      // Pod eviction timeout
+		100,              // Eviction limiter QPS
+		100,              // Secondary eviction limiter QPS
+		50,               // Large cluster threshold
+		0.55,             // Unhealthy zone threshold
+		true,             // Run taint manager
+		true,             // Use taint based evictions
+		false,            // Enabled TaintNodeByCondition feature
+	)
+	if err != nil {
+		t.Errorf("Failed to create node controller: %v", err)
+		return
+	}
+
+	go nc.Run(context.stopCh)
+
+	// Waiting for all controller sync.
+	externalInformers.Start(context.stopCh)
+	externalInformers.WaitForCacheSync(context.stopCh)
+	informers.Start(context.stopCh)
+	informers.WaitForCacheSync(context.stopCh)
+
+	nodeRes := v1.ResourceList{
+		v1.ResourceCPU:    resource.MustParse("4000m"),
+		v1.ResourceMemory: resource.MustParse("16Gi"),
+		v1.ResourcePods:   resource.MustParse("110"),
+	}
+	node1Taints := []v1.Taint{
+		{
+			Key:    schedulerapi.TaintNodeUnreachable,
+			Effect: v1.TaintEffectNoExecute,
+		}}
+
+	node0 := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "node-0",
+			Labels: map[string]string{v1.LabelZoneRegion: "region1", v1.LabelZoneFailureDomain: "zone1"},
+		},
+		Spec: v1.NodeSpec{},
+		Status: v1.NodeStatus{
+			Capacity:    nodeRes,
+			Allocatable: nodeRes,
+			Conditions: []v1.NodeCondition{
+				{
+					Type:   v1.NodeReady,
+					Status: v1.ConditionTrue,
+				},
+			},
+		},
+	}
+	node1 := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "node-1",
+			Labels: map[string]string{v1.LabelZoneRegion: "region1", v1.LabelZoneFailureDomain: "zone1"},
+		},
+		Spec: v1.NodeSpec{},
+		Status: v1.NodeStatus{
+			Capacity:    nodeRes,
+			Allocatable: nodeRes,
+			Conditions: []v1.NodeCondition{
+				{
+					Type:   v1.NodeReady,
+					Status: v1.ConditionTrue,
+				},
+			},
+		},
+	}
+	node2 := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "node-2",
+			Labels: map[string]string{v1.LabelZoneRegion: "region1", v1.LabelZoneFailureDomain: "zone1"},
+		},
+		Spec: v1.NodeSpec{},
+		Status: v1.NodeStatus{
+			Capacity:    nodeRes,
+			Allocatable: nodeRes,
+			Conditions: []v1.NodeCondition{
+				{
+					Type:   v1.NodeReady,
+					Status: v1.ConditionTrue,
+				},
+			},
+		},
+	}
+	if _, err := cs.CoreV1().Nodes().Create(node0); err != nil {
+		t.Errorf("Failed to create node, err: %v", err)
+	}
+	if _, err := cs.CoreV1().Nodes().Create(node1); err != nil {
+		t.Errorf("Failed to create node, err: %v", err)
+	}
+	if _, err := cs.CoreV1().Nodes().Create(node2); err != nil {
+		t.Errorf("Failed to create node, err: %v", err)
+	}
+
+	node1.Status.Conditions = []v1.NodeCondition{
+		{
+			Type:   v1.NodeReady,
+			Status: v1.ConditionUnknown,
+		},
+	}
+
+	// Update node condition.
+	err = updateNodeStatus(cs, node1)
+	if err != nil {
+		t.Fatalf("Cannot update node: %v", err)
+	}
+
+	if err := waitForNodeTaints(cs, node1, node1Taints); err != nil {
+		t.Errorf("Failed to taint node <%s>, err: %v", node1.Name, err)
+	}
+
+	cleanupNodes(cs, t)
+	waitForSchedulerCacheCleanup(context.scheduler, t)
+
+}
+
+func TestTaintBasedEvictionForNodeNotReady(t *testing.T) {
+	// Enable TaintNodeByCondition
+	defer featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.TaintBasedEvictions, true)()
+
+	// Build PodToleration Admission.
+	admission := podtolerationrestriction.NewPodTolerationsPlugin(&pluginapi.Configuration{})
+
+	context := initTestMaster(t, "default", admission)
+
+	// Build clientset and informers for controllers.
+	externalClientset := kubernetes.NewForConfigOrDie(&restclient.Config{
+		QPS:           -1,
+		Host:          context.httpServer.URL,
+		ContentConfig: restclient.ContentConfig{GroupVersion: &schema.GroupVersion{Group: "", Version: "v1"}}})
+	externalInformers := informers.NewSharedInformerFactory(externalClientset, time.Second)
+
+	admission.SetExternalKubeClientSet(externalClientset)
+	admission.SetExternalKubeInformerFactory(externalInformers)
+
+	// Apply feature gates to enable TaintNodesByCondition
+	algorithmprovider.ApplyFeatureGates()
+
+	context = initTestScheduler(t, context, true, nil)
+	cs := context.clientSet
+	informers := context.informerFactory
+	nsName := context.ns.Name
+
+	// Start NodeLifecycleController for taint.
+	nc, err := nodelifecycle.NewNodeLifecycleController(
+		informers.Coordination().V1beta1().Leases(),
+		informers.Core().V1().Pods(),
+		informers.Core().V1().Nodes(),
+		informers.Apps().V1().DaemonSets(),
+		cs,
+		5*time.Second,    // Node monitor grace period
+		time.Minute,      // Node startup grace period
+		time.Millisecond, // Node monitor period
+		time.Second,      // Pod eviction timeout
+		100,              // Eviction limiter QPS
+		100,              // Secondary eviction limiter QPS
+		50,               // Large cluster threshold
+		0.55,             // Unhealthy zone threshold
+		true,             // Run taint manager
+		true,             // Use taint based evictions
+		false,            // Enabled TaintNodeByCondition feature
+	)
+	if err != nil {
+		t.Errorf("Failed to create node controller: %v", err)
+		return
+	}
+
+	go nc.Run(context.stopCh)
+
+	// Waiting for all controller sync.
+	externalInformers.Start(context.stopCh)
+	externalInformers.WaitForCacheSync(context.stopCh)
+	informers.Start(context.stopCh)
+	informers.WaitForCacheSync(context.stopCh)
+
+	nodeRes := v1.ResourceList{
+		v1.ResourceCPU:    resource.MustParse("4000m"),
+		v1.ResourceMemory: resource.MustParse("16Gi"),
+		v1.ResourcePods:   resource.MustParse("110"),
+	}
+
+	node2Taints := []v1.Taint{
+		{
+			Key:    schedulerapi.TaintNodeNotReady,
+			Effect: v1.TaintEffectNoExecute,
+		},
+	}
+
+	node0 := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "node-0",
+			Labels: map[string]string{v1.LabelZoneRegion: "region1", v1.LabelZoneFailureDomain: "zone1"},
+		},
+		Spec: v1.NodeSpec{},
+		Status: v1.NodeStatus{
+			Capacity:    nodeRes,
+			Allocatable: nodeRes,
+			Conditions: []v1.NodeCondition{
+				{
+					Type:   v1.NodeReady,
+					Status: v1.ConditionTrue,
+				},
+			},
+		},
+	}
+	node1 := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "node-1",
+			Labels: map[string]string{v1.LabelZoneRegion: "region1", v1.LabelZoneFailureDomain: "zone1"},
+		},
+		Spec: v1.NodeSpec{},
+		Status: v1.NodeStatus{
+			Capacity:    nodeRes,
+			Allocatable: nodeRes,
+			Conditions: []v1.NodeCondition{
+				{
+					Type:   v1.NodeReady,
+					Status: v1.ConditionTrue,
+				},
+			},
+		},
+	}
+	node2 := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "node-2",
+			Labels: map[string]string{v1.LabelZoneRegion: "region1", v1.LabelZoneFailureDomain: "zone1"},
+		},
+		Spec: v1.NodeSpec{},
+		Status: v1.NodeStatus{
+			Capacity:    nodeRes,
+			Allocatable: nodeRes,
+			Conditions: []v1.NodeCondition{
+				{
+					Type:   v1.NodeReady,
+					Status: v1.ConditionTrue,
+				},
+			},
+		},
+	}
+	if _, err := cs.CoreV1().Nodes().Create(node0); err != nil {
+		t.Errorf("Failed to create node, err: %v", err)
+	}
+	if _, err := cs.CoreV1().Nodes().Create(node1); err != nil {
+		t.Errorf("Failed to create node, err: %v", err)
+	}
+	if _, err := cs.CoreV1().Nodes().Create(node2); err != nil {
+		t.Errorf("Failed to create node, err: %v", err)
+	}
+
+	testPod1 := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{Name: "testpod1"},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{Name: "container", Image: imageutils.GetPauseImageName()},
+			},
+		},
+	}
+	zero := int64(0)
+	testPod1.Spec.Tolerations = []v1.Toleration{
+		{
+			Key:               schedulerapi.TaintNodeUnreachable,
+			Operator:          v1.TolerationOpExists,
+			Effect:            v1.TaintEffectNoExecute,
+			TolerationSeconds: &zero,
+		},
+	}
+
+	testPod1, err = cs.CoreV1().Pods(context.ns.Name).Create(testPod1)
+	if err != nil {
+		t.Fatalf("Test Failed: error: %v, while creating pod", err)
+	}
+
+	if err := waitForPodToSchedule(cs, testPod1); err != nil {
+		t.Errorf("Failed to schedule pod %s/%s on the node, err: %v",
+			testPod1.Namespace, testPod1.Name, err)
+	}
+	testPod1, err = cs.CoreV1().Pods(context.ns.Name).Get(testPod1.Name, metav1.GetOptions{})
+	if err != nil {
+		t.Fatalf("Test Failed: error: %v, while creating pod", err)
+	}
+	neededNode, err := cs.CoreV1().Nodes().Get(testPod1.Spec.NodeName, metav1.GetOptions{})
+	if err != nil {
+		t.Fatalf("Error while getting node associated with pod %v with err %v", testPod1.Name, err)
+	}
+	neededNode.Status.Conditions = []v1.NodeCondition{
+		{
+			Type:   v1.NodeReady,
+			Status: v1.ConditionFalse,
+		},
+	}
+
+	// Update node condition.
+	err = updateNodeStatus(cs, neededNode)
+	if err != nil {
+		t.Fatalf("Cannot update node: %v", err)
+	}
+
+	if err := waitForNodeTaints(cs, neededNode, node2Taints); err != nil {
+		t.Errorf("Failed to taint node <%s>, err: %v", neededNode.Name, err)
+	}
+	err = pod.WaitForPodCondition(cs, nsName, testPod1.Name, "test pod should be deleted", time.Second*15, func(pod *v1.Pod) (bool, error) {
+		// as node is unreachable, pod0 is expected to be in Terminating status
+		// rather than getting deleted
+		if pod.DeletionTimestamp != nil {
+			return true, nil
+		}
+		return false, nil
+	})
+	if err != nil {
+		pod, _ := cs.CoreV1().Pods(nsName).Get(testPod1.Name, metav1.GetOptions{})
+		t.Fatalf("Expected test pod %v to be evicted immediately but it's in %v", err, pod)
+	}
+
+	cleanupPods(cs, t, []*v1.Pod{testPod1})
+	cleanupNodes(cs, t)
+	waitForSchedulerCacheCleanup(context.scheduler, t)
+
 }
