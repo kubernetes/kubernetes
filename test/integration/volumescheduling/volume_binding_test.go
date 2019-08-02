00c3f6b2142d1688fbad5a76b2d99ae45713e80a
Michelle Au <msau@google.com>
2019-08-02 11:55:50 -0700

    test boilerplate
    Change-Id: Ia91830fc1a7f2f794fcaa78ffa6b78ca14786e5f

--- test/integration/volumescheduling/volume_binding_test.go
+++ test/integration/volumescheduling/volume_binding_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package scheduler
+package volumescheduling
 
 // This file tests the VolumeScheduling feature.
 
