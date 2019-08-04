17fb7b108de051517c673e9047d5ab32421f3422
Mike Spreitzer <mspreitz@us.ibm.com>
2019-08-04 00:41:10 -0400

    Continue refactoring started in PR 80628
    Moved the top-level logic associated with the data in
    k8s.io/apiserver/pkg/util/flowcontrol into the same package, greatly
    simplifying its interface.
    
    Also plumbed the call to NewRequestManagementSystem (1) into
    `RecommendedOptions::ApplyTo(RecommendedConfig)` for running in
    aggregated servers and (2) into buildGenericConfig in the
    kube-apiserver code.
    
    Also moved the definitions of FairQueuingSystem and FairQueuingFactory
    into the directory where Aaron is developing his implementation of
    those.
    
    Also did some renaming to satisfy golint: FairQueuingSytem became
    QueueSet, and FairQueuingFactory became QueueSetFactory.
    
    Also realized that the clock should be passed in every Create... call
    to the Factory, the Factory should hold the clock to use (as well as
    the WaitGroup that will be helpful for testing).

--- staging/src/k8s.io/apiserver/pkg/util/flowcontrol/reqmgmt.go
+++ staging/src/k8s.io/apiserver/pkg/util/flowcontrol/reqmgmt.go
@@ -31,9 +31,11 @@ import (
 
 	"k8s.io/apiserver/pkg/authentication/user"
 	"k8s.io/apiserver/pkg/endpoints/request"
+	fq "k8s.io/apiserver/pkg/util/flowcontrol/fairqueuing"
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog"
 
 	rmtypesv1alpha1 "k8s.io/api/flowcontrol/v1alpha1"
 	rmlistersv1alpha1 "k8s.io/client-go/listers/flowcontrol/v1alpha1"
@@ -41,69 +43,19 @@ import (
 
 // Interface defines how the request-management filter interacts with the underlying system.
 type Interface interface {
-	GetCurrentState() *RequestManagementState
+	// Wait decides what to do about the request with the given digest
+	// and, if appropriate, enqueues that request and waits for it to be
+	// dequeued before returning.  If `execute == false` then the request
+	// is being rejected.  If `execute == true` then the caller should
+	// handle the request and then call `afterExecute()`.
+	Wait(requestDigest RequestDigest) (execute bool, afterExecute func())
+
+	// Run monitors config objects from the main apiservers and causes
+	// any needed changes to local behavior
 	Run(stopCh <-chan struct{}) error
 }
 
 // This request filter implements https://github.com/kubernetes/enhancements/blob/master/keps/sig-api-machinery/20190228-priority-and-fairness.md
-
-// FairQueuingFactory knows how to make FairQueueingSystem objects.
-// This filter makes a FairQueuingSystem for each priority level.
-type FairQueuingFactory interface {
-	NewFairQueuingSystem(concurrencyLimit, numQueues, queueLengthLimit int, requestWaitLimit time.Duration, clk clock.Clock) FairQueuingSystem
-}
-
-// FairQueuingSystem is the abstraction for the queuing and
-// dispatching functionality of one non-exempt priority level.  It
-// covers the functionality described in the "Assignment to a Queue",
-// "Queuing", and "Dispatching" sections of
-// https://github.com/kubernetes/enhancements/blob/master/keps/sig-api-machinery/20190228-priority-and-fairness.md
-// .  Some day we may have connections between priority levels, but
-// today is not that day.
-type FairQueuingSystem interface {
-	// SetConfiguration updates the configuration
-	SetConfiguration(concurrencyLimit, desiredNumQueues, queueLengthLimit int, requestWaitLimit time.Duration)
-
-	// Quiesce controls whether this system is quiescing.  Passing a
-	// non-nil handler means the system should become quiescent, a nil
-	// handler means the system should become non-quiescent.  A call
-	// to Wait while the system is quiescent will be rebuffed by
-	// returning `quiescent=true`.  If all the queues have no requests
-	// waiting nor executing while the system is quiescent then the
-	// handler will eventually be called with no locks held (even if
-	// the system becomes non-quiescent between the triggering state
-	// and the required call).
-	//
-	// The filter uses this for a priority level that has become
-	// undesired, setting a handler that will cause the priority level
-	// to eventually be removed from the filter if the filter still
-	// wants that.  If the filter later changes its mind and wants to
-	// preserve the priority level then the filter can use this to
-	// cancel the handler registration.
-	Quiesce(EmptyHandler)
-
-	// Wait, in the happy case, shuffle shards the given request into
-	// a queue and eventually dispatches the request from that queue.
-	// Dispatching means to return with `quiescent==false` and
-	// `execute==true`.  In one unhappy case the request is
-	// immediately rebuffed with `quiescent==true` (which tells the
-	// filter that there has been a timing splinter and the filter
-	// re-calcuates the priority level to use); in all other cases
-	// `quiescent` will be returned `false` (even if the system is
-	// quiescent by then).  In the non-quiescent unhappy cases the
-	// request is eventually rejected, which means to return with
-	// `execute=false`.  In the happy case the caller is required to
-	// invoke the returned `afterExecution` after the request is done
-	// executing.  The hash value and hand size are used to do the
-	// shuffle sharding.
-	Wait(hashValue uint64, handSize int32) (quiescent, execute bool, afterExecution func())
-}
-
-// EmptyHandler can be notified that something is empty
-type EmptyHandler interface {
-	// HandleEmpty is called to deliver the notification
-	HandleEmpty()
-}
 
 // RequestManagementState is the variable state that this filter is working with at a
 // given point in time.
@@ -115,16 +67,6 @@ type RequestManagementState struct {
 	// priorityLevelStates maps the PriorityLevelConfiguration object
 	// name to the state for that level
 	priorityLevelStates map[string]*PriorityLevelState
-}
-
-// GetFlowSchemas returns an array of latest observed flow-schemas by the system
-func (s *RequestManagementState) GetFlowSchemas() []*rmtypesv1alpha1.FlowSchema {
-	return s.flowSchemas
-}
-
-// GetPriorityLevelStates returns a map of latest states of the existing priority-levels
-func (s *RequestManagementState) GetPriorityLevelStates() map[string]*PriorityLevelState {
-	return s.priorityLevelStates
 }
 
 // FlowSchemaSequence holds sorted set of pointers to FlowSchema objects.
@@ -141,17 +83,7 @@ type PriorityLevelState struct {
 	// concurrencyLimit is the limit on number executing
 	concurrencyLimit int
 
-	fairQueuingSystem FairQueuingSystem
-}
-
-// IsExempt returns if the priority-level is exempt
-func (s *PriorityLevelState) IsExempt() bool {
-	return s.config.Exempt
-}
-
-// GetHandSize returns the hand-size of the priority-level
-func (s *PriorityLevelState) GetHandSize() int32 {
-	return s.config.HandSize
+	queues fq.QueueSet
 }
 
 // requestManagement holds all the state and infrastructure of this
@@ -159,7 +91,7 @@ func (s *PriorityLevelState) GetHandSize() int32 {
 type requestManagementSystem struct {
 	clk clock.Clock
 
-	fairQueuingFactory FairQueuingFactory
+	fairQueuingFactory fq.QueueSetFactory
 
 	// configQueue holds TypedConfigObjectReference values, identifying
 	// config objects that need to be processed
@@ -200,17 +132,6 @@ type requestManagementSystem struct {
 	curState atomic.Value
 }
 
-// TypedConfigObjectReference is a reference to a relevant config API object.
-// No namespace is needed because none of these objects is namespaced.
-type TypedConfigObjectReference struct {
-	Kind string
-	Name string
-}
-
-func (tr *TypedConfigObjectReference) String() string {
-	return tr.Kind + "/" + tr.Name
-}
-
 // NewRequestManagementSystem creates a new instance of request-management system
 func NewRequestManagementSystem(
 	informerFactory kubeinformers.SharedInformerFactory,
@@ -230,20 +151,10 @@ func NewRequestManagementSystem(
 	return reqMgmt
 }
 
-// Run bootstraps the request-management system
-func (r *requestManagementSystem) Run(stopCh <-chan struct{}) error {
+// Run runs the controller that deals with config API objects
+func (reqMgmt *requestManagementSystem) Run(stopCh <-chan struct{}) error {
 	// TODO: implement
 	return nil
-}
-
-// GetCurrentState returns the current state of the request-management system
-func (r *requestManagementSystem) GetCurrentState() *RequestManagementState {
-	return r.curState.Load().(*RequestManagementState)
-}
-
-// GetFairQueuingSystem returns the fair-queuing system of the priority-level
-func (s *PriorityLevelState) GetFairQueuingSystem() FairQueuingSystem {
-	return s.fairQueuingSystem
 }
 
 // RequestDigest holds necessary info from request for flow-control
@@ -252,23 +163,36 @@ type RequestDigest struct {
 	User        user.Info
 }
 
-// PickFlowSchema returns a best-matching flow-schema according to the request
-func PickFlowSchema(digest RequestDigest, flowSchemas FlowSchemaSequence, priorityLevelStates map[string]*PriorityLevelState) *rmtypesv1alpha1.FlowSchema {
-	// TODO: implement
-	return nil
+func (reqMgmt *requestManagementSystem) Wait(requestDigest RequestDigest) (execute bool, afterExecute func()) {
+	for {
+		rmState := reqMgmt.curState.Load().(*RequestManagementState)
+		fs := rmState.pickFlowSchema(requestDigest)
+		// RequestManagementState is constructed to guarantee that the following succeeds
+		ps := rmState.priorityLevelStates[fs.Spec.PriorityLevelConfiguration.Name]
+		if ps.config.Exempt {
+			klog.V(7).Infof("Serving %v without delay", requestDigest)
+			return true, func() {}
+		}
+		flowDistinguisher := requestDigest.ComputeFlowDistinguisher(fs.Spec.DistinguisherMethod)
+		hashValue := hashFlowID(fs.Name, flowDistinguisher)
+		quiescent, execute, afterExecute := ps.queues.Wait(hashValue, ps.config.HandSize)
+		if quiescent {
+			klog.V(5).Infof("Request %v landed in timing splinter, re-classifying", requestDigest)
+			continue
+		}
+		return execute, afterExecute
+	}
 }
 
-// ComputeFlowDistinguisher computes flow-distinguisher according to request information for a flow-schema
-func ComputeFlowDistinguisher(digest RequestDigest, flowSchema *rmtypesv1alpha1.FlowSchema) uint64 {
-	var fDistinguisher string
+func (rmState *RequestManagementState) pickFlowSchema(rd RequestDigest) *rmtypesv1alpha1.FlowSchema {
+	return nil
 	// TODO: implement
-	return hashFlowID(flowSchema.Name, fDistinguisher)
 }
 
-// RequestPriorityState requests for the current state of a priority-level
-func RequestPriorityState(digest RequestDigest, fs *rmtypesv1alpha1.FlowSchema, priorityLevelStates map[string]*PriorityLevelState) *PriorityLevelState {
+// ComputeFlowDistinguisher extracts the flow distinguisher according to the given method
+func (rd RequestDigest) ComputeFlowDistinguisher(method *rmtypesv1alpha1.FlowDistinguisherMethod) string {
 	// TODO: implement
-	return nil
+	return ""
 }
 
 // HashFlowID hashes the inputs into 64-bits
