6bf4b250e3a476353be5d5283317de57619f0d54
Ziheng Liu <lzhfromustc@gmail.com>
2019-08-03 23:30:55 -0700

    Add critical section to protect m.allocatedDevices

--- pkg/kubelet/cm/devicemanager/manager.go
+++ pkg/kubelet/cm/devicemanager/manager.go
@@ -824,6 +824,8 @@ func (m *ManagerImpl) sanitizeNodeAllocatable(node *schedulernodeinfo.NodeInfo) 
 }
 
 func (m *ManagerImpl) isDevicePluginResource(resource string) bool {
+	m.mutex.Lock()
+        defer m.mutex.Unlock()
 	_, registeredResource := m.healthyDevices[resource]
 	_, allocatedResource := m.allocatedDevices[resource]
 	// Return true if this is either an active device plugin resource or
