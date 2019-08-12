56a2cd15fd51b49a5ab2f6e3a491a02dcd47aadb
Lubomir I. Ivanov <lubomirivanov@vmware.com>
2019-08-13 02:14:03 +0300

    kubeadm: add a upgrade health check that deploys a Job
    - Add a new preflight check for upgrade that runs the pause container
    with -v in a Job.
    - Wait for the Job to complete and return an error after N seconds.
    - Manually clean the Job because we don't have the TTL controller
    enabled in kubeadm yet (it's still alpha).

--- cmd/kubeadm/app/cmd/upgrade/common.go
+++ cmd/kubeadm/app/cmd/upgrade/common.go
@@ -126,7 +126,7 @@ func enforceRequirements(flags *applyPlanFlags, dryRun bool, newK8sVersion strin
 	}
 
 	// Run healthchecks against the cluster
-	if err := upgrade.CheckClusterHealth(client, ignorePreflightErrorsSet); err != nil {
+	if err := upgrade.CheckClusterHealth(client, &cfg.ClusterConfiguration, ignorePreflightErrorsSet); err != nil {
 		return nil, nil, nil, errors.Wrap(err, "[upgrade/health] FATAL")
 	}
 
