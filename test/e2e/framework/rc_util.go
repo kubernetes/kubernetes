6499f932666ff944b0e7aef4650a1c619bc4e175
Kenichi Omichi <ken-oomichi@wx.jp.nec.com>
2019-08-20 21:36:11 +0000

    Use log functions of core framework on [r-u]
    This makes sub packages of e2e test framework to use log functions
    of core framework instead for avoiding circular dependencies.

--- test/e2e/framework/rc_util.go
+++ test/e2e/framework/rc_util.go
@@ -30,7 +30,6 @@ import (
 	clientset "k8s.io/client-go/kubernetes"
 	scaleclient "k8s.io/client-go/scale"
 	api "k8s.io/kubernetes/pkg/apis/core"
-	e2elog "k8s.io/kubernetes/test/e2e/framework/log"
 	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
 	testutils "k8s.io/kubernetes/test/utils"
 )
@@ -101,7 +100,7 @@ func UpdateReplicationControllerWithRetries(c clientset.Interface, namespace, na
 		// Apply the update, then attempt to push it to the apiserver.
 		applyUpdate(rc)
 		if rc, err = c.CoreV1().ReplicationControllers(namespace).Update(rc); err == nil {
-			e2elog.Logf("Updating replication controller %q", name)
+			Logf("Updating replication controller %q", name)
 			return true, nil
 		}
 		updateErr = err
@@ -147,10 +146,10 @@ func WaitForReplicationController(c clientset.Interface, namespace, name string,
 	err := wait.PollImmediate(interval, timeout, func() (bool, error) {
 		_, err := c.CoreV1().ReplicationControllers(namespace).Get(name, metav1.GetOptions{})
 		if err != nil {
-			e2elog.Logf("Get ReplicationController %s in namespace %s failed (%v).", name, namespace, err)
+			Logf("Get ReplicationController %s in namespace %s failed (%v).", name, namespace, err)
 			return !exist, nil
 		}
-		e2elog.Logf("ReplicationController %s in namespace %s found.", name, namespace)
+		Logf("ReplicationController %s in namespace %s found.", name, namespace)
 		return exist, nil
 	})
 	if err != nil {
@@ -167,13 +166,13 @@ func WaitForReplicationControllerwithSelector(c clientset.Interface, namespace s
 		rcs, err := c.CoreV1().ReplicationControllers(namespace).List(metav1.ListOptions{LabelSelector: selector.String()})
 		switch {
 		case len(rcs.Items) != 0:
-			e2elog.Logf("ReplicationController with %s in namespace %s found.", selector.String(), namespace)
+			Logf("ReplicationController with %s in namespace %s found.", selector.String(), namespace)
 			return exist, nil
 		case len(rcs.Items) == 0:
-			e2elog.Logf("ReplicationController with %s in namespace %s disappeared.", selector.String(), namespace)
+			Logf("ReplicationController with %s in namespace %s disappeared.", selector.String(), namespace)
 			return !exist, nil
 		default:
-			e2elog.Logf("List ReplicationController with %s in namespace %s failed: %v", selector.String(), namespace, err)
+			Logf("List ReplicationController with %s in namespace %s failed: %v", selector.String(), namespace, err)
 			return false, nil
 		}
 	})
@@ -230,25 +229,25 @@ waitLoop:
 		for _, podID := range pods {
 			running := RunKubectlOrDie("get", "pods", podID, "-o", "template", getContainerStateTemplate, fmt.Sprintf("--namespace=%v", ns))
 			if running != "true" {
-				e2elog.Logf("%s is created but not running", podID)
+				Logf("%s is created but not running", podID)
 				continue waitLoop
 			}
 
 			currentImage := RunKubectlOrDie("get", "pods", podID, "-o", "template", getImageTemplate, fmt.Sprintf("--namespace=%v", ns))
 			currentImage = trimDockerRegistry(currentImage)
 			if currentImage != containerImage {
-				e2elog.Logf("%s is created but running wrong image; expected: %s, actual: %s", podID, containerImage, currentImage)
+				Logf("%s is created but running wrong image; expected: %s, actual: %s", podID, containerImage, currentImage)
 				continue waitLoop
 			}
 
 			// Call the generic validator function here.
 			// This might validate for example, that (1) getting a url works and (2) url is serving correct content.
 			if err := validator(c, podID); err != nil {
-				e2elog.Logf("%s is running right image but validator function failed: %v", podID, err)
+				Logf("%s is running right image but validator function failed: %v", podID, err)
 				continue waitLoop
 			}
 
-			e2elog.Logf("%s is verified up and running", podID)
+			Logf("%s is verified up and running", podID)
 			runningPods = append(runningPods, podID)
 		}
 		// If we reach here, then all our checks passed.
@@ -257,5 +256,5 @@ waitLoop:
 		}
 	}
 	// Reaching here means that one of more checks failed multiple times.  Assuming its not a race condition, something is broken.
-	e2elog.Failf("Timed out after %v seconds waiting for %s pods to reach valid state", PodStartTimeout.Seconds(), testname)
+	Failf("Timed out after %v seconds waiting for %s pods to reach valid state", PodStartTimeout.Seconds(), testname)
 }
