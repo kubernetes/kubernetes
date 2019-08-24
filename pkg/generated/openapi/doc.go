0af5692641a2f440b3a970297af851c093aef317
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2019-08-24 10:32:21 +0200

    Unify files in generated openapi packages

--- pkg/generated/openapi/doc.go
+++ pkg/generated/openapi/doc.go
@@ -14,5 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// openapi generated definitions.
+// openapi contains generated OpenAPI definitions.
 package openapi
