e5f99fac6ab90c5cc1628f58ccae58f71452dabf
Paul Fisher <pfisher@lyft.com>
2019-08-30 09:51:30 -0700

    pkg/kubelet: fix 1.14 compat for container restore error text
    Datadog: **NOT FROM UPSTREAM K8S**. From Lyft: https://github.com/lyft/kubernetes/commit/7d64f03cf8ee0551c589ad2152bc9abcf12fcb98
    
    datadog:patch

--- pkg/kubelet/kuberuntime/kuberuntime_container.go
+++ pkg/kubelet/kuberuntime/kuberuntime_container.go
@@ -716,10 +716,10 @@ func (m *kubeGenericRuntimeManager) killContainer(ctx context.Context, pod *v1.P
 			// after a kubelet restart, it's not 100% certain that the
 			// pod we're given has the container we need in the spec
 			// -- we try to recover that here.
-			restoredPod, restoredContainer, err := m.restoreSpecsFromContainerLabels(containerID)
+			restoredPod, restoredContainer, err := m.restoreSpecsFromContainerLabels(ctx, containerID)
 			if err != nil {
 				return fmt.Errorf("failed to get containerSpec %q(id=%q) in pod %q when killing container for reason %q. error: %v",
-					containerName, containerID.String(), format.Pod(pod), reason, err)
+					containerName, containerID.String(), format.Pod(pod), message, err)
 			}
 			pod, containerSpec = restoredPod, restoredContainer
 		}
