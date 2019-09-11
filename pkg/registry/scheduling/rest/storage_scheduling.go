df0d36d4a37c16c51fcff9f835df00f23b3f5e6e
draveness <i@draveness.me>
2019-09-11 09:17:23 +0800

    feat: add default quota to allow critical pods in kube-system

--- pkg/registry/scheduling/rest/storage_scheduling.go
+++ pkg/registry/scheduling/rest/storage_scheduling.go
@@ -22,6 +22,7 @@ import (
 
 	"k8s.io/klog"
 
+	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
@@ -30,6 +31,7 @@ import (
 	"k8s.io/apiserver/pkg/registry/rest"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	serverstorage "k8s.io/apiserver/pkg/server/storage"
+	clientset "k8s.io/client-go/kubernetes"
 	schedulingclient "k8s.io/client-go/kubernetes/typed/scheduling/v1"
 	"k8s.io/kubernetes/pkg/api/legacyscheme"
 	"k8s.io/kubernetes/pkg/apis/scheduling"
@@ -37,9 +39,17 @@ import (
 	schedulingapiv1alpha1 "k8s.io/kubernetes/pkg/apis/scheduling/v1alpha1"
 	schedulingapiv1beta1 "k8s.io/kubernetes/pkg/apis/scheduling/v1beta1"
 	priorityclassstore "k8s.io/kubernetes/pkg/registry/scheduling/priorityclass/storage"
+
+	"k8s.io/klog"
 )
 
-const PostStartHookName = "scheduling/bootstrap-system-priority-classes"
+const (
+	PostStartHookName = "scheduling/bootstrap-scheduler-defaults"
+
+	// DefaultSystemQuotaName is the name of default system quota which allows unlimited
+	// system critical pods to be created
+	DefaultSystemQuotaName = "default-system-quota"
+)
 
 type RESTStorageProvider struct{}
 
@@ -108,48 +118,110 @@ func (p RESTStorageProvider) v1Storage(apiResourceConfigSource serverstorage.API
 }
 
 func (p RESTStorageProvider) PostStartHook() (string, genericapiserver.PostStartHookFunc, error) {
-	return PostStartHookName, AddSystemPriorityClasses(), nil
+	return PostStartHookName, addSchedulingDefaults(), nil
 }
 
-func AddSystemPriorityClasses() genericapiserver.PostStartHookFunc {
+func (p RESTStorageProvider) GroupName() string {
+	return scheduling.GroupName
+}
+
+// addSchedulingDefaults adds the default cluster critical priorityClasses and clusterResourceQuota which allows
+// unlimited number of critical pods be created in `kube-system` namespace.
+func addSchedulingDefaults() genericapiserver.PostStartHookFunc {
 	return func(hookContext genericapiserver.PostStartHookContext) error {
 		// Adding system priority classes is important. If they fail to add, many critical system
 		// components may fail and cluster may break.
-		err := wait.Poll(1*time.Second, 30*time.Second, func() (done bool, err error) {
-			schedClientSet, err := schedulingclient.NewForConfig(hookContext.LoopbackClientConfig)
-			if err != nil {
-				utilruntime.HandleError(fmt.Errorf("unable to initialize client: %v", err))
-				return false, nil
-			}
-
-			for _, pc := range schedulingapiv1.SystemPriorityClasses() {
-				_, err := schedClientSet.PriorityClasses().Get(pc.Name, metav1.GetOptions{})
-				if err != nil {
-					if apierrors.IsNotFound(err) {
-						_, err := schedClientSet.PriorityClasses().Create(pc)
-						if err != nil && !apierrors.IsAlreadyExists(err) {
-							return false, err
-						} else {
-							klog.Infof("created PriorityClass %s with value %v", pc.Name, pc.Value)
-						}
-					} else {
-						// Unable to get the priority class for reasons other than "not found".
-						klog.Warningf("unable to get PriorityClass %v: %v. Retrying...", pc.Name, err)
-						return false, nil
-					}
-				}
-			}
-			klog.Infof("all system priority classes are created successfully or already exist.")
-			return true, nil
-		})
-		// if we're never able to make it through initialization, kill the API server.
-		if err != nil {
+		if err := addSystemPriorityClasses(hookContext); err != nil {
 			return fmt.Errorf("unable to add default system priority classes: %v", err)
+		}
+		// Add default resource quota which allows unlimited number of critical pods to be created in kube-system
+		// namespace. This is to ensure backwards compatibility with existing system where we limit critical pods
+		// to be created in `kube-system`.
+		if err := addDefaultSystemQuota(hookContext); err != nil {
+			return fmt.Errorf("unable to add default system resource quota: %v", err)
 		}
 		return nil
 	}
 }
 
-func (p RESTStorageProvider) GroupName() string {
-	return scheduling.GroupName
+func addSystemPriorityClasses(hookContext genericapiserver.PostStartHookContext) error {
+	return wait.Poll(1*time.Second, 30*time.Second, func() (done bool, err error) {
+		schedClientSet, err := schedulingclient.NewForConfig(hookContext.LoopbackClientConfig)
+		if err != nil {
+			utilruntime.HandleError(fmt.Errorf("unable to initialize client: %v", err))
+			return false, nil
+		}
+
+		for _, pc := range schedulingapiv1.SystemPriorityClasses() {
+			_, err := schedClientSet.PriorityClasses().Get(pc.Name, metav1.GetOptions{})
+			if apierrors.IsNotFound(err) {
+				_, err := schedClientSet.PriorityClasses().Create(pc)
+				if err != nil && !apierrors.IsAlreadyExists(err) {
+					return false, err
+				}
+
+				klog.Infof("created PriorityClass %s with value %v", pc.Name, pc.Value)
+			}
+
+			if err != nil {
+				// Unable to get the priority class for reasons other than "not found".
+				klog.Warningf("unable to get PriorityClass %v: %v. Retrying...", pc.Name, err)
+				return false, nil
+			}
+		}
+		klog.Infof("all system priority classes are created successfully or already exist.")
+		return true, nil
+	})
+}
+
+// addDefaultSystemQuota adds a quota which allows unlimited critical pods to be created in `kube-system` namespace
+// This quota can be deleted/updated by cluster-admin. If the cluster-admin wants critical pods to be created in
+// namespace other than `kube-system`, he/she can configure quotas which allows these critical pods to be created
+// in that namespace.
+func addDefaultSystemQuota(hookContext genericapiserver.PostStartHookContext) error {
+	return wait.Poll(1*time.Second, 30*time.Second, func() (done bool, err error) {
+		client := clientset.NewForConfigOrDie(hookContext.LoopbackClientConfig)
+		_, err = client.CoreV1().ResourceQuotas(metav1.NamespaceSystem).Get(DefaultSystemQuotaName, metav1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			_, err := client.CoreV1().ResourceQuotas(metav1.NamespaceSystem).Create(systemDefaultQuota())
+			if err != nil && !apierrors.IsAlreadyExists(err) {
+				klog.V(5).Infof("Unable to create system default resourceQuota %v: %v. Retrying..", DefaultSystemQuotaName, err)
+				return false, nil
+			}
+
+			klog.V(5).Infof("Created defaultResourceQuota %v which allows unlimited critical pods to be created in kube-system namespace", DefaultSystemQuotaName)
+		}
+
+		if err != nil {
+			// Unable to get the default resource quota for reasons other than "not found".
+			klog.Warningf("Unable to get system default resourceQuota %v: %v. Retrying..", DefaultSystemQuotaName, err)
+			return false, nil
+		}
+		klog.Info("Required default resource quota is created or already exist")
+		return true, nil
+	})
+}
+
+// systemDefaultQuota returns a default system quota. This default quota allows unlimited number of critical pods
+// to be created in `kube-system` namespace. This is needed for backwards compatibility with current system
+// as we allow unlimited critical pods to be created in `kube-system` namespace. The DefaultSystemQuotaName gets
+// automatically created when we start kube-apiserver. The cluster-admin is allowed to create/delete or update
+// this quota. In order to create critical pods in other namespaces, cluster-admin can create quotas
+// in those namespaces.
+func systemDefaultQuota() *v1.ResourceQuota {
+	defaultQuota := &v1.ResourceQuota{
+		ObjectMeta: metav1.ObjectMeta{Name: DefaultSystemQuotaName, Namespace: metav1.NamespaceSystem},
+		Spec: v1.ResourceQuotaSpec{
+			ScopeSelector: &v1.ScopeSelector{
+				MatchExpressions: []v1.ScopedResourceSelectorRequirement{
+					{
+						ScopeName: v1.ResourceQuotaScopePriorityClass,
+						Operator:  v1.ScopeSelectorOpIn,
+						Values:    []string{"system-cluster-critical", "system-node-critical"},
+					},
+				},
+			},
+		},
+	}
+	return defaultQuota
 }
