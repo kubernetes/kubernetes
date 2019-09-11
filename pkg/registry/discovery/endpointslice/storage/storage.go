57757b65c738f15d83354bb7854963a4ccc8a0f3
Rob Scott <robertjscott@google.com>
2019-09-11 16:16:06 -0700

    Fixing EndpointSlice kubectl output

--- pkg/registry/discovery/endpointslice/storage/storage.go
+++ pkg/registry/discovery/endpointslice/storage/storage.go
@@ -21,6 +21,9 @@ import (
 	"k8s.io/apiserver/pkg/registry/generic"
 	genericregistry "k8s.io/apiserver/pkg/registry/generic/registry"
 	"k8s.io/kubernetes/pkg/apis/discovery"
+	"k8s.io/kubernetes/pkg/printers"
+	printersinternal "k8s.io/kubernetes/pkg/printers/internalversion"
+	printerstorage "k8s.io/kubernetes/pkg/printers/storage"
 	"k8s.io/kubernetes/pkg/registry/discovery/endpointslice"
 )
 
@@ -32,16 +35,15 @@ type REST struct {
 // NewREST returns a RESTStorage object that will work against endpoint slices.
 func NewREST(optsGetter generic.RESTOptionsGetter) (*REST, error) {
 	store := &genericregistry.Store{
-		NewFunc:     func() runtime.Object { return &discovery.EndpointSlice{} },
-		NewListFunc: func() runtime.Object { return &discovery.EndpointSliceList{} },
-		ObjectNameFunc: func(obj runtime.Object) (string, error) {
-			return obj.(*discovery.EndpointSlice).Name, nil
-		},
+		NewFunc:                  func() runtime.Object { return &discovery.EndpointSlice{} },
+		NewListFunc:              func() runtime.Object { return &discovery.EndpointSliceList{} },
 		DefaultQualifiedResource: discovery.Resource("endpointslices"),
 
 		CreateStrategy: endpointslice.Strategy,
 		UpdateStrategy: endpointslice.Strategy,
 		DeleteStrategy: endpointslice.Strategy,
+
+		TableConvertor: printerstorage.TableConvertor{TableGenerator: printers.NewTableGenerator().With(printersinternal.AddHandlers)},
 	}
 	options := &generic.StoreOptions{RESTOptions: optsGetter}
 	if err := store.CompleteWithOptions(options); err != nil {
