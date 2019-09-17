10ac64f62a42aaaee8aedd3b379e711a2f6802b0
Wei Huang <wei.huang1@ibm.com>
2019-09-17 12:39:11 -0700

    manual API changes
    - add Policy API to pkg/scheduler/apis/config and staging/src/k8s.io/kube-scheduler/config/v1
    - dual-register Policy as apiGroup "v1" and "kubescheduler.config.k8s.io"

--- staging/src/k8s.io/kube-scheduler/config/v1/register.go
+++ staging/src/k8s.io/kube-scheduler/config/v1/register.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 The Kubernetes Authors.
+Copyright 2019 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package config
+package v1
 
 import (
 	"k8s.io/apimachinery/pkg/runtime"
@@ -25,7 +25,7 @@ import (
 const GroupName = "kubescheduler.config.k8s.io"
 
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: runtime.APIVersionInternal}
+var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1"}
 
 var (
 	// SchemeBuilder is the scheme builder with scheme init functions to run for this API package
@@ -37,8 +37,9 @@ var (
 // addKnownTypes registers known types to the given scheme
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
-		&KubeSchedulerConfiguration{},
 		&Policy{},
 	)
+	// also register into the v1 group for API backward compatibility
+	scheme.AddKnownTypeWithName(schema.GroupVersionKind{Group: "", Version: "v1", Kind: "Policy"}, &Policy{})
 	return nil
 }
