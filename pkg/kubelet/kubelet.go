ddcbf69ef92ec6688e435a0e42523bfce67b2136
kitianFresh <1549722424@qq.com>
2019-09-17 11:04:12 +0800

    avoid circuit break for pod start metrics when there is no podStatus in status manager

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -1512,7 +1512,7 @@ func (kl *Kubelet) syncPod(o syncPodOptions) error {
 
 	// Record the time it takes for the pod to become running.
 	existingStatus, ok := kl.statusManager.GetPodStatus(pod.UID)
-	if !ok || existingStatus.Phase == v1.PodPending && apiPodStatus.Phase == v1.PodRunning &&
+	if (!ok || existingStatus.Phase == v1.PodPending) && apiPodStatus.Phase == v1.PodRunning &&
 		!firstSeenTime.IsZero() {
 		metrics.PodStartDuration.Observe(metrics.SinceInSeconds(firstSeenTime))
 		metrics.DeprecatedPodStartLatency.Observe(metrics.SinceInMicroseconds(firstSeenTime))
