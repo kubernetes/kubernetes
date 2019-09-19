e49d5b53d514bb467762771fefad54f143ff2079
mahendrakariya <mahendrakariya@gmail.com>
2019-09-20 01:39:12 +0530

    Fix package name in comments
    Ref #68026

--- pkg/apis/core/doc.go
+++ pkg/apis/core/doc.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // +k8s:deepcopy-gen=package
 
-// Package api contains the latest (or "internal") version of the
+// Package core contains the latest (or "internal") version of the
 // Kubernetes API objects. This is the API objects as represented in memory.
 // The contract presented to clients is located in the versioned packages,
 // which are sub-directories. The first one is "v1". Those packages
