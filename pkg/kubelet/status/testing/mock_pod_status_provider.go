5a2e6bd0001a96c4e7137c3b65155d600d6e55e2
Uzuku <danielwsam@gmail.com>
2019-09-21 23:39:36 +0800

    Fix golint failures of pkg/kubelet/status/...

--- pkg/kubelet/status/testing/mock_pod_status_provider.go
+++ pkg/kubelet/status/testing/mock_pod_status_provider.go
@@ -23,10 +23,12 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
+// MockStatusProvider mocks a PodStatusProvider.
 type MockStatusProvider struct {
 	mock.Mock
 }
 
+// GetPodStatus implements PodStatusProvider.
 func (m *MockStatusProvider) GetPodStatus(uid types.UID) (v1.PodStatus, bool) {
 	args := m.Called(uid)
 	return args.Get(0).(v1.PodStatus), args.Bool(1)
