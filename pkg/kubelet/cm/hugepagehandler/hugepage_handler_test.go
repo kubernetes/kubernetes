4acfbf796f6b18e8f510bd3dc72c65e08036ab16
sewon.oh <sewon.oh@samsung.com>
2019-10-01 16:58:14 +0900

    Implement hugepage handler unit test
    Signed-off-by: sewon.oh <sewon.oh@samsung.com>

---
+++ pkg/kubelet/cm/hugepagehandler/hugepage_handler_test.go
@@ -0,0 +1,95 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hugepagehandler
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	api "k8s.io/kubernetes/pkg/apis/core"
+
+	runtimeapi "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+)
+
+type mockRuntimeService struct {
+	err error
+}
+
+func (rt mockRuntimeService) UpdateContainerResources(id string, resources *runtimeapi.LinuxContainerResources) error {
+	result := &runtimeapi.HugepageLimit{
+		PageSize: "1GB",
+		Limit:    1073741824,
+	}
+	if !reflect.DeepEqual(result, resources.HugepageLimits[0]) {
+		return fmt.Errorf("Fail to parse hugepage limit")
+	}
+	return rt.err
+}
+
+func makePod(hugepageSize string, hugepageAmount string) *v1.Pod {
+	return &v1.Pod{
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Resources: v1.ResourceRequirements{
+						Limits: v1.ResourceList{
+							v1.ResourceName(api.ResourceHugePagesPrefix + hugepageSize): resource.MustParse(hugepageAmount),
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func TestHugepageHandlerAdd(t *testing.T) {
+	testCases := []struct {
+		description string
+		updateErr   error
+		expErr      error
+	}{
+		{
+			description: "hugepage handler add - no error",
+			updateErr:   nil,
+			expErr:      nil,
+		},
+		{
+			description: "hugepage handler add - container update error",
+			updateErr:   fmt.Errorf("fake update error"),
+			expErr:      fmt.Errorf("fake update error"),
+		},
+	}
+
+	for _, testCase := range testCases {
+		hdr := &handler{
+			containerRuntime: mockRuntimeService{
+				err: testCase.updateErr,
+			},
+		}
+
+		pod := makePod("1Gi", "1Gi")
+		container := &pod.Spec.Containers[0]
+		err := hdr.AddContainer(pod, container, "fakeID")
+		if !reflect.DeepEqual(err, testCase.expErr) {
+			t.Errorf("Hugepage Handler AddContainer() error (%v). expected error: %v but got: %v",
+				testCase.description, testCase.expErr, err)
+		}
+	}
+}
