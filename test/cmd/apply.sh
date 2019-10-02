e4368eb67e363d3d03f81214a8929268d2fe88ff
Julian V. Modesto <julianvmodesto@gmail.com>
2019-10-02 11:13:40 -0400

    Implement server-side apply upgrade & downgrade.
    - Allow client-side to server-side apply upgrade.
    
      Ensure that a user can change management of an object from client-side apply to
      server-side apply without conflicts.
    
    - Allow server-side apply to client-side downgrade.
    
      For an object managed with client-side apply, a user may upgrade to
      managing the object with server-side apply, then decide to downgrade.
    
      We can support this downgrade by keeping the last-applied-configuration
      annotation for client-side apply updated with server-side apply.

--- test/cmd/apply.sh
+++ test/cmd/apply.sh
@@ -393,6 +393,34 @@ run_kubectl_server_side_apply_tests() {
   # clean-up
   kubectl delete -f hack/testdata/pod.yaml "${kube_flags[@]:?}"
 
+  ## kubectl apply upgrade
+  # Pre-Condition: no POD exists
+  kube::test::get_object_assert pods "{{range.items}}{{${id_field:?}}}:{{end}}" ''
+
+  kube::log::status "Testing upgrade kubectl client-side apply to server-side apply"
+  # run client-side apply
+  kubectl apply -f hack/testdata/pod.yaml "${kube_flags[@]:?}"
+  # test upgrade does not work with non-standard server-side apply field manager
+  ! kubectl apply --server-side --field-manager="not-kubectl" -f hack/testdata/pod-apply.yaml "${kube_flags[@]:?}" || exit 1
+  # test upgrade from client-side apply to server-side apply
+  kubectl apply --server-side -f hack/testdata/pod-apply.yaml "${kube_flags[@]:?}"
+  # Post-Condition: pod "test-pod" has configuration annotation
+  grep -q kubectl.kubernetes.io/last-applied-configuration <<< "$(kubectl get pods test-pod -o yaml "${kube_flags[@]:?}")"
+  output_message=$(kubectl apply view-last-applied pod/test-pod -o json 2>&1 "${kube_flags[@]:?}")
+  kube::test::if_has_string "${output_message}" '"name": "test-pod-applied"'
+
+  kube::log::status "Testing downgrade kubectl server-side apply to client-side apply"
+  # test downgrade from server-side apply to client-side apply
+  kubectl apply --server-side -f hack/testdata/pod.yaml "${kube_flags[@]:?}"
+  # Post-Condition: pod "test-pod" has configuration annotation
+  grep -q kubectl.kubernetes.io/last-applied-configuration <<< "$(kubectl get pods test-pod -o yaml "${kube_flags[@]:?}")"
+  output_message=$(kubectl apply view-last-applied pod/test-pod -o json 2>&1 "${kube_flags[@]:?}")
+  kube::test::if_has_string "${output_message}" '"name": "test-pod-label"'
+  kubectl apply -f hack/testdata/pod-apply.yaml "${kube_flags[@]:?}"
+
+  # clean-up
+  kubectl delete -f hack/testdata/pod.yaml "${kube_flags[@]:?}"
+
   ## kubectl apply dry-run on CR
   # Create CRD
   kubectl "${kube_flags_with_token[@]}" create -f - << __EOF__
