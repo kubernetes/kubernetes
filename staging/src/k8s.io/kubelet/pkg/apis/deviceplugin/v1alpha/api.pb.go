f8d3c45d1a9fb25ab3df697a0a59c05c7d60d586
chenyaqi01 <chenyaqi01@baidu.com>
2019-10-11 10:26:19 +0800

    fix inconsistent comment in device plugin api

--- staging/src/k8s.io/kubelet/pkg/apis/deviceplugin/v1alpha/api.pb.go
+++ staging/src/k8s.io/kubelet/pkg/apis/deviceplugin/v1alpha/api.pb.go
@@ -199,7 +199,7 @@ func (m *ListAndWatchResponse) GetDevices() []*Device {
 // E.g:
 // struct Device {
 //    ID: "GPU-fef8089b-4820-abfc-e83e-94318197576e",
-//    State: "Healthy",
+//    Health: "Healthy",
 //}
 type Device struct {
 	// A unique ID assigned by the device plugin used
