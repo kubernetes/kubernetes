60bf035727910a43cb41db692b33391ed62db3bc
Pradeep Hiremande <16595434+phiremande@users.noreply.github.com>
2019-10-18 10:08:54 +0530

    Issue 83458:Changed the return check for GetNodeInfo

--- pkg/scheduler/algorithm/predicates/predicates.go
+++ pkg/scheduler/algorithm/predicates/predicates.go
@@ -28,7 +28,6 @@ import (
 	v1 "k8s.io/api/core/v1"
 	storagev1 "k8s.io/api/storage/v1"
 	storagev1beta1 "k8s.io/api/storage/v1beta1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
