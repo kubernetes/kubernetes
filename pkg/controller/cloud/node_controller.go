897b8d74765f97ca9392e859eaaf59be4b4cde73
Andrew Sy Kim <kiman@vmware.com>
2019-10-28 14:36:29 -0400

    improve error handling in cloud node controller
    Signed-off-by: Andrew Sy Kim <kiman@vmware.com>

--- pkg/controller/cloud/node_controller.go
+++ pkg/controller/cloud/node_controller.go
@@ -147,7 +147,7 @@ func (cnc *CloudNodeController) updateNodeAddress(ctx context.Context, node *v1.
 
 	nodeAddresses, err := getNodeAddressesByProviderIDOrName(ctx, instances, node)
 	if err != nil {
-		klog.Errorf("%v", err)
+		klog.Errorf("Error getting node addresses for node %q: %v", node.Name, err)
 		return
 	}
 
@@ -177,7 +177,7 @@ func (cnc *CloudNodeController) updateNodeAddress(ctx context.Context, node *v1.
 	// it can be found in the cloud as well (consistent with the behaviour in kubelet)
 	if nodeIP, ok := ensureNodeProvidedIPExists(node, nodeAddresses); ok {
 		if nodeIP == nil {
-			klog.Errorf("Specified Node IP not found in cloudprovider")
+			klog.Errorf("Specified Node IP not found in cloudprovider for node %q", node.Name)
 			return
 		}
 	}
@@ -373,7 +373,7 @@ func getNodeAddressesByProviderIDOrName(ctx context.Context, instances cloudprov
 		providerIDErr := err
 		nodeAddresses, err = instances.NodeAddresses(ctx, types.NodeName(node.Name))
 		if err != nil {
-			return nil, fmt.Errorf("NodeAddress: Error fetching by providerID: %v Error fetching by NodeName: %v", providerIDErr, err)
+			return nil, fmt.Errorf("error fetching node by provider ID: %v, and error by node name: %v", providerIDErr, err)
 		}
 	}
 	return nodeAddresses, nil
