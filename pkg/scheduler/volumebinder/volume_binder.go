7cba40fb09df70cb50acba3e2de67e7f12461ca8
Fabio Bertinatto <fbertina@redhat.com>
2019-11-01 12:06:40 +0100

    Use CSI translation lib in VolumeBinderPredicate

--- pkg/scheduler/volumebinder/volume_binder.go
+++ pkg/scheduler/volumebinder/volume_binder.go
@@ -19,9 +19,10 @@ package volumebinder
 import (
 	"time"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	coreinformers "k8s.io/client-go/informers/core/v1"
 	storageinformers "k8s.io/client-go/informers/storage/v1"
+	storagev1beta1informers "k8s.io/client-go/informers/storage/v1beta1"
 	clientset "k8s.io/client-go/kubernetes"
 	volumescheduling "k8s.io/kubernetes/pkg/controller/volume/scheduling"
 )
@@ -35,13 +36,14 @@ type VolumeBinder struct {
 func NewVolumeBinder(
 	client clientset.Interface,
 	nodeInformer coreinformers.NodeInformer,
+	csiNodeInformer storagev1beta1informers.CSINodeInformer,
 	pvcInformer coreinformers.PersistentVolumeClaimInformer,
 	pvInformer coreinformers.PersistentVolumeInformer,
 	storageClassInformer storageinformers.StorageClassInformer,
 	bindTimeout time.Duration) *VolumeBinder {
 
 	return &VolumeBinder{
-		Binder: volumescheduling.NewVolumeBinder(client, nodeInformer, pvcInformer, pvInformer, storageClassInformer, bindTimeout),
+		Binder: volumescheduling.NewVolumeBinder(client, nodeInformer, csiNodeInformer, pvcInformer, pvInformer, storageClassInformer, bindTimeout),
 	}
 }
 
