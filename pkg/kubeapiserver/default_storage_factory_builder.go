03dfa1a6419c4f3c969ecde442dd4a0e49296c12
Igor Zibarev <zibarev.i@gmail.com>
2019-11-05 22:25:32 +0300

    Fix golint issues in pkg/kubeapiserver

--- pkg/kubeapiserver/default_storage_factory_builder.go
+++ pkg/kubeapiserver/default_storage_factory_builder.go
@@ -51,6 +51,7 @@ var SpecialDefaultResourcePrefixes = map[schema.GroupResource]string{
 	{Group: "policy", Resource: "podsecuritypolicies"}:     "podsecuritypolicy",
 }
 
+// NewStorageFactoryConfig returns a new StorageFactoryConfig set up with necessary resource overrides.
 func NewStorageFactoryConfig() *StorageFactoryConfig {
 
 	resources := []schema.GroupVersionResource{
@@ -73,9 +74,10 @@ func NewStorageFactoryConfig() *StorageFactoryConfig {
 	}
 }
 
+// StorageFactoryConfig is a configuration for creating storage factory.
 type StorageFactoryConfig struct {
 	StorageConfig                    storagebackend.Config
-	ApiResourceConfig                *serverstorage.ResourceConfig
+	APIResourceConfig                *serverstorage.ResourceConfig
 	DefaultResourceEncoding          *serverstorage.DefaultResourceEncodingConfig
 	DefaultStorageMediaType          string
 	Serializer                       runtime.StorageSerializer
@@ -84,6 +86,7 @@ type StorageFactoryConfig struct {
 	EncryptionProviderConfigFilepath string
 }
 
+// Complete completes the StorageFactoryConfig with provided etcdOptions returning completedStorageFactoryConfig.
 func (c *StorageFactoryConfig) Complete(etcdOptions *serveroptions.EtcdOptions) (*completedStorageFactoryConfig, error) {
 	c.StorageConfig = etcdOptions.StorageConfig
 	c.DefaultStorageMediaType = etcdOptions.DefaultStorageMediaType
@@ -92,10 +95,15 @@ func (c *StorageFactoryConfig) Complete(etcdOptions *serveroptions.EtcdOptions) 
 	return &completedStorageFactoryConfig{c}, nil
 }
 
+// completedStorageFactoryConfig is a wrapper around StorageFactoryConfig completed with etcd options.
+//
+// Note: this struct is intentionally unexported so that it can only be constructed via a StorageFactoryConfig.Complete
+// call. The implied consequence is that this does not comply with golint.
 type completedStorageFactoryConfig struct {
 	*StorageFactoryConfig
 }
 
+// New returns a new storage factory created from the completed storage factory configuration.
 func (c *completedStorageFactoryConfig) New() (*serverstorage.DefaultStorageFactory, error) {
 	resourceEncodingConfig := resourceconfig.MergeResourceEncodingConfigs(c.DefaultResourceEncoding, c.ResourceEncodingOverrides)
 	storageFactory := serverstorage.NewDefaultStorageFactory(
@@ -103,7 +111,7 @@ func (c *completedStorageFactoryConfig) New() (*serverstorage.DefaultStorageFact
 		c.DefaultStorageMediaType,
 		c.Serializer,
 		resourceEncodingConfig,
-		c.ApiResourceConfig,
+		c.APIResourceConfig,
 		SpecialDefaultResourcePrefixes)
 
 	storageFactory.AddCohabitatingResources(networking.Resource("networkpolicies"), extensions.Resource("networkpolicies"))
