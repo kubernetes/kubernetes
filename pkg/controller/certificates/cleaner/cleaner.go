cd0eb2649e0f5ae686b3a086a49beb5c87296121
Haiwei Li <lihaiwei@tencent.com>
2019-11-07 09:43:14 +0800

    Using time.Since() instead

--- pkg/controller/certificates/cleaner/cleaner.go
+++ pkg/controller/certificates/cleaner/cleaner.go
@@ -172,7 +172,7 @@ func isIssuedPastDeadline(csr *capi.CertificateSigningRequest) bool {
 
 // isOlderThan checks that t is a non-zero time after time.Now() + d.
 func isOlderThan(t metav1.Time, d time.Duration) bool {
-	return !t.IsZero() && time.Until(t.Time) < -1*d
+	return (!t.IsZero()) && (time.Since(t.Time) > d)
 }
 
 // isIssued checks if the CSR has `Issued` status. There is no explicit
