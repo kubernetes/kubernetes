31c7365a65d82d8d1a7d3695a2bca86541301997
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2019-11-08 09:40:22 +0100

    apiextensions: clarify what atomic means

--- staging/src/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/types_jsonschema.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/types_jsonschema.go
@@ -103,7 +103,7 @@ type JSONSchemaProps struct {
 	//      may be used on any type of list (struct, scalar, ...).
 	// 2) `set`:
 	//      Sets are lists that must not have multiple items with the same value. Each
-	//      value must be a scalar (or another atomic type).
+	//      value must be a scalar or an array with x-kubernetes-list-type `atomic`.
 	// 3) `map`:
 	//      These lists are like maps in that their elements have a non-index key
 	//      used to identify them. Order is preserved upon merge. The map tag
