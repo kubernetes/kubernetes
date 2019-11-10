db0f616974503e8831dbe8502beefc79beefcd00
Ted Yu <yute@vmware.com>
2019-11-09 16:25:15 -0800

    Handle error return from allocatePodResources

--- pkg/kubelet/cm/devicemanager/manager_test.go
+++ pkg/kubelet/cm/devicemanager/manager_test.go
@@ -745,7 +745,9 @@ func TestPodContainerDeviceAllocation(t *testing.T) {
 				testCase.description, testCase.expErr, err)
 		}
 		runContainerOpts, err := testManager.GetDeviceRunContainerOptions(pod, &pod.Spec.Containers[0])
-		as.Nil(err)
+		if testCase.expErr == nil {
+			as.Nil(err)
+		}
 		if testCase.expectedContainerOptsLen == nil {
 			as.Nil(runContainerOpts)
 		} else {
