74aa59b842ce071192d798587de3b5f7bb3b44c3
Anish Ramasekar <anish.ramasekar@gmail.com>
2019-11-11 15:41:59 -0800

    remove disk locks per vm
    maintain map with nodename and lock
    
    move lock map to utils
    
    add unit tests

--- pkg/cloudprovider/providers/azure/azure.go
+++ pkg/cloudprovider/providers/azure/azure.go
@@ -522,6 +522,7 @@ func initDiskControllers(az *Cloud) error {
 		resourceGroup:         az.ResourceGroup,
 		subscriptionID:        az.SubscriptionID,
 		cloud:                 az,
+		vmLockMap:             newLockMap(),
 	}
 
 	az.BlobDiskController = &BlobDiskController{common: common}
