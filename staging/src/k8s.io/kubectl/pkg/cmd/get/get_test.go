571d1bb55c20c79088786973185e1924cf98b01c
Dong Gang <dong.gang@daocloud.io>
2019-11-13 14:09:46 +0800

    test:Omit namespace when no resource found

--- staging/src/k8s.io/kubectl/pkg/cmd/get/get_test.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/get/get_test.go
@@ -558,7 +558,7 @@ func TestNoBlankLinesForGetAll(t *testing.T) {
 	if e, a := expected, buf.String(); e != a {
 		t.Errorf("expected\n%v\ngot\n%v", e, a)
 	}
-	expectedErr := `No resources found in test namespace.
+	expectedErr := `No resources found
 `
 	if e, a := expectedErr, errbuf.String(); e != a {
 		t.Errorf("expectedErr\n%v\ngot\n%v", e, a)
@@ -648,7 +648,7 @@ func TestGetEmptyTable(t *testing.T) {
 	if e, a := expected, buf.String(); e != a {
 		t.Errorf("expected\n%v\ngot\n%v", e, a)
 	}
-	expectedErr := `No resources found in test namespace.
+	expectedErr := `No resources found
 `
 	if e, a := expectedErr, errbuf.String(); e != a {
 		t.Errorf("expectedErr\n%v\ngot\n%v", e, a)
