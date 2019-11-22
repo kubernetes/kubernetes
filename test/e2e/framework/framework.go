c3c7ea2efd8d3161496394eebf83067aa371325b
Tim Schrodi <tim.schrodi@sap.com>
2019-11-22 12:14:28 +0100

    Dump namespace if the namespace could not be cleaned up

--- test/e2e/framework/framework.go
+++ test/e2e/framework/framework.go
@@ -334,6 +334,11 @@ func (f *Framework) AfterEach() {
 				if err := f.ClientSet.CoreV1().Namespaces().Delete(ns.Name, nil); err != nil {
 					if !apierrors.IsNotFound(err) {
 						nsDeletionErrors[ns.Name] = err
+
+						// Dump namespace if we are unable to delete the namespace and the dump was not already performed.
+						if !ginkgo.CurrentGinkgoTestDescription().Failed && TestContext.DumpLogsOnFailure {
+							DumpAllNamespaceInfo(f.ClientSet, ns.Name)
+						}
 					} else {
 						Logf("Namespace %v was already deleted", ns.Name)
 					}
