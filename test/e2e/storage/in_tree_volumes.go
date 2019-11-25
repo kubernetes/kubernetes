dd76fdfce5554cc15ee7ed999bea1cffb0436d0a
Ernest Wong <chuwon@microsoft.com>
2019-11-25 15:51:30 -0800

    Rename Azure driver to Azure Disk driver

--- test/e2e/storage/in_tree_volumes.go
+++ test/e2e/storage/in_tree_volumes.go
@@ -36,7 +36,7 @@ var testDrivers = []func() testsuites.TestDriver{
 	drivers.InitCinderDriver,
 	drivers.InitGcePdDriver,
 	drivers.InitVSphereDriver,
-	drivers.InitAzureDriver,
+	drivers.InitAzureDiskDriver,
 	drivers.InitAwsDriver,
 	drivers.InitLocalDriverWithVolumeType(utils.LocalVolumeDirectory),
 	drivers.InitLocalDriverWithVolumeType(utils.LocalVolumeDirectoryLink),
