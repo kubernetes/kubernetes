347580c2b47a8b3c515eabc79ec3158f81b55624
Fabricio Voznika <fvoznika@google.com>
2019-12-05 17:29:41 -0800

    Remove GvisorReject admission controller
    GvisorReject is not needed because RuntimeClass will reject
    attempts to use gVisor on non-gVisor nodes. No one is using
    GvisorReject, so it's safe to remove.
    
    Change-Id: Ib9115b2bdb701efe09d4f8a401064320d4dbb69e

--- pkg/kubeapiserver/options/plugins.go
+++ pkg/kubeapiserver/options/plugins.go
@@ -34,7 +34,6 @@ import (
 	"k8s.io/kubernetes/plugin/pkg/admission/exec"
 	"k8s.io/kubernetes/plugin/pkg/admission/extendedresourcetoleration"
 	"k8s.io/kubernetes/plugin/pkg/admission/gc"
-	"k8s.io/kubernetes/plugin/pkg/admission/gvisor_reject"
 	"k8s.io/kubernetes/plugin/pkg/admission/imagepolicy"
 	"k8s.io/kubernetes/plugin/pkg/admission/limitranger"
 	"k8s.io/kubernetes/plugin/pkg/admission/namespace/autoprovision"
@@ -117,7 +116,6 @@ func RegisterAllAdmissionPlugins(plugins *admission.Plugins) {
 	exec.Register(plugins)
 	extendedresourcetoleration.Register(plugins)
 	gc.Register(plugins)
-	gvisor_reject.Register(plugins)
 	imagepolicy.Register(plugins)
 	limitranger.Register(plugins)
 	autoprovision.Register(plugins)
