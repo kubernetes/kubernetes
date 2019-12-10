5d1442eb227ff3f80ffed874aad2c4b61e8ebe0a
hwdef <hwdef97@gmail.com>
2019-12-10 17:49:09 +0800

    cmd/linkcheck: Fix static check failures

--- cmd/linkcheck/links.go
+++ cmd/linkcheck/links.go
@@ -72,7 +72,7 @@ var (
 )
 
 func newWalkFunc(invalidLink *bool, client *http.Client) filepath.WalkFunc {
-	return func(filePath string, info os.FileInfo, err error) error {
+	return func(filePath string, info os.FileInfo, _ error) error {
 		hasSuffix := false
 		for _, suffix := range *fileSuffix {
 			hasSuffix = hasSuffix || strings.HasSuffix(info.Name(), suffix)
