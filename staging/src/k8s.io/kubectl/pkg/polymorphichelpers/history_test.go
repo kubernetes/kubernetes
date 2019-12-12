5b2cc9c2fd69d766e05c2cf476e9a9a344005155
whalecold <zls1129@gmail.com>
2019-12-12 23:14:47 +0800

    Fix(kubectl): the field of history controllerrevision will be covered by daemonset

--- staging/src/k8s.io/kubectl/pkg/polymorphichelpers/history_test.go
+++ staging/src/k8s.io/kubectl/pkg/polymorphichelpers/history_test.go
@@ -17,12 +17,14 @@ limitations under the License.
 package polymorphichelpers
 
 import (
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"reflect"
 	"testing"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/kubernetes/fake"
 )
@@ -104,4 +106,124 @@ func TestViewHistory(t *testing.T) {
 		t.Fatalf("unexpected output  (%v was expected but got %v)", expected, result)
 	}
 
+}
+
+func TestApplyDaemonSetHistory(t *testing.T) {
+	tests := []struct {
+		name     string
+		source   *appsv1.DaemonSet
+		expected *appsv1.DaemonSet
+	}{
+		{
+			"test_less",
+			&appsv1.DaemonSet{
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Annotations: map[string]string{"version": "v3"},
+						},
+						Spec: corev1.PodSpec{
+							InitContainers:   []corev1.Container{{Name: "i0"}},
+							Containers:       []corev1.Container{{Name: "c0"}},
+							Volumes:          []corev1.Volume{{Name: "v0"}},
+							NodeSelector:     map[string]string{"1dsa": "n0"},
+							ImagePullSecrets: []corev1.LocalObjectReference{{Name: "ips0"}},
+							Tolerations:      []corev1.Toleration{{Key: "t0"}},
+							HostAliases:      []corev1.HostAlias{{IP: "h0"}},
+							ReadinessGates:   []corev1.PodReadinessGate{{ConditionType: corev1.PodScheduled}},
+						},
+					},
+				},
+			},
+			&appsv1.DaemonSet{
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{{Name: "c1"}},
+							// keep diversity field, eg: nil or empty slice
+							InitContainers: []corev1.Container{},
+						},
+					},
+				},
+			},
+		},
+		{
+			"test_more",
+			&appsv1.DaemonSet{
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							InitContainers: []corev1.Container{{Name: "i0"}},
+						},
+					},
+				},
+			},
+			&appsv1.DaemonSet{
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Annotations: map[string]string{"version": "v3"},
+						},
+						Spec: corev1.PodSpec{
+							InitContainers: []corev1.Container{{Name: "i1"}},
+							Containers:     []corev1.Container{{Name: "c1"}},
+							Volumes:        []corev1.Volume{{Name: "v1"}},
+						},
+					},
+				},
+			},
+		},
+		{
+			"test_equal",
+			&appsv1.DaemonSet{
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Annotations: map[string]string{"version": "v0"},
+						},
+						Spec: corev1.PodSpec{
+							Containers:     []corev1.Container{{Name: "c1"}},
+							InitContainers: []corev1.Container{{Name: "i0"}},
+							Volumes:        []corev1.Volume{{Name: "v0"}},
+						},
+					},
+				},
+			},
+			&appsv1.DaemonSet{
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Annotations: map[string]string{"version": "v1"},
+						},
+						Spec: corev1.PodSpec{
+							InitContainers: []corev1.Container{{Name: "i1"}},
+							Containers:     []corev1.Container{{Name: "c1"}},
+							Volumes:        []corev1.Volume{{Name: "v1"}},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			patch, err := getDaemonSetPatch(tt.expected)
+			if err != nil {
+				t.Errorf("getDaemonSetPatch failed : %v", err)
+			}
+			cr := &appsv1.ControllerRevision{
+				Data: runtime.RawExtension{
+					Raw: patch,
+				},
+			}
+			tt.source, err = applyDaemonSetHistory(tt.source, cr)
+			if err != nil {
+				t.Errorf("applyDaemonSetHistory failed : %v", err)
+			}
+			if !apiequality.Semantic.DeepEqual(tt.source, tt.expected) {
+				t.Errorf("expected out [%v]  but get [%v]", tt.expected, tt.source)
+			}
+		})
+	}
 }
