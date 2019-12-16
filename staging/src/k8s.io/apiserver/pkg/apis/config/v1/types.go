a16808f353afb6abf402c862d5f859b949d2027a
Monis Khan <i@monis.app>
2019-12-15 23:30:36 -0500

    kms: use negative cachesize value to disable caching
    This change relaxes the KMS config cache size validation to allow
    for negative values.  The KMS code already treats all values <= 0 to
    mean that the cache is disabled (zero is still a validation error).
    
    Signed-off-by: Monis Khan <mok@vmware.com>

--- staging/src/k8s.io/apiserver/pkg/apis/config/v1/types.go
+++ staging/src/k8s.io/apiserver/pkg/apis/config/v1/types.go
@@ -88,12 +88,13 @@ type IdentityConfiguration struct{}
 type KMSConfiguration struct {
 	// name is the name of the KMS plugin to be used.
 	Name string `json:"name"`
-	// cacheSize is the maximum number of secrets which are cached in memory. The default value is 1000.
+	// cachesize is the maximum number of secrets which are cached in memory. The default value is 1000.
+	// Set to a negative value to disable caching.
 	// +optional
 	CacheSize *int32 `json:"cachesize,omitempty"`
 	// endpoint is the gRPC server listening address, for example "unix:///var/run/kms-provider.sock".
 	Endpoint string `json:"endpoint"`
-	// Timeout for gRPC calls to kms-plugin (ex. 5s). The default is 3 seconds.
+	// timeout for gRPC calls to kms-plugin (ex. 5s). The default is 3 seconds.
 	// +optional
 	Timeout *metav1.Duration `json:"timeout,omitempty"`
 }
