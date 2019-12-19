fafcc81fa0cbbafd8b6eef73470cf7045c6036bd
David Eads <deads@redhat.com>
2019-12-19 14:21:28 -0500

    UPSTREAM: <drop>: skip PSP test that is inconsistently being skipped
    Origin-commit: 131dbb4770bb3bed0c07d2a6ca0cbe4cba2556bb

--- test/e2e/auth/pod_security_policy.go
+++ test/e2e/auth/pod_security_policy.go
@@ -52,6 +52,7 @@ var _ = SIGDescribe("PodSecurityPolicy [Feature:PodSecurityPolicy]", func() {
 	var c clientset.Interface
 	var ns string // Test namespace, for convenience
 	ginkgo.BeforeEach(func() {
+		e2eskipper.Skipf("This test should always be skipped for us, but occasionally PSP is on!")
 		if !framework.IsPodSecurityPolicyEnabled(f.ClientSet) {
 			framework.Failf("PodSecurityPolicy not enabled")
 			return
