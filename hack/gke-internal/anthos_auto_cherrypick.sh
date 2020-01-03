992e185c363bce8726cf26a4767ed705f38850fa
Lantao Liu <lantaol@google.com>
2020-01-02 23:58:46 -0800

    Support automatic cherrypick based on anthos version.
    BUG=b/145773620
    
    Example for anthos version:
    ```
    $ hack/gke-internal/anthos_auto_cherrypick.sh -d f61f2f22ab1e2fc038eb43b37753505fb1f162e7 1.1
    Dry run mode, no actual change pushed
    Target branches generated from the anthos repo:
    	pre-release-1.1: release-1.13.7-gke
    	pre-release-1.2: release-1.14.7-gke
    	master: release-1.14.7-gke
    Branches to cherrypick:
    	release-1.17
    	release-1.16
    	release-1.16.4-gke
    	release-1.16.3-gke
    	release-1.16.2-gke
    	release-1.16.0-gke
    	release-1.15
    	release-1.15.7-gke
    	release-1.15.6-gke
    	release-1.15.5-gke
    	release-1.15.4-gke
    	release-1.15.3-gke
    	release-1.15.2-gke
    	release-1.15.1-gke
    	release-1.15.0-gke
    	release-1.14
    	release-1.14.10-gke
    	release-1.14.9-gke
    	release-1.14.8-gke
    	release-1.14.7-gke
    	release-1.13
    	release-1.13.12-gke
    	release-1.13.11-gke
    	release-1.13.10-gke
    	release-1.13.9-gke
    	release-1.13.8-gke
    	release-1.13.7-gke
    ```
    
    Example for kubernetes version:
    ```
    $ hack/gke-internal/anthos_auto_cherrypick.sh -dk f61f2f22ab1e2fc038eb43b37753505fb1f162e7 1.15.4 1.14.4
    Dry run mode, no actual change pushed
    Use Kubernetes version
    Generated target branches:
    	release-1.15.4-gke
    	release-1.14.4-gke
    Branches to cherrypick:
    	release-1.17
    	release-1.16
    	release-1.16.4-gke
    	release-1.16.3-gke
    	release-1.16.2-gke
    	release-1.16.0-gke
    	release-1.15
    	release-1.15.7-gke
    	release-1.15.6-gke
    	release-1.15.5-gke
    	release-1.15.4-gke
    	release-1.14
    	release-1.14.10-gke
    	release-1.14.9-gke
    	release-1.14.8-gke
    	release-1.14.7-gke
    	release-1.14.6-gke
    	release-1.14.5-gke
    	release-1.14.4-gke
    ```
    
    Change-Id: Icf83445c2cd31f4d6d21c1d9e48aa2885d89173b

--- hack/gke-internal/anthos_auto_cherrypick.sh
+++ hack/gke-internal/anthos_auto_cherrypick.sh
@@ -18,64 +18,45 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-usage="The script automatically cherrypicks a commit into target branches and all branches newer than them.
+usage="The script automatically cherrypicks a kubernetes patch for an anthos version.
 
-Usage: $(basename $0) [-hd] COMMIT_NUMBER TARGET_BRANCH1 [TARGET_BRANCH2 ...]
+Usage: $(basename $0) [-dhkv] COMMIT_NUMBER ANTHOS_MAJOR_MINOR_VERSION|K8S_MAJOR_MINOR_PATCH_VERSION
 
 Options:
+  -d  Dry run without actual change pushed.
   -h  Print help message.
-  -d  Dry run without actual change pushed."
+  -k  Use Kubernetes major minor patch version instead.
+  -v  Print verbose output."
 
 
-while getopts 'hd' o; do
+while getopts 'dhkv' o; do
   case "$o" in
+    d) echo "Dry run mode, no actual change pushed"
+      dryrun="true"
+      ;;
     h) echo "$usage"
       exit
       ;;
-    d) echo "Dry run mode, no actual change pushed"
-      dryrun="true"
-      shift
+    k) echo "Use Kubernetes version"
+      use_k8s_version="true"
+      ;;
+    v) echo "Verbose output mode"
+      set -o xtrace
+      verbose="true"
       ;;
     \?) echo "$usage" >&2
       exit 1
       ;;
   esac
 done
-
-if [[ $# -lt 2 ]]; then
-  echo "Missing arguments!" >&2
-  echo "$usage" >&2
-  exit
-fi
+shift $(($OPTIND-1))
 
 commit="$1"
-shift
-target_branches=($*)
-gke_branch_pattern="release-[0-9]+\.[0-9]+\.[0-9]+-gke"
-oss_branch_pattern="release-[0-9]+\.[0-9]+"
-
-# Make sure target branches are all gke patch branches.
-for b in "${target_branches[@]}"; do
-  if [[ ! "$b" =~ ^${gke_branch_pattern}$ ]]; then
-    echo "Version \"$b\" doesn't match pattern \"${gke_branch_pattern}\"" >&2
-    echo "$usage" >&2
-    exit 1
-  fi
-done
-
-oss_branches=($(git branch -r | grep -oP "(?<=origin/)${oss_branch_pattern}$"))
-gke_branches=($(git branch -r | grep -oP "(?<=origin/)${gke_branch_pattern}$"))
-oss_branch_suffix=".9999999"
-# Add a big suffix to oss branches, so that it will be greater than gke branches
-# for sorting.
-for i in "${!oss_branches[@]}"; do
-  oss_branches[i]="${oss_branches[$i]}${oss_branch_suffix}"
-done
-branches=("${oss_branches[@]}" "${gke_branches[@]}")
-
-# Sort both branches and target branches from newer version to older version.
-IFS=$'\n' branches=($(sort -Vr <<<"${branches[*]}")); unset IFS
-IFS=$'\n' target_branches=($(sort -Vr <<<"${target_branches[*]}")); unset IFS
+major_minor_pattern="[0-9]+\.[0-9]+"
+major_minor_patch_pattern="[0-9]+\.[0-9]+\.[0-9]+"
+anthos_branch_pattern="pre-release-${major_minor_pattern}"
+gke_branch_pattern="release-${major_minor_patch_pattern}-gke"
+oss_branch_pattern="release-${major_minor_pattern}"
 
 # compare_versions returns:
 # * 0 if $1 >= $2;
@@ -90,6 +71,90 @@ compare_versions() {
   fi
   return 1
 }
+
+# run_git runs git and generates git output based on verbose mode.
+run_git() {
+  if [[ ${verbose:-"false"} == "true" ]]; then
+    git "$@"
+  else
+    git "$@" &>/dev/null
+  fi
+}
+
+target_branches=()
+if [[ "${use_k8s_version:-false}" == "true" ]]; then
+  if [[ $# -lt 2 ]]; then
+    echo "Missing arguments!" >&2
+    echo "$usage" >&2
+    exit
+  fi
+  shift
+  versions=($*)
+
+  for v in "${versions[@]}"; do
+    if [[ ! "$v" =~ ^$major_minor_patch_pattern$ ]];then
+      echo "Kubernetes version \"$v\" doesn't match pattern \"^$major_minor_patch_pattern$\"" >&2
+      echo "$usage" >&2
+      exit 1
+    fi
+    target_branches+=("release-$v-gke")
+  done
+
+  echo "Generated target branches:"
+  printf "\t%s\n" "${target_branches[@]}"
+else
+  if [[ $# -ne 2 ]]; then
+    echo "Missing arguments!" >&2
+    echo "$usage" >&2
+    exit
+  fi
+
+  version="$2"
+
+  if [[ ! "$version" =~ ^$major_minor_pattern$ ]];then
+    echo "Anthos version \"$version\" doesn't match pattern \"^$major_minor_pattern$\"" >&2
+    echo "$usage" >&2
+    exit 1
+  fi
+
+  anthos_repo=$(mktemp -d)
+  trap "rm -rf $anthos_repo" EXIT
+  run_git clone sso://gke-internal/syllogi/cluster-management "$anthos_repo"
+  pushd "$anthos_repo" &>/dev/null
+  anthos_branches=($(git branch -r | grep -oP "(?<=origin/)${anthos_branch_pattern}$"))
+  anthos_branches+=("master")
+
+  # Collect target Kubernetes branches from corresponding anthos branches.
+  output=()
+  for b in "${anthos_branches[@]}"; do
+    # Skip branches older than the target.
+    if ! compare_versions "$b" "pre-release-$version" && [[ "$b" != "master" ]]; then
+      continue
+    fi
+    run_git checkout "origin/$b"
+    # TODO(random-liu): Update this after upgrade simplification.
+    k8s_version=$(cat on_prem_bundle/bundles/gkeonprem/bundler.yaml | grep -oP "(?<=KubeAPIServerImageTag: \"v)${major_minor_patch_pattern}(?=-gke\.[0-9]+\")")
+    target_branches+=("release-${k8s_version}-gke")
+    output+=("$b: release-${k8s_version}-gke")
+  done
+  popd &>/dev/null
+
+  echo "Target branches generated from the anthos repo:"
+  printf "\t%s\n" "${output[@]}"
+fi
+
+oss_branches=($(git branch -r | grep -oP "(?<=origin/)${oss_branch_pattern}$"))
+gke_branches=($(git branch -r | grep -oP "(?<=origin/)${gke_branch_pattern}$"))
+oss_branch_suffix=".9999999"
+# Add a big suffix to oss branches, so that it will be greater than gke branches
+# for sorting.
+for i in "${!oss_branches[@]}"; do
+  oss_branches[i]="${oss_branches[$i]}${oss_branch_suffix}"
+done
+branches=("${oss_branches[@]}" "${gke_branches[@]}")
+# Sort branches and target branches from newer version to older version.
+IFS=$'\n' branches=($(sort -Vru <<<"${branches[*]}")); unset IFS
+IFS=$'\n' target_branches=($(sort -Vru <<<"${target_branches[*]}")); unset IFS
 
 # Remove all branches lower than the oldest target branch.
 oldest_target_branch="${target_branches[-1]}"
@@ -124,17 +189,21 @@ done
 echo "Branches to cherrypick:"
 printf "\t%s\n" "${branches[@]}"
 
-git fetch origin > /dev/null
+run_git fetch origin
+checkout_num=0
 for b in "${branches[@]}"; do
   remote_branch=origin/$b
   echo "Cherrypicking $commit to branch $remote_branch..."
-  git checkout "$remote_branch" > /dev/null
-  if ! git cherry-pick "$commit" > /dev/null; then
+  run_git checkout "$remote_branch"
+  (( checkout_num+=1 ))
+  if ! run_git cherry-pick "$commit"; then
     while true; do
       echo "Please resolve the conflicts in another window (and remember to 'git add / git cherry-pick --continue')"
-      read -p "Proceed (anything but 'y' aborts the cherry-pick)? [y/n] " reply
-      if ! [[ "${reply}" =~ ^[yY]$ ]]; then
-        git cherry-pick --abort > /dev/null
+      read -p "Proceed? [y/s/n] ('s' skips the cherry-pick, anything but 'y' or 's' aborts the cherry-pick)" reply
+      if [[ "${reply}" =~ ^[sS]$ ]]; then
+        run_git cherry-pick --skip
+      elif ! [[ "${reply}" =~ ^[yY]$ ]]; then
+        run_git cherry-pick --abort
         echo "Aborting."
         exit 1
       fi
@@ -144,13 +213,15 @@ for b in "${branches[@]}"; do
       echo "Please finish the onging cherrypick"
     done
   fi
-  if git diff "$remote_branch" --exit-code > /dev/null; then
-    echo "Nothing to push to branch $remote_branch. Skipping older branches..."
-    break
+  if git diff "$remote_branch" --exit-code &> /dev/null; then
+    echo "Nothing to push to branch $remote_branch."
+    continue
   fi
   if [[ "${dryrun:-"false"}" != "true" ]]; then
     echo "Pushing to branch $remote_branch..."
-    git push origin "HEAD:refs/for/$b"
+    # Amend the commit to make sure it has a change ID.
+    run_git commit --amend --no-edit
+    run_git push origin "HEAD:refs/for/$b"
   else
     echo "Push skipped in dry run mode"
   fi
@@ -159,5 +230,7 @@ for b in "${branches[@]}"; do
   # unnecessary conflict.
   commit=$(git rev-parse HEAD)
 done
+# Checkout back to the initial state
+run_git checkout @{-$checkout_num}
 
 echo "Done!"
