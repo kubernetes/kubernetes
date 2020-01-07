f2a8e2d9c9cadc8a969efa1d4edef833ff701e2f
Mike Spreitzer <mspreitz@us.ibm.com>
2020-01-07 02:23:29 -0500

    finished pass over comments on Controller, and commented sharedIndexInformer

--- staging/src/k8s.io/client-go/tools/cache/controller.go
+++ staging/src/k8s.io/client-go/tools/cache/controller.go
@@ -87,10 +87,22 @@ type controller struct {
 	clock          clock.Clock
 }
 
-// Controller is a low-level controller used in sharedIndexInformer.
+// Controller is a low-level controller that is parameterized by a
+// Config and used in sharedIndexInformer.
 type Controller interface {
+	// Run does two things.  One is to construct and run a Reflector
+	// to pump objects/notifications from the Config's ListerWatcher
+	// to the Config's Queue and possibly invoke the occasional Resync
+	// on that Queue.  The other is to repeatedly Pop from the Queue
+	// and process with the Config's ProcessFunc.  Both of these
+	// continue until `stopCh` is closed.
 	Run(stopCh <-chan struct{})
+
+	// HasSynced delegates to the Queue
 	HasSynced() bool
+
+	// LastSyncResourceVersion delegates to the Reflector when there
+	// is one, otherwise returns the empty string
 	LastSyncResourceVersion() string
 }
 
