dcc9887e5ec1a7daa942511f3e7bb16ac44ccaff
Kewei Ma <46899279+kolorful@users.noreply.github.com>
2020-01-13 10:09:06 -0600

    Fix a comment in job_controller

--- pkg/controller/job/job_controller.go
+++ pkg/controller/job/job_controller.go
@@ -202,7 +202,7 @@ func (jm *Controller) resolveControllerRef(namespace string, controllerRef *meta
 func (jm *Controller) addPod(obj interface{}) {
 	pod := obj.(*v1.Pod)
 	if pod.DeletionTimestamp != nil {
-		// on a restart of the controller controller, it's possible a new pod shows up in a state that
+		// on a restart of the controller, it's possible a new pod shows up in a state that
 		// is already pending deletion. Prevent the pod from being a creation observation.
 		jm.deletePod(pod)
 		return
