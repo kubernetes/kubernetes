f20945aa416bc8dae263d0e31a00f54d677946f3
Caleb Woodbine <caleb@ii.coop>
2020-01-15 00:16:24 +0000

    Fix: formatting

--- test/e2e/common/configmap.go
+++ test/e2e/common/configmap.go
@@ -22,10 +22,10 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/uuid"
 	"k8s.io/kubernetes/test/e2e/framework"
 	imageutils "k8s.io/kubernetes/test/utils/image"
-	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/onsi/ginkgo"
 )
