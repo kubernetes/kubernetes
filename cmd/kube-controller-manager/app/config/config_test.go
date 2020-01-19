2b588018b40109bebb519464e816979ec54c9bfd
obitech <ajknipping@mailbox.org>
2020-01-19 16:00:22 +0100

    Load config success

--- cmd/kube-controller-manager/app/config/config_test.go
+++ cmd/kube-controller-manager/app/config/config_test.go
@@ -22,7 +22,9 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	kubectrlmgrconfigv1alpha1 "k8s.io/kube-controller-manager/config/v1alpha1"
 	kubectrlmgrconfig "k8s.io/kubernetes/pkg/controller/apis/config"
+	kubectrlmgrsscheme "k8s.io/kubernetes/pkg/controller/apis/config/scheme"
 	utilfiles "k8s.io/kubernetes/pkg/kubelet/kubeletconfig/util/files"
 	utilfs "k8s.io/kubernetes/pkg/util/filesystem"
 )
@@ -32,14 +34,14 @@ const (
 	ctrlmgrFile = "controller-manager-file"
 )
 
-func addFile(fs utilfs.Filesystem, path string, file string) error {
-	if err := utilfiles.EnsureDir(fs, filepath.Dir(path)); err != nil {
-		return err
+func TestConfigLoad(t *testing.T) {
+	var simpleErr = func(err error) bool {
+		if err == nil {
+			return false
+		}
+		return true
 	}
-	return utilfiles.ReplaceFile(fs, path, []byte(file))
-}
 
-func TestLoadConfig(t *testing.T) {
 	testCases := []struct {
 		desc   string
 		file   *string
@@ -47,13 +49,63 @@ func TestLoadConfig(t *testing.T) {
 		errFn  func(err error) bool
 	}{
 		{
-			desc: "missing file",
-			errFn: func(err error) bool {
-				if err == nil {
-					return false
-				}
-				return true
-			},
+			desc:  "missing file",
+			errFn: simpleErr,
+		},
+		{
+			desc:  "empty file",
+			file:  newString(``),
+			errFn: simpleErr,
+		},
+		{
+			desc:  "invalid yaml",
+			file:  newString(`*`),
+			errFn: simpleErr,
+		},
+		{
+			desc:  "invalid json",
+			file:  newString(`{*`),
+			errFn: simpleErr,
+		},
+		{
+			desc:  "missing kind",
+			file:  newString(`{"apiVersion":"kubecontrollermanager.config.k8s.io.config.k8s.io/v1alpha1"}`),
+			errFn: simpleErr,
+		},
+		{
+			desc:  "missing version",
+			file:  newString(`{"kind":"KubeControllerManagerConfiguration"}`),
+			errFn: simpleErr,
+		},
+		{
+			desc:  "unregistered kind",
+			file:  newString(`{"kind":"boguskind","apiVersion":"kubecontrollermanager.config.k8s.io.config.k8s.io/v1alpha1"}`),
+			errFn: simpleErr,
+		},
+		{
+			desc:  "unregistered version",
+			file:  newString(`{"kind":"KubeControllerManagerConfiguration","apiVersion":"bogusversion"}`),
+			errFn: simpleErr,
+		},
+		{
+			desc: "default from yaml",
+			file: newString(`kind: KubeControllerManagerConfiguration
+apiVersion: kubecontrollermanager.config.k8s.io/v1alpha1`),
+			expect: newConfig(t),
+		},
+		{
+			desc:   "default from json",
+			file:   newString(`{"kind": "KubeControllerManagerConfiguration", "apiVersion": "kubecontrollermanager.config.k8s.io/v1alpha1"}`),
+			expect: newConfig(t),
+		},
+		{
+			desc: "full from yaml",
+			file: fullConfig(),
+			expect: func() *kubectrlmgrconfig.KubeControllerManagerConfiguration {
+				conf := newConfig(t)
+				conf.Generic.Port = 1234
+				return conf
+			}(),
 		},
 	}
 
@@ -62,22 +114,59 @@ func TestLoadConfig(t *testing.T) {
 			fs := utilfs.NewFakeFs()
 			path := filepath.Join(configDir, ctrlmgrFile)
 			if test.file != nil {
-				if err := utilfiles.EnsureDir(fs, filepath.Dir(path)); err != nil {
-					t.Fatalf("unexpected error: %v", err)
-				}
-				if err := utilfiles.ReplaceFile(fs, path, []byte(*test.file)); err != nil {
-					t.Fatalf("unexpected error: %v", err)
+				if err := addFile(fs, path, *test.file); err != nil {
+					t.Fatalf("unexpected error adding %q: %v", path, err)
 				}
 			}
 			loader, err := NewFsLoader(fs, path)
 			if err != nil {
-				t.Fatalf("unexpected error: %v", err)
+				t.Fatalf("unexpected error creating fsLoader: %v", err)
 			}
 			cmc, err := loader.Load()
-			if test.errFn != nil {
-				assert.True(t, test.errFn(err), test.desc)
+			if err != nil {
+				if test.errFn != nil {
+					assert.True(t, test.errFn(err), test.desc)
+				} else {
+					t.Fatalf("unexepected error: %v", err)
+				}
+
 			}
 			assert.Equal(t, test.expect, cmc, test.desc)
 		})
 	}
+}
+
+func newString(s string) *string {
+	return &s
+}
+
+func newConfig(t *testing.T) *kubectrlmgrconfig.KubeControllerManagerConfiguration {
+	kcmScheme, _, err := kubectrlmgrsscheme.NewSchemeAndCodecs()
+	if err != nil {
+		t.Fatalf("unexpected error creating kcm scheme: %v", err)
+	}
+	external := &kubectrlmgrconfigv1alpha1.KubeControllerManagerConfiguration{}
+	kcmScheme.Default(external)
+	internal := &kubectrlmgrconfig.KubeControllerManagerConfiguration{}
+	err = kcmScheme.Convert(external, internal, nil)
+	if err != nil {
+		t.Fatalf("unexpected error while trying to convert from external to internal kcm config: %v", err)
+	}
+	return internal
+}
+
+func addFile(fs utilfs.Filesystem, path string, file string) error {
+	if err := utilfiles.EnsureDir(fs, filepath.Dir(path)); err != nil {
+		return err
+	}
+	return utilfiles.ReplaceFile(fs, path, []byte(file))
+}
+
+func fullConfig() *string {
+	c := `kind: KubeControllerManagerConfiguration
+apiVersion: kubecontrollermanager.config.k8s.io/v1alpha1
+Generic:
+  Port: 1234
+  Address: 0.0.0.0`
+	return &c
 }
