65483a19abd31e77750ff5d8c24c1cffac0c1dcd
Mike Danese <mikedanese@google.com>
2020-02-03 13:43:35 -0800

    generated: update clients

--- staging/src/k8s.io/client-go/kubernetes/typed/authorization/v1beta1/fake/fake_selfsubjectrulesreview.go
+++ staging/src/k8s.io/client-go/kubernetes/typed/authorization/v1beta1/fake/fake_selfsubjectrulesreview.go
@@ -18,7 +18,27 @@ limitations under the License.
 
 package fake
 
+import (
+	v1beta1 "k8s.io/api/authorization/v1beta1"
+	schema "k8s.io/apimachinery/pkg/runtime/schema"
+	testing "k8s.io/client-go/testing"
+)
+
 // FakeSelfSubjectRulesReviews implements SelfSubjectRulesReviewInterface
 type FakeSelfSubjectRulesReviews struct {
 	Fake *FakeAuthorizationV1beta1
+}
+
+var selfsubjectrulesreviewsResource = schema.GroupVersionResource{Group: "authorization.k8s.io", Version: "v1beta1", Resource: "selfsubjectrulesreviews"}
+
+var selfsubjectrulesreviewsKind = schema.GroupVersionKind{Group: "authorization.k8s.io", Version: "v1beta1", Kind: "SelfSubjectRulesReview"}
+
+// Create takes the representation of a selfSubjectRulesReview and creates it.  Returns the server's representation of the selfSubjectRulesReview, and an error, if there is any.
+func (c *FakeSelfSubjectRulesReviews) Create(selfSubjectRulesReview *v1beta1.SelfSubjectRulesReview) (result *v1beta1.SelfSubjectRulesReview, err error) {
+	obj, err := c.Fake.
+		Invokes(testing.NewRootCreateAction(selfsubjectrulesreviewsResource, selfSubjectRulesReview), &v1beta1.SelfSubjectRulesReview{})
+	if obj == nil {
+		return nil, err
+	}
+	return obj.(*v1beta1.SelfSubjectRulesReview), err
 }
