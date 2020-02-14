c83a860e9d83a3772d29a64bbee65cea468ab814
Wei Huang <wei.huang1@ibm.com>
2020-02-13 16:44:17 -0800

    Fix a bug in e2epod function

--- test/e2e/framework/pod/resource.go
+++ test/e2e/framework/pod/resource.go
@@ -533,8 +533,9 @@ func GetPodsInNamespace(c clientset.Interface, ns string, ignoreLabels map[strin
 		return []*v1.Pod{}, err
 	}
 	ignoreSelector := labels.SelectorFromSet(ignoreLabels)
-	filtered := []*v1.Pod{}
-	for _, p := range pods.Items {
+	var filtered []*v1.Pod
+	for i := range pods.Items {
+		p := pods.Items[i]
 		if len(ignoreLabels) != 0 && ignoreSelector.Matches(labels.Set(p.Labels)) {
 			continue
 		}
