233773befbb42b0d8f51556c5c7f897dc491e844
Pengfei Ni <peni@microsoft.com>
2020-02-16 11:12:50 +0000

    Fix route conflicted operations when updating multiple routes together

--- staging/src/k8s.io/legacy-cloud-providers/azure/azure.go
+++ staging/src/k8s.io/legacy-cloud-providers/azure/azure.go
@@ -229,6 +229,7 @@ type Cloud struct {
 	kubeClient       clientset.Interface
 	eventBroadcaster record.EventBroadcaster
 	eventRecorder    record.EventRecorder
+	routeUpdater     *delayedRouteUpdater
 
 	vmCache  *timedCache
 	lbCache  *timedCache
@@ -508,6 +509,10 @@ func (az *Cloud) InitializeCloudFromConfig(config *Config, fromSecret bool) erro
 	if err := initDiskControllers(az); err != nil {
 		return err
 	}
+
+	// start delayed route updater.
+	az.routeUpdater = newDelayedRouteUpdater(az, routeUpdateInterval)
+	go az.routeUpdater.run()
 
 	return nil
 }
