56f16b3b243b9aae144eca65b76e66925b796a24
mattjmcnaughton <mattjmcnaughton@gmail.com>
2020-02-18 08:35:29 -0500

    Delete TODO in `image_gc_manager`
    I think the TODO here may have actually been unnecessary. There isn't a
    ton of interest around merging
    https://github.com/kubernetes/kubernetes/pull/87425, which contains a
    fix. Delete the TODO so we don't devote time to working on this area in
    the future.

--- pkg/kubelet/images/image_gc_manager.go
+++ pkg/kubelet/images/image_gc_manager.go
@@ -190,7 +190,6 @@ func (im *realImageGCManager) Start() {
 	}, 5*time.Minute, wait.NeverStop)
 
 	// Start a goroutine periodically updates image cache.
-	// TODO(random-liu): Merge this with the previous loop.
 	go wait.Until(func() {
 		images, err := im.runtime.ListImages()
 		if err != nil {
