22f8a698ffcfd8e25110d4f27b6de93aa990534e
Caleb Woodbine <caleb@ii.coop>
2020-02-20 08:27:39 +1300

    Fix: metadata

--- test/e2e/common/podtemplates.go
+++ test/e2e/common/podtemplates.go
@@ -33,9 +33,9 @@ var _ = ginkgo.Describe("[sig-architecture] PodTemplates", func() {
 	f := framework.NewDefaultFramework("podtemplate")
 	/*
 	   Release : v1.18
-	   Testname: ConfigMap lifecycle
-	   Description: Attempt to create a ConfigMap. Patch the created ConfigMap. Fetching the ConfigMap MUST reflect changes.
-	          By fetching all the ConfigMaps via a Label selector it MUST find the ConfigMap by it's static label and updated value. The ConfigMap must be deleted by Collection.
+	   Testname: PodTemplate lifecycle
+	   Description: Attempt to create a PodTemplate. Patch the created PodTemplate. Fetching the PodTemplate MUST reflect changes.
+	          By fetching all the PodTemplates via a Label selector it MUST find the PodTemplate by it's static label and updated value. The PodTemplate must be deleted by Collection.
 	*/
 	framework.ConformanceIt("should run the lifecycle of PodTemplates", func() {
 		testNamespaceName := f.Namespace.Name
