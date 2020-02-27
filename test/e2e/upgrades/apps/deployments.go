2c91f89c7f64580eba7812cb7c9ac85dc4f28ae9
Tomas Nozicka <tnozicka@gmail.com>
2020-02-27 18:06:37 +0100

    Enhance Deployment upgrade tests

--- test/e2e/upgrades/apps/deployments.go
+++ test/e2e/upgrades/apps/deployments.go
@@ -21,6 +21,9 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
+	g "github.com/onsi/ginkgo"
+	o "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -31,7 +34,6 @@ import (
 	e2edeploy "k8s.io/kubernetes/test/e2e/framework/deployment"
 	"k8s.io/kubernetes/test/e2e/upgrades"
 
-	"github.com/onsi/ginkgo"
 	imageutils "k8s.io/kubernetes/test/utils/image"
 )
 
@@ -45,12 +47,60 @@ const (
 // TODO: Test that the deployment stays available during master (and maybe
 // node and cluster upgrades).
 
+type DeploymentSnapshot struct {
+	Deployment  *appsv1.Deployment
+	ReplicaSets map[types.UID]*appsv1.ReplicaSet
+	// Pods        map[types.UID]*corev1.Pod
+}
+
+func snaphotDeployment(ctx context.Context, c clientset.Interface, namespace, name string) (*DeploymentSnapshot, error) {
+	var err error
+	var s DeploymentSnapshot
+
+	s.Deployment, err = c.AppsV1().Deployments(namespace).Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("can't get deployment: %w", err)
+	}
+
+	rsSelector, err := metav1.LabelSelectorAsSelector(s.Deployment.Spec.Selector)
+	if err != nil {
+		return nil, fmt.Errorf("can't create RS selector: %w", err)
+	}
+	rsCandidateList, err := c.AppsV1().ReplicaSets(s.Deployment.Namespace).List(ctx, metav1.ListOptions{
+		LabelSelector: rsSelector.String(),
+	})
+	s.ReplicaSets = make(map[types.UID]*appsv1.ReplicaSet, rsCandidateList.Size())
+	for i := range rsCandidateList.Items {
+		rs := &(rsCandidateList.Items[i])
+		if metav1.IsControlledBy(rs, s.Deployment) {
+			s.ReplicaSets[rs.UID] = rs
+		}
+	}
+
+	// s.Pods = make(map[types.UID][types.UID]*corev1.Pod)
+	// for _, rs := range s.ReplicaSets {
+	// 	podSelector, err := metav1.LabelSelectorAsSelector(rs.Spec.Selector)
+	// 	if err != nil {
+	// 		return nil, fmt.Errorf("can't create Pod selector: %w", err)
+	// 	}
+	// 	podCandidateList, err := c.CoreV1().Pods(rs.Namespace).List(ctx, metav1.ListOptions{
+	// 		LabelSelector: podSelector.String(),
+	// 	})
+	// 	for i := range podCandidateList.Items {
+	// 		pod := &(podCandidateList.Items[i])
+	// 		if metav1.IsControlledBy(pod, rs) {
+	// 			s.Pods[pod.UID] = pod
+	// 		}
+	// 	}
+	// }
+
+	return &s, nil
+}
+
 // DeploymentUpgradeTest tests that a deployment is using the same replica
 // sets before and after a cluster upgrade.
 type DeploymentUpgradeTest struct {
-	oldDeploymentUID types.UID
-	oldRSUID         types.UID
-	newRSUID         types.UID
+	old *DeploymentSnapshot
 }
 
 // Name returns the tracking name of the test.
@@ -58,120 +108,116 @@ func (DeploymentUpgradeTest) Name() string { return "[sig-apps] deployment-upgra
 
 // Setup creates a deployment and makes sure it has a new and an old replicaset running.
 func (t *DeploymentUpgradeTest) Setup(f *framework.Framework) {
+	ctx := context.TODO()
 	c := f.ClientSet
-	nginxImage := imageutils.GetE2EImage(imageutils.Nginx)
 
-	ns := f.Namespace.Name
-	deploymentClient := c.AppsV1().Deployments(ns)
-	rsClient := c.AppsV1().ReplicaSets(ns)
-
-	ginkgo.By(fmt.Sprintf("Creating a deployment %q with 1 replica in namespace %q", deploymentName, ns))
-	d := e2edeploy.NewDeployment(deploymentName, int32(1), map[string]string{"test": "upgrade"}, "nginx", nginxImage, appsv1.RollingUpdateDeploymentStrategyType)
-	deployment, err := deploymentClient.Create(context.TODO(), d, metav1.CreateOptions{})
+	g.By("Creating a deployment with 1 replica")
+	d := e2edeploy.NewDeployment(
+		deploymentName,
+		int32(1),
+		map[string]string{"test": "upgrade"},
+		"nginx",
+		imageutils.GetE2EImage(imageutils.Nginx),
+		appsv1.RollingUpdateDeploymentStrategyType,
+	)
+	deployment, err := c.AppsV1().Deployments(f.Namespace.Name).Create(ctx, d, metav1.CreateOptions{})
 	framework.ExpectNoError(err)
 
-	ginkgo.By(fmt.Sprintf("Waiting deployment %q to complete", deploymentName))
+	g.By("Waiting deployment to complete")
 	framework.ExpectNoError(e2edeploy.WaitForDeploymentComplete(c, deployment))
 
-	ginkgo.By(fmt.Sprintf("Getting replicaset revision 1 of deployment %q", deploymentName))
-	rsSelector, err := metav1.LabelSelectorAsSelector(d.Spec.Selector)
-	framework.ExpectNoError(err)
-	rsList, err := rsClient.List(context.TODO(), metav1.ListOptions{LabelSelector: rsSelector.String()})
-	framework.ExpectNoError(err)
-	rss := rsList.Items
-	if len(rss) != 1 {
-		framework.ExpectNoError(fmt.Errorf("expected one replicaset, got %d", len(rss)))
-	}
-	t.oldRSUID = rss[0].UID
-
-	ginkgo.By(fmt.Sprintf("Waiting for revision of the deployment %q to become 1", deploymentName))
+	g.By("Waiting for revision of the deployment to become 1")
 	framework.ExpectNoError(waitForDeploymentRevision(c, deployment, "1"))
 
 	// Trigger a new rollout so that we have some history.
-	ginkgo.By(fmt.Sprintf("Triggering a new rollout for deployment %q", deploymentName))
-	deployment, err = e2edeploy.UpdateDeploymentWithRetries(c, ns, deploymentName, func(update *appsv1.Deployment) {
+	g.By("Triggering a new rollout")
+	deployment, err = e2edeploy.UpdateDeploymentWithRetries(c, deployment.Namespace, deploymentName, func(update *appsv1.Deployment) {
 		update.Spec.Template.Spec.Containers[0].Name = "updated-name"
 	})
 	framework.ExpectNoError(err)
 
-	ginkgo.By(fmt.Sprintf("Waiting deployment %q to complete", deploymentName))
-	framework.ExpectNoError(e2edeploy.WaitForDeploymentComplete(c, deployment))
-
-	ginkgo.By(fmt.Sprintf("Getting replicasets revision 1 and 2 of deployment %q", deploymentName))
-	rsList, err = rsClient.List(context.TODO(), metav1.ListOptions{LabelSelector: rsSelector.String()})
-	framework.ExpectNoError(err)
-	rss = rsList.Items
-	if len(rss) != 2 {
-		framework.ExpectNoError(fmt.Errorf("expected 2 replicaset, got %d", len(rss)))
-	}
-
-	ginkgo.By(fmt.Sprintf("Checking replicaset of deployment %q that is created before rollout survives the rollout", deploymentName))
-	switch t.oldRSUID {
-	case rss[0].UID:
-		t.newRSUID = rss[1].UID
-	case rss[1].UID:
-		t.newRSUID = rss[0].UID
-	default:
-		framework.ExpectNoError(fmt.Errorf("old replicaset with UID %q does not survive rollout", t.oldRSUID))
-	}
-
-	ginkgo.By(fmt.Sprintf("Waiting for revision of the deployment %q to become 2", deploymentName))
+	g.By("Waiting for revision to become 2")
 	framework.ExpectNoError(waitForDeploymentRevision(c, deployment, "2"))
 
-	t.oldDeploymentUID = deployment.UID
+	g.By("Waiting deployment to complete")
+	framework.ExpectNoError(e2edeploy.WaitForDeploymentComplete(c, deployment))
+
+	t.old, err = snaphotDeployment(ctx, c, deployment.Namespace, deployment.Name)
+	framework.ExpectNoError(err)
+	// sanity check
+	framework.ExpectEqual(t.old.Deployment.UID, deployment.UID)
 }
 
 // Test checks whether the replicasets for a deployment are the same after an upgrade.
 func (t *DeploymentUpgradeTest) Test(f *framework.Framework, done <-chan struct{}, upgrade upgrades.UpgradeType) {
 	// Block until upgrade is done
-	ginkgo.By(fmt.Sprintf("Waiting for upgrade to finish before checking replicasets for deployment %q", deploymentName))
+	g.By(fmt.Sprintf("Waiting for upgrade to finish before checking replicasets for deployment %q", deploymentName))
 	<-done
 
+	ctx := context.TODO()
 	c := f.ClientSet
-	ns := f.Namespace.Name
-	deploymentClient := c.AppsV1().Deployments(ns)
-	rsClient := c.AppsV1().ReplicaSets(ns)
 
-	deployment, err := deploymentClient.Get(context.TODO(), deploymentName, metav1.GetOptions{})
-	framework.ExpectNoError(err)
+	old := t.old
 
-	ginkgo.By(fmt.Sprintf("Checking UID to verify deployment %q survives upgrade", deploymentName))
-	framework.ExpectEqual(deployment.UID, t.oldDeploymentUID)
-
-	ginkgo.By(fmt.Sprintf("Verifying deployment %q does not create new replicasets", deploymentName))
-	rsSelector, err := metav1.LabelSelectorAsSelector(deployment.Spec.Selector)
-	framework.ExpectNoError(err)
-	rsList, err := rsClient.List(context.TODO(), metav1.ListOptions{LabelSelector: rsSelector.String()})
-	framework.ExpectNoError(err)
-	rss := rsList.Items
-	if len(rss) != 2 {
-		framework.ExpectNoError(fmt.Errorf("expected 2 replicaset, got %d", len(rss)))
-	}
-
-	switch t.oldRSUID {
-	case rss[0].UID:
-		framework.ExpectEqual(rss[1].UID, t.newRSUID)
-	case rss[1].UID:
-		framework.ExpectEqual(rss[0].UID, t.newRSUID)
-	default:
-		framework.ExpectNoError(fmt.Errorf("new replicasets are created during upgrade of deployment %q", deploymentName))
-	}
-
-	ginkgo.By(fmt.Sprintf("Verifying revision of the deployment %q is still 2", deploymentName))
-	framework.ExpectEqual(deployment.Annotations[deploymentutil.RevisionAnnotation], "2")
-
-	ginkgo.By(fmt.Sprintf("Waiting for deployment %q to complete adoption", deploymentName))
-	framework.ExpectNoError(e2edeploy.WaitForDeploymentComplete(c, deployment))
-
-	// Verify the upgraded deployment is active by scaling up the deployment by 1
-	ginkgo.By(fmt.Sprintf("Scaling up replicaset of deployment %q by 1", deploymentName))
-	deploymentWithUpdatedReplicas, err := e2edeploy.UpdateDeploymentWithRetries(c, ns, deploymentName, func(deployment *appsv1.Deployment) {
-		*deployment.Spec.Replicas = *deployment.Spec.Replicas + 1
+	g.By("Making a dummy update on the deployment")
+	deployment, err := e2edeploy.UpdateDeploymentWithRetries(c, old.Deployment.Namespace, old.Deployment.Name, func(d *appsv1.Deployment) {
+		if d.Annotations == nil {
+			d.Annotations = map[string]string{}
+		}
+		d.Annotations["dummyAnnotationToTestDefaultingOnUpdate"] = "42"
 	})
 	framework.ExpectNoError(err)
 
-	ginkgo.By(fmt.Sprintf("Waiting for deployment %q to complete after scaling", deploymentName))
-	framework.ExpectNoError(e2edeploy.WaitForDeploymentComplete(c, deploymentWithUpdatedReplicas))
+	// If the nodes were restarted during cluster upgrade we need to wait for the deployment to be fully available
+	g.By("Waiting deployment to be available")
+	framework.ExpectNoError(e2edeploy.WaitForDeploymentComplete(c, old.Deployment))
+
+	g.By("Snaphotting the Deployment and its assets.")
+	new, err := snaphotDeployment(ctx, c, old.Deployment.Namespace, old.Deployment.Name)
+	framework.ExpectNoError(err)
+
+	g.By("Validating the Deployment.")
+	framework.ExpectEqual(new.Deployment.UID, old.Deployment.UID)
+	framework.ExpectEqual(new.Deployment.Generation, old.Deployment.Generation)
+	framework.ExpectEqual(
+		new.Deployment.Annotations[deploymentutil.RevisionAnnotation],
+		old.Deployment.Annotations[deploymentutil.RevisionAnnotation],
+	)
+
+	g.By("Validating ReplicaSets.")
+	framework.ExpectEqual(len(new.ReplicaSets), len(old.Deployment.UID))
+	var oldRSUIDs []types.UID
+	for uid, _ := range old.ReplicaSets {
+		oldRSUIDs = append(oldRSUIDs, uid)
+	}
+	var newRSUIDs []types.UID
+	for uid, _ := range new.ReplicaSets {
+		newRSUIDs = append(newRSUIDs, uid)
+	}
+	framework.ExpectEqual(newRSUIDs, oldRSUIDs, "New ReplicaSet UIDs differ: "+cmp.Diff(newRSUIDs, oldRSUIDs))
+	for _, newRS := range new.ReplicaSets {
+		oldRS, ok := old.ReplicaSets[newRS.UID]
+		o.Expect(ok).To(o.BeTrue())
+		framework.ExpectEqual(oldRS.Namespace, newRS.Namespace)
+		framework.ExpectEqual(oldRS.Name, newRS.Name)
+		framework.ExpectEqual(oldRS.UID, newRS.UID)
+
+		g.By(fmt.Sprintf("Validating ReplicaSet %s/%s (uid=%s)", newRS.Namespace, newRS.Name, newRS.UID))
+		framework.ExpectEqual(newRS.Generation, oldRS.Generation)
+		framework.ExpectEqual(newRS.Spec.Replicas, oldRS.Spec.Replicas)
+		framework.ExpectEqual(newRS.Status.AvailableReplicas, oldRS.Status.AvailableReplicas)
+		framework.ExpectEqual(newRS.Status.ObservedGeneration, oldRS.Status.ObservedGeneration)
+	}
+
+	// Verify the upgraded deployment is active by scaling up the deployment by 1
+	g.By("Scaling up deployment by 1")
+	deployment, err = e2edeploy.UpdateDeploymentWithRetries(c, new.Deployment.Namespace, new.Deployment.Name, func(d *appsv1.Deployment) {
+		*d.Spec.Replicas = *new.Deployment.Spec.Replicas + 1
+	})
+	framework.ExpectNoError(err)
+
+	g.By("Waiting for deployment to complete after scaling")
+	framework.ExpectNoError(e2edeploy.WaitForDeploymentComplete(c, deployment))
 }
 
 // Teardown cleans up any remaining resources.
