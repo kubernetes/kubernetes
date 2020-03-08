ef2eaa4f8f7ce85478310775a134047a77a8e310
Marius Ziemke <marius.ziemke@lancom.de>
2020-03-08 17:49:18 +0100

    fix kubectl port-forward for services with explicit local port

--- staging/src/k8s.io/kubectl/pkg/cmd/portforward/portforward_test.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/portforward/portforward_test.go
@@ -210,6 +210,35 @@ func TestTranslateServicePortToTargetPort(t *testing.T) {
 			err:        false,
 		},
 		{
+			name: "test success 1 (int port with explicit local port)",
+			svc: corev1.Service{
+				Spec: corev1.ServiceSpec{
+					Ports: []corev1.ServicePort{
+						{
+							Port:       8080,
+							TargetPort: intstr.FromInt(8080),
+						},
+					},
+				},
+			},
+			pod: corev1.Pod{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http",
+									ContainerPort: int32(8080)},
+							},
+						},
+					},
+				},
+			},
+			ports:      []string{"8000:8080"},
+			translated: []string{"8000:8080"},
+			err:        false,
+		},
+		{
 			name: "test success 2 (clusterIP: None)",
 			svc: corev1.Service{
 				Spec: corev1.ServiceSpec{
