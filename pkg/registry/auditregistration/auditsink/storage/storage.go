96fadf9ed2c4ec7f11081bb60667f7eaa9a79e89
Jordan Liggitt <liggitt@google.com>
2020-03-09 10:08:37 -0400

    Define default table converters for missing resources

--- pkg/registry/auditregistration/auditsink/storage/storage.go
+++ pkg/registry/auditregistration/auditsink/storage/storage.go
@@ -20,6 +20,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/registry/generic"
 	genericregistry "k8s.io/apiserver/pkg/registry/generic/registry"
+	"k8s.io/apiserver/pkg/registry/rest"
 	"k8s.io/kubernetes/pkg/apis/auditregistration"
 	auditstrategy "k8s.io/kubernetes/pkg/registry/auditregistration/auditsink"
 )
@@ -42,6 +43,9 @@ func NewREST(optsGetter generic.RESTOptionsGetter) (*REST, error) {
 		CreateStrategy: auditstrategy.Strategy,
 		UpdateStrategy: auditstrategy.Strategy,
 		DeleteStrategy: auditstrategy.Strategy,
+
+		// TODO: define table converter that exposes more than name/creation timestamp
+		TableConvertor: rest.NewDefaultTableConvertor(auditregistration.Resource("auditsinks")),
 	}
 	options := &generic.StoreOptions{RESTOptions: optsGetter}
 	if err := store.CompleteWithOptions(options); err != nil {
