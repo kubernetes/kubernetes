4303e797a65fa1600469dde69f4ad53869580cd4
willise <sunshuai@cmss.chinamobile.com>
2020-03-19 16:37:43 +0800

    fix(apimachinery): trivial typo in comment of label

--- staging/src/k8s.io/apimachinery/pkg/labels/selector.go
+++ staging/src/k8s.io/apimachinery/pkg/labels/selector.go
@@ -221,7 +221,7 @@ func (r *Requirement) Matches(ls Labels) bool {
 			return false
 		}
 
-		// There should be only one strValue in r.strValues, and can be converted to a integer.
+		// There should be only one strValue in r.strValues, and can be converted to an integer.
 		if len(r.strValues) != 1 {
 			klog.V(10).Infof("Invalid values count %+v of requirement %#v, for 'Gt', 'Lt' operators, exactly one value is required", len(r.strValues), r)
 			return false
