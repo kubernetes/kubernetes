42bb845f40229ef00db008e5adeef34a96841c25
Kenichi Omichi <ken-oomichi@wx.jp.nec.com>
2020-03-27 03:43:33 +0000

    Separate RestartControllerManager() as e2ekubesystem
    RestartControllerManager() is kube-controller specific function
    and it is better to separate the function as subpackage of e2e
    test framework.
    In addition, the function made invalid dependency into e2essh.
    So this separates the function into e2ekubesystem subpackage.

--- test/e2e/framework/util.go
+++ test/e2e/framework/util.go
@@ -72,7 +72,6 @@ import (
 	e2emetrics "k8s.io/kubernetes/test/e2e/framework/metrics"
 	e2enode "k8s.io/kubernetes/test/e2e/framework/node"
 	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
-	e2essh "k8s.io/kubernetes/test/e2e/framework/ssh"
 )
 
 const (
@@ -143,9 +142,6 @@ const (
 
 	// TODO(justinsb): Avoid hardcoding this.
 	awsMasterIP = "172.20.0.9"
-
-	// ssh port
-	sshPort = "22"
 )
 
 var (
@@ -1112,40 +1108,6 @@ func AllNodesReady(c clientset.Interface, timeout time.Duration) error {
 		return fmt.Errorf("Not ready nodes: %#v", msg)
 	}
 	return nil
-}
-
-// RestartControllerManager restarts the kube-controller-manager.
-func RestartControllerManager() error {
-	// TODO: Make it work for all providers and distros.
-	if !ProviderIs("gce", "aws") {
-		return fmt.Errorf("unsupported provider for RestartControllerManager: %s", TestContext.Provider)
-	}
-	if ProviderIs("gce") && !MasterOSDistroIs("gci") {
-		return fmt.Errorf("unsupported master OS distro: %s", TestContext.MasterOSDistro)
-	}
-	cmd := "pidof kube-controller-manager | xargs sudo kill"
-	Logf("Restarting controller-manager via ssh, running: %v", cmd)
-	result, err := e2essh.SSH(cmd, net.JoinHostPort(GetMasterHost(), sshPort), TestContext.Provider)
-	if err != nil || result.Code != 0 {
-		e2essh.LogResult(result)
-		return fmt.Errorf("couldn't restart controller-manager: %v", err)
-	}
-	return nil
-}
-
-// WaitForControllerManagerUp waits for the kube-controller-manager to be up.
-func WaitForControllerManagerUp() error {
-	cmd := "curl http://localhost:" + strconv.Itoa(InsecureKubeControllerManagerPort) + "/healthz"
-	for start := time.Now(); time.Since(start) < time.Minute; time.Sleep(5 * time.Second) {
-		result, err := e2essh.SSH(cmd, net.JoinHostPort(GetMasterHost(), sshPort), TestContext.Provider)
-		if err != nil || result.Code != 0 {
-			e2essh.LogResult(result)
-		}
-		if result.Stdout == "ok" {
-			return nil
-		}
-	}
-	return fmt.Errorf("waiting for controller-manager timed out")
 }
 
 // LookForStringInLog looks for the given string in the log of a specific pod container
--- test/e2e/storage/nfs_persistent_volume-disruptive.go
+++ test/e2e/storage/nfs_persistent_volume-disruptive.go
@@ -31,6 +31,7 @@ import (
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/kubernetes/test/e2e/framework"
+	e2ekubesystem "k8s.io/kubernetes/test/e2e/framework/kubesystem"
 	e2enode "k8s.io/kubernetes/test/e2e/framework/node"
 	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
 	e2epv "k8s.io/kubernetes/test/e2e/framework/pv"
@@ -214,9 +215,9 @@ var _ = utils.SIGDescribe("NFSPersistentVolumes[Disruptive][Flaky]", func() {
 			pvc2 = nil
 
 			ginkgo.By("Restarting the kube-controller-manager")
-			err = framework.RestartControllerManager()
+			err = e2ekubesystem.RestartControllerManager()
 			framework.ExpectNoError(err)
-			err = framework.WaitForControllerManagerUp()
+			err = e2ekubesystem.WaitForControllerManagerUp()
 			framework.ExpectNoError(err)
 			framework.Logf("kube-controller-manager restarted")
 
