935c03351bc0f5ee3e2c124c960508f7412ed4c7
Srinivas Boga <bseenu@gmail.com>
2020-04-03 04:10:20 -0700

    [KUBECTL-808] kubectl logs command with label selector option doesn't print error messages

--- staging/src/k8s.io/kubectl/pkg/cmd/logs/logs.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/logs/logs.go
@@ -275,6 +275,9 @@ func (o *LogsOptions) Complete(f cmdutil.Factory, cmd *cobra.Command, args []str
 			return errors.New("expected a resource")
 		}
 		o.Object = infos[0].Object
+		if o.Selector != "" && len(o.Object.(*corev1.PodList).Items) == 0 {
+			return errors.New(fmt.Sprintf("no resources found in %s namespace", o.Namespace))
+		}
 	}
 
 	return nil
