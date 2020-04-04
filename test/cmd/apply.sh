bb8c46ba21f88a6973237c0daca733b9030e44dc
Prashant Sharma <prashsh1@in.ibm.com>
2020-04-04 08:53:12 +0530

    get_object_assert -> wait_object_assert

--- test/cmd/apply.sh
+++ test/cmd/apply.sh
@@ -79,11 +79,10 @@ run_kubectl_apply_tests() {
   ! grep -q emptyDir <<< "$(kubectl get deployments test-deployment-retainkeys -o yaml "${kube_flags[@]:?}")" || exit 1
   # Clean up
   kubectl delete deployments test-deployment-retainkeys "${kube_flags[@]:?}"
-  kubectl wait pod --for=delete --all --timeout=15s || true
 
   ## kubectl apply -f with label selector should only apply matching objects
   # Pre-Condition: no POD exists
-  kube::test::get_object_assert pods "{{range.items}}{{${id_field:?}}}:{{end}}" ''
+  kube::test::wait_object_assert pods "{{range.items}}{{${id_field:?}}}:{{end}}" ''
   # apply
   kubectl apply -l unique-label=bingbang -f hack/testdata/filter "${kube_flags[@]:?}"
   # check right pod exists
