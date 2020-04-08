a4e29f248134db32978dd10e84b51d5313b9ffd1
Caleb Woodbine <caleb@ii.coop>
2020-04-09 09:29:44 +1200

    Fix formatting

--- test/e2e/common/pods.go
+++ test/e2e/common/pods.go
@@ -887,7 +887,7 @@ var _ = framework.KubeDescribe("Pods", func() {
 
 		testPod := v1.Pod{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: testPodName,
+				Name:   testPodName,
 				Labels: testPodLabels,
 			},
 			Spec: v1.PodSpec{
@@ -925,7 +925,7 @@ var _ = framework.KubeDescribe("Pods", func() {
 			},
 			Spec: v1.PodSpec{
 				Containers: []v1.Container{{
-					Name: testPodName,
+					Name:  testPodName,
 					Image: testPodImage2,
 				}},
 			},
