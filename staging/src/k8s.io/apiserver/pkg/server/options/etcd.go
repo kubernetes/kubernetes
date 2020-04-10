368e976113ca035854083212275a0c39373f152e
Maurizio Drocco <maurizio.drocco@ibm.com>
2020-04-10 13:21:59 -0400

    Update etcd.go

--- staging/src/k8s.io/apiserver/pkg/server/options/etcd.go
+++ staging/src/k8s.io/apiserver/pkg/server/options/etcd.go
@@ -117,8 +117,8 @@ func (s *EtcdOptions) AddFlags(fs *pflag.FlagSet) {
 	fs.StringSliceVar(&s.EtcdServersOverrides, "etcd-servers-overrides", s.EtcdServersOverrides, ""+
 		"Per-resource etcd servers overrides, comma separated. The individual override "+
 		"format: group/resource#servers, where servers are URLs, semicolon separated. "+
-		"Note that this applies only to static API types compiled into this server binary, "+
-		"so it does not affect aggregated APIs or dynamic CRDs.")
+		"Note that this applies only to resources compiled into this server binary, "+
+		"so it does not affect resources served by aggregated API servers nor ones defined by CRDs.")
 
 	fs.StringVar(&s.DefaultStorageMediaType, "storage-media-type", s.DefaultStorageMediaType, ""+
 		"The media type to use to store objects in storage. "+
