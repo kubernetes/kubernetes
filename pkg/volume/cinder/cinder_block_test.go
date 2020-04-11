11d54320ac1679e986a5c8cc8ec397b1d7fbb52b
Gaurav Singh <gaurav1086@gmail.com>
2020-04-11 10:02:56 -0400

    Correct error message

--- pkg/volume/cinder/cinder_block_test.go
+++ pkg/volume/cinder/cinder_block_test.go
@@ -71,7 +71,7 @@ func TestGetVolumeSpecFromGlobalMapPath(t *testing.T) {
 	block := v1.PersistentVolumeBlock
 	specMode := spec.PersistentVolume.Spec.VolumeMode
 	if specMode == nil {
-		t.Fatalf("Invalid volumeMode from GlobalMapPath spec: %v expected: %v", &specMode, block)
+		t.Fatalf("Failed to get volumeMode from PersistentVolumeBlock")
 	}
 	if *specMode != block {
 		t.Errorf("Invalid volumeMode from GlobalMapPath spec: %v expected: %v", *specMode, block)
