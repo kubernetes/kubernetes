76b1754cbd86e3494faa65938f809addbe5fda6b
pacoxu <paco.xu@daocloud.io>
2020-04-13 20:16:01 +0800

    ipv6 support: addr scope should be global or site Signed-off-by: pacoxu <paco.xu@daocloud.io>

--- pkg/kubelet/network/plugins.go
+++ pkg/kubelet/network/plugins.go
@@ -259,27 +259,36 @@ func (plugin *NoopNetworkPlugin) Status() error {
 }
 
 func getOnePodIP(execer utilexec.Interface, nsenterPath, netnsPath, interfaceName, addrType string) (net.IP, error) {
-	// Try to retrieve ip inside container network namespace
-	output, err := execer.Command(nsenterPath, fmt.Sprintf("--net=%s", netnsPath), "-F", "--",
-		"ip", "-o", addrType, "addr", "show", "dev", interfaceName, "scope", "global").CombinedOutput()
-	if err != nil {
-		return nil, fmt.Errorf("Unexpected command output %s with error: %v", output, err)
+	scopes := []string{
+		"global"
 	}
+	if addrType == "-6" {
+		// IPv6 site scope ips will be deprecated later
+		append(scopes, "site")
+	}
+	for _, scope := range scopes {
+		// Try to retrieve ip inside container network namespace
+		output, err := execer.Command(nsenterPath, fmt.Sprintf("--net=%s", netnsPath), "-F", "--",
+			"ip", "-o", addrType, "addr", "show", "dev", interfaceName, "scope", "global").CombinedOutput()
+		if err != nil {
+			return nil, fmt.Errorf("Unexpected command output %s with error: %v", output, err)
+		}
 
-	lines := strings.Split(string(output), "\n")
-	if len(lines) < 1 {
-		return nil, fmt.Errorf("Unexpected command output %s", output)
-	}
-	fields := strings.Fields(lines[0])
-	if len(fields) < 4 {
-		return nil, fmt.Errorf("Unexpected address output %s ", lines[0])
-	}
-	ip, _, err := net.ParseCIDR(fields[3])
-	if err != nil {
-		return nil, fmt.Errorf("CNI failed to parse ip from output %s due to %v", output, err)
-	}
+		lines := strings.Split(string(output), "\n")
+		if len(lines) < 1 {
+			return nil, fmt.Errorf("Unexpected command output %s", output)
+		}
+		fields := strings.Fields(lines[0])
+		if len(fields) < 4 {
+			return nil, fmt.Errorf("Unexpected address output %s ", lines[0])
+		}
+		ip, _, err := net.ParseCIDR(fields[3])
+		if err != nil {
+			return nil, fmt.Errorf("CNI failed to parse ip from output %s due to %v", output, err)
+		}
 
-	return ip, nil
+		return ip, nil
+	}
 }
 
 // GetPodIP gets the IP of the pod by inspecting the network info inside the pod's network namespace.
