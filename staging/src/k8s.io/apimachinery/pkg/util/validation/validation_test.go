18856dace935db46d3ba84374ce23438922e272b
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-04-15 21:04:02 +0900

    Add DNS1123Label validation to IsFullyQualifiedDomainName func
    This patch adds IsDNS1123Label validation to
    IsFullyQualifiedDomainName func.
    
    Even when one label is longer than 64 characters, the current
    validation  does not validate it. Hence this patch adds the label
    check and do not allow invalid domain.

--- staging/src/k8s.io/apimachinery/pkg/util/validation/validation_test.go
+++ staging/src/k8s.io/apimachinery/pkg/util/validation/validation_test.go
@@ -557,7 +557,8 @@ func TestIsFullyQualifiedDomainName(t *testing.T) {
 		"bbc.co.uk",
 		"10.0.0.1", // DNS labels can start with numbers and there is no requirement for letters.
 		"hyphens-are-good.k8s.io",
-		strings.Repeat("a", 246) + ".k8s.io",
+		strings.Repeat("a", 63) + ".k8s.io",
+		strings.Repeat("a", 63) + "." + strings.Repeat("b", 63) + "." + strings.Repeat("c", 63) + "." + strings.Repeat("d", 54) + ".k8s.io",
 	}
 	for _, val := range goodValues {
 		if err := IsFullyQualifiedDomainName(field.NewPath(""), val).ToAggregate(); err != nil {
@@ -579,7 +580,8 @@ func TestIsFullyQualifiedDomainName(t *testing.T) {
 		"underscores_are_bad.k8s.io",
 		"foo@bar.example.com",
 		"http://foo.example.com",
-		strings.Repeat("a", 247) + ".k8s.io",
+		strings.Repeat("a", 64) + ".k8s.io",
+		strings.Repeat("a", 63) + "." + strings.Repeat("b", 63) + "." + strings.Repeat("c", 63) + "." + strings.Repeat("d", 55) + ".k8s.io",
 	}
 	for _, val := range badValues {
 		if err := IsFullyQualifiedDomainName(field.NewPath(""), val).ToAggregate(); err == nil {
