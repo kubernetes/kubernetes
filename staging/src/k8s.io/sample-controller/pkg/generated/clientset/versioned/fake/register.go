b113638cc8f9f38f33f355ee0200e00a56703c6f
Markus Thömmes <markusthoemmes@me.com>
2020-04-16 10:53:36 +0200

    Run ./hack/update-codegen.sh.

--- staging/src/k8s.io/sample-controller/pkg/generated/clientset/versioned/fake/register.go
+++ staging/src/k8s.io/sample-controller/pkg/generated/clientset/versioned/fake/register.go
@@ -29,7 +29,7 @@ import (
 
 var scheme = runtime.NewScheme()
 var codecs = serializer.NewCodecFactory(scheme)
-var parameterCodec = runtime.NewParameterCodec(scheme)
+
 var localSchemeBuilder = runtime.SchemeBuilder{
 	samplecontrollerv1alpha1.AddToScheme,
 }
