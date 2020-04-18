903b98d2bf95d68e6d682dfc31b8878b57bb324b
Gaurav Singh <gaurav1086@gmail.com>
2020-04-18 18:56:59 -0400

    [AWS] fix block test

--- pkg/volume/awsebs/aws_ebs_block_test.go
+++ pkg/volume/awsebs/aws_ebs_block_test.go
@@ -78,8 +78,8 @@ func TestGetVolumeSpecFromGlobalMapPath(t *testing.T) {
 	}
 	block := v1.PersistentVolumeBlock
 	specMode := spec.PersistentVolume.Spec.VolumeMode
-	if &specMode == nil {
-		t.Errorf("Invalid volumeMode from GlobalMapPath spec: %v - %v", &specMode, block)
+	if specMode == nil {
+		t.Fatalf("Failed to get spec from GlobalMapPath: %v", err)
 	}
 	if *specMode != block {
 		t.Errorf("Invalid volumeMode from GlobalMapPath spec: %v - %v", *specMode, block)
