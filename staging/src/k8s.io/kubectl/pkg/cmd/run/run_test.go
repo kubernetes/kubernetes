b7d690549cba9c076a5a490a582def0ccde00c2a
zhouya0 <yao.zhoupolytech@gmail.com>
2020-05-04 22:35:03 +0800

    Fix kubectl run ignore namespace

--- staging/src/k8s.io/kubectl/pkg/cmd/run/run_test.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/run/run_test.go
@@ -382,6 +382,8 @@ func TestGenerateService(t *testing.T) {
 				PrintObj: func(obj runtime.Object) error {
 					return printer.PrintObj(obj, buff)
 				},
+
+				Namespace: "test",
 			}
 
 			cmd := &cobra.Command{}
@@ -398,7 +400,7 @@ func TestGenerateService(t *testing.T) {
 				test.params["port"] = test.port
 			}
 
-			_, err = opts.generateService(tf, cmd, test.serviceGenerator, test.params, "test")
+			_, err = opts.generateService(tf, cmd, test.serviceGenerator, test.params)
 			if test.expectErr {
 				if err == nil {
 					t.Error("unexpected non-error")
