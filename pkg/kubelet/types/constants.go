839f43a482b4d8c3fbcae771446fe4308754b18e
iamchuckss <chuck017@umn.edu>
2020-05-18 19:59:29 +0800

    Fix log timestamps to be displayed in fixed width

--- pkg/kubelet/types/constants.go
+++ pkg/kubelet/types/constants.go
@@ -29,4 +29,9 @@ const (
 	SystemReservedEnforcementKey  = "system-reserved"
 	KubeReservedEnforcementKey    = "kube-reserved"
 	NodeAllocatableNoneKey        = "none"
+
+	// fixed width version of time.RFC3339Nano
+	RFC3339NanoFixed = "2006-01-02T15:04:05.000000000Z07:00"
+	// variable width RFC3339 time format for lenient parsing of strings into timestamps
+	RFC3339NanoLenient = "2006-01-02T15:04:05.999999999Z07:00"
 )
