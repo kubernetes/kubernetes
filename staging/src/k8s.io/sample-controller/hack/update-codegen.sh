aa41ee274d853f658ced452ba5c7e1e4ee19c1c0
bigpigeon <bigpigeon0@gmail.com>
2020-05-22 18:18:49 +0800

    1. add go module support for sample-controller/hack/update-codegen.sh

--- staging/src/k8s.io/sample-controller/hack/update-codegen.sh
+++ staging/src/k8s.io/sample-controller/hack/update-codegen.sh
@@ -19,7 +19,13 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+
+if [[ $(go env GO111MODULE) == "on" ]]; then
+  CODEGEN_PKG=$(go list -f '{{.Dir}}' -m k8s.io/code-generator)
+else
+  CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+fi
+
 
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
