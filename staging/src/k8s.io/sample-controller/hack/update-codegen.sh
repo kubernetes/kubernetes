22eb45f2e304bbe715a2fd117435695f1d31401c
bigpigeon <12421954+bigpigeon@users.noreply.github.com>
2020-05-23 13:08:18 +0800

    explicit envvar  "CODEGEN_PKG"

--- staging/src/k8s.io/sample-controller/hack/update-codegen.sh
+++ staging/src/k8s.io/sample-controller/hack/update-codegen.sh
@@ -19,11 +19,10 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
 if [[ $(go env GO111MODULE) == "on" ]]; then
   CODEGEN_PKG=$(go list -f '{{.Dir}}' -m k8s.io/code-generator)
-else
-  CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 fi
 
 
