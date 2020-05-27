50474d4cbbeb5bd5203863a25954ed127b8edd40
ZhiFeng1993 <helenfengzhi@gmail.com>
2020-05-26 21:49:38 -0700

    Add usage in some hack/update scripts

--- hack/update-generated-protobuf.sh
+++ hack/update-generated-protobuf.sh
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This script generates all go files from the corresponding protobuf files.
+# Usage: `hack/update-generated-protobuf.sh`.
+
 set -o errexit
 set -o nounset
 set -o pipefail
