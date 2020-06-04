3e055581017bd846c8944c97e18bf33c6aac3438
Li Zhijian <lizhijian@cn.fujitsu.com>
2020-06-04 15:50:32 +0800

    hack/local-up-cluster.sh: Fix shutdown doesn't cleanup KUBE_TEMP
    Previously, KUBE_TEMP is not cleaned up after cluster exited
    $ find /tmp -name cfssl -type d 2>/dev/null
    /tmp/tmp.B54BGylaai/cfssl
    /tmp/tmp.7lF5rEVpCO/cfssl
    /tmp/tmp.dIibXvwNpt/cfssl
    /tmp/tmp.TK26cKan6R/cfssl
    
    utilize kube::util::trap_add() which allows trapping mutilple functions for same signal
    to trap cleanup() as well
    
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>

--- hack/local-up-cluster.sh
+++ hack/local-up-cluster.sh
@@ -376,7 +376,6 @@ cleanup()
   if [[ "${PRESERVE_ETCD}" == "false" ]]; then
     [[ -n "${ETCD_DIR-}" ]] && kube::etcd::clean_etcd_dir
   fi
-  exit 0
 }
 
 # Check if all processes are still running. Prints a warning once each time
@@ -1053,7 +1052,7 @@ echo "Detected host and ready to start services.  Doing some housekeeping first.
 echo "Using GO_OUT ${GO_OUT}"
 export KUBELET_CIDFILE=/tmp/kubelet.cid
 if [[ "${ENABLE_DAEMON}" = false ]]; then
-  trap cleanup EXIT
+  kube::util::trap_add cleanup EXIT
 fi
 
 echo "Starting services now!"
