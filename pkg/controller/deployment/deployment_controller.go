3649a72f7e0edad6809243ffef778b6e756e2233
Lu Fengqi <lufq.fnst@cn.fujitsu.com>
2020-06-09 15:25:43 +0800

    deployment: get PodMap only if need it
    Because of commit 647d8d8a226a ("Deployment to stop adding pod-template-hash
    labels/selector on adoption"), need getPodMapForDeployment only if use
    the Recreate strategy.
    
    Signed-off-by: Lu Fengqi <lufq.fnst@cn.fujitsu.com>

--- pkg/controller/deployment/deployment_controller.go
+++ pkg/controller/deployment/deployment_controller.go
@@ -600,15 +600,6 @@ func (dc *DeploymentController) syncDeployment(key string) error {
 	if err != nil {
 		return err
 	}
-	// List all Pods owned by this Deployment, grouped by their ReplicaSet.
-	// Current uses of the podMap are:
-	//
-	// * check if a Pod is labeled correctly with the pod-template-hash label.
-	// * check that no old Pods are running in the middle of Recreate Deployments.
-	podMap, err := dc.getPodMapForDeployment(d, rsList)
-	if err != nil {
-		return err
-	}
 
 	if d.DeletionTimestamp != nil {
 		return dc.syncStatusOnly(d, rsList)
@@ -642,6 +633,12 @@ func (dc *DeploymentController) syncDeployment(key string) error {
 
 	switch d.Spec.Strategy.Type {
 	case apps.RecreateDeploymentStrategyType:
+		// List all Pods owned by this Deployment, grouped by their ReplicaSet.
+		// use to check that no old Pods are running in the middle of Recreate Deployments.
+		podMap, err := dc.getPodMapForDeployment(d, rsList)
+		if err != nil {
+			return err
+		}
 		return dc.rolloutRecreate(d, rsList, podMap)
 	case apps.RollingUpdateDeploymentStrategyType:
 		return dc.rolloutRolling(d, rsList)
