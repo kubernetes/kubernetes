b9bb2d3b8510062f2e90ecdbe06014a5dde3beea
Yang Lu <yluu@google.com>
2020-06-11 05:10:26 -0700

    Flush cache to disk to persist setup status

--- cluster/gce/windows/configure.ps1
+++ cluster/gce/windows/configure.ps1
@@ -152,6 +152,9 @@ try {
     Configure-LoggingAgent
     Restart-LoggingAgent
   }
+  # Flush cache to disk before starting kubelet & kube-proxy services
+  # to make metadata server route and stackdriver service more persistent.
+  Write-Volumecache C -PassThru
   Start-WorkerServices
   Log-Output 'Waiting 15 seconds for node to join cluster.'
   Start-Sleep 15
@@ -162,6 +165,8 @@ try {
   Schedule-LogRotation -Pattern '.*\.log$' -Path ${env:LOGS_DIR} -RepetitionInterval $(New-Timespan -Hour 1) -Config $config
 
   Pull-InfraContainer
+  # Flush cache to disk to persist the setup status
+  Write-Volumecache C -PassThru
 }
 catch {
   Write-Host 'Exception caught in script:'
