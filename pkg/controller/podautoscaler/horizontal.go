507c85262e652f1718ae9634978de566136ddd6e
Jack Farzan <jpfarzan@gmail.com>
2020-06-12 10:17:18 -0400

    accept caps cpu

--- pkg/controller/podautoscaler/horizontal.go
+++ pkg/controller/podautoscaler/horizontal.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2015 The Kubernetes Authors.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -467,7 +464,9 @@ func (a *HorizontalController) computeStatusForResourceMetricGeneric(currentRepl
 	if err != nil {
 		return 0, nil, time.Time{}, "", condition, fmt.Errorf("failed to get %s utilization: %v", resourceName, err)
 	}
-
+	if metricSpec.Resource.Name == "CPU" {
+		metricSpec.Resource.Name = "cpu"
+	}
 	metricNameProposal = fmt.Sprintf("%s resource utilization (percentage of request)", resourceName)
 	status := autoscalingv2.MetricValueStatus{
 		AverageUtilization: &percentageProposal,
