441eb28c92043a64a514b92662a6ff97e678e84e
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2020-06-16 10:07:00 +0200

    proxy retry a happy path test scenario

--- staging/src/k8s.io/kube-aggregator/pkg/apiserver/handler_proxy_test.go
+++ staging/src/k8s.io/kube-aggregator/pkg/apiserver/handler_proxy_test.go
@@ -18,8 +18,19 @@ package apiserver
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/websocket"
 	"io/ioutil"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/proxy"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apiserver/pkg/authentication/user"
+	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
+	apiregistration "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
+	"k8s.io/utils/pointer"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
@@ -28,16 +39,6 @@ import (
 	"strings"
 	"sync/atomic"
 	"testing"
-
-	"golang.org/x/net/websocket"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/proxy"
-	"k8s.io/apimachinery/pkg/util/sets"
-	"k8s.io/apiserver/pkg/authentication/user"
-	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
-	apiregistration "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
-	"k8s.io/utils/pointer"
 )
 
 type targetHTTPHandler struct {
@@ -45,6 +46,7 @@ type targetHTTPHandler struct {
 	headers map[string][]string
 	path    string
 	host    string
+	proto   string
 }
 
 func (d *targetHTTPHandler) Reset() {
@@ -52,6 +54,7 @@ func (d *targetHTTPHandler) Reset() {
 	d.called = false
 	d.headers = nil
 	d.host = ""
+	d.proto = ""
 }
 
 func (d *targetHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -59,6 +62,7 @@ func (d *targetHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	d.called = true
 	d.headers = r.Header
 	d.host = r.Host
+	d.proto = r.Proto
 	w.WriteHeader(http.StatusOK)
 }
 
@@ -456,6 +460,154 @@ func TestProxyUpgrade(t *testing.T) {
 		}()
 	}
 }
+
+func TestProxyRetries(t *testing.T) {
+	testCases := map[string]struct {
+		APIService *apiregistration.APIService
+	}{
+		"happy path: HTTP/2.0 established test->aggregator->backend": {
+			APIService: &apiregistration.APIService{
+				Spec: apiregistration.APIServiceSpec{
+					CABundle: testCACrt,
+					Group:    "mygroup",
+					Version:  "v1",
+					Service:  &apiregistration.ServiceReference{Name: "test-service", Namespace: "test-ns", Port: pointer.Int32Ptr(443)},
+				},
+				Status: apiregistration.APIServiceStatus{
+					Conditions: []apiregistration.APIServiceCondition{
+						{Type: apiregistration.Available, Status: apiregistration.ConditionTrue},
+					},
+				},
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		testCaseName := name
+		path := "/apis/" + testCase.APIService.Spec.Group + "/" + testCase.APIService.Spec.Version + "/foo"
+
+		func() {
+			// set up the backend server
+			backendHandler := &targetHTTPHandler{}
+			backendServer := httptest.NewUnstartedServer(backendHandler)
+			backendCert, err := tls.X509KeyPair(svcCrt, svcKey)
+			if err != nil {
+				t.Fatalf("backend: invalid x509/key pair: %v", err)
+			}
+			backendServer.TLS = &tls.Config{
+				Certificates: []tls.Certificate{backendCert},
+				NextProtos:   []string{http2.NextProtoTLS},
+			}
+			backendServer.StartTLS()
+			defer backendServer.Close()
+
+			// set up the aggregator
+			serverURL, _ := url.Parse(backendServer.URL)
+			proxyHandler := &proxyHandler{
+				serviceResolver: &mockedRouter{destinationHost: serverURL.Host},
+				proxyTransport:  &http.Transport{},
+			}
+			proxyHandler.updateAPIService(testCase.APIService)
+
+			aggregator := httptest.NewUnstartedServer(contextHandler(proxyHandler, &user.DefaultInfo{Name: "username"}))
+			aggregatorCert, err := tls.X509KeyPair(aggregatorCrt, aggregatorKey)
+			if err != nil {
+				t.Fatalf("aggregator: invalid x509/key pair: %v", err)
+			}
+			aggregator.TLS = &tls.Config{
+				Certificates: []tls.Certificate{aggregatorCert},
+				NextProtos:   []string{http2.NextProtoTLS},
+			}
+			aggregator.StartTLS()
+			defer aggregator.Close()
+
+			clientCACertPool := x509.NewCertPool()
+			clientCACertPool.AppendCertsFromPEM(aggregatorCrt)
+			clientTLSConfig := &tls.Config{
+				RootCAs:    clientCACertPool,
+				NextProtos: []string{http2.NextProtoTLS},
+			}
+			client := &http.Client{}
+			client.Transport = &http2.Transport{
+				TLSClientConfig: clientTLSConfig,
+			}
+
+			// act
+			resp, err := client.Get(fmt.Sprintf("https://localhost:%d%s", aggregator.Listener.Addr().(*net.TCPAddr).Port, path))
+			if err != nil {
+				t.Fatalf("%s: %v", testCaseName, err)
+			}
+
+			// validate
+			defer resp.Body.Close()
+			_, err = ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("%s: %v", testCaseName, err)
+			}
+			if resp.StatusCode != 200 {
+				t.Errorf("unexpected HTTP staus: %v, expected: 200", resp.Proto)
+			}
+			expectedProto := "HTTP/2.0"
+			if resp.Proto != expectedProto {
+				t.Errorf("unexpected response proto: %v, expected: %v", resp.Proto, expectedProto)
+			}
+			if backendHandler.proto != expectedProto {
+				t.Errorf("unexpected backend (aggregator->backend) proto: %v, expected: %v", backendHandler.proto, expectedProto)
+			}
+		}()
+	}
+}
+
+// valid for localhost
+var aggregatorCrt = []byte(`-----BEGIN CERTIFICATE-----
+MIIC5TCCAc2gAwIBAgIJAOS8kx4rqQxcMA0GCSqGSIb3DQEBCwUAMBQxEjAQBgNV
+BAMMCWxvY2FsaG9zdDAeFw0yMDA2MTUxMTE2MDFaFw0yMDA3MTUxMTE2MDFaMBQx
+EjAQBgNVBAMMCWxvY2FsaG9zdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+ggEBAPL7bdiu1h8BadQPn5tgN4cBbMLmP8jpNduoC7KExbtKz7mdbCi7t5/vRgEq
+tEgJqcsBbCCZzAYQHExkRqchaiVOQf1JvYywHSJ0IQ9IpIB4WwZiRitKsBoUwufn
+ekpvHNOUwKbjQWdxCz26sCSgDsLNK2COmJwFoTFUQWuC0X1SYsT5KqnJwTMP19Xq
+GFI0sWsZoQxe7QhJBYu8ierA+OkS0yZiBvFX8Cb1ChjUA3D1Bred2eNSZSafij2z
+ZsvpAQea7lUmRVAJe/+HHGgptXiHR+voWh5LnI+SGTfRIjgXSogc6rSlkDxBL3qs
+BSOKoiF8sy9WkowY8FKGGQkMmJcCAwEAAaM6MDgwFAYDVR0RBA0wC4IJbG9jYWxo
+b3N0MAsGA1UdDwQEAwIHgDATBgNVHSUEDDAKBggrBgEFBQcDATANBgkqhkiG9w0B
+AQsFAAOCAQEAvqdSHV2OAY36Xwe+5egq2oH98zfxTyp9hgsIO/8VJf/ukw+sSKFY
+ZEl3ABzjHk9BDyLLoj6DjvjHva6Ghk/ruYg9Q312+dkn/RRCuKx2cOUSq+SFZxra
+Lv4BMO8miiPeVmvP1klhqZZMCV7qpC/MdVVn3SgGYB9ymhGQa0iE0scUk1+zDNIg
+p7iHbi227WZ/pROEFt8sSf1MltaQ/0QI9G2yCxDgjPSNte8vCqVDbXZkXBE5i6qF
+TbvIk4/K13UC3YAgfhedNzf5Smbe6moK008BCp7itKL6IDb20gI9htBsgzolT8O+
+G5lxVI5gSU/VdcGjW0EyWcKEct4LZMUTCg==
+-----END CERTIFICATE-----
+`)
+
+var aggregatorKey = []byte(`-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDy+23YrtYfAWnU
+D5+bYDeHAWzC5j/I6TXbqAuyhMW7Ss+5nWwou7ef70YBKrRICanLAWwgmcwGEBxM
+ZEanIWolTkH9Sb2MsB0idCEPSKSAeFsGYkYrSrAaFMLn53pKbxzTlMCm40FncQs9
+urAkoA7CzStgjpicBaExVEFrgtF9UmLE+SqpycEzD9fV6hhSNLFrGaEMXu0ISQWL
+vInqwPjpEtMmYgbxV/Am9QoY1ANw9Qa3ndnjUmUmn4o9s2bL6QEHmu5VJkVQCXv/
+hxxoKbV4h0fr6FoeS5yPkhk30SI4F0qIHOq0pZA8QS96rAUjiqIhfLMvVpKMGPBS
+hhkJDJiXAgMBAAECggEAIreyFkfE6GE3Ucl5sKWqyWt2stJbQsWvoFb+dN9rsTsb
+OxY3IgrQTdXOVtRXNgPLcuodHPtcn3El2fRp8+9eTz5DR4GFx9hSEV4uaxSiDIkl
+2F+qTv049EELKD92xbPiloimjjHiYnlQdd161YDZGxRdoko9m+1h/r5fKpFihVk8
+5H6RaGb2hga6iuIvAoZ0sGPOIchSOOXC0Dpz7AimSW8JnE2aWNlRu/jiBQ9RxhAr
+WP5Ey2FpNqgQfD22pbx3Ql7ULdFV2GP4owo3eWDbvHtIq+Q9WibE7zfWTtBuTKYu
+oeo2e8mkKR83KmtqWzLRGEgxDvzwT/fk8ldoiSZewQKBgQD8kdEYrqyJA6kGCrQY
+YjX8BXu+c4fkm3yxwGLJiA7RExckQ5smxy1Fzl4I4PApxWStOWxm5Uh2s0xSbDW3
+TnRyuzVq5XehQiB5vFzPgU3ywKLy4hXrKxSotH/k6yHQMF4QJZaPpkxPQNIbdN03
+6yntrdNB4sUxpYbrtAeSYaqziQKBgQD2SEbbOOO6Zl96KAiQ0D3v7vP86H4gjyLV
+w1VDiyRCPimHbT3kCNKQZMQdKPssvf3ie6JBMNQc0K3lkzU2qvihI6jOb6QK6QIF
+5eqySPDV7ZysU4CaFHSLXg6pyJ5XB+3Y8mmxnEm6EmpeOuI+4MCZ5zcFR1+kIRHU
+ORzLGERDHwKBgQDKHXJjuxyNBKXVFOmr/aPPyx+Md+2OjrMJl7g2KDAbNZi2R3e4
+X3mmPA/aMQ9fjfwT9zj9WoxTmQYBi2CtERZ03cVQhtLl9AIDCS6IS6RyF6AOl8gM
+ikwc+VzDdzp23M3ZRAspZ133qhq5KBsDbaf+8LR3LB67rQe8RTQt+wRcaQKBgQDE
+BS7wWU1YFRc1IRwANt61U5k62MlanNJ7FWeNxPdtChD/y0EReLwvVSSKmQ2hxO6I
+DyNLg9Ovw6BFM2+NPXN6vekjtdP5IxALJb4xfMDDZMXomuWmvVUtgAVnuVfdqV/z
+5q2dQemkgffLXE6rATQKyu8N8or7FZ8dLP/v3jamvQKBgER5Rr91lvS2HFXow8Rg
+tAmpti96MRH0SK2gdAoT7Xr9hsqqC8dAtgAdF+jzeecQk5IaNlGS0SRQpCdMIvE1
+Qy8OUgg/TEsBhDpg4FbFXwqlOE1PVsV+HNw578YHkOkamSH1rxTW9EJI8h+aiwqr
+Yw8ovJTCPLC33LushxKas9hY
+-----END PRIVATE KEY-----
+`)
 
 var testCACrt = []byte(`-----BEGIN CERTIFICATE-----
 MIICxDCCAaygAwIBAgIBATANBgkqhkiG9w0BAQsFADASMRAwDgYDVQQDEwd0ZXN0
