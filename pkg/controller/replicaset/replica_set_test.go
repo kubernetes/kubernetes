f3112a6626c5be793974f3b513fa9c609cb38281
teivah <t.harsanyi@thebeat.co>
2020-06-25 23:45:47 +0200

    fix pkg/controller/replicaset staticcheck

--- pkg/controller/replicaset/replica_set_test.go
+++ pkg/controller/replicaset/replica_set_test.go
@@ -504,13 +504,6 @@ func TestPodControllerLookup(t *testing.T) {
 	}
 }
 
-// byName sorts pods by their names.
-type byName []*v1.Pod
-
-func (pods byName) Len() int           { return len(pods) }
-func (pods byName) Swap(i, j int)      { pods[i], pods[j] = pods[j], pods[i] }
-func (pods byName) Less(i, j int) bool { return pods[i].Name < pods[j].Name }
-
 func TestRelatedPodsLookup(t *testing.T) {
 	someRS := newReplicaSet(1, map[string]string{"foo": "bar"})
 	someRS.Name = "foo1"
@@ -879,7 +872,6 @@ func TestControllerUpdateStatusWithFailure(t *testing.T) {
 			}
 		default:
 			t.Errorf("Unexpected action %+v", a)
-			break
 		}
 	}
 	if gets != 1 || updates != 2 {
@@ -1124,7 +1116,11 @@ func TestDeleteControllerAndExpectations(t *testing.T) {
 	manager.deleteRS(rs)
 	manager.syncReplicaSet(GetKey(rs, t))
 
-	if _, exists, err = manager.expectations.GetExpectations(rsKey); exists {
+	_, exists, err = manager.expectations.GetExpectations(rsKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
 		t.Errorf("Found expectations, expected none since the ReplicaSet has been deleted.")
 	}
 
@@ -1216,7 +1212,7 @@ func TestExpectationsOnRecreate(t *testing.T) {
 		t.Fatalf("Deleting RS didn't result in new item in the queue: %v", err)
 	}
 
-	rsExp, exists, err = manager.expectations.GetExpectations(oldRSKey)
+	_, exists, err = manager.expectations.GetExpectations(oldRSKey)
 	if err != nil {
 		t.Fatal(err)
 	}
