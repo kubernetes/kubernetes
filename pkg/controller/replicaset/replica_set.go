54ac3e3bf83fe90e5c9799a69d206068ca9b34ef
Gaurav Singh <gaurav1086@gmail.com>
2020-06-30 00:36:01 -0400

    [controller] fix deadlock in manageReplicas
    Signed-off-by: Gaurav Singh <gaurav1086@gmail.com>

--- pkg/controller/replicaset/replica_set.go
+++ pkg/controller/replicaset/replica_set.go
@@ -609,11 +609,8 @@ func (rsc *ReplicaSetController) manageReplicas(filteredPods []*v1.Pod, rs *apps
 		rsc.expectations.ExpectDeletions(rsKey, getPodKeys(podsToDelete))
 
 		errCh := make(chan error, diff)
-		var wg sync.WaitGroup
-		wg.Add(diff)
 		for _, pod := range podsToDelete {
 			go func(targetPod *v1.Pod) {
-				defer wg.Done()
 				if err := rsc.podControl.DeletePod(rs.Namespace, targetPod.Name, rs); err != nil {
 					// Decrement the expected number of deletes because the informer won't observe this deletion
 					podKey := controller.PodKey(targetPod)
@@ -625,7 +622,6 @@ func (rsc *ReplicaSetController) manageReplicas(filteredPods []*v1.Pod, rs *apps
 				}
 			}(pod)
 		}
-		wg.Wait()
 
 		select {
 		case err := <-errCh:
@@ -633,7 +629,6 @@ func (rsc *ReplicaSetController) manageReplicas(filteredPods []*v1.Pod, rs *apps
 			if err != nil {
 				return err
 			}
-		default:
 		}
 	}
 
