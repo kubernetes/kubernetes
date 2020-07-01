f66513d98a7759ece08b67e0c173f30a96b5591e
Lubomir I. Ivanov <lubomirivanov@vmware.com>
2020-07-01 22:17:46 +0300

    kubeadm: add --port=0 for kube-controller-manager and kube-scheduler
    Kubeadm setup of kube-controller-manager and kube-scheduler is
    lacking the --port=0 option which caused the component to enable
    the insecure port by default and serve insecurely on the default
    node interface.
    
    Add --port=0 by default to both components. Users are still allowed
    the explicitly set the flag (via extraArgs), which allows them
    to override this default kubeadm behavior and enable the insecure port.
    
    NOTE: the flag is deprecated and should be removed from kubeadm manifests
    once it's removed from core.

--- cmd/kubeadm/app/phases/controlplane/manifests.go
+++ cmd/kubeadm/app/phases/controlplane/manifests.go
@@ -322,6 +322,7 @@ func getControllerManagerCommand(cfg *kubeadmapi.ClusterConfiguration) []string 
 	caFile := filepath.Join(cfg.CertificatesDir, kubeadmconstants.CACertName)
 
 	defaultArguments := map[string]string{
+		"port":                             "0",
 		"bind-address":                     "127.0.0.1",
 		"leader-elect":                     "true",
 		"kubeconfig":                       kubeconfigFile,
@@ -392,6 +393,7 @@ func getControllerManagerCommand(cfg *kubeadmapi.ClusterConfiguration) []string 
 func getSchedulerCommand(cfg *kubeadmapi.ClusterConfiguration) []string {
 	kubeconfigFile := filepath.Join(kubeadmconstants.KubernetesDir, kubeadmconstants.SchedulerKubeConfigFileName)
 	defaultArguments := map[string]string{
+		"port":                      "0",
 		"bind-address":              "127.0.0.1",
 		"leader-elect":              "true",
 		"kubeconfig":                kubeconfigFile,
