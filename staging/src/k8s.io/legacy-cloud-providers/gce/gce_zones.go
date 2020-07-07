a7c3c9f0d504ad2f2a74fdd380ef3ac63fd7d8fe
Jing Xu <jinxu@google.com>
2020-07-07 12:15:11 -0700

    Fix ListZonesInRegion() after client BasePath change
    This path fixes region Regex for listing zones.
    Compute client BasePath changed to compute.googleapis.com, but resource URI were left as www.googleapis.com

--- staging/src/k8s.io/legacy-cloud-providers/gce/gce_zones.go
+++ staging/src/k8s.io/legacy-cloud-providers/gce/gce_zones.go
@@ -20,6 +20,7 @@ package gce
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	compute "google.golang.org/api/compute/v1"
@@ -79,7 +80,9 @@ func (g *Cloud) ListZonesInRegion(region string) ([]*compute.Zone, error) {
 	defer cancel()
 
 	mc := newZonesMetricContext("list", region)
-	list, err := g.c.Zones().List(ctx, filter.Regexp("region", g.getRegionLink(region)))
+	// Use regex match instead of an exact regional link constructed from getRegionalLink below.
+	// See comments in issue kubernetes/kubernetes#87905
+	list, err := g.c.Zones().List(ctx, filter.Regexp("region", fmt.Sprintf(".*/regions/%s", region)))
 	if err != nil {
 		return nil, mc.Observe(err)
 	}
