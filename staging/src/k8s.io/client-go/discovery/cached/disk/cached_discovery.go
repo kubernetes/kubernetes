6c6cd2deeb03040d458b00824fd6c1a900a81b51
Maciej Szulik <maszulik@redhat.com>
2020-07-08 15:43:25 +0200

    Setup cache dirs properly based on --cache-dir value

--- staging/src/k8s.io/client-go/discovery/cached/disk/cached_discovery.go
+++ staging/src/k8s.io/client-go/discovery/cached/disk/cached_discovery.go
@@ -268,8 +268,6 @@ func (d *CachedDiscoveryClient) Invalidate() {
 // CachedDiscoveryClient cache data. If httpCacheDir is empty, the restconfig's transport will not
 // be updated with a roundtripper that understands cache responses.
 // If discoveryCacheDir is empty, cached server resource data will be looked up in the current directory.
-// TODO(juanvallejo): the value of "--cache-dir" should be honored. Consolidate discoveryCacheDir with httpCacheDir
-// so that server resources and http-cache data are stored in the same location, provided via config flags.
 func NewCachedDiscoveryClientForConfig(config *restclient.Config, discoveryCacheDir, httpCacheDir string, ttl time.Duration) (*CachedDiscoveryClient, error) {
 	if len(httpCacheDir) > 0 {
 		// update the given restconfig with a custom roundtripper that
