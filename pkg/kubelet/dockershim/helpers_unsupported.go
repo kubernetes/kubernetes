aea228f5dd3ad928dcb4c932fce8a80a74539d7f
wawa0210 <xiaozhang0210@hotmail.com>
2020-07-15 01:27:22 +0800

    fix no-new-privileges on windows

--- pkg/kubelet/dockershim/helpers_unsupported.go
+++ pkg/kubelet/dockershim/helpers_unsupported.go
@@ -36,6 +36,11 @@ func (ds *dockerService) getSecurityOpts(seccompProfile string, separator rune) 
 	return nil, nil
 }
 
+func (ds *dockerService) getSandBoxSecurityOpts(separator rune) []string {
+	klog.Warningf("getSandBoxSecurityOpts is unsupported in this build")
+	return nil
+}
+
 func (ds *dockerService) updateCreateConfig(
 	createConfig *dockertypes.ContainerCreateConfig,
 	config *runtimeapi.ContainerConfig,
