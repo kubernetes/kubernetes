091adbf817a5c5cec6f44507780fac100e238812
lixiaobing1 <li.xiaobing1@zte.com.cn>
2020-07-15 18:14:10 +0800

    fix HandleCrash() order

--- staging/src/k8s.io/apimachinery/pkg/watch/streamwatcher.go
+++ staging/src/k8s.io/apimachinery/pkg/watch/streamwatcher.go
@@ -97,9 +97,9 @@ func (sw *StreamWatcher) stopping() bool {
 
 // receive reads result from the decoder in a loop and sends down the result channel.
 func (sw *StreamWatcher) receive() {
+	defer utilruntime.HandleCrash()
 	defer close(sw.result)
 	defer sw.Stop()
-	defer utilruntime.HandleCrash()
 	for {
 		action, obj, err := sw.source.Decode()
 		if err != nil {
