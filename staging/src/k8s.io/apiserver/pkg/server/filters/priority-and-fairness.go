842cd8e315566388025f509edf563b20022922f6
Zhou Peng <p@ctriple.cn>
2020-07-20 17:06:01 +0800

    [k8s.io/apiserver/pkg/server/filters]: fixup go vet warning
    Signed-off-by: Zhou Peng <p@ctriple.cn>

--- staging/src/k8s.io/apiserver/pkg/server/filters/priority-and-fairness.go
+++ staging/src/k8s.io/apiserver/pkg/server/filters/priority-and-fairness.go
@@ -135,7 +135,7 @@ func WithPriorityAndFairness(
 			w.Header().Set(responseHeaderMatchedFlowSchemaUID, string(classification.FlowSchemaUID))
 			handler.ServeHTTP(w, innerReq)
 		}
-		digest := utilflowcontrol.RequestDigest{requestInfo, user}
+		digest := utilflowcontrol.RequestDigest{RequestInfo: requestInfo, User: user}
 		fcIfc.Handle(ctx, digest, note, func(inQueue bool) {
 			if inQueue {
 				noteWaitingDelta(1)
