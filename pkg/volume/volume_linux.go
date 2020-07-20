c6e069ad92e68ed13930f1fa582c146e18181a4c
José Guilherme Vanz <jguilhermevanz@suse.com>
2020-07-20 10:41:53 -0300

    cleanup: print warning message after timeout
    If a pod has a configmap/secret volume an annoying message shows up
    in the log approximately every 70 seconds. This happens because the
    desiredStateOfWorldPopulator sync loop always call the
    MarkRemountRequired. The function finds the volume plugin and check
    if the plugin requires mount. Configmap and secret plugins always
    returns true for that. Thus, the reconciler code of the volume manager
    remounts the volume every time. This commit change the SetVolumeOwnership
    to print the warning only if the function does not finish within 30
    seconds.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

--- pkg/volume/volume_linux.go
+++ pkg/volume/volume_linux.go
@@ -23,6 +23,7 @@ import (
 	"syscall"
 
 	"os"
+	"time"
 
 	"k8s.io/klog"
 )
@@ -42,7 +43,10 @@ func SetVolumeOwnership(mounter Mounter, fsGroup *int64) error {
 		return nil
 	}
 
-	klog.Warningf("Setting volume ownership for %s and fsGroup set. If the volume has a lot of files then setting volume ownership could be slow, see https://github.com/kubernetes/kubernetes/issues/69699", mounter.GetPath())
+	timer := time.AfterFunc(30*time.Second, func() {
+		klog.Warningf("Setting volume ownership for %s and fsGroup set. If the volume has a lot of files then setting volume ownership could be slow, see https://github.com/kubernetes/kubernetes/issues/69699", mounter.GetPath())
+	})
+	defer timer.Stop()
 
 	return filepath.Walk(mounter.GetPath(), func(path string, info os.FileInfo, err error) error {
 		if err != nil {
