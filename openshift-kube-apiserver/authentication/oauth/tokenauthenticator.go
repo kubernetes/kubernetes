a2b1c6ee561d29654f788f7b73a5f78283b92f48
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2020-08-05 13:53:22 +0200

    UPSTREAM: <carry>: oauth-token-authn: switch to sha256~ prefix

--- openshift-kube-apiserver/authentication/oauth/tokenauthenticator.go
+++ openshift-kube-apiserver/authentication/oauth/tokenauthenticator.go
@@ -37,7 +37,7 @@ func NewTokenAuthenticator(tokens oauthclient.OAuthAccessTokenInterface, users u
 	}
 }
 
-const sha256Prefix = "sha256:"
+const sha256Prefix = "sha256~"
 
 func (a *tokenAuthenticator) AuthenticateToken(ctx context.Context, name string) (*kauthenticator.Response, bool, error) {
 	// hash token for new-style sha256: prefixed token
