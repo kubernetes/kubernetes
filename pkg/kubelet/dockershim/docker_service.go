b605a78881a6440029d8f5ef6ac71d056aa4cb6d
knight42 <anonymousknight96@gmail.com>
2020-08-07 16:54:38 +0800

    fix(kubelet): protect `containerCleanupInfos` from concurrent map writes
    Signed-off-by: knight42 <anonymousknight96@gmail.com>

--- pkg/kubelet/dockershim/docker_service.go
+++ pkg/kubelet/dockershim/docker_service.go
@@ -31,7 +31,7 @@ import (
 	dockertypes "github.com/docker/docker/api/types"
 	"k8s.io/klog/v2"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	runtimeapi "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 	kubeletconfig "k8s.io/kubernetes/pkg/kubelet/apis/config"
 	"k8s.io/kubernetes/pkg/kubelet/checkpointmanager"
@@ -316,6 +316,7 @@ type dockerService struct {
 	// (see `applyPlatformSpecificDockerConfig` and `performPlatformSpecificContainerCleanup`
 	// methods for more info).
 	containerCleanupInfos map[string]*containerCleanupInfo
+	cleanupInfosLock      sync.RWMutex
 }
 
 // TODO: handle context.
