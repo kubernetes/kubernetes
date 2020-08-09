084da78b952634e0edade1bd363cd2ae714a9059
alam0rt <samlockart@gmail.com>
2020-08-09 12:16:25 +1000

    make function more readable

--- pkg/scheduler/framework/plugins/nodeports/node_ports.go
+++ pkg/scheduler/framework/plugins/nodeports/node_ports.go
@@ -61,10 +61,10 @@ func (pl *NodePorts) Name() string {
 func getContainerPorts(pods ...*v1.Pod) []*v1.ContainerPort {
 	ports := []*v1.ContainerPort{}
 	for _, pod := range pods {
-		for j := range pod.Spec.Containers {
-			container := &pod.Spec.Containers[j]
-			for k := range container.Ports {
-				ports = append(ports, &container.Ports[k])
+		for _, container := range pod.Spec.Containers {
+			for i := range container.Ports {
+				port := &container.Ports[i]
+				ports = append(ports, port)
 			}
 		}
 	}
