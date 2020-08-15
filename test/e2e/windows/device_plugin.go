7258cecab5aa4a2d9aedcf8986999636b297235d
Kerstin08 <kerstin_hofer90@gmx.at>
2020-08-14 20:23:20 -0400

    Correct spacing, which was missed in the last commit.

--- test/e2e/windows/device_plugin.go
+++ test/e2e/windows/device_plugin.go
@@ -116,7 +116,7 @@ var _ = SIGDescribe("Services", func() {
 
 		ginkgo.By("verifying device access in Windows testing Pod")
 		command := []string{"cmd.exe", "dxdiag", "/t", "dxdiag_output.txt", "&", "type", "dxdiag_output.txt"}
-                expectedString  := "Todo: DirectX Version: DirectX 12"
+		expectedString  := "Todo: DirectX Version: DirectX 12"
 		_, err = framework.LookForStringInPodExec(ns, windowsPod.Name, command, expectedString, time.Minute)
 		framework.ExpectNoError(err, "failed: didn't find expected string in dxdiag output.")
 	})
