2be66feb4943acafc755350077168f18cec8b08f
Hu Shuai <hus.fnst@cn.fujitsu.com>
2020-08-31 15:27:20 +0800

    Improve the prompt message when kubeadm init is successful
    Signed-off-by: Hu Shuai <hus.fnst@cn.fujitsu.com>

--- cmd/kubeadm/app/cmd/init.go
+++ cmd/kubeadm/app/cmd/init.go
@@ -56,6 +56,10 @@ var (
 		  sudo cp -i {{.KubeConfigPath}} $HOME/.kube/config
 		  sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+		Alternatively, if you are the root user, you can run:
+
+		  export KUBECONFIG=/etc/kubernetes/admin.conf
+
 		You should now deploy a pod network to the cluster.
 		Run "kubectl apply -f [podnetwork].yaml" with one of the options listed at:
 		  https://kubernetes.io/docs/concepts/cluster-administration/addons/
