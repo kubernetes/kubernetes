e1e022f54841f897c4afb3ef00b9b13780543e63
Mike Spreitzer <mspreitz@us.ibm.com>
2020-09-10 01:17:01 -0400

    Start drafting SamplingHistogram
    This covers SamplingHistogram but not SamplingHistogramVec (because
    the vec support of prometheus is not public).

---
+++ staging/src/k8s.io/component-base/metrics/prometheus_extension/sampling-histogram.go
@@ -0,0 +1,120 @@
+/*
+Copyright 2020 Mike Spreitzer.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package prometheus_extension
+
+import (
+	"sync"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+	"k8s.io/utils/clock"
+)
+
+// A SamplingHistogram samples the values of a float64 variable at a
+// configured rate.  The samples contribute to a Histogram.
+type SamplingHistogram interface {
+	prometheus.Metric
+	prometheus.Collector
+
+	// Sets the variable to the given value.
+	Set(float64)
+}
+
+type SamplingHistogramOpts struct {
+	prometheus.HistogramOpts
+
+	// The initial value of the variable
+	InitialValue float64
+
+	// The variable is sampled once every this often
+	SamplingPeriod time.Duration
+}
+
+func NewSamplingHistogram(opts SamplingHistogramOpts) SamplingHistogram {
+	return NewTestableSamplingHistogram(clock.RealClock{}, opts)
+}
+
+func NewTestableSamplingHistogram(clock clock.Clock, opts SamplingHistogramOpts) SamplingHistogram {
+	desc := prometheus.NewDesc(
+		prometheus.BuildFQName(opts.Namespace, opts.Subsystem, opts.Name),
+		opts.Help,
+		nil,
+		opts.ConstLabels,
+	)
+	return newSamplingHistogram(clock, desc, opts)
+}
+
+func newSamplingHistogram(clock clock.Clock, desc *prometheus.Desc, opts SamplingHistogramOpts, labelValues ...string) SamplingHistogram {
+	return &samplingHistogram{
+		samplingPeriod:  opts.SamplingPeriod,
+		histogram:       prometheus.NewHistogram(opts.HistogramOpts),
+		clock:           clock,
+		lastSampleIndex: clock.Now().UnixNano() / int64(opts.SamplingPeriod),
+		value:           opts.InitialValue,
+	}
+}
+
+type samplingHistogram struct {
+	samplingPeriod time.Duration
+	histogram      prometheus.Histogram
+	clock          clock.Clock
+	lock           sync.Mutex
+
+	// identifies the last sampling period completed
+	lastSampleIndex int64
+	value           float64
+}
+
+var _ SamplingHistogram = &samplingHistogram{}
+
+func (sh *samplingHistogram) Set(newValue float64) {
+	sh.Update(func(float64) float64 { return newValue })
+}
+
+func (sh *samplingHistogram) Update(updateFn func(float64) float64) {
+	oldValue, numSamples := func() (float64, int64) {
+		sh.lock.Lock()
+		defer sh.lock.Unlock()
+		newSampleIndex := sh.clock.Now().UnixNano() / int64(sh.samplingPeriod)
+		deltaIndex := newSampleIndex - sh.lastSampleIndex
+		sh.lastSampleIndex = newSampleIndex
+		oldValue := sh.value
+		sh.value = updateFn(sh.value)
+		return oldValue, deltaIndex
+	}()
+	for i := int64(0); i < numSamples; i++ {
+		sh.histogram.Observe(oldValue)
+	}
+}
+
+func (sh *samplingHistogram) Desc() *prometheus.Desc {
+	return sh.histogram.Desc()
+}
+
+func (sh *samplingHistogram) Write(dest *dto.Metric) error {
+	return sh.histogram.Write(dest)
+}
+
+func (sh *samplingHistogram) Describe(ch chan<- *prometheus.Desc) {
+	sh.histogram.Describe(ch)
+}
+
+func (sh *samplingHistogram) Collect(ch chan<- prometheus.Metric) {
+	sh.Update(func(value float64) float64 { return value })
+	sh.histogram.Collect(ch)
+}
---
+++ staging/src/k8s.io/component-base/metrics/prometheus_extension/sampling-histogram_test.go
@@ -0,0 +1,120 @@
+/*
+Copyright 2020 Mike Spreitzer.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package prometheus_extension
+
+import (
+	"testing"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+	testclock "k8s.io/utils/clock/testing"
+)
+
+func TestSamplingHistogram(t *testing.T) {
+	clk := testclock.NewFakeClock(time.Unix(time.Now().Unix(), 999999990))
+	sh := NewTestableSamplingHistogram(clk, SamplingHistogramOpts{
+		HistogramOpts: prometheus.HistogramOpts{
+			Namespace:   "test",
+			Subsystem:   "func",
+			Name:        "one",
+			Help:        "a helpful string",
+			ConstLabels: map[string]string{"l1": "v1", "l2": "v2"},
+			Buckets:     []float64{0, 1, 2},
+		},
+		InitialValue:   1,
+		SamplingPeriod: time.Second,
+	})
+	expectHistogram(t, "After creation", sh, 0, 0, 0, 0)
+	sh.Set(2)
+	expectHistogram(t, "After initial Set", sh, 0, 0, 0, 0)
+	clk.Step(9 * time.Nanosecond)
+	expectHistogram(t, "Just before the end of the first sampling period", sh, 0, 0, 0, 0)
+	clk.Step(1 * time.Nanosecond)
+	expectHistogram(t, "At the end of the first sampling period", sh, 0, 0, 1, 1)
+	clk.Step(1 * time.Nanosecond)
+	expectHistogram(t, "Barely into second sampling period", sh, 0, 0, 1, 1)
+	sh.Set(-0.5)
+	sh.Set(0.5)
+	clk.Step(999999998 * time.Nanosecond)
+	expectHistogram(t, "Just before the end of second sampling period", sh, 0, 0, 1, 1)
+	clk.Step(1 * time.Nanosecond)
+	expectHistogram(t, "At the end of second sampling period", sh, 0, 1, 2, 2)
+}
+
+func expectHistogram(t *testing.T, when string, sh SamplingHistogram, buckets ...uint64) {
+	metrics := make(chan prometheus.Metric, 2)
+	sh.Collect(metrics)
+	var dtom dto.Metric
+	select {
+	case metric := <-metrics:
+		metric.Write(&dtom)
+	default:
+		t.Errorf("%s, zero Metrics collected", when)
+	}
+	select {
+	case metric := <-metrics:
+		t.Errorf("%s, collected more than one Metric; second Metric = %#+v", when, metric)
+	default:
+	}
+	missingLabels := map[string]string{"l1": "v1", "l2": "v2"}
+	for _, lp := range dtom.Label {
+		if lp == nil || lp.Name == nil || lp.Value == nil {
+			continue
+		}
+		if val, ok := missingLabels[*(lp.Name)]; ok {
+			if val != *(lp.Value) {
+				t.Errorf("%s, found label named %q with value %q instead of %q", when, *(lp.Name), *(lp.Value), val)
+			}
+		} else {
+			t.Errorf("%s, got unexpected label name %q", when, *(lp.Name))
+		}
+		delete(missingLabels, *(lp.Name))
+	}
+	if len(missingLabels) > 0 {
+		t.Errorf("%s, missed labels %#+v", when, missingLabels)
+	}
+	if dtom.Histogram == nil {
+		t.Errorf("%s, Collect returned a Metric without a Histogram: %#+v", when, dtom)
+	}
+	mh := dtom.Histogram
+	if len(mh.Bucket) != len(buckets)-1 {
+		t.Errorf("%s, expected %d buckets but got %d: %#+v", when, len(buckets)-1, len(mh.Bucket), mh.Bucket)
+	}
+	if mh.SampleCount == nil {
+		t.Errorf("%s, got Histogram with nil SampleCount", when)
+	}
+	if *(mh.SampleCount) != buckets[len(mh.Bucket)] {
+		t.Errorf("%s, SampleCount=%d but expected %d", when, *(mh.SampleCount), buckets[len(mh.Bucket)])
+	}
+	for i, mhBucket := range mh.Bucket {
+		if mhBucket == nil {
+			t.Errorf("%s, bucket %d was nil", when, i)
+		}
+		if mhBucket.UpperBound == nil || mhBucket.CumulativeCount == nil {
+			t.Errorf("%s, bucket %d had nil bound or count", when, i)
+		}
+		ub := float64(i)
+		if ub != *(mhBucket.UpperBound) {
+			t.Errorf("%s, bucket %d's upper bound was %v", when, i, *(mhBucket.UpperBound))
+		}
+		expectedCount := buckets[i]
+		if expectedCount != *(mhBucket.CumulativeCount) {
+			t.Errorf("%s, bucket %d's count was %d rather than %d", when, i, mhBucket.CumulativeCount, expectedCount)
+		}
+	}
+}
