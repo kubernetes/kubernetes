168873b30af663be5b82df0e6e08318d933c4af6
Jan Chaloupka <jchaloup@redhat.com>
2020-09-10 16:09:13 +0200

    UPSTREAM: revert: 94423: debugger scheduler plugin

--- pkg/scheduler/algorithmprovider/registry_test.go
+++ pkg/scheduler/algorithmprovider/registry_test.go
@@ -27,7 +27,6 @@ import (
 	featuregatetesting "k8s.io/component-base/featuregate/testing"
 	"k8s.io/kubernetes/pkg/features"
 	schedulerapi "k8s.io/kubernetes/pkg/scheduler/apis/config"
-	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/debugger"
 	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/defaultbinder"
 	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/imagelocality"
 	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/interpodaffinity"
@@ -61,7 +60,6 @@ func TestClusterAutoscalerProvider(t *testing.T) {
 				{Name: podtopologyspread.Name},
 				{Name: interpodaffinity.Name},
 				{Name: volumebinding.Name},
-				{Name: debugger.Name},
 			},
 		},
 		Filter: &schedulerapi.PluginSet{
@@ -154,7 +152,6 @@ func TestApplyFeatureGates(t *testing.T) {
 						{Name: podtopologyspread.Name},
 						{Name: interpodaffinity.Name},
 						{Name: volumebinding.Name},
-						{Name: debugger.Name},
 					},
 				},
 				Filter: &schedulerapi.PluginSet{
@@ -235,7 +232,6 @@ func TestApplyFeatureGates(t *testing.T) {
 						{Name: podtopologyspread.Name},
 						{Name: interpodaffinity.Name},
 						{Name: volumebinding.Name},
-						{Name: debugger.Name},
 					},
 				},
 				Filter: &schedulerapi.PluginSet{
