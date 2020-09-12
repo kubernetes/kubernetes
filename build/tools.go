0e0bcd9c4f3fa1c7166f65cb1f084cfa6a9d1185
Mike Spreitzer <mspreitz@us.ibm.com>
2020-09-11 22:03:55 -0400

    Add hack to get k8s.io/utils/clock/testing in vendor/
    As per https://github.com/kubernetes/kubernetes/pull/84205/#issuecomment-660362689

--- build/tools.go
+++ build/tools.go
@@ -29,4 +29,5 @@ import (
 	_ "k8s.io/gengo/examples/import-boss/generators"
 	_ "k8s.io/gengo/examples/set-gen/generators"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
+	_ "k8s.io/utils/clock/testing"
 )
