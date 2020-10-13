49dd22a5f14a0b0c4e05277bf34c80c79ead7359
Shihang Zhang <zshihang@google.com>
2020-10-13 14:40:21 -0700

    update NPD hash to sha512
    BUG: 153659289
    
    Change-Id: I32331541909cc15a66d35c06cdb690522655575a

--- cluster/gce/gci/gke-internal-configure.sh
+++ cluster/gce/gci/gke-internal-configure.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 NODE_PROBLEM_DETECTOR_VERSION="${NODE_PROBLEM_DETECTOR_VERSION:-v0.8.1}"
-NODE_PROBLEM_DETECTOR_TAR_HASH="${NODE_PROBLEM_DETECTOR_TAR_HASH:-0f25126c5c85bc66e5a066e92e12958c93459799}"
+NODE_PROBLEM_DETECTOR_TAR_HASH="${NODE_PROBLEM_DETECTOR_TAR_HASH:-40a742dfe1f967bb37ae14516ef11ee630874b89e3b1f6ae236dd467db663780d59bc1dddfc7ca7540434dc16b67a19390964b702aaf7c11dadc67da4f6559e0}"
 NODE_PROBLEM_DETECTOR_RELEASE_PATH="${NODE_PROBLEM_DETECTOR_RELEASE_PATH:-https://storage.googleapis.com/kubernetes-release}"
 
 NPD_CUSTOM_PLUGINS_VERSION="${NPD_CUSTOM_PLUGINS_VERSION:-v1.0.1}"
-NPD_CUSTOM_PLUGINS_TAR_HASH="${NPD_CUSTOM_PLUGINS_TAR_HASH:-910097d0cffaabcc35c2738c05cc31ddcbbc87e9}"
+NPD_CUSTOM_PLUGINS_TAR_HASH="${NPD_CUSTOM_PLUGINS_TAR_HASH:-6d3cdcced8cab4c6631b32bff2241e81a86949163dde58c8464d3cfb1fa8508c5595ec18f36fc636f5db4bf5458bfc8668d850eb68298e3bc39f83e6355d2c83}"
 NPD_CUSTOM_PLUGINS_RELEASE_PATH="${NPD_CUSTOM_PLUGINS_RELEASE_PATH:-https://storage.googleapis.com/gke-release}"
 
 # Install node problem detector custom plugins.
 function install-npd-custom-plugins {
   local -r version="${NPD_CUSTOM_PLUGINS_VERSION}"
-  local -r sha1="${NPD_CUSTOM_PLUGINS_TAR_HASH}"
+  local -r hash="${NPD_CUSTOM_PLUGINS_TAR_HASH}"
   local -r release_path="${NPD_CUSTOM_PLUGINS_RELEASE_PATH}"
   local -r tar="npd-custom-plugins-${version}.tar.gz"
 
   echo "Downloading ${tar}."
-  download-or-bust "${sha1}" "${release_path}/npd-custom-plugins/${version}/${tar}"
+  download-or-bust "${hash}" "${release_path}/npd-custom-plugins/${version}/${tar}"
   local -r dir="${KUBE_HOME}/npd-custom-plugins"
   mkdir -p "${dir}"
   tar xzf "${KUBE_HOME}/${tar}" -C "${dir}" --overwrite
