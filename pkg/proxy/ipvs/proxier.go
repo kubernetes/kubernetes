9ad39c9edafabbcdbea2ee5a6ce75bda9b5b9f65
qingsenLi <li.qingsen5@zte.com.cn>
2020-10-22 17:26:29 +0800

    format incorrectAddresses in klog

--- pkg/proxy/ipvs/proxier.go
+++ pkg/proxy/ipvs/proxier.go
@@ -448,7 +448,7 @@ func NewProxier(ipt utiliptables.Interface,
 	var incorrectAddresses []string
 	nodePortAddresses, incorrectAddresses = utilproxy.FilterIncorrectCIDRVersion(nodePortAddresses, isIPv6)
 	if len(incorrectAddresses) > 0 {
-		klog.Warning("NodePortAddresses of wrong family; ", incorrectAddresses)
+		klog.Warningf("NodePortAddresses of wrong family; %s", incorrectAddresses)
 	}
 	proxier := &Proxier{
 		portsMap:              make(map[utilproxy.LocalPort]utilproxy.Closeable),
