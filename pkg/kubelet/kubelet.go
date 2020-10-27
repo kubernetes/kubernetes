1c70b91df81deb675cfee0ed3a451e929ca5819a
Sergey Kanzhelev <S.Kanzhelev@live.com>
2020-10-27 07:02:44 +0000

    follow up for #94109

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -2151,7 +2151,6 @@ func (kl *Kubelet) updateRuntimeUp() {
 		return
 	}
 	// Periodically log the whole runtime status for debugging.
-	// condition is unmet.
 	klog.V(4).Infof("Container runtime status: %v", s)
 	networkReady := s.GetRuntimeCondition(kubecontainer.NetworkReady)
 	if networkReady == nil || !networkReady.Status {
