7b9aa03e49181efb99674ce0787776c2636bdb38
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2020-10-29 13:55:58 +0100

    UPSTREAM: <carry>: create termination events
    Origin-commit: a869af0c97e3d97bddedcd76af8a62da6c879c02
    
    UPSTREAM: <carry>: apiserver: log new connections during termination
    
    Origin-commit: 89d1c3ceeb91755aae9099cd5f76c42a22de18c5
    
    UPSTREAM: <carry>: apiserver: create LateConnections events on events in the last 20% of graceful termination time
    
    Origin-commit: 91bc33b6ddf9e1d80906717db5bd9096183e8795
    
    UPSTREAM: <carry>: apiserver: log source in LateConnections event
    
    Origin-commit: 575e54740eb7c2ba635c73f24c22ad77cb5a6e70
    
    UPSTREAM: <carry>: apiserver: skip local IPs and probes for LateConnections
    
    Origin-commit: 2109b95866e81b84a290f34f0806becc2cbd83e9
    
    UPSTREAM: <carry>: only create valid LateConnections/GracefulTermination events
    
    UPSTREAM: <carry>: kube-apiserver: log non-probe requests before ready
    
    UPSTREAM: <carry>: apiserver: create hasBeenReadyCh channel
    
    UPSTREAM: <carry>: kube-apiserver: log non-probe requests before ready
    
    UPSTREAM: <carry>: kube-apiserver: log non-probe requests before ready
    
    UPSTREAM: <carry>: fix termination event(s) validation failures
    
    UPSTREAM: <carry>: during the rebase collapse to create termination event
    
    it makes recording termination events a non-blocking operation.
    previously closing delayedStopCh might have been delayed on preserving data in the storage.
    the delayedStopCh is important as it signals the HTTP server to start the shutdown procedure.
    
    it also sets a hard timeout of 3 seconds for the storage layer since we are bypassing the API layer.

--- cmd/kube-apiserver/app/server.go
+++ cmd/kube-apiserver/app/server.go
@@ -20,6 +20,7 @@ limitations under the License.
 package app
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"net"
@@ -36,7 +37,9 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 
+	corev1 "k8s.io/api/core/v1"
 	extensionsapiserver "k8s.io/apiextensions-apiserver/pkg/apiserver"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	utilnet "k8s.io/apimachinery/pkg/util/net"
 	"k8s.io/apimachinery/pkg/util/sets"
@@ -44,6 +47,7 @@ import (
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 	genericapifilters "k8s.io/apiserver/pkg/endpoints/filters"
 	openapinamer "k8s.io/apiserver/pkg/endpoints/openapi"
+	"k8s.io/apiserver/pkg/endpoints/request"
 	genericfeatures "k8s.io/apiserver/pkg/features"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	"k8s.io/apiserver/pkg/server/egressselector"
@@ -73,6 +77,8 @@ import (
 
 	"k8s.io/kubernetes/cmd/kube-apiserver/app/options"
 	"k8s.io/kubernetes/pkg/api/legacyscheme"
+	"k8s.io/kubernetes/pkg/apis/core"
+	v1 "k8s.io/kubernetes/pkg/apis/core/v1"
 	"k8s.io/kubernetes/pkg/capabilities"
 	"k8s.io/kubernetes/pkg/controlplane"
 	"k8s.io/kubernetes/pkg/controlplane/reconcilers"
@@ -81,6 +87,7 @@ import (
 	kubeapiserveradmission "k8s.io/kubernetes/pkg/kubeapiserver/admission"
 	kubeauthenticator "k8s.io/kubernetes/pkg/kubeapiserver/authenticator"
 	"k8s.io/kubernetes/pkg/kubeapiserver/authorizer/modes"
+	eventstorage "k8s.io/kubernetes/pkg/registry/core/event/storage"
 	rbacrest "k8s.io/kubernetes/pkg/registry/rbac/rest"
 	"k8s.io/kubernetes/pkg/serviceaccount"
 )
@@ -305,6 +312,13 @@ func CreateKubeAPIServerConfig(s completedServerRunOptions, stopCh <-chan struct
 
 	s.Metrics.Apply()
 	serviceaccount.RegisterMetrics()
+
+	var eventStorage *eventstorage.REST
+	eventStorage, err = eventstorage.NewREST(genericConfig.RESTOptionsGetter, uint64(s.EventTTL.Seconds()))
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	genericConfig.EventSink = eventRegistrySink{eventStorage}
 
 	config := &controlplane.Config{
 		GenericConfig: genericConfig,
@@ -754,4 +768,39 @@ func getServiceIPAndRanges(serviceClusterIPRanges string) (net.IP, net.IPNet, ne
 		secondaryServiceIPRange = *secondaryServiceClusterCIDR
 	}
 	return apiServerServiceIP, primaryServiceIPRange, secondaryServiceIPRange, nil
+}
+
+// eventRegistrySink wraps an event registry in order to be used as direct event sync, without going through the API.
+type eventRegistrySink struct {
+	*eventstorage.REST
+}
+
+var _ genericapiserver.EventSink = eventRegistrySink{}
+
+func (s eventRegistrySink) Create(v1event *corev1.Event) (*corev1.Event, error) {
+	ctx := request.WithNamespace(request.WithRequestInfo(request.NewContext(), &request.RequestInfo{APIVersion: "v1"}), v1event.Namespace)
+	// since we are bypassing the API set a hard timeout for the storage layer
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	var event core.Event
+	if err := v1.Convert_v1_Event_To_core_Event(v1event, &event, nil); err != nil {
+		return nil, err
+	}
+
+	obj, err := s.REST.Create(ctx, &event, nil, &metav1.CreateOptions{})
+	if err != nil {
+		return nil, err
+	}
+	ret, ok := obj.(*core.Event)
+	if !ok {
+		return nil, fmt.Errorf("expected corev1.Event, got %T", obj)
+	}
+
+	var v1ret corev1.Event
+	if err := v1.Convert_core_Event_To_v1_Event(ret, &v1ret, nil); err != nil {
+		return nil, err
+	}
+
+	return &v1ret, nil
 }
