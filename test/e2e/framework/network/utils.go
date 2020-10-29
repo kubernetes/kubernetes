a80de673396af87423c049b542211af698f73454
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2020-10-29 13:55:59 +0100

    UPSTREAM: <carry>: Remove excessive e2e logging
    UPSTREAM: <carry>: Remove a redundant output in the tests
    
    This line is not necessary for our test usage and should not be an
    issue in OpenShift (openshift-tests already verifies this correctly).
    
    UPSTREAM: <carry>: Remove excessive logging during e2e upgrade test
    
    This line makes the upgrade log output unreadable and provides
    no value during the set of tests it's used in:
    
    ```
    Jan 12 20:49:25.628: INFO: cluster upgrade is Progressing: Working towards registry.svc.ci.openshift.org/ci-op-jbtg7jjb/release@sha256:144e73d125cce620bdf099be9a85225ade489a95622a70075d264ea3ff79219c: downloading update
    Jan 12 20:49:26.692: INFO: Poke("http://a74e3476115ce4d2d817a1e5ea608dad-802917831.us-east-1.elb.amazonaws.com:80/echo?msg=hello"): success
    Jan 12 20:49:28.727: INFO: Poke("http://a74e3476115ce4d2d817a1e5ea608dad-802917831.us-east-1.elb.amazonaws.com:80/echo?msg=hello"): success
    ```
    
    Origin-commit: 1cdf04c0e15b79fad3e3a6ba896ed2bb3df42b78

--- test/e2e/framework/network/utils.go
+++ test/e2e/framework/network/utils.go
@@ -985,8 +985,6 @@ func PokeHTTP(host string, port int, path string, params *HTTPPokeParams) HTTPPo
 		params.Timeout = 10 * time.Second
 	}
 
-	framework.Logf("Poking %q", url)
-
 	resp, err := httpGetNoConnectionPoolTimeout(url, params.Timeout)
 	if err != nil {
 		ret.Error = err
@@ -1038,7 +1036,8 @@ func PokeHTTP(host string, port int, path string, params *HTTPPokeParams) HTTPPo
 	}
 
 	ret.Status = HTTPSuccess
-	framework.Logf("Poke(%q): success", url)
+	// causes excessive logging that provides no value
+	// framework.Logf("Poke(%q): success", url)
 	return ret
 }
 
