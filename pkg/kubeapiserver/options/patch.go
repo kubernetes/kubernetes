bcf574c65d1d26b090163373920816e7015925f3
David Eads <deads@redhat.com>
2020-10-29 13:55:57 +0100

    UPSTREAM: <carry>: kube-apiserver: allow rewiring
    openshift-rebase(v1.24):source=87f75213acc
    
    openshift-rebase(v1.24):source=87f75213acc
    
    openshift-rebase(v1.24):source=87f75213acc

--- pkg/kubeapiserver/options/patch.go
+++
@@ -1,9 +0,0 @@
-package options
-
-import "k8s.io/apiserver/pkg/admission"
-
-var RegisterAllAdmissionPlugins = registerAllAdmissionPlugins
-
-var DefaultOffAdmissionPlugins = defaultOffAdmissionPlugins
-
-var Decorators = []admission.Decorator{}
