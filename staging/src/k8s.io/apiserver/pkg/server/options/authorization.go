bb9c3262208eed28d8bfa6f26a2c19c8982648ba
Michal Fojtik <mfojtik@redhat.com>
2020-10-29 13:55:54 +0100

    UPSTREAM: 74956: apiserver: switch authorization to use protobuf client

--- staging/src/k8s.io/apiserver/pkg/server/options/authorization.go
+++ staging/src/k8s.io/apiserver/pkg/server/options/authorization.go
@@ -239,5 +239,10 @@ func (s *DelegatingAuthorizationOptions) getClient() (kubernetes.Interface, erro
 		clientConfig.Wrap(s.CustomRoundTripperFn)
 	}
 
-	return kubernetes.NewForConfig(clientConfig)
+	// make the client use protobuf
+	protoConfig := rest.CopyConfig(clientConfig)
+	protoConfig.AcceptContentTypes = "application/vnd.kubernetes.protobuf,application/json"
+	protoConfig.ContentType = "application/vnd.kubernetes.protobuf"
+
+	return kubernetes.NewForConfig(protoConfig)
 }
