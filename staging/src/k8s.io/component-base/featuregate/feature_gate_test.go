f67528a15c4aa81e25c2871a398c11a110ab0c1c
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2020-10-29 13:55:58 +0100

    UPSTREAM: <carry>: warn only about unknown feature gates
    OpenShift-Rebase-Source: a1370096b39

--- staging/src/k8s.io/component-base/featuregate/feature_gate_test.go
+++ staging/src/k8s.io/component-base/featuregate/feature_gate_test.go
@@ -56,7 +56,7 @@ func TestFeatureGateFlag(t *testing.T) {
 				testAlphaGate: false,
 				testBetaGate:  false,
 			},
-			parseError: "unrecognized feature gate: fooBarBaz",
+			//parseError: "unrecognized feature gate: fooBarBaz",
 		},
 		{
 			arg: "AllAlpha=false",
@@ -360,7 +360,7 @@ func TestFeatureGateSetFromMap(t *testing.T) {
 				testAlphaGate: false,
 				testBetaGate:  false,
 			},
-			setmapError: "unrecognized feature gate:",
+			//setmapError: "unrecognized feature gate:",
 		},
 		{
 			name: "set locked gates",
