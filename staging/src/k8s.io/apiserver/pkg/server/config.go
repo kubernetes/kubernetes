6d6b285919fd33153648e1eb7c5d789a6346a3c3
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2020-10-29 13:55:57 +0100

    UPSTREAM: <carry>: kube-apiserver: wire through isTerminating into handler chain
    UPSTREAM: <carry>: use lifeCycleSignals for isTerminating
    
    OpenShift-Rebase-Source: a736659ee67

--- staging/src/k8s.io/apiserver/pkg/server/config.go
+++ staging/src/k8s.io/apiserver/pkg/server/config.go
@@ -1053,7 +1053,7 @@ func DefaultBuildHandlerChain(apiHandler http.Handler, c *Config) http.Handler {
 	if c.ShutdownSendRetryAfter {
 		handler = genericfilters.WithRetryAfter(handler, c.lifecycleSignals.NotAcceptingNewRequest.Signaled())
 	}
-	handler = genericfilters.WithHTTPLogging(handler)
+	handler = genericfilters.WithHTTPLogging(handler, c.newIsTerminatingFunc())
 	if c.FeatureGate.Enabled(genericfeatures.APIServerTracing) {
 		handler = genericapifilters.WithTracing(handler, c.TracerProvider)
 	}
