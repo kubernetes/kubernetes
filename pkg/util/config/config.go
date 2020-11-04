d1321d1f1bbcf7fd15a6d3e34dd3a8fc8d889692
Tony <tchang144@gmail.com>
2020-11-04 02:09:23 -0600

    Fix golint errors in pkg/util/config

--- pkg/util/config/config.go
+++ pkg/util/config/config.go
@@ -22,6 +22,8 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
+// Merger is an interface that allows funcs and structs to
+// be able to receive updates and merge.
 type Merger interface {
 	// Invoked when a change from a source is received.  May also function as an incremental
 	// merger if you wish to consume changes incrementally.  Must be reentrant when more than
@@ -32,6 +34,8 @@ type Merger interface {
 // MergeFunc implements the Merger interface
 type MergeFunc func(source string, update interface{}) error
 
+// Merge will allow an update to be merged with a source
+// using MergeFunc f and return its result.
 func (f MergeFunc) Merge(source string, update interface{}) error {
 	return f(source, update)
 }
@@ -94,10 +98,13 @@ type Accessor interface {
 // AccessorFunc implements the Accessor interface.
 type AccessorFunc func() interface{}
 
+// MergedState will call and return the return value of AccessorFunc f().
 func (f AccessorFunc) MergedState() interface{} {
 	return f()
 }
 
+// Listener interface allows for listening of changes and can invoke OnUpdate
+// when a change is made.
 type Listener interface {
 	// OnUpdate is invoked when a change is made to an object.
 	OnUpdate(instance interface{})
@@ -106,10 +113,14 @@ type Listener interface {
 // ListenerFunc receives a representation of the change or object.
 type ListenerFunc func(instance interface{})
 
+// OnUpdate receives a representation of the change or object
+// and invokes ListenerFunc f upon receiving the update.
 func (f ListenerFunc) OnUpdate(instance interface{}) {
 	f(instance)
 }
 
+// Broadcaster is a struct that will contain a mutex
+// and an array of Listener objects.
 type Broadcaster struct {
 	// Listeners for changes and their lock.
 	listenerLock sync.RWMutex
