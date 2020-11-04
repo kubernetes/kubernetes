7340c3498ac23f46fc8b6bff4d5ac664a9c64a3b
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2020-11-04 11:14:44 +0100

    DelegatingAuthenticationOptions: allows for setting a timeout for the TokenReview client that is used by for the webhook authenticator
    Previously no timeout was set. Requests without explicit timeout might potentially hang forever and lead to starvation of the application.
    When no timeout was specified a default one will be applied.

--- cmd/kube-controller-manager/app/options/options_test.go
+++ cmd/kube-controller-manager/app/options/options_test.go
@@ -406,6 +406,7 @@ func TestAddFlags(t *testing.T) {
 		}).WithLoopback(),
 		Authentication: &apiserveroptions.DelegatingAuthenticationOptions{
 			CacheTTL:            10 * time.Second,
+			ClientTimeout:       10 * time.Second,
 			WebhookRetryBackoff: apiserveroptions.DefaultAuthWebhookRetryBackoff(),
 			ClientCert:          apiserveroptions.ClientCertAuthenticationOptions{},
 			RequestHeader: apiserveroptions.RequestHeaderAuthenticationOptions{
