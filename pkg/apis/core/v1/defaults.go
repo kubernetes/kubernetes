24cbc56479215b9fb672560cdea05efa76333b96
Patrik Cyvoct <patrik@ptrk.io>
2020-11-07 09:59:43 +0100

    fix defaulting
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

--- pkg/apis/core/v1/defaults.go
+++ pkg/apis/core/v1/defaults.go
@@ -171,9 +171,9 @@ func SetDefaults_Service(obj *v1.Service) {
 		obj.Spec.Type == v1.ServiceTypeLoadBalancer {
 		ipMode := v1.LoadBalancerIPModeVIP
 
-		for _, ing := range obj.Status.LoadBalancer.Ingress {
-			if ing.IPMode == nil {
-				ing.IPMode = &ipMode
+		for i, ing := range obj.Status.LoadBalancer.Ingress {
+			if ing.IP != "" && ing.IPMode == nil {
+				obj.Status.LoadBalancer.Ingress[i].IPMode = &ipMode
 			}
 		}
 	}
