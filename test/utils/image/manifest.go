0f9e315fb7b27d10144fb40713b045b4e8f04e85
Kerstin08 <kerstin_hofer90@gmx.at>
2020-11-10 13:08:22 -0500

    Add missing MicrosoftRegistry.

--- test/utils/image/manifest.go
+++ test/utils/image/manifest.go
@@ -119,7 +119,7 @@ var (
 	// Preconfigured image configs
 	imageConfigs = initImageConfigs()
 
-        microsoftRegistry = registry.Microsoft
+        microsoftRegistry = registry.MicrosoftRegistry
 )
 
 const (
