971faa4c554ed90f2d5f42ac08e086b8879de369
b3atlesfan <74183294+b3atlesfan@users.noreply.github.com>
2020-11-10 15:31:52 +0100

    Import k8s.io/api/core/v1.

--- test/e2e/windows/device_plugin.go
+++ test/e2e/windows/device_plugin.go
@@ -20,7 +20,7 @@ import (
 	"context"
 	"time"
 
-//	v1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/kubernetes/test/e2e/framework"
 	appsv1 "k8s.io/api/apps/v1"
