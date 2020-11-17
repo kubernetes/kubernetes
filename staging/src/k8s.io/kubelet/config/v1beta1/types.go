b7cfc40debe424837423052ba67f2be9ea633809
Artyom Lukianov <alukiano@redhat.com>
2020-11-18 00:15:35 +0200

    memory manager: update kubelet config API
    Update kubelet config API to use slice of MemoryReservation
    structs instead of slice of maps.
    
    Signed-off-by: Artyom Lukianov <alukiano@redhat.com>

--- staging/src/k8s.io/kubelet/config/v1beta1/types.go
+++ staging/src/k8s.io/kubelet/config/v1beta1/types.go
@@ -838,14 +838,22 @@ type KubeletConfiguration struct {
 	// Default: "10s"
 	// +optional
 	ShutdownGracePeriodCriticalPods metav1.Duration `json:"shutdownGracePeriodCriticalPods,omitempty"`
-	// A comma separated list of bracket-enclosed configurations for memory manager.
-	// Each configuration describes pre-reserved memory for the certain memory type on a specific NUMA node.
-	// The Memory Manager validates whether total amount of pre-reserved memory is identical to reserved-memory by the Node Allocatable feature.
-	// The format is {numa-node=integer, memory-type=string, limit=string}
-	// (e.g. {numa-node=0, type=memory, limit=1Gi}, {numa-node=1, type=memory, limit=1Gi})
+	// ReservedMemory specifies a comma-separated list of memory reservations for NUMA nodes.
+	// The parameter makes sense only in the context of the memory manager feature. The memory manager will not allocate reserved memory for container workloads.
+	// For example, if you have a NUMA0 with 10Gi of memory and the ReservedMemory was specified to reserve 1Gi of memory at NUMA0,
+	// the memory manager will assume that only 9Gi is available for allocation.
+	// You can specify a different amount of NUMA node and memory types.
+	// You can omit this parameter at all, but you should be aware that the amount of reserved memory from all NUMA nodes
+	// should be equal to the amount of memory specified by the node allocatable features(https://kubernetes.io/docs/tasks/administer-cluster/reserve-compute-resources/#node-allocatable).
+	// If at least one node allocatable parameter has a non-zero value, you will need to specify at least one NUMA node.
+	// Also, avoid specifying:
+	// 1. Duplicates, the same NUMA node, and memory type, but with a different value.
+	// 2. zero limits for any memory type.
+	// 3. NUMAs nodes IDs that do not exist under the machine.
+	// 4. memory types except for memory and hugepages-<size>
 	// Default: nil
 	// +optional
-	ReservedMemory []map[string]string `json:"reservedMemory,omitempty"`
+	ReservedMemory []MemoryReservation `json:"reservedMemory,omitempty"`
 }
 
 type KubeletAuthorizationMode string
@@ -925,4 +933,10 @@ type SerializedNodeConfigSource struct {
 	// Source is the source that we are serializing
 	// +optional
 	Source v1.NodeConfigSource `json:"source,omitempty" protobuf:"bytes,1,opt,name=source"`
+}
+
+// MemoryReservation specifies the memory reservation of different types for each NUMA node
+type MemoryReservation struct {
+	NumaNode int32           `json:"numaNode"`
+	Limits   v1.ResourceList `json:"limits"`
 }
