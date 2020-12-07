9f5dedeeefd4b0338514ede2b3fa82f05811b8ca
Ling Samuel <lingsamuelgrace@gmail.com>
2020-12-07 10:45:44 +0800

    api-server add --lease-max-object-count
    Signed-off-by: Ling Samuel <lingsamuelgrace@gmail.com>

--- cmd/kube-apiserver/app/options/options_test.go
+++ cmd/kube-apiserver/app/options/options_test.go
@@ -163,6 +163,7 @@ func TestAddFlags(t *testing.T) {
 				CountMetricPollPeriod: time.Minute,
 				LeaseManagerConfig: etcd3.LeaseManagerConfig{
 					ReuseDurationSeconds: 100,
+					MaxObjectCount:       1000,
 				},
 			},
 			DefaultStorageMediaType: "application/vnd.kubernetes.protobuf",
