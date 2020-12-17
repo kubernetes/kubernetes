aa47c64f10f955b97424450caab7d657841faa77
Linus Arver <linusa@google.com>
2020-12-17 15:52:25 -0800

    change download link in welcome message
    This is extracted out of
    https://gke-internal-review.googlesource.com/c/kubernetes/+/202451
    because it is independent of the BoringCrypto changes (regarding using
    the BoringCrypto-enabled version of the golang compiler).
    
    Original Change-Id: I56c4148b1a0e665ce5c86a945bb35a98bd5ea581
    
    Change-Id: Iba979a4f255de7eedfd1f658341b85e2fe062da0

--- gke/cluster/gce/gci/configure-helper.sh
+++ gke/cluster/gce/gci/configure-helper.sh
@@ -2970,7 +2970,7 @@ You can find documentation for Kubernetes at:
 The source for this release can be found at:
   /home/kubernetes/kubernetes-src.tar.gz
 Or you can download it at:
-  https://storage.googleapis.com/kubernetes-release/release/${version}/kubernetes-src.tar.gz
+  https://storage.googleapis.com/kubernetes-release-gke/release/${version}/kubernetes-src.tar.gz
 
 It is based on the Kubernetes source at:
   https://github.com/kubernetes/kubernetes/tree/${gitref}
