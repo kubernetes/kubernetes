ba0a87015a828d67e0df7ded8ef11e59ebf653ee
jay vyas <jvyas@vmware.com>
2021-01-04 21:59:50 -0500

    set numberOfWorkers for netpol suite to the ideal value (3) that is non-disruptive

--- test/e2e/network/netpol/probe.go
+++ test/e2e/network/netpol/probe.go
@@ -42,7 +42,7 @@ type ProbeJobResults struct {
 
 // ProbePodToPodConnectivity runs a series of probes in kube, and records the results in `testCase.Reachability`
 func ProbePodToPodConnectivity(k8s *kubeManager, model *Model, testCase *TestCase) {
-	numberOfWorkers := 30
+	numberOfWorkers := 3 // See https://github.com/kubernetes/kubernetes/pull/97690
 	allPods := model.AllPods()
 	size := len(allPods) * len(allPods)
 	jobs := make(chan *ProbeJob, size)
