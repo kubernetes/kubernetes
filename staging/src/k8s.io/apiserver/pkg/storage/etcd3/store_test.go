aaa7084a4a548795c987b29dffb48749531b60a4
pacoxu <paco.xu@daocloud.io>
2021-01-07 11:21:50 +0800

    fix static check for vendor/k8s.io/apiserver/pkg/storage/etcd3/

--- staging/src/k8s.io/apiserver/pkg/storage/etcd3/store_test.go
+++ staging/src/k8s.io/apiserver/pkg/storage/etcd3/store_test.go
@@ -2078,7 +2078,6 @@ func Test_decodeContinue(t *testing.T) {
 
 func Test_growSlice(t *testing.T) {
 	type args struct {
-		t               reflect.Type
 		initialCapacity int
 		v               reflect.Value
 		maxCapacity     int
