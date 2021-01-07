f51014673dfe3277cb2301916d0cdb6a211c51f9
qini <pomelonicky@gmail.com>
2021-01-07 10:18:42 +0800

    Use network.Interface.VirtualMachine.ID to get the binded VM

--- staging/src/k8s.io/legacy-cloud-providers/azure/azure_loadbalancer.go
+++ staging/src/k8s.io/legacy-cloud-providers/azure/azure_loadbalancer.go
@@ -1129,6 +1129,10 @@ func (az *Cloud) reconcileLoadBalancer(clusterName string, service *v1.Service, 
 						if err != nil {
 							return nil, err
 						}
+						if nodeName == "" {
+							// VM may under deletion
+							continue
+						}
 						// If a node is not supposed to be included in the LB, it
 						// would not be in the `nodes` slice. We need to check the nodes that
 						// have been added to the LB's backendpool, find the unwanted ones and
