3f6a0bb91f4ea228559b41fd3483e03a1542f973
JunYang <yang.jun22@zte.com.cn>
2021-01-11 12:19:44 +0800

    fix error description of secondaryNodeIP
    Signed-off-by: JunYang <yang.jun22@zte.com.cn>

--- pkg/kubelet/nodestatus/setters.go
+++ pkg/kubelet/nodestatus/setters.go
@@ -87,7 +87,7 @@ func NodeAddress(nodeIPs []net.IP, // typically Kubelet.nodeIPs
 		}
 		if secondaryNodeIPSpecified {
 			if err := validateNodeIPFunc(secondaryNodeIP); err != nil {
-				return fmt.Errorf("failed to validate secondaryNodeIP: %v", err)
+				return fmt.Errorf("failed to validate secondary nodeIP: %v", err)
 			}
 			klog.V(4).Infof("Using secondary node IP: %q", secondaryNodeIP.String())
 		}
