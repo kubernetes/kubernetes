f1829a7744a1c4d2211188e67a1d2d4231edd103
JieJhih Jhang <jiejhihjhang@gmail.com>
2021-01-12 06:35:52 +0800

    fix golint in volume fs

--- pkg/volume/util/fs/fs_windows_test.go
+++ pkg/volume/util/fs/fs_windows_test.go
@@ -27,35 +27,35 @@ import (
 
 func TestDiskUsage(t *testing.T) {
 
-	dir_1, err := ioutil.TempDir("", "dir_1")
+	dir1, err := ioutil.TempDir("", "dir_1")
 	if err != nil {
 		t.Fatalf("TestDiskUsage failed: %s", err.Error())
 	}
-	defer os.RemoveAll(dir_1)
+	defer os.RemoveAll(dir1)
 
-	tmpfile_1, err := ioutil.TempFile(dir_1, "test")
-	if _, err = tmpfile_1.WriteString("just for testing"); err != nil {
+	tmpfile1, err := ioutil.TempFile(dir1, "test")
+	if _, err = tmpfile1.WriteString("just for testing"); err != nil {
 		t.Fatalf("TestDiskUsage failed: %s", err.Error())
 	}
-	dir_2, err := ioutil.TempDir(dir_1, "dir_2")
+	dir2, err := ioutil.TempDir(dir1, "dir_2")
 	if err != nil {
 		t.Fatalf("TestDiskUsage failed: %s", err.Error())
 	}
-	tmpfile_2, err := ioutil.TempFile(dir_2, "test")
-	if _, err = tmpfile_2.WriteString("just for testing"); err != nil {
+	tmpfile2, err := ioutil.TempFile(dir2, "test")
+	if _, err = tmpfile2.WriteString("just for testing"); err != nil {
 		t.Fatalf("TestDiskUsage failed: %s", err.Error())
 	}
 
-	dirInfo1, err := os.Lstat(dir_1)
+	dirInfo1, err := os.Lstat(dir1)
 	if err != nil {
 		t.Fatalf("TestDiskUsage failed: %s", err.Error())
 	}
-	dirInfo2, err := os.Lstat(dir_2)
+	dirInfo2, err := os.Lstat(dir2)
 	if err != nil {
 		t.Fatalf("TestDiskUsage failed: %s", err.Error())
 	}
-	file1 := dir_1 + "/" + "test"
-	file2 := dir_2 + "/" + "test"
+	file1 := dir1 + "/" + "test"
+	file2 := dir2 + "/" + "test"
 	fileInfo1, err := os.Lstat(file1)
 	if err != nil {
 		t.Fatalf("TestDiskUsage failed: %s", err.Error())
@@ -66,7 +66,7 @@ func TestDiskUsage(t *testing.T) {
 	}
 	total := dirInfo1.Size() + dirInfo2.Size() + fileInfo1.Size() + fileInfo2.Size()
 
-	size, err := DiskUsage(dir_1)
+	size, err := DiskUsage(dir1)
 	if err != nil {
 		t.Fatalf("TestDiskUsage failed: %s", err.Error())
 	}
