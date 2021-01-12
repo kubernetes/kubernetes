8e44c89eefe2b23fed48ec53b2df2109fd37c81c
Sergey Kanzhelev <S.Kanzhelev@live.com>
2021-01-12 19:27:29 +0000

    Revert "Merge pull request #92817 from kmala/kubelet"
    This reverts commit 88512be213159f87ed2dad07f75c02d355eadac5, reversing
    changes made to c3b888f647f99eba246468ac3e13afc49542c06d.

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -665,7 +665,6 @@ func NewMainKubelet(kubeCfg *kubeletconfiginternal.KubeletConfiguration,
 	}
 	klet.containerGC = containerGC
 	klet.containerDeletor = newPodContainerDeletor(klet.containerRuntime, integer.IntMax(containerGCPolicy.MaxPerPodContainer, minDeadContainerInPod))
-	klet.sandboxDeleter = newPodSandboxDeleter(klet.containerRuntime)
 
 	// setup imageManager
 	imageManager, err := images.NewImageGCManager(klet.containerRuntime, klet.StatsProvider, kubeDeps.Recorder, nodeRef, imageGCPolicy, crOptions.PodSandboxImage)
@@ -1100,9 +1099,6 @@ type Kubelet struct {
 
 	// trigger deleting containers in a pod
 	containerDeletor *podContainerDeletor
-
-	// trigger deleting sandboxes in a pod
-	sandboxDeleter *podSandboxDeleter
 
 	// config iptables util rules
 	makeIPTablesUtilChains bool
@@ -1930,9 +1926,6 @@ func (kl *Kubelet) syncLoopIteration(configCh <-chan kubetypes.PodUpdate, handle
 				klog.V(4).Infof("SyncLoop (PLEG): ignore irrelevant event: %#v", e)
 			}
 		}
-		if e.Type == pleg.ContainerRemoved {
-			kl.deletePodSandbox(e.ID)
-		}
 
 		if e.Type == pleg.ContainerDied {
 			if containerID, ok := e.Data.(string); ok {
@@ -2253,16 +2246,6 @@ func (kl *Kubelet) fastStatusUpdateOnce() {
 			kl.syncNodeStatus()
 			return
 		}
-	}
-}
-
-func (kl *Kubelet) deletePodSandbox(podID types.UID) {
-	if podStatus, err := kl.podCache.Get(podID); err == nil {
-		toKeep := 1
-		if kl.IsPodDeleted(podID) {
-			toKeep = 0
-		}
-		kl.sandboxDeleter.deleteSandboxesInPod(podStatus, toKeep)
 	}
 }
 
