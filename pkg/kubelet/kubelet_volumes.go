fea2ef68e187742346d011131cba2927f2f2c630
Niels de Vos <ndevos@redhat.com>
2021-01-12 16:53:30 +0100

    UPSTREAM: 97972: Add support for gathering metrics from CSI block-mode volumes
    Cherry-picked the following upstream commits:
    
    e99ced31149 Add support for gethering metrics from CSI block-mode volumes
    e22012950b4 Add Kubelet.ListBlockVolumesForPod()
    de3a4429d94 Make type BlockVolume a MetricsProvider
    fb703b4cc1a Include metrics of BlockVolumes in volumeStatCalculator

--- pkg/kubelet/kubelet_volumes.go
+++ pkg/kubelet/kubelet_volumes.go
@@ -50,6 +50,26 @@ func (kl *Kubelet) ListVolumesForPod(podUID types.UID) (map[string]volume.Volume
 	return volumesToReturn, len(volumesToReturn) > 0
 }
 
+// ListBlockVolumesForPod returns a map of the mounted volumes for the given
+// pod. The key in the map is the OuterVolumeSpecName (i.e.
+// pod.Spec.Volumes[x].Name)
+func (kl *Kubelet) ListBlockVolumesForPod(podUID types.UID) (map[string]volume.BlockVolume, bool) {
+	volumesToReturn := make(map[string]volume.BlockVolume)
+	podVolumes := kl.volumeManager.GetMountedVolumesForPod(
+		volumetypes.UniquePodName(podUID))
+	for outerVolumeSpecName, volume := range podVolumes {
+		// TODO: volume.Mounter could be nil if volume object is recovered
+		// from reconciler's sync state process. PR 33616 will fix this problem
+		// to create Mounter object when recovering volume state.
+		if volume.BlockVolumeMapper == nil {
+			continue
+		}
+		volumesToReturn[outerVolumeSpecName] = volume.BlockVolumeMapper
+	}
+
+	return volumesToReturn, len(volumesToReturn) > 0
+}
+
 // podVolumesExist checks with the volume manager and returns true any of the
 // pods for the specified volume are mounted.
 func (kl *Kubelet) podVolumesExist(podUID types.UID) bool {
