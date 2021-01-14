30674db1595e3a24273ceb71cbfe67bb300ad951
Jan Chaloupka <jchaloup@redhat.com>
2021-01-14 09:08:20 +0100

    kubectl explain: detect resource group in case there are two or more groups discovered
    Assume the following CRDs exist (ordered by priority, the first is the highest):
    - authentications.migration.k8s.io (K=Authentications, G=migration.k8s.io)
    - authentications.metal3.io (K=Authentications, G=metal3.io)
    - authentications.whereabouts.cni.cncf.io (K=Authentications, G=whereabouts.cni.cncf.io)
    - authentications.snapshot.storage.k8s.io (K=Authentications, G=snapshot.storage.k8s.io)
    
    In case 'kubectl explain authentications' is ran, the highest priority definition (in this case authentications.migration.k8s.io)
    is returned. In case a user wants to explain authentication CRD of a different group, --api-version flag has to be set alongside
    to point to a specific group and version. E.g. --api-version=metal3.io/v1
    
    This PR allows to dismiss --api-version flag and perform a prefix check to select a resource (e.g. CRD) whose (resource, group) pair
    fully prefixes requested resource. E.g. running 'kubectl explain authentications.metal3.io' will return
    description of authentications.metal3.io. The same holds for optional field path.
    I.e. 'kubectl explain authentications.metal3.io.spec' will return description of spec field
    of authentications.metal3.io.spec. In case no resource match is found, the search falls back
    to selecting the highest priority gvr that matches the resource.
    In case --api-version is set, no prefix matching is performed. To cover cases
    such as 'kubectl explain authentications.metal3.io --api-version=authentications.metal3.io/v1' where
    fields path coincide with the resource fully specified name (to access .metal3.io field of authentications.metal3.io).

--- staging/src/k8s.io/kubectl/pkg/explain/explain.go
+++ staging/src/k8s.io/kubectl/pkg/explain/explain.go
@@ -50,6 +50,43 @@ func SplitAndParseResourceRequest(inResource string, mapper meta.RESTMapper) (sc
 	if err != nil {
 		return schema.GroupVersionResource{}, nil, err
 	}
+
+	return gvr, fieldsPath, nil
+}
+
+// SplitAndParseResourceRequestWithMatchingPrefix separates the users input into a model and fields
+// while selecting gvr whose (resource, group) prefix matches the resource
+func SplitAndParseResourceRequestWithMatchingPrefix(inResource string, mapper meta.RESTMapper) (gvr schema.GroupVersionResource, fieldsPath []string, err error) {
+	// ignore trailing period
+	inResource = strings.TrimSuffix(inResource, ".")
+	dotParts := strings.Split(inResource, ".")
+
+	gvrs, err := mapper.ResourcesFor(schema.GroupVersionResource{Resource: dotParts[0]})
+	if err != nil {
+		return schema.GroupVersionResource{}, nil, err
+	}
+
+	for _, gvrItem := range gvrs {
+		// Find first gvr whose gr prefixes requested resource
+		groupResource := gvrItem.GroupResource().String()
+		if strings.HasPrefix(inResource, groupResource) {
+			resourceSuffix := inResource[len(groupResource):]
+			if len(resourceSuffix) > 0 {
+				dotParts := strings.Split(resourceSuffix, ".")
+				if len(dotParts) > 0 {
+					fieldsPath = dotParts[1:]
+				}
+			}
+			return gvrItem, fieldsPath, nil
+		}
+	}
+
+	// If no match, take the first (the highest priority) gvr
+	if len(gvrs) > 0 {
+		gvr = gvrs[0]
+		_, fieldsPath = splitDotNotation(inResource)
+	}
+
 	return gvr, fieldsPath, nil
 }
 
