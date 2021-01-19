94a067da83322b016f2580ddd52561dffa6b05a4
coderfornow <39123181+coderfornow@users.noreply.github.com>
2021-01-20 03:36:55 +0530

    Update kuberuntime_container_windows.go
    Remove WindowsGMSA and WindowsRunAsUserName feature gates in 1.20 #95707

--- pkg/kubelet/kuberuntime/kuberuntime_container_windows.go
+++ pkg/kubelet/kuberuntime/kuberuntime_container_windows.go
@@ -22,9 +22,7 @@ import (
 	"runtime"
 
 	"k8s.io/api/core/v1"
-	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	runtimeapi "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
-	kubefeatures "k8s.io/kubernetes/pkg/features"
 	kubeletapis "k8s.io/kubernetes/pkg/kubelet/apis"
 	kubecontainer "k8s.io/kubernetes/pkg/kubelet/container"
 	"k8s.io/kubernetes/pkg/securitycontext"
@@ -127,11 +125,6 @@ func (m *kubeGenericRuntimeManager) generateWindowsContainerConfig(container *v1
 
 	if username != "" {
 		wc.SecurityContext.RunAsUsername = username
-	}
-	if utilfeature.DefaultFeatureGate.Enabled(kubefeatures.WindowsGMSA) &&
-		effectiveSc.WindowsOptions != nil &&
-		effectiveSc.WindowsOptions.GMSACredentialSpec != nil {
-		wc.SecurityContext.CredentialSpec = *effectiveSc.WindowsOptions.GMSACredentialSpec
 	}
 
 	// override with Windows options if present
