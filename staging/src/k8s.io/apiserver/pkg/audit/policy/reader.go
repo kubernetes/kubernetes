eb8f8368bc33a46c0ec595e3f015979420b49a5c
lala123912 <guoxiaofeng@huawei.com>
2021-01-21 11:43:30 +0800

    staging/src/k8s.io/apiserver/pkg/audit/policy/reader.go migrate logs to structured logging

--- staging/src/k8s.io/apiserver/pkg/audit/policy/reader.go
+++ staging/src/k8s.io/apiserver/pkg/audit/policy/reader.go
@@ -85,6 +85,7 @@ func LoadPolicyFromBytes(policyDef []byte) (*auditinternal.Policy, error) {
 	if policyCnt == 0 {
 		return nil, fmt.Errorf("loaded illegal policy with 0 rules")
 	}
-	klog.V(4).Infof("Loaded %d audit policy rules", policyCnt)
+
+	klog.V(4).InfoS("Load audit policy rules success", "policyCnt", policyCnt)
 	return policy, nil
 }
