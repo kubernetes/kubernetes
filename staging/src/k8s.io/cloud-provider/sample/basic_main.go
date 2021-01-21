548156795782ce5b222d1d4e1e97ec0cb86c76ee
cici37 <cicih@google.com>
2021-01-21 12:47:45 -0800

    Separate func

--- staging/src/k8s.io/cloud-provider/sample/basic_main.go
+++ staging/src/k8s.io/cloud-provider/sample/basic_main.go
@@ -54,7 +54,7 @@ func main() {
 		klog.Fatalf("unable to initialize command options: %v", err)
 	}
 
-	command := app.NewCloudControllerManagerCommand(sampleCloudProviderName, ccmOptions, cloudInitializer, app.DefaultInitFuncConstructors)
+	command := app.NewCloudControllerManagerCommand(ccmOptions, cloudInitializer, app.DefaultInitFuncConstructors)
 
 	// TODO: once we switch everything over to Cobra commands, we can go back to calling
 	// utilflag.InitFlags() (by removing its pflag.Parse() call). For now, we have to set the
