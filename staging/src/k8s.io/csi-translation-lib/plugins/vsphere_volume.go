066868fb123317976f885b1a2a0742986ba515ae
Guangze GAO <gaoguangze111@gmail.com>
2021-01-26 11:38:57 +0800

    Update logs in staging/src/k8s.io/csi-translation-lib

--- staging/src/k8s.io/csi-translation-lib/plugins/vsphere_volume.go
+++ staging/src/k8s.io/csi-translation-lib/plugins/vsphere_volume.go
@@ -96,7 +96,7 @@ func (t *vSphereCSITranslator) TranslateInTreeStorageClassToCSI(sc *storage.Stor
 		case "iopslimit":
 			params[paramIopslimit] = v
 		default:
-			klog.V(2).Infof("StorageClass parameter [name:%q, value:%q] is not supported", k, v)
+			klog.V(2).InfoS("StorageClass parameter is not supported", "name", k, "value", v)
 		}
 	}
 
