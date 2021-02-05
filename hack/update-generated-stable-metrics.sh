7c57931612544c4ed0a4fa5c0c182eb0136436f9
Han Kang <hankang@google.com>
2021-02-04 19:08:23 -0800

    remove the rest of the bazel test wiring for metrics stability

--- hack/update-generated-stable-metrics.sh
+++ hack/update-generated-stable-metrics.sh
@@ -23,8 +23,50 @@ set -o nounset
 set -o pipefail
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-export KUBE_ROOT
+source "${KUBE_ROOT}/hack/lib/init.sh"
+source "${KUBE_ROOT}/hack/lib/util.sh"
 
-source "${KUBE_ROOT}/test/instrumentation/stability-utils.sh"
+kube::golang::verify_go_version
+extra_gopath=$(mktemp -d)
+cd "${KUBE_ROOT}"
+export KUBE_EXTRA_GOPATH=$extra_gopath
+kube::golang::setup_env
 
-kube::update::stablemetrics
+find_files() {
+  find . -not \( \
+      \( \
+        -wholename './output' \
+        -o -wholename './_output' \
+        -o -wholename './_gopath' \
+        -o -wholename './release' \
+        -o -wholename './target' \
+        -o -wholename '*/third_party/*' \
+        -o -wholename '*/vendor/*' \
+        -o -wholename '*/hack/*' \
+        -o -wholename '**/*_test.go' \
+        \) -prune \
+    \) \
+    \( -wholename '**/*.go' \
+  \)
+}
+
+static_checked_files=$(find_files | grep -E ".*.go" | grep -v ".*_test.go") || true
+pushd "${KUBE_EXTRA_GOPATH}" >/dev/null
+  GO111MODULE=on go get "gopkg.in/yaml.v2"
+popd >/dev/null
+
+for i in "${static_checked_files[@]}"
+do
+  temp_file=$(mktemp)
+  gopathfiles=$(find_files | grep -E 'test/instrumentation/.*.go' | grep -v "test/instrumentation/main.*go" | cut -c 3-)
+  # Deliberately allow word split here
+  # shellcheck disable=SC2086
+  stabilitycheck=$(go run "test/instrumentation/main.go" $gopathfiles -- $i 1>$temp_file)
+  if ! $stabilitycheck; then
+    echo "!!! updating golden list of metrics has failed!" >&2
+    exit 1
+  fi
+  mv -f "$temp_file" "${KUBE_ROOT}/test/instrumentation/testdata/stable-metrics-list.yaml"
+  echo "Updated golden list of stable metrics."
+done
+
