5cdbe0b1c6af878cc1588390ebbc41691081a394
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2021-02-08 18:04:54 +0100

    UPSTREAM: <carry>: kube-apiserver: ignore SIGTERM/INT after the first one
    UPSTREAM: <carry>: kube-apiserver: set up separate signal handler functions to ignore further signals
    
    This patches the changes from https://github.com/openshift/kubernetes/pull/558 to provide
    these new functions without changing the behavior for other repos that depend on them, such
    as library-go.
    
    OpenShift-Rebase-Source: 63ed2006a96

--- cmd/kube-apiserver/app/server.go
+++ cmd/kube-apiserver/app/server.go
@@ -156,7 +156,7 @@ cluster's shared state through which all other components interact.`,
 			}
 			// add feature enablement metrics
 			utilfeature.DefaultMutableFeatureGate.AddMetrics()
-			return Run(completedOptions, genericapiserver.SetupSignalHandler())
+			return Run(completedOptions, genericapiserver.SetupSignalHandlerIgnoringFurtherSignals())
 		},
 		Args: func(cmd *cobra.Command, args []string) error {
 			for _, arg := range args {
