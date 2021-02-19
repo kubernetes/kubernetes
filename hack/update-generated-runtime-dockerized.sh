36240337688763b0562e0501c0ba5fc2b5b521ba
Jordan Liggitt <liggitt@google.com>
2021-02-18 23:56:34 -0500

    drop vendor from go install paths

--- hack/update-generated-runtime-dockerized.sh
+++ hack/update-generated-runtime-dockerized.sh
@@ -29,7 +29,7 @@ runtime_versions=("v1alpha2" "v1")
 
 kube::golang::setup_env
 
-go install k8s.io/kubernetes/vendor/k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo
+go install k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo
 
 if [[ -z "$(which protoc)" || "$(protoc --version)" != "libprotoc 3."* ]]; then
   echo "Generating protobuf requires protoc 3.0.0-beta1 or newer. Please download and"
