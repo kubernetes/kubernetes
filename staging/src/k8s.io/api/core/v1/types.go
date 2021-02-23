80eb06c39151b8f00dabd8d4de32eda00cbad104
李志锋10253908 <li.zhifeng@zte.com.cn>
2021-02-23 11:07:40 +0800

    Modify lifecycle comments for consistency

--- staging/src/k8s.io/api/core/v1/types.go
+++ staging/src/k8s.io/api/core/v1/types.go
@@ -2370,18 +2370,18 @@ type Lifecycle struct {
 	// PostStart is called immediately after a container is created. If the handler fails,
 	// the container is terminated and restarted according to its restart policy.
 	// Other management of the container blocks until the hook completes.
+	// No parameters are passed to the handler.
 	// More info: https://kubernetes.io/docs/concepts/containers/container-lifecycle-hooks/#container-hooks
 	// +optional
 	PostStart *Handler `json:"postStart,omitempty" protobuf:"bytes,1,opt,name=postStart"`
 	// PreStop is called immediately before a container is terminated due to an
 	// API request or management event such as liveness/startup probe failure,
 	// preemption, resource contention, etc. The handler is not called if the
-	// container crashes or exits. The reason for termination is passed to the
-	// handler. The Pod's termination grace period countdown begins before the
+	// container crashes or exits. The Pod's termination grace period countdown begins before the
 	// PreStop hooked is executed. Regardless of the outcome of the handler, the
 	// container will eventually terminate within the Pod's termination grace
 	// period. Other management of the container blocks until the hook completes
-	// or until the termination grace period is reached.
+	// or until the termination grace period is reached. No parameters are passed to the handler.
 	// More info: https://kubernetes.io/docs/concepts/containers/container-lifecycle-hooks/#container-hooks
 	// +optional
 	PreStop *Handler `json:"preStop,omitempty" protobuf:"bytes,2,opt,name=preStop"`
