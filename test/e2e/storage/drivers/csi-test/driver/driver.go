700819609387e24e66abc82f301a5f987175fec4
Patrick Ohly <patrick.ohly@intel.com>
2021-02-24 15:24:58 +0100

    csi-test: import copyright clarification
    The code originates in csi-test and this copyright change was made by
    the original author in https://github.com/kubernetes-csi/csi-test/pull/324

--- test/e2e/storage/drivers/csi-test/driver/driver.go
+++ test/e2e/storage/drivers/csi-test/driver/driver.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Luis Pabón luis@portworx.com
+Copyright 2021 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
