6215b12c8475cd4ea0815a72dea97a8c054c9f49
Derek Carr <decarr@redhat.com>
2021-03-01 17:32:00 -0500

    e2e testing for hugepages downward api

--- test/e2e/framework/skipper/skipper.go
+++ test/e2e/framework/skipper/skipper.go
@@ -46,6 +46,8 @@ import (
 // New local storage types to support local storage capacity isolation
 var localStorageCapacityIsolation featuregate.Feature = "LocalStorageCapacityIsolation"
 
+var downwardAPIHugePages featuregate.Feature = "DownwardAPIHugePages"
+
 func skipInternalf(caller int, format string, args ...interface{}) {
 	msg := fmt.Sprintf(format, args...)
 	framework.Logf(msg)
@@ -134,6 +136,12 @@ func SkipUnlessAtLeast(value int, minValue int, message string) {
 func SkipUnlessLocalEphemeralStorageEnabled() {
 	if !utilfeature.DefaultFeatureGate.Enabled(localStorageCapacityIsolation) {
 		skipInternalf(1, "Only supported when %v feature is enabled", localStorageCapacityIsolation)
+	}
+}
+
+func SkipUnlessDownwardAPIHugePagesEnabled() {
+	if !utilfeature.DefaultFeatureGate.Enabled(downwardAPIHugePages) {
+		skipInternalf(1, "Only supported when %v feature is enabled", downwardAPIHugePages)
 	}
 }
 
