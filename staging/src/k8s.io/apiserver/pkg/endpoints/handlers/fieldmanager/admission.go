98d498117b5566c1229d754d97923f61600660d8
Kevin Wiesmueller <kwiesmul@redhat.com>
2021-03-01 20:29:15 +0100

    only use managedFields decoding for admission check

--- staging/src/k8s.io/apiserver/pkg/endpoints/handlers/fieldmanager/admission.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/handlers/fieldmanager/admission.go
@@ -67,7 +67,7 @@ func (admit *managedFieldsValidatingAdmissionController) Admit(ctx context.Conte
 		return err
 	}
 	managedFieldsAfterAdmission := objectMeta.GetManagedFields()
-	if err := ValidateManagedFields(managedFieldsAfterAdmission); err != nil {
+	if _, err := DecodeManagedFields(managedFieldsAfterAdmission); err != nil {
 		objectMeta.SetManagedFields(managedFieldsBeforeAdmission)
 		warning.AddWarning(ctx, "",
 			fmt.Sprintf(InvalidManagedFieldsAfterMutatingAdmissionWarningFormat,
