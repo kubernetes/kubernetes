036b11c98952e4e39ff418acbb5c1f23005f12f7
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2021-03-04 09:58:56 +0100

    UPSTREAM: <carry>: allows for switching KCM to talk to Kube API over localhost
    to force KCM to use localhost set the following flag in kubecontrollermanager (oc edit kubecontrollermanager cluster)
    
    unsupportedConfigOverrides:
      extendedArguments:
        unsupported-kube-api-over-localhost:
          - "true"
    
    openshift-rebase(v1.24):source=0ac43f622c4
    
    openshift-rebase(v1.24):source=0ac43f622c4
    
    openshift-rebase(v1.24):source=0ac43f622c4

--- cmd/kube-controller-manager/app/controllermanager.go
+++ cmd/kube-controller-manager/app/controllermanager.go
@@ -73,6 +73,8 @@ import (
 	kubectrlmgrconfig "k8s.io/kubernetes/pkg/controller/apis/config"
 	serviceaccountcontroller "k8s.io/kubernetes/pkg/controller/serviceaccount"
 	"k8s.io/kubernetes/pkg/serviceaccount"
+
+	libgorestclient "github.com/openshift/library-go/pkg/config/client"
 )
 
 func init() {
@@ -129,6 +131,11 @@ controller, and serviceaccounts controller.`,
 				return err
 			}
 			cliflag.PrintFlags(cmd.Flags())
+
+			if err := SetUpPreferredHostForOpenShift(s); err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
 
 			c, err := s.Config(KnownControllers(), ControllersDisabledByDefault.List())
 			if err != nil {
@@ -194,6 +201,17 @@ func Run(c *config.CompletedConfig, stopCh <-chan struct{}) error {
 		cfgz.Set(c.ComponentConfig)
 	} else {
 		klog.Errorf("unable to register configz: %v", err)
+	}
+
+	// start the localhost health monitor early so that it can be used by the LE client
+	if c.OpenShiftContext.PreferredHostHealthMonitor != nil {
+		hmCtx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		go func() {
+			<-stopCh
+			cancel()
+		}()
+		go c.OpenShiftContext.PreferredHostHealthMonitor.Run(hmCtx)
 	}
 
 	// Setup any healthz checks we will want to use.
@@ -717,7 +735,7 @@ func createClientBuilders(c *config.CompletedConfig) (clientBuilder clientbuilde
 		}
 
 		clientBuilder = clientbuilder.NewDynamicClientBuilder(
-			restclient.AnonymousClientConfig(c.Kubeconfig),
+			libgorestclient.AnonymousClientConfigWithWrapTransport(c.Kubeconfig),
 			c.Client.CoreV1(),
 			metav1.NamespaceSystem)
 	} else {
