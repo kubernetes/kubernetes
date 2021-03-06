7705d8c3de25c1dcd9a20768c6821cb81e64d3ce
Benjamin Elder <bentheelder@google.com>
2021-03-06 13:14:28 -0800

    upgrade shellcheck to v0.7.1, use multi-arch digest

--- hack/verify-shellcheck.sh
+++ hack/verify-shellcheck.sh
@@ -27,9 +27,9 @@ source "${KUBE_ROOT}/hack/lib/util.sh"
 
 # required version for this script, if not installed on the host we will
 # use the official docker image instead. keep this in sync with SHELLCHECK_IMAGE
-SHELLCHECK_VERSION="0.7.0"
+SHELLCHECK_VERSION="0.7.1"
 # upstream shellcheck latest stable image as of October 23rd, 2019
-SHELLCHECK_IMAGE="koalaman/shellcheck-alpine:v0.7.0@sha256:24bbf52aae6eaa27accc9f61de32d30a1498555e6ef452966d0702ff06f38ecb"
+SHELLCHECK_IMAGE="docker.io/koalaman/shellcheck-alpine:v0.7.1@sha256:d6147f30864ddb7c9cf983fc277d345bc315e798e309ddf70062b194843ee252"
 
 # disabled lints
 disabled=(
