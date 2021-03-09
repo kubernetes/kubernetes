6896bef0255f70cf29c40aaf4dc76164e5a8f6dc
afrouz <mashayekhiafrouz@gmail.com>
2021-03-09 12:29:32 +0330

    Migrate cmd/kubelet, image, cadvisor and cri to structured logging

--- cmd/kubelet/app/server_linux.go
+++ cmd/kubelet/app/server_linux.go
@@ -24,19 +24,19 @@ import (
 func watchForLockfileContention(path string, done chan struct{}) error {
 	watcher, err := inotify.NewWatcher()
 	if err != nil {
-		klog.Errorf("unable to create watcher for lockfile: %v", err)
+		klog.ErrorS(err, "Unable to create watcher for lockfile")
 		return err
 	}
 	if err = watcher.AddWatch(path, inotify.InOpen|inotify.InDeleteSelf); err != nil {
-		klog.Errorf("unable to watch lockfile: %v", err)
+		klog.ErrorS(err, "Unable to watch lockfile")
 		return err
 	}
 	go func() {
 		select {
 		case ev := <-watcher.Event:
-			klog.Infof("inotify event: %v", ev)
+			klog.InfoS("inotify event", "event", ev)
 		case err = <-watcher.Error:
-			klog.Errorf("inotify watcher error: %v", err)
+			klog.ErrorS(err, "inotify watcher error")
 		}
 		close(done)
 	}()
