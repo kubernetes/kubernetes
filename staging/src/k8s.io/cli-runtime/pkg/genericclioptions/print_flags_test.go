69d7582b4b1bae1c3cafda7b7bb46872dc515a7c
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-03-24 09:57:06 +0900

    remove unnecessary import prefix

--- staging/src/k8s.io/cli-runtime/pkg/genericclioptions/print_flags_test.go
+++ staging/src/k8s.io/cli-runtime/pkg/genericclioptions/print_flags_test.go
@@ -20,7 +20,7 @@ import (
 	"bytes"
 	"testing"
 
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
