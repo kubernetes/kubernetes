dfa598b21f5c6262800286c1638f05ee41ab5c0c
Ryan Phillips <ryan@trolocsis.com>
2021-03-29 12:02:13 -0500

    UPSTREAM: <carry>: add management support to kubelet
    UPSTREAM: <carry>: management workloads enhancement 741
    
    UPSTREAM: <carry>: lower verbosity of managed workloads logging
    
    Support for managed workloads was introduced by PR#627.  However, the
    the CPU manager reconcile loop now seems to flood kubelet log with
    "reconcileState: skipping pod; pod is managed" warnings.  Lower the
    verbosity of these log messages.
    
    UPSTREAM: <carry>: set correctly static pods CPUs when workload partitioning is disabled
    
    UPSTREAM: <carry>: Remove reserved CPUs from default set
    
    Remove reserved CPUs from default set when workload partitioning is
    enabled.
    
    Co-Authored-By: Brent Rowsell <browsell@redhat.com>
    Signed-off-by: Artyom Lukianov <alukiano@redhat.com>
    Signed-off-by: Don Penney <dpenney@redhat.com>
    OpenShift-Rebase-Source: b762ced05a6
    OpenShift-Rebase-Source: 63cf7937fbc
    OpenShift-Rebase-Source: 32af64c1458

--- pkg/kubelet/cm/cpumanager/policy_static.go
+++ pkg/kubelet/cm/cpumanager/policy_static.go
@@ -30,6 +30,7 @@ import (
 	"k8s.io/kubernetes/pkg/kubelet/cm/cpuset"
 	"k8s.io/kubernetes/pkg/kubelet/cm/topologymanager"
 	"k8s.io/kubernetes/pkg/kubelet/cm/topologymanager/bitmask"
+	"k8s.io/kubernetes/pkg/kubelet/managed"
 	"k8s.io/kubernetes/pkg/kubelet/metrics"
 )
 
@@ -202,6 +203,10 @@ func (p *staticPolicy) validateState(s state.State) error {
 		// state is empty initialize
 		allCPUs := p.topology.CPUDetails.CPUs()
 		s.SetDefaultCPUSet(allCPUs)
+		if managed.IsEnabled() {
+			defaultCpus := s.GetDefaultCPUSet().Difference(p.reserved)
+			s.SetDefaultCPUSet(defaultCpus)
+		}
 		return nil
 	}
 
