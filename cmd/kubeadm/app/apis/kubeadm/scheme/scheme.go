02b455b565a730a16d22b0a1ef90764729b693be
Lubomir I. Ivanov <lubomirivanov@vmware.com>
2021-03-30 03:12:09 +0300

    kubeadm: add v1beta3 copied from v1beta2
    Add a new versioned API v1beta3.

--- cmd/kubeadm/app/apis/kubeadm/scheme/scheme.go
+++ cmd/kubeadm/app/apis/kubeadm/scheme/scheme.go
@@ -24,6 +24,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm"
 	"k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta2"
+	"k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta3"
 )
 
 // Scheme is the runtime.Scheme to which all kubeadm api types are registered.
@@ -41,5 +42,6 @@ func init() {
 func AddToScheme(scheme *runtime.Scheme) {
 	utilruntime.Must(kubeadm.AddToScheme(scheme))
 	utilruntime.Must(v1beta2.AddToScheme(scheme))
+	utilruntime.Must(v1beta3.AddToScheme(scheme))
 	utilruntime.Must(scheme.SetVersionPriority(v1beta2.SchemeGroupVersion))
 }
