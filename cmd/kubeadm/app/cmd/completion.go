1d9c4c78e17fe5f14fdec6c362fb436f7b511468
LinshanYu <1149439148@qq.com>
2021-03-31 19:26:39 +0800

    edit cmd/kubeadn/app/cmd/completion.go to structured Log

--- cmd/kubeadm/app/cmd/completion.go
+++ cmd/kubeadm/app/cmd/completion.go
@@ -134,7 +134,7 @@ func RunCompletion(out io.Writer, boilerPlate string, cmd *cobra.Command, args [
 }
 
 func runCompletionBash(out io.Writer, kubeadm *cobra.Command) error {
-	klog.V(1).Infoln("[completion] writing completion code for Bash")
+	klog.V(1).InfoS("[completion] writing completion code for Bash")
 	return kubeadm.GenBashCompletion(out)
 }
 
@@ -280,12 +280,12 @@ __kubeadm_convert_bash_to_zsh() {
 	-e "s/\\\$(type${RWORD}/\$(__kubeadm_type/g" \
 	<<'BASH_COMPLETION_EOF'
 `
-	klog.V(1).Infoln("[completion] writing completion code for Zsh")
+	klog.V(1).InfoS("[completion] writing completion code for Zsh")
 	out.Write([]byte(zshInitialization))
 
 	buf := new(bytes.Buffer)
 	kubeadm.GenBashCompletion(buf)
-	klog.V(1).Infoln("[completion] writing completion code for Bash")
+	klog.V(1).InfoS("[completion] writing completion code for Bash")
 	out.Write(buf.Bytes())
 
 	zshTail := `
