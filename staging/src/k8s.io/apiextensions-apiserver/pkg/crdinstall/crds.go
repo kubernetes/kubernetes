4a87808843f668812427adbe4ada6ac13cc399c2
Yuvaraj Kakaraparthi <yuva2811@gmail.com>
2021-04-07 23:55:38 -0700

    initial commit for CRD install POC

--- staging/src/k8s.io/apiextensions-apiserver/pkg/crdinstall/crds.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/crdinstall/crds.go
@@ -1,15 +1,49 @@
 package crdinstall
 
-import "k8s.io/apimachinery/pkg/runtime"
+import (
+	"embed"
+	"fmt"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
 
-var installCRDs []runtime.Object
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+//go:embed configs/*.yaml
+var content embed.FS
+
+const configsDir = "configs"
+
+var installCRDs []*unstructured.Unstructured
 
 func init() {
-	// objects added to installCRDs will be installed during the cluster boot up
-	// the objects are "applied" in the given order
-	installCRDs = append(installCRDs, panipuriCRD)
+
+	decoder := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+
+	items, err := content.ReadDir(configsDir)
+	if err != nil {
+		fmt.Println("failed to the content of the config dir")
+		return
+	}
+
+	for _, item := range items {
+		// read the config
+		b, err := content.ReadFile(fmt.Sprintf("%v/%v", configsDir, item.Name()))
+		if err != nil {
+			fmt.Printf("failed to readfile %v : %v\n", item.Name(), err)
+			return
+		}
+		obj := &unstructured.Unstructured{}
+		_, _, err = decoder.Decode(b, nil, obj)
+		if err != nil {
+			fmt.Printf("failed to decode %v : %v\n", item.Name(), err)
+			return
+		}
+
+		// add to list
+		installCRDs = append(installCRDs, obj)
+	}
 }
 
-func Objects() []runtime.Object {
+func Objects() []*unstructured.Unstructured {
 	return installCRDs
 }
