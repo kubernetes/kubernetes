df621653bd0d061c2a8eea39f5a3df70dadee688
Martin Ostrowski <mostrowski@google.com>
2021-04-08 21:59:27 +0000

    istio: config
    Icc56b37e6fdebd7d29fd6be7986f28e8b826a886
    I9db9606a401022f970d69f9714b75e33f8604713
    
    Change-Id: I2feca4893a8bca574cf1053e44b9c7aef79628fd

--- gke/cluster/gce/gci/configure-helper.sh
+++ gke/cluster/gce/gci/configure-helper.sh
@@ -2777,11 +2777,24 @@ EOF
     local -r metadata_proxy_yaml="${dst_dir}/metadata-proxy/gce/metadata-proxy.yaml"
     update-daemon-set-prometheus-to-sd-parameters ${metadata_proxy_yaml}
   fi
+
   if [[ "${ENABLE_ISTIO:-}" == "true" ]]; then
-    if [[ "${ISTIO_AUTH_TYPE:-}" == "MUTUAL_TLS" ]]; then
-      setup-addon-manifests "addons" "istio/auth"
+    if [[ "${ISTIO_OPERATOR:-}" == "true" ]]; then
+      setup-addon-manifests "addons" "istio/operator"
     else
-      setup-addon-manifests "addons" "istio/noauth"
+      if [[ "${ENABLE_CLUSTER_MONITORING:-}" == "stackdriver" || "${MONITORING_FLAG_SET}" == "true" ]]; then
+        if [[ "${ISTIO_AUTH_TYPE:-}" == "MUTUAL_TLS" ]]; then
+          setup-addon-manifests "addons" "istio/auth-sd"
+        else
+          setup-addon-manifests "addons" "istio/noauth-sd"
+        fi
+      else
+        if [[ "${ISTIO_AUTH_TYPE:-}" == "MUTUAL_TLS" ]]; then
+          setup-addon-manifests "addons" "istio/auth"
+        else
+          setup-addon-manifests "addons" "istio/noauth"
+        fi
+      fi
     fi
   fi
   if [[ -n "${EXTRA_ADDONS_URL:-}" ]]; then
