7dc4d4de9143707ba50cbc78d0965a2fde6dfea4
Jordan Liggitt <liggitt@google.com>
2021-04-09 01:11:55 +0000

    vpa: config
    vpa: manifests
    Change-Id: I34c2b943ec9cb9f1ce6fb4f3745bb10a5b99c195
    
    Remove code for falling back to generating certs for VPA
    
    GKE is unlikely to roll back to version that doesn't generate certs for
    VPA while adding new K8s versions that would pull in this change (fixes
    b/119761988).
    
    Change-Id: I9d179fa5533686c5bcd5137e3f241bb3e8ddcd7e
    
    vpa: Allow running Unified Autoscaler adapter when VPA is not on.
    Change-Id: Iaa5d577f48b36979e38019f3dacb149472d9c4f3
    
    vpa: refactor static token and kubeconfig creation
    Change-Id: I95657decf01103f9df1f3493871d5e3d3ae1beff
    
    Rename vertical-pod-autoscaler addon as pod-autoscaler.
    
    Change-Id: Ifa141b50b8c7ba2aea5b53eca80adeb8755c9252
    
    vpa: Split parameters passed to VPA binaries in preparation for distroless images
    Change-Id: I9d63743dedb85258a07a1d8e3a9611196cc137ca
    
    vpa: Handle empty uas_params
    Change-Id: I8881aebd83fb8f48ec298e7511cd9ed51f63b72a
    
    vpa: Add memory limit for vpa-recommender binary
    Change-Id: Iaa18ffba8203c2e88a72ea81bb0c5e6795cfcc08

--- gke/cluster/gce/gci/gke-internal-configure-helper.sh
+++ gke/cluster/gce/gci/gke-internal-configure-helper.sh
@@ -46,9 +46,111 @@ function start_internal_ca {
   cp "${src_file}" /etc/kubernetes/manifests
 }
 
+function add_vpa_admission_webhook_host {
+  original_ipv6_loopback_line=`grep "^::1[[:space:]]" /etc/hosts`
+  tmp_file=`mktemp`
+  grep -v "^::1[[:space:]]" /etc/hosts >${tmp_file}
+  cat ${tmp_file} >/etc/hosts
+  if [[ -n "${original_ipv6_loopback_line:-}" ]]; then
+    echo "${original_ipv6_loopback_line} vpa.admissionwebhook.localhost" >>/etc/hosts
+  else
+    echo "::1 vpa.admissionwebhook.localhost" >>/etc/hosts
+  fi
+}
+
+function start_pod_autoscaler {
+  local -r manifests_dir="${KUBE_HOME}/kube-manifests/kubernetes/gci-trusty"
+  mkdir -p "${manifests_dir}/pod-autoscaler"
+  if [[ "${ENABLE_VERTICAL_POD_AUTOSCALER:-}" == "true" ]]; then
+    echo "Start Vertical Pod Autoscaler (VPA)"
+    generate_vertical_pod_autoscaler_admission_controller_certs
+    add_vpa_admission_webhook_host
+
+    cp "${manifests_dir}/internal-vpa-crd.yaml" "${manifests_dir}/pod-autoscaler"
+    cp "${manifests_dir}/internal-vpa-rbac.yaml" "${manifests_dir}/pod-autoscaler"
+    setup-addon-manifests "addons" "pod-autoscaler"
+
+    for component in admission-controller recommender updater; do
+      setup_pod_autoscaler_component ${component} ${manifests_dir}
+    done
+  elif [[ "${ENABLE_UNIFIED_AUTOSCALING:-}" == "true" ]]; then
+    cp "${manifests_dir}/internal-kuba-rbac.yaml" "${manifests_dir}/pod-autoscaler"
+    setup-addon-manifests "addons" "pod-autoscaler"
+
+    echo "Start Kubernetes Adapter for Unified Autoscaler (KUBA)"
+    setup_pod_autoscaler_component "recommender" ${manifests_dir}
+  fi
+}
+
+function base64_decode_or_die {
+  local variable_name=$1
+  local out_file=$2
+  if [[ -n "${!variable_name}" ]]; then
+    if ! base64 -d - <<<${!variable_name} >${out_file}; then
+      echo "==error base 64 decoding ${variable_name}=="
+      echo "==the value of the variable is ${!variable_name}=="
+      exit 1
+    fi
+  else
+    echo "==VPA enabled but ${variable_name} is not set=="
+    exit 1
+  fi
+}
+
+function setup_pod_autoscaler_component {
+  local component=$1
+  local manifests_dir=$2
+  create-static-auth-kubeconfig-for-component vpa-${component}
+
+  # Prepare manifest
+  local src_file="${manifests_dir}/internal-vpa-${component}.manifest"
+
+  if [[ ${component} == "recommender" ]]; then
+    local uas_params="${UAS_PARAMS:-}"
+    # split the params into separate arguments passed to binary
+    local uas_params_split
+    uas_params_split=$(eval "for param in $uas_params; do echo -n ,\\\"\$param\\\"; done")
+    sed -i -e "s@{{uas_params}}@${uas_params_split:-}@g" "${src_file}"
+
+    # set memory limit for vpa-recommender. Limit default value
+    # justification in http://b/163760835#comment8
+    local memory_limit="${POD_AUTOSCALER_MEMORY_LIMIT:-4Gi}"
+    sed -i -e "s@{{memory_limit}}@${memory_limit}@g" "${src_file}"
+  fi
+
+  sed -i -e "s@{{cloud_config_mount}}@${CLOUD_CONFIG_MOUNT}@g" "${src_file}"
+  sed -i -e "s@{{cloud_config_volume}}@${CLOUD_CONFIG_VOLUME}@g" "${src_file}"
+  sed -i -e "s@{%.*%}@@g" "${src_file}"
+
+  cp "${src_file}" /etc/kubernetes/manifests
+}
+
+function generate_vertical_pod_autoscaler_admission_controller_certs {
+  local certs_dir="/etc/tls-certs" #TODO: what is the best place for certs?
+  echo "Generating certs for the VPA Admission Controller in ${certs_dir}."
+  mkdir -p ${certs_dir}
+  if [[ -n "${CA_CERT:-}" ]] && [[ -n "${VPA_AC_KEY:-}" ]] && [[ -n "${VPA_AC_CERT:-}" ]]; then
+    base64_decode_or_die "CA_CERT" ${certs_dir}/caCert.pem
+    base64_decode_or_die "VPA_AC_KEY" ${certs_dir}/serverKey.pem
+    base64_decode_or_die "VPA_AC_CERT" ${certs_dir}/serverCert.pem
+  else
+    echo "==At least one of CA_CERT, VPA_AC_KEY, VPA_AC_CERT is missing=="
+    exit 1
+  fi
+}
+
+function create-static-auth-kubeconfig-for-component {
+  local component=$1
+  echo "Creating token for component ${component}"
+  local token="$(secure_random 32)"
+  append_or_replace_prefixed_line /etc/srv/kubernetes/known_tokens.csv "${token}," "system:${component},uid:system:${component}"
+  create-kubeconfig ${component} ${token}
+}
+
 function gke-internal-master-start {
   echo "Internal GKE configuration start"
   compute-master-manifest-variables
   start_internal_cluster_autoscaler
+  start_pod_autoscaler
   echo "Internal GKE configuration done"
 }
