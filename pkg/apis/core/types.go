da98bec4a00ff147520edf99a344c2232ec331b8
mkuger <michael@mikuger.de>
2021-04-11 21:53:23 +0200

    Broken link fixed

--- pkg/apis/core/types.go
+++ pkg/apis/core/types.go
@@ -2832,7 +2832,7 @@ type PodSpec struct {
 	// If specified, all readiness gates will be evaluated for pod readiness.
 	// A pod is ready when all its containers are ready AND
 	// all conditions specified in the readiness gates have status equal to "True"
-	// More info: https://git.k8s.io/enhancements/keps/sig-network/0007-pod-ready%2B%2B.md
+	// More info: https://git.k8s.io/enhancements/keps/sig-network/580-pod-readiness-gates/README.md
 	// +optional
 	ReadinessGates []PodReadinessGate
 	// RuntimeClassName refers to a RuntimeClass object in the node.k8s.io group, which should be used
