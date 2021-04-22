d4951b4d35a2d36c8229b4afebfdfb48eeeed52f
Mike Dame <mdame@redhat.com>
2021-04-22 14:35:26 -0400

    Wire contexts to RBAC controllers

--- cmd/kube-controller-manager/app/rbac.go
+++ cmd/kube-controller-manager/app/rbac.go
@@ -31,6 +31,6 @@ func startClusterRoleAggregrationController(ctx context.Context, controllerConte
 	go clusterroleaggregation.NewClusterRoleAggregation(
 		controllerContext.InformerFactory.Rbac().V1().ClusterRoles(),
 		controllerContext.ClientBuilder.ClientOrDie("clusterrole-aggregation-controller").RbacV1(),
-	).Run(5, controllerContext.Stop)
+	).Run(ctx, 5, controllerContext.Stop)
 	return nil, true, nil
 }
