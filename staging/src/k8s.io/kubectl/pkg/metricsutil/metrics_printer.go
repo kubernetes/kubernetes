6273438a7e74666cefc83ce743b6c054a43fdc06
Joshua Kurz <josh.kurz@weightwatchers.com>
2021-04-29 12:45:27 -0400

    fix(kubectl): remove vscode stuff

--- staging/src/k8s.io/kubectl/pkg/metricsutil/metrics_printer.go
+++ staging/src/k8s.io/kubectl/pkg/metricsutil/metrics_printer.go
@@ -21,7 +21,7 @@ import (
 	"io"
 	"sort"
 
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/cli-runtime/pkg/printers"
 	metricsapi "k8s.io/metrics/pkg/apis/metrics"
