11d6b0f02e707e48a12ce3c09805b98a4ff7940b
forest <thickforest@126.com>
2021-04-30 19:52:31 +0800

    Migrate pkg/kubemark to structured logging

--- pkg/kubemark/hollow_kubelet.go
+++ pkg/kubemark/hollow_kubelet.go
@@ -153,7 +153,7 @@ type HollowKubletOptions struct {
 func GetHollowKubeletConfig(opt *HollowKubletOptions) (*options.KubeletFlags, *kubeletconfig.KubeletConfiguration) {
 	testRootDir := utils.MakeTempDirOrDie("hollow-kubelet.", "")
 	podFilePath := utils.MakeTempDirOrDie("static-pods", testRootDir)
-	klog.Infof("Using %s as root dir for hollow-kubelet", testRootDir)
+	klog.InfoS("Using root dir for hollow-kubelet", "rootDir", testRootDir)
 
 	// Flags struct
 	f := options.NewKubeletFlags()
