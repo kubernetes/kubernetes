d8aad7944cc71314913fc0b43d6731e2c36f4928
Aldo Culquicondor <acondor@google.com>
2021-05-20 20:27:21 +0000

    Remove unused util CreatePods
    And rename CreatePodsWithControllerRef to simply CreatePods

--- pkg/controller/job/job_controller.go
+++ pkg/controller/job/job_controller.go
@@ -875,7 +875,7 @@ func (jm *Controller) manageJob(job *batch.Job, activePods []*v1.Pod, succeeded 
 					}
 					defer wait.Done()
 					generateName := podGenerateNameWithIndex(job.Name, completionIndex)
-					err := jm.podControl.CreatePodsWithControllerRefAndGenerateName(job.Namespace, template, job, metav1.NewControllerRef(job, controllerKind), generateName)
+					err := jm.podControl.CreatePodsWithGenerateName(job.Namespace, template, job, metav1.NewControllerRef(job, controllerKind), generateName)
 					if err != nil {
 						if apierrors.HasStatusCause(err, v1.NamespaceTerminatingCause) {
 							// If the namespace is being torn down, we can safely ignore
