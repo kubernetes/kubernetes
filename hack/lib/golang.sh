39d9e98a6f9e51a1b2951055bbd60d6161715cd9
Sascha Grunert <sgrunert@redhat.com>
2021-05-26 09:45:44 +0200

    Build non-static binaries with PIE buildmode
    We now add the `-buildmode pie` flag when building non-static binaries,
    which enables the ASLR security mechanism.
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- hack/lib/golang.sh
+++ hack/lib/golang.sh
@@ -412,6 +412,10 @@ kube::golang::set_platform_envs() {
         export CGO_ENABLED=1
         export CC=${KUBE_LINUX_AMD64_CC:-x86_64-linux-gnu-gcc}
         ;;
+      "linux/386")
+        export CGO_ENABLED=1
+        export CC=${KUBE_LINUX_386_CC:-i686-linux-gnu-gcc}
+        ;;
       "linux/arm")
         export CGO_ENABLED=1
         export CC=${KUBE_LINUX_ARM_CC:-arm-linux-gnueabihf-gcc}
@@ -721,6 +725,7 @@ kube::golang::build_binaries_for_platform() {
       -gcflags "${gogcflags:-}"
       -asmflags "${goasmflags:-}"
       -ldflags "${goldflags:-}"
+      -buildmode pie
       -tags "${gotags:-}"
     )
     kube::golang::build_some_binaries "${nonstatics[@]}"
