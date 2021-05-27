33c2c533dbcca165a99b500b821f063a72ccb8e9
Jerry-Ge <jerry.ge@arm.com>
2021-05-27 09:38:17 +0800

    fix the failure from integration test

--- test/integration/scheduler/queue_test.go
+++ test/integration/scheduler/queue_test.go
@@ -124,7 +124,7 @@ func TestServiceAffinityEnqueue(t *testing.T) {
 		t.Fatalf("Cannot find the profile for Pod %v", podInfo.Pod.Name)
 	}
 	// Schedule the Pod manually.
-	_, fitError := testCtx.Scheduler.Algorithm.Schedule(ctx, fwk, framework.NewCycleState(), podInfo.Pod)
+	_, fitError := testCtx.Scheduler.Schedule(ctx, fwk, framework.NewCycleState(), podInfo.Pod)
 	// The fitError is expected to be:
 	// 0/2 nodes are available: 1 Too many pods, 1 node(s) didn't match service affinity.
 	if fitError == nil {
