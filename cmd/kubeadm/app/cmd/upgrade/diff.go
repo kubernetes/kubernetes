897f8012fdd3739ebbb726c8356b91552ad26f84
navist2020 <navist2020@163.com>
2021-06-03 17:56:19 +0800

    kubeadm:Run preflight checks for diff to check that the manifests already exist

--- cmd/kubeadm/app/cmd/upgrade/diff.go
+++ cmd/kubeadm/app/cmd/upgrade/diff.go
@@ -19,6 +19,7 @@ package upgrade
 import (
 	"io"
 	"io/ioutil"
+	"os"
 
 	"github.com/pkg/errors"
 	"github.com/pmezard/go-difflib/difflib"
@@ -65,7 +66,13 @@ func newCmdDiff(out io.Writer) *cobra.Command {
 		Use:   "diff [version]",
 		Short: "Show what differences would be applied to existing static pod manifests. See also: kubeadm upgrade apply --dry-run",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO: Run preflight checks for diff to check that the manifests already exist.
+			// Run preflight checks for diff to check that the manifests already exist.
+			if err := validateManifestsPath(
+				flags.apiServerManifestPath,
+				flags.controllerManagerManifestPath,
+				flags.schedulerManifestPath); err != nil {
+				return err
+			}
 			return runDiff(flags, args)
 		},
 	}
@@ -78,6 +85,25 @@ func newCmdDiff(out io.Writer) *cobra.Command {
 	cmd.Flags().IntVarP(&flags.contextLines, "context-lines", "c", 3, "How many lines of context in the diff")
 
 	return cmd
+}
+
+func validateManifestsPath(manifests ...string) (err error) {
+	for _, manifestPath := range manifests {
+		if len(manifestPath) == 0 {
+			return errors.New("empty manifest path")
+		}
+		s, err := os.Stat(manifestPath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				return errors.Wrapf(err, "the manifest file %q does not exist", manifestPath)
+			}
+			return errors.Wrapf(err, "error obtaining stats for manifest file %q", manifestPath)
+		}
+		if s.IsDir() {
+			return errors.Errorf("%q is a directory", manifestPath)
+		}
+	}
+	return nil
 }
 
 func runDiff(flags *diffFlags, args []string) error {
