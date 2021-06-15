83e3ee780aded4ef5034f5117c2b7c1a6bce3c1b
Chris Henzie <chrishenzie@google.com>
2021-06-14 17:14:25 -0700

    Rename access mode contains helper method
    So it is consistent with other methods performing the same check (one
    for internal and external types)

--- pkg/volume/quobyte/quobyte.go
+++ pkg/volume/quobyte/quobyte.go
@@ -373,7 +373,7 @@ type quobyteVolumeProvisioner struct {
 }
 
 func (provisioner *quobyteVolumeProvisioner) Provision(selectedNode *v1.Node, allowedTopologies []v1.TopologySelectorTerm) (*v1.PersistentVolume, error) {
-	if !util.AccessModesContainedInAll(provisioner.plugin.GetAccessModes(), provisioner.options.PVC.Spec.AccessModes) {
+	if !util.ContainsAllAccessModes(provisioner.plugin.GetAccessModes(), provisioner.options.PVC.Spec.AccessModes) {
 		return nil, fmt.Errorf("invalid AccessModes %v: only AccessModes %v are supported", provisioner.options.PVC.Spec.AccessModes, provisioner.plugin.GetAccessModes())
 	}
 
