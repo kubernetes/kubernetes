ba4324e0f7b2b058dfd0c45f910d37f9528ae983
Jerry-Ge <jerry.ge@arm.com>
2021-06-17 11:24:51 +0800

    fix queue_test tests 2

--- test/integration/scheduler/queue_test.go
+++ test/integration/scheduler/queue_test.go
@@ -301,7 +301,7 @@ func TestCustomResourceEnqueue(t *testing.T) {
 		t.Fatalf("Cannot find the profile for Pod %v", podInfo.Pod.Name)
 	}
 	// Schedule the Pod manually.
-	_, fitError := testCtx.Scheduler.Algorithm.Schedule(ctx, nil, fwk, framework.NewCycleState(), podInfo.Pod)
+	_, fitError := testCtx.Scheduler.Algorithm.Schedule(30, ctx, nil, fwk, framework.NewCycleState(), podInfo.Pod)
 	// The fitError is expected to be non-nil as it failed the fakeCRPlugin plugin.
 	if fitError == nil {
 		t.Fatalf("Expect Pod %v to fail at scheduling.", podInfo.Pod.Name)
