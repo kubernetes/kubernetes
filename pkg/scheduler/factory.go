2fdb62fcdf81c4017190d63539e6bd4f40194457
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2021-06-19 18:07:31 +0900

    Fix frameworkImpl.extenders being not set

--- pkg/scheduler/factory.go
+++ pkg/scheduler/factory.go
@@ -144,6 +144,7 @@ func (c *Configurator) create() (*Scheduler, error) {
 		frameworkruntime.WithCaptureProfile(frameworkruntime.CaptureProfile(c.frameworkCapturer)),
 		frameworkruntime.WithClusterEventMap(clusterEventMap),
 		frameworkruntime.WithParallelism(int(c.parallellism)),
+		frameworkruntime.WithExtenders(extenders),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("initializing profiles: %v", err)
