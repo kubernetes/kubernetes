d111f67177c4343de140010ba5704bafd46ca4a8
Tamal Saha <tamal@appscode.com>
2021-06-26 02:09:40 -0700

    Allow custom transport even when client cert auth is used
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- staging/src/k8s.io/client-go/transport/transport.go
+++ staging/src/k8s.io/client-go/transport/transport.go
@@ -34,11 +34,6 @@ import (
 // New returns an http.RoundTripper that will provide the authentication
 // or transport level security defined by the provided Config.
 func New(config *Config) (http.RoundTripper, error) {
-	// Set transport level security
-	if config.Transport != nil && (config.HasCA() || config.HasCertAuth() || config.HasCertCallback() || config.TLS.Insecure) {
-		return nil, fmt.Errorf("using a custom transport with TLS certificate options or the insecure flag is not allowed")
-	}
-
 	var (
 		rt  http.RoundTripper
 		err error
