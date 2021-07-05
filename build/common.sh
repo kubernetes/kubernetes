cymtrick/kubernetes-error-fuzzer

Update debian-iptables image to buster-v1.6.5

build/common.sh
@@ -86,7 +86,7 @@ readonly KUBE_RSYNC_PORT="${KUBE_RSYNC_PORT:-}"
 readonly KUBE_CONTAINER_RSYNC_PORT=8730
 
 # These are the default versions (image tags) for their respective base images.
-readonly __default_debian_iptables_version=buster-v1.6.2
+readonly __default_debian_iptables_version=buster-v1.6.5
 readonly __default_go_runner_version=v2.3.1-go1.16.5-buster.0
 readonly __default_setcap_version=buster-v2.0.1
 

kubernetes/kubernetes

build/common.sh
@@ -93,7 +93,7 @@ readonly KUBE_CONTAINER_RSYNC_PORT=8730
 #
 # $1 - server architecture
 kube::build::get_docker_wrapped_binaries() {
-  local debian_iptables_version=buster-v1.6.1
+  local debian_iptables_version=buster-v1.6.5
   local go_runner_version=v2.3.1-go1.15.13-buster.0
   ### If you change any of these lists, please also update DOCKERIZED_BINARIES
   ### in build/BUILD. And kube::golang::server_image_targets