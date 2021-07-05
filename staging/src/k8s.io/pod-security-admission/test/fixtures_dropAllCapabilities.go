eefe26bee4b722d8dbabac532e8dd8973f1e97cc
mgutierrez98 <mannygutierrez919@gmail.com>
2021-07-05 01:07:57 +0000

    Implement check_dropAllCapabilities.go and test/fixtures_dropAllCapabilities.go

---
+++ staging/src/k8s.io/pod-security-admission/test/fixtures_dropAllCapabilities.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package test
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/pod-security-admission/api"
+)
+
+/*
+TODO: include field paths in reflect-based unit test
+containerFields: []string{
+	`securityContext.capabilities.drop`,
+},
+*/
+
+func init() {
+	fixtureData_1_22 := fixtureGenerator{
+		expectErrorSubstring: "drop all",
+		generatePass: func(p *corev1.Pod) []*corev1.Pod {
+			return []*corev1.Pod{p}
+		},
+		generateFail: func(p *corev1.Pod) []*corev1.Pod {
+			p = ensureCapabilities(p)
+			return []*corev1.Pod{
+				tweak(p, func(p *corev1.Pod) {
+					p.Spec.Containers[0].SecurityContext.Capabilities.Drop = []corev1.Capability{}
+				}),
+				tweak(p, func(p *corev1.Pod) {
+					p.Spec.InitContainers[0].SecurityContext.Capabilities.Drop = []corev1.Capability{}
+				}),
+			}
+		},
+	}
+
+	registerFixtureGenerator(
+		fixtureKey{level: api.LevelBaseline, version: api.MajorMinorVersion(1, 22), check: "dropAllCapabilities"},
+		fixtureData_1_22,
+	)
+}
