97ba475941f64f7ab41a92c51adeb4401146eed1
jyz0309 <45495947@qq.com>
2021-07-20 19:28:33 +0800

    add new metric
    Signed-off-by: jyz0309 <45495947@qq.com>
    
    add comment
    
    Signed-off-by: jyz0309 <45495947@qq.com>
    
    add promlint
    
    Signed-off-by: jyz0309 <45495947@qq.com>
    
    address comment
    
    Signed-off-by: jyz0309 <45495947@qq.com>
    
    go fmt code
    
    Signed-off-by: jyz0309 <45495947@qq.com>

--- staging/src/k8s.io/apiserver/pkg/endpoints/metrics/metrics.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/metrics/metrics.go
@@ -80,6 +80,14 @@ var (
 		},
 		[]string{"verb", "dry_run", "group", "version", "resource", "subresource", "scope", "component", "code"},
 	)
+	longRunningRequestsGauge = compbasemetrics.NewGaugeVec(
+		&compbasemetrics.GaugeOpts{
+			Name:           "apiserver_longrunning_requests",
+			Help:           "Gauge of all active long-running apiserver requests broken out by verb, group, version, resource, scope and component. Not all requests are tracked this way.",
+			StabilityLevel: compbasemetrics.ALPHA,
+		},
+		[]string{"verb", "group", "version", "resource", "subresource", "scope", "component"},
+	)
 	longRunningRequestGauge = compbasemetrics.NewGaugeVec(
 		&compbasemetrics.GaugeOpts{
 			Name:           "apiserver_longrunning_gauge",
@@ -234,6 +242,7 @@ var (
 	metrics = []resettableCollector{
 		deprecatedRequestGauge,
 		requestCounter,
+		longRunningRequestsGauge,
 		longRunningRequestGauge,
 		requestLatencies,
 		responseSizes,
@@ -408,7 +417,7 @@ func RecordLongRunning(req *http.Request, requestInfo *request.RequestInfo, comp
 	if requestInfo == nil {
 		requestInfo = &request.RequestInfo{Verb: req.Method, Path: req.URL.Path}
 	}
-	var g compbasemetrics.GaugeMetric
+	var g, e compbasemetrics.GaugeMetric
 	scope := CleanScope(requestInfo)
 
 	// We don't use verb from <requestInfo>, as this may be propagated from
@@ -418,12 +427,18 @@ func RecordLongRunning(req *http.Request, requestInfo *request.RequestInfo, comp
 	reportedVerb := cleanVerb(CanonicalVerb(strings.ToUpper(req.Method), scope), req)
 
 	if requestInfo.IsResourceRequest {
+		e = longRunningRequestsGauge.WithContext(req.Context()).WithLabelValues(reportedVerb, requestInfo.APIGroup, requestInfo.APIVersion, requestInfo.Resource, requestInfo.Subresource, scope, component)
 		g = longRunningRequestGauge.WithContext(req.Context()).WithLabelValues(reportedVerb, requestInfo.APIGroup, requestInfo.APIVersion, requestInfo.Resource, requestInfo.Subresource, scope, component)
 	} else {
+		e = longRunningRequestsGauge.WithContext(req.Context()).WithLabelValues(reportedVerb, "", "", "", requestInfo.Path, scope, component)
 		g = longRunningRequestGauge.WithContext(req.Context()).WithLabelValues(reportedVerb, "", "", "", requestInfo.Path, scope, component)
 	}
+	e.Inc()
 	g.Inc()
-	defer g.Dec()
+	defer func() {
+		e.Dec()
+		g.Dec()
+	}()
 	fn()
 }
 
