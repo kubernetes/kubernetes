a1cf44eab44f21c3929ff2d79501c56e6fbcddc3
wojtekt <wojtekt@google.com>
2021-07-21 12:14:30 +0200

    Remove unused promise code from APF

--- staging/src/k8s.io/apiserver/pkg/util/flowcontrol/fairqueuing/promise/lockingpromise/lockingpromise.go
+++ staging/src/k8s.io/apiserver/pkg/util/flowcontrol/fairqueuing/promise/lockingpromise/lockingpromise.go
@@ -23,13 +23,13 @@ import (
 	"k8s.io/apiserver/pkg/util/flowcontrol/fairqueuing/promise"
 )
 
-// promisoid is the data and behavior common to all the promise-like
-// abstractions implemented here.  This implementation is based on a
-// condition variable.  This implementation tracks active goroutines:
+// lockingPromise implementss the promise.LockingWriteOnce interface.
+// This implementation is based on a condition variable.
+// This implementation tracks active goroutines:
 // the given counter is decremented for a goroutine waiting for this
 // varible to be set and incremented when such a goroutine is
 // unblocked.
-type promisoid struct {
+type lockingPromise struct {
 	lock          sync.Locker
 	cond          sync.Cond
 	activeCounter counter.GoRoutineCounter // counter of active goroutines
@@ -38,87 +38,58 @@ type promisoid struct {
 	value         interface{}
 }
 
-func (pr *promisoid) Get() interface{} {
-	pr.lock.Lock()
-	defer pr.lock.Unlock()
-	return pr.GetLocked()
-}
-
-func (pr *promisoid) GetLocked() interface{} {
-	if !pr.isSet {
-		pr.waitingCount++
-		pr.activeCounter.Add(-1)
-		pr.cond.Wait()
-	}
-	return pr.value
-}
-
-func (pr *promisoid) IsSet() bool {
-	pr.lock.Lock()
-	defer pr.lock.Unlock()
-	return pr.IsSetLocked()
-}
-
-func (pr *promisoid) IsSetLocked() bool {
-	return pr.isSet
-}
-
-func (pr *promisoid) SetLocked(value interface{}) {
-	pr.isSet = true
-	pr.value = value
-	if pr.waitingCount > 0 {
-		pr.activeCounter.Add(pr.waitingCount)
-		pr.waitingCount = 0
-		pr.cond.Broadcast()
-	}
-}
-
-type writeOnce struct {
-	promisoid
-}
-
-var _ promise.LockingWriteOnce = &writeOnce{}
+var _ promise.LockingWriteOnce = &lockingPromise{}
 
 // NewWriteOnce makes a new promise.LockingWriteOnce
 func NewWriteOnce(lock sync.Locker, activeCounter counter.GoRoutineCounter) promise.LockingWriteOnce {
-	return &writeOnce{promisoid{
+	return &lockingPromise{
 		lock:          lock,
 		cond:          *sync.NewCond(lock),
 		activeCounter: activeCounter,
-	}}
+	}
 }
 
-func (wr *writeOnce) Set(value interface{}) bool {
-	wr.lock.Lock()
-	defer wr.lock.Unlock()
-	return wr.SetLocked(value)
+func (p *lockingPromise) Get() interface{} {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	return p.GetLocked()
 }
 
-func (wr *writeOnce) SetLocked(value interface{}) bool {
-	if wr.isSet {
+func (p *lockingPromise) GetLocked() interface{} {
+	if !p.isSet {
+		p.waitingCount++
+		p.activeCounter.Add(-1)
+		p.cond.Wait()
+	}
+	return p.value
+}
+
+func (p *lockingPromise) IsSet() bool {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	return p.IsSetLocked()
+}
+
+func (p *lockingPromise) IsSetLocked() bool {
+	return p.isSet
+}
+
+func (p *lockingPromise) Set(value interface{}) bool {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	return p.SetLocked(value)
+}
+
+func (p *lockingPromise) SetLocked(value interface{}) bool {
+	if p.isSet {
 		return false
 	}
-	wr.promisoid.SetLocked(value)
+	p.isSet = true
+	p.value = value
+	if p.waitingCount > 0 {
+		p.activeCounter.Add(p.waitingCount)
+		p.waitingCount = 0
+		p.cond.Broadcast()
+	}
 	return true
-}
-
-type writeMultiple struct {
-	promisoid
-}
-
-var _ promise.LockingWriteMultiple = &writeMultiple{}
-
-// NewWriteMultiple makes a new promise.LockingWriteMultiple
-func NewWriteMultiple(lock sync.Locker, activeCounter counter.GoRoutineCounter) promise.LockingWriteMultiple {
-	return &writeMultiple{promisoid{
-		lock:          lock,
-		cond:          *sync.NewCond(lock),
-		activeCounter: activeCounter,
-	}}
-}
-
-func (wr *writeMultiple) Set(value interface{}) {
-	wr.lock.Lock()
-	defer wr.lock.Unlock()
-	wr.SetLocked(value)
 }
--- staging/src/k8s.io/apiserver/pkg/util/flowcontrol/fairqueuing/promise/lockingpromise/lockingpromise_test.go
+++ staging/src/k8s.io/apiserver/pkg/util/flowcontrol/fairqueuing/promise/lockingpromise/lockingpromise_test.go
@@ -25,67 +25,6 @@ import (
 	"k8s.io/apiserver/pkg/util/flowcontrol/fairqueuing/testing/clock"
 )
 
-func TestLockingWriteMultiple(t *testing.T) {
-	now := time.Now()
-	clock, counter := clock.NewFakeEventClock(now, 0, nil)
-	var lock sync.Mutex
-	wr := NewWriteMultiple(&lock, counter)
-	var gots int32
-	var got atomic.Value
-	counter.Add(1)
-	go func() {
-		got.Store(wr.Get())
-		atomic.AddInt32(&gots, 1)
-		counter.Add(-1)
-	}()
-	clock.Run(nil)
-	time.Sleep(time.Second)
-	if atomic.LoadInt32(&gots) != 0 {
-		t.Error("Get returned before Set")
-	}
-	if wr.IsSet() {
-		t.Error("IsSet before Set")
-	}
-	aval := &now
-	wr.Set(aval)
-	clock.Run(nil)
-	time.Sleep(time.Second)
-	if atomic.LoadInt32(&gots) != 1 {
-		t.Error("Get did not return after Set")
-	}
-	if got.Load() != aval {
-		t.Error("Get did not return what was Set")
-	}
-	if !wr.IsSet() {
-		t.Error("IsSet()==false after Set")
-	}
-	counter.Add(1)
-	go func() {
-		got.Store(wr.Get())
-		atomic.AddInt32(&gots, 1)
-		counter.Add(-1)
-	}()
-	clock.Run(nil)
-	time.Sleep(time.Second)
-	if atomic.LoadInt32(&gots) != 2 {
-		t.Error("Second Get did not return immediately")
-	}
-	if got.Load() != aval {
-		t.Error("Second Get did not return what was Set")
-	}
-	if !wr.IsSet() {
-		t.Error("IsSet()==false after second Set")
-	}
-	later := time.Now()
-	bval := &later
-	wr.Set(bval)
-	if !wr.IsSet() {
-		t.Error("IsSet() returned false after second set")
-	} else if wr.Get() != bval {
-		t.Error("Get() after second Set returned wrong value")
-	}
-}
-
 func TestLockingWriteOnce(t *testing.T) {
 	now := time.Now()
 	clock, counter := clock.NewFakeEventClock(now, 0, nil)
