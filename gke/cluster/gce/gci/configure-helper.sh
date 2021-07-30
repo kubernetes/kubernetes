fa7156d0e7d7056955bda8a0cd93cb87ac37ba14
Yan Huang <yaniscoding@google.com>
2021-07-30 00:51:31 -0700

    In copy-manifests, make sure files exist before chmod
    Change-Id: I81ce548433f685a3f69247f0bf74cd011e2b87dd

--- gke/cluster/gce/gci/configure-helper.sh
+++ gke/cluster/gce/gci/configure-helper.sh
@@ -2446,7 +2446,9 @@ function copy-manifests {
   fi
   chown -R root:root "${dst_dir}"
   chmod 755 "${dst_dir}"
-  chmod 644 "${dst_dir}"/*
+  if [[ "$(ls ${dst_dir})" ]]; then
+    chmod 644 "${dst_dir}"/*
+  fi
 }
 
 # Fluentd resources are modified using ScalingPolicy CR, which may not be
