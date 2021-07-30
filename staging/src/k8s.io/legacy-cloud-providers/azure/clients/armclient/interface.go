c10c92bda9d226ab522c6050f901a9eac6076980
vikram Jadhav <vikramcse.10@gmail.com>
2021-07-30 17:41:26 +0000

    changes made by introducing mockgen command

--- staging/src/k8s.io/legacy-cloud-providers/azure/clients/armclient/interface.go
+++ staging/src/k8s.io/legacy-cloud-providers/azure/clients/armclient/interface.go
@@ -17,6 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//go:generate mockgen -copyright_file=$BUILD_TAG_FILE -source=interface.go  -destination=mockarmclient/interface.go -package=mockarmclient Interface
 package armclient
 
 import (
@@ -35,8 +36,6 @@ type PutResourcesResponse struct {
 }
 
 // Interface is the client interface for ARM.
-// Don't forget to run the following command to generate the mock client:
-// mockgen -source=$GOPATH/src/k8s.io/kubernetes/staging/src/k8s.io/legacy-cloud-providers/azure/clients/armclient/interface.go -package=mockarmclient Interface > $GOPATH/src/k8s.io/kubernetes/staging/src/k8s.io/legacy-cloud-providers/azure/clients/armclient/mockarmclient/interface.go
 type Interface interface {
 	// Send sends a http request to ARM service with possible retry to regional ARM endpoint.
 	Send(ctx context.Context, request *http.Request) (*http.Response, *retry.Error)
--- staging/src/k8s.io/legacy-cloud-providers/azure/clients/securitygroupclient/interface.go
+++ staging/src/k8s.io/legacy-cloud-providers/azure/clients/securitygroupclient/interface.go
@@ -17,6 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//go:generate mockgen -copyright_file=$BUILD_TAG_FILE -source=interface.go  -destination=mocksecuritygroupclient/interface.go -package=mocksecuritygroupclient Interface
 package securitygroupclient
 
 import (
@@ -32,8 +33,6 @@ const (
 )
 
 // Interface is the client interface for SecurityGroups.
-// Don't forget to run the following command to generate the mock client:
-// mockgen -source=$GOPATH/src/k8s.io/kubernetes/staging/src/k8s.io/legacy-cloud-providers/azure/clients/securitygroupclient/interface.go -package=mocksecuritygroupclient Interface > $GOPATH/src/k8s.io/kubernetes/staging/src/k8s.io/legacy-cloud-providers/azure/clients/securitygroupclient/mocksecuritygroupclient/interface.go
 type Interface interface {
 	// Get gets a SecurityGroup.
 	Get(ctx context.Context, resourceGroupName string, networkSecurityGroupName string, expand string) (result network.SecurityGroup, rerr *retry.Error)
