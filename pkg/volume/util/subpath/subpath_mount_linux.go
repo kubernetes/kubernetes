7ca77c087fd6559a2c31b73d455f31c456540d26
Mauricio Poppe <mauriciopoppe@google.com>
2021-08-09 21:33:14 +0000

    Update subpath_mount_linux.go disclaimer date and package name

--- pkg/volume/util/subpath/subpath_mount_linux.go
+++ pkg/volume/util/subpath/subpath_mount_linux.go
@@ -1,7 +1,7 @@
 // +build linux
 
 /*
-Copyright 2014 The Kubernetes Authors.
+Copyright 2021 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package mount
+package subpath
 
 import (
 	"fmt"
