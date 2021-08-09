1ceb118e3cb17b3e08059926f5138af51dd18899
Jordan Liggitt <liggitt@google.com>
2021-08-09 12:38:29 -0400

    Drop legacy status logic for volumeattachments API

--- pkg/registry/storage/volumeattachment/strategy.go
+++ pkg/registry/storage/volumeattachment/strategy.go
@@ -19,12 +19,9 @@ package volumeattachment
 import (
 	"context"
 
-	storageapiv1beta1 "k8s.io/api/storage/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/validation/field"
-	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
 	"k8s.io/apiserver/pkg/storage/names"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	"k8s.io/kubernetes/pkg/api/legacyscheme"
@@ -62,20 +59,8 @@ func (volumeAttachmentStrategy) GetResetFields() map[fieldpath.APIVersion]*field
 
 // ResetBeforeCreate clears the Status field which is not allowed to be set by end users on creation.
 func (volumeAttachmentStrategy) PrepareForCreate(ctx context.Context, obj runtime.Object) {
-	var groupVersion schema.GroupVersion
-
-	if requestInfo, found := genericapirequest.RequestInfoFrom(ctx); found {
-		groupVersion = schema.GroupVersion{Group: requestInfo.APIGroup, Version: requestInfo.APIVersion}
-	}
-
 	volumeAttachment := obj.(*storage.VolumeAttachment)
-
-	switch groupVersion {
-	case storageapiv1beta1.SchemeGroupVersion:
-		// allow modification of status for v1beta1
-	default:
-		volumeAttachment.Status = storage.VolumeAttachmentStatus{}
-	}
+	volumeAttachment.Status = storage.VolumeAttachmentStatus{}
 
 	if !utilfeature.DefaultFeatureGate.Enabled(features.CSIMigration) {
 		volumeAttachment.Spec.Source.InlineVolumeSpec = nil
@@ -88,19 +73,8 @@ func (volumeAttachmentStrategy) Validate(ctx context.Context, obj runtime.Object
 
 	errs := validation.ValidateVolumeAttachment(volumeAttachment)
 
-	var groupVersion schema.GroupVersion
-
-	if requestInfo, found := genericapirequest.RequestInfoFrom(ctx); found {
-		groupVersion = schema.GroupVersion{Group: requestInfo.APIGroup, Version: requestInfo.APIVersion}
-	}
-
-	switch groupVersion {
-	case storageapiv1beta1.SchemeGroupVersion:
-		// no extra validation
-	default:
-		// tighten up validation of newly created v1 attachments
-		errs = append(errs, validation.ValidateVolumeAttachmentV1(volumeAttachment)...)
-	}
+	// tighten up validation of newly created v1 attachments
+	errs = append(errs, validation.ValidateVolumeAttachmentV1(volumeAttachment)...)
 	return errs
 }
 
@@ -119,22 +93,11 @@ func (volumeAttachmentStrategy) AllowCreateOnUpdate() bool {
 
 // PrepareForUpdate sets the Status fields which is not allowed to be set by an end user updating a VolumeAttachment
 func (volumeAttachmentStrategy) PrepareForUpdate(ctx context.Context, obj, old runtime.Object) {
-	var groupVersion schema.GroupVersion
-
-	if requestInfo, found := genericapirequest.RequestInfoFrom(ctx); found {
-		groupVersion = schema.GroupVersion{Group: requestInfo.APIGroup, Version: requestInfo.APIVersion}
-	}
-
 	newVolumeAttachment := obj.(*storage.VolumeAttachment)
 	oldVolumeAttachment := old.(*storage.VolumeAttachment)
 
-	switch groupVersion {
-	case storageapiv1beta1.SchemeGroupVersion:
-		// allow modification of Status via main resource for v1beta1
-	default:
-		newVolumeAttachment.Status = oldVolumeAttachment.Status
-		// No need to increment Generation because we don't allow updates to spec
-	}
+	newVolumeAttachment.Status = oldVolumeAttachment.Status
+	// No need to increment Generation because we don't allow updates to spec
 
 	if !utilfeature.DefaultFeatureGate.Enabled(features.CSIMigration) && oldVolumeAttachment.Spec.Source.InlineVolumeSpec == nil {
 		newVolumeAttachment.Spec.Source.InlineVolumeSpec = nil
