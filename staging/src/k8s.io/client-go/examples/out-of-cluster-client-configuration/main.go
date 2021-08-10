fa3057158db2aed59661a42dc610dd210f3667b8
liudui <1693291525@qq.com>
2021-08-10 11:01:35 +0800

    remove import common

--- staging/src/k8s.io/client-go/examples/out-of-cluster-client-configuration/main.go
+++ staging/src/k8s.io/client-go/examples/out-of-cluster-client-configuration/main.go
@@ -29,15 +29,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
-	//
-	// Uncomment to load all auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth"
-	//
-	// Or uncomment to load specific auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
 
 func main() {
