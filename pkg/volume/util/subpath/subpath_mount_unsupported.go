8337e4764e1efe883b9481d8ffe1e265e44cfe0b
Mauricio Poppe <mauriciopoppe@google.com>
2021-08-13 19:11:12 +0000

    New subpath Mounter implements mount.Interface, update kubelet startup
    - The kubelet startup code will create a new Mounter instance
      that wraps the existing k8s.io/utils/mount instance
    - Provided implementations for all the platforms

--- pkg/volume/util/subpath/subpath_mount_unsupported.go
+++ pkg/volume/util/subpath/subpath_mount_unsupported.go
@@ -1,7 +1,7 @@
 // +build !linux,!windows
 
 /*
-Copyright 2014 The Kubernetes Authors.
+Copyright 2021 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,62 +16,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package mount
+package subpath
 
 import (
 	"errors"
+
+	"k8s.io/utils/mount"
 )
 
 // Mounter implements mount.Interface for unsupported platforms
 type Mounter struct {
+	mount.Interface
 	mounterPath string
 }
 
-var errUnsupported = errors.New("util/mount on this platform is not supported")
+var errUnsupported = errors.New("utils/mount on this platform is not supported")
 
-// New returns a mount.Interface for the current system.
+// NewMounter returns a MountInterface for the current system.
 // It provides options to override the default mounter behavior.
 // mounterPath allows using an alternative to `/bin/mount` for mounting.
-func New(mounterPath string) Interface {
+func NewMounter(mounter mount.Interface, mounterPath string) MountInterface {
 	return &Mounter{
+		Interface:   mounter,
 		mounterPath: mounterPath,
 	}
-}
-
-// Mount always returns an error on unsupported platforms
-func (mounter *Mounter) Mount(source string, target string, fstype string, options []string) error {
-	return errUnsupported
-}
-
-// Mount always returns an error on unsupported platforms
-func (mounter *Mounter) MountSensitive(source string, target string, fstype string, options []string, sensitiveOptions []string) error {
-	return errUnsupported
-}
-
-// Unmount always returns an error on unsupported platforms
-func (mounter *Mounter) Unmount(target string) error {
-	return errUnsupported
-}
-
-// List always returns an error on unsupported platforms
-func (mounter *Mounter) List() ([]MountPoint, error) {
-	return []MountPoint{}, errUnsupported
-}
-
-// IsLikelyNotMountPoint always returns an error on unsupported platforms
-func (mounter *Mounter) IsLikelyNotMountPoint(file string) (bool, error) {
-	return true, errUnsupported
-}
-
-// GetMountRefs always returns an error on unsupported platforms
-func (mounter *Mounter) GetMountRefs(pathname string) ([]string, error) {
-	return nil, errUnsupported
-}
-
-func (mounter *SafeFormatAndMount) formatAndMountSensitive(source string, target string, fstype string, options []string, sensitiveOptions []string) error {
-	return mounter.Interface.Mount(source, target, fstype, options)
-}
-
-func (mounter *SafeFormatAndMount) diskLooksUnformatted(disk string) (bool, error) {
-	return true, errUnsupported
 }
