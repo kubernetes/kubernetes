1f70fe202cf9d920eae6061952dabaa5e7a55f29
Jordan Liggitt <liggitt@google.com>
2021-08-14 17:34:25 -0400

    Fix go build to use module paths

--- hack/lib/golang.sh
+++ hack/lib/golang.sh
@@ -814,7 +814,8 @@ kube::golang::build_binaries() {
         # Assume arguments starting with a dash are flags to pass to go.
         goflags+=("${arg}")
       else
-        targets+=("${arg}")
+        # Strip vendor/ prefix if needed, since we're building in gomodule mode.
+        targets+=("${arg#"vendor/"}")
       fi
     done
 
