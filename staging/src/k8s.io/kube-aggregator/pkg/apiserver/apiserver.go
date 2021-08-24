30214940c21e3affb0c1f0dba09dd83f9e752698
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2021-08-24 15:13:04 +0200

    UPSTREAM: <carry>: skip posting failures to aggregated APIs to avoid getting false positives until the server becomes ready
    the availability checks depend on fully initialized SDN
    OpenShift carries a few reachability checks that affect /readyz protocol
    we skip posting failures to avoid getting false positives until the server becomes ready
    
    UPSTREAM: <carry>: skip posting failures to aggregated APIs to avoid getting false positives until the server becomes ready
    
    marks availability of the server before checking the aggregate APIs
    as it can change as we are running the checks.
    in that case, skip posting failures to avoid false positives.
    
    note on the next rebase please squash with the previous commit

--- staging/src/k8s.io/kube-aggregator/pkg/apiserver/apiserver.go
+++ staging/src/k8s.io/kube-aggregator/pkg/apiserver/apiserver.go
@@ -273,6 +273,7 @@ func (c completedConfig) NewWithDelegate(delegationTarget genericapiserver.Deleg
 		(func() ([]byte, []byte))(s.proxyCurrentCertKeyContent),
 		s.serviceResolver,
 		c.GenericConfig.EgressSelector,
+		c.GenericConfig.LifecycleSignals.HasBeenReady.Signaled(),
 	)
 	if err != nil {
 		return nil, err
