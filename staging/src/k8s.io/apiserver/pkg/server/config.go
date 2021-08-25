95990fa2fe4e1b5301c851d45338f525644d5e82
Abu Kashem <akashem@redhat.com>
2021-08-25 16:13:46 -0400

    UPSTREAM: <carry>: send Retry-After when not ready with a caller opt in
    UPSTREAM: <carry>: change opt-in due to upstream revert
    
    OpenShift-Rebase-Source: cd08005d72d

--- staging/src/k8s.io/apiserver/pkg/server/config.go
+++ staging/src/k8s.io/apiserver/pkg/server/config.go
@@ -1118,6 +1118,7 @@ func DefaultBuildHandlerChain(apiHandler http.Handler, c *Config) http.Handler {
 	if c.ShutdownSendRetryAfter {
 		handler = genericfilters.WithRetryAfter(handler, c.lifecycleSignals.NotAcceptingNewRequest.Signaled())
 	}
+	handler = genericfilters.WithOptInRetryAfter(handler, c.newServerFullyInitializedFunc())
 	handler = genericfilters.WithHTTPLogging(handler, c.newIsTerminatingFunc())
 	handler = genericapifilters.WithLatencyTrackers(handler)
 	// WithRoutine will execute future handlers in a separate goroutine and serving
