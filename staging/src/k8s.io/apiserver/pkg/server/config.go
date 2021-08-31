4954e48523c69c5e2900046ad35946c071384ea5
Abu Kashem <akashem@redhat.com>
2021-08-31 18:11:15 -0400

    UPSTREAM: <carry>: change opt-in due to upstream revert

--- staging/src/k8s.io/apiserver/pkg/server/config.go
+++ staging/src/k8s.io/apiserver/pkg/server/config.go
@@ -893,7 +893,7 @@ func DefaultBuildHandlerChain(apiHandler http.Handler, c *Config) http.Handler {
 	if c.ShutdownSendRetryAfter {
 		handler = genericfilters.WithRetryAfter(handler, c.lifecycleSignals.AfterShutdownDelayDuration.Signaled())
 	}
-	handler = genericfilters.WithOptInRetryAfter(handler, c.newNotReadyRetryAfterFunc())
+	handler = genericfilters.WithOptInRetryAfter(handler, c.newServerFullyInitializedFunc())
 	handler = genericfilters.WithHTTPLogging(handler, c.newIsTerminatingFunc())
 	if utilfeature.DefaultFeatureGate.Enabled(genericfeatures.APIServerTracing) {
 		handler = genericapifilters.WithTracing(handler, c.TracerProvider)
