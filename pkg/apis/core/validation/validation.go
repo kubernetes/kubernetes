3a927cc6b3f157bb2b6d3c1737c2c96ba3aacee8
Andrew Sy Kim <kim.andrewsy@gmail.com>
2021-09-08 13:43:26 -0400

    core/v1: stop defaulting Service internalTrafficPolicy when Type=ExternalName
    Signed-off-by: Andrew Sy Kim <kim.andrewsy@gmail.com>

--- pkg/apis/core/validation/validation.go
+++ pkg/apis/core/validation/validation.go
@@ -4809,7 +4809,10 @@ func validateServiceInternalTrafficFieldsValue(service *core.Service) field.Erro
 
 	if utilfeature.DefaultFeatureGate.Enabled(features.ServiceInternalTrafficPolicy) {
 		if service.Spec.InternalTrafficPolicy == nil {
-			allErrs = append(allErrs, field.Required(field.NewPath("spec").Child("internalTrafficPolicy"), ""))
+			if service.Spec.Type == core.ServiceTypeNodePort ||
+				service.Spec.Type == core.ServiceTypeLoadBalancer || service.Spec.Type == core.ServiceTypeClusterIP {
+				allErrs = append(allErrs, field.Required(field.NewPath("spec").Child("internalTrafficPolicy"), ""))
+			}
 		}
 	}
 
