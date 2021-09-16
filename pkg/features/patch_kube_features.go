9d48b717222ed350b41374d1986a40411024008c
Fabio Bertinatto <fbertina@redhat.com>
2021-09-16 09:46:43 -0300

    UPSTREAM: <carry>: add CSI migration feature gates for vSphere and Azure File
    This commit is the next natural step for commits 2d9a8f90b24
    and d37e84c5426. It introduces custom feature gates to enable
    the CSI migration in vSphere and Azure File plugins.
    
    See openshift/enhancements#549 for details.
    
    Stop <carrying> the patch when CSI migration becomes GA (i.e.
    features.CSIMigrationAzureFile / features.CSIMigrationVSphere
    are GA).
    
    openshift-rebase(v1.24):source=2701d71abb4

--- pkg/features/patch_kube_features.go
+++ pkg/features/patch_kube_features.go
@@ -19,6 +19,12 @@ var (
 	// Enables the Azure Disk CSI migration for the Attach/Detach controller (ADC) only.
 	ADCCSIMigrationAzureDisk featuregate.Feature = "ADC_CSIMigrationAzureDisk"
 
+	// owner: @fbertina
+	// beta: v1.23
+	//
+	// Enables the Azure File CSI migration for the Attach/Detach controller (ADC) only.
+	ADCCSIMigrationAzureFile featuregate.Feature = "ADC_CSIMigrationAzureFile"
+
 	// owner: @jsafrane
 	// beta: v1.21
 	//
@@ -30,13 +36,21 @@ var (
 	//
 	// Enables the GCE CSI migration for the Attach/Detach controller (ADC) only.
 	ADCCSIMigrationGCEPD featuregate.Feature = "ADC_CSIMigrationGCEPD"
+
+	// owner: @fbertina
+	// beta: v1.23
+	//
+	// Enables the vSphere CSI migration for the Attach/Detach controller (ADC) only.
+	ADCCSIMigrationVSphere featuregate.Feature = "ADC_CSIMigrationVSphere"
 )
 
 var ocpDefaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{
 	ADCCSIMigrationAWS:       {Default: true, PreRelease: featuregate.Beta},
 	ADCCSIMigrationAzureDisk: {Default: true, PreRelease: featuregate.Beta},
+	ADCCSIMigrationAzureFile: {Default: true, PreRelease: featuregate.Beta},
 	ADCCSIMigrationCinder:    {Default: true, PreRelease: featuregate.Beta},
 	ADCCSIMigrationGCEPD:     {Default: true, PreRelease: featuregate.Beta},
+	ADCCSIMigrationVSphere:   {Default: true, PreRelease: featuregate.Beta},
 }
 
 func init() {
