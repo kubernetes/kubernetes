c651d502022c30180356f4c0a32cc528858437ab
Karthik K N <karthikkn1997@gmail.com>
2021-09-17 16:27:53 +0530

    Fix incorrect format specifier in test files

--- pkg/controller/job/job_controller_test.go
+++ pkg/controller/job/job_controller_test.go
@@ -1539,7 +1539,7 @@ func TestTrackJobStatusAndRemoveFinalizers(t *testing.T) {
 			succeededIndexes := succeededIndexesFromJob(&tc.job)
 			err := manager.trackJobStatusAndRemoveFinalizers(&tc.job, tc.pods, succeededIndexes, *uncounted, tc.finishedCond, tc.needsFlush)
 			if !errors.Is(err, tc.wantErr) {
-				t.Errorf("Got error %v, want %w", err, tc.wantErr)
+				t.Errorf("Got error %v, want %v", err, tc.wantErr)
 			}
 			if diff := cmp.Diff(tc.wantStatusUpdates, statusUpdates); diff != "" {
 				t.Errorf("Unexpected status updates (-want,+got):\n%s", diff)
