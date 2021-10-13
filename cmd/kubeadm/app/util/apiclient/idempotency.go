8c2067111223728a155daad94a111d1cef9279dc
谢瑶瑶 <yaoyao.xyy@alibaba-inc.com>
2021-10-13 11:09:57 +0800

    GetConfigMapWithRetry is a common function, should not share global default backoff with others  which would cause unexpected situation unless you known what you are doing
    Signed-off-by: 谢瑶瑶 <yaoyao.xyy@alibaba-inc.com>

--- cmd/kubeadm/app/util/apiclient/idempotency.go
+++ cmd/kubeadm/app/util/apiclient/idempotency.go
@@ -19,6 +19,8 @@ package apiclient
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"time"
 
 	"github.com/pkg/errors"
 
@@ -31,8 +33,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
 	"k8s.io/apimachinery/pkg/util/wait"
 	clientset "k8s.io/client-go/kubernetes"
-	clientsetretry "k8s.io/client-go/util/retry"
-
 	"k8s.io/kubernetes/cmd/kubeadm/app/constants"
 )
 
@@ -346,15 +346,25 @@ func PatchNode(client clientset.Interface, nodeName string, patchFn func(*v1.Nod
 func GetConfigMapWithRetry(client clientset.Interface, namespace, name string) (*v1.ConfigMap, error) {
 	var cm *v1.ConfigMap
 	var lastError error
-	err := wait.ExponentialBackoff(clientsetretry.DefaultBackoff, func() (bool, error) {
-		var err error
-		cm, err = client.CoreV1().ConfigMaps(namespace).Get(context.TODO(), name, metav1.GetOptions{})
-		if err == nil {
-			return true, nil
-		}
-		lastError = err
-		return false, nil
-	})
+	backoff := wait.Backoff{
+		Steps:    4,
+		Duration: 10 * time.Millisecond,
+		Factor:   5.0,
+		Jitter:   0.1,
+	}
+	err := wait.ExponentialBackoff(
+		backoff,
+		func() (bool, error) {
+			var err error
+			cm, err = client.CoreV1().ConfigMaps(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+			if err == nil {
+				return true, nil
+			}
+			lastError = err
+			fmt.Printf("warning: exponential backoff get configmap[%s/%s], %s\n", namespace, name, lastError.Error())
+			return false, nil
+		},
+	)
 	if err == nil {
 		return cm, nil
 	}
