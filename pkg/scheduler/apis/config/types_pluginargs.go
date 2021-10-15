fc9533e72fe0089c9c839dd1cd2bb241d32b9c75
kerthcet <kerthcet@gmail.com>
2021-10-15 22:10:31 +0800

    remove scheduler ServiceAffinity plugin
    Signed-off-by: kerthcet <kerthcet@gmail.com>

--- pkg/scheduler/apis/config/types_pluginargs.go
+++ pkg/scheduler/apis/config/types_pluginargs.go
@@ -198,25 +198,6 @@ type ResourceSpec struct {
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// ServiceAffinityArgs holds arguments used to configure the ServiceAffinity
-// plugin.
-//
-// This plugin has been deprecated and is only configurable through the
-// scheduler policy API and the v1beta1 component config API. It is recommended
-// to use the InterPodAffinity plugin instead.
-type ServiceAffinityArgs struct {
-	metav1.TypeMeta
-
-	// AffinityLabels are homogeneous for pods that are scheduled to a node.
-	// (i.e. it returns true IFF this pod can be added to this node such that all other pods in
-	// the same service are running on nodes with the exact same values for Labels).
-	AffinityLabels []string
-	// AntiAffinityLabelsPreference are the labels to consider for service anti affinity scoring.
-	AntiAffinityLabelsPreference []string
-}
-
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
 // VolumeBindingArgs holds arguments used to configure the VolumeBinding plugin.
 type VolumeBindingArgs struct {
 	metav1.TypeMeta
