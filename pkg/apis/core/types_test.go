d1d7882186b6ff51da1e0979f8294c1bc42b3f69
Lee Verberne <verb@google.com>
2021-10-15 17:50:37 -0700

    Add test for EphemeralContainerCommon struct tags
    The tags for type EphemeralContainerCommon should be kept in sync with
    those of type Container.
    
    Co-authored-by: Jordan Liggitt <liggitt@google.com>

--- pkg/apis/core/types_test.go
+++ pkg/apis/core/types_test.go
@@ -14,31 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1
+package core
 
 import (
 	"reflect"
-	"strings"
 	"testing"
 )
-
-// Test_ServiceSpecRemovedFieldProtobufNumberReservation tests that the reserved protobuf field numbers
-// for removed fields are not re-used. DO NOT remove this test for any reason, this ensures that tombstoned
-// protobuf field numbers are not accidentally reused by other fields.
-func Test_ServiceSpecRemovedFieldProtobufNumberReservation(t *testing.T) {
-	obj := reflect.ValueOf(ServiceSpec{}).Type()
-	for i := 0; i < obj.NumField(); i++ {
-		f := obj.Field(i)
-
-		protobufNum := strings.Split(f.Tag.Get("protobuf"), ",")[1]
-		if protobufNum == "15" {
-			t.Errorf("protobuf 15 in ServiceSpec is reserved for removed ipFamily field")
-		}
-		if protobufNum == "16" {
-			t.Errorf("protobuf 16 in ServiceSpec is reserved for removed topologyKeys field")
-		}
-	}
-}
 
 // TestEphemeralContainer ensures that the tags of Container and EphemeralContainerCommon are kept in sync.
 func TestEphemeralContainer(t *testing.T) {
