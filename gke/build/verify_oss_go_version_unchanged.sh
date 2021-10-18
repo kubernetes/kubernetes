01647cd2c8edc0ce9b6297e3f631784a303daf27
Patrick Rhomberg <patrhom@google.com>
2021-10-18 22:42:05 +0000

    Examine only the Go version when checking boringcrypto for fips.
    BUG=b/190647454
    
    Change-Id: I85e21390d09ade0b158c755dfc59541f4095133d

--- gke/build/verify_oss_go_version_unchanged.sh
+++ gke/build/verify_oss_go_version_unchanged.sh
@@ -32,16 +32,31 @@ function get_last_updated_val_from_config()
   grep "last-updated-for-oss-kubecross-version" "${config_file}" | sed s/[[:space:]]*last-updated-for-oss-kubecross-version:[[:space:]]*//
 }
 
+function kubecross_version_to_go_semver()
+{
+  # Previous cross version scheme: v<Version>-<Build>
+  #   Example: v1.16.6-1
+  # Current cross naming scheme: <K8s version>-go<Go version>-<debian name>.<build>
+  #   Example: v1.23.0-go1.17.2-bullseye.0
+  local version="$1"
+  local left_strip_v=${version#v}
+  local oss_trim_left=${left_strip_v#*-go}  # drop "v1.23.0-go"
+  local oss_trimmed=${oss_trim_left%-*} # drop "-bullseye.0", left with just "1.17.2"
+  echo ${oss_trimmed}
+}
+
 log.info "Reading OSS kube-cross image version..."
 assert_path_exists "${OSS_KUBECROSS_VERSION_FILE}"
 oss_kubecross_version="$(cat "${OSS_KUBECROSS_VERSION_FILE}")"
-log.info "OSS kube-cross version: \"${oss_kubecross_version}\""
+oss_kubecross_go_version=$(kubecross_version_to_go_semver "${oss_kubecross_version}")
+log.info "OSS kube-cross complete version: \"${oss_kubecross_version}\""
+log.info "OSS kube-cross Go version: \"${oss_kubecross_go_version}\""
 
 last_updated_for_oss_kubecross_version="$(get_last_updated_val_from_config "${CONFIG_FILE}")"
 log.info "golang_boringcrypto was last updated for version: \"${last_updated_for_oss_kubecross_version}\""
 
-if [[ "${oss_kubecross_version}" != "${last_updated_for_oss_kubecross_version}" ]]; then
-  log.fail "Golang version was last updated for ${last_updated_for_oss_kubecross_version}, OSS is currently at ${oss_kubecross_version}. See http://goto.google.com/gke-security-compliance#periodics-gob-kubernetes-verify-golang-version-failure-playbook for resolution instructions."
+if [[ "${oss_kubecross_go_version}" != "${last_updated_for_oss_kubecross_version}" ]]; then
+  log.fail "Golang version was last updated for ${last_updated_for_oss_kubecross_version}, OSS is currently at Go version ${oss_kubecross_go_version} (full version: ${oss_kubecross_version}). See http://goto.google.com/gke-security-compliance#periodics-gob-kubernetes-verify-golang-version-failure-playbook for resolution instructions."
 fi
 
 log.info "OK!"
