e0d4e7db3573f33476f359886c3618ff596801ea
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2021-10-19 19:27:32 +0530

    Update pkg/proxy/winkernel/proxier.go
    Co-authored-by: Marek Siarkowicz <marek.siarkowicz@protonmail.com>

--- pkg/proxy/winkernel/proxier.go
+++ pkg/proxy/winkernel/proxier.go
@@ -320,7 +320,7 @@ func (proxier *Proxier) onEndpointsMapChange(svcPortName *proxy.ServicePortName)
 		svcInfo, ok := svc.(*serviceInfo)
 
 		if !ok {
-			klog.ErrorS(nil, "Failed to cast serviceInfo", "svcPortName", svcPortName)
+			klog.ErrorS(nil, "Failed to cast serviceInfo", "servicePortName", svcPortName)
 			return
 		}
 
