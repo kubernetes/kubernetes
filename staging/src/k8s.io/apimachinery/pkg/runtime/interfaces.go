c1a237f8be31b84f7ca4edc8a188ab577e7539c2
yuanjize <1224500078@qq.com>
2021-10-21 14:59:34 +0800

    Fix function comment typo

--- staging/src/k8s.io/apimachinery/pkg/runtime/interfaces.go
+++ staging/src/k8s.io/apimachinery/pkg/runtime/interfaces.go
@@ -153,7 +153,7 @@ type NegotiatedSerializer interface {
 	// EncoderForVersion returns an encoder that ensures objects being written to the provided
 	// serializer are in the provided group version.
 	EncoderForVersion(serializer Encoder, gv GroupVersioner) Encoder
-	// DecoderForVersion returns a decoder that ensures objects being read by the provided
+	// DecoderToVersion returns a decoder that ensures objects being read by the provided
 	// serializer are in the provided group version by default.
 	DecoderToVersion(serializer Decoder, gv GroupVersioner) Decoder
 }
