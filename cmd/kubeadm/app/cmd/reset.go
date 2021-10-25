9fac6f36e58935a972bf15bfb90f55060ba814a7
Lubomir I. Ivanov <lubomirivanov@vmware.com>
2021-10-25 22:47:15 +0300

    kubeadm: remove the reset/update-cluster-status phase
    The phase has been deprecated and a NO-OP since 1.22.
    Remove the phase related code.

--- cmd/kubeadm/app/cmd/reset.go
+++ cmd/kubeadm/app/cmd/reset.go
@@ -197,7 +197,6 @@ func newCmdReset(in io.Reader, out io.Writer, resetOptions *resetOptions) *cobra
 
 	// initialize the workflow runner with the list of phases
 	resetRunner.AppendPhase(phases.NewPreflightPhase())
-	resetRunner.AppendPhase(phases.NewUpdateClusterStatus())
 	resetRunner.AppendPhase(phases.NewRemoveETCDMemberPhase())
 	resetRunner.AppendPhase(phases.NewCleanupNodePhase())
 
