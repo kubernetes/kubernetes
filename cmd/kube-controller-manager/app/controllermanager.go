a99d71856f4e64ac96071853e392282028166ee4
Jan Chaloupka <jchaloup@redhat.com>
2021-10-26 17:31:33 +0200

    kube-controller-manager: install readyz endpoint
    The KCM has currently no /livez endpoint and /healthz is deprecated.

--- cmd/kube-controller-manager/app/controllermanager.go
+++ cmd/kube-controller-manager/app/controllermanager.go
@@ -207,6 +207,7 @@ func Run(c *config.CompletedConfig, stopCh <-chan struct{}) error {
 	var unsecuredMux *mux.PathRecorderMux
 	if c.SecureServing != nil {
 		unsecuredMux = genericcontrollermanager.NewBaseHandler(&c.ComponentConfig.Generic.Debugging, healthzHandler)
+		healthz.InstallReadyzHandler(unsecuredMux, checks...)
 		handler := genericcontrollermanager.BuildHandlerChain(unsecuredMux, &c.Authorization, &c.Authentication)
 		// TODO: handle stoppedCh returned by c.SecureServing.Serve
 		if _, err := c.SecureServing.Serve(handler, 0, stopCh); err != nil {
