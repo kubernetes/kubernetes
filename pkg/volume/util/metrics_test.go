f8da60e181dd459038dcb92cc6592d1789cf842d
Shuhei Kitagawa <shuhei.kitagawa@gmail.com>
2021-11-04 09:24:30 +0900

    Add unit tests for volume operation metrics

--- pkg/volume/util/metrics_test.go
+++ pkg/volume/util/metrics_test.go
@@ -20,9 +20,145 @@ import (
 	"fmt"
 	"testing"
 
+	dto "github.com/prometheus/client_model/go"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/component-base/metrics/legacyregistry"
 	"k8s.io/kubernetes/pkg/volume"
+	"k8s.io/kubernetes/pkg/volume/util/types"
 )
+
+func TestOperationCompleteHook(t *testing.T) {
+	var (
+		fakeFullPluginName = "kubernetes.io/fakePlugin"
+		fakeOperationName  = "volume_fake_operation_name"
+		fakeErr            = fmt.Errorf("fake error")
+		trueV              = true
+		falseV             = false
+	)
+
+	metrics := []string{"storage_operation_duration_seconds"}
+
+	opComplete := OperationCompleteHook(fakeFullPluginName, fakeOperationName)
+
+	testCase := []struct {
+		name         string
+		param        types.CompleteFuncParam
+		wantStatus   string
+		wantMigrated string
+	}{
+		{
+			name:         "error is empty and migrated is nil",
+			param:        types.CompleteFuncParam{},
+			wantStatus:   statusSuccess,
+			wantMigrated: "false",
+		},
+		{
+			name: "error is empty and migrated is false",
+			param: types.CompleteFuncParam{
+				Migrated: &falseV,
+			},
+			wantStatus:   statusSuccess,
+			wantMigrated: "false",
+		},
+		{
+			name: "error is empty and migrated is true",
+			param: types.CompleteFuncParam{
+				Migrated: &trueV,
+			},
+			wantStatus:   statusSuccess,
+			wantMigrated: "true",
+		},
+		{
+			name: "error exists and migrated is nil",
+			param: types.CompleteFuncParam{
+				Err: &fakeErr,
+			},
+			wantStatus:   statusFailUnknown,
+			wantMigrated: "false",
+		},
+		{
+			name: "error exists and migrated is false",
+			param: types.CompleteFuncParam{
+				Err:      &fakeErr,
+				Migrated: &falseV,
+			},
+			wantStatus:   statusFailUnknown,
+			wantMigrated: "false",
+		},
+		{
+			name: "error exists and migrated is true",
+			param: types.CompleteFuncParam{
+				Err:      &fakeErr,
+				Migrated: &trueV,
+			},
+			wantStatus:   statusFailUnknown,
+			wantMigrated: "true",
+		},
+	}
+
+	for _, test := range testCase {
+		t.Run(test.name, func(t *testing.T) {
+			storageOperationMetric.Reset()
+			defer storageOperationMetric.Reset()
+
+			opComplete(test.param)
+			raw, err := legacyregistry.DefaultGatherer.Gather()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			filtered := filterMetrics(raw, metrics)
+			if len(filtered) != 1 {
+				t.Errorf("invalid number of metric families. got: %v, expected: %v", len(filtered), len(metrics))
+			}
+
+			metricFamily := filtered[0]
+			if expected := "storage_operation_duration_seconds"; metricFamily.GetName() != expected {
+				t.Errorf("invalid metric family name. got: %v, expected: %v", metricFamily.GetName(), expected)
+			}
+
+			ms := metricFamily.GetMetric()
+			if got := len(ms); got != 1 {
+				t.Errorf("invalid number of metrics. got: %v, expected: %v", got, 1)
+			}
+
+			metric := ms[0]
+			labels := metric.GetLabel()
+			if got := len(labels); got != 4 {
+				t.Errorf("invalid number of labels. got: %v, expected: %v", got, 4)
+			}
+
+			for _, label := range labels {
+				got := label.GetValue()
+
+				switch label.GetName() {
+				case "volume_plugin":
+					if got != fakeFullPluginName {
+						t.Errorf("invalid volume plugin label value. got: %v, expected: %v", got, fakeFullPluginName)
+					}
+				case "operation_name":
+					if got != fakeOperationName {
+						t.Errorf("invalid operation name label value. got: %v, expected: %v", got, fakeOperationName)
+					}
+				case "status":
+					if got != test.wantStatus {
+						t.Errorf("invalid status label value. got: %v, expected: %v", got, test.wantStatus)
+					}
+				case "migrated":
+					if got != test.wantMigrated {
+						t.Errorf("invalid migrated label value. got: %v, expected: %v", got, test.wantMigrated)
+					}
+				default:
+					t.Errorf("unexpected label: %v", label.GetName())
+				}
+			}
+
+			if count := metric.GetHistogram().SampleCount; *count != 1 {
+				t.Errorf("invalid sample count. got: %v, expected: %v", *count, 1)
+			}
+		})
+	}
+}
 
 func TestGetFullQualifiedPluginNameForVolume(t *testing.T) {
 	var (
@@ -88,4 +224,17 @@ func TestGetFullQualifiedPluginNameForVolume(t *testing.T) {
 			}
 		})
 	}
+}
+
+func filterMetrics(metrics []*dto.MetricFamily, names []string) []*dto.MetricFamily {
+	var filtered []*dto.MetricFamily
+	for _, m := range metrics {
+		for _, name := range names {
+			if m.GetName() == name {
+				filtered = append(filtered, m)
+				break
+			}
+		}
+	}
+	return filtered
 }
