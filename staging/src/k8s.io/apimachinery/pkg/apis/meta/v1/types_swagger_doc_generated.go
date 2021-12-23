c45d13ba2bcff093777492eb3f14330693927564
Chao Xu <xuchao@google.com>
2021-12-22 17:48:38 -0800

    Refers to the GC doc in the blockOwnerDeletion API doc

--- staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/types_swagger_doc_generated.go
+++ staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/types_swagger_doc_generated.go
@@ -268,7 +268,7 @@ var map_OwnerReference = map[string]string{
 	"name":               "Name of the referent. More info: http://kubernetes.io/docs/user-guide/identifiers#names",
 	"uid":                "UID of the referent. More info: http://kubernetes.io/docs/user-guide/identifiers#uids",
 	"controller":         "If true, this reference points to the managing controller.",
-	"blockOwnerDeletion": "If true, AND if the owner has the \"foregroundDeletion\" finalizer, then the owner cannot be deleted from the key-value store until this reference is removed. Defaults to false. To set this field, a user needs \"delete\" permission of the owner, otherwise 422 (Unprocessable Entity) will be returned.",
+	"blockOwnerDeletion": "If true, AND if the owner has the \"foregroundDeletion\" finalizer, then the owner cannot be deleted from the key-value store until this reference is removed. See https://kubernetes.io/docs/concepts/architecture/garbage-collection/#foreground-deletion for how the garbage collector interacts with this field and enforces the foreground deletion. Defaults to false. To set this field, a user needs \"delete\" permission of the owner, otherwise 422 (Unprocessable Entity) will be returned.",
 }
 
 func (OwnerReference) SwaggerDoc() map[string]string {
