9ef51def2ea4ded67c49dac8ed6bd045d08275b9
yuzhiquanlong <yuzhiquanlong@gmail.com>
2021-12-24 15:41:26 +0800

    add unit test for TestGetPodCondition

--- staging/src/k8s.io/component-helpers/node/corev1/helpers_test.go
+++ staging/src/k8s.io/component-helpers/node/corev1/helpers_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package corev1
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -90,5 +91,65 @@ func TestUpdatePodCondition(t *testing.T) {
 		if diff := cmp.Diff(test.expected, resultStatus); diff != "" {
 			t.Fatalf("unexpected PodCondition update:\n%s", diff)
 		}
+	}
+}
+
+func TestGetPodCondition(t *testing.T) {
+	time := metav1.Now()
+	podStatus := &v1.PodStatus{
+		Conditions: []v1.PodCondition{
+			{
+				Type:               v1.PodReady,
+				Status:             v1.ConditionTrue,
+				Reason:             "successfully",
+				Message:            "sync pod successfully",
+				LastProbeTime:      time,
+				LastTransitionTime: metav1.NewTime(time.Add(1000)),
+			},
+		},
+	}
+
+	tests := []struct {
+		status        *v1.PodStatus
+		conditionType v1.PodConditionType
+		expected      struct {
+			i int
+			c *v1.PodCondition
+		}
+		desc string
+	}{
+		{
+			desc:          "when status is nil",
+			status:        nil,
+			conditionType: v1.PodReady,
+			expected: struct {
+				i int
+				c *v1.PodCondition
+			}{i: -1, c: nil},
+		},
+		{
+			desc:          "when status is not nil, and pod in specify conditionType",
+			status:        podStatus,
+			conditionType: v1.PodReady,
+			expected: struct {
+				i int
+				c *v1.PodCondition
+			}{i: 0, c: &podStatus.Conditions[0]},
+		},
+		{
+			desc:          "when status is not nil, but pod NOT in specify conditionType",
+			status:        podStatus,
+			conditionType: v1.PodInitialized,
+			expected: struct {
+				i int
+				c *v1.PodCondition
+			}{i: -1, c: nil},
+		},
+	}
+
+	for _, tt := range tests {
+		i, c := GetPodCondition(tt.status, tt.conditionType)
+		assert.Equal(t, tt.expected.i, i, "TestGetPodCondition return code: "+tt.desc)
+		assert.Equal(t, tt.expected.c, c, "TestGetPodCondition PodCondition: "+tt.desc)
 	}
 }
