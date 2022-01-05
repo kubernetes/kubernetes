b0d52b491e1c5b47f4d7400a6d20205ffd471535
JD Nurme <jdnurme@google.com>
2022-01-05 18:19:14 +0000

    updated name of metric

--- staging/src/k8s.io/component-base/metrics/prometheus/controllers/metrics.go
+++ staging/src/k8s.io/component-base/metrics/prometheus/controllers/metrics.go
@@ -36,7 +36,7 @@ type ControllerMetrics struct {
 func NewControllerMetrics() *ControllerMetrics {
 	controllerInstanceCount := k8smetrics.NewGaugeVec(
 		&k8smetrics.GaugeOpts{
-			Name:           "managed_controller_instances",
+			Name:           "managed_controller_started",
 			Help:           "Indicates where instances of a controller are currently running",
 			StabilityLevel: k8smetrics.ALPHA,
 		},
