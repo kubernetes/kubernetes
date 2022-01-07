61ddea984af474aefa7068162a4faf72b57aee36
yanghesong <hesong.yang@foxmail.com>
2022-01-07 23:11:51 +0800

    Remove runtime in validateHost
    Signed-off-by: yanghesong <hesong.yang@foxmail.com>

--- pkg/security/apparmor/validate_test.go
+++ pkg/security/apparmor/validate_test.go
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"testing"
 
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/stretchr/testify/assert"
@@ -44,9 +44,19 @@ func TestValidateHost(t *testing.T) {
 	t.Skip()
 
 	assert.NoError(t, validateHost())
+	assert.Error(t, validateHost())
 }
 
-func TestValidateProfileFormat(t *testing.T) {
+func TestValidateProfile(t *testing.T) {
+	loadedProfiles := map[string]bool{
+		"docker-default": true,
+		"foo-bar":        true,
+		"baz":            true,
+		"/usr/sbin/ntpd": true,
+		"/usr/lib/connman/scripts/dhclient-script":      true,
+		"/usr/lib/NetworkManager/nm-dhcp-client.action": true,
+		"/usr/bin/evince-previewer//sanitized_helper":   true,
+	}
 	tests := []struct {
 		profile     string
 		expectValid bool
@@ -57,10 +67,12 @@ func TestValidateProfileFormat(t *testing.T) {
 		{"baz", false}, // Missing local prefix.
 		{v1.AppArmorBetaProfileNamePrefix + "/usr/sbin/ntpd", true},
 		{v1.AppArmorBetaProfileNamePrefix + "foo-bar", true},
+		{v1.AppArmorBetaProfileNamePrefix + "unloaded", false}, // Not loaded.
+		{v1.AppArmorBetaProfileNamePrefix + "", false},
 	}
 
 	for _, test := range tests {
-		err := ValidateProfileFormat(test.profile)
+		err := validateProfile(test.profile, loadedProfiles)
 		if test.expectValid {
 			assert.NoError(t, err, "Profile %s should be valid", test.profile)
 		} else {
@@ -109,6 +121,8 @@ func TestValidateValidHost(t *testing.T) {
 		{v1.AppArmorBetaProfileNamePrefix + "foo-container", true},
 		{v1.AppArmorBetaProfileNamePrefix + "/usr/sbin/ntpd", true},
 		{"docker-default", false},
+		{v1.AppArmorBetaProfileNamePrefix + "foo", false},
+		{v1.AppArmorBetaProfileNamePrefix + "", false},
 	}
 
 	for _, test := range tests {
@@ -141,6 +155,23 @@ func TestValidateValidHost(t *testing.T) {
 		},
 	}
 	assert.NoError(t, v.Validate(pod), "Multi-container pod should validate")
+	for k, val := range pod.Annotations {
+		pod.Annotations[k] = val + "-bad"
+		assert.Error(t, v.Validate(pod), fmt.Sprintf("Multi-container pod with invalid profile %s:%s", k, pod.Annotations[k]))
+		pod.Annotations[k] = val // Restore.
+	}
+}
+
+func TestParseProfileName(t *testing.T) {
+	tests := []struct{ line, expected string }{
+		{"foo://bar/baz (kill)", "foo://bar/baz"},
+		{"foo-bar (enforce)", "foo-bar"},
+		{"/usr/foo/bar/baz (complain)", "/usr/foo/bar/baz"},
+	}
+	for _, test := range tests {
+		name := parseProfileName(test.line)
+		assert.Equal(t, test.expected, name, "Parsing %s", test.line)
+	}
 }
 
 func getPodWithProfile(profile string) *v1.Pod {
