8df4408a70e81c150d4ef00215d6060b4edee298
sanposhiho <44139130+sanposhiho@users.noreply.github.com>
2022-01-08 17:53:26 +0900

    Fix e2e test

--- test/integration/scheduler/queue_test.go
+++ test/integration/scheduler/queue_test.go
@@ -75,10 +75,10 @@ func TestCoreResourceEnqueue(t *testing.T) {
 	// - Pod1 is a best-effort Pod, but doesn't have the required toleration.
 	// - Pod2 requests a large amount of CPU resource that the node cannot fit.
 	//   Note: Pod2 will fail the tainttoleration plugin b/c that's ordered prior to noderesources.
-	// - Pod3 has the required toleration, but requests a non-existing PVC.
+	// - Pod3 has the required toleration, but has node affinity which doesn't match created "fake-node" Node.
 	pod1 := st.MakePod().Namespace(ns).Name("pod1").Container("image").Obj()
 	pod2 := st.MakePod().Namespace(ns).Name("pod2").Req(map[v1.ResourceName]string{v1.ResourceCPU: "4"}).Obj()
-	pod3 := st.MakePod().Namespace(ns).Name("pod3").Toleration("foo").PVC("pvc").Container("image").Obj()
+	pod3 := st.MakePod().Namespace(ns).Name("pod3").Toleration("foo").NodeAffinityIn("label1", []string{"value1"}).Container("image").Obj()
 	for _, pod := range []*v1.Pod{pod1, pod2, pod3} {
 		if _, err := cs.CoreV1().Pods(ns).Create(ctx, pod, metav1.CreateOptions{}); err != nil {
 			t.Fatalf("Failed to create Pod %q: %v", pod.Name, err)
