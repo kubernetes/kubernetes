4f38d4aaa1dcc88756a9ba9ea063759e50a15bee
wq <kyu21@outlook.com>
2022-01-09 00:07:43 +0900

    fix a typo in the comment of ImageCredentialProviderConfigFile

--- pkg/kubelet/config/flags.go
+++ pkg/kubelet/config/flags.go
@@ -69,7 +69,7 @@ type ContainerRuntimeOptions struct {
 	// Image credential provider plugin options
 
 	// ImageCredentialProviderConfigFile is the path to the credential provider plugin config file.
-	// This config file is a specification for what credential providers are enabled and invokved
+	// This config file is a specification for what credential providers are enabled and invoked
 	// by the kubelet. The plugin config should contain information about what plugin binary
 	// to execute and what container images the plugin should be called for.
 	// +optional
