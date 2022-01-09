fb372d07982463984dfdab7cf24d937b91b0bb83
zk <scuzk373x@gmail.com>
2022-01-09 23:45:09 +0800

    Update etcdRequestLatency metrics bucket size

--- staging/src/k8s.io/apiserver/pkg/endpoints/metrics/metrics.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/metrics/metrics.go
@@ -103,7 +103,7 @@ var (
 			// This metric is used for verifying api call latencies SLO,
 			// as well as tracking regressions in this aspects.
 			// Thus we customize buckets significantly, to empower both usecases.
-			Buckets: []float64{0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 1.25, 1.5, 2, 3,
+			Buckets: []float64{0.005, 0.025, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 1.25, 1.5, 2, 3,
 				4, 5, 6, 8, 10, 15, 20, 30, 45, 60},
 			StabilityLevel: compbasemetrics.STABLE,
 		},
