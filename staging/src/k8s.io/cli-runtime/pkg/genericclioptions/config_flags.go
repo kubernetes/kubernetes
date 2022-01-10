ed0c7f62640654b00aab8c188919966006c2b83d
Davide Bianchi <bianchidavide12@gmail.com>
2022-01-10 13:13:22 +0100

    fix: update WithDiscoveryQPS method description

--- staging/src/k8s.io/cli-runtime/pkg/genericclioptions/config_flags.go
+++ staging/src/k8s.io/cli-runtime/pkg/genericclioptions/config_flags.go
@@ -398,7 +398,7 @@ func (f *ConfigFlags) WithDiscoveryBurst(discoveryBurst int) *ConfigFlags {
 	return f
 }
 
-// WithDiscoveryBurst sets the RESTClient burst for discovery.
+// WithDiscoveryQPS sets the RESTClient QPS for discovery.
 func (f *ConfigFlags) WithDiscoveryQPS(discoveryQPS float32) *ConfigFlags {
 	f.discoveryQPS = discoveryQPS
 	return f
