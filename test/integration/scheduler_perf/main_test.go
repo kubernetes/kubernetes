408a28e76769b05636fa63f1e1fdef13cdc913be
Patrick Ohly <patrick.ohly@intel.com>
2022-01-23 11:10:48 +0100

    scheduler_perf: support JSON
    This enables benchmarking with JSON selected as output.

--- test/integration/scheduler_perf/main_test.go
+++ test/integration/scheduler_perf/main_test.go
@@ -18,8 +18,14 @@ package benchmark
 
 import (
 	"flag"
+	"fmt"
+	"os"
 	"testing"
 
+	"github.com/spf13/pflag"
+
+	logsapi "k8s.io/component-base/logs/api/v1"
+	_ "k8s.io/component-base/logs/json/register"
 	"k8s.io/klog/v2/ktesting"
 	"k8s.io/kubernetes/test/integration/framework"
 )
@@ -27,6 +33,23 @@ import (
 func TestMain(m *testing.M) {
 	ktesting.DefaultConfig = ktesting.NewConfig(ktesting.Verbosity(0)) // Run with -v=0 by default.
 	ktesting.DefaultConfig.AddFlags(flag.CommandLine)
+
+	c := logsapi.NewLoggingConfiguration()
+
+	// component-base only supports pflag at the moment.
+	var fs pflag.FlagSet
+	logsapi.AddFlags(c, &fs)
+	// Not ideal. https://github.com/spf13/pflag/pull/330 would be better.
+	fs.VisitAll(func(f *pflag.Flag) {
+		if flag.CommandLine.Lookup(f.Name) == nil {
+			flag.CommandLine.Var(f.Value, f.Name, f.Usage)
+		}
+	})
+	flag.Parse()
+	if err := logsapi.ValidateAndApply(c, nil /* no feature gates */); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
 
 	framework.EtcdMain(m.Run)
 }
