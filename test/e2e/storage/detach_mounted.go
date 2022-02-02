97dcb3ade0815663d65391db6bedf78f7ace1eb8
melvinj98 <melvinjohn44@gmail.com>
2022-02-02 00:00:37 -0500

    Refactored reference to SkipUnlessMasterOSDistroIs in detach_mounted

--- test/e2e/storage/detach_mounted.go
+++ test/e2e/storage/detach_mounted.go
@@ -55,7 +55,7 @@ var _ = utils.SIGDescribe("[Feature:Flexvolumes] Detaching volumes", func() {
 
 	ginkgo.BeforeEach(func() {
 		e2eskipper.SkipUnlessProviderIs("gce", "local")
-		e2eskipper.SkipUnlessMasterOSDistroIs("debian", "ubuntu", "gci", "custom")
+		e2eskipper.SkipUnlessControlPlaneOSDistroIs("debian", "ubuntu", "gci", "custom")
 		e2eskipper.SkipUnlessNodeOSDistroIs("debian", "ubuntu", "gci", "custom")
 		e2eskipper.SkipUnlessSSHKeyPresent()
 
