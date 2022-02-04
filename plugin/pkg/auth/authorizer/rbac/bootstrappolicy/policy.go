42c93b058ec72d859783d2c017f277be2fad1b8b
Jordan Liggitt <liggitt@google.com>
2022-02-04 14:37:31 -0500

    Add service account token request permissions to edit and admin clusterroles

--- plugin/pkg/auth/authorizer/rbac/bootstrappolicy/policy.go
+++ plugin/pkg/auth/authorizer/rbac/bootstrappolicy/policy.go
@@ -286,6 +286,7 @@ func ClusterRoles() []rbacv1.ClusterRole {
 				rbacv1helpers.NewRule(Write...).Groups(legacyGroup).Resources("pods", "pods/attach", "pods/proxy", "pods/exec", "pods/portforward").RuleOrDie(),
 				rbacv1helpers.NewRule(Write...).Groups(legacyGroup).Resources("replicationcontrollers", "replicationcontrollers/scale", "serviceaccounts",
 					"services", "services/proxy", "persistentvolumeclaims", "configmaps", "secrets", "events").RuleOrDie(),
+				rbacv1helpers.NewRule("create").Groups(legacyGroup).Resources("serviceaccounts/token").RuleOrDie(),
 
 				rbacv1helpers.NewRule(Write...).Groups(appsGroup).Resources(
 					"statefulsets", "statefulsets/scale",
