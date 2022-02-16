f1717baaaa13d3232e652664f17182f03cee10f5
Jean-Francois Remy <jremy@twitter.com>
2022-02-16 10:28:46 -0800

    Fix nodes volumesAttached status not updated
    The UpdateNodeStatuses code stops too early in case there is
    an error when calling updateNodeStatus. It will return immediately
    which means any remaining node won't have its update status put back
    to true.
    
    Looking at the call sites for UpdateNodeStatuses, it appears this is
    not the only issue. If the lister call fails with anything but a Not Found
    error, it's silently ignored which is wrong in the detach path.
    Also the reconciler detach path calls UpdateNodeStatuses but the real intent
    is to only update the node currently processed in the loop and not proceed
    with the detach call if there is an error updating that specifi node volumesAttached
    property. With the current implementation, it will not proceed if there is
    an error updating another node (which is not completely bad but not ideal) and
    worse it will proceed if there is a lister error on that node which means the
    node volumesAttached property won't have been updated.
    
    To fix those issues, introduce the following changes:
    - [node_status_updater] introduce UpdateNodeStatusForNode which does what
      UpdateNodeStatuses does but only for the provided node
    - [node_status_updater] if the node lister call fails for anything but a Not
      Found error, we will return an error, not ignore it
    - [node_status_updater] if the update of a node volumesAttached properties fails
      we continue processing the other nodes
    - [actual_state_of_world] introduce GetVolumesToReportAttachedForNode which
      does what GetVolumesToReportAttached but for the node whose name is provided
      it returns a bool which indicates if the node in question needs an update as
      well as the volumesAttached list. It is used by UpdateNodeStatusForNode
    - [actual_state_of_world] use write lock in updateNodeStatusUpdateNeeded, we're
      modifying the map content
    - [reconciler] use UpdateNodeStatusForNode in the detach loop

--- pkg/controller/volume/attachdetach/reconciler/reconciler.go
+++ pkg/controller/volume/attachdetach/reconciler/reconciler.go
@@ -202,7 +202,7 @@ func (rc *reconciler) reconcile() {
 			}
 
 			// Update Node Status to indicate volume is no longer safe to mount.
-			err = rc.nodeStatusUpdater.UpdateNodeStatuses()
+			err = rc.nodeStatusUpdater.UpdateNodeStatusForNode(attachedVolume.NodeName)
 			if err != nil {
 				// Skip detaching this volume if unable to update node status
 				klog.ErrorS(err, "UpdateNodeStatuses failed while attempting to report volume as attached", "volume", attachedVolume)
