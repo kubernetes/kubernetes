eddd47a3744eecf6199ca6f904cd93380f9b452d
Abu Kashem <akashem@redhat.com>
2022-02-24 17:28:01 -0500

    client-go: chain the error returned by rate limiter

--- test/integration/client/cert_rotation_test.go
+++ test/integration/client/cert_rotation_test.go
@@ -23,6 +23,7 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
+	"errors"
 	"math"
 	"math/big"
 	"os"
@@ -135,7 +136,9 @@ func TestCertRotationContinuousRequests(t *testing.T) {
 	for range time.Tick(time.Second) {
 		_, err := client.CoreV1().ServiceAccounts("default").List(ctx, v1.ListOptions{})
 		if err != nil {
-			if err == ctx.Err() {
+			// client may wrap the context.Canceled error, so we can't
+			// do 'err == ctx.Err()', instead use 'errors.Is'.
+			if errors.Is(err, context.Canceled) {
 				return
 			}
 
