fe3b09c96ec040a40b980564ff619311958346d0
Tim Bannister <tim@scalefactory.com>
2022-03-01 23:07:00 +0000

    Avoid using Docker Engine as example for container runtime
    Docker Engine isn't directly compatible with recent Kubernetes.

--- staging/src/k8s.io/api/core/v1/types.go
+++ staging/src/k8s.io/api/core/v1/types.go
@@ -4900,7 +4900,7 @@ type NodeSystemInfo struct {
 	KernelVersion string `json:"kernelVersion" protobuf:"bytes,4,opt,name=kernelVersion"`
 	// OS Image reported by the node from /etc/os-release (e.g. Debian GNU/Linux 7 (wheezy)).
 	OSImage string `json:"osImage" protobuf:"bytes,5,opt,name=osImage"`
-	// ContainerRuntime Version reported by the node through runtime remote API (e.g. docker://1.5.0).
+	// ContainerRuntime Version reported by the node through runtime remote API (e.g. containerd://1.4.2).
 	ContainerRuntimeVersion string `json:"containerRuntimeVersion" protobuf:"bytes,6,opt,name=containerRuntimeVersion"`
 	// Kubelet Version reported by the node.
 	KubeletVersion string `json:"kubeletVersion" protobuf:"bytes,7,opt,name=kubeletVersion"`
