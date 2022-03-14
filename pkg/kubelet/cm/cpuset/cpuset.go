5374f6fad84b43c1f783802086c8f3e2f6e124ce
chymy <chang.min1@zte.com.cn>
2022-03-14 16:53:29 +0800

    Fix comment typo
    Signed-off-by: chymy <chang.min1@zte.com.cn>

--- pkg/kubelet/cm/cpuset/cpuset.go
+++ pkg/kubelet/cm/cpuset/cpuset.go
@@ -76,7 +76,7 @@ func NewCPUSet(cpus ...int) CPUSet {
 	return b.Result()
 }
 
-// NewCPUSet returns a new CPUSet containing the supplied elements, as slice of int64.
+// NewCPUSetInt64 returns a new CPUSet containing the supplied elements, as slice of int64.
 func NewCPUSetInt64(cpus ...int64) CPUSet {
 	b := NewBuilder()
 	for _, c := range cpus {
