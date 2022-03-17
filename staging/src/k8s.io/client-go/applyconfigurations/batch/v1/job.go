7a2e86924caac08de703aedc027759719c795ac6
Daniel Smith <dbsmith@google.com>
2022-03-17 18:27:49 +0000

    generated files

--- staging/src/k8s.io/client-go/applyconfigurations/batch/v1/job.go
+++ staging/src/k8s.io/client-go/applyconfigurations/batch/v1/job.go
@@ -235,15 +235,6 @@ func (b *JobApplyConfiguration) WithFinalizers(values ...string) *JobApplyConfig
 	return b
 }
 
-// WithClusterName sets the ClusterName field in the declarative configuration to the given value
-// and returns the receiver, so that objects can be built by chaining "With" function invocations.
-// If called multiple times, the ClusterName field is set to the value of the last call.
-func (b *JobApplyConfiguration) WithClusterName(value string) *JobApplyConfiguration {
-	b.ensureObjectMetaApplyConfigurationExists()
-	b.ClusterName = &value
-	return b
-}
-
 func (b *JobApplyConfiguration) ensureObjectMetaApplyConfigurationExists() {
 	if b.ObjectMetaApplyConfiguration == nil {
 		b.ObjectMetaApplyConfiguration = &v1.ObjectMetaApplyConfiguration{}
