eb6359b1bce5ec506ff9fc5b9414cb61ca7faedc
Davanum Srinivas <davanum@gmail.com>
2022-03-29 15:36:38 -0400

    Switch to pause 3.7
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- test/utils/image/manifest.go
+++ test/utils/image/manifest.go
@@ -260,7 +260,7 @@ func initImageConfigs(list RegistryList) (map[int]Config, map[int]Config) {
 	configs[Nonewprivs] = Config{list.PromoterE2eRegistry, "nonewprivs", "1.3"}
 	configs[NonRoot] = Config{list.PromoterE2eRegistry, "nonroot", "1.2"}
 	// Pause - when these values are updated, also update cmd/kubelet/app/options/container_runtime.go
-	configs[Pause] = Config{list.GcRegistry, "pause", "3.6"}
+	configs[Pause] = Config{list.GcRegistry, "pause", "3.7"}
 	configs[Perl] = Config{list.PromoterE2eRegistry, "perl", "5.26"}
 	configs[PrometheusDummyExporter] = Config{list.GcRegistry, "prometheus-dummy-exporter", "v0.1.0"}
 	configs[PrometheusToSd] = Config{list.GcRegistry, "prometheus-to-sd", "v0.5.0"}
