809f0a943eb394e8f4e52c04efa6e634798118fb
Paco Xu <paco.xu@daocloud.io>
2022-04-01 15:22:34 +0800

    use privileged enforce level for nfs test

--- test/e2e/node/kubelet.go
+++ test/e2e/node/kubelet.go
@@ -271,7 +271,7 @@ var _ = SIGDescribe("kubelet", func() {
 		ns string
 	)
 	f := framework.NewDefaultFramework("kubelet")
-	f.NamespacePodSecurityEnforceLevel = admissionapi.LevelBaseline
+	f.NamespacePodSecurityEnforceLevel = admissionapi.LevelPrivileged
 
 	ginkgo.BeforeEach(func() {
 		c = f.ClientSet
