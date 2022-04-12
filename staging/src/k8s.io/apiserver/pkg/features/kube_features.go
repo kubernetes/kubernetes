3852858822e859d94ef65158076dfb018813ee8b
Patrick Ohly <patrick.ohly@intel.com>
2022-04-12 15:46:53 +0200

    features: reduce risk of merge conflicts
    Merge conflicts become less likely when:
    - features are sorted alphabetically because
      then changes are more likely to be done in
      different parts of the files
    - blank lines separate the hash entries because
      gofmt then doesn't change the formating of
      other entries when adding or removing one
    
    Merge conflicts where pretty common shortly before a code freeze when everyone
    added new features at the end of the files.

--- staging/src/k8s.io/apiserver/pkg/features/kube_features.go
+++ staging/src/k8s.io/apiserver/pkg/features/kube_features.go
@@ -28,7 +28,48 @@ const (
 	//
 	// // owner: @username
 	// // alpha: v1.4
-	// MyFeature() bool
+	// MyFeature featuregate.Feature = "MyFeature"
+	//
+	// Feature gates should be listed in alphabetical, case-sensitive
+	// (upper before any lower case character) order. This reduces the risk
+	// of code conflicts because changes are more likely to be scattered
+	// across the file.
+
+	// owner: @smarterclayton
+	// alpha: v1.8
+	// beta: v1.9
+	//
+	// Allow API clients to retrieve resource lists in chunks rather than
+	// all at once.
+	APIListChunking featuregate.Feature = "APIListChunking"
+
+	// owner: @MikeSpreitzer @yue9944882
+	// alpha: v1.18
+	// beta: v1.20
+	//
+	// Enables managing request concurrency with prioritization and fairness at each server.
+	// The FeatureGate was introduced in release 1.15 but the feature
+	// was not really implemented before 1.18.
+	APIPriorityAndFairness featuregate.Feature = "APIPriorityAndFairness"
+
+	// owner: @ilackams
+	// alpha: v1.7
+	// beta: v1.16
+	//
+	// Enables compression of REST responses (GET and LIST only)
+	APIResponseCompression featuregate.Feature = "APIResponseCompression"
+
+	// owner: @roycaihw
+	// alpha: v1.20
+	//
+	// Assigns each kube-apiserver an ID in a cluster.
+	APIServerIdentity featuregate.Feature = "APIServerIdentity"
+
+	// owner: @dashpole
+	// alpha: v1.22
+	//
+	// Add support for distributed tracing in the API Server
+	APIServerTracing featuregate.Feature = "APIServerTracing"
 
 	// owner: @tallclair
 	// alpha: v1.7
@@ -40,20 +81,12 @@ const (
 	// audited.
 	AdvancedAuditing featuregate.Feature = "AdvancedAuditing"
 
-	// owner: @ilackams
-	// alpha: v1.7
-	// beta: v1.16
+	// owner: @cici37
+	// kep: http://kep.k8s.io/2876
+	// alpha: v1.23
 	//
-	// Enables compression of REST responses (GET and LIST only)
-	APIResponseCompression featuregate.Feature = "APIResponseCompression"
-
-	// owner: @smarterclayton
-	// alpha: v1.8
-	// beta: v1.9
-	//
-	// Allow API clients to retrieve resource lists in chunks rather than
-	// all at once.
-	APIListChunking featuregate.Feature = "APIListChunking"
+	// Enables expression validation for Custom Resource
+	CustomResourceValidationExpressions featuregate.Feature = "CustomResourceValidationExpressions"
 
 	// owner: @apelisse
 	// alpha: v1.12
@@ -65,6 +98,31 @@ const (
 	// committing.
 	DryRun featuregate.Feature = "DryRun"
 
+	// owner: @wojtek-t
+	// alpha: v1.20
+	// beta: v1.21
+	// GA: v1.24
+	//
+	// Allows for updating watchcache resource version with progress notify events.
+	EfficientWatchResumption featuregate.Feature = "EfficientWatchResumption"
+
+	// owner: @jiahuif
+	// kep: http://kep.k8s.io/2887
+	// alpha: v1.23
+	// beta: v1.24
+	//
+	// Enables populating "enum" field of OpenAPI schemas
+	// in the spec returned from kube-apiserver.
+	OpenAPIEnums featuregate.Feature = "OpenAPIEnums"
+
+	// owner: @jefftree
+	// kep: http://kep.k8s.io/2896
+	// alpha: v1.23
+	// beta: v1.24
+	//
+	// Enables kubernetes to publish OpenAPI v3
+	OpenAPIV3 featuregate.Feature = "OpenAPIV3"
+
 	// owner: @caesarxuchao
 	// alpha: v1.15
 	// beta: v1.16
@@ -72,45 +130,6 @@ const (
 	// Allow apiservers to show a count of remaining items in the response
 	// to a chunking list request.
 	RemainingItemCount featuregate.Feature = "RemainingItemCount"
-
-	// owner: @apelisse, @lavalamp
-	// alpha: v1.14
-	// beta: v1.16
-	// stable: v1.22
-	//
-	// Server-side apply. Merging happens on the server.
-	ServerSideApply featuregate.Feature = "ServerSideApply"
-
-	// owner: @caesarxuchao
-	// alpha: v1.14
-	// beta: v1.15
-	//
-	// Allow apiservers to expose the storage version hash in the discovery
-	// document.
-	StorageVersionHash featuregate.Feature = "StorageVersionHash"
-
-	// owner: @caesarxuchao @roycaihw
-	// alpha: v1.20
-	//
-	// Enable the storage version API.
-	StorageVersionAPI featuregate.Feature = "StorageVersionAPI"
-
-	// owner: @wojtek-t
-	// alpha: v1.15
-	// beta: v1.16
-	// GA: v1.17
-	//
-	// Enables support for watch bookmark events.
-	WatchBookmark featuregate.Feature = "WatchBookmark"
-
-	// owner: @MikeSpreitzer @yue9944882
-	// alpha: v1.18
-	// beta: v1.20
-	//
-	// Enables managing request concurrency with prioritization and fairness at each server.
-	// The FeatureGate was introduced in release 1.15 but the feature
-	// was not really implemented before 1.18.
-	APIPriorityAndFairness featuregate.Feature = "APIPriorityAndFairness"
 
 	// owner: @wojtek-t
 	// alpha: v1.16
@@ -128,49 +147,13 @@ const (
 	// Allows label and field based indexes in apiserver watch cache to accelerate list operations.
 	SelectorIndex featuregate.Feature = "SelectorIndex"
 
-	// owner: @wojtek-t
-	// alpha: v1.20
-	// beta: v1.21
-	// GA: v1.24
+	// owner: @apelisse, @lavalamp
+	// alpha: v1.14
+	// beta: v1.16
+	// stable: v1.22
 	//
-	// Allows for updating watchcache resource version with progress notify events.
-	EfficientWatchResumption featuregate.Feature = "EfficientWatchResumption"
-
-	// owner: @roycaihw
-	// alpha: v1.20
-	//
-	// Assigns each kube-apiserver an ID in a cluster.
-	APIServerIdentity featuregate.Feature = "APIServerIdentity"
-
-	// owner: @dashpole
-	// alpha: v1.22
-	//
-	// Add support for distributed tracing in the API Server
-	APIServerTracing featuregate.Feature = "APIServerTracing"
-
-	// owner: @jiahuif
-	// kep: http://kep.k8s.io/2887
-	// alpha: v1.23
-	// beta: v1.24
-	//
-	// Enables populating "enum" field of OpenAPI schemas
-	// in the spec returned from kube-apiserver.
-	OpenAPIEnums featuregate.Feature = "OpenAPIEnums"
-
-	// owner: @cici37
-	// kep: http://kep.k8s.io/2876
-	// alpha: v1.23
-	//
-	// Enables expression validation for Custom Resource
-	CustomResourceValidationExpressions featuregate.Feature = "CustomResourceValidationExpressions"
-
-	// owner: @jefftree
-	// kep: http://kep.k8s.io/2896
-	// alpha: v1.23
-	// beta: v1.24
-	//
-	// Enables kubernetes to publish OpenAPI v3
-	OpenAPIV3 featuregate.Feature = "OpenAPIV3"
+	// Server-side apply. Merging happens on the server.
+	ServerSideApply featuregate.Feature = "ServerSideApply"
 
 	// owner: @kevindelgado
 	// kep: http://kep.k8s.io/2885
@@ -178,6 +161,28 @@ const (
 	//
 	// Enables server-side field validation.
 	ServerSideFieldValidation featuregate.Feature = "ServerSideFieldValidation"
+
+	// owner: @caesarxuchao @roycaihw
+	// alpha: v1.20
+	//
+	// Enable the storage version API.
+	StorageVersionAPI featuregate.Feature = "StorageVersionAPI"
+
+	// owner: @caesarxuchao
+	// alpha: v1.14
+	// beta: v1.15
+	//
+	// Allow apiservers to expose the storage version hash in the discovery
+	// document.
+	StorageVersionHash featuregate.Feature = "StorageVersionHash"
+
+	// owner: @wojtek-t
+	// alpha: v1.15
+	// beta: v1.16
+	// GA: v1.17
+	//
+	// Enables support for watch bookmark events.
+	WatchBookmark featuregate.Feature = "WatchBookmark"
 )
 
 func init() {
@@ -188,23 +193,41 @@ func init() {
 // To add a new feature, define a key for it above and add it here. The features will be
 // available throughout Kubernetes binaries.
 var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{
-	AdvancedAuditing:                    {Default: true, PreRelease: featuregate.GA},
-	APIResponseCompression:              {Default: true, PreRelease: featuregate.Beta},
-	APIListChunking:                     {Default: true, PreRelease: featuregate.Beta},
-	DryRun:                              {Default: true, PreRelease: featuregate.GA},
-	RemainingItemCount:                  {Default: true, PreRelease: featuregate.Beta},
-	ServerSideApply:                     {Default: true, PreRelease: featuregate.GA},
-	StorageVersionHash:                  {Default: true, PreRelease: featuregate.Beta},
-	StorageVersionAPI:                   {Default: false, PreRelease: featuregate.Alpha},
-	WatchBookmark:                       {Default: true, PreRelease: featuregate.GA, LockToDefault: true},
-	APIPriorityAndFairness:              {Default: true, PreRelease: featuregate.Beta},
-	RemoveSelfLink:                      {Default: true, PreRelease: featuregate.GA, LockToDefault: true},
-	SelectorIndex:                       {Default: true, PreRelease: featuregate.GA, LockToDefault: true},
-	EfficientWatchResumption:            {Default: true, PreRelease: featuregate.GA, LockToDefault: true},
-	APIServerIdentity:                   {Default: false, PreRelease: featuregate.Alpha},
-	APIServerTracing:                    {Default: false, PreRelease: featuregate.Alpha},
-	OpenAPIEnums:                        {Default: true, PreRelease: featuregate.Beta},
+	APIListChunking: {Default: true, PreRelease: featuregate.Beta},
+
+	APIPriorityAndFairness: {Default: true, PreRelease: featuregate.Beta},
+
+	APIResponseCompression: {Default: true, PreRelease: featuregate.Beta},
+
+	APIServerIdentity: {Default: false, PreRelease: featuregate.Alpha},
+
+	APIServerTracing: {Default: false, PreRelease: featuregate.Alpha},
+
+	AdvancedAuditing: {Default: true, PreRelease: featuregate.GA},
+
 	CustomResourceValidationExpressions: {Default: false, PreRelease: featuregate.Alpha},
-	OpenAPIV3:                           {Default: true, PreRelease: featuregate.Beta},
-	ServerSideFieldValidation:           {Default: false, PreRelease: featuregate.Alpha},
+
+	DryRun: {Default: true, PreRelease: featuregate.GA},
+
+	EfficientWatchResumption: {Default: true, PreRelease: featuregate.GA, LockToDefault: true},
+
+	OpenAPIEnums: {Default: true, PreRelease: featuregate.Beta},
+
+	OpenAPIV3: {Default: true, PreRelease: featuregate.Beta},
+
+	RemainingItemCount: {Default: true, PreRelease: featuregate.Beta},
+
+	RemoveSelfLink: {Default: true, PreRelease: featuregate.GA, LockToDefault: true},
+
+	SelectorIndex: {Default: true, PreRelease: featuregate.GA, LockToDefault: true},
+
+	ServerSideApply: {Default: true, PreRelease: featuregate.GA},
+
+	ServerSideFieldValidation: {Default: false, PreRelease: featuregate.Alpha},
+
+	StorageVersionAPI: {Default: false, PreRelease: featuregate.Alpha},
+
+	StorageVersionHash: {Default: true, PreRelease: featuregate.Beta},
+
+	WatchBookmark: {Default: true, PreRelease: featuregate.GA, LockToDefault: true},
 }
