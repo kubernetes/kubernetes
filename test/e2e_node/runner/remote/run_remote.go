a009c5b9e24cde4b6269e813a98aab83d74a8ee0
Danielle Lancashire <dani@builds.terrible.systems>
2022-04-20 16:49:29 +0000

    e2e_node: remote runner: Require containerd/crio

--- test/e2e_node/runner/remote/run_remote.go
+++ test/e2e_node/runner/remote/run_remote.go
@@ -705,15 +705,14 @@ func createInstance(imageConfig *internalGCEImage) (string, error) {
 
 		var output string
 		output, err = remote.SSH(name, "sh", "-c",
-			"'systemctl list-units  --type=service  --state=running | grep -e docker -e containerd -e crio'")
+			"'systemctl list-units  --type=service  --state=running | grep -e containerd -e crio'")
 		if err != nil {
-			err = fmt.Errorf("instance %s not running docker/containerd/crio daemon - Command failed: %s", name, output)
+			err = fmt.Errorf("instance %s not running containerd/crio daemon - Command failed: %s", name, output)
 			continue
 		}
-		if !strings.Contains(output, "docker.service") &&
-			!strings.Contains(output, "containerd.service") &&
+		if !strings.Contains(output, "containerd.service") &&
 			!strings.Contains(output, "crio.service") {
-			err = fmt.Errorf("instance %s not running docker/containerd/crio daemon: %s", name, output)
+			err = fmt.Errorf("instance %s not running containerd/crio daemon: %s", name, output)
 			continue
 		}
 		instanceRunning = true
