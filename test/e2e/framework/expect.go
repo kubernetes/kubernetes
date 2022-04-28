3a3e4352f681feeab7a96eb316238ba5eba20864
kidddddddddddddddddddddd <1062602710@qq.com>
2022-04-28 11:35:14 +0800

    ExpectNoErrorExplained

--- test/e2e/framework/expect.go
+++ test/e2e/framework/expect.go
@@ -31,19 +31,69 @@ func ExpectNotEqual(actual interface{}, extra interface{}, explain ...interface{
 }
 
 // ExpectError expects an error happens, otherwise an exception raises
+//
+// Should not be used anymore. Instead call ExpectErrorExplained
+// with an explanation. That helps understand failures when the
+// error is not descriptive enough by itself. Even when most errors
+// that can occur are descriptive, that is hard to be certain about,
+// so it's better to err on the side of caution and always provide
+// an explanation.
 func ExpectError(err error, explain ...interface{}) {
 	gomega.ExpectWithOffset(1, err).To(gomega.HaveOccurred(), explain...)
 }
 
+// ExpectErrorExplained checks that an error happened, otherwise the test fails.
+// An additional printf-style explanation must be provided because often errors
+// cannot be understood without it. If you're unsure what to put for the explanation,
+// try to explain the error to a hypothetical person reading failure logs without any other context about the test
+func ExpectErrorExplained(err error, explainFormatStr string, explainArgs ...interface{}) {
+	ExpectNotEqual(explainFormatStr, "", "An explanation for the error is required.")
+	explainArgs = append([]interface{}{explainFormatStr}, explainArgs...)
+	gomega.ExpectWithOffset(1, err).To(gomega.HaveOccurred(), explainArgs...)
+}
+
 // ExpectNoError checks if "err" is set, and if so, fails assertion while logging the error.
+//
+// Should not be used anymore. Instead call ExpectNoErrorExplained
+// with an explanation. That helps understand failures when the
+// error is not descriptive enough by itself. Even when most errors
+// that can occur are descriptive, that is hard to be certain about,
+// so it's better to err on the side of caution and always provide
+// an explanation.
 func ExpectNoError(err error, explain ...interface{}) {
 	ExpectNoErrorWithOffset(1, err, explain...)
 }
 
+// ExpectNoErrorExplained checks if "err" is set, and if so, fails the assertion while logging the error.
+// An additional printf-style explanation must be provided because often errors
+// cannot be understood without it. If you're unsure what to put for the explanation,
+// try to explain the error to a hypothetical person reading failure logs without any other context about the test
+func ExpectNoErrorExplained(err error, explainFormatStr string, explainArgs ...interface{}) {
+	ExpectNoErrorWithOffsetExplained(1, err, explainFormatStr, explainArgs...)
+}
+
 // ExpectNoErrorWithOffset checks if "err" is set, and if so, fails assertion while logging the error at "offset" levels above its caller
 // (for example, for call chain f -> g -> ExpectNoErrorWithOffset(1, ...) error would be logged for "f").
+//
+// Should not be used anymore. Instead call ExpectNoErrorWithOffsetExplained
+// with an explanation. That helps understand failures when the
+// error is not descriptive enough by itself. Even when most errors
+// that can occur are descriptive, that is hard to be certain about,
+// so it's better to err on the side of caution and always provide
+// an explanation.
 func ExpectNoErrorWithOffset(offset int, err error, explain ...interface{}) {
 	gomega.ExpectWithOffset(1+offset, err).NotTo(gomega.HaveOccurred(), explain...)
+}
+
+// ExpectNoErrorWithOffsetExplained checks if "err" is set, and if so, fails assertion while logging the error at "offset" levels above its caller
+// (for example, for call chain f -> g -> ExpectNoErrorWithOffsetExplained(1, ...) error would be logged for "f").
+// An additional printf-style explanation must be provided because often errors
+// cannot be understood without it. If you're unsure what to put for the explanation,
+// try to explain the error to a hypothetical person reading failure logs without any other context about the test
+func ExpectNoErrorWithOffsetExplained(offset int, err error, explainFormatStr string, explainArgs ...interface{}) {
+	ExpectNotEqual(explainFormatStr, "", "An explanation for the error is required.")
+	explainArgs = append([]interface{}{explainFormatStr}, explainArgs...)
+	gomega.ExpectWithOffset(1+offset, err).NotTo(gomega.HaveOccurred(), explainArgs...)
 }
 
 // ExpectConsistOf expects actual contains precisely the extra elements.  The ordering of the elements does not matter.
