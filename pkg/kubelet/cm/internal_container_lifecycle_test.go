06c39710c720daa34c817c95e723e2e661679695
prakharporwal <prakharporwal@github>
2022-05-02 12:37:39 +0530

    Made a unit test for internal container lifecycle in kubelet

--- pkg/kubelet/cm/internal_container_lifecycle_test.go
+++ pkg/kubelet/cm/internal_container_lifecycle_test.go
@@ -20,8 +20,10 @@ func TestPreStartContainer(t *testing.T) {
 	fakeContainer := &v1.Container{}
 	containerId := "testid"
 	// execute and match expected
-	i.PreStartContainer(fakePod, fakeContainer, containerId)
-
+	err := i.PreStartContainer(fakePod, fakeContainer, containerId)
+	if err != nil {
+		assert.Error(t, err, "Error Occured when trying to prestart container")
+	}
 	assert.NotNil(t, i.cpuManager, "cpu manager Container should not be null %v", i.cpuManager)
 	assert.NotNil(t, i.memoryManager, "memory manager Container should not be null %v", i.cpuManager)
 	assert.NotNil(t, i.topologyManager, "topology manager Container should not be null %v", i.cpuManager)
