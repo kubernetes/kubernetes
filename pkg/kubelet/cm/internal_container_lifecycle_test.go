b3956472cf14474b6d8f21620f0ba559be361bf6
prakharporwal <prakharporwal@github>
2022-05-02 12:35:19 +0530

    Made a unit test for internal container lifecycle in kubelet

---
+++ pkg/kubelet/cm/internal_container_lifecycle_test.go
@@ -0,0 +1,28 @@
+package cm
+
+import (
+	"github.com/stretchr/testify/assert"
+	"k8s.io/api/core/v1"
+	"k8s.io/kubernetes/pkg/kubelet/cm/cpumanager"
+	"k8s.io/kubernetes/pkg/kubelet/cm/memorymanager"
+	"k8s.io/kubernetes/pkg/kubelet/cm/topologymanager"
+	"testing"
+)
+
+func TestPreStartContainer(t *testing.T) {
+	// setup vars
+	i := internalContainerLifecycleImpl{
+		cpuManager:      cpumanager.NewFakeManager(),
+		memoryManager:   memorymanager.NewFakeManager(),
+		topologyManager: topologymanager.NewFakeManager(),
+	}
+	fakePod := &v1.Pod{}
+	fakeContainer := &v1.Container{}
+	containerId := "testid"
+	// execute and match expected
+	i.PreStartContainer(fakePod, fakeContainer, containerId)
+
+	assert.NotNil(t, i.cpuManager, "cpu manager Container should not be null %v", i.cpuManager)
+	assert.NotNil(t, i.memoryManager, "memory manager Container should not be null %v", i.cpuManager)
+	assert.NotNil(t, i.topologyManager, "topology manager Container should not be null %v", i.cpuManager)
+}
