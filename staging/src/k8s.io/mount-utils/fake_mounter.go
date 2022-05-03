3eee8a12caeb8aca7b12a0bc0b618c35a2c2a7de
Carter McKinnon <mckdev@amazon.com>
2022-05-02 19:07:39 -0700

    Add test for CanSafelySkipMountPointCheck

--- staging/src/k8s.io/mount-utils/fake_mounter.go
+++ staging/src/k8s.io/mount-utils/fake_mounter.go
@@ -32,8 +32,9 @@ type FakeMounter struct {
 	MountCheckErrors map[string]error
 	// Some tests run things in parallel, make sure the mounter does not produce
 	// any golang's DATA RACE warnings.
-	mutex       sync.Mutex
-	UnmountFunc UnmountFunc
+	mutex               sync.Mutex
+	UnmountFunc         UnmountFunc
+	skipMountPointCheck bool
 }
 
 // UnmountFunc is a function callback to be executed during the Unmount() call.
@@ -62,6 +63,11 @@ func NewFakeMounter(mps []MountPoint) *FakeMounter {
 	return &FakeMounter{
 		MountPoints: mps,
 	}
+}
+
+func (f *FakeMounter) WithSkipMountPointCheck() *FakeMounter {
+	f.skipMountPointCheck = true
+	return f
 }
 
 // ResetLog clears all the log entries in FakeMounter
@@ -212,9 +218,8 @@ func (f *FakeMounter) IsLikelyNotMountPoint(file string) (bool, error) {
 	return true, nil
 }
 
-// CanSafelySkipMountPointCheck always returns false for FakeMounter.
 func (f *FakeMounter) CanSafelySkipMountPointCheck() bool {
-	return false
+	return f.skipMountPointCheck
 }
 
 // GetMountRefs finds all mount references to the path, returns a
