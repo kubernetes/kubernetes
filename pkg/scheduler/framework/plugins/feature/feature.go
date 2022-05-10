775a878f50b0537d25ef79ab440e7519feec4fe3
bells17 <bells171@gmail.com>
2022-05-11 01:48:27 +0900

    Add storage capacity scoring to VolumeBinding plugin

--- pkg/scheduler/framework/plugins/feature/feature.go
+++ pkg/scheduler/framework/plugins/feature/feature.go
@@ -32,4 +32,5 @@ type Features struct {
 	EnableSchedulingQueueHint                    bool
 	EnableAsyncPreemption                        bool
 	EnablePodLevelResources                      bool
+	EnableStorageCapacityScoring                 bool
 }
