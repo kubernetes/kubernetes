fa2a6d633984aaa36ef988ecf4410c20e27897ea
Steve Kuznetsov <skuznets@redhat.com>
2022-05-11 09:05:18 -0700

    storage: isolate the test resource
    This test resource only exists to test the versioning logic, but it
    cannot live in the general testing package, or import cycles will exist.
    
    Signed-off-by: Steve Kuznetsov <skuznets@redhat.com>

--- staging/src/k8s.io/apiserver/pkg/storage/testresource/types.go
+++ staging/src/k8s.io/apiserver/pkg/storage/testresource/types.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package testing
+package testresource
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
