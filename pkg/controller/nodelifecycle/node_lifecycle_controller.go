9166d9b4bc267854042ebbbc0b07c5e476f5c62c
yylt <yang8518296@163.com>
2022-05-12 17:24:25 +0800

    update check node status

--- pkg/controller/nodelifecycle/node_lifecycle_controller.go
+++ pkg/controller/nodelifecycle/node_lifecycle_controller.go
@@ -836,7 +836,7 @@ func (nc *Controller) monitorNodeHealth(ctx context.Context) error {
 
 			_, needsRetry := nc.nodesToRetry.Load(node.Name)
 			switch {
-			case currentReadyCondition.Status != v1.ConditionTrue && observedReadyCondition.Status == v1.ConditionTrue:
+			case currentReadyCondition.Status == v1.ConditionUnknown && observedReadyCondition.Status != currentReadyCondition.Status:
 				// Report node event only once when status changed.
 				controllerutil.RecordNodeStatusChange(nc.recorder, node, "NodeNotReady")
 				fallthrough
