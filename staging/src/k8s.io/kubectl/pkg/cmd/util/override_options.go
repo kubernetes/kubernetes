4165320e86520d7609cfb08f7d4f20ed348569f1
konrad945 <konrad.maciag@protonmail.com>
2022-05-23 10:31:54 +0200

    Removal of github.com/chai2010/gettext-go (BSD license) from client library

--- staging/src/k8s.io/kubectl/pkg/cmd/util/override_options.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/util/override_options.go
@@ -23,7 +23,6 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/kubectl/pkg/scheme"
-	"k8s.io/kubectl/pkg/util/i18n"
 )
 
 type OverrideType string
@@ -47,7 +46,7 @@ type OverrideOptions struct {
 }
 
 func (o *OverrideOptions) AddOverrideFlags(cmd *cobra.Command) {
-	cmd.Flags().StringVar(&o.Overrides, "overrides", "", i18n.T("An inline JSON override for the generated object. If this is non-empty, it is used to override the generated object. Requires that the object supply a valid apiVersion field."))
+	cmd.Flags().StringVar(&o.Overrides, "overrides", "", "An inline JSON override for the generated object. If this is non-empty, it is used to override the generated object. Requires that the object supply a valid apiVersion field.")
 	cmd.Flags().StringVar((*string)(&o.OverrideType), "override-type", string(DefaultOverrideType), fmt.Sprintf("The method used to override the generated object: %s, %s, or %s.", OverrideTypeJSON, OverrideTypeMerge, OverrideTypeStrategic))
 }
 
