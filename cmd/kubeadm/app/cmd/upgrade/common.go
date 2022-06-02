304eb41e10673987b9f96e61cc5debafba8fbdff
Lubomir I. Ivanov <lubomirivanov@vmware.com>
2022-06-02 14:09:30 +0300

    kubeadm: mutate ClusterConfiguration.imageRepository to "registry.k8s.io"
    If the user runs "kubeadm upgrade apply", kubeadm can download
    a configuration from the cluster. If the configuration contains
    the legacy default imageRepository of "k8s.gcr.io", mutate it
    to the new default of "registry.k8s.io" and update the
    configuration in the config map.
    
    During "upgrade node/diff" download the configuration, mutate the
    image repository locally, but do not mutate the in-cluster value.
    That is done only on "apply".
    
    This ensures that users are migrated from the old default registry
    domain.

--- cmd/kubeadm/app/cmd/upgrade/common.go
+++ cmd/kubeadm/app/cmd/upgrade/common.go
@@ -42,6 +42,7 @@ import (
 	"k8s.io/kubernetes/cmd/kubeadm/app/constants"
 	"k8s.io/kubernetes/cmd/kubeadm/app/features"
 	"k8s.io/kubernetes/cmd/kubeadm/app/phases/upgrade"
+	"k8s.io/kubernetes/cmd/kubeadm/app/phases/uploadconfig"
 	"k8s.io/kubernetes/cmd/kubeadm/app/preflight"
 	kubeadmutil "k8s.io/kubernetes/cmd/kubeadm/app/util"
 	"k8s.io/kubernetes/cmd/kubeadm/app/util/apiclient"
@@ -72,6 +73,14 @@ func loadConfig(cfgPath string, client clientset.Interface, skipComponentConfigs
 	// This is probably 90% of the time. So we handle it first.
 	if cfgPath == "" {
 		cfg, err := configutil.FetchInitConfigurationFromCluster(client, os.Stdout, logPrefix, false, skipComponentConfigs)
+		// In case we fetch a configuration from the cluster, mutate the ImageRepository field
+		// to be 'registry.k8s.io', if it was 'k8s.gcr.io'.
+		// https://github.com/kubernetes/kubeadm/issues/2671
+		if err == nil {
+			if err := uploadconfig.MutateImageRepository(cfg, client); err != nil {
+				return nil, false, err
+			}
+		}
 		return cfg, false, err
 	}
 
