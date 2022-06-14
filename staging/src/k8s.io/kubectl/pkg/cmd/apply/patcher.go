1d1716f1b2f0f004c962f52378486c6680cea417
hwdef <hwdefcom@outlook.com>
2022-06-14 16:19:31 +0800

    remove solved TODO

--- staging/src/k8s.io/kubectl/pkg/cmd/apply/patcher.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/apply/patcher.go
@@ -208,7 +208,6 @@ func (p *Patcher) deleteAndCreate(original runtime.Object, modified []byte, name
 	if err := p.delete(namespace, name); err != nil {
 		return modified, nil, err
 	}
-	// TODO: use wait
 	if err := wait.PollImmediate(1*time.Second, p.Timeout, func() (bool, error) {
 		if _, err := p.Helper.Get(namespace, name); !errors.IsNotFound(err) {
 			return false, err
