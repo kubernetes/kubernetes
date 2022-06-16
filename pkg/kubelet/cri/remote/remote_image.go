f9707064cf91aaf5ec2805e0bf60d22fd8344855
Sascha Grunert <sgrunert@redhat.com>
2022-06-16 10:33:33 +0200

    Remove CRI v1alpha2
    After the removal of dockershim we can finally also drop support for CRI
    v1alpha2.
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- pkg/kubelet/cri/remote/remote_image.go
+++ pkg/kubelet/cri/remote/remote_image.go
@@ -34,16 +34,14 @@ import (
 
 	internalapi "k8s.io/cri-api/pkg/apis"
 	runtimeapi "k8s.io/cri-api/pkg/apis/runtime/v1"
-	runtimeapiV1alpha2 "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 	"k8s.io/kubernetes/pkg/features"
 	"k8s.io/kubernetes/pkg/kubelet/util"
 )
 
 // remoteImageService is a gRPC implementation of internalapi.ImageManagerService.
 type remoteImageService struct {
-	timeout             time.Duration
-	imageClient         runtimeapi.ImageServiceClient
-	imageClientV1alpha2 runtimeapiV1alpha2.ImageServiceClient
+	timeout     time.Duration
+	imageClient runtimeapi.ImageServiceClient
 }
 
 // NewRemoteImageService creates a new internalapi.ImageManagerService.
@@ -82,43 +80,28 @@ func NewRemoteImageService(endpoint string, connectionTimeout time.Duration, tp 
 
 	service := &remoteImageService{timeout: connectionTimeout}
 
-	if err := service.determineAPIVersion(conn, endpoint); err != nil {
-		return nil, err
+	if err := service.validateServiceConnection(conn, endpoint); err != nil {
+		return nil, fmt.Errorf("validate service connection: %w", err)
 	}
 
 	return service, nil
 
 }
 
-// useV1API returns true if the v1 CRI API should be used instead of v1alpha2.
-func (r *remoteImageService) useV1API() bool {
-	return r.imageClientV1alpha2 == nil
-}
-
-// determineAPIVersion tries to connect to the remote image service by using the
-// highest available API version.
-//
-// A GRPC redial will always use the initially selected (or automatically
-// determined) CRI API version. If the redial was due to the container runtime
-// being upgraded, then the container runtime must also support the initially
-// selected version or the redial is expected to fail, which requires a restart
-// of kubelet.
-func (r *remoteImageService) determineAPIVersion(conn *grpc.ClientConn, endpoint string) error {
+// validateServiceConnection tries to connect to the remote image service by
+// using the CRI v1 API version and fails if that's not possible.
+func (r *remoteImageService) validateServiceConnection(conn *grpc.ClientConn, endpoint string) error {
 	ctx, cancel := getContextWithTimeout(r.timeout)
 	defer cancel()
 
-	klog.V(4).InfoS("Finding the CRI API image version")
+	klog.V(4).InfoS("Validating the CRI v1 API image version")
 	r.imageClient = runtimeapi.NewImageServiceClient(conn)
 
 	if _, err := r.imageClient.ImageFsInfo(ctx, &runtimeapi.ImageFsInfoRequest{}); err == nil {
-		klog.V(2).InfoS("Using CRI v1 image API")
+		klog.V(2).InfoS("Validated CRI v1 image API")
 
 	} else if status.Code(err) == codes.Unimplemented {
-		klog.V(2).InfoS("Falling back to CRI v1alpha2 image API (deprecated)")
-		r.imageClientV1alpha2 = runtimeapiV1alpha2.NewImageServiceClient(conn)
-
-	} else {
-		return fmt.Errorf("unable to determine image API version with %q, or API is not implemented: %w", endpoint, err)
+		return fmt.Errorf("CRI v1 image API is not implemented for endpoint %q: %w", endpoint, err)
 	}
 
 	return nil
@@ -129,22 +112,7 @@ func (r *remoteImageService) ListImages(filter *runtimeapi.ImageFilter) ([]*runt
 	ctx, cancel := getContextWithTimeout(r.timeout)
 	defer cancel()
 
-	if r.useV1API() {
-		return r.listImagesV1(ctx, filter)
-	}
-
-	return r.listImagesV1alpha2(ctx, filter)
-}
-
-func (r *remoteImageService) listImagesV1alpha2(ctx context.Context, filter *runtimeapi.ImageFilter) ([]*runtimeapi.Image, error) {
-	resp, err := r.imageClientV1alpha2.ListImages(ctx, &runtimeapiV1alpha2.ListImagesRequest{
-		Filter: v1alpha2ImageFilter(filter),
-	})
-	if err != nil {
-		klog.ErrorS(err, "ListImages with filter from image service failed", "filter", filter)
-		return nil, err
-	}
-	return fromV1alpha2ListImagesResponse(resp).Images, nil
+	return r.listImagesV1(ctx, filter)
 }
 
 func (r *remoteImageService) listImagesV1(ctx context.Context, filter *runtimeapi.ImageFilter) ([]*runtimeapi.Image, error) {
@@ -164,37 +132,7 @@ func (r *remoteImageService) ImageStatus(image *runtimeapi.ImageSpec, verbose bo
 	ctx, cancel := getContextWithTimeout(r.timeout)
 	defer cancel()
 
-	// TODO: for refactoring common code blocks between the cri versions into
-	// one code block in the internal where possible examples:
-	// https://github.com/kubernetes/kubernetes/pull/104575/files#r705600987
-	// https://github.com/kubernetes/kubernetes/pull/104575/files#r696793706
-	if r.useV1API() {
-		return r.imageStatusV1(ctx, image, verbose)
-	}
-
-	return r.imageStatusV1alpha2(ctx, image, verbose)
-}
-
-func (r *remoteImageService) imageStatusV1alpha2(ctx context.Context, image *runtimeapi.ImageSpec, verbose bool) (*runtimeapi.ImageStatusResponse, error) {
-	resp, err := r.imageClientV1alpha2.ImageStatus(ctx, &runtimeapiV1alpha2.ImageStatusRequest{
-		Image:   v1alpha2ImageSpec(image),
-		Verbose: verbose,
-	})
-	if err != nil {
-		klog.ErrorS(err, "Get ImageStatus from image service failed", "image", image.Image)
-		return nil, err
-	}
-
-	if resp.Image != nil {
-		if resp.Image.Id == "" || resp.Image.Size_ == 0 {
-			errorMessage := fmt.Sprintf("Id or size of image %q is not set", image.Image)
-			err := errors.New(errorMessage)
-			klog.ErrorS(err, "ImageStatus failed", "image", image.Image)
-			return nil, err
-		}
-	}
-
-	return fromV1alpha2ImageStatusResponse(resp), nil
+	return r.imageStatusV1(ctx, image, verbose)
 }
 
 func (r *remoteImageService) imageStatusV1(ctx context.Context, image *runtimeapi.ImageSpec, verbose bool) (*runtimeapi.ImageStatusResponse, error) {
@@ -224,31 +162,7 @@ func (r *remoteImageService) PullImage(image *runtimeapi.ImageSpec, auth *runtim
 	ctx, cancel := getContextWithCancel()
 	defer cancel()
 
-	if r.useV1API() {
-		return r.pullImageV1(ctx, image, auth, podSandboxConfig)
-	}
-
-	return r.pullImageV1alpha2(ctx, image, auth, podSandboxConfig)
-}
-
-func (r *remoteImageService) pullImageV1alpha2(ctx context.Context, image *runtimeapi.ImageSpec, auth *runtimeapi.AuthConfig, podSandboxConfig *runtimeapi.PodSandboxConfig) (string, error) {
-	resp, err := r.imageClientV1alpha2.PullImage(ctx, &runtimeapiV1alpha2.PullImageRequest{
-		Image:         v1alpha2ImageSpec(image),
-		Auth:          v1alpha2AuthConfig(auth),
-		SandboxConfig: v1alpha2PodSandboxConfig(podSandboxConfig),
-	})
-	if err != nil {
-		klog.ErrorS(err, "PullImage from image service failed", "image", image.Image)
-		return "", err
-	}
-
-	if resp.ImageRef == "" {
-		klog.ErrorS(errors.New("PullImage failed"), "ImageRef of image is not set", "image", image.Image)
-		errorMessage := fmt.Sprintf("imageRef of image %q is not set", image.Image)
-		return "", errors.New(errorMessage)
-	}
-
-	return resp.ImageRef, nil
+	return r.pullImageV1(ctx, image, auth, podSandboxConfig)
 }
 
 func (r *remoteImageService) pullImageV1(ctx context.Context, image *runtimeapi.ImageSpec, auth *runtimeapi.AuthConfig, podSandboxConfig *runtimeapi.PodSandboxConfig) (string, error) {
@@ -276,16 +190,9 @@ func (r *remoteImageService) RemoveImage(image *runtimeapi.ImageSpec) (err error
 	ctx, cancel := getContextWithTimeout(r.timeout)
 	defer cancel()
 
-	if r.useV1API() {
-		_, err = r.imageClient.RemoveImage(ctx, &runtimeapi.RemoveImageRequest{
-			Image: image,
-		})
-	} else {
-		_, err = r.imageClientV1alpha2.RemoveImage(ctx, &runtimeapiV1alpha2.RemoveImageRequest{
-			Image: v1alpha2ImageSpec(image),
-		})
-	}
-	if err != nil {
+	if _, err = r.imageClient.RemoveImage(ctx, &runtimeapi.RemoveImageRequest{
+		Image: image,
+	}); err != nil {
 		klog.ErrorS(err, "RemoveImage from image service failed", "image", image.Image)
 		return err
 	}
@@ -300,20 +207,7 @@ func (r *remoteImageService) ImageFsInfo() ([]*runtimeapi.FilesystemUsage, error
 	ctx, cancel := getContextWithCancel()
 	defer cancel()
 
-	if r.useV1API() {
-		return r.imageFsInfoV1(ctx)
-	}
-
-	return r.imageFsInfoV1alpha2(ctx)
-}
-
-func (r *remoteImageService) imageFsInfoV1alpha2(ctx context.Context) ([]*runtimeapi.FilesystemUsage, error) {
-	resp, err := r.imageClientV1alpha2.ImageFsInfo(ctx, &runtimeapiV1alpha2.ImageFsInfoRequest{})
-	if err != nil {
-		klog.ErrorS(err, "ImageFsInfo from image service failed")
-		return nil, err
-	}
-	return fromV1alpha2ImageFsInfoResponse(resp).GetImageFilesystems(), nil
+	return r.imageFsInfoV1(ctx)
 }
 
 func (r *remoteImageService) imageFsInfoV1(ctx context.Context) ([]*runtimeapi.FilesystemUsage, error) {
