33a5804a3957641da6b1cac07e2b9436f161d60c
Don Penney <dpenney@redhat.com>
2022-06-16 10:25:54 -0400

    UPSTREAM: <carry>: Remove reserved CPUs from default set
    Remove reserved CPUs from default set when workload partitioning is
    enabled.
    
    Co-Authored-By: Brent Rowsell <browsell@redhat.com>
    Signed-off-by: Don Penney <dpenney@redhat.com>

--- pkg/kubelet/cm/cpumanager/policy_static.go
+++ pkg/kubelet/cm/cpumanager/policy_static.go
@@ -27,6 +27,7 @@ import (
 	"k8s.io/kubernetes/pkg/kubelet/cm/cpuset"
 	"k8s.io/kubernetes/pkg/kubelet/cm/topologymanager"
 	"k8s.io/kubernetes/pkg/kubelet/cm/topologymanager/bitmask"
+	"k8s.io/kubernetes/pkg/kubelet/managed"
 )
 
 const (
@@ -173,6 +174,10 @@ func (p *staticPolicy) validateState(s state.State) error {
 		// state is empty initialize
 		allCPUs := p.topology.CPUDetails.CPUs()
 		s.SetDefaultCPUSet(allCPUs)
+		if managed.IsEnabled() {
+			defaultCpus := s.GetDefaultCPUSet().Difference(p.reserved)
+			s.SetDefaultCPUSet(defaultCpus)
+		}
 		return nil
 	}
 
