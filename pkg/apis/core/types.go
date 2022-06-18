fe49df1f468e700aad64612f4aa076a50282a531
Kensei Nakada <handbomusic@gmail.com>
2022-06-18 12:53:38 +0000

    Graduate MinDomains in Pod Topology Spread to beta

--- pkg/apis/core/types.go
+++ pkg/apis/core/types.go
@@ -5711,7 +5711,7 @@ type TopologySpreadConstraint struct {
 	// because computed skew will be 3(3 - 0) if new Pod is scheduled to any of the three zones,
 	// it will violate MaxSkew.
 	//
-	// This is an alpha field and requires enabling MinDomainsInPodTopologySpread feature gate.
+	// This is an beta field and requires enabling MinDomainsInPodTopologySpread feature gate.
 	// +optional
 	MinDomains *int32
 	// NodeAffinityPolicy indicates how we will treat Pod's nodeAffinity/nodeSelector
