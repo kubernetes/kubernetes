70ee2a46b5f8411cdaa45e1072a56c42c6d73da8
Paco Xu <paco.xu@daocloud.io>
2022-06-23 13:42:46 +0800

    kubectl: add ut for daemonSetFilter
    Signed-off-by: Paco Xu <paco.xu@daocloud.io>

--- staging/src/k8s.io/kubectl/pkg/drain/filter_test.go
+++ staging/src/k8s.io/kubectl/pkg/drain/filter_test.go
@@ -20,8 +20,11 @@ import (
 	"testing"
 	"time"
 
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/utils/pointer"
 )
 
 func TestSkipDeletedFilter(t *testing.T) {
@@ -65,6 +68,120 @@ func TestSkipDeletedFilter(t *testing.T) {
 		podDeleteStatus := h.skipDeletedFilter(pod)
 		if podDeleteStatus.Delete != tc.expectedDelete {
 			t.Errorf("test %v: unexpected podDeleteStatus.delete; actual %v; expected %v", i, podDeleteStatus.Delete, tc.expectedDelete)
+		}
+	}
+}
+
+func TestDaemonSetFilter(t *testing.T) {
+	tCases := []struct {
+		ownerKind            string
+		force                bool
+		ignoreAllDaemonSets  bool
+		podPhase             corev1.PodPhase
+		expectedDelete       bool
+		expectedReason       string
+		expectedErrorMessage string
+	}{
+		{
+			ownerKind:            "DaemonSet",
+			ignoreAllDaemonSets:  true,
+			force:                true,
+			podPhase:             corev1.PodRunning,
+			expectedDelete:       false,
+			expectedReason:       PodDeleteStatusTypeWarning,
+			expectedErrorMessage: daemonSetWarning,
+		},
+		{
+			ownerKind:            "DaemonSet",
+			ignoreAllDaemonSets:  false,
+			force:                true,
+			podPhase:             corev1.PodRunning,
+			expectedDelete:       false,
+			expectedReason:       PodDeleteStatusTypeError,
+			expectedErrorMessage: daemonSetFatal,
+		},
+		{
+			ownerKind:            "DaemonSet",
+			ignoreAllDaemonSets:  false,
+			force:                false,
+			podPhase:             corev1.PodRunning,
+			expectedDelete:       false,
+			expectedReason:       PodDeleteStatusTypeError,
+			expectedErrorMessage: daemonSetFatal,
+		},
+		{
+			ownerKind:            "DaemonSet",
+			ignoreAllDaemonSets:  true,
+			force:                false,
+			podPhase:             corev1.PodRunning,
+			expectedDelete:       false,
+			expectedReason:       PodDeleteStatusTypeWarning,
+			expectedErrorMessage: daemonSetWarning,
+		},
+		{
+			ownerKind:           "DaemonSet",
+			ignoreAllDaemonSets: true,
+			force:               false,
+			podPhase:            corev1.PodFailed,
+			expectedDelete:      true,
+			expectedReason:      PodDeleteStatusTypeOkay,
+		},
+		{
+			ownerKind:           "ReplicaSet",
+			ignoreAllDaemonSets: false,
+			force:               false,
+			podPhase:            corev1.PodRunning,
+			expectedDelete:      true,
+			expectedReason:      PodDeleteStatusTypeOkay,
+		},
+		{
+			ownerKind:           "ReplicaSet",
+			ignoreAllDaemonSets: true,
+			force:               false,
+			podPhase:            corev1.PodRunning,
+			expectedDelete:      true,
+			expectedReason:      PodDeleteStatusTypeOkay,
+		},
+	}
+	for i, tc := range tCases {
+		pod := corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "pod",
+				Namespace: "default",
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion: "apps/v1",
+						Kind:       tc.ownerKind,
+						Controller: pointer.BoolPtr(true),
+						Name:       "bar",
+					},
+				},
+			},
+			Status: corev1.PodStatus{
+				Phase: tc.podPhase,
+			},
+		}
+
+		h := &Helper{
+			Force:               tc.force,
+			IgnoreAllDaemonSets: tc.ignoreAllDaemonSets,
+			Client: fake.NewSimpleClientset(&appsv1.DaemonSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "bar",
+					Namespace: "default",
+				},
+			}),
+		}
+
+		podDeleteStatus := h.daemonSetFilter(pod)
+		if podDeleteStatus.Delete != tc.expectedDelete {
+			t.Errorf("test %v: unexpected podDeleteStatus.delete; actual %v; expected %v", i, podDeleteStatus.Delete, tc.expectedDelete)
+		}
+		if podDeleteStatus.Reason != tc.expectedReason {
+			t.Errorf("test %v: unexpected podDeleteStatus.reason; actual %v; expected %v", i, podDeleteStatus.Reason, tc.expectedReason)
+		}
+		if podDeleteStatus.Message != tc.expectedErrorMessage {
+			t.Errorf("test %v: unexpected podDeleteStatus.message; actual %v; expected %v", i, podDeleteStatus.Message, tc.expectedErrorMessage)
 		}
 	}
 }
