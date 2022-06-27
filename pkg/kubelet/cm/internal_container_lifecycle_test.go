7dfaac3800c667ebe9ac81813330f0c5165b5b63
prakharporwal <rsanshul99@gmail.com>
2022-06-27 10:09:59 +0530

    creating mock for internal lifecycle test

--- pkg/kubelet/cm/internal_container_lifecycle_test.go
+++ pkg/kubelet/cm/internal_container_lifecycle_test.go
@@ -2,6 +2,7 @@ package cm
 
 import (
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
 	"k8s.io/api/core/v1"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	kubefeatures "k8s.io/kubernetes/pkg/features"
@@ -13,119 +14,122 @@ import (
 )
 
 func TestPreStartContainer(t *testing.T) {
-	// setup vars
+	mockCtrl := gomock.NewController(t)
 
 	var tests = []struct {
-		name string
-		i    internalContainerLifecycleImpl
+		name            string
+		i               internalContainerLifecycleImpl
+		cpuManager      cpumanager.MockManager
+		memoryManager   memorymanager.MockManager
+		topologyManager topologymanager.MockManager
 	}{
+		// setup vars
+		//{
+		//	name: "when cpu,memory,topology manager are nil",
+		//},
+		//{
+		//	name: "when only cpu manager is not nil",
+		//
+		//	cpuManager:      *cpumanager.NewMockManager(mockCtrl),
+		//	memoryManager:   nil,
+		//	topologyManager: nil,
+		//},
+		//{
+		//	name: "when cpu and memory manager are not nil",
+		//	//i: internalContainerLifecycleImpl{
+		//	cpuManager:      cpumanager.NewMockManager(mockCtrl),
+		//	memoryManager:   memorymanager.NewMockManager(mockCtrl),
+		//	topologyManager: nil,
+		//	//},
+		//},
+		//{
+		//	name: "when cpu and topology manager are not nil",
+		//	//i: internalContainerLifecycleImpl{
+		//	cpuManager:      cpumanager.NewMockManager(mockCtrl),
+		//	memoryManager:   nil,
+		//	topologyManager: topologymanager.NewMockManager(mockCtrl),
+		//	//},
+		//},
+		//{
+		//	name: "when only memory manager is not nil",
+		//	//i: internalContainerLifecycleImpl{
+		//	cpuManager:      nil,
+		//	memoryManager:   memorymanager.NewMockManager(mockCtrl),
+		//	topologyManager: nil,
+		//	//},
+		//},
+		//{
+		//	name: "when memory and topology manager is not nil",
+		//	//i: internalContainerLifecycleImpl{
+		//	cpuManager:      nil,
+		//	memoryManager:   memorymanager.NewMockManager(mockCtrl),
+		//	topologyManager: topologymanager.NewMockManager(mockCtrl),
+		//	//},
+		//},
+		//{
+		//	name: "when only topology manager is not nil",
+		//	//i: internalContainerLifecycleImpl{
+		//	cpuManager:      nil,
+		//	memoryManager:   nil,
+		//	topologyManager: topologymanager.NewMockManager(mockCtrl),
+		//	//},
+		//},
 		{
-			name: "when cpu,memory,topology manager are nil",
-			i: internalContainerLifecycleImpl{
-				cpuManager:      nil,
-				memoryManager:   nil,
-				topologyManager: nil,
-			},
-		},
-		{
-			name: "when only cpu manager is not nil",
-			i: internalContainerLifecycleImpl{
-				cpuManager:      nil,
-				memoryManager:   nil,
-				topologyManager: nil,
-			},
-		},
-		{
-			name: "when cpu and memory manager are not nil",
-			i: internalContainerLifecycleImpl{
-				cpuManager:      cpumanager.NewFakeManager(),
-				memoryManager:   memorymanager.NewFakeManager(),
-				topologyManager: nil,
-			},
-		},
-		{
-			name: "when cpu and topology manager are not nil",
-			i: internalContainerLifecycleImpl{
-				cpuManager:      cpumanager.NewFakeManager(),
-				memoryManager:   nil,
-				topologyManager: topologymanager.NewFakeManager(),
-			},
-		},
-		{
-			name: "when only memory manager is not nil",
-			i: internalContainerLifecycleImpl{
-				cpuManager:      nil,
-				memoryManager:   memorymanager.NewFakeManager(),
-				topologyManager: nil,
-			},
-		},
-		{
-			name: "when memory and topology manager is not nil",
-			i: internalContainerLifecycleImpl{
-				cpuManager:      nil,
-				memoryManager:   memorymanager.NewFakeManager(),
-				topologyManager: topologymanager.NewFakeManager(),
-			},
-		},
-		{
-			name: "when only topology manager is not nil",
-			i: internalContainerLifecycleImpl{
-				cpuManager:      nil,
-				memoryManager:   nil,
-				topologyManager: topologymanager.NewFakeManager(),
-			},
-		},
-		{
-			name: "when cpu , memory, topology manager are not nil",
-			i: internalContainerLifecycleImpl{
-				cpuManager:      cpumanager.NewFakeManager(),
-				memoryManager:   memorymanager.NewFakeManager(),
-				topologyManager: topologymanager.NewFakeManager(),
-			},
+			name: "when cpu, memory, topology manager are not nil",
+			//i: internalContainerLifecycleImpl{
+			cpuManager:      *cpumanager.NewMockManager(mockCtrl),
+			memoryManager:   *memorymanager.NewMockManager(mockCtrl),
+			topologyManager: *topologymanager.NewMockManager(mockCtrl),
+			//},
 		},
 	}
 
 	for _, test := range tests {
-		mockCtrl := gomock.NewController(t)
-
-		pod := &v1.Pod{}
-		container := &v1.Container{}
-		containerId := "test-id"
-		i := test.i
-
 		t.Run(test.name, func(t *testing.T) {
-			mockCPUManager := cpumanager.NewMockManager(mockCtrl)
-			mockMemoryManager := memorymanager.NewMockManager(mockCtrl)
-			mockTopologyManager := topologymanager.NewMockManager(mockCtrl)
+			i := test.i
+			i.cpuManager = &test.cpuManager
+			i.memoryManager = &test.memoryManager
+			i.topologyManager = &test.topologyManager
 
+			//assert.NoError(t, i.PreStartContainer(pod, container, containerId), "No error expected")
+			//assert.NoError(t, i.PreStartContainer(pod, container, containerId), "PreStartContainer throws error when no error expected")
 			// run the function
-			err := i.PreStartContainer(pod, container, containerId)
-			if err != nil {
-				t.Error("Error Occured!")
-			}
 
-			if i.cpuManager != nil {
-				//i.cpuManager.AddContainer(pod, container, containerId)
-				mockCPUManager.EXPECT().AddContainer(pod, container, containerId).Return(nil).Times(1)
-			} else {
-				mockCPUManager.EXPECT().AddContainer(pod, container, containerId).Return(nil).Times(0)
-			}
+			pod := &v1.Pod{}
+			container := &v1.Container{}
+			containerId := "someid"
 
-			if i.memoryManager != nil {
-				mockMemoryManager.EXPECT().AddContainer(pod, container, containerId).Return(nil).Times(1)
-			} else {
-				mockMemoryManager.EXPECT().AddContainer(pod, container, containerId).Return(nil).Times(0)
-			}
+			store := NewMockInternalContainerLifecycle(mockCtrl)
 
-			if i.topologyManager != nil {
-				if utilfeature.DefaultFeatureGate.Enabled(kubefeatures.TopologyManager) {
-					mockTopologyManager.EXPECT().AddContainer(pod, container, containerId).Return(nil).Times(1)
-				} else {
-					mockTopologyManager.EXPECT().AddContainer(pod, container, containerId).Return(nil).Times(0)
-				}
-			} else {
-				mockTopologyManager.EXPECT().AddContainer(pod, container, containerId).Return(nil).Times(0)
-			}
+			store.EXPECT().PreStartContainer(gomock.Any(), gomock.Any(), gomock.Eq(containerId)).Times(1)
+			test.cpuManager.EXPECT().AddContainer(gomock.Any(), gomock.Any(), gomock.Eq(containerId)).Times(1)
+			test.memoryManager.EXPECT().AddContainer(gomock.Any(), gomock.Any(), gomock.Eq(containerId)).Times(1)
+			test.topologyManager.EXPECT().AddContainer(gomock.Any(), gomock.Any(), gomock.Eq(containerId)).Times(1)
+
+			err := test.i.PreStartContainer(pod, container, containerId)
+			require.NoError(t, err, "No error expected")
+			//if i.cpuManager != nil {
+			//test.cpuManager.AddContainer(pod, container, containerId)
+			//test.cpuManager.EXPECT().AddContainer(pod, container, containerId).Times(1)
+			//} else {
+			//mockCPUManager.EXPECT().AddContainer(pod, container, containerId).Times(0)
+			//}
+			//
+			//if i.memoryManager != nil {
+			//	test.memoryManager.EXPECT().AddContainer(pod, container, containerId).Times(1)
+			//} else {
+			//			mockMemoryManager.EXPECT().AddContainer(pod, container, containerId).Times(0)
+			//}
+			//
+			//if i.topologyManager != nil {
+			//	if utilfeature.DefaultFeatureGate.Enabled(kubefeatures.TopologyManager) {
+			//		test.topologyManager.EXPECT().AddContainer(pod, container, containerId).Times(1)
+			//	} else {
+			//				mockTopologyManager.EXPECT().AddContainer(pod, container, containerId).Times(0)
+			//}
+			//} else {
+			//	//			mockTopologyManager.EXPECT().AddContainer(pod, container, containerId).Times(0)
+			//}
 		})
 	}
 }
