6b89cade2687f1311c7291a1dc7bedeb75c804b1
Marcus Puckett <mpuckett259@gmail.com>
2022-06-28 00:14:39 -0700

    Add case handling for if a user passes a jsonpath string to unset

--- staging/src/k8s.io/kubectl/pkg/cmd/config/unset.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/config/unset.go
@@ -20,6 +20,8 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	clientcmdapiv1 "k8s.io/client-go/tools/clientcmd/api/v1"
 	"reflect"
 
 	"github.com/spf13/cobra"
@@ -33,25 +35,29 @@ import (
 type unsetOptions struct {
 	configAccess clientcmd.ConfigAccess
 	propertyName string
+	jsonPath     bool
 }
 
 var (
 	unsetLong = templates.LongDesc(i18n.T(`
 	Unset an individual value in a kubeconfig file.
 
-	PROPERTY_NAME is a dot delimited name where each token represents either an attribute name or a map key.  Map keys may not contain dots.`))
+	PROPERTY_NAME is either a jsonpath query for where the property name will be, or a dot delimited name.`))
 
 	unsetExample = templates.Examples(`
 		# Unset the current-context
 		kubectl config unset current-context
 
 		# Unset namespace in foo context
-		kubectl config unset contexts.foo.namespace`)
+		kubectl config unset contexts.foo.namespace
+
+		# Unset cluster server using jsonpath
+		kubectl config unset '{.clusters[?(@.name=="cluster-0")].cluster.server}' --jsonpath`)
 )
 
 // NewCmdConfigUnset returns a Command instance for 'config unset' sub command
 func NewCmdConfigUnset(out io.Writer, configAccess clientcmd.ConfigAccess) *cobra.Command {
-	options := &unsetOptions{configAccess: configAccess}
+	options := &unsetOptions{configAccess: configAccess, jsonPath: false}
 
 	cmd := &cobra.Command{
 		Use:                   "unset PROPERTY_NAME",
@@ -80,13 +86,32 @@ func (o unsetOptions) run(out io.Writer) error {
 		return err
 	}
 
-	steps, err := newNavigationSteps(o.propertyName)
-	if err != nil {
-		return err
-	}
-	err = modifyConfig(reflect.ValueOf(config), steps, "", true, true)
-	if err != nil {
-		return err
+	if o.jsonPath {
+		// Convert api config to apiv1 config, so we can use jsonpath properly
+		v1Config := &clientcmdapiv1.Config{}
+		if err := clientcmdapiv1.Convert_api_Config_To_v1_Config(config, v1Config, nil); err != nil {
+			return err
+		}
+
+		if err := modifyConfigJson(v1Config, o.propertyName, "", true, false, false); err != nil {
+			return err
+		}
+
+		// Convert the apiv1 config back to an api config to write back out
+		finalConfig := clientcmdapi.NewConfig()
+		if err := clientcmdapiv1.Convert_v1_Config_To_api_Config(v1Config, finalConfig, nil); err != nil {
+			return err
+		}
+		config = finalConfig
+	} else {
+		steps, err := newNavigationSteps(o.propertyName)
+		if err != nil {
+			return err
+		}
+		err = modifyConfig(reflect.ValueOf(config), steps, "", true, true)
+		if err != nil {
+			return err
+		}
 	}
 
 	if err := clientcmd.ModifyConfig(o.configAccess, *config, false); err != nil {
@@ -95,6 +120,7 @@ func (o unsetOptions) run(out io.Writer) error {
 	if _, err := fmt.Fprintf(out, "Property %q unset.\n", o.propertyName); err != nil {
 		return err
 	}
+
 	return nil
 }
 
@@ -104,6 +130,14 @@ func (o *unsetOptions) complete(cmd *cobra.Command, args []string) error {
 	}
 
 	o.propertyName = args[0]
+
+	// try to determine if we have a jsonpath from first character of first argument
+	// this should only ever be a { if this is a jsonpath string, if it is not we will try to use the old dot delimited
+	// syntax instead.
+	if string(args[0][0]) == "{" {
+		o.jsonPath = true
+	}
+
 	return nil
 }
 
