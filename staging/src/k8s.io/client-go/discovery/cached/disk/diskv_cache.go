509acfd740883e40fd104097400d75b0ab469849
Nic Cope <nicc@rk0n.org>
2022-06-27 21:54:02 -0700

    Duplicate github.com/gregjones/httpcache/diskcache in tree.
    The httpcache package uses github.com/peterbourgon/diskv to back its
    disk cache. It is hard-coded to explicitly call f.Sync() for every file
    it writes to disk, and it writes a file for every HTTP response body
    that it caches. This is extremely slow on MacOS, causing write delays of
    10+ seconds when performing discovery against an API server with ~350
    API groups.
    
    We'd like to change this behaviour, but no httpcache changes have been
    merged in the last 3 years. The project also states that changes to its
    public API won't be considered, and we'd presumably have to make such a
    change in order to maintain backward compatibility by making whether or
    not it should call f.Sync() configurable.
    
    I couldn't immediately find any good replacements for what httpcache
    does, and I'm hesitant to try to reimplement it all in-tree. Instead I
    intend to copy in just the diskcache logic and alter it to disable fsync
    in a subsequent commit.
    
    See https://github.com/kubernetes/kubernetes/issues/110753 for more.
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- staging/src/k8s.io/client-go/discovery/cached/disk/diskv_cache.go
+++ staging/src/k8s.io/client-go/discovery/cached/disk/diskv_cache.go
@@ -1,14 +1,32 @@
-// Package diskcache provides an implementation of httpcache.Cache that uses the diskv package
-// to supplement an in-memory map with persistent storage
-//
-package diskcache
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package disk
+
+// NOTE(negz): This file is copied from the upstream httpcache implementation,
+// which is no longer maintained.
+// https://github.com/gregjones/httpcache/blob/901d90/diskcache/diskcache.go
 
 import (
 	"bytes"
 	"crypto/md5"
 	"encoding/hex"
-	"github.com/peterbourgon/diskv"
 	"io"
+
+	"github.com/peterbourgon/diskv"
 )
 
 // Cache is an implementation of httpcache.Cache that supplements the in-memory map with persistent storage
