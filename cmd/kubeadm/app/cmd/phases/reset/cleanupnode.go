c85759f5f0d35ca1dec947c08251171971f4634f
Dave Chen <dave.chen@arm.com>
2022-07-05 17:22:47 +0800

    Move the logic of file cleanup within each phase
    Guarantee that stale files are removed if end user resets cluster
    by resetting each phase.
    
    Signed-off-by: Dave Chen <dave.chen@arm.com>

--- cmd/kubeadm/app/cmd/phases/reset/cleanupnode.go
+++ cmd/kubeadm/app/cmd/phases/reset/cleanupnode.go
@@ -51,6 +51,7 @@ func NewCleanupNodePhase() workflow.Phase {
 }
 
 func runCleanupNode(c workflow.RunData) error {
+	dirsToClean := []string{filepath.Join(kubeadmconstants.KubernetesDir, kubeadmconstants.ManifestsSubDirName)}
 	r, ok := c.(resetData)
 	if !ok {
 		return errors.New("cleanup-node phase invoked with an invalid data struct")
@@ -82,7 +83,7 @@ func runCleanupNode(c workflow.RunData) error {
 		kubeletRunDir, err := absoluteKubeletRunDirectory()
 		if err == nil {
 			// Only clean absoluteKubeletRunDirectory if umountDirsCmd passed without error
-			r.AddDirsToClean(kubeletRunDir)
+			dirsToClean = append(dirsToClean, kubeletRunDir)
 		}
 	} else {
 		fmt.Printf("[reset] Would unmount mounted directories in %q\n", kubeadmconstants.KubeletRunDirectory)
@@ -101,7 +102,8 @@ func runCleanupNode(c workflow.RunData) error {
 	if certsDir != kubeadmapiv1.DefaultCertificatesDir {
 		klog.Warningf("[reset] WARNING: Cleaning a non-default certificates directory: %q\n", certsDir)
 	}
-	resetConfigDir(kubeadmconstants.KubernetesDir, certsDir, r.DryRun())
+	dirsToClean = append(dirsToClean, certsDir)
+	resetConfigDir(kubeadmconstants.KubernetesDir, dirsToClean, r.DryRun())
 
 	if r.Cfg() != nil && features.Enabled(r.Cfg().FeatureGates, features.RootlessControlPlane) {
 		if !r.DryRun() {
@@ -143,12 +145,8 @@ func removeContainers(execer utilsexec.Interface, criSocketPath string) error {
 	return containerRuntime.RemoveContainers(containers)
 }
 
-// resetConfigDir is used to cleanup the files kubeadm writes in /etc/kubernetes/.
-func resetConfigDir(configPathDir, pkiPathDir string, isDryRun bool) {
-	dirsToClean := []string{
-		filepath.Join(configPathDir, kubeadmconstants.ManifestsSubDirName),
-		pkiPathDir,
-	}
+// resetConfigDir is used to cleanup the files in the folder defined in dirsToClean.
+func resetConfigDir(configPathDir string, dirsToClean []string, isDryRun bool) {
 	if !isDryRun {
 		fmt.Printf("[reset] Deleting contents of directories: %v\n", dirsToClean)
 		for _, dir := range dirsToClean {
