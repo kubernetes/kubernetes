7ff8be658fd6d7b5febbe76ca26db30554354b88
Manish Kumar <manish.kumar1@india.nec.com>
2022-07-13 09:50:15 +0530

    Update key file permission.
    Updated key file mode to 0600

--- staging/src/k8s.io/client-go/util/cert/cert.go
+++ staging/src/k8s.io/client-go/util/cert/cert.go
@@ -191,7 +191,7 @@ func GenerateSelfSignedCertKeyWithFixtures(host string, alternateIPs []net.IP, a
 		if err := ioutil.WriteFile(certFixturePath, certBuffer.Bytes(), 0644); err != nil {
 			return nil, nil, fmt.Errorf("failed to write cert fixture to %s: %v", certFixturePath, err)
 		}
-		if err := ioutil.WriteFile(keyFixturePath, keyBuffer.Bytes(), 0644); err != nil {
+		if err := ioutil.WriteFile(keyFixturePath, keyBuffer.Bytes(), 0600); err != nil {
 			return nil, nil, fmt.Errorf("failed to write key fixture to %s: %v", certFixturePath, err)
 		}
 	}
