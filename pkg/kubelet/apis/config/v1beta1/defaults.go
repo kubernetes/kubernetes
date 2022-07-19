7248a33fbcf5cdfd2ac98f374f86c28166095182
HaoJie Liu <liuhaojie@beyondcent.com>
2022-07-19 11:09:38 +0800

    fix: broken link update
    Signed-off-by: HaoJie Liu <liuhaojie@beyondcent.com>

--- pkg/kubelet/apis/config/v1beta1/defaults.go
+++ pkg/kubelet/apis/config/v1beta1/defaults.go
@@ -44,7 +44,7 @@ const (
 var (
 	zeroDuration = metav1.Duration{}
 	// TODO: Move these constants to k8s.io/kubelet/config/v1beta1 instead?
-	// Refer to [Node Allocatable](https://git.k8s.io/community/contributors/design-proposals/node/node-allocatable.md) doc for more information.
+	// Refer to [Node Allocatable](https://github.com/kubernetes/design-proposals-archive/blob/main/node/node-allocatable.md) doc for more information.
 	DefaultNodeAllocatableEnforcement = []string{"pods"}
 )
 
