5f59159bfc70b85c37ae4fa4039bc4c39db8ca95
Tim Hockin <thockin@google.com>
2022-08-04 10:26:26 -0700

    Docs: node-port-range should not overlap ephemeral

--- cmd/kube-apiserver/app/options/options.go
+++ cmd/kube-apiserver/app/options/options.go
@@ -201,7 +201,7 @@ func (s *ServerRunOptions) Flags() (fss cliflag.NamedFlagSets) {
 		"overlap with any IP ranges assigned to nodes or pods. Max of two dual-stack CIDRs is allowed.")
 
 	fs.Var(&s.ServiceNodePortRange, "service-node-port-range", ""+
-		"A port range to reserve for services with NodePort visibility. "+
+		"A port range to reserve for services with NodePort visibility.  This must not overlap with the ephemeral port range on nodes.  "+
 		"Example: '30000-32767'. Inclusive at both ends of the range.")
 
 	// Kubelet related flags:
