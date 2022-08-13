5359cc4714e6b479cc72cc4f2e6f37c7d991f7f7
Zemtsov Vladimir <vl.zemtsov@gmail.com>
2022-08-13 12:04:21 +0300

    Fix status type in schedule logs
    Signed-off-by: Zemtsov Vladimir <vl.zemtsov@gmail.com>

--- pkg/scheduler/schedule_one.go
+++ pkg/scheduler/schedule_one.go
@@ -108,10 +108,10 @@ func (sched *Scheduler) scheduleOne(ctx context.Context) {
 				// Run PostFilter plugins to try to make the pod schedulable in a future scheduling cycle.
 				result, status := fwk.RunPostFilterPlugins(ctx, state, pod, fitError.Diagnosis.NodeToStatusMap)
 				if status.Code() == framework.Error {
-					klog.ErrorS(nil, "Status after running PostFilter plugins for pod", "pod", klog.KObj(pod), "status", status)
+					klog.ErrorS(nil, "Status after running PostFilter plugins for pod", "pod", klog.KObj(pod), "status", status.Message())
 				} else {
 					fitError.Diagnosis.PostFilterMsg = status.Message()
-					klog.V(5).InfoS("Status after running PostFilter plugins for pod", "pod", klog.KObj(pod), "status", status)
+					klog.V(5).InfoS("Status after running PostFilter plugins for pod", "pod", klog.KObj(pod), "status", status.Message())
 				}
 				if result != nil {
 					nominatingInfo = result.NominatingInfo
