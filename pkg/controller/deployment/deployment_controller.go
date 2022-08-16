8f31eb4c066213db85b8c8abadc2c75300143bde
yangzhen.386071268 <yangzhen.386071268@bytedance.com>
2022-08-16 18:17:20 +0800

    fix goimports

--- pkg/controller/deployment/deployment_controller.go
+++ pkg/controller/deployment/deployment_controller.go
@@ -29,7 +29,7 @@ import (
 	"k8s.io/klog/v2"
 
 	apps "k8s.io/api/apps/v1"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
