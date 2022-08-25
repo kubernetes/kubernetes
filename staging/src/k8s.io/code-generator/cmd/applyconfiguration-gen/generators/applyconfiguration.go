45c4311b4e5926ef00ebcccff8e60716bc844b6f
David Eads <deads@redhat.com>
2022-08-25 16:29:08 -0400

    make applyconfiguration-gen skip generation for types that have generated clients and lack objectmeta

--- staging/src/k8s.io/code-generator/cmd/applyconfiguration-gen/generators/applyconfiguration.go
+++ staging/src/k8s.io/code-generator/cmd/applyconfiguration-gen/generators/applyconfiguration.go
@@ -97,12 +97,9 @@ func (g *applyConfigurationGenerator) GenerateType(c *generator.Context, t *type
 	g.generateStruct(sw, typeParams)
 
 	if typeParams.Tags.GenerateClient {
-		switch {
-		case !hasObjectMetaField(t):
-			sw.Do(clientgenTypeConstructorWithoutObjectMeta, typeParams)
-		case typeParams.Tags.NonNamespaced:
+		if typeParams.Tags.NonNamespaced {
 			sw.Do(clientgenTypeConstructorNonNamespaced, typeParams)
-		default:
+		} else {
 			sw.Do(clientgenTypeConstructorNamespaced, typeParams)
 		}
 		if typeParams.OpenAPIType != nil {
@@ -122,15 +119,6 @@ func (g *applyConfigurationGenerator) GenerateType(c *generator.Context, t *type
 func hasTypeMetaField(t *types.Type) bool {
 	for _, member := range t.Members {
 		if typeMeta.Name == member.Type.Name && member.Embedded {
-			return true
-		}
-	}
-	return false
-}
-
-func hasObjectMetaField(t *types.Type) bool {
-	for _, member := range t.Members {
-		if objectMeta.Name == member.Type.Name && member.Embedded {
 			return true
 		}
 	}
@@ -369,17 +357,6 @@ func $.ApplyConfig.Type|public$(name string) *$.ApplyConfig.ApplyConfiguration|p
 }
 `
 
-var clientgenTypeConstructorWithoutObjectMeta = `
-// $.ApplyConfig.Type|public$ constructs an declarative configuration of the $.ApplyConfig.Type|public$ type for use with
-// apply.
-func $.ApplyConfig.Type|public$(name string) *$.ApplyConfig.ApplyConfiguration|public$ {
-  b := &$.ApplyConfig.ApplyConfiguration|public${}
-  b.WithKind("$.ApplyConfig.Type|singularKind$")
-  b.WithAPIVersion("$.APIVersion$")
-  return b
-}
-`
-
 var constructorWithTypeMeta = `
 // $.ApplyConfig.ApplyConfiguration|public$ constructs an declarative configuration of the $.ApplyConfig.Type|public$ type for use with
 // apply.
@@ -425,18 +402,7 @@ func Extract$.ApplyConfig.Type|public$Status($.Struct|private$ *$.Struct|raw$, f
 }
 `, typeParams)
 	}
-	if !hasObjectMetaField(typeParams.Struct) {
-		sw.Do(`
-func extract$.ApplyConfig.Type|public$($.Struct|private$ *$.Struct|raw$, fieldManager string, subresource string) (*$.ApplyConfig.ApplyConfiguration|public$, error) {
-	b := &$.ApplyConfig.ApplyConfiguration|public${}
-	err := $.ExtractInto|raw$($.Struct|private$, $.ParserFunc|raw$().Type("$.OpenAPIType$"), fieldManager, b, subresource)
-	if err != nil {
-		return nil, err
-	}
-`, typeParams)
-
-	} else { // it has objectMeta
-		sw.Do(`
+	sw.Do(`
 func extract$.ApplyConfig.Type|public$($.Struct|private$ *$.Struct|raw$, fieldManager string, subresource string) (*$.ApplyConfig.ApplyConfiguration|public$, error) {
 	b := &$.ApplyConfig.ApplyConfiguration|public${}
 	err := $.ExtractInto|raw$($.Struct|private$, $.ParserFunc|raw$().Type("$.OpenAPIType$"), fieldManager, b, subresource)
@@ -445,9 +411,8 @@ func extract$.ApplyConfig.Type|public$($.Struct|private$ *$.Struct|raw$, fieldMa
 	}
 	b.WithName($.Struct|private$.Name)
 `, typeParams)
-		if !typeParams.Tags.NonNamespaced {
-			sw.Do("b.WithNamespace($.Struct|private$.Namespace)\n", typeParams)
-		}
+	if !typeParams.Tags.NonNamespaced {
+		sw.Do("b.WithNamespace($.Struct|private$.Namespace)\n", typeParams)
 	}
 	sw.Do(`
 	b.WithKind("$.ApplyConfig.Type|singularKind$")
