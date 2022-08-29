df71672e5798960d0c6b4fa730aa7ac12991aef5
Oldřich Jedlička <oldium.pro@gmail.com>
2022-08-29 22:13:25 +0200

    Fix editor launch with Windows cmd.exe when KUBE_EDITOR has spaces

--- staging/src/k8s.io/kubectl/pkg/cmd/util/editor/editor.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/util/editor/editor.go
@@ -17,17 +17,10 @@ limitations under the License.
 package editor
 
 import (
-	"fmt"
 	"io"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"runtime"
 	"strings"
-
-	"k8s.io/klog/v2"
-
-	"k8s.io/kubectl/pkg/util/term"
 )
 
 const (
@@ -94,45 +87,6 @@ func defaultEnvEditor(envs []string) ([]string, bool) {
 	// rather than parse the shell arguments ourselves, punt to the shell
 	shell := defaultEnvShell()
 	return append(shell, editor), true
-}
-
-func (e Editor) args(path string) []string {
-	args := make([]string, len(e.Args))
-	copy(args, e.Args)
-	if e.Shell {
-		last := args[len(args)-1]
-		args[len(args)-1] = fmt.Sprintf("%s %q", last, path)
-	} else {
-		args = append(args, path)
-	}
-	return args
-}
-
-// Launch opens the described or returns an error. The TTY will be protected, and
-// SIGQUIT, SIGTERM, and SIGINT will all be trapped.
-func (e Editor) Launch(path string) error {
-	if len(e.Args) == 0 {
-		return fmt.Errorf("no editor defined, can't open %s", path)
-	}
-	abs, err := filepath.Abs(path)
-	if err != nil {
-		return err
-	}
-	args := e.args(abs)
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-	klog.V(5).Infof("Opening file with editor %v", args)
-	if err := (term.TTY{In: os.Stdin, TryDev: true}).Safe(cmd.Run); err != nil {
-		if err, ok := err.(*exec.Error); ok {
-			if err.Err == exec.ErrNotFound {
-				return fmt.Errorf("unable to launch the editor %q", strings.Join(e.Args, " "))
-			}
-		}
-		return fmt.Errorf("there was a problem with the editor %q", strings.Join(e.Args, " "))
-	}
-	return nil
 }
 
 // LaunchTempFile reads the provided stream into a temporary file in the given directory
