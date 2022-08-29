8690ff6264cceb38bd81dec99bb8affcc40286a9
jackzhang <x_jackzhang@qq.com>
2022-08-29 09:58:13 +0800

    update lock getter of leaderelection
    The lock acquired by tryAcquireOrRenew is released when the leader ends
    leadership. However, due to the cancellation of the context, the lock may
    be set as an empty lock, so the Update cannot be run normally, resulting
    in a failure to release the lock.
    
    Signed-off-by: jackzhang <x_jackzhang@qq.com>

--- staging/src/k8s.io/client-go/tools/leaderelection/resourcelock/configmaplock.go
+++ staging/src/k8s.io/client-go/tools/leaderelection/resourcelock/configmaplock.go
@@ -44,11 +44,11 @@ type configMapLock struct {
 // Get returns the election record from a ConfigMap Annotation
 func (cml *configMapLock) Get(ctx context.Context) (*LeaderElectionRecord, []byte, error) {
 	var record LeaderElectionRecord
-	var err error
-	cml.cm, err = cml.Client.ConfigMaps(cml.ConfigMapMeta.Namespace).Get(ctx, cml.ConfigMapMeta.Name, metav1.GetOptions{})
+	cm, err := cml.Client.ConfigMaps(cml.ConfigMapMeta.Namespace).Get(ctx, cml.ConfigMapMeta.Name, metav1.GetOptions{})
 	if err != nil {
 		return nil, nil, err
 	}
+	cml.cm = cm
 	if cml.cm.Annotations == nil {
 		cml.cm.Annotations = make(map[string]string)
 	}
