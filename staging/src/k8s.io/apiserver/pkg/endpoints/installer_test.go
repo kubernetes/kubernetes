583f93b61df306863601807124aeb5bbded2d226
Jeffrey Ying <jeffrey.ying86@live.com>
2022-08-31 15:56:13 -0400

    Add tests for converting resourceinfo (#4)

--- staging/src/k8s.io/apiserver/pkg/endpoints/installer_test.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/installer_test.go
@@ -17,7 +17,10 @@ limitations under the License.
 package endpoints
 
 import (
+	"reflect"
 	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestIsVowel(t *testing.T) {
@@ -94,6 +97,153 @@ func TestGetArticleForNoun(t *testing.T) {
 	for _, tt := range tests {
 		if got := GetArticleForNoun(tt.noun, tt.padding); got != tt.want {
 			t.Errorf("%q. GetArticleForNoun() = %v, want %v", tt.noun, got, tt.want)
+		}
+	}
+}
+
+func TestConvertResourceInfoToDiscovery(t *testing.T) {
+	tests := []struct {
+		name                     string
+		resources                []*apiResourceInfo
+		wantAPIResource          []metav1.APIResource
+		wantAPIResourceDiscovery []metav1.APIResourceDiscovery
+	}{
+		{
+			name: "Basic Test",
+			resources: []*apiResourceInfo{
+				{
+					APIResource: metav1.APIResource{
+						Name:       "pods",
+						Namespaced: true,
+						Kind:       "Pod",
+						ShortNames: []string{"po"},
+						Verbs:      []string{"create", "delete", "deletecollection", "get", "list", "patch", "update", "watch"},
+					},
+				},
+			},
+			wantAPIResourceDiscovery: []metav1.APIResourceDiscovery{
+				{
+					Resource: "pods",
+					Scope:    metav1.ScopeNamespace,
+					ReturnType: metav1.APIDiscoveryKind{
+						Kind: "Pod",
+					},
+					ShortNames: []string{"po"},
+					Verbs:      []string{"create", "delete", "deletecollection", "get", "list", "patch", "update", "watch"},
+				},
+			},
+		},
+		{
+			name: "Basic Group Version Test",
+			resources: []*apiResourceInfo{
+				{
+					APIResource: metav1.APIResource{
+						Name:       "cronjobs",
+						Namespaced: true,
+						Group: "batch",
+						Version: "v1",
+						Kind:       "CronJob",
+						ShortNames: []string{"cj"},
+						Verbs:      []string{"create", "delete", "deletecollection", "get", "list", "patch", "update", "watch"},
+					},
+				},
+			},
+			wantAPIResourceDiscovery: []metav1.APIResourceDiscovery{
+				{
+					Resource: "cronjobs",
+					Scope:    metav1.ScopeNamespace,
+					ReturnType: metav1.APIDiscoveryKind{
+						Group:   "batch",
+						Version: "v1",
+						Kind:    "CronJob",
+					},
+					ShortNames: []string{"cj"},
+					Verbs:      []string{"create", "delete", "deletecollection", "get", "list", "patch", "update", "watch"},
+				},
+			},
+		},
+		{
+			name: "Test with subresource",
+			resources: []*apiResourceInfo{
+				{
+					APIResource: metav1.APIResource{
+						Name:       "cronjobs",
+						Namespaced: true,
+						Kind:       "CronJob",
+						Group:      "batch",
+						Version:    "v1",
+						ShortNames: []string{"cj"},
+						Verbs:      []string{"create", "delete", "deletecollection", "get", "list", "patch", "update", "watch"},
+					},
+				},
+				{
+					Subresource: "status",
+					APIResource: metav1.APIResource{
+						Name:       "cronjobs",
+						Namespaced: true,
+						Kind:       "CronJob",
+						Group:      "batch",
+						Version:    "v1",
+						ShortNames: []string{"cj"},
+						Verbs:      []string{"create", "delete", "deletecollection", "get", "list", "patch", "update", "watch"},
+					},
+				},
+			},
+			wantAPIResourceDiscovery: []metav1.APIResourceDiscovery{
+				{
+					Resource: "cronjobs",
+					Scope:    metav1.ScopeNamespace,
+					ReturnType: metav1.APIDiscoveryKind{
+						Group:   "batch",
+						Version: "v1",
+						Kind:    "CronJob",
+					},
+					ShortNames: []string{"cj"},
+					Verbs:      []string{"create", "delete", "deletecollection", "get", "list", "patch", "update", "watch"},
+					Subresources: []metav1.APISubresourceDiscovery{{
+						Subresource: "status",
+						ReturnType: &metav1.APIDiscoveryKind{
+							Group:   "batch",
+							Version: "v1",
+							Kind:    "CronJob",
+						},
+						Verbs: []string{"create", "delete", "deletecollection", "get", "list", "patch", "update", "watch"},
+					}},
+				},
+			},
+		},
+		{
+			name: "Cluster Scope Test",
+			resources: []*apiResourceInfo{
+				{
+					APIResource: metav1.APIResource{
+						Name:       "nodes",
+						Namespaced: false,
+						Kind:       "Node",
+						ShortNames: []string{"no"},
+						Verbs:      []string{"create", "delete", "deletecollection", "get", "list", "patch", "update", "watch"},
+					},
+				},
+			},
+			wantAPIResourceDiscovery: []metav1.APIResourceDiscovery{
+				{
+					Resource: "nodes",
+					Scope:    metav1.ScopeCluster,
+					ReturnType: metav1.APIDiscoveryKind{
+						Kind: "Node",
+					},
+					ShortNames: []string{"no"},
+					Verbs:      []string{"create", "delete", "deletecollection", "get", "list", "patch", "update", "watch"},
+				},
+			},
+		},
+
+	}
+
+	for _, tt := range tests {
+		_, discoveryAPIResources := ConvertResourceInfoToDiscovery(tt.resources)
+		if !reflect.DeepEqual(discoveryAPIResources, tt.wantAPIResourceDiscovery) {
+			t.Errorf("Error: Test %s, Expected %v, got %v\n", tt.name, tt.wantAPIResourceDiscovery, discoveryAPIResources)
 		}
 	}
 }
