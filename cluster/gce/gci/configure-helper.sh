8a0e0c7f79d6d5c9b4acb0accbcb5bbee5cc9ef4
Matthew Cary <mattcary@google.com>
2022-09-01 15:58:43 -0700

    Remove race between kubelet startup and mounter rootfs
    Change-Id: I18d5a7d1f8c475c96da3c4166802ac76e91b3b52

--- cluster/gce/gci/configure-helper.sh
+++ cluster/gce/gci/configure-helper.sh
@@ -3442,6 +3442,10 @@ function main() {
   fi
   log-end 'SetupKubePodLogReadersGroupDir'
 
+  # Note prepare-mounter-rootfs must be called before the kubelet starts, as
+  # kubelet startup updates its nameserver.
+  log-wrap 'PrepareMounterRootfs' prepare-mounter-rootfs
+
   log-wrap 'StartKubelet' start-kubelet
 
   if [[ "${KUBERNETES_MASTER:-}" == "true" ]]; then
@@ -3470,7 +3474,6 @@ function main() {
     fi
   fi
   log-wrap 'ResetMotd' reset-motd
-  log-wrap 'PrepareMounterRootfs' prepare-mounter-rootfs
 
   # Wait for all background jobs to finish.
   wait
