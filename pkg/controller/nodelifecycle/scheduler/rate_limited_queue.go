a5c7c0263ab3bff26172e63fbe462e765921481d
liukai <Lk15516195603@gmail.com>
2022-09-05 14:25:38 +0800

    should omit comparison to bool constant, can be simplified to !q.limiter.TryAccept() (S1002)go-staticcheck

--- pkg/controller/nodelifecycle/scheduler/rate_limited_queue.go
+++ pkg/controller/nodelifecycle/scheduler/rate_limited_queue.go
@@ -300,7 +300,7 @@ func (q *RateLimitedTimedQueue) SwapLimiter(newQPS float32) {
 		// - number of used tokens
 		// - number of available tokens
 		// - something else
-		if q.limiter.TryAccept() == false {
+		if !q.limiter.TryAccept() {
 			newLimiter.TryAccept()
 		}
 	}
