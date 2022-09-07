5d227ad1be25907ec91f890f84f56af52b5debcf
Nabarun Pal <pal.nabarun95@gmail.com>
2022-09-07 22:13:13 +0530

    update the default go-runner version to v2.3.1-go1.19.1-bullseye.0
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

--- build/common.sh
+++ build/common.sh
@@ -91,7 +91,7 @@ readonly KUBE_CONTAINER_RSYNC_PORT=8730
 
 # These are the default versions (image tags) for their respective base images.
 readonly __default_distroless_iptables_version=v0.1.1
-readonly __default_go_runner_version=v2.3.1-go1.19-bullseye.0
+readonly __default_go_runner_version=v2.3.1-go1.19.1-bullseye.0
 readonly __default_setcap_version=bullseye-v1.3.0
 
 # These are the base images for the Docker-wrapped binaries.
