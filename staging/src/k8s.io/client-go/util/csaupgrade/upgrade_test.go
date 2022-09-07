dbff8e117c8d4aeccac6ea211dfa334e30621727
Alexander Zielenski <351783+alexzielenski@users.noreply.github.com>
2022-09-06 20:58:50 -0700

    correct spacing

--- staging/src/k8s.io/client-go/util/csaupgrade/upgrade_test.go
+++ staging/src/k8s.io/client-go/util/csaupgrade/upgrade_test.go
@@ -1,11 +1,11 @@
 /*
-Copyright 2018 The Kubernetes Authors.
+Copyright 2022 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
