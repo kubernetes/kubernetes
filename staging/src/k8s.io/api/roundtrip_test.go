a2b6966cacd65d6224c7ed4a25b5c15a72ef744a
Abu Kashem <akashem@redhat.com>
2022-09-16 08:59:52 -0400

    add testdata for flowcontrol v1beta3

--- staging/src/k8s.io/api/roundtrip_test.go
+++ staging/src/k8s.io/api/roundtrip_test.go
@@ -51,6 +51,7 @@ import (
 	flowcontrolv1alpha1 "k8s.io/api/flowcontrol/v1alpha1"
 	flowcontrolv1beta1 "k8s.io/api/flowcontrol/v1beta1"
 	flowcontrolv1beta2 "k8s.io/api/flowcontrol/v1beta2"
+	flowcontrolv1beta3 "k8s.io/api/flowcontrol/v1beta3"
 	imagepolicyv1alpha1 "k8s.io/api/imagepolicy/v1alpha1"
 	networkingv1 "k8s.io/api/networking/v1"
 	networkingv1alpha1 "k8s.io/api/networking/v1alpha1"
@@ -110,6 +111,7 @@ var groups = []runtime.SchemeBuilder{
 	flowcontrolv1alpha1.SchemeBuilder,
 	flowcontrolv1beta1.SchemeBuilder,
 	flowcontrolv1beta2.SchemeBuilder,
+	flowcontrolv1beta3.SchemeBuilder,
 	imagepolicyv1alpha1.SchemeBuilder,
 	networkingv1.SchemeBuilder,
 	networkingv1beta1.SchemeBuilder,
