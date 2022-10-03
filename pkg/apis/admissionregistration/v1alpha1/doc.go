97bdb0c634ef6e0066a38e200b41eb73b415d37e
Cici Huang <cicih@google.com>
2022-10-03 08:58:41 +0000

    Rename copy to v1alpha1

--- pkg/apis/admissionregistration/v1alpha1/doc.go
+++ pkg/apis/admissionregistration/v1alpha1/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Kubernetes Authors.
+Copyright 2022 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,13 +15,13 @@ limitations under the License.
 */
 
 // +k8s:conversion-gen=k8s.io/kubernetes/pkg/apis/admissionregistration
-// +k8s:conversion-gen-external-types=k8s.io/api/admissionregistration/v1
+// +k8s:conversion-gen-external-types=k8s.io/api/admissionregistration/v1alpha1
 // +k8s:defaulter-gen=TypeMeta
-// +k8s:defaulter-gen-input=k8s.io/api/admissionregistration/v1
+// +k8s:defaulter-gen-input=k8s.io/api/admissionregistration/v1alpha1
 // +groupName=admissionregistration.k8s.io
 
-// Package v1 is the v1 version of the API.
+// Package v1alpha1 is the v1alpha1 version of the API.
 // AdmissionConfiguration and AdmissionPluginConfiguration are legacy static admission plugin configuration
 // ValidatingWebhookConfiguration, and MutatingWebhookConfiguration are for the
 // new dynamic admission controller configuration.
-package v1 // import "k8s.io/kubernetes/pkg/apis/admissionregistration/v1"
+package v1alpha1 // import "k8s.io/kubernetes/pkg/apis/admissionregistration/v1alpha1"
