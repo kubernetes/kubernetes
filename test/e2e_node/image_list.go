f0c28674fd6728276cc8d75f2d20b1b342d2e329
Brian McQueen <bmcqueen@linkedin.com>
2022-10-29 14:46:11 -0700

    fixing mistakes from the merge for the backport registry.k8s.io

--- test/e2e_node/image_list.go
+++ test/e2e_node/image_list.go
@@ -55,7 +55,7 @@ const (
 // before test running so that the image pulling won't fail in actual test.
 var NodePrePullImageList = sets.NewString(
 	imageutils.GetE2EImage(imageutils.Agnhost),
-	"gcr.io/cadvisor/cadvisor:v0.43.0",
+	"google/cadvisor:latest",
 	"registry.k8s.io/stress:v1",
 	busyboxImage,
 	"registry.k8s.io/busybox@sha256:4bdd623e848417d96127e16037743f0cd8b528c026e9175e22a84f639eca58ff",
