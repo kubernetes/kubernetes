101bfb552219618a83d5c44ff5f3adf9a2f1f07a
Shiming Zhang <wzshiming@foxmail.com>
2022-11-03 17:57:15 +0800

    Fix grpc probe log

--- pkg/kubelet/prober/prober.go
+++ pkg/kubelet/prober/prober.go
@@ -170,10 +170,13 @@ func (pb *prober) runProbe(probeType probeType, p *v1.Probe, pod *v1.Pod, status
 	}
 
 	if utilfeature.DefaultFeatureGate.Enabled(kubefeatures.GRPCContainerProbe) && p.GRPC != nil {
-		host := &(status.PodIP)
-		service := p.GRPC.Service
+		host := status.PodIP
+		service := ""
+		if p.GRPC.Service != nil {
+			service = *p.GRPC.Service
+		}
 		klog.V(4).InfoS("GRPC-Probe", "host", host, "service", service, "port", p.GRPC.Port, "timeout", timeout)
-		return pb.grpc.Probe(*host, *service, int(p.GRPC.Port), timeout)
+		return pb.grpc.Probe(host, service, int(p.GRPC.Port), timeout)
 	}
 
 	klog.InfoS("Failed to find probe builder for container", "containerName", container.Name)
