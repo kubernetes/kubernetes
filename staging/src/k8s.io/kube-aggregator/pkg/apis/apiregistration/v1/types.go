8b6ce828aa0fe2603b3d54e733988504ef7fddbe
Shivam Tyagi <mergesuccessful@gmail.com>
2022-11-04 01:37:18 +0530

    Add JSON name to API Fields
    Signed-off-by: Shivam Tyagi <mergesuccessful@gmail.com>

--- staging/src/k8s.io/kube-aggregator/pkg/apis/apiregistration/v1/types.go
+++ staging/src/k8s.io/kube-aggregator/pkg/apis/apiregistration/v1/types.go
@@ -28,15 +28,15 @@ type APIServiceList struct {
 	// +optional
 	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
 
-	// Items is the list of APIService
+	// items is the list of APIService
 	Items []APIService `json:"items" protobuf:"bytes,2,rep,name=items"`
 }
 
 // ServiceReference holds a reference to Service.legacy.k8s.io
 type ServiceReference struct {
-	// Namespace is the namespace of the service
+	// namespace is the namespace of the service
 	Namespace string `json:"namespace,omitempty" protobuf:"bytes,1,opt,name=namespace"`
-	// Name is the name of the service
+	// name is the name of the service
 	Name string `json:"name,omitempty" protobuf:"bytes,2,opt,name=name"`
 	// If specified, the port on the service that hosting webhook.
 	// Default to 443 for backward compatibility.
@@ -48,36 +48,36 @@ type ServiceReference struct {
 // APIServiceSpec contains information for locating and communicating with a server.
 // Only https is supported, though you are able to disable certificate verification.
 type APIServiceSpec struct {
-	// Service is a reference to the service for this API server.  It must communicate
+	// service is a reference to the service for this API server.  It must communicate
 	// on port 443.
-	// If the Service is nil, that means the handling for the API groupversion is handled locally on this server.
+	// If the service is nil, that means the handling for the API groupversion is handled locally on this server.
 	// The call will simply delegate to the normal handler chain to be fulfilled.
 	// +optional
 	Service *ServiceReference `json:"service,omitempty" protobuf:"bytes,1,opt,name=service"`
-	// Group is the API group name this server hosts
+	// group is the API group name this server hosts
 	Group string `json:"group,omitempty" protobuf:"bytes,2,opt,name=group"`
-	// Version is the API version this server hosts.  For example, "v1"
+	// version is the API version this server hosts.  For example, "v1"
 	Version string `json:"version,omitempty" protobuf:"bytes,3,opt,name=version"`
 
-	// InsecureSkipTLSVerify disables TLS certificate verification when communicating with this server.
+	// insecureSkipTLSVerify disables TLS certificate verification when communicating with this server.
 	// This is strongly discouraged.  You should use the CABundle instead.
 	InsecureSkipTLSVerify bool `json:"insecureSkipTLSVerify,omitempty" protobuf:"varint,4,opt,name=insecureSkipTLSVerify"`
-	// CABundle is a PEM encoded CA bundle which will be used to validate an API server's serving certificate.
+	// caBundle is a PEM encoded CA bundle which will be used to validate an API server's serving certificate.
 	// If unspecified, system trust roots on the apiserver are used.
 	// +listType=atomic
 	// +optional
 	CABundle []byte `json:"caBundle,omitempty" protobuf:"bytes,5,opt,name=caBundle"`
 
-	// GroupPriorityMininum is the priority this group should have at least. Higher priority means that the group is preferred by clients over lower priority ones.
-	// Note that other versions of this group might specify even higher GroupPriorityMininum values such that the whole group gets a higher priority.
+	// groupPriorityMininum is the priority this group should have at least. Higher priority means that the group is preferred by clients over lower priority ones.
+	// Note that other versions of this group might specify even higher groupPriorityMininum values such that the whole group gets a higher priority.
 	// The primary sort is based on GroupPriorityMinimum, ordered highest number to lowest (20 before 10).
 	// The secondary sort is based on the alphabetical comparison of the name of the object.  (v1.bar before v1.foo)
 	// We'd recommend something like: *.k8s.io (except extensions) at 18000 and
 	// PaaSes (OpenShift, Deis) are recommended to be in the 2000s
 	GroupPriorityMinimum int32 `json:"groupPriorityMinimum" protobuf:"varint,7,opt,name=groupPriorityMinimum"`
 
-	// VersionPriority controls the ordering of this API version inside of its group.  Must be greater than zero.
-	// The primary sort is based on VersionPriority, ordered highest to lowest (20 before 10).
+	// versionPriority controls the ordering of this API version inside of its group.  Must be greater than zero.
+	// The primary sort is based on versionPriority, ordered highest to lowest (20 before 10).
 	// Since it's inside of a group, the number can be small, probably in the 10s.
 	// In case of equal version priorities, the version string will be used to compute the order inside a group.
 	// If the version string is "kube-like", it will sort above non "kube-like" version strings, which are ordered
@@ -115,12 +115,12 @@ const (
 
 // APIServiceCondition describes the state of an APIService at a particular point
 type APIServiceCondition struct {
-	// Type is the type of the condition.
+	// type is the type of the condition.
 	Type APIServiceConditionType `json:"type" protobuf:"bytes,1,opt,name=type,casttype=APIServiceConditionType"`
-	// Status is the status of the condition.
+	// status is the status of the condition.
 	// Can be True, False, Unknown.
 	Status ConditionStatus `json:"status" protobuf:"bytes,2,opt,name=status,casttype=ConditionStatus"`
-	// Last time the condition transitioned from one status to another.
+	// last time the condition transitioned from one status to another.
 	// +optional
 	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty" protobuf:"bytes,3,opt,name=lastTransitionTime"`
 	// Unique, one-word, CamelCase reason for the condition's last transition.
@@ -155,8 +155,8 @@ type APIService struct {
 	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
 
-	// Spec contains information for locating and communicating with a server
+	// spec contains information for locating and communicating with a server
 	Spec APIServiceSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
-	// Status contains derived information about an API server
+	// status contains derived information about an API server
 	Status APIServiceStatus `json:"status,omitempty" protobuf:"bytes,3,opt,name=status"`
 }
