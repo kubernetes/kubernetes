2818009b3e8e44e965d8efe50fb5a385e5fba476
Cici Huang <cicih@google.com>
2022-11-06 07:06:19 +0000

    Update tests

--- plugin/pkg/admission/resourcequota/admission_test.go
+++ plugin/pkg/admission/resourcequota/admission_test.go
@@ -107,7 +107,7 @@ func createHandlerWithConfig(kubeClient kubernetes.Interface, informerFactory in
 	}
 
 	initializers := admission.PluginInitializers{
-		genericadmissioninitializer.New(kubeClient, informerFactory, nil, nil, stopCh, nil, nil),
+		genericadmissioninitializer.New(kubeClient, nil, informerFactory, nil, nil, stopCh, nil),
 		kubeapiserveradmission.NewPluginInitializer(nil, nil, quotaConfiguration),
 	}
 	initializers.Initialize(handler)
