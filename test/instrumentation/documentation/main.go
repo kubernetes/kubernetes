b61ed2d8757b353e6e3932be1f657f996509fd78
Tim Bannister <tim@scalefactory.com>
2022-11-10 11:37:23 +0000

    Fix autogenerated mark for instrumentation doc
    The https://k8s.io/docs/ website uses this marker to highlight that
    pages should not be directly edited in k/website

--- test/instrumentation/documentation/main.go
+++ test/instrumentation/documentation/main.go
@@ -44,7 +44,7 @@ const (
 	templ = `---
 title: Kubernetes Metrics Reference
 content_type: reference
-auto-generated: true
+auto_generated: true
 description: >-
   Details of the metric data that Kubernetes components export.
 ---
