8a9a294c9fc69b6f1cf1b8c5ebcd1a512af24559
Mengjiao Liu <mengjiao.liu@daocloud.io>
2022-11-16 14:15:27 +0800

    Remove ExpandInUsePersistentVolumes feature gate

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -315,12 +315,6 @@ const (
 	// Lock to default and remove after v1.22 based on user feedback that should be reflected in KEP #1972 update
 	ExecProbeTimeout featuregate.Feature = "ExecProbeTimeout"
 
-	// owner: @mlmhl @gnufied
-	// beta: v1.15
-	// GA: 1.24
-	// Ability to expand persistent volumes' file system without unmounting volumes.
-	ExpandInUsePersistentVolumes featuregate.Feature = "ExpandInUsePersistentVolumes"
-
 	// owner: @gnufied
 	// beta: v1.11
 	// GA: 1.24
@@ -987,8 +981,6 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 	EventedPLEG: {Default: false, PreRelease: featuregate.Alpha},
 
 	ExecProbeTimeout: {Default: true, PreRelease: featuregate.GA}, // lock to default and remove after v1.22 based on KEP #1972 update
-
-	ExpandInUsePersistentVolumes: {Default: true, PreRelease: featuregate.GA}, // remove in 1.26
 
 	ExpandPersistentVolumes: {Default: true, PreRelease: featuregate.GA}, // remove in 1.26
 
