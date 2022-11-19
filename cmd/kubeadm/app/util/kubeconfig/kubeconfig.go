21cd660a1f570dce08bc314241d1acd68fe8e74c
SataQiu <shidaqiu2018@gmail.com>
2022-11-19 16:32:37 +0800

    kubeadm: respect user provided kubeconfig during discovery process

--- cmd/kubeadm/app/util/kubeconfig/kubeconfig.go
+++ cmd/kubeadm/app/util/kubeconfig/kubeconfig.go
@@ -104,15 +104,17 @@ func WriteToDisk(filename string, kubeconfig *clientcmdapi.Config) error {
 }
 
 // GetClusterFromKubeConfig returns the default Cluster of the specified KubeConfig
-func GetClusterFromKubeConfig(config *clientcmdapi.Config) *clientcmdapi.Cluster {
+func GetClusterFromKubeConfig(config *clientcmdapi.Config) (string, *clientcmdapi.Cluster) {
 	// If there is an unnamed cluster object, use it
 	if config.Clusters[""] != nil {
-		return config.Clusters[""]
+		return "", config.Clusters[""]
 	}
-	if config.Contexts[config.CurrentContext] != nil {
-		return config.Clusters[config.Contexts[config.CurrentContext].Cluster]
+
+	currentContext := config.Contexts[config.CurrentContext]
+	if currentContext != nil {
+		return currentContext.Cluster, config.Clusters[currentContext.Cluster]
 	}
-	return nil
+	return "", nil
 }
 
 // HasAuthenticationCredentials returns true if the current user has valid authentication credentials for
