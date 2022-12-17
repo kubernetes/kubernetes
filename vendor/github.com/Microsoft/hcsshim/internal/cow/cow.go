1cfbc8768b203c8d9554573749ef7c384572738a
Paco Xu <paco.xu@daocloud.io>
2022-12-17 22:08:54 +0800

    github.com/Microsoft/hcsshim to v0.8.25

--- vendor/github.com/Microsoft/hcsshim/internal/cow/cow.go
+++ vendor/github.com/Microsoft/hcsshim/internal/cow/cow.go
@@ -86,6 +86,12 @@ type Container interface {
 	// container to be terminated by some error condition (including calling
 	// Close).
 	Wait() error
+	// WaitChannel returns the wait channel of the container
+	WaitChannel() <-chan struct{}
+	// WaitError returns the container termination error.
+	// This function should only be called after the channel in WaitChannel()
+	// is closed. Otherwise it is not thread safe.
+	WaitError() error
 	// Modify sends a request to modify container resources
 	Modify(ctx context.Context, config interface{}) error
 }
