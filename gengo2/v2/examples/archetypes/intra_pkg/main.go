18163fe3e2ffd531a6ddc700c42f085331b8d06b
Tim Hockin <thockin@google.com>
2023-01-01 12:24:35 -0800

    Archetype: gengo owns flag parsing
    All the tools seem to do their own flag parsing, but for no obvious
    reason.  Change the model so that gengo handles flag parsing.

--- gengo2/v2/examples/archetypes/intra_pkg/main.go
+++ gengo2/v2/examples/archetypes/intra_pkg/main.go
@@ -18,5 +18,72 @@ limitations under the License.
 // code into the same package(s) it used as input(s).
 package main
 
+import (
+	"fmt"
+
+	"github.com/spf13/pflag"
+	"k8s.io/gengo/v2/args"
+	"k8s.io/gengo/v2/generator"
+	"k8s.io/gengo/v2/namer"
+	"k8s.io/klog/v2"
+)
+
 func main() {
+	klog.InitFlags(nil)
+	gengo := getArgs()
+
+	// FIXME: these functions could be named better
+	if err := gengo.Execute(getPackageNamers(), getDefaultNamer(), getPackages); err != nil {
+		klog.Fatalf("ERROR: %v", err)
+	}
+	klog.V(2).Info("completed successfully")
+}
+
+// getArgs returns the arguments for this tool.
+func getArgs() *args.GeneratorArgs {
+	//FIXME: rename to something more friendly, like gengo.Args()
+	stdArgs := args.Default()
+	stdArgs.CustomArgs = &customArgs{}
+	stdArgs.Validate = validateArgs
+	// Set any other non-standard defaults here.
+	return stdArgs
+}
+
+// customArgs captures any non-standard arguments this tool needs.
+type customArgs struct {
+	StringArg string
+}
+
+// AddFlagsTo adds the generator flags to the flag set.
+func (ca customArgs) AddFlagsTo(fs *pflag.FlagSet) {
+	pflag.CommandLine.StringVar(&ca.StringArg, "string-arg", "default",
+		"An example string argument")
+}
+
+// validateArgs checks the given arguments.
+func validateArgs(stdArgs *args.GeneratorArgs) error {
+	_ = stdArgs.CustomArgs.(*customArgs)
+
+	// FIXME: should this be standard?
+	if len(stdArgs.OutputFileBaseName) == 0 {
+		return fmt.Errorf("output file base name cannot be empty")
+	}
+
+	return nil
+}
+
+// getPackageNamers returns the name systems used by the generators in this package.
+func getPackageNamers() namer.NameSystems {
+	return nil
+}
+
+// getDefaultNamer returns the default name system for ordering the types to be
+// processed by the generators in this package.
+func getDefaultNamer() string {
+	return ""
+}
+
+// getPackages returns a set of packages to be processed by this tool.
+func getPackages(context *generator.Context, arguments *args.GeneratorArgs) generator.Packages {
+	return nil
 }
