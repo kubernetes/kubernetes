f79a34d26773a877b2364139897dfa620e20bd27
Michal Wozniak <mw219725@gmail.com>
2023-01-09 17:20:36 +0100

    Do not leak cross namespace pod metadata in preemption events

--- pkg/scheduler/framework/preemption/preemption.go
+++ pkg/scheduler/framework/preemption/preemption.go
@@ -378,8 +378,8 @@ func (ev *Evaluator) prepareCandidate(ctx context.Context, c Candidate, pod *v1.
 				return
 			}
 		}
-		fh.EventRecorder().Eventf(victim, pod, v1.EventTypeNormal, "Preempted", "Preempting", "Preempted by %v/%v on node %v",
-			pod.Namespace, pod.Name, c.Name())
+		fh.EventRecorder().Eventf(victim, pod, v1.EventTypeNormal, "Preempted", "Preempting", "Preempted by a pod scheduled by %s on node %v",
+			pod.Spec.SchedulerName, c.Name())
 	}
 
 	fh.Parallelizer().Until(ctx, len(c.Victims().Pods), preemptPod, ev.PluginName)
