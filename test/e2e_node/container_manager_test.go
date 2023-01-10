5289928c2467816000389e68b5e64802d06daa59
Aditi Sharma <adi.sky17@gmail.com>
2023-01-10 16:13:02 +0530

    Fix test validate oomscore for guarnteed pods

--- test/e2e_node/container_manager_test.go
+++ test/e2e_node/container_manager_test.go
@@ -217,7 +217,7 @@ var _ = SIGDescribe("Container Manager Misc [Serial]", func() {
 					}
 
 					return nil
-				}, 30*time.Second, time.Second*4).Should(gomega.BeNil())
+				}, 180*time.Second, time.Second*4).Should(gomega.BeNil())
 
 			})
 			ginkgo.It("burstable container's oom-score-adj should be between [2, 1000)", func(ctx context.Context) {
