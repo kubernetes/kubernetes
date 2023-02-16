0409ba7ff1f61d912b0175fc38c7cd3071150d73
Stanislav Láznička <stlaz.devel@proton.me>
2023-02-16 13:47:35 +0100

    cluster trust CM: wire in the new RequestHeader UID config

--- pkg/controlplane/apiserver/config.go
+++ pkg/controlplane/apiserver/config.go
@@ -337,6 +337,7 @@ func CreateConfig(
 		config.ClusterAuthenticationInfo.RequestHeaderExtraHeaderPrefixes = requestHeaderConfig.ExtraHeaderPrefixes
 		config.ClusterAuthenticationInfo.RequestHeaderGroupHeaders = requestHeaderConfig.GroupHeaders
 		config.ClusterAuthenticationInfo.RequestHeaderUsernameHeaders = requestHeaderConfig.UsernameHeaders
+		config.ClusterAuthenticationInfo.RequestHeaderUIDHeaders = requestHeaderConfig.UIDHeaders
 	}
 
 	// setup admission
