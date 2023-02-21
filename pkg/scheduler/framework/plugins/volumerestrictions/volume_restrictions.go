edc0455c5c257d90babe4a32eefbb741e3d5013a
mantuliu <240951888@qq.com>
2023-02-22 00:51:55 +0800

    Improve performance of method haveOverlap
    Signed-off-by: mantuliu <240951888@qq.com>

--- pkg/scheduler/framework/plugins/volumerestrictions/volume_restrictions.go
+++ pkg/scheduler/framework/plugins/volumerestrictions/volume_restrictions.go
@@ -144,7 +144,7 @@ func haveOverlap(a1, a2 []string) bool {
 	if len(a1) > len(a2) {
 		a1, a2 = a2, a1
 	}
-	m := sets.New[string](a1...)
+	m := sets.New(a1...)
 	for _, val := range a2 {
 		if _, ok := m[val]; ok {
 			return true
