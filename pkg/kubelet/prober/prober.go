45b5b3ebbf8c6bb76847cf698af6f9664924a067
Vishal <vishalpraneeth7771@gmail.com>
2023-02-24 09:34:43 +0530

    The event message will include the probe type and the unknown status

--- pkg/kubelet/prober/prober.go
+++ pkg/kubelet/prober/prober.go
@@ -115,9 +115,20 @@ func (pb *prober) probe(ctx context.Context, probeType probeType, pod *v1.Pod, s
 		pb.recordContainerEvent(pod, &container, v1.EventTypeWarning, events.ContainerProbeWarning, "%s probe warning: %s", probeType, output)
 		klog.V(3).InfoS("Probe succeeded with a warning", "probeType", probeType, "pod", klog.KObj(pod), "podUID", pod.UID, "containerName", container.Name, "output", output)
 	} else {
+		if result != probe.Success && result != probe.Failure {
+			// log the unknown status
+			klog.Warningf("Unknown probe result: %s", result)
+			// record an event to expose the unknown status
+			pb.recordContainerEvent(pod, &container, v1.EventTypeWarning, events.ContainerProbeWarning, "Unknown %s probe status: %s", probeType, result)
+		} else if result == probe.Failure {
+			pb.recordContainerEvent(pod, &container, v1.EventTypeWarning, events.ContainerProbeFailed, "%s probe failed: %s", probeType, output)
+		} else {
+			pb.recordContainerEvent(pod, &container, v1.EventTypeNormal, events.ContainerProbeSucceeded, "%s probe succeeded", probeType)
+		}
 		klog.V(3).InfoS("Probe succeeded", "probeType", probeType, "pod", klog.KObj(pod), "podUID", pod.UID, "containerName", container.Name)
 	}
-	return results.Success, nil
+	return result, nil
+	
 }
 
 // runProbeWithRetries tries to probe the container in a finite loop, it returns the last result
