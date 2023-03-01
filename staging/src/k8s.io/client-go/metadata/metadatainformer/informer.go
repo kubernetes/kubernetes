b99fe0d5b9896dd3fe9a2c1bc3b399a18ad080d2
John Howard <howardjohn@google.com>
2023-02-28 23:09:17 -0800

    client-go: support `Shutdown()` for metadata and dynamic informers (#114434)
    * client-go: support `Shutdown()` for metadata and dynamic informers
    
    Followup to https://github.com/kubernetes/kubernetes/pull/112200,
    specifically
    https://github.com/kubernetes/kubernetes/pull/112200#issuecomment-1344812038.
    
    * add comments
    
    * Defer lock

--- staging/src/k8s.io/client-go/metadata/metadatainformer/informer.go
+++ staging/src/k8s.io/client-go/metadata/metadatainformer/informer.go
@@ -60,6 +60,11 @@ type metadataSharedInformerFactory struct {
 	// This allows Start() to be called multiple times safely.
 	startedInformers map[schema.GroupVersionResource]bool
 	tweakListOptions TweakListOptionsFunc
+	// wg tracks how many goroutines were started.
+	wg sync.WaitGroup
+	// shuttingDown is true when Shutdown has been called. It may still be running
+	// because it needs to wait for goroutines.
+	shuttingDown bool
 }
 
 var _ SharedInformerFactory = &metadataSharedInformerFactory{}
@@ -85,9 +90,21 @@ func (f *metadataSharedInformerFactory) Start(stopCh <-chan struct{}) {
 	f.lock.Lock()
 	defer f.lock.Unlock()
 
+	if f.shuttingDown {
+		return
+	}
+
 	for informerType, informer := range f.informers {
 		if !f.startedInformers[informerType] {
-			go informer.Informer().Run(stopCh)
+			f.wg.Add(1)
+			// We need a new variable in each loop iteration,
+			// otherwise the goroutine would use the loop variable
+			// and that keeps changing.
+			informer := informer.Informer()
+			go func() {
+				defer f.wg.Done()
+				informer.Run(stopCh)
+			}()
 			f.startedInformers[informerType] = true
 		}
 	}
@@ -113,6 +130,15 @@ func (f *metadataSharedInformerFactory) WaitForCacheSync(stopCh <-chan struct{})
 		res[informType] = cache.WaitForCacheSync(stopCh, informer.HasSynced)
 	}
 	return res
+}
+
+func (f *metadataSharedInformerFactory) Shutdown() {
+	// Will return immediately if there is nothing to wait for.
+	defer f.wg.Wait()
+
+	f.lock.Lock()
+	defer f.lock.Unlock()
+	f.shuttingDown = true
 }
 
 // NewFilteredMetadataInformer constructs a new informer for a metadata type.
