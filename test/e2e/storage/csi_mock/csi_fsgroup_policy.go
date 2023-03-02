531324cb1df7cc945736c4e4ab579a14fc7adb15
Hao Ruan <righthandss@gmail.com>
2023-03-02 16:27:50 +0800

    allow FSGroupPolicy and PodInfoOnMount to be mutable in CSIDriver.Spec

--- test/e2e/storage/csi_mock/csi_fsgroup_policy.go
+++ test/e2e/storage/csi_mock/csi_fsgroup_policy.go
@@ -21,9 +21,9 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
-	"time"
 
 	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
 	storagev1 "k8s.io/api/storage/v1"
 	"k8s.io/kubernetes/test/e2e/framework"
 	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
@@ -74,47 +74,67 @@ var _ = utils.SIGDescribe("CSI Mock volume fsgroup policies", func() {
 				})
 				ginkgo.DeferCleanup(m.cleanup)
 
-				// kube-scheduler may need some time before it gets the CSIDriver object.
-				// Without them, scheduling doesn't run as expected by the test.
-				syncDelay := 5 * time.Second
-				time.Sleep(syncDelay)
+				waitUtilFSGroupInPod(ctx, m, test.modified)
 
-				fsGroupVal := int64(rand.Int63n(20000) + 1024)
-				fsGroup := &fsGroupVal
+				// The created resources will be removed by the cleanup() function,
+				// so need to delete anything here.
+			})
+		}
+	})
 
-				_, _, pod := m.createPodWithFSGroup(ctx, fsGroup) /* persistent volume */
-
-				mountPath := pod.Spec.Containers[0].VolumeMounts[0].MountPath
-				dirName := mountPath + "/" + f.UniqueName
-				fileName := dirName + "/" + f.UniqueName
-
-				err := e2epod.WaitForPodNameRunningInNamespace(ctx, m.cs, pod.Name, pod.Namespace)
-				framework.ExpectNoError(err, "failed to start pod")
-
-				// Create the subdirectory to ensure that fsGroup propagates
-				createDirectory := fmt.Sprintf("mkdir %s", dirName)
-				_, _, err = e2evolume.PodExec(f, pod, createDirectory)
-				framework.ExpectNoError(err, "failed: creating the directory: %s", err)
-
-				// Inject the contents onto the mount
-				createFile := fmt.Sprintf("echo '%s' > '%s'; sync", "filecontents", fileName)
-				_, _, err = e2evolume.PodExec(f, pod, createFile)
-				framework.ExpectNoError(err, "failed: writing the contents: %s", err)
-
-				// Delete the created file. This step is mandatory, as the mock driver
-				// won't clean up the contents automatically.
-				defer func() {
-					deleteDir := fmt.Sprintf("rm -fr %s", dirName)
-					_, _, err = e2evolume.PodExec(f, pod, deleteDir)
-					framework.ExpectNoError(err, "failed: deleting the directory: %s", err)
-				}()
-
-				// Ensure that the fsGroup matches what we expect
-				if test.modified {
-					utils.VerifyFSGroupInPod(f, fileName, strconv.FormatInt(*fsGroup, 10), pod)
-				} else {
-					utils.VerifyFSGroupInPod(f, fileName, "root", pod)
+	ginkgo.Context("CSI FSGroupPolicy Update [LinuxOnly]", func() {
+		tests := []struct {
+			name             string
+			oldFSGroupPolicy storagev1.FSGroupPolicy
+			newFSGroupPolicy storagev1.FSGroupPolicy
+		}{
+			{
+				name:             "should update fsGroup if update from None to File",
+				oldFSGroupPolicy: storagev1.NoneFSGroupPolicy,
+				newFSGroupPolicy: storagev1.FileFSGroupPolicy,
+			},
+			{
+				name:             "should update fsGroup if update from None to default",
+				oldFSGroupPolicy: storagev1.NoneFSGroupPolicy,
+				newFSGroupPolicy: storagev1.ReadWriteOnceWithFSTypeFSGroupPolicy,
+			},
+			{
+				name:             "should not update fsGroup if update from File to None",
+				oldFSGroupPolicy: storagev1.FileFSGroupPolicy,
+				newFSGroupPolicy: storagev1.NoneFSGroupPolicy,
+			},
+			{
+				name:             "should update fsGroup if update from File to default",
+				oldFSGroupPolicy: storagev1.FileFSGroupPolicy,
+				newFSGroupPolicy: storagev1.ReadWriteOnceWithFSTypeFSGroupPolicy,
+			},
+			{
+				name:             "should not update fsGroup if update from detault to None",
+				oldFSGroupPolicy: storagev1.ReadWriteOnceWithFSTypeFSGroupPolicy,
+				newFSGroupPolicy: storagev1.NoneFSGroupPolicy,
+			},
+			{
+				name:             "should update fsGroup if update from detault to File",
+				oldFSGroupPolicy: storagev1.ReadWriteOnceWithFSTypeFSGroupPolicy,
+				newFSGroupPolicy: storagev1.FileFSGroupPolicy,
+			},
+		}
+		for _, t := range tests {
+			test := t
+			ginkgo.It(test.name, func(ctx context.Context) {
+				if framework.NodeOSDistroIs("windows") {
+					e2eskipper.Skipf("FSGroupPolicy is only applied on linux nodes -- skipping")
 				}
+				m.init(ctx, testParameters{
+					disableAttach:  true,
+					registerDriver: true,
+					fsGroupPolicy:  &test.oldFSGroupPolicy,
+				})
+				ginkgo.DeferCleanup(m.cleanup)
+
+				waitUtilFSGroupInPod(ctx, m, test.oldFSGroupPolicy != storagev1.NoneFSGroupPolicy)
+				m.update(utils.PatchCSIOptions{FSGroupPolicy: &test.newFSGroupPolicy})
+				waitUtilFSGroupInPod(ctx, m, test.newFSGroupPolicy != storagev1.NoneFSGroupPolicy)
 
 				// The created resources will be removed by the cleanup() function,
 				// so need to delete anything here.
@@ -122,3 +142,56 @@ var _ = utils.SIGDescribe("CSI Mock volume fsgroup policies", func() {
 		}
 	})
 })
+
+func waitUtilFSGroupInPod(ctx context.Context, m *mockDriverSetup, modified bool) {
+	var err error
+
+	utils.WaitUntil(framework.Poll, framework.PodStartTimeout, func() bool {
+		err = gomega.InterceptGomegaFailure(func() {
+			fsGroupVal := int64(rand.Int63n(20000) + 1024)
+			fsGroup := &fsGroupVal
+
+			_, _, pod := m.createPodWithFSGroup(ctx, fsGroup) /* persistent volume */
+			defer func() {
+				err = e2epod.DeletePodWithWait(context.TODO(), m.f.ClientSet, pod)
+				framework.ExpectNoError(err, "failed: deleting the pod: %s", err)
+			}()
+
+			mountPath := pod.Spec.Containers[0].VolumeMounts[0].MountPath
+			dirName := mountPath + "/" + m.f.UniqueName
+			fileName := dirName + "/" + m.f.UniqueName
+
+			err = e2epod.WaitForPodNameRunningInNamespace(ctx, m.cs, pod.Name, pod.Namespace)
+			framework.ExpectNoError(err, "failed to start pod")
+
+			// Create the subdirectory to ensure that fsGroup propagates
+			createDirectory := fmt.Sprintf("mkdir %s", dirName)
+			_, _, err = e2evolume.PodExec(m.f, pod, createDirectory)
+			framework.ExpectNoError(err, "failed: creating the directory: %s", err)
+
+			// Inject the contents onto the mount
+			createFile := fmt.Sprintf("echo '%s' > '%s'; sync", "filecontents", fileName)
+			_, _, err = e2evolume.PodExec(m.f, pod, createFile)
+			framework.ExpectNoError(err, "failed: writing the contents: %s", err)
+
+			// Delete the created file. This step is mandatory, as the mock driver
+			// won't clean up the contents automatically.
+			defer func() {
+				deleteDir := fmt.Sprintf("rm -fr %s", dirName)
+				_, _, err = e2evolume.PodExec(m.f, pod, deleteDir)
+				framework.ExpectNoError(err, "failed: deleting the directory: %s", err)
+			}()
+
+			// Ensure that the fsGroup matches what we expect
+			if modified {
+				utils.VerifyFSGroupInPod(m.f, fileName, strconv.FormatInt(*fsGroup, 10), pod)
+			} else {
+				utils.VerifyFSGroupInPod(m.f, fileName, "root", pod)
+			}
+		})
+
+		return err == nil
+	})
+
+	framework.ExpectNoError(err, "failed: verifing fsgroup in pod: %s", err)
+}
