7e0488995efb153d7ff667a2e7e22b266f206344
xin.li <xin.li@daocloud.io>
2023-03-19 11:57:19 +0800

    [UT] add test for pkg/securitycontext/util.go
    Signed-off-by: xin.li <xin.li@daocloud.io>

--- pkg/securitycontext/util_test.go
+++ pkg/securitycontext/util_test.go
@@ -210,3 +210,388 @@ func TestDetermineEffectiveRunAsUser(t *testing.T) {
 		})
 	}
 }
+
+func Test_securityContextFromPodSecurityContext(t *testing.T) {
+	gmsaCredSpecName := "gmsa spec name"
+	gmsaCredSpec := "credential spec"
+	username := "ContainerAdministrator"
+	asHostProcess := true
+	var asuid int64
+	var asgid int64
+	asuid = 1001
+	asgid = 1001
+	runasnonroot := true
+
+	tests := []struct {
+		name string
+		pod  *v1.Pod
+		want *v1.SecurityContext
+	}{
+		{
+			name: "SecurityContext is nil",
+			pod: &v1.Pod{
+				Spec: v1.PodSpec{},
+			},
+			want: nil,
+		},
+		{
+			name: "test get SecurityContext options",
+			pod: &v1.Pod{
+				Spec: v1.PodSpec{
+					SecurityContext: &v1.PodSecurityContext{
+						SELinuxOptions: &v1.SELinuxOptions{
+							User: "foo",
+						},
+						WindowsOptions: &v1.WindowsSecurityContextOptions{
+							GMSACredentialSpecName: &gmsaCredSpecName,
+							GMSACredentialSpec:     &gmsaCredSpec,
+							RunAsUserName:          &username,
+							HostProcess:            &asHostProcess,
+						},
+						RunAsUser:    &asuid,
+						RunAsGroup:   &asgid,
+						RunAsNonRoot: &runasnonroot,
+					},
+				},
+			},
+			want: &v1.SecurityContext{
+				SELinuxOptions: &v1.SELinuxOptions{
+					User: "foo",
+				},
+				WindowsOptions: &v1.WindowsSecurityContextOptions{
+					GMSACredentialSpecName: &gmsaCredSpecName,
+					GMSACredentialSpec:     &gmsaCredSpec,
+					RunAsUserName:          &username,
+					HostProcess:            &asHostProcess,
+				},
+				RunAsUser:    &asuid,
+				RunAsGroup:   &asgid,
+				RunAsNonRoot: &runasnonroot,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := securityContextFromPodSecurityContext(tt.pod); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("securityContextFromPodSecurityContext() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDetermineEffectiveSecurityContext(t *testing.T) {
+	// pod var
+	podgmsaCredSpecName := "pod gmsa spec name"
+	podgmsaCredSpec := "pod credential spec"
+	podusername := "pod username"
+	podasHostProcess := true
+	var podasuid int64
+	var podasgid int64
+	podasuid = 1001
+	podasgid = 1001
+	podrunasnonroot := true
+
+	// container var
+	containergmsaCredSpecName := "container gmsa spec name"
+	containergmsaCredSpec := "container credential spec"
+	containerusername := "container username"
+	containerasHostProcess := true
+	var containerasuid int64
+	var containerasgid int64
+	containerasuid = 1
+	containerasgid = 1
+	containerrunasnonroot := false
+	privileged := false
+	ReadOnlyRootFilesystem := false
+	AllowPrivilegeEscalation := true
+	ProcMount := v1.DefaultProcMount
+
+	type args struct {
+		pod       *v1.Pod
+		container *v1.Container
+	}
+	tests := []struct {
+		name string
+		args args
+		want *v1.SecurityContext
+	}{
+		{
+			name: "pod and container's SecurityContext are empty",
+			args: args{
+				pod:       &v1.Pod{},
+				container: &v1.Container{},
+			},
+			want: &v1.SecurityContext{},
+		},
+		{
+			name: "pod's SecurityContext is not empty and container's SecurityContext is empty",
+			args: args{
+				pod: &v1.Pod{
+					Spec: v1.PodSpec{
+						SecurityContext: &v1.PodSecurityContext{
+							SELinuxOptions: &v1.SELinuxOptions{
+								User: "foo",
+							},
+						},
+					},
+				},
+				container: &v1.Container{},
+			},
+			want: &v1.SecurityContext{
+				SELinuxOptions: &v1.SELinuxOptions{
+					User: "foo",
+				},
+			},
+		},
+		{
+			name: "pod's SecurityContext is empty and container's SecurityContext is not empty",
+			args: args{
+				pod: &v1.Pod{
+					Spec: v1.PodSpec{},
+				},
+				container: &v1.Container{
+					SecurityContext: &v1.SecurityContext{
+						RunAsUser:  &podasuid,
+						RunAsGroup: &podasgid,
+					},
+				},
+			},
+			want: &v1.SecurityContext{
+				RunAsUser:  &podasuid,
+				RunAsGroup: &podasgid,
+			},
+		},
+		{
+			name: "merge SecurityContexts for all values",
+			args: args{
+				pod: &v1.Pod{
+					Spec: v1.PodSpec{
+						SecurityContext: &v1.PodSecurityContext{
+							SELinuxOptions: &v1.SELinuxOptions{
+								User: "foo",
+							},
+							WindowsOptions: &v1.WindowsSecurityContextOptions{
+								GMSACredentialSpecName: &podgmsaCredSpecName,
+								GMSACredentialSpec:     &podgmsaCredSpec,
+								RunAsUserName:          &podusername,
+								HostProcess:            &podasHostProcess,
+							},
+							RunAsUser:    &podasuid,
+							RunAsGroup:   &podasgid,
+							RunAsNonRoot: &podrunasnonroot,
+						},
+					},
+				},
+				container: &v1.Container{
+					SecurityContext: &v1.SecurityContext{
+						SELinuxOptions: &v1.SELinuxOptions{
+							User: "bar",
+						},
+						WindowsOptions: &v1.WindowsSecurityContextOptions{
+							GMSACredentialSpecName: &containergmsaCredSpecName,
+							GMSACredentialSpec:     &containergmsaCredSpec,
+							RunAsUserName:          &containerusername,
+							HostProcess:            &containerasHostProcess,
+						},
+						RunAsUser:    &containerasuid,
+						RunAsGroup:   &containerasgid,
+						RunAsNonRoot: &containerrunasnonroot,
+						Capabilities: &v1.Capabilities{
+							Add: []v1.Capability{"SYS_CHROOT"},
+						},
+						Privileged:               &privileged,
+						ReadOnlyRootFilesystem:   &ReadOnlyRootFilesystem,
+						AllowPrivilegeEscalation: &AllowPrivilegeEscalation,
+						ProcMount:                &ProcMount,
+					},
+				},
+			},
+			want: &v1.SecurityContext{
+				SELinuxOptions: &v1.SELinuxOptions{
+					User: "bar",
+				},
+				WindowsOptions: &v1.WindowsSecurityContextOptions{
+					GMSACredentialSpecName: &containergmsaCredSpecName,
+					GMSACredentialSpec:     &containergmsaCredSpec,
+					RunAsUserName:          &containerusername,
+					HostProcess:            &containerasHostProcess,
+				},
+				RunAsUser:    &containerasuid,
+				RunAsGroup:   &containerasgid,
+				RunAsNonRoot: &containerrunasnonroot,
+				Capabilities: &v1.Capabilities{
+					Add: []v1.Capability{"SYS_CHROOT"},
+				},
+				Privileged:               &privileged,
+				ReadOnlyRootFilesystem:   &ReadOnlyRootFilesystem,
+				AllowPrivilegeEscalation: &AllowPrivilegeEscalation,
+				ProcMount:                &ProcMount,
+			},
+		},
+		{
+			name: "merge SecurityContexts for part of values",
+			args: args{
+				pod: &v1.Pod{
+					Spec: v1.PodSpec{
+						SecurityContext: &v1.PodSecurityContext{
+							SELinuxOptions: &v1.SELinuxOptions{
+								User: "foo",
+							},
+							WindowsOptions: &v1.WindowsSecurityContextOptions{
+								GMSACredentialSpecName: &podgmsaCredSpecName,
+								GMSACredentialSpec:     &podgmsaCredSpec,
+								RunAsUserName:          &podusername,
+								HostProcess:            &podasHostProcess,
+							},
+							RunAsUser:    &podasuid,
+							RunAsGroup:   &podasgid,
+							RunAsNonRoot: &podrunasnonroot,
+						},
+					},
+				},
+				container: &v1.Container{
+					SecurityContext: &v1.SecurityContext{
+						WindowsOptions: &v1.WindowsSecurityContextOptions{
+							GMSACredentialSpecName: &containergmsaCredSpecName,
+							GMSACredentialSpec:     &containergmsaCredSpec,
+							RunAsUserName:          &containerusername,
+							HostProcess:            &containerasHostProcess,
+						},
+						Privileged:               &privileged,
+						ReadOnlyRootFilesystem:   &ReadOnlyRootFilesystem,
+						AllowPrivilegeEscalation: &AllowPrivilegeEscalation,
+						ProcMount:                &ProcMount,
+					},
+				},
+			},
+			want: &v1.SecurityContext{
+				SELinuxOptions: &v1.SELinuxOptions{
+					User: "foo",
+				},
+				WindowsOptions: &v1.WindowsSecurityContextOptions{
+					GMSACredentialSpecName: &containergmsaCredSpecName,
+					GMSACredentialSpec:     &containergmsaCredSpec,
+					RunAsUserName:          &containerusername,
+					HostProcess:            &containerasHostProcess,
+				},
+				RunAsUser:                &podasuid,
+				RunAsGroup:               &podasgid,
+				RunAsNonRoot:             &podrunasnonroot,
+				Privileged:               &privileged,
+				ReadOnlyRootFilesystem:   &ReadOnlyRootFilesystem,
+				AllowPrivilegeEscalation: &AllowPrivilegeEscalation,
+				ProcMount:                &ProcMount,
+			},
+		},
+		{
+			name: "WindowsOptions values",
+			args: args{
+				pod: &v1.Pod{
+					Spec: v1.PodSpec{
+						SecurityContext: &v1.PodSecurityContext{
+							WindowsOptions: &v1.WindowsSecurityContextOptions{
+								GMSACredentialSpecName: &podgmsaCredSpecName,
+								GMSACredentialSpec:     &podgmsaCredSpec,
+								RunAsUserName:          &podusername,
+								HostProcess:            &podasHostProcess,
+							},
+						},
+					},
+				},
+				container: &v1.Container{
+					SecurityContext: &v1.SecurityContext{
+						WindowsOptions: &v1.WindowsSecurityContextOptions{
+							GMSACredentialSpec: &containergmsaCredSpec,
+							RunAsUserName:      &containerusername,
+							HostProcess:        &containerasHostProcess,
+						},
+					},
+				},
+			},
+			want: &v1.SecurityContext{
+				WindowsOptions: &v1.WindowsSecurityContextOptions{
+					GMSACredentialSpecName: nil,
+					GMSACredentialSpec:     &containergmsaCredSpec,
+					RunAsUserName:          &containerusername,
+					HostProcess:            &containerasHostProcess,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DetermineEffectiveSecurityContext(tt.args.pod, tt.args.container); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DetermineEffectiveSecurityContext() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHasWindowsHostProcessRequest(t *testing.T) {
+	gmsaCredSpecName := "gmsa spec name"
+	asHostProcess := true
+	type args struct {
+		pod       *v1.Pod
+		container *v1.Container
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "WindowsOptions is nil",
+			args: args{
+				pod:       &v1.Pod{},
+				container: &v1.Container{},
+			},
+			want: false,
+		},
+		{
+			name: "WindowsOptions and HostProcess are not nil ",
+			args: args{
+				pod: &v1.Pod{
+					Spec: v1.PodSpec{
+						SecurityContext: &v1.PodSecurityContext{
+							SELinuxOptions: &v1.SELinuxOptions{
+								User: "foo",
+							},
+							WindowsOptions: &v1.WindowsSecurityContextOptions{
+								GMSACredentialSpecName: &gmsaCredSpecName,
+								HostProcess:            &asHostProcess,
+							},
+						},
+					},
+				},
+				container: &v1.Container{},
+			},
+			want: true,
+		},
+		{
+			name: "HostProcess is nil ",
+			args: args{
+				pod: &v1.Pod{
+					Spec: v1.PodSpec{
+						SecurityContext: &v1.PodSecurityContext{
+							SELinuxOptions: &v1.SELinuxOptions{
+								User: "foo",
+							},
+							WindowsOptions: &v1.WindowsSecurityContextOptions{
+								GMSACredentialSpecName: &gmsaCredSpecName,
+							},
+						},
+					},
+				},
+				container: &v1.Container{},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HasWindowsHostProcessRequest(tt.args.pod, tt.args.container); got != tt.want {
+				t.Errorf("HasWindowsHostProcessRequest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
