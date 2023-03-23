9627c50ef37f3b5274486e6f5ad37b73b1b69bf0
Tim Hockin <thockin@google.com>
2023-03-23 11:31:25 -0700

    Replace uses of ObjectGoPrintDiff with cmp.Diff
    ObjectGoPrintDiff is already a shim over cmp.Diff, so no actual output
    or behavior changes

--- staging/src/k8s.io/apiserver/pkg/endpoints/handlers/rest_test.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/handlers/rest_test.go
@@ -28,6 +28,7 @@ import (
 	"time"
 
 	jsonpatch "github.com/evanphx/json-patch"
+	"github.com/google/go-cmp/cmp"
 	fuzz "github.com/google/gofuzz"
 	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -583,7 +584,7 @@ func (tc *patchTestCase) Run(t *testing.T) {
 		reallyExpectedPod := expectedObj.(*example.Pod)
 
 		if !reflect.DeepEqual(*reallyExpectedPod, *resultPod) {
-			t.Errorf("%s mismatch: %v\n", tc.name, diff.ObjectGoPrintDiff(reallyExpectedPod, resultPod))
+			t.Errorf("%s mismatch: %v\n", tc.name, cmp.Diff(reallyExpectedPod, resultPod))
 			continue
 		}
 	}
