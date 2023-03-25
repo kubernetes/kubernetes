9868e243be61c5937c53565ed2af37c89bf06258
Jyoti Mahapatra <jyotima@amazon.com>
2023-03-24 19:52:44 -0700

    exclude kms from kcm health check
    https://github.com/kubernetes/kubernetes/issues/108014
    
    Testing:
    - Create cluster with KMS enabled.
    - Disable KMS key.
    - Restart APIServer to invalidate the DEK cache.
    - Restart KCM.
    - All controllers in KCM still work. Starting 1.21, all controllers have
      moved to use BoundServiceAccountTokens. Secrets are no longer used for
    controller authentication. GarbageCollector Controller needs all
    discovery objects to be synced to create the object graph. Discovery
    Client fails because secret informer cache fails. Other controllers are
    able to be synchronized.
    - Enable KMS key.
    - APIServer secret cache fills up. Secret informer cache in KCM resyncs. Errors in KCM logs relatd to GarbageCollector Controller disapper.
    
    KCM stays healthy throughout the testing process.
    
    Signed-off-by: Jyoti Mahapatra <jyotima@amazon.com>

--- staging/src/k8s.io/controller-manager/app/helper.go
+++ staging/src/k8s.io/controller-manager/app/helper.go
@@ -34,7 +34,7 @@ func WaitForAPIServer(client clientset.Interface, timeout time.Duration) error {
 
 	err := wait.PollImmediate(time.Second, timeout, func() (bool, error) {
 		healthStatus := 0
-		result := client.Discovery().RESTClient().Get().AbsPath("/healthz").Do(context.TODO()).StatusCode(&healthStatus)
+		result := client.Discovery().RESTClient().Get().AbsPath("/healthz").Param("exclude", "kms-provider-0").Do(context.TODO()).StatusCode(&healthStatus)
 		if result.Error() != nil {
 			lastErr = fmt.Errorf("failed to get apiserver /healthz status: %v", result.Error())
 			return false, nil
