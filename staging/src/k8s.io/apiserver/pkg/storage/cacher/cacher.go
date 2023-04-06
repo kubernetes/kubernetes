476e407ffd2ab393840d3f7a9fd01b71698738a3
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2023-04-06 09:54:02 +0200

    cacher: prevent a potential deadlock
    waitUntilWatchCacheFreshAndForceAllEvents must be called without
    a read lock held otherwise the watchcache won't be able to make
    progress (i.e. the watchCache.processEvent method that requries acquiring an exclusive lock)
    
    the deadlock can happen only when the alpha watchlist feature flag is on
    and the client specifically requests streaming.

--- staging/src/k8s.io/apiserver/pkg/storage/cacher/cacher.go
+++ staging/src/k8s.io/apiserver/pkg/storage/cacher/cacher.go
@@ -592,6 +592,18 @@ func (c *Cacher) Watch(ctx context.Context, key string, opts storage.ListOptions
 		identifier,
 	)
 
+	// note that c.waitUntilWatchCacheFreshAndForceAllEvents must be called without
+	// the c.watchCache.RLock held otherwise we are at risk of a deadlock
+	// mainly because c.watchCache.processEvent method won't be able to make progress
+	//
+	// moreover even though the c.waitUntilWatchCacheFreshAndForceAllEvents acquires a lock
+	// it is safe to release the lock after the method finishes because we don't require
+	// any atomicity between the call to the method and further calls that actually get the events.
+	forceAllEvents, err := c.waitUntilWatchCacheFreshAndForceAllEvents(ctx, requestedWatchRV, opts)
+	if err != nil {
+		return newErrWatcher(err), nil
+	}
+
 	// We explicitly use thread unsafe version and do locking ourself to ensure that
 	// no new events will be processed in the meantime. The watchCache will be unlocked
 	// on return from this function.
@@ -599,10 +611,7 @@ func (c *Cacher) Watch(ctx context.Context, key string, opts storage.ListOptions
 	// underlying watchCache is calling processEvent under its lock.
 	c.watchCache.RLock()
 	defer c.watchCache.RUnlock()
-	forceAllEvents, err := c.waitUntilWatchCacheFreshAndForceAllEvents(ctx, requestedWatchRV, opts)
-	if err != nil {
-		return newErrWatcher(err), nil
-	}
+
 	startWatchRV := startWatchResourceVersionFn()
 	var cacheInterval *watchCacheInterval
 	if forceAllEvents {
