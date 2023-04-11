901dea73073c5985563a7d6cdec9b7b1a6d0a3e2
Hemant Kumar <hekumar@redhat.com>
2023-04-11 15:02:25 -0400

    Fix azure disk e2e after migration

--- test/e2e/storage/drivers/in_tree.go
+++ test/e2e/storage/drivers/in_tree.go
@@ -1443,7 +1443,7 @@ func (a *azureDiskDriver) CreateVolume(ctx context.Context, config *storageframe
 		// so pods should be also scheduled there.
 		config.ClientNodeSelection = e2epod.NodeSelection{
 			Selector: map[string]string{
-				v1.LabelFailureDomainBetaZone: zone,
+				v1.LabelTopologyZone: zone,
 			},
 		}
 	}
@@ -1744,6 +1744,10 @@ func getInlineVolumeZone(ctx context.Context, f *framework.Framework) string {
 	zone, ok := node.Labels[v1.LabelFailureDomainBetaZone]
 	if ok {
 		return zone
+	}
+	topologyZone, ok := node.Labels[v1.LabelTopologyZone]
+	if ok {
+		return topologyZone
 	}
 	return ""
 }
