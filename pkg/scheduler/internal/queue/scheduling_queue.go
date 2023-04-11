2bed67d0f1cf126859775cc762aa446c9149fd85
Kensei Nakada <handbomusic@gmail.com>
2023-04-11 05:13:33 +0000

    Revert "Optimization on running prePreEnqueuePlugins before adding pods into activeQ"
    This reverts commit c01fa8279d3af2896744b2d6e011786d2bd62e68.

--- pkg/scheduler/internal/queue/scheduling_queue.go
+++ pkg/scheduler/internal/queue/scheduling_queue.go
@@ -553,9 +553,7 @@ func (p *PriorityQueue) flushBackoffQCompleted() {
 			klog.ErrorS(err, "Unable to pop pod from backoff queue despite backoff completion", "pod", klog.KObj(pod))
 			break
 		}
-		if err := p.activeQ.Add(pInfo); err != nil {
-			klog.ErrorS(err, "Error adding pod to the active queue", "pod", klog.KObj(pInfo.Pod))
-		} else {
+		if added, _ := p.addToActiveQ(pInfo); added {
 			klog.V(5).InfoS("Pod moved to an internal scheduling queue", "pod", klog.KObj(pod), "event", BackoffComplete, "queue", activeQName)
 			metrics.SchedulerQueueIncomingPods.WithLabelValues("active", BackoffComplete).Inc()
 			activated = true
