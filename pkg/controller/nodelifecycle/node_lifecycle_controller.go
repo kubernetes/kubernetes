d09842e0adfcffa2d72361c83d438533f49e3342
Andrea Tosatto <andrea.tosy@gmail.com>
2023-04-12 16:02:33 +0200

    node-lifecycle-controller: improve monitorNodeHealth test-coverage (#116687)
    * node-lifecycle-controller: refactor monitorNodeHealth tests to improve test-coverage
    
    * address PR review comments
    
    * dedupe test logic

--- pkg/controller/nodelifecycle/node_lifecycle_controller.go
+++ pkg/controller/nodelifecycle/node_lifecycle_controller.go
@@ -653,9 +653,11 @@ func (nc *Controller) doNoExecuteTaintingPass(ctx context.Context) {
 	}
 }
 
-// monitorNodeHealth verifies node health are constantly updated by kubelet, and
-// if not, post "NodeReady==ConditionUnknown".
-// This function will taint nodes who are not ready or not reachable for a long period of time.
+// monitorNodeHealth verifies node health are constantly updated by kubelet, and if not, post "NodeReady==ConditionUnknown".
+// This function will
+//   - add nodes which are not ready or not reachable for a long period of time to a rate-limited
+//     queue so that NoExecute taints can be added by the goroutine running the doNoExecuteTaintingPass function,
+//   - update the PodReady condition Pods according to the state of the Node Ready condition.
 func (nc *Controller) monitorNodeHealth(ctx context.Context) error {
 	start := nc.now()
 	defer func() {
