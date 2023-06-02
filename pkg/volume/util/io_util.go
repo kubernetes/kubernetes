5d6417d00ba4c3de3b4a4fc55b4f30fbb08955ce
guoguangwu <guoguangwu@magic-shield.com>
2023-06-02 20:46:52 +0800

    fix: io/ioutil imported and not used

--- pkg/volume/util/io_util.go
+++ pkg/volume/util/io_util.go
@@ -17,8 +17,8 @@ limitations under the License.
 package util
 
 import (
-	"io/ioutil"
 	"os"
+	"io/fs"
 	"path/filepath"
 )
 
@@ -41,7 +41,19 @@ func (handler *osIOHandler) ReadFile(filename string) ([]byte, error) {
 	return os.ReadFile(filename)
 }
 func (handler *osIOHandler) ReadDir(dirname string) ([]os.FileInfo, error) {
-	return ioutil.ReadDir(dirname)
+	entries, err := os.ReadDir(dirname)
+	if err != nil {
+		return nil, err
+	}
+	infos := make([]fs.FileInfo, 0, len(entries))
+	for _, entry := range entries {
+		info, err := entry.Info()
+		if err != nil {
+			return nil, err
+		}
+		infos = append(infos, info)
+	}
+	return infos, nil
 }
 func (handler *osIOHandler) Lstat(name string) (os.FileInfo, error) {
 	return os.Lstat(name)
