00553165f0c15fd6d8baa81f829b3da777f3f6fa
Sohan Kunkerkar <sohank2602@gmail.com>
2023-06-07 14:24:24 -0400

    pkg/kubelet: allow sandbox image pinning from CRI
    As part of this change, the code responsible for managing the sandbox
    image within the kubelet has been removed. Previously, the kubelet used
    to prevent sandbox image from the garbage collection process. However,
    with this update, the responsibility of managing the sandbox containers
    has been shifted to the CRI implementation itself. By allowing sandbox
    image pinning from CRI, we improve efficiency and simplify the kubelet's
    interaction with the container runtime. As a result, the kubelet can now
    rely on the container runtime's built-in mechanisms for sandbox container
    lifecycle management.
    
    Signed-off-by: Sohan Kunkerkar <sohank2602@gmail.com>

--- pkg/kubelet/kubelet_test.go
+++ pkg/kubelet/kubelet_test.go
@@ -311,7 +311,7 @@ func newTestKubeletWithImageList(
 		HighThresholdPercent: 90,
 		LowThresholdPercent:  80,
 	}
-	imageGCManager, err := images.NewImageGCManager(fakeRuntime, kubelet.StatsProvider, fakeRecorder, fakeNodeRef, fakeImageGCPolicy, "", oteltrace.NewNoopTracerProvider())
+	imageGCManager, err := images.NewImageGCManager(fakeRuntime, kubelet.StatsProvider, fakeRecorder, fakeNodeRef, fakeImageGCPolicy, oteltrace.NewNoopTracerProvider())
 	assert.NoError(t, err)
 	kubelet.imageManager = &fakeImageGCManager{
 		fakeImageService: fakeRuntime,
