0a1a0a735360202625661f02e1745768884743a2
Aldo Culquicondor <acondor@google.com>
2023-06-16 14:24:07 -0400

    Skip terminal Pods with a deletion timestamp from the Daemonset sync
    Change-Id: I64a347a87c02ee2bd48be10e6fff380c8c81f742

--- pkg/controller/daemon/daemon_controller.go
+++ pkg/controller/daemon/daemon_controller.go
@@ -761,6 +761,12 @@ func (dsc *DaemonSetsController) getNodesToDaemonPods(ctx context.Context, ds *a
 	nodeToDaemonPods := make(map[string][]*v1.Pod)
 	logger := klog.FromContext(ctx)
 	for _, pod := range claimedPods {
+		if podutil.IsPodTerminal(pod) && pod.DeletionTimestamp != nil {
+			// This Pod has a finalizer or is already scheduled for deletion by the
+			// kubelet or the Pod GC. The DS controller doesn't have anything else to
+			// do with it.
+			continue
+		}
 		nodeName, err := util.GetTargetNodeName(pod)
 		if err != nil {
 			logger.Info("Failed to get target node name of Pod in DaemonSet",
