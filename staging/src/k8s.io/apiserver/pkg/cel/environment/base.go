8510a4ffc05f192ab57e5f018c7258686df18ea8
Alexander Zielenski <zielenski@google.com>
2023-06-22 11:12:09 -0700

    set CEL default env to 1.28 for 1.28 release

--- staging/src/k8s.io/apiserver/pkg/cel/environment/base.go
+++ staging/src/k8s.io/apiserver/pkg/cel/environment/base.go
@@ -41,7 +41,7 @@ import (
 // desirable because it means that CEL expressions are portable across a wider range
 // of Kubernetes versions.
 func DefaultCompatibilityVersion() *version.Version {
-	return version.MajorMinor(1, 27)
+	return version.MajorMinor(1, 28)
 }
 
 var baseOpts = []VersionedOptions{
