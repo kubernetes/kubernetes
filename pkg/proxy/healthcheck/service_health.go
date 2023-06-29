4e6aa5fb86176e69e3c02cf719df5946fe99c799
Cezary Zawadka <czawadka@google.com>
2023-06-29 17:06:01 +0200

    kube-proxy service health: add new return header with number of local endpoints
    - add new header "X-Load-Balancing-Endpoint-Weight" returned from service health. Value of the header is number of local endpoints. Header can be used in weighted load balancing. Parsing header for number of endpoints is faster than unmarshalling json from the content body.
    
    - add missing unit test for new and old headers returned from service health

--- pkg/proxy/healthcheck/service_health.go
+++ pkg/proxy/healthcheck/service_health.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -233,6 +234,8 @@ func (h hcHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 
 	resp.Header().Set("Content-Type", "application/json")
 	resp.Header().Set("X-Content-Type-Options", "nosniff")
+	resp.Header().Set("X-Load-Balancing-Endpoint-Weight", strconv.Itoa(count))
+
 	if count != 0 && kubeProxyHealthy {
 		resp.WriteHeader(http.StatusOK)
 	} else {
