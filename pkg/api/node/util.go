1062079fe6df6f51303d4ddd90eca9783116a841
wafuwafu13 <jaruwafu@gmail.com>
2023-07-02 22:17:45 +0100

    chore(node/util): add app.kubernetes.io/created-by

--- pkg/api/node/util.go
+++ pkg/api/node/util.go
@@ -31,6 +31,7 @@ var deprecatedNodeLabels = map[string]string{
 	`failure-domain.beta.kubernetes.io/region`:      `deprecated since v1.17; use "topology.kubernetes.io/region" instead`,
 	`failure-domain.beta.kubernetes.io/zone`:        `deprecated since v1.17; use "topology.kubernetes.io/zone" instead`,
 	`beta.kubernetes.io/instance-type`:              `deprecated since v1.17; use "node.kubernetes.io/instance-type" instead`,
+	`app.kubernetes.io/created-by`:                  `deprecated since v1.19`,
 	`scheduler.alpha.kubernetes.io/preferAvoidPods`: `deprecated since v1.22; use Taints and Tolerations instead`,
 	`node-role.kubernetes.io/master`:                `use "node-role.kubernetes.io/control-plane" instead`,
 }
