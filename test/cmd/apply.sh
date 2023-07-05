e8d89857dc26828f0cf41dc7dd8faec8d4e47973
yulng <wei.yang@daocloud.io>
2023-07-05 11:38:20 +0800

    remove PruneWhitelist for 1.28 version
    Signed-off-by: yulng <wei.yang@daocloud.io>

--- test/cmd/apply.sh
+++ test/cmd/apply.sh
@@ -231,7 +231,7 @@ __EOF__
   kube::test::get_object_assert pods "{{range.items}}{{${id_field:?}}}:{{end}}" ''
   kubectl delete pvc b-pvc 2>&1 "${kube_flags[@]:?}"
 
-  ## kubectl apply --prune --prune-whitelist
+  ## kubectl apply --prune --prune-allowlist
   # Pre-Condition: no POD exists
   kube::test::get_object_assert pods "{{range.items}}{{${id_field:?}}}:{{end}}" ''
   # apply pod a
@@ -239,7 +239,7 @@ __EOF__
   # check right pod exists
   kube::test::get_object_assert 'pods a' "{{${id_field:?}}}" 'a'
   # apply svc and don't prune pod a by overwriting whitelist
-  kubectl apply --prune -l prune-group=true -f hack/testdata/prune/svc.yaml --prune-whitelist core/v1/Service 2>&1 "${kube_flags[@]:?}"
+  kubectl apply --prune -l prune-group=true -f hack/testdata/prune/svc.yaml --prune-allowlist core/v1/Service 2>&1 "${kube_flags[@]:?}"
   kube::test::get_object_assert 'service prune-svc' "{{${id_field:?}}}" 'prune-svc'
   kube::test::get_object_assert 'pods a' "{{${id_field:?}}}" 'a'
   # apply svc and prune pod a with default whitelist
