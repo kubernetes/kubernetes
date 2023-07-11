bcf44dcdf8edec9828081b89b939c03a780a6164
Alexander Zielenski <zielenski@google.com>
2023-07-11 12:04:13 -0700

    use new namespaceParamRef in VAP controller

--- staging/src/k8s.io/apiserver/pkg/admission/plugin/validatingadmissionpolicy/controller.go
+++ staging/src/k8s.io/apiserver/pkg/admission/plugin/validatingadmissionpolicy/controller.go
@@ -32,6 +32,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	utiljson "k8s.io/apimachinery/pkg/util/json"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
@@ -242,6 +243,12 @@ func (c *celAdmissionController) Validate(
 	policyDatas := c.definitions.Load().([]policyData)
 
 	for _, definitionInfo := range policyDatas {
+		// versionedAttributes will be set to non-nil inside of the loop, but
+		// is scoped outside of the param loop so we only convert once. We defer
+		// conversion so that it is only performed when we know a policy matches,
+		// saving the cost of converting non-matching requests.
+		var versionedAttr *admission.VersionedAttributes
+
 		definition := definitionInfo.lastReconciledValue
 		matches, matchKind, err := c.policyController.matcher.DefinitionMatches(a, o, definition)
 		if err != nil {
@@ -273,21 +280,17 @@ func (c *celAdmissionController) Validate(
 				continue
 			}
 
-			var param runtime.Object
-
-			// versionedAttributes will be set to non-nil inside of the loop, but
-			// is scoped outside of the param loop so we only convert once. We defer
-			// conversion so that it is only performed when we know a policy matches,
-			// saving the cost of converting non-matching requests.
-			var versionedAttr *admission.VersionedAttributes
+			var params []runtime.Object
 
 			// If definition has paramKind, paramRef is required in binding.
 			// If definition has no paramKind, paramRef set in binding will be ignored.
 			paramKind := definition.Spec.ParamKind
 			paramRef := binding.Spec.ParamRef
-			if paramKind != nil && paramRef != nil {
-				paramController := definitionInfo.paramController
-				if paramController == nil {
+			namespaceParamRef := binding.Spec.NamespaceParamRef
+
+			// Make sure the param kind is ready to use
+			if paramKind != nil && (paramRef != nil || namespaceParamRef != nil) {
+				if definitionInfo.paramController == nil {
 					addConfigError(fmt.Errorf("paramKind kind `%v` not known",
 						paramKind.String()), definition, binding)
 					continue
@@ -299,38 +302,136 @@ func (c *celAdmissionController) Validate(
 				timeoutCtx, cancel := context.WithTimeout(c.policyController.context, 1*time.Second)
 				defer cancel()
 
-				if !cache.WaitForCacheSync(timeoutCtx.Done(), paramController.HasSynced) {
+				if !cache.WaitForCacheSync(timeoutCtx.Done(), definitionInfo.paramController.HasSynced) {
 					addConfigError(fmt.Errorf("paramKind kind `%v` not yet synced to use for admission",
 						paramKind.String()), definition, binding)
 					continue
 				}
+			}
+
+			// Find params to use with policy
+			switch {
+			case paramKind == nil:
+				// ParamKind is unset. Ignore any globalParamRef or namespaceParamRef
+				// setting.
+				// TODO(alexzielenski): historical behavior. Should the failure mode apply here instead?
+				//
+			case paramRef == nil && namespaceParamRef == nil:
+				// Policy ParamKind is set, but binding does not use it. No params
+			case paramRef != nil:
+				var param runtime.Object
 
 				if len(paramRef.Namespace) == 0 {
-					param, err = paramController.Informer().Get(paramRef.Name)
+					param, err = definitionInfo.paramController.Informer().Get(paramRef.Name)
 				} else {
-					param, err = paramController.Informer().Namespaced(paramRef.Namespace).Get(paramRef.Name)
+					param, err = definitionInfo.paramController.Informer().Namespaced(paramRef.Namespace).Get(paramRef.Name)
 				}
 
-				if err != nil {
-					// Apply failure policy
+				switch {
+				case err == nil:
+					params = append(params, param)
+				case k8serrors.IsNotFound(err):
+					// Param not yet available. User may need to wait a bit
+					// before being able to use it for validation.
 					addConfigError(err, definition, binding)
-
-					if k8serrors.IsInvalid(err) {
-						// Param mis-configured
-						// require to set paramRef.namespace for namespaced resource and unset paramRef.namespace for cluster scoped resource
-						continue
-					} else if k8serrors.IsNotFound(err) {
-						// Param not yet available. User may need to wait a bit
-						// before being able to use it for validation.
-						continue
-					}
-
+					continue
+				case k8serrors.IsInvalid(err):
+					// Param mis-configured
+					// require to set globalParamRef.namespace for namespaced resource
+					// and unset globalParamRef.namespace for cluster scoped resource
+					addConfigError(err, definition, binding)
+					continue
+				default:
 					// There was a bad internal error
+					addConfigError(err, definition, binding)
 					utilruntime.HandleError(err)
 					continue
 				}
-			}
+			case namespaceParamRef != nil:
+				// Check that param kind is namespaced
+				if definitionInfo.paramScope != nil && definitionInfo.paramScope.Name() != meta.RESTScopeNamespace.Name() {
+					addConfigError(errors.New("NamespaceParamRef may only be used on binding whose ParamKind is namespace-scoped"), definition, binding)
+					continue
+				}
 
+				if len(namespaceParamRef.Name) > 0 {
+					param, err := definitionInfo.paramController.
+						Informer().
+						Namespaced(a.GetNamespace()).
+						Get(namespaceParamRef.Name)
+
+					// Apply failure action on the namespace param ref in case
+					// of error
+					switch {
+					case err == nil:
+						// Successfully grabbed param
+						params = append(params, param)
+					case k8serrors.IsNotFound(err):
+						// Param not yet available. User may need to wait a bit
+						// before being able to use it for validation.
+						//
+						// Allow with no params.
+					case k8serrors.IsInvalid(err):
+						//!TODO: Test this error happens
+						// if the paramKind of the policy referred to by
+						// policyName is cluster scoped, and namespaceParamRef
+						// set, the binding is considered mis-configured, and
+						// the failureMode applies.
+						addConfigError(err, definition, binding)
+						continue
+					default:
+						addConfigError(err, definition, binding)
+						// There was a bad internal error
+						utilruntime.HandleError(err)
+						continue
+					}
+				}
+
+				if namespaceParamRef.Selector != nil {
+					selector, err := metav1.LabelSelectorAsSelector(namespaceParamRef.Selector)
+					if err != nil {
+						// Cannot parse label selector: configuration error
+						addConfigError(err, definition, binding)
+						continue
+					}
+
+					paramList, err := definitionInfo.paramController.
+						Informer().
+						Namespaced(a.GetNamespace()).
+						List(selector)
+
+					// Apply failure action on the namespace param ref in case
+					// of error
+					switch {
+					case err == nil && len(paramList) > 0:
+						// Successfully grabbed param
+						params = append(params, paramList...)
+					case k8serrors.IsNotFound(err) || len(paramList) == 0:
+						// Param not yet available. User may need to wait a bit
+						// before being able to use it for validation.
+						//
+						// Allow with no params.
+					case k8serrors.IsInvalid(err):
+						// Param mis-configured
+						// if the paramKind of the policy referred to by
+						// policyName is cluster scoped, and namespaceParamRef
+						// set, the binding is considered mis-configured, and
+						// the failureMode applies.
+						addConfigError(err, definition, binding)
+						continue
+					default:
+						// There was a bad internal error
+						addConfigError(err, definition, binding)
+						utilruntime.HandleError(err)
+						continue
+					}
+				}
+
+				// Apply fail action for params not found case
+				if len(params) == 0 && namespaceParamRef.ParameterNotFoundAction != nil && *namespaceParamRef.ParameterNotFoundAction == v1alpha1.DenyAction {
+					addConfigError(errors.New("no params found for policy binding with `Deny` failureAction"), definition, binding)
+				}
+			}
 			if versionedAttr == nil {
 				va, err := admission.NewVersionedAttributes(a, matchKind, o)
 				if err != nil {
@@ -341,62 +442,85 @@ func (c *celAdmissionController) Validate(
 				versionedAttr = va
 			}
 
-			validationResult := bindingInfo.validator.Validate(ctx, versionedAttr, param, celconfig.RuntimeCELCostBudget)
+			var validationResults []ValidateResult
 
-			for i, decision := range validationResult.Decisions {
-				switch decision.Action {
-				case ActionAdmit:
-					if decision.Evaluation == EvalError {
-						celmetrics.Metrics.ObserveAdmissionWithError(ctx, decision.Elapsed, definition.Name, binding.Name, "active")
-					}
-				case ActionDeny:
-					for _, action := range binding.Spec.ValidationActions {
-						switch action {
-						case v1alpha1.Deny:
-							deniedDecisions = append(deniedDecisions, policyDecisionWithMetadata{
-								Definition:     definition,
-								Binding:        binding,
-								PolicyDecision: decision,
-							})
-							celmetrics.Metrics.ObserveRejection(ctx, decision.Elapsed, definition.Name, binding.Name, "active")
-						case v1alpha1.Audit:
-							c.publishValidationFailureAnnotation(binding, i, decision, versionedAttr)
-							celmetrics.Metrics.ObserveAudit(ctx, decision.Elapsed, definition.Name, binding.Name, "active")
-						case v1alpha1.Warn:
-							warning.AddWarning(ctx, "", fmt.Sprintf("Validation failed for ValidatingAdmissionPolicy '%s' with binding '%s': %s", definition.Name, binding.Name, decision.Message))
-							celmetrics.Metrics.ObserveWarn(ctx, decision.Elapsed, definition.Name, binding.Name, "active")
+			if len(params) == 0 {
+				validationResults = append(validationResults, bindingInfo.validator.Validate(ctx, versionedAttr, nil, celconfig.RuntimeCELCostBudget))
+			} else {
+				for _, param := range params {
+					var p runtime.Object = param
+					if p.GetObjectKind().GroupVersionKind().Empty() {
+						// Make sure param has TypeMeta populated
+						// This is a simple hack to make sure typeMeta is
+						// available to CEL without making copies of objects, etc.
+						p = &wrappedParam{
+							TypeMeta: metav1.TypeMeta{
+								APIVersion: paramKind.APIVersion,
+								Kind:       paramKind.Kind,
+							},
+							nested: param,
 						}
 					}
-				default:
-					return fmt.Errorf("unrecognized evaluation decision '%s' for ValidatingAdmissionPolicyBinding '%s' with ValidatingAdmissionPolicy '%s'",
-						decision.Action, binding.Name, definition.Name)
+					validationResults = append(validationResults, bindingInfo.validator.Validate(ctx, versionedAttr, p, celconfig.RuntimeCELCostBudget))
 				}
 			}
 
-			for _, auditAnnotation := range validationResult.AuditAnnotations {
-				switch auditAnnotation.Action {
-				case AuditAnnotationActionPublish:
-					value := auditAnnotation.Value
-					if len(auditAnnotation.Value) > maxAuditAnnotationValueLength {
-						value = value[:maxAuditAnnotationValueLength]
+			for _, validationResult := range validationResults {
+				for i, decision := range validationResult.Decisions {
+					switch decision.Action {
+					case ActionAdmit:
+						if decision.Evaluation == EvalError {
+							celmetrics.Metrics.ObserveAdmissionWithError(ctx, decision.Elapsed, definition.Name, binding.Name, "active")
+						}
+					case ActionDeny:
+						for _, action := range binding.Spec.ValidationActions {
+							switch action {
+							case v1alpha1.Deny:
+								deniedDecisions = append(deniedDecisions, policyDecisionWithMetadata{
+									Definition:     definition,
+									Binding:        binding,
+									PolicyDecision: decision,
+								})
+								celmetrics.Metrics.ObserveRejection(ctx, decision.Elapsed, definition.Name, binding.Name, "active")
+							case v1alpha1.Audit:
+								c.publishValidationFailureAnnotation(binding, i, decision, versionedAttr)
+								celmetrics.Metrics.ObserveAudit(ctx, decision.Elapsed, definition.Name, binding.Name, "active")
+							case v1alpha1.Warn:
+								warning.AddWarning(ctx, "", fmt.Sprintf("Validation failed for ValidatingAdmissionPolicy '%s' with binding '%s': %s", definition.Name, binding.Name, decision.Message))
+								celmetrics.Metrics.ObserveWarn(ctx, decision.Elapsed, definition.Name, binding.Name, "active")
+							}
+						}
+					default:
+						return fmt.Errorf("unrecognized evaluation decision '%s' for ValidatingAdmissionPolicyBinding '%s' with ValidatingAdmissionPolicy '%s'",
+							decision.Action, binding.Name, definition.Name)
 					}
-					auditAnnotationCollector.add(auditAnnotation.Key, value)
-				case AuditAnnotationActionError:
-					// When failurePolicy=fail, audit annotation errors result in deny
-					deniedDecisions = append(deniedDecisions, policyDecisionWithMetadata{
-						Definition: definition,
-						Binding:    binding,
-						PolicyDecision: PolicyDecision{
-							Action:     ActionDeny,
-							Evaluation: EvalError,
-							Message:    auditAnnotation.Error,
-							Elapsed:    auditAnnotation.Elapsed,
-						},
-					})
-					celmetrics.Metrics.ObserveRejection(ctx, auditAnnotation.Elapsed, definition.Name, binding.Name, "active")
-				case AuditAnnotationActionExclude: // skip it
-				default:
-					return fmt.Errorf("unsupported AuditAnnotation Action: %s", auditAnnotation.Action)
+				}
+
+				for _, auditAnnotation := range validationResult.AuditAnnotations {
+					switch auditAnnotation.Action {
+					case AuditAnnotationActionPublish:
+						value := auditAnnotation.Value
+						if len(auditAnnotation.Value) > maxAuditAnnotationValueLength {
+							value = value[:maxAuditAnnotationValueLength]
+						}
+						auditAnnotationCollector.add(auditAnnotation.Key, value)
+					case AuditAnnotationActionError:
+						// When failurePolicy=fail, audit annotation errors result in deny
+						deniedDecisions = append(deniedDecisions, policyDecisionWithMetadata{
+							Definition: definition,
+							Binding:    binding,
+							PolicyDecision: PolicyDecision{
+								Action:     ActionDeny,
+								Evaluation: EvalError,
+								Message:    auditAnnotation.Error,
+								Elapsed:    auditAnnotation.Elapsed,
+							},
+						})
+						celmetrics.Metrics.ObserveRejection(ctx, auditAnnotation.Elapsed, definition.Name, binding.Name, "active")
+					case AuditAnnotationActionExclude: // skip it
+					default:
+						return fmt.Errorf("unsupported AuditAnnotation Action: %s", auditAnnotation.Action)
+					}
 				}
 			}
 		}
@@ -499,4 +623,49 @@ func (a auditAnnotationCollector) publish(policyName string, attributes admissio
 			klog.Warningf("Failed to set admission audit annotation %s to %s for ValidatingAdmissionPolicy %s: %v", key, value, policyName, err)
 		}
 	}
+}
+
+// A workaround to fact that native types do not have TypeMeta populated, which
+// is needed for CEL expressions to be able to access the value.
+type wrappedParam struct {
+	metav1.TypeMeta
+	nested runtime.Object
+}
+
+func (w *wrappedParam) MarshalJSON() ([]byte, error) {
+	return nil, errors.New("MarshalJSON unimplemented for wrappedParam")
+}
+
+func (w *wrappedParam) UnmarshalJSON(data []byte) error {
+	return errors.New("UnmarshalJSON unimplemented for wrappedParam")
+}
+
+func (w *wrappedParam) ToUnstructured() interface{} {
+	res, err := runtime.DefaultUnstructuredConverter.ToUnstructured(w.nested)
+
+	if err != nil {
+		return nil
+	}
+
+	metaRes, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&w.TypeMeta)
+	if err != nil {
+		return nil
+	}
+
+	for k, v := range metaRes {
+		res[k] = v
+	}
+
+	return res
+}
+
+func (w *wrappedParam) DeepCopyObject() runtime.Object {
+	return &wrappedParam{
+		TypeMeta: w.TypeMeta,
+		nested:   w.nested.DeepCopyObject(),
+	}
+}
+
+func (w *wrappedParam) GetObjectKind() schema.ObjectKind {
+	return w
 }
