ac55772aef2ae3d63100f995b82e93a141191d91
xuexu6666 <xuexu6666@gmail.com>
2023-07-18 18:27:03 -0500

    resolve comment

--- pkg/kubelet/server/stats/volume_stat_calculator.go
+++ pkg/kubelet/server/stats/volume_stat_calculator.go
@@ -144,7 +144,7 @@ func (s *volumeStatCalculator) calcAndStoreStats(errors map[string]error) {
 			// Expected for Volumes that don't support Metrics
 			if !volume.IsNotSupported(err) {
 				if oldErr, found := errors[name]; !found || oldErr.Error() != err.Error() {
-					klog.InfoS("Failed to calculate volume metrics", "pod", klog.KObj(s.pod), "podUID", s.pod.UID, "volumeName", name, "err", err)
+					klog.ErrorS(err, "Failed to calculate volume metrics", "pod", klog.KObj(s.pod), "podUID", s.pod.UID, "volumeName", name)
 					errors[name] = err
 				}
 			}
