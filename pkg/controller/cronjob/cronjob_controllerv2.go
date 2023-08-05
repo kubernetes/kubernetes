bf9c7ddb14d1d50c7425aa6336208a43ffb5b5e0
Albert Sverdlov <albert.sverdlov@booking.com>
2023-08-05 15:51:45 +0200

    Fix a job quota related deadlock
    In case ResourceQuota is used and sets a max # of jobs, a CronJob may get
    trapped in a deadlock:
      1. Job quota for a namespace is reached.
      2. CronJob controller can't create a new job, because quota is
         reached.
      3. Cleanup of jobs owned by a cronjob doesn't happen, because a
         control loop iteration is finished because of an error to create a
         job.
    
    To fix this we stop early quitting from a control loop iteration when
    cronjob reconciliation failed and always let old jobs to be cleaned up.

--- pkg/controller/cronjob/cronjob_controllerv2.go
+++ pkg/controller/cronjob/cronjob_controllerv2.go
@@ -25,6 +25,7 @@ import (
 	"time"
 
 	"github.com/robfig/cron/v3"
+	"k8s.io/klog/v2"
 
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -46,7 +47,6 @@ import (
 	ref "k8s.io/client-go/tools/reference"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/component-base/metrics/prometheus/ratelimiter"
-	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/controller"
 	"k8s.io/kubernetes/pkg/controller/cronjob/metrics"
 	"k8s.io/kubernetes/pkg/features"
@@ -195,13 +195,6 @@ func (jm *ControllerV2) sync(ctx context.Context, cronJobKey string) (*time.Dura
 	cronJobCopy, requeueAfter, updateStatus, err := jm.syncCronJob(ctx, cronJob, jobsToBeReconciled)
 	if err != nil {
 		klog.V(2).InfoS("Error reconciling cronjob", "cronjob", klog.KRef(cronJob.GetNamespace(), cronJob.GetName()), "err", err)
-		if updateStatus {
-			if _, err := jm.cronJobControl.UpdateStatus(ctx, cronJobCopy); err != nil {
-				klog.V(2).InfoS("Unable to update status for cronjob", "cronjob", klog.KRef(cronJob.GetNamespace(), cronJob.GetName()), "resourceVersion", cronJob.ResourceVersion, "err", err)
-				return nil, err
-			}
-		}
-		return nil, err
 	}
 
 	if jm.cleanupFinishedJobs(ctx, cronJobCopy, jobsToBeReconciled) {
@@ -433,7 +426,7 @@ func (jm *ControllerV2) syncCronJob(
 		if !found && !IsJobFinished(j) {
 			cjCopy, err := jm.cronJobControl.GetCronJob(ctx, cronJob.Namespace, cronJob.Name)
 			if err != nil {
-				return nil, nil, updateStatus, err
+				return cronJob, nil, updateStatus, err
 			}
 			if inActiveList(*cjCopy, j.ObjectMeta.UID) {
 				cronJob = cjCopy
