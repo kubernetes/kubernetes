d124fa651987c5533e88519dc19287378f0f5107
Albert Sverdlov <albert.sverdlov@booking.com>
2023-08-06 22:03:55 +0200

    Dont reorder imports

--- pkg/controller/cronjob/cronjob_controllerv2.go
+++ pkg/controller/cronjob/cronjob_controllerv2.go
@@ -25,7 +25,6 @@ import (
 	"time"
 
 	"github.com/robfig/cron/v3"
-	"k8s.io/klog/v2"
 
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -50,6 +49,7 @@ import (
 	"k8s.io/kubernetes/pkg/controller"
 	"k8s.io/kubernetes/pkg/controller/cronjob/metrics"
 	"k8s.io/kubernetes/pkg/features"
+	"k8s.io/klog/v2"
 	"k8s.io/utils/pointer"
 )
 
