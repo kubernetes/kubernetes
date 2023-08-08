1448e1c3fd39e814acd4c9fd3d6c2b2ee2b982a3
Swarup Ghosh <swghosh@redhat.com>
2023-08-08 13:01:53 +0530

    UPSTREAM: <carry>: Add RouteExternalCertificate validation in Route ObjectValidator
    Signed-off-by: Swarup Ghosh <swghosh@redhat.com>

--- openshift-kube-apiserver/admission/customresourcevalidation/route/validate_route.go
+++ openshift-kube-apiserver/admission/customresourcevalidation/route/validate_route.go
@@ -6,32 +6,20 @@ import (
 	"io"
 
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/apiserver/pkg/admission"
-	"k8s.io/apiserver/pkg/admission/initializer"
-	"k8s.io/client-go/kubernetes"
-	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
-	"k8s.io/component-base/featuregate"
+	authorizationv1client "k8s.io/client-go/kubernetes/typed/authorization/v1"
+	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 
-	configv1 "github.com/openshift/api/config/v1"
 	routev1 "github.com/openshift/api/route/v1"
-	"github.com/openshift/library-go/pkg/route"
 	routevalidation "github.com/openshift/library-go/pkg/route/validation"
-	"k8s.io/kubernetes/openshift-kube-apiserver/admission/customresourcevalidation"
 )
 
 const PluginName = "route.openshift.io/ValidateRoute"
 
 func Register(plugins *admission.Plugins) {
 	plugins.Register(PluginName, func(config io.Reader) (admission.Interface, error) {
-		return customresourcevalidation.NewValidator(
-			map[schema.GroupResource]bool{
-				{Group: routev1.GroupName, Resource: "routes"}: true,
-			},
-			map[schema.GroupVersionKind]customresourcevalidation.ObjectValidator{
-				routev1.GroupVersion.WithKind("Route"): routeV1{},
-			})
+		return NewValidateRoute()
 	})
 }
 
@@ -52,36 +40,9 @@ func toRoute(uncastObj runtime.Object) (*routev1.Route, field.ErrorList) {
 }
 
 type routeV1 struct {
-	secrets                   corev1.SecretsGetter
-	sars                      route.SubjectAccessReviewCreator
-	allowExternalCertificates bool
-	inspectedFeatureGates     bool
-}
-
-var _ initializer.WantsExternalKubeClientSet = &routeV1{}
-var _ initializer.WantsFeatures = &routeV1{}
-
-func (r *routeV1) SetExternalKubeClientSet(k kubernetes.Interface) {
-	r.secrets = k.CoreV1()
-	r.sars = k.AuthorizationV1().SubjectAccessReviews()
-}
-
-func (r *routeV1) InspectFeatureGates(fgs featuregate.FeatureGate) {
-	r.allowExternalCertificates = fgs.Enabled(featuregate.Feature(configv1.FeatureGateRouteExternalCertificate))
-	r.inspectedFeatureGates = true
-}
-
-func (r *routeV1) ValidateInitialization() error {
-	if !r.inspectedFeatureGates {
-		return fmt.Errorf("did not inspect feature gates")
-	}
-	if r.secrets == nil {
-		return fmt.Errorf("missing secrets getter")
-	}
-	if r.sars == nil {
-		return fmt.Errorf("missing subject access review creator")
-	}
-	return nil
+	secretsGetter             func() corev1client.SecretsGetter
+	sarGetter                 func() authorizationv1client.SubjectAccessReviewsGetter
+	routeValidationOptsGetter func() RouteValidationOptionGetter
 }
 
 func (r routeV1) ValidateCreate(ctx context.Context, obj runtime.Object) field.ErrorList {
@@ -90,7 +51,7 @@ func (r routeV1) ValidateCreate(ctx context.Context, obj runtime.Object) field.E
 		return errs
 	}
 
-	return routevalidation.ValidateRoute(ctx, routeObj, nil, r.secrets, route.RouteValidationOptions{AllowExternalCertificates: r.allowExternalCertificates})
+	return routevalidation.ValidateRoute(ctx, routeObj, r.sarGetter().SubjectAccessReviews(), r.secretsGetter(), r.routeValidationOptsGetter().GetValidationOptions())
 }
 
 func (r routeV1) ValidateUpdate(ctx context.Context, obj runtime.Object, oldObj runtime.Object) field.ErrorList {
@@ -104,10 +65,10 @@ func (r routeV1) ValidateUpdate(ctx context.Context, obj runtime.Object, oldObj 
 		return errs
 	}
 
-	return routevalidation.ValidateRouteUpdate(ctx, routeObj, routeOldObj, r.sars, r.secrets, route.RouteValidationOptions{AllowExternalCertificates: r.allowExternalCertificates})
+	return routevalidation.ValidateRouteUpdate(ctx, routeObj, routeOldObj, r.sarGetter().SubjectAccessReviews(), r.secretsGetter(), r.routeValidationOptsGetter().GetValidationOptions())
 }
 
-func (r routeV1) ValidateStatusUpdate(_ context.Context, obj runtime.Object, oldObj runtime.Object) field.ErrorList {
+func (routeV1) ValidateStatusUpdate(_ context.Context, obj runtime.Object, oldObj runtime.Object) field.ErrorList {
 	routeObj, errs := toRoute(obj)
 	if len(errs) > 0 {
 		return errs
