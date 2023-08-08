90c2586f34f06e1fd676bdcb8d4daa070116a7ad
Jeremy Rickard <jeremyrrickard@gmail.com>
2023-08-08 12:45:07 -0600

    [release-1.24] releng/go: Bump images, versions and deps to use Go 1.20.7
    Signed-off-by: Jeremy Rickard <jeremyrrickard@gmail.com>

--- hack/lib/protoc.sh
+++ hack/lib/protoc.sh
@@ -36,18 +36,18 @@ function kube::protoc::generate_proto() {
   kube::protoc::format "${package}"
 }
 
-# Checks that the current protoc version is at least version 3.0.0-beta1
+# Checks that the current protoc version matches the required version and
 # exit 1 if it's not the case
 function kube::protoc::check_protoc() {
-  if [[ -z "$(which protoc)" || "$(protoc --version)" != "libprotoc 3."* ]]; then
-    echo "Generating protobuf requires protoc 3.0.0-beta1 or newer. Please download and"
+  if [[ -z "$(which protoc)" || "$(protoc --version)" != "libprotoc 23.4"* ]]; then
+	  echo "Generating protobuf requires protoc 23.4 or newer. Please download and"
     echo "install the platform appropriate Protobuf package for your OS: "
     echo
     echo "  https://github.com/protocolbuffers/protobuf/releases"
     echo
     echo "WARNING: Protobuf changes are not being validated"
     exit 1
-  fi
+fi
 }
 
 # Generates $1/api.pb.go from the protobuf file $1/api.proto
