04de9736b36b86e54f0ebb12c96229f9981a830a
TommyStarK <thomasmilox@gmail.com>
2023-08-22 08:20:56 +0200

    dra: refactoring overall flow of prepare/unprepare resources
    Signed-off-by: TommyStarK <thomasmilox@gmail.com>

--- pkg/kubelet/cm/dra/plugin/client_test.go
+++ pkg/kubelet/cm/dra/plugin/client_test.go
@@ -18,31 +18,46 @@ package plugin
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"os"
 	"path/filepath"
 	"sync"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
-	drapbv1 "k8s.io/kubelet/pkg/apis/dra/v1alpha3"
+	drapbv1alpha2 "k8s.io/kubelet/pkg/apis/dra/v1alpha2"
+	drapbv1alpha3 "k8s.io/kubelet/pkg/apis/dra/v1alpha3"
 )
 
-type fakeGRPCServer struct {
-	drapbv1.UnimplementedNodeServer
+type fakeV1alpha3GRPCServer struct {
+	drapbv1alpha3.UnimplementedNodeServer
 }
 
-func (f *fakeGRPCServer) NodePrepareResource(ctx context.Context, in *drapbv1.NodePrepareResourcesRequest) (*drapbv1.NodePrepareResourcesResponse, error) {
-	return &drapbv1.NodePrepareResourcesResponse{Claims: map[string]*drapbv1.NodePrepareResourceResponse{"dummy": {CDIDevices: []string{"dummy"}}}}, nil
+func (f *fakeV1alpha3GRPCServer) NodePrepareResource(ctx context.Context, in *drapbv1alpha3.NodePrepareResourcesRequest) (*drapbv1alpha3.NodePrepareResourcesResponse, error) {
+	return &drapbv1alpha3.NodePrepareResourcesResponse{Claims: map[string]*drapbv1alpha3.NodePrepareResourceResponse{"dummy": {CDIDevices: []string{"dummy"}}}}, nil
 }
 
-func (f *fakeGRPCServer) NodeUnprepareResource(ctx context.Context, in *drapbv1.NodeUnprepareResourcesRequest) (*drapbv1.NodeUnprepareResourcesResponse, error) {
-	return &drapbv1.NodeUnprepareResourcesResponse{}, nil
+func (f *fakeV1alpha3GRPCServer) NodeUnprepareResource(ctx context.Context, in *drapbv1alpha3.NodeUnprepareResourcesRequest) (*drapbv1alpha3.NodeUnprepareResourcesResponse, error) {
+	return &drapbv1alpha3.NodeUnprepareResourcesResponse{}, nil
+}
+
+type fakeV1alpha2GRPCServer struct {
+	drapbv1alpha2.UnimplementedNodeServer
+}
+
+func (f *fakeV1alpha2GRPCServer) NodePrepareResource(ctx context.Context, in *drapbv1alpha2.NodePrepareResourceRequest) (*drapbv1alpha2.NodePrepareResourceResponse, error) {
+	return &drapbv1alpha2.NodePrepareResourceResponse{CdiDevices: []string{"dummy"}}, nil
+}
+
+func (f *fakeV1alpha2GRPCServer) NodeUnprepareResource(ctx context.Context, in *drapbv1alpha2.NodeUnprepareResourceRequest) (*drapbv1alpha2.NodeUnprepareResourceResponse, error) {
+	return &drapbv1alpha2.NodeUnprepareResourceResponse{}, nil
 }
 
 type tearDown func()
 
-func setupFakeGRPCServer() (string, tearDown, error) {
+func setupFakeGRPCServer(version string) (string, tearDown, error) {
 	p, err := os.MkdirTemp("", "dra_plugin")
 	if err != nil {
 		return "", nil, err
@@ -62,8 +77,16 @@ func setupFakeGRPCServer() (string, tearDown, error) {
 	}
 
 	s := grpc.NewServer()
-	fakeGRPCServer := &fakeGRPCServer{}
-	drapbv1.RegisterNodeServer(s, fakeGRPCServer)
+	switch version {
+	case v1alpha2Version:
+		fakeGRPCServer := &fakeV1alpha2GRPCServer{}
+		drapbv1alpha2.RegisterNodeServer(s, fakeGRPCServer)
+	case v1alpha3Version:
+		fakeGRPCServer := &fakeV1alpha3GRPCServer{}
+		drapbv1alpha3.RegisterNodeServer(s, fakeGRPCServer)
+	default:
+		return "", nil, fmt.Errorf("unsupported version: %s", version)
+	}
 
 	go func() {
 		go s.Serve(listener)
@@ -75,7 +98,7 @@ func setupFakeGRPCServer() (string, tearDown, error) {
 }
 
 func TestGRPCConnIsReused(t *testing.T) {
-	addr, teardown, err := setupFakeGRPCServer()
+	addr, teardown, err := setupFakeGRPCServer(v1alpha3Version)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -85,11 +108,12 @@ func TestGRPCConnIsReused(t *testing.T) {
 	wg := sync.WaitGroup{}
 	m := sync.Mutex{}
 
-	plugin := &Plugin{
+	p := &plugin{
 		endpoint: addr,
+		version:  v1alpha3Version,
 	}
 
-	conn, err := plugin.getOrCreateGRPCConn()
+	conn, err := p.getOrCreateGRPCConn()
 	defer func() {
 		err := conn.Close()
 		if err != nil {
@@ -101,7 +125,8 @@ func TestGRPCConnIsReused(t *testing.T) {
 	}
 
 	// ensure the plugin we are using is registered
-	draPlugins.Set("dummy-plugin", plugin)
+	draPlugins.add("dummy-plugin", p)
+	defer draPlugins.delete("dummy-plugin")
 
 	// we call `NodePrepareResource` 2 times and check whether a new connection is created or the same is reused
 	for i := 0; i < 2; i++ {
@@ -114,8 +139,8 @@ func TestGRPCConnIsReused(t *testing.T) {
 				return
 			}
 
-			req := &drapbv1.NodePrepareResourcesRequest{
-				Claims: []*drapbv1.Claim{
+			req := &drapbv1alpha3.NodePrepareResourcesRequest{
+				Claims: []*drapbv1alpha3.Claim{
 					{
 						Namespace:      "dummy-namespace",
 						Uid:            "dummy-uid",
@@ -126,9 +151,9 @@ func TestGRPCConnIsReused(t *testing.T) {
 			}
 			client.NodePrepareResources(context.TODO(), req)
 
-			client.(*draPluginClient).plugin.Lock()
-			conn := client.(*draPluginClient).plugin.conn
-			client.(*draPluginClient).plugin.Unlock()
+			client.(*plugin).Lock()
+			conn := client.(*plugin).conn
+			client.(*plugin).Unlock()
 
 			m.Lock()
 			defer m.Unlock()
@@ -144,6 +169,122 @@ func TestGRPCConnIsReused(t *testing.T) {
 	if counter, ok := reusedConns[conn]; ok && counter != 2 {
 		t.Errorf("expected counter to be 2 but got %d", counter)
 	}
+}
 
-	draPlugins.Delete("dummy-plugin")
+func TestNewDRAPluginClient(t *testing.T) {
+	for _, test := range []struct {
+		description string
+		setup       func(string) tearDown
+		pluginName  string
+		shouldError bool
+	}{
+		{
+			description: "plugin name is empty",
+			setup: func(_ string) tearDown {
+				return func() {}
+			},
+			pluginName:  "",
+			shouldError: true,
+		},
+		{
+			description: "plugin name not found in the list",
+			setup: func(_ string) tearDown {
+				return func() {}
+			},
+			pluginName:  "plugin-name-not-found-in-the-list",
+			shouldError: true,
+		},
+		{
+			description: "plugin exists",
+			setup: func(name string) tearDown {
+				draPlugins.add(name, &plugin{})
+				return func() {
+					draPlugins.delete(name)
+				}
+			},
+			pluginName: "dummy-plugin",
+		},
+	} {
+		t.Run(test.description, func(t *testing.T) {
+			teardown := test.setup(test.pluginName)
+			defer teardown()
+
+			client, err := NewDRAPluginClient(test.pluginName)
+			if test.shouldError {
+				assert.Nil(t, client)
+				assert.Error(t, err)
+			} else {
+				assert.NotNil(t, client)
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
+
+func TestNodeUnprepareResource(t *testing.T) {
+	for _, test := range []struct {
+		description   string
+		serverSetup   func(string) (string, tearDown, error)
+		serverVersion string
+		request       *drapbv1alpha3.NodeUnprepareResourcesRequest
+	}{
+		{
+			description:   "server supports v1alpha3",
+			serverSetup:   setupFakeGRPCServer,
+			serverVersion: v1alpha3Version,
+			request:       &drapbv1alpha3.NodeUnprepareResourcesRequest{},
+		},
+		{
+			description:   "server supports v1alpha2, plugin client should fallback",
+			serverSetup:   setupFakeGRPCServer,
+			serverVersion: v1alpha2Version,
+			request: &drapbv1alpha3.NodeUnprepareResourcesRequest{
+				Claims: []*drapbv1alpha3.Claim{
+					{
+						Namespace:      "dummy-namespace",
+						Uid:            "dummy-uid",
+						Name:           "dummy-claim",
+						ResourceHandle: "dummy-resource",
+					},
+				},
+			},
+		},
+	} {
+		t.Run(test.description, func(t *testing.T) {
+			addr, teardown, err := setupFakeGRPCServer(test.serverVersion)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer teardown()
+
+			p := &plugin{
+				endpoint: addr,
+				version:  v1alpha3Version,
+			}
+
+			conn, err := p.getOrCreateGRPCConn()
+			defer func() {
+				err := conn.Close()
+				if err != nil {
+					t.Error(err)
+				}
+			}()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			draPlugins.add("dummy-plugin", p)
+			defer draPlugins.delete("dummy-plugin")
+
+			client, err := NewDRAPluginClient("dummy-plugin")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			_, err = client.NodeUnprepareResources(context.TODO(), test.request)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
 }
