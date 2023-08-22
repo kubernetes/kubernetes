e9c2262b4e74dfb77fc8a49c490d008a42bd5f48
TommyStarK <thomasmilox@gmail.com>
2023-08-22 08:20:56 +0200

    dra: refactoring overall flow of prepare/unprepare resources
    Signed-off-by: TommyStarK <thomasmilox@gmail.com>

--- pkg/kubelet/cm/dra/plugin/plugins_store.go
+++ pkg/kubelet/cm/dra/plugin/plugins_store.go
@@ -17,69 +17,24 @@ limitations under the License.
 package plugin
 
 import (
-	"context"
-	"errors"
-	"net"
 	"sync"
-	"time"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/connectivity"
-	"google.golang.org/grpc/credentials/insecure"
-	utilversion "k8s.io/apimachinery/pkg/util/version"
 	"k8s.io/klog/v2"
 )
 
-// Plugin is a description of a DRA Plugin, defined by an endpoint
-// and the highest DRA version supported.
-type Plugin struct {
-	sync.RWMutex
-	conn                    *grpc.ClientConn
-	endpoint                string
-	highestSupportedVersion *utilversion.Version
-}
-
-func (p *Plugin) getOrCreateGRPCConn() (*grpc.ClientConn, error) {
-	p.Lock()
-	defer p.Unlock()
-
-	if p.conn != nil {
-		return p.conn, nil
-	}
-
-	network := "unix"
-	klog.V(4).InfoS(log("creating new gRPC connection"), "protocol", network, "endpoint", p.endpoint)
-	conn, err := grpc.Dial(
-		p.endpoint,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithContextDialer(func(ctx context.Context, target string) (net.Conn, error) {
-			return (&net.Dialer{}).DialContext(ctx, network, target)
-		}),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	if ok := conn.WaitForStateChange(ctx, connectivity.Connecting); !ok {
-		return nil, errors.New("timed out waiting for gRPC connection to be ready")
-	}
-
-	p.conn = conn
-	return p.conn, nil
-}
-
 // PluginsStore holds a list of DRA Plugins.
-type PluginsStore struct {
+type pluginsStore struct {
 	sync.RWMutex
-	store map[string]*Plugin
+	store map[string]*plugin
 }
+
+// draPlugins map keeps track of all registered DRA plugins on the node
+// and their corresponding sockets.
+var draPlugins = &pluginsStore{}
 
 // Get lets you retrieve a DRA Plugin by name.
 // This method is protected by a mutex.
-func (s *PluginsStore) Get(pluginName string) *Plugin {
+func (s *pluginsStore) get(pluginName string) *plugin {
 	s.RLock()
 	defer s.RUnlock()
 
@@ -88,31 +43,26 @@ func (s *PluginsStore) Get(pluginName string) *Plugin {
 
 // Set lets you save a DRA Plugin to the list and give it a specific name.
 // This method is protected by a mutex.
-func (s *PluginsStore) Set(pluginName string, plugin *Plugin) {
+func (s *pluginsStore) add(pluginName string, p *plugin) {
 	s.Lock()
 	defer s.Unlock()
 
 	if s.store == nil {
-		s.store = make(map[string]*Plugin)
+		s.store = make(map[string]*plugin)
 	}
 
-	s.store[pluginName] = plugin
+	_, exists := s.store[pluginName]
+	if exists {
+		klog.V(1).InfoS(log("plugin: %s already registered, previous plugin will be overridden", pluginName))
+	}
+	s.store[pluginName] = p
 }
 
 // Delete lets you delete a DRA Plugin by name.
 // This method is protected by a mutex.
-func (s *PluginsStore) Delete(pluginName string) {
+func (s *pluginsStore) delete(pluginName string) {
 	s.Lock()
 	defer s.Unlock()
 
 	delete(s.store, pluginName)
-}
-
-// Clear deletes all entries in the store.
-// This methiod is protected by a mutex.
-func (s *PluginsStore) Clear() {
-	s.Lock()
-	defer s.Unlock()
-
-	s.store = make(map[string]*Plugin)
 }
