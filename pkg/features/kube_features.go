191cc12bfae3f6508656ea223eeec08fe8235016
carlory <baofa.fan@daocloud.io>
2023-09-05 17:53:38 +0800

    promote PersistentVolumeLastPhaseTransitionTime to beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1124,7 +1124,7 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 
 	PDBUnhealthyPodEvictionPolicy: {Default: true, PreRelease: featuregate.Beta},
 
-	PersistentVolumeLastPhaseTransitionTime: {Default: false, PreRelease: featuregate.Alpha},
+	PersistentVolumeLastPhaseTransitionTime: {Default: true, PreRelease: featuregate.Beta},
 
 	PodAndContainerStatsFromCRI: {Default: false, PreRelease: featuregate.Alpha},
 
