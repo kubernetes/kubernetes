73ea9e41022d1b83ffce5f779cf7534de4f6f90d
Deepak Kinni <dkinni@vmware.com>
2023-09-10 19:29:33 -0700

    remove dynamicstorageclass for hostpath
    Signed-off-by: Deepak Kinni <dkinni@vmware.com>

--- test/e2e/storage/drivers/in_tree.go
+++ test/e2e/storage/drivers/in_tree.go
@@ -714,12 +714,12 @@ func (h *hostPathDriver) CreateVolume(ctx context.Context, config *storageframew
 	return nil
 }
 
-func (h *hostPathDriver) GetDynamicProvisionStorageClass(ctx context.Context, config *storageframework.PerTestConfig, fsType string) *storagev1.StorageClass {
+/*func (h *hostPathDriver) GetDynamicProvisionStorageClass(ctx context.Context, config *storageframework.PerTestConfig, fsType string) *storagev1.StorageClass {
 	provisioner := "kubernetes.io/host-path"
 	parameters := map[string]string{}
 	ns := config.Framework.Namespace.Name
 	return storageframework.GetStorageClass(provisioner, parameters, nil, ns)
-}
+}*/
 
 // HostPathSymlink
 type hostPathSymlinkDriver struct {
