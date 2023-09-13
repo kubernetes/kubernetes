b00e13e0985aad7a35b691b763432056619052f5
SUSHANTH THATIKONDA <sushanqt@amazon.com>
2023-09-13 21:17:48 +0000

    fixed format error

--- pkg/controller/cronjob/utils_test.go
+++ pkg/controller/cronjob/utils_test.go
@@ -358,14 +358,14 @@ func TestMostRecentScheduleTime(t *testing.T) {
 	tenSeconds := int64(10)
 
 	tests := []struct {
-		name                  string
-		cj                    *batchv1.CronJob
-		includeSDS            bool
-		now                   time.Time
-		expectedEarliestTime  time.Time
-		expectedRecentTime    *time.Time
+		name                   string
+		cj                     *batchv1.CronJob
+		includeSDS             bool
+		now                    time.Time
+		expectedEarliestTime   time.Time
+		expectedRecentTime     *time.Time
 		expectedNumberOfMisses int64
-		wantErr               bool
+		wantErr                bool
 	}{
 		{
 			name: "now before next schedule",
@@ -391,9 +391,9 @@ func TestMostRecentScheduleTime(t *testing.T) {
 					Schedule: "0 * * * *",
 				},
 			},
-			now:                  topOfTheHour().Add(61 * time.Minute),
-			expectedRecentTime:   deltaTimeAfterTopOfTheHour(60 * time.Minute),
-			expectedEarliestTime: *topOfTheHour(),
+			now:                    topOfTheHour().Add(61 * time.Minute),
+			expectedRecentTime:     deltaTimeAfterTopOfTheHour(60 * time.Minute),
+			expectedEarliestTime:   *topOfTheHour(),
 			expectedNumberOfMisses: 1,
 		},
 		{
@@ -406,9 +406,9 @@ func TestMostRecentScheduleTime(t *testing.T) {
 					Schedule: "0 * * * *",
 				},
 			},
-			now:                  *deltaTimeAfterTopOfTheHour(301 * time.Minute),
-			expectedRecentTime:   deltaTimeAfterTopOfTheHour(300 * time.Minute),
-			expectedEarliestTime: *deltaTimeAfterTopOfTheHour(10 * time.Second),
+			now:                    *deltaTimeAfterTopOfTheHour(301 * time.Minute),
+			expectedRecentTime:     deltaTimeAfterTopOfTheHour(300 * time.Minute),
+			expectedEarliestTime:   *deltaTimeAfterTopOfTheHour(10 * time.Second),
 			expectedNumberOfMisses: 5,
 		},
 		{
@@ -424,9 +424,9 @@ func TestMostRecentScheduleTime(t *testing.T) {
 					LastScheduleTime: &metav1HalfPastTheHour,
 				},
 			},
-			now:                  *deltaTimeAfterTopOfTheHour(24*time.Hour + 31*time.Minute),
-			expectedRecentTime:   deltaTimeAfterTopOfTheHour(24*time.Hour + 30*time.Minute),
-			expectedEarliestTime: *deltaTimeAfterTopOfTheHour(30 * time.Minute),
+			now:                    *deltaTimeAfterTopOfTheHour(24*time.Hour + 31*time.Minute),
+			expectedRecentTime:     deltaTimeAfterTopOfTheHour(24*time.Hour + 30*time.Minute),
+			expectedEarliestTime:   *deltaTimeAfterTopOfTheHour(30 * time.Minute),
 			expectedNumberOfMisses: 2,
 		},
 		{
@@ -442,9 +442,9 @@ func TestMostRecentScheduleTime(t *testing.T) {
 					LastScheduleTime: &metav1HalfPastTheHour,
 				},
 			},
-			now:                  *deltaTimeAfterTopOfTheHour(30*time.Hour + 30*time.Minute),
-			expectedRecentTime:   nil,
-			expectedEarliestTime: *deltaTimeAfterTopOfTheHour(30 * time.Minute),
+			now:                    *deltaTimeAfterTopOfTheHour(30*time.Hour + 30*time.Minute),
+			expectedRecentTime:     nil,
+			expectedEarliestTime:   *deltaTimeAfterTopOfTheHour(30 * time.Minute),
 			expectedNumberOfMisses: 30,
 		},
 		{
@@ -460,9 +460,9 @@ func TestMostRecentScheduleTime(t *testing.T) {
 					LastScheduleTime: &metav1HalfPastTheHour,
 				},
 			},
-			now:                  *deltaTimeAfterTopOfTheHour(96*time.Hour + 31*time.Minute),
-			expectedRecentTime:   deltaTimeAfterTopOfTheHour(96*time.Hour + 30*time.Minute),
-			expectedEarliestTime: *deltaTimeAfterTopOfTheHour(30 * time.Minute),
+			now:                    *deltaTimeAfterTopOfTheHour(96*time.Hour + 31*time.Minute),
+			expectedRecentTime:     deltaTimeAfterTopOfTheHour(96*time.Hour + 30*time.Minute),
+			expectedEarliestTime:   *deltaTimeAfterTopOfTheHour(30 * time.Minute),
 			expectedNumberOfMisses: 6,
 		},
 		{
@@ -475,9 +475,9 @@ func TestMostRecentScheduleTime(t *testing.T) {
 					Schedule: "30 6-16/4 * * 1-5",
 				},
 			},
-			now:                  *deltaTimeAfterTopOfTheHour(24*time.Hour + 31*time.Minute),
-			expectedRecentTime:   deltaTimeAfterTopOfTheHour(24*time.Hour + 30*time.Minute),
-			expectedEarliestTime: *topOfTheHour(),
+			now:                    *deltaTimeAfterTopOfTheHour(24*time.Hour + 31*time.Minute),
+			expectedRecentTime:     deltaTimeAfterTopOfTheHour(24*time.Hour + 30*time.Minute),
+			expectedEarliestTime:   *topOfTheHour(),
 			expectedNumberOfMisses: 7,
 		},
 		{
@@ -494,9 +494,9 @@ func TestMostRecentScheduleTime(t *testing.T) {
 					LastScheduleTime: &metav1MinuteAfterTopOfTheHour,
 				},
 			},
-			now:                   *deltaTimeAfterTopOfTheHour(7 * 24 * time.Hour),
-			expectedRecentTime:    deltaTimeAfterTopOfTheHour((6 * 24 * time.Hour) + 23*time.Hour + 1*time.Minute),
-			expectedEarliestTime:  *deltaTimeAfterTopOfTheHour(1 * time.Minute),
+			now:                    *deltaTimeAfterTopOfTheHour(7 * 24 * time.Hour),
+			expectedRecentTime:     deltaTimeAfterTopOfTheHour((6 * 24 * time.Hour) + 23*time.Hour + 1*time.Minute),
+			expectedEarliestTime:   *deltaTimeAfterTopOfTheHour(1 * time.Minute),
 			expectedNumberOfMisses: 167,
 		},
 		{
@@ -509,9 +509,9 @@ func TestMostRecentScheduleTime(t *testing.T) {
 					Schedule: "59 23 31 2 *",
 				},
 			},
-			now:                *deltaTimeAfterTopOfTheHour(1 * time.Hour),
-			expectedRecentTime: nil,
-			wantErr:            true,
+			now:                    *deltaTimeAfterTopOfTheHour(1 * time.Hour),
+			expectedRecentTime:     nil,
+			wantErr:                true,
 			expectedNumberOfMisses: 0,
 		},
 		{
