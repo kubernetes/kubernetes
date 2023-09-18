fc6b19a4ba669da7a55f0c061f465fdfe4510720
Madhav Jivrajani <madhav.jiv@gmail.com>
2023-09-18 22:15:49 +0530

    .: bump govmomi to v0.30.6
    Bumping govmomi to include an error check fix needed
    to work with go1.20. We made this fix in the CI, but
    were reliant on the text matching of error strings,
    which is why it didn't catch the actual issue. This
    
    Fix in https://github.com/vmware/govmomi/commit/b4eac19369bc7421bc337554fd8298f2301dce99
    PR to bump govmomi in cloud-provider-vsphere: https://github.com/kubernetes/cloud-provider-vsphere/pull/738
    
    Signed-off-by: Madhav Jivrajani <madhav.jiv@gmail.com>

--- vendor/github.com/vmware/govmomi/list/lister.go
+++ vendor/github.com/vmware/govmomi/list/lister.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014-2016 VMware, Inc. All Rights Reserved.
+Copyright (c) 2014-2023 VMware, Inc. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -312,6 +312,7 @@ func (l Lister) ListComputeResource(ctx context.Context) ([]Element, error) {
 
 	fields := []string{
 		"host",
+		"network",
 		"resourcePool",
 	}
 
@@ -327,6 +328,7 @@ func (l Lister) ListComputeResource(ctx context.Context) ([]Element, error) {
 
 	childTypes := []string{
 		"HostSystem",
+		"Network",
 		"ResourcePool",
 	}
 
