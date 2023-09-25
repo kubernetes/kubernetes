2a42fe2c9191cebc326f83101faf2f6fdb8da775
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2023-09-25 23:42:31 +0200

    UPSTREAM: <carry>: controlplane/apiserver: disable protobuf for loopback
    Co-authored-by: Marvin Beckers <marvin@kubermatic.com>
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- pkg/controlplane/apiserver/config.go
+++ pkg/controlplane/apiserver/config.go
@@ -137,11 +137,6 @@ func BuildGenericConfig(
 		return
 	}
 
-	// Use protobufs for self-communication.
-	// Since not every generic apiserver has to support protobufs, we
-	// cannot default to it in generic apiserver and need to explicitly
-	// set it in kube-apiserver.
-	genericConfig.LoopbackClientConfig.ContentConfig.ContentType = "application/vnd.kubernetes.protobuf"
 	// Disable compression for self-communication, since we are going to be
 	// on a fast local network
 	genericConfig.LoopbackClientConfig.DisableCompression = true
