9134693bebe38850dbdcaa08e5bcaf2e6a07b9a9
Itamar Holder <iholder@redhat.com>
2023-10-01 17:52:16 +0300

    Add documentation to DefaultMutableFeatureGate regarding use in tests
    The documentation clarifies that this variable can be used only for
    feature gates that are being enabled at start test time, and not
    for feature gates that are being enabled during the test.
    
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- staging/src/k8s.io/apiserver/pkg/util/feature/feature_gate.go
+++ staging/src/k8s.io/apiserver/pkg/util/feature/feature_gate.go
@@ -25,6 +25,9 @@ var (
 	// Only top-level commands/options setup and the k8s.io/component-base/featuregate/testing package should make use of this.
 	// Tests that need to modify feature gates for the duration of their test should use:
 	//   defer featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.<FeatureName>, <value>)()
+	//
+	// When used in tests, it must be used for feature gates that are enabled at test start time.
+	// It will not work if the feature gate is being enabled during the test.
 	DefaultMutableFeatureGate featuregate.MutableFeatureGate = featuregate.NewFeatureGate()
 
 	// DefaultFeatureGate is a shared global FeatureGate.
