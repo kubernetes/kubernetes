47b82889cbcbd6e25090c497df569df2a6cc4970
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2023-10-04 21:12:12 +0200

    wip
    Co-authored-by: deads <deads@redhat.com>

---
+++ staging/src/k8s.io/client-go/features/testing/kube_features_init_test.go
@@ -0,0 +1,50 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package testing
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"k8s.io/client-go/features"
+	"k8s.io/component-base/featuregate"
+)
+
+func TestDriveInitDefaultFeatureGates(t *testing.T) {
+	defaultFeatureGates := features.DefaultFeatureGates()
+	require.Panics(t, func() { defaultFeatureGates.Enabled("FakeFeatureGate") })
+
+	featureGate := &fakeFeatureGate{}
+	require.True(t, featureGate.Enabled("FakeFeatureGate"))
+
+	features.SetFeatureGates(featureGate)
+	defaultFeatureGates = features.DefaultFeatureGates()
+	require.True(t, defaultFeatureGates.Enabled("FakeFeatureGate"))
+}
+
+var _ featuregate.FeatureGate = &fakeFeatureGate{}
+
+type fakeFeatureGate struct{}
+
+func (f *fakeFeatureGate) Enabled(key featuregate.Feature) bool {
+	return key == "FakeFeatureGate"
+}
+
+func (f *fakeFeatureGate) KnownFeatures() []string {
+	return []string{"CoolFeature"}
+}
+func (f *fakeFeatureGate) DeepCopy() featuregate.MutableFeatureGate {
+	return nil
+}
