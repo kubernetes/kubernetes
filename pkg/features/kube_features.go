2e8c3b0e2349c84b392f5ff73e7c8ae7b48d1b70
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2023-10-06 14:51:00 +0200

    add clientgo featuregates to kube and replace the implementation

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -21,6 +21,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/runtime"
 	genericfeatures "k8s.io/apiserver/pkg/features"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
+	clientgofeaturegate "k8s.io/client-go/features"
 	"k8s.io/component-base/featuregate"
 )
 
@@ -956,6 +957,9 @@ const (
 
 func init() {
 	runtime.Must(utilfeature.DefaultMutableFeatureGate.Add(defaultKubernetesFeatureGates))
+	runtime.Must(clientgofeaturegate.AddFeaturesToExistingFeatureGates(utilfeature.DefaultMutableFeatureGate))
+	// replace client-go's feature gate implementation with kube's
+	clientgofeaturegate.SetFeatureGates(utilfeature.DefaultMutableFeatureGate)
 }
 
 // defaultKubernetesFeatureGates consists of all known Kubernetes-specific feature keys.
