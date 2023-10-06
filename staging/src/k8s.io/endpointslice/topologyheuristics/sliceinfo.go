a69dc6cd5fffb083e010e844a162389d0b2adeee
Gaurav Ghildiyal <gauravkg@google.com>
2023-10-05 22:09:16 -0700

    Rename topologycache packge to topologyheuristics

--- staging/src/k8s.io/endpointslice/topologyheuristics/sliceinfo.go
+++ staging/src/k8s.io/endpointslice/topologyheuristics/sliceinfo.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package topologycache
+package topologyheuristics
 
 import (
 	discovery "k8s.io/api/discovery/v1"
