4c54e286357798acb80294546030c285f793f4f0
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2023-10-08 10:53:44 +0200

    UPSTREAM: <carry>: go run ./hack/kcp/resource_quota_controller_patch.go > pkg/controller/resourcequota/resource_quota_controller_patch.go
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>

---
+++ pkg/controller/resourcequota/resource_quota_controller_patch.go
@@ -0,0 +1,111 @@
+/*
+Copyright 2022 The KCP Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resourcequota
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/klog/v2"
+)
+
+// UpdateMonitors determines if there are any newly available or removed API resources, and if so, starts/stops monitors
+// for them. This is similar to Sync, but instead of polling discovery every 30 seconds, this method is invoked by kcp
+// whenever the set of APIs is known to change (CRDs added or removed).
+func (rq *Controller) UpdateMonitors(ctx context.Context, discoveryFunc NamespacedResourcesFunc, period time.Duration) {
+	logger := klog.FromContext(ctx)
+
+	// Something has changed, so track the new state and perform a sync.
+	oldResources := make(map[schema.GroupVersionResource]struct{})
+	wait.UntilWithContext(ctx, func(ctx context.Context) {
+		// Get the current resource list from discovery.
+		newResources, err := GetQuotableResources(discoveryFunc)
+		if err != nil {
+			utilruntime.HandleError(err)
+
+			if discovery.IsGroupDiscoveryFailedError(err) && len(newResources) > 0 {
+				// In partial discovery cases, don't remove any existing informers, just add new ones
+				for k, v := range oldResources {
+					newResources[k] = v
+				}
+			} else {
+				// short circuit in non-discovery error cases or if discovery returned zero resources
+				return
+			}
+		}
+
+		// Decide whether discovery has reported a change.
+		if reflect.DeepEqual(oldResources, newResources) {
+			logger.V(4).Info("no resource updates from discovery, skipping resource quota sync")
+			return
+		}
+
+		// Ensure workers are paused to avoid processing events before informers
+		// have resynced.
+		rq.workerLock.Lock()
+		defer rq.workerLock.Unlock()
+
+		// Something has changed, so track the new state and perform a sync.
+		if loggerV := logger.V(2); loggerV.Enabled() {
+			loggerV.Info("syncing resource quota controller with updated resources from discovery", "diff", printDiff(oldResources, newResources))
+		}
+
+		// Perform the monitor resync and wait for controllers to report cache sync.
+		if err := rq.resyncMonitors(ctx, newResources); err != nil {
+			utilruntime.HandleError(fmt.Errorf("failed to sync resource monitors: %v", err))
+			return
+		}
+		// wait for caches to fill for a while (our sync period).
+		// this protects us from deadlocks where available resources changed and one of our informer caches will never fill.
+		// informers keep attempting to sync in the background, so retrying doesn't interrupt them.
+		// the call to resyncMonitors on the reattempt will no-op for resources that still exist.
+		if rq.quotaMonitor != nil &&
+			!cache.WaitForNamedCacheSync(
+				"resource quota",
+				waitForStopOrTimeout(ctx.Done(), period),
+				func() bool { return rq.quotaMonitor.IsSynced(ctx) },
+			) {
+			utilruntime.HandleError(fmt.Errorf("timed out waiting for quota monitor sync"))
+			return
+		}
+
+		// success, remember newly synced resources
+		oldResources = newResources
+		logger.V(2).Info("synced quota controller")
+	}, period)
+
+	// List all the quotas (this is scoped to the workspace)
+	quotas, err := rq.rqLister.List(labels.Everything())
+	if err != nil {
+		utilruntime.HandleError(fmt.Errorf("error listing all resourcequotas: %w", err))
+	}
+
+	// Requeue all quotas in the workspace
+	for i := range quotas {
+		quota := quotas[i]
+		logger.V(2).Info("enqueuing resourcequota %s/%s because the list of available APIs changed", quota.Namespace, quota.Name)
+		rq.addQuota(logger, quota)
+	}
+}
