d60ba3bdd246c590a1a50c9db7145ee0a8e0807d
Dan Winship <danwinship@redhat.com>
2023-10-30 14:44:29 -0400

    Add k8s.io/apimachinery/pkg/util/ip with net/netip conversion functions
    We will need to deal with both net.IP/net.IPNet and
    netip.Addr/netip.Prefix for a while, so add functions to convert
    between the types.

---
+++ staging/src/k8s.io/apimachinery/pkg/util/ip/convert.go
@@ -0,0 +1,113 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ip
+
+import (
+	"net"
+	"net/netip"
+)
+
+// AddrFromIP converts a net.IP to a netip.Addr. Given a valid input this will always
+// succeed; it will return the zero netip.Addr on nil or garbage input.
+//
+// Use this rather than netip.AddrFromSlice(), which does not correctly handle net.IP's
+// 16-byte encoding of IPv4 addresses.
+func AddrFromIP(ip net.IP) netip.Addr {
+	// The net.IP and netip.Addr parsers/stringers both round-trip correctly with
+	// respect to themselves:
+	//
+	//     net.ParseIP("1.2.3.4").String() => "1.2.3.4"
+	//     netip.MustParseAddr("1.2.3.4").String() => "1.2.3.4"
+	//
+	// But if you parse an IPv4 string as a net.IP and naively convert it to
+	// netip.Addr, you get something that netip.Addr considers to be an IPv6 address:
+	//
+	//     netip.AddrFromSlice(net.ParseIP("1.2.3.4")).String() => "::ffff:1.2.3.4"
+	//     netip.AddrFromSlice(net.ParseIP("1.2.3.4")).Is4() => false
+	//     netip.AddrFromSlice(net.ParseIP("1.2.3.4")).Is6() => true
+	//
+	// To get the desired results, you have to convert IPv4-mapped IPv6 addresses to
+	// 4-byte IPv4 addresses first.
+	if ip4 := ip.To4(); ip4 != nil {
+		ip = ip4
+	}
+	addr, _ := netip.AddrFromSlice(ip)
+	return addr
+}
+
+// IPFromAddr converts a netip.Addr to a net.IP. Given a valid input this will always
+// succeed; it will return nil if addr is the zero netip.Addr.
+func IPFromAddr(addr netip.Addr) net.IP {
+	// addr.AsSlice() returns:
+	//   - a []byte of length 4 if addr is an IPv4 address
+	//   - a []byte of length 16 if addr is an IPv6 address
+	//   - nil if addr is the zero Addr (which is the only other possibility)
+	//
+	// Any of those values can be correctly cast directly to a net.IP.
+	return net.IP(addr.AsSlice())
+}
+
+// PrefixFromIPNet converts a *net.IPNet to a netip.Prefix. Given a valid input this will
+// always succeed; it will return the zero netip.Prefix on nil or garbage input.
+func PrefixFromIPNet(ipnet *net.IPNet) netip.Prefix {
+	if ipnet == nil {
+		return netip.Prefix{}
+	}
+
+	addr := AddrFromIP(ipnet.IP)
+	if !addr.IsValid() {
+		return netip.Prefix{}
+	}
+
+	prefixLen, bits := ipnet.Mask.Size()
+	if prefixLen == 0 && bits == 0 {
+		// non-CIDR Mask representation
+		return netip.Prefix{}
+	}
+	if bits == 128 && addr.Is4() && (bits-prefixLen <= 32) {
+		// Given an IPv4 IP and a 128-bit mask whose top bits are all "1",
+		// net.IPNet/net.IP uses the lower bits as a 32-bit mask.
+		prefixLen -= 128 - 32
+	} else if bits != addr.BitLen() {
+		// invalid IPv4/IPv6 mix
+		return netip.Prefix{}
+	}
+
+	return netip.PrefixFrom(addr, prefixLen)
+}
+
+// IPNetFromPrefix converts a netip.Prefix to a *net.IPNet. Given a valid input this will
+// always succeed; it will return nil if prefix is the zero netip.Prefix.
+func IPNetFromPrefix(prefix netip.Prefix) *net.IPNet {
+	addr := prefix.Addr()
+	bits := prefix.Bits()
+
+	// netip.Prefix allows you to construct a prefix using an IPv4-mapped IPv6
+	// address, but it has broken semantics. (It requires the prefix length to be
+	// between 0 and 32, as though it was an IPv4 CIDR, but if you call .Mask() on it,
+	// it applies the prefix length as though it was an IPv6 CIDR, meaning you always
+	// get back `::` regardless of the input, since the top 32 bits of an IPv4-mapped
+	// IPv6 address are always 0.) So we just treat that as invalid.
+	if bits == -1 || !addr.IsValid() || addr.Is4In6() {
+		return nil
+	}
+
+	return &net.IPNet{
+		IP:   IPFromAddr(addr),
+		Mask: net.CIDRMask(bits, addr.BitLen()),
+	}
+}
---
+++ staging/src/k8s.io/apimachinery/pkg/util/ip/convert_test.go
@@ -0,0 +1,279 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ip
+
+import (
+	"net"
+	"net/netip"
+	"reflect"
+	"testing"
+)
+
+func TestAddrFromIP_IPFromAddr(t *testing.T) {
+	testCases := []struct {
+		desc string
+		ip   net.IP
+		addr netip.Addr
+	}{
+		{
+			desc: "IPv4 all-zeros",
+			ip:   net.IPv4zero,
+			addr: netip.IPv4Unspecified(),
+		},
+		{
+			desc: "IPv6 all-zeros",
+			ip:   net.IPv6zero,
+			addr: netip.IPv6Unspecified(),
+		},
+		{
+			desc: "IPv4 broadcast",
+			ip:   net.IPv4bcast,
+			addr: netip.AddrFrom4([4]byte{0xFF, 0xFF, 0xFF, 0xFF}),
+		},
+		{
+			desc: "IPv4 loopback",
+			ip:   net.IPv4(127, 0, 0, 1),
+			addr: netip.AddrFrom4([4]byte{127, 0, 0, 1}),
+		},
+		{
+			desc: "IPv6 loopback",
+			ip:   net.IPv6loopback,
+			addr: netip.IPv6Loopback(),
+		},
+		{
+			desc: "IPv4 1",
+			ip:   net.IPv4(10, 20, 40, 40),
+			addr: netip.AddrFrom4([4]byte{10, 20, 40, 40}),
+		},
+		{
+			desc: "IPv4 2",
+			ip:   net.IPv4(172, 17, 3, 0),
+			addr: netip.AddrFrom4([4]byte{172, 17, 3, 0}),
+		},
+		{
+			desc: "IPv6 1",
+			ip:   net.IP{0xFD, 0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x60, 0x0D, 0xF0, 0x0D},
+			addr: netip.AddrFrom16([16]byte{0xFD, 0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x60, 0x0D, 0xF0, 0x0D}),
+		},
+		{
+			desc: "IPv6 2",
+			ip:   net.IP{0x20, 0x01, 0x0D, 0xB8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x05},
+			addr: netip.AddrFrom16([16]byte{0x20, 0x01, 0x0D, 0xB8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x05}),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			addr := AddrFromIP(tc.ip)
+			if addr != tc.addr {
+				t.Errorf("AddrFromIP() expected %#v (%s) got %#v (%s)", tc.addr, tc.addr.String(), addr, addr.String())
+			}
+
+			ip := IPFromAddr(tc.addr)
+			if !ip.Equal(tc.ip) {
+				t.Errorf("IPFromAddr() expected %#v (%s) got %#v (%s)", tc.ip, tc.ip.String(), ip, ip.String())
+			}
+		})
+	}
+
+	// Special cases
+	var ip, expectedIP net.IP
+	var addr, expectedAddr netip.Addr
+
+	// IPv4-mapped IPv6 gets converted to plain IPv4, in either direction
+	ip = net.IP{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 1, 2, 3, 4}
+	addr = AddrFromIP(ip)
+	expectedAddr = netip.AddrFrom4([4]byte{1, 2, 3, 4})
+	if addr != expectedAddr {
+		t.Errorf("AddrFromIP(::ffff:1.2.3.4) expected %#v (%s) got %#v (%s)", expectedAddr, expectedAddr.String(), addr, addr.String())
+	}
+
+	addr = netip.AddrFrom16([16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 1, 2, 3, 4})
+	ip = IPFromAddr(addr)
+	expectedIP = net.IP{1, 2, 3, 4}
+	if !ip.Equal(expectedIP) {
+		t.Errorf("IPFromAddr(::ffff:1.2.3.4) expected %#v (%s) got %#v (%s)", expectedIP, expectedIP.String(), ip, ip.String())
+	}
+
+	// nil IP
+	ip = nil
+	addr = AddrFromIP(ip)
+	expectedAddr = netip.Addr{}
+	if addr != expectedAddr {
+		t.Errorf("AddrFromIP(%s) expected %#v (%s) got %#v (%s)", ip.String(), expectedAddr, expectedAddr.String(), addr, addr.String())
+	}
+	ip = IPFromAddr(expectedAddr)
+	if ip != nil {
+		t.Errorf("IPFromAddr(%s) expected nil got %#v (%s)", expectedAddr.String(), ip, ip.String())
+	}
+
+	// invalid IP
+	ip = net.IP{0x1}
+	addr = AddrFromIP(ip)
+	expectedAddr = netip.Addr{}
+	if addr != expectedAddr {
+		t.Errorf("AddrFromIP(%s) expected %#v (%s) got %#v (%s)", ip.String(), expectedAddr, expectedAddr.String(), addr, addr.String())
+	}
+	ip = IPFromAddr(expectedAddr)
+	if ip != nil {
+		t.Errorf("IPFromAddr(%s) expected nil got %#v (%s)", expectedAddr.String(), ip, ip.String())
+	}
+}
+
+func TestPrefixFromIPNet_IPNetFromPrefix(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		ipnet  *net.IPNet
+		prefix netip.Prefix
+	}{
+		{
+			desc: "IPv4 CIDR 1",
+			ipnet: &net.IPNet{
+				IP:   net.IPv4(10, 0, 0, 0),
+				Mask: net.CIDRMask(8, 32),
+			},
+			prefix: netip.PrefixFrom(
+				netip.AddrFrom4([4]byte{10, 0, 0, 0}),
+				8,
+			),
+		},
+		{
+			desc: "IPv4 CIDR 2",
+			ipnet: &net.IPNet{
+				IP:   net.IPv4(192, 168, 0, 0),
+				Mask: net.CIDRMask(16, 32),
+			},
+			prefix: netip.PrefixFrom(
+				netip.AddrFrom4([4]byte{192, 168, 0, 0}),
+				16,
+			),
+		},
+		{
+			desc: "IPv6 CIDR 1",
+			ipnet: &net.IPNet{
+				IP:   net.IPv6zero,
+				Mask: net.CIDRMask(1, 128),
+			},
+			prefix: netip.PrefixFrom(
+				netip.IPv6Unspecified(),
+				1,
+			),
+		},
+		{
+			desc: "IPv6 CIDR 2",
+			ipnet: &net.IPNet{
+				IP:   net.IP{0x20, 0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				Mask: net.CIDRMask(10, 128),
+			},
+			prefix: netip.PrefixFrom(
+				netip.AddrFrom16([16]byte{0x20, 0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}),
+				10,
+			),
+		},
+		{
+			desc: "IPv6 CIDR 3",
+			ipnet: &net.IPNet{
+				IP:   net.IP{0x20, 0x01, 0x0D, 0xB8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				Mask: net.CIDRMask(32, 128),
+			},
+			prefix: netip.PrefixFrom(
+				netip.AddrFrom16([16]byte{0x20, 0x01, 0x0D, 0xB8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}),
+				32,
+			),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			prefix := PrefixFromIPNet(tc.ipnet)
+			if prefix != tc.prefix {
+				t.Errorf("PrefixFromIPNet() expected %#v (%s) got %#v (%s)", tc.prefix, tc.prefix.String(), prefix, prefix.String())
+			}
+
+			ipnet := IPNetFromPrefix(tc.prefix)
+			if !ipnet.IP.Equal(tc.ipnet.IP) || !reflect.DeepEqual(ipnet.Mask, tc.ipnet.Mask) {
+				t.Errorf("IPNetFromPrefix() expected %#v (%s) got %#v (%s)", tc.ipnet, tc.ipnet.String(), ipnet, ipnet.String())
+			}
+		})
+	}
+
+	// Special cases
+	var ipnet *net.IPNet
+	var prefix, expectedPrefix netip.Prefix
+
+	// If you call net.ParseCIDR("1.2.3.0/24") you'd get a 4-byte IP and a 4-byte Mask
+	// like in "IPv4 CIDR 1" above. But if you construct an IPNet by hand with a
+	// parsed IPv4 IP:
+	//
+	//    &net.IPNet{IP: net.ParseIP("1.2.3.0"), Mask: net.CIDRMask(24, 32)}
+	//
+	// you'd get a 16-byte IP and a 4-byte Mask). However, IPNet.String() and
+	// IP.Mask() treat this the same as the 4-byte case, so we do too.
+	ipnet = &net.IPNet{
+		IP:   net.IP{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 1, 2, 3, 0},
+		Mask: net.CIDRMask(24, 32),
+	}
+	prefix = PrefixFromIPNet(ipnet)
+	expectedPrefix = netip.PrefixFrom(netip.AddrFrom4([4]byte{1, 2, 3, 0}), 24)
+	if prefix != expectedPrefix {
+		t.Errorf("PrefixFromIPNet(IPv4-mapped IPv6) expected %#v (%s) got %#v (%s)", expectedPrefix, expectedPrefix.String(), prefix, prefix.String())
+	}
+
+	// Similarly, if you call net.ParseCIDR("::ffff:1.2.3.0/120"), you get a 16-byte
+	// IP and a 16-byte mask, but again it still behaves the same as in the 4-byte
+	// case. (There is no reason anyone *should* do this, but we support the
+	// conversion for backward-compatibility.)
+	ipnet = &net.IPNet{
+		IP:   net.IP{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 1, 2, 3, 0},
+		Mask: net.CIDRMask(120, 128),
+	}
+	prefix = PrefixFromIPNet(ipnet)
+	if prefix != expectedPrefix {
+		t.Errorf("PrefixFromIPNet(IPv4-mapped IPv6) expected %#v (%s) got %#v (%s)", expectedPrefix, expectedPrefix.String(), prefix, prefix.String())
+	}
+
+	// OTOH, there's no good reason to try to support the conversion in the other
+	// direction, since, as noted in convert.go, netip.Prefix is basically broken in
+	// this case.
+	prefix = netip.PrefixFrom(
+		netip.AddrFrom16([16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 1, 2, 3, 0}),
+		120,
+	)
+	ipnet = IPNetFromPrefix(prefix)
+	if ipnet != nil {
+		t.Errorf("IPNetFromPrefix(IPv4-mapped IPv6) expected nil got %#v (%s)", ipnet, ipnet.String())
+	}
+
+	// nil IPNet
+	ipnet = nil
+	prefix = PrefixFromIPNet(ipnet)
+	expectedPrefix = netip.Prefix{}
+	if prefix != expectedPrefix {
+		t.Errorf("PrefixFromIPNet(nil) expected %#v (%s) got %#v (%s)", expectedPrefix, expectedPrefix.String(), prefix, prefix.String())
+	}
+	ipnet = IPNetFromPrefix(expectedPrefix)
+	if ipnet != nil {
+		t.Errorf("IPNetFromPrefix(zero) expected nil got %#v (%s)", ipnet, ipnet.String())
+	}
+
+	// invalid IPNet
+	ipnet = &net.IPNet{IP: net.IP{0x1}}
+	prefix = PrefixFromIPNet(ipnet)
+	expectedPrefix = netip.Prefix{}
+	if prefix != expectedPrefix {
+		t.Errorf("PrefixFromIPNet(invalid) expected %#v (%s) got %#v (%s)", expectedPrefix, expectedPrefix.String(), prefix, prefix.String())
+	}
+}
