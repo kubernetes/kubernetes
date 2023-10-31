ac2b9f8536400ac63ca2cc9abb60b4cb20e80142
carlory <baofa.fan@daocloud.io>
2023-10-31 17:30:07 +0800

    fix

--- test/e2e_node/image_pull_test.go
+++ test/e2e_node/image_pull_test.go
@@ -23,113 +23,15 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	kubeletconfig "k8s.io/kubernetes/pkg/kubelet/apis/config"
 	kubeletevents "k8s.io/kubernetes/pkg/kubelet/events"
 	"k8s.io/kubernetes/test/e2e/framework"
 	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
 	imageutils "k8s.io/kubernetes/test/utils/image"
 	admissionapi "k8s.io/pod-security-admission/api"
-	"k8s.io/utils/ptr"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/pkg/errors"
 )
-
-var _ = SIGDescribe("Pull Image [Serial] [NodeFeature:MaxParallelImagePull]", func() {
-	var pod, pod2 *v1.Pod
-
-	f := framework.NewDefaultFramework("pull-image-test")
-	f.NamespacePodSecurityLevel = admissionapi.LevelPrivileged
-
-	nginxImage := imageutils.GetE2EImage(imageutils.Nginx)
-	nginxNewImage := imageutils.GetE2EImage(imageutils.NginxNew)
-	nginxPodDesc := &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "nginx",
-			Namespace: f.Namespace.Name,
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{{
-				Name:            "nginx",
-				Image:           nginxImage,
-				ImagePullPolicy: v1.PullAlways,
-				Command:         []string{"sh"},
-			}},
-			RestartPolicy: v1.RestartPolicyNever,
-		},
-	}
-	nginxNewPodDesc := &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "nginx",
-			Namespace: f.Namespace.Name,
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{{
-				Name:            "nginx-new",
-				Image:           nginxNewImage,
-				ImagePullPolicy: v1.PullAlways,
-				Command:         []string{"sh"},
-			}},
-			RestartPolicy: v1.RestartPolicyNever,
-		},
-	}
-
-	ginkgo.Context("parallel image pull with MaxParallelImagePulls=5", func() {
-
-		tempSetCurrentKubeletConfig(f, func(ctx context.Context, initialConfig *kubeletconfig.KubeletConfiguration) {
-			initialConfig.SerializeImagePulls = false
-			initialConfig.MaxParallelImagePulls = ptr.To[int32](5)
-		})
-
-		ginkgo.BeforeEach(func(ctx context.Context) {
-			ginkgo.By("cleanup images")
-			RemoveImage(nginxPodDesc.Spec.Containers[0].Image)
-			RemoveImage(nginxNewPodDesc.Spec.Containers[0].Image)
-		})
-
-		ginkgo.It("should pull immediately if no more than 5 pods", func(ctx context.Context) {
-			node := getNodeName(ctx, f)
-			nginxPodDesc.Spec.NodeName = node
-			nginxNewPodDesc.Spec.NodeName = node
-			pod = e2epod.NewPodClient(f).Create(ctx, nginxPodDesc)
-			pod2 = e2epod.NewPodClient(f).Create(ctx, nginxNewPodDesc)
-			framework.ExpectNoError(e2epod.WaitTimeoutForPodNoLongerRunningInNamespace(ctx,
-				f.ClientSet, pod.Name, f.Namespace.Name, framework.PodStartTimeout))
-			framework.ExpectNoError(e2epod.WaitTimeoutForPodNoLongerRunningInNamespace(ctx,
-				f.ClientSet, pod2.Name, f.Namespace.Name, framework.PodStartTimeout))
-
-			events, err := f.ClientSet.CoreV1().Events(f.Namespace.Name).List(ctx, metav1.ListOptions{})
-			framework.ExpectNoError(err)
-			var nginxPulled, nginxNewPulled pulledStruct
-			for _, event := range events.Items {
-				var err error
-				if event.Reason == kubeletevents.PulledImage {
-					if event.InvolvedObject.Name == nginxPodDesc.Name {
-						nginxPulled, err = getDurationsFromPulledEventMsg(event.Message)
-						framework.ExpectNoError(err)
-					} else if event.InvolvedObject.Name == nginxNewPodDesc.Name {
-						nginxNewPulled, err = getDurationsFromPulledEventMsg(event.Message)
-						framework.ExpectNoError(err)
-					}
-				}
-			}
-			deletePodSyncByName(ctx, f, pod.Name)
-			deletePodSyncByName(ctx, f, pod2.Name)
-
-			// as this is parallel image pulling, the waiting duration should be similar with the pulled duration.
-			// use 1.2 as a common ratio
-			if float32(nginxNewPulled.pulledIncludeWaitingDuration/time.Millisecond)/float32(nginxNewPulled.pulledDuration/time.Millisecond) > 1.2 {
-				framework.Failf("the pull duration including waiting %v should be similar with the pulled duration %v",
-					nginxNewPulled.pulledIncludeWaitingDuration, nginxNewPulled.pulledDuration)
-			}
-			if float32(nginxPulled.pulledIncludeWaitingDuration/time.Millisecond)/float32(nginxPulled.pulledDuration/time.Millisecond) > 1.2 {
-				framework.Failf("the pull duration including waiting %v should be similar with the pulled duration %v",
-					nginxPulled.pulledIncludeWaitingDuration, nginxPulled.pulledDuration)
-			}
-		})
-
-	})
-})
 
 var _ = SIGDescribe("Pull Image [Serial]", func() {
 
@@ -179,7 +81,7 @@ var _ = SIGDescribe("Pull Image [Serial]", func() {
 		// })
 
 		ginkgo.BeforeEach(func(ctx context.Context) {
-			ginkgo.By("cleanup images")
+			// ginkgo.By("cleanup images")
 			RemoveImage(nginxPodDesc.Spec.Containers[0].Image)
 			RemoveImage(nginxNewPodDesc.Spec.Containers[0].Image)
 		})
