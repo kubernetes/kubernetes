c6d70610b48ebd3a531575e0b1e7834e64243034
lowang-bh <lhui_wang@163.com>
2023-11-25 21:01:41 +0800

    show pod Status as phase Failed or Succeeded if it has deletionTimestamp
    Signed-off-by: lowang-bh <lhui_wang@163.com>

--- pkg/printers/internalversion/printers.go
+++ pkg/printers/internalversion/printers.go
@@ -830,7 +830,8 @@ func printPod(pod *api.Pod, options printers.GenerateOptions) ([]metav1.TableRow
 	lastRestartDate := metav1.NewTime(time.Time{})
 	lastRestartableInitContainerRestartDate := metav1.NewTime(time.Time{})
 
-	reason := string(pod.Status.Phase)
+	podPhase := pod.Status.Phase
+	reason := string(podPhase)
 	if pod.Status.Reason != "" {
 		reason = pod.Status.Reason
 	}
@@ -955,6 +956,9 @@ func printPod(pod *api.Pod, options printers.GenerateOptions) ([]metav1.TableRow
 		reason = "Unknown"
 	} else if pod.DeletionTimestamp != nil {
 		reason = "Terminating"
+		if podPhase == api.PodSucceeded || podPhase == api.PodFailed {
+			reason = string(podPhase)
+		}
 	}
 
 	restartsStr := strconv.Itoa(restarts)
