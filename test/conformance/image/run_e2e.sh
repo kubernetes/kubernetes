719844e196a56bfe4836f7117c9a35c90b13e245
rjsadow <richard.j.sadowski@gmail.com>
2023-12-23 05:50:16 -0500

    Fix dry-run flag in go-runner and run_e2e.sh scripts
    Signed-off-by: rjsadow <richard.j.sadowski@gmail.com>

--- test/conformance/image/run_e2e.sh
+++ test/conformance/image/run_e2e.sh
@@ -49,7 +49,7 @@ trap shutdown TERM
 
 ginkgo_args=()
 if [[ -n ${E2E_DRYRUN:-} ]]; then
-    ginkgo_args+=("--dryRun=true")
+    ginkgo_args+=("--dry-run=true")
 fi
 
 # NOTE: Ginkgo's default timeout has been reduced from 24h to 1h in V2, set it manually here as "24h"
