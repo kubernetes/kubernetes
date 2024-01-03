7773b0f53fd5003c1e018321efb791e8980cd02f
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2024-01-03 13:32:02 +0100

    client-go/features: introduce WatchListClient feature gate

--- staging/src/k8s.io/client-go/features/known_features.go
+++ staging/src/k8s.io/client-go/features/known_features.go
@@ -16,8 +16,34 @@ limitations under the License.
 
 package features
 
+const (
+	// Every feature gate should add method here following this template:
+	//
+	// // owner: @username
+	// // alpha: v1.4
+	// MyFeature featuregate.Feature = "MyFeature"
+	//
+	// Feature gates should be listed in alphabetical, case-sensitive
+	// (upper before any lower case character) order. This reduces the risk
+	// of code conflicts because changes are more likely to be scattered
+	// across the file.
+
+	// owner: @p0lyn0mial
+	// beta: v1.30
+	//
+	// Allow the client to get a stream of individual items instead of chunking from the server.
+	//
+	// NOTE:
+	//  The feature is disabled in Beta by default because
+	//  it will only be turned on for selected control plane component(s).
+	WatchListClient Feature = "WatchListClient"
+)
+
 // defaultKubernetesFeatureGates consists of all known Kubernetes-specific feature keys.
 //
-// To add a new feature, define a key for it above and add it here. The features will be
-// available throughout Kubernetes binaries.
-var defaultKubernetesFeatureGates = map[Feature]FeatureSpec{}
+// To add a new feature, define a key for it above and add it here.
+// After registering with the binary, the features are, by default, controllable using environment variables.
+// For more details, please see envVarFeatureGates implementation.
+var defaultKubernetesFeatureGates = map[Feature]FeatureSpec{
+	WatchListClient: {Default: false, PreRelease: Beta},
+}
