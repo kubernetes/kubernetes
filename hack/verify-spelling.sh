017010d66e83ae4a4e17335a449f8946bfbdf1a6
Tim Hockin <thockin@google.com>
2024-01-04 14:55:44 -0800

    Fix verify-spelling for workspaces

--- hack/verify-spelling.sh
+++ hack/verify-spelling.sh
@@ -25,6 +25,8 @@ KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 export KUBE_ROOT
 source "${KUBE_ROOT}/hack/lib/init.sh"
 
+kube::golang::setup_env
+
 # Ensure that we find the binaries we build before anything else.
 export GOBIN="${KUBE_OUTPUT_BIN}"
 PATH="${GOBIN}:${PATH}"
