1fed36b9842f3beecfe593f7f1b486b8872c0248
Tim Hockin <thockin@google.com>
2024-01-09 09:20:42 -0800

    codegen: Minor: fix wrong comments

--- staging/src/k8s.io/code-generator/cmd/applyconfiguration-gen/main.go
+++ staging/src/k8s.io/code-generator/cmd/applyconfiguration-gen/main.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// typebuilder-gen is a tool for auto-generating apply builder functions.
+// applyconfiguration-gen is a tool for auto-generating apply builder functions.
 package main
 
 import (
