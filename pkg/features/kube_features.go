75e6651a352e97d2c3bc7619661824083e429c7c
Paco Xu <paco.xu@daocloud.io>
2024-01-11 15:27:23 +0800

    Revert "Graduate Evented PLEG to Beta"
    This reverts commit d971809b491eb7aa1df924eb898185ab1f57f66f.

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -250,7 +250,6 @@ const (
 	// owner: @harche
 	// kep: http://kep.k8s.io/3386
 	// alpha: v1.25
-	// beta: v1.27
 	//
 	// Allows using event-driven PLEG (pod lifecycle event generator) through kubelet
 	// which avoids frequent relisting of containers which helps optimize performance.
@@ -1005,7 +1004,7 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 
 	DynamicResourceAllocation: {Default: false, PreRelease: featuregate.Alpha},
 
-	EventedPLEG: {Default: false, PreRelease: featuregate.Beta}, // off by default, requires CRI Runtime support
+	EventedPLEG: {Default: false, PreRelease: featuregate.Alpha},
 
 	ExecProbeTimeout: {Default: true, PreRelease: featuregate.GA}, // lock to default and remove after v1.22 based on KEP #1972 update
 
