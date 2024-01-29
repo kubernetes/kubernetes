b2420a82347c78e95572f63f27a425c0bc2e6525
José Carlos Chávez <jcchavezs@gmail.com>
2024-01-29 15:34:59 +0100

    chore: changes copyright year to 2024.

--- hack/update-vanity-imports.sh
+++ hack/update-vanity-imports.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2023 The Kubernetes Authors.
+# Copyright 2024 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
