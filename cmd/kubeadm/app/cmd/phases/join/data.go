7db7222592ef1efa6c65617700ef22fa2289d71c
Lubomir I. Ivanov <lubomirivanov@vmware.com>
2024-02-06 19:43:25 +0200

    kubeadm: introduce the WaitForAllControlPlaneComponents feature gate
    WaitForAllControlPlaneComponents is a new feature gate
    that can be used to tell kubeadm to wait for all control plane
    components and not only kube-apiserver.
    
    - Add the Waiter function WaitForControlPlaneComponents
    that waits for all CP components in parallel. Uses the regular
    healthz endpoint for checks of status 200.
    - Add a new experimental phase to kubeadm join called "wait-control-plane".
    A similar phase exists for kubeadm init.

--- cmd/kubeadm/app/cmd/phases/join/data.go
+++ cmd/kubeadm/app/cmd/phases/join/data.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package phases includes command line phases for kubeadm join
 package phases
 
 import (
