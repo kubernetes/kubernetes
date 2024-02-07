ac9482d15f4af13d2bc7ed7b107c46b855afbc27
cpanato <ctadeu@gmail.com>
2024-02-07 11:37:15 +0100

    update distroless-iptables to v0.5.0 debian-base to bookworm-v1.0.1 and setcap to bookworm-v1.0.1
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- build/common.sh
+++ build/common.sh
@@ -96,9 +96,9 @@ readonly KUBE_RSYNC_PORT="${KUBE_RSYNC_PORT:-}"
 readonly KUBE_CONTAINER_RSYNC_PORT=8730
 
 # These are the default versions (image tags) for their respective base images.
-readonly __default_distroless_iptables_version=v0.4.4
+readonly __default_distroless_iptables_version=v0.5.0
 readonly __default_go_runner_version=v2.3.1-go1.22rc2-bookworm.0
-readonly __default_setcap_version=bookworm-v1.0.0
+readonly __default_setcap_version=bookworm-v1.0.1
 
 # These are the base images for the Docker-wrapped binaries.
 readonly KUBE_GORUNNER_IMAGE="${KUBE_GORUNNER_IMAGE:-$KUBE_BASE_IMAGE_REGISTRY/go-runner:$__default_go_runner_version}"
