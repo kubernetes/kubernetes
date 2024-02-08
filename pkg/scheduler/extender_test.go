384019019860fcb16f202e45e3ba389782d91690
sivchari <shibuuuu5@gmail.com>
2024-02-08 12:41:37 +0900

    fix: review point
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- pkg/scheduler/extender_test.go
+++ pkg/scheduler/extender_test.go
@@ -345,7 +345,7 @@ func TestSchedulerWithExtenders(t *testing.T) {
 					t.Errorf("Unexpected error: %v", err)
 					return
 				}
-				if diff := cmp.Diff(result, test.expectedResult); diff != "" {
+				if diff := cmp.Diff(test.expectedResult, result); diff != "" {
 					t.Errorf("Unexpected result (-want +got):\n%s", diff)
 				}
 			}
@@ -417,7 +417,6 @@ func TestIsInterested(t *testing.T) {
 }
 
 func TestConvertToMetaVictims(t *testing.T) {
-	t.Parallel()
 	tests := []struct {
 		name              string
 		nodeNameToVictims map[string]*extenderv1.Victims
@@ -460,9 +459,7 @@ func TestConvertToMetaVictims(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
 			got := convertToMetaVictims(tt.nodeNameToVictims)
 			if diff := cmp.Diff(tt.want, got); diff != "" {
 				t.Errorf("convertToMetaVictims() mismatch (-want +got):\n%s", diff)
