6775ece53a0e655fa4bafe52b72626930e1889a0
Peter Hunt <pehunt@redhat.com>
2024-02-12 17:09:13 -0500

    test/e2e_node: add proc mount tests
    including one Alpha only test, as the feature is in alpha
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- test/e2e/nodefeature/nodefeature.go
+++ test/e2e/nodefeature/nodefeature.go
@@ -95,6 +95,9 @@ var (
 	RecursiveReadOnlyMounts = framework.WithNodeFeature(framework.ValidNodeFeatures.Add("RecursiveReadOnlyMounts"))
 
 	// TODO: document the feature (owning SIG, when to use this feature for a test)
+	ProcMountType = framework.WithNodeFeature(framework.ValidNodeFeatures.Add("ProcMountType"))
+
+	// TODO: document the feature (owning SIG, when to use this feature for a test)
 	ResourceMetrics = framework.WithNodeFeature(framework.ValidNodeFeatures.Add("ResourceMetrics"))
 
 	// TODO: document the feature (owning SIG, when to use this feature for a test)
