kubernetes/code-generator

Rename gengo DefaultGen -> GolangGenerator
Kubernetes-commit: 0ff7dd19430c4db2b93be3658fcacc8ccb06b9c4

cmd/applyconfiguration-gen/generators/internal.go
@@ -30,7 +30,7 @@ import (
 
 // utilGenerator generates the ForKind() utility function.
 type internalGenerator struct {
-	generator.DefaultGen
+	generator.GoGenerator
 	outputPackage string
 	imports       namer.ImportTracker
 	typeModels    *typeModels

cmd/client-gen/generators/fake/generator_fake_for_group.go
@@ -31,7 +31,7 @@ import (
 
 // genFakeForGroup produces a file for a group client, e.g. ExtensionsClient for the extension group.
 type genFakeForGroup struct {
-	generator.DefaultGen
+	generator.GoGenerator
 	outputPackage     string // must be a Go import-path
 	realClientPackage string // must be a Go import-path
 	group             string

cmd/client-gen/generators/fake/generator_fake_for_type.go
@@ -30,7 +30,7 @@ import (
 
 // genFakeForType produces a file for each top-level type.
 type genFakeForType struct {
-	generator.DefaultGen
+	generator.GoGenerator
 	outputPackage             string // Must be a Go import-path
 	group                     string
 	version                   string

cmd/client-gen/generators/generator_for_type.go
@@ -31,7 +31,7 @@ import (
 
 // genClientForType produces a file for each top-level type.
 type genClientForType struct {
-	generator.DefaultGen
+	generator.GoGenerator
 	outputPackage             string // must be a Go import-path
 	inputPackage              string
 	clientsetPackage          string // must be a Go import-path

cmd/informer-gen/generators/factoryinterface.go
@@ -29,7 +29,7 @@ import (
 // factoryInterfaceGenerator produces a file of interfaces used to break a dependency cycle for
 // informer registration
 type factoryInterfaceGenerator struct {
-	generator.DefaultGen
+	generator.GoGenerator
 	outputPackage    string
 	imports          namer.ImportTracker
 	clientSetPackage string

cmd/informer-gen/generators/generic.go
@@ -30,7 +30,7 @@ import (
 
 // genericGenerator generates the generic informer.
 type genericGenerator struct {
-	generator.DefaultGen
+	generator.GoGenerator
 	outputPackage        string
 	imports              namer.ImportTracker
 	groupVersions        map[string]clientgentypes.GroupVersions

cmd/informer-gen/generators/versioninterface.go
@@ -28,7 +28,7 @@ import (
 
 // versionInterfaceGenerator generates the per-version interface file.
 type versionInterfaceGenerator struct {
-	generator.DefaultGen
+	generator.GoGenerator
 	outputPackage             string
 	imports                   namer.ImportTracker
 	types                     []*types.Type

kubernetes/kubernetes

staging/src/k8s.io/code-generator/cmd/applyconfiguration-gen/generators/internal.go
@@ -30,7 +30,7 @@ import (
 
 // utilGenerator generates the ForKind() utility function.
 type internalGenerator struct {
-	generator.DefaultGen
+	generator.GolangGenerator
 	outputPackage string
 	imports       namer.ImportTracker
 	typeModels    *typeModels

staging/src/k8s.io/code-generator/cmd/client-gen/generators/fake/generator_fake_for_group.go
@@ -31,7 +31,7 @@ import (
 
 // genFakeForGroup produces a file for a group client, e.g. ExtensionsClient for the extension group.
 type genFakeForGroup struct {
-	generator.DefaultGen
+	generator.GolangGenerator
 	outputPackage     string // must be a Go import-path
 	realClientPackage string // must be a Go import-path
 	group             string

staging/src/k8s.io/code-generator/cmd/client-gen/generators/fake/generator_fake_for_type.go
@@ -30,7 +30,7 @@ import (
 
 // genFakeForType produces a file for each top-level type.
 type genFakeForType struct {
-	generator.DefaultGen
+	generator.GolangGenerator
 	outputPackage             string // Must be a Go import-path
 	group                     string
 	version                   string

staging/src/k8s.io/code-generator/cmd/client-gen/generators/generator_for_type.go
@@ -31,7 +31,7 @@ import (
 
 // genClientForType produces a file for each top-level type.
 type genClientForType struct {
-	generator.DefaultGen
+	generator.GolangGenerator
 	outputPackage             string // must be a Go import-path
 	inputPackage              string
 	clientsetPackage          string // must be a Go import-path

staging/src/k8s.io/code-generator/cmd/informer-gen/generators/factoryinterface.go
@@ -29,7 +29,7 @@ import (
 // factoryInterfaceGenerator produces a file of interfaces used to break a dependency cycle for
 // informer registration
 type factoryInterfaceGenerator struct {
-	generator.DefaultGen
+	generator.GolangGenerator
 	outputPackage    string
 	imports          namer.ImportTracker
 	clientSetPackage string

staging/src/k8s.io/code-generator/cmd/informer-gen/generators/generic.go
@@ -30,7 +30,7 @@ import (
 
 // genericGenerator generates the generic informer.
 type genericGenerator struct {
-	generator.DefaultGen
+	generator.GolangGenerator
 	outputPackage        string
 	imports              namer.ImportTracker
 	groupVersions        map[string]clientgentypes.GroupVersions

staging/src/k8s.io/code-generator/cmd/informer-gen/generators/versioninterface.go
@@ -28,7 +28,7 @@ import (
 
 // versionInterfaceGenerator generates the per-version interface file.
 type versionInterfaceGenerator struct {
-	generator.DefaultGen
+	generator.GolangGenerator
 	outputPackage             string
 	imports                   namer.ImportTracker
 	types                     []*types.Type