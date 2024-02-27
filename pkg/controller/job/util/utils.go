c856c412b97c44383e000081bb47955319f7980e
Tomas Tormo <tomas.tormo@nearform.com>
2024-02-27 21:31:16 +0000

    Add util pkg to commonize job util functions

--- pkg/controller/job/util/utils.go
+++ pkg/controller/job/util/utils.go
@@ -14,18 +14,35 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package job
+package util
 
 import (
 	batch "k8s.io/api/batch/v1"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 )
+
+// FinishedCondition returns true if a job is finished as well as the condition type indicating that.
+// Returns false and no condition type otherwise
+func FinishedCondition(j *batch.Job) (bool, batch.JobConditionType) {
+	for _, c := range j.Status.Conditions {
+		if (c.Type == batch.JobComplete || c.Type == batch.JobFailed) && c.Status == v1.ConditionTrue {
+			return true, c.Type
+		}
+	}
+	return false, ""
+}
 
 // IsJobFinished checks whether the given Job has finished execution.
 // It does not discriminate between successful and failed terminations.
 func IsJobFinished(j *batch.Job) bool {
+	isFinished, _ := FinishedCondition(j)
+	return isFinished
+}
+
+// IsJobSucceeded returns whether a job has completed successfully.
+func IsJobSucceeded(j *batch.Job) bool {
 	for _, c := range j.Status.Conditions {
-		if (c.Type == batch.JobComplete || c.Type == batch.JobFailed) && c.Status == v1.ConditionTrue {
+		if c.Type == batch.JobComplete && c.Status == v1.ConditionTrue {
 			return true
 		}
 	}
