77f1fe33595c1ddb84a9262cbd0393b129365e89
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2024-03-18 15:32:52 +0100

    OWN_PR: https://github.com/kubernetes/kubernetes/pull/123974

--- staging/src/k8s.io/client-go/features/testing/features_init_test.go
+++ staging/src/k8s.io/client-go/features/testing/features_init_test.go
@@ -27,6 +27,7 @@ import (
 )
 
 func TestDriveInitDefaultFeatureGates(t *testing.T) {
+	restoreDefaultFeatureGatesOnComplete(t)
 	featureGates := features.FeatureGates()
 	assertFunctionPanicsWithMessage(t, func() { featureGates.Enabled("FakeFeatureGate") }, "features.FeatureGates().Enabled", fmt.Sprintf("feature %q is not registered in FeatureGate", "FakeFeatureGate"))
 
@@ -36,8 +37,51 @@ func TestDriveInitDefaultFeatureGates(t *testing.T) {
 	features.ReplaceFeatureGates(fakeFeatureGates)
 	featureGates = features.FeatureGates()
 
-	assertFeatureGatesType(t, featureGates)
+	require.IsType(t, (*alwaysEnabledFakeGates)(nil), featureGates)
 	require.True(t, featureGates.Enabled("FakeFeatureGate"))
+}
+
+func TestSetFeatureGatesDuringTest(t *testing.T) {
+	restoreDefaultFeatureGatesOnComplete(t)
+	unsafeSkipCheckingKnownFeaturesForDurationOfTest(t)
+	featureA := features.Feature("FeatureA")
+	featureB := features.Feature("FeatureB")
+	fakeGates := &fakeFeatureGates{map[features.Feature]bool{featureA: true, featureB: true}}
+	features.ReplaceFeatureGates(fakeGates)
+	t.Cleanup(func() {
+		// since cleanup functions will be called in last added, first called order.
+		// check if the original feature gates were restored
+		require.True(t, features.FeatureGates().Enabled(featureA), "the original feature gates weren't restored")
+		require.IsType(t, (*fakeFeatureGates)(nil), features.FeatureGates())
+	})
+
+	SetFeatureDuringTest(t, featureA, false)
+
+	require.False(t, features.FeatureGates().Enabled(featureA))
+	require.True(t, features.FeatureGates().Enabled(featureB))
+}
+
+func TestSetFeatureGatesDuringTestNegative(t *testing.T) {
+	restoreDefaultFeatureGatesOnComplete(t)
+	featureA := features.Feature("FeatureA")
+	fakeGates := &fakeFeatureGates{map[features.Feature]bool{featureA: true}}
+	fakeTesting := &fakeT{fakeTestName: "fakeTestNameOne", TB: t}
+
+	overrideCleanup, err := overrideFeatureGatesLocked(fakeTesting, fakeGates)
+	require.NoError(t, err)
+	t.Cleanup(overrideCleanup)
+
+	fakeTesting.fakeTestName = "fakeTestNameTwo"
+	_, err = overrideFeatureGatesLocked(fakeTesting, fakeGates)
+	require.Error(t, err)
+}
+
+type fakeFeatureGates struct {
+	features map[features.Feature]bool
+}
+
+func (f *fakeFeatureGates) Enabled(feature features.Feature) bool {
+	return f.features[feature]
 }
 
 type alwaysEnabledFakeGates struct{}
@@ -46,11 +90,13 @@ func (f *alwaysEnabledFakeGates) Enabled(features.Feature) bool {
 	return true
 }
 
-func assertFeatureGatesType(t *testing.T, fg features.Gates) {
-	_, ok := fg.(*alwaysEnabledFakeGates)
-	if !ok {
-		t.Fatalf("passed features.FeatureGates() is NOT of type *alwaysEnabledFakeGates, it is of type = %T", fg)
-	}
+type fakeT struct {
+	fakeTestName string
+	testing.TB
+}
+
+func (t *fakeT) Name() string {
+	return t.fakeTestName
 }
 
 func assertFunctionPanicsWithMessage(t *testing.T, f func(), fName, errMessage string) {
@@ -76,4 +122,12 @@ func didFunctionPanic(f func()) (didPanic bool, panicMessage interface{}) {
 	didPanic = false
 
 	return
+}
+
+func restoreDefaultFeatureGatesOnComplete(t *testing.T) {
+	t.Helper()
+	originalGates := features.FeatureGates()
+	t.Cleanup(func() {
+		features.ReplaceFeatureGates(originalGates)
+	})
 }
