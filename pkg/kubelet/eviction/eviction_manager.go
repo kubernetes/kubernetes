6b3cb26fff2afcb8653e8394bcf78005a84225da
Olga Shestopalova <oshestopalova1@gmail.com>
2024-03-27 14:55:06 -0400

    if a pod is taking too long to evict, move on to others
    Signed-off-by: Olga Shestopalova <oshestopalova@hubspot.com>

--- pkg/kubelet/eviction/eviction_manager.go
+++ pkg/kubelet/eviction/eviction_manager.go
@@ -102,6 +102,8 @@ type managerImpl struct {
 	thresholdsLastUpdated time.Time
 	// whether can support local storage capacity isolation
 	localStorageCapacityIsolation bool
+	// podsEvicted is the set of pods (format: namespace/pod: bool) that we have already tried to evict
+	podsEvicted map[string]bool
 }
 
 // ensure it implements the required interface
@@ -134,6 +136,7 @@ func NewManager(
 		splitContainerImageFs:         nil,
 		thresholdNotifiers:            []ThresholdNotifier{},
 		localStorageCapacityIsolation: localStorageCapacityIsolation,
+		podsEvicted:                   map[string]bool{},
 	}
 	return manager, manager
 }
@@ -354,6 +357,7 @@ func (m *managerImpl) synchronize(diskInfoProvider DiskInfoProvider, podFunc Act
 
 	if len(thresholds) == 0 {
 		klog.V(3).InfoS("Eviction manager: no resources are starved")
+		m.podsEvicted = map[string]bool{}
 		return nil, nil
 	}
 
@@ -405,6 +409,12 @@ func (m *managerImpl) synchronize(diskInfoProvider DiskInfoProvider, podFunc Act
 	// we kill at most a single pod during each eviction interval
 	for i := range activePods {
 		pod := activePods[i]
+		podAndNamespace := pod.Namespace + "/" + pod.Name
+		if _, ok := m.podsEvicted[podAndNamespace]; ok {
+			klog.Warningf("Eviction manager: not evicting pod that we already attempted to evict: %v", podAndNamespace)
+			continue
+		}
+		m.podsEvicted[podAndNamespace] = true
 		gracePeriodOverride := int64(0)
 		if !isHardEvictionThreshold(thresholdToReclaim) {
 			gracePeriodOverride = m.config.MaxPodGracePeriodSeconds
