8d45bbea2b464e856ddcfe3f6ee410ddea0cee32
Alexander Zielenski <zielenski@google.com>
2024-04-08 18:46:59 -0700

    add prerelease lifecycle tags to GA types
    Estimated based upon the milestone of the commit that added the original struct header. If the commit wasn't in a milestone used the deprecation of the prior beta api. If that wasn't available inferred the next available release from the date the commit was merged.

--- staging/src/k8s.io/api/admissionregistration/v1/doc.go
+++ staging/src/k8s.io/api/admissionregistration/v1/doc.go
@@ -17,6 +17,7 @@ limitations under the License.
 // +k8s:deepcopy-gen=package
 // +k8s:protobuf-gen=package
 // +k8s:openapi-gen=true
+// +k8s:prerelease-lifecycle-gen=true
 // +groupName=admissionregistration.k8s.io
 
 // Package v1 is the v1 version of the API.
