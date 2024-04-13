02197a9924231f44da83b401dd1b1ba32e0e07d7
hoyho <luohaihao@gmail.com>
2024-04-13 18:19:37 +0800

    fix warning when using CSI driver to expand volume
    Signed-off-by: hoyho <luohaihao@gmail.com>

--- pkg/volume/plugins.go
+++ pkg/volume/plugins.go
@@ -17,6 +17,7 @@ limitations under the License.
 package volume
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"strings"
@@ -61,6 +62,8 @@ const (
 	ProbeAddOrUpdate ProbeOperation = 1 << iota
 	ProbeRemove
 )
+
+var ErrNoPluiginMatched = errors.New("no volume plugin matched")
 
 // VolumeOptions contains option information about a volume.
 type VolumeOptions struct {
@@ -649,7 +652,7 @@ func (pm *VolumePluginMgr) FindPluginBySpec(spec *Spec) (VolumePlugin, error) {
 	}
 
 	if len(matchedPluginNames) == 0 {
-		return nil, fmt.Errorf("no volume plugin matched")
+		return nil, ErrNoPluiginMatched
 	}
 	if len(matchedPluginNames) > 1 {
 		return nil, fmt.Errorf("multiple volume plugins matched: %s", strings.Join(matchedPluginNames, ","))
@@ -875,6 +878,9 @@ func (pm *VolumePluginMgr) FindExpandablePluginBySpec(spec *Spec) (ExpandableVol
 			// returns success for expand on the controller
 			klog.V(4).InfoS("FindExpandablePluginBySpec -> returning noopExpandableVolumePluginInstance", "specName", spec.Name())
 			return &noopExpandableVolumePluginInstance{spec}, nil
+		}
+		if errors.Is(err, ErrNoPluiginMatched) {
+			return nil, nil
 		}
 		klog.V(4).InfoS("FindExpandablePluginBySpec -> err", "specName", spec.Name(), "err", err)
 		return nil, err
