4f0ee034ecc9b72efebe64e7ea0cc512fc1a0681
carlory <baofa.fan@daocloud.io>
2024-04-24 15:01:20 +0800

    kubeadm: remove the deprecated output.kubeadm.k8s.io/v1alpha2

--- cmd/kubeadm/app/apis/output/scheme/scheme.go
+++ cmd/kubeadm/app/apis/output/scheme/scheme.go
@@ -24,7 +24,6 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 
 	"k8s.io/kubernetes/cmd/kubeadm/app/apis/output"
-	"k8s.io/kubernetes/cmd/kubeadm/app/apis/output/v1alpha2"
 	"k8s.io/kubernetes/cmd/kubeadm/app/apis/output/v1alpha3"
 )
 
@@ -42,7 +41,6 @@ func init() {
 // AddToScheme builds the kubeadm scheme using all known versions of the kubeadm api.
 func AddToScheme(scheme *runtime.Scheme) {
 	utilruntime.Must(output.AddToScheme(scheme))
-	utilruntime.Must(v1alpha2.AddToScheme(scheme))
 	utilruntime.Must(v1alpha3.AddToScheme(scheme))
-	utilruntime.Must(scheme.SetVersionPriority(v1alpha3.SchemeGroupVersion, v1alpha2.SchemeGroupVersion))
+	utilruntime.Must(scheme.SetVersionPriority(v1alpha3.SchemeGroupVersion))
 }
