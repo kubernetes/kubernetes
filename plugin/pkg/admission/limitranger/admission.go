9ee7511a9c577f1802efb9a43dfdf92fdd10e4d2
mouuii <49775493+mouuii@users.noreply.github.com>
2024-05-15 21:04:42 +0800

    allow limitranger limit

--- plugin/pkg/admission/limitranger/admission.go
+++ plugin/pkg/admission/limitranger/admission.go
@@ -421,10 +421,7 @@ func (d *DefaultLimitRangerActions) SupportsAttributes(a admission.Attributes) b
 		return false
 	}
 
-	// Since containers and initContainers cannot currently be added, removed, or updated, it is unnecessary
-	// to mutate and validate limitrange on pod updates. Trying to mutate containers or initContainers on a pod
-	// update request will always fail pod validation because those fields are immutable once the object is created.
-	if a.GetKind().GroupKind() == api.Kind("Pod") && a.GetOperation() == admission.Update {
+	if a.GetKind().GroupKind() == api.Kind("Pod") && a.GetOperation() == admission.Update && !feature.DefaultFeatureGate.Enabled(features.InPlacePodVerticalScaling) {
 		return false
 	}
 
