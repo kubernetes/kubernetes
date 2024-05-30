4b309968b26790c302dde6179385ec9f0abc917e
Humble Chirammal <humble.devassy@gmail.com>
2024-05-30 10:24:21 +0530

    make use of etcd v3.5.14 in the builds.
    https://github.com/etcd-io/etcd/releases/tag/v3.5.14
    
    This release has been built with GO 1.21.10
    
    Signed-off-by: Humble Chirammal <humble.devassy@gmail.com>

--- cmd/kubeadm/app/constants/constants.go
+++ cmd/kubeadm/app/constants/constants.go
@@ -309,7 +309,7 @@ const (
 	MinExternalEtcdVersion = "3.4.13-4"
 
 	// DefaultEtcdVersion indicates the default etcd version that kubeadm uses
-	DefaultEtcdVersion = "3.5.12-0"
+	DefaultEtcdVersion = "3.5.14-0"
 
 	// Etcd defines variable used internally when referring to etcd component
 	Etcd = "etcd"
@@ -464,15 +464,10 @@ var (
 
 	// SupportedEtcdVersion lists officially supported etcd versions with corresponding Kubernetes releases
 	SupportedEtcdVersion = map[uint8]string{
-		22: "3.5.12-0",
-		23: "3.5.12-0",
-		24: "3.5.12-0",
-		25: "3.5.12-0",
-		26: "3.5.12-0",
-		27: "3.5.12-0",
-		28: "3.5.12-0",
-		29: "3.5.12-0",
-		30: "3.5.12-0",
+		28: "3.5.14-0",
+		29: "3.5.14-0",
+		30: "3.5.14-0",
+		31: "3.5.14-0",
 	}
 
 	// KubeadmCertsClusterRoleName sets the name for the ClusterRole that allows
