59ed9da26f43ddce9f1eed5bd1d42454424c2609
Arunvel Arunachalam <30715441+arunvel1988@users.noreply.github.com>
2024-06-03 13:09:30 +0530

    Update gen_kubectl_yaml.go

--- cmd/genyaml/gen_kubectl_yaml.go
+++ cmd/genyaml/gen_kubectl_yaml.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -31,6 +32,7 @@ import (
 	"k8s.io/kubernetes/cmd/genutils"
 )
 
+// cmdOption represents a command option for documentation.
 type cmdOption struct {
 	Name         string
 	Shorthand    string `yaml:",omitempty"`
@@ -38,6 +40,7 @@ type cmdOption struct {
 	Usage        string `yaml:",omitempty"`
 }
 
+// cmdDoc represents the documentation structure for a command.
 type cmdDoc struct {
 	Name             string
 	Synopsis         string      `yaml:",omitempty"`
@@ -49,6 +52,7 @@ type cmdDoc struct {
 }
 
 func main() {
+	// Determine the output directory for the documentation.
 	path := "docs/yaml/kubectl"
 	if len(os.Args) == 2 {
 		path = os.Args[1]
@@ -63,18 +67,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Set environment variables used by kubectl so the output is consistent,
-	// regardless of where we run.
+	// Ensure the output directory exists.
+	err = os.MkdirAll(outDir, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create output directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Set environment variables used by kubectl so the output is consistent.
 	os.Setenv("HOME", "/home/username")
 	kubectl := cmd.NewKubectlCommand(cmd.KubectlOptions{IOStreams: genericiooptions.IOStreams{In: bytes.NewReader(nil), Out: io.Discard, ErrOut: io.Discard}})
+	
+	// Generate YAML documentation for the kubectl command and its subcommands.
 	genYaml(kubectl, "", outDir)
 	for _, c := range kubectl.Commands() {
 		genYaml(c, "kubectl", outDir)
 	}
 }
 
-// Temporary workaround for yaml lib generating incorrect yaml with long strings
-// that do not contain \n.
+// forceMultiLine ensures long strings are treated as multi-line in YAML output.
 func forceMultiLine(s string) string {
 	if len(s) > 60 && !strings.Contains(s, "\n") {
 		s = s + "\n"
@@ -82,40 +93,32 @@ func forceMultiLine(s string) string {
 	return s
 }
 
+// genFlagResult extracts flag details and formats them into cmdOption structures.
 func genFlagResult(flags *pflag.FlagSet) []cmdOption {
 	result := []cmdOption{}
 
 	flags.VisitAll(func(flag *pflag.Flag) {
-		// Todo, when we mark a shorthand is deprecated, but specify an empty message.
-		// The flag.ShorthandDeprecated is empty as the shorthand is deprecated.
-		// Using len(flag.ShorthandDeprecated) > 0 can't handle this, others are ok.
-		if !(len(flag.ShorthandDeprecated) > 0) && len(flag.Shorthand) > 0 {
-			opt := cmdOption{
-				flag.Name,
-				flag.Shorthand,
-				flag.DefValue,
-				forceMultiLine(flag.Usage),
-			}
-			result = append(result, opt)
-		} else {
-			opt := cmdOption{
-				Name:         flag.Name,
-				DefaultValue: forceMultiLine(flag.DefValue),
-				Usage:        forceMultiLine(flag.Usage),
-			}
-			result = append(result, opt)
+		opt := cmdOption{
+			Name:         flag.Name,
+			DefaultValue: forceMultiLine(flag.DefValue),
+			Usage:        forceMultiLine(flag.Usage),
 		}
+		if !(len(flag.ShorthandDeprecated) > 0) && len(flag.Shorthand) > 0 {
+			opt.Shorthand = flag.Shorthand
+		}
+		result = append(result, opt)
 	})
 
 	return result
 }
 
+// genYaml generates YAML documentation for a given command and writes it to a file.
 func genYaml(command *cobra.Command, parent, docsDir string) {
-	doc := cmdDoc{}
-
-	doc.Name = command.Name()
-	doc.Synopsis = forceMultiLine(command.Short)
-	doc.Description = forceMultiLine(command.Long)
+	doc := cmdDoc{
+		Name:        command.Name(),
+		Synopsis:    forceMultiLine(command.Short),
+		Description: forceMultiLine(command.Long),
+	}
 
 	flags := command.NonInheritedFlags()
 	if flags.HasFlags() {
@@ -143,27 +146,27 @@ func genYaml(command *cobra.Command, parent, docsDir string) {
 
 	final, err := yaml.Marshal(&doc)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintf(os.Stderr, "failed to marshal YAML: %v\n", err)
 		os.Exit(1)
 	}
 
 	var filename string
-
 	if parent == "" {
-		filename = docsDir + doc.Name + ".yaml"
+		filename = filepath.Join(docsDir, doc.Name+".yaml")
 	} else {
-		filename = docsDir + parent + "_" + doc.Name + ".yaml"
+		filename = filepath.Join(docsDir, parent+"_"+doc.Name+".yaml")
 	}
 
 	outFile, err := os.Create(filename)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintf(os.Stderr, "failed to create file %s: %v\n", filename, err)
 		os.Exit(1)
 	}
 	defer outFile.Close()
+
 	_, err = outFile.Write(final)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintf(os.Stderr, "failed to write to file %s: %v\n", filename, err)
 		os.Exit(1)
 	}
 }
