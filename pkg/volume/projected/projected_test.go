af5dedabfa8fc33c4a3e9ae8bd5ffcc8b9edc280
ZhangYu <zhang.yu61@zte.com.cn>
2024-06-12 10:18:48 +0800

    pkg/volume: replace deprecated pointer package
    Signed-off-by: ZhangYu <zhang.yu61@zte.com.cn>

--- pkg/volume/projected/projected_test.go
+++ pkg/volume/projected/projected_test.go
@@ -47,7 +47,7 @@ import (
 	"k8s.io/kubernetes/pkg/volume/emptydir"
 	volumetest "k8s.io/kubernetes/pkg/volume/testing"
 	"k8s.io/kubernetes/pkg/volume/util"
-	utilptr "k8s.io/utils/pointer"
+	"k8s.io/utils/ptr"
 )
 
 func TestCollectDataWithSecret(t *testing.T) {
@@ -260,7 +260,7 @@ func TestCollectDataWithSecret(t *testing.T) {
 				Name:      tc.name,
 			}
 
-			source := makeProjection(tc.name, utilptr.Int32Ptr(tc.mode), "secret")
+			source := makeProjection(tc.name, ptr.To[int32](tc.mode), "secret")
 			source.Sources[0].Secret.Items = tc.mappings
 			source.Sources[0].Secret.Optional = &tc.optional
 
@@ -509,7 +509,7 @@ func TestCollectDataWithConfigMap(t *testing.T) {
 				Name:      tc.name,
 			}
 
-			source := makeProjection(tc.name, utilptr.Int32Ptr(tc.mode), "configMap")
+			source := makeProjection(tc.name, ptr.To[int32](tc.mode), "configMap")
 			source.Sources[0].ConfigMap.Items = tc.mappings
 			source.Sources[0].ConfigMap.Optional = &tc.optional
 
@@ -683,7 +683,7 @@ func TestCollectDataWithDownwardAPI(t *testing.T) {
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			source := makeProjection("", utilptr.Int32Ptr(tc.mode), "downwardAPI")
+			source := makeProjection("", ptr.To[int32](tc.mode), "downwardAPI")
 			source.Sources[0].DownwardAPI.Items = tc.volumeFile
 
 			client := fake.NewSimpleClientset(tc.pod)
@@ -743,7 +743,7 @@ func TestCollectDataWithServiceAccountToken(t *testing.T) {
 		{
 			name:        "good service account",
 			audience:    "https://example.com",
-			defaultMode: utilptr.Int32Ptr(0644),
+			defaultMode: ptr.To[int32](0644),
 			path:        "token",
 			expiration:  &minute,
 
@@ -754,7 +754,7 @@ func TestCollectDataWithServiceAccountToken(t *testing.T) {
 		{
 			name:        "good service account other path",
 			audience:    "https://example.com",
-			defaultMode: utilptr.Int32Ptr(0644),
+			defaultMode: ptr.To[int32](0644),
 			path:        "other-token",
 			expiration:  &minute,
 			wantPayload: map[string]util.FileProjection{
@@ -763,7 +763,7 @@ func TestCollectDataWithServiceAccountToken(t *testing.T) {
 		},
 		{
 			name:        "good service account defaults audience",
-			defaultMode: utilptr.Int32Ptr(0644),
+			defaultMode: ptr.To[int32](0644),
 			path:        "token",
 			expiration:  &minute,
 
@@ -773,7 +773,7 @@ func TestCollectDataWithServiceAccountToken(t *testing.T) {
 		},
 		{
 			name:        "good service account defaults expiration",
-			defaultMode: utilptr.Int32Ptr(0644),
+			defaultMode: ptr.To[int32](0644),
 			path:        "token",
 
 			wantPayload: map[string]util.FileProjection{
@@ -787,21 +787,21 @@ func TestCollectDataWithServiceAccountToken(t *testing.T) {
 		},
 		{
 			name:        "fsUser != nil",
-			defaultMode: utilptr.Int32Ptr(0644),
-			fsUser:      utilptr.Int64Ptr(1000),
+			defaultMode: ptr.To[int32](0644),
+			fsUser:      ptr.To[int64](1000),
 			path:        "token",
 			wantPayload: map[string]util.FileProjection{
 				"token": {
 					Data:   []byte("test_projected_namespace:foo:3600:[https://api]"),
 					Mode:   0600,
-					FsUser: utilptr.Int64Ptr(1000),
+					FsUser: ptr.To[int64](1000),
 				},
 			},
 		},
 		{
 			name:        "fsGroup != nil",
-			defaultMode: utilptr.Int32Ptr(0644),
-			fsGroup:     utilptr.Int64Ptr(1000),
+			defaultMode: ptr.To[int32](0644),
+			fsGroup:     ptr.To[int64](1000),
 			path:        "token",
 			wantPayload: map[string]util.FileProjection{
 				"token": {
@@ -812,15 +812,15 @@ func TestCollectDataWithServiceAccountToken(t *testing.T) {
 		},
 		{
 			name:        "fsUser != nil && fsGroup != nil",
-			defaultMode: utilptr.Int32Ptr(0644),
-			fsGroup:     utilptr.Int64Ptr(1000),
-			fsUser:      utilptr.Int64Ptr(1000),
+			defaultMode: ptr.To[int32](0644),
+			fsGroup:     ptr.To[int64](1000),
+			fsUser:      ptr.To[int64](1000),
 			path:        "token",
 			wantPayload: map[string]util.FileProjection{
 				"token": {
 					Data:   []byte("test_projected_namespace:foo:3600:[https://api]"),
 					Mode:   0600,
-					FsUser: utilptr.Int64Ptr(1000),
+					FsUser: ptr.To[int64](1000),
 				},
 			},
 		},
@@ -904,12 +904,12 @@ func TestCollectDataWithClusterTrustBundle(t *testing.T) {
 				Sources: []v1.VolumeProjection{
 					{
 						ClusterTrustBundle: &v1.ClusterTrustBundleProjection{
-							Name: utilptr.String("foo"),
+							Name: ptr.To("foo"),
 							Path: "bundle.pem",
 						},
 					},
 				},
-				DefaultMode: utilptr.Int32(0644),
+				DefaultMode: ptr.To[int32](0644),
 			},
 			bundles: []runtime.Object{
 				&certificatesv1alpha1.ClusterTrustBundle{
@@ -934,7 +934,7 @@ func TestCollectDataWithClusterTrustBundle(t *testing.T) {
 				Sources: []v1.VolumeProjection{
 					{
 						ClusterTrustBundle: &v1.ClusterTrustBundleProjection{
-							SignerName: utilptr.String("foo.example/bar"), // Note: fake client doesn't understand selection by signer name.
+							SignerName: ptr.To("foo.example/bar"), // Note: fake client doesn't understand selection by signer name.
 							LabelSelector: &metav1.LabelSelector{
 								MatchLabels: map[string]string{
 									"key": "non-value", // Note: fake client doesn't actually act on label selectors.
@@ -944,7 +944,7 @@ func TestCollectDataWithClusterTrustBundle(t *testing.T) {
 						},
 					},
 				},
-				DefaultMode: utilptr.Int32(0644),
+				DefaultMode: ptr.To[int32](0644),
 			},
 			bundles: []runtime.Object{
 				&certificatesv1alpha1.ClusterTrustBundle{
@@ -973,12 +973,12 @@ func TestCollectDataWithClusterTrustBundle(t *testing.T) {
 				Sources: []v1.VolumeProjection{
 					{
 						ClusterTrustBundle: &v1.ClusterTrustBundleProjection{
-							Name: utilptr.String("foo"),
+							Name: ptr.To("foo"),
 							Path: "bundle.pem",
 						},
 					},
 				},
-				DefaultMode: utilptr.Int32(0600),
+				DefaultMode: ptr.To[int32](0600),
 			},
 			bundles: []runtime.Object{
 				&certificatesv1alpha1.ClusterTrustBundle{
@@ -1400,7 +1400,7 @@ func makeVolumeSpec(volumeName, name string, defaultMode int32) *v1.Volume {
 	return &v1.Volume{
 		Name: volumeName,
 		VolumeSource: v1.VolumeSource{
-			Projected: makeProjection(name, utilptr.Int32Ptr(defaultMode), "secret"),
+			Projected: makeProjection(name, ptr.To[int32](defaultMode), "secret"),
 		},
 	}
 }
