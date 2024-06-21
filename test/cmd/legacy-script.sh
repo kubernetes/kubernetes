8d67437865a8e1abaed0b813d6913b412f7ed2b4
Jordan Liggitt <liggitt@google.com>
2024-06-21 12:13:08 -0400

    Revert "Add new --wait-for-creation flag in kubectl wait command"
    This reverts commit e24b9a022f3b1e97ea538c9754d4d38f119f275e.

--- test/cmd/legacy-script.sh
+++ test/cmd/legacy-script.sh
@@ -1036,7 +1036,6 @@ runTests() {
   ####################
 
   record_command run_wait_tests
-  record_command run_wait_with_non_existence_check_tests
 
   ####################
   # kubectl debug    #
