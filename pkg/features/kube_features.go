1816cc73882ea24947585eaeba2d509d6c04d4e5
Peter Hunt <pehunt@redhat.com>
2024-07-01 15:22:15 -0400

    KEP-4033: bump to beta
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -353,6 +353,7 @@ const (
 	// owner: @marquiz
 	// kep: http://kep.k8s.io/4033
 	// alpha: v1.28
+	// beta: v1.31
 	//
 	// Enable detection of the kubelet cgroup driver configuration option from
 	// the CRI.  The CRI runtime also needs to support this feature in which
@@ -1052,7 +1053,7 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 
 	JobSuccessPolicy: {Default: false, PreRelease: featuregate.Alpha},
 
-	KubeletCgroupDriverFromCRI: {Default: false, PreRelease: featuregate.Alpha},
+	KubeletCgroupDriverFromCRI: {Default: true, PreRelease: featuregate.Beta},
 
 	KubeletInUserNamespace: {Default: false, PreRelease: featuregate.Alpha},
 
