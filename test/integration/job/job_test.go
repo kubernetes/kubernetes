43baaff7b208809d6e5bae66675b4f988bf95cb3
Michal Wozniak <michalwozniak@google.com>
2024-07-04 14:48:58 +0200

    Fix integration tests

--- test/integration/job/job_test.go
+++ test/integration/job/job_test.go
@@ -2268,12 +2268,10 @@ func TestManagedBy_UsingReservedJobFinalizers(t *testing.T) {
 	// UpdateJob event in reaction to each it would remove the pod's finalizer,
 	// if not for the custom managedBy field.
 	jobObj.Status.Conditions = append(jobObj.Status.Conditions, batchv1.JobCondition{
-		Type:   batchv1.JobComplete,
+		Type:   batchv1.JobSuccessCriteriaMet,
 		Status: v1.ConditionTrue,
 	})
 	jobObj.Status.StartTime = ptr.To(metav1.Now())
-	jobObj.Status.CompletionTime = ptr.To(metav1.Now())
-
 	if jobObj, err = clientSet.BatchV1().Jobs(jobObj.Namespace).UpdateStatus(ctx, jobObj, metav1.UpdateOptions{}); err != nil {
 		t.Fatalf("Error %v when updating the job as finished %v", err, klog.KObj(jobObj))
 	}
