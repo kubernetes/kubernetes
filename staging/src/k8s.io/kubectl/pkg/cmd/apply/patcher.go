b069f89a1288ad151f18852bfa6b838d0397a72e
googs1025 <googs1025@gmail.com>
2024-07-06 15:54:07 +0800

    chore: replace deprecated wait.PollImmediate methods

--- staging/src/k8s.io/kubectl/pkg/cmd/apply/patcher.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/apply/patcher.go
@@ -17,6 +17,7 @@ limitations under the License.
 package apply
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -386,7 +387,7 @@ func (p *Patcher) deleteAndCreate(original runtime.Object, modified []byte, name
 	if err := p.delete(namespace, name); err != nil {
 		return modified, nil, err
 	}
-	if err := wait.PollImmediate(1*time.Second, p.Timeout, func() (bool, error) {
+	if err := wait.PollUntilContextTimeout(context.TODO(), 1*time.Second, p.Timeout, true, func(ctx context.Context) (bool, error) {
 		if _, err := p.Helper.Get(namespace, name); !apierrors.IsNotFound(err) {
 			return false, err
 		}
