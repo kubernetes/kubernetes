aeb51a16e369d5b823a8ae6488d1d5e12c683516
Monis Khan <i@monis.app>
2024-07-16 23:12:16 -0400

    Revert "Move ConsistentListFromCache to Beta default"
    This reverts commit 0c0e19b343d48d4bea0e7fa735e3781c70298a34.
    
    During stress test for SVM controller, the controller is unable to
    make a list call due to following error:
    
    resourceversion.go:155: I0716 21:49:26.973127] storage-version-migrator-controller: Error syncing SVM resource, retrying svm="crdsvm" err="error getting latest resourceVersion for stable.example.com/v1, Resource=testcrds: Timeout: Too large resource version: 28976, current: 20349"
    
    With the feature disabled, the stress test passes.
    
    Signed-off-by: Monis Khan <mok@microsoft.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1233,7 +1233,7 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 
 	genericfeatures.APIServingWithRoutine: {Default: true, PreRelease: featuregate.Beta},
 
-	genericfeatures.ConsistentListFromCache: {Default: true, PreRelease: featuregate.Beta},
+	genericfeatures.ConsistentListFromCache: {Default: false, PreRelease: featuregate.Alpha},
 
 	genericfeatures.CustomResourceValidationExpressions: {Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 1.31
 
