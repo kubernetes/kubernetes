1e458081bb4dd1e84c552d54ff96589b973c4e2e
Aravindh Puthiyaparambil <aravindh.foss@pm.me>
2024-08-06 15:46:15 -0700

    kubelet: use env vars in node log query PS command
    - Use environment variables to pass string arguments in the node log
      query PS command
    - Split getLoggingCmd into getLoggingCmdEnv and getLoggingCmdArgs
      for better modularization

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -586,7 +586,8 @@ const (
 	// kep: http://kep.k8s.io/2271
 	// alpha: v1.27
 	//
-	// Enables querying logs of node services using the /logs endpoint
+	// Enables querying logs of node services using the /logs endpoint. Enabling this feature has security implications.
+	// The recommendation is to enable it on a need basis for debugging purposes and disabling otherwise.
 	NodeLogQuery featuregate.Feature = "NodeLogQuery"
 
 	// owner: @xing-yang @sonasingh46
