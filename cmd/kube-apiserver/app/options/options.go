eec844e22ad039e401afbb9fd85caac0bf2f28c8
Paco Xu <paco.xu@daocloud.io>
2024-08-26 16:29:42 +0800

    Make the service cluster IP range validation message clear

--- cmd/kube-apiserver/app/options/options.go
+++ cmd/kube-apiserver/app/options/options.go
@@ -122,7 +122,8 @@ func (s *ServerRunOptions) Flags() (fss cliflag.NamedFlagSets) {
 
 	fs.StringVar(&s.ServiceClusterIPRanges, "service-cluster-ip-range", s.ServiceClusterIPRanges, ""+
 		"A CIDR notation IP range from which to assign service cluster IPs. This must not "+
-		"overlap with any IP ranges assigned to nodes or pods. Max of two dual-stack CIDRs is allowed.")
+		"overlap with any IP ranges assigned to nodes or pods. Max of two dual-stack CIDRs is allowed." +
+		"The recommended service subnet is /16 for IPv4(no larger than /12) and /112 for IPv6(no larger than /112).")
 
 	fs.Var(&s.ServiceNodePortRange, "service-node-port-range", ""+
 		"A port range to reserve for services with NodePort visibility.  This must not overlap with the ephemeral port range on nodes.  "+
