e2a89072ec2430ea86dc649d32c8dad6a9d79bea
whitewindmills <jayfantasyhjh@gmail.com>
2024-08-29 16:46:57 +0800

    fix serviceaccount words
    Signed-off-by: whitewindmills <jayfantasyhjh@gmail.com>

--- staging/src/k8s.io/api/core/v1/types_swagger_doc_generated.go
+++ staging/src/k8s.io/api/core/v1/types_swagger_doc_generated.go
@@ -1991,7 +1991,7 @@ var map_QuobyteVolumeSource = map[string]string{
 	"registry": "registry represents a single or multiple Quobyte Registry services specified as a string as host:port pair (multiple entries are separated with commas) which acts as the central registry for volumes",
 	"volume":   "volume is a string that references an already created Quobyte volume by name.",
 	"readOnly": "readOnly here will force the Quobyte volume to be mounted with read-only permissions. Defaults to false.",
-	"user":     "user to map volume access to Defaults to serivceaccount user",
+	"user":     "user to map volume access to Defaults to serviceaccount user",
 	"group":    "group to map volume access to Default is no group",
 	"tenant":   "tenant owning the given Quobyte volume in the Backend Used with dynamically provisioned Quobyte volumes, value is set by the plugin",
 }
