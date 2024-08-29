205c34a6a04afdca9da4efcca190922d7f9ac3ca
Luis Sanchez <sanchezl@redhat.com>
2024-08-29 00:06:38 -0400

    UPSTREAM: <carry>: log only deprecated api requests

--- openshift-kube-apiserver/filters/apirequestcount_filter.go
+++ openshift-kube-apiserver/filters/apirequestcount_filter.go
@@ -4,16 +4,27 @@ import (
 	"net/http"
 
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/version"
 	"k8s.io/apiserver/pkg/endpoints/request"
+	versioninfo "k8s.io/component-base/version"
 	"k8s.io/kubernetes/openshift-kube-apiserver/filters/apirequestcount"
 )
 
 // WithAPIRequestCountLogging adds a handler that logs counts of api requests.
 func WithAPIRequestCountLogging(handler http.Handler, requestLogger apirequestcount.APIRequestLogger) http.Handler {
+	currentMinor := version.MustParseSemantic(versioninfo.Get().GitVersion).Minor()
 	handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		defer handler.ServeHTTP(w, req)
 		info, ok := request.RequestInfoFrom(req.Context())
 		if !ok || !info.IsResourceRequest {
+			return
+		}
+		gvr := schema.GroupVersionResource{
+			Group:    info.APIGroup,
+			Version:  info.APIVersion,
+			Resource: info.Resource,
+		}
+		if minor, ok := apirequestcount.DeprecatedAPIRemovedRelease[gvr]; !ok || minor <= currentMinor {
 			return
 		}
 		timestamp, ok := request.ReceivedTimestampFrom(req.Context())
@@ -25,11 +36,7 @@ func WithAPIRequestCountLogging(handler http.Handler, requestLogger apirequestco
 			return
 		}
 		requestLogger.LogRequest(
-			schema.GroupVersionResource{
-				Group:    info.APIGroup,
-				Version:  info.APIVersion,
-				Resource: info.Resource,
-			},
+			gvr,
 			timestamp,
 			user.GetName(),
 			req.UserAgent(),
