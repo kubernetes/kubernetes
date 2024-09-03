98e3cb4c567a35ab5a4863e0ba6023fca12e4046
HirazawaUi <695097494plus@gmail.com>
2024-09-03 23:18:13 +0800

    promote feature gate to beta

--- pkg/features/versioned_kube_features.go
+++ pkg/features/versioned_kube_features.go
@@ -595,6 +595,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	RelaxedEnvironmentVariableValidation: {
 		{Version: version.MustParse("1.30"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.32"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	ReloadKubeletServerCertificateFile: {
