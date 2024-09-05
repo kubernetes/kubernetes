a9b641464ebcb2179f04450180802e0994cc51fe
Anurag252 <Anuragsgsits@gmail.com>
2024-09-05 15:40:35 +0200

    chore: adding support for staticPortRange
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    chore: implementing review comments
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    lint: implementing linting suggestions
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    chore: review comments implementation
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    chore: fixing build errors
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    revert: removing empty spaces
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    chore: implementing review suggestions
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    chore: implementing review comments to get rid of typecasting
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    fix: fixing failed tests
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    fix: fixing failed tests
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    fix: fixing failed tests
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    chore: rename func as per code review
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>
    
    chore: change in comments
    
    Signed-off-by: Anurag252 <Anuragsgsits@gmail.com>

--- test/e2e/framework/service/jig.go
+++ test/e2e/framework/service/jig.go
@@ -20,9 +20,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math/rand"
 	"net"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/onsi/ginkgo/v2"
@@ -58,6 +60,33 @@ var NodePortRange = utilnet.PortRange{Base: 30000, Size: 2768}
 // It is copied from "k8s.io/kubernetes/pkg/registry/core/service/portallocator"
 var errAllocated = errors.New("provided port is already allocated")
 
+// staticPortRange implements port allocation model described here
+// https://github.com/kubernetes/enhancements/tree/master/keps/sig-network/3668-reserved-service-nodeport-range
+type staticPortRange struct {
+	sync.Mutex
+	baseport      int32
+	length        int32
+	reservedPorts sets.Set[int32]
+}
+
+func calculateRange(size int32) int32 {
+	var minPort int32 = 16
+	var step int32 = 32
+	var maxPort int32 = 128
+	return min(max(minPort, size/step), maxPort)
+}
+
+var staticPortAllocator *staticPortRange
+
+// Initialize only once per test
+func init() {
+	staticPortAllocator = &staticPortRange{
+		baseport:      int32(NodePortRange.Base),
+		length:        calculateRange(int32(NodePortRange.Size)),
+		reservedPorts: sets.New[int32](),
+	}
+}
+
 // TestJig is a test jig to help service testing.
 type TestJig struct {
 	Client    clientset.Interface
@@ -80,6 +109,73 @@ func NewTestJig(client clientset.Interface, namespace, name string) *TestJig {
 	j.Labels = map[string]string{"testid": j.ID}
 
 	return j
+}
+
+// reservePort reserves the port provided as input.
+// If an invalid port was provided or if the port is already reserved, it returns false
+func (s *staticPortRange) reservePort(port int32) bool {
+	s.Lock()
+	defer s.Unlock()
+	if port < s.baseport || port > s.baseport+s.length || s.reservedPorts.Has(port) {
+		return false
+	}
+	s.reservedPorts.Insert(port)
+	return true
+}
+
+// getUnusedPort returns a free port from the range and returns its number and nil value
+// the port is not allocated so the consumer should allocate it explicitly calling allocatePort()
+// if none is available then it returns -1 and error
+func (s *staticPortRange) getUnusedPort() (int32, error) {
+	s.Lock()
+	defer s.Unlock()
+	// start in a random offset
+	start := rand.Int31n(s.length)
+	for i := int32(0); i < s.length; i++ {
+		port := s.baseport + (start+i)%(s.length)
+		if !s.reservedPorts.Has(port) {
+			return port, nil
+		}
+	}
+	return -1, fmt.Errorf("no free ports were found")
+}
+
+// releasePort releases the port passed as an argument
+func (s *staticPortRange) releasePort(port int32) {
+	s.Lock()
+	defer s.Unlock()
+	s.reservedPorts.Delete(port)
+}
+
+// GetUnusedStaticNodePort returns a free port in static range and a nil value
+// If no port in static range is available it returns -1 and an error value
+// Note that it is not guaranteed that the returned port is actually available on the apiserver;
+// You must allocate a port, then attempt to create the service, then call
+// ReserveStaticNodePort.
+func GetUnusedStaticNodePort() (int32, error) {
+	return staticPortAllocator.getUnusedPort()
+}
+
+// ReserveStaticNodePort reserves the port provided as input. It is guaranteed
+// that no other test will receive this port from GetUnusedStaticNodePort until
+// after you call ReleaseStaticNodePort.
+//
+// port must have been previously allocated by GetUnusedStaticNodePort, and
+// then successfully used as a NodePort or HealthCheckNodePort when creating
+// a service. Trying to reserve a port that was not allocated by
+// GetUnusedStaticNodePort, or reserving it before creating the associated service
+// may cause other e2e tests to fail.
+//
+// If an invalid port was provided or if the port is already reserved, it returns false
+func ReserveStaticNodePort(port int32) bool {
+	return staticPortAllocator.reservePort(port)
+}
+
+// ReleaseStaticNodePort releases the specified port.
+// The corresponding service should have already been deleted, to ensure that the
+// port allocator doesn't try to reuse it before the apiserver considers it available.
+func ReleaseStaticNodePort(port int32) {
+	staticPortAllocator.releasePort(port)
 }
 
 // newServiceTemplate returns the default v1.Service template for this j, but
