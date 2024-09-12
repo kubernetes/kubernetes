d994895dc4580101f17661eb573fc282d4b8517d
xuzhenglun <xuzhenglun@gmail.com>
2024-09-12 16:59:08 +0800

    add test coverage for cohabitating resources in StroageFactory

--- staging/src/k8s.io/apiserver/pkg/apis/example2/install/install.go
+++ staging/src/k8s.io/apiserver/pkg/apis/example2/install/install.go
@@ -22,12 +22,14 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apiserver/pkg/apis/example"
+	"k8s.io/apiserver/pkg/apis/example2"
 	example2v1 "k8s.io/apiserver/pkg/apis/example2/v1"
 )
 
 // Install registers the API group and adds types to a scheme
 func Install(scheme *runtime.Scheme) {
 	utilruntime.Must(example.AddToScheme(scheme))
+	utilruntime.Must(example2.AddToScheme(scheme))
 	utilruntime.Must(example2v1.AddToScheme(scheme))
 	utilruntime.Must(scheme.SetVersionPriority(example2v1.SchemeGroupVersion))
 }
