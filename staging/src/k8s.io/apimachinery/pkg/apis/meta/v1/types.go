5f50af85d9b68bc6deb231a30c450276eecac7e1
Abu Kashem <akashem@redhat.com>
2024-09-23 12:22:53 -0400

    api: add a new field to meta/v1 DeleteOptions
    add a new boolean field
    IgnoreStoreReadErrorWithClusterBreakingPotential to meta/v1 DeleteOptions

--- staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/types.go
+++ staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/types.go
@@ -560,6 +560,23 @@ type DeleteOptions struct {
 	// +optional
 	// +listType=atomic
 	DryRun []string `json:"dryRun,omitempty" protobuf:"bytes,5,rep,name=dryRun"`
+
+	// IgnoreStoreReadErrorWithClusterBreakingPotential, if set to true,
+	// will trigger an unsafe deletion of the resource in case the
+	// normal deletion flow fails with a corrupt object error.
+	// A resource is considered corrupt if it can not be retrieved from
+	// the underlying storage because of a) its data can not be transformed
+	// successfully, or b) it fails to decode into an object.
+	// NOTE: unsafe deletion ignores finalzer constraints, skips
+	// precondition checks, or any post deletion hooks and
+	// removes the object from the storage.
+	// WARNING: This will break the cluster if the resource has any
+	// dependencies. Use only if you REALLY know what you are doing.
+	// WARNING: Vendor(s) will most likely consider enablement of this
+	// option to be in violation of the support of their product.
+	// The default value is nil, and the user must opt in to enable it
+	// +optional
+	IgnoreStoreReadErrorWithClusterBreakingPotential *bool `json:"ignoreStoreReadErrorWithClusterBreakingPotential,omitempty" protobuf:"varint,6,opt,name=ignoreStoreReadErrorWithClusterBreakingPotential"`
 }
 
 const (
