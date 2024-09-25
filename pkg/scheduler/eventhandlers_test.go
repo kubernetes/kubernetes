afeb8edc83e43f5c9283ef60c58be7f03642cb7b
carlory <baofa.fan@daocloud.io>
2024-09-25 14:16:31 +0800

    make each scheduler test independent
    Signed-off-by: carlory <baofa.fan@daocloud.io>

--- pkg/scheduler/eventhandlers_test.go
+++ pkg/scheduler/eventhandlers_test.go
@@ -48,7 +48,6 @@ import (
 	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/nodename"
 	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/nodeports"
 	"k8s.io/kubernetes/pkg/scheduler/framework/plugins/noderesources"
-	"k8s.io/kubernetes/pkg/scheduler/metrics"
 	st "k8s.io/kubernetes/pkg/scheduler/testing"
 	"k8s.io/kubernetes/pkg/scheduler/util/assumecache"
 )
@@ -56,7 +55,6 @@ import (
 func TestUpdatePodInCache(t *testing.T) {
 	ttl := 10 * time.Second
 	nodeName := "node"
-	metrics.Register()
 
 	tests := []struct {
 		name   string
