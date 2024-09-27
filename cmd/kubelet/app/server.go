38fe239ac44050c2ed83ffb8ea7e514db71f903f
elmiko <msm@opbstudios.com>
2024-09-27 10:03:18 -0400

    factor our cloudprovider.DeprecationWarningForProvider
    this change removes the deprecation warning function in favor of using
    the `cloudprovider.DisableWarningForProvider`. it also fixes some of the
    logic to ensure that non-external providers are properly detected and
    warned about.

--- cmd/kubelet/app/server.go
+++ cmd/kubelet/app/server.go
@@ -651,16 +651,10 @@ func run(ctx context.Context, s *options.KubeletServer, kubeDeps *kubelet.Depend
 	}
 
 	if kubeDeps.Cloud == nil {
-		if !cloudprovider.IsExternal(s.CloudProvider) {
-			cloudprovider.DeprecationWarningForProvider(s.CloudProvider)
-			cloud, err := cloudprovider.InitCloudProvider(s.CloudProvider, s.CloudConfigFile)
-			if err != nil {
-				return err
-			}
-			if cloud != nil {
-				klog.V(2).InfoS("Successfully initialized cloud provider", "cloudProvider", s.CloudProvider, "cloudConfigFile", s.CloudConfigFile)
-			}
-			kubeDeps.Cloud = cloud
+		if !cloudprovider.IsExternal(s.CloudProvider) && len(s.CloudProvider) != 0 {
+			// internal cloud provider loops are disabled
+			cloudprovider.DisableWarningForProvider(s.CloudProvider)
+			return cloudprovider.ErrorForDisabledProvider(s.CloudProvider)
 		}
 	}
 
