e8de5b3199124a62c9feee56113c9ad481b7e9df
Abu Kashem <akashem@redhat.com>
2024-10-07 11:38:46 -0400

    add admission for unsafe delete
    allow unsafe deletion of a corrupt object via delete option
    'ignoreStoreReadErrorWithClusterBreakingPotential' only when
    the user has permission to do 'delete-ignore-read-errors'
    on the resource being deleted

--- test/integration/controlplane/transformation/secrets_transformation_test.go
+++ test/integration/controlplane/transformation/secrets_transformation_test.go
@@ -30,15 +30,20 @@ import (
 	"testing"
 	"time"
 
+	rbacv1 "k8s.io/api/rbac/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/wait"
 	apiserverv1 "k8s.io/apiserver/pkg/apis/apiserver/v1"
 	genericfeatures "k8s.io/apiserver/pkg/features"
 	"k8s.io/apiserver/pkg/storage/value"
 	aestransformer "k8s.io/apiserver/pkg/storage/value/encrypt/aes"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
+	clientset "k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
 	featuregatetesting "k8s.io/component-base/featuregate/testing"
+	"k8s.io/kubernetes/test/integration/authutil"
 	"k8s.io/utils/ptr"
 )
 
@@ -132,7 +137,12 @@ type allowUnsafeMalformedObjectDeletionTestRunner struct {
 	corrupObjDeletNoIgnore verifier
 	// what we expect for DELETE on the corrupt object, after encryption has
 	// broken, with the option to ignore store read error enabled
-	corrupObjDelete verifier
+	// the user does not have the privilege to do unsafe delete
+	corrupObjDeleteWithoutPrivilege verifier
+	// what we expect for DELETE on the corrupt object, after encryption has
+	// broken, with the option to ignore store read error enabled
+	// this time, the user has the permission to do unsafe delete
+	corrupObjDeleteWithPrivilege verifier
 	// what we expect for GET on the corrupt object (post deletion)
 	corrupObjFinalGet verifier
 }
@@ -150,8 +160,12 @@ func TestAllowUnsafeMalformedObjectDeletionFeature(t *testing.T) {
 				},
 				corruptObjGet:          wantAPIStatusError{reason: metav1.StatusReasonInternalError},
 				corrupObjDeletNoIgnore: wantAPIStatusError{reason: metav1.StatusReasonInternalError},
-				corrupObjDelete:        wantNoError{},
-				corrupObjFinalGet:      wantAPIStatusError{reason: metav1.StatusReasonNotFound},
+				corrupObjDeleteWithoutPrivilege: wantAPIStatusError{
+					reason:          metav1.StatusReasonForbidden,
+					messageContains: `user not permitted to do "delete-ignore-read-errors"`,
+				},
+				corrupObjDeleteWithPrivilege: wantNoError{},
+				corrupObjFinalGet:            wantAPIStatusError{reason: metav1.StatusReasonNotFound},
 			},
 		},
 		{
@@ -160,10 +174,11 @@ func TestAllowUnsafeMalformedObjectDeletionFeature(t *testing.T) {
 				encryptionBrokenFn: func(t *testing.T, got apierrors.APIStatus) bool {
 					return got.Status().Reason == metav1.StatusReasonInternalError
 				},
-				corruptObjGet:          wantAPIStatusError{reason: metav1.StatusReasonInternalError},
-				corrupObjDeletNoIgnore: wantAPIStatusError{reason: metav1.StatusReasonInternalError},
-				corrupObjDelete:        wantAPIStatusError{reason: metav1.StatusReasonInternalError},
-				corrupObjFinalGet:      wantAPIStatusError{reason: metav1.StatusReasonInternalError},
+				corruptObjGet:                   wantAPIStatusError{reason: metav1.StatusReasonInternalError},
+				corrupObjDeletNoIgnore:          wantAPIStatusError{reason: metav1.StatusReasonInternalError},
+				corrupObjDeleteWithoutPrivilege: wantAPIStatusError{reason: metav1.StatusReasonInternalError},
+				corrupObjDeleteWithPrivilege:    wantAPIStatusError{reason: metav1.StatusReasonInternalError},
+				corrupObjFinalGet:               wantAPIStatusError{reason: metav1.StatusReasonInternalError},
 			},
 		},
 	}
@@ -212,14 +227,33 @@ func (wantError wantAPIStatusError) verify(t *testing.T, err error) {
 }
 
 func (testrunner allowUnsafeMalformedObjectDeletionTestRunner) Run(t *testing.T) {
-	test, err := newTransformTest(t, aesGCMConfigYAML, true, "", nil, false)
+	test, err := newTransformTest(t, aesGCMConfigYAML, true, "", nil, testrunner.corruptObjDeleteEnabled)
 	if err != nil {
 		t.Fatalf("failed to setup test for envelop %s, error was %v", aesGCMPrefix, err)
 	}
 	defer test.cleanUp()
 
+	// a) set up a distinct client for the test user with the least
+	// privileges, we will grant permission as we progress through the test
+	testUser := "crock"
+	testUserConfig := restclient.CopyConfig(test.kubeAPIServer.ClientConfig)
+	testUserConfig.Impersonate.UserName = testUser
+	testUserClient := clientset.NewForConfigOrDie(testUserConfig)
+	adminClient := test.restClient
+
+	// b) use the admin client to grant the the test user initial permissions,
+	// we are not going to grant 'delete-ignore-read-errors' just yet
+	permitUserToDoVerbOnSecret(t, adminClient, testUser, testNamespace, []string{"create", "get", "delete", "update"})
+
+	// the test should not use the admin client going forward
+	test.restClient = testUserClient
+	defer func() {
+		// for any cleanup that requires admin privileges
+		test.restClient = adminClient
+	}()
+
 	secretA := "unsafe-delete"
-	// a) delete and recreate this secret, we don't expect any error
+	// c) delete and recreate this secret, we don't expect any error
 	_, err = test.createSecret(secretA, testNamespace)
 	if err != nil {
 		t.Fatalf("'%s/%s' failed to create, got error: %v", err, testNamespace, secretA)
@@ -229,13 +263,13 @@ func (testrunner allowUnsafeMalformedObjectDeletionTestRunner) Run(t *testing.T)
 		t.Fatalf("'%s/%s' failed to delete, got error: %v", err, testNamespace, secretA)
 	}
 
-	// b) re-create the secret
+	// d) re-create the secret
 	test.secret, err = test.createSecret(secretA, testNamespace)
 	if err != nil {
 		t.Fatalf("Failed to create test secret, error: %v", err)
 	}
 
-	// c) update the secret with a finalizer
+	// e) update the secret with a finalizer
 	withFinalizer := test.secret.DeepCopy()
 	withFinalizer.Finalizers = append(withFinalizer.Finalizers, "tes.k8s.io/fake")
 	test.secret, err = test.restClient.CoreV1().Secrets(testNamespace).Update(context.Background(), withFinalizer, metav1.UpdateOptions{})
@@ -245,8 +279,8 @@ func (testrunner allowUnsafeMalformedObjectDeletionTestRunner) Run(t *testing.T)
 
 	test.runResource(test.TContext, unSealWithGCMTransformer, aesGCMPrefix, "", "v1", "secrets", test.secret.Name, test.secret.Namespace)
 
-	// d) override the config and break decryption of the old resources,
-	// the secret created in step b will be undecryptable
+	// f) override the config and break decryption of the old resources,
+	// the secret created in step d will be undecryptable
 	encryptionConf := filepath.Join(test.configDir, encryptionConfigFileName)
 	body, _ := ioutil.ReadFile(encryptionConf)
 	t.Logf("file before write: %s", body)
@@ -256,7 +290,7 @@ func (testrunner allowUnsafeMalformedObjectDeletionTestRunner) Run(t *testing.T)
 	body, _ = ioutil.ReadFile(encryptionConf)
 	t.Logf("file after write: %s", body)
 
-	// e) wait for the breaking changes to take effect
+	// g) wait for the breaking changes to take effect
 	testCtx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	err = wait.PollUntilContextTimeout(testCtx, 1*time.Second, 2*time.Minute, true, func(ctx context.Context) (done bool, err error) {
@@ -274,7 +308,7 @@ func (testrunner allowUnsafeMalformedObjectDeletionTestRunner) Run(t *testing.T)
 		t.Fatalf("encryption never broke: %v", err)
 	}
 
-	// f) create a new secret, and then delete it, it should work
+	// h) create a new secret, and then delete it, it should work
 	secretB := "normal-delete"
 	_, err = test.createSecret(secretB, testNamespace)
 	if err != nil {
@@ -285,29 +319,86 @@ func (testrunner allowUnsafeMalformedObjectDeletionTestRunner) Run(t *testing.T)
 		t.Fatalf("'%s/%s' failed to create, got error: %v", err, testNamespace, secretB)
 	}
 
-	// g) let's try to get the broken secret created in step b, we expect it
+	// i) let's try to get the broken secret created in step d, we expect it
 	// to fail, the error will vary depending on whether the feature is enabled
 	_, err = test.restClient.CoreV1().Secrets(testNamespace).Get(context.Background(), secretA, metav1.GetOptions{})
 	testrunner.corruptObjGet.verify(t, err)
 
-	// h) let's try the normal deletion flow, we expect an error
+	// j) let's try the normal deletion flow, we expect an error
 	err = test.restClient.CoreV1().Secrets(testNamespace).Delete(context.Background(), secretA, metav1.DeleteOptions{})
 	testrunner.corrupObjDeletNoIgnore.verify(t, err)
 
-	// i) let's make an attempt to trigger an unsafe deletion flow, it
+	// k) let's make an attempt to trigger an unsafe deletion flow, it
 	// will work if all of the followings are true:
 	//  1) the feature is enabled
 	//  2) the user specifies the IgnoreStoreReadErrorWithClusterBreakingPotential delete option
 	//  3) normal deletion is attempted, but the error represents a corrupt object
+	//
+	// on the other hand, we have not granted the test user permission to use
+	// the 'delete-ignore-read-errors' verb, so we expect the admission plugin
+	// to deny the unsafe delete request
 	options := metav1.DeleteOptions{
 		IgnoreStoreReadErrorWithClusterBreakingPotential: ptr.To[bool](true),
 	}
 	err = test.restClient.CoreV1().Secrets(testNamespace).Delete(context.Background(), secretA, options)
-	testrunner.corrupObjDelete.verify(t, err)
+	testrunner.corrupObjDeleteWithoutPrivilege.verify(t, err)
 
-	// j) final get should return a NotFound error after the secret has been deleted
+	// l) grant the test user to do 'delete-ignore-read-errors' on secrets
+	permitUserToDoVerbOnSecret(t, adminClient, testUser, testNamespace, []string{"delete-ignore-read-errors"})
+
+	// m) let's try to do unsafe delete again
+	err = test.restClient.CoreV1().Secrets(testNamespace).Delete(context.Background(), secretA, options)
+	testrunner.corrupObjDeleteWithPrivilege.verify(t, err)
+
+	// n) final get should return a NotFound error after the secret has been deleted
 	_, err = test.restClient.CoreV1().Secrets(testNamespace).Get(context.Background(), secretA, metav1.GetOptions{})
 	testrunner.corrupObjFinalGet.verify(t, err)
+}
+
+func permitUserToDoVerbOnSecret(t *testing.T, client *clientset.Clientset, user, namespace string, verbs []string) {
+	t.Helper()
+
+	name := fmt.Sprintf("%s-can-do-%s-on-secrets-in-%s", user, strings.Join(verbs, "-"), namespace)
+	_, err := client.RbacV1().Roles(namespace).Create(context.TODO(), &rbacv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				Verbs:     verbs,
+				APIGroups: []string{""},
+				Resources: []string{"secrets"},
+			},
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("error while creating role: %s, err: %v", name, err)
+	}
+
+	_, err = client.RbacV1().RoleBindings(namespace).Create(context.TODO(), &rbacv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind: rbacv1.UserKind,
+				Name: user,
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: rbacv1.GroupName,
+			Kind:     "Role",
+			Name:     name,
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		t.Fatalf("error while creating role binding: %s, err: %v", name, err)
+	}
+
+	authutil.WaitForNamedAuthorizationUpdate(t, context.TODO(), client.AuthorizationV1(),
+		user, namespace, verbs[0], "", schema.GroupResource{Resource: "secrets"}, true)
 }
 
 // Baseline (no enveloping) - use to contrast with enveloping benchmarks.
