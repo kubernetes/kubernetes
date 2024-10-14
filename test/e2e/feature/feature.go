da590321984bfd0127f04957ae24921039e7b044
AxeZhan <kid071999@gmail.com>
2024-10-14 14:01:51 +0800

    Graduate PodLifecycleSleepAction to GA

--- test/e2e/feature/feature.go
+++ test/e2e/feature/feature.go
@@ -253,9 +253,6 @@ var (
 	PodGarbageCollector = framework.WithFeature(framework.ValidFeatures.Add("PodGarbageCollector"))
 
 	// TODO: document the feature (owning SIG, when to use this feature for a test)
-	PodLifecycleSleepAction = framework.WithFeature(framework.ValidFeatures.Add("PodLifecycleSleepAction"))
-
-	// TODO: document the feature (owning SIG, when to use this feature for a test)
 	PodPriority = framework.WithFeature(framework.ValidFeatures.Add("PodPriority"))
 
 	// TODO: document the feature (owning SIG, when to use this feature for a test)
