9c66e8c242a644a6d2374315d5b14d5201b57da6
Joe Betz <jpbetz@google.com>
2024-10-14 17:03:40 -0400

    Add MutatingAdmissionPolicy API
    This is closely aligned with ValidatingAdmissionPolicy
    except that instead of validations that can fail with
    messages, there are mutations, which can be defined
    either with as an ApplyConfiguration or JSONPatch.
    
    Co-authored-by: cici37 <cicih@google.com>

--- pkg/apis/admissionregistration/fuzzer/fuzzer.go
+++ pkg/apis/admissionregistration/fuzzer/fuzzer.go
@@ -107,5 +107,28 @@ var Funcs = func(codecs runtimeserializer.CodecFactory) []interface{} {
 				obj.ParameterNotFoundAction = &v
 			}
 		},
+		func(obj *admissionregistration.MutatingAdmissionPolicySpec, c fuzz.Continue) {
+			c.FuzzNoCustom(obj) // fuzz self without calling this function again
+			if obj.FailurePolicy == nil {
+				p := admissionregistration.FailurePolicyType("Fail")
+				obj.FailurePolicy = &p
+			}
+			obj.ReinvocationPolicy = admissionregistration.NeverReinvocationPolicy
+		},
+		func(obj *admissionregistration.Mutation, c fuzz.Continue) {
+			c.FuzzNoCustom(obj) // fuzz self without calling this function again
+			patchTypes := []admissionregistration.PatchType{admissionregistration.PatchTypeJSONPatch, admissionregistration.PatchTypeApplyConfiguration}
+			obj.PatchType = patchTypes[c.Rand.Intn(len(patchTypes))]
+			if obj.PatchType == admissionregistration.PatchTypeJSONPatch {
+				obj.JSONPatch = &admissionregistration.JSONPatch{}
+				c.Fuzz(&obj.JSONPatch)
+				obj.ApplyConfiguration = nil
+			}
+			if obj.PatchType == admissionregistration.PatchTypeApplyConfiguration {
+				obj.ApplyConfiguration = &admissionregistration.ApplyConfiguration{}
+				c.Fuzz(obj.ApplyConfiguration)
+				obj.JSONPatch = nil
+			}
+		},
 	}
 }
