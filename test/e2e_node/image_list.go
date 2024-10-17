6d0b62a84d19be3a54cc9c20c2666889cf88c9a2
bzsuni <bingzhe.sun@daocloud.io>
2024-10-17 13:49:51 +0800

    Update npd from v0.8.19 to v0.8.20
    Signed-off-by: bzsuni <bingzhe.sun@daocloud.io>

--- test/e2e_node/image_list.go
+++ test/e2e_node/image_list.go
@@ -99,7 +99,7 @@ func isRunningOnArm64() bool {
 }
 
 func getNodeProblemDetectorImage() string {
-	const defaultImage string = "registry.k8s.io/node-problem-detector/node-problem-detector:v0.8.19"
+	const defaultImage string = "registry.k8s.io/node-problem-detector/node-problem-detector:v0.8.20"
 	image := os.Getenv("NODE_PROBLEM_DETECTOR_IMAGE")
 	if image == "" {
 		image = defaultImage
