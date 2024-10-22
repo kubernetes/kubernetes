klearn1/kuberctl-management

tests: Ports volume unit tests to Windows
Ports the atomic_writer unit tests to Windows.

pkg/kubelet/container/os.go
@@ -20,6 +20,8 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
+	volumeutil "k8s.io/kubernetes/pkg/volume/util"
 )
 
 // OSInterface collects system level operations that need to be mocked out
@@ -79,7 +81,7 @@ func (RealOS) Create(path string) (*os.File, error) {
 // Chmod will change the permissions on the specified path or return
 // an error.
 func (RealOS) Chmod(path string, perm os.FileMode) error {
-	return os.Chmod(path, perm)
+	return volumeutil.Chmod(path, perm)
 }
 
 // Hostname will call os.Hostname to return the hostname.

pkg/kubelet/nodeshutdown/storage.go
@@ -22,6 +22,8 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
+	volumeutil "k8s.io/kubernetes/pkg/volume/util"
 )
 
 type storage interface {
@@ -70,7 +72,7 @@ func atomicWrite(filename string, data []byte, perm os.FileMode) error {
 	if err != nil {
 		return err
 	}
-	err = os.Chmod(f.Name(), perm)
+	err = volumeutil.Chmod(f.Name(), perm)
 	if err != nil {
 		f.Close()
 		return err

pkg/volume/csi/csi_attacher_test.go
@@ -25,7 +25,6 @@ import (
 	"os/user"
 	"path/filepath"
 	"reflect"
-	goruntime "runtime"
 	"sync"
 	"testing"
 	"time"
@@ -45,6 +44,7 @@ import (
 	"k8s.io/kubernetes/pkg/features"
 	"k8s.io/kubernetes/pkg/volume"
 	fakecsi "k8s.io/kubernetes/pkg/volume/csi/fake"
+	volutil "k8s.io/kubernetes/pkg/volume/util"
 	volumetypes "k8s.io/kubernetes/pkg/volume/util/types"
 )
 
@@ -1110,7 +1110,6 @@ func TestAttacherMountDevice(t *testing.T) {
 		delegateFSGroupFeatureGate     bool
 		driverSupportsVolumeMountGroup bool
 		shouldFail                     bool
-		skipOnWindows                  bool
 		createAttachment               bool
 		populateDeviceMountPath        bool
 		exitError                      error
@@ -1214,12 +1213,7 @@ func TestAttacherMountDevice(t *testing.T) {
 			createAttachment:        true,
 			populateDeviceMountPath: true,
 			shouldFail:              true,
-			// NOTE: We're skipping this test on Windows because os.Chmod is not working as intended, which means that
-			// this test won't fail on Windows due to permission denied errors.
-			// TODO: Remove the skip once Windows file permissions support is added.
-			// https://github.com/kubernetes/kubernetes/pull/110921
-			skipOnWindows: true,
-			spec:          volume.NewSpecFromPersistentVolume(makeTestPV(pvName, 10, testDriver, "test-vol1"), true),
+			spec:                    volume.NewSpecFromPersistentVolume(makeTestPV(pvName, 10, testDriver, "test-vol1"), true),
 		},
 		{
 			testName:                       "fsgroup provided, DelegateFSGroupToCSIDriver feature enabled, driver supports volume mount group; expect fsgroup to be passed to NodeStageVolume",
@@ -1284,9 +1278,6 @@ func TestAttacherMountDevice(t *testing.T) {
 			}
 		}
 		t.Run(tc.testName, func(t *testing.T) {
-			if tc.skipOnWindows && goruntime.GOOS == "windows" {
-				t.Skipf("Skipping test case on Windows: %s", tc.testName)
-			}
 			t.Logf("Running test case: %s", tc.testName)
 
 			defer featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.DelegateFSGroupToCSIDriver, tc.delegateFSGroupFeatureGate)()
@@ -1333,7 +1324,7 @@ func TestAttacherMountDevice(t *testing.T) {
 				if err != nil {
 					t.Errorf("error attempting to populate file on parent path: %v", err)
 				}
-				err = os.Chmod(parent, 0555)
+				err = volutil.Chmod(parent, 0555)
 				if err != nil {
 					t.Errorf("error attempting to modify directory permissions: %v", err)
 				}
@@ -1362,7 +1353,7 @@ func TestAttacherMountDevice(t *testing.T) {
 					if os.IsNotExist(err) {
 						t.Errorf("expecting file to exist after err received: %v", err)
 					}
-					err = os.Chmod(parent, 0777)
+					err = volutil.Chmod(parent, 0777)
 					if err != nil {
 						t.Errorf("failed to modify permissions after test: %v", err)
 					}

pkg/volume/downwardapi/downwardapi_test.go
@@ -33,6 +33,7 @@ import (
 	"k8s.io/kubernetes/pkg/volume"
 	"k8s.io/kubernetes/pkg/volume/emptydir"
 	volumetest "k8s.io/kubernetes/pkg/volume/testing"
+	volutil "k8s.io/kubernetes/pkg/volume/util"
 )
 
 const (
@@ -175,9 +176,9 @@ func TestDownwardAPI(t *testing.T) {
 		{
 			name:  "test_item_mode",
 			files: map[string]string{"name_file_name": "metadata.name"},
-			modes: map[string]int32{"name_file_name": 0400},
+			modes: map[string]int32{"name_file_name": 0600},
 			steps: []testStep{
-				verifyMode{stepName{"name_file_name"}, 0400},
+				verifyMode{stepName{"name_file_name"}, 0600},
 			},
 		},
 	}
@@ -350,13 +351,21 @@ type verifyMode struct {
 }
 
 func (step verifyMode) run(test *downwardAPITest) {
-	fileInfo, err := os.Stat(filepath.Join(test.volumePath, step.name))
+	p := filepath.Join(test.volumePath, step.name)
+
+	// For the Windows case, we should evaluate the file mode of the target file, rather
+	// that the symlink's.
+	p, err := filepath.EvalSymlinks(p)
 	if err != nil {
 		test.t.Errorf(err.Error())
 		return
 	}
 
-	actualMode := fileInfo.Mode()
+	actualMode, err := volutil.GetFileMode(p)
+	if err != nil {
+		test.t.Errorf(err.Error())
+		return
+	}
 	expectedMode := os.FileMode(step.expectedMode)
 	if actualMode != expectedMode {
 		test.t.Errorf("Found mode `%v` expected %v", actualMode, expectedMode)

kubernetes/kubernetes

pkg/kubelet/container/os.go
@@ -20,6 +20,8 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
+	filesystem "k8s.io/kubernetes/pkg/util/filesystem"
 )
 
 // OSInterface collects system level operations that need to be mocked out
@@ -79,7 +81,7 @@ func (RealOS) Create(path string) (*os.File, error) {
 // Chmod will change the permissions on the specified path or return
 // an error.
 func (RealOS) Chmod(path string, perm os.FileMode) error {
-	return os.Chmod(path, perm)
+	return filesystem.Chmod(path, perm)
 }
 
 // Hostname will call os.Hostname to return the hostname.

pkg/kubelet/nodeshutdown/storage.go
@@ -22,6 +22,8 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
+	filesystem "k8s.io/kubernetes/pkg/util/filesystem"
 )
 
 type storage interface {
@@ -70,7 +72,7 @@ func atomicWrite(filename string, data []byte, perm os.FileMode) error {
 	if err != nil {
 		return err
 	}
-	err = os.Chmod(f.Name(), perm)
+	err = filesystem.Chmod(f.Name(), perm)
 	if err != nil {
 		f.Close()
 		return err

pkg/volume/csi/csi_attacher_test.go
@@ -24,7 +24,6 @@ import (
 	"os/user"
 	"path/filepath"
 	"reflect"
-	goruntime "runtime"
 	"sync"
 	"testing"
 	"time"
@@ -39,6 +38,7 @@ import (
 	clientset "k8s.io/client-go/kubernetes"
 	fakeclient "k8s.io/client-go/kubernetes/fake"
 	core "k8s.io/client-go/testing"
+	filesystem "k8s.io/kubernetes/pkg/util/filesystem"
 	"k8s.io/kubernetes/pkg/volume"
 	fakecsi "k8s.io/kubernetes/pkg/volume/csi/fake"
 	volumetypes "k8s.io/kubernetes/pkg/volume/util/types"
@@ -991,7 +991,6 @@ func TestAttacherMountDevice(t *testing.T) {
 		expectedVolumeMountGroup       string
 		driverSupportsVolumeMountGroup bool
 		shouldFail                     bool
-		skipOnWindows                  bool
 		createAttachment               bool
 		populateDeviceMountPath        bool
 		exitError                      error
@@ -1096,12 +1095,7 @@ func TestAttacherMountDevice(t *testing.T) {
 			createAttachment:        true,
 			populateDeviceMountPath: true,
 			shouldFail:              true,
-			// NOTE: We're skipping this test on Windows because os.Chmod is not working as intended, which means that
-			// this test won't fail on Windows due to permission denied errors.
-			// TODO: Remove the skip once Windows file permissions support is added.
-			// https://github.com/kubernetes/kubernetes/pull/110921
-			skipOnWindows: true,
-			spec:          volume.NewSpecFromPersistentVolume(makeTestPV(pvName, 10, testDriver, "test-vol1"), true),
+			spec:                    volume.NewSpecFromPersistentVolume(makeTestPV(pvName, 10, testDriver, "test-vol1"), true),
 		},
 		{
 			testName:                       "fsgroup provided, driver supports volume mount group; expect fsgroup to be passed to NodeStageVolume",
@@ -1164,9 +1158,6 @@ func TestAttacherMountDevice(t *testing.T) {
 			}
 		}
 		t.Run(tc.testName, func(t *testing.T) {
-			if tc.skipOnWindows && goruntime.GOOS == "windows" {
-				t.Skipf("Skipping test case on Windows: %s", tc.testName)
-			}
 			t.Logf("Running test case: %s", tc.testName)
 
 			// Setup
@@ -1213,7 +1204,7 @@ func TestAttacherMountDevice(t *testing.T) {
 				if err != nil {
 					t.Errorf("error attempting to populate file on parent path: %v", err)
 				}
-				err = os.Chmod(parent, 0555)
+				err = filesystem.Chmod(parent, 0555)
 				if err != nil {
 					t.Errorf("error attempting to modify directory permissions: %v", err)
 				}
@@ -1242,7 +1233,7 @@ func TestAttacherMountDevice(t *testing.T) {
 					if os.IsNotExist(err) {
 						t.Errorf("expecting file to exist after err received: %v", err)
 					}
-					err = os.Chmod(parent, 0777)
+					err = filesystem.Chmod(parent, 0777)
 					if err != nil {
 						t.Errorf("failed to modify permissions after test: %v", err)
 					}

pkg/volume/downwardapi/downwardapi_test.go
@@ -30,6 +30,7 @@ import (
 	"k8s.io/client-go/kubernetes/fake"
 	utiltesting "k8s.io/client-go/util/testing"
 	"k8s.io/kubernetes/pkg/fieldpath"
+	filesystem "k8s.io/kubernetes/pkg/util/filesystem"
 	"k8s.io/kubernetes/pkg/volume"
 	"k8s.io/kubernetes/pkg/volume/emptydir"
 	volumetest "k8s.io/kubernetes/pkg/volume/testing"
@@ -180,9 +181,9 @@ func TestDownwardAPI(t *testing.T) {
 		{
 			name:  "test_item_mode",
 			files: map[string]string{"name_file_name": "metadata.name"},
-			modes: map[string]int32{"name_file_name": 0400},
+			modes: map[string]int32{"name_file_name": 0600},
 			steps: []testStep{
-				verifyMode{stepName{"name_file_name"}, 0400},
+				verifyMode{stepName{"name_file_name"}, 0600},
 			},
 		},
 	}
@@ -355,13 +356,21 @@ type verifyMode struct {
 }
 
 func (step verifyMode) run(test *downwardAPITest) {
-	fileInfo, err := os.Stat(filepath.Join(test.volumePath, step.name))
+	p := filepath.Join(test.volumePath, step.name)
+
+	// For the Windows case, we should evaluate the file mode of the target file, rather
+	// that the symlink's.
+	p, err := filepath.EvalSymlinks(p)
 	if err != nil {
 		test.t.Error(err.Error())
 		return
 	}
 
-	actualMode := fileInfo.Mode()
+	actualMode, err := filesystem.GetFileMode(p)
+	if err != nil {
+		test.t.Errorf(err.Error())
+		return
+	}
 	expectedMode := os.FileMode(step.expectedMode)
 	if actualMode != expectedMode {
 		test.t.Errorf("Found mode `%v` expected %v", actualMode, expectedMode)