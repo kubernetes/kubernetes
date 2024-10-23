eecd2ed7a2cb01f7f78bea869a9bca673bbc10ed
Kir Kolyshkin <kolyshkin@gmail.com>
2024-10-22 17:01:28 -0700

    staging/src/k8s.io/mount-utils: use moby/sys/userns
    This package was moved out from runc/libcontainer and it will be made
    deprecated.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- staging/src/k8s.io/mount-utils/mount_linux.go
+++ staging/src/k8s.io/mount-utils/mount_linux.go
@@ -35,7 +35,7 @@ import (
 	"github.com/moby/sys/mountinfo"
 	"golang.org/x/sys/unix"
 
-	libcontaineruserns "github.com/opencontainers/runc/libcontainer/userns"
+	inuserns "github.com/moby/sys/userns"
 	"k8s.io/klog/v2"
 	utilexec "k8s.io/utils/exec"
 )
@@ -147,7 +147,7 @@ func (mounter *Mounter) bindMountSensitive(mounterPath string, mountCmd string, 
 		return err
 	}
 	err = mounter.doMount(mounterPath, defaultMountCommand, source, target, fstype, bindRemountOpts, bindRemountOptsSensitive, mountFlags, systemdMountRequired)
-	if libcontaineruserns.RunningInUserNS() {
+	if inuserns.RunningInUserNS() {
 		if err == nil {
 			return nil
 		}
