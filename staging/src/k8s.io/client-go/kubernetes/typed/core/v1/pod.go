938f9f5e3c8090064e4f36b92467fa521de30e31
Anish Shah <anishshah@google.com>
2024-10-24 23:52:49 +0000

    update codegen

--- staging/src/k8s.io/client-go/kubernetes/typed/core/v1/pod.go
+++ staging/src/k8s.io/client-go/kubernetes/typed/core/v1/pod.go
@@ -97,6 +97,7 @@ func (c *pods) UpdateEphemeralContainers(ctx context.Context, podName string, po
 func (c *pods) Resize(ctx context.Context, podName string, pod *corev1.Pod, opts metav1.UpdateOptions) (result *corev1.Pod, err error) {
 	result = &corev1.Pod{}
 	err = c.GetClient().Put().
+		UseProtobufAsDefault().
 		Namespace(c.GetNamespace()).
 		Resource("pods").
 		Name(podName).
