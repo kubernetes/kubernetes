2c23db73118f958bd721151868011ac5480e89bf
Antonio Ojea <aojea@google.com>
2024-11-02 09:58:20 +0000

    don't panic inside e2e test

--- test/e2e/network/dns_common.go
+++ test/e2e/network/dns_common.go
@@ -508,7 +508,9 @@ func assertFilesContain(ctx context.Context, fileNames []string, fileDir string,
 		// grab logs from all the containers
 		for _, container := range pod.Spec.Containers {
 			logs, err := e2epod.GetPodLogs(ctx, client, pod.Namespace, pod.Name, container.Name)
-			framework.ExpectNoError(err)
+			if err != nil {
+				return false, fmt.Errorf("unexpected error getting pod client logs for %s: %v", container.Name, err)
+			}
 			framework.Logf("Pod client logs for %s: %s", container.Name, logs)
 		}
 
