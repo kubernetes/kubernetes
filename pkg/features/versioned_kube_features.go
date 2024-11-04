f7c46df6651c9d8a1b02d5fe154676df2163eab7
Filip Křepinský <atiratree@gmail.com>
2024-11-04 19:13:51 +0100

    api: add terminatingReplicas field to ReplicaSet and Deployment statuses
    - update internal ReplicaSet and Deployment type documentation to match with
      versioned API
    - made Replicaset and Deployment type documentation more consistent

--- pkg/features/versioned_kube_features.go
+++ pkg/features/versioned_kube_features.go
@@ -152,6 +152,10 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 		{Version: version.MustParse("1.32"), Default: true, LockToDefault: true, PreRelease: featuregate.GA},
 	},
 
+	DeploymentPodReplacementPolicy: {
+		{Version: version.MustParse("1.32"), Default: false, PreRelease: featuregate.Alpha},
+	},
+
 	DevicePluginCDIDevices: {
 		{Version: version.MustParse("1.28"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.29"), Default: true, PreRelease: featuregate.Beta},
