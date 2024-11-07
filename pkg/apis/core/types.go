8661f743a3898dc2d169ce5796810ad27bce2e52
Tim Allclair <tallclair@google.com>
2024-11-07 15:52:53 -0800

    Fix AllocatedResources feature gate annotation

--- pkg/apis/core/types.go
+++ pkg/apis/core/types.go
@@ -2811,7 +2811,7 @@ type ContainerStatus struct {
 	// AllocatedResources represents the compute resources allocated for this container by the
 	// node. Kubelet sets this value to Container.Resources.Requests upon successful pod admission
 	// and after successfully admitting desired pod resize.
-	// +featureGate=InPlacePodVerticalScaling
+	// +featureGate=InPlacePodVerticalScalingAllocatedStatus
 	// +optional
 	AllocatedResources ResourceList
 	// Resources represents the compute resource requests and limits that have been successfully
