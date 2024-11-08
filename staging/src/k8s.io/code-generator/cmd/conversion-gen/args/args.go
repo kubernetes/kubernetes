917aa9a8c4658a5cf870ab7e629a73534e3a1808
Vu Dinh <vudinh@outlook.com>
2024-11-08 00:58:39 -0500

    Remove deprecated extra-dirs flag
    The extra-dirs input is no longer in use. The extra-dirs flag, however, is still present
    even though it is not wired in conversion code. As a result, any extra dirs inputs are provided
    via extra-dirs flag are not taken in consideration for conversion-gen. This flag should be removed
    from args options to avoid confusion.
    
    Signed-off-by: Vu Dinh <vudinh@outlook.com>

--- staging/src/k8s.io/code-generator/cmd/conversion-gen/args/args.go
+++ staging/src/k8s.io/code-generator/cmd/conversion-gen/args/args.go
@@ -45,11 +45,6 @@ type Args struct {
 	// generator pick up manually written conversion funcs from external packages.
 	ExtraPeerDirs []string
 
-	// Additional dirs to parse and load, but not consider for peers.  This is
-	// useful when packages depend on other packages and want to call
-	// conversions across them.
-	ExtraDirs []string
-
 	// SkipUnsafe indicates whether to generate unsafe conversions to improve the efficiency
 	// of these operations. The unsafe operation is a direct pointer assignment via unsafe
 	// (within the allowed uses of unsafe) and is equivalent to a proposed Golang change to
@@ -84,8 +79,6 @@ func (args *Args) AddFlags(fs *pflag.FlagSet) {
 		"Comma-separated list of apimachinery import paths which are considered, after tag-specified peers, for conversions. Only change these if you have very good reasons.")
 	fs.StringSliceVar(&args.ExtraPeerDirs, "extra-peer-dirs", args.ExtraPeerDirs,
 		"Application specific comma-separated list of import paths which are considered, after tag-specified peers and base-peer-dirs, for conversions.")
-	fs.StringSliceVar(&args.ExtraDirs, "extra-dirs", args.ExtraDirs,
-		"Application specific comma-separated list of import paths which are loaded and considered for callable conversions, but are not considered peers for conversion.")
 	fs.BoolVar(&args.SkipUnsafe, "skip-unsafe", args.SkipUnsafe,
 		"If true, will not generate code using unsafe pointer conversions; resulting code may be slower.")
 	fs.StringVar(&args.GoHeaderFile, "go-header-file", "",
