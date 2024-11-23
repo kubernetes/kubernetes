8f72688ec2b518d0f27849c36ce15ae1bd6e651b
bachmanity1 <bachmanity138@gmail.com>
2024-11-24 01:06:04 +0900

    fix worker

--- pkg/kubelet/prober/worker.go
+++ pkg/kubelet/prober/worker.go
@@ -321,7 +321,7 @@ func (w *worker) doProbe(ctx context.Context) (keepGoing bool) {
 	}
 
 	// Note, exec probe does NOT have access to pod environment variables or downward API
-	result, err := w.probeManager.prober.probe(ctx, w.probeType, w.pod, status, w.container, w.containerID)
+	result, err := w.probeManager.prober.probe(ctx, w.probeType, w.pod, status, w.container, w.containerID, w.envVars)
 	if err != nil {
 		// Prober error, throw away the result.
 		return true
