f9b0bac662a835ea501dd7e167c3dbca1be36257
ningmingxiao <ning.mingxiao@zte.com.cn>
2024-11-29 22:37:54 +0800

    kubelet:support to print stack when receive signal
    Signed-off-by: ningmingxiao <ning.mingxiao@zte.com.cn>

--- staging/src/k8s.io/apiserver/pkg/server/signal_posix.go
+++ staging/src/k8s.io/apiserver/pkg/server/signal_posix.go
@@ -20,8 +20,34 @@ limitations under the License.
 package server
 
 import (
+	"context"
 	"os"
+	"os/signal"
 	"syscall"
+
+	"k8s.io/klog/v2"
 )
 
 var shutdownSignals = []os.Signal{os.Interrupt, syscall.SIGTERM}
+var signals = []os.Signal{os.Interrupt, syscall.SIGTERM, syscall.SIGUSR1}
+var handlers chan os.Signal
+
+func SetupSignalContextV2(name string) context.Context {
+	close(onlyOneSignalHandler) // panics when called twice
+	handlers = make(chan os.Signal, 3)
+	ctx, cancel := context.WithCancel(context.Background())
+	signal.Notify(handlers, signals...)
+	go func() {
+		for s := range handlers {
+			klog.Infof("received signal %v", s)
+			if s == syscall.SIGUSR1 {
+				dumpStacks(name)
+			} else {
+				cancel()
+				<-handlers
+				os.Exit(1) // second signal. Exit directly.
+			}
+		}
+	}()
+	return ctx
+}
