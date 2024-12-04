d5c50de8fef8b637b19d731a82faaa4080d47010
Davanum Srinivas <davanum@gmail.com>
2024-12-04 09:44:51 -0500

    Revert "delegate authn: don't default the ReqHeaders UID header"
    This reverts commit 26902de531620d2df5ce1bb572d2ea6965a7b7e9.
    
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- cmd/kube-controller-manager/app/options/options_test.go
+++ cmd/kube-controller-manager/app/options/options_test.go
@@ -431,7 +431,7 @@ func TestAddFlags(t *testing.T) {
 			ClientCert:          apiserveroptions.ClientCertAuthenticationOptions{},
 			RequestHeader: apiserveroptions.RequestHeaderAuthenticationOptions{
 				UsernameHeaders:     []string{"x-remote-user"},
-				UIDHeaders:          nil,
+				UIDHeaders:          []string{"x-remote-uid"},
 				GroupHeaders:        []string{"x-remote-group"},
 				ExtraHeaderPrefixes: []string{"x-remote-extra-"},
 			},
--- staging/src/k8s.io/cloud-provider/options/options_test.go
+++ staging/src/k8s.io/cloud-provider/options/options_test.go
@@ -134,7 +134,7 @@ func TestDefaultFlags(t *testing.T) {
 			ClientCert:          apiserveroptions.ClientCertAuthenticationOptions{},
 			RequestHeader: apiserveroptions.RequestHeaderAuthenticationOptions{
 				UsernameHeaders:     []string{"x-remote-user"},
-				UIDHeaders:          nil,
+				UIDHeaders:          []string{"x-remote-uid"},
 				GroupHeaders:        []string{"x-remote-group"},
 				ExtraHeaderPrefixes: []string{"x-remote-extra-"},
 			},
@@ -295,7 +295,7 @@ func TestAddFlags(t *testing.T) {
 			ClientCert:          apiserveroptions.ClientCertAuthenticationOptions{},
 			RequestHeader: apiserveroptions.RequestHeaderAuthenticationOptions{
 				UsernameHeaders:     []string{"x-remote-user"},
-				UIDHeaders:          nil,
+				UIDHeaders:          []string{"x-remote-uid"},
 				GroupHeaders:        []string{"x-remote-group"},
 				ExtraHeaderPrefixes: []string{"x-remote-extra-"},
 			},
