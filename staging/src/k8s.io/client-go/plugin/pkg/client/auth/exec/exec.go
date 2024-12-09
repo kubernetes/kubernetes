5262cba3ddf8a5a083df202a33710372bd9fe42a
Patrick Ohly <patrick.ohly@intel.com>
2024-12-09 12:56:03 +0100

    client-go auth: contextual logging
    No API changes are needed. In one case, a context is passed in via the
    http.Request. In others there is simply no need to change the calls.

--- staging/src/k8s.io/client-go/plugin/pkg/client/auth/exec/exec.go
+++ staging/src/k8s.io/client-go/plugin/pkg/client/auth/exec/exec.go
@@ -363,6 +363,7 @@ func (r *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 		return r.base.RoundTrip(req)
 	}
 
+	ctx := req.Context()
 	creds, err := r.a.getCreds()
 	if err != nil {
 		return nil, fmt.Errorf("getting credentials: %v", err)
@@ -377,7 +378,7 @@ func (r *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	}
 	if res.StatusCode == http.StatusUnauthorized {
 		if err := r.a.maybeRefreshCreds(creds); err != nil {
-			klog.Errorf("refreshing credentials: %v", err)
+			klog.FromContext(ctx).Error(err, "Refreshing credentials failed")
 		}
 	}
 	return res, nil
