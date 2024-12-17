e07c3e133f09647d4da9069d41a0a7b3875638fa
elieser pereira <elieser.pereiraa@gmail.com>
2024-12-17 10:09:10 -0300

    skip defaults only if label-filter not set
    Co-authored-by: Patrick Ohly <patrick.ohly@intel.com>

--- hack/make-rules/test-e2e-node.sh
+++ hack/make-rules/test-e2e-node.sh
@@ -29,8 +29,12 @@ KUBE_PANIC_WATCH_DECODE_ERROR="${KUBE_PANIC_WATCH_DECODE_ERROR:-true}"
 export KUBE_PANIC_WATCH_DECODE_ERROR
 
 focus=${FOCUS:-""}
-skip=${SKIP-"\[Flaky\]|\[Slow\]|\[Serial\]"}
 label_filter=${LABEL_FILTER:-""}
+if [ -n "${label_filter}" ]; then
+  skip=${SKIP:-""} # No default skip when LABEL_FILTER is set.
+else
+  skip=${SKIP-"\[Flaky\]|\[Slow\]|\[Serial\]"}
+fi
 # The number of tests that can run in parallel depends on what tests
 # are running and on the size of the node. Too many, and tests will
 # fail due to resource contention. 8 is a reasonable default for a
