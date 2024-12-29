e9c47f86018eb7866156a9489d7a271c3896e952
Lionel Jouin <lionel.jouin@est.tech>
2024-12-29 12:00:24 +0100

    Add codegen::register to hack/update-codegen.sh
    Co-authored-by: Joe Betz <jpbetz@google.com>
    Signed-off-by: Lionel Jouin <lionel.jouin@est.tech>

--- staging/src/k8s.io/code-generator/cmd/register-gen/output_tests/simpletype/doc.go
+++ staging/src/k8s.io/code-generator/cmd/register-gen/output_tests/simpletype/doc.go
@@ -14,5 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// +k8s:register-gen=simpletype
+
 // +groupName=simpletype.foo.bar
 package simpletype
