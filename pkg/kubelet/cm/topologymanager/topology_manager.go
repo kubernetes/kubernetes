80344cb788b4d8544110d00b2d03b47ad8344e25
Vishnu Lakshmanan <vishnucarbon@gmail.com>
2024-12-29 22:15:10 -0800

    fix: remove redundant policy check in topology manager

--- pkg/kubelet/cm/topologymanager/topology_manager.go
+++ pkg/kubelet/cm/topologymanager/topology_manager.go
@@ -151,7 +151,7 @@ func NewManager(topology []cadvisorapi.Node, topologyPolicyName string, topology
 		return nil, fmt.Errorf("cannot discover NUMA topology: %w", err)
 	}
 
-	if topologyPolicyName != PolicyNone && len(numaInfo.Nodes) > opts.MaxAllowableNUMANodes {
+	if len(numaInfo.Nodes) > opts.MaxAllowableNUMANodes {
 		return nil, fmt.Errorf("unsupported on machines with more than %v NUMA Nodes", opts.MaxAllowableNUMANodes)
 	}
 
