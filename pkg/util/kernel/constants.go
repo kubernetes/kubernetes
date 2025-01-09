93d5301c49a9ab0cf68d2a05580d20c50dc990da
Peter Hunt <pehunt@redhat.com>
2025-01-09 13:47:52 -0500

    UPSTREAM: <carry>: util: inform the kubelet it has a new enough kernel for idmapped mounts
    as we've pulled back the patches to RHEL 9.4 (or kernel 5.14), and the majority of Openshift will
    run on a RHEL 9.4. This warning should still print if running on RHEL 8
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- pkg/util/kernel/constants.go
+++ pkg/util/kernel/constants.go
@@ -46,7 +46,8 @@ const IPVSConnReuseModeFixedKernelVersion = "5.9"
 
 // UserNamespacesSupportKernelVersion is the kernel version where idmap for tmpfs support was added
 // (ref: https://github.com/torvalds/linux/commit/05e6295f7b5e05f09e369a3eb2882ec5b40fff20)
-const UserNamespacesSupportKernelVersion = "6.3"
+// This was pulled back to RHEL 9.4 for Openshift, so it is present after 5.14.
+const UserNamespacesSupportKernelVersion = "5.14"
 
 const TmpfsNoswapSupportKernelVersion = "6.4"
 
