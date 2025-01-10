1fbf4f5f31b68a318dbbea43911785ec75f1fab6
David Eads <deads@redhat.com>
2025-01-10 16:23:23 -0500

    switch to using the real FIFO

--- staging/src/k8s.io/client-go/tools/cache/shared_informer.go
+++ staging/src/k8s.io/client-go/tools/cache/shared_informer.go
@@ -540,11 +540,16 @@ func (s *sharedIndexInformer) RunWithContext(ctx context.Context) {
 		s.startedLock.Lock()
 		defer s.startedLock.Unlock()
 
-		fifo := NewDeltaFIFOWithOptions(DeltaFIFOOptions{
-			KnownObjects:          s.indexer,
-			EmitDeltaTypeReplaced: true,
-			Transformer:           s.transform,
-		})
+		var fifo Queue
+		if clientgofeaturegate.FeatureGates().Enabled(clientgofeaturegate.InOrderInformers) {
+			fifo = NewRealFIFO(MetaNamespaceKeyFunc, s.indexer, s.transform)
+		} else {
+			fifo = NewDeltaFIFOWithOptions(DeltaFIFOOptions{
+				KnownObjects:          s.indexer,
+				EmitDeltaTypeReplaced: true,
+				Transformer:           s.transform,
+			})
+		}
 
 		cfg := &Config{
 			Queue:             fifo,
