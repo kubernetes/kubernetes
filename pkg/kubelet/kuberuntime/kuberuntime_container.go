a8f5e476095ab0db14c84abee1ce3958b2d69326
joey <zchengjoey@gmail.com>
2025-01-13 17:19:17 +0800

    fix container status remains ready state
    Signed-off-by: joey <zchengjoey@gmail.com>

--- pkg/kubelet/kuberuntime/kuberuntime_container.go
+++ pkg/kubelet/kuberuntime/kuberuntime_container.go
@@ -851,6 +851,10 @@ func (m *kubeGenericRuntimeManager) killContainersWithSyncResult(ctx context.Con
 				klog.ErrorS(err, "Kill container failed", "pod", klog.KRef(runningPod.Namespace, runningPod.Name), "podUID", runningPod.ID,
 					"containerName", container.Name, "containerID", container.ID)
 			}
+			// if the container is killed, the container must not be ready for readiness. #129552
+			if gracePeriodOverride != nil {
+				m.readinessManager.Set(container.ID, proberesults.Failure, pod)
+			}
 			containerResults <- killContainerResult
 		}(container)
 	}
