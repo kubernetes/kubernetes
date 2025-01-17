597b54720e1cad40056fcde303a241af37c0d1a1
cpanato <ctadeu@gmail.com>
2025-01-17 13:13:11 +0100

    Bump images, dependencies and versions to go 1.24.0
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- build/common.sh
+++ build/common.sh
@@ -97,8 +97,8 @@ readonly KUBE_RSYNC_PORT="${KUBE_RSYNC_PORT:-}"
 readonly KUBE_CONTAINER_RSYNC_PORT=8730
 
 # These are the default versions (image tags) for their respective base images.
-readonly __default_distroless_iptables_version=v0.6.8
-readonly __default_go_runner_version=v2.4.0-go1.23.6-bookworm.0
+readonly __default_distroless_iptables_version=v0.7.3
+readonly __default_go_runner_version=v2.4.0-go1.24.0-bookworm.0
 readonly __default_setcap_version=bookworm-v1.0.4
 
 # These are the base images for the Docker-wrapped binaries.
