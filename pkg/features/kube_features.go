f4fbf613679928e71436be7fb97942cef43483f9
Paco Xu <paco.xu@daocloud.io>
2025-01-21 18:39:38 +0800

    promote CustomCPUCFSQuotaPeriod to beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1066,6 +1066,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	CPUCFSQuotaPeriod: {
 		{Version: version.MustParse("1.12"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.34"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	CPUManagerPolicyAlphaOptions: {
