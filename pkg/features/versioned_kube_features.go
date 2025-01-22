21f899596ef132aacee3aa80551580542d702e99
sunnylovestiramisu <songsunny@google.com>
2025-01-22 23:09:30 +0000

    Promote AnyVolumeDataSource to GA

--- pkg/features/versioned_kube_features.go
+++ pkg/features/versioned_kube_features.go
@@ -60,6 +60,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	AnyVolumeDataSource: {
 		{Version: version.MustParse("1.18"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.24"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.33"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // GA in 1.33 -> remove in 1.36
 	},
 
 	AppArmor: {
