6ad91995281edbb885ec0eecb40a68ff9b3f6f6e
Lionel Jouin <lionel.jouin@est.tech>
2025-02-04 20:11:37 +0100

    add simpletype pkg in v1 for register-gen test
    Signed-off-by: Lionel Jouin <lionel.jouin@est.tech>

--- staging/src/k8s.io/code-generator/cmd/register-gen/output_tests/simpletype/v1/fake_deepcopy.go
+++ staging/src/k8s.io/code-generator/cmd/register-gen/output_tests/simpletype/v1/fake_deepcopy.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package simpletype
+package v1
 
 import (
 	"k8s.io/apimachinery/pkg/runtime"
