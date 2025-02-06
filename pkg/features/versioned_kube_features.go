cd9fc8bc71fb02f839160190f934ec91358caaa2
Anish Ramasekar <anish.ramasekar@gmail.com>
2025-02-06 14:11:17 -0600

    Enable ServiceAccountNodeAudienceRestriction feature gate by default in v1.33
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- pkg/features/versioned_kube_features.go
+++ pkg/features/versioned_kube_features.go
@@ -681,6 +681,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	ServiceAccountNodeAudienceRestriction: {
 		{Version: version.MustParse("1.32"), Default: false, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.33"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	ServiceAccountTokenJTI: {
