23c8ee16928fa6d7b5d5f53e67c60eb8763b2e70
Michal Wozniak <michalwozniak@google.com>
2025-02-10 09:17:18 +0100

    Fix comment for FailIndex as JobBackoffLimitPerIndex is beta

--- pkg/apis/batch/types.go
+++ pkg/apis/batch/types.go
@@ -226,8 +226,8 @@ type PodFailurePolicyRule struct {
 	//   running pods are terminated.
 	// - FailIndex: indicates that the pod's index is marked as Failed and will
 	//   not be restarted.
-	//   This value is alpha-level. It can be used when the
-	//   `JobBackoffLimitPerIndex` feature gate is enabled (disabled by default).
+	//   This value is beta-level. It can be used when the
+	//   `JobBackoffLimitPerIndex` feature gate is enabled (enabled by default).
 	// - Ignore: indicates that the counter towards the .backoffLimit is not
 	//   incremented and a replacement pod is created.
 	// - Count: indicates that the pod is handled in the default way - the
