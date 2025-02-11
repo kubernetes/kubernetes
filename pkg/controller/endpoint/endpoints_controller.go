ba4a59ecc482cb48e7b53ef1178e0647cc490245
Dan Winship <danwinship@redhat.com>
2025-02-11 17:17:02 -0500

    Remove some dead code in the Endpoints controller
    There was code to deal with upgrades from pre-dual-stack-aware
    apiservers, with a note to "remove when the possibility of upgrading
    from a cluster that does not support dual stack is nil".
    
    (This requires fixing the unit tests to fill in
    service.Spec.IPFamilies like a modern apiserver would do.)

--- pkg/controller/endpoint/endpoints_controller.go
+++ pkg/controller/endpoint/endpoints_controller.go
@@ -214,38 +214,12 @@ func (e *Controller) addPod(obj interface{}) {
 
 func podToEndpointAddressForService(svc *v1.Service, pod *v1.Pod) (*v1.EndpointAddress, error) {
 	var endpointIP string
-	ipFamily := v1.IPv4Protocol
 
-	if len(svc.Spec.IPFamilies) > 0 {
-		// controller is connected to an api-server that correctly sets IPFamilies
-		ipFamily = svc.Spec.IPFamilies[0] // this works for headful and headless
-	} else {
-		// controller is connected to an api server that does not correctly
-		// set IPFamilies (e.g. old api-server during an upgrade)
-		// TODO (khenidak): remove by when the possibility of upgrading
-		// from a cluster that does not support dual stack is nil
-		if len(svc.Spec.ClusterIP) > 0 && svc.Spec.ClusterIP != v1.ClusterIPNone {
-			// headful service. detect via service clusterIP
-			if utilnet.IsIPv6String(svc.Spec.ClusterIP) {
-				ipFamily = v1.IPv6Protocol
-			}
-		} else {
-			// Since this is a headless service we use podIP to identify the family.
-			// This assumes that status.PodIP is assigned correctly (follows pod cidr and
-			// pod cidr list order is same as service cidr list order). The expectation is
-			// this is *most probably* the case.
-
-			// if the family was incorrectly identified then this will be corrected once the
-			// upgrade is completed (controller connects to api-server that correctly defaults services)
-			if utilnet.IsIPv6String(pod.Status.PodIP) {
-				ipFamily = v1.IPv6Protocol
-			}
-		}
-	}
+	wantIPv6 := svc.Spec.IPFamilies[0] == v1.IPv6Protocol
 
 	// find an ip that matches the family
 	for _, podIP := range pod.Status.PodIPs {
-		if (ipFamily == v1.IPv6Protocol) == utilnet.IsIPv6String(podIP.IP) {
+		if wantIPv6 == utilnet.IsIPv6String(podIP.IP) {
 			endpointIP = podIP.IP
 			break
 		}
