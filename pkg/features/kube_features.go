62d5e1732181166647e61da7a491c033403d883e
Morten Torkildsen <mortent@google.com>
2025-02-13 21:20:23 +0000

    DRA: Feature gate for Partitionable Devices

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -215,12 +215,29 @@ const (
 	DRAAdminAccess featuregate.Feature = "DRAAdminAccess"
 
 	// owner: @mortent
+	// kep: http://kep.k8s.io/4815
+	//
+	// Enables support for dynamically partitioning devices based on
+	// which parts of them were allocated during scheduling.
+	//
+	// Additionally, it provides primitives to represent both full devices
+	// and their partitions in a more compact way ("mixins").
+	DRAPartitionableDevices featuregate.Feature = "DRAPartitionableDevices"
+
+	// owner: @mortent
 	// kep: http://kep.k8s.io/4816
 	//
 	// Enables support for providing a prioritized list of requests
 	// for resources. The first entry that can be satisfied will
 	// be selected.
 	DRAPrioritizedList featuregate.Feature = "DRAPrioritizedList"
+
+	// owner: @LionelJouin
+	// kep: http://kep.k8s.io/4817
+	//
+	// Enables support the ResourceClaim.status.devices field and for setting this
+	// status from DRA drivers.
+	DRAResourceClaimDeviceStatus featuregate.Feature = "DRAResourceClaimDeviceStatus"
 
 	// owner: @pohly
 	// kep: http://kep.k8s.io/4381
@@ -229,13 +246,6 @@ const (
 	// that is independent of a Pod. Resource allocation is done by the scheduler
 	// based on "structured parameters".
 	DynamicResourceAllocation featuregate.Feature = "DynamicResourceAllocation"
-
-	// owner: @LionelJouin
-	// kep: http://kep.k8s.io/4817
-	//
-	// Enables support the ResourceClaim.status.devices field and for setting this
-	// status from DRA drivers.
-	DRAResourceClaimDeviceStatus featuregate.Feature = "DRAResourceClaimDeviceStatus"
 
 	// owner: @lauralorenz
 	// kep: https://kep.k8s.io/4603
