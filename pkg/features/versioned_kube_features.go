fba90c17724ebe28134b85b5a636424c0322929c
Jefftree <jeffrey.ying86@live.com>
2025-02-13 15:40:38 +0000

    Update LegacySidecarContainers to be off since inception

--- pkg/features/versioned_kube_features.go
+++ pkg/features/versioned_kube_features.go
@@ -486,7 +486,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 
 	LegacySidecarContainers: {
-		{Version: version.MustParse("1.0"), Default: true, PreRelease: featuregate.GA},
+		{Version: version.MustParse("1.0"), Default: false, PreRelease: featuregate.GA},
 		{Version: version.MustParse("1.33"), Default: false, PreRelease: featuregate.Deprecated},
 	},
 
