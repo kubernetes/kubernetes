d08568b63a33cf7a07c7436e723f22503ee9d8b5
Sotiris Salloumis <sotiris.salloumis@ericsson.com>
2025-02-22 14:45:10 +0100

    Fix mutation heuristic check of mustKeepCPUs, reason is clone

--- pkg/apis/core/validation/validation.go
+++ pkg/apis/core/validation/validation.go
@@ -6321,7 +6321,7 @@ func dropMustKeepCPUsEnvFromContainer(container *core.Container, oldPodSpecConta
 			for _, oldEnv := range oldPodSpecContainer.Env {
 				if oldEnv.Name == "mustKeepCPUs" {
 					existOldMustKeepCPUs = true
-					container.Env[jx] = oldEnv
+					container.Env[jx] = oldEnv // +k8s:verify-mutation:reason=clone
 					break
 				}
 			}
@@ -6335,7 +6335,7 @@ func dropMustKeepCPUsEnvFromContainer(container *core.Container, oldPodSpecConta
 	}
 	// Delete mustKeepCPUs
 	if !existNewMustKeepCPUs && (len(oldPodSpecContainer.Env)-len(container.Env)) == 1 {
-		oldPodSpecContainer.Env = removeEnvVar(oldPodSpecContainer.Env, "mustKeepCPUs")
+		oldPodSpecContainer.Env = removeEnvVar(oldPodSpecContainer.Env, "mustKeepCPUs") // +k8s:verify-mutation:reason=clone
 	}
 	return allErrs
 }
