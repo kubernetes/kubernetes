ed952fc1b34a26c92ff24068d478f9b7e04cf69b
Sreeram <sreeramvenkitesh@gmail.com>
2025-02-26 12:15:30 +0530

    PodLifecycleSleepActionAllowZero to Beta

--- pkg/features/versioned_kube_features.go
+++ pkg/features/versioned_kube_features.go
@@ -596,6 +596,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 	PodLifecycleSleepActionAllowZero: {
 		{Version: version.MustParse("1.32"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.33"), Default: true, PreRelease: featuregate.Beta},
 	},
 	PodSchedulingReadiness: {
 		{Version: version.MustParse("1.26"), Default: false, PreRelease: featuregate.Alpha},
