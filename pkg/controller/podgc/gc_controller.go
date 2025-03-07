7efb3ceacf181aba7da9368485ead50af95b5ed3
Natasha Sarkar <natashasarkar@google.com>
2025-03-07 17:04:22 +0000

    controller sets observedGeneration on pod conditions

--- pkg/controller/podgc/gc_controller.go
+++ pkg/controller/podgc/gc_controller.go
@@ -246,10 +246,11 @@ func (gcc *PodGCController) gcOrphaned(ctx context.Context, pods []*v1.Pod, node
 		}
 		logger.V(2).Info("Found orphaned Pod assigned to the Node, deleting", "pod", klog.KObj(pod), "node", klog.KRef("", pod.Spec.NodeName))
 		condition := &v1.PodCondition{
-			Type:    v1.DisruptionTarget,
-			Status:  v1.ConditionTrue,
-			Reason:  "DeletionByPodGC",
-			Message: "PodGC: node no longer exists",
+			Type:               v1.DisruptionTarget,
+			ObservedGeneration: apipod.GetPodObservedGenerationIfEnabledOnCondition(pod, v1.DisruptionTarget),
+			Status:             v1.ConditionTrue,
+			Reason:             "DeletionByPodGC",
+			Message:            "PodGC: node no longer exists",
 		}
 		if err := gcc.markFailedAndDeletePodWithCondition(ctx, pod, condition); err != nil {
 			utilruntime.HandleError(err)
