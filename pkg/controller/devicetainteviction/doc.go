347fceb749fbf18d1558e4ecf0db6010401fa678
Patrick Ohly <patrick.ohly@intel.com>
2025-03-07 15:21:52 +0100

    WIP: DRA: add device taint eviction controller

--- pkg/controller/devicetainteviction/doc.go
+++ pkg/controller/devicetainteviction/doc.go
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package tainteviction contains the logic implementing taint-based eviction
-// for Pods running on Nodes with NoExecute taints.
-package tainteviction
+// Package devicetainteviction contains the logic implementing taint-based eviction
+// for Pods using device with taints (https://github.com/kubernetes/enhancements/issues/5055).
+package devicetainteviction
