6bca327fcb1b70ad679c56364d200453d91d2ba8
GrigoriyMikhalkin <grigoriymikhalkin@gmail.com>
2025-03-10 23:35:50 +0100

    Skip creating storage for non-stored and non-served versions

--- staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/customresource_handler.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/customresource_handler.go
@@ -652,10 +652,6 @@ func (r *crdHandler) getOrCreateServingInfoFor(uid types.UID, name string) (*crd
 
 	structuralSchemas := map[string]*structuralschema.Structural{}
 	for _, v := range crd.Spec.Versions {
-		if !v.Served {
-			continue
-		}
-
 		val, err := apiextensionshelpers.GetSchemaForVersion(crd, v.Name)
 		if err != nil {
 			utilruntime.HandleError(err)
@@ -710,10 +706,6 @@ func (r *crdHandler) getOrCreateServingInfoFor(uid types.UID, name string) (*crd
 	// Create replicasPathInCustomResource
 	replicasPathInCustomResource := managedfields.ResourcePathMappings{}
 	for _, v := range crd.Spec.Versions {
-		if !v.Served {
-			continue
-		}
-
 		subresources, err := apiextensionshelpers.GetSubresourcesForVersion(crd, v.Name)
 		if err != nil {
 			utilruntime.HandleError(err)
@@ -733,10 +725,6 @@ func (r *crdHandler) getOrCreateServingInfoFor(uid types.UID, name string) (*crd
 	}
 
 	for _, v := range crd.Spec.Versions {
-		if !v.Served {
-			continue
-		}
-
 		// In addition to Unstructured objects (Custom Resources), we also may sometimes need to
 		// decode unversioned Options objects, so we delegate to parameterScheme for such types.
 		parameterScheme := runtime.NewScheme()
@@ -833,6 +821,11 @@ func (r *crdHandler) getOrCreateServingInfoFor(uid types.UID, name string) (*crd
 		if len(listKind.Kind) == 0 {
 			utilruntime.HandleError(fmt.Errorf("CustomResourceDefinition %s has unexpected empty status.acceptedNames.listKind", crd.Name))
 			return nil, fmt.Errorf("the server could not properly serve the list kind")
+		}
+
+		// Do not construct storage if version is neither served nor stored
+		if !v.Storage && !v.Served {
+			continue
 		}
 
 		storages[v.Name], err = customresource.NewStorage(
