0e9c2ca60c3e1751a966e5822efd8aecb0f3f64e
Kobayashi,Daisuke <kobayashi.da-06@fujitsu.com>
2025-03-11 09:40:33 +0900

    Update integration test

--- test/integration/scheduler_perf/scheduler_perf.go
+++ test/integration/scheduler_perf/scheduler_perf.go
@@ -96,6 +96,7 @@ const (
 	startCollectingMetricsOpcode operationCode = "startCollectingMetrics"
 	stopCollectingMetricsOpcode  operationCode = "stopCollectingMetrics"
 	updateDeviceConditionsOpcode operationCode = "updateDeviceConditions"
+	checkPodScheduledOpcode      operationCode = "checkPodScheduled"
 )
 
 const (
@@ -500,7 +501,7 @@ func (op *op) UnmarshalJSON(b []byte) error {
 		startCollectingMetricsOpcode: &startCollectingMetricsOp{},
 		stopCollectingMetricsOpcode:  &stopCollectingMetricsOp{},
 		updateDeviceConditionsOpcode: &updateDeviceConditionsOp{},
-
+		checkPodScheduledOpcode:      &checkPodScheduledOp{},
 		// TODO(#94601): add a delete nodes op to simulate scaling behaviour?
 	}
 	// First determine the opcode using lenient decoding (= ignore extra fields).
