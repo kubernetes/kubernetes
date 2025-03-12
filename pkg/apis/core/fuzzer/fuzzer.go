52b019675333252d0a896dabcf957f43ab17dcd8
Joe Betz <jpbetz@google.com>
2025-03-12 13:20:38 -0400

    fix import

--- pkg/apis/core/fuzzer/fuzzer.go
+++ pkg/apis/core/fuzzer/fuzzer.go
@@ -23,8 +23,6 @@ import (
 
 	"sigs.k8s.io/randfill"
 
-	utilpointer "k8s.io/utils/pointer"
-
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
