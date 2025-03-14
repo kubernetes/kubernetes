cf20c21ef82e19ffc98bffcc3e94a2afc91e6271
Benjamin Elder <bentheelder@google.com>
2025-03-14 09:42:04 -0700

    Revert "stop overriding max concurrency in CI, let automax procs handle it"
    This reverts changes from commit 9e42056a0dfd87b94768c23d16b72ba1e7fd8213.
    
    NOTE: this is not a clean revert bcause of further changes.

--- hack/jenkins/test-dockerized.sh
+++ hack/jenkins/test-dockerized.sh
@@ -25,6 +25,10 @@ set -x;
 # TODO: make test-integration should handle this automatically
 source ./hack/install-etcd.sh
 
+# TODO: drop KUBE_INTEGRATION_TEST_MAX_CONCURRENCY later when we've figured out 
+# stabilizing the tests / CI Setting this to a hardcoded value is fragile.
+export KUBE_INTEGRATION_TEST_MAX_CONCURRENCY=4
+
 # Save the verbose stdout as well.
 export KUBE_KEEP_VERBOSE_TEST_OUTPUT=y
 export LOG_LEVEL=4
--- hack/jenkins/test-integration-dockerized.sh
+++ hack/jenkins/test-integration-dockerized.sh
@@ -26,4 +26,8 @@ set -x;
 # TODO: make test-integration should handle this automatically
 source ./hack/install-etcd.sh
 
+# TODO: drop KUBE_INTEGRATION_TEST_MAX_CONCURRENCY later when we've figured out 
+# stabilizing the tests / CI Setting this to a hardcoded value is fragile.
+export KUBE_INTEGRATION_TEST_MAX_CONCURRENCY=4
+
 make test-integration KUBE_KEEP_VERBOSE_TEST_OUTPUT=y LOG_LEVEL=4
