8eef2256dd7dd48e4c3f740017504175dc4c470c
HirazawaUi <695097494plus@gmail.com>
2025-03-18 19:33:56 +0800

    Optimize conditions for IPPVS to trigger sync pods

--- pkg/kubelet/kuberuntime/kuberuntime_manager.go
+++ pkg/kubelet/kuberuntime/kuberuntime_manager.go
@@ -834,7 +834,7 @@ func (m *kubeGenericRuntimeManager) doPodResizeAction(ctx context.Context, pod *
 
 	// Always update the pod status once. Even if there was a resize error, the resize may have been
 	// partially actuated.
-	defer m.runtimeHelper.SetPodWatchCondition(pod.UID, "doPodResizeAction", func(*kubecontainer.PodStatus) bool { return true })
+	defer m.runtimeHelper.RequestPodReSync(pod.UID)
 
 	if len(podContainerChanges.ContainersToUpdate[v1.ResourceMemory]) > 0 || podContainerChanges.UpdatePodResources {
 		if podResources.Memory == nil {
