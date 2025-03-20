ec1b493a088ef6b2675a3e4696d47c06189f52a6
vinay kulkarni <vskibum@gmail.com>
2025-03-20 01:46:28 +0000

    Populate status memory requests from actuated resources

--- pkg/kubelet/kubelet_pods.go
+++ pkg/kubelet/kubelet_pods.go
@@ -2105,6 +2105,13 @@ func (kl *Kubelet) convertToAPIContainerStatuses(pod *v1.Pod, podStatus *kubecon
 			} else {
 				preserveOldResourcesValue(v1.ResourceCPU, oldStatus.Resources.Requests, resources.Requests)
 			}
+			// TODO(tallclair,vinaykul,InPlacePodVerticalScaling): Investigate defaulting to actuated resources instead of allocated resources above
+			if _, exists := resources.Requests[v1.ResourceMemory]; exists {
+				// Get memory requests from actuated resources
+				if actuatedResources, found := kl.allocationManager.GetActuatedResources(pod.UID, allocatedContainer.Name); found {
+					resources.Requests[v1.ResourceMemory] = actuatedResources.Requests.Memory().DeepCopy()
+				}
+			}
 		}
 
 		return resources
