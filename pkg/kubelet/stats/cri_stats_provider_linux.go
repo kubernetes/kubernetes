6bbaf8cb10459f15f61a26db035e79c7baf00437
Haowei Cai <haoweic@google.com>
2025-03-20 19:28:45 +0000

    Extend CRI stats provider to support PSI

--- pkg/kubelet/stats/cri_stats_provider_linux.go
+++ pkg/kubelet/stats/cri_stats_provider_linux.go
@@ -25,8 +25,10 @@ import (
 
 	cadvisorapiv2 "github.com/google/cadvisor/info/v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	runtimeapi "k8s.io/cri-api/pkg/apis/runtime/v1"
 	statsapi "k8s.io/kubelet/pkg/apis/stats/v1alpha1"
+	"k8s.io/kubernetes/pkg/features"
 )
 
 func (p *criStatsProvider) addCRIPodContainerStats(criSandboxStat *runtimeapi.PodSandboxStats,
@@ -79,6 +81,7 @@ func addCRIPodMemoryStats(ps *statsapi.PodStats, criPodStat *runtimeapi.PodSandb
 		RSSBytes:        valueOfUInt64Value(criMemory.RssBytes),
 		PageFaults:      valueOfUInt64Value(criMemory.PageFaults),
 		MajorPageFaults: valueOfUInt64Value(criMemory.MajorPageFaults),
+		PSI:             makePSIStats(criMemory.Psi),
 	}
 }
 
@@ -91,6 +94,21 @@ func addCRIPodCPUStats(ps *statsapi.PodStats, criPodStat *runtimeapi.PodSandboxS
 		Time:                 metav1.NewTime(time.Unix(0, criCPU.Timestamp)),
 		UsageNanoCores:       valueOfUInt64Value(criCPU.UsageNanoCores),
 		UsageCoreNanoSeconds: valueOfUInt64Value(criCPU.UsageCoreNanoSeconds),
+		PSI:                  makePSIStats(criCPU.Psi),
+	}
+}
+
+func addCRIPodIOStats(ps *statsapi.PodStats, criPodStat *runtimeapi.PodSandboxStats) {
+	if !utilfeature.DefaultFeatureGate.Enabled(features.KubeletPSI) {
+		return
+	}
+	if criPodStat == nil || criPodStat.Linux == nil || criPodStat.Linux.Io == nil {
+		return
+	}
+	criIO := criPodStat.Linux.Io
+	ps.IO = &statsapi.IOStats{
+		Time: metav1.NewTime(time.Unix(0, criIO.Timestamp)),
+		PSI:  makePSIStats(criIO.Psi),
 	}
 }
 
