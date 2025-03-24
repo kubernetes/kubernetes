50f69613cd318e549a070f5efb4e16883325ab19
Zhou Hao <zhouhao@fujitsu.com>
2025-03-24 13:36:04 +0800

    fix api description
    Signed-off-by: Zhou Hao <zhouhao@fujitsu.com>

--- pkg/scheduler/framework/plugins/feature/feature.go
+++ pkg/scheduler/framework/plugins/feature/feature.go
@@ -24,6 +24,7 @@ type Features struct {
 	EnableDRAAdminAccess                         bool
 	EnableDRADeviceTaints                        bool
 	EnableDRADeviceBindingConditions             bool
+	EnableDRAResourceClaimDeviceStatus           bool
 	EnableDynamicResourceAllocation              bool
 	EnableVolumeAttributesClass                  bool
 	EnableCSIMigrationPortworx                   bool
