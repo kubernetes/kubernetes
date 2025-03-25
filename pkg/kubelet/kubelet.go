d8ef74f2ad689afba3b56732803e92933eca9519
HirazawaUi <695097494plus@gmail.com>
2025-03-25 22:39:27 +0800

    Fix the issue where pod resizing is too slow when using evented PLEG

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -3335,5 +3335,8 @@ func (kl *Kubelet) fastStaticPodsRegistration(ctx context.Context) {
 }
 
 func (kl *Kubelet) RequestPodReSync(podUID types.UID) {
+	if utilfeature.DefaultFeatureGate.Enabled(features.EventedPLEG) {
+		kl.eventedPleg.RequestPodReSync(podUID)
+	}
 	kl.pleg.RequestPodReSync(podUID)
 }
