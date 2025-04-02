de6df7cd6b7d29e320772003d6d15b4539107972
Artem Krasovskiy <aibure@altlinux.org>
2025-04-02 16:29:42 +0300

    Remove using underscores in Go names

--- staging/src/k8s.io/apimachinery/pkg/api/resource/suffix_test.go
+++ staging/src/k8s.io/apimachinery/pkg/api/resource/suffix_test.go
@@ -6,11 +6,11 @@ import (
 
 func TestDecimalExponentEdgeCases(t *testing.T) {
 	table := []struct {
-		input         string
-		expect_base   int32
-		expect_exp    int32
-		expect_format Format
-		expect_status bool
+		input        string
+		expectBase   int32
+		expectExp    int32
+		expectFormat Format
+		expectStatus bool
 	}{
 		{"E6024865272343", 0, 0, DecimalExponent, false},
 		{"e14", 10, 14, DecimalExponent, true},
@@ -22,9 +22,9 @@ func TestDecimalExponentEdgeCases(t *testing.T) {
 
 	for _, item := range table {
 		base, exp, format, ok := quantitySuffixer.interpret(suffix(item.input))
-		if base != item.expect_base || exp != item.expect_exp || format != item.expect_format || item.expect_status != ok {
+		if base != item.expectBase || exp != item.expectExp || format != item.expectFormat || item.expectStatus != ok {
 			t.Errorf("expected base=%v exp=%v format=%v status=%v; got base=%v exp=%v format=%v status=%v",
-				item.expect_base, item.expect_exp, item.expect_format, item.expect_status,
+				item.expectBase, item.expectExp, item.expectFormat, item.expectStatus,
 				base, exp, format, ok)
 		}
 	}
