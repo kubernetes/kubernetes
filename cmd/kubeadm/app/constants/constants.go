afa1abbc476dbadd28424279c2c3e85054311a18
yashsingh74 <yashsingh1774@gmail.com>
2025-04-02 13:08:08 +0530

    Update coredns to v1.12.1
    Signed-off-by: yashsingh74 <yashsingh1774@gmail.com>

--- cmd/kubeadm/app/constants/constants.go
+++ cmd/kubeadm/app/constants/constants.go
@@ -364,7 +364,7 @@ const (
 	CoreDNSImageName = "coredns"
 
 	// CoreDNSVersion is the version of CoreDNS to be deployed if it is used
-	CoreDNSVersion = "v1.12.0"
+	CoreDNSVersion = "v1.12.1"
 
 	// ClusterConfigurationKind is the string kind value for the ClusterConfiguration struct
 	ClusterConfigurationKind = "ClusterConfiguration"
