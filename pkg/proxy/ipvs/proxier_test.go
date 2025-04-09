30f1bcadea431dcc014caebc1031aab78417a98a
Dan Winship <danwinship@redhat.com>
2025-04-09 19:09:19 -0400

    Add a unit test for iptables.CleanupLeftovers, improve ipvs test

--- pkg/proxy/ipvs/proxier_test.go
+++ pkg/proxy/ipvs/proxier_test.go
@@ -30,6 +30,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/lithammer/dedent"
 	"github.com/stretchr/testify/assert"
 
 	v1 "k8s.io/api/core/v1"
@@ -234,6 +235,39 @@ func TestCleanupLeftovers(t *testing.T) {
 	}
 	ipvs := ipvstest.NewFake()
 	ipset := ipsettest.NewFake(testIPSetVersion)
+
+	// Preload some iptables rules
+	initial := dedent.Dedent(`
+		*filter
+		:INPUT - [0:0]
+		:FORWARD - [0:0]
+		:OUTPUT - [0:0]
+		:KUBE-FIREWALL - [0:0]
+		:KUBE-KUBELET-CANARY - [0:0]
+		-A INPUT -j KUBE-FIREWALL
+		-A INPUT -m comment --comment "someone else's input rule" -s 1.2.3.4 -j DROP
+		-A FORWARD -m comment --comment "someone else's forward rule" -s 1.2.3.4 -j DROP
+		-A OUTPUT -j KUBE-FIREWALL
+		-A KUBE-FIREWALL -m comment --comment "block incoming localnet connections" -d 127.0.0.0/8 ! -s 127.0.0.0/8 -m conntrack ! --ctstate RELATED,ESTABLISHED,DNAT -j DROP
+		-A OUTPUT -m comment --comment "someone else's output rule" -s 1.2.3.4 -j DROP
+		COMMIT
+		*nat
+		:PREROUTING - [0:0]
+		:INPUT - [0:0]
+		:OUTPUT - [0:0]
+		:POSTROUTING - [0:0]
+		:KUBE-KUBELET-CANARY - [0:0]
+		COMMIT
+		*mangle
+		:KUBE-IPTABLES-HINT - [0:0]
+		:KUBE-KUBELET-CANARY - [0:0]
+		COMMIT
+		`)
+	err := ipt.RestoreAll([]byte(initial), utiliptables.NoFlushTables, utiliptables.NoRestoreCounters)
+	if err != nil {
+		t.Fatalf("Unexpected error setting up iptables state: %v", err)
+	}
+
 	fp := NewFakeProxier(ctx, ipt, ipvs, ipset, nil, nil, v1.IPv4Protocol)
 	svcIP := "10.20.30.41"
 	svcPort := 80
@@ -273,9 +307,54 @@ func TestCleanupLeftovers(t *testing.T) {
 	fp.syncProxyRules()
 
 	// test cleanup left over
-	if cleanupLeftovers(ctx, ipvs, ipts, ipset) {
+	encounteredError := cleanupLeftovers(ctx, ipvs, ipts, ipset)
+	if encounteredError {
 		t.Errorf("Cleanup leftovers failed")
 	}
+
+	var buf bytes.Buffer
+	err = ipt.SaveInto("", &buf)
+	if err != nil {
+		t.Fatalf("Unexpected error reading filter table: %v", err)
+	}
+	got := buf.String()
+
+	// FIXME: KUBE-MARK-MASQ should be deleted
+
+	expected := strings.TrimLeft(dedent.Dedent(`
+		*nat
+		:PREROUTING - [0:0]
+		:INPUT - [0:0]
+		:OUTPUT - [0:0]
+		:POSTROUTING - [0:0]
+		:KUBE-KUBELET-CANARY - [0:0]
+		:KUBE-MARK-MASQ - [0:0]
+		-A KUBE-MARK-MASQ -j MARK --or-mark
+		COMMIT
+		*filter
+		:INPUT - [0:0]
+		:FORWARD - [0:0]
+		:OUTPUT - [0:0]
+		:KUBE-FIREWALL - [0:0]
+		:KUBE-KUBELET-CANARY - [0:0]
+		-A INPUT -j KUBE-FIREWALL
+		-A INPUT -m comment --comment "someone else's input rule" -s 1.2.3.4 -j DROP
+		-A FORWARD -m comment --comment "someone else's forward rule" -s 1.2.3.4 -j DROP
+		-A OUTPUT -j KUBE-FIREWALL
+		-A OUTPUT -m comment --comment "someone else's output rule" -s 1.2.3.4 -j DROP
+		-A KUBE-FIREWALL -m comment --comment "block incoming localnet connections" -d 127.0.0.0/8 ! -s 127.0.0.0/8 -m conntrack ! --ctstate RELATED,ESTABLISHED,DNAT -j DROP
+		COMMIT
+		*mangle
+		:KUBE-IPTABLES-HINT - [0:0]
+		:KUBE-KUBELET-CANARY - [0:0]
+		COMMIT
+		`), "\n")
+
+	if got != expected {
+		t.Fatalf("expected post-cleanup rules to be:\n%s\n\ngot:\n%s\n", expected, got)
+	}
+
+	// FIXME: check ipvs and ipset state
 }
 
 func TestCanUseIPVSProxier(t *testing.T) {
