f89a86475260b9ed2fc36056dca4878465499638
Francesco Romani <fromani@redhat.com>
2025-04-10 15:21:09 +0200

    e2e: node: rewrite more compatibility tests
    Complete the rewrite the policy option compatibility tests,
    rewriting the tests which check compatibility
    between the `full-pcpus-only` and `distribute-cpus-across-numa`.
    
    All testcases are preserved.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- test/e2e_node/cpu_manager_metrics_test.go
+++ test/e2e_node/cpu_manager_metrics_test.go
@@ -412,7 +412,7 @@ var _ = SIGDescribe("CPU Manager Metrics", framework.WithSerial(), feature.CPUMa
 			updateKubeletConfig(ctx, f, newCfg, true)
 
 			ginkgo.By("Checking the cpumanager allocation per NUMA metric right after the kubelet restart, with no pods running")
-			numaNodes, _, _ := hostCheck()
+			numaNodes, _, _, _ := hostCheck()
 
 			framework.Logf("numaNodes on the system %d", numaNodes)
 
@@ -436,7 +436,6 @@ var _ = SIGDescribe("CPU Manager Metrics", framework.WithSerial(), feature.CPUMa
 		})
 
 		ginkgo.It("should report allocation per NUMA metric when handling guaranteed pods", func(ctx context.Context) {
-			var cpusNumPerNUMA, coresNumPerNUMA, numaNodes, threadsPerCore int
 			cpuPolicyOptions := map[string]string{
 				cpumanager.DistributeCPUsAcrossNUMAOption: "true",
 				cpumanager.FullPCPUsOnlyOption:            "true",
@@ -452,13 +451,7 @@ var _ = SIGDescribe("CPU Manager Metrics", framework.WithSerial(), feature.CPUMa
 
 			updateKubeletConfig(ctx, f, newCfg, true)
 
-			numaNodes, coresNumPerNUMA, threadsPerCore = hostCheck()
-			cpusNumPerNUMA = coresNumPerNUMA * threadsPerCore
-
-			framework.Logf("numaNodes on the system %d", numaNodes)
-			framework.Logf("Cores per NUMA on the system %d", coresNumPerNUMA)
-			framework.Logf("Threads per Core on the system %d", threadsPerCore)
-			framework.Logf("CPUs per NUMA on the system %d", cpusNumPerNUMA)
+			numaNodes, _, _, cpusNumPerNUMA := hostCheck()
 
 			smtLevel = smtLevelFromSysFS()
 			framework.Logf("SMT Level on the system %d", smtLevel)
