156741665fcd436e4ab5f2ef512db0835d0d8996
Morten Torkildsen <mortent@google.com>
2025-04-11 20:02:52 +0000

    Add e2e tests for Partitionable Devices

--- test/e2e/feature/feature.go
+++ test/e2e/feature/feature.go
@@ -138,6 +138,18 @@ var (
 	DownwardAPIHugePages = framework.WithFeature(framework.ValidFeatures.Add("DownwardAPIHugePages"))
 
 	// owning-sig: sig-scheduling
+	// kep: https://kep.k8s.io/4815
+	// test-infra jobs:
+	// - "ci-kind-dra-all" in https://testgrid.k8s.io/sig-node-dynamic-resource-allocation
+	//
+	// This label is used for tests which need:
+	// - the DynamicResourceAllocation *and* DRAPartitionableDevices feature gates
+	// - the resource.k8s.io API group
+	// - a container runtime where support for CDI (https://github.com/cncf-tags/container-device-interface)
+	//   is enabled such that passing CDI device IDs through CRI fields is supported
+	DRAPartitionableDevices = framework.WithFeature(framework.ValidFeatures.Add("DRAPartitionableDevices"))
+
+	// owning-sig: sig-scheduling
 	// kep: https://kep.k8s.io/4816
 	// test-infra jobs:
 	// - "ci-kind-dra-all" in https://testgrid.k8s.io/sig-node-dynamic-resource-allocation
