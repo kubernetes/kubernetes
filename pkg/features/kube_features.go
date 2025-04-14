331a1c42c8291ba223d335e3faabd894892b7f44
Harshal Neelkamal <harshaln@google.com>
2025-04-14 22:33:39 +0000

    KEP-740: promote ExternalJWTSigner feature to beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1192,6 +1192,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	ExternalServiceAccountTokenSigner: {
 		{Version: version.MustParse("1.32"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.34"), Default: false, PreRelease: featuregate.Beta},
 	},
 
 	genericfeatures.AggregatedDiscoveryRemoveBetaType: {
