61d39c7f82c47d7c54013bd0f410eecd0b5e596b
Karl Isenberg <karlisenberg@google.com>
2025-04-27 20:23:09 -0700

    test: validate that watchers close without error
    - Add channel test helpers to k8s.io/client-go/util/testing
    - Use the new channel test helpers in the client and server watch
      tests to validate that the result channels closes without error
      when the client stops the watcher.
    - Use the new channel test helpers in the client watcher decoder
      tests to validate that encoded watch events can be decoded and
      that the decoder errors with EOF when stopped asynchronously.
    
    These new tests uncovered existing errors (added TODOs):
    1.  The watch client doesn't close the response body when it
        encounters a NegotiateError.
    2.  The watch client sometimes sends a decode error on the result
        channel after the client watcher has been stopped.
    3.  StreamWatcher.receive uses %v instead of %w when wrapping errors.

--- staging/src/k8s.io/client-go/rest/watch/decoder_test.go
+++ staging/src/k8s.io/client-go/rest/watch/decoder_test.go
@@ -21,10 +21,10 @@ import (
 	"fmt"
 	"io"
 	"testing"
-	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/core/v1"
-	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	runtimejson "k8s.io/apimachinery/pkg/runtime/serializer/json"
@@ -32,6 +32,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes/scheme"
+	utiltesting "k8s.io/client-go/util/testing"
 )
 
 // getDecoder mimics how k8s.io/client-go/rest.createSerializers creates a decoder
@@ -45,86 +46,103 @@ func TestDecoder(t *testing.T) {
 	table := []watch.EventType{watch.Added, watch.Deleted, watch.Modified, watch.Error, watch.Bookmark}
 
 	for _, eventType := range table {
-		out, in := io.Pipe()
+		t.Run(string(eventType), func(t *testing.T) {
+			ctx := t.Context()
+			out, in := io.Pipe()
+			defer assertNoCloseError(t, in)
+			decoder := NewDecoder(streaming.NewDecoder(out, getDecoder()), getDecoder())
+			defer decoder.Close()
+			expect := &v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "foo"}}
+			encoder := json.NewEncoder(in)
+			eType := eventType
 
-		decoder := NewDecoder(streaming.NewDecoder(out, getDecoder()), getDecoder())
-		expect := &v1.Pod{ObjectMeta: metav1.ObjectMeta{Name: "foo"}}
-		encoder := json.NewEncoder(in)
-		eType := eventType
-		errc := make(chan error)
+			encodeErrCh := make(chan error)
+			go func() {
+				defer close(encodeErrCh)
+				data, err := runtime.Encode(scheme.Codecs.LegacyCodec(v1.SchemeGroupVersion), expect)
+				if err != nil {
+					encodeErrCh <- fmt.Errorf("encode error: %w", err)
+					return
+				}
+				event := metav1.WatchEvent{
+					Type:   string(eType),
+					Object: runtime.RawExtension{Raw: json.RawMessage(data)},
+				}
+				if err := encoder.Encode(&event); err != nil {
+					encodeErrCh <- fmt.Errorf("encode error: %w", err)
+					return
+				}
+			}()
 
-		go func() {
-			data, err := runtime.Encode(scheme.Codecs.LegacyCodec(v1.SchemeGroupVersion), expect)
-			if err != nil {
-				errc <- fmt.Errorf("Unexpected error %v", err)
-				return
-			}
-			event := metav1.WatchEvent{
-				Type:   string(eType),
-				Object: runtime.RawExtension{Raw: json.RawMessage(data)},
-			}
-			if err := encoder.Encode(&event); err != nil {
-				t.Errorf("Unexpected error %v", err)
-			}
-			in.Close()
-		}()
+			decodeErrCh := make(chan error)
+			go func() {
+				defer close(decodeErrCh)
+				action, got, err := decoder.Decode()
+				if err != nil {
+					decodeErrCh <- fmt.Errorf("decode error: %w", err)
+					return
+				}
+				assert.Equal(t, eType, action)
+				assert.Equal(t, expect, got)
+			}()
 
-		done := make(chan struct{})
-		go func() {
-			action, got, err := decoder.Decode()
-			if err != nil {
-				errc <- fmt.Errorf("Unexpected error %v", err)
-				return
-			}
-			if e, a := eType, action; e != a {
-				t.Errorf("Expected %v, got %v", e, a)
-			}
-			if e, a := expect, got; !apiequality.Semantic.DeepDerivative(e, a) {
-				t.Errorf("Expected %v, got %v", e, a)
-			}
-			t.Logf("Exited read")
-			close(done)
-		}()
-		select {
-		case err := <-errc:
-			t.Fatal(err)
-		case <-done:
-		}
+			// Wait for encoder and decoder to return without error
+			err := utiltesting.WaitForAllChannelsToCloseWithTimeout(ctx,
+				wait.ForeverTestTimeout, encodeErrCh, decodeErrCh)
+			require.NoError(t, err)
 
-		done = make(chan struct{})
-		go func() {
-			_, _, err := decoder.Decode()
-			if err == nil {
-				t.Errorf("Unexpected nil error")
-			}
-			close(done)
-		}()
-		<-done
+			// Close the input pipe, which should cause the decoder to error
+			require.NoError(t, in.Close())
 
-		decoder.Close()
+			// Wait for decoder EOF error
+			decodeErrCh = make(chan error)
+			go func() {
+				defer close(decodeErrCh)
+				_, _, err := decoder.Decode()
+				if err != nil {
+					decodeErrCh <- err
+				}
+			}()
+
+			// Wait for decoder EOF error
+			decodeErr, err := utiltesting.WaitForChannelEventWithTimeout(ctx, wait.ForeverTestTimeout, decodeErrCh)
+			require.NoError(t, err)
+			require.Equal(t, io.EOF, decodeErr)
+		})
 	}
 }
 
 func TestDecoder_SourceClose(t *testing.T) {
+	ctx := t.Context()
 	out, in := io.Pipe()
+	defer assertNoCloseError(t, in)
 	decoder := NewDecoder(streaming.NewDecoder(out, getDecoder()), getDecoder())
+	defer decoder.Close()
 
-	done := make(chan struct{})
-
+	errCh := make(chan error)
 	go func() {
+		defer close(errCh)
 		_, _, err := decoder.Decode()
-		if err == nil {
-			t.Errorf("Unexpected nil error")
+		if err != nil {
+			errCh <- err
 		}
-		close(done)
 	}()
 
-	in.Close()
+	// Close the input pipe, which should cause the decoder to error
+	require.NoError(t, in.Close())
 
-	select {
-	case <-done:
-		break
-	case <-time.After(wait.ForeverTestTimeout):
-		t.Error("Timeout")
-	}
+	// Wait for decoder EOF error
+	decodeErr, err := utiltesting.WaitForChannelEventWithTimeout(ctx, wait.ForeverTestTimeout, errCh)
+	require.NoError(t, err)
+	require.Equal(t, io.EOF, decodeErr)
+
+	// Wait for errCh to close
+	err = utiltesting.WaitForChannelToCloseWithTimeout(ctx, wait.ForeverTestTimeout, errCh)
+	require.NoError(t, err)
+}
+
+// assertNoCloseError asserts that closing the Closer doesn't error.
+// Safe to call in a defer to ensure Closer.Close is called.
+func assertNoCloseError(t *testing.T, c io.Closer) {
+	assert.NoError(t, c.Close())
 }
