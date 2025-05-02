115d821e0d92ab634d0b50f6d7bb184669a53c34
Hironori Shiina <shiina.hironori@gmail.com>
2025-05-02 10:57:29 +0200

    Address feedback

--- pkg/kubelet/kuberuntime/kuberuntime_manager.go
+++ pkg/kubelet/kuberuntime/kuberuntime_manager.go
@@ -766,7 +766,7 @@ func (m *kubeGenericRuntimeManager) doPodResizeAction(ctx context.Context, pod *
 		return resizeResult
 	}
 
-	const maxValue = int64(-1)
+	const maxValue = int64(-1) // handled as max in libcontainer, which updates pod cgroup values.
 	setPodCgroupConfig := func(rName v1.ResourceName, setLimitValue bool) error {
 		var err error
 		resizedResources := &cm.ResourceConfig{}
@@ -840,8 +840,7 @@ func (m *kubeGenericRuntimeManager) doPodResizeAction(ctx context.Context, pod *
 
 	if len(podContainerChanges.ContainersToUpdate[v1.ResourceMemory]) > 0 || podContainerChanges.UpdatePodResources {
 		if podResources.Memory == nil {
-			value := maxValue
-			podResources.Memory = &value
+			podResources.Memory = ptr.To(maxValue)
 		}
 		if errResize := resizeContainers(v1.ResourceMemory, int64(*currentPodMemoryConfig.Memory), *podResources.Memory, 0, 0); errResize != nil {
 			resizeResult.Fail(kubecontainer.ErrResizePodInPlace, errResize.Error())
@@ -857,8 +856,7 @@ func (m *kubeGenericRuntimeManager) doPodResizeAction(ctx context.Context, pod *
 		}
 
 		if podResources.CPUQuota == nil {
-			val := maxValue
-			podResources.CPUQuota = &val
+			podResources.CPUQuota = ptr.To(maxValue)
 			// CPUPeriod is not set in podResources by ResourceConfigForPod() when no limit is configured.
 			// This is necessary for update the CPU limit in the runtime.
 			podResources.CPUPeriod = &cpuPeriod
