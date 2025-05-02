e0983dbee876e93a825dc93602c9fb46562fbfe6
Benjamin Elder <bentheelder@google.com>
2025-05-02 14:23:49 -0700

    remove unused shell imports in verify-shellcheck

--- hack/verify-shellcheck.sh
+++ hack/verify-shellcheck.sh
@@ -22,8 +22,6 @@ set -o nounset
 set -o pipefail
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-source "${KUBE_ROOT}/hack/lib/init.sh"
-source "${KUBE_ROOT}/hack/lib/util.sh"
 
 # allow overriding docker cli, which should work fine for this script
 DOCKER="${DOCKER:-docker}"
