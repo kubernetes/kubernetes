8a42f4ccce5929029334c0262fcd108d5dd33b2f
cici37 <cicih@google.com>
2025-05-07 07:41:31 -0700

    Add support for CEL list library.
    Signed-off-by: Cici Huang <cicih@google.com>

--- staging/src/k8s.io/apiserver/pkg/cel/environment/base.go
+++ staging/src/k8s.io/apiserver/pkg/cel/environment/base.go
@@ -183,6 +183,13 @@ var baseOptsWithoutStrictCost = []VersionedOptions{
 			library.SemverLib(library.SemverVersion(1)),
 		},
 	},
+	// List library
+	{
+		IntroducedVersion: version.MajorMinor(1, 34),
+		EnvOptions: []cel.EnvOption{
+			ext.Lists(ext.ListsVersion(3)),
+		},
+	},
 }
 
 var (
