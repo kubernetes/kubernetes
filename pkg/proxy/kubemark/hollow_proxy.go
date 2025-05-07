81fd2095d379ece11b86467e38538e5d1b6a1659
Daman Arora <aroradaman@gmail.com>
2025-05-07 21:45:59 +0530

    kube-proxy: add NodeTopologyConfig for tracking topology labels
    This simplifies how the proxier receives update for change in node
    labels. Instead of passing the complete Node object we just pass
    the proxy relevant topology labels extracted from the complete list
    of labels, and the downstream event handlers will only be notified
    when there are changes in topology labels.
    
    Signed-off-by: Daman Arora <aroradaman@gmail.com>

--- pkg/proxy/kubemark/hollow_proxy.go
+++ pkg/proxy/kubemark/hollow_proxy.go
@@ -30,7 +30,6 @@ import (
 	"k8s.io/client-go/tools/events"
 	proxyapp "k8s.io/kubernetes/cmd/kube-proxy/app"
 	proxyconfigapi "k8s.io/kubernetes/pkg/proxy/apis/config"
-	proxyconfig "k8s.io/kubernetes/pkg/proxy/config"
 	"k8s.io/utils/ptr"
 )
 
@@ -38,9 +37,7 @@ type HollowProxy struct {
 	ProxyServer *proxyapp.ProxyServer
 }
 
-type FakeProxier struct {
-	proxyconfig.NoopNodeHandler
-}
+type FakeProxier struct{}
 
 func (*FakeProxier) Sync() {}
 func (*FakeProxier) SyncLoop() {
@@ -55,6 +52,7 @@ func (*FakeProxier) OnEndpointSliceUpdate(oldSlice, slice *discoveryv1.EndpointS
 func (*FakeProxier) OnEndpointSliceDelete(slice *discoveryv1.EndpointSlice)           {}
 func (*FakeProxier) OnEndpointSlicesSynced()                                          {}
 func (*FakeProxier) OnServiceCIDRsChanged(_ []string)                                 {}
+func (*FakeProxier) OnTopologyChange(_ map[string]string)                             {}
 
 func NewHollowProxy(
 	nodeName string,
