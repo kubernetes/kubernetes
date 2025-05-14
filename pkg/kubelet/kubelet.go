a67ed57d68f6257360970da413f72a275866a165
Chunxia Guo/vRAN Solution Lab /SRC-Beijing/Staff Engineer/Samsung Electronics <cx.guo@samsung.com>
2025-05-14 19:36:21 +0800

    Release non-restart InitContainer CPUs after terminated
    # Conflicts:
    #	pkg/kubelet/kubelet.go

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -2713,6 +2713,26 @@ func (kl *Kubelet) HandlePodAdditions(pods []*v1.Pod) {
 	}
 }
 
+func (kl *Kubelet) UpdatePodAllocation(pod *v1.Pod) *v1.Pod{
+	if pod == nil {
+		return pod
+	}
+
+	for _, c := range append(pod.Spec.InitContainers, pod.Spec.Containers...)  {
+		if cAlloc, ok := kl.allocationManager.GetAllocatedResources(pod.UID, c.Name); ok {
+			// add feature exclusive
+			allocateCpus := kl.containerManager.GetCPUs(string(pod.UID), c.Name)
+			cpuRequests := cAlloc.Requests[v1.ResourceCPU]
+			klog.V(4).InfoS("UpdatePodAllocation", "c.Name", c.Name, "allocateCpus", allocateCpus, "len(allocateCpus)", len(allocateCpus), "int(cpuRequests.Value())", int(cpuRequests.Value()))
+			if len(allocateCpus) != int(cpuRequests.Value()) {
+				// Allocation differs from pod spec, update
+				kl.allocationManager.SetAllocatedResourcesFromAllocateCpus(pod.UID, c.Name, int64(len(allocateCpus)))
+			}
+		}
+	}
+	return pod
+}
+
 // HandlePodUpdates is the callback in the SyncHandler interface for pods
 // being updated from a config source.
 func (kl *Kubelet) HandlePodUpdates(pods []*v1.Pod) {
@@ -2730,7 +2750,12 @@ func (kl *Kubelet) HandlePodUpdates(pods []*v1.Pod) {
 		}
 
 		if utilfeature.DefaultFeatureGate.Enabled(features.InPlacePodVerticalScaling) {
-			if recordContainerResizeOperations(oldPod, pod) {
+			//if recordContainerResizeOperations(oldPod, pod) {
+				if podstatus, ok := kl.statusManager.GetPodStatus(pod.UID); ok{
+					if utilfeature.DefaultFeatureGate.Enabled(features.InPlacePodVerticalScalingExclusiveCPUs) && podstatus.QOSClass == v1.PodQOSGuaranteed{
+						kl.UpdatePodAllocation(pod)
+					}
+				}
 				_, updatedFromAllocation := kl.allocationManager.UpdatePodFromAllocation(pod)
 				if updatedFromAllocation {
 					kl.allocationManager.PushPendingResize(pod.UID)
@@ -2742,7 +2767,7 @@ func (kl *Kubelet) HandlePodUpdates(pods []*v1.Pod) {
 					// so we need to clear the pending resize condition.
 					kl.statusManager.ClearPodResizePendingCondition(pod.UID)
 				}
-			}
+			//}
 		}
 
 		kl.podWorkers.UpdatePod(UpdatePodOptions{
