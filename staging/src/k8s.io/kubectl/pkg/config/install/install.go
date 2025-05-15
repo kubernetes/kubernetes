39195f9a463cce6b3ce4e3d9f648c2915407596d
Maciej Szulik <soltysh@gmail.com>
2025-05-15 14:14:59 +0200

    kuberc: add v1beta1 types
    Signed-off-by: Maciej Szulik <soltysh@gmail.com>

--- staging/src/k8s.io/kubectl/pkg/config/install/install.go
+++ staging/src/k8s.io/kubectl/pkg/config/install/install.go
@@ -23,10 +23,12 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/kubectl/pkg/config"
 	"k8s.io/kubectl/pkg/config/v1alpha1"
+	"k8s.io/kubectl/pkg/config/v1beta1"
 )
 
 // Install registers the API group and adds types to a scheme
 func Install(scheme *runtime.Scheme) {
 	utilruntime.Must(config.AddToScheme(scheme))
+	utilruntime.Must(v1beta1.AddToScheme(scheme))
 	utilruntime.Must(v1alpha1.AddToScheme(scheme))
 }
