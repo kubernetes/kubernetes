5d56fe995bb4625a86223363e9c4b9f616d81c3a
joshjms <joshjms1607@gmail.com>
2025-05-17 08:31:27 +0800

    etcd: update etcd version to v3.6.0 for k8s v1.34
    Signed-off-by: joshjms <joshjms1607@gmail.com>

--- cmd/kubeadm/app/constants/constants.go
+++ cmd/kubeadm/app/constants/constants.go
@@ -323,10 +323,10 @@ const (
 	KubeletHealthzPort = 10248
 
 	// MinExternalEtcdVersion indicates minimum external etcd version which kubeadm supports
-	MinExternalEtcdVersion = "3.5.11-0"
+	MinExternalEtcdVersion = "3.5.21-0"
 
 	// DefaultEtcdVersion indicates the default etcd version that kubeadm uses
-	DefaultEtcdVersion = "3.5.21-0"
+	DefaultEtcdVersion = "3.6.0-0"
 
 	// Etcd defines variable used internally when referring to etcd component
 	Etcd = "etcd"
@@ -498,7 +498,7 @@ var (
 		31: "3.5.21-0",
 		32: "3.5.21-0",
 		33: "3.5.21-0",
-		34: "3.5.21-0",
+		34: "3.6.0-0",
 	}
 
 	// KubeadmCertsClusterRoleName sets the name for the ClusterRole that allows
