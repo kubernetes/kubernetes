80f2ce7b91c8dfcc9e78fd783aa486f4f8859b0a
Joe Betz <jpbetz@google.com>
2025-06-03 08:33:14 -0400

    Switch to using named args in output_tests

--- staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/union/undiscriminated/multiple/doc.go
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/union/undiscriminated/multiple/doc.go
@@ -30,19 +30,19 @@ type Struct struct {
 
 	NonUnionField string `json:"nonUnionField"`
 
-	// +k8s:unionMember={"union": "union1"}
+	// +k8s:unionMember(union: "union1")
 	// +k8s:optional
 	U1M1 *M1 `json:"u1m1"`
 
-	// +k8s:unionMember={"union": "union1"}
+	// +k8s:unionMember(union: "union1")
 	// +k8s:optional
 	U1M2 *M2 `json:"u1m2"`
 
-	// +k8s:unionMember={"union": "union2"}
+	// +k8s:unionMember(union: "union2")
 	// +k8s:optional
 	U2M1 *M1 `json:"u2m1"`
 
-	// +k8s:unionMember={"union": "union2"}
+	// +k8s:unionMember(union: "union2")
 	// +k8s:optional
 	U2M2 *M2 `json:"u2m2"`
 }
