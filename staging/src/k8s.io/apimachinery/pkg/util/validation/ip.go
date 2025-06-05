d300410bf6f7ba7ab66c1a01234cc6bdbfde92a2
Tim Hockin <thockin@google.com>
2025-06-04 22:47:13 -0700

    Rename format "ip-strict" to "k8s-ip"

--- staging/src/k8s.io/apimachinery/pkg/util/validation/ip.go
+++ staging/src/k8s.io/apimachinery/pkg/util/validation/ip.go
@@ -91,13 +91,13 @@ func IsValidIPForLegacyField(fldPath *field.Path, value string, strictValidation
 func IsValidIP(fldPath *field.Path, value string) field.ErrorList {
 	ip, allErrors := parseIP(fldPath, value, true)
 	if len(allErrors) != 0 {
-		return allErrors.WithOrigin("format=ip-strict")
+		return allErrors.WithOrigin("format=k8s-ip")
 	}
 
 	if value != ip.String() {
 		allErrors = append(allErrors, field.Invalid(fldPath, value, fmt.Sprintf("must be in canonical form (%q)", ip.String())))
 	}
-	return allErrors.WithOrigin("format=ip-strict")
+	return allErrors.WithOrigin("format=k8s-ip")
 }
 
 // GetWarningsForIP returns warnings for IP address values in non-standard forms. This
