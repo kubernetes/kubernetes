c2635fac0fd18efd3278ff3649396fc3789e5628
Basanta Niraula <subashniraula6@gmail.com>
2025-06-07 16:48:33 -0400

    docs(core/v1): clarify that only 'storage' resource is valid in PVC requests

--- staging/src/k8s.io/api/core/v1/types.go
+++ staging/src/k8s.io/api/core/v1/types.go
@@ -2718,6 +2718,7 @@ type VolumeResourceRequirements struct {
 	// Requests describes the minimum amount of compute resources required.
 	// If Requests is omitted for a container, it defaults to Limits if that is explicitly specified,
 	// otherwise to an implementation-defined value. Requests cannot exceed Limits.
+	// Only "storage" is supported as a key. Other keys like "cpu", "memory", etc. are invalid and ignored.
 	// More info: https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/
 	// +optional
 	Requests ResourceList `json:"requests,omitempty" protobuf:"bytes,2,rep,name=requests,casttype=ResourceList,castkey=ResourceName"`
