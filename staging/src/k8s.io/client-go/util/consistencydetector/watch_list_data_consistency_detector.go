bad49390493c10e5288ecdd5d81dc6c16f2b7520
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2025-06-17 13:37:57 +0200

    client-go/util/consistencydetector: remove CheckListFromCacheDataConsistencyIfRequested

--- staging/src/k8s.io/client-go/util/consistencydetector/watch_list_data_consistency_detector.go
+++ staging/src/k8s.io/client-go/util/consistencydetector/watch_list_data_consistency_detector.go
@@ -17,12 +17,8 @@ limitations under the License.
 package consistencydetector
 
 import (
-	"context"
 	"os"
 	"strconv"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 )
 
 var dataConsistencyDetectionForWatchListEnabled = false
@@ -35,20 +31,4 @@ func init() {
 // the KUBE_WATCHLIST_INCONSISTENCY_DETECTOR environment variable was set during a binary startup.
 func IsDataConsistencyDetectionForWatchListEnabled() bool {
 	return dataConsistencyDetectionForWatchListEnabled
-}
-
-// CheckWatchListFromCacheDataConsistencyIfRequested performs a data consistency check only when
-// the KUBE_WATCHLIST_INCONSISTENCY_DETECTOR environment variable was set during a binary startup.
-//
-// The consistency check is meant to be enforced only in the CI, not in production.
-// The check ensures that data retrieved by the watch-list api call
-// is exactly the same as data received by the standard list api call against etcd.
-//
-// Note that this function will panic when data inconsistency is detected.
-// This is intentional because we want to catch it in the CI.
-func CheckWatchListFromCacheDataConsistencyIfRequested[T runtime.Object](ctx context.Context, identity string, listItemsFn ListFunc[T], optionsUsedToReceiveList metav1.ListOptions, receivedList runtime.Object) {
-	if !IsDataConsistencyDetectionForWatchListEnabled() {
-		return
-	}
-	checkListFromCacheDataConsistencyIfRequestedInternal(ctx, identity, listItemsFn, optionsUsedToReceiveList, receivedList)
 }
