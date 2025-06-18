caa64afd01764ae608bfde4d06e319959b120dea
Shingo Omura <everpeace@gmail.com>
2025-06-18 22:21:29 +0900

    merge constants.go into attribute.go
    Signed-off-by: Shingo Omura <everpeace@gmail.com>

--- staging/src/k8s.io/dynamic-resource-allocation/deviceattribute/attribute.go
+++ staging/src/k8s.io/dynamic-resource-allocation/deviceattribute/attribute.go
@@ -20,6 +20,18 @@ import (
 	resourceapi "k8s.io/dynamic-resource-allocation/api"
 )
 
+const (
+	// StandardDeviceAttributePrefix is the prefix used for standard device attributes.
+	StandardDeviceAttributePrefix = "resource.kubernetes.io/"
+
+	// StandardDeviceAttributePCIeRoot is a standard device attribute name
+	// which describe PCIe Root Complex of the PCIe device.
+	// The value is a string value in the format `pci<domain>:<bus>`,
+	// referring to a PCIe (Peripheral Component Interconnect Express) Root Complex.
+	// This attribute can be used to identify devices that share the same PCIe Root Complex.
+	StandardDeviceAttributePCIeRoot resourceapi.QualifiedName = StandardDeviceAttributePrefix + "pcieRoot"
+)
+
 // DeviceAttribute represents a device attribute name and its value
 type DeviceAttribute struct {
 	// Name is the qualified name of the device attribute.
