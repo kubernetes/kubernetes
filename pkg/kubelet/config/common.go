1e40eb8eddf93173f8c035ec3b7d53f09fc00f54
sreeram-venkitesh <sreeramvenkitesh@gmail.com>
2025-06-25 23:59:26 +0530

    Added podutil.HasAPIObjectReference to deny admission for static pods referencing API objects

--- pkg/kubelet/config/common.go
+++ pkg/kubelet/config/common.go
@@ -28,8 +28,11 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	utilyaml "k8s.io/apimachinery/pkg/util/yaml"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
+	podutil "k8s.io/kubernetes/pkg/api/pod"
 	api "k8s.io/kubernetes/pkg/apis/core"
 	"k8s.io/kubernetes/pkg/apis/core/helper"
+	"k8s.io/kubernetes/pkg/features"
 
 	// TODO: remove this import if
 	// api.Registry.GroupOrDie(v1.GroupName).GroupVersion.String() is changed
@@ -144,19 +147,31 @@ func tryDecodeSinglePod(data []byte, defaultFn defaultFunc) (parsed bool, pod *v
 		return true, nil, err
 	}
 
-	for _, v := range v1Pod.Spec.Volumes {
-		if v.Projected == nil {
-			continue
+	if utilfeature.DefaultFeatureGate.Enabled(features.PreventStaticPodAPIReferences) {
+		// Check if pod has references to API objects
+		_, resource, err := podutil.HasAPIObjectReference(newPod)
+		if err != nil {
+			return true, nil, err
 		}
+		if resource != "" {
+			return true, nil, fmt.Errorf("static pods may not reference %s", resource)
+		}
+	} else {
+		// TODO: Remove this else block once the PreventStaticPodAPIReferences gate is GA
+		for _, v := range v1Pod.Spec.Volumes {
+			if v.Projected == nil {
+				continue
+			}
 
-		for _, s := range v.Projected.Sources {
-			if s.ClusterTrustBundle != nil {
-				return true, nil, ErrStaticPodTriedToUseClusterTrustBundle
+			for _, s := range v.Projected.Sources {
+				if s.ClusterTrustBundle != nil {
+					return true, nil, ErrStaticPodTriedToUseClusterTrustBundle
+				}
 			}
 		}
-	}
-	if len(v1Pod.Spec.ResourceClaims) > 0 {
-		return true, nil, ErrStaticPodTriedToUseResourceClaims
+		if len(v1Pod.Spec.ResourceClaims) > 0 {
+			return true, nil, ErrStaticPodTriedToUseResourceClaims
+		}
 	}
 
 	return true, v1Pod, nil
@@ -187,6 +202,16 @@ func tryDecodePodList(data []byte, defaultFn defaultFunc) (parsed bool, pods v1.
 		if errs := validation.ValidatePodCreate(newPod, validation.PodValidationOptions{}); len(errs) > 0 {
 			err = fmt.Errorf("invalid pod: %v", errs)
 			return true, pods, err
+		}
+		if utilfeature.DefaultFeatureGate.Enabled(features.PreventStaticPodAPIReferences) {
+			// Check if pod has references to API objects
+			_, resource, err := podutil.HasAPIObjectReference(newPod)
+			if err != nil {
+				return true, pods, err
+			}
+			if resource != "" {
+				return true, pods, fmt.Errorf("static pods may not reference %s", resource)
+			}
 		}
 	}
 	v1Pods := &v1.PodList{}
