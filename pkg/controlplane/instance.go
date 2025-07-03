82c443f1d945866821ac8d610cf6de8da3608990
Patrick Ohly <patrick.ohly@intel.com>
2025-07-03 15:17:35 +0200

    DRA API: v1 registration + tests

--- pkg/controlplane/instance.go
+++ pkg/controlplane/instance.go
@@ -49,6 +49,7 @@ import (
 	nodev1 "k8s.io/api/node/v1"
 	policyapiv1 "k8s.io/api/policy/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	resourcev1 "k8s.io/api/resource/v1"
 	resourcev1alpha3 "k8s.io/api/resource/v1alpha3"
 	resourcev1beta1 "k8s.io/api/resource/v1beta1"
 	resourcev1beta2 "k8s.io/api/resource/v1beta2"
@@ -459,6 +460,7 @@ var (
 		nodev1.SchemeGroupVersion,
 		policyapiv1.SchemeGroupVersion,
 		rbacv1.SchemeGroupVersion,
+		resourcev1.SchemeGroupVersion,
 		storageapiv1.SchemeGroupVersion,
 		schedulingapiv1.SchemeGroupVersion,
 		flowcontrolv1.SchemeGroupVersion,
