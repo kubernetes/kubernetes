0e0416ae9b7c846a19003b6188a03af6ee07066f
Patrick Ohly <patrick.ohly@intel.com>
2025-07-04 17:43:31 +0200

    DRA: use v1 API
    As before when adding v1beta2, DRA drivers built using the
    k8s.io/dynamic-resource-allocation helper packages remain compatible with all
    Kubernetes release >= 1.32. The helper code picks whatever API version is
    enabled from v1beta1/v1beta2/v1.
    
    However, the control plane now depends on v1, so a cluster configuration where
    only v1beta1 or v1beta2 are enabled without the v1 won't work.

--- staging/src/k8s.io/dynamic-resource-allocation/structured/internal/types.go
+++ staging/src/k8s.io/dynamic-resource-allocation/structured/internal/types.go
@@ -20,7 +20,7 @@ import (
 	"context"
 
 	v1 "k8s.io/api/core/v1"
-	resourceapi "k8s.io/api/resource/v1beta1"
+	resourceapi "k8s.io/api/resource/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 	draapi "k8s.io/dynamic-resource-allocation/api"
 )
