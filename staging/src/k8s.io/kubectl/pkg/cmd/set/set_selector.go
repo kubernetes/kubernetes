afccc34df1f73fa00054ed680ceb19dd702d6a53
tom1299 <tom1299@jeeatwork.com>
2025-07-07 08:15:52 +0200

    Fix help for set selector

--- staging/src/k8s.io/kubectl/pkg/cmd/set/set_selector.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/set/set_selector.go
@@ -72,9 +72,9 @@ var (
 		If --resource-version is specified, then updates will use this resource version, otherwise the existing resource-version will be used.
         Note: currently selectors can only be set on Service objects.`))
 	selectorExample = templates.Examples(`
-        # Set the labels and selector before creating a deployment/service pair
+        # Set the selector for a service before creating a matching pod
         kubectl create service clusterip my-svc --clusterip="None" -o yaml --dry-run=client | kubectl set selector --local -f - 'environment=qa' -o yaml | kubectl create -f -
-        kubectl create deployment my-dep -o yaml --dry-run=client | kubectl label --local -f - environment=qa -o yaml | kubectl create -f -`)
+        kubectl run my-pod --image=nginx --labels=environment=qa`)
 )
 
 // NewSelectorOptions returns an initialized SelectorOptions instance
