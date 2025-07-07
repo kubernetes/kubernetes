7a43f8bd273901ae8e8a9d31fa1e021038d47372
Chris Bandy <bandy.chris@gmail.com>
2025-07-07 17:37:39 -0500

    Omit values from validation rule error expressions

--- staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/schema/cel/validation.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/apiserver/schema/cel/validation.go
@@ -494,7 +494,9 @@ func (s *Validator) validateExpressions(ctx context.Context, fldPath *field.Path
 		}
 
 		value := obj
-		if sts.Type == "object" || sts.Type == "array" {
+		if ok {
+			value = field.OmitValueType{}
+		} else if sts.Type == "object" || sts.Type == "array" {
 			value = sts.Type
 		}
 
