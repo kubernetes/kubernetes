48f8458c41f9a73528ee73c3169cee84242c83b9
Rodrigo Campos <rodrigoca@microsoft.com>
2025-07-10 12:52:54 +0200

    validation: Fix user-namespaces test case name
    hostPath is a supported volume since Kubernetes 1.28. Let's update it.
    
    Signed-off-by: Rodrigo Campos <rodrigoca@microsoft.com>

--- pkg/apis/core/validation/validation_test.go
+++ pkg/apis/core/validation/validation_test.go
@@ -25618,7 +25618,7 @@ func TestValidateHostUsers(t *testing.T) {
 			}},
 		},
 	}, {
-		name:    "hostUsers=true - unsupported volume",
+		name:    "hostUsers=true - stateful volume",
 		success: true,
 		spec: &core.PodSpec{
 			SecurityContext: &core.PodSecurityContext{
