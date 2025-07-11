8c49e9612062df81e9d2ba28e5f5426674127de4
Min Jin <minkimzz@amazon.com>
2025-07-11 10:57:58 -0700

    fixes scheduler nil panic due to empty init container request&limit
    Signed-off-by: Min Jin <minkimzz@amazon.com>

--- staging/src/k8s.io/component-helpers/resource/helpers.go
+++ staging/src/k8s.io/component-helpers/resource/helpers.go
@@ -404,7 +404,12 @@ func maxResourceList(list, newList v1.ResourceList) {
 // max returns the result of max(a, b...) for each named resource and is only used if we can't
 // accumulate into an existing resource list
 func max(a v1.ResourceList, b ...v1.ResourceList) v1.ResourceList {
-	result := a.DeepCopy()
+	var result v1.ResourceList
+	if a != nil {
+		result = a.DeepCopy()
+	} else {
+		result = v1.ResourceList{}
+	}
 	for _, other := range b {
 		maxResourceList(result, other)
 	}
