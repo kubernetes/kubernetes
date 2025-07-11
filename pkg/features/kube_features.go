770c242989065fb1ed50959bc0ba8e99132401af
Marek Siarkowicz <siarkowicz@google.com>
2025-07-11 21:52:17 +0200

    Graduate ListFromCacheSnapshot to Beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1798,6 +1798,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	genericfeatures.ListFromCacheSnapshot: {
 		{Version: version.MustParse("1.33"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.34"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	genericfeatures.MutatingAdmissionPolicy: {
