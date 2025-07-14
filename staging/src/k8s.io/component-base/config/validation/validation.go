0d8fd61e64c99f1b6d97eed2c9d98f5947a6ec62
xiaoweim <xiaoweim@google.com>
2025-07-14 18:37:50 +0000

    address review comments

--- staging/src/k8s.io/component-base/config/validation/validation.go
+++ staging/src/k8s.io/component-base/config/validation/validation.go
@@ -49,13 +49,13 @@ func ValidateLeaderElectionConfiguration(cc *config.LeaderElectionConfiguration,
 		allErrs = append(allErrs, field.Invalid(fldPath.Child("leaseDuration"), cc.RenewDeadline, "must be greater than `RenewDeadline`"))
 	}
 	if len(cc.ResourceLock) == 0 {
-		allErrs = append(allErrs, field.Invalid(fldPath.Child("resourceLock"), cc.ResourceLock, "is required"))
+		allErrs = append(allErrs, field.Required(fldPath.Child("resourceLock"), ""))
 	}
 	if len(cc.ResourceNamespace) == 0 {
-		allErrs = append(allErrs, field.Invalid(fldPath.Child("resourceNamespace"), cc.ResourceNamespace, "is required"))
+		allErrs = append(allErrs, field.Required(fldPath.Child("resourceNamespace"), ""))
 	}
 	if len(cc.ResourceName) == 0 {
-		allErrs = append(allErrs, field.Invalid(fldPath.Child("resourceName"), cc.ResourceName, "is required"))
+		allErrs = append(allErrs, field.Required(fldPath.Child("resourceName"), ""))
 	}
 	return allErrs
 }
