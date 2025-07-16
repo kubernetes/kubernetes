e8536c01910f46fd7309cb4381c221d3d73efda9
Aaron Prindle <aprindle@google.com>
2025-07-16 22:01:04 +0000

    add item + union and item + zeroroneof output tests

---
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/item/zerorooneof/simple/doc_test.go
@@ -0,0 +1,59 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package zeroroneofsimple
+
+import (
+	"testing"
+
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+func Test(t *testing.T) {
+	st := localSchemeBuilder.Test(t)
+
+	st.Value(&Struct{
+		Tasks: []Task{
+			{Name: "other", State: "Other"},
+		},
+	}).ExpectValid()
+
+	st.Value(&Struct{
+		Tasks: []Task{
+			{Name: "succeeded", State: "Succeeded"},
+		},
+	}).ExpectValid()
+
+	st.Value(&Struct{
+		Tasks: []Task{},
+	}).ExpectValid()
+
+	invalidBothSet := &Struct{
+		Tasks: []Task{
+			{Name: "succeeded", State: "Succeeded"},
+			{Name: "failed", State: "Failed"},
+		},
+	}
+	st.Value(invalidBothSet).ExpectMatches(
+		field.ErrorMatcher{}.ByType().ByField().ByOrigin(),
+		field.ErrorList{
+			field.Invalid(field.NewPath("tasks"), nil, "").WithOrigin("zeroOrOneOf"),
+		},
+	)
+
+	// Test ratcheting.
+	st.Value(invalidBothSet).OldValue(invalidBothSet).ExpectValid()
+}
