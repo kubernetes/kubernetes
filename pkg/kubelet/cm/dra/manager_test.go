fd2d2fd66ff2fb34dc38711ce1c8b04a1bccd893
Sascha Grunert <sgrunert@redhat.com>
2025-07-17 13:06:41 +0200

    Convert `k8s.io/kubelet/pkg/apis/dra` from gogo to protoc
    Use standard protoc for the dra instead of gogo.
    
    Part of kubernetes#96564
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- pkg/kubelet/cm/dra/manager_test.go
+++ pkg/kubelet/cm/dra/manager_test.go
@@ -83,17 +83,17 @@ func (s *fakeDRADriverGRPCServer) NodePrepareResources(ctx context.Context, req 
 	}
 
 	if s.prepareResourcesResponse == nil {
-		cdiDeviceName := "claim-" + req.Claims[0].UID
+		cdiDeviceName := "claim-" + req.Claims[0].Uid
 		cdiID := s.driverName + "/" + driverClassName + "=" + cdiDeviceName
 		return &drapb.NodePrepareResourcesResponse{
 			Claims: map[string]*drapb.NodePrepareResourceResponse{
-				req.Claims[0].UID: {
+				req.Claims[0].Uid: {
 					Devices: []*drapb.Device{
 						{
 							PoolName:     poolName,
 							DeviceName:   deviceName,
 							RequestNames: []string{req.Claims[0].Name},
-							CDIDeviceIDs: []string{cdiID},
+							CdiDeviceIds: []string{cdiID},
 						},
 					},
 				},
@@ -114,7 +114,7 @@ func (s *fakeDRADriverGRPCServer) NodeUnprepareResources(ctx context.Context, re
 	if s.unprepareResourcesResponse == nil {
 		return &drapb.NodeUnprepareResourcesResponse{
 			Claims: map[string]*drapb.NodeUnprepareResourceResponse{
-				req.Claims[0].UID: {},
+				req.Claims[0].Uid: {},
 			},
 		}, nil
 	}
@@ -699,7 +699,7 @@ func TestPrepareResources(t *testing.T) {
 							PoolName:     poolName,
 							DeviceName:   deviceName,
 							RequestNames: []string{requestName},
-							CDIDeviceIDs: []string{cdiID},
+							CdiDeviceIds: []string{cdiID},
 						},
 					},
 				},
@@ -719,7 +719,7 @@ func TestPrepareResources(t *testing.T) {
 							PoolName:     poolName,
 							DeviceName:   deviceName,
 							RequestNames: []string{requestName},
-							CDIDeviceIDs: []string{cdiID},
+							CdiDeviceIds: []string{cdiID},
 						},
 					},
 				},
@@ -747,7 +747,7 @@ func TestPrepareResources(t *testing.T) {
 							PoolName:     poolName,
 							DeviceName:   deviceName,
 							RequestNames: []string{requestName},
-							CDIDeviceIDs: []string{cdiID},
+							CdiDeviceIds: []string{cdiID},
 						},
 					},
 				},
@@ -767,7 +767,7 @@ func TestPrepareResources(t *testing.T) {
 							PoolName:     poolName,
 							DeviceName:   deviceName,
 							RequestNames: []string{"container-0-request-0"},
-							CDIDeviceIDs: []string{cdiID},
+							CdiDeviceIds: []string{cdiID},
 						},
 					},
 				},
