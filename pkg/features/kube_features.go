e9492239b24341ced29bc0bed66c17856833c33e
zylxjtu <zhang.yuanliang@hotmail.com>
2025-07-18 16:29:31 +0000

    Promote windows graceful shutdown to Beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1760,6 +1760,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	WindowsGracefulNodeShutdown: {
 		{Version: version.MustParse("1.32"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.34"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	WindowsHostNetwork: {
