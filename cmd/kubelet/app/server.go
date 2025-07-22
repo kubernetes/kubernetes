f1b28b0d1f7d9f363e5b56cedb77d9951531f820
zhangzhifei16 <zhangzhifei16@jd.com>
2025-07-22 13:36:19 +0800

    chore(kubelet): migrate watchdog to contextual logging
    fix: fix failed typecheck
    
    fix unit test

--- cmd/kubelet/app/server.go
+++ cmd/kubelet/app/server.go
@@ -606,17 +606,17 @@ func getReservedCPUs(machineInfo *cadvisorapi.MachineInfo, cpus string) (cpuset.
 }
 
 func run(ctx context.Context, s *options.KubeletServer, kubeDeps *kubelet.Dependencies, featureGate featuregate.FeatureGate) (err error) {
+	logger := klog.FromContext(ctx)
 	if utilfeature.DefaultFeatureGate.Enabled(features.SystemdWatchdog) {
 		// NewHealthChecker returns an error indicating that the watchdog is configured but the configuration is incorrect,
 		// the kubelet will not be started.
-		healthChecker, err := watchdog.NewHealthChecker()
+		healthChecker, err := watchdog.NewHealthChecker(logger)
 		if err != nil {
 			return fmt.Errorf("create health checker: %w", err)
 		}
 		kubeDeps.HealthChecker = healthChecker
 		healthChecker.Start(ctx)
 	}
-	logger := klog.FromContext(ctx)
 	// Set global feature gates based on the value on the initial KubeletServer
 	err = utilfeature.DefaultMutableFeatureGate.SetFromMap(s.KubeletConfiguration.FeatureGates)
 	if err != nil {
