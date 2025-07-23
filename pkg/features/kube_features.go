b656027b9363c4c6dbf4fa634a71d355abd99345
Heba Elayoty <heelayot@microsoft.com>
2025-07-23 10:52:55 -0700

    Update MaxUnavailableStatefulSet feature gate to beta
    Signed-off-by: Heba Elayoty <heelayot@microsoft.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1404,6 +1404,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	MaxUnavailableStatefulSet: {
 		{Version: version.MustParse("1.24"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.35"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	MemoryManager: {
