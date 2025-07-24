86028eecf2af658cdba6f763f118c4d507c45d24
Joe Betz <jpbetz@google.com>
2025-07-24 15:36:13 -0400

    Add ratcheting of selectableFields

--- staging/src/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/validation/validation.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/validation/validation.go
@@ -145,7 +145,15 @@ type validationOptions struct {
 	// allowInvalidCABundle allows an invalid conversion webhook CABundle on update only if the existing CABundle is invalid.
 	// An invalid CABundle is also permitted on create and before a CRD is in an Established=True condition.
 	allowInvalidCABundle bool
+
+	// allowTooManySelectableFields allows more than the MaxSelectableFields on update only if the existing
+	// selectable field count is more than the max selectable fields and the selectable fields are unchanged.
+	allowTooManySelectableFields tooManySelectableFields
 }
+
+// tooManySelectableFields keys are either version names or "" for the unversioned root selectableFields.
+// The value is true when the selectable fields are allowed to be more than MaxSelectableFields.
+type tooManySelectableFields map[string]bool
 
 type preexistingExpressions struct {
 	rules              sets.Set[string]
@@ -238,8 +246,9 @@ func ValidateCustomResourceDefinitionUpdate(ctx context.Context, obj, oldObj *ap
 		preexistingExpressions:                   findPreexistingExpressions(&oldObj.Spec),
 		versionsWithUnchangedSchemas:             findVersionsWithUnchangedSchemas(obj, oldObj),
 		// strictCost is always true to enforce cost limits.
-		celEnvironmentSet:    environment.MustBaseEnvSet(environment.DefaultCompatibilityVersion(), true),
-		allowInvalidCABundle: allowInvalidCABundle(oldObj),
+		celEnvironmentSet:            environment.MustBaseEnvSet(environment.DefaultCompatibilityVersion(), true),
+		allowInvalidCABundle:         allowInvalidCABundle(oldObj),
+		allowTooManySelectableFields: allowTooManySelectableFields(obj, oldObj),
 	}
 	return validateCustomResourceDefinitionUpdate(ctx, obj, oldObj, opts)
 }
@@ -310,7 +319,7 @@ func validateCustomResourceDefinitionVersion(ctx context.Context, version *apiex
 			if err != nil {
 				allErrs = append(allErrs, field.Invalid(fldPath.Child("schema.openAPIV3Schema"), "", err.Error()))
 			}
-			allErrs = append(allErrs, ValidateCustomResourceSelectableFields(version.SelectableFields, schema, fldPath.Child("selectableFields"))...)
+			allErrs = append(allErrs, ValidateCustomResourceSelectableFields(version.SelectableFields, schema, fldPath.Child("selectableFields"), opts, version.Name)...)
 		}
 	}
 	return allErrs
@@ -484,7 +493,7 @@ func validateCustomResourceDefinitionSpec(ctx context.Context, spec *apiextensio
 				allErrs = append(allErrs, field.Invalid(fldPath.Child("schema.openAPIV3Schema"), "", err.Error()))
 			}
 
-			allErrs = append(allErrs, ValidateCustomResourceSelectableFields(spec.SelectableFields, schema, fldPath.Child("selectableFields"))...)
+			allErrs = append(allErrs, ValidateCustomResourceSelectableFields(spec.SelectableFields, schema, fldPath.Child("selectableFields"), opts, "")...)
 		}
 	}
 
@@ -563,6 +572,34 @@ func allowInvalidCABundle(oldCRD *apiextensions.CustomResourceDefinition) bool {
 		return false
 	}
 	return len(webhook.ValidateCABundle(field.NewPath("caBundle"), oldConversion.WebhookClientConfig.CABundle)) > 0
+}
+
+// allowTooManySelectableFields returns a struct indicating which selectable field sets are allowed to be invalid.
+// A set of selectable fields is allowed to be invalid if the existing custom resource definition has more
+// selectable fields than MaxSelectableFields and the selectable fields are unchanged.
+func allowTooManySelectableFields(obj *apiextensions.CustomResourceDefinition, oldCRD *apiextensions.CustomResourceDefinition) tooManySelectableFields {
+	result := tooManySelectableFields{}
+
+	if len(oldCRD.Spec.SelectableFields) > MaxSelectableFields && apiequality.Semantic.DeepEqual(obj.Spec.SelectableFields, oldCRD.Spec.SelectableFields) {
+		result[""] = true
+	}
+
+	oldVersions := make(map[string]apiextensions.CustomResourceDefinitionVersion, len(oldCRD.Spec.Versions))
+	for _, v := range oldCRD.Spec.Versions {
+		oldVersions[v.Name] = v
+	}
+
+	for _, v := range obj.Spec.Versions {
+		oldV, ok := oldVersions[v.Name]
+		if !ok {
+			continue
+		}
+		if len(oldV.SelectableFields) > MaxSelectableFields && apiequality.Semantic.DeepEqual(v.SelectableFields, oldV.SelectableFields) {
+			result[v.Name] = true
+		}
+	}
+
+	return result
 }
 
 // hasValidConversionReviewVersion return true if there is a valid version or if the list is empty.
@@ -813,7 +850,7 @@ func ValidateCustomResourceColumnDefinition(col *apiextensions.CustomResourceCol
 	return allErrs
 }
 
-func ValidateCustomResourceSelectableFields(selectableFields []apiextensions.SelectableField, schema *structuralschema.Structural, fldPath *field.Path) (allErrs field.ErrorList) {
+func ValidateCustomResourceSelectableFields(selectableFields []apiextensions.SelectableField, schema *structuralschema.Structural, fldPath *field.Path, opts validationOptions, version string) (allErrs field.ErrorList) {
 	uniqueSelectableFields := sets.New[string]()
 	for i, selectableField := range selectableFields {
 		indexFldPath := fldPath.Index(i)
@@ -840,7 +877,11 @@ func ValidateCustomResourceSelectableFields(selectableFields []apiextensions.Sel
 		}
 	}
 	uniqueSelectableFieldCount := uniqueSelectableFields.Len()
-	if uniqueSelectableFieldCount > MaxSelectableFields {
+	allowInvalid := false
+	if opts.allowTooManySelectableFields != nil {
+		allowInvalid = opts.allowTooManySelectableFields[version]
+	}
+	if uniqueSelectableFieldCount > MaxSelectableFields && !allowInvalid {
 		allErrs = append(allErrs, field.TooMany(fldPath, uniqueSelectableFieldCount, MaxSelectableFields))
 	}
 	return allErrs
