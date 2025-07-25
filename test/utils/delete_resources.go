505ed56cb5a0342dd88cafc0e4ed4c58fa8964f7
Adam Byrne <adam.r.byrne@gmail.com>
2025-07-25 12:21:18 +0100

    refactor: encapsulated delete via options in delete_resources to clean up

--- test/utils/delete_resources.go
+++ test/utils/delete_resources.go
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// TODO: Refactor common part of functions in this file for generic object kinds.
-
 package utils
 
 import (
@@ -31,26 +29,37 @@ import (
 	extensionsinternal "k8s.io/kubernetes/pkg/apis/extensions"
 )
 
+// ResourceDeleter interface for common delete operations
+type ResourceDeleter interface {
+	Delete(context.Context, string, metav1.DeleteOptions) error
+}
+
+// deleteGenericResource handles deletion for any resource implementing ResourceDeleter
+func deleteGenericResource(ctx context.Context, deleter ResourceDeleter, name string, options metav1.DeleteOptions) error {
+	return deleter.Delete(ctx, name, options)
+}
+
 func DeleteResource(c clientset.Interface, kind schema.GroupKind, namespace, name string, options metav1.DeleteOptions) error {
+	ctx := context.TODO()
 	switch kind {
 	case api.Kind("Pod"):
-		return c.CoreV1().Pods(namespace).Delete(context.TODO(), name, options)
+		return deleteGenericResource(ctx, c.CoreV1().Pods(namespace), name, options)
 	case api.Kind("ReplicationController"):
-		return c.CoreV1().ReplicationControllers(namespace).Delete(context.TODO(), name, options)
+		return deleteGenericResource(ctx, c.CoreV1().ReplicationControllers(namespace), name, options)
 	case extensionsinternal.Kind("ReplicaSet"), appsinternal.Kind("ReplicaSet"):
-		return c.AppsV1().ReplicaSets(namespace).Delete(context.TODO(), name, options)
+		return deleteGenericResource(ctx, c.AppsV1().ReplicaSets(namespace), name, options)
 	case extensionsinternal.Kind("Deployment"), appsinternal.Kind("Deployment"):
-		return c.AppsV1().Deployments(namespace).Delete(context.TODO(), name, options)
+		return deleteGenericResource(ctx, c.AppsV1().Deployments(namespace), name, options)
 	case extensionsinternal.Kind("DaemonSet"):
-		return c.AppsV1().DaemonSets(namespace).Delete(context.TODO(), name, options)
+		return deleteGenericResource(ctx, c.AppsV1().DaemonSets(namespace), name, options)
 	case batchinternal.Kind("Job"):
-		return c.BatchV1().Jobs(namespace).Delete(context.TODO(), name, options)
+		return deleteGenericResource(ctx, c.BatchV1().Jobs(namespace), name, options)
 	case api.Kind("Secret"):
-		return c.CoreV1().Secrets(namespace).Delete(context.TODO(), name, options)
+		return deleteGenericResource(ctx, c.CoreV1().Secrets(namespace), name, options)
 	case api.Kind("ConfigMap"):
-		return c.CoreV1().ConfigMaps(namespace).Delete(context.TODO(), name, options)
+		return deleteGenericResource(ctx, c.CoreV1().ConfigMaps(namespace), name, options)
 	case api.Kind("Service"):
-		return c.CoreV1().Services(namespace).Delete(context.TODO(), name, options)
+		return deleteGenericResource(ctx, c.CoreV1().Services(namespace), name, options)
 	default:
 		return fmt.Errorf("unsupported kind when deleting: %v", kind)
 	}
