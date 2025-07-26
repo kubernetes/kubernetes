3a68a386eb7713dc6fa3a0bb4799d78bcb54409e
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2025-07-26 14:08:52 +1000

    Use DifferenceSeq() when iterating to avoid materializing the difference as a set

--- pkg/proxy/ipvs/proxier.go
+++ pkg/proxy/ipvs/proxier.go
@@ -1895,7 +1895,7 @@ func (proxier *Proxier) syncEndpoint(svcPortName proxy.ServicePortName, onlyNode
 	}
 
 	// Delete old endpoints
-	for _, ep := range curEndpoints.Difference(newEndpoints).UnsortedList() {
+	for ep := range curEndpoints.DifferenceSeq(newEndpoints) {
 		// if curEndpoint is in gracefulDelete, skip
 		uniqueRS := vs.String() + "/" + ep
 		if proxier.gracefuldeleteManager.InTerminationList(uniqueRS) {
