f39069b69a6784acee7a6614aae7606230865506
Yevhen Dubovskoi <ydubovskoi@google.com>
2025-07-29 09:58:52 +0000

    add fake-registry-server command to agnhost
    * command run the limited subset of OCI API and serve static
    images from the registry (which are pre-loaded when building an image)
    * add pause version to zeitgeist rule for agnhost
    * bump version of agnhost to 2.57

---
+++ test/images/agnhost/fakeregistryserver/prepare_registry.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Copyright 2025 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# This script prepares a directory with container images to be used as a fake registry,
+# then creates a tarball of that directory inside the container.
+
+# function to download an image manifest and its blobs to create a fake registry layout.
+prepare_image() {
+    local image_name="$1"
+    local tag="$2"
+    local internal_tag="$3"
+    local registry_dir="$4"
+    local registry_url="registry.k8s.io"
+    local image_dir="$registry_dir/$image_name"
+
+    echo "--- Preparing image: ${image_name}:${tag} as ${image_name}:${internal_tag} ---"
+
+    mkdir -p "$image_dir/manifests"
+    mkdir -p "$image_dir/blobs"
+
+    echo "Downloading and filtering manifest list for $image_name:$tag..."
+    local manifest_list_path="$image_dir/manifests/${internal_tag}_index"
+    # download the manifest and pipe it to jq to filter out windows images
+    crane manifest "$registry_url/$image_name:$tag" | jq '.manifests |= map(select(.platform.os != "windows"))' > "$manifest_list_path"
+    echo "Saved manifest list to $manifest_list_path"
+
+    local manifest_digest="sha256:$(sha256sum < "$manifest_list_path" | awk '{print $1}')"
+    cp "$manifest_list_path" "$image_dir/manifests/$manifest_digest"
+    echo "Created digest-named copy of manifest list: $manifest_digest"
+
+    echo "Parsing manifest list and downloading individual manifests and blobs..."
+    
+    jq -r '.manifests[].digest' < "$manifest_list_path" | while read -r individual_manifest_digest; do
+      echo "  Downloading manifest $individual_manifest_digest..."
+      local individual_manifest_path="$image_dir/manifests/$individual_manifest_digest"
+      crane manifest "$registry_url/$image_name@$individual_manifest_digest" > "$individual_manifest_path"
+      echo "  Saved manifest to $individual_manifest_path"
+
+      local config_digest
+      config_digest=$(jq -r '.config.digest' < "$individual_manifest_path")
+      echo "    Downloading config blob $config_digest..."
+      crane blob "$registry_url/$image_name@$config_digest" > "$image_dir/blobs/$config_digest"
+      echo "    Saved config blob to $image_dir/blobs/$config_digest"
+
+      jq -r '.layers[].digest' < "$individual_manifest_path" | while read -r layer_digest; do
+        echo "    Downloading layer blob $layer_digest..."
+        crane blob "$registry_url/$image_name@$layer_digest" > "$image_dir/blobs/$layer_digest"
+        echo "    Saved layer blob to $image_dir/blobs/$layer_digest"
+      done
+    done
+
+    echo "--- Successfully prepared ${image_name}:${internal_tag} ---"
+}
+
+# create the registry directory
+mkdir -p /registry
+
+echo "--> Processing images.txt..."
+while read -r image tag internal_tag; do
+    # skip empty lines or comments
+    [[ -z "$image" || "$image" == \#* ]] && continue
+    prepare_image "$image" "$tag" "$internal_tag" "/registry"
+done < /images.txt
+
+# create the final tarball inside the container
+echo "--> Creating registry tarball..."
+tar -czf /registry.tar.gz -C / registry
+
+echo "--> Done"
