3e3b2447ffe7643eb47eae41f2b9ed29e8775b0e
Dixita <ndixita@google.com>
2025-07-29 17:28:26 -0700

    Promote PodLevelResources to Beta (#132999)
    Signed-off-by: ndixita <ndixita@google.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1536,6 +1536,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	PodLevelResources: {
 		{Version: version.MustParse("1.32"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.34"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	PodLifecycleSleepAction: {
