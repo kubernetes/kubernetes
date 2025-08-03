e6a7a8cc7f581bd23d5a101fb6582d7c31f54c69
Tim Hockin <thockin@google.com>
2025-08-02 18:34:06 -0700

    Remove ExpectInvalid()
    All use cases are converted.
    
    Co-Authored-by: Yongrui Lin <yongrlin@outlook.com>

--- staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/listmap/multiple_keys/doc_test.go
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/listmap/multiple_keys/doc_test.go
@@ -105,26 +105,26 @@ func TestUpdateCorrelation(t *testing.T) {
 
 	st.Value(&structA2).OldValue(&structA1).ExpectValid()
 
-	st.Value(&structA1).OldValue(&structB).ExpectInvalid(
-		field.Forbidden(field.NewPath("listField").Index(0), "field is immutable"),
-		field.Forbidden(field.NewPath("listField").Index(1), "field is immutable"),
-		field.Forbidden(field.NewPath("listTypedefField").Index(0), "field is immutable"),
-		field.Forbidden(field.NewPath("listTypedefField").Index(1), "field is immutable"),
-		field.Forbidden(field.NewPath("typedefField").Index(0), "field is immutable"),
-		field.Forbidden(field.NewPath("typedefField").Index(1), "field is immutable"),
-	)
+	st.Value(&structA1).OldValue(&structB).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByDetailSubstring().ByOrigin(), field.ErrorList{
+		field.Forbidden(field.NewPath("listField").Index(0), "immutable"),
+		field.Forbidden(field.NewPath("listField").Index(1), "immutable"),
+		field.Forbidden(field.NewPath("listTypedefField").Index(0), "immutable"),
+		field.Forbidden(field.NewPath("listTypedefField").Index(1), "immutable"),
+		field.Forbidden(field.NewPath("typedefField").Index(0), "immutable"),
+		field.Forbidden(field.NewPath("typedefField").Index(1), "immutable"),
+	})
 
-	st.Value(&structB).OldValue(&structA1).ExpectInvalid(
-		field.Forbidden(field.NewPath("listField").Index(0), "field is immutable"),
-		field.Forbidden(field.NewPath("listField").Index(1), "field is immutable"),
-		field.Forbidden(field.NewPath("listField").Index(2), "field is immutable"),
-		field.Forbidden(field.NewPath("listTypedefField").Index(0), "field is immutable"),
-		field.Forbidden(field.NewPath("listTypedefField").Index(1), "field is immutable"),
-		field.Forbidden(field.NewPath("listTypedefField").Index(2), "field is immutable"),
-		field.Forbidden(field.NewPath("typedefField").Index(0), "field is immutable"),
-		field.Forbidden(field.NewPath("typedefField").Index(1), "field is immutable"),
-		field.Forbidden(field.NewPath("typedefField").Index(2), "field is immutable"),
-	)
+	st.Value(&structB).OldValue(&structA1).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByDetailSubstring().ByOrigin(), field.ErrorList{
+		field.Forbidden(field.NewPath("listField").Index(0), "immutable"),
+		field.Forbidden(field.NewPath("listField").Index(1), "immutable"),
+		field.Forbidden(field.NewPath("listField").Index(2), "immutable"),
+		field.Forbidden(field.NewPath("listTypedefField").Index(0), "immutable"),
+		field.Forbidden(field.NewPath("listTypedefField").Index(1), "immutable"),
+		field.Forbidden(field.NewPath("listTypedefField").Index(2), "immutable"),
+		field.Forbidden(field.NewPath("typedefField").Index(0), "immutable"),
+		field.Forbidden(field.NewPath("typedefField").Index(1), "immutable"),
+		field.Forbidden(field.NewPath("typedefField").Index(2), "immutable"),
+	})
 }
 
 func TestRatcheting(t *testing.T) {
