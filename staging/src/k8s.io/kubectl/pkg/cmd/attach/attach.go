640dabd58b04b72f646ed85947cb8b407b36dc08
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2025-08-03 20:35:40 +1000

    Decouple term and remotecommand packages
    This allows consumers of term to not pull in dependencies on
    github.com/gorilla/websocket and github.com/moby/spdystream.

--- staging/src/k8s.io/kubectl/pkg/cmd/attach/attach.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/attach/attach.go
@@ -42,6 +42,7 @@ import (
 	"k8s.io/kubectl/pkg/util/completion"
 	"k8s.io/kubectl/pkg/util/i18n"
 	"k8s.io/kubectl/pkg/util/templates"
+	"k8s.io/kubectl/pkg/util/term"
 )
 
 var (
@@ -300,7 +301,9 @@ func (o *AttachOptions) Run() error {
 			sizePlusOne.Height++
 
 			// this call spawns a goroutine to monitor/update the terminal size
-			sizeQueue = t.MonitorSize(&sizePlusOne, size)
+			sizeQueue = &terminalSizeQueueAdapter{
+				delegate: t.MonitorSize(&sizePlusOne, size),
+			}
 		}
 
 		o.DisableStderr = true
@@ -356,4 +359,19 @@ func (o *AttachOptions) reattachMessage(containerName string, rawTTY bool) strin
 		return fmt.Sprintf("Session ended, the ephemeral container will not be restarted but may be reattached using '%s %s -c %s -i -t' if it is still running", o.CommandName, o.Pod.Name, containerName)
 	}
 	return fmt.Sprintf("Session ended, resume using '%s %s -c %s -i -t' command when the pod is running", o.CommandName, o.Pod.Name, containerName)
+}
+
+type terminalSizeQueueAdapter struct {
+	delegate term.TerminalSizeQueue
+}
+
+func (a *terminalSizeQueueAdapter) Next() *remotecommand.TerminalSize {
+	next := a.delegate.Next()
+	if next == nil {
+		return nil
+	}
+	return &remotecommand.TerminalSize{
+		Width:  next.Width,
+		Height: next.Height,
+	}
 }
