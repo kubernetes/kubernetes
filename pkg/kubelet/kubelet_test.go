9857f6c25c369c9c272e0db2986be5c6e7fd594e
Natasha Sarkar <natashasarkar@google.com>
2025-08-05 22:27:46 +0000

    move resize infeasibility check into an admitHandler

--- pkg/kubelet/kubelet_test.go
+++ pkg/kubelet/kubelet_test.go
@@ -398,6 +398,7 @@ func newTestKubeletWithImageList(
 	kubelet.evictionManager = evictionManager
 	handlers := []lifecycle.PodAdmitHandler{}
 	handlers = append(handlers, evictionAdmitHandler)
+	handlers = append(handlers, allocation.NewPodResizesAdmitHandler(kubelet.containerManager, fakeRuntime, kubelet.allocationManager, logger))
 
 	// setup shutdown manager
 	shutdownManager := nodeshutdown.NewManager(&nodeshutdown.Config{
