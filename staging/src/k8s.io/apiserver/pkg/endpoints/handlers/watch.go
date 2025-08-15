09183490f7f071f2386553ebf04a1edadb5ad9a7
David Bimmler <david.bimmler@isovalent.com>
2025-08-15 08:30:57 +0200

    handlers: do not allocate chan for nothing
    Nil chan has the desired semantics already, and this breaks
    testing/synctest because the channel is allocated outside of the bubble.
    
    Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

--- staging/src/k8s.io/apiserver/pkg/endpoints/handlers/watch.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/handlers/watch.go
@@ -39,9 +39,6 @@ import (
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 )
 
-// nothing will ever be sent down this channel
-var neverExitWatch <-chan time.Time = make(chan time.Time)
-
 // timeoutFactory abstracts watch timeout logic for testing
 type TimeoutFactory interface {
 	TimeoutCh() (<-chan time.Time, func() bool)
@@ -56,7 +53,8 @@ type realTimeoutFactory struct {
 // and a cleanup function to call when this happens.
 func (w *realTimeoutFactory) TimeoutCh() (<-chan time.Time, func() bool) {
 	if w.timeout == 0 {
-		return neverExitWatch, func() bool { return false }
+		// nothing will ever be sent down this channel
+		return nil, func() bool { return false }
 	}
 	t := time.NewTimer(w.timeout)
 	return t.C, t.Stop
