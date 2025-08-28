12a784b46bd7ba03beda3b4b2c979f03799f068f
kincoy <kincoyao@gmail.com>
2025-08-28 14:28:55 +0800

    cleanup: remove redundant type conversions in podautoscaler
    Signed-off-by: kincoy <kincoyao@gmail.com>

--- pkg/controller/podautoscaler/replica_calculator.go
+++ pkg/controller/podautoscaler/replica_calculator.go
@@ -185,7 +185,7 @@ func (c *ReplicaCalculator) GetMetricReplicas(currentReplicas int32, targetUsage
 		return 0, 0, time.Time{}, fmt.Errorf("unable to get metric %s: %v", metricName, err)
 	}
 
-	replicaCount, usage, err = c.calcPlainMetricReplicas(metrics, currentReplicas, targetUsage, tolerances, namespace, selector, v1.ResourceName(""))
+	replicaCount, usage, err = c.calcPlainMetricReplicas(metrics, currentReplicas, targetUsage, tolerances, namespace, selector, "")
 	return replicaCount, usage, timestamp, err
 }
 
