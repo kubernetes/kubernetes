0cac2cb1ed61cbfab0ae517b07c79b1a10a3e15e
Marek Siarkowicz <siarkowicz@google.com>
2025-09-03 15:42:41 +0200

    Disable estimating resource size for resources with watch cache disabled
    Listing all keys from etcd turned out to be too expensive, negativly
    impacting events POST latency. Events resource is the only resource that
    by default has watch cache disabled and which includes very
    large number of small objects making it very costly to list keys.
    
    Expected impact:
    * No apiserver_resource_size_estimate_bytes metric for events.
    * APF overestimating LIST request cost to events. Fallback assumes
      object size of 1.5MB, meaning LIST events will always get maxSeats

--- staging/src/k8s.io/apiserver/pkg/storage/etcd3/store_test.go
+++ staging/src/k8s.io/apiserver/pkg/storage/etcd3/store_test.go
@@ -384,6 +384,7 @@ func TestStats(t *testing.T) {
 			featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.SizeBasedListCostEstimate, sizeBasedListCostEstimate)
 			// Match transformer with cacher tests.
 			ctx, store, _ := testSetup(t)
+			store.SetKeysFunc(store.getKeys)
 			storagetesting.RunTestStats(ctx, t, store, store.codec, store.transformer, sizeBasedListCostEstimate)
 		})
 	}
@@ -1041,15 +1042,30 @@ func TestPrefixStats(t *testing.T) {
 	tcs := []struct {
 		name        string
 		estimate    bool
+		setKeys     bool
 		expectStats storage.Stats
 	}{
 		{
-			name:        "SizeBasedListCostEstimate=false",
+			name:        "SizeBasedListCostEstimate=false,SetKeys=false",
+			setKeys:     false,
 			estimate:    false,
 			expectStats: storage.Stats{ObjectCount: 1},
 		},
 		{
-			name:        "SizeBasedListCostEstimate=true",
+			name:        "SizeBasedListCostEstimate=false,SetKeys=true",
+			setKeys:     true,
+			estimate:    false,
+			expectStats: storage.Stats{ObjectCount: 1},
+		},
+		{
+			name:        "SizeBasedListCostEstimate=true,SetKeys=false",
+			setKeys:     false,
+			estimate:    true,
+			expectStats: storage.Stats{ObjectCount: 1},
+		},
+		{
+			name:        "SizeBasedListCostEstimate=true,SetKeys=true",
+			setKeys:     true,
 			estimate:    true,
 			expectStats: storage.Stats{ObjectCount: 1, EstimatedAverageObjectSizeBytes: 3},
 		},
@@ -1058,6 +1074,9 @@ func TestPrefixStats(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.SizeBasedListCostEstimate, tc.estimate)
 			ctx, store, c := testSetup(t, withPrefix("/registry"), withResourcePrefix("pods"))
+			if tc.setKeys {
+				store.SetKeysFunc(store.getKeys)
+			}
 			_, err := c.KV.Put(ctx, "key", "a")
 			if err != nil {
 				t.Fatal(err)
