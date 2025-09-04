925d51af43ca70c076101f153d7456bfbf46106f
choejwoo <jaewoo45@gmail.com>
2025-09-04 14:27:27 +0900

    fix: correct typos in cronjob and job controller comments
    - Fix 'expexted' -> 'expected' in utils_test.go
    - Fix 'rouge' -> 'rogue' in utils_test.go
    - Fix 'expotential' -> 'exponential' in job_controller.go
    - Fix 'MaxGeneneratedNameLength' -> 'MaxGeneratedNameLength' in indexed_job_utils_test.go
    
    Signed-off-by: choejwoo <jaewoo45@gmail.com>

--- pkg/controller/job/job_controller.go
+++ pkg/controller/job/job_controller.go
@@ -123,7 +123,7 @@ type Controller struct {
 
 	clock clock.WithTicker
 
-	// Store with information to compute the expotential backoff delay for pod
+	// Store with information to compute the exponential backoff delay for pod
 	// recreation in case of pod failures.
 	podBackoffStore *backoffStore
 
