kubernetes/apiserver

Cleanup enabling resource size estimate
Kubernetes-commit: edc8dafc6993dd7928cc148687a23d7eaaa23f99

pkg/storage/etcd3/stats.go
@@ -18,7 +18,6 @@ package etcd3
 
 import (
 	"context"
-	"errors"
 	"strings"
 	"sync"
 
@@ -34,11 +33,11 @@ import (
 
 const sizerRefreshInterval = time.Minute
 
-func newStatsCache(prefix string, getKeys storage.KeysFunc) *statsCache {
+func newResourceSizeEstimator(prefix string, getKeys storage.KeysFunc) *resourceSizeEstimator {
 	if prefix[len(prefix)-1] != '/' {
 		prefix += "/"
 	}
-	sc := &statsCache{
+	sc := &resourceSizeEstimator{
 		prefix:  prefix,
 		getKeys: getKeys,
 		stop:    make(chan struct{}),
@@ -52,23 +51,21 @@ func newStatsCache(prefix string, getKeys storage.KeysFunc) *statsCache {
 	return sc
 }
 
-// statsCache efficiently estimates the average object size
+// resourceSizeEstimator efficiently estimates the average object size
 // based on the last observed state of individual keys.
-// By plugging statsCache into GetList and Watch functions,
+// By plugging resourceSizeEstimator into GetList and Watch functions,
 // a fairly accurate estimate of object sizes can be maintained
 // without additional requests to the underlying storage.
 // To handle potential out-of-order or incomplete data,
 // it uses a per-key revision to identify the newer state.
 // This approach may leak keys if delete events are not observed,
 // thus we run a background goroutine to periodically cleanup keys if needed.
-type statsCache struct {
+type resourceSizeEstimator struct {
 	prefix         string
 	stop           chan struct{}
 	wg             sync.WaitGroup
 	lastKeyCleanup atomic.Pointer[time.Time]
-
-	getKeysLock sync.Mutex
-	getKeys     storage.KeysFunc
+	getKeys        storage.KeysFunc
 
 	keysLock sync.Mutex
 	keys     map[string]sizeRevision
@@ -79,10 +76,8 @@ type sizeRevision struct {
 	revision  int64
 }
 
-var errStatsDisabled = errors.New("key size stats disabled")
-
-func (sc *statsCache) Stats(ctx context.Context) (storage.Stats, error) {
-	keys, err := sc.GetKeys(ctx)
+func (sc *resourceSizeEstimator) Stats(ctx context.Context) (storage.Stats, error) {
+	keys, err := sc.getKeys(ctx)
 	if err != nil {
 		return storage.Stats{}, err
 	}
@@ -98,30 +93,12 @@ func (sc *statsCache) Stats(ctx context.Context) (storage.Stats, error) {
 	return stats, nil
 }
 
-func (sc *statsCache) GetKeys(ctx context.Context) ([]string, error) {
-	sc.getKeysLock.Lock()
-	getKeys := sc.getKeys
-	sc.getKeysLock.Unlock()
-
-	if getKeys == nil {
-		return nil, errStatsDisabled
-	}
-	// Don't execute getKeys under lock.
-	return getKeys(ctx)
-}
-
-func (sc *statsCache) SetKeysFunc(keys storage.KeysFunc) {
-	sc.getKeysLock.Lock()
-	defer sc.getKeysLock.Unlock()
-	sc.getKeys = keys
-}
-
-func (sc *statsCache) Close() {
+func (sc *resourceSizeEstimator) Close() {
 	close(sc.stop)
 	sc.wg.Wait()
 }
 
-func (sc *statsCache) run() {
+func (sc *resourceSizeEstimator) run() {
 	jitter := 0.5 // Period between [interval, interval * (1.0 + jitter)]
 	sliding := true
 	// wait.JitterUntilWithContext starts work immediately, so wait first.
@@ -132,16 +109,14 @@ func (sc *statsCache) run() {
 	wait.JitterUntilWithContext(wait.ContextForChannel(sc.stop), sc.cleanKeysIfNeeded, sizerRefreshInterval, jitter, sliding)
 }
 
-func (sc *statsCache) cleanKeysIfNeeded(ctx context.Context) {
+func (sc *resourceSizeEstimator) cleanKeysIfNeeded(ctx context.Context) {
 	lastKeyCleanup := sc.lastKeyCleanup.Load()
 	if lastKeyCleanup != nil && time.Since(*lastKeyCleanup) < sizerRefreshInterval {
 		return
 	}
-	keys, err := sc.GetKeys(ctx)
+	keys, err := sc.getKeys(ctx)
 	if err != nil {
-		if !errors.Is(err, errStatsDisabled) {
-			klog.InfoS("Error getting keys", "err", err)
-		}
+		klog.InfoS("Error getting keys", "err", err)
 		return
 	}
 	sc.keysLock.Lock()
@@ -149,7 +124,7 @@ func (sc *statsCache) cleanKeysIfNeeded(ctx context.Context) {
 	sc.cleanKeys(keys)
 }
 
-func (sc *statsCache) cleanKeys(keepKeys []string) {
+func (sc *resourceSizeEstimator) cleanKeys(keepKeys []string) {
 	newKeys := make(map[string]sizeRevision, len(keepKeys))
 	for _, key := range keepKeys {
 		// Handle cacher keys not having prefix.
@@ -171,14 +146,14 @@ func (sc *statsCache) cleanKeys(keepKeys []string) {
 	sc.lastKeyCleanup.Store(&now)
 }
 
-func (sc *statsCache) keySizes() (totalSize int64) {
+func (sc *resourceSizeEstimator) keySizes() (totalSize int64) {
 	for _, sizeRevision := range sc.keys {
 		totalSize += sizeRevision.sizeBytes
 	}
 	return totalSize
 }
 
-func (sc *statsCache) Update(kvs []*mvccpb.KeyValue) {
+func (sc *resourceSizeEstimator) Update(kvs []*mvccpb.KeyValue) {
 	sc.keysLock.Lock()
 	defer sc.keysLock.Unlock()
 	for _, kv := range kvs {
@@ -186,14 +161,14 @@ func (sc *statsCache) Update(kvs []*mvccpb.KeyValue) {
 	}
 }
 
-func (sc *statsCache) UpdateKey(kv *mvccpb.KeyValue) {
+func (sc *resourceSizeEstimator) UpdateKey(kv *mvccpb.KeyValue) {
 	sc.keysLock.Lock()
 	defer sc.keysLock.Unlock()
 
 	sc.updateKey(kv)
 }
 
-func (sc *statsCache) updateKey(kv *mvccpb.KeyValue) {
+func (sc *resourceSizeEstimator) updateKey(kv *mvccpb.KeyValue) {
 	key := string(kv.Key)
 	keySizeRevision := sc.keys[key]
 	if keySizeRevision.revision >= kv.ModRevision {
@@ -206,7 +181,7 @@ func (sc *statsCache) updateKey(kv *mvccpb.KeyValue) {
 	}
 }
 
-func (sc *statsCache) DeleteKey(kv *mvccpb.KeyValue) {
+func (sc *resourceSizeEstimator) DeleteKey(kv *mvccpb.KeyValue) {
 	sc.keysLock.Lock()
 	defer sc.keysLock.Unlock()
 

pkg/storage/etcd3/store.go
@@ -25,6 +25,7 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"go.etcd.io/etcd/api/v3/mvccpb"
@@ -90,8 +91,10 @@ type store struct {
 
 	resourcePrefix string
 	newListFunc    func() runtime.Object
-	stats          *statsCache
 	compactor      Compactor
+
+	collectorMux          sync.RWMutex
+	resourceSizeEstimator *resourceSizeEstimator
 }
 
 var _ storage.Interface = (*store)(nil)
@@ -189,13 +192,8 @@ func New(c *kubernetes.Client, compactor Compactor, codec runtime.Codec, newFunc
 		newListFunc:    newListFunc,
 		compactor:      compactor,
 	}
-	// Collecting stats requires properly set resourcePrefix to call getKeys.
-	if utilfeature.DefaultFeatureGate.Enabled(features.SizeBasedListCostEstimate) {
-		stats := newStatsCache(pathPrefix, nil)
-		s.stats = stats
-		w.stats = stats
-	}
 
+	w.getResourceSizeEstimator = s.getResourceSizeEstimator
 	w.getCurrentStorageRV = func(ctx context.Context) (uint64, error) {
 		return s.GetCurrentResourceVersion(ctx)
 	}
@@ -218,9 +216,16 @@ func (s *store) Versioner() storage.Versioner {
 }
 
 func (s *store) Close() {
-	if s.stats != nil {
-		s.stats.Close()
+	stats := s.getResourceSizeEstimator()
+	if stats != nil {
+		stats.Close()
 	}
+}
+
+func (s *store) getResourceSizeEstimator() *resourceSizeEstimator {
+	s.collectorMux.RLock()
+	defer s.collectorMux.RUnlock()
+	return s.resourceSizeEstimator
 }
 
 // Get implements storage.Interface.Get.
@@ -633,13 +638,12 @@ func getNewItemFunc(listObj runtime.Object, v reflect.Value) func() runtime.Obje
 	}
 }
 
-func (s *store) Stats(ctx context.Context) (stats storage.Stats, err error) {
-	if s.stats != nil {
-		stats, err := s.stats.Stats(ctx)
-		if !errors.Is(err, errStatsDisabled) {
-			return stats, err
-		}
+func (s *store) Stats(ctx context.Context) (storage.Stats, error) {
+	if collector := s.getResourceSizeEstimator(); collector != nil {
+		return collector.Stats(ctx)
 	}
+	// returning stats without resource size
+
 	startTime := time.Now()
 	prefix, err := s.prepareKey(s.resourcePrefix)
 	if err != nil {
@@ -658,10 +662,17 @@ func (s *store) Stats(ctx context.Context) (stats storage.Stats, err error) {
 	}, nil
 }
 
-func (s *store) SetKeysFunc(keys storage.KeysFunc) {
-	if s.stats != nil {
-		s.stats.SetKeysFunc(keys)
+func (s *store) EnableResourceSizeEstimation(getKeys storage.KeysFunc) error {
+	if getKeys == nil {
+		return errors.New("KeysFunc cannot be nil")
 	}
+	s.collectorMux.Lock()
+	defer s.collectorMux.Unlock()
+	if s.resourceSizeEstimator != nil {
+		return errors.New("resourceSizeEstimator already enabled")
+	}
+	s.resourceSizeEstimator = newResourceSizeEstimator(s.pathPrefix, getKeys)
+	return nil
 }
 
 func (s *store) getKeys(ctx context.Context) ([]string, error) {
@@ -780,6 +791,8 @@ func (s *store) GetList(ctx context.Context, key string, opts storage.ListOption
 		metricsOp = "list"
 	}
 
+	stats := s.getResourceSizeEstimator()
+
 	aggregator := s.listErrAggrFactory()
 	for {
 		startTime := time.Now()
@@ -821,8 +834,8 @@ func (s *store) GetList(ctx context.Context, key string, opts storage.ListOption
 		} else {
 			growSlice(v, 2048, len(getResp.Kvs))
 		}
-		if s.stats != nil {
-			s.stats.Update(getResp.Kvs)
+		if stats != nil {
+			stats.Update(getResp.Kvs)
 		}
 
 		// take items from the response until the bucket is full, filtering as we go

kubernetes/kubernetes

staging/src/k8s.io/apiserver/pkg/storage/etcd3/stats.go
@@ -18,7 +18,6 @@ package etcd3
 
 import (
 	"context"
-	"errors"
 	"strings"
 	"sync"
 
@@ -34,11 +33,11 @@ import (
 
 const sizerRefreshInterval = time.Minute
 
-func newStatsCache(prefix string, getKeys storage.KeysFunc) *statsCache {
+func newStatsCache(prefix string, getKeys storage.KeysFunc) *statsCollector {
 	if prefix[len(prefix)-1] != '/' {
 		prefix += "/"
 	}
-	sc := &statsCache{
+	sc := &statsCollector{
 		prefix:  prefix,
 		getKeys: getKeys,
 		stop:    make(chan struct{}),
@@ -52,23 +51,21 @@ func newStatsCache(prefix string, getKeys storage.KeysFunc) *statsCache {
 	return sc
 }
 
-// statsCache efficiently estimates the average object size
+// statsCollector efficiently estimates the average object size
 // based on the last observed state of individual keys.
-// By plugging statsCache into GetList and Watch functions,
+// By plugging statsCollector into GetList and Watch functions,
 // a fairly accurate estimate of object sizes can be maintained
 // without additional requests to the underlying storage.
 // To handle potential out-of-order or incomplete data,
 // it uses a per-key revision to identify the newer state.
 // This approach may leak keys if delete events are not observed,
 // thus we run a background goroutine to periodically cleanup keys if needed.
-type statsCache struct {
+type statsCollector struct {
 	prefix         string
 	stop           chan struct{}
 	wg             sync.WaitGroup
 	lastKeyCleanup atomic.Pointer[time.Time]
-
-	getKeysLock sync.Mutex
-	getKeys     storage.KeysFunc
+	getKeys        storage.KeysFunc
 
 	keysLock sync.Mutex
 	keys     map[string]sizeRevision
@@ -79,10 +76,8 @@ type sizeRevision struct {
 	revision  int64
 }
 
-var errStatsDisabled = errors.New("key size stats disabled")
-
-func (sc *statsCache) Stats(ctx context.Context) (storage.Stats, error) {
-	keys, err := sc.GetKeys(ctx)
+func (sc *statsCollector) Stats(ctx context.Context) (storage.Stats, error) {
+	keys, err := sc.getKeys(ctx)
 	if err != nil {
 		return storage.Stats{}, err
 	}
@@ -98,30 +93,12 @@ func (sc *statsCache) Stats(ctx context.Context) (storage.Stats, error) {
 	return stats, nil
 }
 
-func (sc *statsCache) GetKeys(ctx context.Context) ([]string, error) {
-	sc.getKeysLock.Lock()
-	getKeys := sc.getKeys
-	sc.getKeysLock.Unlock()
-
-	if getKeys == nil {
-		return nil, errStatsDisabled
-	}
-	// Don't execute getKeys under lock.
-	return getKeys(ctx)
-}
-
-func (sc *statsCache) SetKeysFunc(keys storage.KeysFunc) {
-	sc.getKeysLock.Lock()
-	defer sc.getKeysLock.Unlock()
-	sc.getKeys = keys
-}
-
-func (sc *statsCache) Close() {
+func (sc *statsCollector) Close() {
 	close(sc.stop)
 	sc.wg.Wait()
 }
 
-func (sc *statsCache) run() {
+func (sc *statsCollector) run() {
 	jitter := 0.5 // Period between [interval, interval * (1.0 + jitter)]
 	sliding := true
 	// wait.JitterUntilWithContext starts work immediately, so wait first.
@@ -132,16 +109,14 @@ func (sc *statsCache) run() {
 	wait.JitterUntilWithContext(wait.ContextForChannel(sc.stop), sc.cleanKeysIfNeeded, sizerRefreshInterval, jitter, sliding)
 }
 
-func (sc *statsCache) cleanKeysIfNeeded(ctx context.Context) {
+func (sc *statsCollector) cleanKeysIfNeeded(ctx context.Context) {
 	lastKeyCleanup := sc.lastKeyCleanup.Load()
 	if lastKeyCleanup != nil && time.Since(*lastKeyCleanup) < sizerRefreshInterval {
 		return
 	}
-	keys, err := sc.GetKeys(ctx)
+	keys, err := sc.getKeys(ctx)
 	if err != nil {
-		if !errors.Is(err, errStatsDisabled) {
-			klog.InfoS("Error getting keys", "err", err)
-		}
+		klog.InfoS("Error getting keys", "err", err)
 		return
 	}
 	sc.keysLock.Lock()
@@ -149,7 +124,7 @@ func (sc *statsCache) cleanKeysIfNeeded(ctx context.Context) {
 	sc.cleanKeys(keys)
 }
 
-func (sc *statsCache) cleanKeys(keepKeys []string) {
+func (sc *statsCollector) cleanKeys(keepKeys []string) {
 	newKeys := make(map[string]sizeRevision, len(keepKeys))
 	for _, key := range keepKeys {
 		// Handle cacher keys not having prefix.
@@ -171,14 +146,14 @@ func (sc *statsCache) cleanKeys(keepKeys []string) {
 	sc.lastKeyCleanup.Store(&now)
 }
 
-func (sc *statsCache) keySizes() (totalSize int64) {
+func (sc *statsCollector) keySizes() (totalSize int64) {
 	for _, sizeRevision := range sc.keys {
 		totalSize += sizeRevision.sizeBytes
 	}
 	return totalSize
 }
 
-func (sc *statsCache) Update(kvs []*mvccpb.KeyValue) {
+func (sc *statsCollector) Update(kvs []*mvccpb.KeyValue) {
 	sc.keysLock.Lock()
 	defer sc.keysLock.Unlock()
 	for _, kv := range kvs {
@@ -186,14 +161,14 @@ func (sc *statsCache) Update(kvs []*mvccpb.KeyValue) {
 	}
 }
 
-func (sc *statsCache) UpdateKey(kv *mvccpb.KeyValue) {
+func (sc *statsCollector) UpdateKey(kv *mvccpb.KeyValue) {
 	sc.keysLock.Lock()
 	defer sc.keysLock.Unlock()
 
 	sc.updateKey(kv)
 }
 
-func (sc *statsCache) updateKey(kv *mvccpb.KeyValue) {
+func (sc *statsCollector) updateKey(kv *mvccpb.KeyValue) {
 	key := string(kv.Key)
 	keySizeRevision := sc.keys[key]
 	if keySizeRevision.revision >= kv.ModRevision {
@@ -206,7 +181,7 @@ func (sc *statsCache) updateKey(kv *mvccpb.KeyValue) {
 	}
 }
 
-func (sc *statsCache) DeleteKey(kv *mvccpb.KeyValue) {
+func (sc *statsCollector) DeleteKey(kv *mvccpb.KeyValue) {
 	sc.keysLock.Lock()
 	defer sc.keysLock.Unlock()
 

staging/src/k8s.io/apiserver/pkg/storage/etcd3/store.go
@@ -25,6 +25,7 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"go.etcd.io/etcd/api/v3/mvccpb"
@@ -90,8 +91,10 @@ type store struct {
 
 	resourcePrefix string
 	newListFunc    func() runtime.Object
-	stats          *statsCache
 	compactor      Compactor
+
+	collectorMux               sync.RWMutex
+	resourceSizeStatsCollector *statsCollector
 }
 
 var _ storage.Interface = (*store)(nil)
@@ -189,13 +192,8 @@ func New(c *kubernetes.Client, compactor Compactor, codec runtime.Codec, newFunc
 		newListFunc:    newListFunc,
 		compactor:      compactor,
 	}
-	// Collecting stats requires properly set resourcePrefix to call getKeys.
-	if utilfeature.DefaultFeatureGate.Enabled(features.SizeBasedListCostEstimate) {
-		stats := newStatsCache(pathPrefix, nil)
-		s.stats = stats
-		w.stats = stats
-	}
 
+	w.getStatsCollector = s.getResourceSizeStatsCollector
 	w.getCurrentStorageRV = func(ctx context.Context) (uint64, error) {
 		return s.GetCurrentResourceVersion(ctx)
 	}
@@ -218,9 +216,16 @@ func (s *store) Versioner() storage.Versioner {
 }
 
 func (s *store) Close() {
-	if s.stats != nil {
-		s.stats.Close()
+	stats := s.getResourceSizeStatsCollector()
+	if stats != nil {
+		stats.Close()
 	}
+}
+
+func (s *store) getResourceSizeStatsCollector() *statsCollector {
+	s.collectorMux.RLock()
+	defer s.collectorMux.RUnlock()
+	return s.resourceSizeStatsCollector
 }
 
 // Get implements storage.Interface.Get.
@@ -633,13 +638,12 @@ func getNewItemFunc(listObj runtime.Object, v reflect.Value) func() runtime.Obje
 	}
 }
 
-func (s *store) Stats(ctx context.Context) (stats storage.Stats, err error) {
-	if s.stats != nil {
-		stats, err := s.stats.Stats(ctx)
-		if !errors.Is(err, errStatsDisabled) {
-			return stats, err
-		}
+func (s *store) Stats(ctx context.Context) (storage.Stats, error) {
+	if collector := s.getResourceSizeStatsCollector(); collector != nil {
+		return collector.Stats(ctx)
 	}
+	// returning stats without resource size
+
 	startTime := time.Now()
 	prefix, err := s.prepareKey(s.resourcePrefix)
 	if err != nil {
@@ -658,9 +662,11 @@ func (s *store) Stats(ctx context.Context) (stats storage.Stats, err error) {
 	}, nil
 }
 
-func (s *store) SetKeysFunc(keys storage.KeysFunc) {
-	if s.stats != nil {
-		s.stats.SetKeysFunc(keys)
+func (s *store) EnableResourceSizeEstimation(getKeys storage.KeysFunc) {
+	s.collectorMux.Lock()
+	defer s.collectorMux.Unlock()
+	if s.resourceSizeStatsCollector == nil && getKeys != nil {
+		s.resourceSizeStatsCollector = newStatsCache(s.pathPrefix, getKeys)
 	}
 }
 
@@ -780,6 +786,8 @@ func (s *store) GetList(ctx context.Context, key string, opts storage.ListOption
 		metricsOp = "list"
 	}
 
+	stats := s.getResourceSizeStatsCollector()
+
 	aggregator := s.listErrAggrFactory()
 	for {
 		startTime := time.Now()
@@ -821,8 +829,8 @@ func (s *store) GetList(ctx context.Context, key string, opts storage.ListOption
 		} else {
 			growSlice(v, 2048, len(getResp.Kvs))
 		}
-		if s.stats != nil {
-			s.stats.Update(getResp.Kvs)
+		if stats != nil {
+			stats.Update(getResp.Kvs)
 		}
 
 		// take items from the response until the bucket is full, filtering as we go