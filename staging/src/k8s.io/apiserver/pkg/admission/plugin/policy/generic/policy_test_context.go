5a5bcdc44a9161c56a2b9e14130c3fde646020cb
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2025-09-08 22:07:18 +0200

    fix: TestPolicySourceHasSyncedEmpty

--- staging/src/k8s.io/apiserver/pkg/admission/plugin/policy/generic/policy_test_context.go
+++ staging/src/k8s.io/apiserver/pkg/admission/plugin/policy/generic/policy_test_context.go
@@ -163,7 +163,8 @@ func NewPolicyTestContext[P, B runtime.Object, E Evaluator](
 				return policiesAndBindingsTracker.List(fakePolicyGVR, fakePolicyGVK, "")
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-				return policiesAndBindingsTracker.Watch(fakePolicyGVR, fakePolicyGVK, "")
+				options.Watch = true
+				return policiesAndBindingsTracker.Watch(fakePolicyGVR, fakePolicyGVK, "", options)
 			},
 		},
 		Pexample,
@@ -176,7 +177,8 @@ func NewPolicyTestContext[P, B runtime.Object, E Evaluator](
 				return policiesAndBindingsTracker.List(fakeBindingGVR, fakeBindingGVK, "")
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-				return policiesAndBindingsTracker.Watch(fakeBindingGVR, fakeBindingGVK, "")
+				options.Watch = true
+				return policiesAndBindingsTracker.Watch(fakeBindingGVR, fakeBindingGVK, "", options)
 			},
 		},
 		Bexample,
