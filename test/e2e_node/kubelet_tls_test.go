6c9fa3296b885cf5aba90052bf5b611ef499f373
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2025-09-16 22:00:24 +0200

    Address PR comments/lint failures

--- test/e2e_node/kubelet_tls_test.go
+++ test/e2e_node/kubelet_tls_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package e2enode
 
 import (
@@ -45,12 +61,12 @@ func getCert(addr string) (*x509.Certificate, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer conn.Close()
+	defer func() { _ = conn.Close() }()
 
 	return conn.ConnectionState().PeerCertificates[0], nil
 }
 
-var _ = SIGDescribe("KubletTLS", framework.WithSerial(), func() {
+var _ = SIGDescribe("KubletTLS", framework.WithSerial(), framework.WithNodeConformance(), func() {
 	f := framework.NewDefaultFramework("kubelet-tls-test")
 
 	ginkgo.Context("certificate reload", func() {
@@ -94,7 +110,7 @@ var _ = SIGDescribe("KubletTLS", framework.WithSerial(), func() {
 				}
 
 				return newCert.Equal(oldCert), nil
-			}).Should(gomega.BeFalse())
+			}).Should(gomega.BeFalseBecause("new certificate should be loaded"))
 		})
 	})
 })
