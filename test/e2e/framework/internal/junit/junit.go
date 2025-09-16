05ab707117922fc4a79ac07aadd88b101c4042a7
Patrick Ohly <patrick.ohly@intel.com>
2025-09-16 14:34:04 +0200

    E2E: treat data races in e2e suite as failures
    Ginkgo itself doesn't do this, in which case prun-junit-xml drops the output
    and Spyglass wouldn't show the test as failed. If the data race warning is
    captured, we now treat that as the failure of a test if it hasn't already
    failed for other reasons.
    
    While at it, the entire report cleanup gets moved to our junit package.

--- test/e2e/framework/internal/junit/junit.go
+++ test/e2e/framework/internal/junit/junit.go
@@ -17,6 +17,11 @@ limitations under the License.
 package junit
 
 import (
+	"regexp"
+	"slices"
+	"strings"
+	"time"
+
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/ginkgo/v2/reporters"
 	"github.com/onsi/ginkgo/v2/types"
@@ -42,5 +47,60 @@ func WriteJUnitReport(report ginkgo.Report, filename string) error {
 		OmitSpecLabels: true,
 	}
 
+	// Sort specs by full name. The default is by start (or completion?) time,
+	// which is less useful in spyglass because those times are not shown
+	// and thus tests seem to be listed with no apparent order.
+	slices.SortFunc(report.SpecReports, func(a, b types.SpecReport) int {
+		res := strings.Compare(a.FullText(), b.FullText())
+		if res == 0 {
+			// Use start time as tie-breaker in the unlikely
+			// case that two specs have the same full name.
+			return a.StartTime.Compare(b.StartTime)
+		}
+		return res
+	})
+
+	// Detect data races in output and mark those tests as failed.
+	// Ginkgo itself captures the warning in the output of those
+	// tests where it is printed, but does not mark the tests as failed.
+	// This is not exactly wrong (the reason might be in code that was
+	// started elsewhere), but without a test being marked as "failed",
+	// prune-junit-xml will prune the output and spyglass won't show
+	// the test as failed.
+	//
+	// Both variants of captured output get checked, just to be on the safe side.
+	for i, spec := range report.SpecReports {
+		if spec.State == types.SpecStatePassed {
+			ginkgoWriterRace := containsDataRace(spec.CapturedGinkgoWriterOutput)
+			stdoutRace := containsDataRace(spec.CapturedStdOutErr)
+			if ginkgoWriterRace || stdoutRace {
+				spec.State = types.SpecStateFailed
+				spec.Failure = types.Failure{
+					FailureNodeContext: types.FailureNodeIsLeafNode,
+					FailureNodeType:    spec.LeafNodeType,
+					Location:           types.NewCustomCodeLocation("output analysis"),
+					TimelineLocation: types.TimelineLocation{
+						Time: time.Now(),
+					},
+				}
+				// Let's move that text to the failure message.
+				if stdoutRace {
+					spec.Failure.Message = spec.CapturedStdOutErr
+					spec.CapturedStdOutErr = ""
+				} else {
+					spec.Failure.Message = spec.CapturedGinkgoWriterOutput
+					spec.CapturedGinkgoWriterOutput = ""
+				}
+				report.SpecReports[i] = spec
+			}
+		}
+	}
+
 	return reporters.GenerateJUnitReportWithConfig(report, filename, config)
+}
+
+var dataRaceRE = regexp.MustCompile(`(?m)^WARNING: DATA RACE$`)
+
+func containsDataRace(output string) bool {
+	return dataRaceRE.MatchString(output)
 }
