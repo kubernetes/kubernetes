kubernetes/component-base

chore: reorganize `component-base/metrics`
This commit reorganizes component-base's metrics package to bring about
the following changes:
* migrate all related functions and methods to options.go (from
  registry.go)
* inline v1.MetricsConfiguration in Options and expose it in api/v1,
  also re-generate deepcopy assets for the same
* add missing validation functions for MetricsConfiguration fields
* add missing setter functions for MetricsConfiguration fields
* addressed a logical bug in SetDisabledMetric to not increment the
  disabled metrics counter when a duplicate metric is disabled again
* refactored some tests to also take into consideration meta metrics,
  wherever applicable
* changed validateAllowListMappingManifest's godoc to mention that
  components are expected to pass in the *absolute* path to the manifest
  file

Signed-off-by: Pranshu Srivastava <rexagod@gmail.com>

Kubernetes-commit: e74b4063d825a88136d40c539a50a0367aeb7d1e

metrics/options.go
@@ -17,32 +17,59 @@ limitations under the License.
 package metrics
 
 import (
-	"fmt"
-	"regexp"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"sync/atomic"
 
-	"github.com/blang/semver/v4"
 	"github.com/spf13/pflag"
-
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/component-base/version"
+	"k8s.io/klog/v2"
+
+	"go.yaml.in/yaml/v2"
+	"k8s.io/component-base/metrics/api/v1"
+)
+
+var (
+	disabledMetricsLock sync.RWMutex
+	disabledMetrics     = map[string]struct{}{}
+	showHiddenOnce      sync.Once
+	showHidden          atomic.Bool
+)
+
+var (
+	disabledMetricsTotal = NewCounter(
+		&CounterOpts{
+			Name:           "disabled_metrics_total",
+			Help:           "The count of disabled metrics.",
+			StabilityLevel: BETA,
+		},
+	)
+
+	hiddenMetricsTotal = NewCounter(
+		&CounterOpts{
+			Name:           "hidden_metrics_total",
+			Help:           "The count of hidden metrics.",
+			StabilityLevel: BETA,
+		},
+	)
+
+	cardinalityEnforcementUnexpectedCategorizationsTotal = NewCounter(
+		&CounterOpts{
+			Name:           "cardinality_enforcement_unexpected_categorizations_total",
+			Help:           "The count of unexpected categorizations during cardinality enforcement.",
+			StabilityLevel: ALPHA,
+		},
+	)
 )
 
 // Options has all parameters needed for exposing metrics from components
-// +k8s:deepcopy-gen=true
 type Options struct {
-	// ShowHiddenMetricsForVersion is the previous version for which you want to show hidden metrics.
-	// Only the previous minor version is meaningful, other values will not be allowed.
-	// The format is <major>.<minor>, e.g.: '1.16'.
-	// The purpose of this format is make sure you have the opportunity to notice if the next release hides additional metrics,
-	// rather than being surprised when they are permanently removed in the release after that.
-	ShowHiddenMetricsForVersion string `json:"showHiddenMetricsForVersion,omitempty"`
-	// DisabledMetrics is a list of fully qualified metric names that should be disabled.
-	// Disabling metrics is higher in precedence than showing hidden metrics.
-	DisabledMetrics []string `json:"disabledMetrics,omitempty"`
-	// AllowListMapping is the map from metric-label to value allow-list of this label.
-	// The key's format is <MetricName>,<LabelName>, while its value is a list of allowed values for that label of that metric. For e.g., "metric1,label1": "v1,v2,v3".
-	AllowListMapping map[string]string `json:"allowListMapping,omitempty"`
-	// The path to the manifest file that contains the allow-list mapping. Provided for convenience over AllowListMapping.
-	AllowListMappingManifest string `json:"allowListMappingManifest,omitempty"`
+	// Configuration serialization is omitted here since the parent is never expected to be embedded.
+	v1.MetricsConfiguration `json:"-"`
 }
 
 // NewOptions returns default metrics options
@@ -50,29 +77,8 @@ func NewOptions() *Options {
 	return &Options{}
 }
 
-// Validate validates metrics flags options.
-func (o *Options) Validate() []error {
-	if o == nil {
-		return nil
-	}
-
-	var errs []error
-	err := validateShowHiddenMetricsVersion(parseVersion(version.Get()), o.ShowHiddenMetricsForVersion)
-	if err != nil {
-		errs = append(errs, err)
-	}
-
-	if err := validateAllowMetricLabel(o.AllowListMapping); err != nil {
-		errs = append(errs, err)
-	}
-
-	if len(errs) == 0 {
-		return nil
-	}
-	return errs
-}
-
 // AddFlags adds flags for exposing component metrics.
+// This won't be called in embedded instances within component configurations.
 func (o *Options) AddFlags(fs *pflag.FlagSet) {
 	if o == nil {
 		return
@@ -95,8 +101,140 @@ func (o *Options) AddFlags(fs *pflag.FlagSet) {
 			"e.g. metric1,label1='v1,v2,v3', metric1,label2='v1,v2,v3' metric2,label1='v1,v2,v3'.")
 	fs.StringVar(&o.AllowListMappingManifest, "allow-metric-labels-manifest", o.AllowListMappingManifest,
 		"The path to the manifest file that contains the allow-list mapping. "+
-			"The format of the file is the same as the flag --allow-metric-labels. "+
+			"The format of the file is the same as the flag --allow-metric-labels, i.e., \n"+
+			"allowListMapping:\n  \"metric1,label1\": \"value11,value12\"\n  \"metric2,label2\": \"\"\n"+
 			"Note that the flag --allow-metric-labels will override the manifest file.")
+}
+
+// SetShowHidden will enable showing hidden metrics. This will no-opt
+// after the initial call
+func SetShowHidden() {
+	showHiddenOnce.Do(func() {
+		showHidden.Store(true)
+
+		// re-register collectors that has been hidden in phase of last registry.
+		for _, r := range registries {
+			r.enableHiddenCollectors()
+			r.enableHiddenStableCollectors()
+		}
+	})
+}
+
+// ShouldShowHidden returns whether showing hidden deprecated metrics is enabled.
+// While the primary use case for this is internal (to determine registration behavior) this can also be used to introspect.
+func ShouldShowHidden() bool {
+	return showHidden.Load()
+}
+
+// SetDisabledMetric will disable a metric by name.
+// This will also increment the disabled metrics counter.
+// Note that this is a no-op if the metric is already disabled.
+func SetDisabledMetrics(names []string) {
+	for _, name := range names {
+		func(name string) {
+			// An empty metric name is not a valid Prometheus metric.
+			if name == "" {
+				klog.Warningf("Attempted to disable an empty metric name, ignoring.")
+				return
+			}
+			disabledMetricsLock.Lock()
+			defer disabledMetricsLock.Unlock()
+			if _, ok := disabledMetrics[name]; !ok {
+				disabledMetrics[name] = struct{}{}
+				disabledMetricsTotal.Inc()
+			}
+		}(name)
+	}
+}
+
+type MetricLabelAllowList struct {
+	labelToAllowList map[string]sets.Set[string]
+}
+
+func (allowList *MetricLabelAllowList) ConstrainToAllowedList(labelNameList, labelValueList []string) {
+	for index, value := range labelValueList {
+		name := labelNameList[index]
+		if allowValues, ok := allowList.labelToAllowList[name]; ok {
+			if !allowValues.Has(value) {
+				labelValueList[index] = "unexpected"
+				cardinalityEnforcementUnexpectedCategorizationsTotal.Inc()
+			}
+		}
+	}
+}
+
+func (allowList *MetricLabelAllowList) ConstrainLabelMap(labels map[string]string) {
+	for name, value := range labels {
+		if allowValues, ok := allowList.labelToAllowList[name]; ok {
+			if !allowValues.Has(value) {
+				labels[name] = "unexpected"
+				cardinalityEnforcementUnexpectedCategorizationsTotal.Inc()
+			}
+		}
+	}
+}
+
+func SetLabelAllowList(allowListMapping map[string]string) {
+	if len(allowListMapping) == 0 {
+		klog.Errorf("empty allow-list mapping supplied, ignoring.")
+		return
+	}
+
+	allowListLock.Lock()
+	defer allowListLock.Unlock()
+	for metricLabelName, labelValues := range allowListMapping {
+		metricName := strings.Split(metricLabelName, ",")[0]
+		labelName := strings.Split(metricLabelName, ",")[1]
+		valueSet := sets.New[string](strings.Split(labelValues, ",")...)
+
+		allowList, ok := labelValueAllowLists[metricName]
+		if ok {
+			allowList.labelToAllowList[labelName] = valueSet
+		} else {
+			labelToAllowList := make(map[string]sets.Set[string])
+			labelToAllowList[labelName] = valueSet
+			labelValueAllowLists[metricName] = &MetricLabelAllowList{
+				labelToAllowList,
+			}
+		}
+	}
+}
+
+func SetLabelAllowListFromManifest(manifest string) {
+	if manifest == "" {
+		klog.Errorf("The manifest file is empty, ignoring.")
+		return
+	}
+
+	data, err := os.ReadFile(filepath.Clean(manifest))
+	if err != nil {
+		klog.Errorf("Failed to read allow list manifest: %v", err)
+		return
+	}
+	allowListMapping := make(map[string]string)
+	err = yaml.Unmarshal(data, &allowListMapping)
+	if err != nil {
+		klog.Errorf("Failed to parse allow list manifest: %v", err)
+		return
+	}
+	SetLabelAllowList(allowListMapping)
+}
+
+// Apply applies a MetricsConfiguration into global configuration of metrics.
+func Apply(c *v1.MetricsConfiguration) {
+	if c == nil {
+		return
+	}
+
+	if len(c.ShowHiddenMetricsForVersion) > 0 {
+		SetShowHidden()
+	}
+	SetDisabledMetrics(c.DisabledMetrics)
+	if c.AllowListMapping != nil {
+		SetLabelAllowList(c.AllowListMapping)
+	} else {
+		SetLabelAllowListFromManifest(c.AllowListMappingManifest)
+	}
 }
 
 // Apply applies parameters into global configuration of metrics.
@@ -104,44 +242,34 @@ func (o *Options) Apply() {
 	if o == nil {
 		return
 	}
-	if len(o.ShowHiddenMetricsForVersion) > 0 {
-		SetShowHidden()
-	}
-	// set disabled metrics
-	for _, metricName := range o.DisabledMetrics {
-		SetDisabledMetric(metricName)
-	}
-	if o.AllowListMapping != nil {
-		SetLabelAllowListFromCLI(o.AllowListMapping)
-	} else if len(o.AllowListMappingManifest) > 0 {
-		SetLabelAllowListFromManifest(o.AllowListMappingManifest)
-	}
+
+	Apply(&o.MetricsConfiguration)
 }
 
-func validateShowHiddenMetricsVersion(currentVersion semver.Version, targetVersionStr string) error {
-	if targetVersionStr == "" {
-		return nil
-	}
-
-	validVersionStr := fmt.Sprintf("%d.%d", currentVersion.Major, currentVersion.Minor-1)
-	if targetVersionStr != validVersionStr {
-		return fmt.Errorf("--show-hidden-metrics-for-version must be omitted or have the value '%v'. Only the previous minor version is allowed", validVersionStr)
+// ValidateShowHiddenMetricsVersion checks invalid version for which show hidden metrics.
+// TODO: This is kept here for backward compatibility in Kubelet (as metrics configuration fields were exposed on an individual basis earlier).
+// TODO: Revisit this after Kubelet supports the new metrics configuration API.
+func ValidateShowHiddenMetricsVersion(v string) []error {
+	err := v1.ValidateShowHiddenMetricsVersionForKubeletBackwardCompatOnly(parseVersion(version.Get()), v)
+	if err != nil {
+		return []error{err}
 	}
 
 	return nil
 }
 
-func validateAllowMetricLabel(allowListMapping map[string]string) error {
-	if allowListMapping == nil {
+// Validate validates metrics flags options.
+func (o *Options) Validate() []error {
+	if o == nil {
 		return nil
 	}
-	metricNameRegex := `[a-zA-Z_:][a-zA-Z0-9_:]*`
-	labelRegex := `[a-zA-Z_][a-zA-Z0-9_]*`
-	for k := range allowListMapping {
-		reg := regexp.MustCompile(metricNameRegex + `,` + labelRegex)
-		if reg.FindString(k) != k {
-			return fmt.Errorf("--allow-metric-labels must have a list of kv pair with format `metricName,labelName=labelValue, labelValue,...`")
-		}
+	currentVersion := parseVersion(version.Get())
+	fldPath := field.NewPath("metrics")
+	fldErrs := v1.Validate(&o.MetricsConfiguration, currentVersion, fldPath)
+
+	if len(fldErrs) == 0 {
+		return nil
 	}
-	return nil
+
+	return fldErrs.ToAggregate().Errors()
 }

kubernetes/kubernetes

staging/src/k8s.io/component-base/metrics/options.go
@@ -18,58 +18,69 @@ package metrics
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
+	"sync"
+	"sync/atomic"
 
 	"github.com/blang/semver/v4"
 	"github.com/spf13/pflag"
-
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/component-base/version"
+	"k8s.io/klog/v2"
+
+	"go.yaml.in/yaml/v2"
+	"k8s.io/component-base/metrics/api/v1"
+)
+
+var (
+	disabledMetricsLock sync.RWMutex
+	disabledMetrics     = map[string]struct{}{}
+	showHiddenOnce      sync.Once
+	showHidden          atomic.Bool
+
+	metricNameExpr           = `[a-zA-Z_:][a-zA-Z0-9_:]*`
+	labelExpr                = `[a-zA-Z_][a-zA-Z0-9_]*`
+	metricNameRegex          = regexp.MustCompile(`^` + metricNameExpr + `$`)
+	allowListMappingKeyRegex = regexp.MustCompile(metricNameExpr + `,` + labelExpr)
+)
+
+var (
+	disabledMetricsTotal = NewCounter(
+		&CounterOpts{
+			Name:           "disabled_metrics_total",
+			Help:           "The count of disabled metrics.",
+			StabilityLevel: BETA,
+		},
+	)
+
+	hiddenMetricsTotal = NewCounter(
+		&CounterOpts{
+			Name:           "hidden_metrics_total",
+			Help:           "The count of hidden metrics.",
+			StabilityLevel: BETA,
+		},
+	)
+
+	cardinalityEnforcementUnexpectedCategorizationsTotal = NewCounter(
+		&CounterOpts{
+			Name:           "cardinality_enforcement_unexpected_categorizations_total",
+			Help:           "The count of unexpected categorizations during cardinality enforcement.",
+			StabilityLevel: ALPHA,
+		},
+	)
 )
 
 // Options has all parameters needed for exposing metrics from components
-// +k8s:deepcopy-gen=true
 type Options struct {
-	// ShowHiddenMetricsForVersion is the previous version for which you want to show hidden metrics.
-	// Only the previous minor version is meaningful, other values will not be allowed.
-	// The format is <major>.<minor>, e.g.: '1.16'.
-	// The purpose of this format is make sure you have the opportunity to notice if the next release hides additional metrics,
-	// rather than being surprised when they are permanently removed in the release after that.
-	ShowHiddenMetricsForVersion string `json:"showHiddenMetricsForVersion,omitempty"`
-	// DisabledMetrics is a list of fully qualified metric names that should be disabled.
-	// Disabling metrics is higher in precedence than showing hidden metrics.
-	DisabledMetrics []string `json:"disabledMetrics,omitempty"`
-	// AllowListMapping is the map from metric-label to value allow-list of this label.
-	// The key's format is <MetricName>,<LabelName>, while its value is a list of allowed values for that label of that metric. For e.g., "metric1,label1": "v1,v2,v3".
-	AllowListMapping map[string]string `json:"allowListMapping,omitempty"`
-	// The path to the manifest file that contains the allow-list mapping. Provided for convenience over AllowListMapping.
-	AllowListMappingManifest string `json:"allowListMappingManifest,omitempty"`
+	v1.MetricsConfiguration `json:",inline"`
 }
 
 // NewOptions returns default metrics options
 func NewOptions() *Options {
 	return &Options{}
-}
-
-// Validate validates metrics flags options.
-func (o *Options) Validate() []error {
-	if o == nil {
-		return nil
-	}
-
-	var errs []error
-	err := validateShowHiddenMetricsVersion(parseVersion(version.Get()), o.ShowHiddenMetricsForVersion)
-	if err != nil {
-		errs = append(errs, err)
-	}
-
-	if err := validateAllowMetricLabel(o.AllowListMapping); err != nil {
-		errs = append(errs, err)
-	}
-
-	if len(errs) == 0 {
-		return nil
-	}
-	return errs
 }
 
 // AddFlags adds flags for exposing component metrics.
@@ -99,6 +110,106 @@ func (o *Options) AddFlags(fs *pflag.FlagSet) {
 			"Note that the flag --allow-metric-labels will override the manifest file.")
 }
 
+// SetShowHidden will enable showing hidden metrics. This will no-opt
+// after the initial call
+func SetShowHidden() {
+	showHiddenOnce.Do(func() {
+		showHidden.Store(true)
+
+		// re-register collectors that has been hidden in phase of last registry.
+		for _, r := range registries {
+			r.enableHiddenCollectors()
+			r.enableHiddenStableCollectors()
+		}
+	})
+}
+
+// ShouldShowHidden returns whether showing hidden deprecated metrics is enabled.
+// While the primary use case for this is internal (to determine registration behavior) this can also be used to introspect.
+func ShouldShowHidden() bool {
+	return showHidden.Load()
+}
+
+// SetDisabledMetric will disable a metric by name.
+// This will also increment the disabled metrics counter.
+// Note that this is a no-op if the metric is already disabled.
+func SetDisabledMetric(name string) {
+	// An empty metric name is not a valid Prometheus metric.
+	if name == "" {
+		klog.Warningf("Attempted to disable an empty metric name, ignoring.")
+		return
+	}
+	disabledMetricsLock.Lock()
+	defer disabledMetricsLock.Unlock()
+	if _, ok := disabledMetrics[name]; !ok {
+		disabledMetrics[name] = struct{}{}
+		disabledMetricsTotal.Inc()
+	}
+}
+
+type MetricLabelAllowList struct {
+	labelToAllowList map[string]sets.Set[string]
+}
+
+func (allowList *MetricLabelAllowList) ConstrainToAllowedList(labelNameList, labelValueList []string) {
+	for index, value := range labelValueList {
+		name := labelNameList[index]
+		if allowValues, ok := allowList.labelToAllowList[name]; ok {
+			if !allowValues.Has(value) {
+				labelValueList[index] = "unexpected"
+				cardinalityEnforcementUnexpectedCategorizationsTotal.Inc()
+			}
+		}
+	}
+}
+
+func (allowList *MetricLabelAllowList) ConstrainLabelMap(labels map[string]string) {
+	for name, value := range labels {
+		if allowValues, ok := allowList.labelToAllowList[name]; ok {
+			if !allowValues.Has(value) {
+				labels[name] = "unexpected"
+				cardinalityEnforcementUnexpectedCategorizationsTotal.Inc()
+			}
+		}
+	}
+}
+
+func SetLabelAllowList(allowListMapping map[string]string) {
+	allowListLock.Lock()
+	defer allowListLock.Unlock()
+	for metricLabelName, labelValues := range allowListMapping {
+		metricName := strings.Split(metricLabelName, ",")[0]
+		labelName := strings.Split(metricLabelName, ",")[1]
+		valueSet := sets.New[string](strings.Split(labelValues, ",")...)
+
+		allowList, ok := labelValueAllowLists[metricName]
+		if ok {
+			allowList.labelToAllowList[labelName] = valueSet
+		} else {
+			labelToAllowList := make(map[string]sets.Set[string])
+			labelToAllowList[labelName] = valueSet
+			labelValueAllowLists[metricName] = &MetricLabelAllowList{
+				labelToAllowList,
+			}
+		}
+	}
+}
+
+func SetLabelAllowListFromManifest(manifest string) {
+	data, err := os.ReadFile(filepath.Clean(manifest))
+	if err != nil {
+		klog.Errorf("Failed to read allow list manifest: %v", err)
+		return
+	}
+	allowListMapping := make(map[string]string)
+	err = yaml.Unmarshal(data, &allowListMapping)
+	if err != nil {
+		klog.Errorf("Failed to parse allow list manifest: %v", err)
+		return
+	}
+	SetLabelAllowList(allowListMapping)
+}
+
 // Apply applies parameters into global configuration of metrics.
 func (o *Options) Apply() {
 	if o == nil {
@@ -107,12 +218,11 @@ func (o *Options) Apply() {
 	if len(o.ShowHiddenMetricsForVersion) > 0 {
 		SetShowHidden()
 	}
-	// set disabled metrics
 	for _, metricName := range o.DisabledMetrics {
 		SetDisabledMetric(metricName)
 	}
 	if o.AllowListMapping != nil {
-		SetLabelAllowListFromCLI(o.AllowListMapping)
+		SetLabelAllowList(o.AllowListMapping)
 	} else if len(o.AllowListMappingManifest) > 0 {
 		SetLabelAllowListFromManifest(o.AllowListMappingManifest)
 	}
@@ -131,17 +241,85 @@ func validateShowHiddenMetricsVersion(currentVersion semver.Version, targetVersi
 	return nil
 }
 
-func validateAllowMetricLabel(allowListMapping map[string]string) error {
-	if allowListMapping == nil {
-		return nil
+// ValidateShowHiddenMetricsVersion checks invalid version for which show hidden metrics.
+// TODO: This is kept here for backward compatibility in Kubelet (as metrics configuration fields were exposed on an individual basis earlier).
+// TODO: Revisit this after Kubelet supports the new metrics configuration API.
+func ValidateShowHiddenMetricsVersion(v string) []error {
+	err := validateShowHiddenMetricsVersion(parseVersion(version.Get()), v)
+	if err != nil {
+		return []error{err}
 	}
-	metricNameRegex := `[a-zA-Z_:][a-zA-Z0-9_:]*`
-	labelRegex := `[a-zA-Z_][a-zA-Z0-9_]*`
-	for k := range allowListMapping {
-		reg := regexp.MustCompile(metricNameRegex + `,` + labelRegex)
-		if reg.FindString(k) != k {
-			return fmt.Errorf("--allow-metric-labels must have a list of kv pair with format `metricName,labelName=labelValue, labelValue,...`")
+
+	return nil
+}
+
+func validateDisabledMetrics(names []string) error {
+	for _, name := range names {
+		if !metricNameRegex.MatchString(name) {
+			return fmt.Errorf("--disabled-metrics must be fully qualified metric names matching %q, got %q", metricNameRegex.String(), name)
 		}
 	}
 	return nil
+}
+
+func validateAllowListMapping(allowListMapping map[string]string) error {
+	for k := range allowListMapping {
+		if allowListMappingKeyRegex.FindString(k) != k {
+			return fmt.Errorf("--allow-metric-labels must have a list of kv pair with format `metricName,labelName=labelValue, labelValue,...`")
+		}
+	}
+
+	return nil
+}
+
+// validateAllowListMappingManifest validates the allow list mapping manifest file.
+// This function is used to validate the manifest file provided via the flag --allow-metric-labels-manifest, or the configuration file.
+// In the former case, the path resolution is relative to the current working directory.
+// In the latter case, the path resolution is relative to the configuration file's location, and components are required to pass in the resolved absolute path.
+// NOTE: If its the latter case, components are expected to pass in the *absolute* path to the manifest file.
+func validateAllowListMappingManifest(allowListMappingManifestPath string) error {
+	if allowListMappingManifestPath == "" {
+		return nil
+	}
+	data, err := os.ReadFile(filepath.Clean(allowListMappingManifestPath))
+	if err != nil {
+		return fmt.Errorf("failed to read allow list manifest: %v", err)
+	}
+	allowListMapping := make(map[string]string)
+	err = yaml.Unmarshal(data, &allowListMapping)
+	if err != nil {
+		return fmt.Errorf("failed to parse allow list manifest: %v", err)
+	}
+	if err = validateAllowListMapping(allowListMapping); err != nil {
+		return fmt.Errorf("invalid allow list mapping in manifest: %v", err)
+	}
+
+	return nil
+}
+
+// Validate validates metrics flags options.
+func (o *Options) Validate() []error {
+	if o == nil {
+		return nil
+	}
+
+	var errs []error
+	err := validateShowHiddenMetricsVersion(parseVersion(version.Get()), o.ShowHiddenMetricsForVersion)
+	if err != nil {
+		errs = append(errs, err)
+	}
+	if err = validateDisabledMetrics(o.DisabledMetrics); err != nil {
+		errs = append(errs, err)
+	}
+	if err = validateAllowListMapping(o.AllowListMapping); err != nil {
+		errs = append(errs, err)
+	}
+	if err = validateAllowListMappingManifest(o.AllowListMappingManifest); err != nil {
+		errs = append(errs, err)
+	}
+
+	if len(errs) == 0 {
+		return nil
+	}
+	return errs
 }