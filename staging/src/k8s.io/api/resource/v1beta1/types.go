ff8f39fa1d05685aa6d3c3b3fd33f29465476674
Lalit Chauhan <lalitchauhan@google.com>
2025-10-01 17:42:16 +0000

    Add format validator for DeviceClass.ExtendedResourceName

--- staging/src/k8s.io/api/resource/v1beta1/types.go
+++ staging/src/k8s.io/api/resource/v1beta1/types.go
@@ -1723,6 +1723,7 @@ type DeviceClassSpec struct {
 	// This is an alpha field.
 	// +optional
 	// +featureGate=DRAExtendedResource
+	// +k8s:format=k8s-extended-resource-name
 	ExtendedResourceName *string `json:"extendedResourceName,omitempty" protobuf:"bytes,4,opt,name=extendedResourceName"`
 }
 
