kubernetes/kubectl

Deprecate obsolete slice utility functions
... and update users to use standard library functions.

Signed-off-by: Stephen Kitt <skitt@redhat.com>

Kubernetes-commit: d42d1e3d1f73ac2975b89f9482c2ecfaeeed13f6

pkg/util/slice/slice.go
@@ -18,36 +18,35 @@ package slice
 
 import "slices"
 
-// SortInts64 sorts []int64 in increasing order
+// SortInts64 sorts []int64 in increasing order.
+//
+// Deprecated: Use slices.Sort instead.
+//
+//go:fix inline
 func SortInts64(a []int64) { slices.Sort(a) }
 
 // Contains checks if a given slice of type T contains the provided item.
 // If a modifier func is provided, it is called with the slice item before the comparation.
+//
+// Deprecated: Use slices.Contains or slices.ContainsFunc instead.
 func Contains[T comparable](slice []T, s T, modifier func(s T) T) bool {
-	for _, item := range slice {
-		if item == s {
-			return true
-		}
-		if modifier != nil && modifier(item) == s {
-			return true
-		}
+	if slices.Contains(slice, s) {
+		return true
+	}
+	if modifier != nil {
+		return slices.ContainsFunc(slice, func(item T) bool { return modifier(item) == s })
 	}
 	return false
 }
 
 // ContainsString checks if a given slice of strings contains the provided string.
 // If a modifier func is provided, it is called with the slice item before the comparation.
-// Deprecated: Use Contains[T] instead
+//
+// Deprecated: Use slices.Contains or slices.ContainsFunc instead.
+//
+//go:fix inline
 func ContainsString(slice []string, s string, modifier func(s string) string) bool {
-	for _, item := range slice {
-		if item == s {
-			return true
-		}
-		if modifier != nil && modifier(item) == s {
-			return true
-		}
-	}
-	return false
+	return Contains(slice, s, modifier)
 }
 
 // ToSet returns a single slice containing the unique values from one or more slices. The order of the items in the

kubernetes/kubernetes

pkg/util/slice/slice.go
@@ -18,37 +18,34 @@ limitations under the License.
 package slice
 
 import (
-	"sort"
+	"slices"
 )
 
 // CopyStrings copies the contents of the specified string slice
 // into a new slice.
-func CopyStrings(s []string) []string {
-	if s == nil {
-		return nil
-	}
-	c := make([]string, len(s))
-	copy(c, s)
-	return c
-}
+//
+// Deprecated: Use slices.Clone instead.
+var CopyStrings = slices.Clone[[]string]
 
 // SortStrings sorts the specified string slice in place. It returns the same
 // slice that was provided in order to facilitate method chaining.
+//
+// Deprecated: Use slices.Sort instead.
 func SortStrings(s []string) []string {
-	sort.Strings(s)
+	slices.Sort(s)
 	return s
 }
 
 // ContainsString checks if a given slice of strings contains the provided string.
 // If a modifier func is provided, it is called with the slice item before the comparation.
+//
+// Deprecated: Use slices.ContainsFunc or slices.Contains instead.
 func ContainsString(slice []string, s string, modifier func(s string) string) bool {
-	for _, item := range slice {
-		if item == s {
-			return true
-		}
-		if modifier != nil && modifier(item) == s {
-			return true
-		}
+	if slices.Contains(slice, s) {
+		return true
+	}
+	if modifier != nil {
+		return slices.ContainsFunc(slice, func(item string) bool { return modifier(item) == s })
 	}
 	return false
 }