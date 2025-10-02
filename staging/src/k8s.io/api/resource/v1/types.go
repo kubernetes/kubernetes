d550a08fc6d7bde1024f0a1e6a4a627992a56715
Lalit Chauhan <lalitchauhan@google.com>
2025-10-02 19:00:38 +0000

    Fix rebase issues

--- staging/src/k8s.io/api/resource/v1/types.go
+++ staging/src/k8s.io/api/resource/v1/types.go
@@ -1714,8 +1714,8 @@ type DeviceClassSpec struct {
 	//
 	// This is an alpha field.
 	// +optional
-	// +k8s:optional
 	// +featureGate=DRAExtendedResource
+	// +k8s:optional
 	// +k8s:format=k8s-extended-resource-name
 	ExtendedResourceName *string `json:"extendedResourceName,omitempty" protobuf:"bytes,4,opt,name=extendedResourceName"`
 }
