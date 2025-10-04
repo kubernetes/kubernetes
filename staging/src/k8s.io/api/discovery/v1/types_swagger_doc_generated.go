e60cc9da48839c6c7554d933675962418a2da6d3
Dan Winship <danwinship@redhat.com>
2025-10-04 08:31:57 -0400

    Belatedly remove references to PreferSameTrafficDistribution being Alpha

--- staging/src/k8s.io/api/discovery/v1/types_swagger_doc_generated.go
+++ staging/src/k8s.io/api/discovery/v1/types_swagger_doc_generated.go
@@ -57,7 +57,7 @@ func (EndpointConditions) SwaggerDoc() map[string]string {
 var map_EndpointHints = map[string]string{
 	"":         "EndpointHints provides hints describing how an endpoint should be consumed.",
 	"forZones": "forZones indicates the zone(s) this endpoint should be consumed by when using topology aware routing. May contain a maximum of 8 entries.",
-	"forNodes": "forNodes indicates the node(s) this endpoint should be consumed by when using topology aware routing. May contain a maximum of 8 entries. This is an Alpha feature and is only used when the PreferSameTrafficDistribution feature gate is enabled.",
+	"forNodes": "forNodes indicates the node(s) this endpoint should be consumed by when using topology aware routing. May contain a maximum of 8 entries.",
 }
 
 func (EndpointHints) SwaggerDoc() map[string]string {
