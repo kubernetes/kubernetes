cb01a28df40235645c969eca9acbaae5e76e8900
Baptiste Girard-Carrabin <baptiste.girardcarrabin@datadoghq.com>
2025-10-07 11:00:55 +0200

    Ensure CSINode is owned by node when installing CSI drivers
    CSI drivers can be installed while the node is still initializing since the daemonsets usually tolerate all taints. As a result, we also need to make sure the outdated CSINode object has been removed before installing a new CSI driver.

--- pkg/volume/csi/nodeinfomanager/nodeinfomanager_test.go
+++ pkg/volume/csi/nodeinfomanager/nodeinfomanager_test.go
@@ -308,6 +308,37 @@ func TestInstallCSIDriver(t *testing.T) {
 			},
 		},
 		{
+			name: "pre-existing node info, but owned by previous node",
+			existingNode: func() *v1.Node {
+				node := generateNode(nil /*nodeIDs*/, nil /*labels*/, nil /*capacity*/)
+				node.UID = types.UID("node1")
+				return node
+			}(),
+			existingCSINode: func() *storage.CSINode {
+				csiNode := generateCSINode(nil /*nodeIDs*/, nil /*volumeLimits*/, nil /*topologyKeys*/)
+				csiNode.OwnerReferences[0].UID = types.UID("node2")
+				return csiNode
+			}(),
+			inputNodeID: "com.example.csi/csi-node1",
+			expectedNode: &v1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        "node1",
+					UID:         types.UID("node1"),
+					Annotations: map[string]string{annotationKeyNodeID: marshall(nodeIDMap{"": "com.example.csi/csi-node1"})},
+				},
+			},
+			expectedCSINode: &storage.CSINode{
+				ObjectMeta: getCSINodeObjectMeta(),
+				Spec: storage.CSINodeSpec{
+					Drivers: []storage.CSINodeDriver{
+						{
+							NodeID: "com.example.csi/csi-node1",
+						},
+					},
+				},
+			},
+		},
+		{
 			name:          "nil topology, empty node",
 			driverName:    "com.example.csi.driver1",
 			existingNode:  generateNode(nil /* nodeIDs */, nil /* labels */, nil /*capacity*/),
