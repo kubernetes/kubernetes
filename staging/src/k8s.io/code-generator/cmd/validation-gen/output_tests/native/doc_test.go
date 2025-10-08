jpbetz/validation-gen

Add DV only infra

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/native/doc_test.go
@@ -1,0 +1,169 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package native
+
+import (
+	"strings"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+func TestMyObjectValidation(t *testing.T) {
+	strPtr := func(s string) *string {
+		return &s
+	}
+	validObj := MyObject{
+		UUIDField:                       "a0a2a2d2-0b87-4964-a123-78d00a8787a6",
+		UUIDFieldWithoutDV:              "a0a2a2d2-0b87-4964-a123-78d00a8787a6",
+		UUIDPtrField:                    strPtr("a0a2a2d2-0b87-4964-a123-78d00a8787a6"),
+		UUIDPtrFieldWithoutDV:           strPtr("a0a2a2d2-0b87-4964-a123-78d00a8787a6"),
+		UUIDTypedefField:                "a0a2a2d2-0b87-4964-a123-78d00a8787a6",
+		UUIDTypedefFieldWithoutDV:       "a0a2a2d2-0b87-4964-a123-78d00a8787a6",
+		FieldForLength:                  strings.Repeat("a", 55),
+		FieldForLengthWithoutDV:         strings.Repeat("a", 55),
+		StableTypeField:                 StableType{InnerField: "abc"},
+		StableTypeFieldWithoutDV:        StableType{InnerField: "abc"},
+		StableTypeFieldPointer:          &StableType{InnerField: "abc"},
+		StableTypeFieldPointerWithoutDV: &StableType{InnerField: "abc"},
+		StableTypeSlice:                 []StableType{{InnerField: "abc"}},
+		StableTypeSliceWithoutDV:        []StableType{{InnerField: "abc"}},
+		SetList:                         []string{"a", "b"},
+		SetListWithoutDV:                []string{"a", "b"},
+		NestedStable:                    NestedStableType{NestedField: StableType{InnerField: "abc"}, NestedFieldWithoutDV: StableType{InnerField: "abc"}},
+		NestedStableWithoutDV:           NestedStableType{NestedField: StableType{InnerField: "abc"}, NestedFieldWithoutDV: StableType{InnerField: "abc"}},
+		IPAddress:                       "1.2.3.4",
+		IPAddressWithoutDV:              "1.2.3.4",
+	}
+
+	tests := []struct {
+		name string
+		obj  MyObject
+		errs field.ErrorList
+	}{
+		{
+			name: "valid",
+			obj:  *validObj.DeepCopy(),
+		},
+		{
+			name: "uuid",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.UUIDField = "invalid"
+				obj.UUIDFieldWithoutDV = "invalid"
+				obj.UUIDPtrField = strPtr("invalid")
+				obj.UUIDPtrFieldWithoutDV = strPtr("invalid")
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Invalid(field.NewPath("uuidField"), "invalid", "is not a valid UUID").MarkCoveredByDeclarative(),
+				field.Invalid(field.NewPath("uuidFieldWithoutDV"), "invalid", "is not a valid UUID"),
+				field.Invalid(field.NewPath("uuidPtrField"), "invalid", "is not a valid UUID").MarkCoveredByDeclarative(),
+				field.Invalid(field.NewPath("uuidPtrFieldWithoutDV"), "invalid", "is not a valid UUID"),
+			},
+		},
+		{
+			name: "typedef",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.UUIDTypedefField = "invalid"
+				obj.UUIDTypedefFieldWithoutDV = "invalid"
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Invalid(field.NewPath("uuidTypedefField"), UUIDString("invalid"), "is not a valid UUID").MarkCoveredByDeclarative(),
+				field.Invalid(field.NewPath("uuidTypedefFieldWithoutDV"), UUIDString("invalid"), "is not a valid UUID"),
+			},
+		},
+		{
+			name: "required",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.StableTypeField.InnerField = ""
+				obj.StableTypeFieldWithoutDV.InnerField = ""
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Required(field.NewPath("stableTypeField", "innerField"), "").MarkCoveredByDeclarative(),
+				field.Required(field.NewPath("stableTypeFieldWithoutDV", "innerField"), ""),
+			},
+		},
+		{
+			name: "slice",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.StableTypeSlice = []StableType{{InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}}
+				obj.StableTypeSliceWithoutDV = []StableType{{InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}}
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.TooMany(field.NewPath("stableTypeSlice"), 6, 5).MarkCoveredByDeclarative(),
+				field.TooMany(field.NewPath("stableTypeSliceWithoutDV"), 6, 5),
+			},
+		},
+		{
+			name: "set",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.SetList = []string{"a", "a"}
+				obj.SetListWithoutDV = []string{"a", "a"}
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Duplicate(field.NewPath("setList").Index(1), "a").MarkCoveredByDeclarative(),
+				field.Duplicate(field.NewPath("setListWithoutDV").Index(1), "a"),
+			},
+		},
+		{
+			name: "nested",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.NestedStable.NestedField.InnerField = ""
+				obj.NestedStable.NestedFieldWithoutDV.InnerField = ""
+				obj.NestedStableWithoutDV.NestedFieldWithoutDV.InnerField = ""
+				obj.NestedStableWithoutDV.NestedField.InnerField = ""
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Required(field.NewPath("nestedStable", "nestedField", "innerField"), "").MarkCoveredByDeclarative(),
+				field.Required(field.NewPath("nestedStable", "nestedFieldWithoutDV", "innerField"), "").MarkCoveredByDeclarative(),
+				field.Required(field.NewPath("nestedStableWithoutDV", "nestedFieldWithoutDV", "innerField"), ""),
+				field.Required(field.NewPath("nestedStableWithoutDV", "nestedField", "innerField"), "").MarkCoveredByDeclarative(),
+			},
+		},
+		{
+			name: "ip",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.IPAddress = "invalid"
+				obj.IPAddressWithoutDV = "invalid"
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Invalid(field.NewPath("ipAddress"), "invalid", "is not a valid IP address").MarkCoveredByDeclarative(),
+				field.Invalid(field.NewPath("ipAddressWithoutDV"), "invalid", "is not a valid IP address"),
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			st := localSchemeBuilder.Test(t)
+			st.Value(&tc.obj).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByCoveredByDeclarative(), tc.errs)
+		})
+	}
+}

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/enum/doc_test.go
@@ -57,7 +57,7 @@ func Test(t *testing.T) {
 		Enum2PtrField:   ptr.To(Enum2("x")),
 		NotEnumField:    "x",
 		NotEnumPtrField: ptr.To(NotEnum("x")),
-	}).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByOrigin(), field.ErrorList{
+	}).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByOrigin().ByCoveredByDeclarative(), field.ErrorList{
 		field.NotSupported(field.NewPath("enum0Field"), Enum0("x"), []Enum0{}).WithOrigin("enum"),
 		field.NotSupported(field.NewPath("enum0PtrField"), Enum0("x"), []Enum0{}).WithOrigin("enum"),
 		field.NotSupported(field.NewPath("enum1Field"), Enum1("x"), []Enum1{E1V1}).WithOrigin("enum"),

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/format/k8s-label-key/doc_test.go
@@ -72,7 +72,7 @@ func Test(t *testing.T) {
 			LabelKeyField:        s,
 			LabelKeyPtrField:     ptr.To(s),
 			LabelKeyTypedefField: LabelKeyStringType(s),
-		}).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByOrigin(), field.ErrorList{
+		}).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByOrigin().ByCoveredByDeclarative(), field.ErrorList{
 			field.Invalid(field.NewPath("labelKeyField"), nil, "").WithOrigin("format=k8s-label-key"),
 			field.Invalid(field.NewPath("labelKeyPtrField"), nil, "").WithOrigin("format=k8s-label-key"),
 			field.Invalid(field.NewPath("labelKeyTypedefField"), nil, "").WithOrigin("format=k8s-label-key"),

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/item/zerorooneof/typedef/doc_test.go
@@ -50,7 +50,7 @@ func Test(t *testing.T) {
 	}
 
 	st.Value(invalidBothSet).ExpectMatches(
-		field.ErrorMatcher{}.ByType().ByField().ByOrigin(),
+		field.ErrorMatcher{}.ByType().ByField().ByOrigin().ByCoveredByDeclarative(),
 		field.ErrorList{
 			field.Invalid(field.NewPath("tasks"), nil, "").WithOrigin("zeroOrOneOf"),
 		},

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/update_validations/primitive_pointers/doc_test.go
@@ -52,7 +52,7 @@ func Test(t *testing.T) {
 
 	st.Value(&structA1).OldValue(&structA2).ExpectValid()
 
-	st.Value(&structA1).OldValue(&structB).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByDetailSubstring().ByOrigin(), field.ErrorList{
+	st.Value(&structA1).OldValue(&structB).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByDetailSubstring().ByOrigin().ByCoveredByDeclarative(), field.ErrorList{
 		field.Forbidden(field.NewPath("sp"), "").WithOrigin("immutable"),
 		field.Forbidden(field.NewPath("ip"), "").WithOrigin("immutable"),
 		field.Forbidden(field.NewPath("bp"), "").WithOrigin("immutable"),

kubernetes/kubernetes

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/native/doc_test.go
@@ -1,0 +1,169 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package native
+
+import (
+	"strings"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+func TestMyObjectValidation(t *testing.T) {
+	strPtr := func(s string) *string {
+		return &s
+	}
+	validObj := MyObject{
+		UUIDField:                       "a0a2a2d2-0b87-4964-a123-78d00a8787a6",
+		UUIDFieldWithoutDV:              "a0a2a2d2-0b87-4964-a123-78d00a8787a6",
+		UUIDPtrField:                    strPtr("a0a2a2d2-0b87-4964-a123-78d00a8787a6"),
+		UUIDPtrFieldWithoutDV:           strPtr("a0a2a2d2-0b87-4964-a123-78d00a8787a6"),
+		UUIDTypedefField:                "a0a2a2d2-0b87-4964-a123-78d00a8787a6",
+		UUIDTypedefFieldWithoutDV:       "a0a2a2d2-0b87-4964-a123-78d00a8787a6",
+		FieldForLength:                  strings.Repeat("a", 55),
+		FieldForLengthWithoutDV:         strings.Repeat("a", 55),
+		StableTypeField:                 StableType{InnerField: "abc"},
+		StableTypeFieldWithoutDV:        StableType{InnerField: "abc"},
+		StableTypeFieldPointer:          &StableType{InnerField: "abc"},
+		StableTypeFieldPointerWithoutDV: &StableType{InnerField: "abc"},
+		StableTypeSlice:                 []StableType{{InnerField: "abc"}},
+		StableTypeSliceWithoutDV:        []StableType{{InnerField: "abc"}},
+		SetList:                         []string{"a", "b"},
+		SetListWithoutDV:                []string{"a", "b"},
+		NestedStable:                    NestedStableType{NestedField: StableType{InnerField: "abc"}, NestedFieldWithoutDV: StableType{InnerField: "abc"}},
+		NestedStableWithoutDV:           NestedStableType{NestedField: StableType{InnerField: "abc"}, NestedFieldWithoutDV: StableType{InnerField: "abc"}},
+		IPAddress:                       "1.2.3.4",
+		IPAddressWithoutDV:              "1.2.3.4",
+	}
+
+	tests := []struct {
+		name string
+		obj  MyObject
+		errs field.ErrorList
+	}{
+		{
+			name: "valid",
+			obj:  *validObj.DeepCopy(),
+		},
+		{
+			name: "uuid",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.UUIDField = "invalid"
+				obj.UUIDFieldWithoutDV = "invalid"
+				obj.UUIDPtrField = strPtr("invalid")
+				obj.UUIDPtrFieldWithoutDV = strPtr("invalid")
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Invalid(field.NewPath("uuidField"), "invalid", "is not a valid UUID").MarkDeclarativeOnly(),
+				field.Invalid(field.NewPath("uuidFieldWithoutDV"), "invalid", "is not a valid UUID"),
+				field.Invalid(field.NewPath("uuidPtrField"), "invalid", "is not a valid UUID").MarkDeclarativeOnly(),
+				field.Invalid(field.NewPath("uuidPtrFieldWithoutDV"), "invalid", "is not a valid UUID"),
+			},
+		},
+		{
+			name: "typedef",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.UUIDTypedefField = "invalid"
+				obj.UUIDTypedefFieldWithoutDV = "invalid"
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Invalid(field.NewPath("uuidTypedefField"), UUIDString("invalid"), "is not a valid UUID").MarkDeclarativeOnly(),
+				field.Invalid(field.NewPath("uuidTypedefFieldWithoutDV"), UUIDString("invalid"), "is not a valid UUID"),
+			},
+		},
+		{
+			name: "required",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.StableTypeField.InnerField = ""
+				obj.StableTypeFieldWithoutDV.InnerField = ""
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Required(field.NewPath("stableTypeField", "innerField"), "").MarkDeclarativeOnly(),
+				field.Required(field.NewPath("stableTypeFieldWithoutDV", "innerField"), ""),
+			},
+		},
+		{
+			name: "slice",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.StableTypeSlice = []StableType{{InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}}
+				obj.StableTypeSliceWithoutDV = []StableType{{InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}, {InnerField: "a"}}
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.TooMany(field.NewPath("stableTypeSlice"), 6, 5).MarkDeclarativeOnly(),
+				field.TooMany(field.NewPath("stableTypeSliceWithoutDV"), 6, 5),
+			},
+		},
+		{
+			name: "set",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.SetList = []string{"a", "a"}
+				obj.SetListWithoutDV = []string{"a", "a"}
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Duplicate(field.NewPath("setList").Index(1), "a").MarkDeclarativeOnly(),
+				field.Duplicate(field.NewPath("setListWithoutDV").Index(1), "a"),
+			},
+		},
+		{
+			name: "nested",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.NestedStable.NestedField.InnerField = ""
+				obj.NestedStable.NestedFieldWithoutDV.InnerField = ""
+				obj.NestedStableWithoutDV.NestedFieldWithoutDV.InnerField = ""
+				obj.NestedStableWithoutDV.NestedField.InnerField = ""
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Required(field.NewPath("nestedStable", "nestedField", "innerField"), "").MarkDeclarativeOnly(),
+				field.Required(field.NewPath("nestedStable", "nestedFieldWithoutDV", "innerField"), "").MarkDeclarativeOnly(),
+				field.Required(field.NewPath("nestedStableWithoutDV", "nestedFieldWithoutDV", "innerField"), ""),
+				field.Required(field.NewPath("nestedStableWithoutDV", "nestedField", "innerField"), "").MarkDeclarativeOnly(),
+			},
+		},
+		{
+			name: "ip",
+			obj: func() MyObject {
+				obj := validObj.DeepCopy()
+				obj.IPAddress = "invalid"
+				obj.IPAddressWithoutDV = "invalid"
+				return *obj
+			}(),
+			errs: field.ErrorList{
+				field.Invalid(field.NewPath("ipAddress"), "invalid", "is not a valid IP address").MarkDeclarativeOnly(),
+				field.Invalid(field.NewPath("ipAddressWithoutDV"), "invalid", "is not a valid IP address"),
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			st := localSchemeBuilder.Test(t)
+			st.Value(&tc.obj).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByDeclarativeOnly(), tc.errs)
+		})
+	}
+}

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/enum/doc_test.go
@@ -57,7 +57,7 @@ func Test(t *testing.T) {
 		Enum2PtrField:   ptr.To(Enum2("x")),
 		NotEnumField:    "x",
 		NotEnumPtrField: ptr.To(NotEnum("x")),
-	}).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByOrigin(), field.ErrorList{
+	}).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByOrigin().ByDeclarativeOnly(), field.ErrorList{
 		field.NotSupported(field.NewPath("enum0Field"), Enum0("x"), []Enum0{}).WithOrigin("enum"),
 		field.NotSupported(field.NewPath("enum0PtrField"), Enum0("x"), []Enum0{}).WithOrigin("enum"),
 		field.NotSupported(field.NewPath("enum1Field"), Enum1("x"), []Enum1{E1V1}).WithOrigin("enum"),

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/format/k8s-label-key/doc_test.go
@@ -72,7 +72,7 @@ func Test(t *testing.T) {
 			LabelKeyField:        s,
 			LabelKeyPtrField:     ptr.To(s),
 			LabelKeyTypedefField: LabelKeyStringType(s),
-		}).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByOrigin(), field.ErrorList{
+		}).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByOrigin().ByDeclarativeOnly(), field.ErrorList{
 			field.Invalid(field.NewPath("labelKeyField"), nil, "").WithOrigin("format=k8s-label-key"),
 			field.Invalid(field.NewPath("labelKeyPtrField"), nil, "").WithOrigin("format=k8s-label-key"),
 			field.Invalid(field.NewPath("labelKeyTypedefField"), nil, "").WithOrigin("format=k8s-label-key"),

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/item/zerorooneof/typedef/doc_test.go
@@ -50,7 +50,7 @@ func Test(t *testing.T) {
 	}
 
 	st.Value(invalidBothSet).ExpectMatches(
-		field.ErrorMatcher{}.ByType().ByField().ByOrigin(),
+		field.ErrorMatcher{}.ByType().ByField().ByOrigin().ByDeclarativeOnly(),
 		field.ErrorList{
 			field.Invalid(field.NewPath("tasks"), nil, "").WithOrigin("zeroOrOneOf"),
 		},

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/update_validations/primitive_pointers/doc_test.go
@@ -52,7 +52,7 @@ func Test(t *testing.T) {
 
 	st.Value(&structA1).OldValue(&structA2).ExpectValid()
 
-	st.Value(&structA1).OldValue(&structB).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByDetailSubstring().ByOrigin(), field.ErrorList{
+	st.Value(&structA1).OldValue(&structB).ExpectMatches(field.ErrorMatcher{}.ByType().ByField().ByDetailSubstring().ByOrigin().ByDeclarativeOnly(), field.ErrorList{
 		field.Forbidden(field.NewPath("sp"), "").WithOrigin("immutable"),
 		field.Forbidden(field.NewPath("ip"), "").WithOrigin("immutable"),
 		field.Forbidden(field.NewPath("bp"), "").WithOrigin("immutable"),