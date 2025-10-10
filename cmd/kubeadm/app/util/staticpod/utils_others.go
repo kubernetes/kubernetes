da0491d701b15528dc84d9dfd8e7ea7a617eef9b
HirazawaUi <695097494plus@gmail.com>
2025-10-10 18:36:42 +0800

    Revert "Merge pull request #134178 from HirazawaUi/remove-RootlessControlPlane"
    This reverts commit fce5a08b9247b608c73cdeacde939c7e4951bce3, reversing
    changes made to d39b162163006799cc8e0198fa83d0b6d36a41b9.

---
+++ cmd/kubeadm/app/util/staticpod/utils_others.go
@@ -0,0 +1,32 @@
+//go:build !linux
+// +build !linux
+
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package staticpod
+
+import (
+	v1 "k8s.io/api/core/v1"
+
+	kubeadmapi "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm"
+	"k8s.io/kubernetes/cmd/kubeadm/app/util/users"
+)
+
+// RunComponentAsNonRoot is a NO-OP on non linux.
+func RunComponentAsNonRoot(componentName string, pod *v1.Pod, usersAndGroups *users.UsersAndGroups, cfg *kubeadmapi.ClusterConfiguration) error {
+	return nil
+}
