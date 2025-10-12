8799f7799b6d767db93585a6d5c8b676825e7446
Naoki Sega <499108+nsega@users.noreply.github.com>
2025-10-12 15:55:26 -0700

    fix(test): remove unnecessary comment

--- pkg/scheduler/framework/runtime/framework_test.go
+++ pkg/scheduler/framework/runtime/framework_test.go
@@ -3894,7 +3894,6 @@ func TestRecordingMetricsWithMocks(t *testing.T) {
 						},
 					},
 				},
-				// Additional test cases from original TestRecordingMetrics
 				{
 					name:                            "PreScore - Success",
 					action:                          func(ctx context.Context, f framework.Framework) { f.RunPreScorePlugins(ctx, state, pod, nil) },
