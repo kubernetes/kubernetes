86c76c51aca4c813a591e379c228e3c04a98e1bb
Hironori Shiina <shiina.hironori@gmail.com>
2025-10-13 21:07:25 +0900

    api: accept removing requsets & limits for Burstable pods

--- pkg/apis/core/validation/validation.go
+++ pkg/apis/core/validation/validation.go
@@ -6314,30 +6314,9 @@ func isPodResizeRequestSupported(pod core.Pod) bool {
 func validateContainerResize(newRequirements, oldRequirements *core.ResourceRequirements, resizePolicies []core.ContainerResizePolicy, fldPath *field.Path) field.ErrorList {
 	var allErrs field.ErrorList
 
-	// Removing resource requirements is not supported.
-	if resourcesRemoved(newRequirements.Requests, oldRequirements.Requests) {
-		allErrs = append(allErrs, field.Forbidden(fldPath.Child("requests"), "resource requests cannot be removed"))
-	}
-	if resourcesRemoved(newRequirements.Limits, oldRequirements.Limits) {
-		allErrs = append(allErrs, field.Forbidden(fldPath.Child("limits"), "resource limits cannot be removed"))
-	}
-
 	// TODO(tallclair): Move resizable resource checks here.
 
 	return allErrs
-}
-
-func resourcesRemoved(resourceList, oldResourceList core.ResourceList) bool {
-	if len(oldResourceList) > len(resourceList) {
-		return true
-	}
-	for name := range oldResourceList {
-		if _, ok := resourceList[name]; !ok {
-			return true
-		}
-	}
-
-	return false
 }
 
 // ValidatePodBinding tests if required fields in the pod binding are legal.
