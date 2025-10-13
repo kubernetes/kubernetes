cec7bfa7fd2bc3dbabd9452feae883bade0e0f6c
Aaron Prindle <aprindle@google.com>
2025-10-13 00:22:32 +0000

    feat: add +k8s:enum declarative validation to ResourceClaim DeviceTolerationOperator and DeviceTaintEffect

--- staging/src/k8s.io/api/resource/v1beta1/types.go
+++ staging/src/k8s.io/api/resource/v1beta1/types.go
@@ -654,6 +654,7 @@ type DeviceTaint struct {
 }
 
 // +enum
+// +k8s:enum
 type DeviceTaintEffect string
 
 const (
@@ -1372,6 +1373,7 @@ type DeviceToleration struct {
 // A toleration operator is the set of operators that can be used in a toleration.
 //
 // +enum
+// +k8s:enum
 type DeviceTolerationOperator string
 
 const (
