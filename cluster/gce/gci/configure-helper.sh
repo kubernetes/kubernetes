ec5513b65a6b0618edb2a16bd78d73ee547596da
Carlos Panato <ctadeu@gmail.com>
2025-10-14 10:17:37 +0200

    update setcap and debian-base to bookworm-v1.0.6
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- cluster/gce/gci/configure-helper.sh
+++ cluster/gce/gci/configure-helper.sh
@@ -3223,7 +3223,7 @@ spec:
   - name: vol
   containers:
   - name: pv-recycler
-    image: registry.k8s.io/build-image/debian-base:bookworm-v1.0.4
+    image: registry.k8s.io/build-image/debian-base:bookworm-v1.0.6
     command:
     - /bin/sh
     args:
