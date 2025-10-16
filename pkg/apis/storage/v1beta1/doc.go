4bea7935887714a7788ec63977173b8fd2de3d37
darshansreenivas <darshants.darshan1@gmail.com>
2025-10-16 09:47:26 -0700

    upgrade tags to declarative tags

--- pkg/apis/storage/v1beta1/doc.go
+++ pkg/apis/storage/v1beta1/doc.go
@@ -19,5 +19,7 @@ limitations under the License.
 // +groupName=storage.k8s.io
 // +k8s:defaulter-gen=TypeMeta
 // +k8s:defaulter-gen-input=k8s.io/api/storage/v1beta1
+// +k8s:validation-gen=TypeMeta
+// +k8s:validation-gen-input=k8s.io/api/storage/v1beta1
 
 package v1beta1
