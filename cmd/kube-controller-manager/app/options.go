adivyoseph/ai_kubelet

kcm: Extend --controllers flag usage string
Print the feature gates required for each controller.

cmd/kube-controller-manager/app/options.go
@@ -1,0 +1,46 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"fmt"
+	"strings"
+)
+
+func newControllerListFormatter(descriptors map[string]*ControllerDescriptor) func([]string) string {
+	return func(controllers []string) string {
+		var out strings.Builder
+		out.WriteString("\n")
+		for _, name := range controllers {
+			// In case there is no descriptor for the controller, just write the name.
+			// This should never happen in practice, but better not to panic.
+			desc, ok := descriptors[name]
+			if !ok {
+				fmt.Fprintf(&out, "%s\n", name)
+				continue
+			}
+
+			gates := desc.GetAllRequiredFeatureGateStrings()
+			if len(gates) == 0 {
+				fmt.Fprintf(&out, "%s\n", name)
+			} else {
+				fmt.Fprintf(&out, "%s [feature gates required: %s]\n", name, strings.Join(gates, ", "))
+			}
+		}
+		return out.String()
+	}
+}

kubernetes/kubernetes

cmd/kube-controller-manager/app/options.go
@@ -1,0 +1,46 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package app
+
+import (
+	"fmt"
+	"strings"
+)
+
+func newControllerListFormatter(descriptors map[string]*ControllerDescriptor) func([]string) string {
+	return func(controllers []string) string {
+		var out strings.Builder
+		out.WriteString("\n")
+		for _, name := range controllers {
+			// In case there is no descriptor for the controller, just write the name.
+			// This should never happen in practice, but better not to panic.
+			desc, ok := descriptors[name]
+			if !ok {
+				fmt.Fprintf(&out, "%s\n", name)
+				continue
+			}
+
+			gates := desc.GetRequiredFeatureGates()
+			if len(gates) == 0 {
+				fmt.Fprintf(&out, "%s\n", name)
+			} else {
+				fmt.Fprintf(&out, "%s (requires feature gates: %s)\n", name, strings.Join(gates, ", "))
+			}
+		}
+		return out.String()
+	}
+}