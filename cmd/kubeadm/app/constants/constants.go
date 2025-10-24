a9ea4da48f3fe128b31bd97c490569b8bb0bf2a2
joshjms <joshjms1607@gmail.com>
2025-10-25 01:20:04 +0800

    etcd: bump etcd to v3.5.24 for k8s v1.31, v1.32, v1.33
    Signed-off-by: joshjms <joshjms1607@gmail.com>

--- cmd/kubeadm/app/constants/constants.go
+++ cmd/kubeadm/app/constants/constants.go
@@ -319,7 +319,7 @@ const (
 	KubeletHealthzPort = 10248
 
 	// MinExternalEtcdVersion indicates minimum external etcd version which kubeadm supports
-	MinExternalEtcdVersion = "3.5.11-0"
+	MinExternalEtcdVersion = "3.5.24-0"
 
 	// DefaultEtcdVersion indicates the default etcd version that kubeadm uses
 	DefaultEtcdVersion = "3.5.15-0"
@@ -481,9 +481,9 @@ var (
 
 	// SupportedEtcdVersion lists officially supported etcd versions with corresponding Kubernetes releases
 	SupportedEtcdVersion = map[uint8]string{
-		29: "3.5.15-0",
-		30: "3.5.15-0",
-		31: "3.5.15-0",
+		29: "3.5.24-0",
+		30: "3.5.24-0",
+		31: "3.5.24-0",
 	}
 
 	// KubeadmCertsClusterRoleName sets the name for the ClusterRole that allows
