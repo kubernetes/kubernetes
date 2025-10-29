8aad85c9fa8ab9c1dc3cfd093ce19bf0f7ab0f87
Natasha Sarkar <natashasarkar@google.com>
2025-10-29 16:01:21 +0000

    promote ippr to ga

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1277,6 +1277,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	InPlacePodVerticalScaling: {
 		{Version: version.MustParse("1.27"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.33"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.35"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 1.38
 	},
 
 	InPlacePodVerticalScalingAllocatedStatus: {
