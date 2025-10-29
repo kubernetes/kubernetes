4403b1587767e400d1e34819bbe32dccca99d5d4
darshansreenivas <darshants.darshan1@gmail.com>
2025-10-28 20:46:21 -0700

    add required tag for attacher

---
+++ pkg/registry/storage/volumeattachment/declarative_validation_test.go
@@ -0,0 +1,156 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package volumeattachment
+
+import (
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
+	apitesting "k8s.io/kubernetes/pkg/api/testing"
+	storage "k8s.io/kubernetes/pkg/apis/storage"
+)
+
+func TestDeclarativeValidate(t *testing.T) {
+	// VolumeAttachment existed as v1beta1 and v1
+	apiVersions := []string{"v1beta1", "v1"}
+	for _, apiVersion := range apiVersions {
+		t.Run(apiVersion, func(t *testing.T) {
+			testDeclarativeValidate(t, apiVersion)
+		})
+	}
+}
+
+func testDeclarativeValidate(t *testing.T, apiVersion string) {
+	ctx := genericapirequest.WithRequestInfo(genericapirequest.NewDefaultContext(), &genericapirequest.RequestInfo{
+		APIGroup:          "storage.k8s.io",
+		APIVersion:        apiVersion,
+		Resource:          "volumeattachments",
+		IsResourceRequest: true,
+		Verb:              "create",
+	})
+
+	testCases := map[string]struct {
+		input        storage.VolumeAttachment
+		expectedErrs field.ErrorList
+	}{
+		"valid": {
+			input: mkValidVolumeAttachment(),
+		},
+		"invalid attacher (required)": {
+			input: mkValidVolumeAttachment(func(obj *storage.VolumeAttachment) {
+				obj.Spec.Attacher = ""
+			}),
+			expectedErrs: field.ErrorList{
+				field.Required(field.NewPath("spec", "attacher"), ""),
+			},
+		},
+		// TODO: Add more test cases (e.g., attacher format constraints) if desired.
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			apitesting.VerifyValidationEquivalence(t, ctx, &tc.input, Strategy.Validate, tc.expectedErrs)
+		})
+	}
+}
+
+func TestDeclarativeValidateUpdate(t *testing.T) {
+	apiVersions := []string{"v1beta1", "v1"}
+	for _, apiVersion := range apiVersions {
+		t.Run(apiVersion, func(t *testing.T) {
+			testDeclarativeValidateUpdate(t, apiVersion)
+		})
+	}
+}
+
+func testDeclarativeValidateUpdate(t *testing.T, apiVersion string) {
+	testCases := map[string]struct {
+		oldObj       storage.VolumeAttachment
+		updateObj    storage.VolumeAttachment
+		expectedErrs field.ErrorList
+	}{
+		"valid update": {
+			oldObj:    mkValidVolumeAttachment(func(obj *storage.VolumeAttachment) { obj.ResourceVersion = "1" }),
+			updateObj: mkValidVolumeAttachment(func(obj *storage.VolumeAttachment) { obj.ResourceVersion = "1" }),
+		},
+		"invalid update attacher (required + immutable)": {
+			oldObj: mkValidVolumeAttachment(func(obj *storage.VolumeAttachment) { obj.ResourceVersion = "1" }),
+			updateObj: mkValidVolumeAttachment(func(obj *storage.VolumeAttachment) {
+				obj.ResourceVersion = "1"
+				obj.Spec.Attacher = ""
+			}),
+			expectedErrs: field.ErrorList{
+				field.Required(field.NewPath("spec", "attacher"), ""),
+				field.Forbidden(field.NewPath("spec", "attacher"), "updates to attacher are forbidden."),
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			ctx := genericapirequest.WithRequestInfo(genericapirequest.NewDefaultContext(), &genericapirequest.RequestInfo{
+				APIPrefix:         "apis",
+				APIGroup:          "storage.k8s.io",
+				APIVersion:        apiVersion,
+				Resource:          "volumeattachments",
+				Name:              "valid-volume-attachment",
+				IsResourceRequest: true,
+				Verb:              "update",
+			})
+			apitesting.VerifyUpdateValidationEquivalence(t, ctx, &tc.updateObj, &tc.oldObj, Strategy.ValidateUpdate, tc.expectedErrs)
+		})
+	}
+}
+
+func mkValidVolumeAttachment(tweaks ...func(obj *storage.VolumeAttachment)) storage.VolumeAttachment {
+	pvName := "pv-001"
+	obj := storage.VolumeAttachment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "valid-volume-attachment",
+		},
+		Spec: storage.VolumeAttachmentSpec{
+			// Use an in-tree style qualified name to satisfy legacy validation rules.
+			Attacher: "example.com",
+			Source: storage.VolumeAttachmentSource{
+				PersistentVolumeName: &pvName,
+			},
+			NodeName: "node-1",
+		},
+	}
+	for _, tweak := range tweaks {
+		tweak(&obj)
+	}
+	return obj
+}
