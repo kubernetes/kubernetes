8d1eb06808d71c580871f49b43452e31e315138b
Abhijit Hoskeri <abhijithoskeri@gmail.com>
2025-10-29 17:06:45 -0700

    Allow overriding default event namespace in Kubelet.
    Kubelet uses the "default" namespace to post events
    about itself.
    
    Cluster operators may want to implement restrictions on the "default"
    namespace or discourage its use. This would result in errors in Kubelet,
    and the loss of those events.
    
    Allow overriding the namespace Kubelet posts events to.
    The default behavior is unchanged.
    
    Requires the KubeletEventsNamespace feature gate.

--- pkg/kubelet/apis/config/v1beta1/zz_generated.conversion.go
+++ pkg/kubelet/apis/config/v1beta1/zz_generated.conversion.go
@@ -602,6 +602,7 @@ func autoConvert_v1beta1_KubeletConfiguration_To_config_KubeletConfiguration(in 
 		return err
 	}
 	out.EventBurst = in.EventBurst
+	out.EventNamespace = in.EventNamespace
 	if err := v1.Convert_Pointer_bool_To_bool(&in.EnableDebuggingHandlers, &out.EnableDebuggingHandlers, s); err != nil {
 		return err
 	}
@@ -812,6 +813,7 @@ func autoConvert_config_KubeletConfiguration_To_v1beta1_KubeletConfiguration(in 
 		return err
 	}
 	out.EventBurst = in.EventBurst
+	out.EventNamespace = in.EventNamespace
 	if err := v1.Convert_bool_To_Pointer_bool(&in.EnableDebuggingHandlers, &out.EnableDebuggingHandlers, s); err != nil {
 		return err
 	}
