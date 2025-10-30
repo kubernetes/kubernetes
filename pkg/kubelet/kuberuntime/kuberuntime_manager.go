debb69b526ba84b8f019559f92c193ff3fb801db
HirazawaUi <695097494plus@gmail.com>
2025-10-30 21:46:51 +0800

    using real-time container events for pod state determination

--- pkg/kubelet/kuberuntime/kuberuntime_manager.go
+++ pkg/kubelet/kuberuntime/kuberuntime_manager.go
@@ -832,10 +832,6 @@ func (m *kubeGenericRuntimeManager) doPodResizeAction(ctx context.Context, pod *
 		return err
 	}
 
-	// Always update the pod status once. Even if there was a resize error, the resize may have been
-	// partially actuated.
-	defer m.runtimeHelper.RequestPodReSync(pod.UID)
-
 	if len(podContainerChanges.ContainersToUpdate[v1.ResourceMemory]) > 0 || podContainerChanges.UpdatePodResources {
 		if podResources.Memory == nil {
 			// Default pod memory limit to the current memory limit if unset to prevent it from updating.
@@ -1101,12 +1097,12 @@ func (m *kubeGenericRuntimeManager) computePodActions(ctx context.Context, pod *
 		return changes
 	}
 
+	logger.Info("DEBUG", "status", podStatus)
 	// Number of running containers to keep.
 	keepCount := 0
 	// check the status of containers.
 	for idx, container := range pod.Spec.Containers {
 		containerStatus := podStatus.FindContainerStatusByName(container.Name)
-
 		// Call internal container post-stop lifecycle hook for any non-running container so that any
 		// allocated cpus are released immediately. If the container is restarted, cpus will be re-allocated
 		// to it.
@@ -1224,6 +1220,10 @@ func (m *kubeGenericRuntimeManager) SyncPod(ctx context.Context, pod *v1.Pod, po
 			logger.V(4).Info("SyncPod received new pod, will create a sandbox for it", "pod", klog.KObj(pod))
 		}
 	}
+
+	// To ensure state consistency and avoid race conditions,
+	// we update the container cache after the completion of SyncPod.
+	defer m.runtimeHelper.RequestPodReSync(pod.UID)
 
 	// Step 2: Kill the pod if the sandbox has changed.
 	if podContainerChanges.KillPod {
