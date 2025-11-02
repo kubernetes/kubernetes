836ba23eb266bded3e2be5cc97a65a5ceb9f4a77
pranshul gupta <pranshulg01@gmail.com>
2025-11-02 08:35:38 +0530

    Enable Declarative Validation for ClusterRoleBinding in RBAC API group

--- pkg/registry/rbac/clusterrolebinding/strategy.go
+++ pkg/registry/rbac/clusterrolebinding/strategy.go
@@ -19,6 +19,7 @@ package clusterrolebinding
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/operation"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/apiserver/pkg/registry/rest"
@@ -71,7 +72,9 @@ func (strategy) PrepareForUpdate(ctx context.Context, obj, old runtime.Object) {
 // Validate validates a new ClusterRoleBinding. Validation must check for a correct signature.
 func (strategy) Validate(ctx context.Context, obj runtime.Object) field.ErrorList {
 	clusterRoleBinding := obj.(*rbac.ClusterRoleBinding)
-	return validation.ValidateClusterRoleBinding(clusterRoleBinding)
+	allErrs := validation.ValidateClusterRoleBinding(clusterRoleBinding)
+
+	return rest.ValidateDeclarativelyWithMigrationChecks(ctx, legacyscheme.Scheme, clusterRoleBinding, nil, allErrs, operation.Create)
 }
 
 // WarningsOnCreate returns warnings for the creation of the given object.
@@ -84,7 +87,11 @@ func (strategy) Canonicalize(obj runtime.Object) {
 
 // ValidateUpdate is the default update validation for an end user.
 func (strategy) ValidateUpdate(ctx context.Context, obj, old runtime.Object) field.ErrorList {
-	return validation.ValidateClusterRoleBindingUpdate(obj.(*rbac.ClusterRoleBinding), old.(*rbac.ClusterRoleBinding))
+	newObj := obj.(*rbac.ClusterRoleBinding)
+	oldObj := old.(*rbac.ClusterRoleBinding)
+	errs := validation.ValidateClusterRoleBindingUpdate(newObj, oldObj)
+
+	return rest.ValidateDeclarativelyWithMigrationChecks(ctx, legacyscheme.Scheme, newObj, oldObj, errs, operation.Update)
 }
 
 // WarningsOnUpdate returns warnings for the given update.
