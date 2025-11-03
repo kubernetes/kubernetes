1926192072d0122a9343827b41e7f33e571e5b89
pranshul gupta <pranshulg01@gmail.com>
2025-11-03 10:42:22 +0530

    test(rbac): Verify Subject.Name is required in ClusterRoleBinding list dquote> dquote> Adds a declarative validation (DV) test case for ClusterRoleBinding to ensure that the Subject.Name field is correctly enforced as required

--- pkg/registry/rbac/clusterrolebinding/declarative_validation_test.go
+++ pkg/registry/rbac/clusterrolebinding/declarative_validation_test.go
@@ -19,6 +19,7 @@ package clusterrolebinding
 import (
 	"testing"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
 	apitesting "k8s.io/kubernetes/pkg/api/testing"
@@ -42,6 +43,34 @@ func testDeclarativeValidateForDeclarative(t *testing.T, apiVersion string) {
 		input        rbac.ClusterRoleBinding
 		expectedErrs field.ErrorList
 	}{
+		"valid: subject name is present": {
+			input:        newClusterRoleBindingDeclarative("crb-valid-subject-name", "role-a", "ClusterRole"),
+			expectedErrs: nil,
+		},
+		"invalid: subject name is empty string (required)": {
+			input: func() rbac.ClusterRoleBinding {
+				crb := newClusterRoleBindingDeclarative("crb-missing-name", "role-a", "ClusterRole")
+				crb.Subjects[0].Name = ""
+				return crb
+			}(),
+			expectedErrs: field.ErrorList{
+				field.Required(field.NewPath("subjects").Index(0).Child("name"), "name is required for Subject"),
+			},
+		},
+		"invalid: multiple subjects, one name missing (required)": {
+			input: func() rbac.ClusterRoleBinding {
+				crb := newClusterRoleBindingDeclarative("crb-two-subjects-one-missing", "role-a", "ClusterRole")
+				crb.Subjects = append(crb.Subjects, rbac.Subject{
+					Kind:     "User",
+					Name:     "",
+					APIGroup: rbac.GroupName,
+				})
+				return crb
+			}(),
+			expectedErrs: field.ErrorList{
+				field.Required(field.NewPath("subjects").Index(1).Child("name"), "name is required for Subject"),
+			},
+		},
 		// TODO: Add more test cases
 	}
 	for k, tc := range testCases {
@@ -73,5 +102,27 @@ func testValidateUpdateForDeclarative(t *testing.T, apiVersion string) {
 		t.Run(k, func(t *testing.T) {
 			apitesting.VerifyUpdateValidationEquivalence(t, ctx, &tc.update, &tc.old, Strategy.ValidateUpdate, tc.expectedErrs)
 		})
+	}
+}
+
+// Helper to create a base ClusterRoleBinding for declarative tests
+func newClusterRoleBindingDeclarative(name, roleName, roleKind string) rbac.ClusterRoleBinding {
+	return rbac.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            name,
+			ResourceVersion: "1",
+		},
+		RoleRef: rbac.RoleRef{
+			APIGroup: rbac.GroupName,
+			Kind:     roleKind,
+			Name:     roleName,
+		},
+		Subjects: []rbac.Subject{
+			{
+				Kind:     "User",
+				Name:     "test-user",
+				APIGroup: rbac.GroupName,
+			},
+		},
 	}
 }
