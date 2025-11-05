ad0a3de152ccbe47da58e7a2aa9ec9106544e0e9
Stanislav Láznička <stlaz.devel@proton.me>
2025-11-05 15:32:18 +0100

    node-conformance-e2e: restart the kubelet after loading credential config
    Signed-off-by: Stanislav Láznička <slznika@microsoft.com>

--- test/e2e_node/runtime_conformance_test.go
+++ test/e2e_node/runtime_conformance_test.go
@@ -76,7 +76,7 @@ var _ = SIGDescribe("Container Runtime Conformance Test", func() {
 					})
 				})
 
-				f.It(testCase.description+"", f.WithNodeConformance(), f.WithFlaky() /* https://github.com/kubernetes/kubernetes/issues/134998 */, func(ctx context.Context) {
+				f.It(testCase.description+"", f.WithNodeConformance(), f.WithSerial(), func(ctx context.Context) {
 					name := "image-pull-test"
 					container := node.ConformanceContainer{
 						PodClient: e2epod.NewPodClient(f),
@@ -94,6 +94,10 @@ var _ = SIGDescribe("Container Runtime Conformance Test", func() {
 
 					auth := e2eregistry.User1DockerSecret(registryAddress).Data[v1.DockerConfigJsonKey]
 					configFile := filepath.Join(services.KubeletRootDirectory, "config.json")
+
+					// the kubelet would cache the config for 5 minutes, let's restart instead
+					restartKubelet(context.Background(), true)
+
 					err := os.WriteFile(configFile, []byte(auth), 0644)
 					framework.ExpectNoError(err)
 					ginkgo.DeferCleanup(func() { framework.ExpectNoError(os.Remove(configFile)) })
