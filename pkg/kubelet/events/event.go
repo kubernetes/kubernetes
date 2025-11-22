4bb8d814bb41e93884f2b03f9d913e7a98973e2e
Maxim-Durand <72691393+Maxim-Durand@users.noreply.github.com>
2025-11-22 16:13:33 +0100

    Fixed typo left in event.go

--- pkg/kubelet/events/event.go
+++ pkg/kubelet/events/event.go
@@ -1,4 +1,4 @@
-c/*
+/*
 Copyright 2014 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -94,8 +94,8 @@ const (
 
 // Probe event reason list
 const (
-	ContainerUnhealthy    = "Unhealthy"
-	ContainerProbeWarning = "ProbeWarning"
+	ContainerUnhealthy          = "Unhealthy"
+	ContainerProbeWarning       = "ProbeWarning"
 	ContainerProbeResultFailure = "ContainerProbeResultFailure"
 	ContainerProbeResultSuccess = "ContainerProbeResultSuccess"
 )
