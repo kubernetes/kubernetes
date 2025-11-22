0da6b1ff3b894c3677c205eeafefb69ce26857cd
Maxim-Durand <72691393+Maxim-Durand@users.noreply.github.com>
2025-11-22 14:27:41 +0100

    Faithful copy of https://github.com/kubernetes/kubernetes/pull/115963

--- pkg/kubelet/events/event.go
+++ pkg/kubelet/events/event.go
@@ -1,4 +1,4 @@
-/*
+c/*
 Copyright 2014 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -97,6 +97,8 @@ const (
 const (
 	ContainerUnhealthy    = "Unhealthy"
 	ContainerProbeWarning = "ProbeWarning"
+	ContainerProbeResultFailure = "ContainerProbeResultFailure"
+	ContainerProbeResultSuccess = "ContainerProbeResultSuccess"
 )
 
 // Pod worker event reason list
