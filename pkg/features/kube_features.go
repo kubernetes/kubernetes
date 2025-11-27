203b7c05d9ebf981850d0c799b5ad7d5ae4f82a7
Adrian Moisey <adrian@changeover.za.net>
2025-11-27 10:26:32 +0200

    KEP-5311 - Revert RelaxedServiceNameValidation promote to beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1707,7 +1707,6 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	RelaxedServiceNameValidation: {
 		{Version: version.MustParse("1.34"), Default: false, PreRelease: featuregate.Alpha},
-		{Version: version.MustParse("1.35"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	ReloadKubeletServerCertificateFile: {
