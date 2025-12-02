a29b8077c88f8eb63a002157ae038c3d67fe857e
胡玮文 <huweiwen.hww@alibaba-inc.com>
2025-12-02 16:31:19 +0800

    workqueue: modernize test
    Use synctest and get rid of wait loop.

--- staging/src/k8s.io/client-go/util/workqueue/delaying_queue_test.go
+++ staging/src/k8s.io/client-go/util/workqueue/delaying_queue_test.go
@@ -21,195 +21,166 @@ import (
 	"math/rand"
 	"reflect"
 	"testing"
+	"testing/synctest"
 	"time"
 
-	"k8s.io/apimachinery/pkg/util/wait"
 	testingclock "k8s.io/utils/clock/testing"
 )
 
 func TestSimpleQueue(t *testing.T) {
-	fakeClock := testingclock.NewFakeClock(time.Now())
-	q := NewDelayingQueueWithConfig(DelayingQueueConfig{Clock: fakeClock})
+	synctest.Test(t, func(t *testing.T) {
+		q := NewDelayingQueue()
+		defer q.ShutDown()
 
-	first := "foo"
+		first := "foo"
 
-	q.AddAfter(first, 50*time.Millisecond)
-	if err := waitForWaitingQueueToFill(q); err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
+		q.AddAfter(first, 50*time.Millisecond)
+		synctest.Wait() // wait for waiting queue to fill
 
-	if q.Len() != 0 {
-		t.Errorf("should not have added")
-	}
-
-	fakeClock.Step(60 * time.Millisecond)
-
-	if err := waitForAdded(q, 1); err != nil {
-		t.Errorf("should have added")
-	}
-	item, _ := q.Get()
-	q.Done(item)
-
-	// step past the next heartbeat
-	fakeClock.Step(10 * time.Second)
-
-	err := wait.Poll(1*time.Millisecond, 30*time.Millisecond, func() (done bool, err error) {
-		if q.Len() > 0 {
-			return false, fmt.Errorf("added to queue")
+		if q.Len() != 0 {
+			t.Errorf("should not have added")
 		}
 
-		return false, nil
-	})
-	if !wait.Interrupted(err) {
-		t.Errorf("expected timeout, got: %v", err)
-	}
+		time.Sleep(60 * time.Millisecond)
 
-	if q.Len() != 0 {
-		t.Errorf("should not have added")
-	}
+		waitForAdded(t, q, 1)
+		item, _ := q.Get()
+		q.Done(item)
+
+		// step past the next heartbeat
+		time.Sleep(10 * time.Second)
+
+		if q.Len() != 0 {
+			t.Errorf("should not have added")
+		}
+	})
 }
 
 func TestDeduping(t *testing.T) {
-	fakeClock := testingclock.NewFakeClock(time.Now())
-	q := NewDelayingQueueWithConfig(DelayingQueueConfig{Clock: fakeClock})
+	synctest.Test(t, func(t *testing.T) {
+		q := NewDelayingQueue()
+		defer q.ShutDown()
 
-	first := "foo"
+		first := "foo"
 
-	q.AddAfter(first, 50*time.Millisecond)
-	if err := waitForWaitingQueueToFill(q); err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
-	q.AddAfter(first, 70*time.Millisecond)
-	if err := waitForWaitingQueueToFill(q); err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
-	if q.Len() != 0 {
-		t.Errorf("should not have added")
-	}
+		q.AddAfter(first, 50*time.Millisecond)
+		synctest.Wait() // wait for waiting queue to fill
+		q.AddAfter(first, 70*time.Millisecond)
+		synctest.Wait() // wait for waiting queue to fill
+		if q.Len() != 0 {
+			t.Errorf("should not have added")
+		}
 
-	// step past the first block, we should receive now
-	fakeClock.Step(60 * time.Millisecond)
-	if err := waitForAdded(q, 1); err != nil {
-		t.Errorf("should have added")
-	}
-	item, _ := q.Get()
-	q.Done(item)
+		// step past the first block, we should receive now
+		time.Sleep(60 * time.Millisecond)
+		waitForAdded(t, q, 1)
+		item, _ := q.Get()
+		q.Done(item)
 
-	// step past the second add
-	fakeClock.Step(20 * time.Millisecond)
-	if q.Len() != 0 {
-		t.Errorf("should not have added")
-	}
+		// step past the second add
+		time.Sleep(20 * time.Millisecond)
+		if q.Len() != 0 {
+			t.Errorf("should not have added")
+		}
 
-	// test again, but this time the earlier should override
-	q.AddAfter(first, 50*time.Millisecond)
-	q.AddAfter(first, 30*time.Millisecond)
-	if err := waitForWaitingQueueToFill(q); err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
-	if q.Len() != 0 {
-		t.Errorf("should not have added")
-	}
+		// test again, but this time the earlier should override
+		q.AddAfter(first, 50*time.Millisecond)
+		q.AddAfter(first, 30*time.Millisecond)
+		synctest.Wait() // wait for waiting queue to fill
+		if q.Len() != 0 {
+			t.Errorf("should not have added")
+		}
 
-	fakeClock.Step(40 * time.Millisecond)
-	if err := waitForAdded(q, 1); err != nil {
-		t.Errorf("should have added")
-	}
-	item, _ = q.Get()
-	q.Done(item)
+		time.Sleep(40 * time.Millisecond)
+		waitForAdded(t, q, 1)
+		item, _ = q.Get()
+		q.Done(item)
 
-	// step past the second add
-	fakeClock.Step(20 * time.Millisecond)
-	if q.Len() != 0 {
-		t.Errorf("should not have added")
-	}
+		// step past the second add
+		time.Sleep(20 * time.Millisecond)
+		if q.Len() != 0 {
+			t.Errorf("should not have added")
+		}
+	})
 }
 
 func TestAddTwoFireEarly(t *testing.T) {
-	fakeClock := testingclock.NewFakeClock(time.Now())
-	q := NewDelayingQueueWithConfig(DelayingQueueConfig{Clock: fakeClock})
+	synctest.Test(t, func(t *testing.T) {
+		q := NewDelayingQueue()
+		defer q.ShutDown()
 
-	first := "foo"
-	second := "bar"
-	third := "baz"
+		first := "foo"
+		second := "bar"
+		third := "baz"
 
-	q.AddAfter(first, 1*time.Second)
-	q.AddAfter(second, 50*time.Millisecond)
-	if err := waitForWaitingQueueToFill(q); err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
+		q.AddAfter(first, 1*time.Second)
+		q.AddAfter(second, 50*time.Millisecond)
+		synctest.Wait() // wait for waiting queue to fill
 
-	if q.Len() != 0 {
-		t.Errorf("should not have added")
-	}
+		if q.Len() != 0 {
+			t.Errorf("should not have added")
+		}
 
-	fakeClock.Step(60 * time.Millisecond)
+		time.Sleep(60 * time.Millisecond)
 
-	if err := waitForAdded(q, 1); err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
-	item, _ := q.Get()
-	if !reflect.DeepEqual(item, second) {
-		t.Errorf("expected %v, got %v", second, item)
-	}
+		waitForAdded(t, q, 1)
+		item, _ := q.Get()
+		if !reflect.DeepEqual(item, second) {
+			t.Errorf("expected %v, got %v", second, item)
+		}
 
-	q.AddAfter(third, 2*time.Second)
+		q.AddAfter(third, 2*time.Second)
 
-	fakeClock.Step(1 * time.Second)
-	if err := waitForAdded(q, 1); err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
-	item, _ = q.Get()
-	if !reflect.DeepEqual(item, first) {
-		t.Errorf("expected %v, got %v", first, item)
-	}
+		time.Sleep(1 * time.Second)
+		waitForAdded(t, q, 1)
+		item, _ = q.Get()
+		if !reflect.DeepEqual(item, first) {
+			t.Errorf("expected %v, got %v", first, item)
+		}
 
-	fakeClock.Step(2 * time.Second)
-	if err := waitForAdded(q, 1); err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
-	item, _ = q.Get()
-	if !reflect.DeepEqual(item, third) {
-		t.Errorf("expected %v, got %v", third, item)
-	}
+		time.Sleep(2 * time.Second)
+		waitForAdded(t, q, 1)
+		item, _ = q.Get()
+		if !reflect.DeepEqual(item, third) {
+			t.Errorf("expected %v, got %v", third, item)
+		}
+	})
 }
 
 func TestCopyShifting(t *testing.T) {
-	fakeClock := testingclock.NewFakeClock(time.Now())
-	q := NewDelayingQueueWithConfig(DelayingQueueConfig{Clock: fakeClock})
+	synctest.Test(t, func(t *testing.T) {
+		q := NewDelayingQueue()
+		defer q.ShutDown()
 
-	first := "foo"
-	second := "bar"
-	third := "baz"
+		first := "foo"
+		second := "bar"
+		third := "baz"
 
-	q.AddAfter(first, 1*time.Second)
-	q.AddAfter(second, 500*time.Millisecond)
-	q.AddAfter(third, 250*time.Millisecond)
-	if err := waitForWaitingQueueToFill(q); err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
+		q.AddAfter(first, 1*time.Second)
+		q.AddAfter(second, 500*time.Millisecond)
+		q.AddAfter(third, 250*time.Millisecond)
+		synctest.Wait() // wait for waiting queue to fill
 
-	if q.Len() != 0 {
-		t.Errorf("should not have added")
-	}
+		if q.Len() != 0 {
+			t.Errorf("should not have added")
+		}
 
-	fakeClock.Step(2 * time.Second)
+		time.Sleep(2 * time.Second)
 
-	if err := waitForAdded(q, 3); err != nil {
-		t.Fatalf("unexpected err: %v", err)
-	}
-	actualFirst, _ := q.Get()
-	if !reflect.DeepEqual(actualFirst, third) {
-		t.Errorf("expected %v, got %v", third, actualFirst)
-	}
-	actualSecond, _ := q.Get()
-	if !reflect.DeepEqual(actualSecond, second) {
-		t.Errorf("expected %v, got %v", second, actualSecond)
-	}
-	actualThird, _ := q.Get()
-	if !reflect.DeepEqual(actualThird, first) {
-		t.Errorf("expected %v, got %v", first, actualThird)
-	}
+		waitForAdded(t, q, 3)
+		actualFirst, _ := q.Get()
+		if !reflect.DeepEqual(actualFirst, third) {
+			t.Errorf("expected %v, got %v", third, actualFirst)
+		}
+		actualSecond, _ := q.Get()
+		if !reflect.DeepEqual(actualSecond, second) {
+			t.Errorf("expected %v, got %v", second, actualSecond)
+		}
+		actualThird, _ := q.Get()
+		if !reflect.DeepEqual(actualThird, first) {
+			t.Errorf("expected %v, got %v", first, actualThird)
+		}
+	})
 }
 
 func BenchmarkDelayingQueue_AddAfter(b *testing.B) {
@@ -217,34 +188,22 @@ func BenchmarkDelayingQueue_AddAfter(b *testing.B) {
 	q := NewTypedDelayingQueueWithConfig(TypedDelayingQueueConfig[string]{Clock: fakeClock})
 
 	// Add items
-	for n := 0; n < b.N; n++ {
+	for n := range b.N {
 		data := fmt.Sprintf("%d", n)
 		q.AddAfter(data, time.Duration(rand.Int63n(int64(10*time.Minute))))
 	}
 
 	// Exercise item removal as well
 	fakeClock.Step(11 * time.Minute)
-	for n := 0; n < b.N; n++ {
+	for range b.N {
 		_, _ = q.Get()
 	}
 }
 
-func waitForAdded(q DelayingInterface, depth int) error {
-	return wait.Poll(1*time.Millisecond, 10*time.Second, func() (done bool, err error) {
-		if q.Len() == depth {
-			return true, nil
-		}
-
-		return false, nil
-	})
-}
-
-func waitForWaitingQueueToFill(q DelayingInterface) error {
-	return wait.Poll(1*time.Millisecond, 10*time.Second, func() (done bool, err error) {
-		if len(q.(*delayingType[any]).waitingForAddCh) == 0 {
-			return true, nil
-		}
-
-		return false, nil
-	})
+func waitForAdded(t *testing.T, q DelayingInterface, depth int) {
+	t.Helper()
+	synctest.Wait()
+	if q.Len() != depth {
+		t.Fatalf("expected depth %d, got %d", depth, q.Len())
+	}
 }
