c3050f189b2f2c22c41322070de25d1175c4996b
caesarsage <destinyerhabor6@gmail.com>
2025-12-06 21:31:36 +0100

    Mark PriorityClass Value validation as covered by declarative validation

--- pkg/apis/scheduling/validation/validation.go
+++ pkg/apis/scheduling/validation/validation.go
@@ -34,7 +34,7 @@ import (
 // set correctly.
 func ValidatePriorityClass(pc *scheduling.PriorityClass) field.ErrorList {
 	allErrs := field.ErrorList{}
-	allErrs = append(allErrs, apivalidation.ValidateObjectMeta(&pc.ObjectMeta, false, apimachineryvalidation.NameIsDNSSubdomain, field.NewPath("metadata"))...)
+	allErrs = append(allErrs, apivalidation.ValidateObjectMeta(&pc.ObjectMeta, false, apimachineryvalidation.NameIsDNSSubdomain, field.NewPath("metadata")).MarkCoveredByDeclarative()...)
 	// If the priorityClass starts with a system prefix, it must be one of the
 	// predefined system priority classes.
 	if strings.HasPrefix(pc.Name, scheduling.SystemPriorityClassPrefix) {
@@ -43,7 +43,7 @@ func ValidatePriorityClass(pc *scheduling.PriorityClass) field.ErrorList {
 		}
 	} else if pc.Value > scheduling.HighestUserDefinablePriority {
 		// Non-system critical priority classes are not allowed to have a value larger than HighestUserDefinablePriority.
-		allErrs = append(allErrs, field.Forbidden(field.NewPath("value"), fmt.Sprintf("maximum allowed value of a user defined priority is %v", scheduling.HighestUserDefinablePriority)))
+		allErrs = append(allErrs, field.Forbidden(field.NewPath("value"), fmt.Sprintf("maximum allowed value of a user defined priority is %v", scheduling.HighestUserDefinablePriority)).MarkCoveredByDeclarative())
 	}
 	if pc.PreemptionPolicy != nil {
 		allErrs = append(allErrs, apivalidation.ValidatePreemptionPolicy(pc.PreemptionPolicy, field.NewPath("preemptionPolicy"))...)
@@ -55,10 +55,10 @@ func ValidatePriorityClass(pc *scheduling.PriorityClass) field.ErrorList {
 // set and are valid. PriorityClass does not allow updating name, value, and preemptionPolicy.
 func ValidatePriorityClassUpdate(pc, oldPc *scheduling.PriorityClass) field.ErrorList {
 	// name is immutable and is checked by the ObjectMeta validator.
-	allErrs := apivalidation.ValidateObjectMetaUpdate(&pc.ObjectMeta, &oldPc.ObjectMeta, field.NewPath("metadata"))
+	allErrs := apivalidation.ValidateObjectMetaUpdate(&pc.ObjectMeta, &oldPc.ObjectMeta, field.NewPath("metadata")).MarkCoveredByDeclarative()
 	// value is immutable.
 	if pc.Value != oldPc.Value {
-		allErrs = append(allErrs, field.Forbidden(field.NewPath("value"), "may not be changed in an update."))
+		allErrs = append(allErrs, field.Forbidden(field.NewPath("value"), "may not be changed in an update.").MarkCoveredByDeclarative())
 	}
 	// preemptionPolicy is immutable.
 	allErrs = append(allErrs, apivalidation.ValidateImmutableField(pc.PreemptionPolicy, oldPc.PreemptionPolicy, field.NewPath("preemptionPolicy"))...)
