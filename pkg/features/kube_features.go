a16b61d4bd9590ad693230fa59f1180e916728a2
Xinyun Liu <liuxinyun6@yahoo.com>
2025-12-06 04:56:04 +0000

    Update Events Tpye for Image Manager

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1363,7 +1363,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 
 	ImageManagerEventRecorder: {
-		{Version: version.MustParse("1.36"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.35"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	ImageMaximumGCAge: {
