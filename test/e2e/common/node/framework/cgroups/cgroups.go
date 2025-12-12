1716029e43460cfb4f5b425781397b1e09a6fb0f
Benjamin Elder <bentheelder@google.com>
2025-12-12 12:13:00 -0800

    duplicate libcontainers cpu share conversion for testing
    allows us to trivially support all platforms for the test binary
    
    this conversion ideally needs to be stable anyhow to avoid test incompatibility ...
    
    https://github.com/opencontainers/cgroups/issues/18
    https://github.com/kubernetes/kubernetes/issues/135214

--- test/e2e/common/node/framework/cgroups/cgroups.go
+++ test/e2e/common/node/framework/cgroups/cgroups.go
@@ -19,6 +19,7 @@ package cgroups
 import (
 	"context"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 	"sync"
@@ -442,4 +443,46 @@ func IsPodOnCgroupv2Node(f *framework.Framework, pod *v1.Pod) (result bool) {
 	// In this case, "<mount path>/unified" is detected by "mount -t cgroup2" if cgroup hybrid mode is configured on the host.
 	// So, we need to see if "/sys/fs/cgroup" is contained in the output.
 	return strings.Contains(out, "/sys/fs/cgroup")
+}
+
+func getExpectedCPUShares(cpuRequest, cpuLimit *resource.Quantity, podOnCgroupv2 bool) []string {
+	// This function is moved out from cgroups.go because opencontainers/cgroups can only be compiled in linux platforms.
+	var shares int64
+	if cpuRequest.IsZero() && !cpuLimit.IsZero() {
+		shares = int64(kubecm.MilliCPUToShares(cpuLimit.MilliValue()))
+	} else {
+		shares = int64(kubecm.MilliCPUToShares(cpuRequest.MilliValue()))
+	}
+	if podOnCgroupv2 {
+		// Because of https://github.com/kubernetes/kubernetes/issues/131216, the way of conversion has been changed.
+		// runc: https://github.com/opencontainers/runc/pull/4785
+		// crun: https://github.com/containers/crun/issues/1721
+		// This is dependent on the container runtime version. In order not to break the tests when we upgrade the
+		// container runtimes, we check if either the old or the new conversion matches the actual value for now.
+		// TODO: Remove the old conversion once container runtimes are updated.
+		oldConverted := 1 + ((shares-2)*9999)/262142
+		converted := convertCPUSharesToCgroupV2Value(uint64(shares))
+		return []string{strconv.FormatInt(oldConverted, 10), strconv.FormatInt(int64(converted), 10)}
+	} else {
+		return []string{strconv.FormatInt(shares, 10)}
+	}
+}
+
+// this matches opencontainers/cgroups.ConvertCPUSharesToCgroupV2Value
+func convertCPUSharesToCgroupV2Value(cpuShares uint64) uint64 {
+	// The value of 0 means "unset".
+	if cpuShares == 0 {
+		return 0
+	}
+	if cpuShares <= 2 {
+		return 1
+	}
+	if cpuShares >= 262144 {
+		return 10000
+	}
+	l := math.Log2(float64(cpuShares))
+	// Quadratic function which fits min, max, and default.
+	exponent := (l*l+125*l)/612.0 - 7.0/34.0
+
+	return uint64(math.Ceil(math.Pow(10, exponent)))
 }
