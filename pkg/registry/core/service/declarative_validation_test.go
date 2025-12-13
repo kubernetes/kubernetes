2525f882c459cd8ae3ce2124b30b8f2cc78a8525
ahmed -k <ahmedkamal200427@gmail.com>
2025-12-13 18:16:13 +0200

    add more tests for service declarative validation

--- pkg/registry/core/service/declarative_validation_test.go
+++ pkg/registry/core/service/declarative_validation_test.go
@@ -19,11 +19,18 @@ package service
 import (
 	"testing"
 
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
 	apitesting "k8s.io/kubernetes/pkg/api/testing"
+	"k8s.io/kubernetes/pkg/apis/core"
 	api "k8s.io/kubernetes/pkg/apis/core"
 )
+
+var supportedPortProtocols = sets.New(
+	core.ProtocolTCP,
+	core.ProtocolUDP,
+	core.ProtocolSCTP)
 
 func TestDeclarativeValidateForDeclarative(t *testing.T) {
 	ctx := genericapirequest.WithRequestInfo(genericapirequest.NewDefaultContext(), &genericapirequest.RequestInfo{
@@ -35,10 +42,73 @@ func TestDeclarativeValidateForDeclarative(t *testing.T) {
 		expectedErrs field.ErrorList
 	}{
 		// baseline
-		"default resource": {
-			input: *makeValidService(),
+		"no change": {
+			input: *mkValidService(),
 		},
-		// TODO: Add more tests.
+		"invalid name": {
+			input : *mkValidService(func(svc *api.Service) {
+				svc.ObjectMeta.Name = "_invalid_name_"
+			}),
+			expectedErrs: field.ErrorList{
+				field.Invalid(field.NewPath("metadata.name"), nil, ""),
+			},
+		},
+		"empty port name": {
+			input: *mkValidService(func(svc *api.Service) {
+				svc.Spec.Ports[0].Name = ""
+			}),
+			expectedErrs: field.ErrorList{
+				field.Required(field.NewPath("spec.ports[0].name"), ""),
+			},
+		},
+		"invalid port name": {
+			input: *mkValidService(func(svc *api.Service) {
+				svc.Spec.Ports[0].Name = "port.one"
+			}),
+			expectedErrs: field.ErrorList{
+				field.Invalid(field.NewPath("spec.ports[0].name"), nil, "").WithOrigin("format=k8s-short-name"),
+			},
+		},
+		"duplicate ports names": {
+			input: *mkValidService(func(svc *api.Service) {
+				svc.Spec.Ports[0].Name = "p"
+				svc.Spec.Ports[1].Name = "p"
+			}),
+			expectedErrs: field.ErrorList{
+				field.Duplicate(field.NewPath("spec.ports[1].name"), nil),
+			},
+		},
+		"supported protocol": {
+			input: *mkValidService(func(svc *api.Service) {
+				svc.Spec.Ports[0].Protocol = "TCP"
+			}),
+		},
+		"unsupported protocol": {
+			input: *mkValidService(func(svc *api.Service) {
+				svc.Spec.Ports[0].Protocol = "HTTP"
+			}),
+			expectedErrs: field.ErrorList{
+				field.NotSupported(field.NewPath("spec.ports[0].protocol"), nil, sets.List(supportedPortProtocols)),
+			},
+		},
+		"cluster ip mismatch": {
+			input: *mkValidService(func(svc *api.Service) {
+				svc.Spec.ClusterIP = "1.2.3.5"
+			}),
+			expectedErrs: field.ErrorList{
+				field.Invalid(field.NewPath("spec.clusterIPs"), nil, ""),
+			},
+		},
+		"invalid cluster ip": {
+			input: *mkValidService(func(svc *api.Service) {
+				svc.Spec.ClusterIP = "1.2.3"
+				svc.Spec.ClusterIPs[0] = "1.2.3"
+			}),
+			expectedErrs: field.ErrorList{
+				field.Invalid(field.NewPath("spec.clusterIPs[0]"), nil, "").WithOrigin("format=ip-sloppy"),
+			},
+		},
+		// TODO: Add more comprehensive tests.
 	}
 	for k, tc := range testCases {
 		t.Run(k, func(t *testing.T) {
@@ -62,35 +132,21 @@ func TestValidateUpdateForDeclarative(t *testing.T) {
 			old:    *mkValidService(),
 			update: *mkValidService(),
 		},
-		// TODO: Add more tests.
+		"different cluster ip": {
+			old:    *mkValidService(),
+			update: *mkValidService(func (svc *api.Service)  {
+				svc.Spec.ClusterIPs[0] = "1.2.3.5"
+			}),
+			expectedErrs: field.ErrorList{
+				field.Invalid(field.NewPath("spec.clusterIPs"), nil, ""), 
+				field.Invalid(field.NewPath("spec.clusterIPs[0]"), nil, ""),
+			},
+		},
+		// TODO: Add more comprehensive tests.
 	}
 	for k, tc := range testCases {
 		t.Run(k, func(t *testing.T) {
 			apitesting.VerifyUpdateValidationEquivalence(t, ctx, &tc.update, &tc.old, Strategy.ValidateUpdate, tc.expectedErrs)
-		})
-	}
-}
-
-func TestValidateUpdateStatusForDeclarative(t *testing.T) {
-	ctx := genericapirequest.WithRequestInfo(genericapirequest.NewDefaultContext(), &genericapirequest.RequestInfo{
-		APIGroup:   "",
-		APIVersion: "v1",
-	})
-	testCases := map[string]struct {
-		old          api.Service
-		update       api.Service
-		expectedErrs field.ErrorList
-	}{
-		// baseline
-		"no change": {
-			old:    *mkValidService(),
-			update: *mkValidService(),
-		},
-		// TODO: Add more tests.
-	}
-	for k, tc := range testCases {
-		t.Run(k, func(t *testing.T) {
-			apitesting.VerifyUpdateValidationEquivalence(t, ctx, &tc.update, &tc.old, StatusStrategy.ValidateUpdate, tc.expectedErrs)
 		})
 	}
 }
