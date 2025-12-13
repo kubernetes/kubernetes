d00cc9dd22010053bdeba48ef052cba7a019a48f
HirazawaUi <695097494plus@gmail.com>
2025-12-13 23:59:21 +0800

    promote NodeLocalCRISocket to GA

--- cmd/kubeadm/app/features/features.go
+++ cmd/kubeadm/app/features/features.go
@@ -59,7 +59,7 @@ var InitFeatureGates = FeatureList{
 			" Once UserNamespacesSupport graduates to GA, kubeadm will start using it and RootlessControlPlane will be removed.",
 	},
 	ControlPlaneKubeletLocalMode: {FeatureSpec: featuregate.FeatureSpec{Default: true, PreRelease: featuregate.GA, LockToDefault: true}},
-	NodeLocalCRISocket:           {FeatureSpec: featuregate.FeatureSpec{Default: true, PreRelease: featuregate.Beta}},
+	NodeLocalCRISocket:           {FeatureSpec: featuregate.FeatureSpec{Default: true, PreRelease: featuregate.GA, LockToDefault: true}},
 }
 
 // Feature represents a feature being gated
