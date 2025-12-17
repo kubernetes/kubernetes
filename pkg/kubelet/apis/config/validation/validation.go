cf24a936b3e338fabd9a1fcd9dc54f28b003773f
Peter Hunt <pehunt@redhat.com>
2025-12-17 15:53:52 -0500

    kubelet: relax validation for image MaxAge
    There are cases where it makes sense to have a lower MaxAge than MinAge, so we should allow it
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- pkg/kubelet/apis/config/validation/validation.go
+++ pkg/kubelet/apis/config/validation/validation.go
@@ -99,9 +99,6 @@ func ValidateKubeletConfiguration(kc *kubeletconfig.KubeletConfiguration, featur
 	if kc.ImageMaximumGCAge.Duration < 0 {
 		allErrors = append(allErrors, fmt.Errorf("invalid configuration: imageMaximumGCAge %v must not be negative", kc.ImageMaximumGCAge.Duration))
 	}
-	if kc.ImageMaximumGCAge.Duration > 0 && kc.ImageMaximumGCAge.Duration <= kc.ImageMinimumGCAge.Duration {
-		allErrors = append(allErrors, fmt.Errorf("invalid configuration: imageMaximumGCAge %v must be greater than imageMinimumGCAge %v", kc.ImageMaximumGCAge.Duration, kc.ImageMinimumGCAge.Duration))
-	}
 	if utilvalidation.IsInRange(int(kc.IPTablesDropBit), 0, 31) != nil {
 		allErrors = append(allErrors, fmt.Errorf("invalid configuration: iptablesDropBit (--iptables-drop-bit) %v must be between 0 and 31, inclusive", kc.IPTablesDropBit))
 	}
