31ea9f9a66af464b8f38f663372018c3b4b18460
Saurabh Kumar Singh <singh1203.ss@gmail.com>
2025-12-23 19:52:00 +0530

    flowcontrol: migrate FlowSchema.Spec.PriorityLevelConfiguration.Name to +k8s:required
    Signed-off-by: Saurabh Kumar Singh <singh1203.ss@gmail.com>

--- pkg/registry/flowcontrol/flowschema/declarative_validation_test.go
+++ pkg/registry/flowcontrol/flowschema/declarative_validation_test.go
@@ -15,3 +15,138 @@ limitations under the License.
 */
 
 package flowschema
+
+import (
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
+	apitesting "k8s.io/kubernetes/pkg/api/testing"
+	"k8s.io/kubernetes/pkg/apis/flowcontrol"
+)
+
+func TestDeclarativeValidate(t *testing.T) {
+	apiVersions := []string{"v1", "v1beta3", "v1beta2", "v1beta1"}
+	for _, apiVersion := range apiVersions {
+		t.Run(apiVersion, func(t *testing.T) {
+			testDeclarativeValidate(t, apiVersion)
+		})
+	}
+}
+
+func testDeclarativeValidate(t *testing.T, apiVersion string) {
+	ctx := genericapirequest.WithRequestInfo(genericapirequest.NewDefaultContext(), &genericapirequest.RequestInfo{
+		APIGroup:          "flowcontrol.apiserver.k8s.io",
+		APIVersion:        apiVersion,
+		Resource:          "flowschemas",
+		IsResourceRequest: true,
+		Verb:              "create",
+	})
+
+	testCases := map[string]struct {
+		input        flowcontrol.FlowSchema
+		expectedErrs field.ErrorList
+	}{
+		"valid": {
+			input: mkValidFlowSchema(),
+		},
+		"invalid empty priorityLevelConfiguration name": {
+			input: mkValidFlowSchema(func(obj *flowcontrol.FlowSchema) {
+				obj.Spec.PriorityLevelConfiguration.Name = ""
+			}),
+			expectedErrs: field.ErrorList{
+				field.Required(field.NewPath("spec", "priorityLevelConfiguration", "name"), ""),
+			},
+		},
+	}
+	for k, tc := range testCases {
+		t.Run(k, func(t *testing.T) {
+			apitesting.VerifyValidationEquivalence(t, ctx, &tc.input, Strategy.Validate, tc.expectedErrs)
+		})
+	}
+}
+
+func TestDeclarativeValidateUpdate(t *testing.T) {
+	apiVersions := []string{"v1", "v1beta3", "v1beta2", "v1beta1"}
+	for _, apiVersion := range apiVersions {
+		t.Run(apiVersion, func(t *testing.T) {
+			testDeclarativeValidateUpdate(t, apiVersion)
+		})
+	}
+}
+
+func testDeclarativeValidateUpdate(t *testing.T, apiVersion string) {
+	testCases := map[string]struct {
+		oldObj       flowcontrol.FlowSchema
+		updateObj    flowcontrol.FlowSchema
+		expectedErrs field.ErrorList
+	}{
+		"valid update": {
+			oldObj:    mkValidFlowSchema(func(obj *flowcontrol.FlowSchema) { obj.ResourceVersion = "1" }),
+			updateObj: mkValidFlowSchema(func(obj *flowcontrol.FlowSchema) { obj.ResourceVersion = "1" }),
+		},
+		"invalid update empty priorityLevelConfiguration name": {
+			oldObj: mkValidFlowSchema(func(obj *flowcontrol.FlowSchema) { obj.ResourceVersion = "1" }),
+			updateObj: mkValidFlowSchema(func(obj *flowcontrol.FlowSchema) {
+				obj.ResourceVersion = "1"
+				obj.Spec.PriorityLevelConfiguration.Name = ""
+			}),
+			expectedErrs: field.ErrorList{
+				field.Required(field.NewPath("spec", "priorityLevelConfiguration", "name"), ""),
+			},
+		},
+	}
+	for k, tc := range testCases {
+		t.Run(k, func(t *testing.T) {
+			ctx := genericapirequest.WithRequestInfo(genericapirequest.NewDefaultContext(), &genericapirequest.RequestInfo{
+				APIPrefix:         "apis",
+				APIGroup:          "flowcontrol.apiserver.k8s.io",
+				APIVersion:        apiVersion,
+				Resource:          "flowschemas",
+				Name:              "valid-flow-schema",
+				IsResourceRequest: true,
+				Verb:              "update",
+			})
+			apitesting.VerifyUpdateValidationEquivalence(t, ctx, &tc.updateObj, &tc.oldObj, Strategy.ValidateUpdate, tc.expectedErrs)
+		})
+	}
+}
+
+func mkValidFlowSchema(tweaks ...func(obj *flowcontrol.FlowSchema)) flowcontrol.FlowSchema {
+	obj := flowcontrol.FlowSchema{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "valid-flow-schema",
+		},
+		Spec: flowcontrol.FlowSchemaSpec{
+			PriorityLevelConfiguration: flowcontrol.PriorityLevelConfigurationReference{
+				Name: "system-leader-election",
+			},
+			MatchingPrecedence: 1000,
+			Rules: []flowcontrol.PolicyRulesWithSubjects{
+				{
+					Subjects: []flowcontrol.Subject{
+						{
+							Kind: flowcontrol.SubjectKindUser,
+							User: &flowcontrol.UserSubject{
+								Name: "system:kube-controller-manager",
+							},
+						},
+					},
+					ResourceRules: []flowcontrol.ResourcePolicyRule{
+						{
+							Verbs:      []string{"*"},
+							APIGroups:  []string{"*"},
+							Resources:  []string{"*"},
+							Namespaces: []string{"*"},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tweak := range tweaks {
+		tweak(&obj)
+	}
+	return obj
+}
