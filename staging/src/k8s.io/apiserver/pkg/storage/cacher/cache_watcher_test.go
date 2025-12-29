kubernetes/apiserver

Move dummy testing to subpackage
Change-Id: I52863cf256fc52b863c182932eb9520f3626adc3

Kubernetes-commit: f42b1db39aa772bcf47773ae9208ba41634ffc9e

pkg/storage/cacher/cache_watcher_test.go
@@ -36,6 +36,8 @@ import (
 	utilflowcontrol "k8s.io/apiserver/pkg/util/flowcontrol"
 	"k8s.io/client-go/tools/cache"
 	testingclock "k8s.io/utils/clock/testing"
+
+	cachertesting "k8s.io/apiserver/pkg/storage/cacher/testing"
 )
 
 // verifies the cacheWatcher.process goroutine is properly cleaned up even if
@@ -246,7 +248,7 @@ func TestCacheWatcherStoppedInAnotherGoroutine(t *testing.T) {
 }
 
 func TestCacheWatcherStoppedOnDestroy(t *testing.T) {
-	backingStorage := &dummyStorage{}
+	backingStorage := &cachertesting.MockStorage{}
 	cacher, _, err := newTestCacher(backingStorage)
 	if err != nil {
 		t.Fatalf("Couldn't create cacher: %v", err)

pkg/storage/cacher/lister_watcher_test.go
@@ -32,12 +32,14 @@ import (
 	"k8s.io/client-go/util/watchlist"
 	featuregatetesting "k8s.io/component-base/featuregate/testing"
 	"k8s.io/utils/ptr"
+
+	cachertesting "k8s.io/apiserver/pkg/storage/cacher/testing"
 )
 
 func TestDoesClientSupportWatchListSemanticsForKubeClient(t *testing.T) {
-	target1 := &dummyStorage{}
+	target1 := &cachertesting.MockStorage{}
 	if !watchlist.DoesClientNotSupportWatchListSemantics(target1) {
-		t.Fatalf("dummyStorage should NOT support WatchList semantics")
+		t.Fatalf("cachertesting.MockStorage should NOT support WatchList semantics")
 	}
 
 	server, target2 := newEtcdTestStorage(t, "/pods/")
@@ -274,9 +276,9 @@ func TestListerWatcherListResourceVersionPropagation(t *testing.T) {
 		t.Run(scenario.name, func(t *testing.T) {
 			featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.WatchList, scenario.watchListEnabled)
 
-			backingStorage := &dummyStorage{}
+			backingStorage := &cachertesting.MockStorage{}
 			var capturedOpts storage.ListOptions
-			backingStorage.getListFn = func(ctx context.Context, resPrefix string, opts storage.ListOptions, listObj runtime.Object) error {
+			backingStorage.GetListFn = func(ctx context.Context, resPrefix string, opts storage.ListOptions, listObj runtime.Object) error {
 				capturedOpts = opts
 				return nil
 			}

kubernetes/kubernetes

staging/src/k8s.io/apiserver/pkg/storage/cacher/cache_watcher_test.go
@@ -36,6 +36,8 @@ import (
 	utilflowcontrol "k8s.io/apiserver/pkg/util/flowcontrol"
 	"k8s.io/client-go/tools/cache"
 	testingclock "k8s.io/utils/clock/testing"
+
+	cachertesting "k8s.io/apiserver/pkg/storage/cacher/testing"
 )
 
 // verifies the cacheWatcher.process goroutine is properly cleaned up even if
@@ -246,7 +248,7 @@ func TestCacheWatcherStoppedInAnotherGoroutine(t *testing.T) {
 }
 
 func TestCacheWatcherStoppedOnDestroy(t *testing.T) {
-	backingStorage := &dummyStorage{}
+	backingStorage := &cachertesting.DummyStorage{}
 	cacher, _, err := newTestCacher(backingStorage)
 	if err != nil {
 		t.Fatalf("Couldn't create cacher: %v", err)

staging/src/k8s.io/apiserver/pkg/storage/cacher/lister_watcher_test.go
@@ -32,12 +32,14 @@ import (
 	"k8s.io/client-go/util/watchlist"
 	featuregatetesting "k8s.io/component-base/featuregate/testing"
 	"k8s.io/utils/ptr"
+
+	cachertesting "k8s.io/apiserver/pkg/storage/cacher/testing"
 )
 
 func TestDoesClientSupportWatchListSemanticsForKubeClient(t *testing.T) {
-	target1 := &dummyStorage{}
+	target1 := &cachertesting.DummyStorage{}
 	if !watchlist.DoesClientNotSupportWatchListSemantics(target1) {
-		t.Fatalf("dummyStorage should NOT support WatchList semantics")
+		t.Fatalf("cachertesting.DummyStorage should NOT support WatchList semantics")
 	}
 
 	server, target2 := newEtcdTestStorage(t, "/pods/")
@@ -274,9 +276,9 @@ func TestListerWatcherListResourceVersionPropagation(t *testing.T) {
 		t.Run(scenario.name, func(t *testing.T) {
 			featuregatetesting.SetFeatureGateDuringTest(t, utilfeature.DefaultFeatureGate, features.WatchList, scenario.watchListEnabled)
 
-			backingStorage := &dummyStorage{}
+			backingStorage := &cachertesting.DummyStorage{}
 			var capturedOpts storage.ListOptions
-			backingStorage.getListFn = func(ctx context.Context, resPrefix string, opts storage.ListOptions, listObj runtime.Object) error {
+			backingStorage.GetListFn = func(ctx context.Context, resPrefix string, opts storage.ListOptions, listObj runtime.Object) error {
 				capturedOpts = opts
 				return nil
 			}