f9a22d2a5967c4fc18794453cde921909d96885f
xin.li <xin.li@daocloud.io>
2026-01-08 09:59:30 +0800

    Update CoreDNS to v1.14.0
    Signed-off-by: xin.li <xin.li@daocloud.io>

--- cmd/kubeadm/app/constants/constants.go
+++ cmd/kubeadm/app/constants/constants.go
@@ -364,7 +364,7 @@ const (
 	CoreDNSImageName = "coredns"
 
 	// CoreDNSVersion is the version of CoreDNS to be deployed if it is used
-	CoreDNSVersion = "v1.13.1"
+	CoreDNSVersion = "v1.14.0"
 
 	// ClusterConfigurationKind is the string kind value for the ClusterConfiguration struct
 	ClusterConfigurationKind = "ClusterConfiguration"
