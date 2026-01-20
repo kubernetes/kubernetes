ccb70a6577d7c79391e1d1a25335bb87ffff33b8
Francesco Romani <fromani@redhat.com>
2026-01-20 11:25:51 +0100

    kubelet: deprecate DisableCPUQuotaWithExclusiveCPUs
    We missed the proper process initially, it should have
    been set to deprecated from the beginning, to convey
    this is a safety FG for a delicate, long needed fix
    rather than a FG for a proper feature
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1246,6 +1246,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	DisableCPUQuotaWithExclusiveCPUs: {
 		{Version: version.MustParse("1.33"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.Deprecated}, // LockToDefault(true) in 1.37, remove in 1.38
 	},
 
 	DisableNodeKubeProxyVersion: {
