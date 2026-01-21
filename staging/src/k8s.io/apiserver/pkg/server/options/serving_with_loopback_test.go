b7494e787f81b9eb84a94d269de090e7158ed027
Bryce Palmer <bpalmer@redhat.com>
2026-01-21 15:48:42 -0500

    bugfix: add loopback certificate expiration health check
    so that configured liveness probes will fail
    if the loopback certificate has expired,
    forcing a restart.
    
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

--- staging/src/k8s.io/apiserver/pkg/server/options/serving_with_loopback_test.go
+++ staging/src/k8s.io/apiserver/pkg/server/options/serving_with_loopback_test.go
@@ -17,11 +17,17 @@ limitations under the License.
 package options
 
 import (
+	"errors"
 	"net"
 	"testing"
+	"time"
 
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apiserver/pkg/server"
+	"k8s.io/apiserver/pkg/server/healthz"
 	"k8s.io/client-go/rest"
+	clocktesting "k8s.io/utils/clock/testing"
 	netutils "k8s.io/utils/net"
 )
 
@@ -48,5 +54,75 @@ func TestEmptyMainCert(t *testing.T) {
 	}
 	if e, a := 1, len(secureServingInfo.SNICerts); e != a {
 		t.Errorf("expected %d SNICert, got %d", e, a)
+	}
+}
+
+func TestAddSecureServingHealthEndpoint(t *testing.T) {
+	type testcase struct {
+		name        string
+		expired     bool
+	}
+
+	testcases := []testcase{
+		{
+			name: "current time not after certificate expiry",
+		},
+		{
+			name:        "current time after certificate expiry",
+			expired:     true,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := server.NewConfig(serializer.NewCodecFactory(runtime.NewScheme()))
+
+			ssowlb := (&SecureServingOptions{
+				BindAddress: netutils.ParseIPSloppy("127.0.0.1"),
+			}).WithLoopback()
+			now := time.Date(2026, time.January, 1, 0, 0, 0, 0, time.Local)
+			ssowlb.clock = clocktesting.NewFakeClock(now)
+
+			ln, err := net.Listen("tcp", "127.0.0.1:0")
+			if err != nil {
+				t.Fatalf("failed to listen on 127.0.0.1:0")
+			}
+			defer ln.Close()
+			ssowlb.Listener = ln
+			ssowlb.BindPort = ln.Addr().(*net.TCPAddr).Port
+
+			if err := ssowlb.ApplyWithServerConfigTo(cfg); err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			if tc.expired {
+				ssowlb.clock = clocktesting.NewFakeClock(now.Add(maxAge * 2))
+			}
+
+			var foundChecker healthz.HealthChecker
+
+			for _, livezCheck := range cfg.LivezChecks {
+				if livezCheck.Name() == "secure-serving" {
+					foundChecker = livezCheck
+					break
+				}
+			}
+
+			if foundChecker == nil {
+				t.Fatal("secure-serving health checker not found")
+			}
+
+			err = foundChecker.Check(nil)
+			if tc.expired {
+				if !errors.Is(err, LoopbackCertificateExpiredError{}) {
+					t.Errorf("expected error and received error do not match. expected: %v , received: %v", LoopbackCertificateExpiredError{}, err)
+				}
+				return
+			}
+
+			if err != nil {
+				t.Errorf("received an unexpected error: %v", err)
+			}
+		})
 	}
 }
