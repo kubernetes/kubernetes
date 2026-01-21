1b338eec1c67be9d86cc844715e7382ba5a4cbe7
Adrian Moisey <adrian@changeover.za.net>
2026-01-21 15:14:37 +0200

    Promote KEP-5311 (Relaxed validation for Services names) to beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1683,6 +1683,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	RelaxedServiceNameValidation: {
 		{Version: version.MustParse("1.34"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	ReloadKubeletServerCertificateFile: {
