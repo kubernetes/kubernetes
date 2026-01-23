b7355510ef13630fdf016f8c40cc412468402906
Rohit Chaudhari <100275369+rohitkbc@users.noreply.github.com>
2026-01-24 01:28:47 +0530

    Fix copyright notice in container_manager_test.go
    Correct copyright year in container_manager_test.go

--- pkg/kubelet/cm/container_manager_test.go
+++ pkg/kubelet/cm/container_manager_test.go
@@ -1,7 +1,7 @@
 //go:build linux
 
 /*
-Copyright 2026 The Kubernetes Authors.
+Copyright The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
