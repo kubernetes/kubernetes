62487bd645c8d12f45587f72a3aeb2033c18bd56
Maria Romano <mariafromano@google.com>
2026-01-26 19:53:36 +0000

    updated kube_feature.go

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1419,6 +1419,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	KubeletPSI: {
 		{Version: version.MustParse("1.33"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.34"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.GA, LockToDefault: true},
 	},
 
 	KubeletPodResourcesDynamicResources: {
