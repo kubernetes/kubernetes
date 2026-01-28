f6edf23ba4476f8b3dfef2a6cba6ff54fa4ebb1e
Daniel Maizel <dani98000k@gmail.com>
2026-01-28 16:07:25 +0200

    fix: handle nil ResourceList in max() to prevent panic

--- staging/src/k8s.io/kubectl/pkg/util/resource/resource.go
+++ staging/src/k8s.io/kubectl/pkg/util/resource/resource.go
@@ -152,7 +152,12 @@ func determineContainerReqs(pod *corev1.Pod, container *corev1.Container, cs *co
 // max returns the result of max(a, b...) for each named resource and is only used if we can't
 // accumulate into an existing resource list
 func max(a corev1.ResourceList, b ...corev1.ResourceList) corev1.ResourceList {
-	result := a.DeepCopy()
+	var result corev1.ResourceList
+	if a != nil {
+		result = a.DeepCopy()
+	} else {
+		result = corev1.ResourceList{}
+	}
 	for _, other := range b {
 		maxResourceList(result, other)
 	}
