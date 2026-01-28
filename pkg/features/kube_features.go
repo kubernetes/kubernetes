e2484621fa2e7e796f0bca245d999dde726231c4
Anish Ramasekar <anish.ramasekar@gmail.com>
2026-01-28 12:58:45 -0600

    Mark CSIServiceAccountTokenSecrets feature gate as GA
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1151,6 +1151,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	CSIServiceAccountTokenSecrets: {
 		{Version: version.MustParse("1.35"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // GA in 1.36; remove in 1.39
 	},
 
 	CSIVolumeHealth: {
