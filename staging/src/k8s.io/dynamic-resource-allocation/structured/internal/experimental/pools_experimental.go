790093d873e7211bac9b495acf0906f08e2aa4c3
Troy Chiu <troychiu@google.com>
2026-01-30 01:59:37 +0000

    sort slices and devices lexicographically in experimental allocator

--- staging/src/k8s.io/dynamic-resource-allocation/structured/internal/experimental/pools_experimental.go
+++ staging/src/k8s.io/dynamic-resource-allocation/structured/internal/experimental/pools_experimental.go
@@ -17,6 +17,7 @@ limitations under the License.
 package experimental
 
 import (
+	"cmp"
 	"context"
 	"fmt"
 	"slices"
@@ -230,11 +231,11 @@ func addSlice(pools map[PoolID][]*draapi.ResourceSlice, s *resourceapi.ResourceS
 	return nil
 }
 
-func buildPool(id PoolID, slices []*draapi.ResourceSlice, features Features, allSlicesForPool []*resourceapi.ResourceSlice) (*Pool, error) {
+func buildPool(id PoolID, resourceSlices []*draapi.ResourceSlice, features Features, allSlicesForPool []*resourceapi.ResourceSlice) (*Pool, error) {
 	var deviceSlices []*draapi.ResourceSlice
 	var counterSetSlices []*draapi.ResourceSlice
 	if features.PartitionableDevices {
-		for _, slice := range slices {
+		for _, slice := range resourceSlices {
 			if len(slice.Spec.SharedCounters) > 0 {
 				counterSetSlices = append(counterSetSlices, slice)
 			} else {
@@ -242,8 +243,22 @@ func buildPool(id PoolID, slices []*draapi.ResourceSlice, features Features, all
 			}
 		}
 	} else {
-		deviceSlices = slices
+		deviceSlices = resourceSlices
 	}
+
+	// Sort slices and the devices within them by name to ensure a deterministic
+	// allocation order regardless of the order in which the DRA driver provides them.
+	// Because the allocator uses a first-fit search, this allows driver authors
+	// to influence prioritization through their naming conventions.
+	slices.SortFunc(deviceSlices, func(a, b *draapi.ResourceSlice) int {
+		return cmp.Compare(a.Name, b.Name)
+	})
+	for _, slice := range deviceSlices {
+		slices.SortFunc(slice.Spec.Devices, func(a, b draapi.Device) int {
+			return cmp.Compare(a.Name.String(), b.Name.String())
+		})
+	}
+
 	if err := validateDeviceNames(deviceSlices); err != nil {
 		return &Pool{
 			PoolID:        id,
@@ -269,7 +284,7 @@ func buildPool(id PoolID, slices []*draapi.ResourceSlice, features Features, all
 		}, nil
 	}
 
-	if err := validateDeviceCounterConsumption(counterSets, slices); err != nil {
+	if err := validateDeviceCounterConsumption(counterSets, resourceSlices); err != nil {
 		return &Pool{
 			PoolID:        id,
 			IsInvalid:     true,
@@ -278,7 +293,7 @@ func buildPool(id PoolID, slices []*draapi.ResourceSlice, features Features, all
 	}
 	// If we have already seen all slices (both with counter sets and devices),
 	// we don't need to do any more validation.
-	if allSlicesForPool == nil || len(slices) == len(allSlicesForPool) {
+	if allSlicesForPool == nil || len(resourceSlices) == len(allSlicesForPool) {
 		return &Pool{
 			PoolID:                    id,
 			DeviceSlicesTargetingNode: deviceSlices,
@@ -293,7 +308,7 @@ func buildPool(id PoolID, slices []*draapi.ResourceSlice, features Features, all
 	// We only want to convert the slices we haven't already converted, so make it easy to
 	// look up the names of converted slices.
 	slicesTargetingNodeNames := sets.New[string]()
-	for _, slice := range slices {
+	for _, slice := range resourceSlices {
 		slicesTargetingNodeNames.Insert(slice.Name)
 	}
 	var slicesNotTargetingNode []*draapi.ResourceSlice
