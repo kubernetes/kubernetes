a55ce884d37cd2dcabf5ca27618ee3e5b48da4ef
Damien Grisonnet <dgrisonn@redhat.com>
2026-02-03 15:17:56 +0100

    chore: cleanup deprecated metrics
    Signed-off-by: Damien Grisonnet <dgrisonn@redhat.com>

--- pkg/registry/core/pod/rest/metrics.go
+++ pkg/registry/core/pod/rest/metrics.go
@@ -44,19 +44,6 @@ var (
 		[]string{"usage"},
 	)
 
-	// deprecatedPodLogsUsage counts and categorizes how the insecure backend skip TLS option is used and allowed.
-	deprecatedPodLogsUsage = metrics.NewCounterVec(
-		&metrics.CounterOpts{
-			Namespace:         namespace,
-			Subsystem:         subsystem,
-			Name:              "pods_logs_insecure_backend_total",
-			Help:              "Total number of requests for pods/logs sliced by usage type: enforce_tls, skip_tls_allowed, skip_tls_denied",
-			StabilityLevel:    metrics.ALPHA,
-			DeprecatedVersion: "1.27.0",
-		},
-		[]string{"usage"},
-	)
-
 	// podLogsTLSFailure counts how many attempts to get pod logs fail on tls verification
 	podLogsTLSFailure = metrics.NewCounter(
 		&metrics.CounterOpts{
@@ -67,18 +54,6 @@ var (
 			StabilityLevel: metrics.ALPHA,
 		},
 	)
-
-	// deprecatedPodLogsTLSFailure counts how many attempts to get pod logs fail on tls verification
-	deprecatedPodLogsTLSFailure = metrics.NewCounter(
-		&metrics.CounterOpts{
-			Namespace:         namespace,
-			Subsystem:         subsystem,
-			Name:              "pods_logs_backend_tls_failure_total",
-			Help:              "Total number of requests for pods/logs that failed due to kubelet server TLS verification",
-			StabilityLevel:    metrics.ALPHA,
-			DeprecatedVersion: "1.27.0",
-		},
-	)
 )
 
 var registerMetricsOnce sync.Once
@@ -87,7 +62,5 @@ func registerMetrics() {
 	registerMetricsOnce.Do(func() {
 		legacyregistry.MustRegister(podLogsUsage)
 		legacyregistry.MustRegister(podLogsTLSFailure)
-		legacyregistry.MustRegister(deprecatedPodLogsUsage)
-		legacyregistry.MustRegister(deprecatedPodLogsTLSFailure)
 	})
 }
