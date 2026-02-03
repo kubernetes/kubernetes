768627b32c1f1f90540f2d7258562d17a455c9cd
hahahaheihei <1475660997@qq.com>
2026-02-03 15:38:49 +0800

    feat: set CPUIdleForBestEffortQoS default value to true

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1181,7 +1181,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 
 	CPUIdleForBestEffortQoS: {
-		{Version: version.MustParse("1.36"), Default: false, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	ClusterTrustBundleProjection: {
