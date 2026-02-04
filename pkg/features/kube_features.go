900b546083c4dd24a594c32fb1812659794dd725
Swati Gupta <swatig@nvidia.com>
2026-02-04 11:47:38 -0800

    FG Lock to default in 1.36
    Signed-off-by: Swati Gupta <swatig@nvidia.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1426,13 +1426,13 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	KubeletPodResourcesDynamicResources: {
 		{Version: version.MustParse("1.27"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.34"), Default: true, PreRelease: featuregate.Beta},
-		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.GA}, // lock to default in 1.38
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 1.37
 	},
 
 	KubeletPodResourcesGet: {
 		{Version: version.MustParse("1.27"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.34"), Default: true, PreRelease: featuregate.Beta},
-		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.GA}, // lock to default in 1.38
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 1.37
 	},
 
 	KubeletPodResourcesListUseActivePods: {
