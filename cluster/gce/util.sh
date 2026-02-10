e868db24e7badab4c74404fb8e1feb2827989c58
Tomasz Lenarcik <tlenarcik@google.com>
2026-02-10 12:10:27 +0000

    Skip legacy version check
    The original check was supposed to gate against a legacy version that
    would be almost 10 years old as of today.

--- cluster/gce/util.sh
+++ cluster/gce/util.sh
@@ -2144,18 +2144,14 @@ function update-or-verify-gcloud() {
     version=$(gcloud version --format=json)
     python3 -c"
 import json,sys
-from packaging import version
 
-minVersion = version.parse('1.3.0')
 required = [ 'alpha', 'beta', 'core' ]
 data = json.loads(sys.argv[1])
 rel = data.get('Google Cloud SDK')
 if 'CL @' in rel:
   print('Using dev version of gcloud: %s' %rel)
   exit(0)
-if rel != 'HEAD' and version.parse(rel) < minVersion:
-  print('gcloud version out of date ( < %s )' % minVersion)
-  exit(1)
+
 missing = []
 for c in required:
   if not data.get(c):
