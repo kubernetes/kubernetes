2dadc5c7bdf353f9ee74287d164fe45d6f4e08a3
Troy Chiu <troychiu@google.com>
2026-02-10 22:04:59 +0000

    promote DRAPrioritizedList to GA

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1223,6 +1223,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	DRAPrioritizedList: {
 		{Version: version.MustParse("1.33"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.34"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.GA},
 	},
 
 	DRAResourceClaimDeviceStatus: {
