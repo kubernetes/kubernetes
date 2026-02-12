642f4798462feb7cb4c7045fde103bca1eca8e23
Sai Ramesh Vanka <v.sairamesh1@gmail.com>
2026-02-12 20:29:02 +0530

    Add upgrade/downgrade e2e tests for the DRAExtendedResources feature
    - Implement e2e tests based on the scenarios described in the PR
      kubernetes/enhancements#5751 that are required to graduate the DRAExtendedResources feature to Beta
    
    Signed-off-by: Sai Ramesh Vanka <svanka@redhat.com>

--- test/e2e_dra/upgradedowngrade_test.go
+++ test/e2e_dra/upgradedowngrade_test.go
@@ -76,6 +76,7 @@ var subTests = map[string]initialTestFunc{
 	"core DRA":                    coreDRA,
 	"ResourceClaim device status": resourceClaimDeviceStatus,
 	"DeviceTaints":                deviceTaints,
+	"DRA extended resources":      extendedResourcesUpgradeDowngrade,
 }
 
 type initialTestFunc func(tCtx ktesting.TContext, builder *drautils.Builder) upgradedTestFunc
@@ -213,7 +214,7 @@ func testUpgradeDowngrade(tCtx ktesting.TContext) {
 		cluster = localupcluster.New(tCtx)
 		localUpClusterEnv := map[string]string{
 			"RUNTIME_CONFIG": "resource.k8s.io/v1beta1,resource.k8s.io/v1beta2,resource.k8s.io/v1alpha3",
-			"FEATURE_GATES":  "DynamicResourceAllocation=true,DRADeviceTaintRules=true,DRADeviceTaints=true",
+			"FEATURE_GATES":  "DynamicResourceAllocation=true,DRADeviceTaintRules=true,DRADeviceTaints=true,DRAExtendedResource=true",
 			// *not* needed because driver will run in "local filesystem" mode (= driver.IsLocal): "ALLOW_PRIVILEGED": "1",
 		}
 		cluster.Start(tCtx, binDir, localUpClusterEnv)
