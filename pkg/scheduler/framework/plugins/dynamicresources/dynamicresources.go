124a3662567444eeac468a6ce1dace221cbce4c2
Nour <nurmn3m@gmail.com>
2026-02-13 17:04:33 +0200

    scheduler: use contextual logging for event emission
    Signed-off-by: Nour <nurmn3m@gmail.com>

--- pkg/scheduler/framework/plugins/dynamicresources/dynamicresources.go
+++ pkg/scheduler/framework/plugins/dynamicresources/dynamicresources.go
@@ -1094,7 +1094,7 @@ func (pl *DynamicResources) PreBind(ctx context.Context, cs fwk.CycleState, pod 
 	}
 
 	// We need to wait for the device to be attached to the node.
-	pl.fh.EventRecorder().Eventf(pod, nil, v1.EventTypeNormal, "BindingConditionsPending", "Scheduling", "waiting for binding conditions for device on node %s", nodeName)
+	pl.fh.EventRecorder().WithLogger(logger).Eventf(pod, nil, v1.EventTypeNormal, "BindingConditionsPending", "Scheduling", "waiting for binding conditions for device on node %s", nodeName)
 	err = wait.PollUntilContextTimeout(ctx, 5*time.Second, pl.bindingTimeout, true,
 		func(ctx context.Context) (bool, error) {
 			return pl.isPodReadyForBinding(state)
