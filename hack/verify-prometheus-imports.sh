be4757ba89a6af1da0881b7fd702e40abcd0e754
ChengHao Yang <17496418+tico88612@users.noreply.github.com>
2026-02-28 01:13:42 +0800

    Remove dep. Prometheus from test/integration/apiserver/flowcontrol
    Signed-off-by: ChengHao Yang <17496418+tico88612@users.noreply.github.com>

--- hack/verify-prometheus-imports.sh
+++ hack/verify-prometheus-imports.sh
@@ -71,8 +71,6 @@ allowed_prometheus_importers=(
   ./staging/src/k8s.io/component-base/metrics/wrappers.go
   ./test/e2e/node/pods.go
   ./test/instrumentation/main_test.go
-  ./test/integration/apiserver/flowcontrol/concurrency_test.go
-  ./test/integration/apiserver/flowcontrol/concurrency_util_test.go
   ./test/integration/metrics/metrics_test.go
 )
 
