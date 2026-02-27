51e7be953179e19e50e4587518dc70da1e4af6ea
Tim Allclair <tallclair@google.com>
2026-02-27 22:40:20 +0000

    Per-pod cache refresh timestamp

--- pkg/kubelet/container/cache.go
+++ pkg/kubelet/container/cache.go
@@ -40,6 +40,10 @@ type Cache interface {
 	// if the data is newer than the cache based on the provided
 	// time stamp. Returns if the cache was updated.
 	Set(types.UID, *PodStatus, error, time.Time) (updated bool)
+	// SetObservedTime modifies the observed timestamp of a pod in the cache.
+	// This indicates that the pod's state was recently confirmed to be accurate
+	// by the runtime, even if its status wasn't modified.
+	SetObservedTime(types.UID, time.Time)
 	Delete(types.UID)
 	UpdateTime(time.Time)
 }
@@ -61,6 +65,10 @@ type data struct {
 	err error
 	// Time when the data was last modified.
 	modified time.Time
+	// Time when the data was last individually observed/verified by the runtime.
+	// The actual observed time should be the more recent of observedTime and the global cache timestamp.
+	// This is updated when the pod is relisted but its status hasn't changed.
+	observedTime time.Time
 }
 
 type subRecord struct {
@@ -116,9 +124,21 @@ func (c *cache) Set(id types.UID, status *PodStatus, err error, timestamp time.T
 		}
 	}
 
-	c.pods[id] = &data{status: status, err: err, modified: timestamp}
+	c.pods[id] = &data{status: status, err: err, modified: timestamp, observedTime: timestamp}
 	c.notify(id, timestamp)
 	return true
+}
+
+// SetObservedTime modifies the observed timestamp of a pod in the cache and
+// notify subscribers if needed.
+func (c *cache) SetObservedTime(id types.UID, timestamp time.Time) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if d, ok := c.pods[id]; ok {
+		d.observedTime = timestamp
+	}
+	// Notify all the subscribers if the condition is met.
+	c.notify(id, timestamp)
 }
 
 // Delete removes the entry of the pod.
@@ -136,7 +156,10 @@ func (c *cache) UpdateTime(timestamp time.Time) {
 	c.timestamp = &timestamp
 	// Notify all the subscribers if the condition is met.
 	for id := range c.subscribers {
-		c.notify(id, *c.timestamp)
+		// The global timestamp is only used for pods that are missing from the cache.
+		if _, ok := c.pods[id]; !ok {
+			c.notify(id, *c.timestamp)
+		}
 	}
 }
 
@@ -168,9 +191,10 @@ func (c *cache) getIfNewerThan(id types.UID, minTime time.Time) *data {
 		// minTime, return the default status.
 		return makeDefaultData(id)
 	}
-	if ok && (d.modified.After(minTime) || globalTimestampIsNewer) {
+	if ok && (globalTimestampIsNewer || d.modified.After(minTime) || d.observedTime.After(minTime)) {
 		// Status is cached, return status if either of the following is true.
 		//   * status was modified after minTime
+		//   * status was observed after minTime
 		//   * the global timestamp of the cache is newer than minTime.
 		return d
 	}
