ef742006915022af22a0b1238c2028a373fffafc
Gavin Lam <gavin.oss@tutamail.com>
2026-03-03 22:20:13 -0500

    Simulate featuregate in beta and default enabled to run e2e tests

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1135,7 +1135,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 
 	AtomicWriteVolumeUserFields: {
-		{Version: version.MustParse("1.37"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	AuthorizeNodeWithSelectors: {
