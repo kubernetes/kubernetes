0a849dd76fe54a91ffbbf12674318ef23a4fd706
HirazawaUi <695097494plus@gmail.com>
2026-03-04 23:52:43 +0800

    add feature gate

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -164,6 +164,12 @@ const (
 	// Enables configuring custom stop signals for containers from container lifecycle
 	ContainerStopSignals featuregate.Feature = "ContainerStopSignals"
 
+	// owner: @HirazawaUi
+	// kep: https://kep.k8s.io/5758
+	//
+	// Enable support for configuring Ulimit for containers
+	ContainerUlimits featuregate.Feature = "ContainerUlimits"
+
 	// owner: @jefftree
 	// kep: https://kep.k8s.io/4355
 	//
@@ -1280,6 +1286,10 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 		{Version: version.MustParse("1.33"), Default: false, PreRelease: featuregate.Alpha},
 	},
 
+	ContainerUlimits: {
+		{Version: version.MustParse("1.37"), Default: false, PreRelease: featuregate.Alpha},
+	},
+
 	CrossNamespaceVolumeDataSource: {
 		{Version: version.MustParse("1.26"), Default: false, PreRelease: featuregate.Alpha},
 	},
@@ -2278,6 +2288,8 @@ var defaultKubernetesFeatureGateDependencies = map[featuregate.Feature][]feature
 	ContainerRestartRules: {},
 
 	ContainerStopSignals: {},
+
+	ContainerUlimits: {NodeDeclaredFeatures},
 
 	CoordinatedLeaderElection: {},
 
