e78f8511775cd040a9cf587bcf8d8dbe51cfd43c
Benjamin Elder <bentheelder@google.com>
2026-03-06 15:40:21 -0800

    split linux and portable deviceattribute unit tests

--- staging/src/k8s.io/dynamic-resource-allocation/deviceattribute/machine_linux_test.go
+++ staging/src/k8s.io/dynamic-resource-allocation/deviceattribute/machine_linux_test.go
@@ -18,33 +18,23 @@ package deviceattribute
 
 import (
 	"io/fs"
+	"path/filepath"
 	"testing"
-	"testing/fstest"
 )
 
-func TestWithFS(t *testing.T) {
+func TestWithFSFromRoot(t *testing.T) {
 	mc := machine{}
-	path := "dra.test"
-	content := "DRA deviceattribute"
-	mode := fs.FileMode(0o640) // any random mode pattern which is not the default
-	fakefs := fstest.MapFS{
-		path: &fstest.MapFile{
-			Data: []byte(content),
-			Mode: mode,
-		},
-	}
-
-	mod := WithFS(fakefs)
+	// we are using real files from sysfs pseudofs, but we are using files which
+	// are standard since pretty much the inception of sysfs
+	mod := WithFSFromRoot("/sys")
 	mod(&mc)
 
-	got, err := mc.sysfs.Lstat(path)
+	srcPath := filepath.Join("devices", "system", "cpu", "online")
+	got, err := fs.ReadFile(mc.sysfs, srcPath)
 	if err != nil {
-		t.Fatalf("cannot Lstat %q: %v", path, err)
+		t.Fatalf("cannot read %q: %v", srcPath, err)
 	}
-	if got.Size() != int64(len(content)) {
-		t.Fatalf("did not found content: got %v expected %v", got.Size(), content)
-	}
-	if got.Mode() != mode {
-		t.Fatalf("found mismatched file mode: got %v expected %v", got.Mode(), mode)
+	if len(got) == 0 {
+		t.Fatalf("empty content in %q", srcPath)
 	}
 }
