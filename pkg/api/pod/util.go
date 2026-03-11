3016d69e646fcaa81c484040eb1649c5e53111b5
Amritansh Amritansh <aamritan@redhat.com>
2026-03-11 22:02:25 +0530

    Add feature gate drop logic for gRPC TLS

--- pkg/api/pod/util.go
+++ pkg/api/pod/util.go
@@ -432,6 +432,7 @@ func GetValidationOptionsFromPodSpecAndMeta(podSpec, oldPodSpec *api.PodSpec, po
 		// This also allows restart rules on sidecar containers.
 		AllowRestartAllContainers:  utilfeature.DefaultFeatureGate.Enabled(features.RestartAllContainersOnContainerExits),
 		AllowImageVolumeWithDigest: utilfeature.DefaultFeatureGate.Enabled(features.ImageVolumeWithDigest),
+		AllowGRPCContainerProbeTLS: utilfeature.DefaultFeatureGate.Enabled(features.GRPCContainerProbeTLS),
 	}
 
 	// If old spec uses relaxed validation or enabled the RelaxedEnvironmentVariableValidation feature gate,
@@ -481,6 +482,7 @@ func GetValidationOptionsFromPodSpecAndMeta(podSpec, oldPodSpec *api.PodSpec, po
 
 		opts.AllowContainerRestartPolicyRules = opts.AllowContainerRestartPolicyRules || containerRestartRulesInUse(oldPodSpec)
 		opts.AllowRestartAllContainers = opts.AllowRestartAllContainers || restartAllContainersActionInUse(oldPodSpec)
+		opts.AllowGRPCContainerProbeTLS = opts.AllowGRPCContainerProbeTLS || grpcProbeTLSInUse(oldPodSpec)
 
 		// If old spec has userns and volume devices (doesn't work), we still allow
 		// modifications to it.
@@ -756,6 +758,7 @@ func dropDisabledFields(
 	dropDisabledClusterTrustBundleProjection(podSpec, oldPodSpec)
 	dropDisabledPodCertificateProjection(podSpec, oldPodSpec)
 	dropDisabledSchedulingGroup(podSpec, oldPodSpec)
+	dropDisabledGRPCContainerProbeTLS(podSpec, oldPodSpec)
 
 	if !utilfeature.DefaultFeatureGate.Enabled(features.InPlacePodVerticalScaling) && !inPlacePodVerticalScalingInUse(oldPodSpec) {
 		// Drop ResizePolicy fields. Don't drop updates to Resources field as template.spec.resources
@@ -999,6 +1002,43 @@ func podLifecycleSleepActionZeroValueInUse(podSpec *api.PodSpec) bool {
 		return true
 	})
 	return inUse
+}
+
+func grpcProbeTLSInUse(podSpec *api.PodSpec) bool {
+	if podSpec == nil {
+		return false
+	}
+	var inUse bool
+	VisitContainers(podSpec, AllContainers, func(c *api.Container, _ ContainerType) bool {
+		if probeUsesGRPCTLS(c.LivenessProbe) || probeUsesGRPCTLS(c.ReadinessProbe) || probeUsesGRPCTLS(c.StartupProbe) {
+			inUse = true
+			return false
+		}
+		return true
+	})
+	return inUse
+}
+
+func probeUsesGRPCTLS(probe *api.Probe) bool {
+	return probe != nil && probe.GRPC != nil && probe.GRPC.TLS != nil
+}
+
+func dropDisabledGRPCContainerProbeTLS(podSpec, oldPodSpec *api.PodSpec) {
+	if utilfeature.DefaultFeatureGate.Enabled(features.GRPCContainerProbeTLS) || grpcProbeTLSInUse(oldPodSpec) {
+		return
+	}
+	VisitContainers(podSpec, AllContainers, func(c *api.Container, _ ContainerType) bool {
+		dropProbeGRPCTLS(c.LivenessProbe)
+		dropProbeGRPCTLS(c.ReadinessProbe)
+		dropProbeGRPCTLS(c.StartupProbe)
+		return true
+	})
+}
+
+func dropProbeGRPCTLS(p *api.Probe) {
+	if p != nil && p.GRPC != nil {
+		p.GRPC.TLS = nil
+	}
 }
 
 // dropDisabledPodStatusFields removes disabled fields from the pod status
