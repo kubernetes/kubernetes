946d5ef7b1df161351064949162b938e15c7e30a
Richa Banker <richabanker@google.com>
2026-03-19 18:13:41 -0700

    fix duplicate kubelet_container_log_filesystem_used_bytes metric collection
    Signed-off-by: Richa Banker <richabanker@google.com>

--- pkg/kubelet/metrics/collectors/log_metrics_test.go
+++ pkg/kubelet/metrics/collectors/log_metrics_test.go
@@ -41,13 +41,17 @@ func TestNoMetricsCollected(t *testing.T) {
 }
 
 func TestMetricsCollected(t *testing.T) {
-	// Refresh Desc to share with different registry
-	descLogSize = descLogSize.GetRawDesc()
+	size18 := uint64(18)
+	size8192 := uint64(8192)
 
-	size := uint64(18)
-	collector := &logMetricsCollector{
-		podStats: func(_ context.Context) ([]statsapi.PodStats, error) {
-			return []statsapi.PodStats{
+	tests := []struct {
+		name     string
+		pods     []statsapi.PodStats
+		expected string
+	}{
+		{
+			name: "single container",
+			pods: []statsapi.PodStats{
 				{
 					PodRef: statsapi.PodReference{
 						Namespace: "some-namespace",
@@ -58,21 +62,65 @@ func TestMetricsCollected(t *testing.T) {
 						{
 							Name: "containerName1",
 							Logs: &statsapi.FsStats{
-								UsedBytes: &size,
+								UsedBytes: &size18,
 							},
 						},
 					},
 				},
-			}, nil
+			},
+			expected: `
+				# HELP kubelet_container_log_filesystem_used_bytes [ALPHA] Bytes used by the container's logs on the filesystem.
+				# TYPE kubelet_container_log_filesystem_used_bytes gauge
+				kubelet_container_log_filesystem_used_bytes{container="containerName1",namespace="some-namespace",pod="podName1",uid="UID_some_id"} 18
+`,
+		},
+		{
+			name: "duplicate container stats are deduped",
+			pods: []statsapi.PodStats{
+				{
+					PodRef: statsapi.PodReference{
+						Namespace: "kubemark",
+						Name:      "hollow-node-jlglr",
+						UID:       "d43e3a5a-40c3-427c-b3b9-69a3aab282dd",
+					},
+					Containers: []statsapi.ContainerStats{
+						{
+							Name: "hollow-proxy",
+							Logs: &statsapi.FsStats{
+								UsedBytes: &size8192,
+							},
+						},
+						{
+							Name: "hollow-proxy",
+							Logs: &statsapi.FsStats{
+								UsedBytes: &size8192,
+							},
+						},
+					},
+				},
+			},
+			expected: `
+				# HELP kubelet_container_log_filesystem_used_bytes [ALPHA] Bytes used by the container's logs on the filesystem.
+				# TYPE kubelet_container_log_filesystem_used_bytes gauge
+				kubelet_container_log_filesystem_used_bytes{container="hollow-proxy",namespace="kubemark",pod="hollow-node-jlglr",uid="d43e3a5a-40c3-427c-b3b9-69a3aab282dd"} 8192
+`,
 		},
 	}
 
-	err := testutil.CustomCollectAndCompare(collector, strings.NewReader(`
-		# HELP kubelet_container_log_filesystem_used_bytes [ALPHA] Bytes used by the container's logs on the filesystem.
-		# TYPE kubelet_container_log_filesystem_used_bytes gauge
-		kubelet_container_log_filesystem_used_bytes{container="containerName1",namespace="some-namespace",pod="podName1",uid="UID_some_id"} 18
-`), "kubelet_container_log_filesystem_used_bytes")
-	if err != nil {
-		t.Fatal(err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Refresh Desc to share with different registry
+			descLogSize = descLogSize.GetRawDesc()
+
+			collector := &logMetricsCollector{
+				podStats: func(_ context.Context) ([]statsapi.PodStats, error) {
+					return tt.pods, nil
+				},
+			}
+
+			if err := testutil.CustomCollectAndCompare(collector, strings.NewReader(tt.expected), "kubelet_container_log_filesystem_used_bytes"); err != nil {
+				t.Fatal(err)
+			}
+		})
 	}
 }
