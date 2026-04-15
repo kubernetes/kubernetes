d475e7a1a75e97e2c629194bb6b1f6292d3c16f7
Ankit Pramanik <59945244+ankit98040@users.noreply.github.com>
2026-04-15 22:55:24 +0530

    fix(prober): remove unused loop variable i

--- pkg/kubelet/prober/prober.go
+++ pkg/kubelet/prober/prober.go
@@ -269,7 +269,7 @@ func (pb *prober) runProbeWithRetries(ctx context.Context, action ProberAction, 
 	var err error
 	var result probe.Result
 	var output string
-	for i := range retries {
+	for range retries {
 		result, output, err = action.Run(ctx, status, containerID)
 		if err == nil {
 			return result, output, nil
