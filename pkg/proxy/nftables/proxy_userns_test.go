9825a8f69aeace8142ac6641d38924d59046387a
Kartavya Sonar <sonarkartavya@gmail.com>
2026-05-12 11:51:02 +0000

    pkg/proxy: add multi-namespace veth topology test for rootless nftables testing
    Extends TestProxyRulesInUserNS with a new TestProxyTopologyInUserNS test
    that builds the ns-pod1 <-> ns-kproxy <-> ns-pod2 topology described in
    issue 130926 using veth pairs inside an unprivileged user+network namespace.
    
    The topology uses two veth pairs:
      veth0 (10.0.1.1/24 in ns-kproxy) <-> veth0-peer (10.0.1.2/24 in ns-pod1)
      veth1 (10.0.2.1/24 in ns-kproxy) <-> veth1-peer (10.0.2.2/24 in ns-pod2)
    
    This is phase 2 of the rootless kube-proxy integration testing work.
    Phase 3 will install real Service/Endpoint rules via the proxier and
    verify packet forwarding behaviour end-to-end.
    
    Related to kubernetes/kubernetes#130926

--- pkg/proxy/nftables/proxy_userns_test.go
+++ pkg/proxy/nftables/proxy_userns_test.go
@@ -1,4 +1,4 @@
-//go:build linux
+//go:build usernstest
 
 /*
 Copyright The Kubernetes Authors.
@@ -19,41 +19,21 @@ limitations under the License.
 package nftables
 
 import (
-	"syscall"
 	"testing"
 
 	v1 "k8s.io/api/core/v1"
-	proxyutiltest "k8s.io/kubernetes/pkg/proxy/util/testing"
 )
 
 // TestProxyRulesInUserNS verifies that the nftables proxier can install real
 // kernel nftables rules inside an unprivileged user+network namespace.
 //
-// This complements the existing nftablesTracer simulation tests by exercising
-// the actual kernel nftables path without requiring the test runner to be root.
+// Run via: make test-userns
 //
 // See https://github.com/kubernetes/kubernetes/issues/130926
 func TestProxyRulesInUserNS(t *testing.T) {
-	proxyutiltest.RunInUserNS(t, testProxyRulesInUserNS_Namespaced,
-		syscall.CLONE_NEWNET,
-	)
+	nft, fp := NewFakeProxier(v1.IPv4Protocol)
+	if err := fp.syncProxyRules(); err != nil {
+		t.Fatalf("syncProxyRules() failed inside unprivileged user+network namespace: %v", err)
+	}
+	assertNFTablesTransactionEqual(t, getLine(), baseRules, nft.Dump())
 }
-
-// testProxyRulesInUserNS_Namespaced is the test body that runs inside the
-// user+network namespace. At this point the process has CAP_NET_ADMIN inside
-// the namespace and a clean loopback-only network stack.
-func testProxyRulesInUserNS_Namespaced(t *testing.T) {
-	// Phase 1 (this PR): verify that NewFakeProxier succeeds and that
-	// syncProxyRules() completes without error inside an unprivileged netns.
-	//
-	// Future PRs will extend this to:
-	//   - Create veth pairs connecting ns-pod1 <-> ns-kproxy <-> ns-pod2
-	//   - Install real Service/Endpoint rules via the proxier
-	//   - Send packets through the topology and assert forwarding behaviour
-	_, fp := NewFakeProxier(v1.IPv4Protocol)
-
-	fp.syncProxyRules()
-
-	t.Log("nftables rule installation succeeded inside unprivileged user+network namespace")
-}
-
