3d4febbee27e431326da261b262c24d921a89940
Filipe Xavier <felipe_life@live.com>
2026-05-14 12:07:49 -0300

    dra: add v1beta1 device metadata format and set it as default
    Add the v1beta1 API version for the DRA device metadata file format,
    graduating it from v1alpha1 as part of KEP-5304.
    
    Add api/metadata/v1beta1 package with the same schema as v1alpha1 (no field changes at graduation).
    
    v1alpha1 will be removed one release later once consumers have had time to migrate.

--- staging/src/k8s.io/dynamic-resource-allocation/api/metadata/install/install.go
+++ staging/src/k8s.io/dynamic-resource-allocation/api/metadata/install/install.go
@@ -24,12 +24,14 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/dynamic-resource-allocation/api/metadata"
 	"k8s.io/dynamic-resource-allocation/api/metadata/v1alpha1"
+	"k8s.io/dynamic-resource-allocation/api/metadata/v1beta1"
 )
 
-// Install registers the internal and v1alpha1 metadata types with the given scheme.
+// Install registers the internal, v1alpha1, and v1beta1 metadata types with the given scheme.
 func Install(scheme *runtime.Scheme) {
 	utilruntime.Must(metadata.AddToScheme(scheme))
 	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+	utilruntime.Must(v1beta1.AddToScheme(scheme))
 }
 
 // NewScheme returns a new runtime.Scheme with all metadata versions registered.
