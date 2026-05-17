1d06782d7edaa22d3f4d9d8134cb4ce5e0d53594
HirazawaUi <695097494plus@gmail.com>
2026-05-17 20:32:00 +0800

    Promote Envfiles feature gate to GA

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1383,6 +1383,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	EnvFiles: {
 		{Version: version.MustParse("1.34"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.35"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.37"), Default: true, PreRelease: featuregate.GA, LockToDefault: true},
 	},
 	EventedPLEG: {
 		{Version: version.MustParse("1.26"), Default: false, PreRelease: featuregate.Alpha},
