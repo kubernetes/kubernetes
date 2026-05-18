42355806ced88155c41d31da2d5e46a3987d6500
Praveen Krishna <pkrishn@google.com>
2026-05-18 00:11:18 +0000

    Promote NodeDeclaredFeatures FG to GA

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1629,6 +1629,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	NodeDeclaredFeatures: {
 		{Version: version.MustParse("1.35"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.37"), Default: true, PreRelease: featuregate.GA, LockToDefault: true},
 	},
 
 	NodeInclusionPolicyInPodTopologySpread: {
