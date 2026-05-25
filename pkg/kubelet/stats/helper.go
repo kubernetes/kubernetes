d17cb8f705a2272b350e9cb2baacd0a0039b9be5
Qi Wang <qiwan@redhat.com>
2026-05-24 23:38:35 -0400

    Add memory.event stats to MemoryStats
    PR [cadvisor#3870](https://github.com/google/cadvisor/pull/3870) exposes `memory.events` (high/max) through cadvisor's Prometheus metrics and REST API. This follow-up surface them through kubelet's Summary Stats API
    
    Signed-off-by: Qi Wang <qiwan@redhat.com>

--- pkg/kubelet/stats/helper.go
+++ pkg/kubelet/stats/helper.go
@@ -77,6 +77,8 @@ func cadvisorInfoToCPUandMemoryStats(info *cadvisorapiv2.ContainerInfo) (*statsa
 			availableBytes := info.Spec.Memory.Limit - cstat.Memory.WorkingSet
 			memoryStats.AvailableBytes = &availableBytes
 		}
+		memoryStats.HighEvents = &cstat.Memory.Events.High
+		memoryStats.MaxEvents = &cstat.Memory.Events.Max
 		if utilfeature.DefaultFeatureGate.Enabled(features.KubeletPSI) {
 			memoryStats.PSI = cadvisorPSIToStatsPSI(&cstat.Memory.PSI)
 		}
