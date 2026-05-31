4eb5179ad041127aed80c7ee17c7434f6d2fff49
nispriha <njagan@redhat.com>
2026-05-31 19:26:37 +0530

    Pass emptyDir mountOptions through CRI to container runtime

--- pkg/kubelet/kuberuntime/kuberuntime_container.go
+++ pkg/kubelet/kuberuntime/kuberuntime_container.go
@@ -496,6 +496,7 @@ func (m *kubeGenericRuntimeManager) makeMounts(opts *kubecontainer.RunContainerO
 			RecursiveReadOnly: v.RecursiveReadOnly,
 			Image:             v.Image,
 			ImageSubPath:      v.ImageSubPath,
+			MountOptions:      v.MountOptions,
 		}
 
 		volumeMounts = append(volumeMounts, mount)
@@ -772,6 +773,7 @@ func (m *kubeGenericRuntimeManager) toKubeContainerStatus(ctx context.Context, p
 			Propagation:       mount.Propagation,
 			Image:             mount.Image,
 			ImageSubPath:      mount.ImageSubPath,
+			MountOptions:      mount.MountOptions,
 		})
 	}
 	return cStatus
