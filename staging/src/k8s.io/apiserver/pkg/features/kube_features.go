6172b64f35eccd158db3bc41ea1d5f335493a7eb
Marek Siarkowicz <siarkowicz@google.com>
2026-06-02 20:43:09 +0200

    Graduate WatchCacheInitializationPostStartHook to GA

--- staging/src/k8s.io/apiserver/pkg/features/kube_features.go
+++ staging/src/k8s.io/apiserver/pkg/features/kube_features.go
@@ -506,6 +506,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	WatchCacheInitializationPostStartHook: {
 		{Version: version.MustParse("1.31"), Default: false, PreRelease: featuregate.Beta},
 		{Version: version.MustParse("1.36"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.37"), Default: true, PreRelease: featuregate.GA, LockToDefault: true},
 	},
 
 	WatchList: {
