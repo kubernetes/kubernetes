c9792893530c073a23db071ee56902cd35d307ae
Lalit Chauhan <lalitchauhan@google.com>
2026-06-03 23:06:39 +0000

    Add declarative validation tests for apps API group

---
+++ test/declarative_validation/apps/daemonset/declarative_validation_test.go
@@ -0,0 +1,117 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package daemonset
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
+	apps "k8s.io/kubernetes/pkg/apis/apps"
+	_ "k8s.io/kubernetes/pkg/apis/apps/install"
+	core "k8s.io/kubernetes/pkg/apis/core"
+	"k8s.io/kubernetes/pkg/registry/apps/daemonset"
+	"k8s.io/kubernetes/test/declarative_validation/meta"
+	"testing"
+)
+
+// TODO: remove this apiVersions variable once coverage tests are generated for this package.
+var apiVersions = []string{"v1", "v1beta2"}
+
+// Helper function to create a baseline valid DaemonSet with optional tweaks
+func mkDaemonSet(tweaks ...func(*apps.DaemonSet)) apps.DaemonSet {
+	var terminationGracePeriodSeconds int64 = 30
+	obj := apps.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "valid-resource-name",
+		},
+		Spec: apps.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"a": "b"}},
+			Template: core.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"a": "b"},
+				},
+				Spec: core.PodSpec{
+					Containers: []core.Container{
+						{
+							Name:                     "test",
+							Image:                    "test",
+							TerminationMessagePolicy: core.TerminationMessageReadFile,
+							ImagePullPolicy:          core.PullIfNotPresent,
+						},
+					},
+					RestartPolicy:                 core.RestartPolicyAlways,
+					DNSPolicy:                     core.DNSClusterFirst,
+					TerminationGracePeriodSeconds: &terminationGracePeriodSeconds,
+				},
+			},
+			UpdateStrategy: apps.DaemonSetUpdateStrategy{
+				Type: apps.OnDeleteDaemonSetStrategyType,
+			},
+		},
+	}
+	for _, tweak := range tweaks {
+		tweak(&obj)
+	}
+	return obj
+}
+
+func TestDeclarativeValidate(t *testing.T) {
+	for _, apiVersion := range apiVersions {
+		t.Run(apiVersion, func(t *testing.T) {
+			strategy := daemonset.Strategy
+			var namespace string
+			if strategy.NamespaceScoped() {
+				namespace = metav1.NamespaceDefault
+			}
+			ctx := genericapirequest.WithRequestInfo(genericapirequest.NewDefaultContext(), &genericapirequest.RequestInfo{
+				APIGroup:          "apps",
+				APIVersion:        apiVersion,
+				Resource:          "daemonsets",
+				Namespace:         namespace,
+				IsResourceRequest: true,
+				Verb:              "create",
+			})
+			obj := mkDaemonSet(func(o *apps.DaemonSet) {
+				o.Namespace = namespace
+			})
+			meta.RunObjectMetaTestCases(t, ctx, &obj, strategy)
+		})
+	}
+}
+
+func TestDeclarativeValidateUpdate(t *testing.T) {
+	for _, apiVersion := range apiVersions {
+		t.Run(apiVersion, func(t *testing.T) {
+			strategy := daemonset.Strategy
+			var namespace string
+			if strategy.NamespaceScoped() {
+				namespace = metav1.NamespaceDefault
+			}
+			ctx := genericapirequest.WithRequestInfo(genericapirequest.NewDefaultContext(), &genericapirequest.RequestInfo{
+				APIGroup:          "apps",
+				APIVersion:        apiVersion,
+				Resource:          "daemonsets",
+				Namespace:         namespace,
+				IsResourceRequest: true,
+				Verb:              "update",
+			})
+			obj := mkDaemonSet(func(o *apps.DaemonSet) {
+				o.Namespace = namespace
+			})
+			meta.RunObjectMetaUpdateTestCases(t, ctx, &obj, strategy)
+		})
+	}
+}
