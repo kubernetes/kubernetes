6841ed769ffb2d6590e0fc8949ec0d859d2f815c
Lucas Käldström <lucas.kaldstrom@upbound.io>
2026-06-10 11:29:45 +0300

    Add PossibleDecisions, FailureDecision, ContainsAllowOrDeny methods to ConditionsAwareDecision

--- staging/src/k8s.io/apiserver/pkg/authorization/authorizer/conditionsawaredecision.go
+++ staging/src/k8s.io/apiserver/pkg/authorization/authorizer/conditionsawaredecision.go
@@ -21,6 +21,28 @@ import (
 	"strings"
 
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+// conditionsAwareDecisionType is a small enum-like type for keeping track of what type a ConditionsAwareDecision is.
+// These values must never be exposed to users outside of this package, and should not be used for anything else than
+// keeping track of what type of a ConditionsAwareDecision is.
+type conditionsAwareDecisionType int
+
+const (
+	// conditionsAwareDecisionTypeDeny represents the unconditional Deny decision.
+	// It is zero such that ConditionsAwareDecision{}.IsDeny() == true
+	conditionsAwareDecisionTypeDeny conditionsAwareDecisionType = 0
+	// conditionsAwareDecisionTypeAllow represents the unconditional Allow decision.
+	// It has a different value from DecisionAllow to never be conflated with that.
+	conditionsAwareDecisionTypeAllow conditionsAwareDecisionType = 11
+	// conditionsAwareDecisionTypeNoOpinion represents the unconditional NoOpinion decision.
+	// It has a different value from DecisionNoOpinion to never be conflated with that.
+	conditionsAwareDecisionTypeNoOpinion conditionsAwareDecisionType = 12
+	// conditionsAwareDecisionTypeConditionsMap represents the conditional ConditionsMap decision.
+	conditionsAwareDecisionTypeConditionsMap conditionsAwareDecisionType = 13
+	// conditionsAwareDecisionTypeUnion represents a conditional Union decision.
+	conditionsAwareDecisionTypeUnion conditionsAwareDecisionType = 14
 )
 
 // ConditionsAwareDecision models an authorization decision that is conditions-aware.
@@ -34,7 +56,7 @@ import (
 // The zero value (ConditionsAwareDecision{}) is equivalent to ConditionsAwareDecisionDeny().
 // A ConditionsAwareDecision is passed by value.
 type ConditionsAwareDecision struct {
-	unconditionalDecision Decision
+	decisionType conditionsAwareDecisionType
 
 	reason string
 	err    error
@@ -43,35 +65,35 @@ type ConditionsAwareDecision struct {
 // ConditionsAwareDecisionDeny constructs a Deny decision with the given reason and error.
 func ConditionsAwareDecisionDeny(reason string, err error) ConditionsAwareDecision {
 	return ConditionsAwareDecision{
-		// DecisionDeny == int(0) == zero value
+		// conditionsAwareDecisionTypeDeny == 0 == zero value
 		// => ConditionsAwareDecision{} == ConditionsAwareDecisionDeny()
-		unconditionalDecision: DecisionDeny,
-		reason:                reason,
-		err:                   err,
+		decisionType: conditionsAwareDecisionTypeDeny,
+		reason:       reason,
+		err:          err,
 	}
 }
 
 // ConditionsAwareDecisionAllow constructs an Allow decision with the given reason and error.
 func ConditionsAwareDecisionAllow(reason string, err error) ConditionsAwareDecision {
 	return ConditionsAwareDecision{
-		unconditionalDecision: DecisionAllow,
-		reason:                reason,
-		err:                   err,
+		decisionType: conditionsAwareDecisionTypeAllow,
+		reason:       reason,
+		err:          err,
 	}
 }
 
 // ConditionsAwareDecisionNoOpinion constructs a NoOpinion decision with the given reason and error.
 func ConditionsAwareDecisionNoOpinion(reason string, err error) ConditionsAwareDecision {
 	return ConditionsAwareDecision{
-		unconditionalDecision: DecisionNoOpinion,
-		reason:                reason,
-		err:                   err,
+		decisionType: conditionsAwareDecisionTypeNoOpinion,
+		reason:       reason,
+		err:          err,
 	}
 }
 
 // ConditionsAwareDecisionFromParts is meant to be used by conditions-unaware Authorizer implementations
-// in order to implement Authorizer.ConditionsAwareAuthorize as:
-// "return authorizer.ConditionsAwareDecisionFromParts(self.Authorize(ctx, a))"
+// in order to implement ConditionsAwareAuthorize as:
+// "return ConditionsAwareDecisionFromParts(self.Authorize(ctx, a))"
 func ConditionsAwareDecisionFromParts(unconditional Decision, reason string, err error) ConditionsAwareDecision {
 	switch unconditional {
 	case DecisionAllow:
@@ -92,17 +114,17 @@ func ConditionsAwareDecisionFromParts(unconditional Decision, reason string, err
 
 // IsAllow returns true if the decision is an unconditional Allow.
 func (d ConditionsAwareDecision) IsAllow() bool {
-	return d.unconditionalDecision == DecisionAllow
+	return d.decisionType == conditionsAwareDecisionTypeAllow
 }
 
 // IsNoOpinion returns true if the decision is an unconditional NoOpinion.
 func (d ConditionsAwareDecision) IsNoOpinion() bool {
-	return d.unconditionalDecision == DecisionNoOpinion
+	return d.decisionType == conditionsAwareDecisionTypeNoOpinion
 }
 
 // IsDeny returns true if the decision is an unconditional Deny.
 func (d ConditionsAwareDecision) IsDeny() bool {
-	return d.unconditionalDecision == DecisionDeny // == 0 == zero value
+	return d.decisionType == conditionsAwareDecisionTypeDeny // == 0 == zero value
 }
 
 // IsUnconditional is true if d is Allow, Deny or NoOpinion.
@@ -110,12 +132,50 @@ func (d ConditionsAwareDecision) IsUnconditional() bool {
 	return d.IsAllow() || d.IsDeny() || d.IsNoOpinion()
 }
 
-// Reason returns the reason associated with the decision.
+// PossibleDecisions details what are the possible decision outcomes of this
+// ConditionsAwareDecision. The return value is a subset of {Allow, Deny, NoOpinion},
+// but never the empty set. If the set only contains a single value, it means the
+// decision is unconditional.
+func (d ConditionsAwareDecision) PossibleDecisions() sets.Set[Decision] {
+	switch {
+	case d.IsAllow():
+		return sets.New(DecisionAllow)
+	case d.IsNoOpinion():
+		return sets.New(DecisionNoOpinion)
+	default:
+		return sets.New(DecisionDeny)
+	}
+}
+
+// FailureDecision returns either a Deny or NoOpinion decision that the caller can
+// use if the caller encounters an unrecoverable error while processing a
+// ConditionsAwareDecision. If this decision is or could evaluate to Deny
+// this function returns DecisionDeny to the caller, in order to fail closed
+// as a conservative approximation. Otherwise, NoOpinion is returned.
+func (d ConditionsAwareDecision) FailureDecision() Decision {
+	if d.PossibleDecisions().Has(DecisionDeny) {
+		return DecisionDeny
+	}
+	return DecisionNoOpinion
+}
+
+// ContainsAllowOrDeny returns true whether there union contains at least one
+// Allow or Deny decision within the tree of decisions.
+func (d ConditionsAwareDecision) ContainsAllowOrDeny() bool {
+	if d.IsAllow() || d.IsDeny() {
+		return true
+	}
+	return false // NoOpinion
+}
+
+// Reason returns the reason supplied when constructing the decision
+// (if Allow/Deny/NoOpinion/ConditionsMap), or an aggregated reason (if Union).
 func (d ConditionsAwareDecision) Reason() string {
 	return d.reason
 }
 
-// Error returns the error associated with the decision.
+// Error returns the error supplied when constructing the decision
+// (if Allow/Deny/NoOpinion/ConditionsMap), or an aggregated error (if Union).
 func (d ConditionsAwareDecision) Error() error {
 	return d.err
 }
