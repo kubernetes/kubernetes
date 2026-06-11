3907039da37124e13fb7d13618a82ba2787d9f77
srayansh-gupta <sr.gupta621@gmail.com>
2026-06-11 09:17:40 +0530

    Add +k8s:alpha(since: 1.37)=+k8s:required to Condition.Status

--- staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/types.go
+++ staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/types.go
@@ -1637,6 +1637,7 @@ type Condition struct {
 	// +required
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Enum=True;False;Unknown
+	// +k8s:alpha(since: "1.37")=+k8s:required
 	Status ConditionStatus `json:"status" protobuf:"bytes,2,opt,name=status"`
 	// observedGeneration represents the .metadata.generation that the condition was set based upon.
 	// For instance, if .metadata.generation is currently 12, but the .status.conditions[x].observedGeneration is 9, the condition is out of date
