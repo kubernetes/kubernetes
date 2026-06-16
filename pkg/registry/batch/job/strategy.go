d27b6cab33d9d7365d86275932414b2aa172da3c
helayoty <heelayot@microsoft.com>
2026-06-16 10:15:11 +0000

    wire Job scheduling configuration into registry strategy
    Signed-off-by: helayoty <heelayot@microsoft.com>

--- pkg/registry/batch/job/strategy.go
+++ pkg/registry/batch/job/strategy.go
@@ -92,17 +92,19 @@ func (jobStrategy) GetResetFields() map[fieldpath.APIVersion]*fieldpath.Set {
 
 // PrepareForCreate clears the status of a job before creation.
 func (jobStrategy) PrepareForCreate(ctx context.Context, obj runtime.Object) {
-	job := obj.(*batch.Job)
-	generateSelectorIfNeeded(job)
-	job.Status = batch.JobStatus{}
+	newJob := obj.(*batch.Job)
+	generateSelectorIfNeeded(newJob)
+	newJob.Status = batch.JobStatus{}
 
-	job.Generation = 1
+	newJob.Generation = 1
 
 	if !utilfeature.DefaultFeatureGate.Enabled(features.JobManagedBy) {
-		job.Spec.ManagedBy = nil
+		newJob.Spec.ManagedBy = nil
 	}
 
-	pod.DropDisabledTemplateFields(&job.Spec.Template, nil)
+	job.DropDisabledFields(&newJob.Spec, nil)
+
+	pod.DropDisabledTemplateFields(&newJob.Spec.Template, nil)
 }
 
 // PrepareForUpdate clears fields that are not allowed to be set by end users on update.
@@ -110,6 +112,8 @@ func (jobStrategy) PrepareForUpdate(ctx context.Context, obj, old runtime.Object
 	newJob := obj.(*batch.Job)
 	oldJob := old.(*batch.Job)
 	newJob.Status = oldJob.Status
+
+	job.DropDisabledFields(&newJob.Spec, &oldJob.Spec)
 
 	pod.DropDisabledTemplateFields(&newJob.Spec.Template, &oldJob.Spec.Template)
 
@@ -126,6 +130,16 @@ func (jobStrategy) Validate(ctx context.Context, obj runtime.Object) field.Error
 	job := obj.(*batch.Job)
 	opts := validationOptionsForJob(job, nil)
 	return batchvalidation.ValidateJob(job, opts)
+}
+
+// DeclarativeValidationConfig implements rest.DeclarativeValidationConfigurer to supply declarative
+// validation options to the generic BeforeCreate/BeforeUpdate code path.
+func (jobStrategy) DeclarativeValidationConfig(ctx context.Context, obj, oldObj runtime.Object) rest.DeclarativeValidationConfig {
+	opts := []string{}
+	if utilfeature.DefaultFeatureGate.Enabled(features.WorkloadWithJob) {
+		opts = append(opts, string(features.WorkloadWithJob))
+	}
+	return rest.DeclarativeValidationConfig{Options: opts}
 }
 
 // shouldAllowMutablePodTemplate returns true if the Job's pod template should
