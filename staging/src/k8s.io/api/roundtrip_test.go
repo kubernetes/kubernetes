kubernetes/api

introduce EvictionRequest and Eviction API
to new lifecycle group

- EvictionRequest is managed by a requester
- Eviction is managed by the evictionrequest-controller
- add storage, strategy, printers and validation

Kubernetes-commit: fc558fb4e9fa9da1dc583f5c71f27cd54debc4ae

roundtrip_test.go
@@ -56,6 +56,7 @@ import (
 	flowcontrolv1beta2 "k8s.io/api/flowcontrol/v1beta2"
 	flowcontrolv1beta3 "k8s.io/api/flowcontrol/v1beta3"
 	imagepolicyv1alpha1 "k8s.io/api/imagepolicy/v1alpha1"
+	lifecyclev1alpha1 "k8s.io/api/lifecycle/v1alpha1"
 	networkingv1 "k8s.io/api/networking/v1"
 	networkingv1beta1 "k8s.io/api/networking/v1beta1"
 	nodev1 "k8s.io/api/node/v1"
@@ -123,6 +124,7 @@ var groups = []runtime.SchemeBuilder{
 	flowcontrolv1beta3.SchemeBuilder,
 	flowcontrolv1.SchemeBuilder,
 	imagepolicyv1alpha1.SchemeBuilder,
+	lifecyclev1alpha1.SchemeBuilder,
 	networkingv1.SchemeBuilder,
 	networkingv1beta1.SchemeBuilder,
 	nodev1.SchemeBuilder,

kubernetes/kubernetes

staging/src/k8s.io/api/roundtrip_test.go
@@ -43,6 +43,7 @@ import (
 	certificatesv1alpha1 "k8s.io/api/certificates/v1alpha1"
 	certificatesv1beta1 "k8s.io/api/certificates/v1beta1"
 	coordinationv1 "k8s.io/api/coordination/v1"
+	coordinationv1alpha1 "k8s.io/api/coordination/v1alpha1"
 	coordinationv1alpha2 "k8s.io/api/coordination/v1alpha2"
 	coordinationv1beta1 "k8s.io/api/coordination/v1beta1"
 	corev1 "k8s.io/api/core/v1"
@@ -111,6 +112,7 @@ var groups = []runtime.SchemeBuilder{
 	coordinationv1.SchemeBuilder,
 	coordinationv1beta1.SchemeBuilder,
 	coordinationv1alpha2.SchemeBuilder,
+	coordinationv1alpha1.SchemeBuilder,
 	corev1.SchemeBuilder,
 	discoveryv1.SchemeBuilder,
 	discoveryv1beta1.SchemeBuilder,