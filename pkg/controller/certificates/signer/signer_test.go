f355e700bf6800394694e239f049da65b3a5ed60
Yeshwanth Raj Selvaraj <yeshselva1@gmail.com>
2026-07-05 20:40:23 +0530

    pkg/auth, pkg/controller/certificates: replace deprecated io/ioutil with io and os

--- pkg/controller/certificates/signer/signer_test.go
+++ pkg/controller/certificates/signer/signer_test.go
@@ -24,7 +24,7 @@ import (
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"fmt"
-	"io/ioutil"
+	"os"
 	"math/rand"
 	"reflect"
 	"testing"
@@ -88,7 +88,7 @@ func TestSigner(t *testing.T) {
 		t.Fatalf("failed to create signer: %v", err)
 	}
 
-	csrb, err := ioutil.ReadFile("./testdata/kubelet.csr")
+	csrb, err := os.ReadFile("./testdata/kubelet.csr")
 	if err != nil {
 		t.Fatalf("failed to read CSR: %v", err)
 	}
