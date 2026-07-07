a8d510f930ba4330477ff136e4ec908ee2b15b26
Heba Elayoty <hebaelayoty@gmail.com>
2026-07-07 22:52:15 +0000

    Graduate TAS to beta
    Signed-off-by: Heba Elayoty <hebaelayoty@gmail.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -2098,6 +2098,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	TopologyAwareWorkloadScheduling: {
 		{Version: version.MustParse("1.36"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.37"), Default: false, PreRelease: featuregate.Beta},
 	},
 
 	TopologyManagerPolicyAlphaOptions: {
