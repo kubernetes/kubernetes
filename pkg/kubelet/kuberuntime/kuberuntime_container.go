bd79e93fc70a4d724fd40d2291e02b69a81c60c3
Tim Allclair <tallclair@google.com>
2026-07-09 23:44:47 +0000

    Always set UpdatePodResources when a starting container is resized
    Co-authored-by: hai.yue <20416005+yuehaii@users.noreply.github.com>

--- pkg/kubelet/kuberuntime/kuberuntime_container.go
+++ pkg/kubelet/kuberuntime/kuberuntime_container.go
@@ -423,6 +423,7 @@ func (m *kubeGenericRuntimeManager) updateContainerResources(ctx context.Context
 func (m *kubeGenericRuntimeManager) setActuatedContainerResources(pod *v1.Pod, container *v1.Container) error {
 	containerResources := container.Resources
 	if utilfeature.DefaultFeatureGate.Enabled(features.InPlacePodLevelResourcesVerticalScaling) {
+		containerResources = *containerResources.DeepCopy()
 		containerResources.Limits = kubeutil.GetLimits(&kubeutil.ResourceOpts{PodResources: pod.Spec.Resources, ContainerResources: &container.Resources})
 	}
 	return m.actuatedState.SetContainerResources(pod.UID, container.Name, containerResources)
@@ -1286,6 +1287,11 @@ func (m *kubeGenericRuntimeManager) computeInitContainerActions(ctx context.Cont
 	for i := 0; i < l/2; i++ {
 		changes.InitContainersToStart[i], changes.InitContainersToStart[l-1-i] =
 			changes.InitContainersToStart[l-1-i], changes.InitContainersToStart[i]
+	}
+
+	if !changes.UpdatePodLevelResources && !changes.UpdatePodResources {
+		// If any starting containers have resized, we need to resize the pod resources.
+		changes.UpdatePodResources = m.startingResizedContainer(logger, pod, pod.Spec.InitContainers, changes.InitContainersToStart)
 	}
 
 	return podHasInitialized
