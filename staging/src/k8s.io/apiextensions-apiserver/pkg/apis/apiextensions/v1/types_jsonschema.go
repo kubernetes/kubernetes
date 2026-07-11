14ae6aa348ebc69e366a868e2f5d814e467b299c
SURAJ KUMAR <surajkumar21012010@gmail.com>
2026-07-11 23:02:45 +0530

    Fix broken JSON-Schema URL in v1 and v1beta1 JSONSchemaProps doc comments

--- staging/src/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1/types_jsonschema.go
+++ staging/src/k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1/types_jsonschema.go
@@ -36,7 +36,8 @@ const (
 	FieldValueForbidden FieldValueErrorReason = "FieldValueForbidden"
 )
 
-// JSONSchemaProps is a JSON-Schema following Specification Draft 4 (http://json-schema.org/).
+// JSONSchemaProps is a JSON-Schema following Specification Draft 4.
+// See: https://json-schema.org/
 type JSONSchemaProps struct {
 	ID          string        `json:"id,omitempty" protobuf:"bytes,1,opt,name=id"`
 	Schema      JSONSchemaURL `json:"$schema,omitempty" protobuf:"bytes,2,opt,name=schema"`
