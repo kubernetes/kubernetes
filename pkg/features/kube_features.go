6dde6d61f83ee1d5c5399db0ee829b4c2b40ec26
Benjamin Elder <bentheelder@google.com>
2026-07-14 10:34:14 -0700

    Promote ManifestBasedAdmissionControlConfig to beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -2115,6 +2115,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	genericfeatures.ManifestBasedAdmissionControlConfig: {
 		{Version: version.MustParse("1.36"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.37"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	genericfeatures.MutatingAdmissionPolicy: {
--- staging/src/k8s.io/apiserver/pkg/features/kube_features.go
+++ staging/src/k8s.io/apiserver/pkg/features/kube_features.go
@@ -389,6 +389,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	ManifestBasedAdmissionControlConfig: {
 		{Version: version.MustParse("1.36"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.37"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	MutatingAdmissionPolicy: {
