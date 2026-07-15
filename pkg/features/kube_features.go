e25f25f7eb84ccf146328f46a22467fd9bb9b469
Kevin Torres <kevtorres@google.com>
2026-07-15 23:17:08 +0000

    Graduate PodLevelResourceManagers to Beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1801,6 +1801,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	PodLevelResourceManagers: {
 		{Version: version.MustParse("1.36"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.37"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	PodLevelResources: {
