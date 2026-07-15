8d2fa3f09aa70f5199fafcff085a7afd8fb0d17d
Sergey Sannikov <sergey@sannikov.dev>
2026-07-15 13:04:08 +0400

    Fix CEL map-list merge of duplicate new keys

--- staging/src/k8s.io/apiserver/pkg/cel/common/valuesreflect.go
+++ staging/src/k8s.io/apiserver/pkg/cel/common/valuesreflect.go
@@ -529,6 +529,7 @@ func addToMapList(elements []ref.Val, other traits.Lister, escapedKeyProps []str
 			elements[overwritePosition] = v
 		} else {
 			elements = append(elements, v)
+			keyToIdx[k] = len(elements) - 1
 		}
 	}
 	return &refValMapList{
