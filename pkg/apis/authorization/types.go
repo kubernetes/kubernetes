674679273c6fb843536a6af825c74b04ecec3b21
Lucas Käldström <lucas.kaldstrom@upbound.io>
2026-07-22 23:08:04 +0300

    Add API serializations of the new authorizer types, including authorization.k8s.io/v1alpha1
    fixup conversions

--- pkg/apis/authorization/types.go
+++ pkg/apis/authorization/types.go
@@ -18,6 +18,8 @@ package authorization
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/kubernetes/pkg/apis/admission"
 )
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -175,6 +177,12 @@ type SubjectAccessReviewSpec struct {
 	Extra map[string]ExtraValue
 	// UID information about the requesting user.
 	UID string
+
+	// AuthorizationOptions contains options for specifying the client's authorization abilities.
+	// Requires the ConditionalAuthorization feature to be enabled.
+	// +optional
+	// +featureGate=ConditionalAuthorization
+	AuthorizationOptions *AuthorizationOptions
 }
 
 // ExtraValue masks the value so protobuf can generate
@@ -188,16 +196,24 @@ type SelfSubjectAccessReviewSpec struct {
 	ResourceAttributes *ResourceAttributes
 	// NonResourceAttributes describes information for a non-resource access request
 	NonResourceAttributes *NonResourceAttributes
+
+	// AuthorizationOptions contains options for specifying the client's authorization abilities.
+	// Requires the ConditionalAuthorization feature to be enabled.
+	// +optional
+	// +featureGate=ConditionalAuthorization
+	AuthorizationOptions *AuthorizationOptions
 }
 
 // SubjectAccessReviewStatus represents the current state of a SubjectAccessReview.
 type SubjectAccessReviewStatus struct {
 	// Allowed is required. True if the action would be allowed, false otherwise.
+	// allowed=true is mutually exclusive with denied=true and conditionalDecision != nil.
 	Allowed bool
-	// Denied is optional. True if the action would be denied, otherwise
-	// false. If both allowed is false and denied is false, then the
-	// authorizer has no opinion on whether to authorize the action. Denied
-	// may not be true if Allowed is true.
+	// denied is optional. True if the action would be denied, otherwise false
+	// If allowed is false, denied is false, and conditionalDecision is unset,
+	// then the authorizer has no opinion on whether to authorize the action.
+	// denied=true is mutually exclusive with allowed=true and conditionalDecision != nil.
+	// +optional
 	Denied bool
 	// Reason is optional.  It indicates why a request was allowed or denied.
 	Reason string
@@ -205,6 +221,17 @@ type SubjectAccessReviewStatus struct {
 	// It is entirely possible to get an error and be able to continue determine authorization status in spite of it.
 	// For instance, RBAC can be missing a role, but enough roles are still present and bound to reason about the request.
 	EvaluationError string
+
+	// conditionalDecision represents a conditional decision returned by the authorizer.
+	// Mutually exclusive with allowed=true and denied=true.
+	// The top-level decision type should be ConditionsAwareDecisionTypeConditionsMap or
+	// ConditionsAwareDecisionTypeUnion, as Allow/Deny/NoOpinion decisions can be represented
+	// with SubjectAccessReviewStatus.Allowed and SubjectAccessReviewStatus.Denied alone.
+	// May only be set if spec.conditionalAuthorization is non-null.
+	// Requires the ConditionalAuthorization feature to be enabled.
+	// +optional
+	// +featureGate=ConditionalAuthorization
+	ConditionalDecision *ConditionsAwareDecision
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -275,4 +302,219 @@ type NonResourceRule struct {
 	// NonResourceURLs is a set of partial urls that a user should have access to.  *s are allowed, but only as the full,
 	// final step in the path.  "*" means all.
 	NonResourceURLs []string
+}
+
+// AuthorizationOptions contains options for specifying the client's authorization abilities.
+type AuthorizationOptions struct {
+	// HandledDecisionTypes specifies what decision types the client can handle in the context it is in.
+	// Currently valid values are:
+	// - [Allow, Deny, NoOpinion] (for conditions-unaware clients) or
+	// - [Allow, Deny, NoOpinion, ConditionsMap, Union] (for conditions-aware clients)
+	// If the authorizer would like to return conditions, but the client does not opt in to handle those here,
+	//   the authorizer must fail closed to a safe unconditional decision using ConditionsAwareDecision.FailureDecision()
+	//   (Deny if any Deny conditions were present, otherwise NoOpinion).
+	// Order does not matter in this slice; set semantics should be used.
+	// The server should not reject unrecognized decision types (hence the k8s:opaqueType), but focus on whether the client
+	// supports a mode that the server does. All clients must support "classic", conditions-unaware authorization.
+	// +listType=set
+	// +required
+	HandledDecisionTypes []ConditionsAwareDecisionType
+}
+
+// Condition represents a single authorization condition to be evaluated against
+// data available later in the request chain, e.g. objects available in admission.
+type Condition struct {
+	// ID uniquely identifies this condition within the scope of the authorizer
+	// that authored it and ConditionsMap it is part of. Validated as a Kubernetes label key.
+	// Any domain of form *.k8s.io or *.kubernetes.io is reserved for Kubernetes use.
+	// +required
+	ID string
+
+	// Condition returns a string encoding of the condition to be evaluated.
+	// It is a pure, deterministic function from ConditionsData to a boolean (or error).
+	// Might or might not be human-readable.
+	// Optional, if the ID alone is enough for the authorizer to know how to evaluate the condition.
+	// +optional
+	Condition string
+
+	// Type describes the type of the condition, if there are multiple possibilities.
+	// Should be formatted as a Kubernetes label key.
+	// Any domain of form *.k8s.io or *.kubernetes.io is reserved for Kubernetes use.
+	// Optional. Can be omitted if the authorizer already knows how to evaluate the condition.
+	// +optional
+	Type string
+
+	// Description is an optional human-friendly description that can be shown
+	// as an error message or for debugging. Optional.
+	// +optional
+	Description string
+}
+
+// ConditionsMap represents a map of conditions, keyed by ID across all conditions, across
+// all effects. The ConditionsMap must have at least one Allow condition or at least one
+// Deny condition. It cannot contain more than 128 conditions in total. The conditions are evaluated
+// against data available later, to determine whether the authorizer that authored the conditions
+// allows or denies the request.
+// If all conditions in the map evaluate to false, the final decision must be NoOpinion.
+type ConditionsMap struct {
+	// DenyConditions contains the conditions with Deny effect. If any such condition evaluates to
+	// true or error, the ConditionsMap as a whole must evaluate to Deny.
+	// +listType=map
+	// +listMapKey=id
+	// +optional
+	DenyConditions []Condition
+
+	// NoOpinionConditions contains the conditions with NoOpinion effect. If any such condition evaluates to
+	// true or error, the ConditionsMap as a whole must evaluate to NoOpinion.
+	// +listType=map
+	// +listMapKey=id
+	// +optional
+	NoOpinionConditions []Condition
+
+	// AllowConditions contains the conditions with Allow effect. If any such condition evaluates to
+	// true, the ConditionsMap as a whole must evaluate to Allow.
+	// +listType=map
+	// +listMapKey=id
+	// +optional
+	AllowConditions []Condition
+}
+
+// ConditionsAwareDecisionType is an enum representing what kind of authorization decision
+// the ConditionsAwareDecision represents.
+type ConditionsAwareDecisionType string
+
+const (
+	// ConditionsAwareDecisionTypeDeny represents an unconditional Deny authorizer decision.
+	ConditionsAwareDecisionTypeDeny ConditionsAwareDecisionType = "Deny"
+
+	// ConditionsAwareDecisionTypeAllow represents an unconditional Allow authorizer decision.
+	ConditionsAwareDecisionTypeAllow ConditionsAwareDecisionType = "Allow"
+
+	// ConditionsAwareDecisionTypeNoOpinion represents an unconditional NoOpinion authorizer decision,
+	// which means that the authorizer does not have a specific opinion on whether the request
+	// should be allowed or denied, and thus can other authorizers later in the union have their say.
+	ConditionsAwareDecisionTypeNoOpinion ConditionsAwareDecisionType = "NoOpinion"
+
+	// ConditionsAwareDecisionTypeConditionsMap represents an authorizer decision that is dependent
+	// on request data available later in the request chain, and thus at this stage conditional.
+	ConditionsAwareDecisionTypeConditionsMap ConditionsAwareDecisionType = "ConditionsMap"
+
+	// ConditionsAwareDecisionTypeUnion is a decision type whose final decision is computed by
+	// an ordered list of sub-authorizers, with their individual decisions. A decision can thus
+	// be represented as a tree, with Union decisions being internal nodes, and
+	// Deny/Allow/NoOpinion/ConditionsMap decisions being leaf nodes, which are visited in depth-first order.
+	ConditionsAwareDecisionTypeUnion ConditionsAwareDecisionType = "Union"
+)
+
+// ConditionsAwareDecision represents one authorizer's decision. It is an enum type,
+// with variants described in ConditionsAwareDecisionType, plus a reason and error.
+type ConditionsAwareDecision struct {
+	// Type describes the type of the decision, and acts as an enum discriminator.
+	// +required
+	Type ConditionsAwareDecisionType
+
+	// Deny represents an unconditional Deny decision.
+	// Must be non-null when type == "Deny", otherwise this field must be unset.
+	// +optional
+	Deny *UnconditionalDecision
+
+	// NoOpinion represents an unconditional NoOpinion decision.
+	// Must be non-null when type == "NoOpinion", otherwise this field must be unset.
+	// +optional
+	NoOpinion *UnconditionalDecision
+
+	// Allow represents an unconditional Allow decision.
+	// Must be non-null when type == "Allow", otherwise this field must be unset.
+	// +optional
+	Allow *UnconditionalDecision
+
+	// ConditionsMap represents a conditional decision, modelled as a map of conditions.
+	// Must be non-null when type == "ConditionsMap", otherwise this field must be unset.
+	// +optional
+	ConditionsMap *ConditionsMap
+
+	// Union forms an ordered tree of decisions, where the union decision is represented by
+	// an internal node, and all other decision types are leaf nodes. During evaluation, the
+	// leaf decisions are evaluated in depth-first order, until an Allow or Deny decision is found.
+	// The order of the decisions must match exactly the order of the authorizers in the union authorizer.
+	// At least one of the leaves must be of type ConditionsMap, as otherwise the union could be trivially
+	// reduced to just a single Allow/Deny/NoOpinion.
+	//
+	// Must have at least one element when type == "Union", otherwise this field must be unset.
+	//
+	// +optional
+	// +listType=map
+	// +listMapKey=authorizerName
+	Union []NamedConditionsAwareDecision
+}
+
+// NamedConditionsAwareDecision is a named ConditionsAwareDecision, returned by a unioned authorizer.
+type NamedConditionsAwareDecision struct {
+	// AuthorizerName details the name of the authorizer that authored the condition, such that
+	// the right Decision can be paired with the right authorizer when evaluating the conditions,
+	// even across API server replicas. The name must be stable over time.
+	// This name must be unique within a given union authorizer, not necessarily globally.
+	// +required
+	AuthorizerName string
+
+	// Decision carries the inner decision returned from the authorizer.
+	// +required
+	Decision ConditionsAwareDecision
+}
+
+// UnconditionalDecision represents the data associated with an unconditional decision.
+type UnconditionalDecision struct {
+	// Reason is optional. It indicates why a request was allowed or denied.
+	// +optional
+	Reason string
+
+	// EvaluationError is an indication that some error occurred during the authorization check.
+	// It is entirely possible to get an error and be able to continue determine authorization status in spite of it.
+	// For instance, RBAC can be missing a role, but enough roles are still present and bound to reason about the request.
+	// +optional
+	EvaluationError string
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AuthorizationConditionsReview describes a request to evaluate authorization conditions.
+type AuthorizationConditionsReview struct {
+	metav1.TypeMeta
+	// metadata is the standard list metadata.
+	// In AuthorizationConditionsReview, it must be an empty struct.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+	// +optional
+	metav1.ObjectMeta
+
+	// Request describes the attributes for the authorization conditions request.
+	// +optional
+	Request *AuthorizationConditionsRequest
+	// Response describes the attributes for the authorization conditions response.
+	// +optional
+	Response *AuthorizationConditionsResponse
+}
+
+// AuthorizationConditionsRequest describes the authorization conditions request.
+type AuthorizationConditionsRequest struct {
+	// Decision contains the conditional decision the authorizer authored at authorization time.
+	// +required
+	Decision ConditionsAwareDecision
+
+	// AdmissionRequest may contain additional information for evaluating the conditions.
+	// +optional
+	AdmissionRequest *admission.AdmissionRequest
+}
+
+// AuthorizationConditionsResponse describes an authorization conditions response.
+type AuthorizationConditionsResponse struct {
+	// UID is an identifier for the individual request/response.
+	// This must be copied over from the corresponding AuthorizationConditionsRequest.
+	// It is possible that the same request content (except uid) is sent to the
+	// authorizer multiple times.
+	// +optional
+	UID types.UID
+
+	// Decision contains the authorizer's decision after seeing the data.
+	// +required
+	Decision ConditionsAwareDecision
 }
