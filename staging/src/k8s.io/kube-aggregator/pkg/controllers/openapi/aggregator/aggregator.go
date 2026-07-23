98ddb5ea27794f247125749e612e23f76b5ab18e
Jefftree <jeffrey.ying86@live.com>
2026-07-23 19:56:45 +0000

    Instrument aggregated OpenAPI v2 handler with apiserver_request_total

--- staging/src/k8s.io/kube-aggregator/pkg/controllers/openapi/aggregator/aggregator.go
+++ staging/src/k8s.io/kube-aggregator/pkg/controllers/openapi/aggregator/aggregator.go
@@ -26,6 +26,7 @@ import (
 
 	restful "github.com/emicklei/go-restful/v3"
 
+	"k8s.io/apiserver/pkg/endpoints/metrics"
 	"k8s.io/apiserver/pkg/server"
 	"k8s.io/klog/v2"
 	v1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
@@ -100,8 +101,26 @@ func buildAndRegisterSpecAggregatorForLocalServices(downloader *Downloader, aggr
 	}
 
 	s.openAPIVersionedService = handler.NewOpenAPIServiceLazy(s.buildMergeSpecLocked())
-	s.openAPIVersionedService.RegisterOpenAPIVersionedService("/openapi/v2", pathHandler)
+	s.openAPIVersionedService.RegisterOpenAPIVersionedService("/openapi/v2", instrumentedPathHandler{delegate: pathHandler})
 	return s
+}
+
+type instrumentedPathHandler struct {
+	delegate common.PathHandler
+}
+
+// Handle is called once to register the handler, not per request.
+func (i instrumentedPathHandler) Handle(path string, h http.Handler) {
+	i.delegate.Handle(path, metrics.InstrumentHandlerFunc("GET",
+		/* group = */ "",
+		/* version = */ "",
+		/* resource = */ "",
+		/* subresource = */ "openapi/v2",
+		/* scope = */ "",
+		/* component = */ "",
+		/* deprecated = */ false,
+		/* removedRelease = */ "",
+		h.ServeHTTP))
 }
 
 // BuildAndRegisterAggregator registered OpenAPI aggregator handler. This function is not thread safe as it only being called on startup.
