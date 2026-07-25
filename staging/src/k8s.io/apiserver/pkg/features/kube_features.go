4a6973ac180b8b0d5e4bac3756c2382f6c5f4036
Sean Sullivan <seans3@gmail.com>
2026-07-24 18:06:42 -0700

    DO NOT MERGE: enable OpenAPIV2BytesCache by default for CI

--- staging/src/k8s.io/apiserver/pkg/features/kube_features.go
+++ staging/src/k8s.io/apiserver/pkg/features/kube_features.go
@@ -438,7 +438,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 
 	OpenAPIV2BytesCache: {
-		{Version: version.MustParse("1.37"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.37"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	RemoteRequestHeaderUID: {
