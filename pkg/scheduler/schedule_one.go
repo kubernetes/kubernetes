3c20a289bc2a3d68562d85364defd69bf3b36710
brejman <brejman@google.com>
2026-07-29 09:08:50 +0000

    Add tie-breaker to non-extender node scores

--- pkg/scheduler/schedule_one.go
+++ pkg/scheduler/schedule_one.go
@@ -959,6 +959,7 @@ func prioritizeNodes(
 			result = append(result, fwk.NodePluginScores{
 				Name:       nodes[i].Node().Name,
 				TotalScore: 1,
+				Randomizer: rand.Int(),
 			})
 		}
 		return result, nil
@@ -1042,9 +1043,12 @@ func prioritizeNodes(
 			if score, ok := allNodeExtendersScores[nodes[i].Node().Name]; ok {
 				nodesScores[i].Scores = append(nodesScores[i].Scores, score.Scores...)
 				nodesScores[i].TotalScore += score.TotalScore
-				nodesScores[i].Randomizer = rand.Int()
 			}
 		}
+	}
+
+	for i := range nodesScores {
+		nodesScores[i].Randomizer = rand.Int()
 	}
 
 	if loggerVTen.Enabled() {
