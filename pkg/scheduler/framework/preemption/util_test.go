00fe48640f6979e2d72ccbaf2d9ace42c1392cf9
Dasmat13 <gptdasmat@gmail.com>
2026-07-29 16:48:57 +0530

    fix(test): update preemption unit test to call MoreImportantVictim directly

--- pkg/scheduler/framework/preemption/util_test.go
+++ pkg/scheduler/framework/preemption/util_test.go
@@ -704,9 +704,9 @@ func TestLessByStartTimeThenIdentity(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := lessByStartTimeThenIdentity(tt.vi1, tt.vi2)
+			got := MoreImportantVictim(tt.vi1, tt.vi2)
 			if got != tt.want {
-				t.Errorf("lessByStartTimeThenIdentity() = %v, want %v", got, tt.want)
+				t.Errorf("MoreImportantVictim() = %v, want %v", got, tt.want)
 			}
 		})
 	}
