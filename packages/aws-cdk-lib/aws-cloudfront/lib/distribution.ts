36471e3eba83a61ea02b6d42249a46448c4b33dd
syukawa-gh <maguro470@gmail.com>
2026-08-08 01:15:31 +0900

    docs(cloudfront): correct HttpVersion documentation to match AWS docs (#37468)
    Closes #37092
    
    The HttpVersion enum JSDoc incorrectly stated 'Maximum HTTP version to support'. Per AWS documentation, this property specifies which HTTP versions are enabled. Notably, setting HTTP3 enables HTTP 3 only (not HTTP 2).
    
    Exemption Request: Documentation-only change, no code behavior change.

--- packages/aws-cdk-lib/aws-cloudfront/lib/distribution.ts
+++ packages/aws-cdk-lib/aws-cloudfront/lib/distribution.ts
@@ -205,7 +205,7 @@ export interface DistributionProps {
   readonly geoRestriction?: GeoRestriction;
 
   /**
-   * Specify the maximum HTTP version that you want viewers to use to communicate with CloudFront.
+   * The HTTP version(s) to enable on the distribution.
    *
    * For viewers and CloudFront to use HTTP/2, viewers must support TLS 1.2 or later, and must support server name identification (SNI).
    *
@@ -901,7 +901,13 @@ export class Distribution extends Resource implements IDistribution {
   }
 }
 
-/** Maximum HTTP version to support */
+/**
+ * The HTTP version(s) to enable on the distribution.
+ *
+ * Note: Setting `HTTP3` enables HTTP 3 only (not HTTP 2). To support both HTTP 2 and HTTP 3, use `HTTP2_AND_3`.
+ *
+ * @see https://docs.aws.amazon.com/cloudfront/latest/APIReference/API_UpdateDistribution.html#cloudfront-UpdateDistribution-request-HttpVersion
+ */
 export enum HttpVersion {
   /** HTTP 1.1 */
   HTTP1_1 = 'http1.1',
@@ -909,7 +915,7 @@ export enum HttpVersion {
   HTTP2 = 'http2',
   /** HTTP 2 and HTTP 3 */
   HTTP2_AND_3 = 'http2and3',
-  /** HTTP 3 */
+  /** HTTP 3 only (does not include HTTP 2) */
   HTTP3 = 'http3',
 }
 
