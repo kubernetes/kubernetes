f2ca601f3c23f725c28eb5c7e92f3125c0c48d55
obarisk <obarisk@gmail.com>
2026-08-22 21:31:34 +0800

    metrics: graduate volume_operation_total_errors to beta

--- pkg/controller/volume/persistentvolume/metrics/metrics.go
+++ pkg/controller/volume/persistentvolume/metrics/metrics.go
@@ -147,7 +147,7 @@ var (
 		&metrics.CounterOpts{
 			Name:           "volume_operation_errors_total",
 			Help:           "Total volume operation errors",
-			StabilityLevel: metrics.ALPHA,
+			StabilityLevel: metrics.BETA,
 		},
 		[]string{"plugin_name", "operation_name"})
 
