80c7d444b6cda1a9bf6dfdd1860de23c376e4745
Taqi Chadi <chaditaqi3@gmail.com>
2026-09-14 22:24:06 +0100

    refactor(apimachinery): use NoUnset validation

--- staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/validation/zz_generated.validations.go
+++ staging/src/k8s.io/apimachinery/pkg/apis/meta/v1/validation/zz_generated.validations.go
@@ -301,11 +301,6 @@ func Validate_ObjectMeta(
 	ctx context.Context, op operation.Operation, fldPath *field.Path,
 	obj, oldObj *v1.ObjectMeta) (errs field.ErrorList) {
 
-	// custom validation
-	if e := ValidateCustom_ObjectMeta(ctx, op, fldPath, obj, oldObj).MarkAlpha(); len(e) != 0 {
-		errs = append(errs, e...)
-	}
-
 	// field v1.ObjectMeta.Name has no validation
 	// field v1.ObjectMeta.GenerateName has no validation
 	// field v1.ObjectMeta.Namespace has no validation
@@ -343,7 +338,38 @@ func Validate_ObjectMeta(
 		errs = append(errs, fn(fldPath.Child("uid"), &obj.UID, oldVal, oldObj != nil)...)
 	}
 
-	// field v1.ObjectMeta.ResourceVersion has no validation
+	{ // field v1.ObjectMeta.ResourceVersion
+		fn := func(
+			fldPath *field.Path,
+			obj, oldObj *string,
+			oldValueCorrelated bool) (errs field.ErrorList) {
+			// don't revalidate unchanged data
+			if oldValueCorrelated && op.Type == operation.Update {
+				if obj == oldObj || (obj != nil && oldObj != nil && *obj == *oldObj) {
+					return nil
+				}
+			}
+			// call field-attached validations
+			earlyReturn := false
+			if e := validate.OptionalValue(ctx, op, fldPath, obj, oldObj).MarkAlpha().MarkShortCircuit(); len(e) != 0 {
+				earlyReturn = true
+			}
+			if e := validate.UpdateValue(ctx, op, fldPath, obj, oldObj,
+				func(a string, b string) bool { return a == b }, validate.NoUnset).MarkAlpha().MarkShortCircuit(); len(e) != 0 {
+				errs = append(errs, e...)
+				earlyReturn = true
+			}
+			if earlyReturn {
+				return // do not proceed
+			}
+			return
+		}
+		oldVal := safe.Field(oldObj,
+			func(oldObj *v1.ObjectMeta) *string {
+				return &oldObj.ResourceVersion
+			})
+		errs = append(errs, fn(fldPath.Child("resourceVersion"), &obj.ResourceVersion, oldVal, oldObj != nil)...)
+	}
 
 	{ // field v1.ObjectMeta.Generation
 		fn := func(
