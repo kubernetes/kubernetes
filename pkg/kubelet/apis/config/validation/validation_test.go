17c3814002b97d21d4974450f9ff194df20232df
Ryan Phillips <rphillips@redhat.com>
2026-09-18 11:40:15 -0500

    kubelet: add check for mldsa and tls version

--- pkg/kubelet/apis/config/validation/validation_test.go
+++ pkg/kubelet/apis/config/validation/validation_test.go
@@ -942,6 +942,43 @@ func TestValidateKubeletConfiguration(t *testing.T) {
 				return conf
 			},
 			errMsg: `invalid configuration: serverCertificateKeyAlgorithm "BOGUS" is not a supported algorithm`,
+		}, {
+			name: "valid ML-DSA ServerCertificateKeyAlgorithm with TLSMinVersion VersionTLS13",
+			configure: func(conf *kubeletconfig.KubeletConfiguration) *kubeletconfig.KubeletConfiguration {
+				conf.ServerCertificateKeyAlgorithm = ptr.To(kubeletconfig.CertificateKeyAlgorithmMLDSA65)
+				conf.TLSMinVersion = "VersionTLS13"
+				return conf
+			},
+		}, {
+			name: "valid ML-DSA ServerCertificateKeyAlgorithm with TLSMinVersion unset",
+			configure: func(conf *kubeletconfig.KubeletConfiguration) *kubeletconfig.KubeletConfiguration {
+				conf.ServerCertificateKeyAlgorithm = ptr.To(kubeletconfig.CertificateKeyAlgorithmMLDSA65)
+				conf.TLSMinVersion = ""
+				return conf
+			},
+		}, {
+			name: "valid ECDSA ServerCertificateKeyAlgorithm with TLSMinVersion VersionTLS12",
+			configure: func(conf *kubeletconfig.KubeletConfiguration) *kubeletconfig.KubeletConfiguration {
+				conf.ServerCertificateKeyAlgorithm = ptr.To(kubeletconfig.CertificateKeyAlgorithmECDSAP256)
+				conf.TLSMinVersion = "VersionTLS12"
+				return conf
+			},
+		}, {
+			name: "invalid ML-DSA-44 ServerCertificateKeyAlgorithm with TLSMinVersion VersionTLS12",
+			configure: func(conf *kubeletconfig.KubeletConfiguration) *kubeletconfig.KubeletConfiguration {
+				conf.ServerCertificateKeyAlgorithm = ptr.To(kubeletconfig.CertificateKeyAlgorithmMLDSA44)
+				conf.TLSMinVersion = "VersionTLS12"
+				return conf
+			},
+			errMsg: `invalid configuration: tlsMinVersion "VersionTLS12" is incompatible with serverCertificateKeyAlgorithm "ML-DSA-44", ML-DSA requires VersionTLS13`,
+		}, {
+			name: "invalid ML-DSA-87 ServerCertificateKeyAlgorithm with TLSMinVersion VersionTLS10",
+			configure: func(conf *kubeletconfig.KubeletConfiguration) *kubeletconfig.KubeletConfiguration {
+				conf.ServerCertificateKeyAlgorithm = ptr.To(kubeletconfig.CertificateKeyAlgorithmMLDSA87)
+				conf.TLSMinVersion = "VersionTLS10"
+				return conf
+			},
+			errMsg: `invalid configuration: tlsMinVersion "VersionTLS10" is incompatible with serverCertificateKeyAlgorithm "ML-DSA-87", ML-DSA requires VersionTLS13`,
 		},
 	}
 
